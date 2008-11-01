b2cd5b79618921a8e3500e285a93093d70cc0e07
Mike Lockwood <lockwood@android.com>
2008-11-01 15:35:28 -0400

    logwrapper: switch from root to user/group "log" after forking child.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- logwrapper/logwrapper.c
+++ logwrapper/logwrapper.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35c6781015505cd9298ebc7aec96e0e25b28ac04927869a5239a3e022f9f9bf8
-size 3202
+oid sha256:da9f29cc66d73ba19f5581940204de7c87f0a334567b4d945f34d3366eba27c6
+size 3453
