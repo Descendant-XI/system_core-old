7be4c9dd2786807a597d5c098bfebfbca819725c
Ivan Djelic <ivan.djelic@parrot.com>
2008-11-23 22:26:39 +0100

    show error message when exec fails during service startup

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90dc397ab514470c127df6bf0817ea43e0d61de218f387e81179f4d7ea9955a4
-size 23271
+oid sha256:aa7cf809af7153c25f5de27b4d7b2328a24151db7ee52ad852282de1aa7be3f5
+size 23370
