0225f573bc52b131b2e94280cf3db741049bf443
Xiaopeng Yang <xiaopeng.yang@windriver.com>
2008-11-25 16:20:07 -0800

    Create sound device nodes in /dev/snd/.

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d405eacaac1f31fa0a1d776e30a967c10c03cbd03c4a7a8452ef164e2d86b3
-size 17463
+oid sha256:7d68721d7ea2325b01d9af01adc4dd09bd0899b8e64b3250c51cceccfab458b5
+size 17660
