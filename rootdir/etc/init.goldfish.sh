db95d65385b7176ba6fe0e70b045487c6e0feb20
Simon Braunschmidt <simon.braunschmidt@gmail.com>
2008-11-28 01:24:13 +0100

    BUGFIX - ro.kernel.android.{qemud | ril} and ro.android.noril
    This fixes both a typo and the fact that sh seems not to have
    test or [.
    (see also sh/builtins.def: ##testcmd commented out)
    
    The handling of these properties was broken, having the effect
    that the Modem in ... internal/telephony/test/Simulated*.java
    was never run, even if it would have been the users/developers
    intention.
    
    See also issue #1380
    http://code.google.com/p/android/issues/detail?id=1380
    
    Signed-off-by: Simon.Braunschmidt@gmail.com>

--- rootdir/etc/init.goldfish.sh
+++ rootdir/etc/init.goldfish.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bf99258b397dfee57c464a6241e296e4e772525a3749bf5ae504c59492f0e27
-size 1176
+oid sha256:a98a5af77c746b1b7bf389ee6b24ffdce1c3ae514dd118dfeab34b1093435389
+size 1200
