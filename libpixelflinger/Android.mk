7e2112752ad8e159dbf144949f5e430a47f68c6c
Brian Swetland <swetland@google.com>
2008-12-23 22:29:27 -0800

    fix TINY ANDROID build
    A better fix would be to banish this qemu tracing stuff from libhardware
    and also banish it from non-emulator builds, but this at least gets the
    minimal build building again.

--- libpixelflinger/Android.mk
+++ libpixelflinger/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac1a2f98ab621350f2f070a137b6a4b726463e97b74fad4c25ccbf90e352e980
-size 2011
+oid sha256:fda8d749b99c3a5e7ae1be568314592bf63c811994fb202e70b044a7421c3be3
+size 2249
