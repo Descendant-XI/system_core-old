1515848e8280f6ac9abf39194df00b40b70572a9
Jay Freeman (saurik) <saurik@saurik.com>
2009-01-13 21:55:35 +0000

    Added a move flag for init's mount command that maps to MS_MOVE.
    Change-Id: I7bc1a8ac5f73a34c101247f984cbf9ff29e02e7f

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50a6772085d39a3759ae296d11d9ee0a7d98fe6d890b535f258c3c7b5755a876
-size 11943
+oid sha256:cc112dbeb815847c7b68066b007e4a566f2153ee779c59e257aae893fb0a0b61
+size 11974
