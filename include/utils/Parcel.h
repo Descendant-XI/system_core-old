9e1ec93ff320e4fffcd1affe4ba63b88362971e8
The Android Open Source Project <initial-contribution@android.com>
2009-01-20 14:03:58 -0800

    auto import from //branches/cupcake/...@127101

--- include/utils/Parcel.h
+++ include/utils/Parcel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ba41bd5ba40eee4e54b6aa519fdff9a148740aca62c06d68c3b108e40c2cc6
-size 7614
+oid sha256:7511af8a67def8ca3369902deee7db3b5251198c520e62db0a44eb5995759acb
+size 8110
--- libs/utils/Parcel.cpp
+++ libs/utils/Parcel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c956d2c5dee3d73e1c4a2ba5f7adf96777cb65d2f46b5ef36cca799fb046f7bb
-size 37393
+oid sha256:1c6b3e16a43ca5af65b8da732fec96235c22d6548b75c35fc0165fff4671ae7a
+size 38962
