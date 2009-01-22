f088ee4d22e1cb3f9a1c38640c67bbf037ffb893
The Android Open Source Project <initial-contribution@android.com>
2009-01-22 00:13:45 -0800

    auto import from //branches/cupcake/...@127436

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88de460c7a67f5752f1bd11e7c66e128be102073e549e281af2f881c548adf50
-size 17627
+oid sha256:a709179569311aa2fe2776d5bb9a14f9e78bd14399dfd4446b2d49f4882034b1
+size 17589
