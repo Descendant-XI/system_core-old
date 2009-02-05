14cdd218eaec7e4788c177aeb2f73edafa844c72
ralf <ralf@android.com>
2009-02-05 11:58:27 -0800

    AAPT needs a buffer larger than 1 MB on the host to decode resources.arsc from SDK/android.jar. This leaves the asset unzip buffer to 1 MB on the device and 2 MB on the host.

--- include/utils/Asset.h
+++ include/utils/Asset.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db548d93ac78d4bdea302a86939b24c95a3063c1d40b6d33f12cb832aecfb40
-size 9700
+oid sha256:97d80c641caa59913b95c0ae08026de22f11b53fc5b4283a7a4b7e144b03d455
+size 9782
