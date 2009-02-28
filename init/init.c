d958450ee79d139e3bef31815df69354b2b7488f
Sean McNeil <sean.mcneil@windriver.com>
2009-02-28 14:41:33 +0700

    Use complete hardware name without spaces.
    Many targets use generic first portions like 'OMAP'
    
    Change-Id: I00ba4e64cffc48353c29e409c3c19238024fbd8c

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2c3b4e27fdc7fd18978dcaab8dd4d08e8a87dd4d0e5ac46a796681b863e21d6
-size 27877
+oid sha256:67c52f2d7a5c760751637cc367ebd9af6d61b0b018abc499af47b0aa18d5bccd
+size 27913
