76bb7d0a4c0d8809124cc478b2f77c99fb0126ae
The Android Open Source Project <initial-contribution@android.com>
2009-03-09 11:52:15 -0700

    auto import from //branches/cupcake/...@137197

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f8a9d9a43f736988bc5ba124b3f66173ce44f4a2bbfc373606c1072a0181a05
-size 472
+oid sha256:4cfe24b1fe692a324c285ea7d5917376d134032a47658bc54a806887a50d6db0
+size 432
