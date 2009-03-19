81554d3fe2971c43b1862a83d0c73ac30b082b4a
The Android Open Source Project <initial-contribution@android.com>
2009-03-18 17:39:46 -0700

    auto import from //branches/cupcake_rel/...@140373

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca8a984a4ee14533eb0ddefc70bac97b8be1486343548bfd1bebf473c4a86b08
+oid sha256:9337102900619f206c5b5f55655a5a1ec1cac44ef896a87aa6af1c59f766db8b
 size 59448
