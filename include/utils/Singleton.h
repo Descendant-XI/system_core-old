2a87e2ff85bd70e84d13d154526a241990e1293e
Mathias Agopian <mathias@google.com>
2009-04-15 18:34:24 -0700

    fix a rookie mistake causing Singleton<> to be a "multiton". Also improve the BufferMapper's debugging, but turn it off.
    Squashed commit of the following:
    
    commit 04e9cae7f806bd65f2cfe35c011b47a36773bbe5
    Author: Mathias Agopian <mathias@google.com>
    Date:   Wed Apr 15 18:30:30 2009 -0700
    
        fix and improve BufferMapper's tracking of mapped buffers.
    
    commit 1a8deaed15811092b2349cc3c40cafb5f722046c
    Author: Mathias Agopian <mathias@google.com>
    Date:   Wed Apr 15 00:52:02 2009 -0700
    
        fix some bugs with the Singleton<> class. untested.
    
    commit ed01cc06ad70cf640ce1258f01189cb1a96fd3a8
    Author: Mathias Agopian <mathias@google.com>
    Date:   Tue Apr 14 19:29:25 2009 -0700
    
        some work to debug the Singleton<> template.

--- include/utils/Singleton.h
+++ include/utils/Singleton.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff0f5cf6797ea0843ca74f6f58926e6690f2d9c37a9b041e109bd4de5cdfd145
-size 1613
+oid sha256:792a8286afa454d2b09934cb27e33b00dbf24858ef40d8f730b4c973bf3902d6
+size 1501
