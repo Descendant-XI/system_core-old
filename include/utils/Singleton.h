0d8fce852327509f100a5c2e3eab9d73dd5b66d0
Mathias Agopian <mathias@google.com>
2009-04-17 14:15:18 -0700

    Squashed commit of the following:
    commit e5c24638f98162c3b75b4c67a16b510d38e31341
    Author: Mathias Agopian <mathias@google.com>
    Date:   Fri Apr 17 14:09:03 2009 -0700
    
        oops forgot this file.
    
    commit 282642632d0cb12882eecf42e0fdfb2343275de1
    Author: Mathias Agopian <mathias@google.com>
    Date:   Fri Apr 17 14:07:32 2009 -0700
    
        use a helper macro for creating Singleton<>'s static attributes instances.

--- include/utils/Singleton.h
+++ include/utils/Singleton.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:792a8286afa454d2b09934cb27e33b00dbf24858ef40d8f730b4c973bf3902d6
-size 1501
+oid sha256:b2b56e5c930d88b35655aea23cb7a031b6d53f5b36d8e190ed59f3f9a3159cd9
+size 1933
