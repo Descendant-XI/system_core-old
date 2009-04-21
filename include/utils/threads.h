16292537d0a720f60965d02600f4c312141d6902
San Mehat <san@google.com>
2009-04-21 14:06:36 -0700

    Add support for changing a threads scheduler group. Three groups are available (default, background non interactive, foreground boost). Setting a thread priority to PRIORITY_BACKGROUND will transparently change groups to background
    Signed-off-by: San Mehat <san@google.com>

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:889776323a08e3d707177ac979cf34c01bebc28b86edb7a4765f4e424d7631ce
-size 10743
+oid sha256:9fc9ada09e77d3c42a8f7b370b3da3ee92bd41c838ba65f00a1c582e7cd13611
+size 10940
