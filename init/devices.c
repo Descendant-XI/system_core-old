d9a6072350131ff269aa8e002e1250d509d4698a
Iliyan Malchev <malchev@google.com>
2009-05-01 10:25:05 -0700

    add support and proper permissions for /dev/msm_camera/
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da32f12c29ef4c708b96534106ee54fb3e1f320c32e55c985be1a907da315b40
-size 17808
+oid sha256:5111de59b75a21815e163f1949de02686f231f01ea73c31bdf40afd198baca1e
+size 18021
