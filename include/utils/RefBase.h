f0cbcc567df8139314122abe4e200cf8a41626de
Mathias Agopian <mathias@google.com>
2009-05-04 14:17:04 -0700

    update surfaceflinger, libui and libagl to the new gralloc api
    - Currently the lock/unlock path is naive and is done for each drawing operation (glDrawElements and glDrawArrays). this should be improved eventually.
    - factor all the lock/unlock code in SurfaceBuffer.
    - fixed "showupdate" so it works even when we don't have preserving eglSwapBuffers().
    - improved the situation with the dirty-region and fixed a problem that caused GL apps to not update.
    - make use of LightRefBase() where needed, instead of duplicating its implementation
    - add LightRefBase::getStrongCount()
    - renamed EGLNativeWindowSurface.cpp to FramebufferNativeWindow.cpp
    
    - disabled copybits test, since it clashes with the new gralloc api
    
    - Camera/Video will be fixed later when we rework the overlay apis

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2390a13590d9fde41e98bf0ba24a85338b7200c99968dd9f9665624d8bf96c83
-size 13775
+oid sha256:6a411dfcb2c4ac61972fbf9e67d467d9d1fdffec4139621f7e043f623ebc13d4
+size 13902
