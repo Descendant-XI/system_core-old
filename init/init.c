ba2367dc065a0e9bea96030f61650b59c5458bac
Dima Zavin <dima@android.com>
2009-05-05 18:33:07 -0700

    init: Fix segfault when log_write() was missing an arg to format.
    Also, flag log_write() as a printf-like beast to gcc.
    
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c7d8b0dd9db83d61c9480ba405954dfbb5f2fffadf97832c82a23a5464fb2d2
-size 25854
+oid sha256:7ff314704d9fb66851d393305420aea3dcde18789b14ea99fd9a86866d30c7d7
+size 25860
--- init/init.h
+++ init/init.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20354c0327c915e9c39e884005eb8da47786561f4e5bff1155ae8b93d29e0036
-size 5069
+oid sha256:11d087cd7e76ceeaad3570fdf1b0c0bad62179dd89d324616e4e2ce864c50141
+size 5112
