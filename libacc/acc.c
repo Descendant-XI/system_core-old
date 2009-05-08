5652b2ccc1740fae866ed15ed3d8e35bfb71c29c
Jack Palevich <jackpal@google.com>
2009-05-08 14:54:15 -0700

    Continue deobfuscation.
    Add a license.
    
    Indent the code.
    Add includes for external functions.
    Improve function prototypes.
    Start adding the correct type to things that are pointers.
    Start fixing compiler warnings.
    
    Instead of directly executing the compiled code (which only works on x86 Linux),
    write the internal compiler state to stdout. This makes it easier to test whether
    or not our refactoring is breaking anything.
    
    Return a zero status on success.
    
    Add error checking for a missing input file.

--- libacc/acc.c
+++ libacc/acc.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fab463df764359dc0161d69aea3ad323594ef9954444636c2101e410343b42ce
-size 3998
+oid sha256:caba4a087a9b4e854c34d366a339661ed792489d70272e5945b73e5460ccdecc
+size 9576
