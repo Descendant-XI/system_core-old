c0ac66bf23337f46b613ff6607c1afd2ae52dfc3
Jack Palevich <jackpal@google.com>
2009-05-08 15:44:22 -0700

    Make our global variables static.

--- libacc/acc.c
+++ libacc/acc.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afd0e6ac56515ad4642248afe04ffd46369b00edc159caa0cac2d410631055b6
-size 9641
+oid sha256:a3eac3b404ea71541ddc647b061530e4e942505f945fff09c69401aa0dba6095
+size 10037
