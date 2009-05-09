82460cc113fedae8293bd595d18daef40af4d3b7
Dima Zavin <dima@android.com>
2009-05-08 18:25:58 -0700

    adb: osx: Match devices based on interface subclass and protocol
    Enumerating all vendor+product id combinations is not practical.
    This modifies the matching algorithm to use the adb interface
    subclass/protocol instead (0x42/0x1).
    
    Signed-off-by: Dima Zavin <dima@android.com>

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:573c3b3f8eec543ad80209f840863cd814a84a550ea9daa1d882be042c67c68e
-size 11670
+oid sha256:9efcf24973ab511122662d9732c7f02dd4db03b691b44766619e72204b6c19d8
+size 11352
