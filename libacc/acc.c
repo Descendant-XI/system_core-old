d36b6fac4785d6c14479149754eba379ca370653
Jack Palevich <jackpal@google.com>
2009-05-08 18:33:45 -0700

    Converted code constants from decimal to hexidecimal.

--- libacc/acc.c
+++ libacc/acc.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e89a807f26f84b9ea9c03a7fbb5ba254e50c766fc0e3b8b7d1429994d5ac8c26
-size 12025
+oid sha256:f54dc0e7d4a2b271f7ad2d74692cfd96bc6064d901f05ddb26e5c886af9c626f
+size 13894
