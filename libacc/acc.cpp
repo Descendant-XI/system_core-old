3bd6f564129d61eb5869f017d1c28c7fd8b7775c
Jack Palevich <jackpal@google.com>
2009-05-11 11:54:30 -0700

    Added command-line option "-t" to allow run-time switching between running and dumping.
    Fixed some C++ warnings reported by g++ .
    Verified that the compiler actually works when run on 32-bit Linux.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edf9d944855c3e85c8d0019ebee862078a79e439c17d3df75668d9b1502c3376
-size 15406
+oid sha256:58da3c200d808ce4657f94c51bb5f812e7a4d0105fcbebd12abc1e657da1fe2a
+size 17214
