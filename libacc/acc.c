b38247ce0dcd5bb88c210b30082cc0a2fc036365
Jack Palevich <jackpal@google.com>
2009-05-10 19:59:24 -0700

    converted to C++
    Base address of constant table changed, so had to update the "-orig" files.

--- libacc/acc.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e5d47d8ef6e707ad422ae97d6ce5b5b962a0b71af94ab53f3a1cc3c28e3fb0cb
-size 15056
---
+++ libacc/acc.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:edf9d944855c3e85c8d0019ebee862078a79e439c17d3df75668d9b1502c3376
+size 15406
