70d56a6d9884766bfba0db2a8002c47cf5c7ed88
San Mehat <san@google.com>
2009-05-12 14:36:32 -0700

    nexus: Use constants for errorcodes
    Signed-off-by: San Mehat <san@google.com>

---
+++ nexus/ErrorCode.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7d73aa7d19219620ace361a4c87ce03475e2bd29fc51af104dcdafd394775c6
+size 1361
--- nexus/Supplicant.cpp
+++ nexus/Supplicant.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75e994dc00a761423bfad1c53b59ab3ecf3b30c4e79751f88a5a12ead7eab1e6
-size 12341
+oid sha256:8b9ea3ac6bdf1f08efdbdeacce1519a18e7f56f9ec3a9dd74a557c0739fec1e4
+size 12368
