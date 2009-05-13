0a2e4fc70850af40f252c5f8a227185fa82034e2
San Mehat <san@google.com>
2009-05-13 08:58:43 -0700

    libsysutils: General clean up + disable build in simulator builds
    Signed-off-by: San Mehat <san@google.com>

--- libsysutils/src/FrameworkListener.cpp
+++ libsysutils/src/FrameworkListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5474d742a15dff997a38aee009f84dadf04d0e17f882c475ebd498c8bb7f4967
-size 2408
+oid sha256:af1459c0317fcf9fb81d4182e17981afbd1d2383d35f1ddaf3843323b4b19b27
+size 2373
--- libsysutils/src/SocketListener.cpp
+++ libsysutils/src/SocketListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ec87f313f2d91980053d8e670b19e6055589ff01834b7cf003c78bdc7dd4c07
-size 5987
+oid sha256:631c26bb78b33dfd71f242a21a4bfd0803ba6371b0b7a20f5b08d685b401080e
+size 5810
