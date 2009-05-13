0ee464d42416c0213467d8e4be34671754fad5ac
Joe Onorato <joeo@android.com>
2009-05-13 18:57:29 -0400

    Get the backup calling through to the file backup helper.
    This includes some cleanup to make the parameters match
    between BackupService.onBackup and FileBackupHelper.performBackup.

--- include/utils/backup_helpers.h
+++ include/utils/backup_helpers.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f5f3ecae2ba606cc992769f6c8761cd1073ba6de4ecd27a01469190dfb5a98c
+oid sha256:c8bd98694b481cdf4b1146e63180d60ae6042ceafc97d82b310551db06332ee9
 size 405
--- libs/utils/file_backup_helper.cpp
+++ libs/utils/file_backup_helper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8be133ad9235910d0fabce0cc12b0bbbef6213e92f728b860fd361b0aadc812
-size 18840
+oid sha256:d288021da119e7f91f2eb66fbe4926b80897106f931457d4cfaee661a07a6f28
+size 18889
