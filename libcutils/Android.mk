2a6a6a19b0827fec5a6eed3abb941b89e447486e
Nick Pelly <npelly@google.com>
2009-05-19 19:22:42 -0700

    Helper to perform abortable blocking operations on a socket:    asocket_connect()    asocket_accept()    asocket_read()    asocket_write()  These calls are similar to the regular syscalls, but can be aborted with:    asocket_abort()
    Calling close() on a regular POSIX socket does not abort blocked syscalls on
    that socket in other threads.
    
    After calling asocket_abort() the socket cannot be reused.
    
    Call asocket_destory() *after* all threads have finished with the socket to
    finish closing the socket and free the asocket structure.
    
    The helper is implemented by setting the socket non-blocking to initiate
    syscalls connect(), accept(), read(), write(), then using a blocking poll()
    on both the primary socket and a local pipe. This makes the poll() abortable
    by writing a byte to the local pipe in asocket_abort().
    
    asocket_create() sets the fd to non-blocking mode. It must not be changed to
    blocking mode.
    
    Using asocket will triple the number of file descriptors required per
    socket, due to the local pipe. It may be possible to use a global pipe per
    process rather than per socket, but we have not been able to come up with a
    race-free implementation yet.
    
    All functions except asocket_init() and asocket_destroy() are thread safe.

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f53a0670f51ddae47cac8bf047d4abff22d68f5663adbe4f77b6aacab758262
-size 2906
+oid sha256:dd64503f5301fe65c30d3f06a906b394f709a6d94ec16b7e16ef7a5c820a6fbe
+size 2924
---
+++ libcutils/abort_socket.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5221cd15f966092a51d07dc64d8ec3a7fe9f34d64d32bf9cc8af59150cbd6f77
+size 7258
