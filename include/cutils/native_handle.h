819d747bb9db7d4bafeeacac8d2ba6ffbca8620f
Mathias Agopian <mathias@google.com>
2009-05-20 14:16:34 -0700

    move native_handle stuff from master_gl

--- include/cutils/native_handle.h
+++ include/cutils/native_handle.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:548004bc838c2226f5052d139a8de3cc561f62c829ef7b1b1ee4567d7f576aba
-size 972
+oid sha256:dc233dd83ad5c88b14bf5e51c474e3e39357bdc66df06c3971714259df2a134a
+size 1931
--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8ed906236b601c3f48ef8fa58847f18d9e71cba1758bdb26f5e7051941049bc
-size 2886
+oid sha256:fe201113f9bfbefd5815863968734791cb4a58fada6fbecbae5054b01ead4329
+size 2912
