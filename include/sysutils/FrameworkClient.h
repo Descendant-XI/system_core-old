8436eb9dffba782543e98175f25db592d9b948b8
San Mehat <san@google.com>
2009-05-20 15:27:14 -0700

    libsysutils: const correctness fixes + remove some debugging
    Signed-off-by: San Mehat <san@google.com>

--- include/sysutils/FrameworkClient.h
+++ include/sysutils/FrameworkClient.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f890f7ac19409f787e69d99cb25f8e2fce432467b6e86d5879829367ea0d449a
-size 445
+oid sha256:34f4256ccbe1afe8073b45e0df92c3c2560b32006940b6b5ac518da921a9b84d
+size 463
--- include/sysutils/SocketClient.h
+++ include/sysutils/SocketClient.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edab4e32c39079a113e62a646da3c6985c5aa23857159f87b222e78b9b5d6d2e
-size 478
+oid sha256:e78af3903b93dc34a5e536f48ec79998b7e6445869ae1935847b2eac34006e94
+size 490
