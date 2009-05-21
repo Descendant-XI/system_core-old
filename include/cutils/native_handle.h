b7c90818c2a615e655fd6db856de31e1160f63dc
Mathias Agopian <mathias@google.com>
2009-05-21 13:08:39 -0700

    don't use cdefs.h as it breaks the windows build.

--- include/cutils/native_handle.h
+++ include/cutils/native_handle.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc233dd83ad5c88b14bf5e51c474e3e39357bdc66df06c3971714259df2a134a
-size 1931
+oid sha256:9c1cc96133f0512469126809ac42d873059842fe602f9f5fc60c46c0b7524bac
+size 1948
