60872a74dc01c88b5c97695d830951d4dc8f8ecf
Raphael <raphael@google.com>
2009-05-26 13:23:23 -0700

    Don't compile abort_socket.c for host targets. It is only intended to run on Linux targets.

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:422c9efee3bd6aafa54a4c9f3cfb4ec0e62861ba38b0493ec81c0c7ba27830e9
-size 3023
+oid sha256:15ea334ec08e058d22a1ef4f3427011743dd5576ad89014065b364a28f80135a
+size 3058
