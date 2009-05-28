ef7d8c1b139fe8ed58b533284b33fb98917d6a72
Mathias Agopian <mathias@google.com>
2009-05-28 12:31:31 -0700

    force explicit instantiation of Singleton<> objects

--- include/utils/Singleton.h
+++ include/utils/Singleton.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2b56e5c930d88b35655aea23cb7a031b6d53f5b36d8e190ed59f3f9a3159cd9
-size 1933
+oid sha256:aadebba44576d369935230457f9aa581574bffb8c0e366ac9beb367020d1ff4c
+size 1995
