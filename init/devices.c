1412d0dc219bd083b874f591738a666f475175f1
Masaki Sato <masaki.sato@motorola.com>
2009-05-29 17:18:01 -0500

    Change permissions on sound devices.

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a999871a5653f3e9be4b4fa5e53ff831f0021a48ac0b5dbfacdfd1b40583b068
-size 18257
+oid sha256:5d5825744a5db33d0645cbf4c2650f6e593fac5195f277d5da31335a6ed7e773
+size 18476
