a09a7207a1c9f136ea18fa141d4f8a655dc7d696
Mathias Agopian <mathias@google.com>
2009-05-31 18:50:09 -0700

    get rid of utils/executablepath.h, which now lives in the simulator

--- adb/get_my_path_darwin.c
+++ adb/get_my_path_darwin.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1055e28bb19ac09a69daa7bae390f6852ad01ed09e64805f4cd93cb5c330c89
-size 1070
+oid sha256:dfdf37afdf9cf73454ce89e93f418f85b4ce4f87046244a9c2ed818ad3d89823
+size 1036
