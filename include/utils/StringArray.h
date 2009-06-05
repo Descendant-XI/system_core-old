73e14ecc5d427d883edfffdcfad9f3d0ef987db6
Mathias Agopian <mathias@google.com>
2009-06-05 01:26:23 -0700

    rename string_array.h to StringArray.h and move the implementation from the header file to a new cpp file.
    StringArray is used in two places in framework/base and in the Sim. Ideally we should get rid of it and use Vector<String8> instead of creating new code.

---
+++ include/utils/StringArray.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c8d397f93111088d26ab9da3101d336091aead81d85b78369c24c7249293d28
+size 1965
--- libs/utils/Android.mk
+++ libs/utils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:224e7a828b6e3e891b45834e7536a355cd310cf4b8db9e9d7bf2724a0d7c83ba
-size 3118
+oid sha256:b8ba623493d18134b9bc26747aaf58855376d33aae0f08a074109108129eefe4
+size 3137
