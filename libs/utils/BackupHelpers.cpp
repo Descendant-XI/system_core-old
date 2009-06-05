d7542911b7182bf409049de5f7480c02c385c89a
Christopher Tate <ctate@google.com>
2009-06-04 17:01:06 -0700

    Fix back_up_files() error detection when opening/CRCing the file

--- libs/utils/BackupHelpers.cpp
+++ libs/utils/BackupHelpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5716073c52cd0484d5ff2c154535337dd5fc578d1b07b60dbea1f49c605d9bf9
-size 31117
+oid sha256:04ffdbc371a56c596bb4d9142715f6707d420a851d904b3fbe9f5ea8f46aae82
+size 31197
