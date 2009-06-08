d7a727da2d2938f35c7764c321add737a3300b5d
Kenny Root <kenny@the-b.org>
2009-06-08 14:40:30 -0500

    Fix typo in adb commandline help
    Android developers are not expected to have PDP-11s.

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03cc95319a2dd986b9cee4e9a28b771eca750900d80291df2ab6c6e9bd0813e3
+oid sha256:6d6513c3022ef3b66319aa7c334d1d43db8ca749fd276ea849a3e0a7b06676c7
 size 35388
