cd8e125eb6e9882fd61e35189a79a01111b4d62f
Jack Palevich <jackpal@google.com>
2009-06-09 22:53:47 +0000

    Make otcc code work in x64 based system with 32-bit chroot.
    Set execute permission on code before running it.
    Handle negative relative offsets for global variables.
    Add printfs to report the progress of nested compiles.
    Change way we detect whether we can run the host compiler
    or not. We used to check if we were running on a 32-bit
    Linux. Now we check if the executable is a 32-bit Linux
    executable.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4e4720c8615b3eb27b697dca2bccf97559088e3258707266af3fba5f67be75e
-size 70432
+oid sha256:caba2d7d531427c613ec162aaa8bad4d6b13bf5b1c5234a1aefb8512e77f9a32
+size 70894
