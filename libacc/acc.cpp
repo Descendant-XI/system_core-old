63b7abb8f7867c364b0fdf8efa1393caaef4848b
Jack Palevich <jackpal@google.com>
2009-06-12 11:53:07 -0700

    Make #define work again. (Had accidentally omitted the keyword.)

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:934e8089ae7aa9c50df375c82c5c5e85ee7ab3bed3a1601f3abcaecf5dc69c3f
-size 82949
+oid sha256:e8f70f19c88ea301060837cd8105062b1457d8cead2af8e53abdfbc1c69ee741
+size 83128
