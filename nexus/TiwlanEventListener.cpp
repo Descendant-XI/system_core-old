45f5d7ea4bc0b6084134afdf1b1e19c59abdc018
San Mehat <san@google.com>
2009-06-16 12:01:24 -0700

    nexus: Less verbosity in TiwlanEventListener for now
    Signed-off-by: San Mehat <san@google.com>

--- nexus/TiwlanEventListener.cpp
+++ nexus/TiwlanEventListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b33dfa6f2c07585343ea5901b31092f4d5b1f698f6e867ae9a3fa473c96b36fd
-size 1873
+oid sha256:074d36874a30abbcf6cb58f318296832d0f84fe30d9e295ae72cb16ca5d69ed0
+size 1877
