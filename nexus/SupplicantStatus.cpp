fe5cd7e8fb3af4c42965988137eaad97051e1032
San Mehat <san@google.com>
2009-06-16 12:01:50 -0700

    nexus: Fix SupplicantStatus decoding
    Signed-off-by: San Mehat <san@google.com>

--- nexus/SupplicantStatus.cpp
+++ nexus/SupplicantStatus.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65868526bbb7f629455d0ec63938b23eaea039fde47dfeb7698a0424914074c0
-size 2060
+oid sha256:9d8af62e0c1ddc39f281e09a292b9d214c2ab0c74ace20880e8defc1b1f9323b
+size 3093
