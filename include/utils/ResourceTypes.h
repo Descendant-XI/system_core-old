9483bec39be963417fcc04670d3614761ec0b3ad
Dianne Hackborn <hackbod@google.com>
2009-06-19 15:13:28 -0700

    Report densities in badging, debugging for nine patch bug.
    The aapt tool now reports all available densities like it already did
    for locales.  Also this includes more resource data output, which I
    was using to examine bug #1867049 (which at this point I am unable to
    reproduce).

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9438d3a27254c6c6629a49abab32495c50a39ce539b3e6d6787311f99e741cf
-size 62507
+oid sha256:e670c420ff90e1685ee39b2bcc0a93ed201b78e81de42a56465c0dd2892c6091
+size 62522
--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db6d9cb98cb114909a87f160db341d7982df5e3f830c98086dfb2c20c7346d6d
-size 143137
+oid sha256:26c32946b64fb81fb4c0716d0492444e1fdc00a77c5f0b20ac039ba7a8f29286
+size 147655
