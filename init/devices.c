c8c06af9ea4a828f314faa63ba696984c7b66abe
Chia-chi Yeh <chiachi@android.com>
2009-06-19 14:59:08 +0800

    Add device, property, directory, and file permissions for VPN.

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5111de59b75a21815e163f1949de02686f231f01ea73c31bdf40afd198baca1e
-size 18021
+oid sha256:e86cf5c8722368ed86f36d47f3c8e13c32487fa65de11e95f3bb161dc4ae4b2a
+size 18094
