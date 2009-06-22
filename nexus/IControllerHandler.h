f575c2a282bd27449e7a45868f46dbac8b7b626a
San Mehat <san@google.com>
2009-06-22 10:39:36 -0700

    nexus: Rename onInterfaceStarted -> onInterfaceConnected and add Dhcp start/stop control
    Signed-off-by: San Mehat <san@google.com>

--- nexus/IControllerHandler.h
+++ nexus/IControllerHandler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc73266f1d048ce8b51f9e0f2318dfec239d87cfa5c4616481261eba545dc5dc
-size 932
+oid sha256:2c8791d359791469a7b43011ce5dec8dcc342931bd9527e6880700d8c115b845
+size 938
