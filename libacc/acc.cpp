e5cfce7b9b3887c89b8a2fd6538ca13f827c734e
Jack Palevich <jackpal@google.com>
2009-07-02 14:46:19 -0700

    Implement our hard casts using our type system.
    Doesn't add any new capabilities, but we now generate error
    messages if you attempt casts we don't support,
    and we also found and fixed some bugs in declaration parsing.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b19495cc1da8a69719dc9ee8950b7db4c33e78d93f17cf187abb1ede6b565b
-size 97964
+oid sha256:68e9522217f056dde76142e06b655f9b5c4063e94bfe2a6c5a0745fcb6300754
+size 102349
