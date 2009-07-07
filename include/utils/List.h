c61d82ec0240f4ff64184958b800a141777014f0
Andy McFadden <fadden@android.com>
2009-07-07 10:01:12 -0700

    Fix sim build.
    Looks like older gcc (4.1.x) doesn't properly handle templated
    fanciness.  Apparently that's what we have on the build server.

--- include/utils/List.h
+++ include/utils/List.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb84a505cf6edfb0302f7b08b18849652fe72042635eb36e1543fcda18979f0b
-size 10202
+oid sha256:4b8c75af7e7136f4c7e7fe6e3495e570a0c0911d79185f8e3a8a6eb10c0825cd
+size 10250
