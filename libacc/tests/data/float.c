7d59e6aefc0ee801fd57896767d39b112ae1dd64
Jack Palevich <jackpal@google.com>
2009-07-08 16:48:41 -0700

    Implement global, local, and stack based float and double variables.

--- libacc/tests/data/float.c
+++ libacc/tests/data/float.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bea823d898b2705c2f75514dedd2d56cd56513742d2e53b37eb537fbd39dc664
-size 403
+oid sha256:e9e34c2db53219f8ad9315deff5b7e711e03e3b5db8390c1c7bf544149c464d4
+size 1026
