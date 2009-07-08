4d9f1e4d506335ee8159a3b8100163eb5c8d3860
Marco Nelissen <marcone@google.com>
2009-07-08 16:59:18 -0700

    Class with virtual methods should have virtual destructors too.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cffac9005033a08fad1444e1fd9a8416699748045305a8ef3f026a42aeca26a6
-size 123064
+oid sha256:da45e0df8db664837bc3d703f845f4207131f5e97c28d74907cfd125f9772ae0
+size 123126
