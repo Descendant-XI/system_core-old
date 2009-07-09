c2c68cc3ab7d62ae1e1be514dcaf3e68fe496a68
Jack Palevich <jackpal@google.com>
2009-07-09 13:54:54 -0700

    Add x86 floating point test.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0de63bab0bbf65fdfe33f91de6f26273dabb3f9be47b32d3db3b4ea0beabffe3
-size 4284
+oid sha256:304148496be5ee45b353e5612394fdcee49cd1cf17ecd0a80af3f80fdb3bedf1
+size 6789
