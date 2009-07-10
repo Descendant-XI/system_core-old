c4373d93cc33aa50543c52eac47232e0729f471e
Jack Palevich <jackpal@google.com>
2009-07-09 22:00:24 -0700

    Implement floating point for ARM.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:304148496be5ee45b353e5612394fdcee49cd1cf17ecd0a80af3f80fdb3bedf1
-size 6789
+oid sha256:6beabc06ffe7928791f137c2ee552c33686810401252f53693ef0f7275a7735e
+size 6973
