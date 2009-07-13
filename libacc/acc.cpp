54210863536103f430bbf2887a100ff70dc293c4
Jack Palevich <jackpal@google.com>
2009-07-13 16:56:28 -0700

    Implement support for "char" local and global variables.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aa30004f1dc3f31c04d407155592b07f4be1d67c25e141e7ac8f3098be360ab
-size 152380
+oid sha256:065fd4d8896763eed37770b48db941e42a3c04acf3f99801d128d913e30e8fbb
+size 155132
