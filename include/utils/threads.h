f5bf7aa79e7a84f100fc3405c29b75b7cc16010c
Mathias Agopian <mathias@google.com>
2009-07-13 21:59:37 -0700

    add a ctor to Mutex to specify the type, which can be shared. This is used by sf and af an soon will allow some optimization in the kernel for non shared mutexes

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be7dec74e448b6edb7e0bacb4db19c8be9815914611ee5b30ee9996fc697d49e
-size 13066
+oid sha256:c1fa6d825e7f0430a9396d53010965d46930dcc07d79d2474bb858b544a57882
+size 13554
