069826b58c9dc4754342142cf0c23a2420f988cb
Jack Palevich <jackpal@google.com>
2009-07-14 19:39:36 -0700

    Add test for passing floats and doubles as ints, floats, and doubles.

--- libacc/tests/data/flops.c
+++ libacc/tests/data/flops.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef54580f445606b88d6399dd693cbdf9a1e065aa858937617e9f06a80a636808
-size 4297
+oid sha256:ac92b42fd6bbad99fb331ad2e6b70dda067bcfff4c8b8ee3fad69dda2773d089
+size 5035
--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b127170b5195f502c554d35dd5d542a41e5cc1f44f0ddbac64dba19eea89eecb
-size 8783
+oid sha256:3279400386db07e62a6c813dc20a7cd736d09b2dfe77a04557db88f7dd372803
+size 9044
