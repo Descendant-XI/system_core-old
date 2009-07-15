a59959329e0069bf4c57cc84881a32d1f1d4babc
Jack Palevich <jackpal@google.com>
2009-07-15 16:16:37 -0700

    Improve numerical constant parsing.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72e61a6faf18f2dbfef8b42afcfa805a1c3046ddf3296f491a7b9fbea2325bd0
-size 161118
+oid sha256:1c6c26ab8f3d4409f23887dc106cbbd68e59d2f189824e5a4caf9de7dc194b35
+size 162214
