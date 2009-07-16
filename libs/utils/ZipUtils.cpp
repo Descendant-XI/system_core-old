5c5e11650181094ea410f8e5eebab2fceb9a45f7
Doug Zongker <dougz@android.com>
2009-07-16 11:27:13 -0700

    fix FILE* version of ZipUtils::inflateToBuffer
    This can't have ever worked; a successful fread() was returning 1
    instead of getSize.

--- libs/utils/ZipUtils.cpp
+++ libs/utils/ZipUtils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5eced48daab276286249687c6c0b38b5a53493d50f46a957ea50bd8d32ea3b
-size 9653
+oid sha256:31d68426019a534b2b96c4e1e32d000ef51d61e00f7865a44c06b59bf26873bd
+size 9652
