898ae69c4b4310867a4096d18466acf0318e59a2
San Mehat <san@google.com>
2009-07-16 07:25:57 -0700

    vold: Allow a damaged volume to be re-evaluated for mounting
    Signed-off-by: San Mehat <san@google.com>

--- vold/volmgr.c
+++ vold/volmgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a65e22d2ffa2159c1c1fe19b6052bb98889e85dce237707bbdaf467e0f7ecfe4
-size 35455
+oid sha256:241f31b721c46f48a2ea1b8732232ff428c65a0738b2b91c79c54db5858356c7
+size 35413
