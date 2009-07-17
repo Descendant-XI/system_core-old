eeadefd76749932c446125309f5c7d49f31bfced
Jack Palevich <jackpal@google.com>
2009-07-16 19:05:07 -0700

    Report error (rather than crashing) when a declaration name is missing.
    Repo case:
    
    void main()
    {
       int );
    }

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee56a937cffc3999d17d31af897e2e86e4544c6000cf9c3023bccdadf05ba4f1
-size 163651
+oid sha256:a46388dca69ad8c742b77c3a967bf7a8fa58bedd6e1cffa858403a520ac71a77
+size 163964
