40e355f337f6b681906ffea2bd9531e732a1ec7a
Joe Onorato <joeo@android.com>
2009-07-28 18:24:51 -0700

    Only restore the bits for wallpapers that aren't built in.

--- libs/utils/BackupData.cpp
+++ libs/utils/BackupData.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd4119db97be0a40e87ffb0d9f84acd620367c95738be71458a5248d3033857a
-size 8387
+oid sha256:d414abee4e75937c6508e262eb8e416b8166642aeeedfc653300b176eeaca20f
+size 8502
