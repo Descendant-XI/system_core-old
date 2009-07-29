4974ca839f27c000c0a08015d1cf8ba46182a3bc
Jack Palevich <jackpal@google.com>
2009-07-29 16:22:26 -0700

    Load function symbols using lea syntax.
    Use a common code path for ordinary, forward, and indirect calls.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4bbbe10bd2be7a4a10434be3c8d3b601f501c647b0721c60a1fc2329055917e
-size 9541
+oid sha256:1da28ed0af5f7a02bbbc9b5c7373b6fe127e473ed4f80a5c72227b5dd10700b5
+size 9551
