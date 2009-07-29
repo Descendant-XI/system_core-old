17abcbc7c150926218f0890328f8596826d5fb1a
Joe Onorato <joeo@android.com>
2009-07-28 18:23:05 -0700

    Fix the IOException in wallpaper restore -- the padding isn't required at the end.

--- libs/utils/BackupData.cpp
+++ libs/utils/BackupData.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6878b0e4aeeca8197b0b640a1e8b79797701f0c39caf24a3d86574b67a4ee3b9
-size 8129
+oid sha256:cd4119db97be0a40e87ffb0d9f84acd620367c95738be71458a5248d3033857a
+size 8387
