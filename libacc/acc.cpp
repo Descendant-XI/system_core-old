ece8b8155e4c4ac1aaf10ec51ea33c020038e665
Jack Palevich <jackpal@google.com>
2009-07-30 16:19:43 -0700

    Fix bad ARM code generation for '||' and '&&' operators.
    Add tests of '&', '&&', '|' and '||' operators.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:225aa6fd2bd159d91151c8035463072b7ba9bf1e157a06d1f48016a3b08f1c48
-size 161494
+oid sha256:897cc9fc1b655d236aa47c8e4f27eb3d5bd0c297068f7c14850b7cba569c08fd
+size 161490
--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da28ed0af5f7a02bbbc9b5c7373b6fe127e473ed4f80a5c72227b5dd10700b5
-size 9551
+oid sha256:2e57bf519c3cbb22d0fb17516e613ca4f5df3aad99d7a733138dd98eee475441
+size 9702
