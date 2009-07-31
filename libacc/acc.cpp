a0ed570b91562fb121b44bab3882b37abd6f62c5
Jack Palevich <jackpal@google.com>
2009-07-31 14:55:07 -0700

    Clean up the way we handle postfix operators.
    Function names are now treated just like any other variable.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a69d294999d56138cbc8ca30f9ccba4ac1f7f822a3fbf6c8695d76a81f7e227
-size 159076
+oid sha256:476aa0a6a71b6fc64295170305ee1b6e2be35f0da53dc01ec6b12571627e00b3
+size 159022
