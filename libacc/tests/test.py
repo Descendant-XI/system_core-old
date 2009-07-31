0e7ae1132fcfa697f13a1ece313235d5224598bd
Jack Palevich <jackpal@google.com>
2009-07-31 15:25:53 -0700

    Support brackets for accessing array values.
    Don't yet support allocating arrays.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04662164754d76bffb4d2c254473cdf42c8c3aa85ef3b3ae5b9c873a8f7f989e
-size 10418
+oid sha256:e4b57ec50cf073bf990e630e8e5425f626eb2e0ebb683033c103d5da6a4fe03f
+size 10573
