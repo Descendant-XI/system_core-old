5553d5ded75a22bfa146afdb2c7478a23c92b31c
Jack Palevich <jackpal@google.com>
2009-07-31 15:58:19 -0700

    Fix parsing of function declarations that return pointers.
    Check that <op>= only evaluates the left-hand-side once.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a95bccdcb13c892ebdce1707aa880e1d6fb7fcf4fa60d8e31bc312637676bac
-size 159231
+oid sha256:20a97d39f3758cd62f45f13f8c9f6c0611d43aff8914ab548cf30f8c3a408e59
+size 159132
