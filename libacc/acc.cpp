9e9cdb19dc1b192fc311f97171b69121fe42c0fa
Jack Palevich <jackpal@google.com>
2009-07-30 19:38:55 -0700

    Assignment in ordinary expressions is now handled using lvals and rvals.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c59eab39469d5622124eec634be1d1561922e6aff7fd60466a9a958906525cda
-size 158615
+oid sha256:dc2c0859b258a0fb0d807c8045ca1af0660475b90103fcffd43ddaf7030641c5
+size 158181
