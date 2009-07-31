6336e1148aae91b690a7df974fe16c43112c15dc
Jack Palevich <jackpal@google.com>
2009-07-31 11:27:29 -0700

    Test multiple levels of pointer indirection.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e57bf519c3cbb22d0fb17516e613ca4f5df3aad99d7a733138dd98eee475441
-size 9702
+oid sha256:f489c33b098641c157d76ffc6233df6ade55c76936c78572a9d9fba5dfada4fd
+size 9963
