c6ceb16e8d2d0de3e24955f33ee4caf165de4b3a
Iliyan Malchev <malchev@google.com>
2009-08-03 16:27:02 -0700

    init: add permissions for /dev/diag and /dev/diag_arm9
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adeda1a394bca3df7eaeed23f5a8729df568b97c31a2cf190bfc20c52609b7c5
-size 18841
+oid sha256:0a280f6b3c689e46fc3739bb532feeed78e8d9a160eca57f5677f6e02e192eff
+size 18989
