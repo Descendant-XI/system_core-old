609d64a1f2238035e873aa458b903010c2142409
Shin-ichiro KAWASAKI <shinichiro.kawasaki.mg@hitachi.com>
2009-08-04 19:14:22 +0900

    added SuperH atomic support to libcutils

---
+++ include/arch/linux-sh/AndroidConfig.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:32c6fc31a6971635b3ebeec58e66aeb8a22efa60ffed5a868f2d4773aca1c561
+size 7256
--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:662017068e016cecfa4ff3761cad31cb66b28841af0d0d7e5c6279ddb8f744da
-size 2930
+oid sha256:1c5cdbdc1509226354be324440bccd9b824c127d8cd3aa6de3136cbff658dece
+size 3027
