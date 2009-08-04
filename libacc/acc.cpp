de9d535251b8f4091ffabc9c22a98306a6782d7b
Jack Palevich <jackpal@google.com>
2009-08-04 15:39:49 -0700

    Support 2D arrays.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0f9dd0e61f125afc147fe7f461dc69d642841cdbe390a25ddc4791bea6b1ece
-size 165713
+oid sha256:574b8e5efa43b6e1126c4973d77298d2a342a2ecc44b9d99578a242a16fcb36d
+size 166165
--- libacc/tests/data/array.c
+++ libacc/tests/data/array.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4fbf12ea0afe089d8ec8efe5c30466be5eb9934ec07baeab6fd0069e2fe3563
-size 1219
+oid sha256:5d05e2be3ffe6190ddc5fc240af73420edb3ecc6a4727fcef70f3f2829f9d791
+size 1736
