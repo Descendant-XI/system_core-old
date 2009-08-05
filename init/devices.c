f314881cf1ad8ef72aaeb4fc2490d058065653fd
Iliyan Malchev <malchev@google.com>
2009-08-05 09:50:12 -0700

    init: /dev/diag, /dev/diag_arm9 now owned by AID_RADIO
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14470ce57974a90a0ce079154bd41dd0250ac31ab60129ff8e04da17d5f22d49
-size 19133
+oid sha256:3edbd4cdeaf2c4d3ec64c109415cc5e964d2b2334eadd1f76fc21219499afd7e
+size 19135
