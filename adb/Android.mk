3a60862275d87000bc190dafb64cde2b8743da0b
Mike Lockwood <lockwood@android.com>
2009-08-06 10:58:00 -0400

    adb: Remove adbd from simulator build.
    It no longer compiles without recent kernel headers.
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1055808f6decea392a89acaf72b84057dd079da00f423bc61b2424d1c7f2f41
-size 3164
+oid sha256:8161ca52c0d0f2ee70d936c8c4fd7289cf15a9a5959a04fe7c01c76b734ecfd1
+size 3167
