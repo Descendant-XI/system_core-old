c9da08195d4c62b722098987efb885db2f3b44e2
Guang Zhu <guangzhu@google.com>
2009-08-06 17:21:52 -0700

    adb: Use correct language ID when retrieving USB serial number.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/usb_osx.c
+++ adb/usb_osx.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:962e8be6c0b1b1a09f2bc1c20ed092212d07d5c08fa92b6e0fdd8a59a263bd20
-size 16535
+oid sha256:4de23a6670449ff826831e364f0c796ac4bfb1343dba4837873befd4bb5e23e9
+size 17050
