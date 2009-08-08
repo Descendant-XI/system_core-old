a63cd30e37947b46371c6e7e45a27f0f89583480
Mike Lockwood <lockwood@android.com>
2009-08-08 13:16:54 -0400

    adb: update call to register_usb_transport in Mac and Windows builds.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/usb_osx.c
+++ adb/usb_osx.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4de23a6670449ff826831e364f0c796ac4bfb1343dba4837873befd4bb5e23e9
-size 17050
+oid sha256:cd26adbbc4b4422855b940360abf853796d41e074e9d43109d5187a92642e981
+size 17053
