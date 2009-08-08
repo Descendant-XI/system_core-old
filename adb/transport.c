2ddc3da9c7f09cad79d6b9e0e5741269b51f72be
Mike Lockwood <lockwood@android.com>
2009-08-08 13:53:16 -0400

    adb: print better error message when there are insufficient permissions for a device.
    Now, a command like "adb shell" will print "insufficient permissions for device"
    instead of "device not found" if adb does not have permissions to communicate with the device.
    
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/transport.c
+++ adb/transport.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02ef440cb8e76e1103f963e1e92fd294564842c2a6f08461049b9b4970905ce1
-size 25135
+oid sha256:052816c381eb077d2df3c3d15898ea4d271884dda67ba7cdf87e0630a3daca90
+size 25060
