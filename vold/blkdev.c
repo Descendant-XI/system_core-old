0c54dab44ea600f4b1fe3440c7d9c378423f6cec
Ben Winslow <rain@bluecherry.net>
2009-08-15 09:52:10 -0400

    vold: Check partitions 1-4 instead of 0-3
    In mmc_bootstrap_mmcblk, bootstrap partitions 1-4 instead of 0-3, since
    that's how the kernel labels them.  Additionally, use the NDOSPART constant
    from diskmbr.h instead of hardcoding 4 when scanning partitions.
    
    Signed-off-by: San Mehat <san@google.com>

--- vold/blkdev.c
+++ vold/blkdev.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e90b551fe891ba9588a0635be0f8dbce8889e234d5e2c07aad63184ecc2d4e0
-size 8589
+oid sha256:015801b3f3910d8757d54cfde8fe70441f8db0115465a91c991db21aac364291
+size 8603
