35b4679b8f6a7f702f80e428a56f55454e1f5046
Jack Palevich <jackpal@google.com>
2009-08-18 16:04:03 -0700

    Allow parenthesized expressions as the value of defines
    For example, this now works:
    
    #define A (1 + 2)
    
    Note that we still don't support defines with argument lists, so this is
    still illegal:
    
    #define A(X) (X + 2)
    
    Also in this change: The compiler test script allows command-line
    arguments to disable testing on ARM and to disable testing the output
    of the old OTCC compiler.
    
    Disabling testing on ARM is handy for developing front-end code when no
    device or emulator is available.
    
    Disabling testing OTCC output is handy for some 64-bit Linux environments,
    because the original OTCC needs some tweaking to be fully compatible, and
    I don't have time to investigate this problem right now.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b16a418f3c0e98202475b3a38c2dbafe0f2d0f3352f7ccb77720e747c6b0ba7
-size 11190
+oid sha256:2c8b7d39c4b6ca11781798c9c4e1b730658d0488f8f024496986c5502e47b35f
+size 12017
