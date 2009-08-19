ad5e1cd06635f8ffcbca8705c6bdc7ac4a2a8a22
Jack Palevich <jackpal@google.com>
2009-08-19 11:12:56 -0700

    Add a --nox86 flag to allow disabling x86 tests
    The x86 tests don't work on non-Linux systems.

--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:596cffa3241618b41ad017a03090fda090a90dd095029769ed915e3ee238f2ff
-size 12149
+oid sha256:c0b173d9a3c23431e2a686413435ca3671907897eb34aafb0ee4195e5966cdb8
+size 12260
