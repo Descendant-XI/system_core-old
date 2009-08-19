67da1edb8be7a540eb3a58c8fd4919840d511f55
Jack Palevich <jackpal@google.com>
2009-08-18 17:44:12 -0700

    Allow '//'-style comments in #defines.

--- libacc/tests/data/defines.c
+++ libacc/tests/data/defines.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d4b59ada44067019e13c0780b238e051e7620e899ac8875d127f95fbd9f074e
-size 87
+oid sha256:42c586ccb453927f1da170b8214ddf96ebbd15e8838b769b016287ba5414b4eb
+size 141
