56c61c3646ab336132518bdcba7ec10dc30d23bc
John Michelau <w14107@motorola.com>
2009-08-19 10:01:55 -0500

    Route all log tags with "RIL" prefix to radio buffer.

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ceafe38f7d5c99003f1b81519b9c87f130e3dc1d2ca17eb9e9c5badfcf67d4
-size 6461
+oid sha256:5502d7a9151025df59e404af55b9fe2ae96ace78603c637d185cd3f8f32c742e
+size 6380
