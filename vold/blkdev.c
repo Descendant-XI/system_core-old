83cb66bd1012bbc00fc9a942f9aa0e362580ca68
San Mehat <san@google.com>
2009-08-21 08:16:56 -0700

    system/core: vold: Change permission of external storage device nodes
    Signed-off-by: San Mehat <san@google.com>

--- vold/blkdev.c
+++ vold/blkdev.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0724c02632d1296a1757a203c5c90fcc15e5742e878b770116de3ce9616dd6f
+oid sha256:43a4ebf3f41364069e633b860a67a7c42cbfdda180f9daed2c41c6d1b265f3b5
 size 8449
