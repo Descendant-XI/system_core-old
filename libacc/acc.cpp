96be9600703de095ed1c67884318920528ec73cd
Jack Palevich <jackpal@google.com>
2009-08-25 12:23:43 -0700

    Make pointer casting work.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1518ef8e3723b55ed924646c1b327e7913d9e61327e8c610d7e15cad35364a8e
-size 176676
+oid sha256:49321b7f09ba2bf774e843af90edcf467b786fbe8be9a18ff704d870caee3810
+size 176724
