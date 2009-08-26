328ca8a651bf4b5e0e8486d2f92978c1cddc8a41
Tammo Spalink <tammo@google.com>
2009-08-26 10:13:20 +0800

    Add persistent radio prop perms ; cleanup ws and typos.
    Change-Id: I7d6b51bbb8a9658179bd01c4ff3b5356ec8b4b6c

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4529d536d8bf40735c47d7c47c2e4867ab067f2170bed92e006b5f697b61241e
-size 13239
+oid sha256:6274a0ecc5c37a0b3c30a596efd9f05934e4b44e200477d96feb30a8d66723fb
+size 13384
