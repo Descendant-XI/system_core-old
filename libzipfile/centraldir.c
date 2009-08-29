a5abb5d24606bc4986738c80670f9f0c10310c30
Kenny Root <kenny@the-b.org>
2009-08-29 09:51:49 -0500

    Fix use of memset function
    memset was called with is second and third arguments reversed resulting
    in a virtual no-op.

--- libzipfile/centraldir.c
+++ libzipfile/centraldir.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e0da6249e3ec5d245407989a6105392dc3083e90a5c2ad14e08185887379d16
+oid sha256:bedc6001ecce488ed36e91e316d93dc63cb0b4484ae0e61b8925ba169113f489
 size 7687
