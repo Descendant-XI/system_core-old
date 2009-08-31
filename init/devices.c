3457f9c1606aa8e93b4eeb1c9a28982c5beae32b
Dima Zavin <dima@android.com>
2009-08-31 13:40:23 -0700

    init: add vdec device node
    Change-Id: I7e83b99111adfb2b10ee91b78f922864999d4b9d
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e5758f16071d27b38ef658da10827eba3fe51d992dd1379676caa305a8d03ec
-size 19340
+oid sha256:6f979fccdc824cc3c6e6722ab452fa3652f69e36ca73fc7bcdfb74bfa2382e1e
+size 19413
