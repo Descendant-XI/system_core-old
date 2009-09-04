98a78db9252cd8c0a93c1cfb42c300c618741e0d
Jack Palevich <jackpal@google.com>
2009-09-04 15:34:21 -0700

    Improve error message for unknown struct members
    We now customize the error message to correctly refer to the '.' or
    '->' operator , whichever one is actually being used when the error
    occurs.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d9bf42f9f55c51cd5b8c14632bc29b66e8c075acb23b2f46444532047c37d40
-size 189059
+oid sha256:0e864c1d77cce2058d0db95b9ba792b422cce695f8608d3883d1df21d1fc5fea
+size 189125
