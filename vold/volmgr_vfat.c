ca65f29f398762f7701fd2d696d592ea9dccae96
Bob Lee <crazybob@crazybob.org>
2009-09-04 18:32:25 -0700

    Added a hack that makes the SD card world-writable when the sampling profiler is turned out. I'll remove this once egnor has the drop box in system_server working.

--- vold/volmgr_vfat.c
+++ vold/volmgr_vfat.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:484bc1f84886d3a55b589f991fb77b6285f72224f45f42b30712e83f08d635d6
-size 4325
+oid sha256:e2fd94a0a82e49a95abb419d0b12e8b8057f97baba9c5611f02f3c7fe203cd60
+size 5049
