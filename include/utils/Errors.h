6fcf4649e98ec8013eb9210c04e0fe028f819c58
Mathias Agopian <mathias@google.com>
2009-09-07 16:32:45 -0700

    fix [2068105] implement queueBuffer/lockBuffer/dequeueBuffer properly
    Rewrote SurfaceFlinger's buffer management from the ground-up.
    The design now support an arbitrary number of buffers per surface, however the current implementation is limited to four. Currently only 2 buffers are used in practice.
    
    The main new feature is to be able to dequeue all buffers at once (very important when there are only two).
    
    A client can dequeue all buffers until there are none available, it can lock all buffers except the last one that is used for composition. The client will block then, until a new buffer is enqueued.
    
    The current implementation requires that buffers are locked in the same order they are dequeued and enqueued in the same order they are locked. Only one buffer can be locked at a time.
    
    eg. Allowed sequence:   DQ, DQ, LOCK, Q, LOCK, Q
    eg. Forbidden sequence: DQ, DQ, LOCK, LOCK, Q, Q

--- include/utils/Errors.h
+++ include/utils/Errors.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af84996a1a159ca571082dfbcc7c94bab5a2ade6d099339ac1bb00fbd7589371
-size 2433
+oid sha256:17e27e13dc66ba445dc74df6d0d63d807816ead1fe2748cf79f9c42f394c00e6
+size 2437
