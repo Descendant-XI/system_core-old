8291847e46753efec7faf557600b7613dcd6eea4
Dianne Hackborn <hackbod@google.com>
2009-09-20 12:40:03 -0700

    Fix issue #1862317: Browser does not appear to honor anchors (#es) in links
    Also a little improved debugging output of bad resource identifiers.
    
    Change-Id: I054064ef22855608ffd722e4ccf12ce57d1992b2

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a18ed6d2150b28f163337721d72cefaa363a74af2b2153478c5d078d02f12b7a
-size 150770
+oid sha256:c0385f43d555bc4da0bff1c7b789276c2809c544c45ebcca429bec8b80d1d4ad
+size 151093
