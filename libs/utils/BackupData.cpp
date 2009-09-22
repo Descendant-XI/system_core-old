d9663f7bd26779b2727ea592709823817ab9f7f2
Christopher Tate <ctate@android.com>
2009-09-21 19:36:51 -0700

    Clear the device's data from the transport when backup is disabled
    Turning off backup in the Settings UI constitutes an opt-out of the whole
    mechanism.  For privacy reasons we instruct the backend to wipe all of the data
    belonging to this device when the user does this.  If the attempt fails it is
    rescheduled in the future based on the transport's requestBackupTime()
    suggestion.  If network connectivity changes prompt the transport to indicate a
    backup pass is appropriate "now," any pending init operation is processed before
    the backup schedule is resumed.
    
    The broadcasts used internally to the backup manager are now fully protected;
    third party apps can neither send nor receive them.
    
    (Also a minor logging change; don't log 'appropriate' EOF encountered during
    parsing of a backup data stream.)

--- libs/utils/BackupData.cpp
+++ libs/utils/BackupData.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d414abee4e75937c6508e262eb8e416b8166642aeeedfc653300b176eeaca20f
-size 8502
+oid sha256:755a4f0ed7bd3a63f6cccb7360505aa1cea7660c62149317a7ca53345ea644df
+size 8566
