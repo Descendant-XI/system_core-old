8e114bc82ddf837727c248c0f8541e46a8e875e6
Andy McFadden <fadden@android.com>
2009-09-27 15:13:56 -0700

    Fix up ls -s.
    Added the "total" line, which makes it easy to answer questions like,
    "how the hell much space am I wasting on /data/dalvik-cache?".
    
    Also fixed the -s filename output, which was showing mostly-full paths
    when a simple filename was called for.

--- toolbox/ls.c
+++ toolbox/ls.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59625e40549a7a4387dbd716e6a602e72c501d36fcb66186ef0b42dd9c28cc6
-size 7750
+oid sha256:8273eddc94b92848941be44dec3aa1e4005f8753e0c625849037407ee8cae604
+size 8843
