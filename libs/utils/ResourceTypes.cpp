0d84ebcdcbb98488555aca13fe57aa7891b038ca
Doug Zongker <dougz@android.com>
2009-09-27 17:00:13 -0700

    fix "pacakge" typo in error log message

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0385f43d555bc4da0bff1c7b789276c2809c544c45ebcca429bec8b80d1d4ad
+oid sha256:39206681d32fa8e28079c511561af0b22010ca2d2dc3c8957d4fcb5c02c36a50
 size 151093
