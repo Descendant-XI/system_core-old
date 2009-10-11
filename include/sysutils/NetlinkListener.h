2857f05eb9d0b7f2148c5539df8ed443558da1a4
San Mehat <san@android.com>
2009-10-10 17:35:13 -0700

    libsysutils: Fix some bugs in NetlinkListener and NetlinkEvent
    Signed-off-by: San Mehat <san@android.com>

--- include/sysutils/NetlinkListener.h
+++ include/sysutils/NetlinkListener.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0925620e0b3f0134702f6769176e13eae2d07250bc8df9af57723f8519f02d0
-size 952
+oid sha256:9d64e3634c4bef13f2827ab534b4c22625870428a3784b95620d9333fbdf6fef
+size 1002
