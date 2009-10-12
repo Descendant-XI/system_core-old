c0dca00795ab731460a898096ba97f90d001b08e
Mike Lockwood <lockwood@android.com>
2009-10-11 23:04:18 -0400

    adb: Add "adb disconnect" command for disconnecting TCP/IP devices.
    Also check that device is not already connected in "adb connect"
    
    Change-Id: I5f84b56b63d8c6932f23791cac319fd6bc39d36c
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/transport.c
+++ adb/transport.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5db1cc992032f7439ed0cf79a74c68641484f3380dec4e3ba0cbd589139e42d
-size 25077
+oid sha256:ad283f02016f3d294a894d3858b78ba9304492ebea37f83a96219b4c5404dbef
+size 25711
