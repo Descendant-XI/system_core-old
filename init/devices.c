7cf3c89d173f09a6f3109cdc4efe1fedfc28ff75
Dima Zavin <dima@android.com>
2009-10-15 02:24:06 -0700

    init: devices: Remove pmem_gpu regions as they are not needed since we have an mmu
    Change-Id: I2f8f0440d9a21230c7711cb6d9a20ec6887b7cb9
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96446cfb2a4854f4d60191a3b7fa85a11cef17e002524a8b557ba98349c738d9
-size 19751
+oid sha256:294fb0253a11cb90e0d4336d655495e2aa505a42da10ec5ca194a8e9243693ab
+size 19560
