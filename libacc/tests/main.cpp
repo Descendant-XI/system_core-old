da44435453621d844969d10a5b6834e7e6a937bb
Jack Palevich <jackpal@google.com>
2009-10-27 13:58:08 -0700

    Print out error message when symbol lookup fails.

--- libacc/tests/main.cpp
+++ libacc/tests/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adbd5a12cd5805bbe8d5a03c2036e2838c945a09d246b8846fa28dc817e74981
-size 5340
+oid sha256:e20a916964dff17401dc8a8cff971f35a05a9cb0b5d38515b5bab39eddb612d1
+size 5573
