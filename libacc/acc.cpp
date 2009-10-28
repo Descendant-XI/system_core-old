9c56eebf7349d716c4ab51dc49489132d9b944fa
Jack Palevich <jackpal@google.com>
2009-10-27 17:23:20 -0700

    Support nested macros. (Still don't support macro arguments.)
    Now you can say:
    
        #define A B
        #define B C
        #define C 4
    
    int x = A;
    
    And it will work as expected.
    
    Print an error message rather than assert when we're expecting a
    function value, but don't find one.

--- libacc/acc.cpp
+++ libacc/acc.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a66e4d5eefabea3d002a769e4e2b9348dd70b2212ce16093fde7a1743a273b2
-size 198948
+oid sha256:67c0f29ecefd9b92f431a85a06c4be38a38d2c67b29b024cc2c43f49391ecf1d
+size 200909
--- libacc/tests/test.py
+++ libacc/tests/test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba00e5cd9b68614809fd6624624eeb8efd31dbb3d9d42f2e9579f2b3550b8587
-size 12951
+oid sha256:151758f7f78f4d90219e60485e574a45dcaed89dcb942eeb061f68a95d834529
+size 13092
