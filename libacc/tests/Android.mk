3f5a093ad9ade7409c7e9e76fb373ee2dcfbc332
Jack Palevich <jackpal@google.com>
2009-11-12 15:38:44 +0800

    Build accRuntimeTest with static library rather than shared library.

--- libacc/tests/Android.mk
+++ libacc/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56e4fa1c999065445172d98f5bcaa07ebd214e21123ea5fc9a74202c5a568f17
-size 1197
+oid sha256:0496760b80378e78f8b4690e807137403e2b44f8ac83be0c3a428b24fbca6b20
+size 1213
