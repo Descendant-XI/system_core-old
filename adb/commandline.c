546dfd2dce551033df612ced69522ad95277061c
Mike Lockwood <lockwood@android.com>
2009-11-28 12:46:13 -0500

    adb: fix -d and -e options for "adb forward" command.
    Change-Id: I9166572a1c398ce5ef1423d19a30895385118ee5
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53dcd08cb52ec1c176b14aeddf18717a969f50316dd441e46c142fd2c45f5801
-size 36513
+oid sha256:c954b3b0122ad392f58ae6193b083c6b992aa3245c0bf112e2266b30b6597d8b
+size 36777
