cf083c5287afd3d1611fd990b3ced9921fa5cfb0
Martyn Capewell <martyn.capewell@arm.com>
2009-12-04 16:44:58 +0000

    Adds support for UBFX to JIT and Disassembler
    This introduces UBFX instruction generation abilities to the Pixelflinger JIT,
    and also modifies the component extraction function to generate the
    instruction.
    
    The extract function contains defines to prevent generation of UBFX on pre-v7
    cores. The JIT itself retains the ability to produce the instruction even on
    v5/6.
    
    This patch only generates UBFX when MOV, AND or BIC can't be used. Based on
    the TRM, this appears to be faster on A9 than using UBFX in all cases.
    
    On startup, Pixelflinger JITs three chunks of code. UBFX improves these as
    follows:
    
     00000077:03515104_00000000_00000000
     (Blends a single colour into an RGB565 buffer.)
      Before: 27 inst/pixel, After: 24 inst/pixel, Improvement: 12.5%
     00000077:03545404_00000A01_00000000
     (Blends RGBA8888 texture into an RGB565 buffer using alpha.)
      Before: 30 inst/pixel, After: 27 inst/pixel, Improvement: 11.1%
     00000077:03545404_00000A04_00000000
     (Blends RGB565 texture into an RGB565 buffer using alpha.)
      Before: 29 inst/pixel, After: 27 inst/pixel, Improvement: 7.4%

--- libpixelflinger/codeflinger/load_store.cpp
+++ libpixelflinger/codeflinger/load_store.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0819b2e8c3c784ecf383bef8df39e7e423b621c01f6126f853248bdfd9fcb41
-size 11734
+oid sha256:2794d69463514292f7b97cf710b55a9700435ef5074d561927ffb2b00295d72d
+size 12422
