db50c8892f2551ef554d5c1cfc45dfa03245ac70
Martyn Capewell <martyn.capewell@arm.com>
2009-12-07 15:24:08 +0000

    Fix LDM addressing mode disassembly
    The Pixelflinger disassembler does not handle LDM addressing modes correctly,
    assuming that the P and U bits in the instruction mean the same in both LDM and
    STM. This results in the disassembler producing sequences like:
    
      stmfd r13!, {r4-r11, r14}
      ...
      ...
      ...
      ldmea r13!, {r4-r11, r14}
    
    This small patch fixes it by EORing the P and U bits with the Load/Store bit.
    
    Change-Id: Ic7a1556642c4e29415fc3697019f1239b6c26fc2

--- libpixelflinger/codeflinger/disassem.c
+++ libpixelflinger/codeflinger/disassem.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4617165f673fbdafc625cba6094cacd045578769958ca97be9c5e5fffa970b03
-size 21822
+oid sha256:a4ef7dd10ac7276bcfc26fb9339a261cdea119095aab1b693ba88835e0d5ce96
+size 21840
