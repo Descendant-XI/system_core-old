c860dbe3d2e843d996461e3a79147f81f4643c5e
Dianne Hackborn <hackbod@google.com>
2009-12-07 17:59:37 -0800

    Propagate background scheduling class across processes.
    This is a very simply implementation: upon receiving an IPC, if the handling
    thread is at a background priority (the driver will have taken care of
    propagating this from the calling thread), then stick it in to the background
    scheduling group.  Plus an API to turn this off for the process, which is
    used by the system process.
    
    This also pulls some of the code for managing scheduling classes out of
    the Process JNI wrappers and in to some convenience methods in thread.h.

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54a88b1e667a46a978754a8d4356c7b166e2af0a7024b582a349c9d6936cac05
-size 13615
+oid sha256:51bd3a4c0bcf7861061e0f7cdd705412f60b70225dbb5a29610e9de38bab6dfa
+size 14424
--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd5f65ea7c912f329fbda83585e0e35e3a18a1c2d798c1ef7008715cca4bcb44
-size 21977
+oid sha256:03f08d752aa789cc68ed659f51aa21fffe2a324b870ae6c42e60161781fe267e
+size 22983
