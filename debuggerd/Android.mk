af4ce55397c9fc8c0aef629b92f388338e342b80
Ben Cheng <bccheng@google.com>
2009-12-17 12:50:58 -0800

    Dump VFP registers and status word when native app crashes.
    Bug: 2226399
    Caveat: d0/d1 appear to be clobbered by ptrace. It will be tracked by a new bug.

--- debuggerd/Android.mk
+++ debuggerd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb43571a7b18828074444a89d53de5d12edd459cb076e233145ae79781b7fc1c
-size 650
+oid sha256:cc7b888ade7be71bdc9a2ae754af34bf89fe329e82db9cfe83c758de55f2be3f
+size 965
--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7155b641e7731ed1b3cb8888c77316ec6a9c457cc098d7a84ce74d94103190f
-size 23421
+oid sha256:ac38541655a4676d6817e6efedee6646e63ec58c0973bf1aa3199ccee690f3de
+size 23920
