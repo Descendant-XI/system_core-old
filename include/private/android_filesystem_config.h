ebb3eef0aaecaabe23ec14242671be6705d287b8
Jaikumar Ganesh <jaikumar@google.com>
2010-01-07 20:24:55 -0800

    Update bluetooth data files path.

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99af337d6a99c28b5bdb02e4d9523f59dc6d2892c1a9d1f0cb47011ba3f07dbd
-size 8696
+oid sha256:5383aff2b9d5da827cb00dcbd5af0900afaf4f7ae6d5b7c173acb232bfbc6723
+size 8795
