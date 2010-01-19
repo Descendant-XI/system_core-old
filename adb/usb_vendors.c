126a6299c5830209c1deee9c527020658ec7d911
Mike Lockwood <lockwood@android.com>
2010-01-19 11:46:08 -0500

    adb: Add Dell's USB vendor ID.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ac0edb8e9f422f14a104d38140aa6a701c40787358e859cbe771df2c877ac30
-size 4474
+oid sha256:9ce242233fe9eb6eb5f74f2d6273f5bb87a794dbcfc0c59b25de7ba34a7e3fbd
+size 4557
