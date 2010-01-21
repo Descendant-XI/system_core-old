8ce8899f9f074ec05c53206cd27c935c7a5e6ad9
Erik Gilling <konkers@android.com>
2010-01-20 17:40:05 -0800

    fastboot: add Nvidia vendor id to fastboot
    Signed-off-by: Erik Gilling <konkers@android.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f9f186ce75c2b0ead6a34b9c7282e358d6fea16427bdcb4b177ba01dc960c05
-size 19721
+oid sha256:4ab22964db7b533dd9ff2cbdcc677084b36aa307b38bf76812fa81ffdcd4c2df
+size 19771
