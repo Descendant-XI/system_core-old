d06e191c845c9bed8886f8fb43e0a887874718a7
Erik Gilling <konkers@android.com>
2010-01-20 18:19:58 -0800

    adb: add Nvidia's vendor id to adb
    Signed-off-by: Erik Gilling <konkers@android.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce242233fe9eb6eb5f74f2d6273f5bb87a794dbcfc0c59b25de7ba34a7e3fbd
-size 4557
+oid sha256:17e6c9375f2e2d735cbb834344d0d6c76f93ed7623556cc68e02ce19ebd0f5e0
+size 4644
