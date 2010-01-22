aa78b9c435398fbe03f6d820ff88f99b48315868
Xavier Ducrohet <xav@android.com>
2010-01-21 17:24:36 -0800

    Add DELL's USB Vendor ID to adb - DO NOT MERGE
    Change-Id: Ie1a6b6a81f50956362aee2a2d6d1b805f0e85b7a

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ee5012902078b427f0b8aef636ff9cc056e7b2a2d0181f13b2173e220697eff
-size 4384
+oid sha256:da89897b390b0853c5911ee69474cdc843f763160462ff8dff24f3eaaf61684a
+size 4467
