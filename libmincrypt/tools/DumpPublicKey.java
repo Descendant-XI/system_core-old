5049cbdd490eaaf6ff00b8c73c2e17f4676fb353
Doug Zongker <dougz@android.com>
2010-01-29 10:47:38 -0800

    make DumpPublicKeys write 32-bit values as unsigned
    Write the 32-bit chunks of N and RR as unsigned, so we get:
    
      {64,0xc926ad21,{1795090719,2141396315,950055447,2581568430,4268923165,...
    
    instead of:
    
      {64,0xc926ad21,{1795090719,2141396315,950055447,-1713398866,-26044131,...
    
    Change-Id: I575224fd7f7c34e06a1b6ae976eaa0bef41d2942

--- libmincrypt/tools/DumpPublicKey.java
+++ libmincrypt/tools/DumpPublicKey.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:951f7411760aa0932d20538e6e0ea5869c8dcb390ccc3ec2e89605d875776b30
-size 4204
+oid sha256:46c317809cfd0f15ed1478ba19781d81fd4eacb77c2e78173a28870d7d0ab23e
+size 4208
