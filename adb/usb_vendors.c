8e552f9a03a3103a863ab00b74e23e57ff00d882
Raphael <raphael@google.com>
2010-02-01 09:56:47 -0800

    ADB: Garmin-Asus's USB Vendor ID
    Change-Id: I3249d629f59d40f291079fc214b643ac6c927f76

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17e6c9375f2e2d735cbb834344d0d6c76f93ed7623556cc68e02ce19ebd0f5e0
-size 4644
+oid sha256:b18b127c6b2d90a1e342e82ffecba194c32e62a7f33479418d6785e785a4217a
+size 4740
