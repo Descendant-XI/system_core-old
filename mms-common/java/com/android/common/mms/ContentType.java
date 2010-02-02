04ebc1aabbd7b5c7a85c9807bac801f7bdd4343f
Ficus Kirkpatrick <ficus@android.com>
2010-02-02 15:22:35 -0800

    Fix mms-common's pathnames to match package spec.
    Change-Id: I4ae65227ea29dff8a79bd1d9373dec46c4b53598

--- mms-common/java/com/android/common/mms/ContentType.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e568f89989ee6d59e4442364c1b0d77c0f39bdf62b57abefc208264b7ad1f61d
-size 9721
---
+++ mms-common/java/com/android/mmscommon/mms/ContentType.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:faa97c44cd68d5819836f045032ccb344b282f568728f788b3b07ecb87fbf67f
+size 9727
