86bdb02a5cf60b8c2777ac17701f14826185c53f
Tobias Haamel <haamel@google.com>
2010-02-09 23:09:17 +0100

    Introduce special UI modes for night and car usage.
    The device mode is now called ui mode. Furthermore is the order of
    precedence for the resources now in such a way that the ui mode needs
    to be specified after the orientation and before the density.
    
    The ui mode can be set, like it is done for the locale, as follows:
    
    IActivityManager am = ActivityManagerNative.getDefault();
    Configuration config = am.getConfiguration();
    config.uiMode = Configuration.UI_MODE_TYPE_CAR | Configuration.UI_MODE_NIGHT_ANY;
    am.updateConfiguration(config);
    
    To allow users to disable the car mode and set the night mode the IUiModeManager
    interface is used.
    
    The automatic night mode switching will be added in a separate change.

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef4466300ce7fd3d0d719a34d81b04bb9f2745cb8553802655f84af18f911e45
-size 66811
+oid sha256:a9d7576a6f82ea6da00407f8fa97fdc175afddcb1118424ceb52655b993ab1ef
+size 68865
