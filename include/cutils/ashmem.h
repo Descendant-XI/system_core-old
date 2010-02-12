298e905d77126025328f70e719c22c5d855237d5
Carl Shapiro <cshapiro@google.com>
2010-02-12 12:45:28 -0800

    Include stddef.h to make the size_t visible.  Resolves a simulator build break caused by a use of ashmem.h in an environment without size_t.

--- include/cutils/ashmem.h
+++ include/cutils/ashmem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd4b4b8a610d1ef48a21aa9530293455d95f5dfb54f315c45ccc3a40062bb4b
-size 1121
+oid sha256:a6aaa0d2da45e47c3915c98174db302d9675e2df76073615f164be08e6c51fd9
+size 1141
