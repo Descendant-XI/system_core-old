4b15d5d83a9034a94118b10d5305bdc467a1914a
Carl Shapiro <cshapiro@google.com>
2010-02-14 19:01:31 -0800

    Add a new method to export the extent of the break, akin to return value of sbrk(0) in UNIX.  In terms of dlmalloc and our proprietary contiguous mspace class, this is the highest address returned by its morecore method.

--- libcutils/mspace.c
+++ libcutils/mspace.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4da96e9f85ec3348cafca10941de7f9ef5ef89bfe85db4385c536a93b6dc453
-size 7523
+oid sha256:2a3a4696413d1ba10243f6394cf2dc691c6ea99007862923479167c8b26c66c6
+size 7850
