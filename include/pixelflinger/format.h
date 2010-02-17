481d480c23fcbc1bdeacd540fcc8d9b9e57d524c
Mathias Agopian <mathias@google.com>
2010-02-16 16:54:53 -0800

    remove unused YUV constants

--- include/pixelflinger/format.h
+++ include/pixelflinger/format.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d657b487cf28dd43f805e64e1fbfa75d92772fe930048d1411d05b54b006752d
-size 4522
+oid sha256:e0cb16a955feed117f3a53b7127d98cac6e4dc3996ec05a651ad42933e315314
+size 4411
