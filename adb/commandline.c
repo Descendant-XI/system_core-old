bbc5125bcb57d75840f0119707d438cca0b9d534
Mike Lockwood <lockwood@android.com>
2010-02-19 17:53:27 -0500

    adb: add -s option to adb install for installing apps on SD card.
    Change-Id: Ic6b24411e594d160ddcf862cc9f1283e1e17bb4d
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4cb9515ebe4847c1408cd61f76168b4391d891bdb637ad19448a4d7c1d8c6b0
-size 37158
+oid sha256:565e0159b718a7bec72d85abc8ee5e0b6ee9e8fa918bfd3175c2e8ce50405f51
+size 37473
