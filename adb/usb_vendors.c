401f41226b2c253be139ce3787d7fb4c0ba3ab6f
Takeshi Kishimoto <kishimoto@google.com>
2010-02-22 19:30:41 +0800

    Added ZTE's USB Vendor ID (0x19D2) upon ZTE's request.

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:714a471778472d0ff506d7ad8f75377d4ae34741b83ec82342cda35ef5bd28d6
-size 4825
+oid sha256:e273f5905cfa9770f74dd0bdcd869b2c5574e64435318fd88fa8a42852663356
+size 4906
