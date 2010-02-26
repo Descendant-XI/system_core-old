5232183f4539dc0e2c4a77a35c09b55bd428563e
Joe Onorato <joeo@android.com>
2010-02-26 10:04:23 -0800

    Allow logcat to take multiple -b options, and interleave the results.

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b421e05bf8011dc5d50fd1a37de3f1d960c8579f9609ff8599d39ea84895b0da
-size 16411
+oid sha256:4ae80fd31a206f6d8992acf2b98c5d100b63ba9bf6c04ae3eb81178ac30a075b
+size 21899
