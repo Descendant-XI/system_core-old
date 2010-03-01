f18a2f59370a7029b42bc247cddb8560ea4e9f58
Ken Shirriff <kens@google.com>
2010-03-01 14:19:26 -0800

    Remove DateException and obsolete DateUtils functions.
    These functions are deprecated and no longer used.

--- calendar/Android.mk
+++ calendar/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c631781ee68600e97b673447d0fff4fe23290a879c957be71e9277ee56fd652
-size 580
+oid sha256:735060f13ef2a7f74df13cdfd92efb90a2c27f0100464d8e6e69ca2ed7ede02b
+size 535
