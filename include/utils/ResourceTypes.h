90ad7f9c4db589d597d1d2fcc84f193810ae97e2
Dianne Hackborn <hackbod@google.com>
2010-03-04 18:41:49 -0800

    Refactor car mode.
    Extract all UI behavior from dock observer and ACTION_DOCK_EVENT.
    
    Also introduce a desk type to go along with the car type all through
    the resource system, since we now need to have corresponding high-level
    broadcasts for desk dock mode.  As part of that I also reworked some
    of the logic for switching modes to all funnel through a single
    update() call that looks all of the current state to decide what to
    do next, and fixed various locking issues.
    
    In addition I found there were bugs in the configuration change
    handling causing us to only switch into the car mode config and
    then never get out of it.  Unfortunately now that we are actually
    changing the configuration for each mode change, the transitions
    between them are really crummy as we restart all kinds of
    activities. :(

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f17ffe49213457945f6561c509667655e99447eaa0c2e30763ad88b1d1d52f8d
-size 68936
+oid sha256:408aad477c1f6739bc7d8c12c4d6c6a037ff8e65cbaeb974252fb38f4ddbd96b
+size 68970
