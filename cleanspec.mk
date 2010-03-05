cfbfeef0e14e9cc587ddd81b52d70a03f47d0f1d
Christopher Tate <ctate@google.com>
2010-03-05 15:46:30 -0800

    Refactor android.backup => android.app.backup
    Change-Id: I0b21316ff890d7f3c7d4b82837bb60670724c2e8

--- cleanspec.mk
+++ cleanspec.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139d1dc1326521e58b121e65f747e93e4d9fb08da4d486718cd74e41acea62d1
-size 158
+oid sha256:a69571a01b0d09276ad4aabd7c31829f1b5ca1eacb7d5932f4876d212dde53ad
+size 427
