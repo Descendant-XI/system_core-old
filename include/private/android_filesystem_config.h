80e22d63a04c1a2195ab8bd98050debe673f1c71
Nick Kralevich <nnk@google.com>
2010-03-08 11:36:10 -0800

    Change netcfg permissions to 2750. Fix bug 2104034.
    Change-Id: I30bcc41f33623cadc63693fd54206ccbb1886a5a

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5383aff2b9d5da827cb00dcbd5af0900afaf4f7ae6d5b7c173acb232bfbc6723
+oid sha256:7e8bced86880acca87408fc13428ecb7d58eb9eed9ce61bae6d1c5c1c0cfb558
 size 8795
