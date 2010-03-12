ff15bc11b13f320c3b3122ba13dd01f30e93f862
Dan Egnor <egnor@google.com>
2010-03-11 20:32:17 -0800

    Add '-t N' flag to logcat, to print only the last N lines of logcat (to be used in capturing a merged mini logcat for crash reports)
    Change-Id: I68149eb80f2f79812f43b07d80fbea822476859e

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a422641c11c372fe2a11e8ec3b196fdefa9b4c1a4e3e05a98a693a98b34edba2
-size 22562
+oid sha256:c8f34d4913e55c33a36fe7cc06fdb720ccaac7c24ee83c3b40665fa8bb534dd9
+size 22952
