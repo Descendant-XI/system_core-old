cf607ebd4ba1894c9e485c321131d8da6f9870cd
Wink Saville <wink@google.com>
2010-03-11 16:22:44 -0800

    Change permissions on pppd-ril
    On devices that use a pppd for cellular network access, a pppd-ril
    process needs to run as root to configure the connection.  Changing
    the process to suid-root in the build image is the only known way
    to avoid needing to remount the system image at boot to accomplish
    the same thing.
    
    Change-Id: I6c778391b8040ee818343e304891e7d341cc729b

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e8bced86880acca87408fc13428ecb7d58eb9eed9ce61bae6d1c5c1c0cfb558
-size 8795
+oid sha256:a61efd04b8ec406876576a32b00f6d0469aa9cdc6ac08b1d6685e965264592a6
+size 8863
