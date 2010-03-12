2a0b6a5274bcab64034e8fcde981c1cb930b6125
Takeshi Kishimoto <kishimoto@google.com>
2010-03-12 21:34:58 +0800

    Added Kyocera's VID (0x0482), upon their request.
    Change-Id: I8f2f2a02c22d618c0b42d4f287d88a98af3ac4fa

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e273f5905cfa9770f74dd0bdcd869b2c5574e64435318fd88fa8a42852663356
-size 4906
+oid sha256:66d3cef05deed0bb9b0fc99d3e187398f550c439a807413789031e6c3dd4487d
+size 4995
