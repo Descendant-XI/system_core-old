4b924b7de07b18bf056cbea4bfe12f232cc0c544
Eric Olsen <eolsen@android.com>
2010-03-16 10:22:06 -0700

    Add Bluetooth SCO connection blacklist
    Change-Id: Ieb11debfea66ec715f48e8069619c2c15acf2035
    Signed-off-by: Eric Olsen <eolsen@android.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc8ef5c5db6567cbc4fdd7eaa619f7eb3bc0783ab4e4d77da9a611347abf64a7
-size 9019
+oid sha256:fb4d4a7bfb20c0e7fa424c8bb082d5a6b589097b5c43e21a44252349d373d8e6
+size 9103
