380792cd45bb7d140d8a4bb6917f291608030e89
Mathias Agopian <mathias@google.com>
2010-03-19 16:14:13 -0700

    libutils Condition are now PRIVATE by default
    Condition must be initialized with SHARED for the old behavior, where
    they can be used accross processes.
    
    Updated the two places android that require SHARED conditions.
    
    PRIVATE conditions (and mutexes) use more efficient syscalls.
    
    Change-Id: I9a281a4b88206e92ac559c66554e886b9c62db3a

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51bd3a4c0bcf7861061e0f7cdd705412f60b70225dbb5a29610e9de38bab6dfa
-size 14424
+oid sha256:662be2bc29cdf1b42971bf6abaf2b8e219fabf3ac1a2d3e168bebd822ed3018a
+size 14859
