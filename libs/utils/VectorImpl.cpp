38886ac3d157c7dd20404d17b5d078497297a114
Dianne Hackborn <hackbod@google.com>
2010-03-30 21:04:17 -0700

    Revert "fix [2542425] memory leak during video recording"
    This reverts commit 544592e14f8d7643238e40ba9879727497900f35.

--- libs/utils/VectorImpl.cpp
+++ libs/utils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f9f9fefe3b2274c97bbac8ab804ff6109a1fe49d292b96653d313ee91710d9f
-size 17714
+oid sha256:8b08a65147dfbda7f3b8551c5222f012b4fc94b70486e439f82081e3ddd264c7
+size 17652
