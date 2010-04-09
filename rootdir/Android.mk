88f9c48f8f1e284c3421a53794ef4201dba44c9d
Colin Cross <ccross@android.com>
2010-04-09 12:39:16 -0700

    Add a build dependency for ramdisk.img on system/core/rootdir/init.rc
    Change-Id: Id9698a88720cc0c70e3d9c43c9bac9b74f4798b6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2ee378db85ba93cf06e30024e2da1068d3a1ad935239571844d3ff9f2314969
-size 1681
+oid sha256:ba522eb5fb836f67edbc6af070dacc1afce3657082c1e7fc41b0065ec8e1f134
+size 1718
