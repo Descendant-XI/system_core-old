909e5326c0196561a830ed91a5c0659536cafae2
Colin Cross <ccross@android.com>
2010-04-16 20:28:11 -0700

    init: reap exited child processes on signal_init
    If any child processes exit before signal_init, they won't get reaped
    unless another child process exits after signal_init.  Calling
    handle_signal from signal_init forces them to be reaped immediately.
    
    Change-Id: I459cfbfe6cf00f29454c62a8c840baf21cb1fb03

--- init/signal_handler.c
+++ init/signal_handler.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88836776b3bc4ca00363fd2d2c90c36e17efc3ff8f4db102e94df9db3dcf478a
-size 4341
+oid sha256:3376eed23b5aae176902ba432859aeff4eaba5c4eef991ea06e0db405137002a
+size 4363
