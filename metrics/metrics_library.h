462e092458f54f046843afc64c767284d9ce19d6
Darin Petkov <petkov@chromium.org>
2010-04-21 15:45:10 -0700

    Add support for linear/enumeration histograms.
    Review URL: http://codereview.chromium.org/1747008

--- metrics/metrics_library.h
+++ metrics/metrics_library.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eca1d5e24faeb81563b1b584c23f983edd5083c4b9ecf4ca1b3094cf1e4f614d
-size 1364
+oid sha256:25870e64c7801a9afd12890dcb1b4ad8c8644c955bb5bd3b344fa765549b0576
+size 1925
