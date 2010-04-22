b8c7f8e6b65e9a7948c14df5befc551aa182b3ae
Colin Cross <ccross@android.com>
2010-04-22 11:52:23 -0700

    init: Fix init spinning once init.rc is complete
    When the init process ran out of actions to execute, cur_action was
    NULL and action_queue_empty() was true, but cur_command was still
    set.  This patch clears cur_command when a new action is retreived,
    and only depends on cur_action and action_queue_empty() to determine
    the poll timeout.
    
    Change-Id: Iaa95063c8c267a5b1ada9f20363b99c433e61ac4

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b5ef9da6a4853cc4aee72f2b52fcae9c4630cd3d9d874919067bb57b807e4d9
-size 21814
+oid sha256:a6e59b8ba46718d531231345cfaca2a1097d492c3154936a8dc371616726ba68
+size 21841
