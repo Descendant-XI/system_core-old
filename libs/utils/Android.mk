bcd65693327843d9d854c4693569bf5affa037ef
Dan Egnor <egnor@google.com>
2010-05-06 00:55:09 -0700

    Make static versions of libutils and libbinder.
    Fix some small static-initialization-order issues (and a static-
    initializers-missing issue) that result from doing so.  The static
    libraries don't actually get used for anything real at the moment --
    they're used for perf tests of bug 2660235.
    
    Bug: 2660235
    Change-Id: Iee2f38f79cc93b395e8d0a5a144ed92461f5ada0

--- libs/utils/Android.mk
+++ libs/utils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e96bf788393c30ed796c149a801f760a350bbf03fdaaac57027ab007fa075283
-size 2325
+oid sha256:10b74a1e07889d57606cd9b94f847981ad8055a4decc38ee436bdd16e4f9d7b8
+size 2636
--- libs/utils/String8.cpp
+++ libs/utils/String8.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db06a9226b28899c8c4d15acc6f68bc4e8ec5a265dc3767f1803eebfd10ef60b
-size 22225
+oid sha256:724fff4ef12beb9e444077e323605dc340d14929f579ca48a5ee3a4a74a58919
+size 22432
