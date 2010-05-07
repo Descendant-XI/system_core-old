9be63ca44d45c8b5801842c8aaa177da788718ae
Brad Fitzpatrick <bradfitz@android.com>
2010-05-07 12:06:05 -0700

    More fixes to Binder perf regression from Eclair
    Re-use the file descriptor to change the cgroups.  There's a fast path
    in the kernel for the case where the value's already uncahnged.  Also,
    this is okay because the kernel ignores the write offset.  This is
    essentially a ioctl-ish/datagram interface, despite being a fd we're
    writing to.
    
    This reduces Dalvik->Dalvik void/void Binder calls on Sapphire from
    ~1.2 to ~0.8 ms.  This is a bit slower than the ~0.7 ms we get
    avoiding the write altogether.
    
    strace confirms:
    
    [pid  1557] ioctl(21, 0xc0186201 <unfinished ...>
    [pid  1556] <... ioctl resumed> , 0x45399cd0) = 0
    [pid  1556] write(22, "1556", 4)        = 4
    [pid  1556] ioctl(21, 0xc0186201 <unfinished ...>
    [pid  1555] <... ioctl resumed> , 0x45299cd0) = 0
    [pid  1555] write(22, "1555", 4)        = 4
    [pid  1555] ioctl(21, 0xc0186201 <unfinished ...>
    [pid  1554] <... ioctl resumed> , 0x45199cd0) = 0
    [pid  1554] write(22, "1554", 4)        = 4
    
    BUG=2660235
    
    Change-Id: Ia9f9e1d5b792eaebc9560f89931faf6df0cf9c0d

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:491c96420f4233f0c15d1675ebaf868d8b4a40827079ad5af5319135324b1d8d
-size 6252
+oid sha256:8e83861b3b6e516572276ae36dd46fa1bff0ebeb1110a0aa43c1e12b5df49ff0
+size 6830
