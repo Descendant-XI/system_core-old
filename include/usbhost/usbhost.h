2da8e488e682ede6466f967d7c0d6bec8880c7d2
Mike Lockwood <lockwood@android.com>
2010-05-12 08:53:49 -0400

    libusbhost: add usb_endpoint_get_device()
    Change-Id: Ibe4ce0551faca5d2d8bec0fbd21315a393b9f208
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83fd150d9e613d27cbf969db063558bcc7b7b24270df5ebcf73f72cca62242b7
-size 5087
+oid sha256:1866fc55175335c7af0ab1e17bc130282c0cc4f96d097fe4b71031fcd8a45a7e
+size 5209
