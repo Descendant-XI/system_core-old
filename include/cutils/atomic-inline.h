9926f2bb2d9db265507340330a5cc0e01a368bb5
Andy McFadden <fadden@android.com>
2010-05-19 22:33:28 -0700

    Atomic/SMP update.
    Added atomic-inline.h.  Added a platform-specific memory barrier call
    there.
    
    Added android_atomic_acquire_cmpxchg() and android_atomic_release_store().
    Not tested on Mac OS X or SH.
    
    Added memory barrier calls to linux-x86 atomics.  Mac OS X has barrier
    functions already.  sh isn't really SMP-ready.  linux-arm needs work
    (to be done in a separate change).
    
    Updated the makefile to make the SMP state visible to the code here.
    Note that host binaries are NOT built with SMP enabled; while our hosts
    are very likely SMP, it's not worth figuring out e.g. whether it's okay
    to use the SSE2 mfence instruction or have to use something else.  We
    haven't had barriers enabled in host tools before, so there's probably
    no need to stat now.
    
    Removed quasiatomic 64-bit calls (now part of Dalvik).
    
    Change-Id: I49e5e6c8abe70f304cdedb9d7b8e6e65f8925815

---
+++ include/cutils/atomic-inline.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a3493ae5da5bca195c2eb2445a7f1346f31efc2ee58b433b4988eeaee2e3cc3
+size 3162
--- libcutils/atomic-android-sh.c
+++ libcutils/atomic-android-sh.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:778e4ade48b5145e05cc4e702a5f2e3e44c3d67d52376e0b21e06c9ef323c6f2
-size 4265
+oid sha256:f9924918448de2d0070cef0b38fe422a786b92856f429dc5618829e3592e4960
+size 3575
