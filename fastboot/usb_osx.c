5332692d06e6b6c18737ca52b0932c698cfae4ce
Jeff Brown <jeffbrown@google.com>
2010-05-21 13:20:47 -0700

    Fix kernel panics in fastboot on OS X.
    The kernel panic seems to be related to the driver trying to allocate
    too many pages from the IO mapper.  That may be caused by the fact
    that we try to perform a 100+ MiB transfer in a single IO operation.
    This change breaks the transfer down into 1 MiB chunks.
    
    So far after a day of testing, no kernel panics have occurred compared
    to 5 in the previous 24 hours!
    
    Change-Id: I8d27a546e0c0bf4fe2f0fc7fcad65a88d3e6bee0

--- fastboot/usb_osx.c
+++ fastboot/usb_osx.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18c3258c8a747b0da13c97e817eff4f7ef5628702f9a93204f7107296e51a083
-size 15679
+oid sha256:cd0c305256c9102c1842b8bec8b0418cc1ceead50a306dfab53e1a7ed5fdd7b5
+size 16387
