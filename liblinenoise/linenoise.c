a614cfb9c4d76e16f240532a56d366c51f374947
Jack Palevich <jackpal@google.com>
2010-05-25 08:20:12 +0800

    Move linenoise to its own static library.
    This allows us to publish its license, as required
    by the terms of its license.

---
+++ liblinenoise/linenoise.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef78f999db966d229bb354806282b446b8fe2c4b910bc137fe577053a028a69e
+size 14087
--- sh/Android.mk
+++ sh/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af9f8450b9b05d1004417d807481b4026a31068722caf7670d880b98e7bdbdc8
-size 966
+oid sha256:2b208805e42f5785296faa6efac3dc31979dc687fd529e794515859f483fa259
+size 991
