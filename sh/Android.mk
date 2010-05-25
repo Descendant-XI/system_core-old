c4cd4211a31e3348996e54cecff469e56bd64dfe
Jack Palevich <jackpal@google.com>
2010-05-25 08:36:36 +0800

    Include the linenoise.h header in a cleaner way.
    (This was a comment to an earlier change that I
    overlooked.)

--- sh/Android.mk
+++ sh/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b208805e42f5785296faa6efac3dc31979dc687fd529e794515859f483fa259
-size 991
+oid sha256:a392230bdf421d6b5aa28548c4cc922483d3bcf72bfc974afb3a994635587643
+size 1037
--- sh/input.c
+++ sh/input.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a61620349bc5c97e3964b0ed4fcf54912901c9d9b1ddc36dfdd95d0e217609e
-size 12685
+oid sha256:455d541c8ac9cb8f8b0db766f29e9d4a0469427e99136594bd6e20390d13c34d
+size 12669
