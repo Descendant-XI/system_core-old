71a29b458dba8444c1aef3541cb6e0540c3e6cc7
Andy McFadden <fadden@android.com>
2010-05-27 10:10:18 -0700

    Atomic/SMP update, part 2.
    Added new atomic functions, renamed some old ones.  Some #defines have
    been added for backward compatibility.
    
    Merged the pre- and post-ARMv6 implementations into a single file.
    
    Renamed the semi-private __android_membar_full_smp to USE_SCREAMING_CAPS
    since that's more appropriate for a macro.
    
    Added lots of comments.
    
    Note Mac OS X primitives have not been tested.
    
    Change-Id: If827260750aeb61ad5c2b760e30658e29dbb26f2

--- include/cutils/atomic-inline.h
+++ include/cutils/atomic-inline.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a3493ae5da5bca195c2eb2445a7f1346f31efc2ee58b433b4988eeaee2e3cc3
-size 3162
+oid sha256:d04bb3fae6cf7e7c3db0f8133a58e28c77b439cf77e29760da7ae7611a0ee7a1
+size 3356
--- libcutils/atomic.c
+++ libcutils/atomic.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f1772432ed2af49a1f2b5b2a2e08d7b5af723b6a3b290334d3cf052cdd69aa
-size 5343
+oid sha256:c00e8133a06492acff6b6b6df1db2501834aa7ac934424c1a83cb07a296a330b
+size 6073
