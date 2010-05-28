a3a1e04f0a6fb6cda2018fde9c5faa6f1c4b7401
Mathias Agopian <mathias@google.com>
2010-05-28 15:13:30 -0700

    Fix a typo in Singleton<>
    it could cause the sLock field to be emitted several times
    in different compilation unit. it also prevented to
    have 2 Singleton<> in the same file.

--- include/utils/Singleton.h
+++ include/utils/Singleton.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aadebba44576d369935230457f9aa581574bffb8c0e366ac9beb367020d1ff4c
-size 1995
+oid sha256:e7a446d9f8847a803bf13a72a0a0499880dc82f6e726e8b938aefc884dae1d70
+size 2156
