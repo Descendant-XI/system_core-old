73dd0a0b0b801656c7c3f4cf1887c60fa1b6b250
Andy McFadden <fadden@android.com>
2010-06-01 09:55:18 -0700

    Fix build.
    Missing a #define to map android_atomic_swap to its new name.  Worked
    fine in the other branches, because nobody was using android_atomic_swap
    in the other branches.
    
    Change-Id: I0e730e9823ac5cd2fee5a3f856ce05d36df5d87e

--- include/cutils/atomic.h
+++ include/cutils/atomic.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30ed22a9c66735673060ff8bcc07202af03fd663f759f1ca12a56dc89a905d08
-size 5006
+oid sha256:b866c5c580794411e86f0b99fbe17f7665bab456dcee7df15cd5aa0877e6ca77
+size 5062
