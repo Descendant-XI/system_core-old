911eda9737c4f84a43c1af94aa068cdecbf7bec4
Colin Cross <ccross@android.com>
2010-06-03 12:21:01 -0700

    Reap firmware child processes to prevent zombies
    Change-Id: Ic6c50b929a025f05450cd309238e60a857690d9b

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1817c0a5a07e0d8063b34c227a465e4aa3be9f4256624a2350b36ee9da05ffc
-size 16840
+oid sha256:876b8d22f9272155c382d8a288d7a43dae1ec60b9093de787c99ca17da47a698
+size 17008
