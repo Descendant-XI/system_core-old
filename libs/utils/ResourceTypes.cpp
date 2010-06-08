fc73ffd8c343fa21cb56634bbd37f49a15dbc316
Kenny Root <kroot@google.com>
2010-06-08 12:45:31 -0700

    Range checking for ResTable_map
    Correct previous offset change and get ready for safe-iop.
    
    Change-Id: Ib276c726f32b0711a72f47d3263eb21640e5a800

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16de5df00fe0b7ccf66882145a6d1c7d43c4f83b028037db1e52b6f086e89ac0
-size 164854
+oid sha256:d3720488c2ce0b658329aac0e4aedd6fdd25d7e04b7896d3e2c81de9f55b4fe7
+size 164920
