932893a0f41081522ed735638e963e22783a0d99
Kenny Root <kroot@google.com>
2010-06-08 10:16:48 -0700

    Improve checking in aapt dump
    The aapt dump reading had less error checking than the actual parsing,
    so this change brings it more into parity so that bad APKs don't crash
    "aapt dump"
    
    Change-Id: Ib30e63e41be5c652645c4aa0de580a87b184529d

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dcbead0fff26ef5d5c02e7dd89457c9f131367b4510737557b899e689ffd93d
-size 164278
+oid sha256:16de5df00fe0b7ccf66882145a6d1c7d43c4f83b028037db1e52b6f086e89ac0
+size 164854
