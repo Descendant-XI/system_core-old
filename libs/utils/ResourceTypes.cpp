e8a87f1f87d667410e7c1efad90649f2cf220968
Kenny Root <kroot@google.com>
2010-06-08 12:34:43 -0700

    Add invariant check for stylesString size
    It was possible for stylesStrings to claim to start past the end of the
    data area thereby making mStringPoolSize larger than the data area.
    
    Change-Id: Ibc4d5b429e3a388516135801c8abc3681daae291

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3720488c2ce0b658329aac0e4aedd6fdd25d7e04b7896d3e2c81de9f55b4fe7
-size 164920
+oid sha256:4c59e161fa86cacac3ad90c7bafc256dd4666b09606b35835b9b2826be7cf50b
+size 165288
