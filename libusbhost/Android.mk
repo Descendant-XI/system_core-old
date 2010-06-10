4e3310538adebf704ade8d68bae98fdece1a3d17
Mike Lockwood <lockwood@android.com>
2010-06-10 10:20:49 -0400

    libusbhost: Use logcat for debugging when building device library.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- libusbhost/Android.mk
+++ libusbhost/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edfaf894242064ed7f14034b53f173db3b98c227517ef74aabbf2bd861137ee6
-size 1072
+oid sha256:24c3c8901bd2ad72f26a906c12f216d391536c8425473c45718342e87995f0a9
+size 1105
--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80fb974a9487fac96fb1eaaccfa058d9ce50e72996fe9a4c73ed4ae8fa76ed17
-size 12952
+oid sha256:75f165e0f17914f1cdda3b638c7fe38688b7d736780da62f1e375fff138b02e4
+size 13228
