fcd56a45f6a311dca8e133b8697614f6a832e039
Andreas Huber <andih@google.com>
2010-06-10 11:14:26 -0700

    Fix String8 to free its memory only after assignment operations based on pointers are finished in case that pointer referred to the string's original contents.
    Change-Id: I6961f3cf10ba3b728579ea63262db750a4cf8577

--- libs/utils/String8.cpp
+++ libs/utils/String8.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:724fff4ef12beb9e444077e323605dc340d14929f579ca48a5ee3a4a74a58919
-size 22432
+oid sha256:f1d74914e7767fe667272c57c9eca02042d7f902bf757aca14ee21236359a6f0
+size 22588
