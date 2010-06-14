55b954820bc471638a0f3a9fe96925ba1c97c9b9
Jeff Brown <jeffbrown@google.com>
2010-06-13 20:21:19 -0700

    Remove PollLoop from host build.
    We can't build (and we don't need) PollLoop in the SDK on all platforms.
    
    Change-Id: I3b0f327bbb8bc58b9b8630b0161cf360403fea68

--- libs/utils/Android.mk
+++ libs/utils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e735c08c6f1acbee57354d72d1c05db197873cd30a32ee0b9f920cde568fe9f
-size 2664
+oid sha256:6afe4db63a616e6623e4323344b518f7a42694e19ce1743d6c1a77971ff96ef6
+size 2658
