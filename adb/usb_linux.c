bf15872b54e675be6002a331f36dbfd8325e413a
Mike Lockwood <lockwood@android.com>
2010-06-14 14:43:50 -0700

    Revert "adb: Adjust usb_linux.c for changes in libusbhost API."
    This reverts commit 36b99e4cac293ae4b2400ae3fcc06b7056893060.

--- adb/usb_linux.c
+++ adb/usb_linux.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cb873a665c51cf02523efc56b708d2da36099f0ba0447f48f210216765fc3a8
-size 12885
+oid sha256:bf2541b36ded1d6a334ae31df3a43f36ec706a37403fbeddced305e526fec0a8
+size 12841
