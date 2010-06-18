48ec8e1e896023f10c045fbfde8ac746e45e4d18
Wink Saville <wink@google.com>
2010-06-18 10:15:08 -0700

    Update the help text to reflect new capabilities of -b parameter.
    The -b parameter now support specifying multiple ring buffers
    and the results are interleaved. In addition a new ring buffer
    has been created, 'system', and the default is now -b main -b system.
    
    Change-Id: Ibc25a31fb8fe21d75dda4598a83cf3426f759eb1

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ee28a91f2b301a1c514d54dc3e90245c1344c1ec5ba9d78c9e23b1b2fba1e80
-size 22980
+oid sha256:a4dd5e545f60dc150bbad08d8e6ab94b75a0313fa41eeb9de49340ad6b8bc1c6
+size 23126
