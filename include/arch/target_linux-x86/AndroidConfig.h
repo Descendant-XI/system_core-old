e894be169f2241d1183afdcd3429c5ef4ec70736
Bruce Beare <brucex.j.beare@intel.com>
2010-06-24 14:35:30 -0700

    DBUS is not fully implemented. Disable it for now
    Change-Id: I2286064bffc7e9fa7381aa04d5d20abf11c3aab7
    Signed-off-by: Bruce Beare <brucex.j.beare@intel.com>

--- include/arch/target_linux-x86/AndroidConfig.h
+++ include/arch/target_linux-x86/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12cb7e762e66bb986409af300974422088ff31fe8b7fc8c82fa571d412b59634
-size 6680
+oid sha256:3ff0a2250a2f6e33491a8b962625f742ad6a392dc8f157f802d399d68bde5afb
+size 6686
