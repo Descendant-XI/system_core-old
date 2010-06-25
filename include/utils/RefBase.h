021f8076a79605862329799590a37dc6ab49d60b
Mathias Agopian <mathias@google.com>
2010-06-24 21:49:02 -0700

    Fix a bug in sp<> and wp<> which could cause memory corruptions
    when assigning a smart pointer to another one, we need to make
    sure to read all the data we need from the right-hand-side
    reference (the assignee) before we decRef the assigned.
    
    This bug would cause linked-list of smart-pointers to fail
    miserably.
    
    Change-Id: Ibb554c15fddf909f7737c632b7c80322e80ea93f

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a411dfcb2c4ac61972fbf9e67d467d9d1fdffec4139621f7e043f623ebc13d4
-size 13902
+oid sha256:a6734b697c043b499de82380973b801de7876679908d4350964115f8ebd9cb3c
+size 14120
