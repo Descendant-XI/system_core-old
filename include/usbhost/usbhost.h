30403b4f91b097ae646862bf859bf65b5d2d4c3c
Mike Lockwood <lockwood@android.com>
2010-07-01 11:33:41 -0400

    libusbhost: The client is now responsible for creating the thread that monitors the bus
    This is to allow using a thread that is capable of calling through JNI to Java code
    to report USB device attached/removed events.
    
    Change-Id: Ia58592607a2c1f4357b31072044f5db5617d7f5b
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3264d4dd4dd7112de9312ec0ee8fb1eef3396d552a25a92d67c91a7a4e1a77d0
-size 5459
+oid sha256:e3856d9a5c70649f07b4678e5b1636eeeb00fd3af5b30e0565b67fc838103a2a
+size 5971
--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75f165e0f17914f1cdda3b638c7fe38688b7d736780da62f1e375fff138b02e4
-size 13228
+oid sha256:fb5e33e320a6211456dad54ea2adc6c4dc452c6b3992e0c1bdb7fc2f5515da0c
+size 13260
