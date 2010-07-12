b045134ecefabf03b6bcdd1688826d441fc7422f
Kenny Root <kroot@google.com>
2010-07-12 09:02:18 -0700

    Tweak ObbFile class
    * Move error messages around to clarify the errors.
    
    * Add extra error check when reading a file.
    
    * Seek to the end of a file when writing the signature so the users of
      the API don't have to remember to do it.
    
    Change-Id: I2337051b9f9fa8147c5900237deec790dcd92436

--- libs/utils/tests/ObbFile_test.cpp
+++ libs/utils/tests/ObbFile_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b694e77ff1086720490883b949c46bea02a230b7ddfb4b655e516fa8edfeb597
-size 2200
+oid sha256:cdbd7182891bdedb3e4bacce29d69c4ffffbca2ccd1047ab935946d01ff8d90f
+size 2409
