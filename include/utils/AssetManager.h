156eb75965e5635c36103d1813ca30d97f678c5c
Christopher Tate <ctate@google.com>
2010-07-12 18:21:36 -0700

    Add native C APIs for working with the Asset Manager
    Change-Id: I493b142c4b35e5cc1a1e85283bb5dfb306a6d261

--- include/utils/AssetManager.h
+++ include/utils/AssetManager.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5f3bcec58293d635f98aa074b3e3544bc04a76b371880ef5047243de4e8fc59
-size 12333
+oid sha256:3716f277ea415e5f45ed1da7a1cbc3990f696c715ab9bc3ff4609c1f501f04f9
+size 12609
