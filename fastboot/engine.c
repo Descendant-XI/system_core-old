11b95828c6f396f17989c6254aa1da4360b8658b
Bruce Beare <brucex.j.beare@intel.com>
2010-07-15 08:52:01 -0700

    Check fastboot oem command line length
    Change-Id: I301af09c4ec460c9049b75cc7f4d2c50cfe00ceb
    Signed-off-by: Bruce Beare <brucex.j.beare@intel.com>

--- fastboot/engine.c
+++ fastboot/engine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c0188d991e4bdc87994031e514acca2904dc2d86ac0ad30012e7e8fdb7d8a6a
-size 7663
+oid sha256:4fd2671027caa7416de5f1d81345ccd51ea7f808cb1c867075ffcab98761d13b
+size 7858
