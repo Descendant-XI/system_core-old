fdcee0282cb644a4d52387b8221c1ee8072f824a
Jeff Brown <jeffbrown@google.com>
2010-07-14 22:40:08 -0700

    Don't build framework tests for simulator target.
    Change-Id: I70f29c7eb307e4f3ec5702f4eb9d97b4342e2f36

--- libs/utils/tests/Android.mk
+++ libs/utils/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e08f9654aba4014b192f271b75d9437a6fcd1f6576200d4dc419ff258e763d6a
-size 888
+oid sha256:fcc7f13522f997bb9fafce9707d0e8167363b47bc50e7db981e261505b5a512c
+size 952
