bd648d1996f9ecdab9c0b36f690e49c53f7eb48f
Kenny Root <kroot@google.com>
2010-07-19 10:31:34 -0700

    Initial tool for OBB manipulation
    Add "obbtool" host command for adding, removing, and querying Opaque
    Binary Blob (OBB) information from a file.
    
    Change-Id: Id2ac41e687ad2a500c362616d6738a8ae7e8f5c3

--- include/utils/ObbFile.h
+++ include/utils/ObbFile.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cfb3af765c98ed1d1126fe388d4e3dabf0a0d2ff9eb1f035b96472643ecfe47
-size 2032
+oid sha256:9f7637f75eb9b6255ea3fa651354288bbda6bf2d23e8fecebcf519a018e87115
+size 2130
--- libs/utils/ObbFile.cpp
+++ libs/utils/ObbFile.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91b7206f54778f56ef44d76bff0f7de3ec21865ad0049ba95d97ac6b4e02056d
-size 8346
+oid sha256:a340433a22897fbdf3bc984ab2d588ce1174ed080563788251277169ef07f1d6
+size 8954
