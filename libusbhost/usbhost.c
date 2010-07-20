09161b385c10e21d18fc00139290dd4974eb4a47
Mike Lockwood <lockwood@android.com>
2010-07-20 16:31:42 -0400

    libusbhost: Fix deadlock on devices without USB host support.
    Change-Id: Ic91f99d1169fcdc00474dd6a870eb7e614128bc2
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e962f8e987b2e9595950c11dc584b85d459a39aa4879297472accc0d7482755
-size 13394
+oid sha256:0a0062be21453bfae2af6398077c8675ea10fa034ed975d9ee4f487fe3c9b454
+size 13469
