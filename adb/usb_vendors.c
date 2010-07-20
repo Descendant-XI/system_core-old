d5e6d186bdc47e5741de64acbc46b59c7a4a20fd
Takeshi Kishimoto <kishimoto@google.com>
2010-07-20 17:08:09 +0800

    Added Toshiba's USB VID (0x0930) upon their request.
    Change-Id: I9f7fca6b98b0664ef9af4f7b86f0f5863e91e4c1

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85af110c210120971bbc8b49fa23d829865c4287d16ebcfe576bd09c4aa33d01
-size 5365
+oid sha256:548811b920f953edc00b36b1567f1080605fd03892ef2ac5909d4f6ac7c2b215
+size 5454
