49e504741ea2d6f2e9227f4d3f6a3c8e0ab8838c
Nick Pelly <npelly@google.com>
2010-07-22 10:59:55 -0700

    Allow OSX fastboot to work with more devices.
    ClearPipeStallBothEnds() appears to fail on some devices, but if we ignore
    that error then fastboot does work. So ignore the error.
    
    Change-Id: Ic866ece28a57c081e34079e1a032fa8155301ddf

--- fastboot/usb_osx.c
+++ fastboot/usb_osx.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0c305256c9102c1842b8bec8b0418cc1ceead50a306dfab53e1a7ed5fdd7b5
-size 16387
+oid sha256:a445c01f2c73a2bd1ddf90d37270a1188c893e0842afeb1ab4972e745e4c881b
+size 16356
