f572418beabb0448dece1c5d5c11612fa636e23a
Christopher Tate <ctate@google.com>
2010-07-29 13:16:38 -0700

    Attempt to fix the SDK build
    On the assumption that the local min() function declaration is in
    conflict with some 'min' #define floating around, rename the local
    function to min_of().
    
    Change-Id: I62aa27f213c6093cc78805de611cf4aa75f0eef2

--- libs/utils/StreamingZipInflater.cpp
+++ libs/utils/StreamingZipInflater.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:559c8cd945115b977a6a87d58fc47d5f926c884bc64113a3ff047a1bc961f8c5
-size 8029
+oid sha256:eec017cd5583bec6cb8a63efbc3423aba678b8f4e4209e5b3b437098fa57b493
+size 8041
