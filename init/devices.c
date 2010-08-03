4b3f2ee05e02eb33ed740fd9387acdf2bb336fe8
Benoit Goby <benoit@android.com>
2010-08-03 14:36:02 -0700

    init: Add symlinks to USB character devices
    Since we now support USB host mode, devices might get a different device
    file depending if other devices are connected on the host port. This
    patch creates symlinks to USB character devices that depend on the port
    devices are connected to, but not on the order they have been probed.
    (e.g. /dev/usb/tty2-1:1.0 -> /dev/ttyUSB0)
    
    Change-Id: I285bcc2d59446fbff8a7abbe39c21781f3bb4bd3

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b1a9fb5eae75a6a483f1bf4b1676b2bf91bacd741b81a300df848179af5f69
-size 17904
+oid sha256:1914b20f8e1c5fb4cfb391f3818f0d503ffa1c4002361552423b782395d8d60a
+size 19179
