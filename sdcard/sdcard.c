52bc2b7fcad940e70e2c785197e4682c550044be
Mike Lockwood <lockwood@android.com>
2010-08-20 10:29:29 -0400

    sdcard: Add support for the O_TRUNC open() flag
    BUG: 2935163
    
    Change-Id: I9f76b24147b2f87ddb7869bb72baac03e86ef7e6
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f4b46d247a18301c0ec9991b025be2c790779985fac6d55be881fb57ee8c9fa
-size 21089
+oid sha256:b131cdc1a8368a8ea8c478f0c007feed7d5b02b86fda1a346a916247c71cc64d
+size 21107
