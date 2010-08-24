7bfca8c5e7253eea9aa29830e4b7accfeaac5a32
Maarten Hooft <mthooft@google.com>
2010-08-24 15:24:50 -0700

    Fix the pagesize argument parsing
    Change-Id: I8a3d1a2a70c5ad972d3e150b320ee2184c9d5b4a

--- mkbootimg/mkbootimg.c
+++ mkbootimg/mkbootimg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bad28edffe7888cde3181a4af88106b148f14c5847d6e338ebeb00bdf66f013
+oid sha256:cfc8e210616f337e120a3a53d4c71c8fa9a5b7b0331deabbba595e9d24207cc8
 size 7133
