002717726be07dc013d58206419ab9f59565472d
Robert Greenwalt <robdroid@android.com>
2010-08-25 11:18:30 -0700

    Fix sim build - remove unncesessary header.
    Change-Id: Ibeba75848eaf732b7d0c7aecc9e7be8dbd509a1c

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:907b3a67f68299301e78cd0d4a0e162a6e060e55f10442d56e9a313b21c17b93
-size 15081
+oid sha256:eca3f2309514c12c31d4239c4bdf122029ac8ef65b1e0a3739efec8efd9b8fe5
+size 15061
