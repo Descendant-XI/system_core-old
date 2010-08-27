c731528ca92076c00270d715a2b3068f3a635a3c
Hristo Bojinov <hristo@google.com>
2010-08-27 14:00:57 -0700

    Debuggerd now outputs the offset of libc.so on the device as a separate item.
    Change-Id: I350c9ddb496a76d1ee897ae84893bf0e86abd45c

--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d06f3c4e73e15c47c7d0efe34f97630785774b382821bbbaa90c77299e2d628b
-size 23949
+oid sha256:e7acdd1689acb9303ff6790e8af1526cebaa7675a7110687be98b08eaecb6126
+size 26098
