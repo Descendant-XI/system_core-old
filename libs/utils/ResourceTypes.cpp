c4e89f0e904f3dfcfb126bea27e32361eb45e324
Kenny Root <kroot@google.com>
2010-09-02 14:58:47 -0700

    Add better debug output for ResourceType errors
    An error code is returned as -ESOMETHING, but the debug code was
    printing out 0x%08x for these codes making -74 print out as 0xffffffb5
    which is kind of silly.
    
    Change-Id: I7d77fb3da2e146845949f121404f662b47288e61

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c59e161fa86cacac3ad90c7bafc256dd4666b09606b35835b9b2826be7cf50b
-size 165288
+oid sha256:a21e7d5d81e4f6c72ea7b8467e457d51e034fb321d4e10e12ff3c58d9d0562e3
+size 165287
