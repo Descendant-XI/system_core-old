3c148d76e6a669c8c408bcbd08d33d0ccd559f79
Dianne Hackborn <hackbod@google.com>
2010-09-03 17:07:07 -0700

    Add system property to turn off scheduling groups.
    Do this:
    
    adb shell setprop debug.sys.noschedgroups 1
    
    Change-Id: I6e06a74205fd45ee1526ce71fe33944465d39984

--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b0cd23cd2faebc5dfe8090ffdbd5fe7347df675b55ceb4d2024fe1e10d07cbc
-size 23056
+oid sha256:87993803c50d36dd3357d0b7357f6a6ca774d77d685c35ded903f530ff61dc7c
+size 23722
