8b665d1195ee96effe5be6177180af4e80d3dad8
Dianne Hackborn <hackbod@google.com>
2010-09-09 15:50:18 -0700

    Always set the scheduling group when starting a new thread.
    Change-Id: Ia33acf13fc3752707f3819928c36315e223fa1bd

--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:075023bdeba4de75b5c4e5710aca258674fb63996600d4981a8eb0237de736a5
-size 23756
+oid sha256:a74ea41b51e6f79e281c996db680d29369c0f908e2023cf1e84742477dd9b0d9
+size 24068
