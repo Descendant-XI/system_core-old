87fe1f9610095fc7f4d83b84fbaef14d19f8cfcb
Ken Mixter <kmixter@chromium.org>
2010-09-17 13:50:42 -0700

    Increase number of enqueued crash files, sending rate, and fix minor bug.
    Up to 32 crashes per spool and 32 sends per day.
    
    BUG=6355,6782
    
    Change-Id: I99e86265c035ad62937aa135467681be8ef70f9a
    
    TEST=UserCrash and CrashSender
    
    Review URL: http://codereview.chromium.org/3414006

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b9b26d7734c7cc7ad3b007a97fca8b29bfb3f4b3257d642cb00667a67fb565c
-size 6000
+oid sha256:4bf150f63fd079735b348442a3e04f1066651251e725e48f87a27a231748dc1a
+size 6508
