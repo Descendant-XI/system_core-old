026fcc95017d49ccf6539604bf9220ac0ec45279
Jeff Brown <jeffbrown@google.com>
2010-09-16 17:04:52 -0700

    Ensure input dispatcher and native looper handles EINTR.
    Change-Id: I0a42db5f273b9bfe4ab174e4ee65d5d852f9f6bc

--- libs/utils/Looper.cpp
+++ libs/utils/Looper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ff17cf35238f9c94d451390085cf673474988f5a8331f011ca3c4911cf971f
-size 11780
+oid sha256:31a2fe3ca5e0982dbc28261f2c64c94c12c5a1cc421ece9db970803688a7bb2d
+size 11922
