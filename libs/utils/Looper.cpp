8d19c299f952092241f72f6e9f4f39deff1ff747
Jeff Brown <jeffbrown@google.com>
2010-09-17 17:01:23 -0700

    Reduce lock thrashing in native Looper.
    In the common case, there is nothing interesting happening on the native
    Looper besides occasional wake ups.  There is no point grabbing the
    semaphore then.
    
    Change-Id: Ib5c426d0e158dfa37891b7ff5537b6f833592fad

--- libs/utils/Looper.cpp
+++ libs/utils/Looper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31a2fe3ca5e0982dbc28261f2c64c94c12c5a1cc421ece9db970803688a7bb2d
-size 11922
+oid sha256:a89c68c1e2eab8a969214038a98727f87c945998195f81cb3e1b80a4e48a48de
+size 11938
