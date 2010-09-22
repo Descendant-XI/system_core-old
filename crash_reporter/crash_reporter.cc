e53ab76f8913eea41d11a6e9e7ade60662b24053
Thieu Le <thieule@chromium.org>
2010-09-21 17:07:05 -0700

    Fix build break.
    BUG=6446
    
    TEST=unit test, logging_UserCrash
    
    Change-Id: Iac9f5484d9218ef91ae606ef410409df5ec16382
    
    Review URL: http://codereview.chromium.org/3380022

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26f819d619ffa2b1839c9dbc7503cb9fe43f28ebb7a2f9f3bccf05fc7a4b9214
-size 5830
+oid sha256:8dc4263464e6ca2568a9940142149029772caf8d86edcb46836be0f3a7c10d32
+size 6091
