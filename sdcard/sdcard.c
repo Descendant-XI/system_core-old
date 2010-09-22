44304a2c76936da1ca1f114c32f58c628e511a54
Paul Eastham <eastham@google.com>
2010-09-21 17:14:31 -0700

    Partially implement SETATTR for sdcard/FUSE
    Handle truncate cases within SETATTR so that truncate() and ftruncate() call
    will work.
    
    Change-Id: I5a9862dcaa6ca7b5e9115cb5d3bfed88787fa7ac
    Signed-off-by: Paul Eastham <eastham@google.com>

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b131cdc1a8368a8ea8c478f0c007feed7d5b02b86fda1a346a916247c71cc64d
-size 21107
+oid sha256:e6107c87ff430e6d87803b9d0d1cdecb0494a70c347a39990818168be925c908
+size 21652
