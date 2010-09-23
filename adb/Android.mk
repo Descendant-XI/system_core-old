292486bb2708dfe76294452514465f11ac6a8782
John Michelau <john.michelau@motorola.com>
2010-09-23 17:08:34 -0500

    Build adb for target-as-host
    Added a new target for adb, so that the adb
    host executable is built for the target as
    well. This allows the target to connect to
    any Android devices which are attached to it.
    
    Bug: 3022194
    Change-Id: Ib01983e70b75cec40a9ee161da7f4cf1343eecf2

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbd998cf1af661d162008c322ba0ef353e4ed2e9f6dd9578945ae17ac8e89849
-size 3491
+oid sha256:2a5260fb582cf39faf1f19f85ebfb24fef60541186a8506fda9c61f788701cca
+size 4157
