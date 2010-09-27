da2c5bc9e170e0d5e8aca1cd147988795117ea1c
Jeff Brown <jeffbrown@google.com>
2010-09-26 22:20:12 -0700

    Add suuport for splitting touch events across windows.
    This feature is currently used to enable dragging the start and end
    selection handles of a TextView at the same time.  Could be used for
    other things later.
    
    Deleted some dead code in ArrowKeyMovementMethod and CursorControllers.
    
    Change-Id: I930accd97ca1ca1917aab8a807db2c950fc7b409

--- include/utils/BitSet.h
+++ include/utils/BitSet.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08bbaabe81879fe148782f4ad6074cd9d592ca9035f040ea3cab28c0117c1cab
-size 2222
+oid sha256:f1438e0e73292ca58afe73b3aac4e3fa82d51d3d66705ad798187b82849ef2c8
+size 2348
