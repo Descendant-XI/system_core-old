703314df90f4efda964d000b4c42b0559908452f
Andy McFadden <fadden@android.com>
2010-10-01 11:29:48 -0700

    Use DMB ST for store fence.
    Change-Id: Ic713f30fcec0f2d2eb486c549917575ff0afa82f

--- include/cutils/atomic-arm.h
+++ include/cutils/atomic-arm.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e20c85851654789c5b41d2bcb5b3c75214c069a0c38ff632f23df6b113aa4a00
-size 8995
+oid sha256:356fcff20fa34a3992680baa459d4b398985038eb577b508879a9d65d23be2e9
+size 8935
