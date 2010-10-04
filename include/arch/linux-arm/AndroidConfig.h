5da1230891b30eb10a287e1aeea94c11f00a5db2
Kenny Root <kroot@google.com>
2010-10-04 14:43:15 -0700

    Add HAVE_PREAD definition to platforms
    ZipFileRO uses pread() to guarantee atomic reads at an offset, so use
    the HAVE_PREAD flag to trigger off of. The only platform that doesn't
    implement it directly is Windows.
    
    Change-Id: I52d4e61010bb97e2d50af09943a2e94c0417bb5f

--- include/arch/linux-arm/AndroidConfig.h
+++ include/arch/linux-arm/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e073a8db167741b689fcbbcdab7c8b1476127b1e286009543f18b71b7daa4d30
-size 7408
+oid sha256:2cbd9377530ee037dad091102ce1e36477b45b5b30d98623b92ec535cfa2e0d1
+size 7465
