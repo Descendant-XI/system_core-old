04255da4acc4c35f8b3fd36d35bf41cbb2b52bc6
Jeff Brown <jeffbrown@google.com>
2010-10-07 16:17:57 -0700

    Revert to using epoll_wait().
    This change depends on the kernel having been patched to use hrtimers
    instead of jiffies for scheduling epoll timeouts.
    
    Change-Id: I216bc1c4f565e67ebcb3d2ba4280cb615932bb9e

--- include/utils/Looper.h
+++ include/utils/Looper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b79b6dd6d7d4ee73697c0c0f37e19acb52df3cec601e48941ca68fa6556d31b6
-size 9933
+oid sha256:83a52cda528d1820fcd362dfc508d68d46a5c1951d34b1d1c3d3eb19df458b10
+size 9876
