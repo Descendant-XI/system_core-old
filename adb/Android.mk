9b98994923cd245e1cb9a382f54b88c030c2bc98
Benoit Goby <benoit@android.com>
2010-10-06 17:11:59 -0700

    Don't build adb for simulator target
    Change-Id: Ibe4ef6bb111dc7123e53262553deb8f5e25e9391
    Signed-off-by: Benoit Goby <benoit@android.com>

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a5260fb582cf39faf1f19f85ebfb24fef60541186a8506fda9c61f788701cca
-size 4157
+oid sha256:9ddc7301da6561fed182ce636d4e337f6515952c51dedce7f9f6b4295bac3c12
+size 4196
