42f78250ca9540264bac66ffae23f3c08ccb4aa9
Xavier Ducrohet <xav@android.com>
2010-10-15 10:25:54 -0700

    Add Asus USB vendor ID to adb.
    Bug: 3099385
    
    Change-Id: Ic068fbf58abd23f4bddfe13ae33854a8b4053cf8

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71847e793d74c4d5700bb146fdd686adf705c2a139d483179f73c40bd7bd4502
-size 5638
+oid sha256:83a140739f3dfe0cf776bb4549f803745b69b459faec447c80ccc8acf7e87186
+size 5721
