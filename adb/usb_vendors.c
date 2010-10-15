4df025483d8c4e7d664c6a3d3f2efeb616d50941
Mike Lockwood <lockwood@google.com>
2010-10-15 18:14:22 -0400

    adb: Add support for Philips's USB vendor ID
    Change-Id: Iaf2e3f6f111198740703b5c3581d95756f797224
    Signed-off-by: Mike Lockwood <lockwood@google.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83a140739f3dfe0cf776bb4549f803745b69b459faec447c80ccc8acf7e87186
-size 5721
+oid sha256:a00ccd84022922b3f67593e712cc0435ade9900cd4aa87c375e91821b8e42eb9
+size 5810
