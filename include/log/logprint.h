1676cbd8751dfd5df1168bdc46e28cf6b013e839
Pierre Zurek <pierrezurek@gmail.com>
2010-10-17 22:39:37 +0200

    liblog: logcat: colored output.
    This patch adds a new '-v color' option to logcat so that the output is
    colored similar to the ones in DDMS. Simply type "adb logcat -v color"
    to use it. Works well with bash in gnome-terminal. NO GUARANTEE IT WILL
    WORK ON A NON xterm STYLE TERMINAL.
    
    Signed-off-by: Michael Zimmermann <sigmaepsilon92@gmail.com>
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Change-Id: I9189c5f27fed991579edbcbc6834536eb8112152

--- include/log/logprint.h
+++ include/log/logprint.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aceeb00dd91117b7a3b9e2d4d73b688c2e311067870f467ab92c2e74331f0c81
-size 3739
+oid sha256:47ecae79cb6edc7a11bbafdde84ec3afe3529e1232d68c3c5803d7a7a1078ac2
+size 3757
