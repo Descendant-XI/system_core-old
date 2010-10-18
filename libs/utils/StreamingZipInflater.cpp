502a5750f794b1fbbe8ba4eee2f4180a08e5f3ee
Joe Onorato <joeo@google.com>
2010-10-18 19:13:23 -0400

    Reduce logging.
    Remember, the system and main logs are
        - Shared resources
        - Primarily for recording problems
        - To be used only for large grained events during normal operation
    
    Bug: 3104855
    Change-Id: I136fbd101917dcbc8ebc3f96f276426b48bde7b7

--- libs/utils/StreamingZipInflater.cpp
+++ libs/utils/StreamingZipInflater.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eec017cd5583bec6cb8a63efbc3423aba678b8f4e4209e5b3b437098fa57b493
-size 8041
+oid sha256:4b56449088360658135ce4411d7b6c3bb2ac7c7e0228aaec47e85e6763f750b6
+size 8062
