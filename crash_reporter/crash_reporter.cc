e7f4a381854f1a137112e9355996bac4d59f1a8f
Ken Mixter <kmixter@chromium.org>
2010-10-20 14:38:53 -0700

    crash-reporter: avoid deadlock if dbus-daemon is hanging
    Change-Id: I40ba7fb556217bf0bea156e0496145d7195a1229
    
    BUG=7783
    TEST=sudo pkill -STOP dbus-daemon;sleep 100&ill -SEGV $!;pgrep crash_reporter shows crash_reporter finished dumping sleep, even though dbus-daemon is unreachable.  Also ran UserCrash and CrashSender.
    
    Review URL: http://codereview.chromium.org/3976001

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15e0726b6d94d187620b9c4ad0f09efad9022a050b5540fa20b32d579aa6f6e8
-size 6042
+oid sha256:ea251fb943aa51702f0e17cbe917c0d051040f638911c0c5acb65d93a71a60ca
+size 6473
