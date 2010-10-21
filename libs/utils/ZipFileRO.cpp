c8d64cde2efd31e20c61294a7106f0bb63a6ee16
Kenny Root <kroot@google.com>
2010-10-21 15:18:28 -0700

    Initialized check in ZipFileRO::findEntryByName
    If a ZipFileRO object is uninitialized, the hash table will not have
    been initialized. This condition wasn't checked in findEntryByName.
    
    Bug: 3121109
    Change-Id: Ib696e0e7e0cb4dd0fb2e456d6a847e5e8f4fe14e

--- libs/utils/ZipFileRO.cpp
+++ libs/utils/ZipFileRO.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc391026cb5a096eac476ef98e2133b618eaf965fd6fce150a5dd0cc9cfa49a1
-size 27689
+oid sha256:716d348a2caea946e4488e9db81f38a3e2233a15b240753a6c05a5565d4bde6f
+size 27897
