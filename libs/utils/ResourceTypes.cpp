db4d9291ee9eee27ddb59240ad93b70dbb0212ce
Kenny Root <kroot@google.com>
2010-11-02 11:27:21 -0700

    Fix default return code for getResource
    Reorganization of getResource to allow for other densities accidentally
    overrode the default return code for getResource from BAD_VALUE to
    BAD_INDEX. This corrects the default return to BAD_VALUE which restores
    other things to working.
    
    Bug: 3155824
    Change-Id: I13dafff85bc6978c5f5435fc09ab0474c7885c4d

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e48063596009aa2b2391f5b3a133262fda830707b5cc6aaff9390da50515867c
+oid sha256:c0bb457a6d1b7afde073bf31c46027eb7b1663590957aed56fa81737eec307dd
 size 166003
