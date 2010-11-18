773f6273ffb1e4cd9622a32808a230fb06f68874
Nick Kralevich <nnk@google.com>
2010-11-17 16:55:42 -0800

    check the return value of setuid and friends
    Under some circumstances, setuid() and family can fail,
    returning a non-zero value.  (see "man setuid" for details).
    If this happens, we want to ensure that init doesn't spawn a
    process which has root privileges when it's not suppose to.
    
    Change-Id: Idd03f2c8f82a7eaf6e696b5bcfe308e51ea58b52

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df946d7d82e65096914dacfa7198df791f4e8ce0c577371f0c315edc70b20119
-size 22007
+oid sha256:f1f7c32b2f5e216950c6d88520d71c30b4af415b1daee1daac929b1309bfdfff
+size 22358
