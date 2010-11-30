7dbde15f4c6b191a25eecf6b54feea57c548630e
Jeff Brown <jeffbrown@google.com>
2010-11-29 17:37:49 -0800

    Support non-orientation aware keyboards and other devices.
    Fixed a bug with dpad keys on external keyboards being rotated
    according to the display orientation by adding a new input device
    configuration property called "keyboard.orientationAware".
    
    Added a mechanism for overriding the key layout and key character
    map in the input device configuration file using the new
    "keyboard.layout" and "keyboard.characterMap" properties.
    
    Also added "trackball.orientationAware", "touch.orientationAware" and
    "touch.deviceType" configuration properties.
    
    Rewrote the configuration property reading code in native code
    so that it can be used by EventHub and other components.
    
    Added basic support for installable idc, kl, and kcm files
    in /data/system/devices.  However, there is no provision for
    copying files there yet.
    
    Disabled long-press character pickers on full keyboards so that
    key repeating works as expected.
    
    Change-Id: I1bd9f0c3d344421db444e7d271eb09bc8bab4791

---
+++ include/utils/PropertyMap.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b92e8b078567f70482d0932a0ee45d828a3d316056ea025f35082a5bdfc52958
+size 3208
---
+++ libs/utils/PropertyMap.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59e4514a51fa54e3fe3c0318f27f6c4010c163a688603fa2fdea77024f6c6249
+size 6205
