49770a035daf2c2f4573b55c2884c7cbf69e5ab7
Erik Gilling <konkers@android.com>
2010-12-09 15:28:06 -0800

    libusbhost: add usb chapter 9 include to usbhost.h
    Change-Id: I4dcadf8e8d9b25782351c9aeefb0d2cea81bbc9e

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4928b067be62ede26fecfb9bd9b9b90e2c62fb240aae740e4b523edd0fe42cd9
-size 6734
+oid sha256:f721663d49ceaa1f8dbbf9f8f4c58dad94bc718579aa54639060e0ce340b0ff2
+size 6879
