035661b1219964e365a11cadd440e95e502aab18
Mike Lockwood <lockwood@android.com>
2010-12-15 12:58:04 -0800

    libusbhost: Fix breakage due to not storing dev_name in struct usb_device
    Change-Id: Iefac9b31f900edb2dbd594f9eff9113b2038757d
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa7f495ba7d7e032294f3999caca4c52b87e7a58572955274e108cdfc6136de6
-size 14713
+oid sha256:c04da9a14621f5d10f2c920d0d1f9221a62ecad0149520424f700f7f12c6c582
+size 14784
