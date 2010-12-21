e4599d71f3a657aeeda11e8d7e1e1f165dec1c44
Irfan Sheriff <isheriff@google.com>
2010-12-21 10:31:05 -0800

    Reduce polling interval to 200ms
    DHCP success can take up to 2 seconds to
    be reported to the statemachine. Move wait
    time after check and reduce it to 200ms
    
    Change-Id: I6b2546f80435a84ee6f44f4d91f348bacfdc8433

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cb3990b14f5c90e3278b01f2307c8cd0863a7f3d59ed1bd9c14e66840f27d9e
-size 7977
+oid sha256:1e2fd4a76256be62d3d5b0d08546d343959217bff60f25a57500bfec4c04baef
+size 7992
