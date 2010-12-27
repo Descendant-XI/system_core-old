e71033d3cc8c1329d1b6987c9cdf4f9541c013e9
Stan Chesnutt <chesnutt@google.com>
2010-12-26 22:15:00 -0800

    Add NETLINK_ROUTE processing to the netlink client code, so that Ethernet physical-layer up/down events can be tracked.  Upper layers will use these events to enable/disable Ethernet connectivity.
    Change-Id: If07c30c4d79f26cf94eb5597be651ebbf9d072c6

--- libsysutils/src/NetlinkListener.cpp
+++ libsysutils/src/NetlinkListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:402d39bc52e407048dcb278433d6d0d909dbdce4e349da31317d7be6784cb544
-size 1422
+oid sha256:72184c730c58bb234fea7dd380d81e2d7b8f2b0dcd8ca72cd5379b6bf55222d4
+size 1479
