3579c6f6e0a3e1404bbe8b2dcec319bfc3547ebb
David 'Digit' Turner <digit@google.com>
2011-01-06 14:11:07 +0100

    adb: improve debug traces readability.
    This patch makes the traces easier to read. For example transports are
    displayed by name/serial instead of their hex address.
    
    Change-Id: I7e8df44ddbec19754d63d989bd56485998b4627b

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e263cfc0b619ab0f1ddca3c93971e277920333318fed96caf7d2176130870b56
-size 36682
+oid sha256:3bd3adf95efb5eb079122a29fe888b9f78b209fad6062839ee47ebda17553bb4
+size 36681
