264bce0e7783c3583e7295dd1a6129ebdcd8c1bc
Mike Lockwood <lockwood@android.com>
2011-01-13 13:38:42 -0500

    sdcard: Have the -f option fix user/group permissions as well as file name case
    Change-Id: I280ded6ce79fb11752c89ebafa663d7ee29edebc
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce2855bc5f6a177938093aafed1a1b9cc996d00786613b919cd91c50f772a3b
-size 26978
+oid sha256:52bbceb1b13e9ce263ab59f01564dbd50ce0479287eadc6e10a01c1bdb5ec9eb
+size 27024
