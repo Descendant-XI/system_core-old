a1ea25f0da15d93bf3d4e487b51a04c803b5a85f
David 'Digit' Turner <digit@google.com>
2011-01-17 03:29:04 +0100

    libsysutils: Fix potential overwrites in FrameworkListener
    + Handle EINTR in read()
    
    Change-Id: If7d486dd4fb5666ce16ef36dca5f417da23e0b73

--- libsysutils/src/FrameworkListener.cpp
+++ libsysutils/src/FrameworkListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb71b99d96f325b3166e81802083ab3fc6b2fa9409874aeb91ccf496fa35180d
-size 3670
+oid sha256:0c4b58ae7324ef2afa548c8f2a26bcff7e76c4d5d8bc8e28385af7ca38a6a6b1
+size 4288
