6b8efab8f5c5a8bfc4080827f79e9afd86de4e16
Daniel Erat <derat@chromium.org>
2011-01-21 11:25:45 -0800

    metrics: Update comment about adding user actions.
    This points people at chrome/tools/extract_actions.py,
    where new actions can be registered without needing to
    modify C++ code (once http://codereview.chromium.org/6266011/
    is committed).
    
    Change-Id: If7ceaa38939ab9c1594aacd999e0ec86c4541d41
    
    BUG=chromium-os:10696
    TEST=none
    
    Review URL: http://codereview.chromium.org/6320009

--- metrics/metrics_library.h
+++ metrics/metrics_library.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:584181601d5f06106a828b4f211f7c6a887978d106a4da7d4c9755efbe7eb789
-size 5289
+oid sha256:082040a337b4aa05e241661ff46d1aee4b00e3758049eac9aa6349e4c9d5f8ee
+size 5436
