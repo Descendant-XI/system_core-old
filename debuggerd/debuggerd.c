f5f3e581ffb87b81d0c5da7ae76051bb2886e105
David 'Digit' Turner <digit@google.com>
2011-01-21 04:27:12 +0100

    debuggerd: properly unblock signal handler.
    This change ensures that debuggered properly releases
    the signal handler that invoked it after the PTRACE_ATTACH.
    
    The previous code simply did a close() of the file descriptor,
    but for some reason, this didn't always make the read() blocking
    the signal handler exit. Instead, the thread would stay blocked
    and never fault, preventing the generation of a useful stack
    trace.
    
    Change-Id: I6b0579041165a710d74ec1bece113ff7b828aed4

--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca4575f62c7c9ed039454d20913d8a81c8efedb71f7b4ad6546945b4d81a9699
-size 21948
+oid sha256:8eaccf8cfc8e7ea4a213c7dd64f9b006181a2350f4458fbff01fe90a2099574f
+size 22474
