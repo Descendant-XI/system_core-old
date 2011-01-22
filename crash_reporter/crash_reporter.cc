475046402483d82ae2a669fcc19635f18f1f034f
Ken Mixter <kmixter@google.com>
2011-01-22 06:16:40 -0800

    crash-reporter: Send systemwide stability information up to Chrome
    Change-Id: Idcb3dff3b98951b41a55b9f48968c3608b44efae
    
    BUG=9352
    TEST=
    1) UserCrash
    2) Crash random process and verify Chrome indicates "other user" crashes
    occurred in its stability UMA data.
    
    Review URL: http://codereview.chromium.org/6138005

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11cbe494827779730c56c55aee22bbbc50a2ebdfc47998bf91dc098f1573c149
-size 7369
+oid sha256:f19c0ca06d2d03bec734e7fd26c750c01cbe61f3f8cf9bc33f32ef7982a79e0b
+size 7754
