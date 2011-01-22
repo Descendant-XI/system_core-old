c7b0eb877d07a297b5726e8af32a59cd3f7a5ef1
Mike Lockwood <lockwood@android.com>
2011-01-22 09:17:07 -0800

    libusbhost: Add usb_device_connect_kernel_driver()
    This can be used to ask the kernel to disconnect its driver for a device
    so usb_device_claim_interface() can claim it instead.
    
    Also increased size of descriptor buffer and added some debugging logs
    
    Change-Id: I4945196d957fb8493716eb9b7e5463c06b168ef1
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68aa3a3ad693055a11019541dbe99cbf464a51cb63e5c65674254b9ee32cf903
-size 7053
+oid sha256:bc7a3dcd13a592c1da3a197047e5e80aef39fbd03816f34006bb1b69768f8a3f
+size 7387
--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca249f6dd1bc6ec22decc6a23d2eacac6f8701b0075999b86d5b4c7ee0b6436d
-size 15282
+oid sha256:82f3ff410b2a12217b2a44f1ad844983ec3056d6159568cdb06be47088220237
+size 15699
