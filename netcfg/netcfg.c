5c3ab9c535dcf3974d16d32e8fb5b021a5a7802c
Dmitry Shmidt <dimitrysh@google.com>
2011-01-24 17:09:32 -0800

    netcfg: Add MAC address to interface dump
    Change-Id: I52b3336a66224c9ae19a4c80cc706890742d4130
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- netcfg/netcfg.c
+++ netcfg/netcfg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d310263ae873e2d21aa1a345f54717689922e88a3d089591feafe138fd0f8b48
-size 4061
+oid sha256:1bce51333e1be2652ce18151b96cfe65fb21d37efa2be7c6dca162d292da84cd
+size 4320
