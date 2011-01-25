87eea6342a4f95a062888574c13a987989fe99d0
Brian Swetland <swetland@google.com>
2011-01-25 00:03:23 -0800

    gpttool: useful for partitioning sdcards
    Where @partitions.txt contains:
    
    :128k
    
    xloader:128k
    bootloader:256k
    environment:128k
    :384k
    boot:8m
    recovery:8m
    system:256m
    cache:256m
    userdata:1g
    
    Change-Id: I1221677e5b3dc4d5b19420c4a64d21b587fb7d47
    media:

---
+++ gpttool/Android.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:311e591f5593880f63bff4c5129a2d4148cb46bebeb5b04677c67cbca71b7ab4
+size 227
---
+++ gpttool/gpttool.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f1957faf1b2d7bd3a95fdef684df8388d21c3ca76ad33edef736442ef9eddd1
+size 7427
