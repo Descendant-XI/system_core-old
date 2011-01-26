5edd9211e39370d1c2527f3c4471ce7ccfd1ccbe
Ritu Srivastava <rsrivast@sta.samsung.com>
2011-01-25 16:23:08 -0800

    fix failing thread object run
    A previously exited Thread object refuses to run again, if the
    thread-id of the caller, conincides with the thread-id it previously
    used in the worker thread. Hence reset the previously used worker
    thread-id to -1 when it exits.
    
     Signed-off-by: Ritu Srivastava <rsrivast@sta.samsung.com>
    
    Change-Id: I873925c312a43ec8a16392b98cc959042ff6bfd2
    
    Signed-off-by: Madan Ankapura <mankapur@sta.samsung.com>

--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e74ecd5bd0d012bcfdde06a614c6a23086d7a966e7036c2b5591cd60a3b5fada
-size 24266
+oid sha256:3ebf6f315fcd4400e81b9bc237fcaa7c3bde59e9c07ab6fdeab16d1fcd8a1da1
+size 24340
