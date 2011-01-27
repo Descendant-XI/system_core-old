6c6a9dd2d720e805158fd053ae66a952a094d0a6
Mike Lockwood <lockwood@android.com>
2011-01-27 10:46:19 -0800

    libusbhost: Add call for synchronous bulk transfers
    Also clean up and add timeout to control request transfer
    
    Change-Id: Ibc2d8ac64d0fa90e0a23e6225ce2c49b23bb0d97
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc7a3dcd13a592c1da3a197047e5e80aef39fbd03816f34006bb1b69768f8a3f
-size 7387
+oid sha256:35dc7dfbb095477922d77bc0d3bf488c12f136595a87d146930c0d5acfae2ce9
+size 7714
