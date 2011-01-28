30c7d0fd6fd6dd7b7f69349a5a8183c00586ac76
Dianne Hackborn <hackbod@google.com>
2011-01-27 19:37:40 -0800

    Fix issue #3392073: At times soft keyboard comes up in...
    ...gallery while attaching picture to gmail message
    
    In various places we could block switching the IME target incorrectly.
    
    Change-Id: I7e647fb35f4ea6f2e39eb7efd911420ea9ee64fa

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1160703d6d1f32c55d28b93d91e94dad80c0f2b114291909c3a85c15b3accdd0
+oid sha256:8108ac10cc79b8fa199eb698dd35d8c1d615cf74931d0c4c44be02cff10ff516
 size 168329
