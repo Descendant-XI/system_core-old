99ee8bae633c4b2e6cf19eeb5cee74fa9f53599a
Patrick McCormick <patrickX.m.mccormick@intel.com>
2011-02-04 10:51:39 -0800

    Extend and document the "wait" command
    The wait command was currently implemented but
    undocumented and only used internally. This change
    adds a second option toi (optionally) override the default
    timeout of 5 seconds.
    
    The wait command can be used to wait for a file to appear
    during the boot process, for example a disk or other device,
    and is a better alternative than an unconditional sleep.
    
    Change-Id: Ia11d8419c0ea84deec0e1bf5adfca5bc76ff38b7
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d19ea567ea0460b0cf065797718c11a22d9020bf94a36bb5d8bdf1c805e9b2f
-size 17342
+oid sha256:95a4d6685dcaae1802d6f79a655a55e4285e08e87e16d278fd5cc1bfe4302192
+size 17434
