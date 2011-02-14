cfb3e2546327814d7d22278f8a16fab451e2fb33
Mike Lockwood <lockwood@android.com>
2011-02-14 08:05:40 -0500

    libusbhost: Save endpoint ID in usb_request struct
    Change-Id: I2cf189cee67c2efbcf919c8bae1447a6cb112f02
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/usbhost/usbhost.h
+++ include/usbhost/usbhost.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35dc7dfbb095477922d77bc0d3bf488c12f136595a87d146930c0d5acfae2ce9
-size 7714
+oid sha256:b8e824aef4caa4c2aa12dee443bd1de9091b0545a1d52faffaff6041ca6fbe69
+size 7803
