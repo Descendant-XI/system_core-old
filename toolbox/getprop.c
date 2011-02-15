a470bfef758e49fd2f9737c044435c85ec018e4e
Brian Muramatsu <btmura@google.com>
2011-02-14 17:17:21 -0800

    Fix getprop Format
    getprop used to output a colon after the property name like:
    [property.name]: [property.value]
    
    Add back the colon that was missing, because
    com.android.ddmlib.GetPropReceiver's regex expects it. Without
    the colon, the GetPropReceiver doesn't parse the device's
    properties causing CTS to not recognize the device.
    
    Change-Id: I9bef5ab2b310c831c49c8c51cae7f129167c2dc5

--- toolbox/getprop.c
+++ toolbox/getprop.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13214216218eb5d41786f215cd8ca3e0cb195e5175cd8696115f6a82ae88eb46
-size 1196
+oid sha256:00a1d6afd75e27a712829dc83f33d3ead7e98a00f58b4e03139a05543d08de74
+size 1197
