48e561cb5dfbbd47c2768ec01ff501137c783511
Raphael Moll <ralf@android.com>
2011-02-16 13:36:42 -0800

    Define O_BINARY for non-Windows platforms.
    All tools (aapt, aidl, dexdump, etc.) need this flag to open()
    binary files, or they risk reading corrupted data on Windows.
    
    A bunch of these tools just conditionaly define O_BINARY in
    their C files. Seems it would just make sense to define it globally.
    
    Change-Id: Ia5c0a59e9e77bbdcbce235233555f611207abed8

--- include/arch/darwin-x86/AndroidConfig.h
+++ include/arch/darwin-x86/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf15cc10e1ca8999a65e7327b483c08aa5f3657df5209a5cfc00abfca1e015d9
-size 6596
+oid sha256:bc3727304afb4220428486cb00d5242b6e6476d8e60fb95c7c21f311d5a512de
+size 6764
--- include/arch/windows/AndroidConfig.h
+++ include/arch/windows/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19e864d61ad54a6c9ce9f186228d220801922130cccb5b23857d6545568d059f
-size 7250
+oid sha256:38ee382a117023bdbee782122657e9fffda668fd09fa89741e78baed3e5d1eab
+size 7411
