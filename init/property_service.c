a9f86c1a19d8870e71a668e251021cee1f14bdbb
Amol Bhatkar <amolbhatkar@motorola.com>
2011-02-18 17:54:17 -0600

    property_service: Give radio permission to set net.qmi*
    Change-Id: I771ef22859799b863ab79de9d3248ef610af4dd8

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03531481dd5094fb059055fd1d8ff72486943e8b1dc23e896f0f387535119dd
-size 14418
+oid sha256:ca0dce5bb3879da422043a8ce4dd83f4d90d29b9ff22283ede3d761a5d8dfd77
+size 14463
