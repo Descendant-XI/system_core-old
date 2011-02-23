ebdaa2f4c3ea9e3af6a202aa4b41d27b8c0217c1
TK MUN <tk.mun@samsung.com>
2011-02-23 18:58:36 +0900

    wimax : Fix for multiple dhcpcd service.
    - Seperate dhcp service for each interface(wifi and wimax).
    
    Change-Id: Ib88436b0e468e93ce3199f3b6c00799fd168b666
    Signed-off-by: TK MUN <tk.mun@samsung.com>

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d16d01746a452d5fadfd178a44c351cc450965e8eec79b56ca1bf96861341cfc
-size 7391
+oid sha256:fc07b4ba21aafc37d26d502cf2e078d16e8b667ca3f1217487806d9227e07a6c
+size 10606
