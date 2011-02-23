8dc0990b6938b8e0ae061352af41cebf87211f0c
TK MUN <tk.mun@samsung.com>
2011-02-23 18:58:36 +0900

    Add per interface dhcp client support
    Added when using dhcp on both wimax and wifi. But should be useful across multiple
    services running dhcp now. Also adds a renewal api
    
    Change-Id: Ic850a612dd429cd675b5b0224ff58895f9d8e4d9
    Signed-off-by: TK MUN <tk.mun@samsung.com>

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c04bb0b90ccc283425434f033622cda46a779992ccc778c78f93a8429eaba6e
-size 8319
+oid sha256:965bd5a9b943692df71ce2b1eb6af4cc0dd4827e189c38b77b2f8fc9e1fcba74
+size 10981
