950c0affba44d6f03b30eab4f9d5f04724f42a31
Fabrice Di Meglio <fdimeglio@google.com>
2011-02-24 19:56:18 -0800

    Add TextLayout Cache
    - use GenerationCache for caching
    - move GenerationCache.h from libs/hwui/utils to include/utils
    - add #define for cache activation / deactivation
    
    Change-Id: Ifaf519f0b5e33b087a453e4aa6430162d8438f20

---
+++ GenerationCache.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac26355052a80046dc8367e4334469a5c5f7a40d7baa4ff4179bdba3e7f840f1
+size 6310
---
+++ include/utils/GenerationCache.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd65730cd74d6048d3fb05b7e541898c0df606c2beeb0755a074c8a7c911551f
+size 6496
