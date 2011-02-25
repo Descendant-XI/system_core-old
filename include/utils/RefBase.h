d8916574fffbd67a73fa58f7f2f26b8249c8858b
Mathias Agopian <mathias@google.com>
2011-02-24 18:12:34 -0800

    Fix a wp<> bug where the owner ID would be wrong
    this was introduced recently. we make sure to use
    the correct owner id (the sp) instead of the wp.
    
    Change-Id: I78fdc6ec0c2d3e687278b70442d74d1924b512a2

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ee8aedcc7768600e072290bf755f5e5ef8b711624f57221d01d764438b8c6f
-size 15249
+oid sha256:4b7c8189aab4216ec701b659b901860c4895476983abaf26e4c0f23c35be9b43
+size 15288
--- include/utils/StrongPointer.h
+++ include/utils/StrongPointer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:798a858af02b0b06b8fa1f956ace8861c8a395558d97174b2b4811b5c62ad798
-size 5809
+oid sha256:dea422fcbfc63a4d5736f251b3f712ba32063c1126761b7521c29282292addfe
+size 5777
