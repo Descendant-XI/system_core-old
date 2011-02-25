bc5d100e672a6a27c762186ba11767a23266489f
Darin Petkov <petkov@chromium.org>
2011-02-25 14:19:30 -0800

    libmetrics: Support partial writes.
    BUG=chromium-os:11125
    TEST=unit tests, tested on device
    
    Change-Id: If9066988b86f61cb5bae413b7250d5426854f31b
    
    Review URL: http://codereview.chromium.org/6592019

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd256798aa4045310b1897c6c80478c8f0738a197650961a31b469fe95e91a8b
-size 7959
+oid sha256:0be1c60197ec9fdd66d7feb9eeef2dcf43877dafc71b1ccb7f20f00870abf4c2
+size 8583
