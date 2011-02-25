bf30a3ec50fd318f8699edd162185f7fb278b716
Ben Cheng <bccheng@android.com>
2011-02-24 16:37:52 -0800

    Fix overflow prevention code in memory dump.
    The delta between start and end pointers in memory dump has to be
    multiples of 4 or 16.
    
    Bug: 3486787
    Change-Id: Ie34aa79ffb704ca647805dbc6a16f2c35adf849c

--- debuggerd/arm/machine.c
+++ debuggerd/arm/machine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fadf0889a9063d6acb418a38e57740f36285c6fea5a3c3f588ebcd238a0e8a3d
-size 6952
+oid sha256:fccda8da96337abaee045b205f2be93559950b1007b1bdec8806976ea2e92dbe
+size 7111
