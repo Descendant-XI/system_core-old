043477a3c5eb73a996f19026e3d7533acd415b9a
Thieu Le <thieule@chromium.org>
2011-02-28 10:51:18 -0800

    Modify crash-reporter to collect crash information for unofficial builds.
    BUG=chromium-os:12207
    TESTS=logging_UserCrash, logging_CrashSender, manual test which involves
    purposely crashing a test to determine if minidump information is properly
    collected
    
    Change-Id: Id2195125289a695515b1722442116d6a6d9c4895
    
    Review URL: http://codereview.chromium.org/6559003

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dc7a5c0d03821cec0c1a1e23d70eca3a298ad20601695d32b5b7c18d79508b2
-size 17975
+oid sha256:5c208f62b10c45d4eca70cdcfb1123ae496b7e2ac167d011568f12ea20f3edf1
+size 18219
