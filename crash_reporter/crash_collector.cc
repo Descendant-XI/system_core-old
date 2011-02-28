5134045c8d467e66422341af2692037503112d36
Thieu Le <thieule@chromium.org>
2011-02-28 12:02:59 -0800

    Revert "Modify crash-reporter to collect crash information for unofficial builds."
    This reverts commit cb5a147e46dbfa454df9d56b4da23480742597c8.
    
    BUG=None
    TEST=
    
    Review URL: http://codereview.chromium.org/6596045
    
    Change-Id: If07530a743b19a76e9a15ef08825c45c38f9dd2b

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:233c7bb13fbeb1ddc81409bd9ab7f22499bb28159b79deedc67303d39b42df9a
-size 14243
+oid sha256:dd90dc862fb29411cdf01249764006c953f7a2bfceab914f7acf6dbde869ec8c
+size 14047
--- crash_reporter/crash_collector.h
+++ crash_reporter/crash_collector.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fa388595634ee62a32d3a12955fb54f6ef59c9d4ec35f7f38e473fe8c9057b7
-size 6201
+oid sha256:e8fec61bbbaf459824189fdb52e4ab7adccba173801db22c397e91fb1b2b36f5
+size 6108
