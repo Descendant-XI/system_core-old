be576b10ee36cffbef81ba56dd38961396eabc8a
JP Abgrall <jpa@google.com>
2011-03-01 23:35:07 -0800

    fastboot: fix arg handling causing no-op.
    Change-Id: I89d4a6d14e542d8f2d7832d94b0a7cee6eb88305

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e06daec56266df004fb30c5ee8912f849d4e5c9c0546dc589fc857f69f58401f
-size 20527
+oid sha256:17cd403868dc24c574ab02f7f4cc7207178a7fdc17ecd120efb0969b6c1a8312
+size 20529
