fe7108e76fe9283d6961378c4af58dbaa7f23676
Raphael <raphael@google.com>
2011-03-02 16:49:34 -0800

    Revert "Define O_BINARY for non-Windows platforms."
    This reverts commit bd70e4aca5680acb7d5da5500b7e8c618fb499f2.

--- include/arch/linux-ppc/AndroidConfig.h
+++ include/arch/linux-ppc/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0fab17ddcc4b78751848be9dee78571946c56846e88a9f4795f9737dc451cf4
-size 7126
+oid sha256:3d83d0c6f89e45e5f1139e41857c9774720e27739271b7c5bd4a1c55f7aa26b6
+size 6958
--- include/arch/linux-x86/AndroidConfig.h
+++ include/arch/linux-x86/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71215e12c2dc50ec3e9726813f7d3500867a61daeeb6a72e610bcae364e64216
-size 7299
+oid sha256:00cb6548ec78ea636b9c0f167d6a7bba3682b2edf5bd04e4216d907f74b50e44
+size 7131
