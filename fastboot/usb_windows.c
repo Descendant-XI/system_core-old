7cdceae8236a20cf77ef37569cc8cc4490f340d0
David Krause <david.krause@motorola.com>
2011-03-08 14:10:16 +0800

    Enlarge USB bulk transfer size for faster downloads
    The default USB transfer bulk is fixed as 4096 in fastboot util code for
    Windows and Linux. Enlarging the bulk size can greatly improve the image
    download speed via USB.
    
    For Windows, adjust the max bulk size to 1MB to maximize the USB transfer
    speed. With this change, the USB transfer speed can be doubled to 20MB/s.
    
    For Linux, adjust the max bulk size to 16384 to maximize the USB transfer
    speed according to MAX_USBFS_BUFFER_SIZE definition in drivers/usb/core/devio.c.
    
    For OSX, the maxLenToSend is already 1MB in code.
    
    Change-Id: If6af8c6301f6f6c2ef345e37241706f16d8f5cda

--- fastboot/usb_windows.c
+++ fastboot/usb_windows.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:381067d7af8ea179af615f51e4ee201b09cf4663a867a784c34f36ae8d8edc19
-size 11625
+oid sha256:f2d37a244a3afbc6b9c987642183a86825a8d5fd6f49a88cebc45d89d6064e8f
+size 11727
