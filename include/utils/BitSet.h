9b47748979357560ab124466ecac3481ffc4f73c
Jeff Brown <jeffbrown@google.com>
2011-03-09 17:39:48 -0800

    Use touch pad gestures to manipulate the pointer.
    1. Single finger tap performs a click.
    2. Single finger movement moves the pointer (hovers).
    3. Button press plus movement performs click or drag.
       While dragging, the pointer follows the finger that is moving
       fastest.  This is important if there are additional fingers
       down on the touch pad for the purpose of applying force
       to an integrated button underneath.
    4. Two fingers near each other moving in the same direction
       are coalesced as a swipe gesture under the pointer.
    5. Two or more fingers moving in arbitrary directions are
       transformed into touches in the vicinity of the pointer.
       This makes scale/zoom and rotate gestures possible.
    
    Added a native VelocityTracker implementation to enable intelligent
    switching of the active pointer during drags.
    
    Change-Id: I5ada57e7f2bdb9b0a791843eb354a8c706b365dc

--- include/utils/BitSet.h
+++ include/utils/BitSet.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1438e0e73292ca58afe73b3aac4e3fa82d51d3d66705ad798187b82849ef2c8
-size 2348
+oid sha256:6fa9e90b0585df726e3bc3139fd18a110a21e561dfc630591841f99a1bb0c030
+size 2609
