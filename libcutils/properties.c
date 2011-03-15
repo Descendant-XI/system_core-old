98424b24931d79793a585d4bd49f00b8bf3b4c39
satok <satok@google.com>
2011-03-15 11:04:58 +0900

    do not merge. Fix a race in system properties.
    Backport Iee1ca9b7defd6
    
    This now passes:
    
    $ adb shell am instrument -w -e class android.os.SystemPropertiesTest \
      com.android.frameworks.coretests.systemproperties/android.test.InstrumentationTestRunner
    
      (which tests setting and re-reading back system properties in a loop)
    
      This patch depends on I110b653a which moves property setting
      from libcutils into bionic.
    
      Bug: 3511230
    
    Change-Id: I145e642bd13079677017bb64e361e176fc73b810

--- libcutils/properties.c
+++ libcutils/properties.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4e18599ff06f1af0d5c0b605431553e71e9ada53442121bd268f01e77cfc62f
-size 9249
+oid sha256:34b9fbd242a50c34909a49a921b11709ae7e29700f65eb574e4ffb21aed71018
+size 8446
