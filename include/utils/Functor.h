ee6e05cb5f6273d59005716cd81f2e3c9c872bf8
Romain Guy <romainguy@google.com>
2011-03-16 15:30:12 -0700

    Modify the GL renderer's functor to pass the clip to WebView
    Change-Id: If5efe399ca58f3000b2883e24e9f3736a2025184

--- include/utils/Functor.h
+++ include/utils/Functor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:210ea8b0b8b574a2e78a0c7e3aa6230172bfa31c8c0b5866714f0ab6ab6e430a
-size 976
+oid sha256:817d8096a45514a9ac9841c9eb99d4529cf936567067e3734f6cdaf18451b0de
+size 923
