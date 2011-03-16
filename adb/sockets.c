cb893aee924fc2051ecd6aa57742f8eeef38a842
Terence Haddock <thaddock@google.com>
2011-03-16 09:43:56 +0100

    In serial, skip over port as well if numbers found after colon.
    Change-Id: Icd45f570a9527790eb0cd614a3f77da1a72375b1

--- adb/sockets.c
+++ adb/sockets.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0d756fed823543e3dd3ba9d13e53c7b1a7f2415e7e3b1630d22f0a821965b9
-size 20801
+oid sha256:18f30afaa1b3172f04a28f637397886596020ca719229bfd8af0a89b5ee8f1dd
+size 21627
