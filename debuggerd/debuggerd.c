4ab6e56a3484e1ffc239c393bd202ca10dc614d6
Brian Carlstrom <bdc@google.com>
2011-03-17 16:38:15 -0700

    Revert "Debuggerd now outputs the offset of libc.so on the device as a separate item."
    This reverts commit c031a3b006129ca408eaade325a53a334daee3b2.
    
    Conflicts:
    
    	debuggerd/debuggerd.c
    
    Change-Id: Ib37ae793bbffdb4a81837bc51171cc62b12abe7d

--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eaccf8cfc8e7ea4a213c7dd64f9b006181a2350f4458fbff01fe90a2099574f
-size 22474
+oid sha256:01b95c9eab119cf26b59246b414c07a7fa07cc44fbe921f3244a0c7c6e579632
+size 20325
