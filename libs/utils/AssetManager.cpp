fbd782fd3308a5905962b6e8cde3325826b4c986
Carl Shapiro <cshapiro@google.com>
2011-03-21 20:26:25 -0700

    Include strings.h for the strcasecmp prototype.
    Change-Id: I0659ce2186547274e58f8b2027e16d3e45189353

--- libs/utils/AssetManager.cpp
+++ libs/utils/AssetManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02e95d89d94c27f2fea1f0a95a8d7a21b22cb0dfa49e98bd76d06ff73d845c65
-size 53957
+oid sha256:5cefbaf5f727fdd070e511c7d116c838cd7f494cce479f0a71371da86fa5d37a
+size 53977
