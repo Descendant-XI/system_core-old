4df5db7cc769097d360fe61741074a00a871fe12
Dima Zavin <dima@android.com>
2011-03-24 11:11:06 -0700

    libcutils: add a simple string parameter parsing ("a=b;c=d;") helper utils
    Change-Id: Ifabeb75550fa6e351434e6e6d036b19331daf6c1
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc539f023fe8ee45df609f47bec184b477fadb31fb130636d7638978eec909fb
-size 3688
+oid sha256:61f0436666314f3547a8734544957db9f1274ef6cb57dfa7622804ad5421a929
+size 3935
