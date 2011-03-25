87c6d7d78e84105414336868baf4193823ac6972
Jeff Hamilton <jham@android.com>
2011-03-24 21:40:43 -0500

    Update the value for the NFC shared UID.
    Change-Id: I47edf39dbdf2ebd41f44ca69aac250d8991dec4c

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1983152be22dd62416a757597dead341ef3ae98b8050df02f5ff83be42499aa4
-size 9477
+oid sha256:abbf1ab645a6d264c2da70b366f692a56c1a412621f7dc14f85d5392f126b0d0
+size 9538
