4d2db997778dcf4295e135fd67ec947031e436cf
JP Abgrall <jpa@google.com>
2011-03-29 12:36:22 -0700

    adb: keep mac build happy
    Use FIONREAD instead of TIOCINQ
    
    Change-Id: Ic9db0228a51c546a798c70f08f2e644a1a376120

--- adb/fdevent.c
+++ adb/fdevent.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c632f228954cd6a5c6607c42c19e744297dab8bc3eedafe990a4afbfd868dae5
-size 17777
+oid sha256:6d3713ac51b48c27c2b76b340b74f9d304a049e16996a9ff8abac3635c59b8b5
+size 17778
