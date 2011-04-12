9ec23158122b607040587dbb6bee80f187e9fbb9
Jake Hamby <jhamby@google.com>
2011-04-11 19:46:41 -0700

    Add missing function required to load Crespo RIL library.
    ifc_set_mask() function was removed in a previous change. This function
    is called by the Crespo RIL, so we can't remove it yet.
    
    Bug: 4268537
    Change-Id: I634580ee8d2a1aff069178837cc121c89c5e73f9

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25ab019af6da41dbd76a1f82922a6718f67c1540400cda8ed5d5202f8dd3059b
-size 16920
+oid sha256:ae128097ddf49bcce331871955c425328f1a7cf3e27b09a3f802ac1dd588e8bb
+size 17127
