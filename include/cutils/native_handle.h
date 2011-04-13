abf459aaa3488d78723bea17a5d50ed929825fc3
Iliyan Malchev <malchev@google.com>
2011-04-13 14:01:38 -0700

    native_handle.h: fix declaration
    Change-Id: I452a517ee6982995318802eb4bbb23bd79401c4d
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- include/cutils/native_handle.h
+++ include/cutils/native_handle.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c1cc96133f0512469126809ac42d873059842fe602f9f5fc60c46c0b7524bac
-size 1948
+oid sha256:dc40921bf7e369619be1fe67b8b4e5280bcc0b402c71107050f8546bb4c745e7
+size 1857
