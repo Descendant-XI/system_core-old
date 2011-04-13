9c18e2fc7ca1e230b0afc0cb4d4eddf66cd932bf
JP Abgrall <jpa@google.com>
2011-04-12 22:01:58 -0700

    adb: Don't allow a forced exit if data is still in the socket
    Fixes the  http://b/4271011 "adb shell cat /init.rc output gets truncated"
    
    
    Change-Id: I6df64374b60789595fb18f96d429315dec032443
    Signed-off-by: JP Abgrall <jpa@google.com>

--- adb/sockets.c
+++ adb/sockets.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:985b11b0fbcab47117524742cbb2e7593ace385608718d2b128fabf5a451f9d3
-size 22724
+oid sha256:8ad3af959599bbdeeb89821e3fd01b4b9f3f3267f44f38a9bf6fb630420f0ba8
+size 22793
