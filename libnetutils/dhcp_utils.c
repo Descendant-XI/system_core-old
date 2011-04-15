544921cf13bbb5c71350bec8d13530bd3231ec63
Irfan Sheriff <isheriff@google.com>
2011-04-15 16:04:24 -0700

    Use DHCP server as gateway when it is absent
    Some routers can return 0.0.0.0 as gateway address,
    use DHCP server as gateway in such instances
    
    Change-Id: I7e8db8e619475efce53ce391f77f2c13c3b7268c

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:965bd5a9b943692df71ce2b1eb6af4cc0dd4827e189c38b77b2f8fc9e1fcba74
-size 10981
+oid sha256:8e55171c7b8304a54eaa1247e60128b981b75d35d4008d9785239413bb142801
+size 11195
