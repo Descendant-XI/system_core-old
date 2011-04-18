087509362d3176a030f22e7bce7f18c85b4f195a
Nick Kralevich <nnk@google.com>
2011-04-18 15:55:59 -0700

    Validate sender credentials on netlink msg receive
    Verify that netlink messages are actually from the kernel,
    and not from a userspace program.
    
    Change-Id: I709c0efe9ba0258f6d79ebcde531d7f7bbe780b2

--- libsysutils/src/NetlinkListener.cpp
+++ libsysutils/src/NetlinkListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:402d39bc52e407048dcb278433d6d0d909dbdce4e349da31317d7be6784cb544
-size 1422
+oid sha256:0233af3514dbb07b59d29dbe4aeb2919ca3a457e260ab6153faacc4150766c78
+size 2186
