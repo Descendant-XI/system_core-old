a261932c8cedaef17f9180b996d8011414a78caa
Dima Zavin <dima@android.com>
2011-04-27 22:26:23 -0700

    libcutils: build str_parms test for target instead of host
    Change-Id: Ic1cd1c26b86b051c25db2521bd90b58731e29135
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:023a45b186a198bbdbfd57754aa3add9a2fd83f2d0aa73a38d2c79c7ff20f890
-size 3944
+oid sha256:84482d43626b99be1ee45ef0131d8e796c75e2d15b98f5666d21c2560aef1542
+size 3939
