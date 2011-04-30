c9990d2bbc87b6a548e2ba9f42c1d5008b8aca8a
Irfan Sheriff <isheriff@google.com>
2011-04-30 12:59:51 -0700

    DO NOT MERGE DHCP changes from master
    Pick dhcp changes from master for the renewal fixes
    
    Bug: 3344732
    Change-Id: I42596513f3ef936ef7160d954e3bd26bc51662c8

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c04bb0b90ccc283425434f033622cda46a779992ccc778c78f93a8429eaba6e
-size 8319
+oid sha256:84bb68f4faa6ef58e03a53a1235cc68bbbc75a7f289209352bee1a0ba5b916ff
+size 11181
