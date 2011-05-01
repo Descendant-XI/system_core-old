81e068ccd7f4dc55a8127899826e8bbd3c162020
Iliyan Malchev <malchev@google.com>
2011-05-01 14:05:30 -0700

    move non-HAL definitions to syste/core/include
    This patch moves camera- and graphics-related definitions out of the hardware
    HAL layer and into system/core.  These definitions are used both by the HAL and
    by higher-level code as well.  More functionality (e.g., audio definitions)
    will be moved here as well.
    
    Change-Id: I225f12374ce54fa393640dce53738267d0d703e9
    Signed-off-by: Iliyan Malchev <malchev@google.com>

---
+++ include/system/camera.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f1bda7d7f8f2600b29243d2e503b2e8691fd6ab0051046f8083d7746e3b7ed53
+size 4819
---
+++ include/system/graphics.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1193789a768d00fe4a6d59c75d1936662da37ddc8ad818093338c32f350722b7
+size 3356
