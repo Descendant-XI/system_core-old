0668e71f8c9e9b2d428722a6975df10c06438da3
Jeff Sharkey <jsharkey@android.com>
2011-05-02 17:52:03 -0700

    Create groups to protect bandwidth statistics.
    Introduces new "net_bw_stats" group which will protect reading
    detailed bandwidth statistics.  Also introduce "net_bw_acct" group
    which will enable specific applications to request that their network
    traffic be counted against other UIDs.
    
    Change-Id: Ic0ec04e0d2bb9573f3c2a094ebd6d6a8cae0aae6

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f34f066812f9ffb6474800cd6e63e6372374ce9bc816d7eb435e9957e8e14431
-size 10307
+oid sha256:f0059da5eefdbd846e8107733cea5a6a8aa2324a9a1c3355c75f56697fd851f2
+size 10532
