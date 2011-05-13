281265d2cab7b304446f130e5539e469543e17a9
Chris Masone <cmasone@chromium.org>
2011-05-13 16:57:09 -0700

    [crash-reporter] rolling to libchrome 87480
    BUG=chromium-os:14304
    TEST=emerge with features=test
    
    Change-Id: Ida7067b3578619bbb99c8b5c626fd6301cdccd8d
    Reviewed-on: http://gerrit.chromium.org/gerrit/887
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f0be2bcbb3c30c4ad4f33408e566a566de83f03b1f925827c5be7c9ef2d8530
-size 12367
+oid sha256:5108b984c32d8ba6d23693caf8d9e78790c7ed948b9c1f568e5bbc5744293dd3
+size 12411
--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04a35cfbef1772199f949ad1c516ed972798d40567f3a55e60d1cbe24cefb6a7
-size 18290
+oid sha256:9a3b499ffea74290dd324718de355b7dea84375f37c4326c3ab035240d7ee08d
+size 18334
