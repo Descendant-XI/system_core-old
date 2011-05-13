d880e374b87f2955b6c98200ff47b4a95d22441e
Christopher Tate <ctate@google.com>
2011-05-12 17:47:12 -0700

    Use pax extended tar format to support long filenames etc.
    'tar' supports only 100-character paths; 'ustar' supports only
    155+100 character prefix + paths; neither supports files larger
    than about 8 gigabytes.  We now use the POSIX.1-2001 'pax'
    extended tar format for those files in the backup stream that
    are too large or have too-long paths for the 'ustar' format.
    
    Change-Id: I2f256823091deaec9b1ccea685d2344753c6cb67

--- libs/utils/BackupHelpers.cpp
+++ libs/utils/BackupHelpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:851bd7de3086ec4391332ce2d93b1911463ba94f7ce7f7d89c17f0c05c7f2869
-size 42799
+oid sha256:eba510c0d569cde61cbf488d52ca34b74a851dd215ae0fa13424710bb02d2b99
+size 45668
