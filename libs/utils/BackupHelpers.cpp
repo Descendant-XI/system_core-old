1fedef6fd8038da1652f5a2c9dd1290c835227d9
Christopher Tate <ctate@google.com>
2011-05-18 16:28:19 -0700

    Restore from a previous full backup's tarfile
    Usage:  adb restore [tarfilename]
    
    Restores app data [and installs the apps if necessary from the backup
    file] captured in a previous invocation of 'adb backup'.  The user
    must explicitly acknowledge the action on-device before it is allowed
    to proceed; this prevents any "invisible" pushes of content from the
    host to the device.
    
    Known issues:
    
    * The settings databases and wallpaper are saved/restored, but lots
      of other system state is not yet captured in the full backup.  This
      means that for practical purposes this is usable for 3rd party
      apps at present but not for full-system cloning/imaging.
    
    Change-Id: I0c748b645845e7c9178e30bf142857861a64efd3

--- libs/utils/BackupHelpers.cpp
+++ libs/utils/BackupHelpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:653641dd244638f655ad2d104eb1a23722aeaaaaf33b1d47a0d64d562c1f9f87
-size 45950
+oid sha256:84e21fcb63d0be28b402bc1aab554a84f7d99b409cf222428ee81b6db9a0331b
+size 46234
