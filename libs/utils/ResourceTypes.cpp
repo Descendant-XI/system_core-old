df759dc3929ab7f794fdcc7b5b09584eead156de
Mårten Kongstad <marten.kongstad@sonyericsson.com>
2011-05-19 16:02:35 +0200

    Continue idmap generation even if name lookup fails.
    In resources.arsc files, a resource is represented by a specification
    block and one or more value blocks. In rare cases, a resource name
    is also given a new resource ID, a specification block and no values
    blocks. This commit ensures idmap generation does not fail if such an
    entry is encountered.
    
    Change-Id: I32302a0b07a7a320b7eeb31886931be3bb7b7e9a

--- libs/utils/ResourceTypes.cpp
+++ libs/utils/ResourceTypes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c8b36e9a4a7b520f23a7133b8487d776df2a3de21b3da74c55e4422f01fd9ac
-size 181045
+oid sha256:29886ad956e8d4114069e3cbb5004816ca75da3684a666f875e3dce537f39860
+size 181126
