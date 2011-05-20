7f7cab9504909e22743870ecb9ffc5892e709467
Dianne Hackborn <hackbod@google.com>
2011-05-20 16:11:04 -0700

    Add "television" mode.
    Change-Id: Ida1fdb61b036a8b489dbeda196fb4bc82e651b2b

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5446b4edac7e76977104ae6f4ced43e1c04b94548ce64be6a19fe268231acc3c
-size 78137
+oid sha256:1464a32643768b1ac0d5c365ebb0630a5a1161ed4c2a1f885b6044f9ac083dd9
+size 78211
