913c9ecc8096860e796d83bfe28a172d7f9b683b
tedbo <tedbo@google.com>
2011-05-24 00:55:33 -0700

    Fix broken comment on closing #endif in window.h
    The closing #endif comment is missing the comment delimiters. This
    change adds them and fixes a compile warning about extra tokens after
    endif.
    
    Change-Id: If8295aa6fbb2b21c2b56fe57a467dcde79c65de3

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8e56637ef4455f7a63b2edd5736a2792c37129c0fcd0932f5d2c30c25d82687
-size 15433
+oid sha256:a3aeedc08e9c6e83eab8ead30c5d8474289f14d9d2072d2b87040b274211928d
+size 15439
