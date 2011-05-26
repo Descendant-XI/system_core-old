d19533f87297dec36b311e76f8160ec49da567de
Arkaitz Ruiz Alvarez <arkaitzr@chromium.org>
2011-05-26 12:22:22 -0700

    Determine guest mode status for metrics collection
    Modification to the function IsGuestMode. It returns true only if the guestfs
    partition is mounted and the file /var/run/state/logged-in exits. Previously
    it did not check for this file existence.
    
    BUG=chromium-os:15763
    TEST=Log in in to guest mode right after installing recovery image and check
    the absence of /home/chronos/prev_stats. Log out and log in as a user and
    confirm the existence of this directory.
    
    Change-Id: I7b0e7e8844332cca3fa67611b90fada3c5ba0eeb
    Reviewed-on: http://gerrit.chromium.org/gerrit/1647
    Tested-by: Arkaitz Ruiz Alvarez <arkaitzr@chromium.org>
    Reviewed-by: Darin Petkov <petkov@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b53d67370a037ca253d987e6073bff9e80ead82c978576f20c24d6f4c1d50ce
-size 8632
+oid sha256:7dd588e90fe1ba82e1236bee804c10a2a7ba0e13310e065aced6dcfc81999738
+size 8634
