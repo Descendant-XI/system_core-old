dc2fee8a8d2b7d3d35fabdfa9c803ef0d085399e
Eric Laurent <elaurent@google.com>
2011-05-26 13:57:03 -0700

    Config utils improvement
    Added a function to free resources allocated by config node tree.
    
    Change-Id: I2ee8ae642899ec4501fa6e490c5be7efaa2d738e

--- libcutils/config_utils.c
+++ libcutils/config_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8d6479437b51ee4edfe05b9fb662c00a35f181cf819503248783d6168115edf
-size 6760
+oid sha256:39448d8704bc3fbb3d42b6e7471da65ce2c88a46c921479d604f146e670ba426
+size 6954
