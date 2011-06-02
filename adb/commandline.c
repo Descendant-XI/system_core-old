b11918ef0561778f90972fe24fd997f669dc8d74
Christopher Tate <ctate@google.com>
2011-06-01 17:56:23 -0700

    Make 'adb backup' -f handling more bulletproof and more flexible
    * "adb backup" as the entire command line no longer crashes
    
    * The "-f filename" option can now appear anywhere in the command line.
      A trailing "-f" at EOL prompts an error message and usage summary.
    
    Change-Id: I040ed73c2ca3687e265e35600eb3ab2b3c879695

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0806828504d4ba2210a9ed874a8d7c401da73330d4ad5dab987feb8e3aed6d21
-size 41254
+oid sha256:441fa9c39c68efc2f6379742abca373cc7441b81dc7d567fc4b2162165383c83
+size 41759
