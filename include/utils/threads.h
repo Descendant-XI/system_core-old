3a16a73ee92d20b4eb5598e8f8cd6b08baffc01a
Glenn Kasten <gkasten@google.com>
2011-06-02 08:59:28 -0700

    Remove redundant memory barrier
    pthread_create already includes the necessary memory barriers:
     - parent at pthread_create : pthread_mutex_unlock(start_mutex)
     - child at __thread_entry : pthread_mutex_lock(start_mutex)
    
    Add lock around uses of mThread.
    
    Added comments:
     - uses of mThread require lock
     - androidCreateRawThreadEtc returned ID is not safe for direct use from non-parent threads.
    
    Change-Id: I18cb296b41ddaf64cf127b57aab31154319b5970

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e3dd930ed3103240219e265eb104a41eacb153a372d9a1c6ab324a38584176c
-size 17260
+oid sha256:c31553dacbe4aba8bc10d2d5e5d9aa76b3bbeca38311d8e197a161033f8c6b82
+size 17309
