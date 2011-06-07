3d4e2d3c5f6142d78ba9c23d045164887f1e9ffd
Dianne Hackborn <hackbod@google.com>
2011-06-07 14:09:47 -0700

    Work on issue #4518815: Compatibility mode introduces compatibility regression...
    ...for Market App iRunner
    
    There were a lot of serious issues with how we updated (or often didn't update)
    the display and resource state when switching compatibility mode in conjunction
    with restarting and updating application components.  This addresses everything
    I could find.
    
    Unfortunately it does *not* fix this particular app.  I am starting to think this
    is just an issue in the app.  This change does fix a number of other problems
    I could repro, such as switching the compatibility mode of an IME.
    
    Also a few changes here and there to get rid of $#*&^!! debug logs.
    
    Change-Id: Ib15572eac9ec93b4b9966ddcbbc830ce9dec1317

--- libs/utils/StreamingZipInflater.cpp
+++ libs/utils/StreamingZipInflater.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc9168914668454ee790b8bf6b4813b1b0988ab035753422039c67752f373727
-size 8068
+oid sha256:ee58e28c2b174f46efea5ac0af8cea2f2952d0bf2c28b088d56a30195bd3143c
+size 8070
