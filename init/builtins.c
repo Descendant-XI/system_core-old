a62e7d4e1eec656443d3fbf0f51a1b5c5a9e5eaf
Mike Lockwood <lockwood@android.com>
2011-06-07 17:30:11 -0700

    init: Add support for writing system property value to a file in init.rc
    The write command will write a property value if the value starts with a "$'
    
    For example:
        write /sys/class/android_usb/iSerial $ro.serialno
    
    Use double leading '$' if you need to write a string that starts with '$':
    
        write /data/foo $$hello
    
    to write "$hello" to /data/foo
    
    Change-Id: I55431ac7715a5347bb95c3a15aee97c575444dde

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee634e8cd46d47969f42612aca885b72ee855051013e3b5e1a9cec4eddf58f46
-size 14406
+oid sha256:022e7af39ec05d57777f9901a81a1caefad987642c9b99e0ac0e44a7123f6f28
+size 14944
