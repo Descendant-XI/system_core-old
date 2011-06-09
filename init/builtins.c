0ab64f17433c3e61a35bdc3e7e86c5797128cc43
Mike Lockwood <lockwood@android.com>
2011-06-08 17:31:27 -0700

    init: Add support for assigning system properties to system properties in init.rc
    For example:
        setprop sys.usb.config $persist.sys.usb.config
    
    Change-Id: I7b4e1ed1335906b32621bd96a341b0f94bbee7f5
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9a494289c6de1770c3033c22f62c2765d3dacb385658f45225877de9b1aa792
-size 14857
+oid sha256:9ff1460756ffea7e0677f584b605d1c69a81ed8af808379919b1efe698e850f9
+size 15434
