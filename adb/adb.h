39de7c779c2dbbefb91f0b37fa5976867f5700fd
Mike Lockwood <lockwood@android.com>
2011-06-12 16:06:52 -0400

    adb: kick the transport after sending the "adb root" command
    This is needed because the USB driver on the device no longer resets the bus
    when exits and restarts.
    Since the host side adb no longer detects that adbd has restarted we need
    to reset the connection on the host side now.
    
    Change-Id: I1a8eabd292e8b45c34fa07a8a8b8c609cc15648d
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b18e07b194c990405bba974603dc607b56ad4c2a5adfcda12ef6b5412c7e67e4
-size 13448
+oid sha256:704fa7993a55665c9cdade3475215febc95f5215a08ae48313180237b0527471
+size 13664
