b1cac7e56f90f78a92839e47dfa89fe5e8f57725
Mathias Agopian <mathias@google.com>
2011-06-12 18:05:53 -0700

    fix RefBase so it retains binary-compatibility with gingerbread (DO NOT MERGE)
    Bug: 4595257
    Change-Id: I1db83149107d7dab1f0b7e73c684e0ff82e17e62

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc2e57c3e07f739f48157c8540838bcce332c53a4cf2e2cedab77d16ff939d5
-size 15623
+oid sha256:e84cabf37c9a14772ba706b9d0952cdbb0df5ad959f698d31c628647c0ea96c1
+size 15678
--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ca4304b2f59dd2560f5e5fc4579cd307eb9c4984523aca1b6c5b9be9e97bb7d
-size 14992
+oid sha256:765eb48d5c00547a56442840d037304f104a48e45001b17372d89165ad39efe7
+size 15625
