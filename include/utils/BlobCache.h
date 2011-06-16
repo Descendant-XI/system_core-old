2d680fda9e89ca8020109fa1d5cc7b8ade30c7ff
Kenny Root <kroot@google.com>
2011-06-15 20:41:15 -0700

    Use rand() for MinGW
    The version of MinGW we use doesn't have nrand48() which is really lame,
    but we need to use libutils in the Windows SDK.
    
    Change-Id: If854c03dbf02bc29e79f49e4539f08c2bf057517

--- include/utils/BlobCache.h
+++ include/utils/BlobCache.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fb6b64f68d35edb54880deb5e4778e804021cc15655b742daa27ad78e227eea
-size 6881
+oid sha256:689a4b33a366085366fe6b2ee21ff37e49f40e210e52391a1b9af3fbae0dc72b
+size 6983
--- libs/utils/BlobCache.cpp
+++ libs/utils/BlobCache.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39fff936962373896931b15a72527581948a8611d4faf6e726993ce53d91fc01
-size 7528
+oid sha256:fdbc0b3d2db3082f714ad83e746d076f80ced10619c40f05c34eed61cf8e20c1
+size 7682
