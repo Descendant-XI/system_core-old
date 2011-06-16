ad11ef5d1fba5465a8918d07de9b89cc836197d2
Jeff Brown <jeffbrown@google.com>
2011-06-15 17:44:52 -0700

    Enhance getevent to print event labels.
    Added -l argument to print labels for event types, codes and values.
    Added -i argument to print all device info.
    Added support for printing input properties.
    
    Change-Id: I3cacb716dbc38f50217b9dfc24ba44d08f352603

--- toolbox/getevent.c
+++ toolbox/getevent.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cb738b69e74cdb97bf93157671e95423b5494dd21f98f1932622e7c5b6fd330
-size 14642
+oid sha256:e8ac8d6ba7286f9285b27eae7193a026f5b697a8675a756a1f60faaa73b983a2
+size 19597
---
+++ toolbox/getevent.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:520bb41c50f995938ae5eea0b076233df8ca6e521d6bff071fbe62f4928285cb
+size 19015
