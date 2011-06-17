0f40174ab7c0279c4a06ab3e92abde73998ff3dc
Mathias Agopian <mathias@google.com>
2011-06-16 17:15:51 -0700

    Revert "merge various SF fixes from gingerbread to honeycomb-mr2" (DO NOT MERGE)
    Also revert all dependent changes:
    This reverts commit 8e18668d14adf601cbe5973030c310ec23d88461.
    This reverts commit 69b4587bfbb3e98f793959d9123340360fa233a2.
    This reverts commit a9c9a4baf24700e8817d47d8ea8da1742caea0b5.
    This reverts commit 2c0042b666a969091c931614f2fc0dce2f1cfac8.
    This reverts commit f6c8206735e7e078461e5f2aef6e1a1446fdd075.
    This reverts commit 24855c09173a6caaec7dcedd0c2d7ce15121d39b.
    
    Change-Id: I33e699640f3f59e42fa03c99a9a1b7af0d27d4d8

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b92815f51d19d666af3b0b8255f2943ade1d145dd454eefd42e777e9484a874
-size 15283
+oid sha256:7067f738a1bb442161d81fcb5b66334c83ae2ad88c0e260d1067a3a9669ed205
+size 15287
