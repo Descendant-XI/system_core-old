dccf5b210cc0818b0ffb8cc16cfa4e131863ee6d
Glenn Kasten <gkasten@google.com>
2011-06-22 16:20:37 -0700

    Add C++ thread API androidGetThreadSchedulingGroup
    This API is intended for applications that need to read a thread's
    scheduling group, while using the higher-level (C++) family of thread APIs.
    
    Change-Id: I5e58017f74c3989b20b5b1cc2bc4483c95720520

--- include/utils/threads.h
+++ include/utils/threads.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e12dbca94d636c120282aacb23861e9c101285e7f6daf13520f1364d1f02bbe
-size 17588
+oid sha256:5391dde4d0e1972555531bdf65b79cd3602524ecb0f939e98ca70d31924d2038
+size 17995
--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3b5711d4d36217eb0fcc41adbad25ce2a0c222ebd35382fad84f2b9f5df7e2a
-size 25739
+oid sha256:6ac81b683873cd72687d20f125c4201766f24078f8100fafef2c5318a10b4730
+size 26763
