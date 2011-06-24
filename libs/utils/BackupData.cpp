b9713cc5b52039a4a699c472f39d66c5258988e9
Christopher Tate <ctate@google.com>
2011-06-24 15:06:48 -0700

    Fix settings restore
    Also correct the debug-mode logging of error locations in backup data.
    Bug 4914182
    
    Change-Id: Ie7dda0192afa819e42490b7ffd2d3db6f11968f6

--- libs/utils/BackupData.cpp
+++ libs/utils/BackupData.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3c50acdad8bc252caf997e42274320e5dcc32aceb84f3388c676120ae35dd3e
-size 9137
+oid sha256:62c03429df49ea781781edb3e2fd5fb178565ebcce6cd9fc5fdf78f4ee50e57e
+size 9178
