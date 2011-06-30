2f7a02c9eb709b47169d7c79d195f1889485e476
Jeff Brown <jeffbrown@google.com>
2011-06-29 20:52:08 -0700

    getevent: Fix a couple of bugs.
    Fixed a bug printing the event value when using labels.
    
    Stop trying to print the available codes for EV_SYN because
    we cannot actually query them.  EVIOCGBIT(0, size) is a special
    case that returns the set of events that are supported, and
    EV_SYN == 0.
    
    Change-Id: Iea086ba24300ca0815e4814a3bc5ff60756612c2

--- toolbox/getevent.c
+++ toolbox/getevent.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8ac8d6ba7286f9285b27eae7193a026f5b697a8675a756a1f60faaa73b983a2
-size 19597
+oid sha256:4aba17bcadde0315542943153cd89c5d2770d57bc7ba29c3567e5c101dad3ea8
+size 19540
