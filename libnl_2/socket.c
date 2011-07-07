56a9f57d2003fa43baa9741c7c6f99cc59c98dfe
Frank Maker <ffrank@google.com>
2011-07-06 22:03:13 -0700

    libnl: nl_socket_alloc() Bugfix
    Clear memory and allow kernel to set local socket pid
    
    Change-Id: I274156fa7bcf8d2fe78f719fb2eb32d963682931

--- libnl_2/socket.c
+++ libnl_2/socket.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1145f3f105557f5416a351fbe75cc37027dc2dea9252adc49ac9431d2b298da
-size 2580
+oid sha256:eebb1c05c1978127f5dfb69bf2b4420d12c155095f609327495c9757781da433
+size 2616
