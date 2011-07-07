5b2d63f1555cd2ae8a3d60b82938c9e5bb93492e
Wink Saville <wink@google.com>
2011-07-07 12:16:12 -0700

    Add reset_mask as parameter to ifc_reset_connections
    The reset_mask allows either IPv4 and or IPv6 connections
    of an interface to be reset.
    
    Bug: 4981919
    Change-Id: Id2d9ab90e30091d3d0764c66d4b01b73c0edbfcc

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a385ca1f6f262550a42078904b44654232ddfb3f6409f77630d8de22648e970
-size 15886
+oid sha256:4ebd1fe1cccc2396eb6407bb8f01bc47899e16e6a6bb97b8cba7e3b7ab53d795
+size 16271
