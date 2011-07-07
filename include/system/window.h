dd560be80dac7dd9437259e56da656fe825bec23
Jamie Gennis <jgennis@google.com>
2011-07-07 11:51:48 -0700

    ANativeWindow: add setters for dimensions and fmt
    This change adds two new 'perform' setters to set the dimensions and
    pixel format of the buffers that will be dequeued from the
    ANativeWindow.  These new setters provide the same functionality as
    _SET_BUFFERS_GEOMETRY, but allow the format and dimensions to be set
    independently.  The _SET_BUFFERS_GEOMETRY setter is still supported to
    maintain backwards compatibility.
    
    Change-Id: Ib49b7798ffebe61eff2c8e4202fc3048cfec7bdd

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a003416f3b36f0747fedaec2b6c9dc55d3482086a474e44e883e494cbca8ee79
-size 15620
+oid sha256:9332338e82f40add6310675f8894808cc788cc41c4500cf90b352352aebe28ad
+size 16865
