112394f78d4ff92ca259a5f4bdb36209956e054d
Ashish Sharma <ashishsharma@google.com>
2011-07-07 18:16:01 -0700

    Add communication support for xt_qtaguid(network traffic accounting) kernel module.
    Change-Id: Ie0fb5b593987c53ee6f906fe6e0caab5a581d5a1

---
+++ include/cutils/qtaguid.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8b0a978ad1cf8c358b65bd1b7f7ce0152cf5995c7ab47b11d107ad96c7a3be6
+size 936
---
+++ libcutils/qtaguid.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbd612033a64b952b98e1dddf68bfce27f6dda6022f7f00ebb15956efdbb1958
+size 1336
