5e1a25818114c6e99cdb455d04ae190b96ca24d3
Christopher Tate <ctate@google.com>
2011-07-11 11:31:57 -0700

    Compress the backup output stream
    Zlib compression, with a full flush between each application's
    data.  Encryption will be performed on the already-compressed data
    once that's implemented.
    
    On restore, the streamed data is similarly uncompressed on the fly.
    
    Change-Id: I19b65c88e759a66527d10913d18fffa9df0bc011

--- libs/utils/BackupHelpers.cpp
+++ libs/utils/BackupHelpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a067667da7bf57f6f2ad72ac3c9e202ed1d734d0dd6759ddbd098bbbfd852ac
-size 46318
+oid sha256:8f39fa47a5d9d15086829cd7d44c8c35bd166f42c77bad25a433d73374dd13c8
+size 46736
