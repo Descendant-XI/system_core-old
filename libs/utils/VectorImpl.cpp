ca4fea17a0b804bf3a3b476b37cfb982d3984e44
Mathias Agopian <mathias@google.com>
2011-07-11 16:26:18 -0700

    Fix typo in an assert's log
    Change-Id: I94883a23a0a92eaf3e4976f942f747a2137499ac

--- libs/utils/VectorImpl.cpp
+++ libs/utils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec8fda933a5825fdcfdff65962e3aa05c51d4e6923085f7b164edcf1aeb588ab
+oid sha256:2b8bb816c5f06b30bb6fc6e519249e4f86964dd67e4e96df84b02940f6fe44da
 size 17900
