c918a0d8d7e0b97cb68604792313f5138cfdd5f3
Kenny Root <kroot@google.com>
2011-07-12 14:14:01 -0700

    Move extract native libraries to JNI code
    The built-in ZipFile class was quite a long time to find an unpack
    libraries. Move everything to using the libutils ZipFileRO class that
    goes quite a bit faster. Initial measurements are 6 times faster than
    the Java code.
    
    Also, read files off the disk and compare their CRC against the APK's
    CRC to see if we need to write the new file to disk. This also cuts down
    the bootup time by up to a second per APK that has native files.
    
    Change-Id: Ic464a7969a17368fb6a6b81d026888c4136c7603

--- include/utils/ZipFileRO.h
+++ include/utils/ZipFileRO.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de83ab8104410780f7f62693ace0a18df4d4ea2bf75f36647a2363db31396c79
-size 7995
+oid sha256:bd8990a5840d7317fd08c37efc0dac62703fde0b1df968428fff71c4b914b510
+size 8537
--- libs/utils/tests/Android.mk
+++ libs/utils/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f093d19960e0716a7454bc87cd0affe9e11b56527b89825c7d94926972620985
-size 972
+oid sha256:c948bb7b65b4a3c7bded21dc995e742680d5f88d2c2ea2a1d1519ddf204dfab1
+size 996
