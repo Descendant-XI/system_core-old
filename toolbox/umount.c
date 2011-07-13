d467990be6fba31c1ed28ac14ae0fdc1761702ff
Ken Sumrall <ksumrall@android.com>
2011-07-12 19:47:06 -0700

    Add -o loop= option to mount, and fix error detection in mount and umount
    This fixes bug http://b.android.com/18419, which complains about a bogus
    error check in the mount command (it also was wrong in the umount command)
    and also asks for the mount command to support more than one loopback
    device, as mentioned in the FIXME comments in mount.c.  This required some
    corresponding changes to umount.c
    
    Change-Id: Ib796c70926395e61557e487bad64984d3295d5f3

--- toolbox/umount.c
+++ toolbox/umount.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c95613412d7321b37b53a7cf372437f31cf084d7361f6435a86ad06d8653ddcf
-size 1574
+oid sha256:4e829cb745463eea230572e0cc664c9faf959201f20061bb362e5f9b2c23a628
+size 1848
