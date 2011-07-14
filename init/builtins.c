361638ecf168dbd169f8f17d602abee4cba8139b
Lars Svensson <lars1.svensson@sonyericsson.com>
2011-07-14 13:39:09 +0200

    init: Add noexec mount option
    Change-Id: Ic13d66797a86d38432e79af0c3387ce986d0a19f

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9f8be6a13cb2992eeccbe037eb0164a10bfa6642ff221271f47a3d177215ba1
-size 17076
+oid sha256:fb22a9cf5be732ce9c2655cb1585ed9ff371fb1d8ef1c9f977e30f87749b0a45
+size 17109
