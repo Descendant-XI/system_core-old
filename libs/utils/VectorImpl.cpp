28a8a1b3209c108db7b02f3395504b12b2614b9d
Jeff Brown <jeffbrown@google.com>
2011-07-13 22:22:02 -0700

    Replace Vector _grow/_shrink checks with assert.
    On review of the code, _grow and _shrink are checking for conditions
    that cannot happen and that don't even really make sense.  For
    example, if _shrink is called with where + amount > mCount then
    this is really bad, however the check only considered the case
    when where >= mCount and then it would arbitrarily choose a new
    value for where.  Huh?
    
    As it happens, the callers are correctly validating the
    arguments before passing them down to these methods so we can
    get rid of this code.
    
    Change-Id: I921852dba8997065bb0e9cac733e82028d14afcd

--- libs/utils/VectorImpl.cpp
+++ libs/utils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b8bb816c5f06b30bb6fc6e519249e4f86964dd67e4e96df84b02940f6fe44da
-size 17900
+oid sha256:f447dcfbfeef836a8fb9132d276f6e67baae3b850541d07a4490f54fb38806c1
+size 18143
