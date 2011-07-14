eeb09a7fadc323c16115c0c6f086c216fb8ac7d2
Michal Frynas <michal.frynas@sonyericsson.com>
2011-07-14 15:20:05 +0200

    Fixed improper size displaying in 'df' utility
    'df' command used to display filesystem usage statistics as integer
    values, in most cases rounding the actual value down. Because of
    that 'df' tended to display faulty size values.
    This fix to 'df' utility calculates the fractional part of the size,
    then it rounds it when needed to the nearest one-digit integer value
    and displays after decimal dot.
    
    Change-Id: I9bc52635d45d3e55ce61b3b1c6b80d1267516e75

--- toolbox/df.c
+++ toolbox/df.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb462ed2dc3b3e2433661ed70bad0590a24bdfd6a68e06d7e43ac13d20a4196
-size 1732
+oid sha256:48e39c306b36119b9534a6941c1e8dc200cacabe2d2cc98a26224027f2bbbeb9
+size 1814
