9258b14d2743fb01f34f22424e740ef39d88bb98
Mathias Agopian <mathias@google.com>
2011-07-19 15:59:15 -0700

    support for "Add an ANativeWindow API for SurfaceFlinger to suggest an optimal buffer orientation"
    Bug: 4487161
    Change-Id: I36708fd155415adbb666137692252381c19abcf6

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:090106c874d53843d2631744dbda1dadd44620615302b6766dc1d2e9af5d7496
-size 18672
+oid sha256:2dad2d6e0cd7c64a8317d901ea2c4cf4d25a16d2466c5cca76597c2a27dc8e61
+size 21122
