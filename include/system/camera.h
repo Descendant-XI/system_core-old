e5c64887576c704e4068ce918858e716d31a2110
Wu-cheng Li <wuchengli@google.com>
2011-07-20 20:06:45 -0700

    Add camera face detection enums.
    bug:4460717
    Change-Id: Id5cadcd7cdab9a9978569caf27d2ef2747fa6570

--- include/system/camera.h
+++ include/system/camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1bda7d7f8f2600b29243d2e503b2e8691fd6ab0051046f8083d7746e3b7ed53
-size 4819
+oid sha256:4af62d88bde8e1d7daeb402941bb94f752ba97a34e063d8e6acc73e3bac7f918
+size 6171
