472aa0bf83d27436e3b7db7cd89f07543d8d705a
Wu-cheng Li <wuchengli@google.com>
2011-07-28 05:31:49 +0800

    Add camera frame metadata and face detection structs.
    bug:4460717
    Change-Id: Ib72abfba080529472cf3fecb712043403bf7295f

--- include/system/camera.h
+++ include/system/camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4af62d88bde8e1d7daeb402941bb94f752ba97a34e063d8e6acc73e3bac7f918
-size 6171
+oid sha256:92132a8a53d6aba17c9457f15ec0d03c1752c0f10007953576b86f2f000c6665
+size 8655
