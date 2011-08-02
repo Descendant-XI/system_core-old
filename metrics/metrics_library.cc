4a07fb7329452e5b366571c5e9c460fac58ce04d
Julian Pastarmov <pastarmovj@chromium.org>
2011-08-02 16:10:49 +0200

    Respect the metrics file if no policy value is set (for legacy migration).
    BUG=chromium-os:18430
    TEST=Update chromeos and check whether the consent for stats is preserved.
    
    Change-Id: I86d4710ef9f381abd9f1eac728040878e6fc5b71
    Reviewed-on: http://gerrit.chromium.org/gerrit/5139
    Reviewed-by: Ken Mixter <kmixter@chromium.org>
    Tested-by: Julian Pastarmov <pastarmovj@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94bbc90e4752c7c9d2e1a63e60bc7afd91ae4205dccb89dcac6965cecd4befe6
-size 9122
+oid sha256:03db07bd19cf22f44772e05f48448009be339202b8e2807a11ae7b5cbbd1c2a9
+size 9501
