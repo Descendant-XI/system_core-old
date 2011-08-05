158f6a0b2141585491dec9d0a00f5987e731ddec
Sergiu Iordache <sergiu@chromium.org>
2011-08-05 15:32:15 -0700

    crash-reporter: Activate updated BadDirectory test
    Activated the BadDirectory test which was breaking the build.
    
    BUG=chromium-os:18622
    TEST=Ran the unit tests locally and it succeded.
    
    Change-Id: I439d9fb40ec37261ef839c82817d6bc67e683c7d
    Signed-off-by: Sergiu Iordache <sergiu@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/5433
    Reviewed-by: Ken Mixter <kmixter@chromium.org>
    Tested-by: Michael Krebs <mkrebs@chromium.org>

--- crash_reporter/kernel_collector_test.cc
+++ crash_reporter/kernel_collector_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9defa008dbac3710df0229595b70932e80ebaa53adea8660adf65fa61d3720e5
-size 18605
+oid sha256:ab759daa11e7482d17822b8c2a1c2956abaf09849609bcafbe41f1af310420ce
+size 18555
