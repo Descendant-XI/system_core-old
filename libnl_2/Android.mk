4d08bad0b9f71ec8486b53b3176f22966ee2ca3b
Dmitry Shmidt <dimitrysh@google.com>
2011-08-10 15:25:51 -0700

    libnl_2: Fix nested attribute setting
    Change-Id: I9dbf72a25e8a939ab6aa35eaf4adbf86d9ed97a1
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- libnl_2/Android.mk
+++ libnl_2/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:442def2555512ea9a569b4efe7e21cc5ff8cc62b863da9bb6aee9edcc9f1c1d7
-size 593
+oid sha256:7b6e6cfa36c33cfe630c698f557ea30cd5555d4c015e01056e422de293db9f49
+size 609
