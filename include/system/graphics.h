40b67d569a8508735e61eed9b9818e3706e46128
Mathias Agopian <mathias@google.com>
2011-08-09 18:55:44 -0700

    Add a priority constant in to the graphics HAL header
    this is needed if a display/gpu driver needs to create
    service threads that can block the main ui thread.
    to avoid priority inversions, these must run at
    HAL_PRIORITY_URGENT_DISPLAY
    
    Change-Id: I3af592e05a6d1b6f39a2b7885a7c842e54abfce2

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae33a7004554d4f79b82974bd7f099ce03b88563acee951d1aad34e21b5567c
-size 3352
+oid sha256:f280a48df4fd853744484d27a2ddc9ab71768bf2ca192c8542951fde8594b746
+size 3779
