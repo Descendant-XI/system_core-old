0c5fab21a3a303a2f47ef123e72ad0c12e158906
JP Abgrall <jpa@google.com>
2011-08-10 12:24:54 -0700

    Move qtaguid.c from common sources in Android.mk to fix windows build.
    Change-Id: I0a2d1615108b6c10064b3635d05699748a1341a4

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10b39325327838245ce4f28f57152192b5969a9ed80ca79d73ba5c872aa78d13
-size 3708
+oid sha256:dd6c0d0422999a776b10decf0c16ead671f67ecb48f9cb5b3e2b02e7656996ce
+size 3705
