80227b144ac15a3f67d348c7b7d820d40df70b2f
Jack Ren <jack.ren@intel.com>
2011-08-12 18:39:04 +0800

    adb: usb_windows: fix adb connection lost issue
    Windows adb connection could be lost if the target
    side kernel enables the kmemleak.
    
    The root cause is that kmemleak downgrades USB
    performance, and lead to Windows adb host application
    timeout because usb_write()/usb_read()'s timeout time is
    very short. That issue is not reproducible in Linux
    host because its usb_write() timeout is 5s and usb_read()
    is blocked until return:
                    usb_write()         usb_read()
    Linux           5000ms              blocked until return
    Windows         500+len*8 ms        500+len*8 ms
    
    To fix that issue, extend the Windows adb host usb_write
    timeout time to 5 seconds and usb_read() as a blocked routine:
                    usb_write()         usb_read()
    Windows         5000ms              blocked until return
    
    Change-Id: If54e2b4c396a5a06318c0ee0b3326a00e7661fbc
    Signed-off-by: Yu Wang <yu.y.wang@intel.com>
    Signed-off-by: Jin Can Zhuang <jin.can.zhuang@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- adb/usb_windows.c
+++ adb/usb_windows.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:102fcda270d9caf61b05abcc3ed053c679fd3e6f3106c90fab5a68f72512d09d
-size 14153
+oid sha256:8240bbcef2a1897a39821e38cf91c6c3fd1e9e875545e8dd2665a9fdc9160d42
+size 14094
