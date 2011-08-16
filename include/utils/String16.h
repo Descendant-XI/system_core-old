9850730e2862e13723f7a847fd6ea1682f8b7384
Josiah Gaskin <josiahgaskin@google.com>
2011-08-16 15:16:04 -0700

    Fix String16 operator+ to actually work
    This change fixes the String16 utility's operator+ function.
    
    Change-Id: Ibed7b07277b50bf178febc72b6ce497893e66f17

--- include/utils/String16.h
+++ include/utils/String16.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b36105d39736adc82614375c7bb8ef732be6356f73be40d402cf751f313818f
-size 7329
+oid sha256:35e0b1f6e3d065bc6207f77fbc9cd09f99ed7327ea1f01714f2ef8c6fc97deed
+size 7336
