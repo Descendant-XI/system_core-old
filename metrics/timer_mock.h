2d474f6919e4bc916faece11190d47255a333890
Bruno Rocha <bpontes@google.com>
2011-08-18 11:28:30 -0700

    Fixed wrong method signatures in TimerReporterMock.
    TimerReporterMock had some wrong method signatures, and was lacking signatures
    for its accessor methods.
    
    BUG=chromium-os:18800
    TEST=None
    
    Change-Id: I08d832857c1ae3df907f5470433117f7a03fe125
    Reviewed-on: http://gerrit.chromium.org/gerrit/6230
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>
    Reviewed-by: Bruno Pontes Soares Rocha <bpontes@chromium.org>
    Tested-by: Bruno Pontes Soares Rocha <bpontes@chromium.org>

--- metrics/timer_mock.h
+++ metrics/timer_mock.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:293e072830e9b9d85969a0f44bbdbe9ea2723cdfcc537174dfe59eb5684d9569
-size 1124
+oid sha256:9958b1d9d5c33d71c7de50180d87ed78929698a20df19cf4676483311cd73d28
+size 1357
