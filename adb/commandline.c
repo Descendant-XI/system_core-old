53461c184faf357924c808b37edf2f8916220a77
Christopher Tate <ctate@google.com>
2011-08-22 17:12:08 -0700

    Fix 'adb backup' command line validation
    The host side wasn't properly checking for argument-list sufficiency
    *after* removing any [-f filename] sequence.
    
    Fixes bug 5164135
    
    Change-Id: I7bc49e37ef168182088e0e664b6897dd2a088ebf

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b605d5ad301ca20e6560674a6214710cda16eabc3d81249a6342b30da26f1b4
-size 43505
+oid sha256:893ecc006e6ac973d0d7775e35bbf71cf32a3d4d2c10bfaef1bf4bad99e3afec
+size 43536
