248ff59ffeb9c55e5e8843ba05cfa847edbeae5b
Dima Zavin <dima@android.com>
2011-08-24 15:28:23 -0700

    init: add charge mode handling
    Introduces a 'charger' section that is processed when androidboot.mode
    supplied on the kernel commandline is "charger".
    
    In this mode, sections such as fs, post-fs, etc are skipped. Only the
    'early-init' and 'init' sections of the init rc files are processed before
    processing the 'charger' section.
    
    Change-Id: If9eb6334de18f04cbcf2aab784578e2993615242
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a4d2dba87efacd56e2f2e63199717c02e81affbd990d713b89b9c97b458091c
-size 23122
+oid sha256:7e4ef963cd38923ceb5d3626c5be005b422ad5be92db43b544304223713a0e06
+size 23314
