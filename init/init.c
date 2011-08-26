0ffa6dd8ca5745d154ed3ef158cbda30b575c59b
Dima Zavin <dima@android.com>
2011-08-26 13:59:18 -0700

    init: check for list empty when getting commands for action
    Change-Id: Id42d6a6b6297919b2e6520e074e31b5e01ae17bc
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0956d546a0394358a2967ad74ff755aff4671bae99869add986325fcba619b2f
-size 23084
+oid sha256:5cae6ef521a68ce2f445aa2b5d8ad28d0ace05b9ac10f47d4d47d57824dd7b76
+size 23114
