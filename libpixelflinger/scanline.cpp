22ca8471771d9a14b8e6d7b7bcb5cbb1470e35e5
Vladimir Chtchetkine <vchtchetkine@google.com>
2011-08-29 10:02:24 -0700

    Fix DEBUG_NEEDS usage
    Change-Id: Ic107c60080e55e1f9092f20fe3bd55e7592ca9fd

--- libpixelflinger/scanline.cpp
+++ libpixelflinger/scanline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2da8e92fcf68c47b0b3d46459a96333d76b24a414bddf3180264d0fab79e61bb
-size 76956
+oid sha256:e786c66357331f44b530f8283a5ea1dbf9448ac3bcc36f05ff41cf972f85d534
+size 76953
