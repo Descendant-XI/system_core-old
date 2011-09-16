7da7547cd568abda3887a013ba1c1d1ce3de65f0
Andreas Huber <andih@google.com>
2011-09-16 11:47:13 -0700

    Fix androidGetThreadPriority for non-pthread configurations.
    related-to-build
    
    Change-Id: Ic865af0865906f96fd615a56a030c8e3adaf13c4

--- libs/utils/Threads.cpp
+++ libs/utils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12c86d982d08f49ebcb153857bfbd786c9199e9418f99cf4795aedbb5634af7c
-size 26884
+oid sha256:461bd45d2928c0a18ab1c78b28236e1906486fe9cf8fd2f4be77bae6f4d0f56b
+size 26960
