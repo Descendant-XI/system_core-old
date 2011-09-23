5cfabdefc984243682c1b8f9c7f9397c2292c458
Kenny Root <kroot@google.com>
2011-09-23 12:46:39 -0700

    Skip argument when parsing adb install
    A command line flag with an argument was checked in the Pm.java code,
    but it wasn't being checked by "adb install" so attempts to use it
    failed.
    
    Change-Id: I0b84a4203a416f7323fa823c0f1f1750670d0c76

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893ecc006e6ac973d0d7775e35bbf71cf32a3d4d2c10bfaef1bf4bad99e3afec
-size 43536
+oid sha256:cb6f10b768886d8b2cdffb12a392bb71b915144423b9eab957ac4a83fe5582d6
+size 43650
