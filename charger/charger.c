3d0a2b309ddcab2ca432d459a21ddba749463818
Dima Zavin <dima@android.com>
2011-09-26 14:14:19 -0700

    charger: use the new asset for almost full, and show full @ 95%
    Change-Id: I5f8729260ae068d35d981ff2c16d576c3d4dd82f
    Signed-off-by: Dima Zavin <dima@android.com>

--- charger/charger.c
+++ charger/charger.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b951ed91e98ffccd83b70b128b583b24044e7c02da267075a4d0396feec61acd
-size 23850
+oid sha256:2866fd25d5af9c7a0467855f147140b606ff859111b1904d5197442d2ab91bd0
+size 24229
