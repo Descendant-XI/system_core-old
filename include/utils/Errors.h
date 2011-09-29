a7cc28e6c7e63f623cf6cae336becf8e8204379f
Dianne Hackborn <hackbod@google.com>
2011-09-28 23:19:47 -0400

    Add mechanism for Parcel to not allow FDs to be written to it.
    This is to help implement issue #5224703.
    
    Change-Id: I026a5890495537d15b57fe61227a640aac806d46

--- include/utils/Errors.h
+++ include/utils/Errors.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17e27e13dc66ba445dc74df6d0d63d807816ead1fe2748cf79f9c42f394c00e6
-size 2437
+oid sha256:5e4b2262beae9daf21869d6e0a4c7db1bb4d511059c747ca7d193aee0001838d
+size 2475
