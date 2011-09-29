72f0f0353b48c62b299ad110b5ab5de57691e389
Xianzhu Wang <wangxianzhu@google.com>
2011-09-29 12:59:55 +0800

    Fix SocketListener socket leak issue.
    The problem was: if a socket is shared between SocketListener and another
    thread, only if the last reference is removed by SocketListener can the socket
    be closed, otherwise the socket will leak. This sometimes happens in netd's
    dnsproxyd.
    
    This change let the SocketClient own the socket and close the socket when
    the SocketClient is destructed.
    
    Change-Id: I2865fbfe9ee4d8b3e43d7e02919dbb2d261f70de

--- libsysutils/src/SocketClient.cpp
+++ libsysutils/src/SocketClient.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f85112a92b669d63de62e2b5b0bd189053fc3faa894f194d5fd2d080049a5d5
-size 2903
+oid sha256:51ce75e78dd3204477b7733ae9a0e4d4d25f707192acff7ecd8b70bd40f8b098
+size 3034
