99ce7a21c85619ff1787b07b6d8f734e41c05940
Steve Block <steveblock@google.com>
2011-10-12 17:22:43 +0100

    Rename (IF_)LOG() to (IF_)ALOG()  DO NOT MERGE
    Bug: 5449033
    Change-Id: Id318736a9caa58c6da2683f8663699ce998e79d8

--- logwrapper/logwrapper.c
+++ logwrapper/logwrapper.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4498b8c011e0b48b706690c0950430de6e24e46508fc5c2f18d210deab97c97
-size 5138
+oid sha256:9ab3e1413a6dd4beadfdcdfd970adfaffc90b78a42b32f7820770c5bb3775f9b
+size 5154
