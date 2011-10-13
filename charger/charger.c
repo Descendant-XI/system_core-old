8f27dfaaba99ae8c4161c9caa3a28cf68971f295
Dima Zavin <dima@android.com>
2011-10-13 13:04:20 -0700

    charger: key state being the same is not an error
    Change-Id: I58d53899dbcca8329b65b189c1a6c5eea413c289
    Signed-off-by: Dima Zavin <dima@android.com>

--- charger/charger.c
+++ charger/charger.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7811343422fd734c75d6ec9e12f1786953a68b05843fc6a3b75809c6ba8dd7bd
-size 25605
+oid sha256:008f3a589ae2464fa88aa15dc139dc0be5c828d3606bc4280e915ce33602cb03
+size 25604
