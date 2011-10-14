b6ac2cf761507d1c99607ef5e788cc86c21814cd
Luigi Semenzato <semenzato@chromium.org>
2011-10-14 15:31:32 -0700

    Sends metrics to Chrome even when user did not consent to send stats.
    BUG=chromium-os:21660
    TEST=observed that Platform.* stats appear in about:histograms also when consent is turned off.
    
    Change-Id: Id70f0cca468ec39fdfe687d89a6f0748a2a07828
    Reviewed-on: http://gerrit.chromium.org/gerrit/10147
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Darin Petkov <petkov@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03db07bd19cf22f44772e05f48448009be339202b8e2807a11ae7b5cbbd1c2a9
-size 9501
+oid sha256:b458ac90205885901b94ae6850261b7f63906283f43327dd7197ebb0de967d5a
+size 9456
--- metrics/metrics_library_test.cc
+++ metrics/metrics_library_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24cfebdb4f51bbd38795a5d1c00daf8ea6b2c117901a586ef1aa054ab39618c8
-size 12526
+oid sha256:5b16d65f61b5c0b2c0cd3e1ac9446e05c86d34fcf5c44a09368ec041cfac566f
+size 11441
