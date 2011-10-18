29fc31167a2a97aa4d9f687632445a92bdd29b47
Andy McFadden <fadden@android.com>
2011-10-18 15:42:03 -0700

    Enhance debuggerd memory dumps
    This makes two changes:
    
    (1) Display ASCII values next to the memory dumps.  For example:
    
    I DEBUG: 00008ac4 706f6f4c 20676e69 74206425 73656d69  Looping %d times
    I DEBUG: 00008ad4 7453000a 6e69726f 6f742067 0a702520  ..Storing to %p.
    I DEBUG: 00008ae4 65642f00 657a2f76 55006f72 6c62616e  ./dev/zero.Unabl
    
    (The hex values are still displayed as little-endian word values, while
    the ASCII part is byte oriented.)
    
    (2) Optionally display memory dumps for all registers, not just LR
    and PC, for the crashing thread.  This is meant for situations where
    we crash dereferencing foo->bar and want to see what the memory near
    "foo" looks like -- could be handy if it got stomped by MUTF-16 text
    or something recognizable.
    
    Change #2 is currently disabled, via a compile-time setting.
    
    Bug 5471955
    
    Change-Id: Iacfd01c314055bad81db2f43b7d239f10086fcfb

--- debuggerd/arm/machine.c
+++ debuggerd/arm/machine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aede2dc1d632e53c018039ef9c7920b9df35864b4813baff0e7a89b7efc340f2
-size 9094
+oid sha256:b2adbbefccb23a34d63cb3985f974345ef306a41e6db46dd26c6b2d3b502797f
+size 10972
