7660a2133a9558ff7f3b5f1d248f29abc7f6b600
Iliyan Malchev <malchev@google.com>
2011-10-19 22:35:56 -0700

    add -ldl to host executables
    This fixes the build on Linux when RefBase is compiled with reference tracking
    enabled.
    
    Change-Id: I3e931e0b463ce836f6fdb30c37068d77144631a3
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- libs/utils/Android.mk
+++ libs/utils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c8fb56679ed853b7560799903204f6fdcee07496a40af2ea515c1c79ff531dc
-size 2869
+oid sha256:5919223a405e68f4ea8ceb80fe7ed2fa369d7400d03abb83204b791c2b816281
+size 2928
