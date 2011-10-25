7fae95ffa172e6828f3b727953f11c7c1705bcae
Ken Mixter <kmixter@chromium.org>
2011-10-25 02:15:05 +0000

    crash-reporter: always write kernel crashes for dev builds
    Added check for devloper build. Always write kernel crash files when
    running devloper builds.
    
    BUG=None
    TEST=run "echo bug > /proc/breakme" and ensure the kcrash files get
    created under /var/spool/crash
    
    Change-Id: Iced35df9db1f997c41138f082ee50a3506a88eb2
    Reviewed-on: https://gerrit.chromium.org/gerrit/10615
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Tested-by: Sameer Nanda <snanda@chromium.org>

--- crash_reporter/kernel_collector.cc
+++ crash_reporter/kernel_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d75f68e287e1b6aa414f8f64686d362eadddf96c72d46417f81ffb53006d177d
-size 16690
+oid sha256:685b8ca9a4b2264181c6c74a3654756b421f8d81e0de074c878ca05bfa83af6c
+size 16854
