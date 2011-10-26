de420612cb8a5da40da2fdcdcab39dfb0a7f265a
Ken Sumrall <ksumrall@android.com>
2011-10-26 16:56:00 -0700

    Fix the class_reset command
    The class_reset command used to reset services that had been set to
    "disabled" in the init.rc file to a non-disabled state.  Now, if the
    service was originally set to "disabled", have the reset command set
    it back to disabled.  Otherwise, set it to the "reset" state as it
    currently does.
    
    Change-Id: I0c10582e46a8e443d4748d9d893ae762b19b653a

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81aa14ef291a1ffea08263586f4bb69d0d510c6a3c98eb08651fb38c6dbce195
-size 22848
+oid sha256:5c9e0a110d64f0536a530df357227fe1e4d6800eaeafd4008100128096cb97aa
+size 22980
--- init/init.h
+++ init/init.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39398bffcaf396bee63cf4523018c38b9431da4cd3a9bea0f6fdc88ee7394ca6
-size 3900
+oid sha256:8be340c5ac122eb6210283516ed73772d08f53cc278e1dd72a071bab45870ce3
+size 3991
