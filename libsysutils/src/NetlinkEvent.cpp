af3ec365cee18be08c9a146a82ee6ab86d471f0e
The Android Automerger <android-build@android.com>
2011-11-02 10:17:46 -0700

    Revert "Merge "Remove log spam" into ics-mr0"
    This reverts commit d4f29767c8cd5a0d8b52b77bed117ca3746af8f3, reversing
    changes made to 604f52e31357a774ad49290e323976b1db5b9f38.

--- libsysutils/src/NetlinkEvent.cpp
+++ libsysutils/src/NetlinkEvent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b87e82bb1cf439c4dcc4cbe4afa0906ac0616f6151c3e3935288b4a219d2bdd7
-size 7087
+oid sha256:f5e2f739b836a4a234742d1a20a6f44233869c1270f9126593597334fd8745ab
+size 7154
