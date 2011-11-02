3fb864cb593d78e07de74ed75a2a2db7e85c9af9
The Android Automerger <android-build@android.com>
2011-11-02 10:18:15 -0700

    Revert "Merge "libnl_2: Fix memory leaks" into ics-mr0"
    This reverts commit 4f7c0843bbaa9cb4ec3a7890461cebbee689530a, reversing
    changes made to 6fb3df8d468a105b49c1cdbbdcb061dc8de022a4.

--- libnl_2/handlers.c
+++ libnl_2/handlers.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:078d927c3cb4989451b4ac140c80fe9cc26ee60b7180179658460b316fd670b8
-size 1940
+oid sha256:838b720cde793bd703db3fdae963724710204158dbd9a65515e70c28f2dcd8a4
+size 1988
