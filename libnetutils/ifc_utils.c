76f46db4bf6ab63501a7596a35fbc082cf21278a
Alex Yakavenka <ayakav@codeaurora.org>
2011-11-08 20:25:23 +0900

    Export flags needed for ifc_reset_connections API in netutils
    Code using ifc_reset_connections api needs to have access to
    predefined masks for reset_mask parameter
    
    Change-Id: I90bc5e1b62ae4a88501c8ad4e353c0d93d319579

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abfa4f3efe59edee93a1f426facab9ee7bd9460b593a736ff977bab4d3bd9204
-size 25341
+oid sha256:c3eec7c024f64d7097d9ea5332cc36c1847b52aa10493df50544ca95564fdddc
+size 25197
