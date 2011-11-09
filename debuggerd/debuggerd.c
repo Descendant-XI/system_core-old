e62d18c99c19b30a824f0535b81467caa12f8d48
Jeff Brown <jeffbrown@google.com>
2011-11-08 20:17:05 -0800

    Print the tombstone path.
    When explicitly requesting a dump, show the user the path of
    the tombstone file that was generated.
    
    Change-Id: I7fff7dea606872a61687ca087e99f604107b70c4

--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d50e7c3a9a18ba788e6818ec6eec1dc72e54b7d2bc69494eb44ec656db9fa4
-size 28292
+oid sha256:e4afb3cfb070a037ac6f5b6b5482b01c8765c0ecfe0a439ce08e1edc4d5ef9b6
+size 29443
