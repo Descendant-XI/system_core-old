d6dffed942735e540757caf4105c4ee1e8a59908
Jeff Brown <jeffbrown@google.com>
2011-11-18 15:34:35 -0800

    Minor robustness tweaks.
    Use the alternate stack specified by sigaltstack (if there is one).
    
    Use tgkill instead of kill to ensure that we send the signal to
    precisely the right thread.
    
    Added timeout logic.
    
    Change-Id: I72922137d1bc0c8becff98244926bde09fe3990e

--- libcorkscrew/backtrace.c
+++ libcorkscrew/backtrace.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdac3ee7a980be355d7aa69f0596a702df25f12990f1d05e8c22295c9ad39434
-size 8264
+oid sha256:fc4cac1125840a2ba846c0f83a203dfd27db39c542504d927c9205b6a6196d3a
+size 10026
