010e5285abb9d41b431e79ae10398aa42776ae10
Jeff Brown <jeffbrown@google.com>
2011-11-23 15:02:41 -0800

    Make timestamps friendlier.
    Change-Id: I378c3028630a7f805c3f730da02bbbfe07ec3654

--- toolbox/getevent.c
+++ toolbox/getevent.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1c83cdd22f646b8172db64853b4e79afd5924fc8cdf7ec0625e13e34c00aea2
-size 20953
+oid sha256:0522ed8ab8b937c4279a8ce42f8b802bd1d44b52e5735a8f8f1eba591e534f72
+size 20957
