06d3fff5ebaf554ad820071490f8cd0cba88c45b
Romain Guy <romainguy@google.com>
2011-12-08 18:19:39 -0800

    GenerationCache::get would return a random value instead of NULL Bug #5401917
    This was causing a ton of random crashes in apps.
    
    Change-Id: I9069a060824ec89115cd3bcd38beaeb9ecc4488e

--- include/utils/GenerationCache.h
+++ include/utils/GenerationCache.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e2bfcb80a25c5ad3f2d1d3adfa17ec9b1f03d27190eb2fe65a615c42cc308e4
-size 6534
+oid sha256:bc07b12160af69a97ec99ddc625459a6e9df6defbf3f225f09f106c7f7d0f371
+size 6583
