91066b6fac4fb2f27f39c2444623597966e48bd0
Vladimir Chtchetkine <vchtchetkine@google.com>
2011-12-09 15:49:47 -0800

    Use QEMU pipe for ADB communication when running in emulator.
    Change-Id: I7934b6272c2a9825d244ce76ff65dbce6e54ebff

--- adb/transport_local.c
+++ adb/transport_local.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fd28bb88412a96b60e60987933dd53a074406288480bfeadf41b173f03aebb3
-size 8645
+oid sha256:0cfea204f259a5b4b49457febb4e7c343ed19e244052789019453eeb7a9b5944
+size 13675
