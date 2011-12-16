81d7aaade016406d20292edbbc0a23ae796b1aa2
Anatol Pomazau <anatol@google.com>
2011-12-15 17:50:18 -0800

    Implement 'fastboot format' command
    Some filesystems (e.g. ext4) require flushing an initial
    fs image, right after erasing it the partition is unusable.
    
    Doing erase,flush emptyfs is a little bit scaring so we have a
    separate command that performs it as atomic step:
    
     - get size of partition
     - create an empty filesystem image
     - erase the partition
     - flush empty fs to the partition
    
    This command applicable only for ext4 filesystem and checks the
    partition type before formatting it.
    
    Change-Id: I8529bc1dc64698f1f0d91312f7c0ab1a6e5d8b44

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:347e32d0fe7660c876259d46f1116b0f522dfadbd65a67b1b3568cd8b6636744
-size 21052
+oid sha256:2115225b57f51008159ff1def733fec0d113b8ebd571ec7f3fb06de2fd49e729
+size 21266
