89dde6518c500e0cb6252239b61d56725bc61a86
Glenn Kasten <gkasten@google.com>
2011-12-19 09:21:48 -0800

    Miscellaneous updates to <system/audio.h>
    Add single-precision floating-point PCM format:
        - AUDIO_FORMAT_PCM_SUB_FLOAT
        - AUDIO_FORMAT_PCM_FLOAT
    
    Add symbols to replace hard-coded constants in code:
        - AUDIO_IO_HANDLE_NONE
        - AUDIO_IN_ACOUSTICS_NONE
        - AUDIO_INTERLEAVE_*
        - AUDIO_STREAM_MIN
    
    Update comments.
    
    Fix whitespace in switch statement.
    
    Change-Id: I61862c188ebac06657b5d9887164edc24b8ab8f5

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63c0785f3f76d8d8d2ebe66e49507d397d996ef628c91de1a9197e3d498e789c
-size 26418
+oid sha256:30931e217c922378f6ac4c5645c009a6e488cf46bbb357fc8418cf7c4f005279
+size 27992
