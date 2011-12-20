c1e387dca6d647bd9d4bbfa79e5991cc08697e1f
Dima Zavin <dima@android.com>
2011-12-20 13:44:41 -0800

    init: use init's property expansion code for setprop/write
    Change-Id: I3c284860cc8d5106ac2b086e62baeb6263873935
    Signed-off-by: Dima Zavin <dima@android.com>

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6a9aa4b3a8a59fa36bc0ab2d548a8a1eaeb47493cb36ab5055f4706e13acabc
-size 15604
+oid sha256:a9b954235326b0e81efb675990569692904f947724d4faacf61442e9176d8921
+size 15106
