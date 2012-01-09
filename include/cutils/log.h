953c163a4b139aed06e6b3f99294e9c93d39c4d8
Steve Block <steveblock@google.com>
2012-01-09 18:31:54 +0000

    Rename LOG_ASSERT to ALOG_ASSERT
    Change-Id: Iff15ac5e7ab226d437c08d23f18fd54e6793e65c

--- include/cutils/log.h
+++ include/cutils/log.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5890857e2641bad7c66a580625cfd3b8d1fadd7edf480fd53f5e0e821ae087ec
-size 14629
+oid sha256:77bc58c2f787d1ab797b30a3e599c48d60aa9b774bbe0c64982f396c8c2913fc
+size 14717
--- libpixelflinger/tinyutils/VectorImpl.cpp
+++ libpixelflinger/tinyutils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f918eed3e243894ced73ce95e5419c116cfc41167f0e77ca533c384d1c877753
-size 15216
+oid sha256:945b02d82e5e015703b3f92bbae15e1f161119fdfce28c7eddc19102ea5cfeca
+size 15222
