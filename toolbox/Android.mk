bb033241af69834dca1a92f56bb961c2fd15da82
Scott Anderson <saa@google.com>
2012-01-11 18:13:26 -0800

    Add md5 command to toolbox.
    This command outputs the MD5 for specified files.  The output is
    in the same form as the md5sum command on Linux.
    
    Signed-off-by: Scott Anderson <saa@google.com>
    
    (cherry picked from commit d0455c952d61d36e662f4a95d5e03689ecedca8f)
    
    Change-Id: I3be7751b6e301eb2d12844f88f1f5b9ed1225f6b

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:178711c6cbea410b95b62afb1f29515301bd1f715781e013b2739b0ab2fb114e
-size 1783
+oid sha256:2bbdafad93b198c0a4c7c453b98bc2f620d288c346193682bfa2f76819894952
+size 1830
