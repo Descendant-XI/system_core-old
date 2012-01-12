0f7cf77d73f9298e86567c6cdaa79b47f6b408cc
Scott Anderson <saa@google.com>
2012-01-12 14:01:43 -0800

    Fix hd command so it doesn't error out on EOF
    hd would error out on files that were not a multiple of its read
    buffer size (4096).  For example:
    Read error on init.rc, offset 17040 len 4096, No such file or directory
    
    The fix is to stop reading on EOF instead of treating it as an
    error.
    
    Change-Id: Ie7f4216e08835150f2f5784e77bb9e2190b0def4
    Signed-off-by: Scott Anderson <saa@google.com>

--- toolbox/hd.c
+++ toolbox/hd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89b1be136986b3ee27c327c1498778414c206001fafc196ec283fc2a04bf5f1f
-size 2024
+oid sha256:2992c6e45df26fb005f318a8074bdf2d2c6bd1e78bde2a61e23dafb25ec2af4f
+size 2050
