d14c1e60d18ec0ebcf60adce90e517d5ddfd9d80
Evgeniy Stepanov <eugenis@google.com>
2012-01-13 15:33:54 +0400

    Fix compilation with Clang.
    warning: extraneous template parameter list in template specialization
    template<>
    
    Change-Id: If3e3e1d06a018343b40ac0984dfd9ecc12629f8d

--- include/utils/TypeHelpers.h
+++ include/utils/TypeHelpers.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d09a4b4f81defd4acea81423d274b929a1d82f681e9b3a0b008f7df680d2a6e
-size 8912
+oid sha256:0161fe790fe51009d4190412869375c711d5f04709518d1ea062b2caf4a58e29
+size 8865
