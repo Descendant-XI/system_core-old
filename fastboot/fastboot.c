c4d46e8adb751ba952848d119dabc9e540200cdf
Wu, Hao <hao.wu@intel.com>
2012-01-17 12:04:11 -0800

    adb,fastboot: add USB support for Intel Vendor ID
    This patch adds Intel USB Vendor ID into the support
    list for adb and fastboot.
    
    Signed-off-by: Wu, Hao <hao.wu@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8020c328940ebfd38ae17dc4457a417216ce653df2aa528f281112a1202607c
-size 20998
+oid sha256:ebee431d6f75c49e03db534ec8f049555153143728af8a15e2e2924dfeb214d2
+size 21047
