4deb6e6a8027637f02344a5132f7360cb2da3e08
Nick Kralevich <nnk@google.com>
2012-01-19 10:18:59 -0800

    Move permission checking code to it's own function.
    Change-Id: Ibe46ca3043791f3b3815080f9e23ac307b1f5900

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06934a6542ceeb0edb04f91159d15a747526dc307257450bf6b3e1122c1ce23c
-size 37240
+oid sha256:a8728949dfd00a0f52df0f2aad1b8e0d9592e505d60273dc0c9033f5b2d90889
+size 37391
