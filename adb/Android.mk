6fb61f785a4b6337a49a9ce37992828fcd4983e3
Nick Kralevich <nnk@google.com>
2012-01-19 13:11:35 -0800

    Don't allow adb to run as root in user builds.
    Change-Id: I4fbd7beb7f76c68fead1a89b745f0dfb931b4597

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9edb99754677c6920ced424fea043468bd2be49209fda9cf070821604ba64a0c
-size 3961
+oid sha256:88a01666fb287e0be442d6ed3ad64781c1e2446e37f59008cc3b55d4ea5dfd16
+size 4061
--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8728949dfd00a0f52df0f2aad1b8e0d9592e505d60273dc0c9033f5b2d90889
-size 37391
+oid sha256:0c4fbe03feaebf73383837a1b9946197f0355c97d1523bd89dbd0dbddb0e117c
+size 37486
