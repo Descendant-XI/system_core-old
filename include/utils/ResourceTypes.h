55ded84e32de4a897f845d29f066fb92405bc9ba
Dianne Hackborn <hackbod@google.com>
2012-01-25 15:12:23 -0800

    Add xxhdpi; fix ActivityManager.getLauncherLargeIconSize() etc.
    Change-Id: I519d6cdc527a402d93b98df17a64fc1da52ad598

--- include/utils/ResourceTypes.h
+++ include/utils/ResourceTypes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a840a43818034ccb3bf91b9b448101e124cab5f71871687df6d01d84e43e92ca
-size 77350
+oid sha256:d1ee06d14654b0a77b8874dc79f3412201376be6ee21bb884e47430bef6c358f
+size 77460
