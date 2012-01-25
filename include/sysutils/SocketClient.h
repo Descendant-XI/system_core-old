0227448f97d0a09f6f8f91f663086266709a0a20
Mark Salyzyn <salyzyn@google.com>
2012-01-24 20:30:10 -0800

    libsysutils: Add iovec/runOnEachSocket
    SocketClient:
    * Replace sendDataLocked with sendDataLockedv which takes an iovec.
    * Add a version of sendData, sendDatav, which takes an iovec.
    * do not preserve iovec content through sendDatav
    
    SocketListener:
    * Add runOnEachSocket, which allows to to specify a SocketClientCommand to
      run individually on each socket. This allows you to do broadcast-like
      actions customized for each individual socket.
    * Client safe list reference counting for sendBroadcast & runOnEach Socket
    
    Signed-off-by: Nick Kralevich <nnk@google.com>
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    
    Change-Id: I716f89c01b4cb7af900045c7e41fac1492defb06

--- include/sysutils/SocketClient.h
+++ include/sysutils/SocketClient.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2d17a3d2f0d3c391de1cb9d49a07336b638de1789551db08efa5e2f8e5c0756
-size 2570
+oid sha256:777793195b007d64d99941ce50c87a9f1aefba7c28cb4863c5d90b6aadba7ecf
+size 2656
--- libsysutils/src/SocketClient.cpp
+++ libsysutils/src/SocketClient.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f0e7e2b4c2b4da10b72102eeab16efa1a4c06e05ba84880c2a3bbd2f4b73f8b
-size 5132
+oid sha256:22718507199a0503733a7ce95cf7ba7fbc6973cd9df74cd9afa2d8991e55cedb
+size 5803
