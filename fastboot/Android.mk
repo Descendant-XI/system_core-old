faeae2d60f5448b7486e7183c65063586795a5e1
Anatol Pomazau <anatol@google.com>
2012-02-03 18:20:02 -0800

    Revert "Implement 'fastboot format' command"
    This reverts commit 230d160a718333651f7ca1557404f96682795b37

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b9f10ff62f69e8a46bfb578e048c9b203026b9a49569e4e304b638fade258b6
-size 1972
+oid sha256:1f99723ff4565df4d567bf3a3624cd844dc4989f24d40ff66bd1289689c52a09
+size 1912
