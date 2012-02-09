171242f771f17d01101cbec6169eac036788aaa7
Nick Kralevich <nnk@google.com>
2012-02-09 11:22:33 -0800

    do more checks on packages.list
    Change-Id: I16d6eab5e674c860be915fde2da7877994bed314

--- run-as/package.c
+++ run-as/package.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69348543deee01a1d73be0913ed81cb7d6a3d0376d4d392db9a847cb524215dc
-size 12528
+oid sha256:ba8b1ba3cd2e19334e789d496f630d4e20b604bcda0924d1ec373b750a460637
+size 12796
