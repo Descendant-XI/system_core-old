4075f7b90560c8f71a7a03e99da9d26bdcb9d314
Dianne Hackborn <hackbod@google.com>
2012-02-09 11:19:00 -0800

    New user definitions for isolated sandboxes.
    Change-Id: I302f89c102bc35dcd70168748e205b41fdbf6576

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef04d2faae17f7d1fc43eea65cc660f4d65dc10e7a5d8005532a1360e29bbc50
-size 10742
+oid sha256:594513e61ba7f70867005bf107608d36c96d724662ce1febba35050a00c47c89
+size 11001
