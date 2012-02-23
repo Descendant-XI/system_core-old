b852d5275177006ee7d65fab6dbabc2d89840269
Nick Kralevich <nnk@google.com>
2012-02-23 13:28:35 -0800

    fix "logcat -c ; logcat -d" hang.
    Change-Id: I1dc3633fc7228a6ff8ed0da4572f40055a28ceb2

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7e1f784c78a69a288b5bf858461e3a15e96ba9a55309e7d1fdaca15677e9c0
-size 23451
+oid sha256:38ede6ab864fe1d244c76ee6aea236f80992abb29d6b4b3176fb89f822a5ca80
+size 23452
