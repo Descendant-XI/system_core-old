e932f4154761edc86bf40f5e31c502ac7bf91305
Chris Masone <cmasone@chromium.org>
2012-02-24 14:38:57 -0800

    [crash-reporter] Move flag files to /var/run from /tmp
    The kernel-crash-detected and unclean-shutdown-detected flag files
    needed to move out of /tmp to protect against symlink attacks by
    a non-root user.  /var/run is owned by root, so non-privileged
    users can't write there.
    
    BUG=chromium-os:17085
    TEST=unit
    TEST=logging_CrashSender,logging_UserCrash
    STATUS=Fixed
    
    Change-Id: Ibf9b314f1a03ee984eea2acf6007c120aa300dd1
    Reviewed-on: https://gerrit.chromium.org/gerrit/16751
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Commit-Ready: Chris Masone <cmasone@chromium.org>

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10c66e847b05c93b3d0feff42b76bb06d7ae06f9167eacfc36efdcc45155491e
-size 8368
+oid sha256:f91f7009e803f62f897b651c221a7f41be4535d3794b6d7125fcda049946ccd8
+size 8518
