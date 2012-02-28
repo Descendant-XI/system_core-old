5df5fc45ea35bdb3323437163eef8458467f0bb4
Dima Zavin <dima@android.com>
2012-02-27 16:33:03 -0800

    ion: fix header for including in C++ code
    Change-Id: I8fc10ac397e60d329a6887f0590d4dba6cbc7545
    Signed-off-by: Dima Zavin <dima@android.com>

--- include/ion/ion.h
+++ include/ion/ion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:123a01a2a2a7d3471187781fd96ebdb71677bf41d5feef74c7cf85b8ee396328
-size 1160
+oid sha256:1bc33147a73fae0f2248787acc10511a0029f7555e15f8b001334d7d4ab17d07
+size 1269
