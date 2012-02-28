4f8bbfa1bb9a14aa95d5ca41b08aacbc65c1ea9e
Andrew Hsieh <andrewhsieh@google.com>
2012-02-27 16:48:18 -0800

    Fixed two 64-bit porting issues; Make pid/tid type consistent
    1. In printf, use "%zu" for variable of type size_t
    2. Print tid in %5d
    3. Make type of pid/tid in AndroidLogEntry and logger_entry consistent
    
    Change-Id: I3e3d9536ee58823f349a4734ae093d30eabe1bfe

--- include/cutils/logprint.h
+++ include/cutils/logprint.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88973f1d5f125b0fb2e93dab28e5288616ed5bd1a48b818298fbfbe981fe21e6
+oid sha256:7a986f9d442cbc7238780498debb7882c3cf57f8aa6053bc05fe31240f1ba832
 size 3748
--- liblog/logprint.c
+++ liblog/logprint.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:953106d4b22d15d62cf0d043e82701a12f1c571477ea34fe65e18e26adacf7e7
-size 28234
+oid sha256:88863fb525f1477dfb1be56157973666c1da6fae05d3a46223a068b960b67109
+size 28213
