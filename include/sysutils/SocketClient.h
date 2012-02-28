22a4a74dc0de97255d98d6feae6d04c424e2fa0d
Selim Gurun <sgurun@google.com>
2012-02-27 16:04:37 -0800

    Add a utility function to send a code + binary msg
    Add a utility function to SocketClient to send a c-string code
    prepended to a binary message. This is necessary to be able to
    send a binary message while keeping compatible with underlying
    text-based protocol.
    
    Change-Id: Ifc6562003a687577d7deb50260533a5147ae4f97

--- include/sysutils/SocketClient.h
+++ include/sysutils/SocketClient.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e3acfe4e3c0a8c2cf97b7809991e065b128bac8e6fb8a470a387c964a2504e9
-size 1749
+oid sha256:812f6d808603f77ba79471a07ad2a9d033b14b87b288b2d56263c4ea5bcc8671
+size 2590
