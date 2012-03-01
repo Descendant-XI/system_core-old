0fb0c1ccc4125cebde8c52666f6ff38016fc1f09
Eric Laurent <elaurent@google.com>
2012-03-01 09:43:07 -0800

    renamed audio policy output flag.
    Renamed AUDIO_POLICY_OUTPUT_FLAG_INDIRECT to AUDIO_POLICY_OUTPUT_FLAG_NONE
    which is more appropriate.
    
    Change-Id: Ie898ddc2279db55a65ece96e09232163c2e78024

--- include/system/audio_policy.h
+++ include/system/audio_policy.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11641512fb568bf8dd42ce9b0d1c39be07de93a56883b3d099128d6dd1b1e8ae
-size 3034
+oid sha256:de9f5589f144b1f9c74faf4ba93bfc6c10388cbe23999d8e9c102b62d7b6c887
+size 3030
