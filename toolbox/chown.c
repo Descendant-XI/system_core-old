2a010a95b2547ad28b3db9cc3ceea947b2bfb6b8
Jeff Sharkey <jsharkey@android.com>
2012-03-02 15:11:53 -0800

    Change "chown" to coreutils syntax.
    Change-Id: I7d997b3254354908317feee54b6df9b2419718a6

--- toolbox/chown.c
+++ toolbox/chown.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:506682c4629bbe371984ea4c7b934f9bea2b6cfee058c3ecb013248f1e36375e
-size 1707
+oid sha256:14b8432f91264e158ef39306d4e4d04b07a5ff6412c86c7e3afd47c1b2d9e150
+size 1786
