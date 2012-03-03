6c529f0fe3c14793ed985585be86b3c3f69d712f
Jeff Brown <jeffbrown@google.com>
2012-03-02 17:33:52 -0800

    Use #define value of 1 to indicate presence of headers.
    strace tests the value of HAVE_TERMIO_H and HAVE_SYS_UIO_H using
    
    Change-Id: If899845503547410cf17d005733d1c0c6741e939

--- include/arch/linux-arm/AndroidConfig.h
+++ include/arch/linux-arm/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:565bae01d07c9d92003156d20b9cafdf53600e2d8d3960ed57d698748998e9a2
-size 7739
+oid sha256:cf4c84980b89398c17f0465865483cd2c69e4dd79d0f908dd4a74757fba9bec2
+size 7743
--- include/arch/linux-x86/AndroidConfig.h
+++ include/arch/linux-x86/AndroidConfig.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a12177d7a67a39ec07adbdec2447f42923f8aa6c423542a3230491c9882c7005
-size 7133
+oid sha256:edff71dd414f814ff11bdd6f4df478f6f36c4d2f5267d45e392b268b33b9cd35
+size 7137
