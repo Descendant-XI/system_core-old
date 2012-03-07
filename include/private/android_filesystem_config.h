caf5a766ecec723f0549fccb7129ad426962cb1c
Dianne Hackborn <hackbod@google.com>
2012-03-07 12:57:14 -0800

    Add AID_SDCARD_R.
    Change-Id: I47f650e415e06c0b69790ffefea7f208c910ebd7

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594513e61ba7f70867005bf107608d36c96d724662ce1febba35050a00c47c89
-size 11001
+oid sha256:abb9d0a9824544ea24b3cb4c941f3688d8e669924af3019cb5e510a1a5eabafa
+size 11068
