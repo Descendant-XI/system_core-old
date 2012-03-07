95b06b75bbe46d2bbdf7acd8bf858271b1024da1
Mike Frysinger <vapier@chromium.org>
2012-03-07 10:35:29 -0500

    include base/stringprintf.h directly
    This code uses StringPrintf, but doesn't include the header.  Older
    versions of libbase would implicitly include it, but newer ones do
    not.  So include it ourselves to fix building with newer versions.
    
    BUG=chromium-os:25872
    TEST=`emerge-x86-alex metrics` works (with newer and older libbase)
    
    Change-Id: I7859f67767958f70853a40e8df4a33a64b36d010
    Reviewed-on: https://gerrit.chromium.org/gerrit/17487
    Reviewed-by: Darin Petkov <petkov@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Ready: Mike Frysinger <vapier@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a86c990db44c93568218c8e766d49efc007afc78a0f82f074f094019a9c7d3f
-size 34378
+oid sha256:13289873108c1f6801618e052fa38285bd55794cf6dd9bdffa8feebc93bce8a6
+size 34409
