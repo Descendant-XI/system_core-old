cfe3d2734a49db13c9c3bd8b4450060d25cdaa98
Andy McFadden <fadden@android.com>
2012-03-08 15:27:49 -0800

    Reduce suicidal tendencies
    Some changes to debuggerd:
    
    (1) Send a SIGSTOP to the target process before doing any of the
    PTRACE_ATTACH work.  This causes the threads that weren't hit by
    the signal to stop moving, which seems to get rid of the "ptrace
    detach failed: No such process" problems that result in missed
    stack traces and debuggerd self-immolation.
    
    (2) SIGPIPE was in the list of "interesting" signals caught by
    debugger_signal_handler(), but debuggerd didn't recognize it, and
    you'd get "unexpected signal 13" complaints.  It's now in the list.
    
    (3) Fixed a bit-rotted XLOG.
    
    Change-Id: I33297ea6b09ce450671c07dc25846f684afbf480

--- debuggerd/debuggerd.c
+++ debuggerd/debuggerd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4afb3cfb070a037ac6f5b6b5482b01c8765c0ecfe0a439ce08e1edc4d5ef9b6
-size 29443
+oid sha256:3a5bff2047424e7497fa568f59ea7441e2f1bb36f7162bcbb30c2258b02902a9
+size 29929
