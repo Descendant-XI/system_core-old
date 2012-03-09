c1cb7753969d1e8444e39bda852f3ee263b9025c
Robert Greenwalt <rgreenwalt@google.com>
2012-03-08 16:10:06 -0800

    Fix pointer arith.
    Sending 3digit code, null, binary-length and data.  Current code sends
    3digit code, space, null, binary-length, binary-data.
    
    The space should not be sent.
    
    Change-Id: I1df00a3e70805b39b6105966c7f3c613461a6858

--- libsysutils/src/SocketClient.cpp
+++ libsysutils/src/SocketClient.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c06ac775934e0f7e30f427098c874fd6c5106ac21c05ab7566a76a899ba5687a
-size 4296
+oid sha256:e77843630b1815f4203af6d1f89873aeccbf642457abe893baa77275c1bafe49
+size 4371
