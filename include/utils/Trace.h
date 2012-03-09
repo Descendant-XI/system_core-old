b3420f62bb84c82e971092d18c31d66c7b6974c1
Jeff Brown <jeffbrown@google.com>
2012-03-09 14:46:01 -0800

    Expose the set of enabled tags.
    This is used by the Java trace wrappers to avoid JNI overhead
    when trace tags are disabled.
    
    Also added a new tag for the input subsystem and view hierarchy.
    
    Change-Id: Ia04a507e42a37b1d3bbb19859e6c07a92f4fe9aa

--- include/utils/Trace.h
+++ include/utils/Trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecc2572df703c7d76b497eec6d66e942244cb448a97a996a7e9a10a69ce010ea
-size 5726
+oid sha256:02ec73c8979aafa1f8a53dbe7e0cd77408089d29cef76438cb57d6f0f7ca0b95
+size 6007
--- libs/utils/Trace.cpp
+++ libs/utils/Trace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b73ba7b5619833d099f87c80c3f5632bcfbd004348b6edb754a3559defdc013
-size 1476
+oid sha256:018bac872ee31080fa52f9b93172531759da03419225e8cc116154165c77b01b
+size 1600
