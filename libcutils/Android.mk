05cd4e2efa37580b9aba30cb4b28c01776ee70dd
Dima Zavin <dima@android.com>
2012-03-12 15:04:52 -0700

    Revert "cutils: str_parms: make test executable build for host instead"
    This reverts commit 7d31e6e53392ca6ce7fbe8f17efbeda8b0e91ccf.

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7919f122af05cf58e2b6b1741f22c9df3d36a25e355ba70c52c644daa653f38
-size 3816
+oid sha256:73a27e1c936c4e14d3213ca3f49b419dc2cd5e210572dd61bcda288a9b9dfe91
+size 3811
