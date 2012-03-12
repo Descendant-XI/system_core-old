7a0248dbe3ea85b493ec57ce88ebaefa51fdfe4d
Dima Zavin <dima@android.com>
2012-03-12 11:02:00 -0700

    cutils: str_parms: test str_parms_add_str too
    Change-Id: I93f0e577aff6b8ff23a44e29f9942a26545b6dc8
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcutils/str_parms.c
+++ libcutils/str_parms.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ee648e56cd84a7098334c594d9a40533cbbabaef3ac4ae80b3a23a3379af754
-size 7421
+oid sha256:cff7ef88a330578c6ab54b9b11e4b37b6a4faf075d2b6c3410594010905e32ae
+size 7471
