6be274ad9970a013713feaccaf7b2884ac460d7e
Dima Zavin <dima@android.com>
2012-03-14 23:12:40 -0700

    cutils: str_parms: when deleting entry, need to also remove key
    When removing one entry, we can't just remove it from the map since
    key will not be freed. So, instead, search through the map for the key
    and free it once it is found.
    
    Change-Id: Id745c166a4acc5ea25f6d6cdbfe4f8fbc21cbbb3
    Reported-by: Xin Qian <xqian@marvell.com>
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcutils/str_parms.c
+++ libcutils/str_parms.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9a0d644b64f003578b7db06a6f74d51bc327cc9db6eaabc58554c9ebd60fc8d
-size 7580
+oid sha256:c37fddae2deb4b9f746b876a857bd2d5c32b6d3283c9944d70ba30d1450f6b61
+size 8471
