472a5adde180c9c1529c55ca302b4ff92c94cbed
Glenn Kasten <gkasten@google.com>
2012-03-16 09:43:19 -0700

    Scheduling group cleanup - add comment, SP_DEFAULT
    Also tid == 0 maps to gettid() on platforms that support it.
    This is needed by mediaserver code that was formerly using
    the old C++ API.
    
    Change-Id: I8baaa3e42315c9b924cec26b6da01e7cb69e0078

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:760064101851e7fabfb69fd444c8bafb8f4271960b87984461c7e633831b3965
-size 7177
+oid sha256:d7c15e33bdaa9ace47d6972e6ec282edc212a17dc243508309c66e40d7bbda8a
+size 7693
