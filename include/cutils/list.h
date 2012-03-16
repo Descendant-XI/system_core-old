195ca415738bb2dad9672c6d76359a22be7d8fc1
Kenny Root <kroot@google.com>
2012-03-15 21:45:00 -0700

    Add cdefs decoration to list.h
    In case someone includes this from C++ code.
    
    Change-Id: I3b5a8bde5b5b9e8dcee64abaf33383f21eff7046

--- include/cutils/list.h
+++ include/cutils/list.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6073ab926e123b677ede2f65bcb4594acd34dc36f146180ff4e1a9912387339
-size 1495
+oid sha256:62a3ac24dff1f03d44edd0bf7726444361760797b734e8a3e4236e8176c618a5
+size 1546
