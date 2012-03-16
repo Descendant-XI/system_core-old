fa4910ed746ce44c854ae77e8343833f09e5dc43
Benoit Goby <benoit@android.com>
2012-03-16 14:50:07 -0700

    adb: Don't synchronize with transport using sleep
    Services that cause adbd to reboot are currently implemented this way:
        write(fd, response)
        close(fd)
        sleep(1)
        exit(1)
    sleep(1) is necessary to leave time for the transport to transmit
    the response before the daemon exits. This is slow and unreliable.
    Instead, add a flag to the asocket to make it exit the daemon only
    after the socket is closed.
    
    Change-Id: I9df45ea6221f0d9b828703b9b2316a5d4fe59352

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3873ead07abb80de03c9e41d039199e34da4c4eef9e6e88b5c11b0e2deec92b
-size 13971
+oid sha256:68fc7789914faff41aa27c43a32d638b2305d6094ec53d19fbf8fe1fba07d897
+size 14093
