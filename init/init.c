b294d54bae73e4e37a0dcfaf2ab1ef9bdc002687
Colin Cross <ccross@android.com>
2012-03-18 15:38:19 -0700

    init: manually set controlling tty for console services
    Normally, calling open on a tty will set that tty as the process
    group controlling tty if none already exists.  However, if the tty
    is /dev/console, the kernel will never automatically set it as the
    controlling tty.  Call the TIOCSCTTY manually on the fd, which will
    always attempt to set it as the controlling tty.
    
    Fixes ctrl-c on the console shell when androidboot.console is not
    passed on the kernel command line and the default /dev/console is
    used.
    
    Change-Id: I449cc41b47e93ac38ad6987413bb54131e1ec0cd

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:242c80243db66ac48cd2662d4452810b5e8a84154186c9330451fe95461f4a8b
-size 27606
+oid sha256:ca65e63634a2b121f5ff634459f7ec207d08dfcded42ff5fa58fb4d859be1ddd
+size 27635
