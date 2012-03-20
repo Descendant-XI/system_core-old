408bb3dc24d912c958a6abd208376a3433c47cc0
Kenny Root <kroot@google.com>
2012-03-20 13:46:24 -0700

    MinGW does not have sys/cdefs.h
    Change-Id: I5cafafce8488d355ed3865a9fd1173fbebbc5b03

--- include/cutils/list.h
+++ include/cutils/list.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62a3ac24dff1f03d44edd0bf7726444361760797b734e8a3e4236e8176c618a5
-size 1546
+oid sha256:7946b97b5120c85f4fcdf319e527587c9730f9d9596e4c6b4fb5ba6199f1f59b
+size 1601
