23be4d82aa14c37d19979b951724d13f24a3d61c
Nick Kralevich <nnk@google.com>
2012-03-22 14:28:16 -0700

    logger: Add the update kernel struct to userspace
    Android's kernel logger can optionally return UID information
    in addition to the previously returned information. This
    information is available by telling the kernel to use the
    updated structure via the newly introduced LOGGER_SET_VERSION
    ioctl.
    
    int fd = open("/dev/log/main", O_RDONLY);
    int version = 2;
    ioctl(fd, LOGGER_SET_VERSION, &version);
    
    Change-Id: I6bfe7bfb9df45a56124aa8d0d19e7d7a18e06862

--- include/cutils/logger.h
+++ include/cutils/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0f4cb2f2cf2d847cf3efdf9de9c04c9fd6e8d03683bad6cbe052ed404d4e494
-size 1392
+oid sha256:5a85065bf15252195cb2bc20f5e3eb302fcf6c3c44ee26a21fbc839d68b2ff5e
+size 2701
