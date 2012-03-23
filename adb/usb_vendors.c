65887223262b0fbf8173ee890d5b654749bd1eab
Andrew Hsieh <andrewhsieh@google.com>
2012-03-23 15:28:56 +0800

    Remove duplicated VENDOR_ID_INTEL
    VENDOR_ID_INTEL defined and appear in builtInVendorIds[] twice.
    Remove the 2nd one.
    
    Change-Id: I62f7bba392012aafb8ea9730959362db4bc6c669

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40d91888a044eb0b1f78a9f47df9cdb8498f3bb3b3a6b6369e4705f1f8cab1f4
-size 7696
+oid sha256:364c9b1670bbc51da90466fbd84d8bfa6c84848f5bcc35e23f489c9cee207513
+size 7612
