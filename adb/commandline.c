625a3dc14ce0cfb86b26ab1f59830dc5789c7db1
Kenny Root <kroot@google.com>
2012-03-26 16:14:02 -0700

    Make sure "adb install" work is cleaned after error
    If an error occurs during the verification of an APK, it could be left
    in the temporary installation directory.
    
    Change-Id: I4afa5535fc0b978532b31d5b8fb63776963c39db

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f57fec728f7aafc69594932e2b6109331b3fc691ce7f227eb54709ca9e0cf6aa
-size 46773
+oid sha256:9c7ec4c8e40d278af2d346375e3b1c9f6a31df674b9505f803fb7d9cca710f5a
+size 46779
