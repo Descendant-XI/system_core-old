b1b6a0827a6af064bc43a314dda45d1044d3cb59
Geremy Condra <gcondra@google.com>
2012-03-29 14:46:07 -0700

    cutils: return the remote uid on netlink recv failure
    This change adds a layer of indirection to uevent's
    uevent_kernel_multicast_recv, allowing dependent code to
    get the remote endpoint if desired.
    
    Change-Id: If2f00708a1dd6196c415b1ebc2a7eeb72399a90e

--- include/cutils/uevent.h
+++ include/cutils/uevent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5920fe2eb843582936fb2701ae630f08fdad9fe50d333fc0431eef34d27e8a33
-size 951
+oid sha256:3ff1460c57d1d87ef5997539e66fd88e2bd29bd0795c4a797761da1bc72a72f0
+size 1046
