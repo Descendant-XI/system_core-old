c62e0f5fff5d0adb27318d6f016c2684523d9ae9
Kenny Root <kroot@google.com>
2012-03-30 20:44:45 -0700

    fastboot: add support for SELinux and fs_config_func
    There were some changes to make_ext4fs to add SELinux context. Also, we
    changed the "android" argument to an fs_config_func that allows you to
    customize the permissions put into a ext4 filesystem.
    
    Change-Id: If23152c40659122a91771fcbd094f713b38e321e

--- fastboot/engine.c
+++ fastboot/engine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb8da494d9d6e5b1d357d7d5eb096ae8aebc9493a0e7cd5a76ca68c9a9c16941
-size 14226
+oid sha256:cf12f2642f048af573ee870dcf8df71614de13e4f1d232491569cf0e6fcc3eb6
+size 14235
