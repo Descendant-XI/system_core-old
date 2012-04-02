f76faf72405f23b2dc596bba5c7ca857ff687eab
Nick Kralevich <nnk@google.com>
2012-04-02 13:00:35 -0700

    adb: set umask to 000
    Init is going to be modified to set the umask to 077.
    This will impact adb, as "adb install" and "adb sync"
    assume that files will be world-readable / writable.
    
    Keep adb's umask at 000 for now.
    
    Bug: 3272072
    Change-Id: I75978e8dd7bbdf4dbe7c9a691ef516235f207da2

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb0460af6f1e44e7d8455f1b7405fb39bde1c25bafe1a06a8d3f6e15aab14bd
-size 38921
+oid sha256:5c8606c7f172aaa0bae4af5f9009928087dd7702c45dca4b1f85dfd5194b37c7
+size 38938
