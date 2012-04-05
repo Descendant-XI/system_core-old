7c8c16a3685c7e861aeacf6e39e27c2fe0026891
Kenny Root <kroot@google.com>
2012-04-05 15:23:20 -0700

    Add signal names to kill
    mksh provides a kill primitive with signal names, but also offer it in
    our toolbox binary. This will allow anyone to use "kill -HUP <pid>" or
    "kill -l" to look at the name to number signal mapping.
    
    Change-Id: Id683721f4ad3f2b518b4dc54a6073510290cbe04

--- toolbox/kill.c
+++ toolbox/kill.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5247d46529dfdbce49dda19156d86ebd8882e6a9999ff1d51cefac405010c84a
-size 633
+oid sha256:2c443f806fe7ade8042edf97137731ce0f456610e049882514302b35db980269
+size 2932
