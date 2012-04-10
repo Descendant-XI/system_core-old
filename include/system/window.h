714c14817758a10a27aac569f2c1b992d0efc0ac
Michael I. Gold <gold@nvidia.com>
2012-04-09 18:21:13 -0700

    window: add API for native_window_set_buffers_user_dimensions
    Add token and wrapper function for a new private function
    native_window_set_buffers_user_dimensions which overrides the native window
    size as the default buffer size.  This is intended for use primarily by
    ANativeWindow_SetBuffersGeometry so that application-set buffer dimensions may
    co-exist with pre-rotation in e.g. the GLES driver which calls
    native_window_set_buffers_dimension.
    
    Change-Id: I6c0fc334c070290cc15b04daae1484d0a28cd8ad

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfc533f5df77537877f954ecc70e9d33291fc6b0ff5c244583b762fbaf9909e0
-size 22907
+oid sha256:36a63f8052140d084d328d871f8ae411dc3badb3e323457de195714288469e1e
+size 24106
