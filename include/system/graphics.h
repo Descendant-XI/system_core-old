5ac01a93ea36a750ad2f46fcbe55239451d63a8d
Eino-Ville Talvala <etalvala@google.com>
2012-04-10 15:10:50 -0700

    Add new raw image sensor HAL pixel format.
    This format is exposed outside of the HAL to applications.
    
    RAW_SENSOR is a single-channel 16-bit format, typically representing raw
    Bayer-pattern images from an image sensor, with minimal processing.
    
    The exact pixel layout of the data in the buffer is sensor-dependent, and
    needs to be queried from the camera device.
    
    Generally, not all 16 bits are used; more common values are 10 or 12
    bits. All parameters to interpret the raw data (black and white points,
    color space, etc) must be queried from the camera device.
    
    This format assumes
    - an even width
    - an even height
    - a horizontal stride multiple of 16 pixels (32 bytes).
    
    Change-Id: I67fc72d1c2c899e258fcd53b3ab91b459e9210f0

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1735b107cb83423be55ff28245c4000fdef4f10d5cd1077be5b168811f0c517
-size 3820
+oid sha256:e7dbf42eea586df55b9a2c3c561c60de40d26285820499db1d57254dbd4b7696
+size 4653
