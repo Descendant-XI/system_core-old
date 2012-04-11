b3f0602494def0323f5fb2970803936eacaad737
Ashish Sharma <ashishsharma@google.com>
2012-04-10 18:59:16 -0700

    libsysutils: NetlinkEvent: add support for interface xt_IDLETIMER events.
    xt_IDLETIMER events are used to infer the interface (radio) state
    based on a inactivity timeout value.
    
    Change-Id: I755eb54a048ca1c6ce0c646564d61188d8241851

--- include/sysutils/NetlinkEvent.h
+++ include/sysutils/NetlinkEvent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37b6ae1ec8c08e1436bc470951daa4f1be68562ca751c80fcb8b1c5df4de58de
-size 1542
+oid sha256:49e488ff6ba7e406557a183ea6c42d539a712c8a9a0e4d48721f7512daebc079
+size 1624
