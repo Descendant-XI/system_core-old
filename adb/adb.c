9c29de3da8a4ad6156c4d395b39c02efd4f2ca0c
Benoit Goby <benoit@android.com>
2012-04-12 12:23:49 -0700

    adb: Add public key authentification
    Secure adb using a public key authentication, to allow USB debugging
    only from authorized hosts.
    
    When a device is connected to an unauthorized host, the adb daemon sends
    the user public key to the device. A popup is shown to ask the user to
    allow debugging once or permanantly from the host. The public key is
    installed on the device in the later case. Other keys may be installed
    at build time.
    
    On the host, the user public/private key pair is automatically generated,
    if it does not exist, when the adb daemon starts and is stored in
    $HOME/.android/adb_key(.pub) or in $ANDROID_SDK_HOME on windows. If needed,
    the ADB_KEYS_PATH env variable may be set to a ;-separated list of private
    keys, e.g. company-wide or vendor keys.
    
    On the device, vendors public keys are installed at build time in
    /adb_keys. User-installed keys are stored in /data/misc/adb/adb_keys.
    
    ADB Protocol change:
    If the device needs to authenticate the host, it replies to CNXN
    packets with an AUTH packet. The AUTH packet payload is a random token.
    The host signs the token with one of its private keys and sends an AUTH(0)
    packet. If the signature verification succeeds, the device replies with
    a CNXN packet. Otherwise, it sends a new AUTH packet with a new token so
    that the host can retry with another private key. Once the host has tried
    all its keys, it can send an AUTH(1) packet with a public key as
    payload. adbd then sends the public key to the framework (if it has been
    started) for confirmation.
    
    Change-Id: Idce931a7bfe4ce878428eaa47838e5184ac6073f

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acbf69ed25cc85c421a147f76d78f75290c84ad6ff6d155212f7278e0dfdb204
-size 42084
+oid sha256:ed6840e551bc63659431f47662101e70af78f4be7f17d9c7a69aa1d5a18c71fc
+size 44587
