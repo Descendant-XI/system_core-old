5a7718cd521774d5c79fc23f97a141ff575f0513
Eric Laurent <elaurent@google.com>
2012-04-13 16:52:58 -0700

    audio policy: add deep audio buffering flag.
    Add a value in audio policy flags to indicate an output
    stream providing deep audio PCM buffers. Such an output
    helps reducing the power consumption at the expense of audio latency.
    
    Stagefright will indicate a preference for such an output when playing music
    only (no video) and the audio duration is more than a given threshold.
    
    Change-Id: I0e6a9f5746a22d2bfea2b21dd182489a34a4b528

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2858df0c38241c425adbade684abf229b6b2d2ae0574743eb3c9b5888061924b
-size 21049
+oid sha256:9651d1080e0ed390b10a2b317ceddd3ccdd840fefb7da95dda63e040c7d9ca85
+size 21145
