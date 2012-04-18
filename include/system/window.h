d1614db9027780be6a588081dbddceaf2c15d448
Jamie Gennis <jgennis@google.com>
2012-04-17 19:37:48 -0700

    add query for finding out if consumer runs behind of producer
    Change-Id: I3baae99d4da00d962d3afe6bdfc16e3eb04ede24

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8fe25433fc07515c2455ed8277056903e6017c83a48d06291b77de8be9dae40
-size 24308
+oid sha256:452df9a5d29d1cdfd8efd4036128072dd9727797ee7196e0c04f21a9a583909c
+size 24482
