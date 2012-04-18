989c705c75b2d66bd5eb243bd4fed6bcf6196d5b
Jamie Gennis <jgennis@google.com>
2012-04-17 19:37:48 -0700

    ANativeWindow: add set_active_rect
    This change adds the set_active_rect 'method' to the ANativeWindow interface.
    
    Change-Id: Iae910d259ba5c3e94594fa1ffec5fc0662546ad0

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8fe25433fc07515c2455ed8277056903e6017c83a48d06291b77de8be9dae40
-size 24308
+oid sha256:9579d26837d90fa14fb2a5b64f4be293bf766aa4a2d8f93573555d5104787cdf
+size 25541
