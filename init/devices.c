0fc99855565ff3f67025fe9823a9a9ac7ddd24bf
Johan Redestig <johan.redestig@sonymobile.com>
2012-04-18 16:41:19 +0200

    Allow more characters in partition name links
    We have several partitions with underscores in their names
    which would not be properly linked in:
    /dev/block/platform/msm_sdcc.1/by-name/
    
    With this change more characters (_-.) are allowed in
    partition name links. Also, any other character is
    replaced with '_' so the resulting link names have the
    same length as the partition name.
    
    Change-Id: I746566c03db98b10326c755692362d2c10e528ae

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83b2db1b1c3fe6af6ae6e87e5d4a1d4f90fc5365e7c0968e604bca283cf35c6a
-size 24177
+oid sha256:e8eb44fa6b449cecbdba6f2db3fa0658f16e541f23c242b1ceaea0dfde690575
+size 24307
