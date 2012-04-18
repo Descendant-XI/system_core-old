34756fd146eaf1b9ce162369e218dc053b48dd75
Ken Sumrall <ksumrall@android.com>
2012-04-17 17:20:16 -0700

    Change init to use libfs_mgr to mount filesystems.
    The new fs_mgr library moves much of the knowledge of what filesystems
    to mount into a new fstab.<device> file, and just calls one function to
    mount all the filesystems.
    
    Change-Id: If3db37530a0676000cba3e679db27aca734227e5

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82835b07b08ebc259e1d090df8e6f6cdb9eaabff4cf7bc543b6a96c5de5bdbab
-size 1444
+oid sha256:ac97d8b6c538117c687f63caf7263403660425c596e2e5c9381463a96631c0b6
+size 1454
