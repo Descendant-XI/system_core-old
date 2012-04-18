5dbbac02732615400b7ed8f4ec8ad3ae4de5215f
JP Abgrall <jpa@google.com>
2012-04-18 15:30:56 -0700

    fastboot: Keep borked bootloaders happy by shrinking URB len.
    Some devices will fail to return the requested data if the final
    URB len of the request to > 255.
    So shrink the current data buffer from 128 to 127 in the acse of getting
    the serial number seems to work just fine.
    
    Change-Id: Ic24e6c86ec93f002dc721d92309b3f880f281f20

--- fastboot/usb_linux.c
+++ fastboot/usb_linux.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1480478ce5048a8a01b057672fb5be731eb3e53cf7f0cbcdf25c9079a567036
-size 10372
+oid sha256:ff3e6c02d13d53ff2af332ba131220e946a69859e932d9ce0e9354e6740e3986
+size 10496
