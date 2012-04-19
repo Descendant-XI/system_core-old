82a5d1b4bdd0f5f2df5d0e6d82a22243971fdb21
Glenn Kasten <gkasten@google.com>
2012-04-19 15:25:58 -0700

    Add more SP_ constants
    Change-Id: I52c8ea33b9b915fc08546f2ca0b191391f58506b

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7c15e33bdaa9ace47d6972e6ec282edc212a17dc243508309c66e40d7bbda8a
-size 7693
+oid sha256:357f570d4ac32b57f6606d01e0398db997f4054daaebbb9f9d0ef0412b3a6cf2
+size 9540
