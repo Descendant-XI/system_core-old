644f4861d9b56ccdd35fd7279096dc363a52c5a5
Jean-Baptiste Queru <jbq@google.com>
2012-04-24 16:45:38 -0700

    Forward compatibility
    Change-Id: I51a5eb98fb3fa3e09f2eb75de128068572081046

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1735b107cb83423be55ff28245c4000fdef4f10d5cd1077be5b168811f0c517
-size 3820
+oid sha256:a70a90bc3e14521d313bbc6465d697aadc41542157481bd19fe55a577bae8ed3
+size 3891
