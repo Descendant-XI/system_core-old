c60fe7e657ba20a3d0373ac8579ed6cb5fc1e450
Simon Que <sque@chromium.org>
2012-04-30 15:41:10 -0700

    crash collector: compress udev crash logs
    BUG=chrome-os-partner:6492
    TEST=Run:
    "crash_reporter --udev=ACTION=change:KERNEL=card0:SUBSYSTEM=drm"
    Check that a card0-drm log.gz file has been created under /var/spool/crash
    Check that the corresponding .meta file points to the log.gz file.
    
    Change-Id: Id1ecc82b9fac118f8bf1e5edc40c3a2f61abd4c4
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/21495
    Reviewed-by: Ben Chan <benchan@chromium.org>

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fa96b918449c1b5dbad78dbe881d5437515df85ff79337138d87c913d0a4aaa
-size 16583
+oid sha256:6893066fba620a8800734f0a9a8e980b740b83b5671eaf1c9033bd55ed0f3db5
+size 17150
--- crash_reporter/crash_collector.h
+++ crash_reporter/crash_collector.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec59a1481bd78d3e4b257f9bc280f417cc3bb5be9507e5bc9a50bcfe13a0b530
-size 6335
+oid sha256:796285a13c22cdc8d13c0d662e077c8c803f53ec269e97fb21621203afaaf09e
+size 6395
