593660ba8e591456cddbef84abfa5c90d301cfa7
Stephen Smalley <sds@tycho.nsa.gov>
2012-05-01 15:02:53 -0400

    Add support for reloading policy from /data/system.
    To support runtime policy management, add support for reloading
    policy from /data/system.  This can be triggered by setting the
    selinux.loadpolicy property to 1, whether from init.rc after
    mounting /data or from the system_server (e.g. upon invocation of
    a new device admin API for provisioning policy). ueventd and
    installd are restarted upon policy reloads to pick up the new
    policy configurations relevant to their operation.
    
    Change-Id: I97479aecef8cec23b32f60e09cc778cc5520b691
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- toolbox/restorecon.c
+++ toolbox/restorecon.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22c4aa07d6a4538034a6ae05bda8241d7ee4a0dbbe900bb9f0b240e286c3bbda
-size 3717
+oid sha256:24ba5f77d283a1ab85efe5862bc0fc928c036dec2b880c6c8d85481778d2a104
+size 3524
