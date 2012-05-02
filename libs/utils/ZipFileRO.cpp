f7104d7e360712f5e9993fb1afe7359442107a86
Andrew Hsieh <andrewhsieh@google.com>
2012-05-02 12:30:00 +0800

    Fixed 64-bit porting issues
    Fixed two issues about casting between int and pointer which
    cause compilation error in 64-bit mode (eg. for 64-bit emulator)
    
    Change-Id: I5ca9a933a239db73e56d6f27de05aa42bd06a650

--- libs/utils/ZipFileRO.cpp
+++ libs/utils/ZipFileRO.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f42f57f4516a6ad0bf747bd11d923561eb4d74eb9743f1382a486c8b7b9854a8
-size 27982
+oid sha256:962c98eff7ad3e31f9348325add8bf51c2f8f100d0e6434f79031908d38f1a50
+size 27996
