9fd03a89cd9e9c0316d2cae9617eb075330cac5c
Yunlian Jiang <yunlian@chromium.org>
2012-05-02 14:24:04 -0700

    Add unisdt.h to make gcc 4.7 compile it
    BUG=None
    TEST=compile passed
    
    Change-Id: I3c52e0c7b60572058d277c05ea05471327149601
    Reviewed-on: https://gerrit.chromium.org/gerrit/21672
    Reviewed-by: Darin Petkov <petkov@chromium.org>
    Commit-Ready: Yunlian Jiang <yunlian@chromium.org>
    Reviewed-by: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>

--- metrics/metrics_library.h
+++ metrics/metrics_library.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d2e9899dfea070b5a748dedb539f345c36e8e3281714e0dc788a7e87aba44aa
-size 5870
+oid sha256:8155c121f1330431e58669d681a376298185d724167ac8b06d60d33c7b819aab
+size 5890
