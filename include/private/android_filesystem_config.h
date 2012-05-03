44e69a5d1135dba049779f85c001c0307620f3ca
Doug Zongker <dougz@android.com>
2012-05-03 14:16:54 -0700

    change /fstab.* perms back to 0644, to unbreak incremental OTAs
    Change-Id: I21a991148fd1de604ecf87abad0b49970d2abe84

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79e612bb758257eb51212d6b1a8b99aacc9c5594f2e43c64d204c4836196897d
-size 11240
+oid sha256:97cfabc3fbff5002b61245a224a9d8cb9a76522750ef496b55ffc5a4c707bf00
+size 11227
