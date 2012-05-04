f7355212b345304a23d3118d61447a3bd59dfa6b
Colin Cross <ccross@android.com>
2012-05-03 17:30:16 -0700

    libsuspend: create new library to handle triggering suspend
    libsuspend provides functions autosuspend_enable() and
    autosuspend_disable() to trigger suspend on a variety of different
    kernels.
    
    Change-Id: I5dc28fb51532fa7c514330f1cfde7698d31d734c

---
+++ libsuspend/autosuspend_earlysuspend.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d210635b94e2ad65741c09e268337a14c739d8ce2d73bf644eeee77ef3c16ea
+size 2870
---
+++ libsuspend/include/suspend/autosuspend.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d35f7dd89c161cc04506b1fab15bdc24714df33de64592b6878f68a89a4a4a59
+size 1231
