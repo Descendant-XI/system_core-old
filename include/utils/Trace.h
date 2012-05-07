bbcb1d211755b124eff0e7c2bab1b6049ba149bc
Glenn Kasten <gkasten@google.com>
2012-05-07 11:08:31 -0700

    Add audio tag to systrace
    Change-Id: I4a558c43e4ccd81a4ed94684c5fad6b3c486bd14

--- include/utils/Trace.h
+++ include/utils/Trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a58dce443d4e122dac144838fd7a5bacb674d92df25c42e0222322f45b197fdb
-size 6227
+oid sha256:e208cd1bc13bb036db06e45b162ab0778128255331930d545e57427fd9710023
+size 6263
