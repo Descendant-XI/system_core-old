1d5980bca565a0228f30f54f60356da0b69165af
Colin Cross <ccross@android.com>
2012-05-18 14:49:50 -0700

    libsparse: pseudo-subclass output_file for normal and gz files
    Create two subclasses of output_file that can handle normal
    and gzipped files, and refactor open_output_fd.  Will allow
    adding support for an output_file type that is not file
    backed.
    
    Change-Id: I26744c74d13f205cf17df1ea9caac1eea9c57357

--- libsparse/output_file.c
+++ libsparse/output_file.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad754e83b1be19c047b4e460d18e8de9fb1daeedd6b5d0d7c1dfc213a2f2ac0
-size 12934
+oid sha256:d78579aa51bc34fa6e66b34281edea6c1884d01990a773fe612778cdb120681d
+size 14650
--- libsparse/output_file.h
+++ libsparse/output_file.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e74f3c624c3bfe60cb9d4d45f3cc4b37ae879cbb057749b10296dca73161455f
-size 1503
+oid sha256:c3191d3261f39c6f9ab778d62ab24b54b3c881de1a50646f549d5df9e27e5b8f
+size 1360
