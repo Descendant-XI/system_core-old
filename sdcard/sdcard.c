8c558f0e5f421d17fe93e62da1b6c5693334df8f
Jeff Brown <jeffbrown@google.com>
2012-05-25 15:35:28 -0700

    Move buffers into a handler structure.
    Also use PATH_MAX instead of PATH_BUFFER to determine the
    maximum path length.
    
    Change-Id: Ic78f731d339a2a97766d29d222dd27cac4e620ce

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdeaba85d8cf680131fab1e2ff83e3cc7a4a07473661eb0d9113990e5e3476c9
-size 30818
+oid sha256:3ea5bf58a3ed5291f3bea8b6097c53551939c99726d20821c66cdcf00080ee32
+size 31875
