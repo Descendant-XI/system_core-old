21a54edc38424ee21b8969baa7dc4fbdd3b892d2
Duane Sand <duanes@mips.com>
2012-05-24 22:18:38 -0700

    Add Mips memset speedup
    Change-Id: I157aa8b500e271c213bf615dd4ceb25b3aedd7c2

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3225df5e36e64ca2c7a52b481ac16975d378207e12d6e58fe387478e9f0266b0
-size 4159
+oid sha256:7a45f0170f8e9ba80e39bf3b07f0cae173307678d50bd378bbb8bec5acbb57de
+size 4259
