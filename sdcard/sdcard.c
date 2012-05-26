b93f54cc3d842858f76d16df1ae6720eacdfcec5
Jeff Brown <jeffbrown@google.com>
2012-05-25 17:24:17 -0700

    Refactor request opcode handlers.
    This is mostly a structural change.  The handlers have been moved
    into individual functions, which will help with upcoming changes.
    
    Change-Id: I774739d859e177d6b5d4186d2771444166b734fa

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ea5bf58a3ed5291f3bea8b6097c53551939c99726d20821c66cdcf00080ee32
-size 31875
+oid sha256:c4f920ca07c30e1b6f361a1cc4400c22d77926075c2fbeee65ff049be4cfd8b6
+size 36773
