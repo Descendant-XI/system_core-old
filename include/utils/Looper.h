1fece19f7ee532ac9b781a34e15a23c6dcee069c
Jeff Brown <jeffbrown@google.com>
2012-05-30 19:17:47 -0700

    Remove unused statistics code.
    Bug: 6559630
    Change-Id: Iacdf4bb4c1c125c09305cbd8cb443c7c80cfc010

--- include/utils/Looper.h
+++ include/utils/Looper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:914ffc00a8e8649833d48586cdfe30c4b81cafe350f65da4b1ef340f4493078b
-size 13020
+oid sha256:162410c2857ce06541caca6bec6f5e12b3ac291c51f284b46b045ad5418c550d
+size 12444
