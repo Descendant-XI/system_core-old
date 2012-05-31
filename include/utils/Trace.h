3081b9daafa348f8e2987e4dee4c733cabe23fdc
Eino-Ville Talvala <etalvala@google.com>
2012-05-31 15:49:43 -0700

    Add camera as an ATRACE tag
    Change-Id: Ib4f22d593836c4b973d122efa114d359f1529914

--- include/utils/Trace.h
+++ include/utils/Trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:525ffc5502d491ebae4208d07210cb33f65009447e6ccc9b6b8e72c7287f1a59
-size 6438
+oid sha256:d4574723907fefebc3b93fec4f6755c457a60644cb5b9b9d5b27f5cce223acaa
+size 6483
