44a4cb36b5263b2c5319bb510c33fae578604f0f
Dima Zavin <dima@android.com>
2012-06-04 13:14:36 -0700

    cutils: sched_policy: remove audio app/sys groups, merge into fg
    Bug: 6528015
    Change-Id: I6c2658b62442d811b53c6d18476fbd6cd45f9e3f
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:000b604fa5afcb09d57ecb3807f71a97406b2400c5f0d4d011256e7c3f91e408
-size 9808
+oid sha256:728b317479b3f79fcabec5c6e1cbc0ae307d59b861093eaf264ae06e8568236b
+size 8845
