4a29142468c7147f8f15aa55c7e4b15305e875d6
Scott Anderson <saa@android.com>
2012-06-05 17:54:27 -0700

    adb: Use bionic's strtok_r() for Windows
    A recent change use strtok_r which broke the Windows build.
    Clear this potential landmine by adding adb_strtok_r to sysdeps.h
    in the typical fashion.  For Windows, the actual implementation
    in sysdeps_win32.c was copied from bionic/libc/string/strtok.c.
    
    Change-Id: Ibb71555bc429f7058c07c3d39e0b62859c79635c
    Signed-off-by: Scott Anderson <saa@android.com>

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28eec0d916b81aca7f55f7fd9c5b8ae723fa83bd380ca4044db30be54da857a0
-size 39369
+oid sha256:9b00880551b15601a9cbb42ddaf3f529f45db22af82a791d7e1e12a60db07dcb
+size 39377
