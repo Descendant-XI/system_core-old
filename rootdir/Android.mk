8338cb1ead25b86ddd2275cdda1a31deba3dc533
Bhanu Chetlapalli <bhanu@mips.com>
2012-06-07 15:15:52 -0700

    [MIPS] Enable SD-card functionality to work
    Change-Id: I711f533214b9b7bce2ff1000a55d6b8f310f2399
    Signed-Off-By: Bhanu Chetlapalli <bhanu@mips.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f989e814dcef04e98d4c22e1b2fdf1924e4ce1c81cde2d3a348625fb1c5b91a1
-size 2197
+oid sha256:71e2acc41c45d69e7e797434b265544020e2e3c5e91f2015b0248a960756e0a5
+size 2267
