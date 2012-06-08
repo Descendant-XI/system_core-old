42173033110b90f905c72e9eacc4199e1b6f1d9a
Eino-Ville Talvala <etalvala@google.com>
2012-06-07 17:52:15 -0700

    Add HAL_PIXEL_FORMAT_BLOB
    This format describes buffers that are not structured like normal
    image data, and can only be meaningfully understood by the endpoints.
    
    As an example, this format is used for transporting JPEG-compressed
    image buffers from the Camera HAL to the framework/apps.
    
    Buffers of this format must have a height of 1 and a width equal to
    their size in bytes. Otherwise their interpretation is up to the two
    endpoints.
    
    Bug: 6243944
    Change-Id: Ia81015694cbf3a89e29bfbf3624c3ea2ef4f66fd

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2560103b8d421b8abe590910e0e070234a3c9c6c1325f25b81e43e92dafda0a6
-size 4682
+oid sha256:468eae6d972fbeb6eea8602366fb870b117a7d39e1bc9fd831d815d3a2271360
+size 5213
