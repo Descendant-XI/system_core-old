55b65792a2fd126e5b4c92fc4e7f6c76935d639d
Rebecca Schultz Zavin <rebecca@android.com>
2012-06-11 15:12:37 -0700

    Modify ion to use new definition of ALLOC ioctl argument
    Also add ion_alloc_fd helper for when you only want a filedescriptor
    and know you won't need to access this handle again by its ion_handle
    
    Signed-off-by: Rebecca Schultz Zavin <rebecca@android.com>
    
    Change-Id: Ia4bae22946b0078084b62f5447fecbf261dfaa83

--- include/ion/ion.h
+++ include/ion/ion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc33147a73fae0f2248787acc10511a0029f7555e15f8b001334d7d4ab17d07
-size 1269
+oid sha256:e852976590eda36a26900800fa357221ff8dd50bd944f93ced36879678eb4fb5
+size 1401
--- libion/ion.c
+++ libion/ion.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:612ca7799cd06ff3782b8770807d62e8553facbdac6a0328f9ebaf92ebb3bb6a
-size 3460
+oid sha256:97392baf25794495b5535f33c33d0c565dde3ba98d434a39eae7d9774b7fa74e
+size 3827
