eb51370f0b31246d5443313310f2209ca44eef81
James Sullins <jcsullins@gmail.com>
2012-06-24 07:07:42 -0500

    toolbox/dmesg: do not hardcode KLOG_BUF_LEN
    Change-Id: Ia99654a53d6adfeba5a5088b7cff45c6e47b6188

--- toolbox/dmesg.c
+++ toolbox/dmesg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a054c4acd7ae85141362000472d808f502055aedadfc7e04b505c7767ae7e9f
-size 856
+oid sha256:ea37afe71a00d5ba0019695d3355bc905f6d192f829dc60271ee5675c2998482
+size 1152
