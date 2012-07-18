a604390cb974453a807d5ec9dcdc58a25ca3dbcb
Joe Onorato <joeo@google.com>
2012-07-18 12:03:33 -0700

    libext4_utils on host is now called libext4_utils_host so there is no name conflict
    Change-Id: Ibc7923676925ed6942b3f672ee764cfaa98ee96e

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0264b3606112a229068aaccc90605aa41cadefa6bfb816377bbd1552a9f23176
-size 2124
+oid sha256:1c1b23a51dc686030b9ca6cd216b209b16e51fec621dacb88cda524258e68b8e
+size 2165
