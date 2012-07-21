71f23fb1ef210e292c67e851842e7b604d17181a
Mike J. Chen <mjchen@google.com>
2012-07-20 18:35:32 -0700

    Fix build by removing TAGS from host module
    Change-Id: I0118cebff021b435baea0b46dd3c43250ef96632
    Signed-off-by: Mike J. Chen <mjchen@google.com>

--- gpttool/Android.mk
+++ gpttool/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:311e591f5593880f63bff4c5129a2d4148cb46bebeb5b04677c67cbca71b7ab4
-size 227
+oid sha256:7659dfa86599d9a02413209ea209a36461e41eac7a66832cbb1da12537ab3e79
+size 202
