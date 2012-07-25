8c48a64328b89db8550570ac17bd7cb4c85682de
Doug Zongker <dougz@android.com>
2012-07-25 12:08:33 -0700

    support e=65537 for libmincrypt, DumpPublicKey
    The output produced by DumpPublicKey now has a version tag on each
    line (ie, each key).  The existing keys are retroactively dubbed
    "version 1", and we add a version 2 for 2048-bit e=65537 keys.
    
    Change-Id: I204ec615c8f2346670220a1aeb99269e4abd5f81

--- libmincrypt/Android.mk
+++ libmincrypt/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:792c5ed0c46f7b62afec79ac1f5d7b924663f03750ddd0d646c4b51473350dc0
-size 405
+oid sha256:e295788b9950fd4d61692b4bead8b91456df108550c83eaa3ae0d1fe113671e3
+size 447
