38c144b86c41b9865df3e43c5935d4caa746c816
Colin Cross <ccross@android.com>
2012-07-24 18:05:21 -0700

    fastboot: change default sparse limit to disabled
    Change the default sparse limit to disabled to prevent resparsing files
    unless the bootloader or user specifies a sparse limit.  Some
    bootloaders issue an erase before every flash command, which causes
    earlier parts of the downloaded image to get erased.
    
    Change-Id: Iac6f3b05580aba8b82ed0f2f800979a49c33c691

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:026d2431679b5adf847f2d60f37d8b8fb38e7f07dc7e4eaa5babfa2841c5e8d6
-size 25936
+oid sha256:856cc854e8c9384380dd9bd579d55b00267e3c8672e13c9407de1ef7cb322a82
+size 25852
