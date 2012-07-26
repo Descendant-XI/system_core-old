38fc6d28d9bd82425e13bd9be1f2c158074499da
Edwin Vane <edwin.vane@intel.com>
2012-07-26 14:18:23 -0400

    Fix unused parameter warnings
    -Werror is used for this project so these warnings were causing the
    clang build to fail.
    
    Change-Id: I18c447ce239645e05f59c3cf0e2b8bb17d9d3030
    Author: Edwin Vane <edwin.vane@intel.com>
    Reviewed-by: Tarea A Siraj <tareq.a.siraj@intel.com>

--- libcorkscrew/arch-x86/ptrace-x86.c
+++ libcorkscrew/arch-x86/ptrace-x86.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:214abf30aa8134dbff2a7f0f16b81198c90767c15a3fcf026f7912a5651251e7
-size 897
+oid sha256:a6f46c83dc2a0ed8fa87c7f727b53018b2e2cec1e34d3810e9a2936ccd5f2d5f
+size 1125
