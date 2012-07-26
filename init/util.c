eb9304bcff7aa6a3463312782a498cf66cac9f77
Edwin Vane <edwin.vane@intel.com>
2012-07-26 14:09:13 -0400

    Fixed a discarded const qualifier warning
    Change-Id: I9007924d0bf49705363aa130e6e588a9e0ccdc31
    Author: Edwin Vane <edwin.vane@intel.com>
    Reviewed-by: Tareq A Siraj <tareq.a.siraj@intel.com>

--- init/util.c
+++ init/util.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2ede8674615ea5d01dd55f82cdb76a7d52eb011d79938e62f332b61e29c8b14
-size 10833
+oid sha256:4dbae60a3a9f1f192e3173d2ee91af02ef3a483c9f34b026965aaa6944cb7026
+size 10839
