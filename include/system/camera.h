537f5f4f8a2caf3b0b16cc9b7d23148476a81106
James Dong <jdong@google.com>
2012-08-01 16:41:24 -0700

    Added a cmd for configuring number of video buffers for camera, if supported
    o currently, only available in Camera2
    
    Change-Id: Id9bf157496209af9be65d147e70aa4830158c88d

--- include/system/camera.h
+++ include/system/camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b1a04aa572647f9ad9cf9865a417ea8195472c59399192947d544d55e6eddbf
-size 10061
+oid sha256:1b26b9bc6a31463ba0f64591ac812ba679ec9019e18e559357aded7c77f62c81
+size 10634
