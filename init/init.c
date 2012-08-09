5738e0a0796afff876e72df1eb5e90faaa977085
rpcraig <rpcraig@tycho.ncsc.mil>
2012-08-09 10:05:49 -0400

    Implement SELinux/MAC checks for property service.
    This is a set of changes to the init property service
    implementation to apply a SELinux check over who can
    change what properties. Also included control hooks
    for the 'ctl' keys.
    
    Change-Id: I5a18809bf5536f6459a36b6bf0d622b9f5061aa0
    Signed-off-by: rpcraig <rpcraig@tycho.ncsc.mil>

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:153b11c6cb4eaeaf7c56da6214c804b3be9ef7037af77790f49e46cf577aa4e2
-size 25989
+oid sha256:11b17c043e7ac763e0f022d5f8ebfcb9c1ed2808e544194667ba3f34e98f29e4
+size 27233
