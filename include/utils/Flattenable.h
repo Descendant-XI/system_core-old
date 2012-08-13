98a1026c3292cc36b654bb941400f403e9788bb5
Mathias Agopian <mathias@google.com>
2012-08-12 19:37:16 -0700

    improve [un]marshalling of non-binder objects
    this change introduces a new class LightFlattenable<> which is
    a protocol to flatten simple objects that don't require
    binders or file descriptors; the benefit of this protocol is that
    it doesn't require the objects to have a virtual table and give us
    a consitant way of doing this.
    
    we also introduce an implementation of this protocol for
    POD structures, LightFlattenablePod<>.
    
    Parcel has been update to handle this protocol automatically.
    
    Sensor, Rect, Point and Region now use this new protocol.
    
    Change-Id: Icb3ce7fa1d785249eb666f39c2129f2fc143ea4a

--- include/utils/Flattenable.h
+++ include/utils/Flattenable.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee4c47d2e37e0737fddf733fb585280c8113024a325bbefb1c843800a052399e
-size 1964
+oid sha256:0be07c6976cf2c1bac78cdafc92bfd13d30cc9c591feb31b3fc428b4ea7de962
+size 4115
