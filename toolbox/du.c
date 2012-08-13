ba68fd22fc2e47399a19685f52da67358d55ab2d
Jon Larimer <jlarimer@google.com>
2012-08-13 13:10:41 -0400

    Make usage() static in du.c to avoid conflicts
    Change-Id: Ic6b036d050943fb5f0af8553c081ca75362167e8

--- toolbox/du.c
+++ toolbox/du.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dbe5504828f9adaf80e4c03e4e30525763dd85e037e638c05ab5c82bbd8f61b
-size 8096
+oid sha256:ed032d34ee7b1aadcbde5dff0cb22d67a145f7994a9004f09a60eab6ac1cd6ef
+size 8110
