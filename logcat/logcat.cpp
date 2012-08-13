9d8f4119031a2e3ebe377d417e805d43d2f18bcc
Edwin Vane <edwin.vane@intel.com>
2012-08-13 12:55:07 -0400

    Fixed an unused param warning in logcat
    Changed a function to actually use its parameter instead of a global
    variable. All callers of the function pass the global variable as an
    argument anyway so behaviour is unchanged.
    
    Change-Id: Ib84d45c17d2213c4d441bf2f423feca14e7aa2f4
    Author: Edwin Vane <edwin.vane@intel.com>
    Reviewed-by: Ariel J Bernal <ariel.j.bernal@intel.com>

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38ede6ab864fe1d244c76ee6aea236f80992abb29d6b4b3176fb89f822a5ca80
-size 23452
+oid sha256:9c9f68af8f910823731f68e4656f756c65a27f02ef54f6dd6dac467b39aafd3c
+size 23444
