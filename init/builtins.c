612710ba9f3b62437e980ad79548593b271adadd
Benoit Goby <benoit@android.com>
2012-08-14 15:43:46 -0700

    init: Fix mkdir command when using ISUID or ISGID bit
    On first boot, the directory is created with root:root ownership and
    then chowned. chown clears the ISUID and ISGID bits, so we need to chmod
    the directory again after chown.
    
    Change-Id: I02dfe7a19a637678256b4e7cc09e6b5431e6f11e

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74cb987a858465cb5312a5f85b4ff6665f7581f8dfc922adc7ce70c85f5ff351
-size 17150
+oid sha256:c89237a61b897170098e5c85e745b770cc6480e6e45bc0569b112f0525adc7d4
+size 17388
