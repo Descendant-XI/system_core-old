3cdab688ca20dc79a839119ae8f86d083854ede2
Stephen Hines <srhines@google.com>
2012-08-23 15:17:09 -0700

    Link directly with libm due to libpng dependency.
    Change-Id: I9a9b8c3db025b8391b6a0e96ca24ed9cda3ab9a2

--- charger/Android.mk
+++ charger/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd7dde86deee1a593b2223091d2afca7d027ce5432a7b951c55bb15b0f4b7ba8
-size 1241
+oid sha256:40d0c8a8dbefc78cc310796eca5522f0845abf3fa3f377a990d94103aaf1a1dd
+size 1246
