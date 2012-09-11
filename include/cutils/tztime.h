aca40220f5ba1602ea8caf8071f661d29a55d213
Elliott Hughes <enh@google.com>
2012-09-11 11:18:38 -0700

    The localtime_tz and mktime_tz extensions are now in bionic.
    (cherry-pick of d03072ffde3279fbb60591a0d07c820d8e792762. This change
    was originally committed to AOSP, is in internal master, but was somehow
    lost from AOSP.)
    
    Bug: 7012465
    
    Conflicts:
    
    	libcutils/Android.mk
    
    Change-Id: I08ee8eb9b8ce5d3f9d8f2e67437d653e08697bc1

--- include/cutils/tztime.h
+++ include/cutils/tztime.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f65c32acb7295acf080bf86c0087fda14f2ec83a3146dff464854b7d6fa1b5c
-size 1722
+oid sha256:cda33c983270d7949d8b74c2cdd54908673baa6bafa379e3df706d8646e7b5d2
+size 801
--- libcutils/tztime.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9c0ca30251bce676db77a5ce9ba692f470bb810fd796f7628458386371539c8b
-size 49598
