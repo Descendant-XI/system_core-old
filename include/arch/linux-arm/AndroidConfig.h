12e4ae4b4e8bd11a8959e0bc4ac36f6da5aa4fc0
Ying Wang <wangying@google.com>
2012-09-20 10:05:51 -0700

    Move AndroidConfig.hs to the build project.
    Change-Id: I482e36f9e7f665d7cfe43463c9cbc15846e8b20a

--- include/arch/linux-arm/AndroidConfig.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:77618d2930605559997ca43a74ab728433b2a4e0e2da990b3cb1373393c1d272
-size 7983
--- include/arch/windows/AndroidConfig.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a00bf7dc16dfe53dd6d14009dd32c23c395a897b9afabe65622996a0d1e34b25
-size 7595
