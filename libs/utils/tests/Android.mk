eeb7c24edcf90eea6584cfba43b09100839edea5
Kenny Root <kroot@google.com>
2012-10-02 09:09:10 -0700

    Update tests for new build target
    Change-Id: Ia1740d1b2c0b611c4559c5c846b12fb5c3e81b07

--- libs/utils/tests/Android.mk
+++ libs/utils/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a45b0c6a1229010ca23ad6a590dc75e9a6ea8c2406291649bcc5f5aa0c3d9c9
-size 1020
+oid sha256:d74a7c577147c032eda853db2315831b7af4e268f1e02463e6d5c810276665f1
+size 772
