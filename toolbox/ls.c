914d6db999dc77d02a36e883e14e6b8e6b38dd6a
Kenny Root <kroot@google.com>
2012-10-10 11:26:33 -0700

    Remove HAVE_SELINUX guards
    Change-Id: Idef0a784a1d237257ff4135bb1df62ff8a67ded3

--- toolbox/ls.c
+++ toolbox/ls.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b81523dd292aed63455eb36a95789f34556ddd943df9d654c8e9afd616d3e59
-size 12341
+oid sha256:6b6dd841a3e5c6f9389ea8d43321a508d9cd2db5aaf572f5aed1cff053b3a682
+size 12253
