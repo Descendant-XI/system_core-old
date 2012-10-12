3a6733c6a2cfb2b3745e10190d65f58414e03cfe
Kenny Root <kroot@google.com>
2012-10-11 23:42:05 -0700

    Add TEMP_FAILURE_RETRY around open and write calls
    Bug: 7330849
    Change-Id: I9aef3c3d3a248c3eea7ca060124ad6decaa6b4da

--- libs/utils/ZipFileRO.cpp
+++ libs/utils/ZipFileRO.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ab71ef228b66b65c1621d2b01d766c0b4d2fd21a96bf491f3e9e6a7f23ec11d
-size 28318
+oid sha256:4f72f1e54a299bdd96ae8aef7efa9d02008d8076ea40c568ac0cc86f3e369c50
+size 28509
