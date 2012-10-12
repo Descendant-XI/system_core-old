9eb5c387ac3a4f5599fc15df1bf222976358e5dd
Duane Sand <duanes@mips.com>
2012-10-12 14:25:19 -0700

    [MIPS] Benchmark test for MIPS memset16/memset32
    Author: Chris Dearman <chris@mips.com>
    AuthorDate: 2011-07-26 19:24:40
    Change-Id: I0a7b3360aaf45de8ee47744f7031b84f15f37611

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b4a931b66b13c17ca334debfc6a5f246243488ee597d56081ab0cd967d38d46
-size 4198
+oid sha256:9cbd33dd825439f1421c38f3353b3b512d7475f8ad5629e756ae94dd21d0e2cf
+size 4249
