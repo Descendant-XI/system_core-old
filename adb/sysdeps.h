5feba6ea8e60a6795d0413504a43e47dfb0a27ef
Kenny Root <kroot@google.com>
2012-10-13 11:59:01 -0700

    Add TEMP_FAILURE_RETRY macro for darwin
    Darwin doesn't define the TEMP_FAILURE_RETRY macro in unistd.h so we
    need to add it everywhere. Joy!
    
    (cherry picked from commit ec90f1dc11488f0ab8a25a9159965b4ae3c4ca88)
    
    Change-Id: Ida554fc65193672cc4616dec79e6282e06cc1b28

--- adb/sysdeps.h
+++ adb/sysdeps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c6bd7d1d30606e3e6b8cf6af35b74361fe84980bc08536e03a980f8fa42eaf7
-size 11870
+oid sha256:f235a40aad8ae704f648973243e7119cf5aa7003cc5f768bd9702a9a76d12945
+size 12385
