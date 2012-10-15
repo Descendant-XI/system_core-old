1c71f9170aec125528b3863534793d1de783e334
Nick Kralevich <nnk@google.com>
2012-10-15 15:51:33 -0700

    fs.c: Fix implicit declaration of function 'mkstemp'
    system/core/libcutils/fs.c: In function 'fs_write_atomic_int':
    system/core/libcutils/fs.c:109:5: warning: implicit declaration of function 'mkstemp' [-Wimplicit-function-declaration]
    
    Change-Id: I337ecb818f85e20b058f782df45987acf2e9d6c1

--- libcutils/fs.c
+++ libcutils/fs.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f731aa34130d8245410fa33dfb0a97c6407caedfc37b97a686d20fbd9df0f748
-size 3815
+oid sha256:f3291be2e26254e9454c18296235299d1aec68a494e419b0ecbe19eb34eed7d7
+size 3835
