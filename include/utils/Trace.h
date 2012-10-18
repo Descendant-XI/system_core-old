b5121fa74d699dd617ca97dec438159d1444406f
Romain Guy <romainguy@google.com>
2012-10-18 16:16:10 -0700

    Add runtime debugging capabilities to OpenGL
    The shell property debug.egl.trace can now be set to:
    
    0
      disables tracing
    1
      logs all GL calls
    error
      checks glGetError after every GL call, logs a stack trace on error
    systrace
      logs each GL call to systrace
    
    Change-Id: I34a2a2d4e19c373fd9eaa1b0cd93e67c87378996

--- include/utils/Trace.h
+++ include/utils/Trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4574723907fefebc3b93fec4f6755c457a60644cb5b9b9d5b27f5cce223acaa
-size 6483
+oid sha256:3cbaf15170f5046159c740041c52e42c239ebb148f21a209fb5a61579406a805
+size 6734
