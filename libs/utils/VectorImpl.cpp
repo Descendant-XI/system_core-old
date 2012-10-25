614d55664ea0caefbc126c9dcaac0ed3bdaaf632
Dave Burke <daveburke@google.com>
2012-10-24 17:30:31 -0700

    Revert "Revert "put back the unused virtuals in Vector<>""
    This reverts commit 225c66a48cdc3acef21ee380dc134449749d3cb3
    
    Change-Id: If31a04b81052cbc7dd7bf237c07107c33066d03d

--- libs/utils/VectorImpl.cpp
+++ libs/utils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ec180b1f7b80b4d85071731bde315c9c0232ae6457554e4029ed5b85d3a7d68
-size 17528
+oid sha256:90ec754759d7c734331af64ddb10e2e92efa2dd1dd112b6a03d9a9fd6e39d278
+size 18323
