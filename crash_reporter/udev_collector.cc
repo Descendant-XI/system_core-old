623b3c2e4fc7aad066646aaf8fe86be2cd147bf3
Yufeng Shen <miletus@chromium.org>
2012-11-02 14:21:21 -0400

    Construct signature and exec_name from udev rules
    Current all udev collected reports have the same signature and
    exec_name. This patch makes that different udev rules generate
    reports with different signatures and exec_name so that it is
    easier to search a specific kind of udev report on crash server.
    
    BUG=chromium-os:35169
    TEST=Tested locally, with a fake udev rule
    ACTION=="change", SUBSYSTEM=="i2c", DRIVER=="cyapa", ENV{ERROR}=="1", RUN+="/sbin/crash_reporter --udev=SUBSYSTEM=i2c-cyapa:ACTION=change"
    and fake crash_reporter_logs.conf entry
    crash_reporter-udev-collection-change--i2c-cyapa:grep -C 20 cyapa /var/log/messages
    Generate a report log with meta data:
    "
    sig=crash_reporter-udev-collection-change--i2c-cyapa
    exec_name=udev-i2c-cyapa
    ver=3120.0.2012_10_31_1248
    payload=/var/spool/crash/change__i2c_cyapa.20121102.125731.0.log.gz
    payload_size=6557
    done=1
    "
    
    Change-Id: I9f7905b69e786a2a13cb8a8df46eeba243157009
    Reviewed-on: https://gerrit.chromium.org/gerrit/37262
    Reviewed-by: Simon Que <sque@chromium.org>
    Reviewed-by: Michael Krebs <mkrebs@chromium.org>
    Commit-Ready: Yufeng Shen <miletus@chromium.org>
    Tested-by: Yufeng Shen <miletus@chromium.org>

--- crash_reporter/udev_collector.cc
+++ crash_reporter/udev_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec8a365bf562d0dcdce83d0dda35e82c408284ad57ef9734253e97d0628f3f74
-size 3411
+oid sha256:ad4dbda21ec759a7238afe6cdd16f8da502028207efbac0ab8a4566362c1fe30
+size 3495
