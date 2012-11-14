4e9c23897d80110d07ff16145b3f47b355fcb2e4
David 'Digit' Turner <digit@android.com>
2012-11-14 15:01:55 +0100

    adb: Improve ADB's forward redirection management.
    This adds a few new options/modes to 'adb forward':
    
      adb forward --list
      adb forward --remove <local>
      adb forward --remove-all
      adb forward --no-rebind <local> <remote>
    
    For more context, see http://code.google.com/p/android/issues/detail?id=39631
    
    Note that this only affects the host adb client and server programs,
    i.e. it's compatible with devices running older adbd versions.
    
    Change-Id: I9cda3ba12b5a8560a2061620bc7f948e5c1e70f7

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4741a50c559a7ab7c82fc262cbf6850323a2f6f7550e8856bac5b81b8523b6bd
-size 44245
+oid sha256:8450bbcc18435f61a87e5bf0b5eca57969ac553dfc966620d54e3d50e661d292
+size 48387
--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:125ca1a81c4aa86ebb2ce87be2a78c9608de402fb382c404a8f624e3f8c1554f
-size 47776
+oid sha256:541458cc7176f93c9a0de461ee4f002c3c8329b72598c496017e79535012cf0f
+size 50518
