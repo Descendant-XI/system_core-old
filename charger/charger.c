6f2e726229ac57145f56baaed49df414db65e3c5
choongryeol.lee <choongryeol.lee@lge.com>
2012-11-15 17:03:03 -0800

    charger: suspend enable in charger mode
    To reduce power consumption after charging completion, enable suspend when LCD
    is turned off.
    
    Bug: 7429504
    Change-Id: I34731dc392661c9051a20cea74f70d94a8aaeb42
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- charger/charger.c
+++ charger/charger.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae53288808fa72fbbb1a219029d8abd40020eeb7ad7645b48e7c313e9cb7a46a
-size 25990
+oid sha256:c581d4d03cf32618381ddcf26bc5b889272c396a783a07ec1eed5ea224869f1d
+size 26426
