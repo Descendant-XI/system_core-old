9d73f174d7fa05481a3c9617896653ee4499cd5d
Alex Ray <aray@google.com>
2012-11-20 01:39:09 -0800

    cutils: trace: add atrace_update_tags()
    Adds a function to read the current trace system property value, and
    sets the trace tags to it, do be used as a sysprop change callback.
    
    Change-Id: Ia6336652173aa5b07188898736c2c795a69fe79a

--- include/cutils/trace.h
+++ include/cutils/trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:878eb766408538fdde1d0dda763f8a24ed39a8f3fdfbeb28951b9c5dbc9d7b87
-size 6623
+oid sha256:fb124c7b12f32e8c1e022608d833775f4e792d2348c2543fc68161ac94298165
+size 6806
