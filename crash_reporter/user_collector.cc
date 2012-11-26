f79b4398d28f293e4763e7e00852332cbf46f5a2
Michael Krebs <mkrebs@chromium.org>
2012-11-26 14:26:17 -0800

    crash-reporter: Add a new Chrome thread to ignore
    A bunch of "supplied_MediaPipeline" crashes are showing up for product
    "Chrome_ChromeOS", so add "MediaPipeline" as a thread name to ignore as a
    Chrome crash.
    
    BUG=chrome-os-partner:16165
    TEST=Ran unittests
    
    Change-Id: Iadcba53400c63b1e7fe93d7f1e74c8dd5c37ebd9
    Reviewed-on: https://gerrit.chromium.org/gerrit/38673
    Commit-Ready: Michael Krebs <mkrebs@chromium.org>
    Tested-by: Michael Krebs <mkrebs@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8618f5f425a51c15b865ddf7ddd8009af057830a30e91016d91f40c72e58afc
-size 25852
+oid sha256:8ea317fab94ff28ab0ea193c4971da4e74cc20677218cf3bf4420a59341ac9f3
+size 25939
