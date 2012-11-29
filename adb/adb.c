de3e36758c3aac4299ec4b994019520b0bf91657
Ray Donnelly <mingw.android@gmail.com>
2012-11-29 01:18:50 +0000

    Windows adb: Make client stdout and stderr handles uninheritable
    Change-Id: Ib0519a199c9504aad1d0ecc3757f4d162984bf22

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4741a50c559a7ab7c82fc262cbf6850323a2f6f7550e8856bac5b81b8523b6bd
-size 44245
+oid sha256:025dd6e27c16ef8f2ee0ca10d6a1ccb02b3b87d04b270e687dcb1b334ee98260
+size 45378
