64d9ca1216e3a3faa3703df0a141c8ba3e7510d7
Alex Ray <aray@google.com>
2012-11-30 19:32:12 -0800

    utils: clarify scoped tracing functionality
    ScopedTrace objects were being used in place of ATRACE_NAME because of a
    misunderstanding of it's function.  Cleared up documentation for usage.
    
    Also explicitly use global namespace for sysprop callback.
    
    Change-Id: I7c248b486b614ccdb841659ca0dcfc644fda670a

--- include/utils/Trace.h
+++ include/utils/Trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3250ceec8f9584a51e8676d101e06db8d0686089e9d72f32cc9ddaa483f99ac1
-size 1736
+oid sha256:5b5477e8638ad44ac00b59f4d05853583a67fa41121329af2cfd6fb3e4b17dad
+size 1593
--- libs/utils/Trace.cpp
+++ libs/utils/Trace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae5067c0a9fc1426ee977a241efed13f77ff5f49beeb7af4cd47a9027cd28049
-size 819
+oid sha256:5fd07134148d49c4ce8c19f2f1cd9e2aa87b1413108d1cfe3ae0769249c7f738
+size 821
