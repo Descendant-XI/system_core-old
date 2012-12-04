13e3521795b87a7b887878def0cad70971485907
Andrew Boie <andrew.p.boie@intel.com>
2012-12-04 15:47:20 -0800

    ueventd: Increase uevent buffer size
    As it turns out, no, 64K is not enough. A device provisioning
    tool was generating many uevents as it manipulated the GPT,
    causing events near the end of the process (including events
    as a result of the final BLKRRPART ioctl()) to get lost.
    
    It's not clear what the best value for this should be, but
    increasing to 256K fixed the problem in this case.
    
    Change-Id: I4883b34e96c89e8a6fa581bc9cd121bb021b5694
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20fc1c8b814dc5d81f791100420c5c001f919e563e5825e6c6111f9ef7b91dcc
-size 23745
+oid sha256:8dc0367c0457aad1005a04ffe0ecf9d53818e3e21e084678647ced1146b557eb
+size 23747
