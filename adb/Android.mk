268fc4f35cb55cd8c316a3b36ef18c67c4940975
JP Abgrall <jpa@google.com>
2012-12-06 18:18:12 -0800

    adb: HACK: (linux only) allow temp mitigation for multithreaded issues
    There are serious multithreading issues between the fdevent and transport
    subsystems which both manipulate struct asocket and struct fde concurrently.
    The prevalent symptom being around multiple socket closures which stomp
     on each other, typically causing:
       "glibc detected *** adb: double free or corruption ..."
    
    This HACK allows forcing CPU affinity via an env var. E.g.:
      export ADB_CPU_AFFINITY_BUG6558362=0
    which will cause ONLY the adb server and all its threads to be pegged
    to CPU 0.
    
    The result is visible in valgrind's helgrind: no *socket_close() related
    data races. But tons of other races are still there.
    
    Bug: 6558362
    Change-Id: I0f112390a6a921c64b2a783297be9e99ce27fd56

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0486782fab0f06a009305c395e00ba9a549128d123cbcca7c4220dbed01d6fc7
-size 3917
+oid sha256:93849c520fe57600b17fa92f7910804ddfe362997a21273df24f0285be21d4e7
+size 3959
--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:657fd63a97998921429ab920c2b57ac029c84992a51521473acd02df9911ade8
-size 48642
+oid sha256:761d88d3bd336199ac340bbdf2c9b9eb72644376c6ce67e4f876ab3e4e0e33fe
+size 49512
