3cae2c25ad3c571178cdeee4b5ceb64ca4ed59ab
Ben Cheng <bccheng@google.com>
2012-12-10 14:51:32 -0800

    Fix ELF header parser bug.
    Program header size should be 32 instead of 40.
    
    Bug: 7696928
    
    Change-Id: I8298a030790e9c28e8e8a0d2ad4e9ef294708eb6

--- libcorkscrew/arch-arm/ptrace-arm.c
+++ libcorkscrew/arch-arm/ptrace-arm.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e194bffa918e510c8827cc7ed75840306b65bb8eb19ea5e69754dc1f5f4a0eb
-size 2704
+oid sha256:d5138d7eca75755315bc32375e829b10a719798e06e1a39fab7269159abc7224
+size 2867
