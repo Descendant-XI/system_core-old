a17b2364fe29e1ee68dc0dc2a5f098c3ff1a7dba
Elliott Hughes <enh@google.com>
2012-12-10 14:15:42 -0800

    Expand crasher with a "thread-" prefix to crash on another thread.
    So "thread-nostack" runs the "nostack" code on a new thread, and
    "thread-abort" runs the "abort" code on a new thread, and so on.
    
    Bug: http://code.google.com/p/android/issues/detail?id=16672
    Change-Id: I026a0e43eea147f1a7a74243846954599bcf4238

--- debuggerd/crasher.c
+++ debuggerd/crasher.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df8188f3fb183c7fb05fc976185039e23bdf5e175b34187fd709c214fcc28938
-size 2135
+oid sha256:aeb89cc6fa60ad43dce8a2de7afcfe870d4bc023daeb5911e9c65f57891a2d60
+size 2617
