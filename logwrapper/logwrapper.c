fbc51d4bb7b136ef911364750df80f4d34ecd337
Rom Lemarchand <romlem@google.com>
2013-01-03 14:37:57 -0800

    Remove -d option from logwrapper
    Removing the ability of logwrapper to die with a SIGSEGV at address
    "return code from wait".
    
    Change-Id: I563715db2b1e5e789af84190fc2ff78664d63572

--- logwrapper/logwrapper.c
+++ logwrapper/logwrapper.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75559cc9ae890cea252ee08de20ac7475bb01283f04375a8ebd30a53b5d24e49
-size 5269
+oid sha256:90f460c66963a46ac8a31cbc1af44327b1e883f4fcdde562680d93fba5e95706
+size 4791
