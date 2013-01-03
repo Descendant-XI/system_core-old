882cf2661beda345edacb25723430766a08e8e24
Alex Ray <aray@google.com>
2013-01-03 10:57:13 -0800

    cutils: Add bitmask utilities to bitops
    Bitmask utils handle resource tracking using a (multi-word) bitmask.
    
    Change-Id: I2ae45df1ac8c665c29ea9cae32a757168686b96c

--- include/cutils/bitops.h
+++ include/cutils/bitops.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f7c19ce7fd3ecc1e498aab0950329a56931d9a187419353e90472559b35986a
-size 1016
+oid sha256:adc6941846408754da744338983f7dd8b3a355cf17aa4f12fce58f371fa894f8
+size 2792
