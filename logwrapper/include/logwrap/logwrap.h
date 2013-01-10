a352969b4bae9563aa020cfcc5f7f6ed2d93639f
Rom Lemarchand <romlem@google.com>
2013-01-10 15:21:18 -0800

    logwrapper: split into liblogwrap and the executable itself
    Abstracting the functionality of logwrapper into a library and making
    use of it for the logwrapper executable.
    
    Change-Id: I2bcf722413f3a8454c6f52137dec86c4477fb8b5

---
+++ logwrapper/include/logwrap/logwrap.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a3b7c3b16158753165f8a92c8ad3b4a149098b1e27ce787e246ecd6313f7963
+size 820
---
+++ logwrapper/logwrap.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:647e721ca0eac888bb7c85190a4dc33798e94c6c3d25252c469a01e7c82b65b2
+size 4652
