30cf4ee985845f53b176963dfb94882854ac5019
Benoit Goby <benoit@android.com>
2013-01-14 21:26:30 -0800

    adb: Read secure adb keys on every auth request
    The framework can now clear the user key list, so we need to reload the
    key list on every auth request instead of loading it once when adbd
    starts.
    This also fixes issues with encrypted devices, where the user key file
    is only readable after the user has unlocked the device.
    
    Change-Id: I350c5aab986f8ca86b95f316398d03012553e581

--- adb/adb_auth.h
+++ adb/adb_auth.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce57adfe649638d45ba95a55185fd3deb94229727ba2f8b304880741f01d89c5
-size 1940
+oid sha256:3948fdeaf3a7a7c753d094b635163976a37f7cc18e164b6335ecf5af4a5d6b8b
+size 1857
