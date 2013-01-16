555c0a42cf56ae7ae20e7784f07b706faedfd89b
Rom Lemarchand <romlem@google.com>
2013-01-16 12:08:04 -0800

    logwrapper: switch to signal handlers for SIGCHLD
    Switch to a signal handler to monitor SIGCHLD.
    Also, clean up the exit path and log a warning when the
    previous SIGCHLD sigaction is not default or ignore.
    
    Change-Id: I7ef7daabee18d80e9e82fba3943cf1d03b504ae3

--- logwrapper/logwrap.c
+++ logwrapper/logwrap.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c3fb30fd6d5501f64f68ed3b99d248a29fa39f0e3e9d130596a1f278c8f1e56
-size 6430
+oid sha256:20c533d3993e6367f3a8521e0adfb8f17d2df07dccffc02a7f0b0d0c7702b0b9
+size 8018
