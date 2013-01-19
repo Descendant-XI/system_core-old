3bf130367a69e0f66a71859394a35bdfda924b11
Mike Frysinger <vapier@chromium.org>
2013-01-19 11:03:37 -0500

    use volatile markings in crash test
    This makes it work with clang (supposedly).
    
    BUG=chromium-os:37965
    TEST=`cros_run_unit_tests --board=stumpy -p crash-reporter` passes
    
    Change-Id: I598df1be7c8a38e14cde5f5328a3b6e2e1532ea7
    Reviewed-on: https://gerrit.chromium.org/gerrit/43377
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c99e48c73d75d8324975c9c44f1432045dfbd49ebcd73e7d679bb26650da6136
-size 8951
+oid sha256:2d876702c8ad1a9114478370510181b6c22315f9d8db0ccd16b97806c4f5f93d
+size 8960
