39edf75f6c51298cc4ad7315ce989e15f3286def
Dmitry Shmidt <dimitrysh@google.com>
2013-01-22 15:41:58 -0800

    toolbox: renice: Add -t TYPE parameter and switch to getopt
    Change-Id: Idaedabe1505a9c373629ef1fd31aa4b4beb068c2
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- toolbox/renice.c
+++ toolbox/renice.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aadfd0f861e830102f5901b114a039b8a2d988abddf5f49b10fe6be89c850ad8
-size 3810
+oid sha256:5553840aeeee3ef64b5e43af6e9c952b2e121eb7a1a4e213c8c866c9d62a3124
+size 4316
