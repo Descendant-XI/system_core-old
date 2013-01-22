486e0bfb410ff10214be2ee77123618bc80fa208
Nick Kralevich <nnk@google.com>
2013-01-22 12:46:09 -0800

    Revert "init: make system properties more secure."
    This reverts commit 51e06618dbd87c4374c56d9193a5e567aa3d02ac.
    
    Bug: 8045561

--- init/property_service.h
+++ init/property_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b26dbf54e4f16bbe82308274e10dd20e8b9d5a0e9444f23a33d19a3c48203a8c
-size 1103
+oid sha256:be00352ffce41f7b9781d7895c78a9fd2ebe36b6824562b3f8f4d44118ecb684
+size 1150
