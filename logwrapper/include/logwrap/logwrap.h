13144acab535efa63ae06edcc67b7277ca5f0eef
Rom Lemarchand <romlem@google.com>
2013-01-24 10:57:44 -0800

    logwrap: add quiet flag to logwrap()
    Add a quiet flag to the logwrap function that prevents messages from
    getting logged.
    
    Change-Id: Ic56c011fb608babae32b03900b955833a6bdd070

--- logwrapper/include/logwrap/logwrap.h
+++ logwrapper/include/logwrap/logwrap.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4e9601cc0f6f8a8da79649d400e6afb6a795c6241b283f10ee0af13f5013217
-size 2041
+oid sha256:8a9116d0095dfb6afbf8c397cb5c0d9c951ab4037d1c76f052963700fb41155b
+size 2111
--- logwrapper/logwrap.c
+++ logwrapper/logwrap.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe1b2471d9fbd44fc2235baf58fb705d384ab850765fcbfd43db45b1c7bec132
-size 8728
+oid sha256:f256a75f38a98b2a66fb44f783a2aa71039de5447c7c03eae91a022ab3c0a565
+size 9186
