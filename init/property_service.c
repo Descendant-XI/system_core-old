30cf25c95811a1e921946e3aa9235bbb95b16718
Colin Cross <ccross@android.com>
2013-01-23 23:09:48 -0800

    init: move the system property writer implementation
    Move the system property writer implementation into bionic to keep
    it next to the reader implementation and allow for better testing.
    
    Change-Id: I9026e604109e30546b2849b60cab2e7e5ff00ba5

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01da58eebba3e399de49ac1422880d7cddf17c1db4d1b0ca715d3b6cea1a654c
-size 17761
+oid sha256:cd938dea54e6b814aa71ccb8fa1a9cf627a9e7a6ca37b0264a6a362a0e7ce3b8
+size 16440
