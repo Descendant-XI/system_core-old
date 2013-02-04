68b9e5b18abcbbd403e0cdba96adbe46a5719c6c
Julian Pastarmov <pastarmovj@chromium.org>
2013-02-04 17:58:14 +0100

    Make policy reloads explicit.
    BUG=chromium-os:38541
    TEST=unit tests pass
    
    CQ-DEPEND=I20461078ca890c6ec2f81ad5383c06c4d75a64cd
    
    Change-Id: I042e9d1cfbefa479ff18c0b1bc170a15cec705bc
    Reviewed-on: https://gerrit.chromium.org/gerrit/42538
    Reviewed-by: Will Drewry <wad@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Tested-by: Julian Pastarmov <pastarmovj@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b458ac90205885901b94ae6850261b7f63906283f43327dd7197ebb0de967d5a
-size 9456
+oid sha256:0c1e1859e8c51568fa00dd22e584e593e1a84e085ca1380da924c1d3b4bb106e
+size 9445
