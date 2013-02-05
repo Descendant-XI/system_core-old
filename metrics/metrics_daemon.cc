83760e4a2e8094587119129f1572eb881c03208e
Luigi Semenzato <semenzato@chromium.org>
2013-02-04 19:50:45 -0800

    Add meminfo swap metrics.
    The current metrics don't show how much swap we're using at a given
    time.  We only have indirect measures (page faults rate for instance).
    
    We also need to add one type of histogram scale, since compressed swap
    can exceed total RAM, and all current histograms are on a scale from
    zero to total RAM.
    
    BUG=chromium-os:38583
    TEST=stared at the code really hard and compiled it
    BRANCH=none
    
    Change-Id: Icb9dce5efe5dbd78123aad51ffd369cb46721096
    Reviewed-on: https://gerrit.chromium.org/gerrit/42598
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13289873108c1f6801618e052fa38285bd55794cf6dd9bdffa8feebc93bce8a6
-size 34409
+oid sha256:c7451a1ca25539984a53889d912fc74f25853bdcdc1f97dcb8887a189b3572fd
+size 34755
--- metrics/metrics_daemon.h
+++ metrics/metrics_daemon.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4b61b59e4f5d62fd0140113280c8a6af9c7d4ed9ee9e4f889f809b27a73d16f
-size 15184
+oid sha256:5660ec7b6e6882ae317f75c59aef613eb6541307e1b0963d0b464fd7c96c8282
+size 15558
