7874b59bb0d899775c7739aca57137bca567468a
Dave Burke <daveburke@google.com>
2013-02-11 19:48:43 +0000

    Revert "Revert "init: Set ADDR_COMPAT_LAYOUT before spawning processes.""
    This reverts commit 87980b5c9fab12737196b8505e948186a210d96e
    
    Getting more reports of app crashes (e.g. 7990090), so given timeline for MR2, want to revert.
    
    Change-Id: Ic2d101d2d1156b3864d5c9b88aa8c94cd5aa345f

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58a5684ebbdce7c4fd2b518c8ee18cfc47c5668a549efd706eddc8b95b3c94a9
-size 27349
+oid sha256:76e0c2579e636d181dbbacc2f33d73d2ddb8b2b4f918a4dcd0adbb9a6ae6e719
+size 27996
