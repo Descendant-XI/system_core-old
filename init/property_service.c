a030e13f50ad36911f7718878638110e3e28c045
Greg Hackmann <ghackmann@google.com>
2013-02-12 14:39:31 -0800

    init: move initial property area allocation into bionic
    bionic's __system_property_add() now expands the property area as needed
    by mapping in more pages.  Rather than duplicate the mapping code, move
    it inside bionic and have bionic's __system_property_area_init() set up
    the first page.
    
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    
    (cherry picked from commit f14eef0c3c456bfe39f7e9d57c8f7ae4ec775972)
    
    Change-Id: Ieb94caab1527c71f2155efe3795490b0ea215a29

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89ceaf711e6efff314de313099ae440d7f55127e0e8b685bec83ab0a145170e6
-size 16222
+oid sha256:4c61c4aaaeeddfc73e8dd27422c832495907fd2c9869d55c65a616bc0e5540a5
+size 15730
