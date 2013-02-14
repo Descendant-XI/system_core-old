770c207014cce76e632d3aa19bde6931c54bdd07
Elliott Hughes <enh@google.com>
2013-02-14 14:41:57 -0800

    Add a "smash-stack" option to crasher.
    bionic's __stack_chk_fail was preventing debuggerd from dumping
    stacks, which was not helpful.
    
    Bug: 2487269
    Change-Id: Idba2a274037b960dfb2ac1c21686323268c4b372

--- debuggerd/Android.mk
+++ debuggerd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e41a64c58024725ecf57a4a5759681a397a12b3f2d3353789bee100d8e6ec673
-size 1431
+oid sha256:0c678e9ee8ee6a8ac1b7152b31f8ddf643e1f3c4d2b399b0ddb634fe987824d1
+size 1469
