6028cb0177a3cb7938d8c0377380550c746a2b06
Chris Masone <cmasone@chromium.org>
2013-02-14 12:15:35 -0800

    [metrics] Update metrics to build against libchrome-180609
    base/eintr_wrapper.h moved to base/posix/eintr_wrapper.h
    
    CQ-DEPEND=Ib19c1382ab28ae7632728aa672478da2feb3950e
    
    BUG=chromium-os:38941
    TEST=emerge the metrics package with tests enabled.
    STATUS=Fixed
    
    Change-Id: I5d72d8934930ce394786ee151fd8f390b5caf2e1
    Reviewed-on: https://gerrit.chromium.org/gerrit/43297
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Chris Masone <cmasone@chromium.org>

--- metrics/counter_test.cc
+++ metrics/counter_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa9657f9c4745d864d393ffb404e346c960952e43bb2e0a32ce22872a344589
-size 12981
+oid sha256:8bcf69c6db35133f59fc82c07eadfe3f672fadf00f778c089f65f1c6206c5d7c
+size 12987
--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c1e1859e8c51568fa00dd22e584e593e1a84e085ca1380da924c1d3b4bb106e
-size 9445
+oid sha256:827fa1a34f05df5f6ab7a7620c241f38978c2e6bed9d081c841e8b1143b89d97
+size 9451
