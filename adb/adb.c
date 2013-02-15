5c781e8b04f7a26bfbf9e6f3e2d9ee9cf3d2106a
Nick Kralevich <nnk@google.com>
2013-02-15 14:39:15 -0800

    adb: drop capability bounding set on user builds
    run-as: don't require CAP_DAC_OVERRIDE.
    
    Prevent an adb spawned application from acquiring capabilities
    other than
    
    * CAP_NET_RAW
    * CAP_SETUID
    * CAP_SETGID
    
    The only privileged programs accessible on user builds are
    * /system/bin/ping
    * /system/bin/run-as
    
    and the capabilities above are sufficient to cover those
    two programs.
    
    If the kernel doesn't support file capabilities, we ignore
    a prctl(PR_CAPBSET_DROP) failure. In a future CL, this could
    become a fatal error.
    
    Change-Id: I45a56712bfda35b5ad9378dde9e04ab062fe691a

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0461336e7edb184172eaa8fb757b7e87fed0f1a6432fb38bd7a427fee9d70b00
-size 50929
+oid sha256:3468e44f34ffeabfd08fc0dc247b068af6c43891405a65e7429936f1a8ee3a8f
+size 51848
--- run-as/package.c
+++ run-as/package.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba8b1ba3cd2e19334e789d496f630d4e20b604bcda0924d1ec373b750a460637
-size 12796
+oid sha256:7083f3d82b0a642147678af2545fbea0cfcf1103bb9a10fcf47b7d3cada15e35
+size 13111
