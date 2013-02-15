1ccc035f325cc1e77fcd7e87f238c8f682f26155
Mathias Agopian <mathias@google.com>
2013-02-14 19:03:56 -0800

    Refactoring: Rename SurfaceTextureClient to Surface
    Change-Id: Ie745d9adf5465834bb8bf751a2f88b19435fdf46

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f75262dd7946eb5a1a31429a5d91a1bf65c95c5ab72d38162ea19415db579cad
-size 32362
+oid sha256:4f295bd172ce0bd924eecb4bc1f0ead224fe9a165c7b2796d617a2aa6028b096
+size 32286
