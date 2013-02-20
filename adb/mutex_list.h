1290b274a7cc21915dc2275451794151a2e190aa
Benoit Goby <benoit@android.com>
2013-02-20 15:04:53 -0800

    adb: Cleanup dead code
    dns_service is unused and recover_service has been replaced by adb
    sideload
    
    Change-Id: Ie90000d7f672e8299ee1622a9690c7371b214dc1

--- adb/mutex_list.h
+++ adb/mutex_list.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e591baf7fe8b5520351ec1059173fb80c4356854eb74e16a0466131449e5f1f0
-size 827
+oid sha256:f7dc3e3c40cae5bb107e9e9786feeb042a8be23a970f2aab617a3d54eb527b07
+size 807
