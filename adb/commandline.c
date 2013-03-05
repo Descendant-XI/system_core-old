f2d05a81c17f491328d85301cd8998a07a531272
Magnus Eriksson <eriksson.mag@gmail.com>
2013-03-05 07:37:32 +0100

    Fix issues where the filename is referred to as 'sideload'
    The adb sideload utility referes to the filename as 'sideload' in some
    places. This patch changes the printouts to display the filename instead.
    
    Change-Id: I38ada01a08bed53a8d9697c03f55ce8cee2abe12
    Signed-off-by: Magnus Eriksson <eriksson.mag@gmail.com>

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dad711ba85a6a8be0f874c9b2d65e9ff8a3c219e5d4be09834837c5e0c0f5834
-size 52221
+oid sha256:25c510469c7eaf0d4dd3c312dfc41bf94c3bd501ff08dbc26fb08c135d99bdeb
+size 52231
