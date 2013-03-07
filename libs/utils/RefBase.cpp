f3a2bf2dcbc19051f772487c34d27e215932ad2f
Mathias Agopian <mathias@google.com>
2013-03-06 17:51:15 -0800

    Fix RefBase debugging. O_CREAT must specify the mode.
    Change-Id: I51c6df3cfd59b20ca73c3edee86bc2f74dbde1b1

--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29bb1c288ba1a3c23986514b5ba7d3a86dd7c5157980030d0cd8538d0dbb089f
-size 18393
+oid sha256:bd771c361013049aabffc341f7af0d35d5b3fd5a2d90570e59d853a3f31b7f83
+size 18398
