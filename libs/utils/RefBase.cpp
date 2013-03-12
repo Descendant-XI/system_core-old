43f158781b9408750ae2fcd1a8afc4c09e50bf94
Mathias Agopian <mathias@google.com>
2013-03-11 21:27:16 -0700

    temporary: enable ASSERTs in RefBase
    this is in an attempt to get more data on
    bug 8328715.
    
    Change-Id: I9333a67c2d7f67f4d9b2fc5eb1ad8a7b2d1c6dcb

--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd771c361013049aabffc341f7af0d35d5b3fd5a2d90570e59d853a3f31b7f83
-size 18398
+oid sha256:f2516d10807b48a288032bdc4d41599e212bcd6bafe668d1bdb0b414ab266a76
+size 18419
