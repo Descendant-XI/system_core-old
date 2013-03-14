924781858095571a2b4e898c8eb759d21dcf11ce
Dianne Hackborn <hackbod@google.com>
2013-03-14 15:26:30 -0700

    fix a couple race-conditions in RefBase::promote()
    Bug: 8390295
    Change-Id: I7a48e3bf5b213cc1da2b8e844c6bb37ee24cb047

--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2516d10807b48a288032bdc4d41599e212bcd6bafe668d1bdb0b414ab266a76
-size 18419
+oid sha256:15ea59a64b11ba3de347809c36420f8d27ae6fe95048dac7d8a9ba176ab8669f
+size 20171
