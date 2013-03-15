18b15896100efd8a4e314ff0792aec6eafd605d7
sunxiaoqi <sun.xiaoqi@byd.com>
2013-03-15 16:17:37 +0800

    Add BYD's USB Vendor ID
    Change-Id: I0694ed72d70638f0a4a252b1c802ae8f4e33385e

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de8637b41974549829489f3f2099a2d2cfc2f7ad3739a7cdb89be8b7e10210b5
-size 8349
+oid sha256:42b864b50aa0dbc96ecccd5d789e6845965df109be8533855eb84c6dd433cd5c
+size 8430
