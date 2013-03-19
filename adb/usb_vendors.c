650c3db23d46f12cc317fa5716163e4e0dbbe10f
Gary Hua <huachengping@xiaomi.com>
2013-03-19 16:47:46 +0800

    Add USB Vendor ID for Xiaomi
    Change-Id: I9f0622e05334a5074604bd88093172654b4c3e7d
    Signed-off-by: Gary Hua <huachengping@xiaomi.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ea5e38100c27b09c63744252080a7c96bfd0889468dd99e2c675fb9eeeff21
-size 8262
+oid sha256:de8637b41974549829489f3f2099a2d2cfc2f7ad3739a7cdb89be8b7e10210b5
+size 8349
