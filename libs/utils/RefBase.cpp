36107db3ca8f9ce36aba671e59e0fe48e2f9b06e
Mathias Agopian <mathias@google.com>
2013-03-18 20:31:18 -0700

    A few tweaks to RefBase debugging
    - stacks are now saved in /data/debug which must be
      created and writable by the user.
    
    - removed "always fatal" DEBUG_REFS option, it wasn't
      really needed.
    
    - DEBUG_REFS_ENABLED_BY_DEFAULT is not the default anymore
      (usually people want to target which refs they're tracking)
    
    Change-Id: I37fae72e9dacde6ce1fa8f7dbe2bc01b1a1b95e5

--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15ea59a64b11ba3de347809c36420f8d27ae6fe95048dac7d8a9ba176ab8669f
-size 20171
+oid sha256:ad819cf59c8378f45891380fee9f19e1bd3ef0ac2d1ccb24551754f1df1f2fe5
+size 20074
