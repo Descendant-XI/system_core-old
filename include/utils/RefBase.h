8208fa06bba9320135a492b823e51d762646663a
Mathias Agopian <mathias@google.com>
2013-03-18 22:27:41 -0700

    Fix a crasher with RefBase debugging and vectors of wp<>
    background:
    we have some code to fix-up the IDs of references when
    using RefBase's DEBUG_REFS when those refs are managed by
    arrays wp<> or sp<> (this is because wp<> / sp<> don't have
    a trivial ctor when DEBUG_REFS is enabled, and Vector
    treats them as trivial for obvious performance reasons)
    
    this is complicated by the fact that we don't want to have
    to recompile everything when enabling DEBUG_REFs (i.e.: the
    Vector code cannot know wheter it's enabled or not for its
    template stuff).
    
    problem:
    there was a bug in the fix-up code for wp<> which was trying
    to access the weakref_impl from the RefBase* however, this was
    moronic since RefBase could have been destroyed if there wasn't
    any more strong refs -- and this happned. Instead we need to get
    the weakref_impl directly from the wp<>
    
    Change-Id: Ie16e334204205fdbff142acb9faff8479a78450b

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30d2dfb881cab6d7699cc62564ccf423bee8a009101cba6a17c9bde7f6ac3121
-size 15552
+oid sha256:a3a5b9558296d520bca857a2872355d6e12893f201c47347df93d9a9a69233e3
+size 15885
--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad819cf59c8378f45891380fee9f19e1bd3ef0ac2d1ccb24551754f1df1f2fe5
-size 20074
+oid sha256:621bbc07c336fe40bac319ceb4383219c123d0172ecd6ea78477f1616d8a4070
+size 20101
