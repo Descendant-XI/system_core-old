6f8ea33e167cf82c8f1332d01c49e65d9142d48b
Bjorn Bringert <bringert@android.com>
2013-03-22 14:26:33 +0000

    Build a static device libutils
    This is needed to make tools such as aapt more compatible when
    built for the device.
    
    Change-Id: I1261d47b6e24595f1be547b9202892863f66a1de

--- libs/utils/Android.mk
+++ libs/utils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f93cf02d0b51c7ea0de61ce00d0dad03196b3cd10e7006788b67ff230edac1c5
-size 3296
+oid sha256:fab942495f93a77fc6609238f723d6d44d21de96de217b8ae45a4a0e19b76b67
+size 3670
