6da21cd2d9d9221467504153dcdf8ad5e19a0739
Mathias Agopian <mathias@google.com>
2013-03-21 17:12:40 -0700

    improved CallStack a bit
    - added a ctor that updates and dumps the stack immediately
    - added a "logtag" parameter to dump()
    
    Change-Id: Ie51c256071d282591752243bdb4f68cf9ff8829d

--- include/utils/CallStack.h
+++ include/utils/CallStack.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d67d66a201ecd9b5fa8a799097876645fb6ea5a81f6ad0bb0fb900977294741
-size 2002
+oid sha256:450954af13b3252a25afe75fe22e3a530e5e4df7c8281dbefe2cf5d02a9ccb41
+size 2146
--- libs/utils/RefBase.cpp
+++ libs/utils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:587cd8ee9de838dff9699518990eb7237834bb6ccb2dcdf0680b06b1d0c97146
-size 20104
+oid sha256:2a17899646f7807625e9039c4220285eef4a0fa58be211aa74fbfe4404e634f5
+size 20028
