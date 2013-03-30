8c83780f736ed7ce13617a4ab26e4f644d5d65d9
Benoit Goby <benoit@android.com>
2013-03-29 18:22:36 -0700

    adb: Handle adb connect in a thread
    adb connect calls connect() in the event loop. If you pass a wrong ip
    address or the server is slow to respond, this will block the event loop
    and you can't even kill the adb server with adb kill-server. Handle connect
    requests in a service thread instead.
    
    Change-Id: I2ee732869a3dc22a6d3b87cf8ac80acaa7790037

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de87b2a5643adc07d5dc9ee2946a71595bde77106b3bc48839b50d292a27c7f3
-size 51252
+oid sha256:050cde4fd31ccd93d3d30830522c91b2a566341fcb465b2b1f5defcabb864363
+size 47154
