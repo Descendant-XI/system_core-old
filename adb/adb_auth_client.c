f3c3dc765404fdfb02a4952ffbc42bc455bba63f
Benoit Goby <benoit@android.com>
2013-04-01 17:39:06 -0700

    adb: Prevent registering usb_disconnect twice
    adbd can receive multiple AUTH_RSAPUBLICKEY packets. This happens for
    example when booting with usb attached when we retry authenticating
    after the framework is done booting. Make sure usb_disconnect is only
    registered once, otherwise this creates a loop in the disconnects list.
    
    Bug: 8504991
    Change-Id: Ia1f9a37005dd17b7eefee1493d622e1679263eea

--- adb/adb_auth_client.c
+++ adb/adb_auth_client.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e09e7835d0998b84a846188339dfcf8f57f0754c01542e31a6bd382a20fb325
-size 6037
+oid sha256:1c50d6ec0c3b4a0699ccffe14b0a300eb3ca251cbc8f3d8f1d7bad347801a816
+size 6081
