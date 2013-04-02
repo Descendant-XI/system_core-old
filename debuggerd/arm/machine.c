75cbd7ec43ea0374ea9ea805b4aa54904482e9d4
Christopher Tate <ctate@google.com>
2013-04-02 14:00:27 -0700

    Uploaded native crash dumps do not include memory contents
    Also fixed the LOG() macro to actually write to the log again, tracking
    the change in _LOG() argument semantics.
    
    Bug 8322568
    
    Change-Id: I79330c85c26d3ffb734315b6d0f2c0bb80bd234a

--- debuggerd/arm/machine.c
+++ debuggerd/arm/machine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0ea0365027062138fe7bb470ac4b632dac9143e4820a6e2f20b8b6634d8ae84
-size 6181
+oid sha256:0f06c151f20e00ae3922fb9f08c574beadc0e7f0c1eabd188b0867870d60cb81
+size 6296
--- debuggerd/utility.h
+++ debuggerd/utility.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31119ad7746c31be099ac0c2c67d8cc4a5f1fa865d45acc1ded5abb4f54ad767
-size 1655
+oid sha256:05807cef08773aa19955c84e57f979fdce51a59bd339b63787aa9e0d2066bef5
+size 2095
