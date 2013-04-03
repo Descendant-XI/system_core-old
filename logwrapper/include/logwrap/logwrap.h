31749afb21aa10ebb031c725eae110478916a542
Ken Sumrall <ksumrall@android.com>
2013-04-03 13:45:10 -0700

    logwrapper: Add ability to log to kernel log
    Also add ability to do abbreviated logging where only the first
    4K bytes and last 4K bytes of output are added to the desginated log.
    
    Also update standalog logwrapper command to support the new options.
    
    Change-Id: Ia49cbe58479b9f9ed077498d6852e20b21287bad

--- logwrapper/include/logwrap/logwrap.h
+++ logwrapper/include/logwrap/logwrap.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:918d1a50231112ee681822ca1c521286c46f84cb1cbb3a072dcfa6d981ae35d1
-size 2359
+oid sha256:1a487eb0bf25fd4cfcaa13ebb540c6ee985d41f934877a7a01819c6d43a3ecdd
+size 3341
