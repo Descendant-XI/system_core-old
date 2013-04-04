f3a9e9839d2ec50ae1ee79cc4868b6bbcb775bcd
Elliott Hughes <enh@google.com>
2013-04-04 13:52:01 -0700

    Modify debuggerd to cope with the abort message.
    Bug: 8531731
    Change-Id: I416ec1da38a8a1b0d0a582ccd7c8aaa681ed4a29

--- include/cutils/debugger.h
+++ include/cutils/debugger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc495c031b8a76b077411ba70497a7118f98ebda384fcd43ab6a82e7bb5c2bc9
-size 1612
+oid sha256:80e9ed77d2ba50065f91b5b4155a8a3efbbd46740401f4f2d05dac0b811e9c38
+size 1610
