799d6706093a8539f1bab55fb542602dee8f16af
Dianne Hackborn <hackbod@google.com>
2013-04-12 14:51:43 -0700

    Add new resources trace.
    Change-Id: I63398f282fe7bf04f1c4cefa8f602100f403fa7c

--- include/cutils/trace.h
+++ include/cutils/trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb12bf71be152747e8568e773051c77f6ef8e6bdc71c4810c89e28c05024ec4c
-size 8585
+oid sha256:5a7688a986ac268c70292d9f384feed4fb0e04200e3b894a322f272e380bdada
+size 8635
