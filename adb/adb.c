456efdf8055ff6cb91e837c45397e4c6b47112c3
trevd <trevd1234@gmail.com>
2013-04-17 14:34:23 +0100

    adb : add missing connection states
    Both CS_RECOVERY and CS_SIDELOAD where not being checked by
    connection_state_name which resulted in adb get-state returning
    unknown when a device is in those modes.
    
    Change-Id: I00716024d6a0bdb68d6e2380c8cd7b5d056bd15f
    Signed-off-by: trevd <trevd1234@gmail.com>

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd625e8ecd70214c400e9a6f447f8f0affbf82278bdb39e203072f6a2ba9b7f7
-size 49949
+oid sha256:f310d6ad8887e48dbe192f53c7c4824936b34dcb3ecbdde40247f4a5a63fbc62
+size 50047
