9adb3724e4e56323f22f32a69f644759e51ab628
Christopher Tate <ctate@google.com>
2013-04-18 14:05:52 -0700

    Fix regression: log mem maps around native fault address
    Restore the logging of the mem maps around the fault address along
    with the rest of the faulting thread's information.  (It was still
    being written to the tombstone file, but the logging got dropped on
    the floor in the refactoring around AM report integration).
    
    Bug 8654694
    
    Change-Id: Id8851fa765dfe6b6ce41ccfc39e85eaac0acc629

--- debuggerd/tombstone.c
+++ debuggerd/tombstone.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09ae83843e3e08da8891d547aaa454fddb4197ce16efb5362b4a7b1fcb1658a9
-size 26531
+oid sha256:dedcbc03e0e88d395ad8c3afdaec75803663c43083cecae05426b47907113315
+size 26660
