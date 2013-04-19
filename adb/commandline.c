e8c8a0b537cc560fac1085728c2e3d572665df34
Lajos Molnar <lajos@google.com>
2013-04-19 12:41:09 -0700

    adb: added support for adb pull -a to preserve time stamps and mode
    Added -a flag to adb pull that preserves time and mode.  Mode is
    subjected to umask for security.  We only receive modification
    time from adb server, so creation time will be set to the modification
    time as well.
    
    Signed-off-by: Lajos Molnar <lajos@google.com>
    
    (cherry picked from commit de8ff4adcaa487259f9ddcd0eab4d1117d1cca71)
    
    Change-Id: I03bb5cc14ce542299cf3b221a8be318a28ee8a8d

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed63db789e26b40181f36a9f0cafb510ebfc1f0f3ae59ea72357806eb284f940
-size 54949
+oid sha256:cbb746c6990d747ffe4164aebb4c4b40c0d87ae8727212191f1cf8889845619d
+size 55312
