dd58497df21029917df9611517eb92bc41f716a7
kongfanqing <kong.fanqing@byd.com>
2013-05-15 14:17:41 +0800

    Modify BYD's USB Vendor ID
    Change-Id: I391487a12ba7ed86aac81457f8026acd3fd7576c

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76970577854ccc7b2e1066b665dec35ad194ab212d748b556781d8d312604150
+oid sha256:eedf29a2fff847bed8d74e14fcc7547ff721785ed7b173d65e51dc34324fdf69
 size 8513
