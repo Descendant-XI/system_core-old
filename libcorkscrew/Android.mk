2efe624f99737015ba8b3d32ac1b1b2e0999119a
Brian Carlstrom <bdc@google.com>
2013-05-15 09:13:04 -0700

    Disable darwin build
    Change-Id: Ie72bbaab8604cdc14082859b735458a2f73c3074

--- libcorkscrew/Android.mk
+++ libcorkscrew/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d3f577651a3f3e47486c6918a3803ee2be50e2cd8985bab198088644deb5133
-size 2485
+oid sha256:40a04e49349c5c627d1da24dbb08850ee9c3c6633b47600650ba1f12c60e6e68
+size 2556
