e8eca1ccf08ffe7267a3465331486b233f95bde4
repo sync <gcondra@google.com>
2013-05-17 12:48:34 -0700

    Revert "Add logic to fixup file contexts after a policy update."
    This reverts commit ebcf93e3bf92bf46c54c8e0f2ec8936bb2db6aea.

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010cd3d9a28c383f51a664b85ebc6caa61a98b167164d1f5da324aedd0743513
-size 27501
+oid sha256:1c7bc86d7a8fb206a235a3357ac35d55550c9e9a92a1503ead13c453f25f18b8
+size 27379
