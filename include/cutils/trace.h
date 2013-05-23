43ddb1de800e5de523b098c42fc2a9850d29ac29
Tim Murray <timmurray@google.com>
2013-05-23 13:46:12 -0700

    Add ATRACE_TAG_RS.
    Change-Id: Iaff497a1cb04a797441b5bb47b30ad6f09db8e95

--- include/cutils/trace.h
+++ include/cutils/trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdc7e1a62cb015f3634a3e35d7285cd90314e3210de21150143b6dcb9ef6da4f
-size 9189
+oid sha256:c81fa5c5937c37460017f53dee70a644e4dc24f0e855c81efa6de72461d5cb61
+size 9229
