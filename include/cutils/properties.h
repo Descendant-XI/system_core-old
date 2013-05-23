c999844b7184a38d9c2b7f7129ef59b687e8918f
Nick Kralevich <nnk@google.com>
2013-05-23 10:01:42 -0700

    property_get: do argument checking.
    Try to verify, at compile time, that the supplied
    value buffer is large enough.
    
    Change-Id: I91fa560d3ceff87609194269356ac634bdbf2ede

--- include/cutils/properties.h
+++ include/cutils/properties.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2202bc86b99fe1bd96e9145e3ee39f15e21faf07144a652092f053274d769648
-size 2163
+oid sha256:c34d33009af94ecb32e73239b850fe4c1032f926353d834eabd7378df1c2fc1a
+size 2769
