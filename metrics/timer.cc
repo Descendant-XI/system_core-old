5dc9e84a24efee02dbd6ea28c2298441b52f4a29
repo sync <wdg@chromium.org>
2013-05-28 14:19:53 -0700

    metrics: Creates |Pause| and |Resume| methods for timers.
    BUG=chromium:244589
    TEST=unittest
    
    Change-Id: I54e6dd7f5a8eb08ff42b1ee0e60df5cc895d0819
    Reviewed-on: https://gerrit.chromium.org/gerrit/56849
    Reviewed-by: Darin Petkov <petkov@chromium.org>
    Reviewed-by: Wade Guthrie <wdg@chromium.org>
    Tested-by: Wade Guthrie <wdg@chromium.org>
    Commit-Queue: Wade Guthrie <wdg@chromium.org>

--- metrics/timer.cc
+++ metrics/timer.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95adea8b801fe9201696b80b141ac4c21a23307c0fbed90b0c53b0a086e2da9a
-size 1964
+oid sha256:a840db1c868c28fce8f0bb850f9e3d16c38f77ac44893fab5a93142de98adc85
+size 2810
--- metrics/timer.h
+++ metrics/timer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:968f009699f5d14c5f5020c782a2e9c2a2ee6674b82b6d390cc880d8960bfe96
-size 4059
+oid sha256:9bdc5ced92df779c64afe5697eeb16a01b1977fbbb4059ace20af92ed8d4b986
+size 4925
