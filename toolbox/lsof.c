6ab62b49850842ea7f13853cfd019463fc8d1759
Nick Kralevich <nnk@google.com>
2013-06-03 12:10:30 -0700

    fix strncat misuse
    The third argument of strncat() tells how much data to append from
    src, not the total size of the buffer.
    
    Change uses of strncat() to strlcat(), which has the buffer overflow
    protection behavior intended by the original author.
    
    This fixes the following compiler warning:
    
    In function 'strncat',
        inlined from 'print_type' at system/core/toolbox/lsof.c:76:12:
    bionic/libc/include/string.h:142:5: warning: call to __builtin___strncat_chk might overflow destination buffer [enabled by default]
    
    Change-Id: Id69edc641de3cb87d1867a409cd57b04b12f90a7

--- toolbox/lsof.c
+++ toolbox/lsof.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5f03acc76f0807b6c90a8431e4dab53d8b23bdceef617f701851d9cf1cf07b6
-size 7161
+oid sha256:4aaab1e7a241e8f1c304f85d18957883aa486b1a02a6fbf75038eb1a47df7122
+size 7196
