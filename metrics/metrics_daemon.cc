47a1b3b65c4a4b737f3275ac175ce0167a9dec2f
Luigi Semenzato <semenzato@chromium.org>
2013-06-04 13:24:21 -0700

    Fix wrong sysfs pathname.
    This bug was introduced while adding test code
    which was supposed to improve the reliability
    of this program.
    
    BUG=chromium:238890
    TEST=verified that about:histograms contains samples
    
    Change-Id: I66323292f9261f5715760d7c884f91aca1f7e453
    Reviewed-on: https://gerrit.chromium.org/gerrit/57501
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6805b7c73a231034a3e356456d4eb3d0565dc7a4e6f8a90dfe43875ec33fea15
-size 38381
+oid sha256:c02936e68b33f862e9a0eaac6a416f05f1f31e115f469741219eedd031900f6c
+size 38247
