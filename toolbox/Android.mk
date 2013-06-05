9758aaf7b508fe8bec97174a74f9708ed6b1eb14
Rom Lemarchand <romlem@google.com>
2013-06-05 13:25:12 -0700

    toolbox: swap utils
    Add swap utilities (mkswap, swapon, swapoff) to the toolbox
    
    Change-Id: If5ed6981670a1cdda6b528b587dbc1be7ccdf832
    Signed-off-by: Rom Lemarchand <romlem@google.com>

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbcfb0f1f66d9cd85840c8ef300069b4b7425bd9148d2f8a32bfebb6a93d91f0
-size 2119
+oid sha256:a3893a84c191a8caef103cf0b7906632766f73788f6bfce4131c17944bb5a9c0
+size 2150
---
+++ toolbox/mkswap.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be62da0b81b8c896decfd128d768cee698884575c6749725c8f35c1c85984b57
+size 2475
