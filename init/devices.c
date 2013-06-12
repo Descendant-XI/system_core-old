dbb3b26a5adb1845923cac586db489e83149ee12
Ajay Dudani <adudani@codeaurora.org>
2013-06-12 15:55:26 -0700

    Free file handle for /firmware/image after use
    Change-Id: Id59e25486b45dffc87a89a961970a6e8128a5237

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25d284553829824aaa0a506f44c6a0f653a71d4fb65403a64e9c4eb72ebd697b
-size 23912
+oid sha256:d79ccc2e36e89f5f1000ad0a69ebadf5458e60d02cec194f88fcb4fdd2402b7c
+size 23929
