db8be801ed579a987d24c0a88877b368966197ea
Colin Cross <ccross@android.com>
2013-06-17 16:20:08 -0700

    init: fix copying boot properties
    The previous patch "init: verify size of property buffers passed
    to property_get" incorrectly modified one of the callers,
    resulting in ro.serialno, ro.bootmode, ro.baseband, and
    ro.bootloader always being set to their default values.
    
    Bug: 9469860
    Change-Id: Id45bd8dd657e8d61f4cfaf7e6b2559d2bfd05181

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d4a53e35827bb569fb6dadd1842a04cb1eec6770815c36a872ef42a57439327
-size 27429
+oid sha256:e235d752cc64e8403fea58044b389bedd6e61e9aa2ce636f57949de14ebceb16
+size 27495
