2673a24fb6b8fbd34332aaf65557522887bb0ca6
Tobias Grosser <grosser@google.com>
2013-06-17 18:35:46 -0700

    Set ATRACE_ENABLE() to '0' if not availble
    Change-Id: Iec61d275cd7b2be65f828c282f97cc59adf4cc0b

--- include/cutils/trace.h
+++ include/cutils/trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c81fa5c5937c37460017f53dee70a644e4dc24f0e855c81efa6de72461d5cb61
-size 9229
+oid sha256:dc1ef327e6621e2add0c010b9b3128e324d18df5d2ec97de814126e53175ce7c
+size 9231
