74a4c0a104e9d1d213edc7be27cce9a52bb99033
Yufeng Shen <miletus@chromium.org>
2013-06-19 16:50:37 -0400

    kernel_log_collector.sh : correct kernel message pattern
    The timestamp in the kernel message can have either "+" or "-"
    sign. We want to match both of them.
    
    BUG=chromium:226186
    TEST=Make sure the script can capture message starting with
    "2013-06-19T20:38:58.663410+00:00 localhost kernel: ["
    
    Change-Id: I93647d4fc8b3d796166d2e959b24364de4bc60c5
    Reviewed-on: https://gerrit.chromium.org/gerrit/59297
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Yufeng Shen <miletus@chromium.org>
    Tested-by: Yufeng Shen <miletus@chromium.org>

--- crash_reporter/kernel_log_collector.sh
+++ crash_reporter/kernel_log_collector.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a70680559580ca198c665eaff7a3fcfb0b04820aebe1298e84cfc255e9cf6484
-size 1427
+oid sha256:2a5b93c67d8a8db842dbddc5f7af745e1e99d209efe36d2f3b8b13f62e743100
+size 1519
