9dc95e3519af47e338746cb06e17c1946d0c102f
Ken Lierman <ken.lierman@windriver.com>
2013-06-20 09:27:13 -0700

    Bound the ADB connect time with a non-blocking connect
    After a disconnect, the initial blocking connect takes
    a long time to return, while subsequent calls return
    quicks.  Switch to a non-blocking connect to make the
    re-connect time more consistent and faster overall.
    
    Change-Id: I21d02b22a8eb9a457c2f1fa95eb17894d5612ccd
    Signed-off-by: Ken Lierman <ken.lierman@windriver.com>
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>
    Reviewed-by: Jovanovic, Radivoje <radivoje.jovanovic@intel.com>
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- include/cutils/sockets.h
+++ include/cutils/sockets.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4f635f91bb0e6b0a754be6b93328710fd56070d086fca1a4b16e072ef05d5f1
-size 3439
+oid sha256:a119c6243275445b8d283827f539f88a74d08d0637e711b64a24533fbaf38aac
+size 3573
