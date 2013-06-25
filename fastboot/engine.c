72971de4ba89e100ef030d68d841e318e859090c
JP Abgrall <jpa@google.com>
2013-06-25 11:52:05 -0700

    fastboot: add preflash command to help with large downloads+flash
    Without the partition name, the bootloader would have to store all
    of the data until the next flash command, which potentially wastes
    time.
    To help the bootloader deal with the data more efficiently, we now
    issue a flashing hint ("preflash:%s", partname).
    "preflash:" is used to indicate that the following download
    should ignore any previous "preflash:%s".
    
    Bug: 6045918
    Change-Id: I4e9708f64f28a4781bde14eb3714d36b281ec5be

--- fastboot/engine.c
+++ fastboot/engine.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3bae64dfd101052bc91cdfc1f112fef3e4b44f4d21e09caa0dffe64845895dd
-size 16799
+oid sha256:6e3baf3ad08268eb08058eb5db86b886e2f159f163f69d579fa323d7e4863899
+size 17377
