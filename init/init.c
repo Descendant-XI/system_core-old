4b922b084f533b4a2636895fef46d924565d794c
Nick Kralevich <nnk@google.com>
2013-06-24 17:41:40 -0700

    init: move SELinux into enforcing mode.
    When init starts up, immediately put SELinux into enforcing mode.
    
    This is currently a no-op. We currently have everything in the
    unconfined domain, so this should not break anything.
    (if it does, I'll roll it back immediately)
    
    If the kernel doesn't have SELinux support compiled in, then
    don't try loading a policy and continue without SELinux protections.
    
    Change-Id: Id0279cf82c545ea0f7090137b7566a5bc3ddd641

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e235d752cc64e8403fea58044b389bedd6e61e9aa2ce636f57949de14ebceb16
-size 27495
+oid sha256:1d1c567a65583bac07a458b80b67b2baa8e330cc532b7968616c89d576132a73
+size 27550
