ad8af1208ed240613c9c45c82f05325fda5c5a16
Nick Kralevich <nnk@google.com>
2013-06-26 15:37:26 -0700

    init: allow disabling selinux via a kernel command line
    Create a new "androidboot.selinux" option, to control how userspace
    handles SELinux. This kernel command line can have three options:
    
    * disabled
    * permissive
    * enforcing
    
    "disabled" completely disables userspace support for SELinux. No
    policy is ever loaded, nor is the SELinux filesystem /sys/fs/selinux
    ever mounted.
    
    "permissive" loads the SELinux policy, but puts SELinux into
    permissive mode. SELinux policy violations are logged, but not rejected.
    
    "enforcing", the default, loads the SELinux policy, and places
    SELinux into enforcing mode. Policy violations are rejected.
    
    This change addresses post review comments for change
    b710ed21dec88c0dde8209264df054c842561589 .
    
    Change-Id: I912583db8e6a0e9c63380de32ad8ffc47a8a440f

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77c78b2bf785143a7467d596956d3015d6b9bf8b40806e8f8f1ef9fb740e5d7b
-size 28199
+oid sha256:d8f6338eb424fc30f0ae800d621e8b97ce8ea66573bc94bdf26a3fa715c24848
+size 29335
