7731b601d19443dc486f19d84a61277a116a8c54
Rom Lemarchand <romlem@google.com>
2013-06-28 09:54:59 -0700

    fastboot: add support for sparse images in flashall and update
    Change-Id: I66a73b16a988a65fc91fb22a26d11986025089de
    Signed-off-by: Rom Lemarchand <romlem@google.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:669a501714d86bce61860424cfebfff3e5ff60331719112c0e60d2e250abd924
-size 29400
+oid sha256:bdc85efa6c1cc7545b5920c63f284d75c0519560b6649d846b3df8b012e515cf
+size 30686
