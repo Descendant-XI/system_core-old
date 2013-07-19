ee37c26ab38b7fa79a5b321d0edc30a9843fa858
Darren Krahn <dkrahn@chromium.org>
2013-07-19 14:09:50 -0700

    Define cros event names for chaps database failures.
    BUG=chrome-os-partner:17610
    TEST=emerge
    
    Change-Id: Ic12379645ad1c72388aa4a4758fa4bea5b2f554d
    Reviewed-on: https://gerrit.chromium.org/gerrit/62698
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>
    Commit-Queue: Darren Krahn <dkrahn@chromium.org>
    Tested-by: Darren Krahn <dkrahn@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4575d74d9d8ed2de1fe64d7af5ded76076c2148ffe9064515e38af6b9340e19
-size 10601
+oid sha256:f19c27edfd77fecdd2f957f28f404a77636677b815ff941397babfca071606b2
+size 10714
