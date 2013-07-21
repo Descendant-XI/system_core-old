1c0e219ee38af8bb94d050a2228cb9fd8428cdd0
Ajay Dudani <adudani@codeaurora.org>
2013-07-21 09:03:41 -0700

    init: Increase maximum number of Android properties to 372
    Bug: 9941046 -- encryption failed
    Change-Id: Ib9c2921a2b574542cce98d2eadbd9204a1ad0bf0
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a7d56ff8c1b5b0467ef358a7d33ed346f60ea1f29988572e6434e449a0b86ba
+oid sha256:7b1b1a9c138828b9c7522ed01381b4b6aabdaf7323656d578fd189f970658a6f
 size 17655
