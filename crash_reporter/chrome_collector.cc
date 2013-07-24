721db855c4d08b781da6a159c2d064a56fef0132
Yuly Novikov <ynovikov@chromium.org>
2013-07-24 10:49:49 -0400

    crash_collector: Collect kernel task states for chrome crashes
    When handling chrome crash:
    call debugd to write kernel task states into syslog,
    parse only the relevant task states out of syslog,
    pack them into the log file to be uploaded.
    
    BUG=chromium:244916
    TEST=Kill chrome, see the task states in crash log, on Link, Snow and Alex.
    
    Change-Id: Ib8aad79622886581ae5ce50c03097f01db9153a7
    Reviewed-on: https://gerrit.chromium.org/gerrit/63205
    Reviewed-by: Albert Chaulk <achaulk@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Tested-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- crash_reporter/chrome_collector.cc
+++ crash_reporter/chrome_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d6bf1ab46986ae2c88c210ff6b96eb459250025dec5a952629ace4748f65979
-size 9093
+oid sha256:c5bd7ad82280b0cf6b9db6b9f6451363d872a29d7fd4d58f0916b7a9d091e160
+size 20488
