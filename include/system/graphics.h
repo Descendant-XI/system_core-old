d2febf47b61abc993df047fc51f3e1748aaddc1f
Mathias Agopian <mathias@google.com>
2013-07-25 17:07:11 -0700

    add sRGB pixel formats to the HALs
    Change-Id: I1d5f9dd14a6485dd3a29fb5960edfa79aa86da42

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d1db694b9d1d17647c07cce036c8c9af01d982a9ba355367f9f518676d52c3
-size 9298
+oid sha256:326cfdc8ae80eb154f604439620adb2bebd09ae2bdfb96cf78b960c594109e67
+size 10351
