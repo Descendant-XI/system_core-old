baa330f808fdaa1f3f5e05edb353ce1042a8a642
Jeff Sharkey <jsharkey@android.com>
2013-08-12 20:23:49 -0700

    Add legacy layout support to FUSE, enforce write.
    The legacy internal layout places users at the top-level of the
    filesystem, so handle with new PERM_LEGACY_PRE_ROOT when requested.
    
    Mirror single OBB directory between all users without requiring fancy
    bind mounts by letting a nodes graft in another part of the
    underlying tree.
    
    Move to everything having "sdcard_r" GID by default, and verify that
    calling apps hold "sdcard_rw" when performing mutations. Determines
    app group membership from new packages.list column.
    
    Flag to optionally enable sdcard_pics/sdcard_av permissions
    splitting. Flag to supply a default GID for all files. Ignore
    attempts to access security sensitive files. Fix run-as to check for
    new "package_info" GID.
    
    Change-Id: Id5f3680779109141c65fb8fa1daf56597f49ea0d

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25c66f806b6e2365700e869fef891f012b0921fe6255447a0ac8e0f4630f1f44
-size 52318
+oid sha256:856e86f0558baacc9fa403e693edffd24535e5bbb0b7e59cd10d24fcc19266af
+size 59535
