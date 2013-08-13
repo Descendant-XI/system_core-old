1c02b49dc1709806c8512e25b0554f0da7f07aa4
Wing Tseng <Wing_Tseng@pegatroncorp.com>
2013-08-13 11:50:47 +0800

    Add USB Vendor ID for WACOM
    Change-Id: I3deee6d5bdfb9c5bbc0fddcfa39814d27ee281c2
    Signed-off-by: Wing Tseng <Wing_Tseng@pegatroncorp.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c04a33f3da142858744a325f97d9124dda98cf9d0970671c632a2d89896a74a
-size 9102
+oid sha256:b0619e25542ee9daeee6a75a3962735b6c15d27f464804758dc72bb61a14e0bc
+size 9186
