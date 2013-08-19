978cc31e026ef1d8ec415b25a6059c49662b60dd
Michael Wright <michaelwr@google.com>
2013-08-19 15:56:34 -0700

    Add basic readlink command
    Change-Id: I693c1098e6c6d107a9e97285bf826ab153a1c8f0

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3893a84c191a8caef103cf0b7906632766f73788f6bfce4131c17944bb5a9c0
-size 2150
+oid sha256:c6816785dc3fcb9aad4a6d0575685912ab47c179f1559e39c50dee339760749e
+size 2162
---
+++ toolbox/readlink.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbbb5eff8cb129142fee9405cd5dc336d183fc7b18efe4762e3ee7816eba66da
+size 2125
