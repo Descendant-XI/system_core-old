356f8cba92276424a947f5d1945966729a16eae2
Szymon Starzycki <sstar@google.com>
2013-08-19 16:42:25 -0700

    Fastbootd: flashing certification
    Change-Id: I564086908942463eebee69449d484ba03effc50e

--- fastbootd/fastbootd.c
+++ fastbootd/fastbootd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:818289959f2a0424ec99c75ef4361c0fc283d544945c4cf95381df84e031d3d4
-size 1063
+oid sha256:fe376de4a77959c7f778eb49d8b9a46990e237ea557364f8ad83cb81c4b43094
+size 1107
