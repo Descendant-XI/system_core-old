aaec02b960b5e3ae0c9126091d027c52f049b7e2
DanielMo <DanielMo@fih-foxconn.com>
2013-08-28 18:39:01 +0800

    adb: Add USB vendor ID for Allwinner
    Change-Id: Ic77d5f71c9e329e33cd31fa0854e692ad794a609
    Signed-off-by: DanielMo <DanielMo@fih-foxconn.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac37ccf3416c093406d8f0f61b9b7d016ac42536fc883dac0037f33011bf63d5
-size 9588
+oid sha256:6f5d28cfaae51f26d334c417f31d7fa548cae884a2c0ee3a55a47fe587a8baa6
+size 9681
