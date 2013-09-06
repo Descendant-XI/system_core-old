94db6b7923b90ef3bf500097094e082c03bbf2d3
Szymon Starzycki <sstar@google.com>
2013-09-06 15:09:30 -0700

    Fastbootd: fixed missing file in Android.mk
    Change-Id: I10927f488e35c688483f2d1d8c9f5b7ea1d7912d

--- fastbootd/Android.mk
+++ fastbootd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6071517a3d9c4b928ca37d2ed827ae995fa3735159a10251a00875aa35f1ba98
-size 2088
+oid sha256:b912309c385e1f25f6eff7ae0e2eaddd472b9cb5d80a2dcf522b60d7a999ba12
+size 2124
