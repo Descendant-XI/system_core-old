eaae82565c10c03dfa6b808bc289a455ff4f42c7
Andrew Boie <andrew.p.boie@intel.com>
2013-09-09 13:08:17 -0700

    init: support longer command lines
    The Linux kernel supports command lines up to 2048 bytes on x86,
    see COMMAND_LINE_SIZE in asm/setup.h.
    
    If any androidboot.xxxx arguments were past the 1024 byte limit
    here, they were lost.
    
    Change-Id: I6247c511a7de04109490fffa0125801d274a5501
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- init/util.c
+++ init/util.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89375a480cd4852d501957ecb3998f1ccd49ca405683559defc93c7e52d69ae2
-size 12439
+oid sha256:ee2450325f8e11ea5b78cd7d3510af37891bdc7d1444f5274d2ceeadd6a6851c
+size 12454
