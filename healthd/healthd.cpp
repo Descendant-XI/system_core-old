f949f81efcc2c22c37e3700ad889af784b1b8065
Todd Poynor <toddpoynor@google.com>
2013-09-09 20:02:55 -0700

    healthd: add register event interface
    Facilitates registering events from external source files.
    
    Add epoll events param to callbacks.
    
    Change-Id: Icf8fb0c62af11453cbcc92877ff2c3bc03b9bb1b

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:398d27fe0f2e0da4d5bc3472cfc1ea3112152bfcd7a77847b26695a24ce36803
-size 8458
+oid sha256:e0affda94492d8d791fe568165566776dbd72706289d3d799b1d3247ba42727d
+size 8473
--- healthd/healthd.h
+++ healthd/healthd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76aaf906994e062fff614c71e8c48e84a8d8e265872820116c1092054bfce39f
-size 4318
+oid sha256:84c630870cb1ba4be0908a8890d46246189f4a8de1f30f743b33e5247edda770
+size 4381
