51fca54a5793345ba73e8f37a472c5afb2b02686
Jeff Sharkey <jsharkey@android.com>
2013-09-12 09:44:48 -0700

    Remove mkdir() side effect, add .nomedia, utils.
    Before this change, FUSE lookup() would have the side effect of
    creating the directory on behalf of apps.  This resulted in most
    directories being created just by Settings trying to measure disk
    space.  Instead, we're switching to have vold do directory creation
    when an app doesn't have enough permissions.
    
    Create fs_mkdirs() utility to create all parent directories in a
    path as needed.  Allow traversal (+x) into /storage directories.
    
    Fix FUSE derived permissions to be case insensitive.  Mark well-known
    directories as .nomedia when created.
    
    Bug: 10577808, 10330221
    Change-Id: I53114f2e63ffbe6de4ba6a72d94a232523231cad

--- include/cutils/fs.h
+++ include/cutils/fs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d56a75eb6cb83f2f2542c6b51d9a319ef1906946af35f625eb97bce078331ef
-size 1798
+oid sha256:b4de1eafc04f284b788fe0487f938b085acc161fcf475ffe5178241946376e22
+size 2151
--- libcutils/fs.c
+++ libcutils/fs.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3291be2e26254e9454c18296235299d1aec68a494e419b0ecbe19eb34eed7d7
-size 3835
+oid sha256:7de1880778efde965f7e095f3810f3e641fb234fa1412bc3a0954be74863c747
+size 6479
