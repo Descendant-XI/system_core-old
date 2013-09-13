ee67a4201dbd042024fcbe43d4d145b924e968a0
Szymon Starzycki <sstar@google.com>
2013-09-13 15:37:08 -0700

    Fastbootd: network auto discovery
    Change-Id: I91082f5832412e775244b3974c0ab00952905ad5

--- fastbootd/Android.mk
+++ fastbootd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b7c369b1faf19e520089d01e54b260055f195c67dfbd2648ede23cb6d19a87
-size 2171
+oid sha256:7372fe1a5706255003c0e654de83f7d90c7760c180b8a771784b72e8cf7f5f25
+size 2271
--- fastbootd/utils.c
+++ fastbootd/utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe2d2cf8f6866c0508796a714369dd6ce583eb46d076bcccbeb37a208d669c29
-size 5106
+oid sha256:d8608179f3d01798f456d5549fa641540c4bbfe204af32d403ec278077507c67
+size 6341
