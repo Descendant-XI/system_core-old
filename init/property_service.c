764efdd987a262103083668e4415173a7b0ca113
Nick Kralevich <nnk@google.com>
2013-09-16 11:30:48 -0700

    property_service: address comments from previous review.
    Address post-submit comments from
    694636142113d91c2b9585ad28e143d4ff001584
    
    Bug: 10733330
    Change-Id: I7c652f7ef0379536df48b9478a2362f1bfd252fe

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47495ed36f8d7676f00c6ae5af83db564845c9c2bf6710cb3ae4dc097d2d6f67
-size 17373
+oid sha256:e9a75c7f27c00ad0358271530a586dcb2a2a402df94146567a0f4d3360390c3c
+size 17324
