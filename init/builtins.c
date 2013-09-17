6462a22a50b38e46164188d25cb46e1fdbf5cdfa
Nick Kralevich <nnk@google.com>
2013-09-17 14:43:12 -0700

    write_file: introduce O_NOFOLLOW, use sane mask
    Don't follow symlinks when writing to a file.
    Don't create world-writable files.
    
    Bug: 10802869
    Change-Id: Ifb55600d574307a535df878acb3347e02028cd30

--- init/builtins.c
+++ init/builtins.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af911d68b8278b54c35681de528442b6c8c5b721496c01b41b45e9a9f3aae2ae
-size 18471
+oid sha256:d91fd5fe1c75f4bff7f9f62f20b8614bfe255bb5607b5e474ab61b4127799376
+size 18482
