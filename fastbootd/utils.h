9b614601ebd9e2bd1d4e9252504366c723ab9abf
Szymon Starzycki <sstar@google.com>
2013-09-18 16:12:43 -0700

    Fastbootd: auto ssh server start
    Change-Id: I3e9c53c3d834726c3747e9ee0665ed027f55b08c

--- fastbootd/utils.h
+++ fastbootd/utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:169717ee689d6f2c144a37b031845f5dca10a1fe62bc43e6ca1fba9c88b3742f
-size 2263
+oid sha256:9f8f05b1f199e7d3de167a55be635a8c11fc949f55f2feae83d1515b2db40b3f
+size 2287
