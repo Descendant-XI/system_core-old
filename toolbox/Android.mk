508c94b804998c3cc870c665215966c6dae3afd0
Glenn Kasten <gkasten@google.com>
2013-09-23 15:10:12 -0700

    toolbox: Add nohup command
    Change-Id: I2f7d9934b54d98886d7a6205ea122d9ce91066ec
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad3c41cc3b429519e84bbdc3787dff33ead05c3723a5cd630ead1a2dd36b732b
-size 2547
+oid sha256:2f7880bbc2e05dfe85c22c86caaea043af2f1c1440622d0865d479667010932d
+size 2556
---
+++ toolbox/nohup.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:507ad240359042c2541986d547be8f831b9d712c990064688e148aba5d7a1789
+size 625
