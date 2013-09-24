369e68d89cdbdbdd54c8a623e0876ba8d143f486
Ben Cheng <bccheng@google.com>
2013-09-23 22:39:15 -0700

    Disable timestamp logging.
    The kernel problem has been fixed long time ago and the ad-hoc logging
    mechanism is not thread safe and can flood the log with spurious
    messages.
    
    BUG: 10899829
    Change-Id: I63278db51295e744eed3e47dc8d4cfe621c0d8f7

--- libutils/SystemClock.cpp
+++ libutils/SystemClock.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0cdaef35d2ab92071f936131ec3cfb548d1f6682d6d5716b8d9a7b21adfc943
-size 4256
+oid sha256:c7c5c176e911ed8ca0ab3c7c8398eeafb4b123c2f421a78782b2d77dbc3b3f1d
+size 4613
