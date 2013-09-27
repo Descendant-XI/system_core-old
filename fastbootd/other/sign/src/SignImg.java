704f05673a2f5448d99e004b8c9cce231acd9964
Colin Cross <ccross@google.com>
2013-09-27 20:34:32 +0000

    Revert "Fastbootd: flashing certification"
    This reverts commit 5ccecfd26d2a7c3a79a139f0118b93ee64be4737.
    
    Change-Id: I0ca31094c115e14982a27c258d9139a723da8428

--- fastbootd/other/sign/src/SignImg.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2f5e04bb248ab0c84c1352238a8ce0d002132a93d5fa0424792b587e3348bede
-size 7168
--- fastbootd/secure.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b712d054cdad586424be7fb50221059ef6241dafe60f4cef55469a83cedd6059
-size 2162
