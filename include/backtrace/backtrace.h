3782a66d10e1c59e440ad16ab25c738b9e31240b
Christopher Ferris <cferris@google.com>
2013-09-30 11:16:17 -0700

    Update comment from review.
    Missed this typo that enh pointed out.
    
    Bug: 8410085
    Change-Id: Ic636ae218fc153bc5e3b1a0fdc2f7161980e5eec

--- include/backtrace/backtrace.h
+++ include/backtrace/backtrace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a33d8cc0e437b13e1563b89c1338002cbf9aaabcab64cb622ed3eab27088cfa9
-size 3590
+oid sha256:e98dafb2c14b864cb11eae4c0af190c8652f29a893f2c01d8f876d8954ee04ac
+size 3587
