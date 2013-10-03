f4f07d5c683e1328fb69a2251e6c847de5635826
Szymon Starzycki <sstar@google.com>
2013-10-03 11:06:45 -0700

    Fastbootd: General fixes and changes
    read data once bug fix
    ability to run fastbootd without network and named socket configuration in init.rc
    vendortrigger name changed to fastbootd
    deleted unused function from default implementation of OEM library
    
    Change-Id: Ib957fae8172530f20d51bb51b5e07bccab07e555

--- fastbootd/transport_socket.c
+++ fastbootd/transport_socket.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab0f51bfa0d27a6730b9a4aec8a8d1c95591564c1ba196bf682399427a45a92a
-size 4804
+oid sha256:fcc2645b7393cb8948088bf0ec728b04d02faadbe48fe05def924efe23c9d98e
+size 4749
