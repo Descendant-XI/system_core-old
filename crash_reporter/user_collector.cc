8be2b72d5d3b1460501e4e178fc3343ae351cec2
Mike Frysinger <vapier@chromium.org>
2013-10-08 19:03:23 -0400

    crash_reporter: clarify chrome crash ignore message
    We ignore Chrome crashes when it is the kernel that runs us.  When Chrome
    itself runs us, we will take their crashes.  The log output here though
    has long confused developers, so rephrase it slightly.
    
    BUG=None
    TEST=`cros_run_unit_tests -p crash-reporter --board x86-alex` passes
    TEST=`cbuildbot {amd64,x86}-generic-full` passes (VMTests)
    
    Change-Id: Ia13a766134f5a20a1cc4eaab74ac46633d3177eb
    Reviewed-on: https://chromium-review.googlesource.com/172261
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f29990ee4b955fec0f47985a1a7343d54fde8d3ceff0f6082219aee40f34fa3f
-size 25293
+oid sha256:7a3668c815079dee707d44e4fd8a4e5126ca34516307a2f4dab6c4b4e5d4c11c
+size 25365
--- crash_reporter/user_collector_test.cc
+++ crash_reporter/user_collector_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:379f68e5ced34a50126f31ca541ecb993bdffde6fed63e8bf350f1f6379d0dbd
-size 19600
+oid sha256:2b3a17b1ae1ca507d585447fe4923eca788f43a6cb6012b4f0ab352a7ba065cf
+size 19692
