d26ca68e6ef869be65641f89631ee4decc96fca8
Marcin Chojnacki <marcinch7@gmail.com>
2013-10-16 17:39:16 +0200

    init: remove obsolete rle logo
    Obsolete RLE 565 logo is used nowhere,
    because 565 framebuffer isn't used for years.
    
    It's not necessary to keep this thing alive anymore.
    
    Change-Id: Ie61e168790f791230530cd3eb1c68b1f7344c9a7

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca22ea29b80f0b1fdd968d0b8e0f9a037bd3081bbece2fcdacb6634660ce7961
-size 1426
+oid sha256:2910aae1458171c2789679ab176d9c585febfc75fb4272c77daa694650d2b6f0
+size 1416
