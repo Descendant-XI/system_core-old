d709e401ef32ce93bab4dd1c9e51eafab3181037
Shuo Gao <shuo.gao@intel.com>
2013-10-17 11:36:11 +0800

    fix corruption in Vector<> when malloc falied
    1. When alloc or realloc failed in the function SharedBuffer::editResize,
    it would return a NULL pointer, then mStorage would update to be 1 by
    SharedBuffer::data() if no pointer check here, which is an obviously
    wrong address, and would cause corruption when used it e.g. in capacity().
    
    So add the pointer check here for the return value of SharedBuffer::editResize,
    if it's NULL do not use it to update mStorage, to avoid the value of mStorage
    polluted.
    
    2. when alloc or realloc falied in _grow & _shrink function, mStorage keep
    the original value, so mCount should not be updated here.
    
    Otherwise, mStorage might be 0 but mCount>0, so a corruption would happend
    when it try to delete items from the Vector since mCount>0.
    
    Change-Id: I7c3814e843c459834ca5eed392e8d63d1cb7d2d8
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Jian Luo <jian.luo@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 139626

--- libutils/VectorImpl.cpp
+++ libutils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe4448a353ccfca70f0aedf58b7290cba88c802a3fa9e8b84036d1edf2253022
-size 17809
+oid sha256:f43355586ae97a2bfcca67d2edcde116b0afb0003518d3848cc71686fadb2963
+size 18078
