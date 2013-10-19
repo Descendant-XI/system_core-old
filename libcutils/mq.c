797ca1a90e98dc236a9afe9fb5294b504860c74d
Elliott Hughes <enh@google.com>
2013-10-18 17:11:00 -0700

    Use %zd for size_t.
    Change-Id: I0139243c309e85236eb00bb471736bdd99eeec84

--- libcutils/mq.c
+++ libcutils/mq.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2a1a9678f47cba0b7127c7dd8c106fc167c9c2f0da69ac009df4555fd3a1f86
-size 39625
+oid sha256:e7946b117da2bba795252235c2afaee9ac0b8872fa62fc05e893bd1ab88afeb3
+size 39626
