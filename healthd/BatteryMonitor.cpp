3b1def9b5f54ba675904858d55b2c0f4ec379bb7
Todd Poynor <toddpoynor@google.com>
2013-10-22 18:21:27 -0700

    healthd: remove/rearrange extra battery property handling
    Change-Id: I6cc1570e23c1c1468f08579f633997e95327347a

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae31da7db14c96154aa208e69940f745fab836c99293dea4767c73aaa12d1384
-size 18238
+oid sha256:ca5de917b9f31d8f0ab8c909406e857cb897aa7a526717636d293d4917364972
+size 17828
