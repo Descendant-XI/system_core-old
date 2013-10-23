bb6f67b1880d5fc5f9df8611dd8cbace3202bebe
Sasha Levitskiy <sanek@google.com>
2013-10-23 11:31:02 -0700

    Getevent: make sure the monotonic timestamp is displayed; warning cleanup.
    Change-Id: I6c500ab55470155ddf692fe5f655bdd575f195f5
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- toolbox/getevent.c
+++ toolbox/getevent.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0522ed8ab8b937c4279a8ce42f8b802bd1d44b52e5735a8f8f1eba591e534f72
-size 20957
+oid sha256:e1ddeabfcd89f88952efc5c4d9150c88199e95c180c3289aec710cd5d4930ff4
+size 21144
