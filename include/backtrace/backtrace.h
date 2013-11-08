77ed8f310bca3c69433759fae0271d8c82920f4a
Narayan Kamath <narayan@google.com>
2013-11-08 15:58:05 +0000

    Fix mingw windows host build.
    __BEGIN_DECLS isn't defined on this platform and
    it appears to be missing sys/cdefs.h
    
    Change-Id: I13cc557700198590cf717e3007dee738043ac3bf

--- include/backtrace/backtrace.h
+++ include/backtrace/backtrace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37bdcece1148bc2f27676566bdb37619105cdeb1d9c7e2b116ca9045a4ad102
-size 4369
+oid sha256:208d959d41df4391626eecbf6287f74b873dbff9c964784800d9a6538fa0d497
+size 4586
