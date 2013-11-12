9d1ce5ff71f032fc9db80cc9efdae70e4b69a17c
Christopher Ferris <cferris@google.com>
2013-11-12 10:54:16 -0800

    Move BACKTRACE_NO_TID to BACKTRACE_CURRENT_THREAD.
    Use a bit better name for this. The other name was a bit confusing.
    
    Change-Id: I1261f2ee3854a9c8b82133ad0bfbbbe48b43c9ac

--- include/backtrace/backtrace.h
+++ include/backtrace/backtrace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37bdcece1148bc2f27676566bdb37619105cdeb1d9c7e2b116ca9045a4ad102
-size 4369
+oid sha256:d195ba6610a75e4fdeb2dbfde75ab7c542018d8a70a27b70c385ec63135a8016
+size 4409
--- libbacktrace/Backtrace.cpp
+++ libbacktrace/Backtrace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0a2cc7acf82b00ebb98e08f56c0916a6aaf7455ee47959b9ac861a75ca9fa35
-size 9178
+oid sha256:2ccb1b85002d985c84f90a6c2e6d60dc9e4d2ea284c03ae81660bf9af4f1171e
+size 9194
