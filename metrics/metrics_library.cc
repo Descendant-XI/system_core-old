e5681fdac1e375bdaadc759773487ffdbbef3333
Luigi Semenzato <semenzato@chromium.org>
2013-11-13 16:28:43 -0800

    Add two more charger states to Platform.CrOSEvent.
    This adds SpringPowerSupply.Original.Low and SpringPowerSupply.ChargerIdle.
    
    BUG=chrome-os-partner:23973
    TEST=manual
    
    Change-Id: I35478741e128891667bfd64c45b379c7c9b7d602
    Reviewed-on: https://chromium-review.googlesource.com/176707
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71cdfb01b4125e6a127d7e7977799e89d486034ff883f486fcaaa0a5b212c718
-size 10844
+oid sha256:449ac8d57d3874b6fd05bb6c12a3b0487a6d05d6bdcfe18335583c2a59199582
+size 10928
