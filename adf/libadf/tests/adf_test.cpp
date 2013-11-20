ee3df81e0474a803e897e565526da01f820ae2c4
Greg Hackmann <ghackmann@google.com>
2013-11-20 10:58:41 -0800

    libadf: add tests for various failure modes
    Change-Id: I1b13e84c6663cb73af2829bedd3514119a01764c
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- adf/libadf/tests/adf_test.cpp
+++ adf/libadf/tests/adf_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2d6478a23419eb4a1e5814af7dda9af50f40eb56eb6b3e94362d6ee45e58515
-size 8784
+oid sha256:82f8d5331f5a115718b5dbcff26f05f710f664d910ed8d46e3a1142ea5902736
+size 12266
