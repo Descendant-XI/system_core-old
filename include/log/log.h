dac632d4ac56a2feaa971e6744ce619a1f1b29b9
Mark Salyzyn <salyzyn@google.com>
2013-11-22 12:39:43 -0800

    liblog: Add liblog test suite
    Change-Id: Ia457d518b4e7ff37e840336ff0c48583709700d4

--- include/log/log.h
+++ include/log/log.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38a0e17ca7da2ce6444835bff21879867fb6e16bc8ec36ad8d97eead08beec82
+oid sha256:aa908de69edef095df29fa57db03cc522ef5a5fc59ce37f32d483d8e108bf9f6
 size 15386
---
+++ liblog/tests/liblog_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51009956234c3027cd943124ed5ca6cba0a734c7bb1f92997d1c21c29af7903b
+size 5625
