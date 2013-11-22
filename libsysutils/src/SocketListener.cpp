08a7aa9fa2a150277fc3e3ab140fbacc302b2d25
Mark Salyzyn <salyzyn@google.com>
2013-11-22 07:47:35 -0800

    libsysutils: Get rid of warnings
    - UNUSED argument warnings
    - Remove LOG_NDEBUG define
    
    Change-Id: I48b0942adfdb7a46a7693e580bc6ed5a685b0d5b

--- libsysutils/src/SocketListener.cpp
+++ libsysutils/src/SocketListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac2b5b377b4c289ca1691d97e14fe25c62856e82308664ec53f0e784661fc82d
-size 7804
+oid sha256:d7585eaabc91f2dac882503d4be94b647813ec5559ba4a2a100108855a16d641
+size 7782
