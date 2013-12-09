6ec4159603dabdc6dcd69b025e8af5f65b122e59
Mark Salyzyn <salyzyn@google.com>
2013-12-09 13:47:00 -0800

    logcat: Add -T flag (-t w/o assumption of -d)
    Change-Id: I49763e2db83755e0b7b12dfa3f83a3957c54d389

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797129551691bb1b0c273bcecb1680a6662ce798a7c6d3ef89dde1c018b364b5
-size 8429
+oid sha256:f79a4c1139d9cd1aa42874a8d2558a62eba64e2922f955853d3035a357d6ce31
+size 9711
