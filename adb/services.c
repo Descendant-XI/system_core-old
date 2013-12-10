00778f86fe721d60871cfd0a2b6eb910745e7b9c
Mark Salyzyn <salyzyn@google.com>
2013-12-10 12:37:57 -0800

    adb: deprecate legacy log service interface
    (cherry picked form commit c66a7537df987715acc77eed952111cc56b7e72b)
    
    Change-Id: If96019cc13b3d7a4dcd7785c7137e0cbcc622981

--- adb/services.c
+++ adb/services.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ffa50caab8c2d0d92eca7f01a9856d058d49cc4cfd36e7aa7dbd38c3fce0cd8
-size 17817
+oid sha256:dab347acbac7e1003ade4cf02b8452c4aeaebe81b35c6aaa8ce20d0a5c92e92d
+size 17694
