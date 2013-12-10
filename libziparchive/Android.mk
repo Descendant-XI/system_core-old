a74573048084ee841c43fac9fb3d6d76a7fbafeb
Narayan Kamath <narayan@google.com>
2013-12-10 16:47:14 +0000

    Add unit tests for libziparchive.
    - Add a build rule for host tests
    - Add basic tests for Find / Iterate and Extract
      for both deflated & stored entries.
    - Fix an off by one error that the test uncovered.
    
    Change-Id: If72009b1ea9791d5a265829f05c32ffe1c2752c4

--- libziparchive/Android.mk
+++ libziparchive/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f6e1a4ca0dad42f12f9a5d8695e4f16857e2fc48a5526f957067af9dea021af
-size 1490
+oid sha256:8ae56da51ea913467a58b9d432c92b5d3c609a847f48b41fd75dcc0a8c588fd0
+size 1821
--- libziparchive/zip_archive_test.cc
+++ libziparchive/zip_archive_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a399772b151bb70b9ec6995c4e0a72b44b77e9dccf0661b578609ca6008c2b35
-size 1349
+oid sha256:8eae7562de0201c30e3fc9b1c213d4a7a51e35565d6938d0a3d4e40e2e1adff6
+size 4709
