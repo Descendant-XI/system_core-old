90efe1841c686d066e99caec37cc05b88e76c1a1
Ashok Bhat <ashok.bhat@arm.com>
2013-12-10 12:30:53 +0000

    libdiskconfig: Use uintptr_t instead of uint32_t
    Use uintptr_t instead of uint32_t when pointer is cast
    to an integer type.
    
    Change-Id: I03dd4c7ff66611661af466dfb7b393d461bb2d7d
    Signed-off-by: Ashok Bhat <ashok.bhat@arm.com>

--- libdiskconfig/config_mbr.c
+++ libdiskconfig/config_mbr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98f6948a60f63fa96cf3117edadd094c48feded20ae421bb17dd17430e2c588
-size 10124
+oid sha256:5e6a879566b32c62a176e6ee430f6622719bf4ceed1d6c07c972c924e9db17ed
+size 10125
