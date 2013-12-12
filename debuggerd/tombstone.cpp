114b260c82f978a158e488c157d38f7671442f00
Mark Salyzyn <salyzyn@google.com>
2013-12-12 12:21:20 -0800

    debuggerd: Support newline split in log messages
    Change-Id: I2c19f12ef027ab1e1455ce78824537da1b03edcd

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c88768f5ca5e2c939f20bfa830a675231c45a226300cb2a3aab5e18d3583ff7
-size 23015
+oid sha256:02f3adf4da79cb862f1b4a869a46b4bbb2171ae3895fb6066df51fa8b000fa29
+size 23152
