7f4eed6bb84aee168597a3be9afce2e346e0284c
Narayan Kamath <narayan@google.com>
2013-12-12 10:25:30 +0000

    Get rid of some log spam.
    We don't need a warning if an entry isn't found in a zip
    file. It can happen as part of normal operation.
    
    Change-Id: I86c132a040371f36f0dd981b49c02b3173821439

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a76de3ea42e13dc032023885409aca16340e1a19cc86add0646f0f78bac4e60
+oid sha256:46c5a911346315befa86d3f386ee86fee743c06d306285fb95b2dd8e78bd91b9
 size 34831
