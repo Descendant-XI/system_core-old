9e0bfa1f41a360ca01bfe35b8669e10e0a1b5bf2
Luigi Semenzato <semenzato@chromium.org>
2013-12-16 13:59:03 -0800

    Append function name/offset to kernel warning signature.
    The current crash reporter signature for a kernel warning
    is the 8-digit hex hash for the warning.  For convenience,
    add the function name + offset to the hash, when it is
    available.
    
    BUG=chromium:328948
    TEST=ran unit test
    
    Change-Id: I8f047497c0556227a1dbdf650dabcd4c51aa8340
    Reviewed-on: https://chromium-review.googlesource.com/180320
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- crash_reporter/warn_collector_test.sh
+++ crash_reporter/warn_collector_test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a595b86f010b5ef075ab3cfcfcb676769725dcd6637b06bb99aa068d184ed2
-size 2019
+oid sha256:888e505b174105d835f38e9da5a4cd9a73c3b7da24408003f05ba9f01d74daec
+size 2018
