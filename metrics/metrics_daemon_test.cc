68f553180011096e7e60cf6dbc693501c4b5d72d
Mike Frysinger <vapier@chromium.org>
2013-12-17 15:50:52 -0500

    metrics_daemon_test: drop unused vars
    BUG=None
    TEST=`FEATURES=test emerge-x86-alex platform2` still works
    
    Change-Id: I86aa6a7380d226173c642880a3fc50faf3219d13
    Reviewed-on: https://chromium-review.googlesource.com/180333
    Reviewed-by: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- metrics/metrics_daemon_test.cc
+++ metrics/metrics_daemon_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c67fdbcadee869a342b2453304476d943dafc5bd969ae76a2152818d7d3fab3
-size 27969
+oid sha256:27699bac4609df4a6efab2dfa2e39e4b7999942a7cbec9befaf7750a1f491552
+size 27873
