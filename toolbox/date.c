5b52bcc85dd86aaec36ef68ec4ce27593e31bca8
Greg Hackmann <ghackmann@google.com>
2013-12-17 14:00:03 -0800

    toolbox: date: use RTC subsystem on devices without /dev/alarm
    Change-Id: I48c0fd1f4cffef936afb8d48d6d93babf006004d
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- toolbox/date.c
+++ toolbox/date.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:569725f80b7ee2c01028e8c42b469a99137849ea697b3653610abb8be82f814f
-size 3180
+oid sha256:ea9adec6b1fff4c5fa87623e21bd90ee042557103731e778aaa4c724c25a512b
+size 4446
