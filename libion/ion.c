790e7e3305a4903da7195bbf9e26eb903242aaba
Colin Cross <ccross@android.com>
2013-12-18 15:17:21 -0800

    libion: return -errno from ion_close
    Return -errno from ion_close instead of -1 on error.
    
    Change-Id: Idb828aa4b4d1a660b79d2dba8be32f306eac959d

--- libion/ion.c
+++ libion/ion.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ef373c7b19627050afe8f8549df29813c6e91e68cf65f9fd496637ff9ded659
-size 3958
+oid sha256:90d543ba9e36a8a89277f7aac9ff4f9f139235be9af5a14d1a5e8a5357ad15b8
+size 4017
