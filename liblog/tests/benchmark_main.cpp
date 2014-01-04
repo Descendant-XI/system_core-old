2321bdb9ecefabbd470445a93bb6d141cf3699f8
Mark Salyzyn <salyzyn@google.com>
2014-01-04 00:41:16 +0000

    Revert "liblog: Add liblog test suite"
    This reverts commit 46abc522964f797fafb0a3af2cbbabee05597f47.
    
    Change-Id: Iced83fdfa7de061c0495920ea28a9468023706d3

--- liblog/tests/benchmark_main.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:799ee35fe98091728d9ea8fd3be17c8ac86f339653ea8cf477054a250792d26d
-size 6867
--- liblog/tests/liblog_benchmark.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c942f02c5d1735bfe385cafa89f33e1e19a774b10205be33e10afdb4ce668b53
-size 5598
