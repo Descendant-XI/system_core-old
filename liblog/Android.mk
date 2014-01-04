16ad77b4f4e75197174298cbc10a0bdf80756951
Mark Salyzyn <salyzyn@google.com>
2014-01-03 16:00:02 -0800

    liblog: git_master@964770 build problem
    - darwin does not get log reader
    
    Change-Id: I64eb69a6099a88d2acf501de725c35741fd7e551

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fed4d9647cbcbb05ac34793592253276e481a4effbed9a3b320b6abf12648d4
-size 2391
+oid sha256:2bdf535bc6001020d6c719f01237a2ef03192e782e5fa88bfcfc9f64a5f85f39
+size 2445
