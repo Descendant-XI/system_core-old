0b65112a9528b9a95ea75982ae0b23003073f7b0
Mark Salyzyn <salyzyn@google.com>
2014-01-06 08:14:11 -0800

    liblog: resolve build warning messages
    (cherry picked from commit 153b370624c3da0660281f692fbe3f2da96cfd70)
    
    Change-Id: Icc6f1f2c926a008efc4df71022cfde8d0e285880

--- liblog/fake_log_device.c
+++ liblog/fake_log_device.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185a71baa98b3b911991e47e68423f548b12d80e785eafea7fd98c7de89f1129
-size 19301
+oid sha256:0c2b3ebbd77b11d08d751738ba20442d3a1b230375d788cb546b5addcfb5cee2
+size 19330
