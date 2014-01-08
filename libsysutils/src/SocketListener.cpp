d00fa549daff4e0defad356ce6c69b06394be891
Mark Salyzyn <salyzyn@google.com>
2014-01-08 12:44:23 -0800

    libsysutils: SocketListener export release
    * Foreground thread (legacy) closes connection by
      reporting false return value for onDataAvailable.
    * Background thread can choose instead to close the
      connection asynchronously by calling release, must
      return true for the onDataAvailable method.
    
    (cherry picked from commit d7ad4e409eaac53db3a9789060097b712850b337)
    
    Change-Id: I70ab37d5c22b02804aa3b4dfb26cc9b75291f8b6

--- libsysutils/src/SocketListener.cpp
+++ libsysutils/src/SocketListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2182092a5c149ccc8e12f76fb076727778af1589ef3f1b3f01e92680dad6bdb2
-size 9109
+oid sha256:5bfb859a26ecffa040ea6d8f78a83f85557ca9dc43e23df22dcef85ff4cfb637
+size 9611
