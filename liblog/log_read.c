93764225561b2a134aa1e3e2b0d0f04d863adbca
Mark Salyzyn <salyzyn@google.com>
2014-01-09 07:45:30 -0800

    liblog: high CPU usage from logcat
    BUG: 12457855
    
    - log_read.c poll timeout should be -1 instead of 0 when idling
    
    (cherry picked from commit 080bbfab94ce91e4845860d5253eea6d869bbc18)
    
    Change-Id: I9082a15fae0fbf999c0ed516950a49d65a279298

--- liblog/log_read.c
+++ liblog/log_read.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d0abf1295fc380da3ebe433d090e22cd766faf8c7d5e7ef19a51b9c03ab0453
-size 17266
+oid sha256:b905effd66f359d5e25aeb2ce922c6ff8fc8db9de838c8a8619953350ed2e9ff
+size 17355
