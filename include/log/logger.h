52be112ce15d74ad4e8a86666774b0705d8169c8
Mark Salyzyn <salyzyn@google.com>
2014-01-10 14:07:58 -0800

    liblog: Add const pedantics
    Change-Id: I819695b778ac08fcfc9b1a87f3f86e5715f53084

--- include/log/logger.h
+++ include/log/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:098518816fa32ee726209fb8ea250a2097d6e6e61ba437cde16fe4fd5a803457
-size 5117
+oid sha256:84babd664159c414dbdc6f5bf70c1f8aa775b70c5c322b99284357823d16ab15
+size 5202
