1cb408603a33f009c1a3466c83f6b92265b39300
Christopher Ferris <cferris@google.com>
2014-01-10 16:05:12 -0800

    Move to C++ for debuggerd.
    This is part 1, only including the bare minimum changes because
    our diff tool doesn't easily show differences when a file moves. This
    also breaks it into a small chunk in case some other changes break things,
    as unlikely as I think that will be.
    
    Change-Id: Ib7a3e7a2cc1ac574d15b65fda23813ebcf5d31af

---
+++ debuggerd/debuggerd.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f90bb79b6d36fc61a8b1159e5d0a5f13d15530aed0981cf25f5a9612e4af1a0
+size 17648
---
+++ debuggerd/getevent.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:126ac919d06c7d140bf32242ba6863cfa7b84d527d96eb0b20315790c305c9c8
+size 5650
