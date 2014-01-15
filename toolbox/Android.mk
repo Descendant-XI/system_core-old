afd8f055fc4171c54f091ab5307d24fe907ae673
Sujith Ramakrishnan <sujithrk@google.com>
2014-01-14 16:57:22 -0800

    Add mknod utility to system/core/toolbox
    Change-Id: I46586cba83ecb8095721be8ffad9e386d4698e4a

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:440552253c380d5adbebbb544333675661ffec297a7f075933de8b1b2456f2ef
-size 2171
+oid sha256:0304a1580e711190450ad3be6564f5f5a5ca5d654a8dbee56b3cfd7f3212234b
+size 2180
