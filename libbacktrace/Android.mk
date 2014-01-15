52ed09262407037c80e834fe3c6b48a1b1c6b075
Elliott Hughes <enh@google.com>
2014-01-14 16:44:31 -0800

    Copy the bionic -fstack-protector hack into libbacktrace.
    aarch64 doesn't support -fstack-protector yet.
    
    Change-Id: Id1e589742ba074f59d3390d059248c9b371aff58

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21e014c05a9e3d995b3d6fba700f9bdd18cc85b4343555239b982fb1eeb3667a
-size 5878
+oid sha256:e2b4699b3fbc4eaba42bef54de05b83b8cfbea2ea1e094d8172897429dd91aeb
+size 6067
