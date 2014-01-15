45138ffb6bba83265b04c854ad179c79703a36f0
Nick Kralevich <nnk@google.com>
2014-01-14 16:34:56 -0800

    Remove obsolete vdc call.
    Since ca8e66a8b0f843812014a28d49208f9f6f64ecbc, init has
    been handling reboots and filesystem unmounts. Remove obsolete
    call to vdc.
    
    Bug: 12504045
    Change-Id: If8704ca042cb3a68857743b9973e48c52e7eb881
    (cherry picked from commit 225459a5da21e9397ca49b0d9af7d5fe3462706b)

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af24bda96d3bacb6046adfeda6f462b737ce23a453c8018452494220b62d00cc
-size 47185
+oid sha256:012ea87e95fa3e767ebe2bdea3723b71049146a480ecdb31907ead4d55c5fe41
+size 47104
