e45f6fb78b14aa47cf55fd5cb0886014eb4d6e77
Nick Kralevich <nnk@google.com>
2014-01-18 15:28:11 -0800

    don't load property_contexts from /data
    Bug: 12613118
    Change-Id: I5a84b13d2151075d5136e3682431fbcc9307df13
    (cherry picked from commit dc3a42bb11dc2aefee37f799b5a1cce87cf35aaf)

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5755bf8ed351a79e515869e5a39b523cd6579b1036474634e78dea9662ad204
-size 32265
+oid sha256:4be94186689812b9ff57fb0631bc7a09c8a3b158092e930236e91faed4fed92f
+size 32199
