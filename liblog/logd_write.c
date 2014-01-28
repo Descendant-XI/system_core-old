41957c3a0eef941fd942a9c2d75acf05b97eabc8
Kristian Monsen <kristianm@google.com>
2014-01-28 13:18:24 -0800

    Fix code style, put unused in the end
    Change-Id: I7315b35049924d5067203a432584b8f29e5a1391

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2618ea20249e36dc10329ce5c975b0e7e85daf4aa541a6459b9cd0c7cc9215ef
+oid sha256:a58192e6d00aa5dcc531d61bd2361a5201bc0da90fd342c20b73b38f3aa3f0d5
 size 8887
