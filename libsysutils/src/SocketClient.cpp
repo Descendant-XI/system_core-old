27d9f490f11f43dacbbd52f723109216d053da9c
Mark Salyzyn <salyzyn@google.com>
2014-01-29 11:25:01 -0800

    libsysutil: frequent native crash /system/bin/vold
    regression from commit a6e965578e44f9ae5f98de822ba5decec381dffc
    
    * wrap writev with sigaction SIG_IGN SIGPIPE to emulate the
      send(,,,MSG_NOSIGNAL) call it had replaced.
    
    (cherry pick from commit 83fc720785c4e40f3341daf7c0bf5ee99261fee9)
    
    BUG: 12796279
    Change-Id: I14363630ada79c0a5b85bb6b2afd0a1c4d5c3109

--- libsysutils/src/SocketClient.cpp
+++ libsysutils/src/SocketClient.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22718507199a0503733a7ce95cf7ba7fbc6973cd9df74cd9afa2d8991e55cedb
-size 5803
+oid sha256:84be40c5a96624b223fbb1786940f0e41cd96ef0fca112d797500d490974f39e
+size 6160
