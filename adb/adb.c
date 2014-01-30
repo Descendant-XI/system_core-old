d8baedf20b624b335660267500d99482425f3993
Snild Dolkow <snild.dolkow@sonymobile.com>
2014-01-30 10:08:38 +0100

    Fix adb forward --list when forwarding a lot
    The list action had some problems with large numbers of forwards:
     * adb_query() limited replies to 1024 B (and the print was useless)
     * the reply header's length could overflow (also in other commands)
     * ...and the client had no way of detecting it
     * writex() didn't retry on EAGAIN ("Resource temporarily unavailable")
    
    This patch makes all "OKAY%04x" replies use a common function which
    checks the length and limits it to 0xffff. This means that the client
    can easily check for truncated replies.
    
    Before: forward --list starts failing at 15-30 forwards (depending on
    device serial and forward spec lengths).
    
    After: no problems with forward --list.
    
    Change-Id: Ie1e82c4d622f5c56e51abb26533ba17d40459914

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edaf046e44729f801da71c8ea80cefbde2967d7d5f01ff2b23d5167086c0b31a
-size 49456
+oid sha256:896b318505c1b1861ddaef9e318355526853e8a3da52d902ac228c07936ccd48
+size 49157
