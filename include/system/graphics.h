a99ee9f09ad1815d3e8e14d56167836660096a96
Ruben Brunk <rubenbrunk@google.com>
2014-02-04 18:13:34 -0800

    camera3: Add opaque raw format.
    Change-Id: I7abf0e63a0f74ede46e0fe4dd1285283355624b1

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a535cb046845b27aedb40db351309a83e49223a0da451a5eed499d2277a41056
-size 10338
+oid sha256:aa303aedad24ddc5d4065ba59ee41f7d5e594bd167e2aca1ccd0c03b2295894f
+size 11453
