f434c830b68f9b9ab836f972afb40179c35d3564
Mike Frysinger <vapier@chromium.org>
2014-02-05 21:05:10 -0500

    drop unused makefiles
    We use platform2 now w/gyp, so don't need these anymore.
    
    BUG=chromium:337753
    TEST=`FEATURES=test emerge-x86-alex platform2` still passes
    
    Change-Id: I677b646dd56342be5cd98cb57852cc62c805240b
    Reviewed-on: https://chromium-review.googlesource.com/185561
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/common.mk
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:51cc37da253c199f85db36e1e9fb14fe8460fbc88a970f21361922bfc73c1794
-size 32286
--- crash_reporter/make_tests.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e25316e14711d179de40dee4f5969d670b7e0ab6ac743ef7d01449bd3d002214
-size 329
