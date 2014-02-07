58a53cc05ec2deeacd71679456af76a8f5af0935
Stephen Smalley <sds@tycho.nsa.gov>
2014-02-07 09:14:13 -0500

    Convert all selinux_android_restorecon and _setfilecon calls to new API.
    libselinux selinux_android_restorecon API is changing to the more
    general interface with flags and dropping the older variants.
    
    Also get rid of the old, no longer used selinux_android_setfilecon API
    and rename selinux_android_setfilecon2 to it as it is the only API in use.
    
    Change-Id: I1e71ec398ccdc24cac4ec76f1b858d0f680f4925
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3db267ba4657bab69de4ee77113909ccdcbfd4e7e83cd3d9e447d3aa433075b
-size 23271
+oid sha256:458029b745a6a982f6bfbd420a477bfff2c33403c1d50a0223e46dc11f408d70
+size 23274
