3767aa568377a15e496beb51d9928b53ef969c5a
Mark Salyzyn <salyzyn@google.com>
2014-02-06 18:11:13 -0800

    logd: prune by worst offending UID
    Change-Id: I39965007569123ff5eebe01b5bfa555bbcb2dfe7

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92d629e9c57d7c97454fe466bb382837b30f99450ac9a77c6416fe376e23c29f
-size 7152
+oid sha256:b928661d1466f0c9099c2832c1227473c1f1895b3a111a67dedd141fcaf0eb00
+size 8826
