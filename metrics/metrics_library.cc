662cfe125058bf45021a172df7f32739d6533da0
Darren Krahn <dkrahn@chromium.org>
2014-02-07 16:51:15 -0800

    Added a metric for tracking non-zero TPM dictionary-attack counters.
    BUG=chromium:341358
    TEST=manual
    
    Change-Id: I99d3e4279c6d9185b9b515259d4fe511e0d79874
    Reviewed-on: https://chromium-review.googlesource.com/185493
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Darren Krahn <dkrahn@chromium.org>
    Tested-by: Darren Krahn <dkrahn@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:449ac8d57d3874b6fd05bb6c12a3b0487a6d05d6bdcfe18335583c2a59199582
-size 10928
+oid sha256:5c10a948b18a22dd65e4b88104334608432f66bb8447082ceb4eec062ad47b55
+size 10975
