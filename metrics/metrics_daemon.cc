67a102422491dc254d90408b8ccd990e28b1333b
Luigi Semenzato <semenzato@chromium.org>
2014-02-14 15:05:51 -0800

    metrics daemon: change location of flag files
    For security reasons, we changed the location of some flag
    files to /var/run from /tmp, but we didn't update this
    program to look for the files in their new location.
    This corrects the problem.
    
    BUG=chromium:200181
    TEST=manually verified that kernel crash counters increase
    BRANCH=none
    
    Change-Id: I03efe6179a1ad8dfd4c8cbb1eccf24c7a8494058
    Reviewed-on: https://chromium-review.googlesource.com/186692
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23a24263844a0091960001738d4a44c5e1ea275cd97f03cacd69a47e024c5f0b
-size 39946
+oid sha256:ad6582fae5efea81d59da38c276fe4404b8f4663ebf6add409373980092993b6
+size 39954
