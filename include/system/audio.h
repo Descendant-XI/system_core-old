e63b211e3d8a9ca48f16dc6193468fbd2552ea00
Eric Laurent <elaurent@google.com>
2014-02-18 12:40:36 -0800

    audio: added definitions for audio routing extensions
    Added definitons and structures for new audio routing features
    in the framework and audio HAL.
    The structures describe the objects needed to perform audio
    routing and audio gain control: different variants of audio ports
    and their corresponding configurations as well as gain controllers.
    
    Change-Id: Iecd1b9186831067a52cc55619a63b7bba57dcbd9

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69646d61081158ced6a5ec3b9a522a10a434b69ea4fe3f3f5b7eee420986d6ca
-size 32590
+oid sha256:b3ab89aad12f9f9f5b01c7eb9b52b3d5297fafe7b74e547c4f5317f8a4cede22
+size 42467
