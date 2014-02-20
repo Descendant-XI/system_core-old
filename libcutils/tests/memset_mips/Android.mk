af1bef417c7815aea8e6c93731241dc2e76f20ed
Elliott Hughes <enh@google.com>
2014-02-20 11:12:41 -0800

    Remove dead code.
    We have benchmarking for memset in bionic itself, and a benchmarking
    framework for other native benchmarking.
    
    Change-Id: I241a288a96d3b2d37f3d51b04a519ac23f18a989

--- libcutils/tests/memset_mips/Android.mk
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eb844bed1114e352a68ea2e31439d7b2ec32c4dd9ce9765ed99b081f35809218
-size 421
--- libcutils/tests/memset_mips/test_memset.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:309dbb829d8a04d41ba7b719e576b5f375b1052e86296012303653d74935fba0
-size 6560
