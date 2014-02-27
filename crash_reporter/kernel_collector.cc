1b2f67e7c3f7dada40d8e62f260116642ef43193
Yunlian Jiang <yunlian@google.com>
2014-02-27 14:26:15 -0800

    crash-reporter: convert number to int before calling abs()
    BUG=chromium:347721
    TEST=emerge-x86-generic crash-reporter passes.
    
    Change-Id: Id37528c44d5c318b917932ebc755eb6b4ca01e8f
    Reviewed-on: https://chromium-review.googlesource.com/188212
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>

--- crash_reporter/kernel_collector.cc
+++ crash_reporter/kernel_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6d4e81125681453aa57e5b554d16bcd9edaf9de054f7016747e35ff0e6f442d
-size 18602
+oid sha256:d7dcbc0e63fed5adebc2f040b0f2872f908855a788c51157b6bcbb5f2c52ca86
+size 18628
