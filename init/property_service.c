2ea4a0bf9378102c2493296e8efe00a736393de6
Jeff Sharkey <jsharkey@android.com>
2014-03-06 14:26:36 -0800

    Add "import" support to system property files.
    During boot, allow a property file to import properties from another
    file.  Supports importing the entire file, a specific key, or any
    keys matching a specific prefix.  Here's some example syntax:
    
    import /oem/oem.prop
    import /oem/oem.prop foo.*
    import /oem/oem.prop foo.bar
    
    Bug: 13340779
    Change-Id: I867f9a10ca09684326675d9f051f5cf2ae171617

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b148cfd8a51533b097d15321c2de9f22dcc4edb07e769f97a19f0b82bc6f6954
-size 17380
+oid sha256:2e0584027c4fa67dc2d024eb4027d34fab6e8a2c019a87ab5150a73af8b958bd
+size 18339
