76127fb9b8128338291dd2009d1103ed9d48f48f
Andy Hung <hunga@google.com>
2014-03-06 17:18:58 -0800

    Update audio_bytes_per_sample for PCM 24 bit packed
    Change-Id: Ic79f604893f5c4f2e8e35189d6638b5b160435ed
    Signed-off-by: Andy Hung <hunga@google.com>

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ea8a360802fab19fd8218578466c659967262a0cd5bef8a1fda61b829313ff2
-size 28328
+oid sha256:d8478f46c06d73118ad025e4584bf2b74b6fd073e399dceae634b743039bde7e
+size 28420
