1d9f2af979c050135d98d0307b85bdfb6d689910
Mark Salyzyn <salyzyn@google.com>
2014-03-07 15:08:20 -0800

    libcutils: 64-bit compile issues
    - Use inttypes.h to print uint64_t values.
    
    Change-Id: Ia10a3b67cadcce282563467a464ec07066cc2051

--- libcutils/qtaguid.c
+++ libcutils/qtaguid.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c47c92d63af18162089d26bfac84d630cc2a0cd968e00ba97d0701b030e7326
-size 4868
+oid sha256:893c4f83c3f10645a98f3b6e2fbef6f9c8c7157438535927ecd02fa026b3ed53
+size 4929
