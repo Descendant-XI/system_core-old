d9d8c4c372004001dade79866af7ae29f396b045
Glenn Kasten <gkasten@google.com>
2014-03-07 12:31:03 -0800

    audio_is_valid_format is true for all PCM formats
    Change-Id: I733f3208315816575f6d4f7722e906ab50b161af

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8478f46c06d73118ad025e4584bf2b74b6fd073e399dceae634b743039bde7e
-size 28420
+oid sha256:d3dd25f27df7ec9beb7f1d7559c06cd5f5da0df1559be681baf8f621949d057b
+size 28612
