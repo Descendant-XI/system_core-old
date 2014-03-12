5ba4b8f6e666f60dda88a8b1c221fb22b08bf6ff
Dmitry Grinberg <dmitrygr@google.com>
2014-03-11 18:28:15 -0700

    fastboot: allow format on devices with small buffers
    Formatting large partitions on devices with small transfer buffers did
    not work before since format used a strange path through the code to
    send data. It now uses the normal path. Also cleaned up a bit. FS
    code now lives in a separate file and the custom path for format is
    gone.
    
    Change-Id: If4e01cabc2e250b7c02ca7ce8c268e51d49e1529

--- fastboot/fastboot.h
+++ fastboot/fastboot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:422eafd2e677860b620f0cf4d24896b1d7f3796c79770606dea1cd994cab1910
-size 3037
+oid sha256:eea55a6b7bc7378425d3d52f22c1e3de02f50a9a47d8538d40b51e7db8873eb3
+size 3064
