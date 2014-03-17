ff6e4ee91cfdc2bc033e5adb9b416dcb7736b549
Luigi Semenzato <semenzato@chromium.org>
2014-03-17 12:28:38 -0700

    metrics: add per-version daily stats reporting
    Adds a few kernel crash stats which are reported daily but
    are accumulated from beginning to end of a Chrome OS version.
    
    BUG=chromium:339588
    TEST=ran and checked histograms on device
    BRANCH=none
    
    Change-Id: I630c673156c28dc90ffe0c9c2df58caaada082dc
    Reviewed-on: https://chromium-review.googlesource.com/190404
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7c1835d5dcc1e8ed9c40f0ab58f4e6cf3511ecb5a75bfcb0bce3f88479d63f3
-size 39272
+oid sha256:a9aedbd95a774a1a41dc431b1d9fc9fc99d4d508ece5d44251ad834cfc23f9e4
+size 42991
