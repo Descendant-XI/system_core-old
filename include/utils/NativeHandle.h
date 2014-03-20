c6487671fa64f74cc1a7f26cc4e423063d4104cd
Wonsik Kim <wonsik@google.com>
2014-03-20 15:42:03 +0900

    Clarify ownership for NativeHandle::mHandle
    Change-Id: I0835278df1aa78f10d5493d7ef2c9e4a15c0fee9

--- include/utils/NativeHandle.h
+++ include/utils/NativeHandle.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce23ba7b70a15ee8cbae56393c859ba61fddcc55933b3d06da640cb56f3bb68
-size 1498
+oid sha256:eda690f06d92d6b2f2e9cf9dc8e2b7cda9b5eeaa23233d485f0382c0b4a779a7
+size 1681
