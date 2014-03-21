271e180ae4d22ae14766c493abfd74bb0d5eae37
Mark Salyzyn <salyzyn@google.com>
2014-03-21 14:37:06 -0700

    logd: compile warning message
    Change-Id: I7ddd010031299395cad96f1743d7a7e6a6866b4c

--- logd/LogListener.cpp
+++ logd/LogListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7573d23378982c08dbf364699a349e9e5d70587de13c372b9b67e830c01d83
-size 3154
+oid sha256:0982b636cbb7cc5d222ab2197f2cb13741ba2a3f591aa9e7a7b8943b482e3a3e
+size 3163
