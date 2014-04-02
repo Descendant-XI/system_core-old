4396f1be71e4d38b37263a03023e61c80d737dba
Geremy Condra <gcondra@google.com>
2014-04-02 13:42:06 -0700

    fs_mgr: disable verified partitions on debuggable devices.
    This checks ro.debuggable to determine whether a device is in the
    debuggable state or not, disabling verification if it is.
    
    Eventually we'd like to narrow this down to only devices which
    need to disable it, but we aren't there yet.
    
    Change-Id: I06a329fe5449deab6bae595877dbb1f200850241

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:960dbd9bec27fd1aac48a5429f1ce12eb0f5b2fb72315b13aa2d65c1fce843c4
-size 16245
+oid sha256:3e73e3fa6dc2f8df2b54c60a84b567e3f08c72abf3e2e086ad271a1817e387f8
+size 16549
