ad4cb7c1b0c3a16883b136c0bb991953792d164f
Paul Lawrence <paullawrence@google.com>
2014-04-04 09:34:19 -0700

    Add force_encrypt flag to fstab to force encryption on appropriate devices
    forceencrypt= will encrypt the drive at boot if it is not encrypted. This change
    will not have an impact until we set this flag in appropirate fstabs.
    
    Bug: 11985952
    
    Change-Id: I6c65eda7f109e4936aa160da50f871703aefb351

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e73e3fa6dc2f8df2b54c60a84b567e3f08c72abf3e2e086ad271a1817e387f8
-size 16549
+oid sha256:e1561e4aa702ab213db065315971c478017655d6a0459eff03efa1dd1067c3c5
+size 16052
