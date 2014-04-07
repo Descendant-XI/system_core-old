6aa996fe710a70d07293c90747c05f0bea60a01f
Mark Salyzyn <salyzyn@google.com>
2014-04-07 14:58:08 -0700

    logd: liblog: logcat: debuggerd: Add LOG_ID_CRASH
    Change-Id: Iea453764a1a4fc1661f461f10c641c30150e4d20

--- liblog/logd_write_kern.c
+++ liblog/logd_write_kern.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bd864c4885d092ffdf04dc9310f534302126f9cd7a334bb3f0b49a9111a94cc
-size 8990
+oid sha256:4ef24ec61e5012ee65503830f1e81e73cc98353b8b97ce4dc445632088001dc3
+size 9072
--- logd/tests/logd_test.cpp
+++ logd/tests/logd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfd3c5b4946197432cdfc3f146f8e26b4edd8bdfdbf8d07380e7481b3ac718cb
-size 17381
+oid sha256:0423cb9ad38233099e50a02c6ab0afb84f9b14e707d08ea98784e35170e1a3ed
+size 17383
