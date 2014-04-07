e7c38641de0f6cd6fc4f68ed911a216bb3ef5fc3
Elliott Hughes <enh@google.com>
2014-04-07 15:11:57 -0700

    Move date off the non-portable bionic32 'strtotimeval'.
    Change-Id: Ib706d3a536c409810cfbc47270448c6375314506

--- toolbox/date.c
+++ toolbox/date.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9dce7d91ce5b9eae08a42b1f71a214d5bc994a68850ffba9bc89013b52a2ae2
-size 4445
+oid sha256:6587e0ddc65d3f663e7540ddf75ce8eba0b5f32b44183b087e72c71f5495b5ab
+size 4944
