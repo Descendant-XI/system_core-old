603dff52e872b2bca371f780a87cb1622cfe0ac7
Greg Hackmann <ghackmann@google.com>
2014-04-06 21:25:58 -0700

    logd: fix LogStatistics::format freeing wrong pointer
    Fixes a leak and use-after-free if a client passes in an
    already-allocated target buffer (currently none do)
    
    Change-Id: Ie20c30bd5ba31e1b6db73885bd9d2ebd7e40dc27
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- logd/LogStatistics.cpp
+++ logd/LogStatistics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fa619f6dfac3dc9509bb7f5e5c395f198199c2b1212cf35d751593a00df5c96
-size 21845
+oid sha256:44e39a98bac023e8d3ee482d9e719998f3797c75ca476ebc772b2a9e3489c054
+size 21846
