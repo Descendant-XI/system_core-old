c773725331fada5ed7b7069987fc107283955b85
Ben Cheng <bccheng@google.com>
2014-04-11 16:19:30 -0700

    Fix signed vs unsigned comparison.
    Change-Id: I4011607858f3a82acc04509c9f795f960496ed69

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fd8f596ab82aea3900cfffea3d14eb1007de45696aff7591a0fc9185d0623d7
-size 17238
+oid sha256:eabcef9e02b133a3d7133a3123b24d685c06e5df37d2163309bd9e4d3ce70219
+size 17259
