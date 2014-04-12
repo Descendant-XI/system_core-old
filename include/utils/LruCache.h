e774fd9b96610ecdbc7941f6cb7d1fa5bac833d6
John Reck <jreck@google.com>
2014-04-11 19:14:15 -0700

    Add a peekOldestValue
    Change-Id: I89d8ea14c8ac02653d9c0e1fe24a4b17bb9c87f5

--- include/utils/LruCache.h
+++ include/utils/LruCache.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f458faf6dc31397a93af9e414134b5f6d79e67b2e5ac53d0d103b1f504da07ce
-size 6435
+oid sha256:3f6ca56d9a5299f0e2619853a7dcfe96802672676ea920b0a11baba7454ec29e
+size 6654
