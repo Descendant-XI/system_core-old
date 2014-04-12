73028426b85ff3dd6ab325c30936c84b24d37750
Andreas Gampe <agampe@google.com>
2014-04-11 18:23:11 -0700

    BitSet32 64b compatibility fix
    Using __builtin_c{l,t}zl fails on 64b systems as unsigned long is
    8B there. However, unsigned int is only guaranteed to be 2B. Introduce
    a check that should be optimized away by the compiler.
    
    Change-Id: I854d0817c6bb5ae13c257241240664bf8f1a7c8a

--- include/utils/BitSet.h
+++ include/utils/BitSet.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:081f63d52f4399f100344304f75bad261f0e2e9bd013829dc51d00f37799a295
-size 10283
+oid sha256:d1f5f7eb69ab84f5c30b0a0befdd12ddb83d3f0f6297629ceac2d4597277ebe9
+size 10934
