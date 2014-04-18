c4d86a1b5b67d28a97e5418bd3f057f3160cf1c5
Colin Cross <ccross@android.com>
2014-04-18 13:54:12 -0700

    libsparse: allow including from C++
    Change-Id: I3788fd07e2b52430a410f85fb79dc886c6a07fea

--- libsparse/include/sparse/sparse.h
+++ libsparse/include/sparse/sparse.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:299d8f3b0940002c2c43e7fa1f364e9077c1ddd630a253819de56ee046d1c800
-size 10539
+oid sha256:bcea1988b8febce599143970e795f37346149dbed5c445710357b6250ba247be
+size 10608
