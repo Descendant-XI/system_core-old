f8e732db652e18ecd2801393ebf68bb3dea1290a
Luigi Semenzato <semenzato@chromium.org>
2014-04-21 14:33:32 -0700

    metrics_daemon: log to syslog
    Also log to stderr when not running as daemon.
    
    BUG=chromium:364700
    TEST=manually verified in both daemon and non-daemon modes
    
    Change-Id: If62f70c1d327a5ecaf3075b6689b83a1494c09d5
    Reviewed-on: https://chromium-review.googlesource.com/199380
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Daniel Erat <derat@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon_main.cc
+++ metrics/metrics_daemon_main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8be64e3ce226bba78ae50d9411956a3fcb6ad7b4987a2e34318c498250312e2
-size 1743
+oid sha256:e36c909215e794d01bcc14712e2ab780fcfac7dc54bbc3cbfd8a41084a853b23
+size 2032
