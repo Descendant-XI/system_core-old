c1971da5aff52c66683d3d8bb180c39c75f4bbc9
Andrew Boie <andrew.p.boie@intel.com>
2014-04-24 17:12:45 -0700

    mkbootimg: fix padding calculation for 2ndstage loader
    Change-Id: I8db844cbc52896e51886b725fb86fd1b0fa57899
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- mkbootimg/mkbootimg.c
+++ mkbootimg/mkbootimg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c843a2b16d0ca2b90eea3d4503e3e9e1bc5b1b184f1c4e319590aa075f8c6ba0
-size 7996
+oid sha256:f07717c8b2e43f657ddcf837b586245b8e551becf7cc6e9d04e328925e2a37fc
+size 7995
