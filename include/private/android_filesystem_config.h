932c85f3ad0a52f4cce5b5b1a5eca71cdae1d1c4
Robin Lee <rgl@google.com>
2014-04-25 15:16:02 +0100

    Add new AID shared by all apps in a profile
    Automatically assigned as a GID to all apps running under the same
    Android profile. Used for exposing to or sharing files with apps
    running in a specific profile.
    
    Analogous to AID_SHARED_GID only the sharing is between processes with
    the same userId rather than with the same appId.
    
    The name to be displayed for uids/gids with this AID is 'u*_everyone'.
    
    Change-Id: I46efb749f74cb86868c5609a0ce1a6aab2f9e9c9

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c23f109eadb3b8e2d542c8e4bf13d25d721e013f4d40c4160f18bea2025f4f8
-size 12640
+oid sha256:fd4a71fa31356e2c4b6608b2daf6500c6994edc6ee050b8997f8491d5115dfab
+size 12763
