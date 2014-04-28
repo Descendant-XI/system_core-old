ddd6e63a202cdd9f2d2a1e130d7b1df9d58ea2e3
Ukri Niemimuukko <ukri.niemimuukko@intel.com>
2014-04-29 06:25:28 +0800

    RefBase: make Renamer destructor virtual
    This patch makes the Renamer class destructor virtual and fixes
    the incorrect constructor initialization list ordering.
    
    These issues and related compiler warnings have been suppressed
    by the build system which uses the gcc isystem command
    line option, unless a project explicitly adds the include the path
    system/core/include to Android.mk and uses the templates.
    
    Change-Id: Iff76a655eb8bd547adfe994c7315a005e98aed41
    Signed-off-by: Ukri Niemimuukko <ukri.niemimuukko@intel.com>
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3a5b9558296d520bca857a2872355d6e12893f201c47347df93d9a9a69233e3
-size 15885
+oid sha256:aaaa7f6ceec0a78cb8d3d101734db78b145bf5a6e460f2baa51ad19b9e2a8fac
+size 15955
