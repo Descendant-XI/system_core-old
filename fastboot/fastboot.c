b4c12fbeef4fcc239c588e6eda088bb0e66b0d90
Daniel Rosenberg <drosen@google.com>
2014-04-29 13:45:05 -0700

    fastboot: Fixed optional entries
    Previously, if an image was listed as optional, but was not found,
    flashall would fail. Now it will proceed if optional images are not
    present.
    
    Change-Id: Ic82595cf0cd6ddce4c676de590f03f1a95f32040
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d212b3b74927b041cf3f077e1c596aa90948bc7005db812d8f03948a7699855
-size 32721
+oid sha256:a1695b8d61cc4ea01d2a75e15a5342b195f2064da6ec94bbd2ba0b42df11776f
+size 32698
