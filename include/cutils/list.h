43d8bd4b05ef86e4dae190933761a060fdf08b4e
Thierry Escande <thierry.escande@linux.intel.com>
2014-04-30 02:15:15 +0200

    Fix list_for_each_safe macro
    The second macro parameter is named 'next' like listnode structure
    'next' field. Since the precompiler will expand all 'next' occurrences
    in the macro definition with what is passed by the caller, it is not
    possible to call this macro with something else than 'next' as second
    parameter.
    
    This patch replaces the 'next' parameter with 'n' allowing use of a
    next node not named 'next'.
    
    Change-Id: I78c859caf8193f21fe0bedaeaa8342d6e89ad14b
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/cutils/list.h
+++ include/cutils/list.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f45fe112507c48e2259f7be98e3069a4d91e4b15ae95e0994c134db312e452f
-size 2046
+oid sha256:a085504588e1e8bdd222eb06ec938eb88d712ed888a9d1d6834f647b286c3c2d
+size 2034
