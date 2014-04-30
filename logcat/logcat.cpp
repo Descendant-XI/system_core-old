aa4108397f1d39dfd864e3add6343b1e5d3433ac
Mark Salyzyn <salyzyn@google.com>
2014-04-30 08:50:53 -0700

    liblog: logcat: Move library print test into gTest
    Change-Id: Icd33808d4c974625f4fd0a126a90a2b4c90c8315

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f7346989ac8bfbbceaad8e3f09bd6b63ecceca8fdbcdd7f670da35c10ddb9b3
-size 27004
+oid sha256:4d79cc4b421e930fb4d0186c64f584b47fc255d947035a2ea1fabe353663c70b
+size 26852
