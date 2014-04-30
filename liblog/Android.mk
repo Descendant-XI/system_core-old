151f9727b6de1824f50a06072fdeb29507c0ba0f
Mark Salyzyn <salyzyn@google.com>
2014-04-30 08:50:53 -0700

    liblog: set -Werror
    - Deal with some -Wunused issues
    - Cleanup UNUSED to __unused transition
    
    Change-Id: Icd33808d4c974625f4fd0a126a90a2b4c90c8314

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa5bd4c972dc3d5b511be971e2f7ba76aa57f5f17d1251f1ba5a75bb63606aaf
-size 2713
+oid sha256:3a21c0b123e89b91b8970e6c8b008620a15d18492af11c5daa38fab7a1b94dc4
+size 2711
--- liblog/fake_log_device.c
+++ liblog/fake_log_device.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dbf825917b4ed95d83610d6a5e4faa1437ece832cf8b6e87b0ffbac79e2e2c6
-size 19360
+oid sha256:934f0137cd5bc8667b3e962cfe2571dfc6d6ca58cfd37ba5c1d9b0afd802f9a6
+size 19439
