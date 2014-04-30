6f9d25309b33d95309419560c9c709ae1c360ea5
Mark Salyzyn <salyzyn@google.com>
2014-04-30 15:35:00 -0700

    fs_mgr: turn on -Werror
    - deal with some -Wunused-variable issues
    
    Change-Id: Ie0140d4777ddf862e4bbed76142a1dbb8320c1b0

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efc799b4f08c95c12a2e7a17e46a571a96ebdd644be81d4db8de41138140ee61
-size 10712
+oid sha256:99d3f775328f765796c0ebee88400597b1b48f1db42ec09dd97319d97cdeb1fd
+size 10700
