f7be12f46dc8768300a9ddbde47c1b71dc309941
Mark Salyzyn <salyzyn@google.com>
2014-05-01 07:27:02 -0700

    libzipfile: turn on -Werror
    - resolve some unused references
    
    Change-Id: Ia3748c7e25963c2fd3d58e175177f0f4dd405997

--- libzipfile/Android.mk
+++ libzipfile/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6ecbb11960dae22b00da2211dd8fa28507e27eb67c92a2390a2cfec757e89e9
-size 723
+oid sha256:bce398919888ded9a473f4ef94408c167861607b80bf9dc6e80ea4320c510df3
+size 798
