b8d4774e3e4c74586accdc351793fd00bdedb232
herman_lin <herman_lin@compal.com>
2014-05-05 03:43:20 +0800

    Modify VID for Compal
    The original definition of VENDOR_ID_COMPAL is for Compal Communications, Inc.
    But Compal Communications, Inc(CCI) had been merged into Compal Electronics, Inc(CEI) at April of 2014
    So VENDOR_ID_COMPAL should be changed from 0x1219(CCI's VID) to 0x04B7(CEI's VID).
    And add one new definition(VENDOR_ID_COMPALCOMM) for CCI to maintain old devices that use CCI's VID.
    	modified:   adb/usb_vendors.c
    
    Change-Id: I9e9a96f6cc81273ff2c9353a13e7ce3b0583396b

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d4967428be772033897bcb39dc5bf7a0bcaa040bf83b2e4d6d3a443ade230aa
-size 10415
+oid sha256:3fdf8c65e68ebe6f68752a46bdb6bcbe158afb66c247651f86f422d37bc460be
+size 10510
