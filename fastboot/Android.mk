7ffb66b036a2df82334b7d73c3dc3d03d0645b54
Andrew Hsieh <andrewhsieh@google.com>
2014-05-07 20:21:11 +0800

    Remove obsolete reference to ddk; Fixes for -Werror
    Bug: 14416410
    
    1. The new mingw-w64 toolchain x86_64-w64-mingw32-4.8 has ddk
       in x86_64-w64-mingw32/include/ddk
    2. Add -Wno-error=cpp to suppress a warning that turns into error
       thanks to -Werror:
    
          Please include winsock2.h before windows.h
    3. Cast GetLastError() return type DWORD to "int"
    4. Include direct.h for _mkdir
    5. Include stdint.h for uint8_t on Windows
    
    Change-Id: I4bec0587f6573692f08c760da6c98ae551b8b5eb

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc957d3de20e9ba34f2d4375ad991615d890f5805e7264f744f31d5d54f48f3
-size 2836
+oid sha256:4643c1b8be75b0ff5c54e1dfd52d169be258d5cc872ba3d528193e5d731b0491
+size 2730
