4548f86b1bff16589b15249cdd3d7f7eb1301a3c
Alexander Levitskiy <sanek@google.com>
2014-05-07 23:31:59 +0000

    Revert "System: Fastboot: warning fixit, misc bugs and cleanup."
    This reverts commit 782111b3bc5d372afc8d66ab6023e834b7c23b88.
    
    Change-Id: Ib66400dfaeef7f01e6461a57b9f09be08de2f1fe

--- fastboot/fastboot.h
+++ fastboot/fastboot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f03545f8e2bc34abb5e2d7d2b1d6ae7632047775ae2c32f80e608a0d11a2d592
-size 3103
+oid sha256:577737afc4a1d43350dc98aeb2a19a6b1df57bbd51108d128947a7021d663aac
+size 3091
