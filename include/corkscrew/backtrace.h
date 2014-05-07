2ed9e87b7657e8ed3423c09c5c7d020f76bc0562
Elliott Hughes <enh@google.com>
2014-05-07 09:38:04 -0700

    Remove the dead corkscrew header files too.
    Change-Id: I0dd5cf3966994db19d98f60805211ed39099465a

--- include/corkscrew/backtrace.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3e5fd8a20818e9f47253df5b6077c52aca9e0793405fbc1fe47b5eaa1fc691d8
-size 4269
--- include/corkscrew/map_info.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fcf8171ba74d014bad711686f0fc6ffd9a0839aeb270a70c4b41d05059cb5bf5
-size 2327
