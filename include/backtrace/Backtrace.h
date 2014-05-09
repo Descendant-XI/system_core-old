0429d1988410be9779d2c0550d2b1bd2455b1114
Christopher Ferris <cferris@google.com>
2014-05-09 13:19:34 -0700

    Fix ucontext_t forward declare for mac.
    (cherry picked from commit afa9c9cd725c081e7286f09937a41ae9c28853d6)
    
    Change-Id: I6dacdfacfdff0c3ce5166207cfd60adb347ee347

--- include/backtrace/Backtrace.h
+++ include/backtrace/Backtrace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efc27974d467c1b8c45220e2396df5e0efd28d5e891fac2d9a7dfa8fc873f5ae
-size 4209
+oid sha256:58c231136fb8ee969f97e769a05123db2a75efb48c80effa72d2211a8e799c77
+size 4309
