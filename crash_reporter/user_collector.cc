a8b297baf37b7bc180942f1bef4bd8515b1e0fe5
Thiemo Nagel <tnagel@chromium.org>
2014-05-09 14:48:45 +0200

    UserCollector: only create meta file if payload write succeeds
    This avoids creating meta files that are referencing a non-existant payload
    which can block crash sending.
    
    BUG=chromium:338977
    TEST=Running unit tests locally did succeed.
    
    Change-Id: Ia7039e679150490571d57a0960e984590905ee52
    Reviewed-on: https://chromium-review.googlesource.com/199060
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594e23fcc3afc5b8127c3cba3724387045355ba46db008502bf13edf65888e8c
-size 25350
+oid sha256:1ccd3e0bb22ea6b9712e98d3804b100154d8ee6d8b81ba5866887e5d67f52c8a
+size 25441
