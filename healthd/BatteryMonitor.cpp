95e95fa25892e7ab6265022f26278832f3b3939e
Todd Poynor <toddpoynor@google.com>
2014-05-08 17:15:45 -0700

    healthd: Add ENERGY_COUNTER property placeholder, and 64-bit properties
    Will add healthd HAL support for ENERGY_COUNTER, and support in some HALs,
    in following CLs.
    
    Change-Id: Ife2eaa30473be3a59183c6ce61fdaa61f32dd01b

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c530b72d8d2c20dc949cfff3c373f39015f177a183fcb7de1f24f0c5ded85736
-size 18127
+oid sha256:f81d7603c57362212c3fde668a6c4560dcb4f8c12bd05872b8dc464eeaace8a8
+size 18193
