4d5c234b459506658b3499bad8db6e5f7d147344
Thiemo Nagel <tnagel@chromium.org>
2014-05-13 19:48:32 +0200

    crash_sender: fix orphan logic, prevent deletion of *.log.tar.xz
    get_base() was broken for files with multiple extensions which leads the orphan
    logic to delete all such files that are older than 1 day.  This patch set fixes
    get_base().
    
    BUG=chromium:338977
    TEST=manual test of code fragment
    TEST=`cbuildbot {arm,amd64,x86}-generic-full` pass
    CQ-DEPEND=CL:199820
    
    Change-Id: I1b712f1a5cfc848e5bc2a86c97e0a3e88ce6250d
    Reviewed-on: https://chromium-review.googlesource.com/199511
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74bfce9c88c36e90a94f07fa7138e306f137f951877ef219b975b2e08cc1f728
-size 19648
+oid sha256:c832bdda9c5753844fd41d42450776c647c3cca2b0eb87d601fd78412dccc50c
+size 19759
