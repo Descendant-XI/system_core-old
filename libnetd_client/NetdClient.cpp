3576c75648270191a5c0456b26f3c9dd0c30e25e
Sreeram Ramachandran <sreeram@google.com>
2014-05-13 15:42:23 -0700

    Mark sockets on accept().
    (cherry picked from commit 1321cfecc6fb0e075796a34ed3c6a30d7ff3bea3)
    
    Change-Id: I5d09be413cf720fbed905f96313b007997ada76c

--- libnetd_client/NetdClient.cpp
+++ libnetd_client/NetdClient.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7db8d1a3bbc43f9ac573e1ada54a453f94b25938754eb1dd1af7d3502912bd32
-size 1371
+oid sha256:3b6adad7bc7e9450614e94052132f7dccd85f2d4418146ba2c6085b7225ddead
+size 2627
