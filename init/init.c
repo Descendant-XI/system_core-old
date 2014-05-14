f355d3313e15174253049fa6f8da3a72e85539bd
Elliott Hughes <enh@google.com>
2014-05-14 16:51:09 -0700

    Clean up some unnecessary system_properties cruft.
    Bug: 14970171
    Change-Id: I6f54c35e265b849be914120f795c9f8e0cec34bb

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a7e1fa2dfbdddf06254c1bce711b2ca68cce787b7f9f990574abf31c0c32d6d
-size 32721
+oid sha256:771d36cdd2211c030e32f006a68da294f570750c9d06cefcf248f13459641711
+size 32685
