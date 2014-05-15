b4f61aebb49afb5b6ce911453381cab7a18db9e5
Luigi Semenzato <semenzato@chromium.org>
2014-05-15 10:48:19 -0700

    metrics library: move event file location from /var/log to /var/run
    The event file, which is used for communicating metric events
    from chrome os to chrome, has been living in the very non-kosher
    location /var/log/metrics/uma-events.  This change duplicates it
    in /var/run/metrics/uma-events.  A later chrome change will pick
    up the metrics from there.
    
    I also opened crbug.com/373833 to remove the duplication once
    Chrome has switched over.
    
    BUG=chromium:361331
    TEST=tested with unit tests and manually
    
    Change-Id: I98b741798aa0481b7987f93f087239ad214b759f
    Reviewed-on: https://chromium-review.googlesource.com/200091
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Daniel Erat <derat@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:244bb358e057e06ab957d5e3d40f90dd23b849e9b17d7e60e1dcbc9275337efa
-size 9753
+oid sha256:34d558c8f0aea2522dc64716239dee950d19f2f6d9698b9732e43bdf7adf805a
+size 10423
--- metrics/metrics_library.h
+++ metrics/metrics_library.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ae08cbbf75d28add39f3af764bd3452f1f96034ec8269e6fa6b90a0f8cda37f
-size 6782
+oid sha256:8c592f10ce83234ee44f9ae86948a89d9123c98545d3f5c5c3657788e8656664
+size 7038
