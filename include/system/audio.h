7f715518b4fe55f607e33218081ba5216161d5aa
Andy Hung <hunga@google.com>
2014-05-15 18:35:38 -0700

    Add channel count functions for audio masks
    audio_channel_count_from_in_mask
    audio_channel_count_from_out_mask
    
    Change-Id: Ic519f2c3f85d14acb9526cc8812360fcbd15346d
    Signed-off-by: Andy Hung <hunga@google.com>

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcfca86bdd22c82703c37416c81b9ac3b7ace5ecb39dc4a052a3b8db983fba1e
-size 30416
+oid sha256:6fd5a257753f36a282674f2e063ac73379f493c91104ac456826ffa6dfed3458
+size 30943
