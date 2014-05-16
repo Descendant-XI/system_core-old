5fc79d78e70eb0808284d8f07c2d15c3f6fde679
Colin Cross <ccross@android.com>
2014-05-15 17:49:06 -0700

    healthd: fix warnings
    Fix a few warnings missed when turning on -Werror
    
    Change-Id: I5728664a90d33b95ab3d6ea8823244c59643aedc

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:336ea3d9c646fc2ea8b66a25e60776ef9ccf972b17c309cff548f929b6f6ac94
-size 17997
+oid sha256:4c71f599292e9206e87a95c7a951550a7c8faf022a553dad76ba6ac39c014183
+size 18148
