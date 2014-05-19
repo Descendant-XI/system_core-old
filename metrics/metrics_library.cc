a4e64215c3c04f0dec786db836e0e8301b93ad47
Luigi Semenzato <semenzato@chromium.org>
2014-05-19 08:30:42 -0700

    metrics library: complete switchover from /var/log to /var/run
    This removes the double metrics even file use to facilitate
    the transition.  The corresponding change has been in Chromium
    for 2 days now.
    
    BUG=chromium:373833
    TEST=manually verified
    
    Change-Id: I36ff355e6519b3894c0a4579758ae87bdac6b148
    Reviewed-on: https://chromium-review.googlesource.com/200426
    Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34d558c8f0aea2522dc64716239dee950d19f2f6d9698b9732e43bdf7adf805a
-size 10423
+oid sha256:afd376c7501214036086a106ebdc98e8853ed1fd611e39c80eaf0fd799cbef9d
+size 9753
