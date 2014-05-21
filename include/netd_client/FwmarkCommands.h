13bcb893b25670b55f168e73dd64caa8fa859afa
Sreeram Ramachandran <sreeram@google.com>
2014-05-21 08:55:19 -0700

    Move netd_client into netd.
    (cherry picked from commit f1b21c5c735e9150c8f29bdb52db2f3eb3ffc469)
    
    Change-Id: Ie4b6b303225c93f2448a503d6ea9cebb552cbad5

--- include/netd_client/FwmarkCommands.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:32d1ba572c4889ce79aaa8a6afa323b030a1d85c196d8e983bb2724e494b049a
-size 1110
--- libnetd_client/NetdClient.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:462a836b9195b14d4f44067ba18a99ff52349cb1090aa739f69b97463f60ad7e
-size 2555
