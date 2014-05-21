a6a19b51ecaee28567bcb4efca2e162bd3e0afb6
Todd Poynor <toddpoynor@google.com>
2014-05-21 16:28:13 -0700

    healthd: Set fixed battery level and temperature via properties
    setprop persist.sys.battery.capacity 77
    setprop persist.sys.battery.temperature 123
    
    and reboot to cause a fixed battery level of 77% and temperature of 12.3C
    to be reported to Android.
    
    Typically used on power evaluation boards without batteries connected.
    
    Bug: 14839868
    Change-Id: Ibae5e16429d05891cb0787d74a2fe93b07013699

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62857a8bf6a02741f4e83031ba842e7604ee52d13b562bca202d546f73767400
-size 18359
+oid sha256:0d470f4fcd5355cc4d5c2c35bbaeea8491cd8495e3b8b40bf7af937e26abbf01
+size 18831
