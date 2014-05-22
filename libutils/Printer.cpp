d4dd5345e87be04f645f9b4262416eb77e1c9c56
Elliott Hughes <enh@google.com>
2014-05-22 01:23:29 -0700

    Move libutils from fdprintf to POSIX dprintf.
    Bug: 11156955
    Change-Id: I70a75c07744d9897d76b180e4940b8717297c53f

--- libutils/Printer.cpp
+++ libutils/Printer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7543c77391621fd6ffeeaf708c79b32b2ac06e6f1f54f43887b04524b1e5d647
-size 4060
+oid sha256:34e257d2cb3c5bb042f1e6da8dbba7b4d6dbe2d9172e2bfb80cf27e28b88ba1d
+size 4007
