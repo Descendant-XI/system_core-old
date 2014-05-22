8c8194d41e0ce252917421457a1330fdf89c5bac
Ben Chan <benchan@chromium.org>
2014-05-21 19:39:02 -0700

    libmetrics: remove version 242728
    BUG=chromium:375032
    TEST=Trybot runs on paladin, release and chromiumos-sdk builders.
    
    Change-Id: I661a5646c37f43e92c6666790b11a4cc8451ee05
    Reviewed-on: https://chromium-review.googlesource.com/201004
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>

--- metrics/timer.h
+++ metrics/timer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:370bec97f917493d2c455d8b0b566742f4bac0e6c0d490a8cc3288bb06e3e398
-size 4989
+oid sha256:3f672f27ba4fd39e6add430b7b5f24f7a7d5443d3e91cd967d64f91fd443daee
+size 4930
