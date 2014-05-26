2d6e57e7262327dc2988eac890ad7b150d5cf059
Wing Tseng <Wing_Tseng@pegatroncorp.com>
2014-05-26 13:09:28 +0800

    adb: Add USB vendor ID for Unowhy
    Change-Id: I8bab07734d5b6acb4d8ceaf45b0bab0723f5fe03
    Signed-off-by: Wing Tseng <Wing_Tseng@pegatroncorp.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fdf8c65e68ebe6f68752a46bdb6bcbe158afb66c247651f86f422d37bc460be
-size 10510
+oid sha256:bb7ec9af9e5ce70368809d6458a06e17c9b72ed8b909f1a48d53f5e0c7e88af3
+size 10597
