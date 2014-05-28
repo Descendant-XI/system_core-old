10489f8ed2d917e09524ce8a108cb970eb4251bf
Daniel Rosenberg <drosen@google.com>
2014-05-28 14:10:01 -0700

    Added support for vendor partition and image
    Modified fastboot to flash vendor.img as well. Moved symlink
    for /vendor to occur after mounting partitions. Changed mount
    to also create the mount point.
    
    Change-Id: I78e1ba24e6bb8b4af96a67ee0569af579439e682
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42e34f43a20a6cabca1cad4aa07a11ce4d50e7704a7870fc1cb44547c5057bbd
-size 34910
+oid sha256:0a20e9c7d7c13480d11c59680bae1f082695eb1df54fa71f3381d113e7f32d0f
+size 35033
