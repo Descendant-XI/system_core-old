56c27cdf52c0b0d3577eaaa22e6beb479e887206
Mathieu Chartier <mathieuc@google.com>
2014-06-03 10:24:21 -0700

    Make host ashmem_create_region seed only once.
    Not seeding this each call should help reduce collisions when multiple
    threads are calling ashmem_create_region. Also cleaned up code by
    deleting gotos, and making formatting consistent.
    
    Bug: 15394258
    
    (cherry picked from commit 1104ae8cfc0e5144e7d796cf29ee124d05401cbf)
    
    Change-Id: Iafdaea57b2317e0eb7c40d7b5595c523814fd88c

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b554f71678313f2387b1f2ea114da7bb750637753d6cfd69af3ce6e7a52b0b21
-size 4611
+oid sha256:12096491ca9b4667dedab40789cfbef638658b8fee0beec0ea95bd4fdc5ef567
+size 4664
