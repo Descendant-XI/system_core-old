c692e80e03ef458459f75e7884c3d433a6d8f747
Elliott Hughes <enh@google.com>
2014-06-03 15:12:07 -0700

    Clean up the debuggerd makefile.
    In particular, turn on -Wunused, which is globally off by default
    thanks to build/core/config.mk.
    
    Change-Id: Iba3ee1c0726f43eced972a53959f0e1cd97190c9

--- debuggerd/Android.mk
+++ debuggerd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3342fc00cfaf76026bd4000ac67bd25f4f8321b6648d3ca31254f4fd9f86934
-size 2326
+oid sha256:3eb80043138b759af3f3cf563968ea9e0fe377985139be8797de53f3077f49ab
+size 2375
