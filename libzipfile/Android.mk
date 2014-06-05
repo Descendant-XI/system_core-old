d29d7dcdcc2424b86f6c5150ce76f2818c9494f0
Ian Rogers <irogers@google.com>
2014-06-05 11:42:26 -0700

    Build host with multilib both enabled.
    Change-Id: I58fa91bc097dfba573b6db27e389f5ed6556032f

--- libzipfile/Android.mk
+++ libzipfile/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bce398919888ded9a473f4ef94408c167861607b80bf9dc6e80ea4320c510df3
-size 798
+oid sha256:af6f286a850577f31de100feb3aaddf5655561b33f7501af35a276ad338a8b3f
+size 822
