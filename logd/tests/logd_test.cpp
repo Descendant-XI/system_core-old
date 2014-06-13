7e1f48b27c54d2487a850a2e4c7900f3a4a20273
Mark Salyzyn <salyzyn@google.com>
2014-06-13 10:09:55 -0700

    logd: test: deal with spam filter turned off
    Change-Id: I759c65e869d0c7cd81063fa7993ba3d67ef9cd06

--- logd/tests/logd_test.cpp
+++ logd/tests/logd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb4a3678b53628ae7be3837733180d9c3f7b6924dc9456982cb55fdc08c20508
-size 19735
+oid sha256:c2975fedfbd374105a97f9fb25edddbfbd8dc45f7e126fdb92e1bf543075e827
+size 20562
