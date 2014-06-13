85e7a1d8d9b611c53f2b9f47c161db9d3f59907e
Dan Albert <danalbert@google.com>
2014-06-12 17:53:03 -0700

    Use syscall(2) instead of ioprio_get/ioprio_set.
    Change-Id: Ibfd0bdd3dd0bad9804ec9fd9292f4b68a510afdb

--- libcutils/iosched_policy.c
+++ libcutils/iosched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16132febc96acbf01002f18700e832f55aa56158d66ca85c5a793082fd11ed8f
-size 1781
+oid sha256:04187b63cf1d3d15714b5e85b2082bcd6b53f4479279887b374bef07c7e269c9
+size 1602
