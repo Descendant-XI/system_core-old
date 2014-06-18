ea977078dd695b7ba5b533995d6e3ee31d108fe4
JP Abgrall <jpa@google.com>
2014-06-17 17:01:14 -0700

    F2FS support for fastboot format and fsmgr
    - Enables fsck.f2fs for partitions with type F2FS.
    - Adds support for f2fs as format fs-type.
    - f2fs is optional and support is dlopen()d when needed.
    
    Needs changes in
      system/extras/f2fs_utils
      external/f2fs-tools
    
    Change-Id: I8d141a0d4d14df9fe84d3b131484e9696fcd8870
    Signed-off-by: JP Abgrall <jpa@google.com>

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dd833684157fb57669b65fa013aed5ce4c5acc8ad5a82277dc57a663634b9ee
-size 2232
+oid sha256:3a13bf02caafa4463b89de365752fd54611cd1a8923b7516df96c6f189d56356
+size 2559
