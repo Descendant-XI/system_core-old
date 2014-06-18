7a8cfd4adf5f7f1dc6f0a43546316bd0aed8169f
JP Abgrall <jpa@google.com>
2014-06-18 14:54:37 -0700

    fsmgr: support multiple fs-types/mountpoint
    Previous attempt was broken.
    It would incorrectly be affected by mount failures.
    
    This changes allows an fstab to contain multiple lines for a given
    mount point.
    The lines sharing a mount MUST be after each other.
    
    The 1st matching line is the primary when it comes to mounting
    and look ups for wiping.
    
    Mounting based on a mount_point will attempt each dup in turn
    until one succeeds.
    The reported error will be that of the last failed attempt.
    
    This is to allow quick experimentation between different FSes.
    
    Bug: 15702546
    Change-Id: I378d68ad13eb0098ec1ccb8dcf108b82acbe9ebb
    Signed-off-by: JP Abgrall <jpa@google.com>

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5527678b58402787e210aee9d592d5fd9d67c049a2e00d966c5b77c56edf4099
-size 16722
+oid sha256:73bb7b49640336f709d4a042a828f0ecacb7de9d3bc4dee3082544a263163b6f
+size 18554
