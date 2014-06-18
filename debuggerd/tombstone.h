cc5f7b2a8adf7118e1e98f847175471e72f9d7ea
Brigid Smith <brigidsmith@google.com>
2014-06-18 14:17:57 -0700

    Removed log.quiet and log = NULL cases from debuggerd.
    Now the functionality implemented by these semi-confusing cases has been
    replaced with the same logtype enum behavior that is easier to
    understand, and cases that used log-looking behavior to print to logcat
    (when log = NULL) now use the more transparent ALOGE/ALOGD functions.
    
    Change-Id: I7e38f2d4ca74a828df4d2266b3ea34edd3c6f5bb

--- debuggerd/tombstone.h
+++ debuggerd/tombstone.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0505f597003af73a80e340679105bca5271150132ee9ff02b50736a86ade5f5b
-size 1189
+oid sha256:acaf43ea31cd9e3e598f524508013bcd48bd9eb577fc890bdbc42a2aa70ad3cf
+size 1177
