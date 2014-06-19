5a297018502854425f168f724011ec314ec32546
Brigid Smith <brigidsmith@google.com>
2014-06-19 11:33:38 -0700

    Updated non-ARM archs to reflect logging changes.
    Change-Id: I0af865881c19ea4a57ec1aa9455bc32c08a60d75

--- debuggerd/mips/machine.cpp
+++ debuggerd/mips/machine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0584e25cebccd42b9867f8ddf6f18d4f1ef3bcdd2de368f54f36371069dbf5c
-size 3796
+oid sha256:62a30a70865c7ecacaa017ee69824c91fabe8ea935b3b5ebc537c9927ba3378d
+size 3812
