6a4975d45bf3a7343af560a3ac712f3794425678
Rom Lemarchand <romlem@google.com>
2014-06-20 16:30:54 -0700

    adb: use oom_score_adj instead of oom_adj
    Change-Id: I76c6ce83d7d400c8b84bfece5d30189640cb2505

--- adb/services.c
+++ adb/services.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed85f397cf702d41038ffa5d49b10779a24070992ad3818418895fded175a2cc
-size 19501
+oid sha256:715cd1893a7bf48b6a8860fb309c229fdcc22ae5002b259285cee580c46efd7c
+size 19519
