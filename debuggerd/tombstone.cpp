c6becdbd237eef93b4206411cdba4e26525b9d44
Michael Wright <michaelwr@google.com>
2014-06-20 16:19:58 -0700

    Print ABI in backtraces.
    This lets the stack tool figure out which toolchain to use
    automatically.
    
    Change-Id: Ia235d6904622423fe6fbc7b0315a1861e52a41ad

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42ed62c5311259a3a1937a5a04c2bf180794a8620bc36b8a4d3862cd86ee6d1e
-size 24335
+oid sha256:2e5ee78019e35cb8bccba3c5b0bfd14adebd764d2fea86f7c310860132e42085
+size 23996
