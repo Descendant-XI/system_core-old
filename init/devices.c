d5d3c68f8b6fcd2cc77d41ba924d975375e6915c
Ruchi Kandoi <kandoiruchi@google.com>
2014-06-23 11:22:09 -0700

    ueventd: UEVENT_MSG_LEN changed to 2048 from 1048 in compliance with the BUFFER_LEN in the kobject_uevent in kernel.
    Change-Id: Ib98626f20a7c26e7718bbe4d5ec4513a1ae5d9b7

--- init/devices.c
+++ init/devices.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f566204cfd3a4ab80b368364d04a32a7f740663d06aafd474780ac044ba98329
+oid sha256:04cfe9106253d6e8c0b6b83bdc003af1cf77b1f6dc08d47e5b0c086af470e1fa
 size 27678
