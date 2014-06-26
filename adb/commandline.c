3dc823212429d8d7659e260666ee594660e2ecf4
Doug Zongker <dougz@google.com>
2014-06-26 15:35:36 -0700

    add sideload-host mode to adb
    The sideload-host mode turns the host into a server capable of sending
    the device various pieces of the file on request, rather than
    downloading it all in one transfer.  It's used to support sideloading
    OTA packages to devices without the need for them to hold the whole
    package in RAM.
    
    If the connected device doesn't support sideload-host mode, we fall
    back to the older sideload connection.
    
    Change-Id: I5adaedd8243dc3b76414bba0149879ca2bbf35fa

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7785c2276833dd3b2c4e2aa948968212a576829e79d0bda41c2f6ca60e09827
-size 56573
+oid sha256:2d82c110bf384b2aef944929905a01d7be1cfd16c3a038edcfc897de4af218ec
+size 60034
