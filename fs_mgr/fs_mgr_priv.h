0512d4c2836a5d450da6f839efd105f139ab630e
JP Abgrall <jpa@google.com>
2014-06-27 02:20:20 -0700

    fs_mgr: increase tmpfs size to accommodate bigger dex2oat data
    During encryption and decryption, art is pumping out data via
    dex2oat into the /data that is actually a tmpfs.
    It currently needs ~195MB.
    So we increase the tmpfs from 128MB to 256MB.
    
    Bug: 15909689
    Change-Id: I5d518a87d7b4eccf32fcd77f61980646ee8a3d35
    Signed-off-by: JP Abgrall <jpa@google.com>

--- fs_mgr/fs_mgr_priv.h
+++ fs_mgr/fs_mgr_priv.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71c52ece507935359c1cd91f9426467bef49fa9a011c6715784eca70f6d3dafd
+oid sha256:09c5be500266686839163e2f7f7ea7dc4fac87965196b879b0bf003e514a651b
 size 3398
