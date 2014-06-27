5fcdb41abce75bdb2f96eefa60ec6d94b4572fc5
Terry Heo <terryheo@google.com>
2014-06-27 15:25:56 +0900

    audio: add definition for new audio loopback input device
    Bug: 15437484
    Change-Id: I62da0596d405c3bb5e00b461f7d4fd11627ed391

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe9b3e29d8d0610ee4d02370f85ade2fb1dc027b0b8636a7aaa6895be6038e1c
-size 45676
+oid sha256:118de5fe0f39986d92056e02dc010e6f347b6853af5e026162244eebcf244db3
+size 45809
