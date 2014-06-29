48954e22ea254296954267103587b22490a7d04d
Duane Sand <duane.sand@imgtec.com>
2014-06-28 18:55:26 -0700

    [MIPSR6] Skip pixelflinger, memset assembler code on mips32r6
    Temporarily use generic C-coded libpixelflinger & memset on mips32r6.
    
    Change-Id: I629b11ba955eaba323cba1df96c39f75f4d24d62

--- libpixelflinger/Android.mk
+++ libpixelflinger/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a1d686df68a88638227a6dedb79fb9e20c729f3499da85e5f6edff8273b00c8
-size 2201
+oid sha256:f3fc4f716b60415c57bfb92b58b8f231feb08bf5a0829719039f2306af6e4627
+size 2229
--- libpixelflinger/scanline.cpp
+++ libpixelflinger/scanline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f25e5486b732b56bd081afdfc96aee9175711866d9a8149a43195db7b7f71c9
-size 78553
+oid sha256:27bb066ef009a67ed4c616f78fc5e9d1cbda3452d88b43f934da97fa7be7006e
+size 78706
