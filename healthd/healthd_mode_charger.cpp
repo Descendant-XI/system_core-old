9c673c7f988e5816dc6e8c48a15db704e7651e3c
Todd Poynor <toddpoynor@google.com>
2014-06-30 13:15:05 -0700

    charger: leave screen off when entered with very low battery
    Change-Id: I28e3d6adaf4bb59d14eca7103d7b063d54a41c75

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1fa75dae29bd111424939303ab623545d1850b988e6a3dfcf4befa178bf6f7c
-size 19077
+oid sha256:d59ebefb9b7b18fccb980010d63f4e77c73fa1a4565fc861baeb6fa60ce53ae9
+size 19581
