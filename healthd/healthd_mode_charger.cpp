336ed779346452d7545181183f1b5b16ecec3945
Colin Cross <ccross@android.com>
2014-07-01 10:56:05 -0700

    healthd: fix 64-bit build
    int64_t needs to be printed with PRId64
    
    Change-Id: If59b60bac71856e4266d459d159a570961e7340a

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59ebefb9b7b18fccb980010d63f4e77c73fa1a4565fc861baeb6fa60ce53ae9
-size 19581
+oid sha256:b180af25bc955b188182b7b9f513e5c9db54916a9ae0074d6733c48fe690650e
+size 19588
