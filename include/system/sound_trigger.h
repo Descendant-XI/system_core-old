64cb990412d89636d6c2b7c17bf6b8e941441533
Eric Laurent <elaurent@google.com>
2014-07-06 10:44:18 -0700

    sound trigger: add sound model and key phrase IDs.
    Bug: 12378680.
    Change-Id: I93b1916151108ccf6eb625b0078e6ed574720b76

--- include/system/sound_trigger.h
+++ include/system/sound_trigger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5395fe124af8476899787dc88c382a48c5cbeb6dcddc5813be0be18c0aa1e56
-size 8312
+oid sha256:e7195099c4ac79664f9c8910b40bacc6f5ea87ff263fa19fe313241eba0c9569
+size 8446
