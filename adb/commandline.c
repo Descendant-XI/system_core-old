80ecfc5ea7b985a28c2773c4e6f7cc6ff5a370c6
Colin Cross <ccross@android.com>
2014-07-07 14:12:41 -0700

    adb: fix 64-bit build
    strlen returns a size_t, but the * modifier in printf expects an int.
    On arm64 size_t != int.
    
    Change-Id: I11e84a7b62c935162abc0aba910d14e63d11efd3

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c25e81b4eae53cce82c198742cdd0bb3545f0d27d3a55b93f28df8e2dabdd273
-size 60746
+oid sha256:53bd45a6fb8b59506d9cdc1a7bac87acd4e36548c18523ddbb6ff4f1223aa802
+size 60751
