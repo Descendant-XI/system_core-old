57d508100f7c8aee109cff33430109889255ed42
Markus Mayer <code@mmayer.net>
2014-07-08 13:41:42 -0700

    Fix typo in fs.h
    fs.h defined _FH_H_ rather than _FS_H_
    
    Change-Id: I710ef53e88d3601e486fde9957f6d022871d9602
    Signed-off-by: Markus Mayer <code@mmayer.net>

--- fastboot/fs.h
+++ fastboot/fs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a05189c7e839b9bc16cb49b85d5ac170b63135b6091cefe0efd25182ea5b4cc
+oid sha256:9787d8869ac0d5b8ff2c226cbfe7adfa51cabcac1a0e1ac48e9aea8ed7376a50
 size 243
