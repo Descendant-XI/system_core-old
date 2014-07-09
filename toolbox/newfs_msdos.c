d25757a7ebc4bdd02ec660766f69d8ee4b1ab0dc
Daniel Rosenberg <drosen@google.com>
2014-07-08 19:26:02 -0700

    Made formatting for newfs_msdos.c more uniform
    Change-Id: I3d14350c70c05a7e2f713d0065524d78f9a413c6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- toolbox/newfs_msdos.c
+++ toolbox/newfs_msdos.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f197279574aed57d106e869c85762690a0af4af545edf8ea3d107d6b62d431f5
-size 30300
+oid sha256:e76705ee40a7f89ef9be8b9b11ab54a3a1842afb9142eef24bd05f2fb6e028f8
+size 35566
