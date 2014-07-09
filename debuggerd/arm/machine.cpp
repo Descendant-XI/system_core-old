9162f0c4075193a689ac44f59a7550db922bdf5f
Elliott Hughes <enh@google.com>
2014-07-09 12:11:42 -0700

    Integrate vfp-crasher with crasher.
    There's no good reason to separate "fill the integer registers with
    recognizable patterns and crash" from "fill the FP registers with recognizable
    patterns and crash".
    
    Also remove the incorrect use of ARCH_ARM_HAVE_VFP_D32 rather than try to fix
    it.
    
    Change-Id: I3a4a3aca1575de5489314027ae52168997404d79

--- debuggerd/arm/machine.cpp
+++ debuggerd/arm/machine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f1be15d7e1ff679365ec7e671b1003e56193f5a90788d0918c38f9feff39d98
-size 3705
+oid sha256:45543ad9dd6aaf6efc1c0f61d28e8733ada9fb14fba9a793c0647073e9269bc3
+size 3358
