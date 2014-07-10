95c74eada064ece3b0db7898a13b475624d6f38f
Mark Salyzyn <salyzyn@google.com>
2014-07-10 14:07:39 -0700

    logcat: test: logrotate flags
    - test -f -n and -r flags
    - fix issue with parse error for sizes in bytes (no multiplier)
    
    Bug: 16147190
    Change-Id: Iadf2fd903d5e881d02f1d2b491441cc8090ee1c9

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f56113707b64c466e361448e4e10f185d0f6face88f93c7dff771270d347e624
-size 15518
+oid sha256:b47e2832a4ee25bcfed3bbd98a42443d53f86884b59bccafed3860e5811bd9f7
+size 16914
