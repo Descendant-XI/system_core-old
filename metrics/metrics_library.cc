a41cf9a918d218f0e5fcde707ce7fe6257aeb6c6
Bertrand SIMONNET <bsimonnet@chromium.org>
2014-07-10 17:32:40 -0700

    metrics: remove unused constant
    A constant was left during the refactoring
    (https://chromium-review.googlesource.com/#/c/207237/) and it is failing the
    generic ASAN buildbot.
    
    BUG=chromium:374368
    TEST=FEATURES=test emerge-amd64-generic metrics
    
    Change-Id: Ief9f2a064dbeded87e2e5fc67d3659fedac88867
    Reviewed-on: https://chromium-review.googlesource.com/207460
    Tested-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Bertrand Simonnet <bsimonnet@chromium.org>

--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fb10a9de92272dc58c72157683b7c3fffe55f09f3bb09ba283ba51deab35477
-size 6914
+oid sha256:be586b2f57a2215c5b52afc6f55347c9792b2f55e1d2f220b8e43fd55ea7154c
+size 6873
