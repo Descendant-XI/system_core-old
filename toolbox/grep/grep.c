502f814f2da3f8495c50be1be0578a044fa99ba3
Rocky Zhang <Rocky.Zhang@arm.com>
2014-07-11 09:48:12 +0800

    fix duplicated grep usage printing
    Change-Id: I579d2046f96ba8ae9fd0bf8f36e2e2f64da622ea
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- toolbox/grep/grep.c
+++ toolbox/grep/grep.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e8da1c3bf5030dfc1661cd96299d9dbd7829657c6d12933304e2db2a24443a0
-size 18081
+oid sha256:f0964c613d0c84f01745ac2f809d1a665ebb1077d4453760b3d7765ec2e7029c
+size 18046
