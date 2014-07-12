2fb0f57a40864d4a114b884a6ca10012eb0dcc3f
Colin Cross <ccross@android.com>
2014-07-11 17:14:39 -0700

    lmkd: remove libcutils from LOCAL_SHARED_LIBRARIES
    While lmkd does technically use libcutils' socket.h, it only
    uses a static inline.  Remove libcutils from LOCAL_SHARED_LIBRARIES
    so it doesn't end up mlock'd in memory in the next patch.
    
    Bug: 16236289
    Change-Id: I744d69bfff06cb904836f64e1d5f6e530604bff4

--- lmkd/Android.mk
+++ lmkd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63f3f98b1e644f2971e725f1ae06e5a4ab1f59db42a87e429f094e55d5fcebf4
-size 221
+oid sha256:e0e9322d5fa00ddbee34d14557fe87013d6884f480e7c1d36563733781c172db
+size 211
