b302ca370c4b819305d3818faa9bc7abd376dfae
Colin Cross <ccross@android.com>
2014-07-11 17:16:56 -0700

    lmkd: kill a process if zoneinfo can't be read
    Opening /proc/zoneinfo may return -ENOMEM under memory pressure,
    assume something needs to be killed if that happens.
    
    Bug: 16236289
    Change-Id: I25aab09f4c3f2b9e176eaaeca94fd6bd790dad4e

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07e4e45cb7615732951cbe745954f0907f09535830a375a757524c1028a5ebbe
-size 20607
+oid sha256:0273446f28c5daf27bf76252a883a70ff2ee5ca53cb10a0d509a4f7bcfcbde9e
+size 20720
