ae11e15f2df66e9f827c5b96c4b6d2dc95f68b7d
Colin Cross <ccross@android.com>
2014-07-11 17:53:27 -0700

    lmkd: use open instead of fopen
    fopen and fgets require allocations, switch to open/read with
    stack allocated buffers.
    
    Bug: 16236289
    Change-Id: I10586883fe78caf59d309eff7f7989b3e45beb7d

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34ed8ac0b12f48c285a4b21c7aee6aaa09f75f36e8c45dcb0de4917c4f534b8d
-size 18936
+oid sha256:7730a2e25c4faf846b6cb80664d837f7a9402b48014f91a029913cdc8f7dc3a5
+size 19770
