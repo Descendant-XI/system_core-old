8413420ce3f6368d0fcf724f44bc51c91ea388dd
Elliott Hughes <enh@google.com>
2014-07-11 20:57:03 -0700

    Use <sys/prctl.h> instead of <linux/prctl.h>.
    Change-Id: I708da00e36a9da8be9184c451453774be6b78c06

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fd53481fa46077dfbfc695f13f66cb868d67cec9b7f776db753120a640a6690
-size 9093
+oid sha256:04e2c7039dd42973a46ad73a63b7658edba460fef3f07649b1d7a7137cd89fdd
+size 8985
