c62e3e431ca161769a2647f46c32fe2da85ab1d8
Christopher Tate <ctate@google.com>
2014-07-14 16:45:13 -0700

    Fix the build in clang
    "fprintf(stderr, buf)" is a warning in some compilers, and we're
    building with all warnings promoted to error.
    
    Change-Id: Ie3c6ddcd74cf4dda40cf7b742df955dde1d1a5ff

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30f110462bd8c8c4ff19203afee5d706ef522fd02069f46647534cdf05fc9288
-size 63361
+oid sha256:2b446a6a78dcff3884b6aa1c08bbf343b3e260a73c12c840a30e1867e833b3c0
+size 63353
