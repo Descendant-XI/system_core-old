f8936d4ede9e5f46e645c95b3656e1fa291da082
Dmitriy Ivanov <dimitry@google.com>
2014-07-15 19:33:00 -0700

    Add close_file flag to OpenArchiveFd()
     * We should be able to keep fd alive after CloseArchive()
    
    Change-Id: I1aa2c039bb2a590ae72f256acc9ba5401c2c59b1

--- libziparchive/zip_archive_test.cc
+++ libziparchive/zip_archive_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:498b0abb9b2d252750558df6aea1e6cfb3f812f0d2849a96963eb8ec7a0a67bf
-size 10379
+oid sha256:46b2d6bad718feec48ced555cc172a24212ba9452fecb9ce8cec940192ebd584
+size 11051
