12630e280c6bb90ffcc6a114fee25f4fd2ab422f
Brigid Smith <brigidsmith@google.com>
2014-07-17 14:52:33 -0700

    Added default constructor for log_t.
    Bug: 16357258
    Change-Id: I39169b35653d2b717fadbcff75978db01cc375ef

--- debuggerd/utility.cpp
+++ debuggerd/utility.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea3d381197b428d8c329e319de6156c2ec9dd2b64a0388db6a98754321c1ac4b
-size 6075
+oid sha256:9dfc18da740d02dc855a2c7bd808182a9aaaeab20f593b1d3eeb1ac3eb5ae652
+size 6171
