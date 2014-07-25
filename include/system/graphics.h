d191500b00d172824f5f1cf52fa4f52c591ba25e
Zhijun He <zhijunhe@google.com>
2014-07-25 08:07:48 -0700

    graphics: update raw10 to allow padding between rows
    Change-Id: Ied1f42bb977212d9e5f3b2b2b3caa9c8c2caac5d

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1507ceae78af5ac2de4b48f57f28eb41772924519286928a009139267c6f55b
-size 17917
+oid sha256:d9bba4662d7f4f292a44a12b2d46047cd8c39f18bbe84e8d58b72626441a427a
+size 18196
