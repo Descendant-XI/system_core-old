51b2c421200c066482a9ba1fb49ef1ac6b258bc2
Eric Laurent <elaurent@google.com>
2014-07-28 11:33:43 -0700

    audio: add audio device address helper function
    Add helper function to derive set_parameter command
    string from device type and address.
    
    14815883
    
    Change-Id: Icf47218f1809f567ef93bdd99d6308475886b298

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ba0f6685cd19513d9446c9189e34a65c00f15f8219f2f96b73a51eee692ad08
-size 56917
+oid sha256:8a3ed039b3e19ef8ccbbae339478292427f6e10dc4ffe32113ab2ea89f62c327
+size 57530
