e0d6376364e9a0878e501b9d383e8a6e1ba276e7
Hans Boehm <hboehm@google.com>
2014-07-31 15:56:50 -0700

    Remove android_... store_barriers and 64-bit atomics.
    These are no longer used, and we want to strongly discourage future use.
    Keep the 32-bit variants while there are still uses.  All users should move
    to C11 or C++11 atomics.
    
    Change-Id: I122b541cfd29ef4a6c932647f85d0d6a9d802061

--- include/cutils/atomic-arm64.h
+++ include/cutils/atomic-arm64.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4b998f1131c6d2011d11276807d57697750ea8dabe3cd96936d982fe0017ee9
-size 6111
+oid sha256:61544f28edf483e2c6f1e9b3de4b7d11605fba22a853888a53ce749bbd133c57
+size 4572
--- include/cutils/atomic-x86.h
+++ include/cutils/atomic-x86.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eeffc0867da3077d0a1b56f5ab6f1ee5f37c29ea307bd991a593cbdfa2b7756
-size 4159
+oid sha256:bd87c2476660cc57187e3ee97639f57bdc957e3d03c2047a2707e87c2d10d9a4
+size 3949
