8c299a07250a04f38103841dd5acdba4782b844b
Dmitriy Ivanov <dimitry@google.com>
2014-07-31 10:21:00 -0700

    Exclude bionic private headers from include path
    (cherry picked from commit 9516139c03e7e48b2c23a5b53dc4309989e54c6b)
    
    Change-Id: I2883e2a13a523e6938c8a3aed93e66af948cdcd8

--- libutils/Android.mk
+++ libutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a095eca21f762760045b091786eef3519d6b03289c4253fba16af7417cee9c7c
-size 3136
+oid sha256:465efe9a12c4c534fe9ba7747fdd4667cbf618c52786ba16e8a44860fd15e159
+size 3128
