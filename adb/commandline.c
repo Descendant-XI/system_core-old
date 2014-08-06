9b26b4309de001fe8fc1eb36a2cf26c8c7a14aff
Alexander Ivchenko <alexander.ivchenko@intel.com>
2014-08-06 14:51:40 +0400

    Fix build of adb with gcc-4.9.
    Without this patch we get:
    
    system/core/adb/commandline.c:1629:16: error: array subscript is above
    array bounds [-Werror=array-bounds]
    
    Change-Id: I494eb8b4d0e8082f8ff57bdd33d8d46c8c481c6f
    Signed-off-by: Alexander Ivchenko <alexander.ivchenko@intel.com>

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4732f23fd8f6fc82b54af9852a616859b8fc1fa0b056b96bca87538d48c732f8
-size 56415
+oid sha256:150fc5866c60bfb03bd31bd6ad0d2a3183902726e4ff116a96bd1fb26e3762be
+size 56525
