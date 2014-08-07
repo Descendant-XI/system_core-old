fb9ecd2b6cdaee1d689f1a2eaa06189d060b2ea9
Mark Salyzyn <salyzyn@google.com>
2014-08-07 08:16:52 -0700

    logd: persistent reader threads
    Bug: 16822776
    Change-Id: I5bea468a41089b51108880044f32e2b2df1278e7

--- logd/LogTimes.cpp
+++ logd/LogTimes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f13625784ab856c77f843667ef22d82c4be827ff1d315e22d9ebf4e8b2ab099
-size 5594
+oid sha256:62844b644dc522abe6a8feea74a71d490295935cb181897b3ad3824c5e3110ee
+size 5665
