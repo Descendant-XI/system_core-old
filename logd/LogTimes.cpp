da2156c23602626a6475545d3b9ce75e2673ff33
Mark Salyzyn <salyzyn@google.com>
2014-08-07 08:16:52 -0700

    logd: persistent reader threads
    (cherry picked from commit c113c5813ebd620e0bc60ece9a32ea14c08ea237)
    
    Bug: 16822776
    Change-Id: I5bea468a41089b51108880044f32e2b2df1278e7

--- logd/LogTimes.cpp
+++ logd/LogTimes.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f13625784ab856c77f843667ef22d82c4be827ff1d315e22d9ebf4e8b2ab099
-size 5594
+oid sha256:45bcd13e4ed6d7ee97c2071860323821a59f20f9e6eefc24f8ae2750fb89c57c
+size 5597
