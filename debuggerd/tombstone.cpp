e93bd63d69a9b61f4676ef635bdf698c9f2fba0c
Dmitriy Ivanov <dimitry@google.com>
2014-08-08 16:30:13 -0700

    Do not suppress maps dump when si_addr<PAGE_SIZE
      Also remove alignment of si_addr.
    
    Change-Id: Ie5445bf8a5baa49e04e37c643c25ebd0f1dc9d22

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01685fa9be0dc061fabe934c9538af536efcc2b40f1b10b264e4b61df8a7aae
-size 24750
+oid sha256:15451bbbef6fffe7760af941a657900a7b60fff09ca3c75229f55732fd19cc8a
+size 24684
