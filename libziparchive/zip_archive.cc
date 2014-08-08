bce55a6eb96b28db7a3ffd35eb4149e4635d710b
Piotr Jastrzebski <haaawk@google.com>
2014-08-08 14:02:17 +0100

    Add EndIteration method to free memory allocated
    in StartIteration. This method should always be called when the
    iteration is over to make sure that we don't leak memory.
    
    Change-Id: I5205c754dfafbab9bb5f06003c3663d2ec4e8a35

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e308d293c372175eab9ee57f132db4b147e5d3419f79c93c2e9f3533dbab4cf1
-size 38760
+oid sha256:d71ab30dea3313c3e6908b23ab52695bebf5405e35602f3c47d999512f2c5196
+size 38843
