cc385ecba59e2b6635340f0a25172fb8bed6be32
Ying Wang <wangying@google.com>
2014-08-14 15:50:13 -0700

    Fix 64-bit host build.
    Bug: 13751317
    Change-Id: Ibf62935b7d12a55ffc57242a26a2581b52796847

--- adb/usb_osx.c
+++ adb/usb_osx.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a8eb897afce2be44552ee86aefbaec45b333500839ae53b61c20c0934df70a9
-size 17392
+oid sha256:143a3c094db721ce9b8a6385fcc1bc990f3105ef5cf3995b705426aa311c283e
+size 17429
