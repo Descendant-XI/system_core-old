548a6a1e3dda93887a2ba8458566bb4908a6c8cd
Piotr Jastrzebski <haaawk@google.com>
2014-08-15 12:53:00 +0100

    Reject zip archives with entry names containing \0.
    There should never be a need of an entry name with \0 character.
    
    Bug: 16162465
    Change-Id: Ia2ec57959280c1bb972c4d59d890c8540c5b9081

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a7d7efe6cb9402474f7b254fa037b511fdc530d011ffda201fa1ec570b6436f
-size 39388
+oid sha256:6078eeec937d1873bb5a98d4c418ef5dab955ca50603c27c98b5f231adce0ac7
+size 39590
