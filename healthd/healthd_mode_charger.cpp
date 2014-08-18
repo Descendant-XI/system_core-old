698ef1bb0aeb550cc21626565f66593c7f8d8286
Todd Poynor <toddpoynor@google.com>
2014-08-18 11:23:11 -0700

    healthd: charger: fix check for charger connected to enter suspend
    Logic for this check was inadvertently reversed when porting this from
    the charger daemon to healthd.  Request suspend if a charegr is connected,
    else stay out of suspend waiting for a charger connect or power off if
    none.
    
    Bug: 17112463
    Change-Id: Iffdb6dbcd0d0a2a614ff8f50bf5f2e22e1ad552f

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b180af25bc955b188182b7b9f513e5c9db54916a9ae0074d6733c48fe690650e
-size 19588
+oid sha256:5bc9cf833213396b0574d5523e3c222aba06149f490f2f8967af6b951ce013bb
+size 19587
