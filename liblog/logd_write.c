83d137aeaef4be285964b0e2ef12c8809b948005
Dan Albert <danalbert@google.com>
2014-08-18 17:29:34 -0700

    Update for public android_set_abort_message().
    Since android_set_abort_message() is quite helpful to the platform, it's
    becoming promoted to a real header with a non-private name.
    
    Bug: 17059126
    Change-Id: I3ed12b66eb07f3a6a08366d5eee147809d9a3ce9

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d048aec16f4b8fa4af2dc04cb61aa22b72ace6fdbf794520e515e2ade702e34
-size 13409
+oid sha256:bae553d1013368c3bd08dcd5d840391a48b469dee8600fc59173f93fab4148a5
+size 13410
