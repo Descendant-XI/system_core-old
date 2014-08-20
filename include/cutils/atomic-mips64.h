d9f6ac24ed0fe293283a275add11e9711c83811c
Elliott Hughes <enh@google.com>
2014-08-20 13:05:55 -0700

    There's no non-SMP LP64.
    Bug: 17108682
    Change-Id: I7e44ab48b5cacf989333882ffe67f39e9b0ce843

--- include/cutils/atomic-mips64.h
+++ include/cutils/atomic-mips64.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8984b87f6198657ec26c040124df1b1328cdf87ee4d7bb4f615444522fe0202a
-size 6515
+oid sha256:2ec67c30373a2d5a37b0b0d7f381a09eb7b65149bd564de2d0e0ed6e02e49b1e
+size 6277
--- include/cutils/atomic-x86_64.h
+++ include/cutils/atomic-x86_64.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44024f27ab012886ccd9e935522eea653e9ac9c19682800a5a9f7a46f0a247cf
-size 6529
+oid sha256:86ef41ca84fbe28a7708a41e48a9430887cb051d17c6ff8c406571588d8e71fc
+size 6291
