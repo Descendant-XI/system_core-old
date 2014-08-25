38e70cbb59b94f08b5363066dd8abc668516116d
Dmitriy Ivanov <dimitry@google.com>
2014-08-25 16:26:29 -0700

    Fix typo: ifndef -> ifneq
    Change-Id: Id71c70b6fcd2659078a19cc652fd02807372f3a0

--- liblog/tests/Android.mk
+++ liblog/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db7ffdc3c465b7cbaa128c5395c304a8dc14efb287df5dc4e26192083f188b17
-size 2667
+oid sha256:b9a40755b714bc4a0dfa9d2179d176e0c8c6f7229bb77e23917d1c01b2a0e250
+size 2666
