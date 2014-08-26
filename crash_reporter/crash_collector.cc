e88dfe189fef42d141c297d61e17637987811bfe
Ben Chan <benchan@chromium.org>
2014-08-26 06:46:38 -0700

    crash-reporter: Replace scoped_ptr<char[]> with vector<char>.
    BUG=None
    TEST=`FEATURES=test emerge-$BOARD platform2`
    
    Change-Id: I78d7766b49151f40f42ef00b8204fd2569b45991
    Reviewed-on: https://chromium-review.googlesource.com/214780
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53a85e27f695f6432ff19ef34bd92da0f64571af74644088d4cb9bc20697be43
-size 19765
+oid sha256:275f1075fb85188959892722ec36bd15ad64cee506055835cac4e5600bce44bd
+size 19740
