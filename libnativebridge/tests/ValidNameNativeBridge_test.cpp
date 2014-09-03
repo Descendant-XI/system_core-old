ca9b6aa63433f87f1290c312ebefdeae57d93b2e
Andreas Gampe <agampe@google.com>
2014-09-02 21:17:03 -0700

    NativeBridge: Refactor for new initialization flow
    Setup becomes Load, have explicit Initialize and Unload.
    
    Change-Id: I5a20de1cb68dd1802937b369b14c50c9c1031c67

--- libnativebridge/tests/ValidNameNativeBridge_test.cpp
+++ libnativebridge/tests/ValidNameNativeBridge_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d33f2a218f5bfcdb6d667372c79149fb237776d314e0651ed66e40504bf11dd5
-size 1122
+oid sha256:28890833403eee7b2e6caeb176058b4c86faecb69fa11f3a776b6011d45d56f9
+size 1198
