37a55b13d71c4f5fac882ad93c4cc411f82472a8
Ben Chan <benchan@chromium.org>
2014-09-05 08:20:59 -0700

    Update to build against libchrome-293518.
    libchrome is updated from revision 271506 to 293518. This CL updates
    platform2 code to build against libchrome-293518 with the following
    changes:
    
    - LOG_ERROR_REPORT is removed
      (https://codereview.chromium.org/331143007)
    
    - StringToLowerASCII is moved to base namespace
      (https://codereview.chromium.org/448853002)
    
    BUG=chromium:411001
    CQ-DEPEND=CL:216584
    CQ-DEPEND=CL:216585
    CQ-DEPEND=CL:216586
    CQ-DEPEND=CL:216511
    CQ-DEPEND=CL:217084
    CQ-DEPEND=CL:217085
    TEST=Trybot run on paladin, release, and chromiumos-sdk builders.
    
    Change-Id: I9fbdad30b3a7c79c1ec4e208664b8befea31a3ec
    Reviewed-on: https://chromium-review.googlesource.com/216589
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>

--- crash_reporter/list_proxies.cc
+++ crash_reporter/list_proxies.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2585fc8d51adad17871e8898facdb0e3b02ce1d9f5b100bcb77f2ee124714f6e
-size 9028
+oid sha256:9d02c1b4f7e7827cd1ffad49bca2df59f62d07bc1fc98476d8757b3dad63840d
+size 9034
