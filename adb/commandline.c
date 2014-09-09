ea2f35f858b197ac2f2a961de50d87afeb9bd359
Jeff Sharkey <jsharkey@android.com>
2014-09-09 12:38:30 -0700

    delete_file() should force removal.
    It's a shell command with a pty, but it's not really interactive,
    so force the removal to avoid giving users dead-end prompts.
    
    Bug: 17339227
    Change-Id: Iaf5d95c49f032066aa741a711a2c45557d93c598

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8055d9cb6da28fa1e493a2490d710a28c6c49b6fb4c7431a4ae891b20c46754a
-size 63493
+oid sha256:08044d638a23e6dd25849d13b3332932010caeedf8122ab858711059ce8ccdcb
+size 63496
