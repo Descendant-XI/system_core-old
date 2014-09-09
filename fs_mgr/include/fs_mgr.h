e1520564a80ee66abbe1ba883fd06d290a02dd1d
Paul Lawrence <paullawrence@google.com>
2014-09-09 10:44:51 -0700

    Preserve errno from fsmgr_do_mount
    Bug: 17358530
    Change-Id: I4cd7403c0b7c4f878d6afa5199f998e6f614adb9

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d71c30c6f4c638907d6cdd395a8cd394a1c898597a8ab7023373b539abeae7aa
-size 2540
+oid sha256:c1c2e7011275035906a354ca34ab63829264bead4ac8872dbb29e20583741edd
+size 2601
