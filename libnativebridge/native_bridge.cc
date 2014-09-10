b36577cbb8c8d8ad1b57053bf1470ae2e778a1c6
jgu21 <jinghui.gu@intel.com>
2014-09-10 06:58:32 -0400

    LibNativeBridge: Add early init & env setup
    Add a method to set up /proc/cpuinfo with enough privileges. Set
    up the environment for an app in InitializeNativeBridge().
    
    Turn on -Wall for libnativebridge.
    
    (cherry picked from commit 962eb40abb68eb60077da158755b6a09c306aa41)
    (cherry picked from commit ab0da5a9a6860046619629b8e6b83692d35dff86)
    (cherry picked from commit 2f71cb24fa16c0388591918f1354d1f8608cc6e5)
    (cherry picked from commit 04054e28e24866d76034236843490829b80df40c)
    (cherry picked from commit 4390a632366ecf89af5f6c0fed39baf9aab2d0f1)
    
    Bug: 17671501
    Change-Id: Id4f4127d82737b5e56a77175e1068ff5cea60f9d

--- libnativebridge/native_bridge.cc
+++ libnativebridge/native_bridge.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61b852d35027d50b94c7d156a2356486ef8b25cf4f7f6e0a46d78f603a8db8d4
-size 7991
+oid sha256:9b4e6476d60ddfe2cf4fef9d3d14276897ff231dc158bb226befde5b272f7e49
+size 15658
