fd646608f9d2fdb066081a38c7e186004315bdc8
Nathan Bullock <nathanbullock@google.com>
2014-09-11 10:29:17 -0400

    metrics: Add more log messages to uploader
    TEST=compiled
    BUG=none
    
    Change-Id: Iee49b104d0b6214e035f70fa108ae7464bd7988f
    Reviewed-on: https://chromium-review.googlesource.com/217338
    Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Tested-by: Nathan Bullock <nathanbullock@google.com>
    Commit-Queue: Nathan Bullock <nathanbullock@google.com>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47f0d0bd137bd9a83868a5d30c9e17cdf15a472509fa903bfd7fce474d4f07f1
-size 40347
+oid sha256:cf9d1ef9cd8f567a378df2a6b370d2d8f47179d8427b375975bdd85f0f62d8a6
+size 40384
--- metrics/uploader/upload_service.cc
+++ metrics/uploader/upload_service.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3d39ed78386c5c473b55e242ba6acebc6ff404d5b90b04fdbebb2e2fed5407e
-size 6307
+oid sha256:6a568acd35eb7b8c9c8f1ca10326e6fd1bd9ae81439a7a80f4758ff04823bd13
+size 6381
