3acd19b7549d66c397054031eb1a52fe6c608de5
Dan Albert <danalbert@google.com>
2014-09-12 09:42:15 -0700

    Remove makefile cruft.
    Don't manually include stlport, and don't use private bionic headers.
    
    Change-Id: I0fc4e8b34ab449e9ef07c26f71e472fca5640590

--- liblog/tests/Android.mk
+++ liblog/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9a40755b714bc4a0dfa9d2179d176e0c8c6f7229bb77e23917d1c01b2a0e250
-size 2666
+oid sha256:77124e7e1a8d164482ea646ff2a9b02bf0cb8a2d282921dbed124bbd6eb88039
+size 2560
