e10839a0ab1ae0b0dda495813ee25f73e83c5984
Dmitriy Ivanov <dimitry@google.com>
2014-09-12 10:39:12 -0700

    Add -std=gnu++11 to test cflags
    (cherry picked from commit 5f68087efe0476f15b0df8c78ac7e31689ec0121)
    
    Change-Id: I6fc0d3673391654aa72e1e6bd98f4a8469c79868

--- liblog/tests/Android.mk
+++ liblog/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db7ffdc3c465b7cbaa128c5395c304a8dc14efb287df5dc4e26192083f188b17
-size 2667
+oid sha256:e9da72283aaa15383249c650b7f97184c3f92bfee3c49b853ca3daf72cea390a
+size 2686
