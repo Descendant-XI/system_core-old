27f7738e1985602dea01b52f745f14752949a847
Daniel Erat <derat@chromium.org>
2014-09-12 18:16:02 -0700

    Revert "crash-reporter: Attach Chrome logs to crash reports."
    I'm reverting this until I get a privacy review.
    
    BUG=chromium:405732
    TEST=none
    
    Change-Id: Ib3af4752533f76c340d9b853be985bfc774104da
    Reviewed-on: https://chromium-review.googlesource.com/218010
    Reviewed-by: Daniel Erat <derat@chromium.org>
    Commit-Queue: Daniel Erat <derat@chromium.org>
    Tested-by: Daniel Erat <derat@chromium.org>

--- crash_reporter/chrome_collector.cc
+++ crash_reporter/chrome_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e09d0dafc32540e7c7218c27642b04446aed2df8b49e1cdb6620fe06356613b0
-size 10086
+oid sha256:5e653ca3321db699f560af5fd76292054a10d0af9200af738f1a359833128e91
+size 9348
