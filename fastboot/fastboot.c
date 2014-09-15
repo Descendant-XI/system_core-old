c906b13b8ef669a2047fd5acac31627cf6038598
Daniel Rosenberg <drosen@google.com>
2014-09-15 13:44:07 -0700

    fastboot: Remove tos from flashall
    bug: 17498561
    Change-Id: I0190c1a5ac635375404799c040e4be523fa29622
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2682ac26bfcace683e6dbaab034e475b781c50c67bf05ac0562498948fd2160d
-size 35693
+oid sha256:70d4cca43d0960763df41ccbb5910998ceea9a3143c40b8ef2a4d4cff2305ccf
+size 35586
