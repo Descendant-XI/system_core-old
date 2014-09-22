40af18a08108770759d57810b7dfa0b7013853b5
Mark Salyzyn <salyzyn@google.com>
2014-09-22 10:58:36 -0700

    logd: LogStatistics leak
    - uid = -1 in subtract operation to match add
    
    (cherry pick from commit f48ea7c8dcfbf2220ececccb1f4fb2f42df9048c)
    
    Bug: 15986335
    Change-Id: I7d0b85b5334c5264fd04309cb78e0c9aec0ad261

--- logd/LogStatistics.cpp
+++ logd/LogStatistics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99931e86bac9196d7739a56045af7ee3349ebce7a173b34957c2e06fc0d69bf6
-size 28775
+oid sha256:8b6344246c7794ce347f66bc0ec8a86849bbca20308066fc393e0369ff0be69c
+size 28851
