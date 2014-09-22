1fac92ae9a1d8252804c78f5fc4f715bb5e27bc2
Ben Chan <benchan@chromium.org>
2014-09-21 18:53:40 -0700

    crash-reporter: Clean up angle brackets in template types.
    C++11 no longer has the angle bracket pitfall in template types.
    
    BUG=None
    TEST=`FEATURES=test emerge-$BOARD crash-reporter`
    
    Change-Id: I25410c458f730d8e0a38237b3711439262a1b774
    Reviewed-on: https://chromium-review.googlesource.com/219194
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>

--- crash_reporter/udev_collector.cc
+++ crash_reporter/udev_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:274184e8875ff7aaf4e69add66d61acccd65337d90c1f8cd0a7bc1ab4af8db52
-size 3565
+oid sha256:7bf2602a94423a62bce7922c372408f26005fb293b4cb5086fc15e0d39b6253d
+size 3563
