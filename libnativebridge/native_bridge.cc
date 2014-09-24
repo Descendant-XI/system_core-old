2328352050b0a37ceb73d4c3e5aa566d3bf93d57
Andreas Gampe <agampe@google.com>
2014-09-24 15:58:23 -0700

    Revert "DO NOT MERGE Libnativebridge: Temporarily change back to late dlopen"
    This reverts commit f00de413ed451f1a90d614ea5dce8b3d685e877c.

--- libnativebridge/native_bridge.cc
+++ libnativebridge/native_bridge.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaec170b76b08b0ad8a3fd5c9bb46fe6fa4959c9cfaab92da9b3b8e80b0e78de
-size 9399
+oid sha256:61b852d35027d50b94c7d156a2356486ef8b25cf4f7f6e0a46d78f603a8db8d4
+size 7991
