e02882c422758b7d01856686ae866dc71586be52
Hans Boehm <hboehm@google.com>
2014-10-04 00:49:06 +0000

    Revert "Rewrite atomic.h to use stdatomic.h."
    This reverts commit cb344d464009337c283431dec4441b70481d1a49.
    
    Change-Id: Ib17cd8d00695062177ce863cedc4ec7148012317

---
+++ include/cutils/atomic-mips.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9f34ebfe3d25cb457423b382759fdd86ed0a11a4f55586943bf80c01914449f
+size 4873
---
+++ include/cutils/atomic-mips64.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d33c5bd573fef5b0727d78602f19ab1ffc54ee871198ee05df1007568f4846b9
+size 4743
