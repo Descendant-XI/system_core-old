096d8d1ccd76d9204591f8bdd59545be017b3fdb
Hans Boehm <hboehm@google.com>
2014-10-05 16:04:54 +0000

    Second attempt to "Rewrite atomic.h to use stdatomic.h."
    This should not be committed until win_sdk and aarch64 builds are
    fixed in the presence of this CL.
    
    This reverts commit 2789faabfa9cdf16902a52a1de3fc4a7ae98efed.
    
    We additionally remove uniprocessor support from the earlier CL,
    thus avoiding a potential compiler code reordering issue.
    
    Change-Id: I7207a5ca2efa907a6f757f172d7090a62b2311fe

--- include/cutils/atomic.h
+++ include/cutils/atomic.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dc2d7e1fba248d4ae0a15f71a298aaa78784819266a011b2c682dbe1b871a47
-size 6238
+oid sha256:611e35ef97f30b9cc06cce86b3675eba988d8121b631c179a3c4b7de67225ba6
+size 9190
