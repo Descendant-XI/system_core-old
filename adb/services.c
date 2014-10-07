ee3237b8b460acc6eaede67319d721c2ad3f26cc
Paul Lawrence <paullawrence@google.com>
2014-10-07 11:31:48 -0700

    Enable verity on userdebug, and add disable-verity to adb
    Bug: 17691572
    
    Change-Id: I58f588f318e7952d06a41325337d946d7c007e31

--- adb/services.c
+++ adb/services.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:460ff1c8c54822146e660f43e5fd1766050a67d7e578f13c498e0bd60dbce670
-size 19520
+oid sha256:eae6fd0a332408ee208bc47ee42fc59d90612dc7eebe25e4db606563b8e0157c
+size 19642
