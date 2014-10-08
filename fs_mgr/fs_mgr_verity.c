0a926858ad53b17f24f296466a3c3d0b8093e2bd
Mark Salyzyn <salyzyn@google.com>
2014-10-08 16:43:47 -0700

    fs_mgr: fix build breakage
    Some compilers are not as smart as other compilers
    
    Change-Id: I9d93c73bdbd2dca37373d8a42a2ed510d0559274

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6222d1693301dd10e1aa0a6db0f7aa3c387151c3a044e4dc7276d31a92d9dc
-size 10774
+oid sha256:41d22ea1107c5272e50bb8cd1dbcb302f3c9f1312da2c1b0eb0db9a0627fc03b
+size 10824
