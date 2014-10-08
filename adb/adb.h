b4c57d896a8b01ac2e6d553493c1ddaa676cd7e1
Nick Kralevich <nnk@google.com>
2014-10-08 23:55:05 +0000

    Revert "Enable verity on userdebug, and add disable-verity to adb"
    Build is broken.
    
    system/core/fs_mgr/fs_mgr_verity.c: In function 'fs_mgr_setup_verity':
    system/core/fs_mgr/fs_mgr_verity.c:103:20: error: 'verity_table_signature' may be used uninitialized in this function [-Werror=maybe-uninitialized]
         if (!RSA_verify(key,
                        ^
    system/core/fs_mgr/fs_mgr_verity.c:374:11: note: 'verity_table_signature' was declared here
         char *verity_table_signature;
               ^
    cc1: all warnings being treated as errors
    make: *** [out/target/product/minnow/obj/STATIC_LIBRARIES/libfs_mgr_intermediates/fs_mgr_verity.o] Error 1
    make: *** Waiting for unfinished jobs....
    
    This reverts commit d4cea0bc16d1ad96cc6d6177232d7b339785460c.
    
    Change-Id: I6862cc79ef9d944a2472b6fb2e46dae514cea8ce

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21dcb15783df7c1336a58697c6131e55f0d37eadfd0f9cdba5607fd722763ac6
-size 12139
+oid sha256:d9aab50f24e30d44279d8d7f192b2d51b1fd980d506e89691826942b4fd3d0f9
+size 12088
--- adb/disable_verity_service.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4d661b59566c99f294bcf552dd97730ead9f384dfd6c9798ce75c494515d1d82
-size 5817
