1309b7c78ee33d266c8dabfda9641db5ee7e820e
Paul Lawrence <paullawrence@google.com>
2014-10-09 09:37:00 -0700

    Fix Nick's nits
    Change-Id: Ide7925e7ad328f0343d444d63ff72f1a26206d4c

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fedb116412d25152dfaea82edb2280aa18017e68243050666e54d48d12db831d
-size 12186
+oid sha256:da89509a93b19d3ffef66f78db52d6a0be1d70dca0b853a5702d205347d7dd79
+size 12182
