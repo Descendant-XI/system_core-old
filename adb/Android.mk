54c1d7fe17d4da3199d252fc773a6f674f5fce22
Paul Lawrence <paullawrence@google.com>
2014-10-09 14:22:49 +0000

    Revert "Revert "Enable verity on userdebug, and add disable-verity to adb""
    This reverts commit 152d2d4234ba89e0c20c4af13e291b6049a7bc33.
    
    Fixed build error, and also fixed memory leak spotted from warning.
    
    Bug: 17691572
    Change-Id: I23b5ba537f7b557432041d4338b38b9be434e981

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed6d1b183dc8ce0cc79023ba21bfb1447f6bf7db6c5b7ad02fdad3eef64bbba8
-size 3761
+oid sha256:7c07fbc4e7468078b74b52cdf40c3749a9f4e3d1dd3aa2b8e2f0ec090d289a85
+size 4017
--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9221afb1a83ece752e60feb0c367412a88f0150c1b832d3cfeca08cf2ce820da
-size 11585
+oid sha256:fedb116412d25152dfaea82edb2280aa18017e68243050666e54d48d12db831d
+size 12186
