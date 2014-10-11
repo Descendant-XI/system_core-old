62013605aaf683cc06c5e47fc796649520e5a343
Badhri Jagan Sridharan <badhri@google.com>
2014-10-10 23:19:06 -0700

    init: Add support "&&" operator in property triggers
    "&&" operator can now be used to test the validity
    of two of more properties.
    
    For example:
    
    on property:test.a=1 && property:test.b=1
        setprop test.c 1
    
    The above stub sets the test.c to 1 only when
    both test.a=1 and test.b=1
    
    Change-Id: I72c19f7aa92231372a416193618ee6c7fd368141
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>

--- init/init.c
+++ init/init.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7276ea5c24c46d375f62bc634daa1ebadd98df304d9af2c57d68db97b84252af
-size 34058
+oid sha256:56fb925f6a496426b7eee552674b708012e6ec43913cb79c9e5f5a11f130e61f
+size 34572
--- init/init_parser.c
+++ init/init_parser.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcb41d81d2474d1cf1618d1c719a8973e23ebd4d683504a532b7424697e4697a
-size 25434
+oid sha256:2181887318fca5bb5f32fec7afe5cce80db30400f1b1e481c238265e87288451
+size 26541
