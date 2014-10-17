de6a3efe8a898c0180eb0d14463d1e8f01bc89e2
Calin Juravle <calin@google.com>
2014-10-17 13:45:39 +0100

    Handle code_cache dir creation in libnativebridge
    Also:
    
    - add kPreInitiliazed state to native bridge with check transition:
    kOpened->kPreInitialized->kInitialized
    - made sure we free the memory for the code_cache_path
    - tidy up some error messages
    - tidy up tests
    - add a dummy native bridge to test with
    
    Bug: 18027433
    Bug: 18097480
    
    Change-Id: I39f74c93580f2e224080dd3df2ffaa9cf9f8cd9c

--- libnativebridge/tests/Android.mk
+++ libnativebridge/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a475eceac2fddcf7001fc52a11ba2ca338120d854317e03f577571b26b239cab
-size 1099
+oid sha256:5d3e63e01621f2a2409828a2fab4a710c8b913994f22ec845ef62a178865f711
+size 1302
---
+++ libnativebridge/tests/CompleteFlow_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdb963d3fd78a58850f84e467d656275d23988cc3debc37109933a8b58388f4b
+size 1385
