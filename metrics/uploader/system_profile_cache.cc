1ca9017ef3539a387eb23d1d9716699199c3cebe
Nathan Bullock <nathanbullock@google.com>
2014-10-17 10:03:52 -0400

    libchromeos: void OsReleaseReader.Load()
    Previously OsReleaseReader returned a bool, which meant that code that
    called load wanted to CHECK the return values and possibly log warning
    messages, etc. This was unecessary since OsReleaseReader.Load never
    returns false.
    
    TEST=unittests
    BUG=none
    
    Change-Id: I7064c6a788897b5d5c687d6c9c5f4e03d4ca21a7
    Reviewed-on: https://chromium-review.googlesource.com/223990
    Reviewed-by: Bertrand Simonnet <bsimonnet@google.com>
    Commit-Queue: Nathan Bullock <nathanbullock@google.com>
    Tested-by: Nathan Bullock <nathanbullock@google.com>

--- metrics/uploader/system_profile_cache.cc
+++ metrics/uploader/system_profile_cache.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2b5e91fb818ac4de52a90a497aa28eee90c642276e748be1f5367d9e2d76fe1
-size 6096
+oid sha256:2b36bf5769daa26915f7208642964763cbc14399a2f056f210ea78209bc5692d
+size 5941
