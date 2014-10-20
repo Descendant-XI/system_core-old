587301e93806d9164ff658d0b079ea7b8326d467
Adam Lesinski <adamlesinski@google.com>
2014-10-20 12:31:30 -0700

    MingW doesn't handle __attribute__((weak)) for Windows PE
    Windows PE format doesn't have the same support for weak symbols
    as ELF does.
    
    The symbol android_log_id_to_name was strangely omitted from the
    resulting Windows executable and the callsite where it was supposed
    to be called was relaced with a different symbol, which caused a crash
    in AAPT on Windows.
    
    This change works due to a careful set of #ifdefs that ensure only one
    definition of android_log_id_to_name is defined, but there are other
    uses of __attribute__((weak)) which should be inspected.
    
    Change-Id: I3b58076e31d9b28c5143773a170e9ffda9fa3815

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bae553d1013368c3bd08dcd5d840391a48b469dee8600fc59173f93fab4148a5
-size 13410
+oid sha256:edca045d447053971795424ded3c9204747daf7a28695a2ba4cfebb68fdb93fe
+size 13370
