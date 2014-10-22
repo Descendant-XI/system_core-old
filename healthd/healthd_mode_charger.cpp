9a0844ba5a98f096a57631c7f50557e2e1b61183
Ruchi Kandoi <kandoiruchi@google.com>
2014-10-22 14:16:35 -0700

    Charger: Improve screen on/off scenarios
    In charger-mode, power off/on screen as required, for efficient
    charging.
    Turn on screen on power-key press rather than on key-release.
    On charge-cable removal, kick animation to show the correct battery
    level.
    
    Change-Id: I74eaf8681466c44dbd76a820adf154053e0fc43a
    Signed-off-by: Pavan Kumar S <pavan.kumar.s@intel.com>
    Signed-off-by: Jenny TC <jenny.tc@intel.com>
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9984b02359f79ab48fb7f57c96cfdb3df92097ae800547f8fafa85e67c21d5bc
-size 19968
+oid sha256:8cb36008bea4506715237907560faa11fbae2a9041525f488a6541c54a436653
+size 20363
