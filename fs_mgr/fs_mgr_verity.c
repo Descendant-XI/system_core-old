457ad2f6964dea8777275e6281885441ba1377b3
Sami Tolvanen <samitolvanen@google.com>
2014-10-24 15:41:59 -0700

    Fix verity on system partitions larger than 2G
    If the system partition is larger than 2G, the device fails to read
    verity metadata, because fseek accepts only a 32-bit signed offset.
    Switch from fseek to lseek64 to allow seeking using a 64-bit offset,
    which solves the problem. At the same time, move away from stdio in
    the function.
    
    Bug: 17705619
    Change-Id: I226320498dcb750ec6cde84411c7fe0774c9cab7

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da89509a93b19d3ffef66f78db52d6a0be1d70dca0b853a5702d205347d7dd79
-size 12182
+oid sha256:44b33e04f8a6b885c56005f1564657a6811f81737887c225b785bfb7fee905b9
+size 12674
