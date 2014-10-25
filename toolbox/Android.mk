1e97af96080aecf610704bd0f8f3043196d90bda
Iliyan Malchev <malchev@google.com>
2014-10-25 11:57:35 -0700

    toolbox: add prlimit command
    adb shell prlimit <pid> <cur> <max>
    
    prlimit uses prlimit internally to allow you to set the limits on an arbitrary
    process, if you have the privileges to.
    
    Change-Id: Ie9a5dc510c7c695debd8f948361d4c51befa5604
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48c5c6435578624457bb8b06744093e39a941cff72a2f7c4886dfa70e7b1f73f
-size 6992
+oid sha256:d1bd1dcbc8b17edd25f20e30b08f6e33d62f46f77d5cdb7f63f9ddc72e657f0d
+size 7006
