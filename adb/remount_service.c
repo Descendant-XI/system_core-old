f5cb6cec02a6d4aee1eef064e81340b1316e5de6
Paul Lawrence <paullawrence@google.com>
2014-10-27 10:37:59 -0700

    adb warns on remount when verity is enabled
    (cherry-pick of f643beced1d154726dca7115014d2d1fdfcca993.)
    
    Bug: 18119147
    Change-Id: I75e5edf83fa01dbf2495e24df4597dce41f13654

--- adb/remount_service.c
+++ adb/remount_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2241833c4fe8e9b997edc8d3e44cddb1a6c472733122cfcefa31d5b3c12f993b
-size 3236
+oid sha256:9c28bc0550c13a887ce3fbbd87e207913e4d86f82ca2c9504df21765107e63c8
+size 4404
