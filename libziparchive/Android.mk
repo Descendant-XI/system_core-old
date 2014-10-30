c6194fb5a0082c72d88c23be31eb8269f345256c
Ian Rogers <irogers@google.com>
2014-10-30 09:53:38 -0700

    Add shared library version of libziparchive.
    Change-Id: I9abded3bbff0e7976b1849c73e6978dd10ca84cd

--- libziparchive/Android.mk
+++ libziparchive/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc3c90d2409fbd07aaab2b7f8f642ed57cf2b9db4691dc5f18cbe77e49fe59e8
-size 2033
+oid sha256:4d52f2b1782691a557e7e87c5e373a50583788be3805f5911423add67080c258
+size 2395
