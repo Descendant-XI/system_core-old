d060b9d7929a68d05f5660d9e69047e897dd7bb2
Nick Kralevich <nnk@google.com>
2014-11-02 06:13:53 -0800

    maybe fix mac build
      system/core/libnativebridge/tests/CodeCacheExists_test.cpp:30:15: error:  use of undeclared identifier 'ENOENT'
          ASSERT_EQ(ENOENT, errno);
    
    Change-Id: I188074b878a7ec3955a9f4b5dc7d1bfc56e2ccbc

--- libnativebridge/tests/CodeCacheCreate_test.cpp
+++ libnativebridge/tests/CodeCacheCreate_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:518fd503ee96390ad3a93e6d2504360f113eb8e204d25e1b20a23396f4731082
-size 1532
+oid sha256:5f9678fa6069ea6025dcfbaa81cef59e704d9e42b2699fcffcac70c6a35af380
+size 1551
