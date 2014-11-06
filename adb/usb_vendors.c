32f56c19d8ad6edf16088d75a564509fa324a648
Christopher Ferris <cferris@google.com>
2014-11-06 14:34:24 -0800

    Fix adb/fastboot windows compilations.
    Many of the windows files where not including stdlib.h even though they
    are using malloc/free calls.
    
    Change-Id: If6959df9909d9d9928e9f4a2a96018166361cf3c

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63beeb743080d9bc49042c8c2f025a1ee8500dbb9f7ee854af37799d36de2af3
-size 11316
+oid sha256:aa60cb7f2cc8fe836f4c8eb74df4b5a15569f249addd4c057d871ffe9872e219
+size 11377
