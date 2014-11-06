f0e70f20511be76d8ec5ecccc874693b8742ebfc
Channagoud Kadabi <ckadabi@codeaurora.org>
2014-11-05 16:59:27 -0800

    system: core: fastboot: Increase USB protocol buffer size to 1024
    With USB3.0 SS support the max packet size supported by USB protocol
    is 1024 bytes. When connected to a USB device in SuperSpeed, if only
    512 bytes are sent at a time the device would interpret each transfer
    as a short packet and slows down overall throughput. To accommodate
    for SuperSpeed USB bulk endpoints, increase the buffer size to 1024.
    
    Change-Id: I4f447fe38045bc0008e2d6dd3f36c2d6819d6c15

--- fastboot/protocol.c
+++ fastboot/protocol.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d449459653df2e263d584ab2a1d22acbdd5022ea1002841c7dfbbf00aadb73c
-size 7569
+oid sha256:60df73c14ea4835f4fda84a43a72c5d89ad0db284604f820e46f385e5f362800
+size 7570
