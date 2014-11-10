2df00ed745f9d8e2a13203531ee55a5f57cf297b
Elliott Hughes <enh@google.com>
2014-11-10 11:03:38 -0800

    bionic's struct stat is now POSIX-compliant.
    Right now we still have the kernel names, but they're only there by
    "virtue" of macro namespace pollution, so I'd like to get rid of them.
    
    Bug: 18298106
    Change-Id: Ifed0b3a9238c79a99d8a2b62e0f5897c50a725d1

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0de29767e581396b20fd6165918f6b1b5b1a58f4fdbb054e33e9e4b12f9faeba
-size 62860
+oid sha256:482e6c3a550316610e33054fb46ab4e64ca4045b27ab5cfb2e2aa7d1938265ab
+size 62884
