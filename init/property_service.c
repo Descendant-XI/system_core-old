cba74569f764da3e316b892e01a4d27c3c38ad05
Elliott Hughes <enh@google.com>
2014-11-10 15:00:00 -0800

    init isn't built for platforms without dirent::d_type.
    Change-Id: Icd4b431576f2ba619ff4ae6fb871bb0210d3b2f2

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:268561e87908a3d81b0b86ae44944e986454f7846c1dabbbf23edf63a4593794
-size 15644
+oid sha256:3631e5b2d30a9fe57286c1938d585b0415a38b1e9cfe94657cfb883fe82de699
+size 15614
