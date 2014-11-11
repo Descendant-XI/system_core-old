4f7be542692afc9c5bacfa70b645a263f0fe5a9a
Daniel Rosenberg <drosen@google.com>
2014-11-10 17:01:33 -0800

    property_service: Added vendor build properties to load_all_props
    Change-Id: I267bd848e1f6e2978024c07e603b25f1f41f5fbf
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- init/property_service.c
+++ init/property_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3631e5b2d30a9fe57286c1938d585b0415a38b1e9cfe94657cfb883fe82de699
-size 15614
+oid sha256:c0d17f5bb518101f5653e6fb8a70fb18c1a4f0b140f395050cffe28ee288f41e
+size 15675
