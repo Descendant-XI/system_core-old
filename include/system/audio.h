9df260599e51f040493ca4ad26cbbefa10e21ebc
Eric Laurent <elaurent@google.com>
2014-11-11 14:03:35 -0800

    audio: new stream types
    Added stream types for internal use by audio policy
    and audio flinger.
    
    Bug: 18067208.
    Change-Id: I90fd4df9ed5e0d8fb8c220d67e95fd0e32a06f14

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:694cd2258a6f19f4c787f1f8f51b9a1bc03fb4ace5d931591c6a04962a0d588c
-size 58411
+oid sha256:fd6839db32c01f2ebcf0789b7380db959ced7525cdd38898638c9eb50a0668fc
+size 58872
