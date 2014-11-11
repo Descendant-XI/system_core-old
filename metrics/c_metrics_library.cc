2c654f07516743dc49aa7e7f0d1a874e8795e6ff
Alex Deymo <deymo@chromium.org>
2014-11-10 19:55:35 -0800

    Include the implemented header first in all projects.
    The Google C++ style guide dictates that foo.cc and foo_unittest.cc
    should include foo.h in the first place, so missing headers in foo.h
    are detected with a compile error of the module implementing them and
    not when another module uses them.
    
    This CL sweeps across all the .cc file in platform2 enforcing this.
    
    BUG=None
    TEST=cbuildbot amd64-generic
    
    Change-Id: I41835835caba13f54c3c844ecf552eb0e47efa9d
    Reviewed-on: https://chromium-review.googlesource.com/228894
    Tested-by: Alex Deymo <deymo@chromium.org>
    Reviewed-by: Alex Deymo <deymo@chromium.org>
    Commit-Queue: Alex Vakulenko <avakulenko@chromium.org>

--- metrics/c_metrics_library.cc
+++ metrics/c_metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3fdb6b2255767f629984b2b2e84c706e33a350c0c4e8acc7ac0dd94770d9775
-size 2622
+oid sha256:366fdb73c1fc6faecc1247fe4e90da0b55602f87e688162e20f459b1d50b202d
+size 2623
