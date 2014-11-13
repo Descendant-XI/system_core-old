d6c22dbc99efacbe7356ddc908c9a2d6b412f831
Yabin Cui <yabinc@google.com>
2014-11-13 10:02:08 -0800

    kill HAVE_LOCALTIME_R
    Bug: 18361583
    Change-Id: Icdc745a5204bba26c6438d16ed38ec791e136163

--- liblog/fake_log_device.c
+++ liblog/fake_log_device.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:934f0137cd5bc8667b3e962cfe2571dfc6d6ca58cfd37ba5c1d9b0afd802f9a6
-size 19439
+oid sha256:61db0f674dc943dffd08970757094b44536338ea6f35fd3ea7981c04437ef296
+size 19421
