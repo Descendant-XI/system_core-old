8f2484cc6b2d1fbddb39033ad62efab4e6dc0649
Andreas Gampe <agampe@google.com>
2014-11-13 15:50:17 -0800

    System/core: Use memmove
    Should use memmove when you expect overlap.
    
    Change-Id: I268a173db40a4be54232958e37aa8a03c2a885ee

--- libutils/String8.cpp
+++ libutils/String8.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bec12ebfca1fbd6252c13b02ea6c0e8b10558ff5f958e14d7319a42d57e97741
-size 15194
+oid sha256:8efe67f8fbcd167ac47c95b98b5c5bea847c4b57a1812a7ca7c62a82bfce530c
+size 15195
