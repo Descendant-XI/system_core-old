405491e4cb645ca012837fccd1e12115d1d6d8bf
Johan Redestig <johan.redestig@sonymobile.com>
2014-11-20 14:26:16 +0100

    notify: dynamically allocate filename buffer
    use asprintf to dynamically allocate filename buffer
    rather than just reserving 512 bytes on the stack to
    avoid potential overflow.
    
    Change-Id: Ieeb9c6c2e5f97a9574f8128d84eba0b8efdb7263

--- toolbox/notify.c
+++ toolbox/notify.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce214e1f7ab663989a743e35bd9f1273833e856a2d0c23feda33032d71d5dee1
-size 3893
+oid sha256:ee3009f5374f136fd0378fcf1648b4a20387c94ad5f87599edb0539c6999ff94
+size 4036
