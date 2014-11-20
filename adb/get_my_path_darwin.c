c024f62a33e635f7b85e442ebcaaf5fb8f63fdeb
Al Sutton <al@funkyandroid.com>
2014-11-20 13:33:57 +0000

    Return path including executable instead of without
    The previous implementation returned the path the executable was in instead
    of returning the path including the executable (i.e. it returned ...bin
    instead of ...bin/executable). This is not what the original methods did
    and caused the process forking of adb to fail.
    
    This patch corrects the implementation.
    
    (cherry picked from commit 26b53bb75e6bdf9aa7820a3343845dae145625cc)
    
    Change-Id: Icad6870bbb0a88f611ca71d90fff30f7b576c367

--- adb/get_my_path_darwin.c
+++ adb/get_my_path_darwin.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:821f9c0153de44f437979caf5953070fdb849461794c9885ecfd1f605a57c917
-size 1058
+oid sha256:fe07d27e3ccc4514497da1bf3ca0bb41f440aac477412664ef7990d665a1c37d
+size 1086
