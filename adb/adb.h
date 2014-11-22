01c0af695e2dd556157b26eb982526a94108db45
Elliott Hughes <enh@google.com>
2014-11-21 23:01:59 -0800

    Kill HAVE_BIG_ENDIAN, HAVE_ENDIAN_H, and HAVE_LITTLE_ENDIAN.
    Change-Id: I9e25ac9d27e95b04fb8bf7a66c619af0139d8b8f

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9aab50f24e30d44279d8d7f192b2d51b1fd980d506e89691826942b4fd3d0f9
-size 12088
+oid sha256:b3d9af17a3f46ce926d6923f65326df7ffe8088a224cfbed1520f9a09f21c063
+size 12053
--- adb/file_sync_service.h
+++ adb/file_sync_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d77d717ec23d4ce633481d33712a909bd5f88e448135a5910ec6dc1309cc7bac
-size 2458
+oid sha256:d63e8c044362a7625da4a32263ca06119908a4a36083c15beadd28a664f25990
+size 2084
