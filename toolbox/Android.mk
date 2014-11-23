5a8324f220dd3a73c6604d604abeaa0bb9d82aca
Elliott Hughes <enh@google.com>
2014-11-22 22:58:42 -0800

    Lose clear, false, and true to toybox.
    Change-Id: I125ea881e61b834b188d9a5411f3b6efe59b9bad

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f2e7a16de00bddc86403f7f78f6b4f7f2bfc087f1e3c5aa85cca5a1e2964375
-size 7691
+oid sha256:d67b1a7339275f7b0afd3041381c4a362872b72e525c382d8e0055a210594dd8
+size 7150
