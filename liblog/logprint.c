d23a9bf2577a9e3c7cb88bf69ebcd6a24b133d95
Andreas Gampe <agampe@google.com>
2014-11-24 10:36:55 -0800

    Liblog: Remove unused function
    For build-system CFLAGS clean-up, remove unused function.
    
    Change-Id: I8c28c4bbbadaea6a5b32483e9e220794967bdd4e

--- liblog/logprint.c
+++ liblog/logprint.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:362e94f0036ccf4ec32ea1e5ed94ce3c95e6eef8e9674a6700f8e4af00469e2a
-size 26844
+oid sha256:6d54db8c7ee32f5e284209863affb53005bec84631b500e98cd23baf6e8ddafd
+size 26678
