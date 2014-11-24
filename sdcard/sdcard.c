9b074ceb7ca1ff4e41eb5a73049abda804bd8557
Daisuke Okitsu <daisuke.x.okitsu@sonyericsson.com>
2014-11-24 09:37:55 +0100

    sdcard: mount sdcard with the noexec option
    Vold mounts the sdcard with noexec, but the fuse deamon
    mounts with exec, so it is still possible to execute
    binaries:
    
      /dev/fuse /storage/sdcard1 fuse rw,nosuid,nodev,relatime,
      user_id=1023,group_id=1023,default_permissions,allow_other 0 0
    
      /dev/block/vold/179:65 /mnt/media_rw/sdcard1 vfat rw,dirsync,
      nosuid,nodev,noexec,relatime,uid=1023,gid=1023,fmask=0007,
      dmask=0007,allow_utime=0020,codepage=cp437,iocharset=iso8859-1,
      shortname=mixed,utf8,errors=remount-ro 0 0
    
    With this change both vold and fuse mounts with noexec.
    
    (cherry picked from commit f777d6694eecf6e61d9859df2090199863050017)
    
    Change-Id: I66cbfc3a3a89a26958f83577f5e7a5e27f99184e

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c76bcbe05d6916edd7994525ce9c9182c04d8fdcf0310798ccbd5deffa34f2f2
-size 61885
+oid sha256:5c9cf5280817c77bba2ef3793f8602df9ce2c008d08f0ea02e75d9cc719e67c0
+size 61897
