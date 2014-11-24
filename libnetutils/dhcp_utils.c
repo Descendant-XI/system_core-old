c3dab2a9c2592cc97938b6bae63b851fa47a3ac5
Andreas Gampe <agampe@google.com>
2014-11-24 10:43:53 -0800

    Libnetutils: Remove unused functions & variables
    For build-system CFLAGS clean-up, remove unused functions and
    variables.
    
    Change-Id: I40a78d5a532dc32b53beb1cfc1b7645bab8add16

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2a1b85f54a9694cfbecae013cfc1d917f5e3bd464a6b620651f5b0fbd5e8aac
-size 13545
+oid sha256:0e0894d145d8f471e2262835f0dcd4a97eed63847ef41afd15ee4fb0dcc0beb8
+size 13355
