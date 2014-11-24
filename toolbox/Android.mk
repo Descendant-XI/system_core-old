9cc5be3ab6794dd63011bc3b99eaf3710afc26e9
Elliott Hughes <enh@google.com>
2014-11-24 14:03:28 -0800

    Remove dead tools.
    These have all been obsolete since we first moved to git!
    
    Change-Id: Ie5bfc1b30000930ef6a604b4313ff7af1b7c4573

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58e1a017469a0f4b89b3a1738e6cd0a2186cd98b72f8793f9ffc86870f84b0d8
-size 7113
+oid sha256:d3a1a63f7ad3352e00a3e36e21a89fec292d97d10b3abbd9e1ae0da59ea4230f
+size 6915
--- toolbox/setkey.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6edc212d93263cd49fa50b2ea15d4207980726353fe8d500df516bd201a3cc32
-size 2310
