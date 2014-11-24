f99c523ff8d81f10f7d359385ae9c0d0ce02256e
Elliott Hughes <enh@google.com>
2014-11-24 11:34:14 -0800

    Lose insmod/lsmod/rmmod to toybox.
    Change-Id: Ia654cdca14a4fda958a406f5e6ccd0919ce88a8f

--- toolbox/lsmod.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:775e54256ffd1249936d39cdee7d2f10c65a8db85bb60f78384da493032cf2e8
-size 193
--- toolbox/rmmod.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:634706e98d0aa01f07a00516f9b3564d34885a7d526769c434607487327faf2f
-size 1059
