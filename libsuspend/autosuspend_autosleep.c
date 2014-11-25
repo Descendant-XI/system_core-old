72fd31d21ac09c56fbb63da1523ef32a9facbaba
Andreas Gampe <agampe@google.com>
2014-11-24 18:53:44 -0800

    Libsuspend: Remove unused variable
    For build-system CFLAGS clean-up, remove unused variable.
    
    Change-Id: I34bd33fd24b02ee58b64029146c253c61651d45f

--- libsuspend/autosuspend_autosleep.c
+++ libsuspend/autosuspend_autosleep.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a309f32e9fa7a972eb65ba68168d4dce7bffe070a3732f05a3ad7ccc77448a7
-size 2466
+oid sha256:c24803f08e9955436a75192783316798946a334a654ee32acb4ff29050c3b769
+size 2453
