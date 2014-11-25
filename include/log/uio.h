9ee60967248c043a916900c5e4a71886e1b11f02
Elliott Hughes <enh@google.com>
2014-11-25 13:27:43 -0800

    Only Windows doesn't have <sys/uio.h>.
    This should probably be in libcutils instead, so code that needs to
    care about Windows can use readv/writev.
    
    Change-Id: I7c2ceec3f742cee0e44f69fd4c88459376bd0e08

--- include/log/uio.h
+++ include/log/uio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74a3493e712a4566a592e0bb638fbd0a114cd433cddfab0fd99c811dc9d0db01
-size 1155
+oid sha256:c8c6265e30f22109fbbf6c3d6be2523e84117926fefc1e7aa191481be539496d
+size 1105
--- liblog/uio.c
+++ liblog/uio.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d00f05ff4bcbd942ed506c90f0bbcf839a4de471e5f3e0eb4536cc1a23eeee
-size 1909
+oid sha256:d59f5f4946d6c4661c692f69ea186a4a2604e1c123dd8bb3f44b1fa545a8118f
+size 1884
