5c04e48d179bb97dd97aceddc20a7086ff203db7
Stephen Hines <srhines@google.com>
2014-11-26 01:06:01 -0800

    Fix incorrect path name check.
    This was caught by clang 3.6's -Wpointer-bool-conversion. The device_path
    field is a char[256], and thus never NULL. The intention for this code was
    to check whether or not there is a named path at all. Checking whether this
    is an empty string matches the original intent.
    
    Change-Id: I5ccedc03167e6a457e472089de26130aff7f96e3

--- fastboot/fastboot.c
+++ fastboot/fastboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70d4cca43d0960763df41ccbb5910998ceea9a3143c40b8ef2a4d4cff2305ccf
-size 35586
+oid sha256:6388e19779a49cd5e0594b304ad13ead22869274e1d406b5df3af471b9e132fa
+size 35598
