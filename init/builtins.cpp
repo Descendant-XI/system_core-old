00dc0cbef22d42100d1053930764b252c8bb79ee
Johan Redestig <johan.redestig@sonymobile.com>
2014-12-05 00:36:41 +0100

    Bump do_write buffer size
    In the case of do_write (write to generic file) there
    is really no relation to the PROP_VALUE_MAX limitation
    of system properties. The current value, 92, is not so
    much for writing to files so bump it a bit.
    
    Ideally there should not be any low hard limit in this
    case. Either expand_props should allocate the output
    buffer or take the output fd and to streaming write.
    Such a change is rather invasive though and at this
    point in time it seems reasonable that one dont want
    more than 256 character strings in the init files.
    
    Change-Id: I846a282ae4e747e6171eef38b302b46287951451

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7daf918d7465bd406073738268dde0c418fcd52b9ee6ddc745656cf9c43b3d0e
-size 21733
+oid sha256:68dfa8ed751beb22ae8e982d6e7e7e14c2015ad34b38503f5588f76d77eb36ee
+size 21722
