0da822cf9aa97e310e5eac9cd20b710933c8ec1c
Andreas Gampe <agampe@google.com>
2014-12-04 18:01:26 -0800

    Toolbox: Disable unused-XXX warnings
    For build-system CFLAGS clean-up, disable warnings locally.
    
    Should be reverted when the code is fixed.
    
    Bug: 18632512
    Change-Id: Ia373784ec36d37545a7e50db1df9f8cc37d7bf30

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2f5d5df2ed470592d95a10ae9eddf96ec82ab9332d8bdae19a1d098a972cc50
-size 5591
+oid sha256:e72a0df441098f969be16df7a97979067c2b20589e5d98f94489132112fd4661
+size 5724
