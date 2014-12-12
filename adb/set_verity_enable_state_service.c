c6c0ecd5b2895a54bd75ee60fd9721a776f89521
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2014-12-12 22:22:37 +0100

    Fix build with clang
    Remove 2 unused functions and 1 unused label in the
    !ALLOW_ADBD_DISABLE_VERITY case
    
    Change-Id: Id6b29f57bcb95e54f5a4014021d47d8ca6e4556e
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- adb/set_verity_enable_state_service.c
+++ adb/set_verity_enable_state_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b28b3770adf2220d89187e85785a9fcea458ab2c997c60d6b1c18f31c1de89dd
-size 6600
+oid sha256:e4cb112a772e81b6fd3a75b76f36fee27edd088f6123766b2283cee3e8cdf1a5
+size 6640
