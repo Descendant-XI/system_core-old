26755610a449cb02beedd03b20ce2134390f8292
Elliott Hughes <enh@google.com>
2014-12-11 18:33:46 -0800

    Lose mknod to toybox.
    Change-Id: Ib81efed6c06a776626a42c25508b15461e495288

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f68e78fd67d4acdbc2d8b7adde15f675acefb6b8544a92d5c066270b50793dc
-size 5433
+oid sha256:0c04615ab9aa54fc2b81507bca14a494882a33c21409de027fcde4dcd9382eb1
+size 5421
