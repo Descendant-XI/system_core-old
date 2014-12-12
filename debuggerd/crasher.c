8bf882c56075a07b22e3b0848f9738355938bdb4
Yabin Cui <yabinc@google.com>
2014-12-11 17:46:33 -0800

    Make crasher smash-stack work.
    Bug: 18721888
    Change-Id: Id0280df7cb2c29832256d3d395e6399fcef9aa2b

--- debuggerd/crasher.c
+++ debuggerd/crasher.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f05f23f9eee96129d78f29a3e0436a194d90cd6f1f4c0564b8d499d7ab50ad7c
-size 6484
+oid sha256:205fc6b5295ddc03d3ed47d589508dcec9a96586a3c73cc5e586516339091940
+size 6776
