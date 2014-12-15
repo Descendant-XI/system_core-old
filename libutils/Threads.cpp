ee943b1feb9a93025eebdf5c9a304b89f159fbcc
Elliott Hughes <enh@google.com>
2014-12-15 12:52:53 -0800

    Fix references to HAVE_PRCTL.
    Change-Id: I8343b398f2ce0d743487fdd086e67763c7dd67f1

--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c77a09cd80cf4f4383e5e9b9068fcfd3475bfee2bc13f544c4c5444e60e1837
-size 25539
+oid sha256:d18fea03e1f93118a9f6721b42c18eec2c682e55f785d9403495223abefe062d
+size 25537
