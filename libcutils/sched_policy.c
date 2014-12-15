6af4a3b2b31f1d3dfce1996d799692515510f960
Elliott Hughes <enh@google.com>
2014-12-14 16:51:36 -0800

    HAVE_ANDROID_OS implies that you have <pthread.h> and <sched.h>.
    Change-Id: Ica7940c00d1d5da9d50711fd138dd459deec9b6d

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab09afa294a37168232228474e75341005444d4da428e969902dd28e1a3ca0a2
-size 9019
+oid sha256:7133ddaec963aa9ea93428521c5293b7a53cc3da42ea034608a138dc64e0ab40
+size 8968
