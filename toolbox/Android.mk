2966500deb031b87d31a544879752d817c6a9741
Elliott Hughes <enh@google.com>
2014-12-16 15:28:03 -0800

    Lose rmdir to toybox.
    Change-Id: I80ecc7c19bf7fc44039235c846727853d78a5d15

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b55db5f6f93b933321e7978ce36d8002aeca106282cdd49e4d2ba0697d47f09
-size 5325
+oid sha256:ef458daf92755d686fd0bbe53d44c9d61b4b85a9cf40111957fee05501817fa5
+size 5063
