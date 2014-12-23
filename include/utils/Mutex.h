b2ba731479ad33846afde0f66d31d19775da3ea5
Jesse Hall <jessehall@google.com>
2014-12-23 14:00:29 -0800

    Mutex: add timedLock() method
    This is not available for host builds because OSX doesn't have
    pthread_mutex_timedlock() or equivalent.
    
    Bug: 18842510
    Change-Id: I072e74ab1a6f770fd245828b39c5f954dda1113b

--- include/utils/Mutex.h
+++ include/utils/Mutex.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0676932ecf7cb9dfc2addffb8af816aae5551d9b2dc4a82304f084cf66385b4b
-size 3790
+oid sha256:9f14976e400f77173f95f057506fe6961caad1b9112eaff87734798be10c7803
+size 4505
