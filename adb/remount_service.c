20454cabfbcd9b34b92056c991a4719f57e02203
Yabin Cui <yabinc@google.com>
2015-01-02 14:02:14 -0800

    Use getmntent when accessing /proc/mounts.
    Bug: 18887435
    Change-Id: I6d7f95bbdb976428d4722bd640745e73c9839160

--- adb/remount_service.c
+++ adb/remount_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63ac375ff17d8f81b63b0d91099653722b673517b50a685e52be39d1b906e5c8
-size 5410
+oid sha256:c0acd6bb1be974c3b6676c73abc86e2d848f36a6f4d331df05be447c325628dd
+size 5003
