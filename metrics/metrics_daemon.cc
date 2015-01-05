d162a5c02831a61678fcd04eca791d146208979d
Steve Fung <stevefung@chromium.org>
2015-01-05 13:48:30 -0800

    metrics: Fix metrics_daemon Meminfo callback interval
    With the switch from the glib run loop to base::MessageLoop, the
    interval for scheduling Meminfo callbacks was incorrectly set to
    milliseconds.  Fix it back to seconds.
    
    BUG=chromium:445573
    TEST=strace shows metrics_daemon sleep for 30 seconds between
         collecting statistics
    
    Change-Id: I4b4b597273d3cf04b9972011dd0a7386ea14233d
    Reviewed-on: https://chromium-review.googlesource.com/238466
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Steve Fung <stevefung@chromium.org>
    Tested-by: Steve Fung <stevefung@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa16fa63690853868fe5757f8db1bea7e2012bc7ff18f6d3bffd7c99b72b205
-size 41790
+oid sha256:c05af7e154ef919a8866313f00928cc3874ae562d29704d28ab24f594a72bfb6
+size 41689
