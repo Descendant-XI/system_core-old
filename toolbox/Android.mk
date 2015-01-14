a229864606c057cde18fcd7a5b360db6d1a489d3
Elliott Hughes <enh@google.com>
2015-01-14 11:05:05 -0800

    Lose ifconfig to toybox.
    Change-Id: Id3fd2898e137d3a70a3dc5ed65689ef5e029c8cd

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:181bc8a1d19deb1a108b57799f64e438fcfc1122586142aecf9ef4dbc816a953
-size 3710
+oid sha256:89874d3e7bd3b8ba1acf6d85e83f9cd47a46212a1242648d77cd705bd9e9b2b8
+size 3695
