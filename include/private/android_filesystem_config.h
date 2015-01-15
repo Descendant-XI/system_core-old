27f526b3c5bd0324ef015369d3721032886ac221
Dmitry Shmidt <dimitrysh@google.com>
2015-01-15 10:35:54 -0800

    android_filesystem: Remove setuid bits from tcpdump
    Bug: 18669116
    
    Change-Id: I25486f2fe0e994f569b788fc9f03ef6d383f54d4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:796a8066a1261cb09778f610f54907416be5f3da5cfca8aa9b763bde983f39dc
-size 13051
+oid sha256:7389795ee3bfdbcf0f2d3843b840b68b74afaa1c68fac93defa8a6834eab7c46
+size 12980
