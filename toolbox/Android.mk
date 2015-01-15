988117e4ede9fe1da4e2003256e8f2a474ee2974
Elliott Hughes <enh@google.com>
2015-01-15 14:59:22 -0800

    Lose cmp to toybox.
    Change-Id: Ib8b078565e4951f7621d21fbe3573bfcf2435879

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dccc972621b9d1b354bab19f26e94eee787ff416a5f53e65bd57126e49e3bd2
-size 3405
+oid sha256:5123a8ac09f4f237a52087ecc062782f1e076c2b73c95f7dea3f5f1f06033ed7
+size 3395
--- toolbox/cmp.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6d317d30e6f151d4f598071117f991a01db2efcfae141567ba5b28e4a3d48b29
-size 2035
