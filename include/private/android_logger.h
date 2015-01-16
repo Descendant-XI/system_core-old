6b01f3ae32c62400f62655d04ab7168b3c8a1423
Mark Salyzyn <salyzyn@google.com>
2015-01-16 13:38:07 -0800

    logd: create private/android_logger.h
    - create a structure to depict the private header
      expected at logd end of socket.
    - utilize this new structure instead of unscalable
      byte stream technique used to unpack in logd.
    
    Change-Id: I2d0e5c3531c279f2dc1fbd74807210ff8d804de0

---
+++ include/private/android_logger.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c131f980a53f8fddaa1fc760d267dfc5683a6443aed3c772d338d6002b8c02b3
+size 1054
--- logd/LogListener.cpp
+++ logd/LogListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1155899d36490608e7b54f52bc36b77806d5af4820819cf213d427e0a03013c5
-size 3603
+oid sha256:61ba43cd8ea41c526a1f546ca4d433bfb4d4aa30102f83ac0ce62eddd3518015
+size 3384
