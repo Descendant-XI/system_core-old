71a3103614fd2dcac57c9c5922abc8285cea8b05
Johan Redestig <johan.redestig@sonymobile.com>
2015-01-24 19:00:39 +0100

    sdcard: Turn on noatime for fuse mounted sdcard
    This provides symmetry with /data and /cache that are
    typically mounted with noatime.
    
    Change-Id: I6fe1bead368b52632424b03b50d4081852824cdb

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34659db1f796ce5b729547ca23823ae5184b9f30bf402ff3cf90359921c107c5
-size 64285
+oid sha256:fe73a5190b8c8f3b4f63c270e0100898b4ef3c41574811bef0e7d2160ac01caa
+size 64310
