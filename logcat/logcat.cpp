f881aea81d0ac01ad37d40c5feb312c3b7ee996a
Mark Salyzyn <salyzyn@google.com>
2015-01-26 13:41:33 -0800

    logcat: Add -D print dividers flag
    Permits a single thread to collect several log buffer and
    to separate them out into named buckets by keying off the
    dividers ---------
    
    Change-Id: I5a92c67383a73ea566cb9b9732d26d88d9c41c66

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:413a18ed055ff7f2b6896c17193e9a596f7d85ba9b9e75a6fef4b462ec8b2af7
-size 27796
+oid sha256:5b76a06d9790d18066a9185d1ce7bbf777d8c62324bbee3d4b9dc416802784bb
+size 28186
