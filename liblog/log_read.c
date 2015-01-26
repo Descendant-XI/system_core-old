0a57218da7cb46433443fe6143fb1abc8f9435c8
Mark Salyzyn <salyzyn@google.com>
2015-01-26 10:46:44 -0800

    liblog: introduce ANDROID_LOG_* flags
    Move away from using POSIX open(2) flags and introduce ANDROID_LOG_* flags to
    replace them. Add security by preventing random mode flags from getting into
    underlying POSIX calls. ANDROID_LOG_* flags overlap POSIX O_* flag definitions.
    
    Change-Id: Ib32bb64c287e8bf150be62242e1ba46bb37839fc

--- liblog/log_read.c
+++ liblog/log_read.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c0f47dccdfd863f17863e3d33ff8dc12a2c7b76de2738191a02e3ea08692b9c
-size 18163
+oid sha256:1b808729e92313cb9a3cc8c60ffdd71f02e7e7e040f6480c205a890d30469020
+size 18223
