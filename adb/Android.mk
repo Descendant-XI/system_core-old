905fd012f4cac398bca4ac29cdcf24d5079ff0ef
Dan Albert <danalbert@google.com>
2015-01-26 17:13:54 -0800

    Fix userdebug build.
    Apparently this code I thought was unused is just unused for eng
    builds...
    
    Bug: 17626262
    Change-Id: I2e5f411a2ead7f23d9f5822935de66c992750a03

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6514360aab4dbcc2a6bd19788844b472046e4104ec4bc5cf122e4bd332a126f2
-size 5060
+oid sha256:bc0ddb685f953ea02f177759c624f3970cb5e1f9974857f33c26f6100929f459
+size 5175
