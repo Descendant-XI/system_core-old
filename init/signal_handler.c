d3d9cb62074a4eccb971c13379c3035ba569de3e
Nick Kralevich <nnk@google.com>
2015-02-02 14:37:22 -0800

    Add O_CLOEXEC to lots of open() calls.
    and other related fixups.
    
    Change-Id: Ia88fb37a07ff6777d00c49800081f5a519c0c78d

--- init/signal_handler.c
+++ init/signal_handler.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a42192a5b9f443e3a2a708c3ed156f6d27a624279dde20a651165a72b27760f3
-size 4924
+oid sha256:2cba09ef695fb824fb5f869966a21d414f528e0d56857cb79e12ed1eac5f7e59
+size 4787
