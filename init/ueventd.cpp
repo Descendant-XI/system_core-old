a5813bcf698d18bcfcc4ed416f44662113c16b25
Elliott Hughes <enh@google.com>
2015-02-04 10:19:50 -0800

    Fix clang "ueventd.cpp:111:1: error: no return statement in function returning non-void".
    Change-Id: I69c9e5534975bffa0fbf98ca4af50400f6b94aa0

--- init/ueventd.cpp
+++ init/ueventd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4acf0609bbfb7e175b642b1cfd06086e69c5da12f6429452ebdde3125067dddd
-size 4908
+oid sha256:445d738fa19d825d2c4d3df597e80b993874cd18ea07e48210a1ba497c4ee55a
+size 4923
