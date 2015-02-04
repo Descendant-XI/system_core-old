daad36e1ed8c2b29f21ae969513ca8498bd33959
Mark Salyzyn <salyzyn@google.com>
2015-02-04 12:28:47 -0800

    liblog: test: ARM64: Enable 32 and 64-bit liblog_benchmark
    - Enable build for 32 and 64 bit benchmark executables
    - Fix some cosmetics issues in logd_write.c
    
    Change-Id: I544446e5116607d7fec89171135f6e1eff6aebd8

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d51603b54c837328cce2e327a28ba63fd4ac7250593e547065d450d80ae54f9
+oid sha256:aff401571744f906423b4ef867ec72dc93f408deb27aedb11b0dcf9905b2bf27
 size 14000
--- liblog/tests/Android.mk
+++ liblog/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb8614c73f989e4beaf7fd5a6d26030c863067e846f2915e1478bb28296c9b8c
-size 2540
+oid sha256:7d515a613cc8b1d578ad6c30aab92a238f049582a8adff604ac340c7dc42e32a
+size 2472
