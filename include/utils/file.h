e05dee4fa26718f87bb951b9a05372606cfa4cca
Elliott Hughes <enh@google.com>
2015-02-04 13:19:13 -0800

    Add a WriteStringToFile overload that cares about permissions.
    Change-Id: I857a80b61768d4e9610bdd149eff2d9d8e48d2c0

--- include/utils/file.h
+++ include/utils/file.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad7f93a8cedf2445a2bec4aee3418c3b836d56ae5574deb07aceca5323a86b5
-size 883
+oid sha256:3f7c2359924c5807d26f5415dd3f901ea8fc2df0e6ff402530ee0ba45d7cdfb3
+size 1044
