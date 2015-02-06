f90b3aeefc44f9fa37b8e10f0756e5e7c7378025
Nick Kralevich <nnk@google.com>
2015-02-05 16:02:42 -0800

    fs_mgr: error check umount calls / add retry logic
    Don't silently ignore umount errors. At a minimum, log them.
    
    Add strerror(errno) to another umount call, to make sure we get
    some actionable data.
    
    check_fs: try 5 times umounting the filesystem. It appears that
    the umount is failing, perhaps because some service is opening
    the file on the device or other error. Try unmounting it multiple
    times in case it's a transient problem and we can recover.
    
    Bug: 19199624
    Bug: 19156134
    Change-Id: I7213eb52d55116fb2419a36494d26d5e159981a7

--- fs_mgr/fs_mgr_priv.h
+++ fs_mgr/fs_mgr_priv.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c8c084e13450111d49f590e009edf3553560b2aca6aa3e961a0f689a846daca
-size 3324
+oid sha256:146918eddcb3e126c8c57170f702761e8fbf74b07484aeba533e2251dd913e65
+size 3372
