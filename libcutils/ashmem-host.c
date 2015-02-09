916c1560ccc3959cd51d6de320a224f24302457e
Elliott Hughes <enh@google.com>
2015-02-09 11:41:07 -0800

    Use mkstemp for host ashmem.
    Bug: 19310167
    Change-Id: Ic4e32b3924a9aa0b0d095d445108cbcf2231cd91

--- libcutils/ashmem-host.c
+++ libcutils/ashmem-host.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:742941b53a27809a7c4165cefbefe2f79fe551ecfe38685ec48beef30082b80c
-size 3652
+oid sha256:94df7cc775a5aecf412aa08763e9086cd06a6a3bce1a8044c842a9ea4aa1674d
+size 2235
