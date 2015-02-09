bc1728b989f2947b093bee147b98991d2d829df6
Mark Salyzyn <salyzyn@google.com>
2015-02-09 08:21:05 -0800

    logd: propagate ::log status
    Add a return value for the ::log() methods, this allows
    us to optimize the wakeup for the readers to only occur
    when the log message is actually placed.
    
    This is for a future where we may dedupe identical log
    messages, filter out log messages, and certainly if we
    filter the messages out with an internal logd check of
    __android_log_is_loggable().
    
    Change-Id: I763b2a7c29502ab7fa0a5d5022c7b60244fcfde4

--- logd/LogAudit.cpp
+++ logd/LogAudit.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7c1730f083f3e60c0216bfe37244d8ec726c6db48671baf9df302c827b25229
-size 6651
+oid sha256:eef3ffcc331485c1d459c3f34e5ce8241bf4b3571edfb490d4d4d4594493d867
+size 6807
