89063bd28534c341dc0923a3cd437e9d3733f2a4
Steve Fung <stevefung@chromium.org>
2015-02-12 23:31:20 -0800

    crash: Fix session manager D-Bus call service name
    The D-Bus call to session manager was using the incorrect service
    name field, causing the crash reporter session lookup to fail and
    fallback to placing the crash in /home/chronos/crash.  Fix the
    D-Bus call so that crashes are put in the right place.
    
    Also, fixing this exposed an error with processing the results.
    The previous D-Bus code treated an empty session response the
    same as a D-Bus connection error, with the new code D-Bus errors
    are handled separately, so also check whether the returned array
    is empty.
    
    BUG=chromium:458330
    TEST=triggered crash with 'chrome://crash' page; crash files are
         placed in /home/chronos/user/crash
    TEST=`test_that -b panther <IP> logging_UserCrash` passes
    
    Change-Id: I77733adc5e4287d9eae83a749818d9cc38398c80
    Reviewed-on: https://chromium-review.googlesource.com/249800
    Reviewed-by: Dan Erat <derat@chromium.org>
    Tested-by: Steve Fung <stevefung@chromium.org>
    Commit-Queue: Steve Fung <stevefung@chromium.org>

--- crash_reporter/crash_collector.cc
+++ crash_reporter/crash_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f380d5e2834d2b8a8297f2afc4f4a54d2637d5b535701ccd07d623146025280
-size 17783
+oid sha256:5dd4126150f1b7efb128066066ee36daa6708262ca69cb4aa38de3d41fed47fe
+size 17812
