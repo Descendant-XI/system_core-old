b1bb8441c16019780ec71b34faf86fe5bfa605f2
Sami Tolvanen <samitolvanen@google.com>
2015-02-16 10:27:21 +0000

    Add fs_mgr support for dm-verity modes
    Add support for dm-verity modes and storing persistent state in
    a location specified by the following properties:
    
      ro.verity.state.location
      ro.verity.state.offset
    
    If these properties do not exist, dm-verity is always loaded in
    EIO mode. If the properties do exist, but the location does not
    have valid state data, dm-verity is loaded in RESTART mode. The
    mode is updated to LOGGING if a dm-verity triggered restart has
    occurred.
    
    Change-Id: Ibb82953594d234f81ad21c40f524190b88e4ac8f

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b12b29b727f731aab7cf68a0d31e81bcdf23123142a7aa175e0c1febe38a30f3
-size 12706
+oid sha256:5205b7bdf7fc6151131aac62af9be40d01ecd063372be9aa7147d57249010bed
+size 20685
