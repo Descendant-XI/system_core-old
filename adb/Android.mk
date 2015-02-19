2e7a1442e28d359c0047f81abc1fc149857493cc
Dan Albert <danalbert@google.com>
2015-02-18 17:20:44 -0800

    Move emulator tracing into its own file.
    adb.c is far too monolithic.
    
    Change-Id: I4a9ee97927e4a96a38ea5859d84efac86bfdfc35

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17f8f910ba9311806fcc58448824715c021ec83687c618ba7b7a7f0fa9e233d6
-size 4577
+oid sha256:8cc8a801c83e64358ef93f8bfdeafd2eaded8b7366abbd105e662531a6576ca1
+size 4619
