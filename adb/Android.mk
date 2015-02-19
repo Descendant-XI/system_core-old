8f2890a0a55f91d30c3f030048a98f67acffe843
Dan Albert <danalbert@google.com>
2015-02-18 18:22:45 -0800

    Move adb_main to its own file.
    Change-Id: If6e98c089b39f73bd8f4a878a82669bfeee367f3

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62348b699b6270e7ffabce9732ea7e8e343ac92ee3b26c9f0f6a23f27a79a4c
-size 4713
+oid sha256:c810215cbc885928e9ac0c0ccd006442d03db907ea780cdd219e56b0849d2e17
+size 4741
--- adb/adb_auth.h
+++ adb/adb_auth.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75c38546f66d522ab714f0baaa73a394f5d42f5cf4d25ce7eb0e484ebf5d7026
-size 2125
+oid sha256:b329f0bed619a064d6e01362beb54e8d3562715f10274496288d897514651370
+size 2151
