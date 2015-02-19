23ce2dcabc292bfc14d61bd6acb4bf454e5f98ca
Dan Albert <danalbert@google.com>
2015-02-18 17:47:33 -0800

    Move the adb auth code into its own file.
    Change-Id: I84cf0bd7777f0147119e7c6afc4096c2e93156a2

--- adb/adb.c
+++ adb/adb.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38b988a235f4111bbff5eec879a8115c49712f9f19c3ee73e1d3e78f77a7729d
-size 48986
+oid sha256:c46f34742dd5ea870e439987d2a2142d58d85d4eea32f9300f712700e90643af
+size 47552
