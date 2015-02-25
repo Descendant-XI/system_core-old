2924ac00c57f449c2830d5a3ff21ffa99e6e6398
Dan Albert <danalbert@google.com>
2015-02-24 21:26:58 -0800

    Test readx/writex (now renamed).
    Renamed readx/writex to ReadFdExactly/WriteFdExactly respectively.
    These read/write a full fixed-size buffer. If the whole buffer cannot
    be read/written, these functions return an error.
    
    Rename write_string to WriteStringFully.
    
    Move the TEMP_FAILURE_RETRY definition in sysdeps.h out of the
    !Windows section. It seems Windows won't actually interrupt a call,
    but it's easier to just define it than to #ifdef each call.
    
    Change-Id: Ia8ddffa2a52764a2f9a281c96c937660e002b9b9

--- adb/commandline.c
+++ adb/commandline.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d097d57b3f773473e9309d83f107aace4c0ddf54fdea9d586745ce3c15e4619c
-size 64587
+oid sha256:19aa323cebee9c9ff92f126566458359916987e5c6d1e9dd9e50a72c583a3cd7
+size 64620
--- adb/transport.h
+++ adb/transport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:440cc6567f1987335edd7a75f91bce246ba09d61fdfdb35676592dafdc3399cf
-size 2703
+oid sha256:5a82a931e7657001b1d92fa67466478e6052f182f30141deef5edf5f18955395
+size 2560
