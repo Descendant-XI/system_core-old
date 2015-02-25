7b987767de9979a1b9d866ec2bdb05ef4bfeef8d
Dan Albert <danalbert@google.com>
2015-02-24 22:38:21 -0800

    Remove _(GNU|XOPEN)_SOURCE makefile cruft.
    None of the functions that require these are used.
    
    Change-Id: I10cffab127795b94340131f8737f7924b9138f9e

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c86974a5f645bfcc88cad099e4d7cf6ca6251b7cb3be285cf25808b536bf7f25
-size 5324
+oid sha256:f39d33908f65ee8c4581bf2fa972ac84f84b35cd86d33ad79cc3ed4241c1eaa0
+size 5257
