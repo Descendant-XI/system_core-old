ae138f37c5ca0bc39f0e38daedcd944457b98674
Mark Salyzyn <salyzyn@google.com>
2015-02-26 14:33:35 -0800

    logcat: do not stop on unexpected log ID
    We are changing the log read API to allow event notification
    regarding logging system data loss. We would like these out
    of band events to be reported.
    
    Change-Id: I9e802113604d8cc0fc9adff0d1e014bbc40914fe

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fa14a476ef8399ae811259b2920329dbc7cadc6579180465c16a8b29cf64186
-size 28351
+oid sha256:1056c672b243d3541af6047b919e8f30ef776c2b4dae6e1808f6fe5e137f248e
+size 28368
