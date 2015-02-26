a112809d411e45d693d1b8a74cc9426992ff9a88
Mark Salyzyn <salyzyn@google.com>
2015-02-26 11:21:47 -0800

    liblog: Remove paranoia regarding logd
    We are changing the log read API to allow event notification
    regarding logging system data loss. We would like these out
    of band events to be reported.
    
    Change-Id: I2bcd0ec7499b8139956613d72d7f62aff44b92ec

--- liblog/log_read.c
+++ liblog/log_read.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95f88e1d633d66de582ef651f5cabdabe449e8ef2cba58e0767bbe3e43d0a528
-size 23419
+oid sha256:36ec28617968887e2a01604da71543497db9b83b18a7c4d02a27907dc3d1cf14
+size 23217
