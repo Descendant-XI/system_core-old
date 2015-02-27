08c3862b2b8a55e9f5437ecaba978236ba7af564
Mark Salyzyn <salyzyn@google.com>
2015-02-27 13:41:34 -0800

    logcat: remove dead label code
    Change-Id: I12d6aa4ebf48f06faf9435b4ffedfd309629cf3b

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1056c672b243d3541af6047b919e8f30ef776c2b4dae6e1808f6fe5e137f248e
-size 28368
+oid sha256:cd872437d246906f0567a12b43bce15cce4a05db74447dcbcff4b52749d6a6a2
+size 27973
