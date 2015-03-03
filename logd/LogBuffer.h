31ab7f8216ef57964b679d46628c49aee1b1822b
Mark Salyzyn <salyzyn@google.com>
2015-03-03 11:05:06 -0800

    Revert "logd: Add minimum time bucket statistics"
    This forward port reverts
        commit e457b74ce6ee6d799812dc2ec5e4b8b18bcd3e91
    
    No longer as necessary once we add
        liblog: Instrument logging of logd write drops
    Although this provided an indication of how close statistically we
    were to overloading logd it is simpler to understand fails thus to
    hunt and peck a corrected value for /proc/sys/net/unix/max_dgram_qlen
    
    Change-Id: I2b30e0fc30625a48fd11a12c2d2cc6a41f26226f

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edb0791bd9d37f54e7757d78ba593585dc4d96d0f136ba6d92e23770bbae08f4
-size 2653
+oid sha256:f98d29b5a9f605d77f8f2b437dc210f9cc22d54273aadb9c2f0b56c1d9344a47
+size 2498
