c8f258d6dc0d63564ad92bbcf47a928a15e24ad9
Dan Albert <danalbert@google.com>
2015-03-03 11:47:28 -0800

    Make the root/unroot test more robust.
    * Check the current adb user to choose the order of root/unroot.
    * Re-root the device when finished.
    
    Change-Id: I47a14b89e2c405bd63722e4d2043fcc629fb5e58

--- adb/tests/test_adb.py
+++ adb/tests/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a45d4e66354d24d14e304d415b70a0c29d6b5a535dc6b1da2ecc9799bfa3205c
-size 12661
+oid sha256:9ac53a2d8a1cc83295b4962e230ae8ab6ce117a7eb12a9df3d062efa547e3056
+size 13213
