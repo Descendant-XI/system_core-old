e3191cb6406410e8feb279bb45847eeebed35eab
Erik Kline <ek@google.com>
2015-03-04 17:13:10 +0900

    Make sure errmsg starts out null-terminated.
    Also: clean up some stray trailing whitespace.
    
    Change-Id: I12471b6059971c9c25d7ff6475bfad43cbee44f6

--- libnetutils/dhcp_utils.c
+++ libnetutils/dhcp_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c909ad87dc2922ea5ce773d5ea42c1af364ac77944e8b8b5eb874ad501b38082
-size 13146
+oid sha256:13b911e80fc7669b7f17e52d5fbd9aa00116f01fa98b60ea8d68e1cc33b19fe7
+size 13139
