ce426788f1377a91370fd0fb94d52362501ac05f
Mark Salyzyn <salyzyn@google.com>
2015-03-03 16:21:27 -0800

    logd: use <endian.h>
    Change-Id: Iba843c054ea4fbe1a26c7821b5613fdb5e8001a1

--- logd/LogAudit.cpp
+++ logd/LogAudit.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c13ff1fec4532392e26d22e4576bccc09d670bbbbd94b7a287e27dc09553979a
-size 6639
+oid sha256:d4649801ff0a369672cf4df89bd14f0cb60c61baa6613de080145527e499b114
+size 6560
