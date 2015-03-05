06adbecc33bfd21942e4135728e1a3bfc3ef8e9e
Greg Hackmann <ghackmann@google.com>
2015-03-04 16:45:49 -0800

    libadf: enable -Werror
    Bug: 19606869
    
    Change-Id: If2ea55732e71e8288641b6b34d229b367fd3fb3b
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- adf/libadf/tests/Android.mk
+++ adf/libadf/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf0d011848994b9c78792eb23b227f99567e7251f11fc6f98dcda5f6a843748e
-size 776
+oid sha256:c2b8ebbb49eafeca5dc680c2ea28e45b05ec7a849dc43dd58acdb0326527f786
+size 800
