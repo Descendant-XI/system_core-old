9581535dc5723bb40c4c6a28ea38d73f8f0f41a6
Mark Salyzyn <salyzyn@google.com>
2015-03-05 09:56:27 -0800

    liblog: build cleanup
    - Drop CYGWIN build checking
    - Hard code the "liblog" event tag
    - Drop use of internal WITH_MINGW
    
    Change-Id: I7b63bd7fa4471f340f356b477a0e5e25fe83a851

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d30850f173ce05e9729587b7b8749b9b37dd49f1c008dfd5a2e719c57f9a4f2
-size 2729
+oid sha256:1ea8f15e137c19c8ec26896a4e9a16e703bfa34ca5da957299c25a10f08283c4
+size 2672
