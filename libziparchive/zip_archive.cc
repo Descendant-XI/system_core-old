e5ad629f1464a555eb47c68717e2c7e2784aa148
Dmitriy Ivanov <dimitry@google.com>
2015-03-06 13:27:59 -0800

    Reapply -Wold-style-cast
     Added pragmas to disable checks for the method
     using libz.h macros.
    
    Change-Id: I25aa1adf09f3ecff61d8fea6306b24b37990ab46

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cefebe0d8415aae62c2928728d840c2edc40a21863ce4756d6210b9f7553117
-size 39258
+oid sha256:6e9c951c5ba0e37045b71494e9b37a8e29f2f16b84a3e45346b462f0e1458c37
+size 39545
