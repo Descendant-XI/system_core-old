de7e3e6bdeab740c31a2663c213c8c80fb6cfe34
Luigi Semenzato <semenzato@chromium.org>
2015-03-06 14:43:58 -0800

    metrics: move sample file from /var/run to /var/lib
    Some metrics are generated near shutdown and we expect to collect
    them at reboot, but /var/run is a tmpfs and /var/run/uma-events
    is lost.
    
    This changes the uma events file location from the perspective
    of the metrics daemon.  I am making the same change to Chrome.
    I am adding a symlink to allow an older Chrome to read the
    samples from the old location.  A newer Chrome will be screwed
    but I hope that's OK (i.e. this will work for official builds
    and for Chrome OS developers, but may cause malfunction for
    Chrome developers)
    
    BUG=chromium:447256
    TEST=verified that /var/lib/metrics/uma-events grows and is truncated
    CQ-DEPEND=CL:258971
    
    Change-Id: I677cda16486de239dd205247083d4ad7240d992b
    Reviewed-on: https://chromium-review.googlesource.com/257084
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon_main.cc
+++ metrics/metrics_daemon_main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34d65c3edb70c3788c0680392c218beed61fda11e4d8efa7b7d0ebe18f571aa4
+oid sha256:de528d5d48c58a69aa44470cf8180eb3414434a6d2fc472f999246e3fe55a84b
 size 3403
