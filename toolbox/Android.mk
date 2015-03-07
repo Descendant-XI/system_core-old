0ae161acafe01f51d9544a78e66b30a66e414d5c
Elliott Hughes <enh@google.com>
2015-03-07 11:07:10 -0800

    Remove toolbox's obsolete 'wipe' command.
    It hasn't worked in some time (it removes /data/dalvik-cache, which is
    required by art), and if you're prepared to reboot (to let init put back
    all the directories a working system needs) there are other ways to wipe
    data (such as "fastboot format userdata reboot").
    
    Bug: 19644330
    Change-Id: Ib074ea21cc28409a506d66d75060bb4ad85539e2

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93095fb18578f6b780761f46b049d4eedcdf8bc9ada18c2a1eef283fcb3dbfcd
-size 3307
+oid sha256:7df533399e17c502e377162eac2388a5097df2543bfbaec7ca59a2d40c99210c
+size 3296
