088d14d077a25d06357177a26293e4611a6cb522
Dan Albert <danalbert@google.com>
2015-03-09 13:22:02 -0700

    Fix a writex transcription error.
    Change-Id: I71b11127d41ebac6caf68926089c5a3b99d8c21e

--- adb/file_sync_service.c
+++ adb/file_sync_service.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4304b2efc19c237e9cb25669416ab4da0480f1245fb1e0a6bac8b15cd697f584
-size 11555
+oid sha256:f50a667c0db55393c3f1a396b9323c205310289986fae3e5af9c8fc3d081bfa5
+size 11554
