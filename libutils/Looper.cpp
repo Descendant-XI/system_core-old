8d2e6fb30846fde19f8740536c617f537795d2b7
Jeff Brown <jeffbrown@google.com>
2015-03-10 18:31:12 -0700

    Fixed subtle bugs related to file descriptor recycling.
    Added code to protect against situations that may occur when a
    Looper callback has the side-effect of closing the file descriptor that
    it is watching before it returns.  This code pattern is very
    convenient for implementation but it does expose issues in how
    the list of callbacks is maintained.  In particular, we
    need to watch out for file descriptors which have been reused.
    
    This change may resolve previously unexplained ANRs associated with
    log messages such as: "Error modifying epoll events for fd 44, errno=2"
    
    Bug: 10349083
    Change-Id: I20eedf6ffbdeda382653ca0104962505194741b0

--- libutils/Looper.cpp
+++ libutils/Looper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16af41ad94421b9423c8d9f4b116b4dec84759f1ab04c5631eca83de03aeb813
-size 18158
+oid sha256:536afdfd1ba5192457ab75b1d72b20f4cae000d55e8f23759af75c0555d20d8f
+size 21187
