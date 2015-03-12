ed5468767487917946061a1a0bdfae1fd5184cd0
Mark Salyzyn <salyzyn@google.com>
2015-03-12 15:46:29 -0700

    liblog: __android_log_is_loggable support global properties
    (cherry pick from commit c158456f50817ac506f4eed780978e63dd222fe4)
    
    - Add support for "log.tag" and "persist.log.tag" global
      logging properties, effectively a runtime default minimum
      logging level.
    - Add a thread-safe single level cache plus selective logic for the
      four properties being checked to help speed up logging decision
      on subsequent identical calls.
    - Using new __system_property_area_serial() to make for
      efficient (<100ns) handling of cache misses. Despite adding
      two new properties, we are 8 times faster on subsequent calls
      even if the properties do not exist.
    - A NULL or blank tag is no longer directed to return default,
      it will check the pair of global logging properties first.
    - Add liblog.is_loggable gTest
    - Fixup liblog.android_logger_get_, allow no content in crash buffer
    - Fixup liblog.max_payload, lowered logd priority increases latency
    
    Bug: 19544788
    Bug: 21696721
    Change-Id: Ideb887755aa3f1fd14a2603bda1fe23cba49642c

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64efb1509d7ebd4ad81e8df4cc60f5f4d6a501aed1948c408c59ae7176962c26
-size 22172
+oid sha256:99fee6dbf42a4d6b8cab6174eecbd7379948fdba5a503571c9cadfb9f3e56e67
+size 30539
