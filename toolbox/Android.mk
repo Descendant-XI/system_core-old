02c24a745ea6dd5863e88f0889f3d17fa75b078d
Stephen Smalley <sds@tycho.nsa.gov>
2015-03-13 14:01:58 -0400

    Remove getsebool/setsebool from init and toolbox.
    These were leftovers from the SELinux boolean support that
    was originally merged.  Since Android prohibits SELinux policy
    booleans, we can just drop it.
    
    Change-Id: I02f646a7d8db65e153702205b082b87a73f60d73
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74612ca53c517d61a5f8303f580d0de5f53df9f16b4493ece8a19007732e4ba1
-size 3310
+oid sha256:948d89ec72e53f3ea3e51d907a258bef178372f0f7f2d75ae2dfcb6757f94c77
+size 3278
--- toolbox/getsebool.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5e8b13c50b0ec6eae53f8b2c6cb548cf80eb7fad049df154812a0ff94e9e9484
-size 2844
