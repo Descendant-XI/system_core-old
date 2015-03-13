b92d63e5fb164c08365dbe632845f959ac845719
Jeff Brown <jeffbrown@google.com>
2015-03-12 19:32:39 -0700

    Fix issues related to removing Looper callbacks after close.
    When a file descriptor is closed before removing it from the
    epoll set, it will normally be removed automatically from the
    epoll set by the kernel.  However if there exists a duplicate
    then the original file descriptor may remain in the set and
    continue to receive events until all duplicates have been closed.
    
    Unfortunately due to kernel limitations we need to rebuild the epoll
    set from scratch because it may contain an old file handle that we are
    now unable to remove since its file descriptor is no longer valid.
    No such problem would have occurred if we were using the poll system
    call instead, but that approach carries others disadvantages.
    
    Bug: 19715279
    Change-Id: If1ab8ebda0825755a416d513e888942a02ee3948

--- libutils/Looper.cpp
+++ libutils/Looper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:536afdfd1ba5192457ab75b1d72b20f4cae000d55e8f23759af75c0555d20d8f
-size 21187
+oid sha256:2f55b9ac0a0b7883154ab01dcd9b46938b15de0445959b6867e5e3e89bf28199
+size 23566
