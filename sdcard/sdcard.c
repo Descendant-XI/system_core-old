9e2df01dc45c70b6cce949208ecc30c83f561dd2
Jeff Sharkey <jsharkey@android.com>
2015-03-16 10:17:47 -0700

    Progress towards dynamic storage support.
    To support external storage devices that are dynamically added and
    removed at runtime, we're changing /mnt and /storage to be tmpfs that
    are managed by vold.
    
    To support primary storage being inserted/ejected at runtime in a
    multi-user environment, we can no longer bind-mount each user into
    place.  Instead, we have a new /storage/self/primary symlink which
    is resolved through /mnt/user/n/primary, and which vold updates at
    runtime.
    
    Fix small mode bugs in FUSE daemon so it can be safely mounted
    visible to all users on device.
    
    Bug: 19993667
    Change-Id: I0ebf4d10aba03d73d9a6fa37d4d43766be8a173b

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be4600f2dd757beae5d6eab8258c7886fcbac8c93eb9d82444bc0fbcea971516
-size 64378
+oid sha256:e477e09890f6686f150394128dc791158f3535e5742356854d9b5f18fbcd632d
+size 64405
