2ba78ecc931211863eded27ac3315c4b44a299fd
Elliott Hughes <enh@google.com>
2015-03-16 21:58:32 +0000

    Revert "Revert "adb: support /oem partition""
    This reverts commit 6084a0124f868c7ec43f6c415a27a168f27ff694.
    
    The original build breakage is fixed by (a) building the verity
    code for eng builds as well as userdebug builds and (b) moving
    the exported remount service functions into a new header file.
    
    Change-Id: Ice0c4f97d4db38ab7eb333c7a6e56bbd11123f5b

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:497c78de972530ce02ca9e916a245b25c44ff3955c77cb48b803ff0fb9bcf4b6
-size 10822
+oid sha256:b5f7aef4270f2097f8a83ab160510a5297b58bf7ab8743acb6f0bb4a10e34031
+size 10662
