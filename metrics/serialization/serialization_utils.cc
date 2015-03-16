b5ee9073081c994be18ac0d02cf00ec304cd8017
Stefan Sauer <ensonic@google.com>
2015-03-16 16:54:55 +0100

    metrics: tell what file we can't open on error
    Use PDLOG and also log the filename.
    
    BUG=chromium:467586
    TEST=rebuild image and checked the log
    
    Change-Id: I72f9838477adad71e79cc25f1f3a40bb9781b848
    Reviewed-on: https://chromium-review.googlesource.com/260261
    Tested-by: Stefan Sauer <ensonic@google.com>
    Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Commit-Queue: Stefan Sauer <ensonic@google.com>

--- metrics/serialization/serialization_utils.cc
+++ metrics/serialization/serialization_utils.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab431206e6f9efd8c4d549b9a3a570d2798eb873e434374d0265f1493380117f
-size 6769
+oid sha256:e5b0bc7f580c0f71045406168fa9608a08aa17ddff2536dd0f480fd941d853d2
+size 6756
