d5d1b1114b2a24123495f4c3e3f99a8c7e721923
Sami Tolvanen <samitolvanen@google.com>
2015-03-19 10:00:34 +0000

    Add init command to set verified properties
    Add a command that updates dm-verity state and sets partition.%.verified
    properties used by adb remount.
    
    This is needed in init since fs_mgr cannot set properties:
        I6a28cccb1ccce960841af20a4b20c32d424b5524
    
    Change-Id: I0fdf5bc29c56690dcadff9d0eb216d3c68483538

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9ccd515f779572b3f9ef24fd19e9e89e57a75a88aab37c8ccf3dd2cd5c17f2b
-size 20760
+oid sha256:1e898136acbacedd3b8bc94dc814541dd68db5763fa624c06f0edef7ec5a0bac
+size 21443
