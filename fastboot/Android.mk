d41b9675970bcf9c74401c89a77517e6299073a8
Elliott Hughes <enh@google.com>
2015-03-18 23:12:44 -0700

    Switch fastboot to libziparchive.
    Bug: 19765860
    Change-Id: I83ea56e3b317893995d7106852b65584f99c0e64

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0594ef1974a6e5470063e31ddc57a03c08a27b4b1dbc5f25af46e4f1b50ca59a
-size 2793
+oid sha256:90987ec2b2573a62c789d8b3266625120a68072ba6b3fe74bbe2af4b9e628343
+size 2837
