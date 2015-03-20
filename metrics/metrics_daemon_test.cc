b4815f7afe5bae0084c7f98848c1d4ca8e8b06dd
Luigi Semenzato <semenzato@chromium.org>
2015-03-20 08:35:30 -0700

    metrics: make metrics library also use /var/lib/metrics/uma-events
    I missed this when I changed the location used by the metrics
    daemon.
    
    BUG=chromium:447256
    TEST=none
    BRANCH=none
    
    Change-Id: Ie964abb70911bcdc79993b62a066ea77f65acfa5
    Reviewed-on: https://chromium-review.googlesource.com/261463
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>

--- metrics/metrics_daemon_test.cc
+++ metrics/metrics_daemon_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e26a6b51d188f99cf4fce6134c9baaafa4f6283fd3248eeb97202a4affa2b74
+oid sha256:2db9be37901d1a735cbeee4d34d8df053a2e152ee6444b97d4b43d9d27cb0d7e
 size 15942
--- metrics/metrics_library.cc
+++ metrics/metrics_library.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77840fc47a230a03822c7d6fa924a3dd770c7f600f11bbe44074edd1de7d41d3
+oid sha256:8fdd9daf57e075955cd820319382cf6b43704689a448bb3d998f74874f45a729
 size 6817
