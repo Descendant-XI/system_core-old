e23cb5c5335aa13ce81add7d2f1d476b86684495
Richard Uhler <ruhler@google.com>
2015-03-20 16:59:40 -0700

    Define atrace_* functions for both target and host.
    This change defines atrace_* functions for the host that act as
    no-ops, which makes it easier to add tracing in ART.
    
    Change-Id: I89397e83986686a2b6a6f245c25017eb379081b1

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7459136d49176470717669ac67287fddee29899c2ef8cc0e806e56f61b1fa720
-size 5235
+oid sha256:e92ba3599d364c70ab6f4392bf2353f545e55de8daad7e3539ebd3bd6214fae8
+size 5262
---
+++ libcutils/trace-host.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c602c4484efaa5140b4756ea6809bff2a25402f3b59df4841e370ee19979873
+size 1416
