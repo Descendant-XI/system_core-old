11bf926f60f6d7e2867f15eb6c017eb23e72f3af
Mark Salyzyn <salyzyn@google.com>
2015-03-20 13:44:53 -0700

    logd: statistics: missing comma
    regression from statistics rewrite series resulted in build breakage
    
    Change-Id: I71b532b23437b9b5f3af47b18c9110f3d7dda48a

--- logd/LogStatistics.cpp
+++ logd/LogStatistics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8470704a1228b9e982092e9d011530e9c73dc26f15ee957a5e62105fb525d09
-size 8513
+oid sha256:ae26dc30aeeb169d17c2ec1209dea060b3d493dfba21f595050cc58c19b7e823
+size 8514
