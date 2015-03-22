91492535af7d5247015f0620fcfc54134b7370c0
Sami Tolvanen <samitolvanen@google.com>
2015-03-22 12:40:05 +0000

    Use structured format for verity metadata
    Specify the location of verity metadata in fstab, and use a
    type-length-value format for the metadata that allows other
    data to be stored in the same location in an extensible way.
    
    Change-Id: Id8711f7d51dc1e4e9a4d84f9951240f64528e69d

--- fs_mgr/fs_mgr_fstab.c
+++ fs_mgr/fs_mgr_fstab.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b52d87decf8e2da5b91b37ca46800c5f475be22b0934e67ddd0521e372a0beee
-size 13633
+oid sha256:de764bb3717671991575a50881f2a8e8e96302190907eae536449b6c05a8609f
+size 14127
