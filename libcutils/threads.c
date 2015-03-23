683a152a7a74d2f5164e0ba2fef041dab27c8061
Dan Albert <danalbert@google.com>
2015-03-23 16:07:42 -0700

    Undo mac fix to fix windows, fix mac differently.
    Change-Id: I7d1ee439d25e17c31c0be448e67a90a0f626ff42

--- libcutils/threads.c
+++ libcutils/threads.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01d54cec20d4c5129bc054c7476af875bc763c614331dc872e9c18bd7bf26a38
-size 3068
+oid sha256:f25f86a96b1e51dea4eed7b8b3680a41ff16bec979a38d3837d04a38824e4464
+size 3049
