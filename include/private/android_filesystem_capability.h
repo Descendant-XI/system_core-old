e2e322f6c0affc4e4ad5a02d41b6cc10e2b50ebc
Tim Kilbourn <tkilbourn@google.com>
2015-03-24 16:58:26 -0700

    Allow inputflinger to have CAP_BLOCK_SUSPEND.
    (Cherry picked from commit 83e6f2606b9c3488454a7bb01c7341fb43b59ac7)
    
    Resolved conflict in include/private/android_filesystem_config.h, content
    already in libcutils/fs_config.c.
    
    Change-Id: I817707721786e9b920fef14ebe836fdcd65a8606

--- include/private/android_filesystem_capability.h
+++ include/private/android_filesystem_capability.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1706df5e9020deedcbe930fdd234f6afe62829cf7118de16d081bf619379b812
-size 3260
+oid sha256:051398436ccc5657a44e2a7f754c6a1da4c88a8616a1893991c406fb5e5df549
+size 3315
