ba186369adb587d02d9d3db229bb8e538c774070
Paul Lawrence <paullawrence@google.com>
2015-03-26 15:49:42 +0000

    Revert "Revert "Adding e4crypt support""
    Fix build break caused by original change
    
    This reverts commit 84b0bab58fcc7f225e9a17a15c531b0c2fc509c5.
    
    Change-Id: I99fbd7c3d1ed92db1f546033c8493bb71a327924

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb56b8a6ea697df7f1da2cd6a6f6a9552a8ea82b5e318a272488f3d870c47a7b
-size 25222
+oid sha256:77f027c982102d8d0e7f204aeb19f9a21f8b37760e4420d1e218b9c1bb10bba8
+size 27348
