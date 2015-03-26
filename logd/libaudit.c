79a7d554fc45d2d222cac0b4b7e9d4e8ceb1c9e4
Nick Kralevich <nnk@google.com>
2015-03-26 14:04:13 -0700

    libaudit: limit to 5 selinux denials per sec
    watchdog is triggering on shamu. This may be due to an excessive
    number of SELinux denials. Drop the limit from 20/sec to 5/sec.
    
    Bug: 19950451
    Bug: 19949988
    Change-Id: I979f11e17c241ff2ebda4dec9694ef441dc5d0ed

--- logd/libaudit.c
+++ logd/libaudit.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:589e080e27556abf41f95182807918576864a1e70e9417efee35eae59bbc9c6c
-size 7752
+oid sha256:1b60e1111c25943d59302ef156f62cec23426600a8d7ce8894d109ea9b7bddde
+size 7751
