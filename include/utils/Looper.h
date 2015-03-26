21a5bfeb3864bcf1c1538896788c7b116ec2a060
Tim Kilbourn <tkilbourn@google.com>
2015-03-26 14:36:32 -0700

    Use eventfd instead of pipe in Looper.
    Bug: 19933224
    Change-Id: I6611e918db960bbb0e74f8308e10bdf79510b899

--- include/utils/Looper.h
+++ include/utils/Looper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da2a883580751604bd18b3781eaef285f9bd353cf4c2715c9f584f2ee0770520
-size 16775
+oid sha256:aa82e3bc54051d9efea28be7c2cfe318c06251464f82890bdb2d576b60772698
+size 16733
