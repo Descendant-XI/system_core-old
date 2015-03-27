4ed163f8b9bd6629a0025c3907e44c75bf0e9c30
Dan Albert <danalbert@google.com>
2015-03-27 11:20:14 -0700

    Support arbitrary loggers.
    While the defaults (logd or stderr) make sense for most use cases,
    there are places that can only log to the kernel, or need to log to a
    file, etc.
    
    Allow the user to pass in an arbitrary logging object, and provide
    LogdLogger and StderrLogger as defaults.
    
    Change-Id: I62368acc795ff313242bb205d65017404bf64e88

--- base/test_main.cpp
+++ base/test_main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe64c399b85a6ac429cfde8a887dae13d7efdf78919f686f5c9ef78829ece250
-size 814
+oid sha256:25cad77a6555c4e066a4b256e33851eb9820ecd60e332e81a54b322c046cb315
+size 843
