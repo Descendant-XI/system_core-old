040da0cb7a675f04ede2921df9f438c26b784bd2
Mike Frysinger <vapier@chromium.org>
2015-03-27 17:07:20 -0400

    delete __STDC_{FORMAT,LIMIT}_MACROS
    With newer glibc versions (2.18+), these macros no longer exist (and the
    functionality they protected are always enabled).  Delete them.
    
    BUG=chromium:401360
    TEST=precq passes
    
    Change-Id: I21b0607a874b9f9e39dff050054a2928a6c55023
    Reviewed-on: https://chromium-review.googlesource.com/262903
    Trybot-Ready: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- metrics/metrics_daemon.cc
+++ metrics/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa88f5f048bddb15d24c46df3f7d74e42e96cea679faf3c3fd032269ee2d4f59
-size 42307
+oid sha256:4b8cfa8b36d4218df6d2d4d5ae05146a971221bee647bc99fb212d79063b385f
+size 42178
--- metrics/metrics_daemon_test.cc
+++ metrics/metrics_daemon_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2db9be37901d1a735cbeee4d34d8df053a2e152ee6444b97d4b43d9d27cb0d7e
-size 15942
+oid sha256:046e9ce2740d7d04b30e2b1b5fa89e20076452cd35be53b247f6e72f93a7f353
+size 15912
