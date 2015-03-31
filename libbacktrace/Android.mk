9482552faadbcda69fae4381c7d424691ab13e5a
Stephen Hines <srhines@google.com>
2015-03-31 09:39:20 -0700

    Add -Wno-inline-asm to Clang flags.
    The latest clang (r230699) does not allow SP/PC to be declared in inline asm
    lists. This is a problem for libraries that are attempting to save all
    register state.
    
    Change-Id: I1ce80530f8be85fd74e949d0c65b60080e0aaf2a

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88fde110e23eefe486a76cf74b392429a43e765ca46765633c77edbb4c27484c
-size 3524
+oid sha256:4b6de93dcf5e47061121d8ca2c66021122b69eec62f99a745ca08624fb02b134
+size 3669
