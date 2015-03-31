c9a1872b4105a886ce8e70e2d322daa7b4cd54c9
Sami Tolvanen <samitolvanen@google.com>
2015-03-31 14:36:03 +0100

    Restore verity state when verified partition is reflashed
    Store verity state separately for each verified partition, and store
    a hash of the last verity table signature for each partition. If the
    signature changes, assume the partition has been reflashed and reset
    verity state.
    
    Bug: 20006638
    Change-Id: I1c85fb816bfec1a54b1033c938bf1fdaf572f849

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3a9e56f5a00eeb3e003f9f02bcf2757ceb66cd3adb06a6c0e0a53f42e99b334
-size 23643
+oid sha256:f7128a8bddbdde59611aacc5acd2b6c1788bea0bf15bddc650a15ae97c2707b1
+size 26250
