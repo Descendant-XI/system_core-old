9be255ea9d5425e6467f12c7b43f0ef99abb6e6e
Phil Burk <philburk@google.com>
2015-03-31 14:33:25 -0700

    audio.h: add AUDIO_FORMAT_DTS
    Bug: 18292317
    Change-Id: Icb3855e958d02e3b03517ac0fcf7de743d896c12
    Signed-off-by: Phil Burk <philburk@google.com>

--- include/system/audio.h
+++ include/system/audio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f3459b5c5b214004f6a16a2a55e44194d87970bf8bd0399a82e2488b1bd431b
-size 59084
+oid sha256:f4e7193a9f522fa6ee3f25a6b02cfd03634249a72668dd5fae1d2918565309c2
+size 59248
