f3824f5e47168f197cba89f7121bc5eeaf02501d
Mohamad Ayyash <mkayyash@google.com>
2015-03-31 12:09:29 -0700

    Add verbose param to sparse_file_import_auto
    Change-Id: I0c5607f7aa5e964abc2031bbe71ff5c6e6ef56cc
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>

--- libsparse/append2simg.c
+++ libsparse/append2simg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13f4a584146840341d7b2d093b2efada48617e772af125cb68cbb35e60541dd4
-size 3587
+oid sha256:598727e9fd7be8df6c91e288d2c285e6105f5733864be2feb7ff1103cd1916b4
+size 3593
