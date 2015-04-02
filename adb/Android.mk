f9778666033cbe4bfc7a402e6afead533fde684c
Elliott Hughes <enh@google.com>
2015-04-02 14:31:07 -0700

    Remove LOCAL_ADDITIONAL_DEPENDENCIES in cases where it's not needed.
    Change-Id: I720b8ef1050da45a7833adef8219b6acb2cf3a38

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c199a8f98121bf505f190ab1dd3421e2cbbff994da9db56bb6d416ef4131bdf
-size 5420
+oid sha256:a8fbfaf644f78ce9d79c0ddd15b92abb362e033ca16692b1791831fb27e2b721
+size 5303
--- libsync/tests/Android.mk
+++ libsync/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b818e0ba472b49a7f97e3c199e02d04fa1a92fc8495272426eebad0dfacbb5d4
-size 1082
+oid sha256:e96593672dd16e2de559e55a42f773c5173a37718d47ff34a85cfebaa11c3c90
+size 1024
