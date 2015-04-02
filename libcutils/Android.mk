abc297240bd251e292695e1a28751166ab62380c
Elliott Hughes <enh@google.com>
2015-04-02 15:39:33 -0700

    Be more intention-revealing with libcutils' strlcpy.
    Several people have been independently confused about when this gets
    built and why over the past couple of days.
    
    Change-Id: I2d4a02f66e24c0734327585b7d27e50a344b1e94

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e1ea46a51c45e96f2a3a3aa5918b8dfcd1b88823207519e48c01d13e0a970d7
-size 3842
+oid sha256:34e59114b6f39469c2396de973d2b997a48e2ba79c37076dfe3ae805fd4b0901
+size 3843
