ffe60697fe606f32f3d3244678908ed2bef29483
Mohamad Ayyash <mkayyash@google.com>
2015-04-08 19:31:00 +0000

    Revert "fs_mgr_verity: Add support for squashfs"
    This reverts commit 807f47004f03653997edbe3c83d46350cb056cd4.
    
    Change-Id: I2d7972c0828c842b44747dd08fbe44668f2a55db

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13472436b1c5de5d33004e72b15018d47279ecedd2235f1b721becb1c34645e7
-size 24015
+oid sha256:759cbad5da6653f93b968d9791ea3e750d5b1e2f0ed72c6f37630f69d3494d49
+size 23529
