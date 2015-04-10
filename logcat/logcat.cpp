66d21e3a191e339f32c320372a014fd1b93047f0
Traian Schiau <traianx.schiau@intel.com>
2015-04-10 15:51:39 +0300

    logcat: Minor fixes
    Remove -r default value implementation (since is not functional).
    Check the status of some memory allocations.
    Make sure that the pruning buffer is set to NULL if we get to the end of  the loop.
    Make sure that the file status is successfully retrieved before using the size.
    Add strtouia - string to unsigned argument.
    Add logcat_panic with/without printing an error message and help.
    Play nicer with the android namespace.
    
    Change-Id: Id4c37cf64968128424e29bf1725f6c9a5924a0e2
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f60fc86e8f9e9d5ff82f4d068465137be7729cfbb605adbcdbebfbdd4411907
-size 28162
+oid sha256:dbad8c5eab6fb97550bd8fb3beaeb4bf27e1d3d1957da7d4ce377d2cdf7208ae
+size 28514
