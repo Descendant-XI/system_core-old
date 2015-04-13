f2dcf183c3c215b5791573db2d492578eb15a450
Mark Salyzyn <salyzyn@google.com>
2015-04-13 14:24:45 -0700

    logd: class hierarcy for Uid and Pid statistics.
    (Cherry pick from commit 81b3eabc49736b89c4f99940f79785074955eaa5)
    
    Add EntryBase and EntryBaseDropped base classes for statistical
    entries to inherit from. Abstract add(), subtract() and drop()
    methods to common LogBufferElement in each for entry policy decisions.
    Some move of details out of LogStatistics.cpp and place them into
    LogStatistics.h. Add statistical add(), subtract() and
    drop() methods to hash table to call entries for policy.
    
    Bug: 19608965
    Change-Id: Ib8a33a8fe28871ef165d1632c6546a5c606231e8

--- logd/LogBufferElement.h
+++ logd/LogBufferElement.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:948bb95c9b4baac43f35fa2f0dc88cd46456e69f11e8a063d59387d69d2d6b11
-size 2748
+oid sha256:e72c3aa8aa03aefdc4bef2e4f6cca5337cb3918405391bed4550a4ecc0ce5469
+size 2873
