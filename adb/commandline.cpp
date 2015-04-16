53cee27b8129e5d8d39d89fd8cd7a8f6786d8d4f
Elliott Hughes <enh@google.com>
2015-04-16 16:47:02 -0700

    Move sysdeps_win32 to C++.
    Change-Id: I27ca41b64d62bb3611b3a39a5c3bb4377d0773bc

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:186985469353e7c88c0a62f8db0b9b7b3e7c1ea0b4770a0f10c2e8b587af6c1d
-size 65886
+oid sha256:0ac5052f47c981ba8ad030c71af6b7592193c5fe93efd5aa2d98cbe4aae96391
+size 65873
---
+++ adb/sysdeps_win32.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d58db56470d3a45af07da2d95c50a3ad52de5477d8198693997f35fc25ee71e5
+size 97383
