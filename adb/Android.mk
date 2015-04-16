17d885a9ab79fdb8fe3e91c162ac09b67d60cfd9
Colin Cross <ccross@android.com>
2015-04-16 16:21:44 -0700

    statically link adb and fastboot against libc++
    libc++.so is not widely available on the host, so we compile against
    one built as part of the platform.  This causes problems for adb and
    fastboot, which are distributed through a number of channels - the
    sdk, distro packages, downloaded from the build server, or manually
    copied.  Instead of forcing all users to handle libc++.so too,
    statically link against libc++.
    
    Change-Id: I51b75258653a23558c8b598802005f6c1166a439

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8fbfaf644f78ce9d79c0ddd15b92abb362e033ca16692b1791831fb27e2b721
-size 5303
+oid sha256:366bb7eb81cf824b8159620bd54988170f655fc45b9c3777a731129eac5ffe50
+size 5523
