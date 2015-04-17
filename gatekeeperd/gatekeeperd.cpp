e0421717ea592f0d7d59eb3df3891496fb5d6289
Andres Morales <anmorales@google.com>
2015-04-17 09:00:28 -0700

    Make clear SID delete the file
    This allows us to recover in situations where we manage
    to clear the SID in GateKeeper but fail to remove the
    password in LockSettingsService.
    
    Change-Id: Ib64ead137632f9615745a414c90a9b66b847134f

--- gatekeeperd/gatekeeperd.cpp
+++ gatekeeperd/gatekeeperd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58df0b63dbe53e0847c786483188b2a8a6f3daf4b38190389ff7fbb87f3091cf
-size 8797
+oid sha256:2cea6631042a37e81d6b44f18627705af51aa8fc3a1b368df9da093d660f33f9
+size 9078
