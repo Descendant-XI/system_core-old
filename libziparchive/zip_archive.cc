e74952535d2fe4152f3795fad7b033bcbdcd8120
Narayan Kamath <narayan@google.com>
2015-04-17 11:53:14 +0100

    Avoid mapping output to memory while writing to a file.
    It's unnecessary, and causes issues when the uncompressed
    output is large.
    
    Bug: http://b/21558406
    Change-Id: I99cfb3933b094c2826c7e6c6de9aab03478fcc53
    (cherry picked from commit f899bd534b2dc51b9db8d27c76394b192fe51155)

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9072171e824695471853bffd5c27e793a64ddcfe998eeebf4f386b1406f1c923
-size 40225
+oid sha256:4e4350c4d1480dbc319274090c4c5daab81d466a8fa98863ada5c767256afc48
+size 43162
