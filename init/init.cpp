659c3895e09092bc5b4dceda9c12d68819db4281
Johan Redestig <johan.redestig@sonymobile.com>
2015-04-18 14:13:24 +0200

    Setup signal handler before any exec command
    Fixes init deadlock when an exec command was called
    in an on-init section.
    
    The exec command handling relies on that the signal handler
    mechanism is working to know when to continue executing
    commands.
    
    Change-Id: Ib0ce75ffad7cf3bf926c93d0506b2fe3e5a92630

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ca5f61811ec09ed6d03d8f3c9fe452dd0c858ed30fa577add91c479b9040bb5
-size 33242
+oid sha256:13094ff84416a5fa1fccd50f0377cac902d3da5878c1cfe9e6b868e336b9d5d7
+size 33313
