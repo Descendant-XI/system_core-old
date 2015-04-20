1f1e406f5332450e9b8936ce9090fe58aecdf385
Mark Salyzyn <salyzyn@google.com>
2015-04-20 13:35:15 -0700

    logd: Add TID statistics
    (cherry pick from commit 17ed6797df722464eb5cc6dfc3e1e32aec284b70)
    
    Bug: 19608965
    Change-Id: Ifbf0b00c48ef12b5970b9f9f217bd1dd8f587f2c

--- logd/LogStatistics.cpp
+++ logd/LogStatistics.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80db6c1f33e182c8c395824aad89e1cef22fd61c99d82699d2a2fa049c3b5224
-size 13769
+oid sha256:f1c1a7bed45c96f1252095da3ee2cc50f0bcb6ea89d6bf659e12a5306e638f0a
+size 16078
