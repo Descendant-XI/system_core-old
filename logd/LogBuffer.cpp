b03fb6deb50a0b0f7064cc28c578d84d7268d1ce
Mark Salyzyn <salyzyn@google.com>
2015-04-20 15:36:12 -0700

    logd: per UID less aggressive 12.5% threshold
    Per-UID quota has a threshold of 12.5% of the total log size.
    If less than that space is taken by the UID, then we
    will not engage the pruning based on worst UID.
    
    Change-Id: I9f15c9a26938f1115eb75e9c28ddb073e7680e06

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc92d4a88f2bfb4827566079449f4f2deb03a09d92bd72d81826859fcf42291
-size 17460
+oid sha256:b8c63cdc2828846bb97fd21452ce01f0843f2de3875a139f1ae0162c8178d023
+size 17826
