33326cf77c3310c62569d4da6f0cd7a35c18902a
Tom Cherry <tomcherry@google.com>
2015-04-23 14:35:33 -0700

    init: consider all platform devices for find_platform_device
    Certain platforms list their platform devices with a path other than
    the current /devices or /devices/platform.  Additionally, sometimes
    these paths are a platform device themselves.  An example are devices
    with a path /devices/soc.0/... where /devices/soc.0 is also a platform
    devices.
    
    Instead of continuing to special case these path prefixes, we can
    consider the entire path, given that no mmc or usb devices create
    platform devices as subdevices, which they should not by their very
    nature.
    
    This keeps the symlinks for block devices named identically for
    current platform devices and places them in /dev/block/<full path to
    device> for devices in locations other than /devices or
    /devices/platform, eg /dev/block/platform/soc.0/7824900.sdhci
    
    This keeps the symlinks for character deviced named identically for
    all cases.
    
    Change-Id: Ifc8eedf1086db1826dbc8f7e96cdfcfa5da876b3

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c8457704abaaa57c0e0d73d36db52baa6969b8fd5129bfa8366835808a43e6a
-size 27430
+oid sha256:01456bc02906c4b2e43e98036d1fd4c38441f8f0db18ef1969535e5375d8a4c7
+size 27052
