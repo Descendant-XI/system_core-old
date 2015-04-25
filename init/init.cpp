9e72c30715af484f51fe4f263657029a0e408646
Elliott Hughes <enh@google.com>
2015-04-24 18:50:30 -0700

    Clean up property service initialization.
    All the code that was being delayed does is create a socket. We can
    do that straight away, avoid the overhead, and simplify our main loop.
    
    The keychord fd, on the other hand, seems a little tricky. It looks
    like /dev/keychord isn't immediately available, at least not on N9;
    we have to wait for ueventd to set us up the bomb.
    
    Change-Id: I020e75b8e4b233497707f0a3cbbb6038b714161f

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dd98ea6bf94ced5ca7cc6783a4a6fbafd52e14f77550ce0941b801bdd5d966b
-size 34048
+oid sha256:891bfc5df53d54e9628a1d97e1eb8c6fb2a5134a3766c37a289099008fe2753e
+size 33699
