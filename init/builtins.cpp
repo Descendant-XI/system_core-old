e08aa78aef107409b558a761eeb37c12d6a8ae01
Emmanuel Berthier <emmanuel.berthier@intel.com>
2015-04-27 13:50:09 +0200

    Enable property expansion for insmod
    Useful for dynamic kernel module location.
    This permits to use init rule like:
    
      insmod ${persist.modules.location}/<module_name>
    
    Change-Id: If7479bdcb4e69ea7666f52a0075c785be025c2e9
    Signed-off-by: Emmanuel Berthier <emmanuel.berthier@intel.com>
    (cherry picked from commit ac41230cbc10bae95a049282238adb03605579c2)

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642744a78bb8f8c5fb0276223491cc60c662c5156828d7548ee277a1382f0eb9
-size 21056
+oid sha256:8ba571b0dc59024407a477cad0095ff5c1a79c2bee2f3f01b9e4c383cd27c5cc
+size 21286
