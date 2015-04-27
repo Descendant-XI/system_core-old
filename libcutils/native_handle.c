6bb9ac917a1a9c0218e6944a26be53b1c7bac055
Adam Lesinski <adamlesinski@google.com>
2015-04-27 12:13:33 -0700

    Prevent integer overflow when allocating native_handle_t
    User specified values of numInts and numFds can overflow
    and cause malloc to allocate less than we expect, causing
    heap corruption in subsequent operations on the allocation.
    
    Bug: 19334482
    Change-Id: I43c75f536ea4c08f14ca12ca6288660fd2d1ec55

--- libcutils/native_handle.c
+++ libcutils/native_handle.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad46d3c4002db8330b65eca6b5284a4f0cd0f5316e3e8bca706a5b1b03e89317
-size 1550
+oid sha256:753a8ed3c383e2640cd5f01ef8dbfbf983d3b61f1ce09ce57c134962dbf3064a
+size 1777
