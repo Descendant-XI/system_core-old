0ae0ce10a72cee22aa123e067e6001c594017d48
Paul Lawrence <paullawrence@google.com>
2015-04-28 22:07:10 +0000

    DO NOT MERGE Securely encrypt the master key
    (chery-picked from commit 806d10be2336f32cdca16c2540cbf3d548f2fec7)
    
    Move all key management into vold
    Reuse vold's existing key management through the crypto footer
    to manage the device wide keys.
    
    Use ro.crypto.type flag to determine crypto type, which prevents
    any issues when running in block encrypted mode, as well as speeding
    up boot in block or no encryption.
    
    This is one of four changes to enable this functionality:
      https://android-review.googlesource.com/#/c/148586/
      https://android-review.googlesource.com/#/c/148604/
      https://android-review.googlesource.com/#/c/148606/
      https://android-review.googlesource.com/#/c/148607/
    
    Bug: 18151196
    
    Change-Id: I6a8a18f43ae837e330e2785bd26c2c306ae1816b

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3e867683fb77b0f3c9a87959464222feb4341c08a74803f0de0d45b546d005b
-size 28963
+oid sha256:6e9091f6d4e8f3c0c79cabc4ddb1faa83a6a93cc628e232bc269d55aa9ffbf9f
+size 28516
--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb869cd44ba1fd328126f66c7cd54cb7827b58564e2ae8fec9c11cc119ae57e1
-size 21263
+oid sha256:32668704e7f9a5515219baaec588a7dbb3714b44872469555ca975802127f172
+size 20910
