296c588f8e559cf80485cf41454585ece12bbc23
Elliott Hughes <enh@google.com>
2015-04-29 12:36:06 -0700

    Fix Win32 build.
    Change-Id: Icf2c8df99b4b88bbf85a4097731733c5795fba44

--- adb/adb_client.cpp
+++ adb/adb_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d4d1caf6b757d4d4dfcb8032bf2664377e7e5a7809fcba2c1a22af07739960e
-size 9790
+oid sha256:c99bfaecec9f83d5e214dd012e5cad2a49f7f750ed030d389bd24184dd16c777
+size 9807
