ff2c2f410de3751f377a803600601a8fee9d8f69
Mike Lockwood <lockwood@google.com>
2015-04-29 13:04:10 -0700

    libusbhost: Add usb_device_get_version()
    Change-Id: If2161178e9fe94a94faf01c102ef64f2a72d093b

--- libusbhost/usbhost.c
+++ libusbhost/usbhost.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b99f56e254fe910ec71ae006edfcf85785ffa33d9f149e6ac23f07bd82eb3213
-size 20916
+oid sha256:98acde4b3188c9f8641ff647e6544a163b2a027f94d8fdfab598994d71582a11
+size 21087
