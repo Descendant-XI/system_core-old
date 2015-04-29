b8ca7b263f4d2c5aeade7ce6ba6881ba795efd6f
Elliott Hughes <enh@google.com>
2015-04-29 08:35:59 -0700

    Move __adb_error to std::string, and improve various errors.
    Also remove an sprintf. Also fix various bits of code that were
    reporting stale adb_error values when they meant strerror.
    
    (cherry-pick of 078f0fcf4c63b8d8e8c10a18855eae04ca321e06.)
    
    Bug: http://b/20666660
    Change-Id: Ibeb48b7bc21bb0ec30ba47889d1d671ee480e1b7

--- adb/adb_client.cpp
+++ adb/adb_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b5c4578e29d834cf8c2b98f716ecba6bed00f8b69850d1f236c9297468c54a0
-size 9274
+oid sha256:4d4d1caf6b757d4d4dfcb8032bf2664377e7e5a7809fcba2c1a22af07739960e
+size 9790
