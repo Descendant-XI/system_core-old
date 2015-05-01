6b72eb837848ee06b7a8c928e29340011b276788
Andreas Gampe <agampe@google.com>
2015-04-30 20:39:12 -0700

    LibNativeBridge: Version 2
    Add a callback function to retrieve "signal handler" from the bridge,
    if the bridge wants it to be managed by the runtime. The signal handler
    will be invoked after the runtime's own one, and before any other
    chained handler.
    
    Add a callback function to check compatibility of the bridge with the
    library.
    
    Add a function to expose the native bridge version. Add a test for this
    function.
    
    Bug: 20217701
    
    (cherry picked from commit a6ac9ce98bd38099a4e89010111d14e4d5fc190e)
    
    Change-Id: Ic23a60b949f119c7d8b0e7cb27a61e6c16532a23

--- libnativebridge/tests/Android.mk
+++ libnativebridge/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae9c4abe7edb68fd19ece6b5de6c7dbbe6c424cc5b29f863e97f7ce3d4b9d596
-size 1377
+oid sha256:de1ddf1ea036043be4c8a28d764cbd68396d91b39a2cbb8d80556270fcf379d1
+size 1447
