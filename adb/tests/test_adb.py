6504c563277f17d453d795d30398ebd36fa08143
Elliott Hughes <enh@google.com>
2015-05-04 19:29:32 -0700

    Implement the ssh(1) escaping rules.
    The first rule of ssh(1) escaping is that there is no escaping.
    
    This doesn't undo any of my recent security fixes because they're all
    calling escape_arg themselves.
    
    This fixes "adb shell rm /data/dalvik-cache/arm/*".
    
    Also remove do_cmd which caused concern during code review.
    
    Bug: http://b/20564385
    Change-Id: I4588fd949d51e2a50cff47ea171ed2d75f402d0d
    (cherry picked from commit 2b10111d25adcfe3627103b73edad22f188c97ba)

--- adb/tests/test_adb.py
+++ adb/tests/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de67d8ccd4d19c11dfbd9f2ef661ee4d2a26d65771c2e03e8771680895bb9ee7
-size 13673
+oid sha256:d7b53cd30e4a1dfb99f30030efdd89af13af0cc75b6fe1e57e6d9a5d74c218ad
+size 14310
