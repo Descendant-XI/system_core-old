c52414a219ff81ed93e491c22362f0c943e629d2
Alex Vallée <avallee@chromium.org>
2015-05-06 17:22:25 -0400

    Move mkdirs to adb_utils.
    There were duplicate implementations in commandline.cpp and
    file_sync_client.cpp.
    
    Change-Id: Ib448f76c0d7ffdcd577336b1c610a881425bc2db

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf3691531e1f51bbecc6cd6080499cb9243733a998d572402b47a4c28d9fa008
-size 26763
+oid sha256:14ca9e54bb88b909ed595cded87f2d51613afac18176f9df5b9ed6b655628353
+size 26430
