aad30c24cf5ebcbb5e5b0475dd3daa877e274f58
Andres Morales <anmorales@google.com>
2015-05-07 13:52:55 -0700

    mkbootimg: make mkbootimg print image cksum to stdout
    used by the build system as a fingerprint for the image
    
    Change-Id: Ifaf230b881e68d921a8158ed2e8a3ee41f27a4b3

--- mkbootimg/mkbootimg.c
+++ mkbootimg/mkbootimg.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:350eda0eb01963a65f5ab5f7109ce487c5a26dc2ef2dc0094fd2c06b85a64c01
-size 8065
+oid sha256:4beb30216e00360421e7126359c999581a1e7ab22c340f3190f5dc3ba1fa54f4
+size 8658
