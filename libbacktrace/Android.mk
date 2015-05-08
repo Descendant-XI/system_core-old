e55e0a627ff88eb0b8b1677d7c05b0815dc87763
Christopher Ferris <cferris@google.com>
2015-05-08 15:44:46 -0700

    Add tests for elf unwinding in memory.
    Bug: 19517541
    (cherry picked from commit 67aba6881d8857d3017e11695207eb2ade45a274)
    
    Change-Id: I914636ccd814e041475b6b2d81119cac1745a9ff

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d7c3bcfa8fc9d68ae7dbc77d03fc818d5266d2c8a78c48e7c09e1125dff07fe
-size 3776
+oid sha256:f3d100397e0932dc8dd41dba446cc9e4f92add2e43698bb2c105545d5f97f6e2
+size 3781
--- libbacktrace/backtrace_test.cpp
+++ libbacktrace/backtrace_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f55a938f784022dd7f5de9f4a216e8876dc070b29362b9d902ec4e30e169e030
-size 34692
+oid sha256:a3a7ec782f0efcd7813ed3dd561d779c82e9fa6459973ad23cfcfcbb821e45d4
+size 44383
