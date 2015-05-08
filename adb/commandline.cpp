e8a6ec5b53bc9cc913c2428730970c60165c4a5f
Elliott Hughes <enh@google.com>
2015-05-07 23:37:40 -0700

    Fix "adb remount" for devices without an oem partition.
    On a device without an oem partition, we now have an /oem directory
    anyway. This causes find_mount to fail, and that was returning nullptr
    from a std::string-returning function. Boom!
    
    Also clean up the bits of code I had to trace through between "adb remount"
    on the host to the crash on the device as I debugged this.
    
    The only other meaningful change is the error checking in
    adb_connect_command --- adb_connect can also return -2.
    
    Bug: http://b/20916855
    Change-Id: I4c3b7858e13f3a3a8bbc7d30b3c0ee470bead587
    (cherry picked from commit 5677c23e8d0c085be8d8429a5d125147d11e9bb2)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d699206fc8f44f012cb118e2c4563d70134f00e1bb0b17461df691d134a23be
-size 58418
+oid sha256:c78d4a0e86382d5658c85d645abc85caf167fdfd670bd79ea9d356d8899a0e12
+size 58307
