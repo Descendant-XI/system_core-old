fb2bb3d3b15cc0c45b4522e1643dd74aac3417ef
Elliott Hughes <enh@google.com>
2015-05-07 21:56:31 -0700

    Try to include the SHA in a ddmslib-compatible way.
    Bug: http://b/20918202
    Change-Id: I0c1a48459372b0d28aaf9d09d82540e44b438c9c
    (cherry picked from commit f3bbfa6a216669c4cd91d5f936662e89e0ae07ca)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c05f4b47e3ea4e27e15e7907b9193d57b408ffc0114e410bb092a1a355a88522
-size 58408
+oid sha256:8d699206fc8f44f012cb118e2c4563d70134f00e1bb0b17461df691d134a23be
+size 58418
