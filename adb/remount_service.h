0bf82cbaeb2e3da93516ba5a1dc186629b3a677a
Elliott Hughes <enh@google.com>
2015-05-11 11:55:25 -0700

    Failure to find an oem partition should not be a remount failure.
    Many devices don't have an /oem partition, so find_mount should be
    expected to fail, but shouldn't cause the overall remount to fail.
    
    Also clean up all the error handling and reporting, and remove the
    dead int* globals.
    
    Bug: http://b/21024141
    Change-Id: Ie31021b03c9cab8e972269d7d1ffe383cd30ee9e
    (cherry picked from commit 9aa4fda4e64c1882faf019cc2a483ee4917e0c85)

--- adb/remount_service.h
+++ adb/remount_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7326e9154bff3f96a48b220441b80efc94108a3e5a4ed53b5438852b52f7de76
-size 791
+oid sha256:db6143db257e89ad0e087109f2fac1afadaaf45c57250a0173f30e9c9b5be8ec
+size 792
