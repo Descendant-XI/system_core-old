e156ddf12169b05e8c1d74e5095cfb850080dff7
Narayan Kamath <narayan@google.com>
2015-05-11 16:59:46 +0100

    Fix file descriptor leak when opening invalid archives.
    Also add -Wunreachable-code to the set of compiler flags, otherwise
    noreturn becomes considerably less useful.
    
    bug: https://code.google.com/p/android/issues/detail?id=171099
    Change-Id: I9a95d45633c731c7046d4e4a39844d9cebfd1718

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a81045e1246a85cbcaf16d01946810065724bc5106e94d108a28b9d3036d34d
-size 3137
+oid sha256:66a221cdb77969e7cb73e67143abe0ed3bcceea627e4d555f34de790cc9508a8
+size 3156
