7bf647c2020e60dc50317df92d51b4cd1a0fc2d1
Tom Cherry <tomcherry@google.com>
2015-05-12 13:54:41 -0700

    Fix insmod module size
    read_file() used to append a new line character to the end of the buffer it
    returns, because parse_config() isn't able to cope with input that's not
    '\n'-terminated. Fix read_file() to be less insane, and push the workarounds
    into the parse_config() callers.
    
    Longer term we should rewrite parse_config().
    
    Change-Id: Ie9d9a7adcd33b66621726aef20c4b8cc51c08be7

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79f6824e3533b8b6822ec48aea8db9e22bd474af722dcc7fa6e0278ff451fb2c
-size 11699
+oid sha256:22400287004a1210cae6d6b2a657588e8f96384cbf23cd465713049c584f76f1
+size 11642
