65c3d1da2b3e3a850eb515ff4bf5a55c64c63b32
Elliott Hughes <enh@google.com>
2015-05-14 12:03:14 -0700

    Reduce the coldboot timeout to 1s.
    5s was already a ridiculously long time to delay booting, and some OEMs are
    trying to make it even more insane:
    
      https://www.codeaurora.org/cgit/quic/la/platform/system/core/commit/?h=lp&id=fd23edd48272976d2fb333f377242173f92aa343
    
    Let's at least ensure that Nexus and Android One devices don't take forever
    to boot...
    
    Bug: http://b/19899875
    Change-Id: I9680c166a759360f34118e51cd0645e12b6bd5c8
    (cherry picked from commit c7331d02d1c6b1e51b3a1210506f38fd61819495)

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e95927c1c871ef84bba53972de6bd3a25fe4c48dd79d9fa1d1e02e026e3b585
-size 33216
+oid sha256:14a6096e8f94371b2b661b3e6f0b8f032d3623802f03624a162db4b7e1cb1e46
+size 33416
