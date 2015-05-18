4bf99133465f97c140b9337ab655caa2cc70d612
Dan Albert <danalbert@google.com>
2015-05-18 16:43:57 -0700

    Make connection states a proper type.
    Change-Id: I809f9b327c832b88dd63151bf7dcb012d88e81c4

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76659e302edb971c743466da94bacf2dcf1699fe63975413512d1e022b1a7da8
+oid sha256:ed0ff890f1cafe26395c7536173e875ad4359be989b276d7c5c8d72952981f1c
 size 29351
--- adb/transport.h
+++ adb/transport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fde6cdf20ebbb7da7cf85b0f1dc2aa47f6036ae2cbc18fc619f34bafcf0841d1
-size 2399
+oid sha256:07f966e344e4e5e9e63e24e9d4fe6f7a5a8b46a2d28e12bb1e0a1bcab2d7c218
+size 2409
