a05e8d509e65a8ee321479c5c7c02750b63d7deb
Spencer Low <CompareAndSwap@gmail.com>
2015-05-20 23:17:26 -0700

    adb: win32: fix logging to adb.log
    In the adb client, redirect stdin and stderr of the adb server to `nul',
    so that when the adb server starts up, it avoids issues in the C Runtime
    where it closes stderr, making it hard to properly reopen. There are
    probably other ways to avoid this issue, but I think this is the
    cleanest that will keep working over the years and will exercise the
    most commonly used code-paths in the C Runtime.
    
    Fix some adb_close() calls to be unix_close() (only really matters on
    Windows).
    
    Make stderr non-buffered on Windows, to match the (sensible) Linux
    behavior.
    
    Change-Id: I1b15c64240e50dbeb56788b0d0d901f4536ad788
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98d0a073c25acd41caaf1b1c7fb72526e3e98d46e8284da02111f8c3ff84b0b1
-size 29911
+oid sha256:c2877755f7819c7f0a0c75fbc0ed1a4f1c80b62f2999441c3f3ad4771b32f9e8
+size 31435
