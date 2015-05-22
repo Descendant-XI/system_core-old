8257c4612b99a3e3be1bf5957e6e613339215adc
Sami Tolvanen <samitolvanen@google.com>
2015-05-22 15:43:50 +0100

    Error correction: Use libfec in fs_mgr
    Use libfec to read and parse verity metadata to protect against data
    corruption.
    
    Bug: 21893453
    Change-Id: I3a3543e0d999316707302b3be8735a7133d22946

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6de3dd8dedb6ce5bc702b639e8cd72c4f2e5c45e412d25f1bc144c97da689e3
-size 1339
+oid sha256:028c6458c006944ac2578fbdcaddebc86ed1878c32bfdffb52715fb6e55cf969
+size 1473
