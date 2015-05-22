467e90e8f6ba7a13c7513e03f1df4d6bad02469f
Christopher Ferris <cferris@google.com>
2015-05-22 14:26:13 -0700

    Refactor dump_memory function.
    - Add dumping memory around registers for x86/x86_64.
    - Add unit tests for new dump_memory function.
    - Cleanup all of the machine.cpp files.
    - Increase the high address check for 32 bit, and decrease the high
      address allowed for 64 bit slightly to match mips64.
    
    Bug: 21206576
    Change-Id: I6f75141f3282db48b10f7c695a1cf2eb75a08351

--- debuggerd/x86/machine.cpp
+++ debuggerd/x86/machine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d810d2ee20f3d20647e1b0a59e22ecc48ed0d6b9fdb9807594c6ab1a45aef0c
-size 1531
+oid sha256:008f7543b6549163d82b52ddc2e957999220a0a4ac88dae182a71e87f11a9a12
+size 2270
