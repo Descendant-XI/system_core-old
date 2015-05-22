9d7a3a50f05aece02c5112fedbf1676a37d125bb
Thierry Strudel <tstrudel@google.com>
2015-05-22 15:56:14 -0700

    init: wait_for_file use smaller time resolution
    As 1s timeout can be requested, using second resolution time to check
    for timeout prevent from being accurate on the actual time we wait.
    Use available gettime_ns instead.
    
    Bug: 21374269
    Change-Id: I8be1b69e02beacc7196427f97855e036addf54d1
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:688ecafc1cc2ce162ce21043714490e0d5fdc6cc7d17bd1de64032cd107d8959
-size 11795
+oid sha256:21780d235afc7fb30185259e2edc37e7335a11c7724a9bad2ffe2454cb6d5812
+size 11832
