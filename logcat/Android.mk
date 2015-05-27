5197aa75eb89b404391618c16089b1e504ce8ea2
Mark Salyzyn <salyzyn@google.com>
2015-05-27 07:39:56 -0700

    logcat: -f flag to continue
    (cherry pick from commit f3555d9427425c2cba9600ceffb49305c440aa4a)
    
    On startup, check the current logging content, then grab logs from
    that time forward rather than restarting from the beginning. Add
    support for reading tail time down to the nano-second.
    
    Bonus, permits us to create a logcatd logpersist daemon
    
    Bug: 19608716
    Change-Id: Iaab58de4c02b7f97c69de12cf954fedc2163059e

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab75c252fa9e9ff1719aefcc72c94b044411996a1436efc0a4fc7f701157e64
-size 314
+oid sha256:476eebdcee06805dbeccd0a1fb9428b4973886927a83427fd59c7db8b4a0fa29
+size 322
--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bc5b13d821395d94d83e3885e7c7c1f25059fc15ee0b774d8d2ba2bdd9984f6
-size 28380
+oid sha256:9e546bc96e7f16aef7e7240b45f29da8fa945aad1bd39c83f7471d94c47e2680
+size 31275
