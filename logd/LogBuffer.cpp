e2a82f72f136cf44a0f0ffe3b8a1218c2864c496
Mark Salyzyn <salyzyn@google.com>
2015-06-01 09:41:19 -0700

    logd: KISS & fix preserve a day
    (cherry pick from commit 5921276a16528bf79292e828080bf0ec984cbb23)
    
    Code in 833a9b1e38ce65f2cdf3ebd095aaa99a92eb9467 was borken,
    simpler approach is to simply check last entry (to save a
    syscall) minus EXPIRE_HOUR_THRESHOLD. This does make longer logs
    less likely to call upon the spam detection than the algorithm
    being replaced, but sadly we ended up with a log entry in the
    future at the beginning of the logs confounding the previous
    algorithm.
    
    Bug: 21555259
    Change-Id: I04fad67e95c8496521dbabb73b5f32c19d6a16c2

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ada54793b7e82644e55a99bfea9b7057164f14d9b1aa31edcc4585421aa6182c
-size 19591
+oid sha256:469757f64439ec7ff62ce6bd70eff7531b04fd9e0a303e601c4056221a929227
+size 19106
