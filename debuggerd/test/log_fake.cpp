d94c4cf939957159d46fdf54381859ad21fc67a2
Christopher Ferris <cferris@google.com>
2015-06-02 14:52:44 -0700

    Change the 64 bit map address format.
    Previously, the map printing in tombstones for 64 bit devices uses
    a variable length value. This means that the maps are not lined up.
    The new format is to print the map as 00000000'00000000 in all ways.
    
    Also fix a bug where the backtrace_map_t did not initialize all
    parameters.
    
    Add unit tests for all of the dump_all_maps function.
    
    Bug: 20950813
    Change-Id: I30901c8a0251b00d85c4c01476b033ef8db84e32

--- debuggerd/test/log_fake.cpp
+++ debuggerd/test/log_fake.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddfe22d18315df3c0a316ffe72bf873b0971d8af9667539b4023632bbe25d6cb
-size 1407
+oid sha256:d74ee8ad13b79c2d5347c808ace431498c12a38b6b90691189d28853416c81b7
+size 2133
---
+++ debuggerd/test/sys/system_properties.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d48ef2feddc79edbac102354aeaf1bd506cbc6b937ebf843e9e9280e08cc8574
+size 1677
