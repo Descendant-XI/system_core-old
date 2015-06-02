af82337f1c8fd0592de14a41dd788c81eef369d9
Mark Salyzyn <salyzyn@google.com>
2015-06-02 11:11:02 -0700

    init: change exec parsing to make SECLABEL optional
    (cherry pick from commit 17fff893c04971b519d25d52b07f51111353cba5)
    
    Allow SECLABEL to be - to denote default
    
    Change-Id: I58cdc6c805dc6e50dc42b7e13e72d0eaf4864f11

--- init/init_parser.cpp
+++ init/init_parser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:814bdaf19d4250556982e87b0ae612cfb37b5e517956b9c837f252368f12d944
-size 30076
+oid sha256:012f44f2fc0b7888274fd27d525a03ed9c5f8b60d58f2ef083b1d54e9d4c1600
+size 30147
