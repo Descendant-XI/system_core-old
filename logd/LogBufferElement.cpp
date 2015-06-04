0d035820222c1b6c6c0559dafc282ba88be42c96
Mark Salyzyn <salyzyn@google.com>
2015-06-04 13:35:30 -0700

    logd: filters remove leading expire messages and rate
    (cherry pick from commit 047cc0729fd837a03b99db901941c1421ef15f96)
    
    - Cleanup resulting from experience and feedback
    - When filtering inside logd, drop any leading expire messages, they
      are cluttering up leading edge of tombstones (which filter by pid)
    - Increase and introduce EXPIRE_RATELIMIT from 1 to 10 seconds
    - Increase EXPIRE_THRESHOLD from 4 to 10 count
    - Improve the expire messages from:
       logd : uid=1000(system) too chatty comm=com.google.android.phone,
                                                       expire 2800 lines
      change tag to be more descriptive, and reduce accusatory tone to:
       chatty : uid=1000(system) com.google.android.phone expire 2800
                                                                   lines
    - if the UID name forms a prefix for comm name, then drop UID name
    
    Change-Id: Ied7cc04c0ab3ae02167649a0b97378e44ef7b588

--- logd/LogBufferElement.cpp
+++ logd/LogBufferElement.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f22e30ac1aeb7b16519089a32e0fb0f3d79fb63a998e09744dc8c80ad883f80
-size 6102
+oid sha256:dd4a19020ab217ce1c284633af4cb56f9dde7b8acf8fc1854d3d28985ab959d2
+size 6234
