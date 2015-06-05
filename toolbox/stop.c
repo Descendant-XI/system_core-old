dc986b0bb0778ecec3070f8c02c41e8102f7493d
Elliott Hughes <enh@google.com>
2015-06-05 16:43:23 -0700

    Make start/stop warn if you're not root.
    Bug: https://code.google.com/p/android/issues/detail?id=165602
    Change-Id: Ia1e2de1697d971381b0c949fdaaeff424ab8bf32

--- toolbox/stop.c
+++ toolbox/stop.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aed82d72368f633d8ab209062a44c03cef254021a3c0a29d7081c7a2afc18839
-size 456
+oid sha256:43de149aba9ed190d74020c8afc7ce3e25863603b1d3ccf6455e6199dd574c69
+size 59
