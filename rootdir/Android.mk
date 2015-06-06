27d31119948d2727a384aed140f20bc89560e9f7
Daniel Rosenberg <drosen@google.com>
2015-06-05 18:11:53 -0700

    rootdir: Create folders in Android.mk
    system.img may contain the root directory as well. In that case, we
    need to create some folders init.rc would during the build.
    
    Change-Id: I157ccbebf36bee9916f3f584551704ec481ae1d1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da573c0a2e7c63514c37efcdab1cf5b5ad0c2f2a217500b8a8a792b0187358d9
-size 1478
+oid sha256:6a49cda3ea3bc9196023f41b72d6b9210df1e004b2db23c006460d73c91e7a74
+size 1507
