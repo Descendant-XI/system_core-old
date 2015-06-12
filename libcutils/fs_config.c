04193ccda1a78da1ce289edcfcc7b16a8ae4eef6
Daniel Rosenberg <drosen@google.com>
2015-06-12 15:00:54 -0700

    libcutils: fs_config: Add folders normally created in init.rc
    When the ramdisk is built into the system image, we need to create
    those folders during the build.
    
    Change-Id: I40881940c75e379d7599dc1f743b35fbc7cc66e8

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1205140ffcffa82d56a36641e1c3d6d60ee59dd24d7245236f353496fc97a5c6
-size 10158
+oid sha256:bf903fbedaf8d398ead4d65767ad95886d826d6246155d6e4b3951f4fb62e35f
+size 10364
