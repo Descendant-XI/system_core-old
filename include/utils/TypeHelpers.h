99c1526f6c147275215e3214632cd012fc9b2ad0
Nick Kralevich <nnk@google.com>
2015-06-13 15:28:53 -0700

    TypeHelpers.h: Don't underflow unsigned int
    When decrementing "n", eventually n will equal zero. When that
    happens, n-- underflows. This causes a crash when code which uses
    clang's -fsanitize=unsigned-integer-overflow is run.
    
    Avoid trigging an unsigned integer underflow.
    
    Change-Id: I9705be6580d61a164ef5fb1ec77e98a69d888438

--- include/utils/TypeHelpers.h
+++ include/utils/TypeHelpers.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c88e1e955cb8a34f06b8164b9d3eb8dfc2d067b286614da7107527870691a6f8
-size 9146
+oid sha256:8da0caa132a139ae2dce8c311f550458c7799cbda27b408ffc704007441d3c83
+size 9279
