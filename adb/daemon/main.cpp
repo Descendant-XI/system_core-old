51697ed6a04c4096ab0bac1ecdcc1df6b9478434
Nick Kralevich <nnk@google.com>
2015-06-12 22:03:50 -0700

    Remove calls to is_selinux_enabled()
    d34e407aeb5898f19d4f042b7558420bbb3a1817 removed support for
    running with SELinux completely disabled. SELinux must either be
    in permissive or enforcing mode now.
    
    Remove unnecessary calls to is_selinux_enabled(). It always returns
    true now.
    
    Change-Id: Ife3156b74b13b2e590afe4accf716fc7776567e5

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d47b59d781998871c86cc98f2d8fd55be46a3b0b805ca54a03057388ab64bd5
-size 8618
+oid sha256:2c74d1e8c73b2ca614c0dc4ffe6b384820c4566e01037cad47a1d773366cb816
+size 8586
