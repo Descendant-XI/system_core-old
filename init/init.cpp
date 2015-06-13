7df70088650e2d38bc1068aa42dbf66dea11656b
Elliott Hughes <enh@google.com>
2015-06-12 18:02:20 -0700

    init support for cgroups.
    This adds the "writepid" option that instructs init to write the child's
    pid to the given filenames (such as /dev/cpuctl/bg_non_interactive/cgroup.procs
    and/or /dev/cpuset/foreground/cgroup.procs).
    
    Bug: http://b/21163745
    Change-Id: I121bb22aa208bc99c4fb334eb552fdd5bcc47c1a
    (cherry picked from commit d62f0608d9d67bf647cf15debbd163e84584fe44)

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25176af920d1e921a07d2b3963087766a22e522c6c73a3d0d2f07f8431dd1021
-size 33443
+oid sha256:041e8f4056442ef1abde56af52fc7478da1e6e722ac28dd4a92e8241a1d2188a
+size 33857
--- init/init_parser.cpp
+++ init/init_parser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:012f44f2fc0b7888274fd27d525a03ed9c5f8b60d58f2ef083b1d54e9d4c1600
-size 30147
+oid sha256:b5ab659ad8d0ff44eb6354ca9b6fa18ba1fa2fc26fee19d8199b07d0ea8432c2
+size 30542
