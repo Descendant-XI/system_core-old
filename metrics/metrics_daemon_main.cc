7d695d3639789165d5cd13159eed12230747488d
Alex Vakulenko <avakulenko@chromium.org>
2015-06-15 12:53:22 -0700

    platform2: Fix issues with new version of libchrome
    libchrome r334380 has the following breaking changes that need to be fixed:
    - base::JSONWriter::Write() and base::JSONWriter::WriteWithOptions() take
      "const base::Value&" instead of "const base::Value*"
    - base::JSONReader::Read() and base::JSONReader::ReadAndReturnError()
      return a scoped_ptr<base::Value> instead of base::Value*
    - base/safe_strerror_posix.h is moved to base/posix/safe_strerror.h
    - safe_strerror() is now in "base" namespace
    - StartsWithASCII(), EndsWith(), StringToUpperASCII(), LowerCaseEqualsASCII()
      are now in "base" namespace
    - ObserverList<T> is now in "base" namespace
    - base::PrintTo(base::FilePath) used in gtest is now moved to libchrome-test
      library and as such, unit test runners need to link to this library now.
    - crypto::RSAPrivateKey::CreateSensitive() is now removed from //crypto, so
      some of tests in chromeos-login that used that function had to be changed
      to use crypto::GenerateRSAKeyPairNSS() directly.
    - UnixDomanSocket class is now in "base" namespace
    - Pickle class is now in "base" namespace
    
    BUG=chromium:496469
    TEST=`./build_packages`
    CQ-DEPEND=CL:277662
    
    Change-Id: I36e5fbf2e36a92068873ffbd44020c862a3ed9e3
    Reviewed-on: https://chromium-review.googlesource.com/277671
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Alex Vakulenko <avakulenko@chromium.org>
    Trybot-Ready: Alex Vakulenko <avakulenko@chromium.org>
    Tested-by: Alex Vakulenko <avakulenko@chromium.org>

--- metrics/metrics_daemon_main.cc
+++ metrics/metrics_daemon_main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de528d5d48c58a69aa44470cf8180eb3414434a6d2fc472f999246e3fe55a84b
-size 3403
+oid sha256:3e81cbfaf2f0f3430638ae5311d78f57fe971e8f8b9c153e0b7166b4cb78b6b2
+size 3409
--- metrics/serialization/serialization_utils.cc
+++ metrics/serialization/serialization_utils.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c0ebfe54e0a2e9d68c50a564c3f93f385bba2c2f35c5fc8845fd01fb4f74076
-size 6912
+oid sha256:c587f2f35dfed07ad1bc2fa526da636cc7a0db067b7da1ca263201e85befeaf4
+size 6942
