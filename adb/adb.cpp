f290c15a95c03553f6f6464b137058c760d0b8fd
Elliott Hughes <enh@google.com>
2015-06-17 15:23:42 -0700

    Ignore ro.adb.secure in user builds.
    Require authorization by default, and remove the ability to override
    that in user builds. (userdebug and eng are still free to do whatever
    they want.)
    
    Bug: http://b/21862859
    Change-Id: Ibf8af375be5bf1141c1ad481eee7a59fb10a7adb
    (cherry picked from commit 5cba504215ea91187cc36ec7aec5dce1b0f4b0fe)

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1c49c991d3ec320346498d582eda5358171a0ecf6cc0d74e3f2c6c6e7bea1a
-size 29333
+oid sha256:c2d56789afd54373a5ba07ae43c9b45927d578b9adae30a633a7ff8dbe3b99cd
+size 29335
--- adb/adb_main.cpp
+++ adb/adb_main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa220efe275f6c9dd5b48f4d82344cee8104434d0aa2475545765ffcbd19754
-size 11441
+oid sha256:e5dbc945ef9a7edf37fcc2345e8e5cd6207a30a9f5f93e2d2a8303f56b6ba9ca
+size 11442
