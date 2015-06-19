f1ffb3b25993ca8acf51042a9e9252cc87a415c7
Nick Kralevich <nnk@google.com>
2015-06-18 17:46:54 -0700

    init/util.cpp: don't return a negative unsigned value
    android_name_to_id() returns -1U on error, which causes a
    crash when the following clang options are enabled:
    
      -fsanitize=signed-integer-overflow,unsigned-integer-overflow
      -ftrap-function=abort
      -fsanitize-undefined-trap-on-error
    
    Rather than returning a negative unsigned value (which doesn't
    make a lot of sense, IMHO), return a positive unsigned value.
    
    While we're here, add logging on decode_uid failures.
    
    Bug: 21880301
    Change-Id: I652e4c1daa07c7494cceca2b4e1656b9158f2604

--- init/util_test.cpp
+++ init/util_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bc6146b904c89030cf83b7e077b2107be077c7ea692f50823f9ddade759302c
-size 1242
+oid sha256:b1fccae363255c95fddafdaea658f1b4b27f0140d19a35f89c0c66fad0dfb98f
+size 1247
