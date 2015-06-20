401a1f214b2d9025bbaeb6835bdfbc66bf995623
Yusuke Sato <yusukes@google.com>
2015-06-19 17:04:15 -0700

    Add |optional_suffix| to StartIteration()
    so that PackageManagerService can iterate over files with a
    specific file extension like ".so".
    
    Bug: 21957428
    Change-Id: I76ed9560d4d1e00d297a97d518ec357166be1981

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac21d238db1c1f32c57f3d59db0444e25ebcae176d31b883741e1b0a139c9cc0
-size 43044
+oid sha256:10d398705721632b5939fc9f18a755bad81df1d3c3e4f33e9c090fb8226b15d9
+size 43789
