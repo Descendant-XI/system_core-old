f62d4b6ef60cf839933af7f7130ef613fba72a84
Jeff Sharkey <jsharkey@android.com>
2015-06-23 14:30:37 -0700

    Let's reinvent storage, yet again!
    Now that we're treating storage as a runtime permission, we need to
    grant read/write access without killing the app.  This is really
    tricky, since we had been using GIDs for access control, and they're
    set in stone once Zygote drops privileges.
    
    The only thing left that can change dynamically is the filesystem
    itself, so let's do that.  This means changing the FUSE daemon to
    present itself as three different views:
    
    /mnt/runtime_default/foo - view for apps with no access
    /mnt/runtime_read/foo - view for apps with read access
    /mnt/runtime_write/foo - view for apps with write access
    
    There is still a single location for all the backing files, and
    filesystem permissions are derived the same way for each view, but
    the file modes are masked off differently for each mountpoint.
    
    During Zygote fork, it wires up the appropriate storage access into
    an isolated mount namespace based on the current app permissions.  When
    the app is granted permissions dynamically at runtime, the system
    asks vold to jump into the existing mount namespace and bind mount
    the newly granted access model into place.
    
    Bug: 21858077
    Change-Id: I5a016f0958a92fd390c02b5ae159f8008bd4f4b7

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64b947edaf574fd50a7a61a9ecf195c4095fd42c56f77465845882e0e3165b61
-size 64972
+oid sha256:51694d771825a65cfe28a5572fbb58b6ee50f51040bb339b966cd6fe9b082c1e
+size 59876
