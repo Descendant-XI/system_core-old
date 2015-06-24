2751eee6c68186b85a80c2d120ea6099d939b647
Mark Salyzyn <salyzyn@google.com>
2015-06-24 16:22:54 -0700

    logd: serialize accesses to stats helpers
    Quick low-risk to resolve possible hash table corruption.
    Resolved an unlikely path memory leak.
    
    ToDo: replace lock with nested lock so no lock
          helpers are required.
    
    Bug: 22068332
    Change-Id: I303ab06608502c7d61d42f111a9c43366f184d0c

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cf367acea3d6ae25c2350936218c49522d64c268e78bdf0397205880b9d6434
-size 2663
+oid sha256:3e3bf25fc39bb3b8a31d2b97faa35dc62adcf85444a984929a2844e2094881e6
+size 2845
