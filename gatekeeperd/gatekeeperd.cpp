85343bb3e2c51465cbbf50fe4300506f66d05b0e
Andres Morales <anmorales@google.com>
2015-06-24 10:21:16 -0700

    [gatekeeperd] clear state and mark on cold boot
    required to initialize state by certain HAL impls
    
    Bug: 22011857
    Change-Id: Ibb01a799da983e1a930aae946c331b23f571861d

--- gatekeeperd/gatekeeperd.cpp
+++ gatekeeperd/gatekeeperd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47b8013d4bfeb478414077c9b132a20b9c039ec7ed457364d391c2cfbb4cef20
-size 11722
+oid sha256:0436ef96cb1fb552abf12c1871940743bdaa9a8262bf624e66421a287b8ca138
+size 12506
