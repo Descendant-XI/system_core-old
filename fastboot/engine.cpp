f26c6556b01d73df7478064b045ec2294bcb031f
Elliott Hughes <enh@google.com>
2015-06-23 20:27:58 -0700

    Move fastboot to C++.
    Minimal conversion.
    
    Change-Id: I32cbf125be481a8757720d10fa303c38a7fd5e38

---
+++ fastboot/engine.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbf15a94644be3e9afec2ddd53262e300b698a6fb8ecbcd173bebde75051b3a7
+size 10803
---
+++ fastboot/usb_linux.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:473409d9ec98833adec1445b545e96ee99acc78fbc13816a5d4a4e1e83f61e54
+size 13485
