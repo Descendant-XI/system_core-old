e5fcd7e2792b9a0f5065e5ba328f4bc32c7aa5b0
Sauhard Pande <sauhardp@codeaurora.org>
2015-06-24 22:38:25 -0700

    Camera: Add feature extensions
    This change includes below commits:
    
        Camera bringup changes system-core
        Change-Id: I1cf98641eca9096bd27645e07ea802646ea1fb96
    
        system/core: Fix for HAL compilation issues while integrating HAL 1.0
        Change-Id: Iead9c1ade279b64c5cbdf4d2de1a8b695939c52a
    
        Camera: Add enum to specify the frame type
        Added enum to specify the frame type of either fd/data buffer
        CRs-fixed: 654901
        Change-Id: I1c0b1a2c6a1425cdb6650cdfc20ca65835a1b81f
    
    Change-Id: I654a40661e6e101da2a06986abeceb20639cccd9

--- libsystem/include/system/camera.h
+++ libsystem/include/system/camera.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f02a9968ac1c505cc4f6a4d54be36d233632057902d116b9f849f7fbda613eaf
-size 12037
+oid sha256:2a7bfcf0794999dfa13879d9ec9066fa85b79a7c6fd7fbe636d0b0791ddbed1d
+size 13785
