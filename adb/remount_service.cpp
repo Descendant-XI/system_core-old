22dd5618675ce76f712f7c3b87acd2705d26489a
Daniel Rosenberg <drosen@google.com>
2015-06-29 17:30:28 -0700

    Fix "adb remount" for when the root directory is in system.img
    When the root directory has been built into system.img, it is
    mounted at /.
    
    Change-Id: If01d12efeaa53b4ae59e801a6e9b802a9ae5882d

--- adb/remount_service.cpp
+++ adb/remount_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e98c9f040ac54bc53c10443a2b1c323ccd368488117c3fd3e4464cb85d26c7aa
-size 3687
+oid sha256:71fe516de6fad2b48f8f5727817ba55d7e7825456c98aed78b00ff3a67af98f2
+size 4798
