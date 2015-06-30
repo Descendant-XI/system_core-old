85052c0c74693967f0565aa81dff4b54e1018b99
Daniel Rosenberg <drosen@google.com>
2015-06-29 17:33:05 -0700

    Skip mounting /, just mark block device as ro if needed.
    Change-Id: I7fbb636d296abc1caab6c7bf88017684c9df7759

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e9091f6d4e8f3c0c79cabc4ddb1faa83a6a93cc628e232bc269d55aa9ffbf9f
-size 28516
+oid sha256:202e11cf1734c6702e78a93c74be061033807652e856f7904d1551f78424fe76
+size 28832
