9e934cf8f1b9415003f67285884a18f9f5472c79
Elliott Hughes <enh@google.com>
2015-06-30 08:22:24 -0700

    Consistently use strerror in libutils.
    It's easier for people to debug, and side-steps the problem that errno
    values differ between architectures.
    
    Bug: http://b/17458391
    Change-Id: I1db9b2cbb653839d3936b91e37e5cff02671318a

--- libutils/Looper.cpp
+++ libutils/Looper.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dcf965f64a5e66c0ce7e3bf4298e92df896ba49acbcae7915ad4d474b30513b
-size 18153
+oid sha256:d798f42779c94b6f240f69dc7a8cc0450734f3e55c302a9b30f7505864dc32ef
+size 18227
--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65847c5f376fc91d448609f8eedbeceb731d15b2570f51f2ae5b8a399361533c
-size 25182
+oid sha256:2bcb0c67b65bdc4e41d520528e6167eb4abbb50e814157e355ed658a73997b56
+size 25206
