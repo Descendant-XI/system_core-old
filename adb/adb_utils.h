5eefdc87e58af4c6abdf79a886adbc42a8fa44f3
Dan Albert <danalbert@google.com>
2015-07-08 13:50:42 -0700

    Turn on -Wformat-nonliteral.
    Apparently there are two classes of this warning in clang.
    -Wformat-security is only emitted for cases of
    `func(nonliteral_fmt_string)` (no args), and -Wformat-nonliteral is
    emitted for cases *with* arguments. For whatever reason, the latter
    isn't included in -Wextra and must be manually enabled.
    
    To make this more easily portable to Windows, move the existing
    gnu_printf/__printf__ decision into base/macros.h as ATTRIBUTE_FORMAT.
    
    Change-Id: I3b0990e1d1f0a2e9c13b32f5cd60478946cb5fc6

--- adb/adb_utils.h
+++ adb/adb_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce774bf9606f99752f62773bd3f8b086a7eae60c8f63f01e33e02c7b9fe62e99
-size 871
+oid sha256:3e1892635ea6dd247558b724050f104718064cac854468ebbf046428f8f13b39
+size 914
