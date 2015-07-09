2e847031fb50327354554caefd6caa492b7321e4
Andres Morales <anmorales@google.com>
2015-07-09 16:01:24 -0700

    [gatekeeperd] copy uid to local before passing to u_map
    The compiler will issue the unaligned access instuctions
    when reading from a packed struct. Since 'find' takes a
    reference, if we pass the field directly it is removed
    from its packed context and may be unreadable.
    
    Read the field out directly from the packed struct and pass
    in aligned to u_map to fix.
    
    Bug: 22367550
    Change-Id: Ia3b639c7518154ff5a2b7c233b752e154eab9aad

--- gatekeeperd/SoftGateKeeper.h
+++ gatekeeperd/SoftGateKeeper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61062e096bd167b44731fe09fafaefa28e91127aac8bb66fa0866568b29a3b68
-size 6018
+oid sha256:b7dfcfe53fc1dd2d761d24ee4a18bacdd750263a7e8b99973b4c44131b305c1b
+size 6092
