aba34372726d4d213257da3fd1c82e9cc6b7fd76
Daniel Rosenberg <drosen@google.com>
2015-07-14 17:15:57 -0700

    rootdir: Create cache folder in Android.mk
    system.img may contain the root directory as well. In that case, we
    need to create some folders init.rc would during the build.
    
    Change-Id: I312104ff926fb08d98ac8256b76d01b0a90ea5e5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a49cda3ea3bc9196023f41b72d6b9210df1e004b2db23c006460d73c91e7a74
-size 1507
+oid sha256:9fb8d6ed2d95083b8f95e6144f4be6b34b6b45225addedbd3068d65a0e8e05b3
+size 1513
