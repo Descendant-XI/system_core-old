1101296251b43240fd2a52d1ed7810b7caa61cd7
Tao Bao <tbao@google.com>
2015-07-18 19:46:16 -0700

    Set mode bits for /system/etc/recovery.img.
    /system/etc/recovery.img defaults to 0644. Change it to 0440.
    
    Bug: 22641135
    Change-Id: If55b94a9b9a3c746acc413d9c14ebb97ca4336d9

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef78ceab7948814bd593ae4ae7724fb829130aaf0e68d26618b234e5d5f983c
-size 10835
+oid sha256:a9df51f2657609d1eb251f116e693f04488a37cd29bd9fafb8fc669bb598e0ec
+size 10910
