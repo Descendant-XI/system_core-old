909b9bd35594b2cd275a5681018753d7888fdad8
Yusuke Sato <yusukes@google.com>
2015-07-21 15:50:59 -0700

    Stop calling fsck on shutdown by default
    Instead, run the command only when sys.powerctl is set to
    "shutdown,userrequested". This way, we can avoid running
    fsck when shutdown is triggered due to a low power state.
    
    This is a follow-up CL for http://r.android.com/158525.
    
    Bug: 21853106
    Change-Id: Ie57c23cd25162cc2a8726f876a9ba212080105fb

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0dfa82134ac6ee52b749c9e6d65e91b7fdd8ebf09f1fdf505795d8fa4b10265
-size 24026
+oid sha256:c86740eda1ea898ac6979499c0b546aa8719712a7e2c3a307383a3ad8810cf1d
+size 24320
