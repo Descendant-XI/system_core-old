eadf85e660538e66aff256f2ecf5838548450995
Mark Salyzyn <salyzyn@google.com>
2015-07-23 09:22:50 -0700

    healthd: logd: add timestamp to kernel logged battery messages
    (cherry pick from commit acb1ddf56c98a75a49b263f99ef07ce361dc4323)
    
    Aid monotonic to realtime logging synchronization correction in
    the Android ecosystem by providing a periodic notification.  We
    now have the following messages in the kernel logs:
    
    - PM: suspend entry %Y-%m-%d %H:%M:%S.%09q UTC
    - PM: suspend exit %Y-%m-%d %H:%M:%S.%09q UTC
    - Suspended for %s.%03q seconds
    - healthd: battery l=100 ... %Y-%m-%d %H:%M:%S.%09q UTC
    
    Alter klogd to resynchronize on healthd messages as well.
    
    NB: Time using strftime format, %q is a reference to fractional
    second as introduced into log_time strptime method.
    
    Bug: 21868540
    Change-Id: I854afc0a07dff9c7f26d2b2f68990e52bf90e300

--- logd/LogKlog.cpp
+++ logd/LogKlog.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:421aae1428dd5eaff20a2b208586ba0a1234da04dbcf290a73917ae7394f79fa
-size 17765
+oid sha256:b29ede13d9ab23abc1afac4ead7c317a66660dd450e07145b9a2d7a8f21ff9da
+size 18243
