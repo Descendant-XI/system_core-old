92c1140daf27d6251909182b61edcfafd74588dc
Spencer Low <CompareAndSwap@gmail.com>
2015-07-24 15:38:19 -0700

    adb: win32: file descriptor reliability improvements
    When repeatedly opening and closing a file descriptor, the sequence of
    fds returned was: 100...227,100,100,100,100,100... Basically, the first
    wave was constantly increasing fds, but after the entire fd table was
    traversed once, the alloc algorithm would switch to returning the first
    free fd. This is sub-optimal for reliability because use-after-free bugs
    would be more likely to be hit because right after a close, the same fd
    would be given out next.
    
    This change makes the alloc algorithm use a persistent clock hand that
    walks forward through the fd table (wrapping around if necessary),
    searching for a free fd.
    
    This change adds locking for fd closing:
    
     - This prevents multiple concurrent closes of the same fd.
    
     - There was a race between alloc and close that wasn't guaranteed to be
       correct: close would set f->clazz to NULL last, but without any
       preceding memory barrier/fence, then the alloc thread would check for
       NULL. It probably worked out ok in practice, but it is probably best
       to fix this up with a lock (as in this change) or a memory barrier/fence
       (but this code isn't about performance, so why go with a complicated
       barrier/fence?)
    
    Also in this change:
    
     - Use errno = EMFILE for the out of fds case.
    
     - Clear FH->name
    
    Change-Id: Ic11d2a1a9d53996edfc1ca13566a2f46de4a4316
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- adb/sysdeps_win32.cpp
+++ adb/sysdeps_win32.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9adae2cd0f14127f788ed56a127cd9564e52b098d6d49c31be24e90054e6f07
-size 101515
+oid sha256:50324420d8cea4ee30986e7b1f0ec9af5a0e373535687b50a9924e63d0fc1483
+size 102085
