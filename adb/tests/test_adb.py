b3e4a28b226915c5f927fcddf667ba24f345257d
Elliott Hughes <enh@google.com>
2015-07-24 15:06:46 -0700

    Add a simple test for using non-ASCII paths with adb.
    Bug: https://code.google.com/p/android/issues/detail?id=8185
    Change-Id: If28a0ff345f44b60d6651bb36afb0bf72fe55e16

--- adb/tests/test_adb.py
+++ adb/tests/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4737ff0d60ed3ff2df9c6b53583de4f2876091c9e9a90dd25bddb040d47904eb
-size 16464
+oid sha256:f9c7bbd529f143592be897d3cd16c34c53c659a8c7fd91beff94072f2c6760f0
+size 17115
