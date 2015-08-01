d960290e1ff6e4609dc5f9655acc68586108402f
Yusuke Sato <yusukes@google.com>
2015-07-31 17:48:35 -0700

    perfboot.py: check 2 more event logs by default
    'sf_stop_bootanim' is useful for tracking the time wasted by
    showing the 'outro' part of the boot animation.
    
    'wm_boot_animation_done' is also useful for tracking user
    perceived boot time.
    
    Bug: 22207911
    Change-Id: I0481f93d08f49c0de07c46550d0a113074f30765

--- init/perfboot.py
+++ init/perfboot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c7ea33d19e09da734de7cd08440e567e8deb737ffdb1d83d703de7a7e3946a2
-size 14270
+oid sha256:f266ae1a8186059d7f5b897ca393b82184af5c9ac0645e4866132e9e7436fdc4
+size 14324
