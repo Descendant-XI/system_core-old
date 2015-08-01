7abb08d54df4b641c4a0dcfe3ef6cf0a46b93cf3
Spencer Low <CompareAndSwap@gmail.com>
2015-07-31 20:21:35 -0700

    libbase_test: win32: get some tests working
    * Get it building on Win32 by implementing mkstemp() and mkdtemp().
    
    * Run StringPrintf %z test on Windows because it passes because we build
    with __USE_MINGW_ANSI_STDIO which implements %z.
    
    Change-Id: Ia01f94e8258503381a1df6d3da6e40de59e57125
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- base/test_utils.cpp
+++ base/test_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ee7293623778ba189a88d73754ab3044f6f245af3cb05340d584d0b0189f15d
-size 1941
+oid sha256:d9d785dbe869ba4ab14839ef284ad046ffa08416ee886accf01efcc20cce6965
+size 2708
