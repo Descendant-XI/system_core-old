3e16cb3405f87099a20cd6f96124fc66fc241f22
Yasuhiro Matsuda <mazda@google.com>
2015-08-04 17:48:41 +0900

    Fix perfboot.py to exit by Ctrl+C.
    BUG: 22207911
    Change-Id: I0cc41f834207efd2965483c8636bbc709e54358f

--- init/perfboot.py
+++ init/perfboot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c7ea33d19e09da734de7cd08440e567e8deb737ffdb1d83d703de7a7e3946a2
-size 14270
+oid sha256:8828e502a266e11024914c918aa99cc391cc008087f8056e7fd01eb8d0812720
+size 14304
