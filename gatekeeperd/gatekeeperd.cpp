b3760eb4b4477c149c210f699f6462556dffb075
Andres Morales <anmorales@google.com>
2015-08-04 16:57:12 -0700

    [gatekeeperd] Check parent profile for SID lookups
    Bug: 22257554
    Change-Id: I1a363729b449a2bc8594b48dada719fd79da7036

--- gatekeeperd/gatekeeperd.cpp
+++ gatekeeperd/gatekeeperd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:858bc87727f36fcb5d717c3038ec61107b3f8d83c89c620dca62ad5659ef3e77
-size 12742
+oid sha256:babf1efb375cb3571a31984c555bd577b8eca1a13e42977fe0eac252c9d08a96
+size 13318
