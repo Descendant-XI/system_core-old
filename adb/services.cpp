e8d32d924252ca068033a2c66f2c97de64791dea
David Pursell <dpursell@google.com>
2015-08-10 12:52:16 -0700

    adb: Fix PTY logic for non-interactive shells.
    Change `adb shell` so that interactive sessions use a PTY but
    non-interactive do not. This matches `ssh` functionality better
    and also enables future work to split stdout/stderr for
    non-interactive sessions.
    
    A test to verify this behavior is added to test_device.py with
    supporting modifications in device.py.
    
    Bug: http://b/21215503
    Change-Id: Ib4ba40df85f82ddef4e0dd557952271c859d1c7b

--- adb/services.cpp
+++ adb/services.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1bf3e59b9afff0190949fe00fbca93652e91e4649309038c402a62a27b38082
-size 21276
+oid sha256:07ea8b702a735f02c771437c25bb19d5885f9fcafe0abb4d00e4301e179d7553
+size 22136
