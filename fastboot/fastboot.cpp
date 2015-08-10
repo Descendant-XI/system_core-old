94fe0d35fd4fef9870ea542091c19d140b4aa8b3
Elliott Hughes <enh@google.com>
2015-08-10 15:30:54 -0700

    DO NOT MERGE ANYWHERE Remove the USB vendor id whitelist from fastboot.
    (Backported from AOSP commit: e1746fda6a30ebaea22e492bc7821a9dee5face8)
    
    Change-Id: I661a0538776d4ba74fcee37e2964d987eb861342

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b2e6ace151fd5b96eb5e640f87886cd89145c490e40c4d7fa610f6e505f9460
-size 39225
+oid sha256:ad866af23a9bb5d64cfc2106d687f861554f0c064ee9ee230d909025ac2b6808
+size 38653
