20b8eb876325c4cfdc656a305ff6ef066e109ffa
William Luh <williamluh@google.com>
2015-08-13 10:41:58 -0700

    Add macro to call event logger for errors. DO NOT MERGE
    Bug:23180694
    Change-Id: I566a290334345f48013029534efef3e08e726e40

--- liblog/Android.mk
+++ liblog/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bfc24ea9ba96a77775b1adff6a18594462627e3d4e01d35746455d8e4a59812
-size 2488
+oid sha256:24ca4deb56ab86e516c3672f50da1035903e5584f396e6ba2a08dd4ac4578450
+size 2506
