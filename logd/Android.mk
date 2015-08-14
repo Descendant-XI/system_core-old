7ee394702348c68359bd510f3d19df5b83c29b80
Tom Cherry <tomcherry@google.com>
2015-08-14 13:02:51 -0700

    bundle init.rc contents with its service
    Bug: 23186545
    Change-Id: I52616b8ab1165fdef716f9b8f958665f2308c12e

--- logd/Android.mk
+++ logd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69f535f70840d7c3f36ed733f516461930f839e3fd5956168be14c8fd513cd62
-size 1425
+oid sha256:d29d029738addd476eaa1de402149545ebfc28cab3d7d6359a3c3ae37787b3af
+size 1451
