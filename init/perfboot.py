1bcac753a8897abb906cdc8b3f5be2e6526ed833
Yusuke Sato <yusukes@google.com>
2015-08-17 17:26:10 -0700

    perfboot.py: Add --end-tag option
    The current behavior, which is to use tags[-1] for end_tag, is good
    enough in most cases, but sometime it makes more sense to use a
    different one as end_tag.
    
    For example, when optimizing APK scanning code in package manager,
    using 'boot_progress_pms_scan_end' as end_tag would give us quicker
    benchmark iterations as well as more relevant numbers possibly with
    a smaller standard deviation.
    
    Change-Id: Ic6371c936b36d1d87e9742502fd8958682a3b7d5

--- init/perfboot.py
+++ init/perfboot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:995ed12e67c67a9dd4d4fd7fd9eeb397866c22f772e7eac1e9de6aceafd32365
-size 15781
+oid sha256:37f73b86ee9d591ff346d1c925295b1b9f9a85a963966e6fb29e20a2825b2523
+size 16141
