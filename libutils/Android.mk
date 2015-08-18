416d1e637f3e6ea51fa0aa609b481724bbb2c322
Sergio Giro <sgiro@google.com>
2015-08-18 17:36:50 +0100

    libutils: fix overflow in SharedBuffer [DO NOT MERGE]
    See
    https://code.google.com/p/android/issues/detail?id=181910
    
    Bug: 22952485
    (cherry picked from commit 66b6eb9490beeeabc804d790c1c4060ce047afd4)
    
    Change-Id: I363f49b5b3c6b7b3ac08cba2c14d72c431588c5a

--- libutils/Android.mk
+++ libutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94e35b1c6daa281a51b7abaf0a45606fe5c38266d5f066934909fef1530a0c4f
-size 2636
+oid sha256:3413255a03c3cfecce66f3a862dd9ddb6a690ee19256dd8fbfc3812fdc518b12
+size 3143
