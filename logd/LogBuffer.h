e646cb04082582e1a333a075c92de144b7459ead
Mark Salyzyn <salyzyn@google.com>
2015-08-19 13:41:51 -0700

    logd: log buffer switch to std::list
    (cherry pick from commit 94a89c42fe9521f5ac2d382a172c082ca421d6c3)
    
    Bug: 23350706
    Bug: 23327476
    Change-Id: Ib0530b9dd5842c6d05c84d7a66f2531c97461067

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e3bf25fc39bb3b8a31d2b97faa35dc62adcf85444a984929a2844e2094881e6
-size 2845
+oid sha256:6c558e926fb71ffd442cc6b6940eaf85de4b41eb7f6bf7e62dc8175122c7e9c3
+size 2834
