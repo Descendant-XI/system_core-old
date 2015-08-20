9bada593389c3bfec6339626ae394fab3ff7d05d
Mark Salyzyn <salyzyn@google.com>
2015-08-19 17:06:11 -0700

    logd: worst uid record watermark
    Hold on to last worst uid watermark and bypass a spike to O(n*n*x)
    (n=samples, x=number of spammers) wrt chatty trimming.
    
    Bug: 23327476
    Change-Id: I9f21ce95e969b67e576417a760f75c4d86acf364

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a3ae7fd4428ea3e31142371556aba8bfb3b530d5dd633b6f3846c1662ea7790
-size 19218
+oid sha256:647f08f50fd9eb116a1db33dec14f0c5bde17d2956be0300bc99054abd5ab816
+size 19898
