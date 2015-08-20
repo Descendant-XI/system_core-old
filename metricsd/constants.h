6f9fd2664b69e1f823d1a40c45e7e8c528427bdb
Bertrand SIMONNET <bsimonnet@google.com>
2015-08-20 14:08:41 -0700

    metricsd: Use system properties to store build time metadata.
    Instead of using /etc/lsb-release to store the version, channel and
    build_target_id, use the Android system properties.
    
    BUG: 22879642
    
    Change-Id: Ic79e74bd14bf8e2c55549c08963a66700b49b544

--- metricsd/constants.h
+++ metricsd/constants.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a1bed4b3f117d8e8a10f220a5eed25c43276a901e604c00d892b71d5c38650d
-size 1168
+oid sha256:d903c26fe42210c7b356459d0d73c8c3073cc5fa7e5d2365b8e8e7b40778cfcc
+size 1400
