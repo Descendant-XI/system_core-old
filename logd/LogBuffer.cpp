661689cc0f65fda9566d5d7dbe1d5fd90b3a279a
Mark Salyzyn <salyzyn@google.com>
2015-08-20 10:01:44 -0700

    logd: switch asprintf to std::string
    Bug: 23350706
    Change-Id: I715cdd4563a09de3680081947a3439f0cac623be

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:647f08f50fd9eb116a1db33dec14f0c5bde17d2956be0300bc99054abd5ab816
-size 19898
+oid sha256:4e3bcd668911bb9baf730237a8296ad9b22677a4d2ab21f99c1bfe5833001c92
+size 19921
