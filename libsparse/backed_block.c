5c7c804b4dee9f9798e4b26fdfc5003f77927fe0
lei wang wang <leiwang211@gmail.com>
2015-08-21 11:13:46 +0800

    libsparse: use strcmp and validate last_used pointer
    This patch is used to fix two Bugs in backed_block.c
    
    First, fix wrong comparing string way: we should use strcmp rather than
    just compare their address. Second, fix using illegal memory risk in
    bbl->last_used pointer. When entering queue_bb function,
    bbl->last_used = new_bb, but in the following code if
    queue_bb(xx, bb, new_bb) return ok, the space of new_bb is released. So
    next time, if you use bbl->last_used pointer, may cause segment fault !
    
    Change-Id: I6abb505f9b903b697448639fc64fb7518df5cca1

--- libsparse/backed_block.c
+++ libsparse/backed_block.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccc8404458f8ca87db7864617f5225777ae67aac70739d7c04881b2b4e77c566
-size 8592
+oid sha256:a7cdd8ea3641629b675c68f4e9eef6ab3bf6203f89830e9ad5fbb37d45f58dd4
+size 8743
