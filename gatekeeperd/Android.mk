ebfee464fcf6323d71807b0a791395c267f6c459
Tom Cherry <tomcherry@google.com>
2015-08-21 12:35:14 -0700

    update bundled rc files to their contents from rootdir/init.rc
    Bug 23186545
    
    Change-Id: Ieb03ba89db2dee0365ae346295207ddfd29bd2b7
    (cherry picked from commit 297991b3248f1e15ccc6c775b9a12e62925095f3)

--- gatekeeperd/Android.mk
+++ gatekeeperd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0422fc1f094bc6ebc796be5ce9d71c0086a8a7a8c777e443a87627ead97d7a
-size 1155
+oid sha256:823d4940524d8970d4d851f9025f07b00afd4500318d5122c3cd84270ae87533
+size 1187
