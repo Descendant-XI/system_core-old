50771c36621611ea7c90d77c4cacb617591b9c50
Mark Salyzyn <salyzyn@google.com>
2015-08-21 16:44:30 -0700

    logd: object layer format statistics
    Simplify table generation by placing the line and header formatting
    into each type's (UID, PID, TID) object. Switch to const return
    values for the ownership passing functions (*ToName() functions
    and methods). Use longer variable names to reduce confusion.
    
    Switch from LINES To NUM for pruned column as that more accurately
    reflects what is dropped since one entry can contain several lines.
    
    Bug: 22855208
    Change-Id: Ib110dce98a68cf5f844eb30f8a192a1f691eeba2

--- logd/LogBufferElement.cpp
+++ logd/LogBufferElement.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cd740e83a2bd3a98d3e00b8e0e150192664863d77547ba183a286ce9b66fac6
-size 6578
+oid sha256:d80087d07261bf6454a5bb22ae94798ff7e1c2aa5f7bde1cb3fc2fa96aa26a70
+size 6794
