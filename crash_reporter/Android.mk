9c5608aa6a9c0d90a2cecfdeb4602ec9bda77931
Steve Fung <stevefung@google.com>
2015-08-20 17:07:50 -0700

    crash_reporter: Fix crash_sender
    - Remove all the ChromeOS specific logic.
    - Fix paths to correct Android paths.
    - Add periodic_scheduler, and add crash_sender to init.
    
    Bug: 23231196
    Bug: 23233267
    
    Change-Id: I12de28bfbe5d5b08831eda9b28c6d7a669c22290

--- crash_reporter/Android.mk
+++ crash_reporter/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98ae78ffb38135cdc6974abb427db6b66ef8337b63cd2e19565293271d83dee5
-size 3920
+oid sha256:6736d4c767b80f9ec8b577a66bd0a67d10377175532d1d474103c8ca96c7d4b1
+size 4254
