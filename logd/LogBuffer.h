b7a1399090bf347d538b72c186df912003ffc40f
Mark Salyzyn <salyzyn@google.com>
2015-08-21 16:44:30 -0700

    logd: object layer format statistics
    (cherry pick from commit 758058ffd8820df71c27db7675c50a90a5fa02b4)
    
    Simplify table generation by placing the line and header formatting
    into each type's (UID, PID, TID) object. Switch to const return
    values for the ownership passing functions (*ToName() functions
    and methods). Use longer variable names to reduce confusion.
    
    Switch from LINES To NUM for pruned column as that more accurately
    reflects what is dropped since one entry can contain several lines.
    
    Bug: 22855208
    Change-Id: Ib110dce98a68cf5f844eb30f8a192a1f691eeba2

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87d9e1b5ab29bfcb272e7b82c2ddccb97541c4f3db2d1d49d00a58566a4e6776
-size 3087
+oid sha256:e66153e8c2a38a3a70e8caf8b764d73ef11f281fa5660e48f695418047042f67
+size 3099
--- logd/LogStatistics.h
+++ logd/LogStatistics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e8cbccb68c5b131b1d8e4c0c210aba962b69f0133c0bdbf217e8ab5da892514
-size 9467
+oid sha256:b5d1a437b14bb2d8f122e3ff6d56ee006d47c161c58ad0d23fc424cafaa8a6ae
+size 13003
