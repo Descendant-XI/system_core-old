ca92c1a3deab1ea1681bf8398fb8a427881faec2
Mark Salyzyn <salyzyn@google.com>
2015-08-24 11:08:00 -0700

    logd: statistics report chatty effective percentage
    (cherry pick from commit c723df805a1a3199577cac947cebf2ab53abdb34)
    
    Report the ESTIMATED instantaneous percentage decrease or increase
    that an UID has to the logs as a result of the chatty filtration.
    
    Bug: 22855208
    Change-Id: If1e77afb81a2739a72b39bc7c57071763c1d64d8

--- logd/LogStatistics.h
+++ logd/LogStatistics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f49d6c732d2aa704ecef5c96e4b6a7a75b5df5ed1277e262016d38e9e3f926f
-size 13245
+oid sha256:166d297947c5fdea744b23fb8e7bd4c3fe2c15c79076d2a808f5d346b56f5f80
+size 13267
