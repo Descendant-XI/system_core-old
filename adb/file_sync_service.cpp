7a9e0b76e15056fe2e90fae9d02e775e47ef297f
Elliott Hughes <enh@google.com>
2015-08-24 14:27:03 -0700

    There are no big endian hosts, grandpa.
    Change-Id: I6f64f702f919fe4af10d82c5f395a051571815ed

--- adb/file_sync_service.cpp
+++ adb/file_sync_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf4f50a9a51334e0523df4abb979ca5b2d5f9edb7966c18d000ed701539d3a7
-size 11757
+oid sha256:fec4c00798d2d46d78368d6ff7b6e9712b3ee7f67994608610baf249d04d0e99
+size 11666
