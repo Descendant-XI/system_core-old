663f62a318c3c2b627f80b14591929a72f0c4ae3
Steve Fung <stevefung@google.com>
2015-08-23 17:18:23 -0700

    crash_reporter: Fix tmp directory location
    It is unsafe to use /data/local/tmp.  Use our own temporary
    directory under /data/misc/crash_reporter.
    
    Bug: 23430484
    Change-Id: I186ba6b146bd884064177a3bb29b18b8edcc0b62

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a2b79e2015c9c76b598a5301cbf587a2630903da8fcb211fe077b2b59fd29cf
-size 19469
+oid sha256:0be3f7ee4fb8c2f1c66ed3f229acdf89bda1c00d171d4ec9f784a383d79f496a
+size 19468
