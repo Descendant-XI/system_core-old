c0607c95007f3914ee0114091970869b09ab598e
Elliott Hughes <enh@google.com>
2015-08-25 13:14:07 -0700

    Move adb's selinux_android_restorecon up and check for failures.
    Change-Id: Ib3ff282cf52df03f3b9a0abecf0abda99d7df641

--- adb/file_sync_service.cpp
+++ adb/file_sync_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be37ed4d542a12cadf5231c01cf9eaf38e7b4cc7d167e2770983d43e711e19b4
-size 11623
+oid sha256:a74092d4028c92adc7e4f6cc341d7ad1b8df721dddf8be7864499df212988129
+size 11740
