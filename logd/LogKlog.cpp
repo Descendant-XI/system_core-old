7668e50eaafaf9ae0bb7aee09caa31a396b2628e
Mark Salyzyn <salyzyn@google.com>
2015-08-26 09:30:00 -0700

    logd: klogd and Mediatek
    (cherry pick from commit 46d159d4626ca5d5667b739792840c3178a1457b)
    
    - sniff for PID in kernel log messages if available
    - properly deal with klogd watermark in face of modified output
    - deal more stringently with priority tag, must have [ following
    - suppress process-name stutter in tag that can happen
    - do not use : to demark tag if within [ ]
    
    Mediatek-special change that adds <printk_state>(<cpu>)[<pid>:<comm>]
    as a prefix to the printk messages. Along the lines of (simplified
    for entertainment purposes, YMMV):
    
        char tbuf[50]; /* printk prefix */
        int this_cpu = smp_processor_id();
        char state = __raw_get_cpu_var(printk_state);
        unsigned tlen = snprintf(tbuf, sizeof(tbuf), "%c(%x)[%d:%s]",
                   state, this_cpu, current->pid, current->comm);
    
    Bug: 23517551
    Change-Id: I568e25c5aa6d8474835454a0e83b19c2921b7985

--- logd/LogKlog.cpp
+++ logd/LogKlog.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b29ede13d9ab23abc1afac4ead7c317a66660dd450e07145b9a2d7a8f21ff9da
-size 18243
+oid sha256:dc6bffd70de950c953f2e6ab65d2f88863bd1a73d8a6e2039d6b138bc64e5e92
+size 19195
--- logd/LogKlog.h
+++ logd/LogKlog.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee1e0c745a5a94366d6859189c4e9d077d63750d635c16dff2be2879e42e195d
-size 1841
+oid sha256:f875ea6795c57687345b926cc09eb00f98d3e3f6aacf3a5afb45632c7c524add
+size 1878
