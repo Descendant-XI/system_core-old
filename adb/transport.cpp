01725d67b52922f9cc827a0fb7cd518a9d584528
Yabin Cui <yabinc@google.com>
2015-08-26 11:44:16 -0700

    adb: fix a fake data race on transport:kick reported by tsan.
    It is reported by tsan as a double checked locking. But I think
    it is not a real data race. Because I think the old code is able
    to make sure t->kick() is only called once, and the caller of
    kick_transport is not relying on the side-effect of calling
    t->kick().
    But as it is not perf critical, I don't mind breaking the double
    checked locking pattern.
    
    Bug: 23385662
    Change-Id: Ie3597dd56bb514117c3865d2afcfd7c115731a78

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14e0e9cb7f1a3df0903db7c59a37ea0d096b842c321ab60f12f38f8a6a7bbdee
-size 29901
+oid sha256:bdd5b1e848e1063b506cdd45516b521d40a52c6d673f0c2196492fd2fc5be3c4
+size 29898
