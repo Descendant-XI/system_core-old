16b5b41e712abd0c5581a3f81a723ebaececf8df
Nick Kralevich <nnk@google.com>
2015-08-27 10:30:50 -0700

    strdup16to8.c: fix benign integer underflow.
    A size_t value is not allowed to go below zero.
    
    Change-Id: Ic888c6605d911e0b5dbee1a2fe1daf41e616e106

--- libcutils/strdup16to8.c
+++ libcutils/strdup16to8.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34cc928d0fcbec4d5909c5bcd65b868736f7b817c3a16b48f014576d2c4491cf
-size 4585
+oid sha256:f7cb779fffa9f469fa0d6a7baeea38d9410c4bf791100207acff03730173320a
+size 4643
