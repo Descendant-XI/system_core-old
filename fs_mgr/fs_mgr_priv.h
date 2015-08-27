73822c075d7112c073c3c7e1857ea8e7a743eefb
Daniel Rosenberg <drosen@google.com>
2015-08-27 13:42:20 -0700

    fs_mgr: Add support for A/B partitions
    Allow partitions to be marked as A/B partitions
    using the slotselect flag in fstab. The partitions
    can be identified by appending the correct suffix
    to the block device listed in the fstab. The suffix
    is provided by the bootloader through a command line
    parameter or the device tree, and can be found in
    ro.boot.slot_suffix or read from the boot_control HAL.
    
    Change-Id: I6846d80e857f95bfb8f282f4ab81167394613bbe
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs_mgr/fs_mgr_priv.h
+++ fs_mgr/fs_mgr_priv.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5dcb8cb53d49061d3072b013780249e70cbd23eb8c22870653a82f2f6222aea
-size 3512
+oid sha256:baa7e3ba323bfa6b0ee06ae9996d1f515a3bd6b565bab49424b3f773f122509f
+size 3543
