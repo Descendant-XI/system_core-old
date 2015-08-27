97a0e8ba47e96faae05093b3a9b396d6c6890d2a
Duane Sand <duane.sand@imgtec.com>
2015-08-27 14:25:46 -0700

    android_memset.c: Use sanitized shifts
    Avoid checks and traps on msb=1 unsigned shifts.
    
    Change-Id: I5203a4290296b5557c0420ce0debad3b60d48233

--- libcutils/arch-mips/android_memset.c
+++ libcutils/arch-mips/android_memset.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c31d9e1b440dcffe0b23bf956474c0bd846cd99fbf4d1d8984b4001720d3c1c5
-size 3025
+oid sha256:b3bcfb32fb8f4539ac73a351583497ba1de3c6ab8bf0c4923f1d7c9385b1a07c
+size 3063
