1dc3f3a15004feb8c5b051324c6ccc37f3b8324d
Narayan Kamath <narayan@google.com>
2015-08-28 12:59:48 +0100

    libutils: Fix integer overflows in VectorImpl.
    Use external/safe-iop to check for overflows on arithmetic
    operations.
    
    Also remove an unnecessary copy of Vector/SharedBuffer from
    codeflinger and use the copy from libutils instead.
    
    Note that some of the unit tests are somewhat useless due to
    test-runner limitations : gtest's ability to filter on abort message
    doesn't work when combined with messages formatted by android's logging
    system.
    
    bug: 22953624
    
    (cherry picked from commit c609c31fb56ae434caa2d0153cd0a2f74a715071)
    
    Change-Id: I61644633db6b54fa230683615de9724f7fabf6fb

--- libutils/VectorImpl.cpp
+++ libutils/VectorImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f43355586ae97a2bfcca67d2edcde116b0afb0003518d3848cc71686fadb2963
-size 18078
+oid sha256:f76d83fe6dbb3bb6064d8814f14ccd7f0f4304a6f05111b054eccef75cd59c77
+size 19988
