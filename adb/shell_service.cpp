a0c680f0bdf49296117b23856d243cc4915bf0be
David Pursell <dpursell@google.com>
2015-08-28 18:31:29 -0700

    adb: refactor subprocess code.
    Refactor shell_service.cpp to remove dependencies on service.cpp and
    combine some common logic between PTY and raw subprocesses.
    
    This will make it easier to add additional common code paths for
    the upcoming shell protocol.
    
    Change-Id: I497d30dd388de61b6e68d9086dce38f33dd92876

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26a538935f5a04f3700455734134a25fd4bb442f73b54e4f6c9fc6db64bef527
-size 6425
+oid sha256:9da9aa2ab9d271beab58a1eec571b2c78f3aafcb3a22604452b015f9f8901e76
+size 9114
