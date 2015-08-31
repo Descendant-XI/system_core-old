f50e2766563a85b7c7b3c0d0a95659d73769b2ec
Elliott Hughes <enh@google.com>
2015-08-31 10:46:05 -0700

    Distinguish transport threads.
    Use "<-" and "->" to work around Linux's very short thread name limit.
    
    Bug: http://b/23423333
    Change-Id: I9f3f096415b47162dacb660cd0a97ac7ff6107e6

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33d5b3289f358794e89a349a96f2d686c6598301bc1fc5607e08a6daab32dbdb
-size 28100
+oid sha256:99d53511b275dd4591b05af4b5ee5b445c908dbb2fe50426905a2faeb2683111
+size 28162
