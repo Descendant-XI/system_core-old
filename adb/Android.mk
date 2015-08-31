0c57b77472be674cb718b9b3528a154f4400a9c4
David Pursell <dpursell@google.com>
2015-08-31 15:36:18 -0700

    adb: create shell protocol class (take 2).
    Adds a new class ShellProtocol to help read and write data with
    `adb shell`. This will allow splitting streams and sending out-of-band
    data such as exit codes.
    
    Nothing uses the new class yet except the unit tests.
    
    This is the second attempt at this CL, the first is at
    http://r.android.com/169600. The problems was using sighandler_t
    which is not available on mac. sig_t is used instead which is available
    due to _GNU_SOURCE being defined in Android.mk, which causes
    _BSD_SOURCE -> __USE_BSD -> sig_t to be defined. Nothing else has been
    changed from the original CL.
    
    Bug: http://b/23030641
    Change-Id: I7bd7f5a82ad811fbca7a3eee1236d2c55ae57c48

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:add80295551b9ce5323984182aa98ba79a1ce21dc225b446a7ccf6e5f1029702
-size 7733
+oid sha256:7b27009df3f0f8cdb30a114127ad6895b09ecc83586643d2ac11bc28a4031144
+size 7956
