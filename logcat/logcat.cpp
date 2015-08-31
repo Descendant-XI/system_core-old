0c683af12303d96197844044a7912c61992f06e5
Mark Salyzyn <salyzyn@google.com>
2015-08-31 15:53:41 -0700

    liblog: logcat: add epoch and monotonic format modifiers
    (cherry pick from commit 4cbed02e440dc400668fbdfb8ce4a710e5caebe5)
    
    - '-v epoch' prints seconds since Jan 1 1970
    - '-v monotonic' print cpu seconds since start of device
    - '-T sssss.mmm...' as alternate tail time format
    
    NB: monotonic is a best estimate and may be out by a few ms
        given the synchronization source clue accuracy.
    
    Bug: 23668800
    Change-Id: Ieb924b6d3817669c7e53beb9c970fb626eaad460

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56e69d7cc223a61cb45c63ebd0dbc687825280eae5eabe4098a874271832e163
-size 32083
+oid sha256:1e891de7cd21d4598ee06070d705673b39a462006d1a7b803687da34a0671ea2
+size 32187
