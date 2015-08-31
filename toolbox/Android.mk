9c750b2ec00334ad78290acd08ece0f244ed1e5d
Elliott Hughes <enh@google.com>
2015-08-31 12:45:47 -0700

    Lose ls to toybox.
    Bug: http://b/23040458
    Change-Id: I0e9f89b1169df409a38a2134ac2cb9edd0421be3

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd753222582453533f352bc5b47e084a75f0a2ae0fdc81f50ba40c923f1beffb
-size 3318
+oid sha256:dddeced5eafe052294f2a6fb9dbb5e24b813c18fdadb801dd3703e4e19114f4d
+size 3309
