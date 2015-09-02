7e821687f84969fca0d80ace7ab9007c4dc2eeef
Mark Salyzyn <salyzyn@google.com>
2015-09-02 07:39:53 -0700

    logd: log_strtok_r deal with nuls
    (cherry pick from commit ea1a24110767d6d5666fda8a19dd0b3f954068d6)
    
    Rename to log_strntok_r and change from dealing with strings
    to dealing with a string and an associated length.
    
    Bug: 23517551
    Change-Id: Ia72f1305a53f55eeef9861ac378fb8205fd2378e

--- logd/main.cpp
+++ logd/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e04bfad6cd87a2ee4b0a6e4dfa2bd059172d3cd04ca6b6c0438e16feb813c77c
-size 13209
+oid sha256:2d2ec70096ca29011f313ddd2eb3a6edb41baf82c5b80a2774e7350a060b1af9
+size 13255
