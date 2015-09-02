bff693f157cacb736699b2b0e9e946a4a2b25998
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-02 15:07:10 -0700

    metricsd: Log the histogram name to hash mapping.
    When running the metrics_daemon in verbose mode, log the histogram name
    along with the hash name.
    This is useful when debugging as the protobuf message only contains the
    hash of the name.
    
    Change-Id: Ifea7edce55d62d30994ec7b75bb8728f6a0c40e8

--- metricsd/uploader/metrics_log_base.cc
+++ metricsd/uploader/metrics_log_base.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01ec39460265fd01823f436fbdd93bad1d74c75b344792125a6c8522d05e22d
-size 4986
+oid sha256:f1d945a7d302ad0f60b8ad77f11586d292e8d241cf7c767902c134b23ab25bff
+size 4985
