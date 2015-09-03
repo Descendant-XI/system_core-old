1aadd22ba33cea88d1ddf34f33fab65f50425546
Bill Yi <byi@google.com>
2015-09-03 19:18:56 +0000

    Revert "metricsd: Collect generic stats about the system."
    This reverts commit 90b02cd46d8b9b4c80f99022a685e3a1ae2504a4.
    
    Change-Id: I9950c688433e99b0bc4745f02e2f9ae66f3b5578

--- metricsd/metrics_daemon_main.cc
+++ metricsd/metrics_daemon_main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d18805aed805cb438808f2a2b54eb8e4599a51b402eb79fe85dc4daa34eb943
-size 3829
+oid sha256:ff000eb7864bf6fea08751ed73eab80b0f580df961a2f16091729acb3f0f6a8c
+size 3019
