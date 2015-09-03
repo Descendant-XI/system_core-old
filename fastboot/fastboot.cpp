c2c4d390a3f7756070ad4f78812b71ea69ea74ad
Elliott Hughes <enh@google.com>
2015-09-02 20:33:44 -0700

    Fix the fastboot build.
    load_file takes an int64_t rather than a size_t to support large files on LP32.
    
    Change-Id: I8d3032cab63c30c4158dbc521d3d520415cdf58f

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02e5b00dc97e2f21686590079d2203d40e2f866ec919681f62ad9d2a8ff14aee
-size 39460
+oid sha256:a2813e2df9cbc5fd1fd537b3e7e59681480c5b361a4aacb5d31379615846ac04
+size 39455
