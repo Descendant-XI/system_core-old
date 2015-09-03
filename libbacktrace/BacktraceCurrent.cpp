1debcae2648931c4ac8e06ec175aee87f881b9fb
Christopher Ferris <cferris@google.com>
2015-09-03 11:25:55 -0700

    Add a logging handler on timeout.
    If the signal handler doesn't fire in the given time when trying to unwind
    a thread, put on a logging handler. This prevents crashes if the signal
    does eventually fire.
    
    Bug: 23783762
    
    (cherry picked from commit d7226f9a1485ba19f714369fd7f8b444d66feb61)
    
    Change-Id: Ib990a06733cc93717752ab4998f4ae26afd7e249

--- libbacktrace/BacktraceCurrent.cpp
+++ libbacktrace/BacktraceCurrent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7ff0fe00c8cf804c36ef0b99fd60560e5b89980c812eeae0649ae956f79beef
-size 5483
+oid sha256:8bfb549e6e842706e4c41399b3e3233412138a76000e186783d6d47b3af3bde2
+size 6213
