719a055e03f651e4ece97e8627b68270785a2a3f
Mark Salyzyn <salyzyn@google.com>
2015-09-04 11:37:42 -0700

    logd: klogd deal with nuls in dmesg
    Switch to using string and length in all transactions, treating
    trailing nuls the same as spaces.
    
    ToDo: change dumpstate (bugreport) to use logcat -b printable _regardless_
    
    Bug: 23517551
    Change-Id: I42162365e6bf8ed79d356e7b689a673902116fdb

--- logd/LogKlog.h
+++ logd/LogKlog.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72ede2a2cb039b5b8fd5285e2b044fca8b0b2e99a242a72c1d6fcb37ad19a7c9
-size 1833
+oid sha256:c441c221b619f8347bcf0387c8c532e075cf61788d37106d30c3c0a43c6bdea8
+size 1922
--- logd/main.cpp
+++ logd/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7249b69eda3cd32f38f4c021442959cd1dadda8525291daebe72f4e410ef581c
-size 13193
+oid sha256:e276bcbb7137e6d322051c2adf959df2d2b088cf4c813a174605242ae60c84b4
+size 13214
