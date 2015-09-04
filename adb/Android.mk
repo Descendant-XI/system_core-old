c97632c46672d05722dc10dd6d9d0867e9f8577a
Dan Willemsen <dwillemsen@google.com>
2015-09-03 20:29:56 -0700

    Remove HOST_OS==windows check
    With the HOST_CROSS_OS build system change, LOCAL_MULTILIB := first now
    behaves the same way.
    
    Change-Id: I03294403e5a106a53d4a0d48b176434221284901

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a7995e303f09d8f973c711a4f0ff45f66e2d78b38ecf9e9f9f5b5872ac0a71
-size 7709
+oid sha256:29a167a09a601da1390c8ea97ac73da673bab59be85f5dbeca44aad48f7a34e1
+size 7675
