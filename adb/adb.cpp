ae5502884d338a74ce71a5b866b650230fa4214f
David Pursell <dpursell@google.com>
2015-09-04 16:40:30 -0700

    adb: `features` passes transport features.
    `adb features` previously returned a list of host features which was
    not terribly useful. This CL changes functionality to return the
    transport features instead using the standard targeting args:
      $ adb features  # default target.
      $ adb -e features
      $ adb -s 123456 features
    
    Also adds a "check-feature" service which is currently unused but will
    allow the adb client to easily check for a specific feature.
    
    Bug: http://b/23824036
    Change-Id: Ibc0c420c75f73d363f3bba7705af616ba2059348

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:356483bf3771f4bdd9855f32c14e7564b868f354e4701ea6e0520d58943ddadb
-size 41936
+oid sha256:9e5632b77a9fb2baa87a0b05fecdb7e6b0ae2137c91f3a001d0e4cb930624cc0
+size 42656
