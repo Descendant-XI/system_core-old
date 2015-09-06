510d61df9b58064ba3c0c1c4ad2e4bfc25711b5e
David Pursell <dpursell@google.com>
2015-09-06 01:18:47 +0000

    Revert "adb: create shell protocol class."
    This CL broke the mac build. I'll revert for now and take a look at how to get it working next week.
    
    This reverts commit 73096f2e1d87da571187515af6aa7c2171a70693.
    
    Change-Id: Icb3e015250bcbbc69a45675a1358699ebe01e77b

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91f28ab8fdb2bed92bfaa950dce7272f6d8f1c21f5e8a5520658a1a97f34b482
-size 7982
+oid sha256:2aca178aa5b6202facd11870cd0a8374ee434fd6f5ca06e540dc5d30d6ed3f27
+size 7759
