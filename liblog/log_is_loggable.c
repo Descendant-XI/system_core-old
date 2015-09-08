789973e10be27a186f86662195e19f7b74acea10
Mark Salyzyn <salyzyn@google.com>
2015-09-08 08:32:01 -0700

    liblog: add android_log_timestamp() private function
    Change-Id: Iefbea3b1be2f97cfdeb35e5330e5495e0337215b

--- liblog/log_is_loggable.c
+++ liblog/log_is_loggable.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00e0ff849bf82efb3e7ae2d0159bdc835f799b672ea8f20280e9b2c242bcf604
-size 5051
+oid sha256:94ddb93b23bb747006d0058b80095867c4b98f002ac8a0921a321b39b0368fe1
+size 5703
