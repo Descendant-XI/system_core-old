f85221f41a4dc28d066b468aaf57f1fd40030af6
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-08 10:13:35 -0700

    metricsd: Collect generic stats about the system.
    Collect memory usage and disk IO statistics periodically.
    Also update the Android.mk file to use clang by default.
    
    BUG: 22953719
    TEST: builds on external and internal branches.
    
    Change-Id: I1ee3683d014586cf7f711d2e090a99429752063c

--- metricsd/Android.mk
+++ metricsd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d538bc35165698dd6c957f50fefb4d91b2ec366f48c94ef5567a4bd67fd48390
-size 5369
+oid sha256:d8362cec26bf9a369942887b6eb4dde7269d3c89d81e13bd121875e55086f61b
+size 5445
