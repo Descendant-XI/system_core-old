a4548b1192e37d994037263195817253952ffb9b
James Hawkins <jhawkins@google.com>
2015-09-08 15:18:17 -0700

    metrics: Add an option to metrics_client to dump the logs cache.
    The format of the metrics dump is rudimentary just yet. Here is an example:
    
      Metrics from /data/misc/metrics/uma-events
    
      name: hello     type: USER_ACTION
      name: world     type: USER_ACTION
    
    This required the following changes:
    * Added -d option to metrics_client
    * Refactored file handling in SerializationUtils
     - Factored out file opening and log parsing into helper methods
     - Added ReadMetricsFromFile which is read-only so does not truncate the file
    
    Change-Id: I6032d74242c79c678ec42a14e78fccc54e7af455

--- metricsd/metrics_client.cc
+++ metricsd/metrics_client.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44663b2a2b2a0e1531c593d7a5ea0872405287d3c7542a7ada946784118e0b8b
-size 5831
+oid sha256:e302a7dab2fab580f70629db0458de3bad84849760b22da736a4311c06ff92a2
+size 7183
