a291977d209cef147adec1eced7141b6299c91a8
Mark Salyzyn <salyzyn@google.com>
2015-09-08 07:57:27 -0700

    liblog: logprint: printable nul
    (cherry pick from commit faa92e99151ccec81073cb7ded2d49458468a02d)
    
    Change-Id: I57d1f86ce040ad196c558bb72ac0464af793c214

--- liblog/logprint.c
+++ liblog/logprint.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fdfbb171336f746f16938f5f7a3374e510f106bb5bfcfd8a211bb16fd979c7a
-size 34084
+oid sha256:b584f385bf14ccfaec3da1a681e3a93827b13854c75f4a84315941d53a10643b
+size 34102
