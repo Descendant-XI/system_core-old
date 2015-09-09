34063774e972e9042a8f7f48f77d1d63243e76d3
Andres Morales <anmorales@google.com>
2015-09-08 17:56:07 -0700

    [core][trusty] add keymaster module
    Change-Id: I7495161a96c7a0652ff3d8318ebe6dab533fe319

---
+++ trusty/keymaster/trusty_keymaster_ipc.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e16164f78c40b855dc98cebcc0fe7450589ddfd24f772b4b9dafd57972299787
+size 2457
---
+++ trusty/keymaster/trusty_keymaster_main.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d83977bd3c0f22fbe27b0a96e0e68c0bbcee281408363214c15552abaa3e403
+size 17215
