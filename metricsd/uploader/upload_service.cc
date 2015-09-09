d621a076cf1446f8c385e598b856002abf9cb066
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-09 10:39:51 -0700

    metricsd: Persist the report to disk if an upload fails.
    If the metrics server is unreachable, we may need to resend a metrics
    report later. Instead of keeping the staged report in memory, save it to
    disk to avoid loosing data if the system restarts or crashes.
    
    BUG: 23033262
    TEST: unit tests.
    
    Change-Id: Idd14964e40f022952469f47d675d8cda9586d7cd

--- metricsd/uploader/upload_service.cc
+++ metricsd/uploader/upload_service.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c39da378963aae8c2cb3fd0aa8fbe37b4c2c2949801a7b20e3a75f33c92b2ea
-size 7638
+oid sha256:8e78cca0d25b7097256573f88c97687c25fa3bdb0af00d3e61549bef8b5301c9
+size 8580
--- metricsd/uploader/upload_service_test.cc
+++ metricsd/uploader/upload_service_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ecf3246b8d92be3afcf618f76488e2397edeba1463ee96b7943ca6bf7484d8f
-size 8991
+oid sha256:b476a3815038783f79552014a641fada0e1bec6fe86f0a6118e51215d9c87335
+size 9352
