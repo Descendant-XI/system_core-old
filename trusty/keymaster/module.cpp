18d77d6d22e9bb1feae479ebed5fe8b8bffa82ac
Andres Morales <anmorales@google.com>
2015-09-08 17:56:07 -0700

    trusty: add keymaster module
    Change-Id: Id240b1b33e07d4cb3ea9d188014701e3d2b3c477

---
+++ trusty/keymaster/module.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52996d05a82cb260d0715cb71bf4fc7638f457413098b6fcbf9a622bd0c98960
+size 1939
---
+++ trusty/keymaster/trusty_keymaster_device_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1fff05962e09a92f6429d6d75ec40232c9a44042c13483c98b3b3625024f3b4c
+size 21327
