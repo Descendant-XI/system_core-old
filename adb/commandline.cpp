f006fade9314049fabd99802fd93f372e69cbddb
David Pursell <dpursell@google.com>
2015-09-14 15:33:50 -0700

    adb: Fix missing shell protocol usage.
    Some adb commands use the shell service but weren't using the
    shell protocol, causing a protocol mismatch between the client and
    the device. This CL uses the shell protocol whenever possible.
    
    Change-Id: I4c9c75b7fd1d5bf3dc1f73900711840270527682

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61458ab012a7f0dbc2a63c28a31b20a0380b03c0184c9c430eb7f73d01394d1b
-size 61529
+oid sha256:7e296da676ad09b0169e84c2f8a2ea9e66e84e2df2fbd3cec1631e80c517adf7
+size 61725
