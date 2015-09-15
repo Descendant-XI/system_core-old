260cacebb1d328e1fe3b4c9d183c0f532cf40991
Yabin Cui <yabinc@google.com>
2015-09-15 16:27:09 -0700

    Add unit tests for local socket.
    Add has_write_error flag in asocket, so it will not wait on local_socket_closing_list
    to write pending packets in local_socket_close(). Although it doesn't fix any problem,
    it helps to make the code more stable.
    Add a missing put_apacket() in error handling.
    Add a check when adding local socket in local_socket_closing_list.
    
    Bug: 23314034
    
    Change-Id: I75b07ba8ee59b7f277fba2fb919db63065b291be

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:249edccf8ad35443451a3ab39338b83348f5a35a256e61a93a57ff3641bfa84a
-size 42727
+oid sha256:cc76ae5aa03806a615e0d237c0c7d7460d43de9f9eee04ed6ede3e82e6d931ce
+size 42775
---
+++ adb/socket_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7b5127b6c3d9dcd01e84826d5ff22400b1114ec6c7dfe1aa9dd0ae312eeb3b7
+size 9702
