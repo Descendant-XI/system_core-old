afabc362336d3943a3294abcaa289b5c0f1f9f00
Mark Salyzyn <salyzyn@google.com>
2015-09-16 15:34:00 -0700

    logd: clear return and deal with busy if readers locked
    - Propagate to caller the clearing errors, busy blocked by reader.
    - For clear, perform retries within logd with a one second lul each,
      telling readers to skip, but on final retry to kill all readers if
      problem still persists due to block reader (or high volume logspammer).
    
    Bug: 23711431
    Change-Id: Ie4c46bc9480a7f49b96a81fae25a95c603270c33

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8a5d1206128b5c4121a0e7a7d77d149ecba50b555af00bd6b7fe652ff0c37c8
-size 22966
+oid sha256:9c869f65ac1303921de6b3dddbdfadbfc3b1ff41b8efc476a113f7cfb71417be
+size 24745
