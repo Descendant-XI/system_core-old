7d6c222cd55ce62dc1ca43c2ba270033dd78fdcc
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-18 13:38:10 -0700

    metricsd: Report the data partition usage.
    metricsd should report the stateful partition's usage, both in MB and as
    a percentage of the available space.
    This measure will be reported every 15 minutes.
    
    BUG: 24132871
    
    Change-Id: I1cd8d651a980f90a57be9cc2a5baf6ed59794470

--- metricsd/Android.mk
+++ metricsd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6766a52b656016faff906b81c2b609440ab1d72faf0bea44418c87b8519010b3
-size 5411
+oid sha256:1325cf5952da3d3e855da6adfa3cfdb157cea37bc9e79ea3d6ada47a08bdf7ad
+size 5489
--- metricsd/metrics_daemon.cc
+++ metricsd/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f92b54ef923e0a0fb3f882787836c49417dc2c095470d4ae4d53ea9d4f6d9a57
-size 40527
+oid sha256:45dffa3b3b89ca1e246894e6cc27025c6f975d2c78a902f0bc5e3988c1bcfbda
+size 40633
