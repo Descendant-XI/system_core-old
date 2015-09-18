2ca717016b4086e7198c1ebb7f7c85ca8791bcfb
Spencer Low <CompareAndSwap@gmail.com>
2015-09-17 19:36:10 -0700

    base logging: fix errno restoring, severity conditionality, dangling ifs
    Fix LOG() to properly save and restore errno. Test this properly.
    
    Only do logging if severity is >= the minimum.
    
    Fix dangling if statements in CHECK(), CHECK_STR{EQ,NE}(). Test this
    properly.
    
    Fix base logging tests on Windows. All libbase_tests now pass on
    Windows.
    
    Change place to lock, so the lock can protect logging of all data in
    LogMessage.
    
    Change-Id: I7ff531c67ae10a99ef0a2bbfe279aa77282d5ae9
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- adb/adb_trace.h
+++ adb/adb_trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d4d20195f1536b12bf618aed5117ab48fe2ab61d9afc79f0bc8d187f999ee05
-size 1693
+oid sha256:a6fe4da5128b8ff8d9d9c9b12d7013fed9618d270eecfcb85b39221a13c19235
+size 1611
