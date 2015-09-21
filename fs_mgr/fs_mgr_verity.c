a89188715fd51e26e4d5a5835b746d1fa50eef2f
Sami Tolvanen <samitolvanen@google.com>
2015-09-21 15:12:29 +0100

    fs_mgr: trigger dm-verity error handling for invalid signatures
    Currently, the device doesn't mount verified partitions if the
    verity table signature is invalid, which usually means it fails to
    boot. This change instead sets up dm-verity with an invalid root
    hash and triggers device-specific error handling to recover from
    the corruption.
    
    Bug: 24256506
    Change-Id: I6d693306fa0e7459c5500b028e433df61ecea6fb
    (cherry picked from commit 47caa5c386b436ba13de9f2ef356380f39afaf3f)

--- fs_mgr/fs_mgr_verity.c
+++ fs_mgr/fs_mgr_verity.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ca376ec104232eaf64678b93ed936e4e58a25bac2db2499107b7528b2f407be
-size 27699
+oid sha256:8db8374f8ef37194b4d7c0d7ab6da748bc08ecb5d7e61a8e4429e9f151473473
+size 28705
