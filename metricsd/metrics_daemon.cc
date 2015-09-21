a09d2b18f07c61719a8679a2f075a901952f10c7
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-21 16:48:01 -0700

    metricsd: Cleanup metrics names.
    Rename some metrics for more consistency:
    * *.PerMinute and *.PerSecond instead of the obscur Long and Short
      suffix.
    * *.Percent for metrics reporting a percentage.
    
    BUG: 24206661
    Change-Id: Ib25f864e5e8bf5287ada2a361737def1b7821135

--- metricsd/metrics_daemon.cc
+++ metricsd/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f92b54ef923e0a0fb3f882787836c49417dc2c095470d4ae4d53ea9d4f6d9a57
-size 40527
+oid sha256:9c0e3cc88807c77a5fbb51f3a8ae70496587e4238357e06ea7c1b1d7f0e234f3
+size 40583
