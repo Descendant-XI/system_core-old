22d09dccc7f678f054c6d39269ff770b13c787f5
Tom Cherry <tomcherry@google.com>
2015-09-22 13:51:52 -0700

    Remove ANDROID_PROPERTY_WORKSPACE
    Bug 23290008
    
    Change-Id: I95babe735444ada10c67594ace700cae5d1ec0b3

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e1c0e53329781cbfdc62bf2c9f73aeabc9eb16f49f45e30322f340e2b26f756
-size 17236
+oid sha256:cd10c1600073af43edc998e9940416801747326a1801a51adc60d9cabe330e28
+size 16810
--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39d7674736ba540dfe834336066df6d3b16bb8d01c8d2bcfdbfa8caa43c2db36
-size 25807
+oid sha256:235985a6c44bc00b158a6fe94f41cc9a02c089f015d3bb5f6d35dc195490cce9
+size 25552
