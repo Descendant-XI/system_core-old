514c6e86fe091b351874f2fd6209c1c083c843ff
David Pursell <dpursell@google.com>
2015-09-25 08:37:13 -0700

    adb: bump server version to prevent feature mismatch.
    Devices get a list of supported features from the adb server, not the
    client, so a mismatch between client and server features can cause the
    device to use an incorrect feature set.
    
    Bumping the server version is the easiest way to make sure the client
    and server features match and seems like the best solution at the
    moment.
    
    A more automated fix could be to compare client/server features on each
    connection and restart if they don't match. This requires an extra
    client <-> server round-trip per command, but removes the need to
    manually bump the server version number on feature change. Unless the
    feature set changes often it didn't seem worth the extra overhead.
    
    Bug: http://b/24370690
    Change-Id: I4e43825d1c15c61e5d924fc8d4110b467debde37

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df3a4de1e214f10fe38034ed3ac27a016111670d5a8b936e215977f1a3bf95f0
-size 29016
+oid sha256:e353d335686a2d6717a7bebc37ca77176a11fe083b0a06b9ef8649f291189c9c
+size 29199
