ce460d6e5cff510acddf689de0c15957f3c59cdb
Elliott Hughes <enh@google.com>
2015-09-27 12:55:37 -0700

    Speed up adb sync.
    Sending the SEND/DATA/DONE packets all in one write makes small file and
    symbolic link transfer orders of magnitude faster. Transferring all the
    symbolic links in /system/bin or all the cacerts in /system/etc goes down
    from about 6s in each case to less than 0.5s.
    
    Change-Id: Ia1706cef25535d0b685d3744241a0557a52b22ff

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8585d65391a0c8d39bb935fe5940d9b25d2712904a28c20b26247815805bf25
-size 22659
+oid sha256:9864f76fee7de18d9f4bdf48de7cd95b208039b620150fd8c1ef266ee52bcc68
+size 24280
