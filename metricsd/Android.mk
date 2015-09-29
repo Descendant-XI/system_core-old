e2978795224293317dfed8b0de023c67737846ac
Bertrand SIMONNET <bsimonnet@google.com>
2015-09-29 11:07:24 -0700

    metricsd: Only collect metrics over a short period.
    Instead of reporting the metrics over both a long and a short period,
    collect and report only over a short period. This makes the code simpler
    and the metrics easier to understand.
    
    Also move the collection out of metrics_daemon and into a separate
    collector to make it simpler to understand.
    
    BUG: 24464945
    
    Change-Id: I17e52536aaa75321a5e34f42ed488545c2c3efde

--- metricsd/Android.mk
+++ metricsd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62d5a13074f86d79b3c5e3e2b75f9d32389d87d077ed11e3dcefc4dfc332d1ca
-size 5119
+oid sha256:71b17028840e79e7f930b248214e80c02af7b7b2d9aa5598108e17c90c9b5844
+size 5288
