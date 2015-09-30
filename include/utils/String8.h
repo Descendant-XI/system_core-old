680b23136336f3d71fb662e8d85d96123018e318
Sergio Giro <sgiro@google.com>
2015-09-30 15:28:30 +0100

    system/core: remove methods returning SharedBuffer in String8, String16
    Towards deprecation of SharedBuffer
    
    Change-Id: I3069837db32837bcc0f5d8f1118ccd502c9070dc

--- include/utils/String8.h
+++ include/utils/String8.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a12e3da0cdcfa06ee35e35fc117990d4eb68df85c42624377c2b8f2d4aaf0507
-size 12351
+oid sha256:c83e7b485fa2dd550af4ee1044380c54838afb070668ce6beda07ec6aee60b41
+size 12277
--- libutils/String8.cpp
+++ libutils/String8.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab21b3d1c64bb996142f30b6f186b880f2174a00ddc82e15601ae0eed68316eb
-size 15473
+oid sha256:877500dcb9ee32921967500df5624d956c196d2e359a7be354aac3f2c7ff95b4
+size 15368
