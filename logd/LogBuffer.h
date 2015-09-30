ba462c96953c7c42ebe30e86dffbd99a4bd8760a
Mark Salyzyn <salyzyn@google.com>
2015-09-30 07:40:09 -0700

    logd: use coalesce instead of merge (cleanup)
    - switch to coalesce instead of merge in naming of functions
      and variables. Confusing since we also to merge-sorts and
      other activities in the logger.
    - define maxPrune rather than using a number in the code path.
    
    Bug: 24511000

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c4b6a28f26ec4ef893e919fead590501987b0eeeeae67f72eb2b299b3590eb
-size 3087
+oid sha256:f5cae13c97b12ea8c9dad3387167522a6648c90c96e6efced518718af4e4bf61
+size 3131
--- logd/LogStatistics.h
+++ logd/LogStatistics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e8cbccb68c5b131b1d8e4c0c210aba962b69f0133c0bdbf217e8ab5da892514
-size 9467
+oid sha256:53bf605ad1a52abd3b917b32d993b36fe2848f39a5c1c1aa1b0b893f261935ce
+size 9470
