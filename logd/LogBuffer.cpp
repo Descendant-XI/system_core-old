e31fb48ae2610821f6d3858e3ef65c8e1d500a13
Mark Salyzyn <salyzyn@google.com>
2015-09-30 07:40:09 -0700

    logd: correct for number of elements in prune
    (cherry pick from commit 58b8be8906f903ac3d83c41bcb0fb9c7841945f0)
    
    Chatty logs would distort the average log size by elevating the
    elements, but not the size. Add statistical collection for the
    number of elements that report chatty, and subtract that from
    the number of elements to improve the pruning estimate. Pick
    minElements as 1% rather than 10% of the total with this more
    accurate number of elements, to a minumum of 4.
    
    Bug: 24511000
    Change-Id: I3f36558138aa0b2a50e4fac6440c3a8505d95276

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fbb9d419a20872a5a0941c12116a9b22e422d896f15a268d14a0c611cb9719f
-size 24902
+oid sha256:490ef7d9a9d6e24a197bd26623038feb26d5c0e7d69ea1b70cbd4edad71bf4cd
+size 24991
