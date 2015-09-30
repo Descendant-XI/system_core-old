24989aa42e75c4a75e4274d08456bfb501821504
Steve Fung <stevefung@google.com>
2015-09-30 16:27:56 -0700

    crash_reporter: Set Version and Product ID
    Read in the ro.build.id property for version, and
    ro.product.product_id for the Product ID.
    
    Bug: 22874192
    Change-Id: I9a3fbf375d49d04fc7bf6700c5987cb9e435c318

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b2e52a0ef39641a49896c6834d4a35319f0eecaabceb5ac7d69e3106f7e580
-size 20164
+oid sha256:477717a7494b502623621be1d0a2ea326b44fe622a0f2ff84ba3b26bf8558f7c
+size 20585
