646e644a006fe48cad3891e587ec3718eaa60b93
Dmitry Shmidt <dimitrysh@google.com>
2015-10-01 13:27:56 -0700

    libnetutils: Add synchronization per socket full operation
    Take and release mutex between ifc_init() and ifc_close()
    calls to prevent global ifc_ctl_sock override.
    
    Bug: 22307208
    
    Change-Id: Iacffd329ce2a51defa550c7e33277332045a7b9b
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34431226293c5da1fa89ae4a4f7df7c3053610c1b64cc683cf3b7fb2f12fa5b6
-size 17928
+oid sha256:db4a94e63b925fce895a5d3fed85f31baf6cd500d310315a2d7ef3ce9c924b22
+size 18302
