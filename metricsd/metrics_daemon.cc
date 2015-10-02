539fa96d79e43fca9773f233d6a7bb817bb51d80
Bertrand SIMONNET <bsimonnet@google.com>
2015-10-02 16:45:18 -0700

    metricsd: Expose the metrics status through weave.
    metrics_daemon will export _metrics._AnalyticsReportingState containing
    "enabled"/"disabled" depending on the current state and two weave
    commands (_metrics._{enable,disable}AnalyticsReporting) to update it.
    
    BUG: 24386281
    TEST: manual. Enabled and disabled metrics reporting.
      `metrics_client -c` reports the correct answer.
    
    Change-Id: Ic4a5ffc1e9f9cbc0b47ee34d3af83584d95da155

--- metricsd/metrics_daemon.cc
+++ metricsd/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4c147403a9393e86dde81d577b897ae7ceac365e1fd70d99b2ed2096b20388b
-size 32223
+oid sha256:c883d98c67767870f59cd6fb76f62a29bbf52a602d0a02a9bc1de1ca7350082c
+size 34211
