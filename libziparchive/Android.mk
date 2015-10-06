63c467480417a1128ff7b887429fbd157bdb2716
Adam Lesinski <adamlesinski@google.com>
2015-10-05 18:16:18 -0700

    Implement ZipWriter for quickly writing ZipFiles.
    The ZipWriter implementation exposes a stateful interface that allows
    bytes of data to be streamed in as they arrive. ZipEntries can be
    compressed and/or aligned on a 32-bit boundary for mmapping at runtime.
    
    Bug:22489826
    Change-Id: Ifdabcde3ea5747dd4bcf0c4ac0eb5c394277443c

--- libziparchive/Android.mk
+++ libziparchive/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0149d455f1ce1c85a8c38cc97ae808e09e0fec6f4328989d63911249543a4e6f
-size 2246
+oid sha256:f33a0ba2737b9eef7a7705b69c823e79d6f4792d954d677241d26450dc7d187e
+size 2454
---
+++ libziparchive/zip_writer.cc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c21b57a4f20e4be30fd3f807a36513fa5bf5e84c96ba1fddd8ed1064844f602c
+size 7781
