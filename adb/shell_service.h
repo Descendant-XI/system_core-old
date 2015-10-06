fe407ef221fd035c77c699f9bb66c2f5fe2ab6f4
David Pursell <dpursell@google.com>
2015-10-06 15:30:03 -0700

    adb: non-interactive shell stdin.
    Non-interactive `adb shell` previously only read from the remote shell,
    but we want it to write as well so interactive and non-interactive
    shells can both send data. With this CL, we can now do:
      $ echo foo | adb shell cat
      foo
    
    This is primarily usable with newer devices that support the shell_v2
    features. Older devices will receive stdin but the shell will still
    hang after all input has been sent, requiring user Ctrl+C. This seems
    better than closing communication altogether which could potentially
    miss an unpredictable amount of return data by closing too early.
    
    Known issue: non-interactive stdin to a PTY shell isn't reliable.
    However I don't think this is a common case as ssh doesn't seem to
    handle it properly either. Examples:
      * echo 'echo foo' | adb shell
      * echo 'foo' | adb shell -t cat
    
    Bug: http://b/24565284
    Change-Id: I5b017fd12d8478765bb6e8400ea76d535c24ce42

--- adb/shell_service.h
+++ adb/shell_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b60fd06017f37e14ecadc26099855c27fb5f0c0665388aa7812ad6b4f1c3cb57
-size 4631
+oid sha256:a29b39e9e2300b73139cb1c527166cf1aafe7debe5f5851c069ee1359ba83548
+size 4694
--- adb/shell_service_test.cpp
+++ adb/shell_service_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c72f6664ee2a5524dd346aca8190cd917848bb9fd031469be78963c8801a6992
-size 9432
+oid sha256:11b76a707da63cc4c77be09ee2c49a491467406de239da5f9352da22cc3b8ba8
+size 10185
