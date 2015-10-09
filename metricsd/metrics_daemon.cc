a3b8206ae9c7d5e183cc12eb8111004fd339fdfe
Alex Vakulenko <avakulenko@google.com>
2015-10-09 08:18:35 -0700

    metricsd: Update weave command APIs
    Weave has changed some of D-Bus APIs and metrics needs to be updated
    accrdingly. Command::Done is replaced with Complete(), Command::Abort
    now takes error code and error messages, Command::category is removed
    and Command::status is renamed to Command::state
    
    Change-Id: Ifbfd6d852f4a328168e28484d1152a24b8c8b42b

--- metricsd/metrics_daemon.cc
+++ metricsd/metrics_daemon.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c883d98c67767870f59cd6fb76f62a29bbf52a602d0a02a9bc1de1ca7350082c
-size 34211
+oid sha256:bcb7aa8fd7ed274bddbe4fc57d241425487ee1d34c4b2751d2b1b38705d43681
+size 34336
