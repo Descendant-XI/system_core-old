2914d9a7041670825f32de8cce43f64f922e910c
Sami Tolvanen <samitolvanen@google.com>
2015-10-14 19:46:16 +0000

    Revert "Error correction: Use libfec in fs_mgr"
    This reverts commit 3de3a0f351ee07cbd31db56d49cef3fbeb7e633b.
    
    Change-Id: I1f121cbc4431b8d8ff146eab29832a8dda1eb8ba

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17fe1e999aa8ec6f5efe0082eba7d0b22c378b5fa399e6cc042e28fdefabec17
-size 2696
+oid sha256:0b5bf711cd7584e4289ab35e7f983f18612c486fa004a69eb975772bf989689f
+size 2644
