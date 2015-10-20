bf30c29abb1061c209baed7d52d3d19c02f72580
David Pursell <dpursell@google.com>
2015-10-20 14:19:36 -0700

    adb: fix -Tt options to match ssh.
    ssh has slightly more sophisticated -Tt options that we want to match.
    This CL changes -Tt behavior to match ssh so that we allocate a remote
    PTY in these cases:
         -T: never.
           : if stdin is a terminal and user wants an interactive shell.
         -t: if stdin is a terminal (otherwise print a warning message).
      -t -t: always.
    
    Now this will work as expected:
      $ adb shell < my_script.sh
    
    Also corrects a small unrelated bug with escape sequences so that only
    a single tilde is accepted for the disconnect sequence.
    
    Bug: http://b/24565284
    Change-Id: Idab57ac98d81233b45c2613fe64ceb4398add961

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5525f395c1ea3444723c55c063291d93aee5b442650fdab9a8bcad268d486c2
-size 66935
+oid sha256:3479e45a5a619705eca53f0ca8ac2c69aa0dfb518f7d27a7bbe9dc87732ad42e
+size 68454
