6a60e6a2079d443074507b39f6a90d697fce5bc8
Dan Willemsen <dwillemsen@google.com>
2015-10-22 13:04:22 -0700

    Remove __DATE__/__TIME__ from init and debuggerd
    Keeping these means that every build has different init and debuggerd
    binaries, even if the source was the same. So OTAs that don't touch
    these sources would still need to update the binaries.
    
    Both of these messages are only informational, so can be safely removed.
    Bootchart already encodes build-specific information from the system
    properties.
    
    Bug: 24204119
    Change-Id: I7ebd65a20a3e031e7528c9f741616e20b3cd7446

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdff09512c18235cc9e2c60a493fd885fe0564bc30a6ade4df4d5aab9c940cbc
-size 18643
+oid sha256:a4a9f311cea830a8434b330aabb6786dd8dd4cbd44ed9a6414aab599574d48de
+size 18626
--- init/bootchart.cpp
+++ init/bootchart.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:553f99e05b562d388e18d5b525f9af0a06e1a008e18c94319cb1112b28429ddd
-size 8317
+oid sha256:27df9e4708761704c99777daf63d127c8a5fe312811d9cbbe435bc96daf226df
+size 8303
