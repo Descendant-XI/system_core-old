849f227e3b9879bd8e285464274797bc1b2aae22
Elliott Hughes <enh@google.com>
2015-10-21 18:52:17 -0700

    Fix some compiler warnings in libpixelflinger.
    class/struct mismatch and use of 'register'.
    
    Change-Id: I6cfe0f7fdc214c3a009eb01251f5ea9a8fdb895d

--- libpixelflinger/include/private/pixelflinger/ggl_context.h
+++ libpixelflinger/include/private/pixelflinger/ggl_context.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34c3f0ee5286f173485d898423c9f6149b3e8513e4764337a579f0abb07db343
-size 16405
+oid sha256:d06f66fedecfc1ce3d66ebaf60f2a995fa320acd1955550716266777f9811869
+size 16406
