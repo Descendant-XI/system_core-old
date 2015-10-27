7e046980f41b39987a38ecd098c9bc93db52164c
Elliott Hughes <enh@google.com>
2015-10-27 16:03:15 -0700

    Show transfer progress in adb sync/pull/push.
    Change-Id: If5439877d060f9bab29cf84be996071cf680c2d4

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3479e45a5a619705eca53f0ca8ac2c69aa0dfb518f7d27a7bbe9dc87732ad42e
-size 68454
+oid sha256:6a98871832b84b8a35494c8a2c36bd699606972c68c4ff6ae4a6c25638c3af25
+size 68109
