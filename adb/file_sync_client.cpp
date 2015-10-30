81081a54ffbe765ffdb9f86cf119c338a897d2c1
Josh Gao <jmgao@google.com>
2015-10-30 16:57:19 -0700

    adb: allow multiple args to push/pull.
    Bug: http://b/25324823
    Change-Id: I369dd97adbe1d08e18e84af776ce8b1d61251835

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f636e78a53442b8af512e801d0aa960aa3d637bafb62764432ba319ca205dd09
-size 25630
+oid sha256:afeae61c60f014d78c65c1e11f71ecbde5cc48ea18a11e897c1774ea1fc42ee4
+size 27576
