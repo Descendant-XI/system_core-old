dcc5802d2707b76e13d631705724cd5dacdf86b3
David Pursell <dpursell@google.com>
2015-10-30 11:22:01 -0700

    fastboot: create Transport object.
    This CL creates a Transport object to provide a generic interface for
    various transports. Specifically this is designed to be able to add UDP
    support to fastboot in an upcoming CL without changing the main program
    logic.
    
    Also includes some minor code style fixes and replaces malloc/free
    in the USB implementation files with smart pointers and std::string.
    
    Bug: http://b/22029765
    Change-Id: I68641af0da7d13db4647f5e20a18d04d67f0b327

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97be05ca656b75831229c0cbce7d54b2eb44e80eb91532d2caae7ce8fb17e306
-size 47914
+oid sha256:184ac7e6b124ba0ab1343a0e2c7e4ed8daef86aaa836d5dfc6363f0ced295ab9
+size 48411
