7e548a960af573f5a72266044c5f1b95011a6b24
Elliott Hughes <enh@google.com>
2015-11-02 15:51:12 -0800

    Work around an angler bootloader bug.
    It's probably not the only device whose bootloader is similarly broken.
    NVIDIA did a sufficiently good job with Nexus 9 that it's almost a bad
    idea for me to do most of my development there...
    
    Change-Id: I71436cc5c33023be077ca77f6dad5dbe75b11b09

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4de9ce065b5899858a4720ed928256363a0d15950f8df131c1870fe95f3fb623
-size 38808
+oid sha256:6c7c4ed7c7c5c829061eaa27d1331d83a1c3fafe04be5b866a1bf8ade703c0c4
+size 38975
