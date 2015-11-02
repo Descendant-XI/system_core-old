76504efb5742775f018646cbd45c03da2caa590b
Dan Willemsen <dwillemsen@google.com>
2015-11-02 15:41:59 -0800

    Turn off -Wdate-time for crasher
    This is an optional diagnostic tool where it's useful to know if you are
    running the version that you just built. Allow the use of __TIME__
    without a warning or error.
    
    Bug: 24204119
    Change-Id: I78063c2694b80ccaf5891d1cd649ea17eda978cb

--- debuggerd/Android.mk
+++ debuggerd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8abfa1b29dd3c20f87aa698cb4b4eb60906f1492e6aa9d1855fe52bf5f4fef8
-size 3162
+oid sha256:e7b12c3e72a457e752f71dbee0eda4e451525712849a22db1fee706170f32e24
+size 3177
