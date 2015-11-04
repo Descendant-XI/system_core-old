feedbb46afb4eab85d98e0dbec4147bcd1937915
Todd Kennedy <toddke@google.com>
2015-11-03 16:53:08 -0800

    use new cmd command
    Instead of using the pm command, 'adb install' and 'adb uninstall' will now
    use the cmd command. Additionally, the APK will be streamed directly to
    package installer instead of creating a temporary file.
    
    Change-Id: Ie7529afa5b039d29cc8183f36085ff4e66b3457a

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37d61e5d2fb860c75259491b2cd63e8ba7cc2f296ef1b89ac64453e709b6be2a
-size 68620
+oid sha256:72df167ca9fc41752faef191d40fbfaaa087cf5f84f59bee68391515d5dcf5a0
+size 71954
