9ee66dfcc385d9f005e50611354c14aefb541619
Josh Gao <jmgao@google.com>
2015-11-04 14:35:43 -0800

    adb: remove use of mkdirs from `adb backup`.
    `adb backup -f nonexistent/directories/foo <pkg>` shouldn't create the
    directory structure if they don't already exist.
    
    Change-Id: Ifd8769ed7ee5e733a6635751ad09b714ab58ea4b

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37d61e5d2fb860c75259491b2cd63e8ba7cc2f296ef1b89ac64453e709b6be2a
-size 68620
+oid sha256:91d950fa96d1b3c5159cea2c8cfd6a0310cdd64d902353568299d31de99b9307
+size 68598
