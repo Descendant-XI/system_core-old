5874e581dceb9c27a97d207d01ecda95d4dbd3d6
Elliott Hughes <enh@google.com>
2015-11-05 18:55:20 +0000

    Revert "Revert "Add MIPS64r6 support for libpixelflinger""
    This reverts commit 7fd121788a892a0dfd4a9594304cad85fe366349.
    
    Change-Id: Ic1204a8407c235b07c643764d5f2800631fecd72

--- libpixelflinger/Android.mk
+++ libpixelflinger/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce7d20efa7f2dd8959f0538393618d73bef626b319fadfc0ddbf7daa4f030cc
-size 1925
+oid sha256:af3ffbaae6efc4bcedd672bc5a45da51ba6cab91806d7776b2b0fc1f2cd30174
+size 2191
---
+++ libpixelflinger/codeflinger/MIPS64Assembler.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc19deb87a97d47b53f2f098fac7ce080832a2ac5176c9e7aa0e8abd6b9f6471
+size 14491
