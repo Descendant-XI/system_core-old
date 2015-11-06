bacffc74f07e0595096238ee641e7d2ab352441f
Mark Salyzyn <salyzyn@google.com>
2015-11-06 12:26:52 -0800

    liblog: resolve deadlocks
    Although ever present, an increased regression introduced with
    commit b6bee33182cedea49199eb2252b3f3b442899c6d (liblog: logd:
    support logd.timestamp = monotonic).
    
    A signal handler can interrupt in locked context, if log is written
    in the signal handler, we are in deadlock. To reduce the contention
    and chances for this problem separate out timestamp lock from is
    loggable lock to reduce contention situations. Provide a best-guess
    response if lock would fail in timestamp path.
    
    Use a common lock() inline within each module, with a comment speaking
    to the issues surrounding calling a function that has a mutex within
    a signal handler.
    
    ToDo: Hold off signals temporarily in mainline, restart when unblock.
          Can not use pthread_sigmask(SIG_BLOCK,,) as it breaks AtCmd.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 25563384
    Change-Id: I47e2c87c988c3e359eb9eef129c6a3a08e9eedef

--- liblog/log_is_loggable.c
+++ liblog/log_is_loggable.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94ddb93b23bb747006d0058b80095867c4b98f002ac8a0921a321b39b0368fe1
-size 5703
+oid sha256:f580728fd019350f3839eb243442df0d574a6011283fca4acaaece9e0c594a3c
+size 6504
