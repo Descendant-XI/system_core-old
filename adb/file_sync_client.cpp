5ae88b5203e3a56617b9050f23cc386b5259bc97
Josh Gao <jmgao@google.com>
2015-11-07 15:38:19 -0800

    adb: don't explode directories when pushing/pulling.
    Previously, `adb pull /data/local/tmp` would spew all of the contents of
    /data/local/tmp into the current directory. This patch makes push/pull
    keep directories intact.
    
    Bug: http://b/25394682
    Change-Id: I2304ae9e61745a2b9536f58a6012640bf8ff422a

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2aaaa127571a959f553a83e7b257471397bb673246e8186892eabf21b7b708af
-size 29186
+oid sha256:c330a1665ad33854fd248fa7976ac6beabbe3e7a32edffcd3f0f4746f9d2913d
+size 30431
