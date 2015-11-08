c7041844c689114aefa360f0defdb19b886317ec
Nick Kralevich <nnk@google.com>
2015-11-07 16:52:17 -0800

    Enable hidepid=2 on /proc
    Add the following mount options to the /proc filesystem:
    
      hidepid=2,gid=3009
    
    This change blocks /proc access unless you're in group 3009
    (aka AID_READPROC).
    
    Please see
      https://github.com/torvalds/linux/blob/master/Documentation/filesystems/proc.txt
    for documentation on the hidepid option.
    
    hidepid=2 is preferred over hidepid=1 since it leaks less information
    and doesn't generate SELinux ptrace denials when trying to access
    /proc without being in the proper group.
    
    Add AID_READPROC to processes which need to access /proc entries for
    other UIDs.
    
    Bug: 23310674
    Change-Id: I22bb55ff7b80ff722945e224845215196f09dafa

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24fe7b794d99125404a9a3b6810be2b87bd49c1b3b2d405a1962783d5763572f
-size 20942
+oid sha256:77d35aba20d8f690da2e5ca6ddb1a58109fdbb7044b4a2696006dba7fe2e3e91
+size 21017
