e57bb160f45c6793fd1a5c5e37d64d2466a20914
Todd Kennedy <toddke@google.com>
2015-11-10 00:03:25 +0000

    Revert "Revert "use new cmd command""
    This reverts commit 3e3b4ec00770258f4e66684addded262dc13003a.
    
    Change-Id: I8bfcd8a2a285f4254539ebca9f70222aa2448ef4

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:561d2cf5efcd0a44f9c069b31c4a0b2aeb010c177a18d29f9f521150cb1d3408
-size 29319
+oid sha256:1da33d15ac744a98355291d96b3980c23812e6920f57b30660418af38ce1938b
+size 29427
