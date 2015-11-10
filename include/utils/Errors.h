d397251966f2f62705945407a66b8a2d857d3193
Christopher Wiley <wiley@google.com>
2015-11-10 11:11:08 -0800

    libutils: Add UNEXPECTED_NULL status_t
    We use this specifically in situations where we expect to read
    an object from a Parcel, but fail because the object is null.
    
    Bug: 25617251
    
    (cherry picked from commit c3441e69c06f21a7f7cc5582f99849da0507d744)
    
    Change-Id: I07e50c21ce979484f9a472de490b0bfded37a896

--- include/utils/Errors.h
+++ include/utils/Errors.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e16f6257b3251e1ef16abf5cbbbb24b6841bb9e905068b47e505f40fa61fee4c
-size 2557
+oid sha256:9ce4d4554bfedeb8c242e9fa7ed24dd43d1a710c48713c38973cc246cf641b14
+size 2604
