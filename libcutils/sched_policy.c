0d461e5a2b569736547cf4de74dbc2b6178ace24
Tim Murray <timmurray@google.com>
2015-11-10 14:31:09 -0800

    Improve cpuset support for surfaceflinger.
    SurfaceFlinger needs some of its threads in the system-background cpuset
    and some of its threads (the binder pool) outside of the
    system-background cpuset in order to improve UI perf/power
    consumption. Remove surfaceflinger from the system-background cpuset in
    init.rc and allow a thread to place itself in the system-background
    cpuset given enough permissions.
    
    bug 25745866
    
    Change-Id: I85f7e41c5439e6ad7cc2d355e51f5dfb3a0c7088

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96daafebb2d1c96a19595ef9cce9e068e58a51217724122c3221cb5fcf2c7d7b
-size 9656
+oid sha256:478521de5351c76d911afbd21da910bbdb9d3787038d81b81a85a78bd810dc04
+size 10013
