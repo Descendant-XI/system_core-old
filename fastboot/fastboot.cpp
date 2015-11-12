50f0ea4a1fba8d13fdaebe645cd96e6cc6836119
Elliott Hughes <enh@google.com>
2015-11-12 07:28:39 -0800

    Cope with angler's whitespace-padded partition size reporting.
    Before:
    
      $ fastboot -w
      wiping userdata...
      Couldn't parse partition size '0x        0x66257ee00'.
      wiping cache...
      Couldn't parse partition size '0x        0x6400000'.
      erasing 'userdata'...
    
    Bug: http://b/25653580
    
    (cherry picked from commit a2db2618ecd9e96cb0b00d05a7f357c8b4b5fc62)
    
    Change-Id: I15e6bee4d88695d4482013c4f86586276acdea84

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7af26bf7c49ec3d19a2eec7fe91e9567a8a8f26add05303ee86cf230c83a1a6
-size 47782
+oid sha256:9e266a08e4f992c0911cb92207b86ed79c6cfa10a1a5160015bc87f18551f0a2
+size 47920
