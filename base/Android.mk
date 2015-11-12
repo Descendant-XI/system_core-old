708391225ff4981922155a7e7ff90365bf4c2861
Josh Gao <jmgao@google.com>
2015-11-12 11:54:47 -0800

    libbase: remove exit-time destructors.
    Removed for the same reason as the adb exit-time destructors.
    
    Bug: http://b/23384853
    Change-Id: Ic124ecb9df132b850a3855e207baffec926dde29

--- base/Android.mk
+++ base/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2724e575eee3995ceb68baa89ca1805c28b5c76778c146e0c86218a54972f0a9
-size 3288
+oid sha256:2016fbe574b63a805e92751ec7b44d663a9ecd8a127e7cfc3c31ed54a571bfa7
+size 3535
--- base/logging.cpp
+++ base/logging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b3ab8c72e5751f160b04d11d521d34d4ea740e0589ec018110ace642d7e800f
-size 9875
+oid sha256:1794f611bba24568db7bb8308d37abf418bfa1f7776ae9732845373d5b65992c
+size 9929
