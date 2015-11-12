578c070b6db50ae59e46be2ebd4a583031f4aed1
Daniel Rosenberg <drosen@google.com>
2015-11-11 16:15:30 -0800

    Changed set_active to be a flag
    set_active must be able to accept flag like arguments
    
    Change-Id: I6dd162453835d2d64fd8d877f2a7b98e8dd8a907

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8212cc7158e6521c9ce91e72ca786fda58806ce8bf2d3a4f24f92bf55f78dbfa
-size 45984
+oid sha256:15b143e59d6cd2fd4eb71cf1ef93a1bb5f3c62115ad43b23fc5d6ebd6b254fc5
+size 46761
