89b9411a1854a98e1f7bc6a0e24f011bf664c73d
Josh Gao <jmgao@google.com>
2015-11-13 17:55:45 -0800

    adb: don't link against libcutils on Windows.
    Bug: http://b/25693157
    Change-Id: Ie1f082a094371124b4d66c87c7ab1a0706271b9d

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b4c25e736594d8eafca0bfb7dfed5c41e372a4371cc9b9ea33e7352a321a2d9
-size 9006
+oid sha256:417d9dbeb4e91ec74f45ce9ed4b1e5f100f551e2bdc8ad022789178a84dc41b2
+size 9110
