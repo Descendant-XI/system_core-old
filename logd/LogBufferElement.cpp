6b073ebd8761be39f7654bddddd84cdadf71571d
Mark Salyzyn <salyzyn@google.com>
2015-11-16 19:00:41 +0000

    Revert "liblog: resolve deadlocks"
    (cherry pick from c2e7d4965f86dbe90cece6d25e91d934a698d195)
    
    This reverts commit 7a2a3071921b11d7e476862e7f9cbe72be43d0f7.
    
    Bug: 25693940
    Change-Id: I9a7c926289e972f80c03c92e33535e1dedaa7381

--- logd/LogBufferElement.cpp
+++ logd/LogBufferElement.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a52094e134b74c7c9699436abd4d02c58231a7412d7b2dfd5496aa653e51397c
-size 6906
+oid sha256:d80087d07261bf6454a5bb22ae94798ff7e1c2aa5f7bde1cb3fc2fa96aa26a70
+size 6794
