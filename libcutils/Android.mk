f0f15cd733ec3e362e7668e6e93e09a151713105
Josh Gao <jmgao@google.com>
2015-11-17 14:17:38 -0800

    libcutils: add prefix to local variables in Android.mk.
    Change-Id: I0561affbcc306c20f8aa87beb2d9728a9bb79f5e

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d2c68344e60d08c8b35e245fc425ba57a4dd0f876f9af4a4a0fd8e4e38b854a
-size 4098
+oid sha256:27aa57c13d242505d38c94fcb733d10b566a67137d1567b17b5171ae1c466639
+size 4380
