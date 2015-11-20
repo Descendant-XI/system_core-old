b30a2d78225df996dd96088c6bc45c195948d809
Paul Lawrence <paullawrence@google.com>
2015-11-20 07:07:12 -0800

    Encrypt on reboot
    Change encryption to happen after a reboot, not before. This
    removes the problem whereby if data cannot be unmounted, we cannot
    encrypt.
    
    Bug: 25426629
    
    Change-Id: Icb2ec6cf330b5cc45b7e944c858a314f983fcaa4

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fd49698bf142e31243c108fd9c385055e2dc1c315199a2b3f69feadbc8dabf5
-size 29505
+oid sha256:a5fb3b7a028fa60ac4cd18a265f72d790ae081a130bb547ad4fcaf7759c2483a
+size 29842
