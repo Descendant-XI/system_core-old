1ab81b4a3d2e4da8d22582899b12b22a567f5be0
Elliott Hughes <enh@google.com>
2015-11-20 22:01:06 -0800

    Fix "adb sync" (and "adb push") error reporting.
    This patch ensures that we read any error response from the server if the
    server closes the connection. Unfortunately, that's not sufficient to ensure
    that we always see the server's error message --- sometimes the data just
    gets thrown away because we keep writing without reading. Setting SO_LINGER
    avoids this.
    
    Bug: http://b/25230872
    Change-Id: I96c019cc72bd139198de79bf29e6536cc462c20f

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac99e167d36a7b44f1726f9aef892f44eaf29d32278a480e5204d7ce8fb4621
-size 30219
+oid sha256:629d7cbea899197f7f92826c15f6bd9acd17a172e1939f9e506f69329a3c543d
+size 31075
