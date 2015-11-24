a6432aa0d12f6848ff8577b6f955e25ee5c45047
Josh Gao <jmgao@google.com>
2015-11-24 09:12:21 -0800

    libcutils: turn on -Werror on Win32, fix resulting build break.
    Mingw doesn't provide strndup or strtok_r used in str_parms.c, resulting in
    -Wimplicit-function-declaration being emitted for the Windows build.
    None of the consumers of str_parms.c appear to be compiled for Windows,
    so we can just remove it from the Windows build.
    
    Change-Id: I36b66fc54ed261d38f5294bc8662ddb2db4fe64f

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27aa57c13d242505d38c94fcb733d10b566a67137d1567b17b5171ae1c466639
-size 4380
+oid sha256:aa86cbfd9ef1e70d0eca057c4802d567f33e9923a8575759f0a8da76a0f9aee6
+size 4278
