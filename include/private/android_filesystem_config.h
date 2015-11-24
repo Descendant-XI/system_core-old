3802ca6fa81d754ea31b6dcb69794330a4a57e47
Pavlin Radoslavov <pavlin@google.com>
2015-11-23 17:18:31 -0800

    Use GID "wakelock" to control access to kernel wakelock
    * Added new kernel GID named "wakelock" (AID_WAKELOCK = 3010)
    * Changed the group access for /sys/power/wake_lock and
      /sys/power/wake_unlock from "system" to "wakelock"
    * Added "wakelock" to the list of groups for the healthd process/service
    
    Bug: 25864142
    Change-Id: Ieabee9964cccec3107971a361a43aa9805164aa9

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0c84791bfefe324d0e9e393b4a4fa3acfda15283084d965a2e9060b6debde2
-size 9199
+oid sha256:a2e4b3a44634628c9883a40550156b1e8c230b1b9dc0734fcbe01fd86852c1fa
+size 9317
