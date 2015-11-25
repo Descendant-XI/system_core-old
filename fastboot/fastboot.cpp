367af32b32a51be7cb85732d7318b6522ff86e37
Daniel Rosenberg <drosen@google.com>
2015-11-25 15:17:10 -0800

    fastboot: Re-add set_active as a command.
    It turns out that adding a -- allows suffixes
    starting with - to work fine, and there are
    edge cases where calling set_active twice in
    a command is useful, so the command version
    has been re-added.
    
    Change-Id: I528c258bf23ade61db530eb27586c1a1721896bc

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98de2ae26fbe3acfe319d8715198cfedb0859ed4be479ba90996f6babcc51ecb
-size 48399
+oid sha256:276f51ef35383b2913c04aff13be149a3e6e085121830bdee97405b85162700a
+size 49612
