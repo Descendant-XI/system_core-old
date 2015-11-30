65c578ef9230e61a5d649b75a1b69a50de2bc21f
Josh Gao <jmgao@google.com>
2015-11-30 10:27:54 -0800

    adb: remove extraneous newline from skip message
    Change-Id: I12314da589bf0db14b37ae4c1f526665182f4776

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa646cf44c58da35a24f83d065648486ab70c484fc20cedd1f3879186977455f
-size 30299
+oid sha256:639f0c57f5165db3928cf920fbcd424e1e179aa0e6289ad5d52b6e51710c5e9f
+size 30297
