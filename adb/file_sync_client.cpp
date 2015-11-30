e3918d3237867a0bc8a23d92394dcf2f696cd6ba
Josh Gao <jmgao@google.com>
2015-11-30 10:53:22 -0800

    adb: don't divide by zero
    If we stat a file and get a size of 0, and then successfully read bytes
    from that file, we would previously divide by zero when calculating the
    percentage completion of the file. This case happens either when we're
    racing against something else writing to the file, or when we're pulling
    magical files such as the ones in /dev/cpuctl/ that lie about their
    size.
    
    Bug: http://b/25925733
    Change-Id: I980b9c14f44a1eb4a42bc8736c94fa6db06c08d1

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac99e167d36a7b44f1726f9aef892f44eaf29d32278a480e5204d7ce8fb4621
-size 30219
+oid sha256:2043c92ea935ce3ac1cae6352cb09f6b6b420e2449a04bc4e65988bdceb9aa46
+size 30863
