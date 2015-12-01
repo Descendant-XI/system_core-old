e43bdc507b5578c6cc3df4a196279e79e63907d9
Mark Salyzyn <salyzyn@google.com>
2015-12-01 15:57:25 -0800

    logd: liblog: logcat: switch to android_log_clockid()
    android_log_timestamp returns the property leading letter,
    it is better to return a clockid_t with android_log_clockid()
    
    Bug: 23668800
    Change-Id: I3c4e3e6b87f6676950797f1f0e203b44c542ed43

--- include/log/logger.h
+++ include/log/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ee912887a71a91695554ea505d6fbb100af971f7a737c88600fc943b09239ec
-size 6637
+oid sha256:819627f8f1fa5daaac4760b7819d5313db317225c476dc71265df4be7bc50cfb
+size 6659
