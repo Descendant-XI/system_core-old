11e09cd498667f18c4946c2f9563b2e97a9d347a
Sami Tolvanen <samitolvanen@google.com>
2015-12-02 14:23:09 +0000

    Set up dm-verity in EIO mode instead of logging mode
    If the device is corrupted, set up dm-verity in EIO mode instead of
    logging mode. This prevents corrupted blocks from being returned to
    user space. Note that restart mode is used by default and a warning
    will be displayed to the user after corruption is first detected.
    
    Bug: 19277516
    Change-Id: I38966d73eb814836bc34b4bad1192583e5010b36

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59cf1f087d9e64c0fe78899ae1fcef8914c9b125ffa6bad10221d2ad6ea459e8
-size 26408
+oid sha256:87977f7ba038b7cb9a69f73d723d987b2f54c5f3645ac92c2b887a5172ee0c59
+size 25749
--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da2528742aa789b95359ae1044d603471d7cf2f2686bd3452b2a9904cf9bc1c2
+oid sha256:fe784c90dd5d7c1c5658b182dc78b8e521e5099bf2ef46e56bdb6064c0e126c0
 size 27463
