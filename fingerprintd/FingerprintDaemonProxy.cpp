9dd6fd4007e98e1888ecc522b300ef4183b406a9
Sasha Levitskiy <sanek@google.com>
2015-12-03 13:50:53 -0800

    Fingerprint: Plumb enumerate support.
    Change-Id: I5794f80404faf68d04f7b3f3b7346b9e28c99881
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- fingerprintd/FingerprintDaemonProxy.cpp
+++ fingerprintd/FingerprintDaemonProxy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a18e02c0bc2909ddc2d21bd948056d9e2a5d1a7d66562d77e1065285fa80656a
-size 9101
+oid sha256:fa25076fbf7abac70554e563c62829fa5fca283c467f7b7e620318f429480acd
+size 9742
--- fingerprintd/IFingerprintDaemon.cpp
+++ fingerprintd/IFingerprintDaemon.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47d09ec3e6602d9d63c8435138685ba42933d43458af47cc4ced36885436887b
-size 7551
+oid sha256:392e2d248984976e9469fff1ee2cbbec24886cbce76d8ad3f9dded4c25ff5e64
+size 7918
