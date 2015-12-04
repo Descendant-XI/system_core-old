15e6540754f70930b1f5d2bad690cca04bf657d0
Chien-Yu Chen <cychen@google.com>
2015-12-03 16:32:12 -0800

    Sync internal<->AOSP user list.
    Bug: 27299198
    
    Cherry-picked from:
    
    Restart cameraserver
    
    Bug: 24511454
    Change-Id: Ia27f4ef0eb71f891c789f637a21b04afe0b1c4e9

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3a40795ac7b83840defbd6b4be18d3f5f25e07c8f2a063f5d7465cfcdce39ad
-size 9818
+oid sha256:277fb634f148479e52aa2db602484be0a45c42ea80d5c56119ac6c203905e194
+size 9921
