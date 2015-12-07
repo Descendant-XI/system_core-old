119f40742ed0073b2a0a84182bca7d146d0b285f
Mark Salyzyn <salyzyn@google.com>
2015-12-07 18:30:58 +0000

    Revert "logd: liblog: logcat: switch to android_log_clockid()"
    This reverts commit 77b5696b1dea6f7afed89e113e479f4a131c14fc.
    
    Change-Id: I7711bf1a7e3f72ed29a2498d7287b725a0e624bd

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d74ba68be4ce477c2aaa45c38dab61e64d8cb1c8d67e838d62a8c3a1316d51c8
-size 33243
+oid sha256:dc3dcdca0a46d07b6ab2006d503de6c31eb9177035195a78fa482feb68286265
+size 33266
