0f653eeea326040aa7296204a90e16157d61c1b7
Mark Salyzyn <salyzyn@google.com>
2015-12-07 12:27:06 -0800

    logd: test drop __unused
    Change-Id: I1ccf49809b8a0a125601bdb52834c463e2d94165

--- logd/tests/logd_test.cpp
+++ logd/tests/logd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76ad1efb981ff28a6b85ce9d0be187d6c3b769a398441775c9ba9835261893c3
-size 17622
+oid sha256:5666ac04f3ea18f81e23b0493406307220e4bd6cfeeff0e2c910232c43da07f6
+size 17573
