b54061aa7fc83c50a7fdb43971bdcb8349c537b0
Dimitry Ivanov <dimitry@google.com>
2015-12-08 11:16:56 -0800

    Apply permitted path to the classloader-namespace
    Bug: http://b/25853516
    Bug: http://b/22548808
    Change-Id: I283e6ee8d743bc3ab41aab9f36af0abbe729943f

--- include/nativeloader/native_loader.h
+++ include/nativeloader/native_loader.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdf5670bc93a7edea86dc471118339aac164b69664df1fc257918c01f5fc9a6a
-size 977
+oid sha256:f1b465a193f359387bee8a6f97dedadd3e170ca98b0e9e1f7d274612358c0cbf
+size 1001
