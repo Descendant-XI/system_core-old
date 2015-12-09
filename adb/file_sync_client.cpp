bf3aa8317d43fbebdebc7a0b9691f775c126fef0
Elliott Hughes <enh@google.com>
2015-12-08 16:01:15 -0800

    Simplify adb LinePrinter newline handling.
    We had mostly-working hacks before, but it's time to just modify LinePrinter
    to suit our needs. If we tell LinePrinter what kind of output we're giving
    it, it can manage things automatically.
    
    This fixes the minor bug where we'd sometimes have a blank line after an
    error message.
    
    Change-Id: I07ff52437f2402de311e237dd1b2dd338d9b668a

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aab3ae6752f66763ffb68044fa430da4b68bb53b2f4347d078fc21783f9aff8b
-size 31935
+oid sha256:148280c9c41ae3d9f41876a80aaf21b645b5cfe715c30b55df0fe65ec3ec5e66
+size 31846
--- adb/line_printer.h
+++ adb/line_printer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa59d14e7ba3f9d0ceceb5b589ecd38027c8164b00bc2db8b50da66b94fe2005
-size 2162
+oid sha256:ad98eb76a8e81ad5a400e58d891a6f391092e10c09fcee27874b91ba807e870f
+size 1534
