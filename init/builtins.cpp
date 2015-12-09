f618c104c29c0920b64281fc4d0944d830c253ca
Paul Lawrence <paullawrence@google.com>
2015-12-09 13:53:09 -0800

    Add createuserkey
    Needed so we can make the user0 key on first boot
    
    Change-Id: I4699dbf2efdec44bb09bc4cfa7eeb59e56cb93ab

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11d8cb1871c00b2862c6a7bf191902edcccfec96017fbdf613a24bd9b2b56c52
-size 27471
+oid sha256:51eebce14642b0c19058358829bd0896a2b7b8680d3574a823b4bbdbcb80a588
+size 27840
