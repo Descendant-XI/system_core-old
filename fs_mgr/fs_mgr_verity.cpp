06ea69d846d72c64236b6c4ef4ec14035d16453d
Sami Tolvanen <samitolvanen@google.com>
2015-12-10 00:04:10 +0000

    fs_mgr: support upstream dm-verity without error correction
    Set up dm-verity even if kernel configuration doesn't have
    CONFIG_DM_VERITY_FEC set. Fall back to the always safe EIO
    mode if dm-verity doesn't support feature arguments.
    
    Bug: 21893453
    Change-Id: I4812bd74801c0abc8da479230f48b752858f9cd8

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c6ac6046c7dee4803b5b052a57ce627a24c16d8e3d29fbb19a2254d1e9493d4
-size 26416
+oid sha256:5715a5dbd8207ca0b82745c6824781705ce58874a52d25d2caed9ba1eedf798a
+size 27309
