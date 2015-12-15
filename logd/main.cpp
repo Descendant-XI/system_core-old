4efb79f712707ec4be97644ee7f36ebdaa6dca97
Mark Salyzyn <salyzyn@google.com>
2015-12-14 16:40:12 -0800

    logd: Allow flags "eng" and "svelte" in boolean
    - enhance property_get_bool, drop property_get_bool_svelte
    - enhance base properties with ro and persist variants
    - update and fortify README.property
    - primarily move auditd and kernel logger into a realm where
      they can be controlled by build properties.
    - Move logd.klogd to logd.kernel, and add ro.logd.kernel
      and persist.logd.kernel.
    - Add ro.logd.auditd and persist.logd.auditd.
    - Document persist.logd.security
    - Document log.tag and persist.logd.tag properties.
    - Document ro.logd.size, persist.logd.size and logd.size
      properties.
    
    Bug: 26178938
    Bug: 26029733
    Bug: 17760225
    Change-Id: Ibc1a497e45d69db5cf52133937f7ba6fb1d6cd21

--- logd/main.cpp
+++ logd/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3aa8929a520aa685d2d333129110260faa217f637b966c62e1a87748f5c9508
-size 12833
+oid sha256:9f6c54fe46679ed058fc63bea620adfe80546fec6fe7050346f82d175ecf3536
+size 15069
