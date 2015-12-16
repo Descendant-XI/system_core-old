8151888faa1a23bf5a1b5cda5454cb737461693f
Alex Vakulenko <avakulenko@google.com>
2015-12-16 12:23:14 -0800

    Fix metricsd to conform to the lastest server schema
    The new server push enabled component/trait validation rules. This
    allows us to use the new schema that does not require vendor commands,
    parameters and state to start with underscore ("_").
    
    Also fixed a few minor errors in schema definition.
    
    Change-Id: I184dd8e4c69c05e0dd1b0dddb1f9a7b0aef78841

--- metricsd/metrics_collector.cc
+++ metricsd/metrics_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6e72fe133e04a45d1cf847dca8f6f682d6afd10eafe2720b4fdca7c54bf0a4d
-size 27094
+oid sha256:f4fae31a9784161b5ad453f0589bf574e4ed5294724a2665ca5b741a410a3708
+size 27091
