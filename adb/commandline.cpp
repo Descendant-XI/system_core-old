71bb3e1eee36016d2e0218deb48a59915d9d5589
Elliott Hughes <enh@google.com>
2015-12-17 14:04:38 -0800

    Improve "adb install"'s error message slightly.
    We still don't really have a clue what went wrong, but "Failed to write"
    made it sound like we did.
    
    Change-Id: Ic58b13202e2d8ea6b095f7046db9d0771d77ce65

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d456245731e32c6fbbd66f5dcb3b8866ea9bcfc7acc9b92db46ccd9e5fd93d
-size 75588
+oid sha256:9e9960d4fc43601256671fd3817a7408f35a525440f710b0cb08afa8298c0807
+size 75569
