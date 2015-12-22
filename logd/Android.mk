073e9575aa5ac9244367f9b5297046972c39e107
Mark Salyzyn <salyzyn@google.com>
2015-12-22 15:50:14 -0800

    logd: liblog: whitelist "snet_event_log"
    Dangerous bridge to cross to whitelist, who is special, who is not?
    Rationalized as these events are used to catch exploits on platform.
    As it stands no one should be allowed to block any messages in the
    security context, not even for development purposes.
    
    Bug: 26178938
    Change-Id: Ibdc76bc0fe29ba05be168b623af1c9f41d7edbd2

--- logd/Android.mk
+++ logd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b62192fde1fa1dc60d43e07ac9f23a995128417ddec44f91e2a05d80c2ad6b93
-size 1073
+oid sha256:6bf4e6a8574115da6e75c340126e7192cf253fc89a19be8ebfc6150256dd33a6
+size 1137
