1938445422a40d3ceaac1d710535c9935c0df799
Alex Vakulenko <avakulenko@google.com>
2015-12-29 15:02:00 -0800

    metrics: Switch to use Binder interface to weaved
    Now that weaved provides a binder interface for its IPC, switch
    to using it instead.
    
    BUG: 23782171
    Change-Id: Ic3c485f99580fe41e287eaf238f033149787f181

--- metricsd/metrics_collector.h
+++ metricsd/metrics_collector.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2d368fef6bf742034039b007cf229aee19ff1f624ed35d8ab24c10ec1904565
-size 10622
+oid sha256:f7eb8d38c32209082ee511b4202eea323c1292cfc082d3f13690d46b12159bbc
+size 11013
