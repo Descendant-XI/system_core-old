b52a36cd7d630af09b0e052f0efbefb23f62f11c
Alex Vakulenko <avakulenko@google.com>
2016-01-04 12:22:05 -0800

    libbinderwrapper: Add BinderWrapper::GetOrCreateInstance()
    In libraries relying on binder it is useful to be able to obtain an
    existing copy of BinderWrapper or create one if it doesn't exist.
    
    This allows to abstract the actual RPC (binder) from clients who have
    no other dependencies on this RPC.
    
    BUG: 23782171
    Change-Id: Ie775d3d8ab83d75e38abc7e1385eb39a363555ef

--- include/binderwrapper/binder_wrapper.h
+++ include/binderwrapper/binder_wrapper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95210ab15470b985fc1781c1053f0cf37befad77c58f6760fa0353870acb2e3a
-size 2792
+oid sha256:3219c2007231e3a720eb428eb99de13e9f92ca0014a248a30577c2b481b3680c
+size 3051
