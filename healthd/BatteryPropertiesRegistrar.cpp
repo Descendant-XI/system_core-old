dfffa9291b28c3719720ead67c3da303448f37d6
Todd Poynor <toddpoynor@google.com>
2016-01-05 19:41:34 -0800

    healthd: BatteryPropertiesRegistrar binder service ref count fixup
    Call addService using a reference to a strong pointer to the service object,
    such that the proper IBinder object is ref counted, instead of passing the
    object's this pointer.
    
    Bug: None
    Change-Id: I72cac08c835a17b8cc263b2dc34864420b7a2841

--- healthd/BatteryPropertiesRegistrar.cpp
+++ healthd/BatteryPropertiesRegistrar.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:115c39bbec8f5fd95babdd9248556b6a5778e48a5d8d02cc4d357e2206091abc
-size 3411
+oid sha256:e85c0cea1f36b380d7e7e95834faf779c1c72ad34ed16747b1001e8794586b8e
+size 3464
