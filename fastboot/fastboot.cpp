f305d2e151771a64ffa2959a1c0a9eb64421551b
Daniel Rosenberg <drosen@google.com>
2016-01-05 16:54:40 -0800

    fastboot: Add "--slot other"
    This allows you to flash to a slot other than the current one
    without needing to enter the name of the slots.
    
    Change-Id: I6bf8c29817be0a29b1abb005f1e72056cee126df

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e853b0b10e945de1de55d2a5a5a6eb71e4530b72b3d8227bde7b7733f7c8b9cf
-size 49434
+oid sha256:c4932821568fab9eaa191025d9b0315caf99a19a2dbe8f9d759b41d4dbe7e785
+size 50001
