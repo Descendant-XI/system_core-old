b40483f59759e71f32a74bfdab297cc2cb998ab5
Rubin Xu <rubinxu@google.com>
2016-01-11 10:23:47 +0000

    ADB security logging
    Log adb shell, pull and push operations to the security log.
    
    Bug: 22860162
    Change-Id: I5d24e9d51040ae05a41d9fcb079e84351a217bd3

---
+++ adb/security_log_tags.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d9d818440de48b81f696e516f638fde3a4852c3d41118e0fd5829ad4449c3cd
+size 1056
--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a5ec99996c6f0305b74ec6fc4bc5140dfef443b2dc3633858855374625f76a1
-size 24714
+oid sha256:21bbab64a8cabd3ffb92dffa7e2b7fd4a89a81ef65543585feb5e3d7600a0da9
+size 24970
