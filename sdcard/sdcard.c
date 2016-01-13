a6473f2a87d51bf8fce1b084a8f9ae2a707b1520
Thierry Strudel <tstrudel@google.com>
2016-01-13 15:11:35 -0800

    [DO NOT MERGE] Use FUSE_SHORTCIRCUIT if available
    Use a non yet maintainer reviewed kernel patch from QCOM that greatly
    improves IO speed in case it is available from the device specific
    kernel headers.
    
    Bug: 24216004
    Bug: 30222859
    Change-Id: I4101d80082c9ad9d042dde5c620ddb309d193d52

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0bb4693f06d993233334dbedc8203daa44072069bd80fddd805e2947f1c4e54
-size 69898
+oid sha256:7f4dd1d9a4d01fcf8f6150199a4e61a1f66ee637df22a9c13973b0e6670f0c11
+size 70097
