8d5bdca18bcef910e4dfe01ab673d94fea50c455
Jeff Sharkey <jsharkey@android.com>
2016-01-14 11:53:42 -0700

    Be strict, but not that strict.
    Certain apps decide that they want to chmod() their private data
    directories to gain more security.  We still want to carefully
    enforce owner UID/GID, but relax the mode check for now.
    
    Bug: 26549892
    Change-Id: I362d530ba0b20fb23f427ac082ee003864adc57d

--- include/cutils/fs.h
+++ include/cutils/fs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9465d03695dd601c5b5ac4eb13a49376cc417f30ad322dbcfddbc136d93658
-size 2507
+oid sha256:4857ff6d62a7989a01f42be979453be34c68f736fdd13d63c8df8eacdf4e9a96
+size 2497
--- libcutils/fs.c
+++ libcutils/fs.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e208fc9eecdc7ae411f2ad5221d6f4d7ac65f913f1fbba94691bb266026be4
-size 6955
+oid sha256:11a8b6e943b7119266a1c06941c40500fff23d651572df02f175e877bec2b3d1
+size 7292
