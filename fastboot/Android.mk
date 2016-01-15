d8c3bdada91361edd8a24f66e5449ec7be356d96
David Pursell <dpursell@google.com>
2016-01-15 14:19:56 -0800

    fastboot: use cutils socket functions.
    Now that cutils has cross-platform socket functionality, we can
    restructure fastboot to remove platform-dependent networking code.
    
    This CL adds socket_set_receive_timeout() to libcutils and combines the
    fastboot socket code into a single implementation. It also adds TCP
    functionality to fastboot sockets, but nothing uses it yet except for
    the unit tests. A future CL will add the TCP protocol which will use
    this TCP socket implementation.
    
    Bug: http://b/26558551
    
    Change-Id: If613fb348f9332b31fa2c88d67fb1e839923768a

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4b1a93b1b12dcaa8588d2d9b1775f992ff1de402c8947e271e8cfd285098482
-size 3798
+oid sha256:cee4aa362ec89000e0d320cb174e9d36a08de63aae118649d809a39154981958
+size 3683
