e9f8f2a0c53ee97016c9a316a7d64d4ef04758ec
Alex Vakulenko <avakulenko@google.com>
2016-01-20 07:53:57 -0800

    crash_reporter/metricsd: Update libchrome APIs to r369476
    The new libchrome has been ported from Chromium and some APIs have
    changed. Make necessary changes at call sites.
    
    Change-Id: I9e45975cdef43913270b82e3fca2c027e614a464

--- metricsd/metrics_collector_main.cc
+++ metricsd/metrics_collector_main.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebdfdd8ee8f75d1e969d7f765e6cff107a8faccab3e6cad0729aa246d4d83440
-size 3224
+oid sha256:e49dd5be5ad194fe59b558f55b2ecb12e8cc4abc49035c101befe59e754714da
+size 3274
