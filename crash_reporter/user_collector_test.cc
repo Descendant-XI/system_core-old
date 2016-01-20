ac8b9024c6b74cc935de339ac575951e2ce14b39
Steve Fung <stevefung@google.com>
2016-01-20 01:45:11 -0800

    crash_reporter: Fix unit tests to use ScopedTempDir
    Some of the unit tests manually create their own temp
    directories from a relative path.  This creates a temp folder
    in the folder that the test is run from.  This is a problem
    when using test_droid or a non-interactive ADB shell command,
    since the root directory is not writable.  Instead use a
    ScopedTempDir to handle temporary folders.
    
    Bug: 26671657
    Change-Id: Iaf71d345565dcb4393e80426784a864f7f8b5929
    Test: Unit tests pass when running brillo_WhitelistedGtests

--- crash_reporter/user_collector_test.cc
+++ crash_reporter/user_collector_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9a0a60a791db2c77aa0ee3e64a01537c1c4fca3c747103cf1ac13fb536c7bea
-size 14695
+oid sha256:1041325210a733d1985008b54df58ed06962925de04f2ac6bf7689aeadf97f9e
+size 14744
