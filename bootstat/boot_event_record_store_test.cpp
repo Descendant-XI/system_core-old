c70eddd99a22a2e735c2fc2213665ff28ace5071
James Hawkins <jhawkins@google.com>
2016-01-20 13:16:31 -0800

    bootstat: Followup cleanups from the initial bootstat checkin.
    * Clarify the README.md that the logs data may be utilized by anyone.
    * Fix a whitespace issue.
    
    Bug: 21724738
    Change-Id: Id472485f505afb14588ccd645844a3f4a728abc2

--- bootstat/boot_event_record_store_test.cpp
+++ bootstat/boot_event_record_store_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:786578b7ff3f8f85f0cf30a65a9f923c6e35c24bec215c0d49aa5c3663728ebd
-size 4508
+oid sha256:16ba8504e7ffb4a63e9700122462809dc0a5f742d9a77892812d25096c3bda0a
+size 4507
