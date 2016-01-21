6e05dbda8c87db503c53b87bd5f904025b5d949c
James Hawkins <jhawkins@google.com>
2016-01-21 15:16:36 -0800

    bootstat: Hook up bootstat to record the boot complete signal and log boot events in the LOCAL_INIT_RC file, bootstat.rc.
    This change also creates the /data/misc/bootstat dir during the
    filesystem creation block in init.rc.
    
    Bug: 21724738
    Change-Id: I2ad6913c0235d5f926c6ef8b3c65d650159c6ed7

--- bootstat/Android.mk
+++ bootstat/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9667f6b0f60cc735b0c82be69d0c7dcac0ac8419696c58c2677fa1a4e6678d7
-size 3927
+oid sha256:16aa6c47ecdd3f2547a9c5e00f601d0948deadcd40ba03532d7d669789821f3a
+size 3956
