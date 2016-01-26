e1b4f68239d9c8727adc07a15006fa971d51c3d3
Mark Salyzyn <salyzyn@google.com>
2016-01-26 13:02:20 -0800

    liblog: check getgroups for AID_LOG for AID_LOG_SECURITY
    Bug: 26792035
    Change-Id: I634d3c8f9f3175956bd70e80daa479af40180f90

--- liblog/logd_write.c
+++ liblog/logd_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6c201e8e66e2f9932edf7630a9a2287a7b2ea29f54e0ecd87c48ad388e26801
-size 19513
+oid sha256:bc0bf2befbdabc2d7b1c52c2d1bcfbf1dea9d17fc354c139bd59bca97d1ad549
+size 20522
