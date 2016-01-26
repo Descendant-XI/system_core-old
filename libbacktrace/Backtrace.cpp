b067bdb60c32f2e18f8b131c075f542b1732768d
Christopher Ferris <cferris@google.com>
2016-01-25 16:17:48 -0800

    Put BacktraceOffline code into a separate library.
    BacktraceOffline is the only code that uses libLLVM. Create
    a new shared library, backtrace_offline.so, that references this
    so that including libbacktrace.so doesn't wind up pulling in libLLVM.
    
    Change-Id: I90c502d833f6c464c3810dffde52a0c6da241cce

--- libbacktrace/Backtrace.cpp
+++ libbacktrace/Backtrace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4a87847d1bf2e6d2dbc0e9ad19f2dbb9821595d9e51149148c0714882e3de9d
-size 4509
+oid sha256:41f517c56b282131c94266ad61973205d7eb4e2988f3a32c5b69c8ee3df9aad4
+size 4243
