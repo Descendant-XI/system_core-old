caeac8d1112d30e1902bccdf98f992fbb0534bc6
Josh Gao <jmgao@google.com>
2016-01-28 16:21:12 -0800

    adb: fix mistaken uses of SendFail in file_sync_service.cpp.
    SendFail and SendSyncFail are similarly named, and have the same
    prototype. Use `#pragma GCC poison` to prevent use of SendFail in
    file_sync_service.cpp, and fix the places where it was used
    accidentally.
    
    Change-Id: I6b9a7e565b76dfab10f8ae38f889d046ad80a403

--- adb/file_sync_service.cpp
+++ adb/file_sync_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2220df7e0b6f62d51d366d29eb751c2112bb5158344c1a845e15ab45f64ce491
-size 12577
+oid sha256:3efe3c179c2aa6bd044db0e44fbb9ef8a8665699cb1924c08940aeb20974fba8
+size 12699
