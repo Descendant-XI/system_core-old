ecdda839b3e64ab549e86110ad31bc9d8278a637
Colin Cross <ccross@android.com>
2016-02-01 11:15:30 -0800

    Fix static analyzer issues caught by clang
    system/core/libsparse/sparse_read.c:260:51: warning: Dereference of null pointer (loaded from variable 'crc_ptr')
                            ret = process_crc32_chunk(fd, chunk_data_size, *crc_ptr);
    system/core/libsparse/sparse_read.c:404:9: warning: Potential leak of memory pointed to by 'buf'
            return 0;
    
    Fixes leak in sparse_file_read_normal, and null pointer dereference
    crash if an image with an obsolete CRC chunk was read with CRC checking
    disabled.
    
    Bug: 26904425
    
    Change-Id: Ibc72cd37602929ae2c248bea1cdd1d22ea03baaf

--- libsparse/sparse_read.c
+++ libsparse/sparse_read.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe1bb653a6fcfe0deb30db530dbe6ee007baa0b89a0fb9a244fe9828eebdf42
-size 10712
+oid sha256:e51a5234e6d86b496a5bcca396b39d9d54cef04fea1aea5b449d745278f4e19a
+size 10756
