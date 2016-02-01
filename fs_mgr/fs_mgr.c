7861668746dd44dbe5745af4cf24d1fa26fc4b97
Hung-ying Tyan <tyanh@google.com>
2016-02-01 15:07:40 +0800

    Mount /vendor and /odm early
    Right now these two partitions are mounted in the fs stage of the init
    process. As a result, many vendor/ODM files needed earlier in the boot
    process (e.g., init.<hardware>.rc, fstab.<hardware>.rc,
    uevent.<hardware>.rc, SELinux policy files etc) can only live on the root
    partition.
    
    To prevent vendors/ODMs from polluting the root partition, this patch makes
    it possible to mount the vendor and ODM partitions in the first stage of the
    init process. The fstab info of both partitions to be mounted early is
    composed from new kernel cmdline arguments android.early.prefix and
    android.early.fstab.
    
    For example, with:
    android.early.prefix=/sys/devices/1010000.msdc0/mmc_host/mmc0/mmc0:0001/block/mmcblk0/
    android.early.fstab=mmcblk0p10+/odm+ext4+ro+verify\nmmcblk0p09+/vendor+ext4+ro+verify
    
    the final fstab string will be:
    /sys/devices/1010000.msdc0/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p10 /odm ext4 ro verify
    /sys/devices/1010000.msdc0/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p09 /vendor ext4 ro verify
    
    The android.early.prefix is optional. When it is missing, the final fstab
    string will be directly converted from android.early.fstab.
    
    This patch also makes sure that the early mounted partitions are dm-verity
    enabled so that they are trust worthy to store system files.
    
    BUG=27805372
    
    Change-Id: I3cf32482a5ec65445ba3aedab2164c7ba8f12694

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa654cf0e94cf1ce521a8a50480562e1e19d5e070b8999a704003fce097650d2
-size 29663
+oid sha256:d5c889b7143e4bd89ae162be3fbcdef09ebc301923deee260d2d6693224f82e1
+size 30434
