c9713e7677dce5c10d367a08fb2ed58722dda8ac
Mark Salyzyn <salyzyn@google.com>
2016-02-02 09:19:39 -0800

    libcutils: ashmem print error message for invalid fd
    NB: We decided to not sniff for the constant saved __ashmem_rdev in
    the early error path; requiring either the use of atomic operations,
    or acquiring a lock to do it correctly. The heroics are not worth it.
    
    Bug: 26871259
    Change-Id: I46249838850ae32063eb5b7d08c731c5bb0fbf6b

--- libcutils/ashmem-dev.c
+++ libcutils/ashmem-dev.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b0c59d01fea802b962fc0c5ad4887559225cf574daac411ca7637024ef75c4
-size 4665
+oid sha256:308868a03c687682d311b9722eb5e382b08ca8e80cb19104267f2b4da3eaee88
+size 5268
