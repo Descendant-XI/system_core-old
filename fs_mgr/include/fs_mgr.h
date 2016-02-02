4963d5ed3886525ffbc37c8bc850a494b884b383
Paul Lawrence <paullawrence@google.com>
2016-02-02 10:31:30 -0800

    Remove code to handle non-default file encryption
    Change-Id: I6d986853929c0237c0d958329b8e9aab47907a00

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db6f822ee5aa1c66321735719478460fbe017bc3f0672159321c1fe97dce48c7
-size 3839
+oid sha256:8ebfa4b58d53c8c3cc14709bdf7d54a48d972c11259dabfa8c7bc0e41e0f47f4
+size 3776
