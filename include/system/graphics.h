20700ab5688279320249ae243e56924edfb6a641
Mattias Nissler <mnissler@google.com>
2016-02-03 22:20:06 +0100

    graphics: Include stddef.h for size_t.
    graphics.h uses size_t, but doesn't pull in a declaration. This results
    in compile errors if code that includes graphics.h (such as
    hardware/hardware.h) doesn't happen to have a size_t declaration from
    elsewhere.
    
    Change-Id: I0f3882c3dce20425697d25366d49930b2251ef7f

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbfb54a72a7331a0944354474a9637485aceb723c096b2a3c83ea2003e7b8bfb
-size 28895
+oid sha256:69a83dd7f0dc78b2177973ce41b3e0baa428fc229cc953b4eba1eafb3ed002fc
+size 28915
