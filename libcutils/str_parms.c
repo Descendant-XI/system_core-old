78d728e16bd89688185e733b0ea492b1f0072fb2
Chih-Hung Hsieh <chh@google.com>
2016-02-03 10:40:45 -0800

    Suppress false positive memory leak warnings.
    Clang static analyzer does not know the transfer of
    object ownership to hashmapPut. Use a fake global
    pointer variable to trick the analyzer.
    
    BUG: 26955438
    Change-Id: Id3ac595fff14e48d28a1147461cd836914857086

--- libcutils/str_parms.c
+++ libcutils/str_parms.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfb6959e1de5b15cfd815a47600abb2439ce2888b99f9f20fcd0dc920bf772ec
-size 8250
+oid sha256:209ff1f8d63baadf3bb842dc0577c12d2d086516bf6754bfe736376d5559d84f
+size 8973
