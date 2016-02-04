41560e257f3a41983da09d825ef9984477ec29cc
Josh Gao <jmgao@google.com>
2016-02-04 11:59:12 -0800

    adb: make ctrl-c when spawning a daemon not kill the daemon.
    Previously, using ctrl-c in a command that needs to spawn a daemon
    because one isn't already available would kill the daemon along with the
    foreground process.
    
    Bug: http://b/26982628
    Change-Id: I7fefc531c3e4895423e7b466322b5426d01dc9ef
    (cherry picked from commit b72b3f8c92a0b2d4377d5bec15eb45869b8711ac)

--- adb/client/main.cpp
+++ adb/client/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d186abb9811821438aef93c63eda98fa33c2bd8562426a4c046558c9dfed67e
-size 5588
+oid sha256:c46834a8fe7e6b36f2492819d999727074e5a29708ab3c3e85a27c95fd1af67e
+size 5928
