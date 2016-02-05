bf386f55e0b503a0f8ea6c50fa8c91b96785a648
David Pursell <dpursell@google.com>
2016-02-05 15:35:09 -0800

    fastboot: add UDP protocol.
    Implements a UDP protocol for fastboot, documented in
    fastboot_protocol.txt.
    
    This version of the protocol does not include multi-packet windowing,
    which will likely be needed to achieve reasonable speeds over WiFi.
    Since we don't have any WiFi use cases yet, we'd like to get this in
    now and update the protocol later if it becomes necessary.
    
    Usage:
      fastboot -s udp:<hostname>[:port] <command>
    
    Bug: http://b/26154914
    
    (cherry picked from commit 4601c978cad8b69e15ed63664ebb5dcf6aaf21b3)
    
    Change-Id: Ia5bbae6bcd6405671f594d7120b3994746d234d3

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5602520a2b75609a096649d935a92c083b40e20b08ca39b3c030a963220a3c89
-size 3787
+oid sha256:3247ce6cd4aa20cb23ab606e2a8004e9fcc8204170b8a3ed81d6c00f72060aec
+size 3834
---
+++ fastboot/udp.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5981915d45e4e3ca29fb5160d026cae222c0276f1f250d43234bb403d3f14977
+size 13992
