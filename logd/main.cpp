8c9a0656041fb6f00445422a23f21f0a57ea9b75
Sami Tolvanen <samitolvanen@google.com>
2016-02-05 14:27:52 -0800

    logd: enforce policy integrity
    If a SELinux policy change or a switch to permissive mode is detected
    on a user build, restart the device into safe mode, and keep it there
    until an OTA is applied or user data is wiped.
    
    This change deprecates the ro.logd.auditd property.
    
    Needs matching changes from
      I781c3059ea8d4fb2f0c923e4488b1932d69678d3
      Ica825cf2af74f5624cf4091544bd24bb5482dbe7
      Id3ca7889ede30b54b7af73dd50653ca1a20d59aa
    
    Bug: 26902605
    Change-Id: Idcdc5bff133f13c1267f0ec0a75cc8cf1ddbda0d
    (cherry picked from commit d122ee65b66b5b33d51302dabbaa0d6c84597549)

--- logd/main.cpp
+++ logd/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f6c54fe46679ed058fc63bea620adfe80546fec6fe7050346f82d175ecf3536
-size 15069
+oid sha256:01396e793a46ad1979156ac43789d4d5aaff5fdd82285bb55b94d59643c18079
+size 15024
