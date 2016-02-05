52584d086e1abe2d11f0fbfcfbcda8ea35cd8ef1
Christopher Ferris <cferris@google.com>
2016-02-05 11:07:12 -0800

    Fix use of uninitialized memory.
    When creating an UnwindMapLocal fails in the Build() function call,
    the destructor for UnwindMap is called. Unfortunately, the map_cursor_
    member variable has not been initialized, so the call to destroy it
    winds up operating on garbage data.
    
    Part of this is a result of a bad class hierarchy, so this refactors
    the classes slightly, and properly initializes the map_cursor_ member
    variable in the base class.
    
    Bug: 26931578
    Change-Id: I885596bf65e4ef63559cee2c56cd41576d5ecc1b

--- libbacktrace/UnwindMap.h
+++ libbacktrace/UnwindMap.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8583cf50db6385bcdd24d4d472355fd86a9b142edd05985c085a160a1e4f3f3f
-size 1516
+oid sha256:1b2f427e8ff0f9d55ced665a51a0ed2d45cad9dd04c4b90d57d59a733533bf7f
+size 1601
