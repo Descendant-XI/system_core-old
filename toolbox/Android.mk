8fef93986504b88aa1937da07dfb049b4f95f8f1
Elliott Hughes <enh@google.com>
2016-02-08 14:09:10 -0800

    Fix generation of event labels for getevent.
    The recent uapi header update moved all the interesting constants
    out into another file. Track that, and add the missing makefile
    dependency.
    
    Bug: http://b/27073568
    
    (cherry picked from commit bd02a47b227430e7e9702dcfb11d4eb445847817)
    
    Change-Id: I9323a995511b12905949cba22c59cc28623068f3

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e7109cfe5e9aca14628e1026389976921a91064d19513a3f721e6b7b7009d2c
-size 3083
+oid sha256:bc66e40e0a1f1451b29baaf331f315e73f2462fbd1b3a45f1cb6fcd35c53ef13
+size 3631
