a51f711cddfedfb12be7ec910ccd8713a7c9556e
Mark Salyzyn <salyzyn@google.com>
2016-02-08 10:28:12 -0800

    logd: add internal prdebug function
    Usage: android::prdebug(const char *fmt, ...) __printflike(1, 2);
    
    Will add logd prefix tag, log as debug priority in kernel logs,
    and will suffix a newline if one is not supplied. To be used to
    aid debugging of the logger only.
    
    Change-Id: I217326ef34dc4eb0ea076bacb7a7a8b564c931c3

--- logd/LogUtils.h
+++ logd/LogUtils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b86448705d143b1bba2642f0ba618b6d2d3bfa77d6e679df9f90da7dc74b2fc9
-size 2634
+oid sha256:37e71d24b653e564771f0c8a5c443749796f2ea4adc4885af11ef95332e351b4
+size 2712
