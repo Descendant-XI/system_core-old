2a5e4470a37ac96ccdd6b23ab07814c08a445a11
Yabin Cui <yabinc@google.com>
2016-02-08 22:36:42 -0800

    adb: setsid() for adb host server.
    To create a daemon for adb host server, we should call setsid()
    for the daemon process. However, previously we call setsid() for
    the adb client process, which results in nothing but EPERM error.
    
    Bug: 26982628
    Change-Id: I2763ae3d5a243706927d7ef6af5095138c0ce2d8

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ccfea388960ee2277f500db1148546458f5a454792a687cf5d0d27ddcde5185
-size 42185
+oid sha256:3e05474e510fffbfa5c82fa075be3f9069a962a8bc676a041ece9bb9764a076c
+size 42166
