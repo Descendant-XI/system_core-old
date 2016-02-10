f62f02e51605d6bb10793dc7a5f75a3811729ad9
Mark Salyzyn <salyzyn@google.com>
2016-02-10 07:26:43 -0800

    liblog: add gTests for logging with leading newline
    Bug: 27091726
    Change-Id: Idff0e4e7c025426002c6381c3afb00e12d9fc1b5

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c09a29ac015fb4d7d30d3d52d956ba89c1a8dfce5aecf3bbf75bdcda9792cf0a
-size 53272
+oid sha256:ff3bafc6ebe5d294eb1abdc275ad2154960b3ff246beb1c18b0f4d09e92c77ed
+size 56973
