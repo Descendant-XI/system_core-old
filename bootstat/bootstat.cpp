99d244c82bb88212f4b1b680f22eb15b926eb104
James Hawkins <jhawkins@google.com>
2016-02-12 15:49:16 -0800

    bootstat: Add a few more bootreasons and rebase the index against index 1.
    This is necessary to match the mapping in the internal infrastructure.
    
    Bug: 21724738
    Change-Id: I292166ff5c0dea7f3d771627120f16af4c56073b

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63f6fefb52fd22811f4cfa8336cd634f734b5285d3f3a8ad71d1bdfbc321e0ea
-size 6628
+oid sha256:c9c2c1184864ab0d4b2612a948cdce08754dd88dec838511ac534ef3000d3502
+size 6752
