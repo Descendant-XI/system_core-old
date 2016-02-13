031a636f65603230a694d27d3907415486470e2a
Dimitry Ivanov <dimitry@google.com>
2016-02-12 16:39:09 -0800

    Add missing liblog dependency
    Bug: http://b/27171986
    Change-Id: I43fe595c05f5584e76d0359c42c8dc006bd3f5c5

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc66e40e0a1f1451b29baaf331f315e73f2462fbd1b3a45f1cb6fcd35c53ef13
-size 3631
+oid sha256:360a28bbe42092cab93d6086a577fa67dc18da8cc4b67687463fcbf99997a218
+size 3644
