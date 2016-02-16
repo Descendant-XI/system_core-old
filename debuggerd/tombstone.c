3946b3e17f84dae30ab96ba0a149d969615e37e8
Josh Gao <jmgao@google.com>
2016-02-16 14:26:11 -0800

    Don't create tombstone directory. DO NOT MERGE
    Partial backport of cf79748.
    
    Bug: http://b/26403620
    Change-Id: I412889bfa331c96ad8e2ce6e0d66b54fb230e8c0

--- debuggerd/tombstone.c
+++ debuggerd/tombstone.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd727724fade12c49f9ea7b3b25b2f806469727ce9120a8e656a9328cdcb3dce
-size 26707
+oid sha256:3def0f65269900cee48fdc8570eab0e13ec1eccb76c5e40ae3b5df66cd3d47a7
+size 26505
