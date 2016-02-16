bd94664adc36eb6b0a85cbeee62d4314a16d4052
James Hawkins <jhawkins@google.com>
2016-02-16 15:05:54 -0800

    bootstat: Record metrics for factory_reset and time_since_factory_reset.
    Bug: 25448351
    Change-Id: I9b3a557317bc256a078f76e2b65d16bc071e4a6a

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9c2c1184864ab0d4b2612a948cdce08754dd88dec838511ac534ef3000d3502
-size 6752
+oid sha256:152e638f5614907c8ed3dd7e5f7b8ef8b7cf746241168ee7c51b7cc7588c7e28
+size 8130
