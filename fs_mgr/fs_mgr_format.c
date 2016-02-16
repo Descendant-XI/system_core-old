af73b2b17b3f4c5e4f4807fd982d1d2361b9d430
Mohamad Ayyash <mkayyash@google.com>
2016-02-16 13:14:46 -0800

    Update make_ext4fs_internal param-list to match new implementation
    BUG: 26839493
    Change-Id: I4cd316ebc91457e2ec31c0e8b116798d9f94a223
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>

--- fs_mgr/fs_mgr_format.c
+++ fs_mgr/fs_mgr_format.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56238b10ef35279bbdb2c96d3e90417aaa38f7d471752301080891f08131579e
-size 3151
+oid sha256:d1a0c9b878aef4ae070367663cf8756850e5237a967f0bafb5ad799f2a12e4c2
+size 3157
