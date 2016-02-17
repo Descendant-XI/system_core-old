087b3864785bc451767ee6bb4727fd3fe039c036
Mark Salyzyn <salyzyn@google.com>
2016-02-17 09:54:47 -0800

    fake_log_device: long lived allocations
    (cherry pick from commit 0085a135b9986c969067aa89c4c9985fe5233870)
    
    Use static space for long lived allocations as they
    will appear to act like a memory leak. Resort to a
    larger on-stack iovec to reduce the chances of an
    allocation. Fix bug in writer where not enough size
    was available for "security" buffer name. Minor
    transitions to more consistent coding style.
    
    Bug: 27107691
    Change-Id: I68c918e7b916b1ae3b04829d48b3eddaa0a7e739

--- liblog/fake_log_device.c
+++ liblog/fake_log_device.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:692de6aaf48c996c9e9d110b96fe16138dd9141e5ddad82d93e89af121cb3ada
-size 19807
+oid sha256:097f12d25ecb47a7f67f6626531733bd1340997a103714e8672b22a3cd1cb31f
+size 19972
