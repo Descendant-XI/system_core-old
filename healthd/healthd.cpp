80790db1568caf4f57fa37c4be51eb1f304b2086
Yabin Cui <yabinc@google.com>
2016-02-17 12:21:34 -0800

    healthd: separate libbatterymonitor for use in recovery.
    Bug: 26879394
    
    Change-Id: I97b5b4994c42988ce43f5085b8b4b25e9e80c9fa
    (cherry picked from commit e98e177a34eb3200b9f95be8379223271933b885)

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:535ee494d2db442198bd2601d6d6c39426999226c3525c5a7ff9eae9cfbca932
-size 10651
+oid sha256:ae86b9db6df4069d114873b740e451f8f1c82a9577d68f75f732ed43445cef4c
+size 10667
---
+++ healthd/include/healthd/BatteryMonitor.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9473632fee0093674305c372ab11d1373afe1c671e7c633835581448fb9c0dca
+size 1954
