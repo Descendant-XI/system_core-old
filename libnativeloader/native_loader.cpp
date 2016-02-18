355688dc9af30d4eb03ee1473a917e43841cc42a
Dimitry Ivanov <dimitry@google.com>
2016-02-17 17:20:15 -0800

    Limit libart.so visibility to apps targeting <= M
    Add libart.so to the list of public libraries only for
    apps targeting android M and below.
    
    Bug: http://b/27210062
    Bug: http://b/26436837
    Change-Id: I1ed3b0335a343044a76273741ea8770dcce12ebe

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8216f091261d47e0c35bc3ebf13751cc757d1b06544d47141bdfc716ced546c
-size 6304
+oid sha256:31f880c3ed117090a79d5484ee330ef96f60bedf4fe2787baff224ef61aa2012
+size 6418
