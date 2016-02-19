d0fecb2e60646a1ffd15cd9e1e8f8356e3852e56
Jorge Lucangeli Obes <jorgelo@google.com>
2016-02-19 15:23:28 -0800

    adbd: Remove explicit capability bounding set code.
    Minijail supports dropping capabilities from the bounding set.
    Use that code instead.
    
    This cleans up one small loose end in the work started with
    https://android-review.googlesource.com/#/c/187775/
    
    Final diff: fewer lines, more unit-tested code.
    """
    $ git diff --stat 52cf4fa6aa3e8d13f61b1 adb/daemon/main.cpp
     adb/daemon/main.cpp | 56
     1 file changed, 27 insertions(+), 29 deletions(-)
    """
    
    Bug: 27314096
    Change-Id: I238975920b85fae376522eb26f5ad6d9fee0762a

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3937b0107b8c06f99d3742abd1294dacf3d531bc7d8d1f09435cff8ce01b40ef
-size 8298
+oid sha256:616e16f603fbd5ac807216308a85c9a698ed528e41e059f3e562e0456336f2d8
+size 8066
