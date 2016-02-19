287f5b7d2b66a67ca3e54ce3c29427d186676eb6
Mark Salyzyn <salyzyn@google.com>
2016-02-19 13:08:59 -0800

    liblog: deal with warning messages
    - Fix bug in fake_log_devices when it can not allocate memory
      failing to use the full on-stack buffer as backup.
    - remove superfluous code.
    
    Bug: 27265662
    Change-Id: I97b6cca5f4ce8ecad9beb3a08353de596d6a4ad1

--- liblog/fake_log_device.c
+++ liblog/fake_log_device.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df9ef68a91e6ea0a9a96bc9247f446b772d857061791cfb046d27db36670640
-size 20470
+oid sha256:274fb5721b6b080d10c96727e719dd46f7cafdf5b186edb5237852ff9694a50b
+size 20492
