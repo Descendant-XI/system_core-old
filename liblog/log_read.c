02c90d5c30b1cbb7baf5371fc58c64db3cd29bd4
Mark Salyzyn <salyzyn@google.com>
2016-02-19 13:08:59 -0800

    liblog: deal with warning messages
    (cherry pick from commit b525884edb1f8a80a8fc6cad58c69bdacc5bdb90)
    
    - Fix bug in fake_log_devices when it can not allocate memory
      failing to use the full on-stack buffer as backup.
    - remove superfluous code.
    
    Bug: 27265662
    Change-Id: I97b6cca5f4ce8ecad9beb3a08353de596d6a4ad1

--- liblog/log_read.c
+++ liblog/log_read.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8729751c3256d7dbc9b329014058ad7c5cbc2eaa2e8237bae3304fa757789a36
-size 24139
+oid sha256:befceb32095b7fe085aa85b74dc35dfe389d6a8b883d4c109c445e9a32724a02
+size 24007
