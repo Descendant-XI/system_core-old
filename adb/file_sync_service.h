1d1f76752a53bd03878680b66b8105f6aa26f19d
Josh Gao <jmgao@google.com>
2016-02-19 13:41:33 -0800

    adb: change unsigned to uint32_t in sync struct definitions.
    Change-Id: I9757ab853cfad1a2e1393ef32bcab222ab84acef

--- adb/file_sync_service.h
+++ adb/file_sync_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9792038526ebd92622f7a184f42d89ea0ce6376f34f01f8c19c4d13324fac46c
+oid sha256:60beaa7d8bede20d8c8b290b1ef9f25e02d9c2de80fdfc49524a5427cf205aed
 size 2275
