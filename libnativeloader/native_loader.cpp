459c821f171f7d7892302a87ab1158f580618e62
Dimitry Ivanov <dimitry@google.com>
2016-02-22 13:02:35 -0800

    nativeloader: refactor FindNamespaceByClassLoader method
    FindNamespaceByClassLoader returns android_namespace_t* to make
    it suitable for exporting this functionality.
    
    Bug: http://b/27189432
    Change-Id: Ifef3de753192178e0184fb114596fe956fde2826
    (cherry picked from commit 34fa704df45c5f74aeb5230e4bf3930585cf32fb)

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31f880c3ed117090a79d5484ee330ef96f60bedf4fe2787baff224ef61aa2012
-size 6418
+oid sha256:1ded4a7e0d3f15838014a8ad6122fa1e92cd5a11e4635ad368b23c40dc31d811
+size 6392
