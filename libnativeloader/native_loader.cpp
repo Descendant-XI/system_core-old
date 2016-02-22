f773a9f9bfe1b1a91fd7d73cf51e0030caccfba0
Dimitry Ivanov <dimitry@google.com>
2016-02-22 13:48:22 -0800

    nativeloader: Export FindNamespaceByClassLoader
    Bug: http://b/27189432
    Change-Id: Ib80dcce949276ee620f601c47b20f783708e8e85

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45787a8e1d841732c10c97cbe330db2807b74a0412a0934662247f15b4f58f27
-size 5903
+oid sha256:f933ffe073d7f07bfd61ed983e489f26c5aae9afe2b8b94074a37c8b47ccc289
+size 6093
