df46c5f0c7d963de7d9762a3fadb3123ba5ef7b0
Dan Willemsen <dwillemsen@google.com>
2016-02-23 14:28:20 -0800

    Remove duplicate fdevent.cpp entry
    Fixes the following warnings:
    
    build/core/binary.mk:894: warning: overriding commands for target
    `out/target/product/bullhead/obj/STATIC_LIBRARIES/libadbd_intermediates/fdevent.o'
    build/core/binary.mk:894: warning: ignoring old commands for target
    `out/target/product/bullhead/obj/STATIC_LIBRARIES/libadbd_intermediates/fdevent.o'
    build/core/binary.mk:894: warning: overriding commands for target
    `out/target/product/bullhead/obj_arm/STATIC_LIBRARIES/libadbd_intermediates/fdevent.o'
    build/core/binary.mk:894: warning: ignoring old commands for target
    `out/target/product/bullhead/obj_arm/STATIC_LIBRARIES/libadbd_intermediates/fdevent.o'
    
    Change-Id: Id4b13574e568284cefbc3a763d40843e73ddbd3d
    (cherry picked from commit 582c8fe1d4d5d1a0042a71bc1e8228d66d2600b0)

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:778254f3297e5456581fd3e42d3a14265ec112621fc3f6c42b225dbec32cc2e0
-size 9565
+oid sha256:0e4d63173456b1a46785cabe20636b3585befc97b65cfe85e466294ea6c0f63a
+size 9547
