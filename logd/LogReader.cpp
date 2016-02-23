cd53e98bd8afbf037fe5e5b2b6a551db95c927d9
Mark Salyzyn <salyzyn@google.com>
2016-02-23 08:55:43 -0800

    logd: sock_alloc_send_pskb starves pruning
    (cherry pick from commit 5c77ad55d0cdee84cd45fd5d0d066f3c61d76ce6)
    
    Allow socket send syscall to terminate after 32 seconds if reader
    stalled because of memory pressure allocating new network buffers
    
    Add a gTest to catch regressions, add security buffer to log_dump
    
    Bug: 27242723
    Change-Id: Idaa6699d9d284e7f5f723ae0e76b3d6aa3371489

--- logd/LogReader.cpp
+++ logd/LogReader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b733ee8485343bdc624af416f7a05275946b8cfcb5807317bbc386b2bbf0265c
-size 6526
+oid sha256:765a542d632c8cd3110ab548964940c9aa90dab5405afb8c3ec70d7a53d5a495
+size 6764
