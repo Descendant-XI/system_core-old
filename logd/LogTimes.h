9e77d05aa9bc152ffcc588e2bb1a264390eae83a
Mark Salyzyn <salyzyn@google.com>
2016-02-23 08:55:43 -0800

    logd: Allow (some) headers to be individually importable
    (cherry pick from commit 2ad0bd0a9b594bbe2560b405b0008b7bc742cfca)
    
    LogReader.h needs to be individually importable.
    
    Fix a few others, drop includes of local includes, let them be
    included in source instead and allow headers to be included
    alphabetically. Was not a complete audit since goal was to
    separate LogReader.h out from the pack.
    
    Bug: 27242723
    Change-Id: Ic7759ef90995e5bd285810706af33550c73cf5b5

--- logd/LogTimes.h
+++ logd/LogTimes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7635f8f8044c2408b6b2018fac9945e90fcd2bee3f89a3db85f873ee8070080
-size 3558
+oid sha256:85161e85c2d33243d4059539d9915fbcc62ad8a0ab408c2dc0d27198b1e79fe3
+size 3582
