cd9e295e715c5ec78b8ab6ca381b618f37df2b4a
Dimitry Ivanov <dimitry@google.com>
2016-02-25 11:44:35 -0800

    Make libutils symbols protected
    This change is a workaround for apps linking
    libutils statically and dynamically which causes
    them to crash for newer version of Android.
    
    Bug: http://b/27313399
    Change-Id: I47ac4146041b6eeef03cb605ea436719d552ec8f

--- libutils/Android.mk
+++ libutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea73d7b3df8dc273d3c14bd00db97df2800389e27ee8529b310fdde00403d6ac
-size 3163
+oid sha256:036081ed54992acaf6b294a372e785070be4be628e3d9fc610ba77bcf45607ee
+size 3186
