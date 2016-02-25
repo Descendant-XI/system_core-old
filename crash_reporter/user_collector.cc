76bfc3610643fdb5fda866fbde0c3ba8e2e1870a
Steve Fung <stevefung@google.com>
2016-02-25 03:37:33 -0800

    crash_reporter: Fix product_id in kernel crash reports
    Move the bdk_version, product_id, and product_version metadata
    population into the base crash_collector class so that all
    colletors correctly report those fields in crash reports.
    
    BUG=27344416
    TEST=crash_reporter_tests passes.
    
    Change-Id: I050053055f197d01661a1442e3cdcccc53c1c8fe

--- crash_reporter/user_collector.cc
+++ crash_reporter/user_collector.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a02ca86cb362053d84f2103b6f45590bf6317387b18f34775f036461a29d02c3
-size 23723
+oid sha256:3e3cffdece0202cab006d91cbabf2d0442ed57561b05cb5c7805a0ab6657452a
+size 22724
