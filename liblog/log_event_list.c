9104579598a6ac6b38ef69673cec72a827a78ba9
Mark Salyzyn <salyzyn@google.com>
2016-02-25 08:41:25 -0800

    liblog: add android_log_write_string8_len
    (cherry pick from commit 67d7eafd56010f154137e4316e938f802b2b0732)
    
    android_log_write_string8_len(android_log_context ctx,
                                  const char *value, size_t maxlen)
    
    Caps the supplied string to a maxlen length. Alter API to handle
    a NULL pointer for the value string for this and
    android_log_write_string8() and instead of returning -EINVAL,
    act like a null string "" was supplied to preserve the list
    location. API is also changed to report the number of characters
    actually placed into the android_log_context.
    
    Bug: 27356456
    Bug: 19235719
    Change-Id: I6a03d405eac1d741555dd05555513ec691e7a46e

--- liblog/log_event_list.c
+++ liblog/log_event_list.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e2eceeb04645b95ef8a1ad0076d2506a53e92c2f02908f97f7645e9159c630
-size 16598
+oid sha256:742dc426eff5e32d6ccb45e3f6f60fec0349ec113deff8bf4663cae5e4e62dff
+size 16843
