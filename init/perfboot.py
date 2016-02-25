78ea91e84d2cfa6697ddf2bea6ceeaae33657ac8
Yusuke Sato <yusukes@google.com>
2016-02-25 10:51:57 -0800

    Use ro.debuggable instead of ro.build.type
    Change-Id: I4d7c8c353ff47fbeee065b2c4fdd984768d0da00

--- init/perfboot.py
+++ init/perfboot.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:269bdad64619e7a704dacfba6f7301620c51b432cc87f2e4341b4e668772b2f2
-size 16162
+oid sha256:2fa9dbf2ea8708669e374255f1322c9136a7064bdb29edf25327fbc451e788d7
+size 16159
