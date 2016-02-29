63ba2c117d4a287cce5dc1c6685edb9b060deaa4
Ruchi Kandoi <kandoiruchi@google.com>
2016-02-29 13:13:39 -0800

    healthd: Check if battery device exists instead of charger drivers before setting fake properties.
    healthd used to check for registered charger drivers to determine if the
    device is an always-plugged device with no battery. This patch changes
    it to check for battery instead.
    
    Change-Id: Ie4ff5d3f21dc1a83a780a41eb0c929f66b86d631
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3206bda79bcea556c820a4487064c00e835c2e36f3cc59e27016fe5e1ed425b3
-size 24019
+oid sha256:ec41ebf3bf31441db5d7c2632eaacf9482aacd83a906d19773c5184da11ad752
+size 23854
