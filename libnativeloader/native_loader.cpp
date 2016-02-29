3e1344b3e3c8bc686ab25fa10e29f52a5ab534b5
Dimitry Ivanov <dimitry@google.com>
2016-02-29 13:21:43 -0800

    Fix locking of libnativeloader
    This commit fixes race condition introduced in
    d047c925af62e1fe28fcd1c1940df4afe18d458a
    
    Bug: http://b/27189432
    Bug: http://b/22548808
    Change-Id: I5d94f130937f18d3443878b3521715a8f87427e0

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d100292f618849544b25f1edecae2a6e05a31ff639a12b7bf13364e243ab00d
-size 8106
+oid sha256:c392098a68bf66752621d28886ff6d5b90bab90baaf894e38890382852665c99
+size 8303
