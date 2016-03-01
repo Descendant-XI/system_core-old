aa2572e73adb26cb73a7c1d2d78012837408c1ff
Ying Wang <wangying@google.com>
2016-02-29 19:27:06 -0800

    Also archive adb.exe/fastboot.exe when building win_sdk.
    On linux we archive both Linux and Windows binaries.
    
    Bug: 27315911
    Change-Id: I17d25cd1ea611a9c917f1e78b47f21a8959eea20
    (cherry picked from commit 71edfc8b9ab78f00206828993526aedd115c46b1)

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a877a5fd866615ae65b18af0e4bf6b0a13c22404bb2eed0e85cc953d4112bee
-size 9593
+oid sha256:9fef4560945804948cf57285e521daceb9f39eb65e5e5fd960276565140d013b
+size 9731
