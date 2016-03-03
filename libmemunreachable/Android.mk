ac4d1f9e2ed6d21faba9e4865c8557005c14fb0f
Colin Cross <ccross@android.com>
2016-03-02 17:52:56 -0800

    Compile some tests for the host
    Compile some tests for the host to ease debugging with valgrind or gdb.
    
    Bug: 27208635
    Change-Id: Ib46fcfa333ceb721f26efca00b2fa60b9fba44e6
    (cherry picked from commit b8e20f557f315d2ab5e21f7bf70657bb16d5d42d)

--- libmemunreachable/Android.mk
+++ libmemunreachable/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38b92711dbbb513f3cdb3e4e5abc1c4a4bf765f37da1eff120360ae39de46d7f
-size 1221
+oid sha256:5476398dcc097da6897b426ae9342d53175014d5917a15caace00435378a4979
+size 1613
