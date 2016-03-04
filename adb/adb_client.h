f51aac1452bd6ec33c599daa6a1cccee7c057a60
Josh Gao <jmgao@google.com>
2016-03-04 15:16:18 -0800

    adb: wait for adbd to die and respawn in root/unroot.
    Bug: http://b/19749057
    Change-Id: I57dbc113803b6fd3016c1801410be0f4023245d9

--- adb/adb_client.h
+++ adb/adb_client.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d7d1d6f9e207cc60d8f1c0d4c45b005d86c1af4da6d550643072fd22232a623
-size 2726
+oid sha256:52f6e744a55242d264f135798dd4bfad9b25b1a9f529c376ec92bfefe5bb0d84
+size 2869
--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b39eb459ee386e66fa47fa6f75e98eed3d523819966c73a023384a5ec6e6819
-size 78277
+oid sha256:3198717222f4e2991b3c836d1d47e748f9e4e42d6fe676ce484a9217af4fda84
+size 79686
