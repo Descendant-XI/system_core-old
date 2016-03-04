d8f076a704de24efc01fefe8b9ecdf2667cd5d77
Josh Gao <jmgao@google.com>
2016-03-04 15:15:56 -0800

    adb: make ScopedFd universally accessible and useful.
    Change-Id: I707ffbd10958e7449b4c95dff48638480c746939
    (cherry picked from commit f0d3b4fc11b35ce295bd698555579ed242473e69)

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf8f7bd732c927d257fbfb27520b925a90bfc09129d102274c3dda1feeffa6f3
-size 28568
+oid sha256:bcb65b8835e7398c51640b3b491c6d63dc266719d8c2e74f1560da49bb3c409f
+size 28017
