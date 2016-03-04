a26ed958aefe47c7e2a31149644edfcd8d268f79
Dimitry Ivanov <dimitry@google.com>
2016-03-04 11:00:37 -0800

    Handle the case when libart.so is not in use
    In the case when libartd.so is used libart.so does not have
    to be included in the public namespace.
    
    Bug: http://b/27496326
    Change-Id: I464bafec1d9da5c3f3f03fef02af3f34bf377297

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c392098a68bf66752621d28886ff6d5b90bab90baaf894e38890382852665c99
-size 8303
+oid sha256:fcd592f6a2d5165082c4affff6aec807682ed593afbe77c05be5a67cadb77c92
+size 8474
