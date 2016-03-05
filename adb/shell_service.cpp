94138188bc92c5c43788e702a0f1e0ad7f3fa50d
Josh Gao <jmgao@google.com>
2016-03-04 17:50:10 -0800

    adbd: fix spurious close of uninitialized fd.
    forkpty only returns an fd on the master side; trying to keep track of
    it on both sides is incorrect.
    
    Bug: http://b/27504427
    Change-Id: Ia4a454d8490c77738f9c3350a4236b3d99c8758b
    (cherry picked from commit fcb063ce378419ad1511d3395d44701b4ebaceb0)

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcb65b8835e7398c51640b3b491c6d63dc266719d8c2e74f1560da49bb3c409f
-size 28017
+oid sha256:8ed49a515c2866f8dcff054eaf84d322f45d96f084c198cd44b1a5e150566021
+size 28053
