09d12339610d79ce2593c9f07cf5d7097b70f26a
Michael Ryleev <gmar@google.com>
2016-03-08 15:16:11 -0800

    trusty: storage: implement storage proxy daemon
    Change-Id: I80cdf61e5ced00dd32a3e35eb81969d25b624df9

---
+++ trusty/storage/proxy/Android.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ade08568df850930e8fc680952cb4ad7e03afc75b1240aa425011fbd72371596
+size 918
---
+++ trusty/storage/proxy/log.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a4c4b591f5ddb7e1a2d4a7b003545eecdbccd05f9574f0c6296ed17e4f70903
+size 678
