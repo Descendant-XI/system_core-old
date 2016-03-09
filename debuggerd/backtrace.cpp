b3b34f99e17f7b480996cf7d921cf7cc8d1ba9fa
Christopher Ferris <cferris@google.com>
2016-03-09 14:35:54 -0800

    Add error reporting mechanism for failing Unwind.
    Remove the logging of an error if a thread disappears before the unwind
    can begin. This can happen, so allow the caller to determine if this
    is really a problem worth logging.
    
    Bug: 27449879
    Change-Id: Ie81718d53fb0e519fa0a7db9fd5f314b72bfa431

--- debuggerd/backtrace.cpp
+++ debuggerd/backtrace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bcfcd59e0677b3401a76f9ed705098d3af83f07f3547a897c88545014139eda
-size 3234
+oid sha256:c3d06bcd24d25add86144147c6f8240d67f931bc7926155d3fcc00376e62525e
+size 3306
--- libbacktrace/Backtrace.cpp
+++ libbacktrace/Backtrace.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41f517c56b282131c94266ad61973205d7eb4e2988f3a32c5b69c8ee3df9aad4
-size 4243
+oid sha256:e604bff762889e5dd83831e9ad75d7767078d577accc10dcdc48c6df9b29dbc9
+size 5044
