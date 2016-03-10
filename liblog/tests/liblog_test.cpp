ff8cfd4e273e04e5bdbb5742d08293ef047bce31
Mark Salyzyn <salyzyn@google.com>
2016-03-10 15:25:14 -0800

    liblog: test: do not LOG in signal handler
    - We actually are logging in a signal handler, the title is bluster
      to remind developers. It is not a reliable path though as it can
      lock up. Our goal is to minimize the chances of a lockup regardless
      in the name of stability only. The test remains to catch regression
      in the name of code quality and reliability. Expected to be
      >99.999% reliable.
    - Add a new _correct_ duplicate test that uses signal to release a
      semaphore to a thread that performs the task. This path is expected
      to be 100% reliable.
    
    Bug: 27405083
    Change-Id: Ibb7cf4b13e34ebfac2db2af8724b7db7a27f81a8

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fae1629c79c8477721e7189f01b57f86c533c73d3f4e80c60bde14f860a6ef7b
-size 74250
+oid sha256:de120d68cdef499447b98b06498cd23c5f052d5e9fa3456e3879127c59259bd0
+size 78678
