d00a3d09a6a8599fbb7f6078f83b4360d5d9d88f
Elliott Hughes <enh@google.com>
2016-03-09 19:28:03 -0800

    Fix buffer overrun in adb wait-for-device.
    Bug: http://b/27444063
    
    (cherry picked from commit 34e560b9a1e44dcfa0c220c9499bbb833a95a2b4)
    
    Change-Id: Ic71cb2070f509d76fb4208dbab6711c5dd5e1f62

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db1d49fc16c64c04f95dbb3364d29fb750847491e0b9a04e4f7fd1660ca76da7
-size 79847
+oid sha256:f300e702ecf4a443382795eeff841be8ed8380cfec076df435adb58ba9b652b3
+size 79901
