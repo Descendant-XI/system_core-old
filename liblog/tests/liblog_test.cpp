e262d64b041bf092006e9dddda9498e3081470e5
Mark Salyzyn <salyzyn@google.com>
2016-03-11 12:06:12 -0800

    liblog: truncate logtags that overflow prefixBuf
    (cherry pick from commit 2f83d679dd7910cc76d895eb74ab4fabd1e349b2)
    
    Bug: 27585978
    Change-Id: If2f45e8787b05b46491a771702746cfc248b9ccd

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89e0a76c24d0c9a777350c32014479651b3fbccecc08ebbc53fc00213ddbb489
-size 72646
+oid sha256:14802c3a1dc37791c1d7936a97224b113ff0ab688c79609a131c6a665e7f97cb
+size 74319
