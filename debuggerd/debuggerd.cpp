2747c04da495895bc2b48e74f7e045e9a234e9f9
Josh Gao <jmgao@google.com>
2016-03-16 13:39:38 -0700

    debuggerd: kill crashing processes with the signal they died with.
    Bug: http://b/27675306
    Change-Id: I951c5d7e54c35d88c65c5dc856e0b9d5a93d47b2
    (cherry picked from commit 561497c0a8073d8e04b387be0e0aa995424cee59)

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd2e36fde1f467de0bed934cceff49a898cf8e62eb234160e19cd8afbc6b6f97
-size 23497
+oid sha256:d4dc6146a7a578093f006d4d310aeefbe7fbd680c5be212e02bf9deae91f10b9
+size 23490
