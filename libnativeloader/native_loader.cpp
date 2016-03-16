cd522d0e7ee2e50ef407d8ce3c10a75be40a90a8
Dimitry Ivanov <dimitry@google.com>
2016-03-16 13:54:33 -0700

    Revert "libnativeloader: Make webviewchromium so file optional"
    This reverts commit 75dda7551f6a3499c0c45ed0f9530353c7616357.
    
    Bug: http://b/27546414

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90134474868463196b473474ab31f1cbe51e59ca8a0b4e606a5dedd0457ae946
-size 8519
+oid sha256:fcd592f6a2d5165082c4affff6aec807682ed593afbe77c05be5a67cadb77c92
+size 8474
