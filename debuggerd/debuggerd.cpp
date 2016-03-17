554b40dc6cd67c9644d02209e59955278e638aa1
Josh Gao <jmgao@google.com>
2016-03-16 18:09:15 -0700

    debuggerd: fork the signal sender once.
    Bug: http://b/27427439
    Change-Id: I6294ff68a150bc9950a300264c31d2141307ac66
    (cherry picked from commit f5e8f0b9cd9ec0214a6f9cd38dd6d9af3268f9aa)

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bedafbe8b5cae5fb3da01327b1f4bb046c8a9ac0679e2d6204b269de22ab55e8
-size 23284
+oid sha256:f21e540cb7a9d066658f1b0da336bd3eed859e26ac696ca2042dfbef86de9e3a
+size 21056
---
+++ debuggerd/signal_sender.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2a6935e261e5f5933ea2949852554a3e712061558abdc969fe7399a1fd05e31
+size 998
