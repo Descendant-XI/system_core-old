7adad0b767f2e34918e75ea17ebbf4846b0eb695
Casey Dahlin <sadmac@google.com>
2016-03-17 14:04:52 -0700

    logcat: Add --max-count option
    This new option causes logcat to quit after it prints N messages.
    
    Test: New unit test passes
    Bug: 27706081
    Change-Id: Ie52ddf300160a041e68a6bac0ae7ade68bb28a7c

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e21c8ec0af2a1e38947d6642336fed78582a8c86c9ee15411405c64c912c5a1
-size 40383
+oid sha256:c88fb9d62166358e8f3a7a910e5247b9fa2bc62880827d4cd2d0d082c02e25b9
+size 41262
--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d26c72904e2639ae6a99cf3c2d92d2620fb8ec0ae184a0e70ae2a9230609be11
-size 25217
+oid sha256:eac9e8756b9d5d95776195553bb252164719c5dfa3ede947802d94d46b8134b2
+size 26057
