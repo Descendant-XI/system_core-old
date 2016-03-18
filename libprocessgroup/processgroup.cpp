58b219a4bf5056141668add34b49173fe4b7d64c
Martijn Coenen <maco@google.com>
2016-03-18 15:28:31 +0100

    Don't use mem cgroups for pid accounting.
    Commit b82bab66 introduced the use of memory cgroups for keeping track
    of forked PIDs; it basically creates a separate memory cgroup for every
    process forked from zygote.
    
    Each such memory cgroup which also have its own LRU with (in)active file
    and anonymous pages. The current theory is this could potentially introduce
    two problems:
    1) kswapd runs longer because it has to iterate over the LRUs of all mem
    cgroups, instead of over the LRUs of a single root mem cgroup;
    2) the way kswapd reclaims things will be different also - I think it will
    tend to bias reclaim to smaller mem cgroups, and process private pages
    will end up on ZRAM swap much sooner.
    
    Until we figure this out, fall back to the CPU accounting cgroup for keeping
    track of forked PIDs. This leaves us with a single root mem cgroup again. We
    can also keep userspace lmkd enabled because it only requires the root mem
    cgroup.
    
    Bug: 27381069
    Change-Id: Ife397a6ac232761f2adfe6f5056582be0d1b4ff1

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d24e6c02957ea357a49a67a8943c7c4c1de6ad5bdb590cbaaea3464711271229
-size 10171
+oid sha256:c88f88bc1c8cfc45ee82a77a27e440a223980536cfd0d3eaff7341c3130e8c82
+size 10332
