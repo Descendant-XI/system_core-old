d056db27d0bca9a685b33933c3a9d14321962fbb
Josh Gao <jmgao@google.com>
2016-03-22 15:10:45 -0700

    debuggerd: set the name of the signal sender.
    Bug: http://b/27925028
    Change-Id: I6bff938e229d2368250d4b5c268fa24bd4badea0
    (cherry picked from commit f3dde2f869801f2b5f5fb9cd9ee1a9356a0a1726)

--- debuggerd/signal_sender.cpp
+++ debuggerd/signal_sender.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14621ab50c8dae0b0a50dba05641cb4e56f6d0a57919bf1904a00d5df9a24924
-size 4008
+oid sha256:808566cf7c923679261df655a5e5ff88e7069a49bd04e469b6cde106f9b8fddf
+size 5053
