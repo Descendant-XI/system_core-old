d838c78c3d991d20009a72a4d0201b941ce7bcab
Josh Gao <jmgao@google.com>
2016-03-22 11:29:17 -0700

    Don't demangle symbol names.
    Bug: http://b/27299236
    Change-Id: I3a698c6d93e262fd78e743c1e6e946b054b9dcd1

--- libbacktrace/BacktraceImpl.cpp
+++ libbacktrace/BacktraceImpl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:663b46fdab6237cabc083bc662085c0049ea7e39ab251a1be184fe0ca7c530ed
-size 6131
+oid sha256:ff89e7c74c2a3e6137dfec6bbf2a10e38a65815d99c5239ed2d1022c03b3cff3
+size 5682
