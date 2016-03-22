4ada3b5755d545aa9dd900d851f1efdb9ba4b847
Josh Gao <jmgao@google.com>
2016-03-21 18:32:17 -0700

    Don't demangle symbol names. DO NOT MERGE
    Bug: http://b/27299236
    Change-Id: I80524d8d5df9c5abea8c6db8ded965a5dfa3d607

--- debuggerd/tombstone.c
+++ debuggerd/tombstone.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3def0f65269900cee48fdc8570eab0e13ec1eccb76c5e40ae3b5df66cd3d47a7
-size 26505
+oid sha256:ca7477d3ab464a0f24cd7dee864ac87f3373ac2260fa81343834dea737fa5b85
+size 26334
