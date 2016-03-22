1808b09a5c72855d220af796a095de0abcc9e948
dcashman <dcashman@google.com>
2016-03-22 15:13:42 -0700

    Temporarily add set_mmap_rnd_bits brillo exception
    Bug: 27794137
    Change-Id: Idc6fc1e208bdb19556173d40215feb66e235c6d6

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39fd8c0c607d713d8f9be777b7e75afba8cdce845de93c4d6713b5d878fb0aa3
-size 24034
+oid sha256:96d5fefee4b161f4067ef463321b1b0b7dabc79bb96803854528e696142cd914
+size 24097
