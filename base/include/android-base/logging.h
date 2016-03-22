6554218433a63ef482ef9fe41296ec43f0ec8809
Yabin Cui <yabinc@google.com>
2016-03-21 17:46:21 -0700

    base: Avoid compilation error when compiled with -Wdangling-else.
    As logging macros uses `if xxx else yyy` style, it is reported as an
    error when DCHECK() is compiled with -Wdangling-else option. Because
    after preprocess, DCHECK(x) becomes:
    if (EnableDChecks)
      if (x)
        ;
      else
        LogMessage(FATAL) << yyy;
    
    This CL avoids compilation error by replacing `if xxx else yyy`
    with `xxx && yyy` or `!(xxx) || yyy`.
    
    Bug: 26962895
    
    Change-Id: Ib0bf242cc04a238ec31a1ab66b53fc8a5b5ed28f

--- base/include/android-base/logging.h
+++ base/include/android-base/logging.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7abff9db54d6cb23a1a84e09e25aec4cc56c97cd3851dbbc25e6da933b97898
-size 13860
+oid sha256:280820f41cf637579caa336c25795dcf89e287d6d60f20f00c016b07476398af
+size 13358
