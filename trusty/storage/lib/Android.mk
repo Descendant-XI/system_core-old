79b47189dc51a6723ab0de1fc640d14832b35822
Michael Ryleev <gmar@google.com>
2016-03-23 16:25:05 -0700

    trusty: storage: add client lib for testing
    Change-Id: I593aeed5f657b5de1fce47264ae31aa6e79f2a63

---
+++ trusty/storage/lib/Android.mk
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4a696602c9daf5cbf6d90040b1b367fa06af276323c17af26c23969e2176082
+size 984
---
+++ trusty/storage/lib/include/trusty/lib/storage.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a52adbbb7f887bf689c3dd551537a4e9c7c690df7ba6caf8f838114044b7c89b
+size 5823
