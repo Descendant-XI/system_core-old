118ee01b46e7bbee03d3e024e2c3809d6c8806f6
Josh Gao <jmgao@google.com>
2016-03-23 14:02:52 -0700

    debuggerd: waitpid for all children, and log the result.
    Change-Id: Ic575e6db76ab153b4b238589a8cd299812d0e046
    (cherry picked from commit 280800552165b4685fbe86fd8e9dc3b9d9b062cb)

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:208a651db084a8152fba4d751fe86d3c03e725c3497d9d51efef948b36f4ddd0
-size 23038
+oid sha256:4640d3b330a6ccf37dbc6e04e2165067200da0e813d1c34e4313b6599c8e30cb
+size 23489
