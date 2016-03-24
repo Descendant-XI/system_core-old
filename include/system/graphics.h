86b51e40496a2e80595d23d090f8aa447ab6f713
Dan Stoza <stoza@google.com>
2016-03-24 10:38:46 -0700

    graphics.h: Add android_color_transform_t
    Adds android_color_transform_t, which specifies a range of color
    transforms which may be applied to the whole display.
    
    Bug: 22767098
    Change-Id: Iaf03915c09ac0bdd18512b5f78c39da1705bda08

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b82889b29c3fc732c5229223c7b857846e13d776faa92673db4e75e4fbed4da
-size 39018
+oid sha256:835ec113740d14f9fb391b4fac8d1139ad4d774c5df8ac38a121eb4043083f8d
+size 40044
