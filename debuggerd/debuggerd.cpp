6463c4a6c52058cef86a1b77bd35dc30d54ecbd9
Josh Gao <jmgao@google.com>
2016-03-25 13:22:05 -0700

    debuggerd: add SIGSYS to the list of handled signals.
    Processes using seccomp will get SIGSYS when attempting to use a
    disallowed syscall. We're currently not handling SIGSYS in bionic's
    signal handler, but this change will let us dump a backtrace pointing at
    rt_tgsigqueueinfo in our signal handler if seccomp policies block it
    during a real crash.
    
    Bug: http://b/27853687
    Change-Id: I4e4aacc95eeef7249d895e19dc4ccd77a51c7a17

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4640d3b330a6ccf37dbc6e04e2165067200da0e813d1c34e4313b6599c8e30cb
-size 23489
+oid sha256:fba4b3eadf0e6601581177ecc3376ee91d627c508377c80a7f49c9fe240a5f0b
+size 23508
