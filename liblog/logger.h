4dc5d089aa12849e166c2974e77aa2735b0ddd39
Mark Salyzyn <salyzyn@google.com>
2016-03-25 15:50:46 -0700

    liblog: LIBLOG_ABI_PRIVATE __android_log_is_debuggable()
    Allow our own libraries to use this privately instead of
    running the less efficient get_properties and doing the math.
    
    Test: compile and boot smoke test
    Bug: 27566046
    Bug: 31456426
    Change-Id: I2f677276d27fbcb6af01b600ac1d9891c8938d43

--- liblog/logger.h
+++ liblog/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:516441cf371815e7990110a28f7fb3245fde6fca553dbcdb4127f43ddea0cdeb
-size 5785
+oid sha256:9731a15504305d1a143d7958bbe539efe03d6c37194168a40b1262487a2bf1bf
+size 5736
