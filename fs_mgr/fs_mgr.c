a88bf71d70b87d57186c34ac3baa72b23e68070b
Paul Lawrence <paullawrence@google.com>
2016-03-28 21:54:51 +0000

    Revert "HACK: Run e2fsck with -f"
    This reverts commit 2cd762d932561071308315d3af6f97dc536c1717.
    
    This change adds 12 seconds or so to boot times - we need to revert it
    and see if we can find a proper fix to the original problem if it is
    still occurring.
    
    Bug: 27849759
    Change-Id: Ib3692e436c08468a51529f256f2ce5e9ccd2d35d

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64e5cedd3d8908cddf2b6804765f486ee3946e3261e5f23cbb10f2ed254c28fa
-size 28723
+oid sha256:dd849c213d5c8fd968af09f383f0788797855301048250418ed400c36c41a06c
+size 28709
