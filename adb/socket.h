5c576ef0cfb5d76a74ddb5a0cbbca36abc7582db
Dan Austin <danielaustin@google.com>
2016-03-28 15:32:37 -0700

    Address const issues in preparation for libcxx rebase.
    Change-Id: I4eccc1b5a70da9dd325e1a7e10ab0a3fe588c03f

--- adb/socket.h
+++ adb/socket.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f81903b41fe8d00a5f02163abb3d2732e15bd3616be4fcc9fb86dcf7648e3d2b
-size 3750
+oid sha256:4a57efe359387edee4bfd23bc01682f478f715d3c74f0793921b1fed9f861008
+size 3744
