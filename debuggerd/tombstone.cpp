3466e47ae2a870e5a7850ab288f704a3205d5b6d
Mark Salyzyn <salyzyn@google.com>
2016-03-28 15:43:50 -0700

    debuggerd: use __android_log_is_debuggable()
    Test: compile and signal 8 to some component
    Bug: 27566046
    Bug: 31456426
    Change-Id: I35e83679fbc40d54217850521bff23d9306d86f8

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fd2ef202ee1b3b5e6f199e98970951d9ca740ff0724d48993813802ad910690
-size 22111
+oid sha256:32d5a3d712bac49ebd692ad4225faacb331bef3c6da52cc47545b8d8c2f10117
+size 22055
