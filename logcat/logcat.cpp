715b3569f0c05cb87822573aed1508d091e109a0
Mark Salyzyn <salyzyn@google.com>
2016-03-30 12:38:29 -0700

    logcat: do not assign 0 or NULL to global variables
    (cherry pick from commit c6d6652df52bb65984f6be1f8e57606d3f5a6e69)
    
    Bug: 27706081
    Change-Id: If29d8d688ca7cce9b22227eeff1e9b500e728a50

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc8af459041472a78044632d0b2fa802a44f39ee7b1afb874ded8f9aaf418b75
-size 41257
+oid sha256:623d51fad81082597fd2bb527529ddef9fa25a0722330e1a6e71bfcbff864f62
+size 41226
