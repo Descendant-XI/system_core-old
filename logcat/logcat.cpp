e962922b1ef2e98edf7536674d2f27708926d196
Mark Salyzyn <salyzyn@google.com>
2016-03-30 09:38:31 -0700

    logcat: add --print flag
    (cherry pick from commit c9202777cf0a135be729b1bfdb4c8c57344296e6)
    
    To be used in combination with --regex and --max-count flags
    to allow all prints to pass through, but still stop when
    max-count is hit.
    
    Bug: 27706081
    Change-Id: Idaa9f0b44586a885b9d9f367824b0072c0853227

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd6c1f3a74606392d611724e303dc0a35f51522b6cbe9c9387085dd77ba56fd5
-size 41457
+oid sha256:bdbfc1abcfb5f3f3d491cbbce15552b995ecc33e04779f47dc2b2d9cc28d9694
+size 42562
