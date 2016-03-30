8f9dd1eade75e5a59f020a695e5d28a916439949
Mark Salyzyn <salyzyn@google.com>
2016-03-30 09:38:31 -0700

    logcat: add --print flag
    To be used in combination with --regex and --max-count flags
    to allow all prints to pass through, but still stop when
    max-count is hit.
    
    Bug: 27706081
    Change-Id: Idaa9f0b44586a885b9d9f367824b0072c0853227

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb638c55ceaf309d6ae84f3f1156a2611eb9802763007d5031af1a5dbe69575
-size 41462
+oid sha256:5defd3fe379fadffe342f61b0bf4347ddfd52dc217a734184418c8ab6b43bc1c
+size 42567
