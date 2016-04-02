16c6c5191b4a3f3c16624a5536278bf3d7e8a959
Felipe Leme <felipeal@google.com>
2016-04-01 17:43:27 -0700

    Implements 'adb bugreport <ZIP_FILE>'.
    Dumpstate now supports zipped bugreport, whose output is more complete
    than the flat-file bugreports provided prior to N. As such, adb now has
    a 'adb bugreport <ZIP_FILE>' name whose implementation:
    
    - Calls the new bugreportz binary.
    - Parses its output, which in case of success is the path of the .zip
      file.
    - Pulls the device file and renames it according to the command-line
      argument.
    
    BUG: 27653204
    
    Change-Id: I7169fe157c77bbef1684d0cb4e43095d95ddf2b8

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f300e702ecf4a443382795eeff841be8ed8380cfec076df435adb58ba9b652b3
-size 79901
+oid sha256:16fa969968818500f7fa5b6451b5c67e62d1ec1136b46a9011e49f6ffcde9567
+size 81691
