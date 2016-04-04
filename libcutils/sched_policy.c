56900c274d200983d2103f7789884fb214801095
Martijn Coenen <maco@google.com>
2016-04-04 17:11:56 +0200

    Update get_sched_policy to return TOP_APP correctly.
    Currently get_sched_policy() would only return foreground
    or background, but not TOP_APP, since it's not looking
    at CPUsets at all.
    
    Fixing this makes testing easier, and it also corrects
    the output of "ps -P" to show "ta" for current top app.
    
    Bug: 27857142
    Change-Id: I589cacc107f355de15750f01b06f404b59a0f99b

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a50a9992374ae0e13d72ea494ea191b20fa2ff1a4261d0089dd4f11df510623
-size 10871
+oid sha256:ee51ee4151fb0d38e9c12736936c8c3f12f157161a3052a223aa03239d64c4dc
+size 11493
