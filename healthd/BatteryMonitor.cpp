9a0468f65c5553bae796f3f96fc761d0b9eadc1f
Ruchi Kandoi <kandoiruchi@google.com>
2016-04-07 12:34:40 -0700

    healthd: Add charge counter for BatteryProperties.
    Bug: 27174034
    Change-Id: I26de33714bb353faadb159d898f0b2142c76c657
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5885ff26d920fcc1236b4f5818965def9b0a022d1b2e5beddfbce1e67432d239
-size 24680
+oid sha256:086e96c2a2830c9f38b39525ce2a26f86a9e58c0f433884f1fc6a95e0e996d5e
+size 24871
