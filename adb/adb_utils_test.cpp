274bfa1ee9f9924124e4f59ba0cf54e1e5074b76
David Pursell <dpursell@google.com>
2016-04-07 11:25:48 -0700

    adb: support forwarding TCP port 0.
    This CL adds support to forward or reverse TCP port 0 to allow the
    system to automatically select an open port. The resolved port number
    will be printed to stdout:
      $ adb forward tcp:0 tcp:8000
      12345
      $ adb reverse tcp:0 tcp:9000
      23456
    This allows testing to be more robust by not hardcoding TCP ports which
    may already be in use.
    
    Forwarding port 0 is a host-only change and will work with any device,
    but reversing port 0 requires the device to be updated with a new adbd
    binary.
    
    This CL also does a little bit of cleanup such as moving the alistener
    class out of adb.h, and adds some error checking and additional tests.
    
    Bug: 28051746
    Test: python -m unittest discover
    Test: adb_test
    Test: `adb forward` and `adb reverse` with tcp:0
    Change-Id: Icaa87346685b403ab5da7f0e6aa186aa091da572

--- adb/adb_utils_test.cpp
+++ adb/adb_utils_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f92cbfe3bdbe8d76112d10be5fe3a123f6e2905dba2795f1754d63771100280
-size 4854
+oid sha256:2d6e2638c47bd95a13b62ad1a40e3c073a65a484518eca25c180be54027dfdfa
+size 5850
--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df4aad1733b83457779751421c6019a8a566b9dbdba98f3eaf4d830ea6b05e19
-size 80219
+oid sha256:f9d1c72a02b0d30e41103c7cb57a119ded71f21c9b3ac470bbf7d45ca0945484
+size 81109
