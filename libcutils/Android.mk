50aed21545a192f91a5c3644497b3bb70ea33c6f
Mohamad Ayyash <mkayyash@google.com>
2016-04-07 22:13:43 -0700

    Move canned_fs_config into libcutils
    It's no longer specific to make_ext4fs, will be used for mksquashfs as
    well.
    
    BUG: 27467028
    Change-Id: I41b8c2b168ada45c8378dee522391edfb8f6b9a6
    Signed-off-by: Mohamad Ayyash <mkayyash@google.com>
    BUG: 28704419

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c05ba8ca75d770affb62eb20098c5759788fa90d1d6e791f4edc438d85ccdcae
-size 4791
+oid sha256:983ab712f62b895f44dd36d7c00bf350a08a08c11b40ca0736309d3fcbbd1207
+size 4820
