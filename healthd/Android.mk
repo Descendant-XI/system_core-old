7488b931d69cd5f56c63b1bcaea488eaabbff059
Christopher Wiley <wiley@google.com>
2016-04-12 09:21:51 -0700

    healthd: Explicitly reference headers from libbinder
    When creating static libs with healthd logic, explicitly
    reference libbinder to pick up its header include paths.
    
    Bug: 27804373
    Test: healthd libs compile when libbinder's header include
          paths change.
    
    Change-Id: I1014eb0b73d06815c42a527c4ea7e63c35ca3530

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:384b6774230304ac97b905c9fd6675afca2ea402b07bb0b0535cb993c4e1c45c
-size 2850
+oid sha256:55318d9e335dc50b192e274ddb2a6256867c4beef724e5db29daf8dae577db03
+size 2896
