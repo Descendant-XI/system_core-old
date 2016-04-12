26882e3670314bf199169923b74d949c1b2bb0b9
Mark Salyzyn <salyzyn@google.com>
2016-04-12 09:11:46 -0700

    logcat: Adjust help to make it more meaningful
    (cherry pick from commit 378f4745a27632f3bb26e7029fe55142001f3d47)
    
    It is not clear about the long option aliases, list
    them comma separate first for clarity. Squash lines
    of help content if possible. Lock to 3 indents, one
    for options, another for text on same line as options
    and another for next line or continuation.
    
    Bug: 28120456
    Bug: 28788401
    Bug: 28813587
    Change-Id: I5331acde3ac0fd3b87f6699ba108f6cef46a1563

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f6d395bcc23a3abc6e9962488882f8c911e78cb938699e7262fc8e5b47052c1
-size 42756
+oid sha256:81a4f84bb0ca9699347a62ae5d900a95eb1b8becf314ce1548f73a7f8f566480
+size 42876
