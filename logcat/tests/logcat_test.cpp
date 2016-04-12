2508389bf19b01a0f644da5465adcb2f458c1de7
Mark Salyzyn <salyzyn@google.com>
2016-04-12 11:07:26 -0700

    logcat: apct test failures
    (cherry pick from 3842b1a5f6f12bd1ffb885940aa88a7bb959e58d)
    
    Change-Id: I100a5071c059fe9fa85bbdc33db552b16d7bdf60

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d4ec76564c8823c2ba06c6cfaf5e3e09e3f4346f23127d2c257f8e881f8dc68
-size 26322
+oid sha256:debc3581e964c2daf0345cc811088253a723b85a1e708ebb0c1dbf4515f3735e
+size 25940
