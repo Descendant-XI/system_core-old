1d42d82c27c54e2966a9e8afa7ac0d211f92cb22
Elliott Hughes <enh@google.com>
2016-04-12 08:40:43 -0700

    Fix toolbox failure return to match similar tools.
    Bug: https://code.google.com/p/android/issues/detail?id=182226
    Change-Id: Ib05709b8f9b474d5989b69abd949a6dc661af998

--- toolbox/toolbox.c
+++ toolbox/toolbox.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:635039e4265ad376ca11c9c80cee488481e0bbd9cbfc5980fc1783aac2c1a4e0
-size 1655
+oid sha256:151d269f24fc1da08a4e4dc92f1064b80e9599da94a2ce97f0b46db976ca6665
+size 1656
