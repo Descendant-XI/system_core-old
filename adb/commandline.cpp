af74806d2af7f52500960b586e23130413ac3094
Josh Gao <jmgao@google.com>
2016-04-13 12:18:58 -0700

    adb: add support for 'wait-for-any'.
    Allow waiting for a device in any state.
    
    Bug: http://b/28168212
    Change-Id: I1876ecd70089ca88f2da5de4182e13724ec50501
    (cherry picked from commit 86441c31fe256f37625743e2818377ce5e3dec05)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15e201c004ae6f3c38aef8e005df587fdf517c678455e6c3ce52d73df4914cfc
-size 81367
+oid sha256:f77a6fb73243992b12a7ef7015b7b70f362bb6ff8eb16b038ec3483350bee2ac
+size 81400
