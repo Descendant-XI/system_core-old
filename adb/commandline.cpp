3f412a67255687266c2858643a7e504fdb89b298
Josh Gao <jmgao@google.com>
2016-04-13 12:22:27 -0700

    adb: after `adb root`, wait for the device in any state.
    Otherwise, `adb root` while the device is in recovery will hang.
    
    Bug: http://b/28168212
    Change-Id: Ibc8038b1745139fb505e466c74c605b9df474c8c

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da929ca039552e6a6786061d5c180379723ed5d3fb21c0a450cc4f20e4990f19
-size 79556
+oid sha256:b6e6ae0d9af24b61946ac308b4c13c60222e378235450b73ee2bd3ff96da2436
+size 79553
