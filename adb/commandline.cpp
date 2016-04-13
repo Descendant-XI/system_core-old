451cb8b10e620bb0e316687a2ccdefb8624f45c4
Josh Gao <jmgao@google.com>
2016-04-13 12:14:16 -0700

    adb: make 'wait-for-{recovery,sideload,bootloader} work.
    The help for wait-for-* documents this as working, but we only had a
    special case for 'wait-for-device'. Make the others work as well.
    
    Bug: http://b/28170823
    Change-Id: I57d70f8b9a383947c4f761919bfbd8515a013520

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f27f3ab403be1a88febb53074e9e8a06a5e5eca8cc4632faea714603a111e4c8
-size 79847
+oid sha256:21f88e713ffbba9758a8649fad6b7be1ecb356898b5076615186b8ea0148f86c
+size 79523
