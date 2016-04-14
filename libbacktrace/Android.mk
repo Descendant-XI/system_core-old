d1005afac0a9548ae3ba212ab861efb6f9307ed6
Yabin Cui <yabinc@google.com>
2016-04-13 21:21:47 -0700

    libbacktrace: only build libbacktrace_offline static library.
    As device may not have libbacktrace_offline shared library, simpleperf
    no longer rely on it. So there is no reason to keep libbacktrace_offline
    shared library.
    
    Bug: 28152982
    Change-Id: If8e31b1434646dbbfed60264eb25f034bc5d6946
    (cherry picked from commit 51e607056dd0db93ef683d05349dd98648c47e47)

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:122cdd10d3bc8f179b1861ea39c69d6536a8651c4cdfa8baeffbd5083108a396
-size 5173
+oid sha256:8683a14ffe4b3e2d52642e8c9c869849de6d7937c6f84d6c373c317f41afa9bd
+size 5472
