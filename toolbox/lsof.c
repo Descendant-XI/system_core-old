882b58e6b68e2de980313a919d9c988dad678765
Elliott Hughes <enh@google.com>
2016-04-19 14:57:09 -0700

    Fix scanf %s in lsof.
    Bug: http://b/28175237
    Change-Id: Ief0ba299b09693ad9afc0e3d17a8f664c2fbb8c2

--- toolbox/lsof.c
+++ toolbox/lsof.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aaab1e7a241e8f1c304f85d18957883aa486b1a02a6fbf75038eb1a47df7122
-size 7196
+oid sha256:bb2b66512212dc90e2c6c9e3cb3f67fff6b0260d29a8d2453be528b1419daa12
+size 7169
