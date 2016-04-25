b64b85a7bfa4657c394896d00051260a4e2cf16d
Chih-Hung Hsieh <chh@google.com>
2016-04-25 15:28:36 -0700

    Fix google-explicit-constructor warnings.
    Bug: 28341362
    Change-Id: I0ce541f79feef5c08eac64edc686be3edf9c2aa4

--- libpixelflinger/scanline.cpp
+++ libpixelflinger/scanline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81eff89ec2acf1c995c52fb40acb04c9d0fb60e51e8f0bfcad58a0136e3eb2ff
-size 79567
+oid sha256:40a14b61e115f454edc7c18ffd4d97c048bc86d184cddab98a0b0ffa4d4bb804
+size 79675
