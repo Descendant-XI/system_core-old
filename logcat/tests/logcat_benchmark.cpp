936237a34fcc35905f9654da5ef93335295578f2
Chih-Hung Hsieh <chh@google.com>
2016-04-25 13:49:46 -0700

    Fix google-explicit-constructor warnings.
    Bug: 28341362
    Change-Id: I2cf746fd07addc6e6c22e98e2a5bb8a8ce47c885

--- logcat/tests/logcat_benchmark.cpp
+++ logcat/tests/logcat_benchmark.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc06aa564924df61fdd6992f2111352638e5c7351ddf9cddc330310f962f98c4
-size 3374
+oid sha256:9bc0ccbb708252e75210e7500d00bc5ab0a2e7cfa8623e9fea926c77cb714c2c
+size 3383
--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69fc196090ddc2abeeab25b249b9b85a8495fcb99a0dfb28bd30033ecfe82673
-size 33021
+oid sha256:b86f9e5884427188569a3a8302e066519157588e5dfb69f0d0c8b23452294e7e
+size 33030
