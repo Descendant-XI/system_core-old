8f9dcee9307e8b6b9c96b69514df517f7c232178
Jeremy Compostella <jeremy.compostella@intel.com>
2016-04-26 13:51:27 +0200

    fs_mgr: update block device reference in verity metadata
    The dm-verity metadata contains the block device path that is given to
    the dm-android-verity driver.  If the device is using slot A/B, this
    path is missing the slot suffix.
    
    This patch makes fs_mgr replace all the block device path reference
    with the one including the active slot suffix.
    
    Change-Id: Ib624d0d18b12a8a287cac7f15605a2e0fb7c97c6
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    
    Bug: 28845874

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08cb1bf51cdb827c52063a77d673007fc49a7e4fa530dfeb55ce1bcb3f6f388d
-size 26650
+oid sha256:e52e6d4dc2e18c09cb1e0e8975193d89c6dfe3e03a5b2293b66a9bd4eb5c00b5
+size 27434
