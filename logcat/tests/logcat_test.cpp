e626c2732d7d66177461fb6f1d3fe5e0e8f7cc62
Mark Salyzyn <salyzyn@google.com>
2016-04-29 11:28:03 -0700

    logcat: confirm -g result after -c
    Bug: 28451229
    Change-Id: I43a1721ae991cc1316335ba4b3b868b904926d42

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a2d74a114e6bc6e5db4e44f649fd2b901891d3d2e0f9a0fb65534379f59f8a5
-size 26918
+oid sha256:3e0ce2c789f112908c59960817af84073734af6d64a2ff89119c5f10a8e4978e
+size 28434
