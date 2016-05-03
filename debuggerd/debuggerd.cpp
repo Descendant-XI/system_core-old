e8a1692cf5cddde5bf2df25e2d7b05afe42634fd
Christopher Ferris <cferris@google.com>
2016-05-03 16:32:13 -0700

    Fix problem with wait_for_gdb.
    When someone enables wait_for_gdb, activity manager will kill the
    stopped process before a developer can attach to the process. To
    allow debugging in this case, change the code to only contact the
    activity manager right before continuing the process that is
    crashing.
    
    Also, modify the conditions under which to do a gdb attach. The previous
    code did a partial attach if perform_dump failed. The new version simply
    allows an attach regardless of whether perform_dump passes or fails.
    
    Bug: 28409358
    (cherry picked from commit 9818bd2bbee00c387247210eeae2a0a0d3912c8b)
    
    Change-Id: I42f464b69332748e16b07d9d00f44b3aa26ce8b7

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef1f18b44f383d4f828f41e6239c698139b5099b4f1aa1a8802291d6d22ab09b
-size 25202
+oid sha256:a1ba0a8f07242f80254df11d9ebeeb7da2b1dfd599139c00835ded50678cfe26
+size 27063
