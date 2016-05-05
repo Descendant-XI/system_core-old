c213e67cf3115eb29457918676724fd671a0e83d
Srinath Sridharan <srinathsr@google.com>
2016-05-05 16:10:53 -0700

    Only apply schedTune boost to top-app tasks
    When using EAS, the foreground tasks were all getting boosted
    during touchboosts. Limit it to top-app tasks.
    
    
    BUG: 28378389
    Change-Id: I72b7158a614bfd9b6c61024774e408ceba61fc9c

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee51ee4151fb0d38e9c12736936c8c3f12f157161a3052a223aa03239d64c4dc
+oid sha256:c2e6dfb6dbb6329b6130baf23501166e2daa0a38a68fd7af482725c71dc8044f
 size 11493
