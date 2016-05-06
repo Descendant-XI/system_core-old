ddbb69185acfefb49d9ffc7ec504e9d4f623f6ac
Casey Dahlin <sadmac@google.com>
2016-05-06 16:19:13 -0700

    Make ADBD announce its presence over mDNS
    We now request mdnsd from adb and register a service of type _adb._tcp
    for clients to connect to.
    
    Test: Verified service appears in avahi-browse
    Bug: 28074466
    (cherry picked from 379ac414e4d9f53388d903913022a55695292775)
    
    Change-Id: Ie871b9c8b40e86063cc1e68e8f3e4290ead2d279

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a612bb69513831eaf58fa3e2e6c1bcbeb0acbe212a93060803d9a7c47852c46a
-size 10443
+oid sha256:1371e8990dd55cd2d9193b83425b65be510ad5a77c14bc45f85c324315a883b2
+size 10481
---
+++ adb/daemon/mdns.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9a50cd827bdfef4d903002087f226ce998d6eac8c3716ffcefca5ed192185ad
+size 2699
