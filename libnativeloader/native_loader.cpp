aec7156f01a4741364d181016f9b59a3441f4a27
Dimitry Ivanov <dimitry@google.com>
2016-05-09 10:55:50 -0700

    nativeloader: Fix the case of search_path == null
    When user creates custom classloader which does not extend
    BaseDexClassLoader the librarySearchPath gets set to null
    by java.lang.Runtime.doLoad().
    
    This patch makes nativeloader correctly handle it.
    
    Bug: http://b/28659864
    Change-Id: I1b61c6bc952984d7c49775a9178fc3270948e62a

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:778db1ab4ba42150e7298bc96ac432bccaebcb99224be613e795ca513fcca7ce
-size 9394
+oid sha256:12dd4fcea4e57a5dc64d4354b9419a60a11b5c1540e50244a3c065a9213c58f1
+size 9564
