cef284a831de5d2c7ffe44ca18464d76d639c01d
Dimitry Ivanov <dimitry@google.com>
2016-05-09 17:51:51 -0700

    Move linker-namespace functions declarations
    They are intended for platform use only and we shouldn't have them
    in the public header file. This CL moves them to libnativeloader
    private header file.
    
    Bug: http://b/28174921
    Change-Id: I01cf446d2d8d7cf6f9e31130aa3d4e54fbbbb9fc
    (cherry picked from commit 6796522256b4cd861d3ebaf8264bd272b3c7441f)

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1f1db90182adc87efeee926ab7fed2e04a9c8ddf150e52e06e7d410fff6624e
-size 10653
+oid sha256:5645f292bea71239a2c7ea0bebe5bae0fc7d42fe67e267d9acad0abc0840c4cf
+size 10656
