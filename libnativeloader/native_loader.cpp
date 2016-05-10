7113b686ba8cbbf6209c2d8e3fb6c2d33ba80206
Dimitry Ivanov <dimitry@google.com>
2016-05-10 10:39:48 -0700

    Extend white-listed directories to include /mnt/expand
    Apps on sdcard live under /mnt/expand and therefore
    this directory should be under linker namespaces
    permitted_path.
    
    Bug: http://b/28639227
    Change-Id: I462f9f23656c95d9c2a48bb3f513abcd9d08f340
    (cherry picked from commit f334cbf0e1425633bef96a21b0ce9e30f4c6ffa9)

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12dd4fcea4e57a5dc64d4354b9419a60a11b5c1540e50244a3c065a9213c58f1
-size 9564
+oid sha256:01b6cec1ff4d6f7569e21f4f80d9e28bc18d5f9b653fd1eeaad1a353a1c79e0f
+size 10269
