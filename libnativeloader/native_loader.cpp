3de3180fabec3336a42ec265fde51ecc259cc71e
Dimitry Ivanov <dimitry@google.com>
2016-05-12 15:34:41 -0700

    Set parent namespace for linker-namespaces
    This change allows applications to share RTLD_GLOBAL
    native libraries between namespaces associated with
    different classloaders.
    
    The rule is - if a library is GLOBAL within namespace
    associated with parent classloader, it is shared
    with namespace associated with this classloader
    
    Note that the sharing happens on create_namespace
    event, which is tied to createClassloader in case of
    application classloaders created by the framework, for
    custom application classloaders it is tied to first
    loadLibrary() event.
    
    Bug: http://b/28560538
    Bug: https://code.google.com/p/android/issues/detail?id=208458
    Change-Id: I7ee701166f8ec5eff033b7acc0f80c7aa4ec5bda
    (cherry picked from commit 24db75c1ce7ff8376a475214b059b9a37ac07936)

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af3b166ece06980ca7d442ca198eb9fe75bbc2a45bd6ea2b4005df72defac188
-size 10544
+oid sha256:5fea1391ee3c485004ed500d16016faabf1045604578c5e2164622a5831a7708
+size 11544
