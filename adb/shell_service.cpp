02493a67728fab6ed82b5e4ebbd04613a87728aa
Josh Gao <jmgao@google.com>
2016-05-13 18:16:43 -0700

    adb: replace failing exits in adbd with abort().
    This makes it possible to get a core dump from adbd when it decides to
    exit.
    
    Bug: http://b/28347842
    Change-Id: I4cfe5f273f62b2c32e61232d3c39881ecdd6b582

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b6de8a2654fcf863fa682bb35d8a8e96f9334fa2fe863a8e925e51303e4a68
-size 28049
+oid sha256:d625593569a192ba8837185f4fcc6865124609b783dab44089a2f67bc8bd9309
+size 28046
