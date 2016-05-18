d0fc601ab7d17358997c81ccacdfd4be6d317b81
Josh Gao <jmgao@google.com>
2016-05-17 19:23:39 -0700

    adb: switch the socket list mutex to a recursive_mutex.
    sockets.cpp was branching on whether a socket close function was
    local_socket_close in order to avoid a potential deadlock if the socket
    list lock was held while closing a peer socket.
    
    Bug: http://b/28347842
    Change-Id: I5e56f17fa54275284787f0f1dc150d1960256ab3
    (functionally a cherrypick of 903b749f + 9b587dec, with windows disabled)

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c30e94174bc3eb4dc75041306d833e5d5dd34ff5f0b929961f2c313cefc3eec6
-size 25332
+oid sha256:53dc534bd4795549420afdf5ecad1fc9e775019f4d13ca4d263425afbce0dadb
+size 25220
