cfa4661442f1f0ececcb49e2e50f32b23aae398f
Chih-Hung Hsieh <chh@google.com>
2016-05-18 15:37:20 -0700

    Fix misc-macro-parentheses warnings in lmkd.
    Bug: 28705665
    Change-Id: I43388c0f9fdc8c2a0c0f4495f96bbc27e3de8a1d

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dde4a56e3ccbdbdb88193e828056549dd2effc12f6390c15ee1266b531af6a0
-size 20841
+oid sha256:78bfb3ba1194448cdfa1d09793f844fccc97a1451bbb698367129dbaa7a255f1
+size 20843
