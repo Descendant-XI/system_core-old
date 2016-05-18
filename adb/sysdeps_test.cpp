3a1ad91f88cd1f3693c8805e2bf31853935931a8
Josh Gao <jmgao@google.com>
2016-05-17 19:23:39 -0700

    adb: add implementations of mutex, recursive_mutex.
    Our version of mingw doesn't support std::mutex or
    std::recursive_mutex, so implement our own using the Windows primitives.
    
    Bug: http://b/28347842
    Change-Id: I4e1d56a89bc5fcb5f859bf5014343697a4a85b77
    (cherry picked from commit 903b749f7883394213933f733b902e104ce6dda7)

--- adb/sysdeps_test.cpp
+++ adb/sysdeps_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99680bac8c9146db7c3caa96ad782383f82c4ae632a5c26992a9835074ecd897
-size 6830
+oid sha256:9ab381404b93a7d948294477f72c0e66fb8e9b0c736d410179da4f57fc11b90d
+size 8152
