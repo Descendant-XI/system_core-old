1b1e0d57170979c9a3aede0c90ea7cf982a2a577
Michael Ryleev <gmar@google.com>
2016-05-24 12:45:54 -0700

    trusty: storageproxyd: use upstream kernel headers
    Force usage of upstream kernel headers to pull in
    definition of rpmb related structures.
    
    This change is required until b/25567964 is resolved.
    
    Change-Id: Ic710d32281dcdd093f6308b82ee937143b692613
    Bug: 25567964

--- trusty/storage/proxy/Android.mk
+++ trusty/storage/proxy/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade08568df850930e8fc680952cb4ad7e03afc75b1240aa425011fbd72371596
-size 918
+oid sha256:8c4b34e0dfc43bd180abe72f6827ace3e23289f9247dd98bbb34d23202a16fe0
+size 963
