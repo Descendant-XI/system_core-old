5a4168fe23883bb814c808811f546c8931e7d8d7
Elliott Hughes <enh@google.com>
2016-05-27 17:51:24 -0700

    Kill adb's ScopedFd for unique_fd.
    It's the adb unique_fd, but at least it's the same API...
    
    Change-Id: If5d6642457e145c450374fde6f1358e5ac386a96

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d625593569a192ba8837185f4fcc6865124609b783dab44089a2f67bc8bd9309
-size 28046
+oid sha256:cfbe332f999ff42e5701b68cf15228af639065ed279ebe3af9660eaf0ac9805a
+size 27801
