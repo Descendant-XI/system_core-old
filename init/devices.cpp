93d2ac280e3c0e73ffead3636cc30d4c55d89bf8
William Roberts <william.c.roberts@intel.com>
2016-06-02 09:53:44 -0700

    ueventd: only relabel devices if there's a delta
    Since commit: 24a3cbfa732dc14c1a559f4ad79e1700fbae888f
    ueventd is attempting to relabel devices on calls to
    make_device() when the device already exists. However, it
    should only be called if the contexts for relabelfrom and
    relabelto are different.
    
    When this condition occurs, one will see denials like this:
    
    [   14.409448] type=1400 audit(978307239.225:26): avc: denied { relabelfrom } for pid=1572 comm="ueventd" name="ttyS0" dev="tmpfs" ino=9613 scontext=u:r:ueventd:s0 tcontext=u:object_r:hci_attach_dev:s0 tclass=chr_file permissive=1
    [   14.428107] type=1400 audit(978307239.225:27): avc: denied { relabelto } for pid=1572 comm="ueventd" name="ttyS0" dev="tmpfs" ino=9613 scontext=u:r:ueventd:s0 tcontext=u:object_r:hci_attach_dev:s0 tclass=chr_file permissive=1
    
    Bug: 29106809
    
    Change-Id: I2105b169206f0b26420f3c4b0ba5c49aeb98da92
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bb1ad8e57851d8545a8bf99488558eb8815a608d9b897ef0dd30faedb12d3a7
-size 27626
+oid sha256:a1351520c454463db5b2cbf354f976a125a3a802c2f5f85558a3f5b84bcc1de5
+size 27965
