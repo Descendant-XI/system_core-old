ba8fd6549da9bdb219d208c0a8136bb94034251c
David Pursell <dpursell@google.com>
2016-06-06 09:37:16 -0700

    adbd: properly close subprocess pipes on Ctrl+C.
    When non-interactive sessions exit via Ctrl+C, adbd sends SIGHUP
    to the child process to let it know to exit. However, adbd was not
    closing the pipes to the child process, so if the subprocess ignored
    SIGHUP and continued writing it could fill up the pipe and block
    forever while adbd waits for it to exit.
    
    This CL adds the necessary calls to close the subprocess pipe after
    sending SIGHUP.
    
    Bug: 28981563
    Change-Id: I318e322e563241052648361172f4859c297837fb

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfbe332f999ff42e5701b68cf15228af639065ed279ebe3af9660eaf0ac9805a
-size 27801
+oid sha256:5c3fca2df10f71efeb314072e6770868b5c0a7a4cec8e18fb046cc76b2b86ec1
+size 28090
