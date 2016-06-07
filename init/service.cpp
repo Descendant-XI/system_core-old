4d67bc8a308e8026369189f60ce923bcab8b767f
Tom Cherry <tomcherry@google.com>
2016-06-07 11:22:00 -0700

    init: expand arguments of services when they start
    Arguments of commands are expanded based on properties if they contain
    ${property.name}, however this is not currently done for arguments of
    services.
    
    This patch makes it that arguments of services are expanded each time
    that the service starts at the point immediately before execve().
    
    Change-Id: Iba581a8377e25a6478d4d2ec2e8b29e181d8640c

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b7b316735793c52147ccea0dadc0a2e7cfc3480129fa1ab5c9cfc818d35fdd4
-size 26949
+oid sha256:12925c9c19112ddf91abc296ac5a9aeb3fda252bc5ab6c692c0d4a87db0ee0a4
+size 27310
