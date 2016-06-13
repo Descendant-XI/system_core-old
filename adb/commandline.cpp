d15d52a0a50950fe3240e4c5f0e23ca52daa0eb7
Elliott Hughes <enh@google.com>
2016-06-13 15:01:13 -0700

    Fix the sense of a test in "adb sideload".
    Change-Id: I516a2ad2d74d9322ff9eb2e4cd3c3829e5cbda99

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cf38109701ea1568df830a121f44d55fede4a5574b0cd439273a3a5d7086eea
-size 81233
+oid sha256:f3c59cad213b9c08803106d4c82be99c34fec1e27da0bde6d0e486de97974ee5
+size 81232
