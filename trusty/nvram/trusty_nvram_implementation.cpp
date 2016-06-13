ccc2ea114930aea55d1282db7d464853f49b717f
Mattias Nissler <mnissler@google.com>
2016-06-13 16:10:17 +0200

    trusty: Add nvram-wipe utility.
    This adds a small utility which is useful to trigger access-controlled
    NVRAM wipes from recovery and to disable wiping functionality after
    boot.
    
    BUG: 29260086
    Change-Id: I131d400ab2643ce91d7838a2bb770afd48f83b5f

--- trusty/nvram/trusty_nvram_implementation.cpp
+++ trusty/nvram/trusty_nvram_implementation.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fd0eb84c78b3f1fb2b49f51cbcc2297be09ccb076ab4fef9d459b985c13a7b2
-size 4608
+oid sha256:dd4fe003bff6631356010ea1f9a62ecf6cdc08fd2955564a62af9c0dd99d9fde
+size 2990
---
+++ trusty/nvram/trusty_nvram_implementation.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f490c5ffccfd10331f08e03c27823765cf109d9ae7380224db991330ba139bc
+size 2118
