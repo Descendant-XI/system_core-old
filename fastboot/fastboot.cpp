7945457344fe921a0ade47e44608f9da795535f5
Alex Light <allight@google.com>
2016-06-14 14:29:09 -0700

    Make fastboot flash 'B' partitions automatically.
    Also add ability to specify --slot active to suppress this behavior.
    
    Bug: 29278988
    
    Change-Id: I3ff184be74f9e2d1cb3abc80bd206b4b9faf8a35

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec6233b62fde43befe69108515dfd55a1cbab57eb9707d9539d34ac00d7174dd
-size 55856
+oid sha256:6eca6212c4aa2a7161223e0036d55183c45caaea33964d2923579d0cd3f63fd2
+size 58058
