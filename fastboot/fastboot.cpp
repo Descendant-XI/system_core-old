488e42e14ec130e89dfa5681082e209e35863b18
Alex Light <allight@google.com>
2016-06-16 21:46:06 +0000

    Fix issue where non-AB-ota devices will cause flashall to fail.
    We now check that a device has slots before trying to get the name of
    any of them.
    
    Bug: 29278988
    
    Change-Id: I50f3596e467afae20177ce4e458e697435223c51

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eca6212c4aa2a7161223e0036d55183c45caaea33964d2923579d0cd3f63fd2
-size 58058
+oid sha256:dd9dd7978601f652ea8072b06572cc2aede3ff6c7b35fe85abbd9eaa337f513f
+size 58347
