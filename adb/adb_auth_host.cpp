b3434073aed01e1ea4bf3ff67434290f4aa9a8ea
Elliott Hughes <enh@google.com>
2016-06-21 16:50:48 -0700

    Address old review comments in adb_auth_host.cpp.
    Comments from:
    https://android-review.googlesource.com/#/c/212781/4/adb/adb_auth_host.cpp@107
    
    Bug: http://b/28152031
    Change-Id: I27d062f3eeb8db90f94b1b4f5d7204000a7ac73d
    Test: manually removed ~/.android/adb* and checked they were recreated correctly.

--- adb/adb_auth_host.cpp
+++ adb/adb_auth_host.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9ce862183baac06caf7acabb3c0cce96630f1cfd76c16a237a7c44f3d5c3ee9
-size 8991
+oid sha256:78f8777ed8f7d9ea9d20c290a179b83e877f18671b774471358723f2dce439ee
+size 7775
