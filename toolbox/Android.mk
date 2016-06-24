3660141b3cc2a1a0f70f258251a83bfc955c7958
Elliott Hughes <enh@google.com>
2016-06-24 15:02:03 -0700

    Lose start/stop to toybox.
    Change-Id: Ifd918a40616ec55fd3e782423fba67e0183bb8f3

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0e04ead30b81b15c72d27ec8ebb4e34e927619c000b6d51c671666f75dccfb3
-size 3729
+oid sha256:433621f1cc67b984836f44ca33feb3dee4bdf23071d9080e233d291dfd5762da
+size 3685
--- toolbox/stop.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:43de149aba9ed190d74020c8afc7ce3e25863603b1d3ccf6455e6199dd574c69
-size 59
