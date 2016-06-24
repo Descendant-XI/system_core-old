fe6aa31d3f513b07c61d48e71186b19690aea503
Elliott Hughes <enh@google.com>
2016-06-24 15:15:03 -0700

    Remove MTD cruft from init.
    Bug: http://b/29250988
    Change-Id: I38ab263192944e4ff291fd91b25db163a8848d75

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:537f3f0d1498034db35d9c8d36439a36a45aca51d4cf3f88d509bb2189ae5104
-size 31512
+oid sha256:c341fdaec1a91a98ad7c9078ed2264a771051b539c4e931ee19aef8e04410e8b
+size 31106
--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd5fd7a9ab1adaf822ece3960e47881206b7cdddb4848fd77b2a04510a7276d9
-size 14548
+oid sha256:e5cc433dc2bca02353fca8feb10faeecc917dae39cca9fef201245045e9e5d18
+size 12753
