2bb44ac5555ba09955c422a979aaa88e5b8e8eed
Yabin Cui <yabinc@google.com>
2016-06-24 18:28:03 -0700

    init: use libbootloader_message.
    Bug: 29582118
    Change-Id: I6285b29774a11d6dda8496c7c73e4c1d17a672bd

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad2789c31e54fb5465f25f66058c212680d57f7f5754a6896ec487de1ed6841c
-size 31162
+oid sha256:657439f2b312d578ec09942dad95a138fa694ddf74e025afddfec30c2d94579a
+size 31174
