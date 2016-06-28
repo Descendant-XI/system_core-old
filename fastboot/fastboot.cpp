3b019f0c39025402c5ac12e5dcd709c51622d6a6
Daniel Rosenberg <drosen@google.com>
2016-06-27 19:43:11 -0700

    Call set_active after flashall and update
    If the device has previously failed to boot, and the current slot is
    marked as unbootable, we must call set_active to reenable the slot.
    
    Bug: 29827625
    Change-Id: I8b723dda80e246b48e5967aff4503c3d120bfb9b

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec6233b62fde43befe69108515dfd55a1cbab57eb9707d9539d34ac00d7174dd
-size 55856
+oid sha256:f478141c2f6b57b607b94121a8a1381b765179a3fee3fbcf53da56b281448c61
+size 57040
