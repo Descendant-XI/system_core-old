900531e15d2aba92d18c6c1465a23258e5c1f5ef
Daniel Rosenberg <drosen@google.com>
2016-06-27 23:03:48 -0700

    Handle invalid suffix lists
    Some devices will report an error string as a value
    when unknown variables are queried. This can lead to
    unexpected behavior, so we attempt to detect this case
    by seeing if the suffix list doesn't make sense.
    
    Change-Id: I939b1e01c40ddc05d881fd54423406db250cc8e5
    (cherry-picked from commit 190d968414b3889441843e1bbebdf7acc2dc88c8)

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:761dc0d0915cd5dde35e1f9aa536d4b8896c2d8b071496533a771e694bcd39a5
-size 54290
+oid sha256:4a7c6b9d3d69ecb3cbb6df4945fd61e507fc4561cd0d59b77fb9df14a715f53f
+size 55085
