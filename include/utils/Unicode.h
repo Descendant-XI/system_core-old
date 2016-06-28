6ddbdb8b17a685095daa08a0460677c4fd6fa158
Sergio Giro <sgiro@google.com>
2016-06-28 18:02:29 +0100

    libutils/Unicode.cpp: Correct length computation and add checks for utf16->utf8
    Inconsistent behaviour between utf16_to_utf8 and utf16_to_utf8_length
    is causing a heap overflow.
    
    Correcting the length computation and adding bound checks to the
    conversion functions.
    
    Test: ran libutils_tests
    Bug: 29250543
    Change-Id: I6115e3357141ed245c63c6eb25fc0fd0a9a7a2bb
    (cherry picked from commit c4966a363e46d2e1074d1a365e232af0dcedd6a1)

--- include/utils/Unicode.h
+++ include/utils/Unicode.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fcbd7742c8d38680a6cff89e19fd4718a45f19d9b22b96e5692c11523f75c7a
-size 6885
+oid sha256:52a5769a9405c2588df971dcd32509150a38226aadb1252a437a3cb9740291e5
+size 6917
--- libutils/Unicode.cpp
+++ libutils/Unicode.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca2ce9248f7a8cc101993489fbce4669cf61c36f2c740165ce7242c7a639ce70
-size 17061
+oid sha256:9830f022148b868fe1145598e6c3c221fdf91f1aabaa7661b49709959a67a4ef
+size 17440
