31f512c7b10b5c46681c5b1d12c5b65e1b478d12
Daniel Rosenberg <drosen@google.com>
2016-06-28 01:36:21 -0700

    Add Fastboot support for flashing secondary images
    Fastboot update and flashall will now flash secondary
    images if provided, and if the --slot flag is not set
    to 'all'. Also added flash-primary to preserve the
    previous behavior, and flash-secondary to just flash
    the secondary images.
    
    Bug: 29278988
    Change-Id: Ia870e4be55617c0eefa4e1381287f8cd14affe7e

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:592284823c74898419a025e98e863c27ef4b13a7d5e8316fc82657f2e45731b2
-size 57835
+oid sha256:bb9b7d25bfd5a293ad2cd00db7840c6355ac230cff6e4481a176c55c7c1b6d72
+size 61226
