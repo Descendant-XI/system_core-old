d451b9aee7261c943e402b75f58dc5d87494c647
Alex Light <allight@google.com>
2016-06-29 15:37:29 -0700

    Fix fastboot flashall
    The return value of load_buf was swapped so we were erroneously
    failing.
    
    Bug: 29278988
    Change-Id: I777bfbc174f50ec0b71e973db36543eb37d160db

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7853d47074f50f65bd9cea7db429547141808546b798b02419ed1f1f33266e
-size 61425
+oid sha256:34c7983885fc87f834f7bd97f651785b1b2f6bf46746c20d57dd9fe81cac32de
+size 61426
