8735df8f08430eaeef5a83f8b10ac786148843e4
Elliott Hughes <enh@google.com>
2016-06-29 17:42:01 -0700

    Clean up key handling in adb.
    This includes the locking we need to be able to re-load the keys at runtime.
    
    We should rename "adb_auth_client.cpp" to "adb_auth_adbd.cpp" or
    "adbd_auth.cpp" in a later change.
    
    Change-Id: I9e1d5b6b7d0497d6f6e5d9c4fb660118cdff05a8
    Test: "adb devices" works against a non-AOSP device with $ADB_VENDOR_KEYS set, says "unauthorized" without.
    Bug: http://b/29273531

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7521d00ec974f497cfde152375e220dcb2270321c7b049534788f2b3b3d62641
-size 42900
+oid sha256:d47c4ba4fcdadf51b1a358e336b8581004e66b4148dc61c446a0ba10ee6a9750
+size 42625
