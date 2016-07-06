1aea1bdfeaa3c02a1273a8bcd7c8a9e50106f250
Christopher Tate <ctate@google.com>
2016-07-06 13:22:22 -0700

    DO NOT MERGE: Ensure the target sees a proper EOD marker during restore
    Malformed or corrupt archives may be missing their in-band EOD
    content, so make sure that the target sees one regardless rather
    than continuing to block on read, expecting in-band signaling.
    
    Bug 28056941
    
    Change-Id: Ic39966d3448787a8c511783d39172032ed9589c3
    (cherry picked from commit 2bcdda8e5dfdb33409fce062fdfd9d0735432ef8)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a340a0756bb66795e40b9b6c844729e96256f90bd43ac3e0103fbc7b60f981
-size 83819
+oid sha256:133368f0bfca51670d9a3de1f5ce15d63467d836447ff6c763de93e05ff77a2b
+size 84856
