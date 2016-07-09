057324676eafd6758a67020afd723ad0974b39d3
Dan Willemsen <dwillemsen@google.com>
2016-07-08 18:08:51 -0700

    Use BOARD_USES_VENDORIMAGE to detect vendor image presence - DO NOT MERGE
    When building without vendor/... in the tree, we won't be creating a
    vendorimage, so BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE won't be defined. But
    we still need to use the vendor image that will be present.
    
    Bug: 31545062
    Bug: 30040825
    Change-Id: I756adc615aea11da60da4d3a4568caca9fed93ab

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bb3a4c1f57ccc873aa573e0d27cc971aba2604b9884cb80d027c0b54d9311c6
-size 4712
+oid sha256:d09da33dde356e7d2ae189be7eca07b4044470eea4ec2b4ba2ae890d3195a0be
+size 4700
