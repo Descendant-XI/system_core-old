4c908d5b17450052f45ea52ff3b201f1689d32a9
Tim Murray <timmurray@google.com>
2016-07-11 11:40:15 -0700

    Add support for top-app stune group.
    bug 29512132
    
    Change-Id: I41ec2dd80a469309f48dbb59fc27fbe43fcd67b3

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e84f50ab4634d4c5d028c8def3671e7fcdb4a5e7d9969641fa1d142cc72f97fd
-size 11994
+oid sha256:ef651eda27fc29b1745adf1e8a870aba20fb9811eec34182d44bb76561fe9583
+size 12140
