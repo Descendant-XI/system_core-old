8d8ec91117a2f0d103f8531ee29098b67c99c6b2
Christopher Wiley <wiley@google.com>
2016-07-12 14:30:21 -0700

    Tag hostapd binary with CAP_NET_ADMIN | CAP_NET_RAW
    Bug: 30041118
    Change-Id: I14d1fd601fc4bce12c563a2004e91bd8ba0f42c3
    Test: hostapd can start as the wifi user with these capabilities.
    (cherry picked from commit 25024901781db197880fc398e43c76008ecde524)
    (cherry picked from commit a76088362ec4bb6ee91820dab7dbf225e43baa83)

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03f59b4dadbe3009fe3f7427d7f9055d1051cf69f3169b576f47935dd60bffa1
-size 11309
+oid sha256:607d156b4bfb0176dac20ac50df4dd66baaefac5efc110635a5475a15c27b1bc
+size 11541
