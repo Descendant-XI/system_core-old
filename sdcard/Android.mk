00ac78c11bae6d54b8934cd9e1b6b5476438e209
Jorge Lucangeli Obes <jorgelo@google.com>
2016-07-13 16:52:24 -0400

    Convert fuse.c to C++.
    Fix string literals and cast void* appropriately. Alternatively, we
    could switch some of the allocations to new/delete, but we would need
    to make sure that none of those end up passed to other code that will
    call free(3) on them.
    
    Bug: 30110940
    Change-Id: I6f39df65cd960930530e5a1f8420a28d50adc25d

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1141fe6b43da1bdb11269fad9d0f7fdd150b38d478e464fae0e6fb885ca5672
-size 305
+oid sha256:7e347a2774d8256ce140355e636701dcea19540b0610d45ed8c4dd593da1acbb
+size 307
---
+++ sdcard/fuse.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c4febab0c5d9612efe119f7e4e0d14d8e27ef05a061284af8997bfb6167185b4
+size 48550
