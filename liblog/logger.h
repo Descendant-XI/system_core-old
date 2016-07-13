44fffcc3a34dd71d2109204bdca5cc16e8f648b9
Mark Salyzyn <salyzyn@google.com>
2016-07-13 07:38:39 -0700

    liblog: reset pid and uid cache after a vfork()
    (cherry pick from commit ec4f5c776d2bde026e2a74128218924be7b3f46a)
    
    Bionic getuid() and getpid() calls cache to reduce the need to
    perform a syscall, and also reset their own cache after a vfork().
    No more need for liblog to be performing this flawed cache operation.
    
    Bug: 30085794
    Change-Id: I70feed8bff0ddd919c2885a348ba67b14ddc0e0d

--- liblog/logger.h
+++ liblog/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb6f9ce51bf3a583b38a894e1cda212a049dc7d792ab658ee8d0473fb90f6878
-size 5712
+oid sha256:c06ede2693bef02f49f2a9ef5fd86a0d756f07c33c8017a9d1555d946be0e2f6
+size 5807
