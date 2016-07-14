f671a8643cbaf81eb58e92afe94788df325c5ea2
Daniel Rosenberg <drosen@google.com>
2016-07-13 20:03:25 -0700

    Add skip-secondary flag
    The skip-secondary flag now replaces flash-primary. This flag will
    skip over the secondary images for both flashall and update.
    
    Change-Id: I9f380f3195006d325d6c45776bf79ecec17506ad
    (cherry-picked from commit e180929866b218b901ba2069615794f076d06b13)

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ff00e76069c758fa9c18029fde6779b9b149fc021d3d4cfc869037bd529cb17
-size 59391
+oid sha256:2dd44ba0343a67d200ecf31e7734db63be5116193e6b7971069f758da3afe53e
+size 59697
