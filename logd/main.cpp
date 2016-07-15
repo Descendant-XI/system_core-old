d7598a88cadffc7856bd11a1556b4b72f69d1b94
Jorge Lucangeli Obes <jorgelo@google.com>
2016-07-15 13:57:08 -0400

    logd: Use Minijail for privilege dropping.
    Makes code smaller, removes duplication. Using Minijail also allows
    adding further sandboxing down the road.
    
    Bug: 30156807
    Change-Id: I8fa242fba545049ee82f6655fe6ecc14c53fcab7

--- logd/main.cpp
+++ logd/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68d0aac6897fb70f6372a4ddb8fb1c4985575da27538961ebdfca1e938b25051
-size 15819
+oid sha256:a45ded6ba8eb5a39da56679014ecfd49c44a595d7432df85fb1f547881a5254c
+size 15280
