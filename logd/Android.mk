f3b400cf452c21c313ae6f3e47bf8c8a82228f44
Mark Salyzyn <salyzyn@google.com>
2016-07-15 10:19:16 -0700

    logd: Replace logd with chatty log tag
    Bug: 30118730
    Change-Id: I7c7802fef157d5ded02573621d21e0ded3df9745

--- logd/Android.mk
+++ logd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a14d0b867ed6ec15f93a0b2a2e14e8dc258acfb132bd81efefe844a8211cfea
-size 1164
+oid sha256:324c0b8b02af69ab86fac69cc37fd145dfbe7d3a0a26e31e901846b09ea02a3e
+size 1166
