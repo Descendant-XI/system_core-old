4daaaea6de61884785e102dff7d541eb90c64e85
Jeff Sharkey <jsharkey@android.com>
2016-07-15 16:21:34 -0600

    Only restorecon CE storage after unlocked.
    On FBE devices, the filenames inside credential-encrypted directories
    are mangled until the key is installed.  This means the initial
    restorecon at boot needs to skip these directories until the keys
    are installed.
    
    This CL changes the implementation of the "restorecon_recursive"
    built-in command to use the new SKIPCE flag to avoid labeling files
    in CE directories.  vold will request a restorecon when the keys
    are actually installed.
    
    (cherrypicked from commit 1635afe83d1ebd6b5f1fac1e9c6c6b5cd1c93204)
    
    Bug: 30126557
    Test: Cherry-picked from master
    Change-Id: I320584574a4d712c493b5bbd8a79b56c0c04aa58

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c924030549aa23a116b2147cd95ce7a5d3c95b16382b8cce535336cf4c789d2
-size 33456
+oid sha256:b109395db2da3d37eac867c1cea214735ec531546cd4e3e3b737472179d7e85d
+size 33709
--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a16ce7df4301567266258519e56a16f7eca0edaa513b73d35026b45c6b2ce1
-size 13145
+oid sha256:4f9c0be3dce538ae4f51a3831f3579603f65ba2fb48b1865dea394f96b4bbffe
+size 13337
