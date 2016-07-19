eca714538ebfda1ee4302bac73dc6d3aaef9bc42
Elliott Hughes <enh@google.com>
2016-07-19 11:45:36 -0700

    Remove no-op klog_init.
    Change-Id: I467edad1d004ac16647f4aa1f0eead04e808efa0

--- libcutils/klog.cpp
+++ libcutils/klog.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3798850a98f8b9cb9c44543f4cd82a64127b24754efb3b87a5d4ae1de8ab3e2d
-size 1971
+oid sha256:2b6fa3a7904d097a32fc5aeb319b9c0172aecc83a3d899619c2743763e81e33c
+size 1945
