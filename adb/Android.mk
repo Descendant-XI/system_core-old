d30e92785d1706914a1c7af08532d95249baf40b
Casey Dahlin <sadmac@google.com>
2016-07-19 14:18:54 -0700

    Don't include mDNS support on platforms other than Linux
    Test: Shamu build now passes
    Bug: None
    (cherry picked from 122a7738606d854d6dc10fa361c1d7296cc9670a)
    
    Change-Id: I650adf0bca96fb655163f5e9b27d9995ae448ec2

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b87191de3b31cd3a92d6438976a20127fbdfae58ca91444753e8b9c71904d6f7
-size 10546
+oid sha256:79e8c2961ab019db0c81b005e8f540a82fa0df0897b0e63609f108b52ef77195
+size 10665
