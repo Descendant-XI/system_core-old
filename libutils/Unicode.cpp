83b4d32b0945e3a447b63e0f5745ce2ff13cf466
Sergio Giro <sgiro@google.com>
2016-07-20 20:01:33 +0100

    Unicode: specify destination length in utf8_to_utf16 methods
    String16(const char *utf8) now returns the empty string in case
    a string ends halfway throw a utf8 character.
    
    Bug: 29267949
    Clean cherry-pick from 1dcc0c82394ec9cd6887c7ca39f9b5024db01ac9
    
    Change-Id: I5223caa7d42f4582a982609a898a02043265c6d3

--- libutils/Unicode.cpp
+++ libutils/Unicode.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5609d83271a43af8112ab38020837137ec683aebc253982d4dcb026c06c3221
-size 16686
+oid sha256:ca2ce9248f7a8cc101993489fbce4669cf61c36f2c740165ce7242c7a639ce70
+size 17061
