7167dd1ff053dac6fa9fb59a03218d8edc4de63c
Brian Anderson <brianderson@google.com>
2016-07-22 10:38:53 -0700

    Rename posted to requestedPresent for NATIVE_WINDOW_GET_FRAME_TIMESTAMPS
    Test: Manual testing with Chrome.
    
    Change-Id: Ib7ef5c76dcd71887848c7cb91336f7fcc5815773

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aa99e0e3cf9c0d765d9f0c60a917ef6ef1ac64018b6f1f59264a67b1bd04cd4
-size 39421
+oid sha256:560343624810daf5e0a2222b51740431636e3c81ce73a304c5b10bbf85930936
+size 39453
