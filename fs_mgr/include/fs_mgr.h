2a5ca2429571c42a1c62b85cf5a88b7a50294bf4
liminghao <liminghao@xiaomi.com>
2016-07-22 11:48:14 +0800

    fs_mgr: support to reserved some blocks for an ext2/3/4 partition.
    These reserved blocks used to root or claim to CAP_SYS_RESOURCES process.
    
    Change-Id: I4893bdb08399bf31a7640d4a63f4952cd636576f
    Merged-In: I4893bdb08399bf31a7640d4a63f4952cd636576f
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04bd9dc4fda563c50f6c8e28d7c4965ba2c4921c77e3a7509fd10e3375d37ac7
-size 4503
+oid sha256:ac577aba0380071ef9b8898450336dc5e9cc566ec04f790b3b9989958ab06a9a
+size 4531
