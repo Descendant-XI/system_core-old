a4f6a083cf17593c00dbabac3823aea580dbee17
Hans Boehm <hboehm@google.com>
2016-07-21 18:56:55 -0700

    Improve RefBase documentation, especially for clients.
    Add basic interface documentation to RefBase.h.
    
    Much, but not all, of this is cut-and-pasted from an email message
    from Mathias Agopian. The rest is reconstructed from the code.
    
    Delete some, now redundant, text from Refbase.cpp, and add a bit
    more about the implementation strategy.
    
    Some minor fixes to internal comments.
    
    Bug: 30292291
    Change-Id: I56518ae5553bc6de0cc2331778e7fcf2e6c4fd87

--- include/utils/RefBase.h
+++ include/utils/RefBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58ea4111a03c18d3802a5b22113d5a8fd98a11be07369918f839b4b58f5448aa
-size 16379
+oid sha256:012ede9a04fee3aa69a9935b75eabe6e7338f75f5d6b155d898f6545b96fdc34
+size 23426
