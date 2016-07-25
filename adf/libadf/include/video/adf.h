d2a1c689d812826d66fa496013aa0c7457170d8b
Greg Hackmann <ghackmann@google.com>
2016-07-25 14:49:30 -0700

    libadf: support ADF_{POST,SIMPLE_POST}_CONFIG_V2 ioctls
    adf_{device,interface_simple}_post_v2 let the client choose what kind of
    fence is returned from the kernel:
    
    * ADF_COMPLETE_FENCE_NONE: no fence is returned
    * ADF_COMPLETE_FENCE_PRESENT: the returned fence fires when the
      specified configuration appears on the screen
    * ADF_COMPLETE_FENCE_RELEASE: the returned fence fires when the
      specified configuration is removed from the screen
    
    The "V2" calls with fence type ADF_COMPLETE_FENCE_RELEASE are functionally
    equivalent to "V1" calls.
    
    Test: included gtest (on Nexus 9)
    Change-Id: I36190d1b6cea0fbaed2af3ad64fa4729200c5520
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- adf/libadf/include/video/adf.h
+++ adf/libadf/include/video/adf.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf22c0955cc54cbab0515585c7e57bfba0714c3fa0daded1b207904ab5e92c6
-size 7094
+oid sha256:1a7e99a43bda463c356819599e80d959f2b1bbf24e25da6ff7abfc832bd5f2a8
+size 8174
