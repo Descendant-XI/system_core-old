34c0f03e9fff3d62011ee8ce311d048728b35f55
Felipe Leme <felipeal@google.com>
2016-07-26 12:23:40 -0700

    Show bugreport progress.
    adb calls bugreportz to generate a bugreport; initially, bugreportz
    would only report the final status of the operation (OK or FAIL), but
    now it sends intermediate PROGRESS lines reporting its progress (in the
    form of current/max).
    
    Similarly, the initial implementation of 'adb bugreport <zip_file>'
    would print an initial 'please wait' message and wait for the full
    stdout before parsing the result, but now it uses a new callback class
    to handle the stdout as it is generated by bugreportz.
    
    BUG: 28609499
    
    Change-Id: I6644fc39a686279e1635f946a47f3847b547d1c1

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2f2c94ad518f545d414f0a16e50091799941b9b6439f9cb0e8a3a25e5f732fc
-size 9837
+oid sha256:66f128ebcfd1f738c7effbcf326a488991005ea4ecd23c8788f60f0f49181fb2
+size 9860
