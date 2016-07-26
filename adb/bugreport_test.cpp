d486048f6dd9503bde6e99ba3cc3d0743605c5bc
Felipe Leme <felipeal@google.com>
2016-07-26 12:14:39 -0700

    Refactored functions that copy sdout and stderr to strings to use a callback.
    BUG: 28609499
    
    Change-Id: I04aea346e18678ea00797f7f659480edba4436c2

--- adb/bugreport_test.cpp
+++ adb/bugreport_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84de845d72e20d1243e8c2ff50c6e0a8f9bd3e0e7dcb22a22bd3297b614aeaee
-size 6170
+oid sha256:c75f74e8fdea57b72f1910d54dcda64183956740a0c7cf4596faf060ac2e188a
+size 8986
--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a220813d3ff044dfb63c7ca25b17f8a637265469b04f28bf70c4ff0a3bb9145
-size 81479
+oid sha256:e1d7077efa0395f45694d90362c0700dae2d2c7f6991026ee050a06c68e82d0c
+size 81355
