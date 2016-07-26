aabb0c3bffbfb9edc6aa14069f182500b0cfbc6e
Felipe Leme <felipeal@google.com>
2016-07-26 12:23:40 -0700

    DO NOT MERGE: Show bugreport progress.
    adb calls bugreportz to generate a bugreport; initially, bugreportz
    would only report the final status of the operation (OK or FAIL), but
    now it sends intermediate PROGRESS lines reporting its progress (in the
    form of current/max).
    
    Similarly, the initial implementation of 'adb bugreport <zip_file>'
    would print an initial 'please wait' message and wait for the full
    stdout before parsing the result, but now it uses a new callback class
    to handle the stdout as it is generated by bugreportz.
    
    BUG: 28609499
    
    Change-Id: I6644fc39a686279e1635f946a47f3847b547d1c1
    (cherry picked from commit cd42d658b2d08ace81b5ae3b108acbaca1a1d439)
    (cherry picked from commit 97b73a0daf74081a6324bb41cc34d9f0598cfe29)

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f762a754c83f8089ef20d0fe2e2f061a54474739695e0deff59ed4c306d6e51
-size 9465
+oid sha256:79ebec98b432738775761549f0a36f604c1b5439dee4cac56abc153bc4703160
+size 9488
