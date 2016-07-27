61688412b925656bf00c87909d53a2c59beb7e0f
Jorge Lucangeli Obes <jorgelo@google.com>
2016-07-26 20:07:10 -0400

    Fix one last use of TRACE().
    Bug: 30222003
    Change-Id: I3d0a29cb92e83416eb6851b54cb72886da4f7cf2

--- sdcard/fuse.cpp
+++ sdcard/fuse.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:634b93fc1147ea2e06ad36b77652115c5395315b0caf4bc6bc43ec970ae462a3
-size 50313
+oid sha256:612bf1e8227b5a556f474de9151eb64dd24ebf165b9c477d219a7653a1e97a15
+size 50360
