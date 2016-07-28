96095a3f8a57863433dc32b7b7510bc2404ee998
Felipe Leme <felipeal@google.com>
2016-07-27 19:23:09 -0700

    Removed extra 'pulling file' message.
    Taking a zip bugreport has 2 phases: generating the bugreport and
    pulling the file.
    
    Initially adb was printing 2 messages, but since the latter is almost
    instantaneously, it could be confusing to have 2 lines...
    
    Fixes: 30451250
    Change-Id: I1c6cc6163492c1fb6064667dfdb7aaf6ed4c4c6f

--- adb/bugreport.cpp
+++ adb/bugreport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:556dcc99e92e3cd9882d03130dab966858413cd87983091322e14106d98aaa56
-size 7139
+oid sha256:ac8658a2ddec0ca1eb3e0bafb033502a3b28531d80a03df6e36072d145a779cf
+size 7111
