66e877ead5232b0d29c2faef1f5667e4d705955a
James Hawkins <jhawkins@google.com>
2016-07-28 11:50:23 -0700

    bootstat: Fix a potential unhandled exception for malformed input.
    In rare cases the hardware storage on the device may be hosed and return
    garbage.  Use ParseInt which handles bad input instead of stoi.
    
    BUG: 29334139
    Change-Id: I91aedc169df110bea8097775f73dda11def22311
    (cherry picked from commit 4dded613b3aaee016adffd895edf3866426fc22a)

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edaba9a8b9b41fc1b13be8a1fb74c24f186f8482fe2d2a00d94441a78c76b736
-size 11764
+oid sha256:96173b6315b4c17027fd662429cf278547cd8500e3656a95c1939f1b7c72371c
+size 12004
