51ec469cae2c94316d9f0189681e291bae04ceb4
Felipe Leme <felipeal@google.com>
2016-07-29 15:47:00 -0700

    Minor improvements on bugreport generation.
    - Skipped artificial 100/100 message, since pulling will take care of
      the final 100% progress.
    - Consolidated unsupported lines in just one message.
    - Let user know the bugreport can still be recovered when it could not
      be copied to the destination directory.
    
    BUG: 30451114
    
    Change-Id: Icfce9c1e8e7ed407719728b9874679ac40b21eab

--- adb/bugreport_test.cpp
+++ adb/bugreport_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05b24f5900965265138808effab1ba3065730d6bc938377e5c57add11380d223
-size 12699
+oid sha256:3fa2dd9296baf70bc6216d1fad6a351085f71f870fa34f017e7fd32172867dde
+size 13043
