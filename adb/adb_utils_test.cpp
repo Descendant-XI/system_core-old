4130a524e76f0497f3c4bff6ad6551b6d45527fd
Josh Gao <jmgao@google.com>
2016-07-28 18:30:46 -0700

    adb: extract Windows bits out of directory_exists test.
    Bug: http://b/30481559
    Bug: https://code.google.com/p/android/issues/detail?id=214633
    Change-Id: I8f20b3cd5aef6a77c2b4f194b914b4295397d73f

--- adb/adb_utils_test.cpp
+++ adb/adb_utils_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d6e2638c47bd95a13b62ad1a40e3c073a65a484518eca25c180be54027dfdfa
-size 5850
+oid sha256:139aaf4ebb163a9604158cfc0be27f3d6f2933f997ab412b458df692104c8a5f
+size 6112
