ba300234f99b95352c41300b7eb8957c44e07552
Felipe Leme <felipeal@google.com>
2016-07-29 17:57:00 -0700

    Deprecated 'adb bugreport' with flat files.
    Starting on Android N, zipped bugreports contain more information than
    flat-file, text bugreports. On N, calls to 'adb bugreport' would still
    generate a flat-file bugreport, but with a warning.
    
    With this change, 'adb bugreport' will generate a zipped bugreport in
    the current directory, using the bugreport name provided by the
    device. Similarly, calling 'adb bugreport dir' will generate a bugreport
    with a device-provided name, but in such directory.
    
    BUG: 30451114
    BUG: 29448020
    
    Change-Id: Ibc8920dd44a5f62feb15bf3fefdcb0bdbf389a90
    (cherry picked from commit 307951e124afc0ab385dc679a57562d339049e2b)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12243e52dc0cd15ffdd4cb66fbbc6f22185602e601e850cf38c9b620ecec972
-size 81600
+oid sha256:aa89b1e7e6294e3fe4c171a432a8389728f934f8a9eba8941962a9c39f0ef94c
+size 82122
