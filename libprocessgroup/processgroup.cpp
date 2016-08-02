2264cef246f6836a5e8f26b436cf7556f0c6c756
Elliott Hughes <enh@google.com>
2016-08-02 13:30:30 -0700

    Switch libprocessgroup to libbase logging.
    This lets us see what's going on in init.
    
    Bug: http://b/29751426
    Change-Id: I73432dc7608ca0dc8e421a2f3a750b37c6743f62

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20e27a29df07cdb6b02a3d0ccd09b7c5fd1f9561fecd5f48e6a5733e3344bc0f
-size 10493
+oid sha256:852f0d9c9acde911a49a4c1e54ec8f6b23da5b2747657f8c9ddf66d70a6468fc
+size 10321
