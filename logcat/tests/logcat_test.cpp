431f7e8454d5c7ae3d98cae6dd232c6c46a4dfcd
Mark Salyzyn <salyzyn@google.com>
2016-08-03 14:20:41 -0700

    logcat: add --id=<id> for -f option
    Clear file rotate content if id signature changes. This allows
    logcatd to detect change of property ro.build.id to reset the
    recorded data. The id is stored in the the -f filespec as
    <filespec>.id.
    
    If the id file is missing or unreadable, do not clear. Allow an
    API where chmod 0 to the id file blocks this feature. Does not
    work for root.
    
    Add logcat.logrotate_id gTest.
    
    Bug: 30591615
    Change-Id: I895749ca7c6d4af57ec57f058a29d8e3af2f8b27

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4f95f17403883ca6b093a4be9fa742c0e29b5bd9b8c3eeb2246f93b1d4c687a
-size 30770
+oid sha256:3cd145f1745bcbdf89081a63908edc44be37a957a1cb4a43891e8fb91682b07a
+size 33261
