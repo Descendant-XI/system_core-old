bb8628875d5cd3bbd33d43e8863a776acb91dadc
Mark Salyzyn <salyzyn@google.com>
2016-08-03 14:20:41 -0700

    logcat: switch to android::base::StringPrintf
    asprintf should be replaced with StringPrintf, solves
    a memory leak issue because of exiting scope.
    
    Bug: 30591615
    Change-Id: I88644cc27946ba4f25d4539420199c6f40df1bfa

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16faee0de8ba28ebda30f2c59ef3bbb6b04d98e84f9dfc5dc5b2fc1eaa500c00
-size 42493
+oid sha256:8c6d0942456bcd67cca8b75ae68817f1d6681e42b175db06662b2928966326d6
+size 42691
