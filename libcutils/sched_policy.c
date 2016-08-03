b39de6ddd4ed85493b929fddf2566267fd9a71ba
Elliott Hughes <enh@google.com>
2016-08-03 15:11:01 -0700

    Remove the prctl PR_SET_TIMERSLACK_PID call.
    This is broken on all devices other than N9, and has no real path to fixing
    it because there are so many different values already in use (41, 43, 47,
    127), with no way to tell which you should use on any given device.
    
    Bug: http://b/30597190
    Change-Id: I07eeaaf6f5dc03c5532ffbfbb85ef2ad750206a0

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4467a76d2d8b9f620e92f06ec37385d01202dda61b85d5071f746ef86675768
-size 11033
+oid sha256:e46fafbeabc6103c6bbe5a81b74638c03b82fae6ac4e3e758add2303ec34f3f9
+size 10797
