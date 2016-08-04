48af601081cc9ca6532c7d26e42e02a474ff572b
Mark Salyzyn <salyzyn@google.com>
2016-08-04 07:43:46 -0700

    logcat: lastLogTime only two most recent files
    Scaling issues if -f logrotation is specified and there are a lot of
    log files to process in lastLogTime, focus on only the last two log
    files. This will cover the situation where we have recently rotated
    the logs and the last file is missing or empty.
    
    This also alters monotonic processing as well as it used to focus on
    only the last file; the danger being doubled now if the pair covers
    more than one reboot when filling in missing content with logcat -L.
    Problem was always there with one log file, but now we handle if
    rotation happened recently. A fair tradeoff for KISS.
    
    Bug: 30630936
    Bug: 30612424
    Bug: 28788401
    Change-Id: I1c13b94e88f5edc08ecef52d407e296874ca5807

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66ebf2a3bc310411ffcf7b3100987029594a131ab768f9502c763de71127d794
-size 43744
+oid sha256:e3ce04d44063311c62a010dd0f0c6dd9a0c3d8462e673cec883f751328ec52a6
+size 43400
