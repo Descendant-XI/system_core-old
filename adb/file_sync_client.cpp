a03a9c8df42c332e554c5affb7d9dfa8e52d0db3
Josh Gao <jmgao@google.com>
2016-08-04 14:53:17 -0700

    adb: improve display of transfer rate.
    Show transfer rate for individual files, and for the overall transfer.
    
    Bug: http://b/30411535
    Change-Id: If2f3008f1287b1d1add4a23c3430b39885b6c904
    Test: inspected output manually

--- adb/file_sync_client.cpp
+++ adb/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f2f61d100a6b88ed076e8db7a2a3ebc4c9a6293f92e83603cdf19a662a47376
-size 35091
+oid sha256:247d327662ea1746923d2ce3703803da1b36cd1023962f0bba8f6fb0111de0a9
+size 38277
