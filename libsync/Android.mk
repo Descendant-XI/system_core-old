63c315a1771dbd3dc0df3a33a4a238954d78622e
Greg Hackmann <ghackmann@google.com>
2016-08-05 13:47:32 -0700

    libsync: add static library for recovery
    Change-Id: If43188c3b4b16c698c077338ea0ef1dfd16b9c56
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e59e9c8c373c013a0df7336eb49fee67a0ecc2ac77a478c03badd703accfb7a
-size 499
+oid sha256:c2aea8b9b53b010f0824842cf2cac9b14a10041a31f536e5acf2830477465f10
+size 961
