56cc8f9b658d65576d76057f79d162ecad01c5d1
Greg Hackmann <ghackmann@google.com>
2016-08-05 10:07:28 -0700

    libsync: remove unused liblog dependency
    Change-Id: I69840ece3f13809b2440edbe6cfa412a56aee4aa
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4af724f79fdfd0fcbd813c009cc1eb82356b915e4d6820a91cbc62a4ecea79d7
-size 565
+oid sha256:7e59e9c8c373c013a0df7336eb49fee67a0ecc2ac77a478c03badd703accfb7a
+size 499
