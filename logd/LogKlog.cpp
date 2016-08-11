a340fda7a07e2c4dda1cf60801b5cfa5a5cbb32d
Mark Salyzyn <salyzyn@google.com>
2016-08-11 16:07:10 -0700

    logd: klogd crash (part deux)
    LogBuffer::pidToUid(pid_t pid) { return stats.pidToUid(pid); }
    needs to have LogBuffer::lock()/unlock() to prevent unordered_map
    data corruption. This can lead to multiple symptoms, crashes and
    continuous spins on corrupted iterators.
    
    Bug: 30688716
    Bug: 30050636
    Bug: 30614675
    Bug: 25620123
    Bug: 30792935
    Change-Id: I1d8fec8e5fda98c6a08645e7456b081507696b3c

--- logd/LogKlog.cpp
+++ logd/LogKlog.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11bed16dd8045cfc165c81fb86eba09341d8c61e533fa0232c758fc2ac091362
-size 25652
+oid sha256:ee8bd43f94b029fd8da9d89feec8d41fd5fc990347dc3a8b90f1fb13ee925967
+size 25778
