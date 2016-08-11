71251a2f6fe24686134245c06b87c48605d92350
Josh Gao <jmgao@google.com>
2016-08-10 17:57:01 -0700

    debuggerd: remove useless timeout from wait_for_signal.
    The watchdog already handles things for us, so the timeout logic in
    wait_for_signal is pointless.
    
    Change-Id: Id81ad5f294068216b73e04c4e2ceca072ae1bbc0

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aa07f0bc62a8a0a481526f80afc2c3c0974f8322e3a57d68e47a0bfb8ef7112
-size 27456
+oid sha256:ea27feb075474eaf10a4e0014e17ea076a32cc0db53d836f36768abcadb76e33
+size 27508
