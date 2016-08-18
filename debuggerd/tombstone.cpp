5c7f496ec6b8b1ad49d81599c73e1f876f627ac8
Mark Salyzyn <salyzyn@google.com>
2016-08-18 14:59:41 -0700

    logger: validate hdr_size field in logger entry
    - check hdr_size to make sure it is in the expected range
      from sizeof entry_v1 to entry (entry_v4).
    - alter msg() method to report NULL on invalid hdr_size
    - alter all users of msg() method.
    
    Bug: 30947841
    Change-Id: I9bc1740d7aa9f37df5be966c18de1fb9de63d5dd

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40735486eb729aa94c8b0b1a78dc4bdaec15aac43ad72e75e348410f3a992115
-size 21337
+oid sha256:69e256583ab9e3ab311793bd4b5754d94297d5d74513ddb16684de4d8e6efe2b
+size 21458
