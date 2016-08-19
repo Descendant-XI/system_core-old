88050655a0d7f21bc439d1b5c65623feeb7d4abe
Damien Bargiacchi <drb@google.com>
2016-08-18 17:33:32 -0700

    Update to new minui text API
    Change-Id: Ic753564a397b90a9ba54660c78bec9c1a1ef583d

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7aa047002f772c1de7f48ea6a9d8557764d3b4e29355d8dd24e70c9b468de64
-size 20475
+oid sha256:7b7702a394f0edbfe8a256ef20483b4648bf70895f8c2fe1c8a5f0545c89ae5f
+size 20520
