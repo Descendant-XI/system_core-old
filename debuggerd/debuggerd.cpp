cbc4f493c810070b2451c9c33bda3200b395b94e
Elliott Hughes <enh@google.com>
2016-08-22 14:17:25 -0700

    debuggerd: set SOCK_NONBLOCK in accept rather than later.
    Change-Id: I95586984dd7aec798d75b150be238d5f6f7c91da

--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0de447b3207004d521f421cd18d2bb502d7a9160cb55f3d38f9dffa35f684242
-size 27387
+oid sha256:55e3c5296a8307efb968e17c788e823a6d0243cfc229a9069909b1d5a29b27d7
+size 27368
