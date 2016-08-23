ef5407afe232f12b67742d4e39b538958bab6b05
Wei Wang <wvw@google.com>
2016-08-23 11:58:09 -0700

    Split fstab mount into 2 phases
    This will make it possible to start some key services before mounting
    data partition
    
    Bug: 30118894
    Change-Id: Ia9f8cc035de6cc0df9a61605864915efa0266d7f
    (cherry picked from commit abfbec342fdd2fc9d139a88a2d950953918e1b4e)

--- fs_mgr/fs_mgr_fstab.c
+++ fs_mgr/fs_mgr_fstab.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1faca7e61b0a6ec3dbec8a7d618c8d40f16bf23ca3cbe634a8caed7e952c0871
-size 17592
+oid sha256:edec9c1250422527d7867645bb38a6a69e8b7df10be25153741b6146643421a7
+size 17730
