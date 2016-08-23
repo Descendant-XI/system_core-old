d238457f8bf7fc6a81a3428f75d0277c254cc8b2
Elliott Hughes <enh@google.com>
2016-08-23 12:50:00 -0700

    Most accept/accept4 calls in system/core don't actually want the remote address.
    So don't write the extra code for it or waste the kernel's time
    copying it around.
    
    Change-Id: I93de64064c2d4fe58ba5b5322cfa69bf31a76dad

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78bfb3ba1194448cdfa1d09793f844fccc97a1451bbb698367129dbaa7a255f1
-size 20843
+oid sha256:4e16abc73672e27f5a92d6de50bf0d372973c9a5d8f28e0996ae83f4ddd6a195
+size 20713
