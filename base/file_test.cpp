06902c1893666a9242734838ee09c29283589571
Elliott Hughes <enh@google.com>
2016-08-23 15:53:45 -0700

    Add android::base::Readlink.
    Bug: http://b/30988271
    Change-Id: Ib844d7c9e33465dabf7aee5e24dc3d1d8d799abd

--- base/file_test.cpp
+++ base/file_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1c98dc1ecc3bb2064b685b87270c3f757049f3369ad5b1c0cdbe6700be31ad8
-size 3299
+oid sha256:9064664a794069a889d3b6ba52a757226765cd275ef32d5f25e9776d941c8f0b
+size 4014
