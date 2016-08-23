14558c4a408f5b990ef5d93d22d9fb54c8ee927d
Mark Salyzyn <salyzyn@google.com>
2016-08-23 10:23:36 -0700

    liblog: add __android_log_close()
    Bug: 30963384
    
    (cherry picked from commit df7a4c6bae5f85532d79a93b7d9197a2aab17825)
    
    Change-Id: Iee7cd0db819644299fa086586630e71415643294

--- include/android/log.h
+++ include/android/log.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f91ec17e99bb8fbfb7b3aa843c8047b3756d7b9a17ecdb03292ce7a888bc996e
-size 4223
+oid sha256:327c55ed511dfdca15cddd8ce0e2efe89600b8e0fee5b98e6c9b8d09b93c6fb0
+size 4337
