9a685c7806a67719902575ca4571882870f7e525
Christopher Ferris <cferris@google.com>
2016-08-24 14:49:18 -0700

    Update for kernel headers v4.7.2.
    The new uapi kernel headers added a new SEGV reason code, so
    add it to the tombstone code.
    
    Update sync.c to include the needed data structures itself. Someone
    else will be fixing this to work on old versus new kernels.
    
    Change-Id: Icb27713ea38a20003c7671a9bca262eebd96099b

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6be32613ed9cac84eef492efa0a4caa5e7f3a885ca94bf89d55057a82545511
-size 21649
+oid sha256:63de24e7745e0a963c1d01d64df0eed94dcc7d3d56dc3cf2b6730b4156d029df
+size 21828
