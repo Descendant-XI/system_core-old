1bfbe1ec3232a901608e44dc5d9f705fb34cc13f
Colin Cross <ccross@android.com>
2016-08-26 11:12:38 -0700

    Replace libziparchive-host with libziparchive
    libziparchive-host is no longer necessary, and mixing libziparchive-host
    and libziparchive can cause ODR violations.
    
    Change-Id: I27b2cc9974000ee7bf1d1ac15fd7b069862d9b01

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:078fbe58a99183c6bbe4fc6f37920361fd84de34e6496538fbccfb9f69cca299
-size 3584
+oid sha256:8e635cf632f1a825b5c436117e11401bec180bcef7f13bd903889935ffcea88f
+size 3574
