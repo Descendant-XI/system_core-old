8bbb730e15ae544f86232ec1d0440e22c5f597ad
Brian Anderson <brianderson@google.com>
2016-09-02 10:16:31 -0700

    Update native_window_get_frame_timestamps args
    - Add outLatchTime, outLastRefreshStartTime and
      outDequeueReadyTime.
    - Rename outRefreshStartTime to outFirstRefreshStartTime.
    
    Test: Manual testing with Chrome.
    
    Change-Id: I1ed3237e393803546f627a645f65f3a8f1ab9dfc

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22197e5e4e38874f4a18664ae51214d403a6c8e73fcb8bee313cff48047325af
-size 40142
+oid sha256:19af360f30fd48a1eaabc2566e6c5d1988d9ff7f0dcc2c4df66e46505dd7d2c3
+size 40319
