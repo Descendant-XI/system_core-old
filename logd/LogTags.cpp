c1b6072d035ea111f3a0bec7ffea2afe41069afb
Mark Salyzyn <salyzyn@google.com>
2016-09-12 14:51:54 -0700

    logd: add getEventTag command and service
    Will register a new event tag by name and format, and return an
    event-log-tags format response with the newly allocated tag.
    If format is not specified, then nothing will be recorded, but
    a pre-existing named entry will be listed. If name and format are
    not specified, list all dynamic entries. If name=* list all
    event log tag entries.
    
    Stickiness through logd crash will be managed with the tmpfs file
    /dev/event-log-tags and through a reboot with add_tag entries in
    the pmsg last logcat event log. On debug builds we retain a
    /data/misc/logd/event-log-tags file that aids stickiness and that
    can be picked up by the bugreport.
    
    If we detect truncation damage to /dev/event-log-tags, or to
    /data/misc/logd/event-log-tags, rebuild file with a new first line
    signature incorporating the time so mmap'd readers of the file can
    detect the possible change in shape and order.
    
    Manual testing:
    
    Make sure nc (netcat) is built for the target platform on the host:
    $ m nc
    
    Then the following can be used to issue a request on the platform:
    $ echo -n 'getEventTag name=<name> format="<format>"\0EXIT\0' |
    > nc -U /dev/socket/logd
    
    Test: gTest logd-unit-test --gtest_filter=getEventTag*
    Bug: 31456426
    Change-Id: I5dacc5f84a24d52dae09cca5ee1a3a9f9207f06d

---
+++ logd/LogTags.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be709fa6fab7f3c4bc0cdc66c41f9f29bc8731f82e63a298e614246cd7ab1cc6
+size 30932
--- logd/LogUtils.h
+++ logd/LogUtils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e1a2a92c10535d536a7993cf87c2498ac5986e5ac8bd201e8b56b7bf35d6f30
-size 1828
+oid sha256:e450db5e26c90caa923ed74c08acbb2aae69a41f4765e51fa56c267192ef8235
+size 1845
