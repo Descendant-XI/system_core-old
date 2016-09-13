c1d376c3c5f826f7cefa21afba8ffab7cb66790c
Mark Salyzyn <salyzyn@google.com>
2016-09-13 15:48:35 -0700

    liblog: pmsg read permissions for AID_LOGD
    Test: gTest liblog-unit-test and logd-unit-tests
    Bug: 31456426
    Change-Id: I7a2fe8245ecd651883f7851ce71b6c45e373af82

--- liblog/pmsg_reader.c
+++ liblog/pmsg_reader.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b40599fe4c3e67a4f72afe63bbfb0a6aee8be2b9c8ec00bd87ea7091d18a8ae
-size 20151
+oid sha256:dbfe1493de0c4fb149cd6b3e95e1d96518b57af20289edfb86f11ef5ce45bf54
+size 20172
