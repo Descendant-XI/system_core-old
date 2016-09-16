50cfa56436db2f7c49f0a0eda07800d9cfc2f6b3
Colin Cross <ccross@android.com>
2016-09-15 18:04:04 -0700

    Fix warnings in system/ headers
    system/core/include is included in the global include path using
    -isystem, which hides all warnings.  Fix warnings in
    system/core/include/system in preparation for moving from -isystem to -I.
    
    - Fix invalid doxygen syntax in graphics.h
    - Use a pragma to hide a C99 extension warning on the flexible length
      array in graphics.h
    - Make static functions in radio.h static inline
    - Fix size_t printf and size_t vs. ssize_t comparision in qemu_pipe.h
    - Fix old style cast in window.h
    
    Test: m -j native
    Bug: 31492149
    Change-Id: I857f289e4c8e303494831873282bbb69de155c10

--- include/system/graphics.h
+++ include/system/graphics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d6ba407b66a8a5837932f42de78c353adc06d37a54613c3f6249b4a8f945e50
-size 51612
+oid sha256:1d20ef5eafc3e2f86942851f8ebfffc66877c79cdee4298bec6955960372d6d4
+size 51775
