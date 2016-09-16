3f9accb199985394c24bb98fd3bde27bae294c99
Tao Wu <lepton@google.com>
2016-09-16 13:01:24 -0700

    Fix bug: Doesn't respect ANDROID_ADB_SERVER_PORT
    BUG: 31549442
    Test: export ANDROID_ADB_SERVER_PORT=12345;killall adb;adb devices
    
    Change-Id: If2bfaf44c6567af16cae0d4def2f11be39c2d437
    Signed-off-by: Tao Wu <lepton@google.com>

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa75d26a0a2539059ac06ec9ab3f7c4e24d32efe796cb90d00443d90080ddca8
-size 83651
+oid sha256:308567fab6046206a42486ad406d8d140ad0f9c4e683ac156e1ee3f05ad1209c
+size 84060
