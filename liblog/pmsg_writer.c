078a67eddd1abdd7b805e03b325374b9fde214ee
Mark Salyzyn <salyzyn@google.com>
2016-09-20 16:14:20 +0000

    Revert "Restrict pmsg use to eng builds."
    This reverts commit f894b96a9211dcefb83b175720b53a2e49b8e463.
    
    Kernel binaries have incorporated the performance-enhanced pmsg driver
    that has removed the bounce buffer.
    
    Bug: 31057326
    Bug: 30375418
    Change-Id: Id6cec3ac144f57e8e5465aa48bbdc829cf29e283

--- liblog/pmsg_writer.c
+++ liblog/pmsg_writer.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92093ed1d82681e6c9f11ff39fcf74e45d6fe657eab76e619bd2c61d1c06a8c2
-size 8267
+oid sha256:508e8b327b5ab1c1412af8b20e2b55ae2eacffd286789e5f740c0834ca22e769
+size 7850
