b2fb31df92ecb8bfb96855676cd5977991398c92
Mark Salyzyn <salyzyn@google.com>
2016-09-21 09:24:13 -0700

    liblog: add android_lookupEventTagNum
    android_lookupEventTagNum added.  Adds support for creating a new
    log tag at runtime, registered to the logd service.
    
    Tested on Hikey, all services stopped, shell only access, CPUs not
    locked (there is enough repeatability on this platform).
    
    $ /data/nativetest64/liblog-benchmarks/liblog-benchmarks BM_lookupEventTagNum
    
                              iterations      ns/op
    Precharge: start
    Precharge: stop 231
    NB: only Tag matching, linear lookup (as reference, before unordered_map)
    BM_lookupEventTagNum         1000000       1017
    NB: unordered_map with full Tag & Format lookup, but with Tag hashing
    BM_lookupEventTagNum         2000000        683
    NB: with full Tag & Format hash and lookup for matching
    BM_lookupEventTagNum         2000000        814
    NB: only Tag matching (Hail Mary path)
    BM_lookupEventTagNum         5000000        471
    
    Because the database can now be dynamic, we added reader/writer locks
    which adds a 65ns (uncontended) premium on lookups, and switch to
    check for an allocation adds 25ns (either open code, or using
    string_view, no difference) which means our overall speed takes 90%
    as long as the requests did before we switched to unordered_map.
    Faster than before where we originally utilized binary lookup on
    static content, but not by much.  Dynamic updates that are not cached
    locally take the following times to acquire long path to logd to
    generate.
    
    BM_lookupEventTag           20000000         139
    BM_lookupEventTag_NOT       20000000         87
    BM_lookupEventFormat        20000000         139
    BM_lookupEventTagNum_logd_new   5000         335936
    BM_lookupEventTagNum_logd_existing 10000     249226
    
    The long path pickups are mitigated by the built-in caching, and
    the public mapping in /dev/event-log-tags.
    
    SideEffects: Event tags and signal handlers do not mix
    Test: liblog benchmarks
    Bug: 31456426
    Change-Id: I69e6489d899cf35cdccffcee0d8d7cad469ada0a

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ed3f515a7eb99fa55f1c453e87ad77935becac1f0e171f97b916bd309025684
-size 96119
+oid sha256:b3639134a285a737619b227deafd1239beb34c8d15e23153121f2160b5135f3b
+size 96680
--- logd/tests/logd_test.cpp
+++ logd/tests/logd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ab2f312f3d644c3b6c47c932c534449235f158ed70dd579883cfc64b176bfc9
-size 34792
+oid sha256:4f0b6ecfe6aedc895d81379c580a57ab90289492b7151ee8f0a1d53a6dcbdd06
+size 34815
