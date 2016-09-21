a03c0ff3aef2e25cc5abe5df92830aff99114ae6
Tao Wu <lepton@google.com>
2016-09-20 17:58:55 -0700

    Ignore setsid error in some cases.
    If the calling process has already been a leading process of session.
    setsid just fail with EPERM, ignore such error.
    
    Test: killall adb;exec 3>f;adb fork-server server --reply-fd 3 & cat f
    Change-Id: I1aeac079f29e10aa63ed724b5a43663f25c25ad5
    Signed-off-by: Tao Wu <lepton@google.com>

--- adb/client/main.cpp
+++ adb/client/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a650e2e0b60dd7d602e5bc3414e1d782295cf43770b293d0823530819c21d419
-size 5977
+oid sha256:06652b33ab8ac8be3db9d19f43487f7f681e369c4be2e7cf677f6f28b1af814b
+size 6116
