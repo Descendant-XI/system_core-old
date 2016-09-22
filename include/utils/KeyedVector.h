93ce1fb6a53a2eb0a45f9c2fad4df914a94172e1
Colin Cross <ccross@android.com>
2016-09-22 10:14:05 -0700

    Remove has_trivial_move trait from Vector<T>
    Vector<T> objects are a dynamic type with a vtable, which is not
    trivially moveable.
    
    Impact on aapt for frameworks-res is negligible, ~3%.
    
    Bug: 31595853
    Test: m -j framework-res
    Change-Id: I8a3269c35d95affb6e86d60588bcfca46db7f445

--- include/utils/KeyedVector.h
+++ include/utils/KeyedVector.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d68b26a35e2084575a5253706bcd3c843884c3eab02cc9b0f4b7b5ecb6a2dd8
-size 7265
+oid sha256:62410e5fdd071325e23e0274bf3cecdb4073fe447abdbcbbc8e15ae0de82062f
+size 6944
