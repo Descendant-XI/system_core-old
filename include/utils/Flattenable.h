736b2b77e517ba6f5169a07a04060cc924c06973
Colin Cross <ccross@android.com>
2016-09-26 14:24:48 -0700

    Fix more system/core/include warnings
    The warnings in these files were hidden by -isystem
    framework/native/include.
    
    Bug: 31752268
    Test: m -j
    Change-Id: I2a54376aea380ee24e6483fb7d35fdfe8991c490

--- include/utils/Flattenable.h
+++ include/utils/Flattenable.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79bdcf8aebc9de030f90cb703871872806a2aa1f1cf50fc52942a6a8e331f564
-size 6870
+oid sha256:50245e3bb420b566bdb9521433c8c415116347bae713e4c204fd6c8074284483
+size 6901
