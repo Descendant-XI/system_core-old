7771114c23ad6514e58b0c7cd9c537992df7b3a5
Josh Gao <jmgao@google.com>
2016-09-26 21:18:58 -0700

    Remove useless arguments from is_adb_interface.
    Test: mma
    Change-Id: I8b7b411d7d5ec9d401b61ed8f817b98c61114d4f

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cf9fd38523c9518d550aec69aa273ab94f737f2fdd6cb5f7bd75c45f2867144
-size 6587
+oid sha256:3b19d4c10ca1952bf277e38a52d823f2e69ef17257a6f26568841904ef2d3d7e
+size 6569
