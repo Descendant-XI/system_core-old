88893ba3f76a696f2ba608806eeb17d568fae0d7
Josh Gao <jmgao@google.com>
2016-09-27 12:35:55 -0700

    adb: fix IOKit handle leaks on OS X.
    Bug: https://code.google.com/p/android/issues/detail?id=219085
    Test: Removed sleep in RunLoopThread, compared before/after this patch
    Change-Id: Ia65bbedfb7d47bdb02bdfb69778784e5e855900b

--- adb/usb_osx.cpp
+++ adb/usb_osx.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2fa0605cd826782aecbc681b1039b46d28c05cd1d5b40248d4c7718567ea322
-size 18436
+oid sha256:578b6480b79f324000825e0bfb54972ae52ffc7cb04305e8f4ca854b7f27587a
+size 18630
