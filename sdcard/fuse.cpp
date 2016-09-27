927ea91922b763c7262f65ad1b130e75d99084c5
Andreas Gampe <agampe@google.com>
2016-09-27 08:35:45 -0700

    Sdcard: Do not use fuse padding field
    Instead default-initialize to 0. The third field, which is by
    default padding, may be renamed and used in some implementations.
    
    Test: m
    Change-Id: Iaf0a2b1bf9615f11256c0500fa3ca98b09e34d15

--- sdcard/fuse.cpp
+++ sdcard/fuse.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d2365924ba928651d42077c314bb448ad19f53bf84b1d46783269885030032d
-size 50822
+oid sha256:94d027de9bbb872d5310f0eac6c3e1c5da3a6596cbf8c5ebfcd2f20effc8d1e8
+size 50790
