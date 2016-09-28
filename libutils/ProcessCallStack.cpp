3cca4ad6a631cb9bb1f7a1def7dddf5a3a10f038
Elliott Hughes <enh@google.com>
2016-09-28 13:29:54 -0700

    Use readdir instead of readdir_r.
    http://elliotth.blogspot.com/2012/10/how-not-to-use-readdirr3.html
    
    Test: boots.
    Change-Id: If75532e24fe4d17743bf8e8c9590156dee378a63

--- libutils/ProcessCallStack.cpp
+++ libutils/ProcessCallStack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:779a847fd58f0aec4188bcb50fdb1daa2ab2df81a0ef644b933090e3049a4c7f
-size 7223
+oid sha256:504733fa11b64a2f90c01ed587464d6141b0c14c44980ae67d822eb900113645
+size 6968
