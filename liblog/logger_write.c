1c4f3d6f84ad9589bb66ef039072be082dbc3e81
Mark Salyzyn <salyzyn@google.com>
2016-09-28 09:58:56 -0700

    liblog: logcat: Replace log/logd.h with log/log.h
    Should use log/log.h and in some cases android/log.h instead. Can
    not remove file because still in use by partners, so log/logd.h
    points to log/log.h.
    
    Test: Compile
    Bug: 26552300
    Bug: 31289077
    Change-Id: I3580d46154617abb7231027a44f4ab9ee023febf

--- liblog/logger_write.c
+++ liblog/logger_write.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec21103def373b7da5f7a5e912db6f07a1b411cacc5c74f7977ed1dffacb8dc8
-size 17657
+oid sha256:f1e1a297cba8eef294cbccb8f4c8e86006ebd320b94ca7de498c98d5a9aead85
+size 17656
