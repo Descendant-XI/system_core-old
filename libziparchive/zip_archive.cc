3c0973205030803bd0606fdd4e4d16da823df4df
Tianjie Xu <xunchang@google.com>
2016-09-29 15:27:41 -0700

    Add functions in recovery/minzip to libziparchive
    Add two functions libziparchive that libminzip has. And create
    corresponding unit tests.
    1. Open a zip archive from a memory mapped region.
    2. A new writer that takes a call back function pointer.
    (Used by the OTA updater to stream the data.)
    
    Test: Unit tests passed
    Bug: 19472796
    Change-Id: I2b2daec71174afe221030357e39bff5faea51e72

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e09aa366281d6ab4a5a062a3c833da0834923cc4d8fdb6e0855c4a3ee8daa21
-size 36272
+oid sha256:31497ae5bf614f6742dc60ca06307c2d079e37017aba4bbe95a9b7b236f3c3f5
+size 39827
