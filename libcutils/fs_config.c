de3c528117c442dcc63437724bbcaf3ac715fe5d
Roshan Pius <rpius@google.com>
2016-10-04 11:32:30 -0700

    Rename 'wifi_legacy_hal' to 'android.hardware.wifi@1.0-service'
    Add netlink permissions for the new wifi HAL daemon name.
    
    Bug: 31821133
    Test: Compiled and ensured that the permission denials are no longer
    present in logs.
    
    Change-Id: If939df4760d9f7e85f0f134617d3a79030e09347

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e04a9b01996d0a209f8b69fb8fa0fa214b37a36b7f9a6d3ba6c9b5f80acb72b3
-size 11754
+oid sha256:4457c931ca14496b2c338447ab289ba2e57bda62bdf8b8c6774c80ff65304a8e
+size 11772
