7bcc93b04d8535191eb1b2f1070c8e4db56ee00c
Lingfeng Yang <lfy@google.com>
2016-10-06 12:22:55 -0700

    Fix adb flakiness on reboot
    bug: 31950237
    
    There are two lists of active ADB transports (devices),
    and with the emulator, they can go out of sync.
    
    This CL more conservatively checks if there are no
    transports in either list before commiting to
    register a new transport for the emulator.
    
    Change-Id: Id1201dc59c70825881dad80925c2e5bcc13dbd5e

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cf9fd38523c9518d550aec69aa273ab94f737f2fdd6cb5f7bd75c45f2867144
-size 6587
+oid sha256:41a14ebb3f3409596148856178d1af79a1b7dd1eca7b6d8bb2b603fe59f15aac
+size 6713
