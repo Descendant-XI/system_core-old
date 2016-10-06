5593b1638eb948158032e596e1423c950a205374
Josh Gao <jmgao@google.com>
2016-10-05 19:02:29 -0700

    adb: split up adb_auth.cpp.
    All of the functions in adb_auth.cpp were used in only one of
    adb/adbd. Split up them up into adb_auth_host.cpp and adbd_auth.cpp
    respectively.
    
    Bug: http://b/29273531
    Test: built and flashed bullhead, adb still works
    Change-Id: Ib610c5157522634cc273511175152f1306cc52a7

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b88dd6228bbfc27a45fc062576977768b37fd0e0bc0174184794e7a2c88c2db
-size 9458
+oid sha256:5752a5bfeb71da71cc2ecc59f5eb5026b48bacef9f5f4fa9726f12c665bf3061
+size 9433
