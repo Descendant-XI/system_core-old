ab7d209f08e8cf2bac1c62082ed935c908dd6fb5
Josh Gao <jmgao@google.com>
2016-10-07 16:42:05 -0700

    debuggerd: report crashes even when out of file descriptors.
    Use nasty clone hacks to let us close random file descriptors to be
    able to connect to debuggerd when the fd table is full.
    
    Bug: http://b/32013594
    Test: crasher exhaustfd-SIGSEGV
    Change-Id: I47772e9a5994da4473bd935b105d9c36827c017a

--- debuggerd/crasher.cpp
+++ debuggerd/crasher.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fcbd9896f5818191b3a5c3169e5f487646b855e8184f515e7701815acdca72
-size 7506
+oid sha256:1dcddc73f4212d87dee00a1e78b3d5c4b8b052aeab03014405a466928ffab627
+size 7892
--- debuggerd/debuggerd.cpp
+++ debuggerd/debuggerd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f729983e97f8a86538af43ec5110b8e24f4de2ca3ef9c4eeebafdc5945bc166c
-size 27485
+oid sha256:11c97019665b8bdb70ca5d213f3ef815863f6b02ffe86d4496116bd8af6216f4
+size 27608
