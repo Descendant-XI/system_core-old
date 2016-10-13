98ed6a06d2da919b24b6d91b143b7390091a81e5
Elliott Hughes <enh@google.com>
2016-10-13 16:51:46 -0700

    Remove socket_loopback_client.
    Bug: http://b/31537253
    Test: builds
    Change-Id: If02c8107206fb60e4622f8937bee0d0a362e1c7d

--- include/cutils/sockets.h
+++ include/cutils/sockets.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc440643e47ccafdf93b34aa5faab18d6c2950f9c9e602996d626de54adaaab
-size 5209
+oid sha256:db4ce161c7f4f2f36a0a1038bcd98e3ce2104ac9b0bf210d4f1cede970ef7503
+size 5161
--- libcutils/socket_loopback_client_unix.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:862027aeedbf610e30882247a1a874ee8ee5804fa902d30c4c165f3311685f86
-size 1126
