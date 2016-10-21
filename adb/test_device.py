77fdb46862a1cff02443c0917c13a87290a66213
Josh Gao <jmgao@google.com>
2016-10-21 13:17:32 -0700

    adb: fix test_sighup.
    Bug: http://b/32336914
    Test: python test_device.py
    Change-Id: I34ba5757b5e650d79327dc6779064acd850ce28e

--- adb/test_device.py
+++ adb/test_device.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11386d35af342e62da313a16537e09581362316394dedaa3e0443b425b94b9c5
-size 45502
+oid sha256:6a98ac3c881b496bd9d316cef9249ff678a05a679f4cdf05bf664e45b8b56b96
+size 45540
