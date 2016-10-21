6195a885d43900a255ace9a65c31a172c58889a8
Paul Lawrence <paullawrence@google.com>
2016-10-21 13:13:02 -0700

    Enable seccomp in init with generated policy
    Test: Ran script to test performance - https://b.corp.google.com/issues/32313202#comment3
          Saw no significant regression with this change on or off
          Removed chroot from SYSCALLS.TXT - chroot blocked
          Boot time appears reasonable
          Device boots with no SECCOMP blockings
          Measured per syscall time of 100ns
          Empirically counted <100,000 syscalls a second under heavy load
    
    Bug: 32313202
    Change-Id: Icfcfbcb72b2de1b38f1ad6a82e8ece3bd1c9e7ec

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b22d01083de237ac584ea2003a707db01fdd731ab02687eef900ffb0d8063f26
-size 2948
+oid sha256:f268a4e554449cbb99433825a15d5b1de3887e0ed702c6b6f987075c4e5ccf73
+size 2990
