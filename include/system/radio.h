07fd0de94e1ff0989acab29f42b47adc8706e88a
Eric Laurent <elaurent@google.com>
2016-10-25 11:05:06 -0700

    radio: fix compatibility for 64 bit processes
    Clarify use of 32 bit quantities in all internal and HAL structures
    to make sure that structures can be passed through binder calls between
    processes with different data unit sizes.
    
    NOTE: some structures still contain a pointer to metadata buffer: This pointer
    is always at the end of the structures and special care is taken in binder
    call implementations to cope with pointer size differences.
    
    Change-Id: I2023f70c3c62f6fa45f9eab3f09d18033f0efe4f

--- include/system/radio.h
+++ include/system/radio.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d0e42a4a89362fba90fa5ab4d381dc3912a31c43144dde008e8b6102abf6b6e
-size 10184
+oid sha256:239684a4060d5503c4083b1bc52f0b44273de63542c624b8ba35cd8cf64d3699
+size 10303
