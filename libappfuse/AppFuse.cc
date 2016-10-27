b0ee010f4ac24d810cdc2d41894531101a57530c
Daichi Hirono <hirono@google.com>
2016-10-27 14:57:55 +0900

    Add FuseBridgeLoop to libappfuse.
    The CL adds FuseBridgeLoop class to libappfuse, which is used in the
    system service to proxy fuse commands to applications.
    
    Bug: 29970149
    Test: libappfuse_test
    Change-Id: I0708f608b3868721ab16ba4028fd2c17a6735af7

--- libappfuse/AppFuse.cc
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:122d4f06125ed4a725844e293bcb23e0760f02e40d54b4be079fb012f398aa85
-size 4529
---
+++ libappfuse/FuseBridgeLoop.cc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0731bd0f9aa5335b83d55974fc23e72888c70556c52fbb69cd2f11ed182ddd66
+size 2130
