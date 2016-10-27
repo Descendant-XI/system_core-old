4ec748c9e28120b58cac3793b80c087e164bc99a
Nick Vaccaro <nvaccaro@google.com>
2016-10-26 19:33:48 -0700

    healthd: Show animation before waiting on timer
    Don't wait for first timer to expire before starting animation.
    
    Bug: 32368190
    Change-Id: If023e3a9f1dd0c3982c899bcf22e5335f9319f72

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d7771d8022a50387cc66081f16df52a1acf72629847fe1a0d32517297acf517
-size 11120
+oid sha256:aa94d7f744dd15cda43f4f828689bbb1d753649d14e9ffd1e11e12a56e63ce32
+size 11192
