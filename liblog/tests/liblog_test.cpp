37e5c27117c4007558563d4b2260b355f0a8b1f9
Mark Salyzyn <salyzyn@google.com>
2016-11-02 11:16:38 -0700

    liblog: test report pmsg not configured if ENOMEM return
    NB: bionic stdio is broken when providing appropriate errno value(s)
        on failure so libc.__pstore_append will not report pmsg
        misconfigured correctly on android for now.
    
    Test: misconfigured kernel, watch for this message in liblog-unit-tests
    Change-Id: Ifb1b550c7d3a3888000459c2f68c392cc23c379f

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d84b7bde9d65e967f332344baa2f34204757bb2b3fc1280b4f2a530090d86af
-size 92011
+oid sha256:9ca39cdd47809fd533c86f483b6a154a20298e86d8ffe1163985f943e64d62d2
+size 92376
