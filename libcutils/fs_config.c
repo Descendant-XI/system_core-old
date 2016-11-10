09fe262c5db4f000084ddf24d37571ed6d8921c5
Jin Qian <jinqian@google.com>
2016-11-10 11:56:44 -0800

    libcutils: add file permission for storaged
    Change-Id: I09b11db0a7a94b51b5b279b3e4ec58ea9f0c12d7

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5259bacb99182cf2edd9e4e59ee6867633f1558cfe2e160a7e7ac2856ad408f
-size 13261
+oid sha256:9f1a0cd6bc867fb27927ff4b18f8c0f1c88a328934b5840f398b1931f61d9ff2
+size 13548
