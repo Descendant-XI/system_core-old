ee9e172b8a6f41e2c1192dedc513fc1c20f95806
Daichi Hirono <hirono@google.com>
2016-11-10 09:41:54 +0900

    Use FUSE_COMPAT_22_INIT_OUT_SIZE always if available.
    We return the minor version number 15 to FUSE_INIT since we don't handle
    BATCH_FORGET. Thus the kernel does not accept the latest size of
    fuse_init_out. Instead we need to use FUSE_COMPAT_22_INIT_OUT_SIZE.
    
    Bug: 32779923
    Test: libappfuse_test
    
    Change-Id: I5c979d0e45344ca8adfe3ad3f4a9561442abcb3a

--- libappfuse/FuseBuffer.cc
+++ libappfuse/FuseBuffer.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:859a36cf51d6890674f78e054252bef6fba12e438c42ef08fae4c5e49ca319f2
-size 4532
+oid sha256:b75ede0e50fc07dbd3df0099a01d6f707946c88f4fddadb36cdccbfa04f54ec2
+size 4343
--- libappfuse/tests/FuseBufferTest.cc
+++ libappfuse/tests/FuseBufferTest.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc1aaecd107c12637090f0dfc857347e5f49219863a383d3761214241b3c103e
-size 5734
+oid sha256:ecbeb5e93ff17991aad2d0752b5a7cf002f009b323383183c4b676e4a8ca642b
+size 5741
