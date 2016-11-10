f327199908891e54aa911db45c261e5a29acf3e1
Mark Salyzyn <salyzyn@google.com>
2016-11-10 10:24:44 -0800

    liblog: logprint supports number of seconds time event field
    Add s to report time in seconds.  The time could be a period, duration
    or monotonic, expanded to seconds, minutes, hours and days.  gTest has
    to acquire a dynamic tag allocation as there are no users of this
    feature yet.
    
    Looking to the future, audio media logging has binary content similar
    to the binary events structures Android logging uses and they have
    a definition of a duration field in their internal binary logging, so
    may be of use when we unify the logs.
    
    Test: gTest logcat-unit-tests --gtest_filter=*.descriptive
    Bug: 31456426
    Change-Id: I262c03775983b3bc7b1b00227ce2bb2b0f357bec

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5262a252ce3438d574765edcd3302414d90f9446e4664cd6ee2e81d414de518
-size 51761
+oid sha256:995db054ddad52d5f2dd68447b24fcf6abe33814f9ff8dc3ef54724356285f15
+size 53426
