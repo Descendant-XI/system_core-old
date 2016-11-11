5b626fcf7ad4a383d27929f15dd05c03f04a0866
Mark Salyzyn <salyzyn@google.com>
2016-11-11 14:41:30 -0800

    liblog: logprint: report truncated event log contents if error
    We need to accept that a log tag can contain no payload.  For those
    that are corrupted, and to aid debugging, report what we did manage
    to interpret.  Report last character as a ! for corruption, and ^
    for truncation.  Fix a few Android Coding standard issues.
    
    Test: gTest logcat-unit-tests
    Bug: 32903864
    Change-Id: Id11bef3a7b6569305c51701dd66c45d2038d6628

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:071e8c36f8a7e82e0c8d3fde76b8b73f22518c1f5dc490640011c78fbc201f69
-size 39279
+oid sha256:73631007426ace498a1d29212d602cc27a04a188e4ff5a58f201e90af8b75ea8
+size 39637
