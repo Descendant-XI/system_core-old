933adb01ce3afbbe9b3c9fa58d0a76488644f2e4
Wei Wang <wvw@google.com>
2016-11-14 20:05:21 -0800

    init: fix BuildTriggersString for empty string
    behavior is undefined if pop_back() on empty std::string
    
    Test: grep init log
    Bug: 32712851
    Bug: 32838381
    Change-Id: I5bfac2fb275036abd0158b78df14019d2e82716d

--- init/action.cpp
+++ init/action.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:234f27acd75e5ba8443749187cd45e55b9a32dc1cdbcea3101586d517992fb03
-size 12915
+oid sha256:7217722990b09a0bb5dc0fd4cf642a87e3a731ca3bd211bddaaef6df353ac954
+size 12952
