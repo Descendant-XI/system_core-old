50d9d2b53b6216b7a4c0f42dc03a7b9d17c0f62a
Elliott Hughes <enh@google.com>
2016-11-14 17:08:47 -0800

    Switch fastboot/init/libprocessgroup to std::this_thread::sleep_for.
    Bug: http://b/32878766
    Test: boots
    Change-Id: Ie0ddfb7e60f2da5f6eefbb10c83a92e88c137ae3

--- fastboot/usb_linux.cpp
+++ fastboot/usb_linux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed37e963117e4ae5af3760f6bc6915f4544dda5ce0c440ac5eb670c357b8afd9
-size 14565
+oid sha256:65d86ff267abe96638385d44d9459de6ea41b6b023dc1887232bfcec1d60cbd2
+size 14682
--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90ae05a0023e742e88df86c3cf64953b2a652b91c004d15c24823962ae54acc7
-size 13106
+oid sha256:36a16ce7df4301567266258519e56a16f7eca0edaa513b73d35026b45c6b2ce1
+size 13145
