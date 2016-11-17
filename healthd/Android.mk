c766e540ff92e4d062e016ffd86f4a28af57b8db
Yifan Hong <elsk@google.com>
2016-11-17 13:00:55 -0800

    Split libhidl into base and transport.
    Bug: 32756130
    
    Test: mma
    Change-Id: I1b897d3984b0faf636d40b862b14872ce5d5aa9b

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb970952b7127d569f9b0056e2f83ab453c76d6717295749329b7cba5b72957c
-size 4628
+oid sha256:812bd75c93faceecd30223bb322fab60743283efa813dd04232c5cfbf5dacb20
+size 4655
