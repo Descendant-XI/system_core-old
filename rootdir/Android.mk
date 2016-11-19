85dd7f2e3e2f58fbe0602367df5bcb51582cbe29
Nick Kralevich <nnk@google.com>
2016-11-19 12:09:33 -0800

    change /data/bugreports to /bugreports
    It's 5 characters shorter, has no runtime costs, can be stored on a
    read-only partition, and avoids problems like b/27262109 and b/27204904.
    It allows makes some security hardening easier.
    
    (cherry picked from commit c4084c669495fd1b41ce8b6568c71b1f46d26969)
    
    Bug: 27262109
    Bug: 27204904
    Bug: 32799236
    Test: verified new symlink created and old one not present
    Change-Id: Ief362e13569ad9c868a7f0f9c4dbd6a328c96c6b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:306d6a44c621f063375e2d007febc1fa8f852837c52b3d7ba9b521888523b899
-size 3197
+oid sha256:0d49aa28304e9e10599210f4fd0196f3a531b6c91d9c53c39cc0cd96ffc6092f
+size 3292
