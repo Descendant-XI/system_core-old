513acd7a5cb44ecb29865d5e1a230f7cc65b7418
Andre Eisenbach <eisenbach@google.com>
2016-11-22 14:18:55 -0800

    Allow Bluetooth HAL to access /sys/class/rfkill
    Test: compiles; HAL can write rfkill state
    Change-Id: I5815a1f50ef8d09bf83541a6d2389b61bd007306

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:539af31a38291e6d2ed01a811730f049fd5564d2d996f4e9b383fda03d916aff
-size 13420
+oid sha256:3ea3569a09d41328479a65bc63b23cedf4f67189862e99eb5acb49de5b67fa4a
+size 13665
