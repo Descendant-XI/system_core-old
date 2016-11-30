8571db1c9d3b7ee0a8960bc39862e6b15880746c
Josh Gao <jmgao@google.com>
2016-11-30 11:21:22 -0800

    adb: move adb_strerror to sysdeps/win32/errno.cpp.
    Bug: none
    Test: mma
    Change-Id: I9df4d6faf9883a78f50ca6b2b7c35f095d06ae20

--- adb/sysdeps.h
+++ adb/sysdeps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98a061ac37b0caa31f3fbdf2f9351d026f041ee35e7b3169abb148e62afa6b91
-size 24066
+oid sha256:d341bfa7e1072555a52c359e43aa1cb0d1b2964bc4e1be7450d13f571dee7fd2
+size 24033
