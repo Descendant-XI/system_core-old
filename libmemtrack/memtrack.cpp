a26615ed704fd033b2bad72e313916273f50f99c
Yifan Hong <elsk@google.com>
2016-12-02 11:28:04 -0800

    Update to use the correct logging library.
    Test: compiles
    Change-Id: I0e51e7e7866d2d01f04a05c63c9e1d1846b51f71

--- libmemtrack/memtrack.cpp
+++ libmemtrack/memtrack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c54e66eea0d506e81b8bb48646d2fbd7c74b72d5c876188f75c72658f7c5abe8
-size 4961
+oid sha256:7ff3bfb2cffbddc9c2c99a19a9bd051a030a65e5a150d4fa25355176d419067f
+size 4987
