2303e0325440489c34b4bb738f773ae8bb70612e
Gustavo Padovan <gustavo.padovan@collabora.com>
2016-12-06 16:09:51 -0200

    libsync: tests: remove WaitOnDestroyedTimeline test
    The mainline Sync File implementation doesn't have wait ioctl anymore.
    Only poll is supported now, and we already have a test for that.
    
    Test: Sync unit tests still passes.
    Change-Id: Iadde7b2173024af9b8d20316e640297cf214c645

--- libsync/tests/sync_test.cpp
+++ libsync/tests/sync_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cef08433513ecbd49ba2d347236eba76fdcdfab8126dcd6884beb82334e8b097
-size 16735
+oid sha256:7c12be63b32ea04b5668dd3a1a3739681d3493987c37a59f4f1870a371a0c93a
+size 16110
