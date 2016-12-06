7833be0405119f8f6b345feeff456cfa65e38b61
Gustavo Padovan <gustavo.padovan@collabora.com>
2016-12-06 16:13:17 -0200

    libsync: tests: redefine PollOnDestroyedTimeline()
    On mainline if the sw_sync timeline is destroyed the fences doesn't not
    signal or error. So change the test to check if the fence is still there
    by polling the fence with timeout zero and asserting if it is not
    signalled.
    
    Test: Sync unit tests still passes.
    Change-Id: Icb8e629018eef35074ae91d0f29ed1f12e90492b

--- libsync/tests/sync_test.cpp
+++ libsync/tests/sync_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c12be63b32ea04b5668dd3a1a3739681d3493987c37a59f4f1870a371a0c93a
-size 16110
+oid sha256:b4ee6b609b644db06cbd88c9f6471d0656f510f60bd888e4c3876f2db134afb8
+size 16061
