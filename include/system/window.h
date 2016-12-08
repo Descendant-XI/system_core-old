4b0c9e56e1aaa7e35ac11e27c518113abdf0abce
Brian Anderson <brianderson@google.com>
2016-12-08 12:03:49 -0800

    Add NATIVE_WINDOW_GET_COMPOSITOR_TIMING
    Helps support eglGetCompositorTimingANDROID.
    
    Test: Manual testing with Chrome.
    Change-Id: I107cf14228dcde8da130bde6d477d0d0f1b38c5f

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8c18ac54d44ded97cb6b00c52ebad3dfb6a141ef944c1579510f2cab93dc63c
-size 40856
+oid sha256:944b68e2e7e4c8af5fd425acfe9efda9dd38ec4d1b4f08997ed0069144bb59f4
+size 41262
