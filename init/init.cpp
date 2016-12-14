c555bb393c38442a302379cd91d611b803eb050e
dcashman <dcashman@google.com>
2016-12-14 08:37:11 -0800

    Convert missed ERROR() to LOG(ERROR).
    commit: f86b5a6b90619e02d1d034ef7b0adc3b439f4abb converted init to
    libbase logging, but the internal resolution, commit:
    4dc40842ba310469b770f0e8e019e697777b0782 missed one.  Convert it.
    
    Test: builds.
    Change-Id: Ic190bd8ce232de0acadd980a349de95fa8aed8ad

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcc192240d19ca6679df892222722b3db9aea50e0a47da6a9540a43f241ec57a
-size 28867
+oid sha256:6a5c050c28edfaba5126de3acef0712cc7eafa4126dc950bc525cf8732010dc3
+size 28872
