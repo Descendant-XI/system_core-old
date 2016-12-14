444c0cf5550624c02823b5a83119a4dcf776105c
Romain Guy <romainguy@google.com>
2016-12-14 10:10:49 -0800

    Prevent conflict with deprecated pixel format constants
    Bug: 32984164
    Test: Compile
    Change-Id: Ia075c24740eeb33ca748e3423da156ffb9af0550

--- include/system/graphics-base.h
+++ include/system/graphics-base.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba7d66ef7e9b525ed8341a00978263dcf942e432112cbc327c8fbac7274558a
+oid sha256:9ffadebdab20ad4f85a3de457ed4dea411d4de20597328d76bbb53ef937f1b2a
 size 6453
