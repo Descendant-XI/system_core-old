92f94ed0e25ecc98d3e448121701db551258bcb0
Elliott Hughes <enh@google.com>
2016-12-16 14:45:17 -0800

    Move init bootcharting onto its own thread.
    This reduces the overhead when bootcharting is on (obviously), but also
    removes the "do we need to do anything for bootcharting?" check in cases
    where we're not bootcharting.
    
    Bug: http://b/23478578
    Bug: http://b/33450491
    Test: rebooted with bootcharting on/off
    Change-Id: Id8746b5023b17d7615679eba0bcd02aee048ef1a

--- init/bootchart.cpp
+++ init/bootchart.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8e0cfb9c0e9b7a83706e481e5d22c8501e9687a46ad2da101e3ad76de0e0a30
-size 6800
+oid sha256:60d72f7aeb2ce6ecc28543c6f7baf50a7ca5a1d1703fe6e83e19f8b74e24036f
+size 5992
