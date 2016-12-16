c99e58ea7d8f18ad5a47b1e703e65f850a9f0b07
Hung-ying Tyan <tyanh@google.com>
2016-12-16 15:00:05 +0800

    fs_mgr: add verify_dev flag to fs_mgr_setup_verity()
    Bug: 33682519
    Test: Tested on bullhead with https://android-review.googlesource.com/#/c/315463.
    Change-Id: Ie12230c6f77a2834c179e44b59817292d58726f7

--- fs_mgr/fs_mgr.c
+++ fs_mgr/fs_mgr.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6840996423080b65c80c6f7d4950029180b8228bda156d91dbac4c8ed85d4381
-size 37474
+oid sha256:963964fc3da4fad9c853ee267da164edfdc90075d6f74c41586f2132c6de1a36
+size 37493
