0a115039547397fae031fda394b3432e601b5eca
Mark Salyzyn <salyzyn@google.com>
2016-12-16 16:09:15 -0800

    logd: sum liblog tag messages
    As an extension to the duplicate multiple message filtering, special
    case liblog tagged event messages to be summed.  This solves the
    inefficient and confusing duplicate message report from the DOS attack
    detection such as:
    
    liblog: 2
    liblog: 2
    liblog: 2
    liblog: 2
    liblog: 3
    
    which would result in:
    
    liblog: 2
    chatty: ... expire 2 lines
    liblog: 2
    liblog: 3
    
    And instead sums them and turns them all into:
    
    liblog: 11
    
    liblog messages should never be subject to chatty conversion.
    
    Test: liblog-benchmarks manually check for coalesced liblog messages
          and make sure they do not turn into chatty messages.
          Instrumented code to capture sum intermediates to be sure.
    Bug: 33535908
    Change-Id: I3bf03c4bfa36071b578bcd6f62234b409a91184b

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2a7031aadf735bf954d4ac0dcf680cfdcdebac72a0941080a142bb7660c9142
-size 38674
+oid sha256:e2162e30245c5b9c5ee10484582f4526e0bdf5428d03cfed48db1b8c6b4b314a
+size 41406
