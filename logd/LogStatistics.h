4a2c93fdf71c795d621cb75eec05b25665c1be85
Mark Salyzyn <salyzyn@google.com>
2016-12-19 14:51:15 -0800

    logd: trailing spaces in log statistics (part deux)
    Test: manual
    Bug: 30118730
    Change-Id: Iafda6f95e1b1377649e01868c81c0056c116e01b

--- logd/LogStatistics.h
+++ logd/LogStatistics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4f8462fafb8cbbe10abdf0434c173d16503040b18d55f5ce8ae79c2747859f8
-size 18148
+oid sha256:82d9cb1ac833d181321202b751a6c17a3f7487ced2ff74c91d32a682bf940f2e
+size 18247
