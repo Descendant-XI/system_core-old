7aa5cdf598484456e998ae41f5e3d14b3010e671
Jorge Lucangeli Obes <jorgelo@google.com>
2016-12-20 16:54:04 -0500

    Add a sample service for testing init.
    I find myself using something like this every time I add functionality
    to init. I cannot possibly be the only one doing this. On the other
    hand, if this hasn't been added for so long, maybe there's a reason
    for that.
    
    The advantage of using a test service versus modifying an existing
    service is that the test service doesn't *require* any permissions or
    privileges, so you can add and/or remove whatever you need to test
    without breaking the service.
    
    I found it useful to have the service check its own /proc/<pid>/status
    from command-line arguments, so that's what the service does.
    
    This CL also adds a .clang-format file for init.
    
    Bug: None
    Test: Service runs and exits successfully.
    
    Change-Id: I3e7841a7283158e10c0bf55e0103c03902afb1f0

---
+++ init/test_service/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7adb368feeb86b4281151fd0e920a63a4ac444058b1cbecc578c39ea6813b802
+size 1245
---
+++ init/test_service/test_service.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e92c8528d637f5a104eb7b1194ddb8c91a40037d80c46fdc37a76967c9d18d2c
+size 2681
