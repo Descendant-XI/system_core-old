f6722b92b61455f3d8e89454f557b6c7ef2d3b84
bowgotsai <bowgotsai@google.com>
2017-01-11 22:21:38 +0800

    fs_mgr: support using libavb to enable dm-verity
    external/avb/libavb provides the new Android Verified Boot (AVB) flow.
    It has different verity metadata format than previous formats in
    fs_mgr_verity.cpp fs_mgr should support using libavb to read the metadata
    (a.k.a. HASHTREE descriptor in AVB) to enable dm-verity in kernel.
    
    Two important files in this commit:
    
     - fs_mgr_avb_ops.c: an implementation of struct AvbOps* for libavb to do
       platform dependent I/O operations, e.g., read_from_partition.
    
     - fs_mgr_avb.cpp: it reads the metadata (a.k.a. vbmeta images in AVB) from
       all partitions, verifies its integrity against the values of
       androidboot.vbmeta.{hash_alg, size, digest} passed from bootloader in
       kernel command line. Then enable dm-verity for partitions having the
       corresponding HASHTREE descriptor and with an 'avb' fstab flag.
    
    Bug: 31264231
    Test: Enable dm-verity on /system partition
    Test: Enable dm-verity with FEC on /system partition
    
    Change-Id: I4652806984fe5a30c61be0839135b5ca78323d38

---
+++ fs_mgr/fs_mgr_avb.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd096262a8e37fbb8b0a63cd2cfdb7eb50d1bc87602461994b9d5b2dd8938e85
+size 22033
---
+++ fs_mgr/fs_mgr_avb_ops.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc5cb09fe2120e30bcb07cb7e7a5b8ce388321ecfbe1f507b5d985a4e391e242
+size 7657
