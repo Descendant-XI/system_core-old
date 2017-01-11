f119583482d96e74810f5ca637f889c74f712086
Josh Gao <jmgao@google.com>
2017-01-11 14:39:19 -0800

    adb: remove support for legacy f_adb interface.
    Everything should be using the functionfs interface instead by now.
    
    Bug: http://b/34228376
    Test: grepping for f_adb, android_adb in source tree
    Test: m
    Change-Id: I6bc41049c49a867499832588dac8ed108c636c11

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8eb9c5e2e6d651c503f508af7493e2c94331eeab2ded345256a98041680cf7c
-size 7835
+oid sha256:a574054e8e9a02ffb7ea3c4a0ef11cb0d9956317acf347c0b3eb03da72c9345b
+size 7800
