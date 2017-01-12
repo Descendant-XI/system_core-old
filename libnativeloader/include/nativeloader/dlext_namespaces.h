5e2dbb8d2c484803bfe9ea2fc9d03ca03ccd268d
Jesse Hall <jessehall@google.com>
2017-01-12 14:56:17 -0800

    dlext_namespace.h: Declare android_get_LD_LIBRARY_PATH
    This allows clients creating their own namespace do so by modifying
    the default library path, rather than hardcoding it multiple places in
    the system.
    
    Bug: 33531483
    Test: build
    Change-Id: I321e219392b38c235b220986e1520b49a4669863

--- libnativeloader/include/nativeloader/dlext_namespaces.h
+++ libnativeloader/include/nativeloader/dlext_namespaces.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf89cfde0f3a22a663829eb36f50aa022fb913e78250901c898d2cf8be4b6abd
-size 4260
+oid sha256:491e69105f1800cf85a571825d17b1bf3d3923819c7859bc3f61b833ebeb5730
+size 4860
