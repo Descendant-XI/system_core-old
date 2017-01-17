d45d6b38873b1bb19d90e42669e8f469dc0622be
Jin Qian <jinqian@google.com>
2017-01-17 11:14:22 -0800

    storaged: increase periodic polling interval
    set default polling internal to 1 minute
    set default disk stats publish to 1 hour
    set default emmc info publish to 1 day
    
    Bug: 34331490
    Change-Id: Ifd86ddf9d826fb252de9dc8d6881e80ccb193e3b

--- storaged/include/storaged.h
+++ storaged/include/storaged.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dcef95add32715ef2007aae2712ac1f53dd55a09da52bbd9960e430c4403dfd
-size 10628
+oid sha256:dd285043b6dd2dff955c787e8caae854063ef93b54e9ca96b967226b13f65aa9
+size 10629
