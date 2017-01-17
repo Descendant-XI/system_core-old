d5b0b2d27244c6a8842f7975e6ebdae7ed1481c6
Jin Qian <jinqian@google.com>
2017-01-17 15:08:00 -0800

    storaged: fix crash when cmdline is longer than 64 characters
    Bug: 34331490
    Change-Id: I40302f4d54b7d8fe4fc5ecbc76df42df68a35504

--- storaged/include/storaged.h
+++ storaged/include/storaged.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd285043b6dd2dff955c787e8caae854063ef93b54e9ca96b967226b13f65aa9
-size 10629
+oid sha256:2f434251851f4951899d62ab5a821bef2ea893f9f62a415d9630499c324ed044
+size 10656
