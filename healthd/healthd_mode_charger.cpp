ece13801fc281cbf7247e512e35e71c6e2d71f43
Tao Bao <tbao@google.com>
2017-01-18 22:54:54 -0800

    healthd: Track the API change of minui/minui.h.
    ev_init() and ev_iterate_available_keys() now take std::function
    callback functions.
    
    Test: mmma system/core/healthd
    Change-Id: I8231b57ba816fcba8f698dd7145724021e235664

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:892f111f06194a663d065235fe69dcad775fd1d5f34545dee608cedda4cf17f2
-size 26228
+oid sha256:1c6bd5de07fde8a8cf6128c5804b01675061d3b1970101951f6f3148c07aa6c9
+size 26345
