750c159eeb83dc328536f555127855f473afdd07
Ian Elliott <ianelliott@google.com>
2017-01-20 13:18:05 -0700

    Plumb refresh cycle duration up to VK_GOOGLE_display_timing.
    Test: Manually tested with a modified cube demo, that reports the refresh
    duration returned from this extension.
    
    Change-Id: Ida765850e185f5bdf6570796534be005791a64a8

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19af360f30fd48a1eaabc2566e6c5d1988d9ff7f0dcc2c4df66e46505dd7d2c3
-size 40319
+oid sha256:ed571c7ecbe41e9329967f84357ad254f38ccd57cddf1ec2576c7f0e11dd087c
+size 40676
