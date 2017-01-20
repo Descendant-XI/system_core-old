b6b8258de10a06a321c8ca41c10a4e4ffd39a7d9
Dave Weinstein <olorin@google.com>
2017-01-19 18:03:34 -0800

    Move the kptr_restrict setting from init.rc to init.cpp.
    Also ensure that it uses the highest supported value, and
    abort if the value is not above a minimum threshold.
    
    Test: Tested against the curent kernel (maximum value of 2,
          set to 0 by the kernel initially) and against a
          modified kernel (maximum value of 4, set to 4 by the
          kernel initially)
    
    Bug: 30368199
    Change-Id: I608db577258b68b390ffe96f452e1f7c0bc9ad8a

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:874c665bd14713a169f734a19da87899e184a3eb2235c35c5dd32a1fd788641e
-size 28964
+oid sha256:e7e05d5218cfbd2e0b53ea44827e5633dd95ef123521f038df2c33b19cb8a11c
+size 29805
