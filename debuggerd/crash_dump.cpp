9a2504ade2c7a5b7faf67f0415e17c94fdae4c74
Josh Gao <jmgao@google.com>
2017-01-20 12:51:11 -0800

    crash_dump: don't abort if we fail to attach a sibling.
    A TOCTOU race can occur between listing threads and attaching them.
    Don't abort and leave the process in a stopped state when this happens.
    
    Bug: http://b/34472671
    Test: while true; do debuggerd -b `pidof audioserver`; done
    Change-Id: Ib1632c3423fddf506b5c7874223c82fada78a85e

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98fa9347a01dcd088f3f923a8d0862d73bb55c105377ffcdb7dc0a7a1ac2da74
-size 12874
+oid sha256:693463c4600fc19353a6baaabf19c37058264813fc7c70c21bca3ca337da4497
+size 12955
