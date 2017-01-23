6d502e99803f33304b6460309e5c2b6e815924cb
Chris Phoenix <cphoenix@google.com>
2017-01-23 14:00:27 -0800

    memtrack HAL uses "default" service name
    The getService() and registerAsService() methods of interface objects
    now have default parameters of "default" for the service name. HALs
    will not have to use any service name unless they want to register
    more than one service.
    
    Test: builds; verify HAL still works
    
    Bug: 33844934
    Change-Id: I6cf3f24500228985ddf581ad5a98886585a3fcfe

--- libmemtrack/memtrack.cpp
+++ libmemtrack/memtrack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc91bd29157834fc1a745c202a621a3ff8eb578f6de186cffe01ab2275e3eb2a
-size 4983
+oid sha256:829254f4b50d739495beac3dccca7ec258a9fde61c6db761115fb5f39f33bcc0
+size 4973
