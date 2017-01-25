09d02a1db72b8a2588e7a65cdd7003ea8c8a7d29
Connor O'Brien <connoro@google.com>
2017-01-24 17:50:39 -0800

    Add eraseblk and logicalblk fs_mgr flags
    When doing a factory reset in recovery, flash erase & logical block
    sizes are needed for tuning the parameters of the new userdata image.
    Add flags to allow this information to be stored in recovery.fstab
    
    Test: Factory reset marlin & confirm userdata is rebuilt correctly
    Bug: 33243520
    Change-Id: I6c4414d4e9026be4a3646523656ac151e93015e7
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7796346a307e5e8618ca3873938578c0e64d821e67e79dcf473f0108666611cf
-size 4583
+oid sha256:cd1a5047e36611e7d136c19b893ce412223e643d285e174f97ca3bccbffb8e2e
+size 4651
