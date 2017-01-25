bc7c45152f28ac43ffcfa523844eca364cad9bd6
Jin Qian <jinqian@google.com>
2017-01-24 16:23:13 -0800

    storaged: add dumpsys interface
    Write alerts to an internal buffer and flush to dumpsys
    when dump is called.
    
    Test: adb shell dumpsys storaged
    Bug: 33086174
    Bug: 34198239
    Change-Id: I39e5dfd9c4e9a5f3ec3d74356b28e9ddf77f6624

--- storaged/include/storaged_uid_monitor.h
+++ storaged/include/storaged_uid_monitor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7ce8257da949f03de1b73c3919ca77696bf0a6a3ea6ce6a5d183b02427c56d4
-size 1975
+oid sha256:25baffe8cdbb991e17278885afedf3cfa73b2c805b4308c731ed3825995c933f
+size 2296
