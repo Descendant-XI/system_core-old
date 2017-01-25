4093031d2ddd315cd12f620e1fe5564b3bf6f511
Courtney Goeltzenleuchter <courtneygo@google.com>
2017-01-25 09:14:21 -0700

    Add enums for 10bit color formats
    To improve wide-color behavior it's useful to have more bits
    per color component. This defines 10:10:10:2 and 10:10:10:X
    for that use.
    This can be used for any color space / dataspace that is
    compatible with unsigned integer components, e.g. Display-P3,
    BT2020, etc.
    Test: TBD
    Bug: 29940137
    
    Change-Id: Ieb8757e76b1d5bc45ce37c2473289eed7db66139

--- include/system/graphics-base.h
+++ include/system/graphics-base.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ab5e81b902a3e28cb80cbca1ac291d3252d14664e1fba890071364ababf77bf
-size 6682
+oid sha256:8ca8bfa4f22507c7c767d56d3c88e04a7543f69d8aaba1e0e5af3b95b75bdbea
+size 6730
