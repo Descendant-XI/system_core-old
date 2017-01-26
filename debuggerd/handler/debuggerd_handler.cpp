fa58aaa2c7f6dbf7592229038cc4ecf06c5e589d
Josh Gao <jmgao@google.com>
2017-01-26 15:03:29 -0800

    debuggerd_handler: remove PR_SET_DUMPABLE check.
    crash_dump has CAP_SYS_PTRACE and this was never obeyed by debuggerd.
    
    Change-Id: Ifee5e94b97b1f6440ad0be79758f0db2d2aaba2e

--- debuggerd/handler/debuggerd_handler.cpp
+++ debuggerd/handler/debuggerd_handler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:201d897331554da63465fd240112e1ae311b63d501eb5c1ea68aa0304984f985
-size 14127
+oid sha256:bae714220f2488b2c0d3778333b15d3b81673a9e7abf1daae0379d34fb4ed384
+size 13799
