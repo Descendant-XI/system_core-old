c50982b7916db29521c2fb76a7640c14f1a4c5c5
Elliott Hughes <enh@google.com>
2017-01-26 17:31:40 -0800

    Support arbitrary-length properties in property_list.
    Bug: http://b/33926793
    Test: visual inspection
    Change-Id: Ida84c3aaafa5411a9886f8d3d45679fd60ddc00d

--- libcutils/properties.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af066af78e4e116218f7ab532b77cc3d3af440e2c22ea6e178d8e4c18f65f9a2
-size 4516
---
+++ libcutils/properties.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29e6657cb3e6a6e23e9037f1d84fd79f32074b25922e18e854b7c0f74edf4d82
+size 4516
