1cd1e1775c23690b9b28240429c241b1878bf260
Paul Lawrence <paullawrence@google.com>
2017-01-31 14:49:50 -0800

    Expand seccomp whitelist
    Bug: 34817266
    Test: Boots, app starts
    Change-Id: I39026064ec6c7130256ae463163c1f7634e3bdd4

--- init/seccomp.cpp
+++ init/seccomp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a17b347c88d4a3ab5b1749cce750450eca3f91ba126a79343cbc65767966fd68
-size 6897
+oid sha256:313fe8dcbd87cccdb6d98cd2e8326001477f3cb7a7036b035447a973088ae2f8
+size 6961
