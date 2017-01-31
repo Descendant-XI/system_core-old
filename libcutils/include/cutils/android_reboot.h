d1a61f947c676bbd593a3a51be94a72f5bcfd040
Todd Poynor <toddpoynor@google.com>
2017-01-30 17:28:55 -0800

    libcutils: android_reboot: save reboot reason in file
    Save a string identifying the reason for last Android reboot or power
    off in file /data/misc/recovery/last_reboot_reason .  This file may
    be used for informing users of reboot or shutdown reasons at next
    boot, and for other diagnostic purposes.
    
    Bug: 30994946
    Test: Manual: reboot, setprop sys.powerctl
    Change-Id: I01e44473fdd21b33e9e4dced77aba9a66b6d3755

--- libcutils/include/cutils/android_reboot.h
+++ libcutils/include/cutils/android_reboot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c26a548d2e1cf3d79ff8a497395b46efce09e1398cd165ef6b612e5eaec074
-size 1196
+oid sha256:957bdb8b4ecb517f2d87623ccef2f6c1fe4bfd02544d954b1a86e02c3daf2551
+size 1318
