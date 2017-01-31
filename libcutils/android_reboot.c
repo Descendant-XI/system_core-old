6e4da68b6e22e632ff0b4ca60260d01af0481288
Todd Poynor <toddpoynor@google.com>
2017-01-30 17:27:39 -0800

    libcutils: android_reboot: add thermal shutdown command
    Add ANDROID_RB_THERMOFF command to android_reboot(), denoting a power
    off due to thermal limits exceeded.  When this command is used, Android
    may provide notification to the user about the potentially unexpected
    power off at next boot.
    
    Bug: 30994946
    Test: Manual: setprop sys.powerctl thermal-shutdown
    Change-Id: If2cde1b6a3c281a5750b50295412a4801ed5b2f2

--- libcutils/android_reboot.c
+++ libcutils/android_reboot.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6d002f2dc22891146dc14220caf837bd04614c93174d1f17d70e562f98bc0b6
-size 6822
+oid sha256:7b43fd3879fa394f637f73020dddd59316828505e1cfee066ef07f7d90b3ebd5
+size 6916
