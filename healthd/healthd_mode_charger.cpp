b18bcc4cd13334b2d7f6c490685bf5d5ab1be937
Todd Poynor <toddpoynor@google.com>
2017-01-31 15:51:50 -0800

    healthd: charger mode use reboot(), not android_reboot()
    Preserve last Android reboot reason as recorded by android_reboot()
    prior to entering charger mode.  For example, a thermal shutdown that
    reboots to charger mode leaves that reason in place, for user
    notification purposes at next Android startup.
    
    Bug: 30994946
    Test: manual
    Change-Id: I003f57bfff29a471bb647d2cf6b0986ff428985a

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d992075ca68ce805a1ec855f7b0d8bd878a6aa341b0a2de276389024db0af701
-size 26424
+oid sha256:b077911b5ad85363ae3716911522cce7bb76d3b4d64acc13f1383badbd9998a4
+size 26371
