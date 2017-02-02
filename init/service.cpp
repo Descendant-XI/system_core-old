65f63945f69e209a796966e09823206c4fafe619
Wei Wang <wvw@google.com>
2017-02-02 10:52:39 -0800

    init: clean up exec command
    Add wait time log for exec service execution time
    Fix memory leak when exec service failed to start
    
    Test: on marlin
    Bug: 34518530
    Change-Id: I01736bd9b1429414f3dc91dd5d02d88a681f0985

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:651c5af8dd5ab382124cfd1cdc26d60a4c816d6c8606984fcdeee63047a4f57f
-size 33857
+oid sha256:0cd38d31bb602502df28ef77f184f745f6f433608150aed14127279716a3e1f4
+size 33856
