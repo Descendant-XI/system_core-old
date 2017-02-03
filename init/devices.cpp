7d0c0e5dbe9ac647eeaa6361dce84613642f4b17
Sandeep Patil <sspatil@google.com>
2017-02-03 07:51:55 -0800

    ueventd: make selinux labeling optional for device creation
    This is to setup a way for us to run coldboot during init first stage
    and also at ueventd startup. We do not have all of the file context
    during the first stage, so the "early" coldboot needs to proceed without
    labelling the device nodes. However, the follow up in ueventd must label
    these nodes. This change allows us to do both.
    
    b/27805372
    
    Test: Boot angler successfully and compare do before/after comparison of
          the output of 'ls -AclpqRZ /dev' to ensure there are no differences.
    
    Change-Id: I5e88bd7da8a1d2cc41e3abba30dda463ecbde32e
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:144488944c64e4b9bf2c0b76d974373551e88f90b7abad7f182fe432d1205dda
-size 30538
+oid sha256:eddc899483fa2f1331978591b563d3cc7e8a75206a3600ada1dea2d4882d79c2
+size 30632
