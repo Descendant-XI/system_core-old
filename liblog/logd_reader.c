1353ee1e7653a9cd8b62b794252def4a916b5898
Evan Ralston <eralston@amazon.com>
2017-02-06 11:44:21 -0800

    Check against LOG_ID_MAX instead of LOG_ID_KERNEL
    Logd currently checks against LOG_ID_KERNEL to determine if a given
    log_id is available. While this matches with the comments in log.h to
    always keep LOG_ID_KERNEL at the end it does not match with other
    checks that Logd makes causing inconsistent behavior. This
    inconsistency is noticable on devices with log buffers with id's
    greater than LOG_ID_KERNEL and liblog reports that logd is unavailable
    while logd would accept the request.
    
    Test: compiles. no functional change
    
    Change-Id: I0302d9e3f2657706fdf778f1e78e5604abf566e2

--- liblog/logd_reader.c
+++ liblog/logd_reader.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:885274e4aa3e89b1c26a447a1e70663a77a7b9bed16e18aea83066b6757f99d7
-size 18333
+oid sha256:4ed505e36592e731a3b05f625bec7ad93a0d514190181d0e7cc453a7b6cf2cf0
+size 18357
