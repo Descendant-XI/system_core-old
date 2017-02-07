96ec64543bef59724a05e6950ec586a793cabe0a
Josh Gao <jmgao@google.com>
2017-02-07 13:31:25 -0800

    debuggerd_test: improve error when crasher fails to exec.
    Bug: http://b/35100742
    Test: rm /system/bin/crasher && /data/nativetest/debugerd_test/debuggerd_test32
    Change-Id: I02faec3b7f7ef62bb8a2ac2af730506e3d28e03e

--- debuggerd/debuggerd_test.cpp
+++ debuggerd/debuggerd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:169d309f6ee86b4605a23dcec4687ab832155ffd45ba71687592fb43097d46a3
-size 12608
+oid sha256:19692c4e3c742f787576cfed2c526ffe97ccc4544dc4192bbe370002112bf2fe
+size 12707
