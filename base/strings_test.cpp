a2fc7e753231d021adb898c8c82355481a5d679d
Elliott Hughes <enh@google.com>
2017-02-07 15:30:24 -0800

    Fix ubsan failure in android::base::Split.
    ubsan doesn't like us incrementing std::string::npos, even if we won't use
    the result.
    
    Bug: http://b/28729303
    Test: ran tests
    Change-Id: I8227eca57dc6f3e49626c7025514caa04ef18f0a

--- base/strings_test.cpp
+++ base/strings_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15e54e558c7c021dfc41a03771ffa1021dc06d335b529993d648125d3eaf14b6
-size 8642
+oid sha256:b7e37eb80873dc01eeba76d8773b0ddbf1f603e72b56799f22f68d7093a6430c
+size 8719
