5509776ead8bdf325eadc08578207498cb117ae7
Sandeep Patil <sspatil@google.com>
2017-02-07 18:11:37 -0800

    init: refactor: add support for doing early coldboot
    We don't want to spend time creating devices that are unncessesary
    during early (init first-stage) mount. So, refactor the devices code
    tha allows us to call into coldboot and has the
    
    - ability to only create devices that are specified by the caller
    - ability to stop coldboot cycle when all devices that the caller is
    interested in
    - ability to run coldboot for a specific syspath
    - ability to run ueventd code unmodified
    
    Test: Tested boot on angler, sailfish
    
    Change-Id: Id8f3492380696760414eadc20d624d300c904f8e
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eddc899483fa2f1331978591b563d3cc7e8a75206a3600ada1dea2d4882d79c2
-size 30632
+oid sha256:e9678dfe692b20d8dc4900d6e1b88f4bd6fe3829b905cb3c5e3e2e351022f6cf
+size 31981
--- init/devices.h
+++ init/devices.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:586eacd96adc77f52f91c4a0cb25c603560dec01f8d63f89b1379eb2dbe7dba0
-size 1270
+oid sha256:03bb068679199205f5b19e393862d05e228ce209474b2bbb977f7cd4b6d3856c
+size 2130
