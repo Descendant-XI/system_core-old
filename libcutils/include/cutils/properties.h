a44bfad9f8f890825989323fe970f2cd6f1e60c0
George Burgess IV <gbiv@google.com>
2017-02-08 23:57:59 -0800

    cutils: Add clang FORTIFY support
    Bug: 32073964
    Test: Builds on angler, bullhead, marlin, sailfish. Bionic CTS tests
    pass on Angler and Bullhead.
    
    Change-Id: I3d7f8ff73f92b1deb1e07f08282098e71e2d03a9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- libcutils/include/cutils/properties.h
+++ libcutils/include/cutils/properties.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d828ef922ab457b9165d32c3f045970fdcaef3eafc4f2d6371ebd06a47369952
-size 4756
+oid sha256:8ab4db76cffe1684b5758ece31e2d546d6893bb00b863de05627d7b4bcdca510
+size 6024
