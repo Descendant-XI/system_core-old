e1d5f51adacc9b4e9ef85e5e292f6218730320b2
Neil Fuller <nfuller@google.com>
2017-02-09 13:31:57 +0000

    Rename "Bundle" to "Distro"
    Renaming "Bundle" to "Distro" because bundle is confusing
    given android.os.Bundle.
    
    Bug: 31008728
    Test: mmm tzdatacheck
    Change-Id: I70408678d67eed9f9e0424c0791e856c909837be

--- tzdatacheck/tzdatacheck.cpp
+++ tzdatacheck/tzdatacheck.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f3a3d19151b50d7ad6b9b7038713aa86e172cfdbb31cb80c7a425a17b284e65
+oid sha256:88ddfa3a18d30ca392e33d1a958ca7a67c55424912eda1fadcb84646ea7f1652
 size 16301
