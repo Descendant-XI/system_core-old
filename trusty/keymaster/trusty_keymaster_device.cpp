d761231fdd15868d23f45ca1f0e0c5fa525fc924
Jocelyn Bohr <bohr@google.com>
2017-02-09 17:29:59 -0800

    trusty: keymaster: Implement export_key
    Test: builds
    Change-Id: I47d1ed4144014fca47fdf67f9f4d2eb0d9b1eb8f

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be040a48ee02d35780d7ef88929417dcd456c563c50a311462e686ab2789fd6b
-size 20608
+oid sha256:e1b4084e120c862861aab38da7b95fd4f099926919b294d7542e52fabfbe655c
+size 21525
