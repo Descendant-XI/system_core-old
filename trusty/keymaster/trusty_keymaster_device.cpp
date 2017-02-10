9deb9843fd4e3fa49dc26824e178203d73364d8d
Jocelyn Bohr <bohr@google.com>
2017-02-09 17:16:51 -0800

    trusty: keymaster: Implement generate_key
    Test: builds
    Change-Id: Ib77e29709eed2e5f0cd16ed214db61f4257fcebd

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7238d0630157172f2df10d194b9ced768f8a4be48b69ece143ba912fad20dbdd
-size 16895
+oid sha256:bc271b0c5af1198ee8624dc5bcedb3da5e1d296e085b42aec299dd311acdd148
+size 18311
