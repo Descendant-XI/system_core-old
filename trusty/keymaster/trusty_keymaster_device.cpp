cff9a19a5698074f4fb11a908dc6a2dc4c10592c
Jocelyn Bohr <bohr@google.com>
2017-02-09 17:44:48 -0800

    trusty: keymaster: Implement finish
    Test: builds
    Change-Id: I8505f428613176eea5373a459bbce0de17406c55

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4631dd94d267e7dc4164583bff872e63352e2c225529cef0719c0687b377e9d4
-size 26879
+oid sha256:e08defbb57c977cd03d79480fda717cda5e2420edfceadfaca84c4fc3a671786
+size 28420
