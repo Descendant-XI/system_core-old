12d5607f425b641430c61a48af6b0eb608a67bca
Sandeep Patil <sspatil@google.com>
2017-02-09 19:19:31 -0800

    init: fstab: add support to read fstab entries from device tree
    for early mount, we need a way to tell init where to find vendor,
    odm partitions (also system in case of non-A/B devices). Also, that
    needs to be independent of kernel cmdline since the cmdline will likely
    exceed its limit.
    
    The change adds support for parse and create fstab entries that can be
    directly sent to the fs_mgr for mounting partitions early in init first
    stage.
    
    Sample DT entry to mount vendor partition early on angler-
    
    firmware {
        android {
            compatible = "android,firmware";
            fstab {
                compatible = "android,fstab";
                vendor {
                    compatible = "android,vendor";
                    dev = "/dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor";
                    type = "ext4";
                    mnt_flags = "ro,barrier=1,inode_readahead_blks=8";
                    fsmgr_flags = "wait";
                };
            };
        };
    };
    
    b/27805372
    
    Test: Boot angler and sailfish with early "vendor" partition mount by
    adding aforementioned DT node and enable CONFIG_PROC_DEVICETREE in kernel
    
    Change-Id: I669013e3fdb157e88719436534f63989dec95d60
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93588358221c668a83c8c69b1feadd2b9c1db3b865aa6fa6d68b0b6f0667d640
-size 32420
+oid sha256:2068c5eae0dde3fbf693788ebb789cac2521621f962d5ab079f36934f1f4f091
+size 34974
