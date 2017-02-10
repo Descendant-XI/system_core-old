b331f98e75cc791135c587fa7d0b8e1f9b24fd89
Jocelyn Bohr <bohr@google.com>
2017-02-09 17:32:47 -0800

    trusty: keymaster: Implement attest_key
    Test: builds
    Change-Id: Ic5bf59db43b4301cbc2fa216470b9f07de8336b0

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1b4084e120c862861aab38da7b95fd4f099926919b294d7542e52fabfbe655c
-size 21525
+oid sha256:20e5bb5c3c755ac03472b5d93898b9f0465fe9ffca0330040d8909d7b334547b
+size 23597
