707efe636f9b011c7689e9520f39c59034af0923
Jocelyn Bohr <bohr@google.com>
2017-02-09 17:40:47 -0800

    trusty: keymaster: Implement begin
    Test: builds
    Change-Id: Icb5470a8c95131ee3d68ab2ce41423302b9ed531

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5322f8434fc99515c2ecaf02c1995f52c1667574140c96c66b502ef65c0c6766
-size 24486
+oid sha256:39e7e1580df336614c5aed323f581f3a7cdda6097bf9d82f37e7bd45f771d1bb
+size 25368
