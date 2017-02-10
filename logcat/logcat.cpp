5393451f76c695950d153f125e0068b131fec3fa
Mark Salyzyn <salyzyn@google.com>
2017-02-10 13:09:07 -0800

    logcat: Use std::string instead of large static buffer for -Q
    Test: gTest logcat-unit-tests
    Bug: 35326290
    Change-Id: Ieed3223be2d1386897cfba5fa4d04549e72e4e50

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98a4116abc64027d9660319c18d8c302e97f96b08ff333bb15e201b3fbcea6ed
-size 48280
+oid sha256:3d5eeeceae0555fa60c2758aa9bccbaa2bad0c03ef81899af4a1aa5c85a58074
+size 47588
