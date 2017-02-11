8ac5312517aad7a96475ee24a8be9681f74b3311
Jerry Zhang <zhangjerry@google.com>
2017-02-10 17:43:00 -0800

    adb: Add bMaxBurst to superspeed descriptors
    This gives a large boost to speed on usb 3.0.
    
    Test: adb pull/push multi GB files
    Bug: 31722483
    Change-Id: Iea039f1aba8b2e4d7e4a2ecb504cccb5dd1e4629

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:354586a67260ef11a571ad94f2d5a99fec80794e235f1886015e6f1809eb658b
-size 14655
+oid sha256:c7f138fd44aad240b6e6026cc92856085da9db59fc7320b4a4cad18e1526f50d
+size 14703
