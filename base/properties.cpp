5f86342e11747b9067e15e6901450689bb61957b
Elliott Hughes <enh@google.com>
2017-02-10 19:02:51 -0800

    Implement android::base::WaitForProperty.
    Also adapt libcutils to the bionic change that was necessary for this.
    
    Bug: http://b/35201172
    Test: ran tests
    Change-Id: I72a98b70b03d23e958b46778b505fbd5c86c32ae

--- base/properties.cpp
+++ base/properties.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dd9e56f3f1788497ef44e5e22ba2ce974d9987e9ce1e9f5a98bc1c197b22c9e
-size 3006
+oid sha256:942d9c9acf526e77e467d2fdc13c8adde819262408833d7c71ebf402c833393c
+size 4311
