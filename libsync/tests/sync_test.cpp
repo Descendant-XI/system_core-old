205d2bf81f880ca8c85124c6302f9ee55035e534
Jesse Hall <jessehall@google.com>
2017-02-12 16:32:39 -0800

    sync: implement sync_file_info
    Also modifies sync-unit-test to use sync_file_info instead of the
    deprecated sync_fence_info, but check that they match in several tests.
    
    Bug: 35138793
    Test: sync-unit-tests on bullhead
    Change-Id: Ic672d1c89798435a8b71469500e82c770a66bf4d

--- libsync/tests/sync_test.cpp
+++ libsync/tests/sync_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e008a57a9d64cef82e6e3a73c5b78a0fda7efdfc668298ff0f337c3a76a1ef
-size 16072
+oid sha256:c50ccc1cf951b9b0b8d0f910a43ec65183fda615c5f3f37c285f2218281d89be
+size 17557
