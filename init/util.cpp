0746d5b5d106cea7a15939b16dc7632fd9746092
James Hawkins <jhawkins@google.com>
2017-02-13 15:47:21 -0800

    bootstat: Refactor init/utils/boot_clock into base/chrono_utils.
    Use this for bootstat and init. This replaces the custom uptime parser in
    bootstat.
    
    This is a reland of aosp/332854 with a fix for Darwin.
    
    Bug: 34352037
    Test: chrono_utils_test
    Change-Id: Ib2567d8df0e460ab59753ac1c053dd7f9f1008a7

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b26d717b9e789c2520e8ea0e3a33b2c582620b17a53ec79763623b38c2c0e6f
-size 11699
+oid sha256:0df143e98775513695789e500726d17e206277b142ed9eed6bbff653f37f4bdc
+size 11870
