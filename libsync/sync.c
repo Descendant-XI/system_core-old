d1422aa4db27d04706d0e01b56412fcec56cf1b0
Jesse Hall <jessehall@google.com>
2017-02-12 16:17:22 -0800

    sync: refactor sync_fence_info
    Split the sync_fence_info implementation into multiple functions. This
    clarifies the logic, and allows the parts to be reused in the upcoming
    sync_file_info function.
    
    Test: sync-unit-tests on bullhead
    Change-Id: I0ea37067dddf41b831670f08eb99e0b7fd52adce

--- libsync/sync.c
+++ libsync/sync.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ee1a03697582d0da564146f14dbb8e1b4467b67f598ad1cccdf608594cf17e2
-size 5631
+oid sha256:bde21930078cf6a864d6455950b4c9143886eb5b13969ecdbe00e4aea9f54563
+size 6268
