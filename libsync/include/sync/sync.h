ff71de09503b7822f8760b18dbc86badff6043ec
Jesse Hall <jessehall@google.com>
2017-02-12 16:42:11 -0800

    sync: move declarations from sync/sync.h to sync.c
    None of these should be needed or used outside of sync.c.
    
    Test: lunch bullhead-eng ; m
    Change-Id: I548a499d0e2ada2ab4987e63a8116327c3be0076

--- libsync/include/sync/sync.h
+++ libsync/include/sync/sync.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d11a41d2bce7e660da493ed9dfe4c8d44ffc5cad35d89edff604017620e7d057
-size 3447
+oid sha256:30cf6c74e7d63483fa3a9a54b3bb6a35db9d6caf443e7b123db29a2fd1b80a38
+size 1839
