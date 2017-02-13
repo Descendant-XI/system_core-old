44a2bdbffdf23d6d1e9740b749426109336f2fec
Josh Gao <jmgao@google.com>
2017-02-13 14:46:19 -0800

    debuggerd_handler: set PR_SET_DUMPABLE before running crash_dump.
    Set and restore PR_SET_DUMPABLE when performing a dump, so that
    processes that have it implicitly cleared (e.g. services that acquire
    filesystem capabilities) still get crash dumps.
    
    Bug: http://b/35174939
    Test: debuggerd -b `pidof surfaceflinger`
    Change-Id: Ife933c10086e546726dec12a7efa3f9cedfeea60

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d2f5880af36173df01fe7be0ff8ee1c505a01fa0e7acadab2374b8f2d0bfa23
-size 14292
+oid sha256:0a79ef1cc3d0a1c0a74d183c20f9dad5a8cca1d03bf8baee0dd2e32de1216dc5
+size 14634
