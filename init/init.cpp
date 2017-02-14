b0c81de6b8a03f079035c3f52674d733fd94d5c7
bowgotsai <bowgotsai@google.com>
2017-02-14 22:06:20 +0800

    Set libavb version into system property for Treble OTA
    Treble allows framework-only (system.img) OTA. To libavb statically
    linked in init/fs_mgr cannot parse the AVB metadata of system.img, add
    this property for the Treble OTA match process.
    
    Bug: 35236019
    Test: Normal boots, use 'adb shell getprop' to check the property is set.
    Test: Recovery boots, press 'Mount system' and use 'adb shell getprop'
          to check the property is set
    
    Change-Id: I552be229c4efd45088d93252ac67176606f75d4d

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edeb552156c5e2612e4c119c6a2be4d23016466b082d86f53df43fdb551832fa
-size 35360
+oid sha256:15dfe4fd67e0728aac24a8c9550a988f6bebf57856c71ef0a44e977c64bf275a
+size 35557
