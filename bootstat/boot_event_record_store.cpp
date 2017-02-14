3b16c3f7296a666cc42424b6591843a5bcb7d27d
James Hawkins <jhawkins@google.com>
2017-02-14 19:20:20 +0000

    Revert "bootstat: Refactor init/utils/boot_clock into base/chrono_utils."
    This reverts commit 7c92e484503f239000ef97ef5b067907fbeaa4a6.
    
    Mac sdk still broken (despite testing locally).
    
    Change-Id: I7d9206e15997cd0efe081bd3fa17d53d2b20ec32

--- bootstat/boot_event_record_store.cpp
+++ bootstat/boot_event_record_store.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82cce61f4c65f1dbf76090d18be5453b54eec225ad1ed2fbca088b107696f365
-size 4578
+oid sha256:8a62a5084a76594069ec41678a31344013e2b68dfb793b4b5a1013858885c22d
+size 4429
--- init/service.h
+++ init/service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e62e124ef860004311791c5b30bd4f874a8ba7db8c040ef78b17598d693abc9
-size 8373
+oid sha256:b401e7b571d3ce43d40536f2e88c750e0bb36c2c0e5d2e719adbe05e4928f7f1
+size 8303
