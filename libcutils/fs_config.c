83755e3d343129de50438e273caf652f71b74484
Po-Chien Hsueh <pchsueh@google.com>
2017-02-14 09:39:45 +0800

    fs_config: Move hostapd to vendor partition
    Move hostapd to /vendor/bin/ because it's only used by WIFI HAL.
    
    Bug: 34236942
    Bug: 34237659
    Test: Hotspot works fine. Integration test.
    
    Change-Id: I8d9f51ed85a0614bf0141461dabeddae094ad4e0

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f07efccbf044cfa0e1a72a24d941cd8eedeab7fdacb955a876feb7ac71f0cc9c
+oid sha256:62a3301d8945ae388e7034330743a67928dd4d96454c3b2f265f58726e9ff96b
 size 14200
