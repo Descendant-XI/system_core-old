2a526c0ed472c26890e50ab3ad90332f6064ff70
Sandeep Patil <sspatil@google.com>
2017-02-15 15:36:25 -0800

    fs_mgr: make fs_mgr_setup_verity public API
    Consequently this removes the 'early' version of the exact same API.
    This is to be used by early mount code where we need to do
    1. setup verity
    2. create verity device
    3. mount partition
    as separate steps since that happens during init first stage.
    
    b/27805372
    
    Test: Boot sailfish successfully
    
    Change-Id: I01abecfdfa210d3e240a291ddcb3d2e9ed39ede6
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4254fe167949061abb4aa9218ce8935c4a684a6503af7b27ee9cf0641b7b1f30
-size 27772
+oid sha256:640a3d1c93a2ca36f7894257a65cb0e28186e94344b4d9be1e157b78194467ed
+size 27740
--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:456e1b4b4d73a1ecdcccbec29843400fcc2155a15b53f582dc209ea5a9d3595b
-size 4750
+oid sha256:c528376283366a2d5f2772bb6453cc5f981c12b4a5ca92ee1114f2851212e227
+size 4755
