2259f442f0e406fff4e6aff994a874ada21f4b05
Alex Shlyapnikov <alekseys@google.com>
2017-02-14 17:21:55 -0800

    Fix heap-buffer-overflow detected by ASAN.
    Bug: 34949125
    Bug: 34606909
    Test: Make sure Android boots when built with SANITIZE_TARGET='address'
    Change-Id: I9c004e806f2025098aa72228284b05affd2c2802

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:056187a9545790bcc8609414882a56a3ade8bb95ac14edd646f1d594b4bc4fec
-size 41992
+oid sha256:36670407c503fd2bfcbf3db3e5480bf501d7d880bd2fdf7a05c45a32ee47d70a
+size 42141
