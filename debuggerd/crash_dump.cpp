485877f6d2e56308e2d11fc53b7748f51628c567
Josh Gao <jmgao@google.com>
2017-02-15 15:21:00 -0800

    crash_dump: make output fd O_APPEND.
    Bug: http://b/35209835
    Test: mma
    Change-Id: I447e3cfa3361f9c8b4b3335d0abccd1fe4c98e0f

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a79ef1cc3d0a1c0a74d183c20f9dad5a8cca1d03bf8baee0dd2e32de1216dc5
-size 14634
+oid sha256:af862b2a39d8e5aab9c282178da886a2c6db945bb201493c3911032babfc6292
+size 15052
