abda06bce95a2b2780c9a57e857cf69bb3f246d6
Greg Hartman <ghartman@google.com>
2017-02-15 21:43:30 -0800

    Fix read past end of malloc block in logd
    Bug: 35412453
    Test: Local build
    Change-Id: I0f9dee84ef689b042926b6b48abf0caeaa784add

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4f464d7cfc738d743a8e324238cc5aad1e56a654940b4afab3bc71cc97014cc
-size 41954
+oid sha256:056187a9545790bcc8609414882a56a3ade8bb95ac14edd646f1d594b4bc4fec
+size 41992
