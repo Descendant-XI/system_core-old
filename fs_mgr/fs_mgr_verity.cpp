f2d577c735542c55fa78c1d807813d6216e82753
Sandeep Patil <sspatil@google.com>
2017-02-16 19:15:29 -0800

    fs_mgr: add a generic fs_mgr_get_boot_config internal API
    depending on when fs_mgr is trying to read the configuration passed into
    the kernel commandline, it may be able to read it successfully.
    Specially in the case when init has not initialized properties.
    
    This change adds a new fs_mgr_get_boot_config() API to be used by all
    fs_mgr code in order to get filesystem parameters specified in kernel
    command line or device tree. This way the fs_mgr code doesn't have to
    handle the "early" cases separately anywhere.
    
    Test:
    Tested angler boot with both /system and /vendor mounted in init
    first stage.
    Tested sailfish to make sure /vendor can be continued to be
    mounted early without verity
    
    Change-Id: I9a44cdfc32681f714c5d73ae55c3deda95c02545

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:640a3d1c93a2ca36f7894257a65cb0e28186e94344b4d9be1e157b78194467ed
-size 27740
+oid sha256:c82652510dbf012d9100866117fc269b179bf415ee6521aa349adbacef2cd585
+size 27711
