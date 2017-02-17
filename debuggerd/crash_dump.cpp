153ded8515a4276745a9613e5fecc8f0926c6bcb
Josh Gao <jmgao@google.com>
2017-02-16 19:17:28 -0800

    crash_dump: remove unneeded/faulty checks.
    We already check our /proc/`getppid()` fd every time we attach a thread, so
    these were unneeded at best. The one that happened after dropping
    capabilities was actively wrong, though, because /proc/pid access
    checks happen on every operation. (only on some kernels?)
    
    Also, add a check that getppid() doesn't change after opening
    /proc/getppid().
    
    Bug: http://b/35241370
    Test: debuggerd -b `pidof com.android.bluetooth`
    Change-Id: I807439d8c2afd027f3c382face50167a8a7946c4

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37e70000ea6cb6cfecfbd7c15cc20820144c07d3848e683bde3cdf394c1c451a
-size 15053
+oid sha256:01d98351fe639d3846fe8cd822a8f9381f32ce3bb818945d1aeeb2db633927c4
+size 14699
