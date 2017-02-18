361f6fe7a8e461b0ab47dd088d64ce6bd73be56b
Bowgo Tsai <bowgotsai@google.com>
2017-02-18 18:33:13 +0800

    init: early_mount: removing hard-coded paths
    Current early mount has some hard-coded paths that are not easy to extend
    when we switch verified boot to AVB (external/avb/libavb). This CL uses some
    C++ containers to replace those fixed paths.
    
    Bug: 33254008
    Test: early mount /vendor with dm-verity on sailfish
    Test: early mount /vendor without dm-verity on sailfish
    Test: early mount /vendor with dm-verity on bullhead
    
    Change-Id: I32a22fe486d7649e33bb23c9018ddc0428df6069

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a7dc28d1dc98f0e62e0e34780302e0d55373ecbc00a41104831162a3352523d
-size 35424
+oid sha256:2105d305e54dd76d5dabbad3edee69710763ceab41a7226a874e2203665e76e4
+size 35072
