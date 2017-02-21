c060e498ffc03ce0d705f49a3296c162ef8f87d1
Mark Salyzyn <salyzyn@google.com>
2017-02-21 08:45:21 -0800

    liblogcat: LOCAL_C_INCLUDE_DIRS correction
    Test: compile
    Bug: 35326290
    Change-Id: I62b46443a6b9149577e85d4f512c3946b467a1fb

--- logcat/tests/Android.mk
+++ logcat/tests/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31dd8a08032dd89f0d4066c75ced7069740c9dcebb5447ef140ac706d1c82736
-size 2059
+oid sha256:51c860fd9b2bccff9825528f54d9dd1cc8547e2122e3687a39db3cc97897082c
+size 2014
