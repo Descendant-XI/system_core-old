ab048fabf405515f1c73ff1db5a6338a71d86c92
Mark Salyzyn <salyzyn@google.com>
2017-02-21 16:25:58 -0800

    liblog: event log tags cache miss call logd for update
    Deal with cache miss in the event tag map resources and switch to
    "Plan B" to ask long-path to logd for a determination on the tag,
    name and format.
    
    logcat-unit-tests liblogcat.descriptive does an under-the-hood call
    to logd to add a new logtag but the in-process mapping fails to
    pick it up because the /dev/event-tag-map map is SHARED PRIVATE for a
    few moments to garner some (linux) efficiency with other copies that
    are in the process.  Without the workaround of marking the mapping
    dirty to be reread, we are using this change as the proper workaround
    that fixes the later parts of this test.
    
    Test: gTest logcat-unit-tests --gtest_filter=liblogcat.descriptive
    Bug: 31456426
    Bug: 35326290
    Change-Id: Ibe54d4df00ed92248e1e644ecebc95f60b222b4d

--- liblog/event_tag_map.cpp
+++ liblog/event_tag_map.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b719fc164031ce8fa6397bd4c87d477ae72ba904c93a202b933253a89df4ba86
-size 18906
+oid sha256:d21416963d4d2affdc8e4d90c8270793a2efe02dd7480f69a473cb6b82787593
+size 21327
