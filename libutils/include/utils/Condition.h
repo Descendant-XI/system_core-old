2ae3a78a504162ae1c36451923c10f05ac93eb74
Tom Cherry <tomcherry@google.com>
2017-02-22 17:38:00 -0800

    Make Condition use CLOCK_MONOTONIC
    Changes to the REALTIME clock can cause Condition::waitRelative() to
    timeout early or delayed.  This behavior is undesired and new since
    https://android-review.googlesource.com/#/c/181058/ moved the
    underlying pthread_cond_timedwait() implementation to use absolute
    timeouts rather than relative ones.  Having Condition use
    CLOCK_MONOTONIC prevents these timeout issues.
    
    Bug: 34592766
    Bug: 35678943
    Test: Boot bullhead
    Test: Ensure time changes do not cause Condition::waitRelative() to
    timeout early or delayed
    
    Change-Id: I3a8d7a48f9b42fe990c3c7331313b6d85aa546f9

--- libutils/include/utils/Condition.h
+++ libutils/include/utils/Condition.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b31c6f74ce95dc28322b2fbbf8b8fdfe367daba51a97f6d4f878783bdd1781fe
-size 4910
+oid sha256:747a2be0540773369c52e20b5d9107a4b47c8da2da6dcfc367f6e1592ab5041b
+size 4913
