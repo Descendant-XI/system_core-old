4549820b3c0033655f397201f52adbf0c5d694a5
Colin Cross <ccross@android.com>
2017-02-23 21:23:05 -0800

    Move adb_dirname and adb_basename to libbase
    adb already provides an implementation of dirname and basename that
    take and produce std::strings, move it into libbase so it can be
    used in the implementation of GetExecutableDirectory.
    
    Test: out/host/linux-x86/nativetest64/adb_test/adb_test
    Test: out/host/linux-x86/nativetest64/libbase_test/libbase_test
    Test: adb shell /data/nativetest64/libbase_test/libbase_test64
    Change-Id: Ideb1627607b14562121316d4ed27fa6fb0930684

--- adb/adb_utils_test.cpp
+++ adb/adb_utils_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1a8b1122ff36c675542e82410865e0d42ab0c01df9cbc7efaf4cec34c8e6ebb
-size 6285
+oid sha256:3376847f7d9a7a4b76e0669d43c6fb4a8875f2f8541559290f650714578bce6d
+size 5918
--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:488a3d0dcc07796f845932b477bf4d790211157907b3ce2c458f011e85d802ec
-size 79100
+oid sha256:7ca02536dcc3aa7c727d8062d2b1a90de6174c5f609502e7541f7f6d50f47ef8
+size 79138
