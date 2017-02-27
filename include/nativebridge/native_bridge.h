e83fe697dc28dbf289c896bf299887760e34aab3
Zhenhua WANG <zhenhua.wang@intel.com>
2017-02-27 10:14:45 +0800

    NativeBridge: add "linked namespace" semantic corresponding to linker
    For dynamic linking perspective, semantics of NativeBridge needs to
    align with dynamic linker. This patch adds "linked namespace" semantic
    which shares some libraries from one namespace to another.
    
    Test: make test-art-host-run-test-115-native-bridge
    Change-Id: I71ce1dde19d61363d5eb9731fd4795a8c315b3a0

--- include/nativebridge/native_bridge.h
+++ include/nativebridge/native_bridge.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20c2d49e7c76b686230e75c664366ed53fd6c6e44fe58411b446e5b595b6d0d6
-size 15073
+oid sha256:32b0fab43692f9c2bc17c7b8f4c7a20ad147565f45438b6ccac5f02de50cc379
+size 17289
