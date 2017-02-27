e29cfdf33de9e87272ac66b96ce983160b37ca23
Steven Moreland <smoreland@google.com>
2017-02-27 09:01:21 -0800

    Bluetooth hal: moved to vendor partition.
    Bug: 35328775
    Test: works in both binderized and passthrough modes
    Change-Id: I61f1ff6b777089d7aad5184c0aee4f653897b32e

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d302f674da2b6ac7e250384ff49a5c82167217c6010242eb0a3b49ee9cb4b896
+oid sha256:73195fcdc01d092bccb850b91752dcd2f6109a9539a706213dd400d658f2f508
 size 14418
