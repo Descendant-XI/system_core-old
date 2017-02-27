c1d58611839590f90bead597e7071f890b426fd2
Alex Klyubin <klyubin@google.com>
2017-02-27 12:41:50 -0800

    Make secilc accessible by root only
    There's no reason for SELinux policy compiler to be accessible by
    anybode other than root.
    
    Test: Device boots -- secilc isn't used yet anyway
    Bug: 31363362
    Change-Id: I26cf34f1412b8dd471f79271c491b473617a6df6

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d7f0dcd198f119972645b091a0fbbdae689fc8b62a1452068186fceadd18fe0
-size 14088
+oid sha256:0c811392085e7b6f2704f21734af4ad66fc363bfe6dffcf958f2c0f5179fff2a
+size 14157
