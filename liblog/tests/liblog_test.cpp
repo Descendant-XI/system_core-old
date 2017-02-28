8ce920374d221bad2488ef88e11a2942f275fc25
Mark Salyzyn <salyzyn@google.com>
2017-02-28 12:59:01 -0800

    liblog: add LOGGER_STDERR frontend
    Standalone, this logger provides no end-to-end capability.  Only
    provides a writer, no reader transport.  All output goes, logcat-like,
    into the stderr stream.  Output can be adjusted with environment
    variables ANDROID_PRINTF_LOG and ANDROID_LOG_TAGS.
    
    liblog_*.__android_log_bswrite_and_print___max print fails if a string
    member is truncated with "Binary log entry conversion failed" and -1.
    We expose the truncated content in the tests and in LOGGER_STDERR.
    
    The purpose of this transport selection is for command-line tools,
    providing a means to shunt the logs to be mixed in with the tool's
    error stream.
    
    Test: gTest liblog-unit-tests
    Bug: 27405083
    Change-Id: If344b6e3e67df2dc86ce317cfad8af8e857727b7

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:865c7ad2fbe9dbfa16eb8b7f173e5948baf1b2b09690a46ffc761dd86f201ac2
-size 102767
+oid sha256:e8975e627651dca8c26c9aed4c4af24dbdb6cad228a5fad6a7118cb9f27f5331
+size 106487
---
+++ liblog/tests/liblog_test_stderr.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b95ef1d16a26b02c235b559a887b8aaa8966d141926c92bdbfb18137187a53dc
+size 175
