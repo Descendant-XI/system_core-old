7be3940cc180f0b28798c9204ac040933c83418f
Tom Cherry <tomcherry@google.com>
2017-02-27 17:02:02 -0800

    Clarify that Condition::wait() can spuriously wake up
    Also, remove the clarification that Condition::signal() wakes exactly
    one thread as in the presence of spurious wake ups, this clarification
    does not provide a safe guarantee to developers.
    
    Bug: 34592766
    Test: Build
    Change-Id: I34df02e44a70a18fe04ceda858d002ef129c1fd9

--- libutils/include/utils/Condition.h
+++ libutils/include/utils/Condition.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:747a2be0540773369c52e20b5d9107a4b47c8da2da6dcfc367f6e1592ab5041b
-size 4913
+oid sha256:fb06377eb24e95cea4e7fa08315ffc57b35ee2a06ea14d68f3d085120d6b077a
+size 4722
