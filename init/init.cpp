f0d1db04abbb0276c1c3acd834316c1ad4ad8b29
Alex Klyubin <klyubin@google.com>
2017-02-28 12:32:20 -0800

    Use split SELinux policy at boot, if available
    This modifies init's loading of SELinux policy into the kernel to
    load the split (platform/system vs non-platform/vendor) policy if it's
    present. If the split policy is not present, the usual monolithic
    policy is loaded into the kernel, same as before.
    
    Split policy is loaded by first compiling it from CIL form using
    secilc compiler into the conventional monolithic/compiled form which
    is then loaded into the kernel.
    
    The build system has not yet been modified to place split policy onto
    devices. Thus, this commit currently has no effect. For testing split
    policy, build plat_sepolicy.cil, nonplat_sepolicy.cil, and
    mapping_sepolicy.cil, and place them into the root directory of the
    device.
    
    The following tests were performed for a device with monolithic policy
    and for the same device with split policy.
    
    Test: Device boots, no new denials
    Test: Play Movies plays back movies
    Test: Load ip6.me im Chrome
    Bug: 31363362
    Change-Id: I9a75a48ac88f3392abc36669f91b0803e88cd147

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c9ccc4c319e01f66b821a526de754a1812e94e3cdb6b3cc6d77c04c3e090208
-size 35863
+oid sha256:fcfc9072be3c92ef9e07827b15e0813aa32df255f42de55a7fd3c8d4525f76ce
+size 40865
