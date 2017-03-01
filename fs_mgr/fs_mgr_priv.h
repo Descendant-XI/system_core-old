334c248ce029baf23f00a6ba48ced78484d91a98
Bowgo Tsai <bowgotsai@google.com>
2017-03-02 00:03:56 +0800

    fs_mgr: support AVB in fs_mgr_update_verity_state()
    fs_mgr_update_verity_state() is invoked by 'verity_update_state' in
    init.rc. It will then set property "partition.system.verified" and
    "partition.vendor.verified" to verify_mode. We should support this for
    AVB as well.
    
    Also change the order of static libs in init to fix the build error
    after this change:
      system/extras/ext4_utils/ext4_crypt.cpp:69: error: undefined reference to 'property_get'
    
    Bug: 35416769
    Test: Mount /system and /vendor with vboot 2.0 (AVB), check the following properties exist.
          - [partition.system.verified]: [2]
          - [partition.vendor.verified]: [2]
    Test: Mount /system and /vendor with vboot 1.0, check the following properties exist.
          - [partition.system.verified]: [0]
          - [partition.vendor.verified]: [0]
    
    Change-Id: I4328d66a8cb93f26e7960e620a0b2292d5f15900

--- fs_mgr/fs_mgr_priv.h
+++ fs_mgr/fs_mgr_priv.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18400ddba3557831f85f0c952031b336029e3f6f39aa8a0ef8fb126c9107bcbc
-size 4847
+oid sha256:c0c38fd201608d8f81b14dc04b65860090f5e7feab63f1c468c68e5e316a9a54
+size 4906
