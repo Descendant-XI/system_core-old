1659ed909c6a03acbb2a293d8ff7701612b43cf9
Keun-young Park <keunyoung@google.com>
2017-03-02 14:33:04 -0800

    collect fs stat for ext4
    - This is to collect data to understand if e2fsck -f option
      can be dropped wholly based on information from fs.
    - Ideally e2fsck should not fix fs if it was clean shutdown
      or if it is not enabling quota.
    - The log is added to /dev/fscklogs/log and other system components
      can collect it later.
    
    TODO: add mechanism to distinguish old vs new fs generation tool.
    
    bug: 32246772
    Test: reboot and check saved logs under different shutdown conditions (clean, non-clean)
    
    Change-Id: Id00fad4c5f8ebbb9f9908164a1026e415df06721

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce0b26dddb4d6bc6a25bea9ac34bbb086bd0c5ab5e9d0243287f170ede7ac375
-size 40987
+oid sha256:312eea67db6ca116fd83d36feca5274c20c00deb48ac688b55839c3c0c30f99b
+size 43899
