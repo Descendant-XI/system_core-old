11337cd11064e8b76518c88b3b07459fba46cd7e
Elliott Hughes <enh@google.com>
2017-03-02 19:01:20 -0800

    Small debuggerd improvements.
    Include the ABI in seccomp causes.
    
    Slightly improved command-line usage information.
    
    Fix crasher for seccomp failures.
    
    Bug: N/A
    Test: crasher
    Change-Id: Ie419ecfe72ee4f5ccf49c927be18350a58a66a90

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f255ecb9ddec1bc665d6f6376d693bb4ceb6be964a13b61da73c02deb50097a
-size 24197
+oid sha256:a9ca3cc3b1b8757c3f5c7c5451ab52c4e718ccd4488c0c6ab3b31ee6916dd337
+size 24237
