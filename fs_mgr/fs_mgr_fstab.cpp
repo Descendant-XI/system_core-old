643f4d604d8b07fca2de48ecbe8359bc1e6b8d27
Bowgo Tsai <bowgotsai@google.com>
2017-03-06 22:22:07 +0800

    fs_mgr: add fs_mgr_read_fstab_with_dt() API
    With the early mount support in init, fstab entries of verified partitions
    (e.g., /system, /vendor) will be moved into device tree in kernel image.
    
    Those early mount fstab entries will be removed from the fstab file to
    prevent duplicated and/or inconsistent settings.
    
    This change adds a new function: fs_mgr_read_fstab_with_dt(), to return
    the combined results from both places. It also removes
    fs_mgr_read_fstab_file() from the public APIs and makes it as an
    internal function.
    
    Bug: 35811655
    Test: early mount /vendor with dm-verity on sailfish
    
    Change-Id: I2fba3614685bf9f852a789cda6f53013e2164e60

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e59fa60508d479be08e5f6be1e5f01a168ab8dbf2f27d35765fe4ab11f4f65a5
-size 26811
+oid sha256:4fe18ffd52dbcf16127c7af0a8ff0ae5a8408e478932125baa5967217f532fc4
+size 27466
