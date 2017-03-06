ae0f804d875e3ebd09421ccd32ce591b00eaa057
Bowgo Tsai <bowgotsai@google.com>
2017-03-06 15:39:06 +0800

    fs_mgr: allow disable dm-verity when the device is unlocked without metadata
    To start Treble VTS test, a single AOSP system.img will be flashed onto
    the device. The size of AOSP system.img might be different than the
    system partition size on device, making locating verity metadata fail
    (at the last fixed size of the partition).
    
    This change allows disabling dm-verity on system partition when the
    device is unlocked (orange device state) with invalid metadata.
    
    BUG: 35603549
    
    Test: Boot bullhead with a different-sized system.img, checks verity is
          not enabled via:
    
              "adb shell getprop | grep partition.system.verified"
    
    Change-Id: Id89d023e82c78ff1902ab7a528431cf7f65e49ad

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f8e3a59379b9b0301bbe7c2a78af231c52b828dde4ce2dc8941aff21ff2d491
-size 27472
+oid sha256:4a84109f0ed05dbe023750c164500eb10a8cb31c1069886bef73a8c84d4c6681
+size 27803
