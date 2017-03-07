c2db2a002a4794d0fdd4991943788026a24c72e2
Bowgo Tsai <bowgotsai@google.com>
2017-03-07 18:52:03 +0800

    fs_mgr: support reading fstab file from /odm or /vendor partition
    fstab contains device- and soc- specific content that should reside in
    /odm or /vendor partition. This change searches the fstab.${ro.hardware}
    file from /odm/etc, /vendor/etc and /, then use the first one found.
    
    Bug: 35811655
    Test: boot sailfish
    Change-Id: I82f89b41a849faedb64072a7cfc52d7424e1aaa1

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fe18ffd52dbcf16127c7af0a8ff0ae5a8408e478932125baa5967217f532fc4
-size 27466
+oid sha256:1e8e41234e192396572d5d535b78ad9ef37bb96dbfea8a533ab6c7f2eefc97fb
+size 27706
