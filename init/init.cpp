11f26013802de76303e1bac72251527c016c4edf
Alex Klyubin <klyubin@google.com>
2017-03-07 14:12:01 -0800

    Use precompiled sepolicy when available
    NOTE: This change affects only devices which use SELinux kernel policy
    split over system and vendor directories/partitions.
    
    Prior to this change, init compiled sepolicy from *.cil files on every
    boot, thus slowing boot down by about 400 ms. This change enables init
    to skip the step compilation and thus avoid spending the 400 ms. The
    skipping occurs only if the device's vendor partition includes an
    acceptable precompiled policy file. If no acceptable policy is found,
    the compilation step takes place same as before.
    
    Because such devices support updating system and vendor partitions
    independently of each other, the vendor partition's precompiled policy
    is only used if it was compiled against the system partition's policy.
    The exact mechanism is that both partitions include a file containing
    the SHA-256 digest of the system partition's policy
    (plat_sepolicy.cil) and the precompiled policy is considered usable
    only if the two digests are identical.
    
    Test: Device with monolithic policy boots up just fine
    Test: Device with split policy and with matching precompiled policy
          boots up just fine and getprop ro.boottime.init.selinux returns
          a number below 100 ms. No "Compiling SELinux policy" message in
          dmesg.
    Test: Device with split policy and with non-matching precompiled
          policy boots up just fine and getpropr ro.boottime.init.selinux
          returns a number above 400 ms. There is a "Compiling SELinux
          policy" message in dmesg. The non-matching policy was obtained
          by adding an allow rule to system/sepolicy, building a new
          system image using make systemimage and then flashing it onto
          the device.
    Bug: 31363362
    Change-Id: Ic2e81a83051689b5cd5ef1299ba6aaa1b1df1bdc

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60f2dfb6f25ac16df0e465d6416e6d2eccf38eb6a9170e208409a77f462d944f
-size 42683
+oid sha256:2f3ff8bb34bd9bc7beb129d2965ecb41f91e5e727a4e5e66afcf50fa9baa6ed9
+size 44721
