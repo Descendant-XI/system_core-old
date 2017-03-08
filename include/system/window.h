99b7338ec63e9095cf988a8fd633d32272f5430d
Mathias Agopian <mathias@google.com>
2017-03-08 15:04:09 -0800

    Add an “is valid” query to ANativeWindow
    This will help avoid certain crash (null dereference),
    and return an error instead. This is useful for
    app developers as java language apps are not
    expected to crash when passed bad arguments
    
    This doesn’t fix bug 36065206, but was prompted
    by it.
    
    Test: compile / run
    Bug: 36065206
    Change-Id: I329874821f5cd780eeaacf8945859af5ab790cf4

--- include/system/window.h
+++ include/system/window.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6baf81c0307f82a395311f3cf158ec1a61e73f3391306fef70d4e656b14c6ff5
-size 41850
+oid sha256:82f2373f896f102dba9943a4e16ba9b2b2ac9f52142551fcd21c5da7c12455c1
+size 42057
