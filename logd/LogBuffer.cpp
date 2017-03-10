98ed22e5c67aa84120e84e69da32bdc7524dfaff
Mark Salyzyn <salyzyn@google.com>
2017-03-10 10:31:48 -0800

    logd: continue search out-of-order entries timestamp tail
    Regression from commit 8e8e8db549ffa6e692246abce6f5560c3a1e93c2
    
    For liblogcat reader -t or -T <timestamp> tail requests, continue
    search for pertinent out-of-order entries for an additional 30 seconds
    back into logging history to find a more inclusive starting point.
    
    For example, if you have an out of order landing like
    [..., 3, 6, 1, 8, 2, 5] and ask for 3 you used to get only 5, and now
    you get 3, 6, 8, 5 as 'expected'
    
    Test: gTest liblog-unit-tests logd-unit-tests logcat-unit-tests
    Bug: 35373582
    Change-Id: I2a0732933fa371aed383d49c8d48d01f33db2a79

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b016d24c77a653e916c7a9393b36efa5e4835e4242d27af7e5ddb154a376a4a6
-size 42575
+oid sha256:b0cb71e5b3ec1fd70f027808136888728d7683060a00b69e2d76a7dd6b83c450
+size 42847
