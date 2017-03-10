99fa3f58a5f6729207b3a52b9f76b53c93dc5eae
Steven Moreland <smoreland@google.com>
2017-03-10 10:58:30 -0800

    storaged: remove usage of String16::std_string
    <string> is being removed from String16
    
    Test: adb shell dumpsys storaged <all options>
    Change-Id: I234ecd64d9c481cb2eeee883a715f2fbfaecc511

--- storaged/storaged_service.cpp
+++ storaged/storaged_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d128f3aac7499f4e275525f83ff32b21b270918564bcf6c2946efd2a395b26d8
-size 5492
+oid sha256:93c44b7f07244270c6149f19afd3d5a8e6e6d015886cf7abed837b2fe31a5fb6
+size 5495
