bf2f9ed8ad9ecc609640bbe7e1a19e94493e49a6
Steven Moreland <smoreland@google.com>
2017-03-10 22:31:08 -0800

    libutils: String\d+ make std_string private
    This method is still used by prebuilts, but making it private to start
    the process of removing it from everywhere.
    
    Test: pass
    Bug: 35363681
    Change-Id: I4d53f68d10c9abcca32023c4d930e99912afa697

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb24e0092f8f94af20ac3c73583861888bc3a0abfb9af8d7c0b7d0766b241d2c
-size 24152
+oid sha256:973df5080a9e7d08e3da85894a35a880bf7e9ddf8cf869149591fd07a91df003
+size 24139
