206fae48145d69b82977d4327460e2552cf623a6
Mark Salyzyn <salyzyn@google.com>
2017-03-13 16:16:16 -0700

    logcat: test -v epoch tail time
    Test: gTest logcat-unit-tests
    Bug: 35373582
    Change-Id: I57d84af2eb5d1fce8753539edf3133bfe0bb7213

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd5d60298741cb63f7194b4d8051d5875162a0a03f139cac5ab4e53565d363a4
-size 47909
+oid sha256:1f784a4bad48f2696c9dd7f27090f8b125ef6f351d466e165bb1efed884d0643
+size 47705
