2fe2181107719318655f97b88bd4ff726b9a9b1d
Tom Cherry <tomcherry@google.com>
2017-03-13 11:58:58 -0700

    init: enable C++17
    Test: Boot bullhead
    Change-Id: I40961ff765461e8aef211d27158ffb7c4be76493

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46048ca5a3517c24828eff410a4ca70b42caaf999b31524e0091cece6c93d749
-size 4047
+oid sha256:29b3db2d6a79fed1e4838b7aa33931021a2505894b965688c86fec72f6d6e414
+size 4066
