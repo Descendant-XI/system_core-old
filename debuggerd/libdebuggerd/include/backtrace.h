0b310bd2b96f363183911cf6e706d0f45bae90a2
Josh Gao <jmgao@google.com>
2017-03-15 23:23:22 -0700

    crash_dump: fetch process/thread names before dropping privileges.
    Processes that don't have dumpable set to 1 cannot have their
    process/thread names read by processes that don't have all of their
    capabilities. Fetch these names in crash_dump before dropping
    privileges.
    
    Bug: http://b/36237221
    Test: debuggerd_test
    Test: debuggerd -b `pidof android.hardware.bluetooth@1.0-service`
    Change-Id: I174769e7b3c1ea9f11f9c8cbdff83028a4225783

--- debuggerd/libdebuggerd/include/backtrace.h
+++ debuggerd/libdebuggerd/include/backtrace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c07f0cb7230d8b6417c0f5bcebe78caff98af466ab2cf4534dbcf1c4ac484bff
-size 1437
+oid sha256:32a70ada58d58e6f63028f35eea5498544259815661c48ec46e0094da2bb35b9
+size 1482
--- debuggerd/libdebuggerd/include/tombstone.h
+++ debuggerd/libdebuggerd/include/tombstone.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9434d1bf99a105db50b184dd10e7b4e810d188e322a1adeb69ba575d32e827ca
-size 1924
+oid sha256:4fc55b8848d17737af444b18530af0cd94bde6c13ffebf7bb90de661cdf0fd03
+size 1970
