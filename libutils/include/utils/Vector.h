22e958808d82ff677e0e4eaa893b5982b868b7b7
Vishwath Mohan <vishwath@google.com>
2017-03-16 17:59:57 -0700

    Blacklist some vector functions for CFI.
    This CL blacklists some vector functions (construct, copy, splat,
    move) that use reinterpret_cast on freshly allocated memory (where the
    object doesn't exist yet). This is technically correct, but not
    friendly for CFI, which enforces stricter checking to catch type
    confusion errors. Blacklisting these specific functions from CFI does
    not cause an appreciable coverage loss though, so this should be fine.
    
    Bug: 36219323
    Test: Builds and boots, and the reinterpret error goes away on CFI builds.
    Test: All 98 libutils_tests pass
    Change-Id: I4944b179116bb1e1608d92697e95e182d8c0ac9f

--- libutils/include/utils/Vector.h
+++ libutils/include/utils/Vector.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:947ab999d009821ee8e453554a837109c99dc2f597db03e38ca76eeb88c5b9ff
-size 13563
+oid sha256:c9cae43d45d89f7ccadaba7b64a3b0c6a1cebb7a163400e5812e6667a3555777
+size 13919
