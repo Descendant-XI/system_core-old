6fb172a5bd5409937812d50d1517a76182915b5b
Mark Salyzyn <salyzyn@google.com>
2017-03-20 08:15:40 -0700

    libcutils: fs_config: sort dirs
    Sort android_dirs[] first by requirements, grouping, specificity and
    finally by alphanumeric order.
    
    Test: full build and install smoke test and inspection
    Bug: 36071012
    Change-Id: Iff579600b05d7b2a0b9fc7d9e9d897e0bb69aebd

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42f61a707be591eff1c53658c6e0b8588c4524d25dd6a55141f2d88093ed7306
-size 14236
+oid sha256:29c0d95a35c6e8e513fdf753c42ebbd1dedf81d5f861c0f6ecf1c54b0d59330f
+size 14562
