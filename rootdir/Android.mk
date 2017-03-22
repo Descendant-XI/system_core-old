2298085fc3d0c206641d01cde5b583f15dc44500
Andreas Gampe <agampe@google.com>
2017-03-21 19:36:48 -0700

    ASAN-Extract
    Add init script and shell script to unzip a tar containing ASAN
    libraries on boot.
    
    Bug: 36458146
    Test: m && m SANITIZE_TARGET=address
    Test: manual (build steps for tar missing)
    Change-Id: I1bcf332f86c5bf2e0333cbe3def684999c1002f8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ae21373587ed1d7b7c84d152ebb6f7640dbd713381dba746aa267e1ce02b72
-size 5027
+oid sha256:4ca0339e3543bae4b2d85ad5b94c58fa7f28c5321996b3aaa50e2f383f0fa2c0
+size 5451
