90fdd2e3e6f111499a3db8e2fde6272ba4229eaa
Mikhail Lappo <miklelappo@gmail.com>
2017-03-23 22:32:30 +0100

    Va_end should be used with va_start
    va_start is used and va_end should
    be invoked accordingly
    
    Change-Id: Id7037aa8092438b27e445e5147972836602bfd55

--- libpixelflinger/codeflinger/mips64_disassem.c
+++ libpixelflinger/codeflinger/mips64_disassem.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1793d46a46c37a0863bc6817e32d00490f9dfb407fd63a4f4120a63f15f0b4af
-size 18797
+oid sha256:1aec054c36b708de391a9e408ca9373e503d61f686abceeeee7e92b8a1e3decf
+size 18815
