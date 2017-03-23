811a6dd03dd7a05e4be84c958031d62bbdb6fe72
Keun-young Park <keunyoung@google.com>
2017-03-23 15:33:16 -0700

    add clear log for reboot start / end
    - hard to tell if reboot itself is problem or not.
    
    bug: 36004738
    Test: reboot and check last kmsg
    Change-Id: I0de0e10eac9ac336cc352ddee22a4a1d9e46cb79

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8757b2a716b857737190aea46208baa7d0e0b1609f49b02884956db842ed5e9d
-size 14991
+oid sha256:6375300ad2cc0e63a5fbaad49971ea7d740dd5586ef283fdc34b8e40eac371d3
+size 15135
