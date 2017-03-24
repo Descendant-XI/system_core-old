c5febf6c163d2081757e70d1fa4fe148275d9c4f
Tom Cherry <tomcherry@google.com>
2017-03-24 16:52:29 -0700

    Fix timeouts for android::base::WaitForProperty*
    std::chrono doesn't handle integer overflow, so using
    std::chrono::milliseconds::max() to indicate an infinite timeout is
    not handled well in the current code.  It causes an 'absolute_timeout'
    earlier in time than 'now' and causes the associated WaitForProperty*
    functions to return immediately.
    
    Also, any duration_cast from relative_timeout to nanoseconds would
    cause the same issue, as it would overflow in the conversion and
    result in an invalid results.
    
    This change prevents any duration_casts of relative_timeout to
    nanoseconds and replaces the logic to wait on an absolute timeout with
    logic that compares the time elapsed to the provided relative timeout.
    
    This change also includes a test that std::chrono::milliseconds::max()
    does not return immediately and that negative values do return immediately.
    
    Test: Boot bullhead + libbase_test
    
    Change-Id: I335bfa7ba71e86c20119a0ed46014cad44361162

--- base/properties_test.cpp
+++ base/properties_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:065045665c27205c463e94f95c5c0b1918cbe8cd7696582de279412fc5b0c23b
-size 7247
+oid sha256:bd630a1a9cc96f11191214d82c2bc798adcfd1aeb6c5c779aba0f48105d199c9
+size 8562
