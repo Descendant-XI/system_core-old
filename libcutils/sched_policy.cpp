32b2283f44f46e1190fd8ceb97adc886c955abba
Wei Wang <wvw@google.com>
2017-03-25 09:53:31 -0700

    libcutils: remove schedgroup
    After removing bg_non_interactive cgroup, there is only a ROOT cgroup.
    This CL removes the no-op trying to set cgroup.
    It also removes the attempt to open non-exist bg_non_interactive cgroup.
    
    Bug: 36649832
    Test: marlin boosts and cgroup ROOT task looks normal
    Change-Id: Ibccd6db1499306174c940aec9219b20c1b86173d

--- libcutils/sched_policy.cpp
+++ libcutils/sched_policy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b330cc41328537bb7f7f3fa817b5a7075f90f5f4a5239d0826b1ac1dce8f8f5
-size 14765
+oid sha256:7a5300f07dbe91f999f8d505e56817073f92e46ebc0a08542af8f46e11a65949
+size 13105
