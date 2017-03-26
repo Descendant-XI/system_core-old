c838080d207e16f58351767fae90759e80a253a7
Joel Fernandes <joelaf@google.com>
2017-03-25 22:46:10 -0700

    libcutils: Add support for RT schedtune group
    Add an SP_RT_APP group which will be used to provide minimum
    capacity guarantees to RT tasks sensitive to frequency drops
    such as synthesizer workloads.
    
    Bug: 33085313
    Change-Id: I07cca79e52661d1325a1db9ef3b61eb0f8d20989
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- libcutils/include/cutils/sched_policy.h
+++ libcutils/include/cutils/sched_policy.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42dd0b935e46d0b013d146df725015b77bc7feb2d43ae0b5058af6a2c7f6319e
-size 2656
+oid sha256:3ac28056a1f3defa9a803fb5244558454312f552bea4b58e8bb1f06388fd65a9
+size 2679
--- libcutils/sched_policy.cpp
+++ libcutils/sched_policy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c671d4bedb856f71921146435fba7a26322c144a9c6c8ac03a50afe92ae4393f
-size 13086
+oid sha256:c176ef4e6db5f104e2d6e38aee765b64da817c95f34fe59e8f4cc947859298d3
+size 13416
