5bb93bc3677b2c310ac1d4cbee93399788742d16
Wei Wang <wvw@google.com>
2017-03-27 10:59:11 -0700

    init: add support of multiple class names
    Add support of multiple class names in service, so that related services
    can be grouped together. By doing this, we can start/stop some services
    for special purpose. For example, early zygote, early boot animation
    and etc.
    
    Bug: 36535312
    Test: marlin boots with defined classes
    Change-Id: Ifeaaf034fd836816e24f3775bece53ea83faada6

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:959cfd18b198b6a020790720b9cae222b04c4c483acafe7c6f27ca309362f882
-size 34953
+oid sha256:9fe91c4aaeb1f5f4403eb9e75e717d4415f14d6ad5efc19d7e46fa61c5f73a7e
+size 35094
