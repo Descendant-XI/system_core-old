9dad28318cc42e1aa2ff4724750a01dd24c4c1ec
Tom Cherry <tomcherry@google.com>
2017-03-28 16:40:41 -0700

    init: replace property_get with its android::base equivalent
    Slowly try to decouple property_service.cpp from the rest of init.
    
    Test: Boot bullhead
    Change-Id: I267ae0b057bca0bf657b97cb8bfbb18199282729

--- init/keychords.cpp
+++ init/keychords.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd64d474dd9306f5a11c78739becf5e5c54c2448a182df47bbf47e1bfdd6ae8a
-size 3499
+oid sha256:054e2225c701df6c97c7c1d0bc4e918129a7bb5c0750cb548e6b210b38e1d92a
+size 3525
