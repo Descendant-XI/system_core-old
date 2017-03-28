61128c57b7bf30c2baf003e42ce5b0d10c7f68aa
Mathias Agopian <mathias@google.com>
2017-03-27 17:59:01 -0700

    split LightRefBase out of RefBase
    Bug: 36532900
    Test: compiled
    Change-Id: I3088e1a219e04cf924744d3a0c2d374918bb6395

--- libutils/RefBase.cpp
+++ libutils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47181ce3ca363200a07149204efe661b5c1bf5ccab1af291b9dfb21ccfe594fe
-size 26763
+oid sha256:abb8986a93551b762906677c75d378a441c49e52b3e7b914cd58f3cfbdb9ed54
+size 26715
