40b8e91122bcda00c26076fd53209174e85e013b
Josh Gao <jmgao@google.com>
2017-03-28 13:07:15 -0700

    crash_dump: during early boot, output to kmsg on userdebug.
    Crashes that happen before tombstoned is running are extremely hard to
    diagnose, because tombstones aren't written to disk, and the window of
    opportunity to get logs via `adb logcat` is small (potentially
    nonexistent).
    
    Solve this by adding a world-writable /dev/kmsg_debug on userdebug
    builds, and writing to it in addition to logcat when tombstoned hasn't
    started yet.
    
    Bug: http://b/36574794
    Test: stop tombstoned; crasher; dmesg
    Change-Id: Ib22c02a002afb602933155fb2c9b7a8abbe9ed38

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25824c0d29e590dca8137d6e53ea9a0f212a7ae4349f3c108855cb4616bb07e8
-size 3408
+oid sha256:8914511e2723be53e4ad1241a13e2464163356221ef2cbfa41c21ada1b26fcab
+size 3468
