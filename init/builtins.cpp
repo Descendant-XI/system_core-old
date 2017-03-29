f29169c866a147adc425b5f1b4d9b1c6ede7d38e
Janis Danisevskis <jdanis@google.com>
2017-03-29 14:50:01 -0700

    Make init builtin command installkey respect property updates
    init used to block on installkey such that it was unable to
    process property events. This lead to a deadlock by which
    the Keymaster HAL would wait indefinitely for the
    hwservicemanager.ready=true. 
    
    This fixes the issue by implementing the builtin in terms of 
    do_exec, which allows init to stay responsive to properties 
    while waiting for the child to terminate.
    
    Bug: 36278706
    Test: Add a 3s delay into hwservicemanager before it sets the property
          hwservicemanager.ready and the device still boots.
    
    Change-Id: Iaefd31156ca01f3a44b4f85a8bf78beee8dfe224

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cca486085463f9dd9e23732486fc23d944355e50c43f83b3b7a18c03afcc8a5
-size 30296
+oid sha256:d36074c74669716c30024c2c3769a25979bfe57b0de1bb6c5e4c08bc71683bfe
+size 30576
