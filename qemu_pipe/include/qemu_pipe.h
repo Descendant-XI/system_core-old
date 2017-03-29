d29788207927909b50591cdd0e4793ef630d6811
bohu <bohu@google.com>
2017-03-29 12:19:40 -0700

    Revert "Revert "Qemu-pipe: refactor qemu_pipe.h into libqemu_pipe""
    This reverts commit 63ef9ff5fa4b1e68029a54bb60c97b99bdf5defc.

---
+++ qemu_pipe/include/qemu_pipe.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d34270fecae64457554491d77d7eae6f88544cd2f13f440cd4d0163e64a2d93
+size 2383
---
+++ qemu_pipe/qemu_pipe.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0729ad49e6ae338180e6d0c2ada521807059240ac5b97e2870f5fb4d6377931
+size 4721
