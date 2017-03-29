4116918b445b0565a59dd54b4a66822287abe8b3
Adam Lesinski <adamlesinski@google.com>
2017-03-29 16:10:11 -0700

    ZipWriter: Do not write DataDescriptor for STORED files
    Older implementations of ZIP (Java's ZipInputStream) don't
    like a Data Descriptor trailing after an uncompressed entry.
    
    If the FILE is seekable, and the entry is uncompressed, seek
    back to the header and write out the crc and sizes.
    
    Bug: 36686974
    Test: make ziparchive_tests
    Change-Id: I61664515e5afa3e2ba814874eeac847a2aaac319

--- libziparchive/zip_writer_test.cc
+++ libziparchive/zip_writer_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7c002c844a23212d10f98f2976aa27ea3cc805c97891ec4870949a25767da50
-size 12675
+oid sha256:8aed3ebe28f082b9039573312efcaade74d9964591d36d8aec8b4b797a8c5817
+size 12718
