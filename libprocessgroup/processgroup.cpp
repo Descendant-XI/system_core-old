8feb0cf3ce1759db6fc0af66fc2923dd87a51893
Keun-young Park <keunyoung@google.com>
2017-03-28 17:25:24 -0700

    add killProcessGroupOnce
    - killProcessGroup retries if it fails while
      killProcessGroupOnce only tries once.
    - useful for sending signal like SIGTERM once.
    
    bug: 36696408
    Test: will add usage to init
    Change-Id: Ia1e3d4ec848e785a3238f262b02914695cded4c7

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:154f11b37ee0c3d46b71539ad759c2c523680972edfeafcb2548e5a84c6fbd5d
-size 10301
+oid sha256:c7ab34c6de19f7afed2d10ad82d0710035eff583d9ec376125a0c5c3aeda5e50
+size 10578
