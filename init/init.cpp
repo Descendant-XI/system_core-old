b1f57906965b9f2538a92546caceac0398ba6372
Sandeep Patil <sspatil@google.com>
2017-03-29 10:31:26 -0700

    init: consolidate restorecon after selinux initialization in single function
    Test: Boot sailfish
    Change-Id: I423028f12a84c4e0c12c9bdde52b6d795d45b620
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ed0ead73f11b76c0d605d443930dc1663d16a6a678e961b6795bfc5ec708c2e
-size 46370
+oid sha256:87c0f173cb0d4ce5b72e7e31a11f6701701cc962745c94c6cde9251edf672099
+size 46428
