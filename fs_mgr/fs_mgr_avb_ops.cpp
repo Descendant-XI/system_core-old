726cdb95f8e795bd61fd854ca6d436b37dc43897
Bowgo Tsai <bowgotsai@google.com>
2017-03-30 21:45:51 +0800

    fs_mgr_avb_ops: stores the by-name prefix of /misc instead of fstab struct
    fs_mgr_avb_ops->read_from_partition() relies on the by-name prefix to
    get the device file when reading a partition. Previously we store the
    fstab struct and use fs_mgr_get_entry_for_mount_point() for every read.
    
    It's better to do that once and store the by-name prefix for later use.
    
    Bug: 33254008
    Test: test AVB on bullhead
    Change-Id: Ie999851b529f984e29ccbf39fb52a8220056fe0c

--- fs_mgr/fs_mgr_avb_ops.cpp
+++ fs_mgr/fs_mgr_avb_ops.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4272c35bdd19fb9d83955d1c340616a53f25c57e4be4797be26744a3465f234d
-size 7779
+oid sha256:25aff7890e9ed2717119147c36cfa9cf0eaa0f1792028cedaf69afe5397ce4af
+size 7957
