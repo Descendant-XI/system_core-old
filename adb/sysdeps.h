54c335240b553905e03c05edb7f584206cd210d6
Josh Gao <jmgao@google.com>
2017-03-30 13:04:35 -0700

    Revert "adb: check our socketpair ends in our win32 emulation."
    This reverts commit 5990191c4e4802567881db2f19db4adbddc64e1e.
    
    Some Windows users are seeing mismatches between the two ends.
    
    Bug: https://code.google.com/p/android/issues/detail?id=258400
    Test: mma
    Change-Id: Ie3a121a2754332608ff64b3b016adcb4258d41f3

--- adb/sysdeps.h
+++ adb/sysdeps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4428f38b097306b1d29336fcf964e537ce6ce327cefe8175ceb245c63377c1c6
-size 24332
+oid sha256:9de14df88c744ee13bfa151cd36ea76f2be92b6d1cbd4dc69be32c3f844bab40
+size 24179
