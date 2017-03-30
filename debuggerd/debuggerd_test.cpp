890f8b70ae846f17d06258d5f9a9a7ca72c4f140
Josh Gao <jmgao@google.com>
2017-03-30 16:40:47 -0700

    tombstoned: fix a race between intercept and crash_dump.
    Previously, there was no way to detect when tombstoned processed an
    intercept request packet, making it possible for a intercept request
    followed by a crash_dump being processed in the wrong order.
    
    Add a response to intercept registration, to eliminate this race.
    
    Test: debuggerd_test
    Change-Id: If38c6d14081ebc86ff1ed0edd7afaeafc40a8381

--- debuggerd/debuggerd_test.cpp
+++ debuggerd/debuggerd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01d5f12630a71ad0fa3467a9c36c01d28df73c4bf9be5d9f0d0a543266469236
-size 15218
+oid sha256:8de3c48593c64a192edecb48852c0b0a770ced5d78fbd99d4de43a33910d2cf0
+size 15978
