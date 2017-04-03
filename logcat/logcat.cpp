f9b475047df8e35ce1121299a4402706d94c3e0e
Mark Salyzyn <salyzyn@google.com>
2017-04-03 09:30:20 -0700

    logcat: Add -h and --help flags
    If -h or --help argument was supplied, logcat would report the help
    message but also return an error.  Officially add -h and --help as
    recognized flags, report all help with a (zero) success error code.
    Adding this, and the associated test, was split off as a stepping
    stone to resolving an issue with logcat -L or --last flag operations.
    
    Test: gTest logcat-unit-tests --gtest_filter=*.help
    Bug: 28788401
    Bug: 30041146
    Bug: 30612424
    Bug: 35326290
    Change-Id: I948e7fa4e92bd23f52717758ffd96bbd068c53d4

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9dec5c527409ac04d4010a95692c70b69922c385b50bb9c969a1aa3567cd389
-size 69444
+oid sha256:d1936478e8d89198fc0c2cfe0ea78aace4c25592de46d06118db6472055d6ebd
+size 69610
