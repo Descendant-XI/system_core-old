aef79d5ce998be5f04fad08a7b07f3a4e52717cf
Mark Salyzyn <salyzyn@google.com>
2017-04-03 09:30:20 -0700

    logcatd: introduce logcatd executable
    logcatd is the same as logcat, except that the -L flag, if supplied,
    runs once, then the command re-runs itself without the -L flag with
    the same argument set.  By introducing a logcatd daemon executable
    we can solve the problem of the longish reads from pstore that
    sometimes occur when the system is excessively busy, starving this
    background cgroup daemon as we absorb the delay in a backgrounded
    init "service", rather than in a forgrounded init "exec".  This would
    not have been efficiently possible without the introduction of
    liblogcat.
    
    There are no doubt many flags that make no sense to run twice with,
    and without, the -L flag.  In the general sense we expect the caller
    to perform the correct set of operations and not pick these nonsense
    operations.  logcatd is only supplied on engineering and debug builds
    for logpersist, and is only an automated aid to triage.
    
    Test: gTest logcat-unit-tests
    Test: manual confirm logpersist functions as expected, required reboot
    Bug: 28788401
    Bug: 30041146
    Bug: 30612424
    Bug: 35326290
    Change-Id: I53ba31970749daf37eef42636f039f485932416f

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b558bf9a057c4cac26fa79567c4a1008af57f2240706bcfa51d8b5a95d91f1fa
-size 49720
+oid sha256:47e49cd67900590537ad9d5b5c18d452cc3396279faf38113abfdc01550fac72
+size 50462
