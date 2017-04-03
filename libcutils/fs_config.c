ebd75cf9c1dfa54345b6818dd4a2a92464633a49
Mark Salyzyn <salyzyn@google.com>
2017-04-03 07:30:06 -0700

    libcutils: create private/fs_config.h
    private/fs_config.h is required in order to build an independent
    test that requires internal binary knowledge of the
    etc/fs_config_(files|dirs) files.
    
    Test: compile
    Bug: 36071012
    Change-Id: I268bcfdbb6d45b7bf6040cbf307a4e34812f5fef

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1ea0f61125a5ddc7fad77716dea71d5b58d21c14c3b8567cb5215d2d2093eca
-size 16263
+oid sha256:02973b97d0e95112a372b254d61ba4d9aee48e079b8e77d72565f3631ca3deea
+size 16036
