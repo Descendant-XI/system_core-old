c2ce20fb8e50784fb1c65e3bb1ccaa74406817ae
Josh Gao <jmgao@google.com>
2017-04-04 13:54:30 -0700

    debuggerd_test: remove useless pipe.
    Bug: http://b/36897117
    Test: none
    Change-Id: Ib8369088153fe44de2af9037299bdfce7b709b98
    (cherry picked from commit 951cf7007147168164e8cd60ff093627f387aad7)

--- debuggerd/debuggerd_test.cpp
+++ debuggerd/debuggerd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f2b92156f0eb7225c42f879134b50ec8bea3dfeea39f7414eeec957847e201
-size 19030
+oid sha256:175e6722e94fea6e99e3dc510afbbfb70d5effafe7a13aa0dc083206f0ca40e0
+size 18970
