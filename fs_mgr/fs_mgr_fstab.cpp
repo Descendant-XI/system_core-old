5a2ff4c47ca6cfa4808e7cd821454acb9c035be7
Bowgo Tsai <bowgotsai@google.com>
2017-04-05 00:02:33 +0800

    fs_mgr: adding fs_mgr_get_slot_suffix() public API
    The function returns "_a" or "_b" based on two possible values in
    kernel cmdline:
    
      - androidboot.slot = a or b OR
      - androidboot.slot_suffix = _a or _b
    
    Bug: 33254008
    Bug: 36533366
    Test: boot sailfish
    Change-Id: Ia0a524e4145ebf61af5821f42ecad212c95ed748

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8160fdd6683d4bd558d5a32090256474dfd9c62df531143ad0f1438f09be1457
-size 27796
+oid sha256:339af6f27d237d0d9c54d7d310b360b89e502d2ff877a63abfca9d3c908b66f5
+size 27792
