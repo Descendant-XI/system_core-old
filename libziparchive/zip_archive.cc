821116dd80147c0bfa33d942d1430a77c7c0553e
Tianjie Xu <xunchang@google.com>
2017-04-05 14:46:27 -0700

    Do Not Merge: Fix out of bound read in libziparchive
    We should check the boundary of central directory before checking its
    signature. Swap the order of these two checks.
    
    Bug: 36392138
    Test: libziparchive doesn't read the signature after boundary check fails.
    Change-Id: Ie89f709bb2d1ccb647116fb7ccb1e23c943e5ab8
    (cherry picked from commit 74464a1361562d4042a67c5d66bfcf396ee7e59c)

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98cad56fb24fa64cfa796af9c82d9bbe6d32eeb95935cbaa1ffa555dd5596e66
-size 39138
+oid sha256:9ff09b803e541b6fdefb834c2a4634e770c86fa590f78e0aa79b7f4db8ab2da3
+size 39223
