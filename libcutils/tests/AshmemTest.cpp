5812ce3e8f2633cc5f74a5a02c5fedfaadbf7f8c
Greg Hackmann <ghackmann@google.com>
2017-04-05 10:03:10 -0700

    AshmemTest: test lseek() and read() on ashmem fds
    ashmem has in-kernel handlers for lseek() and read(), but they aren't
    currently being tested.
    
    Add tests for various seeks on a buffer containing holes.  If we land
    inside data, then check that we read() the expected data at that offset.
    
    Test:  /data/nativetest64/libcutils_test/libcutils_test64 \
           --gtest_filter=AshmemTest.* (on hikey)
    Test:  /data/nativetest/libcutils_test/libcutils_test32 \
           --gtest_filter=AshmemTest.* (on hikey)
    
    Bug: 37254818
    Change-Id: I96135a8cea2ce99932e3bc97b5254c95ef6b264a
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- libcutils/tests/AshmemTest.cpp
+++ libcutils/tests/AshmemTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b60f0a850c23a81889c7c16893e76eef9edec7a8190d9cdf18751c8f23892d1
-size 5664
+oid sha256:3786f177cec4f49b0c29e85e1d9afb2da77d1bb761910592f6bea4ab8592b994
+size 7892
