2c7537624b073573681e57694c814935a6bd6fc3
Tom Cherry <tomcherry@google.com>
2017-04-05 15:58:31 -0700

    ueventd: Write tests for the get_*_symlinks() functions
    Bug: 33785894
    Bug: 36250207
    Test: Boot bullhead + new unit tests
    Change-Id: Ia0f290542eb1cffce5ae876dfedb453dde960253

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eeb6476c4829697f1e01946c916dd9869977212dc1c3d75bdc33b0e8722cdf8
-size 11279
+oid sha256:bba1bfa5bfc89c7a441be3dcfaee01688eaf6ddbdf725057107bee116f086d56
+size 10863
