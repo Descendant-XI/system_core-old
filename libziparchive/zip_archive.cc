13dbbad49f68702efdb22afe89f25a7b6bdaff0f
Tianjie Xu <xunchang@google.com>
2017-04-05 14:46:27 -0700

    Fix out of bound read in libziparchive
    We should check the boundary of central directory before checking its
    signature. Swap the order of these two checks.
    
    Bug: 36392138
    Test: libziparchive doesn't read the signature after boundary check fails.
    Merged-In: Ie89f709bb2d1ccb647116fb7ccb1e23c943e5ab8
    Change-Id: Ie89f709bb2d1ccb647116fb7ccb1e23c943e5ab8
    (cherry picked from commit 74464a1361562d4042a67c5d66bfcf396ee7e59c)

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47b3e33312201f668b0c087569d646413eaa42b52bb339f7284571fe74dd9c0b
-size 41898
+oid sha256:e19a534d0e157cc9d5acaac502ea6bb06f4d63274aec22650efbbad6f2346724
+size 41983
