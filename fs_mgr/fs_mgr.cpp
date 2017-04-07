6575831c129c53db266407ab4f48f732fcc27173
Keun-young Park <keunyoung@google.com>
2017-04-07 14:16:42 -0700

    use s_max_mnt_count to detect mke2fs generated partition
    - Old tool will set it to 10 while mke2fs will set it to -1.
    - For now, only tag it.
    - TODO: possibly add different policy per image tool version.
    
    bug: 32246772
    Test: check dmesg after reboot
    
    (cherry picked from commit 95e233f9ff4d51bbafc6314b88920f553edd365f)
    
    Change-Id: Icd4758e240009d7b37df2d8a0e2a20d5ddbe4906

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22feca78a4d922c22a381b998e369fd03cab70c00bfb536309287d7772c849cc
-size 43962
+oid sha256:6407d806b5c9c6178cbae32e79e2fcf5d1822122b9d3cbc256b0f200428a5308
+size 44176
