1fd6f43e93e93967339e819da363c70697a70156
Greg Hackmann <ghackmann@google.com>
2017-04-11 15:08:36 -0700

    AshmemTest: expand ProtTest cases
    ashmem buffers start with PROT_EXEC | PROT_READ | PROT_WRITE and can
    have bits individually removed (but not added) through the
    ASHMEM_SET_PROT_MASK ioctl.  Test that removing prot bits more than once
    works, and that the kernel blocks adding prot bits.
    
    Also test that the complementary ASHMEM_GET_PROT_MASK ioctl returns the
    expected prot mask.
    
    Test:  /data/nativetest64/libcutils_test/libcutils_test64 \
           --gtest_filter=AshmemTest.* (on hikey)
    Test:  /data/nativetest/libcutils_test/libcutils_test32 \
           --gtest_filter=AshmemTest.* (on hikey)
    
    Change-Id: If7b13672547ec4cf6dfd1886197f40f12b2f1c85
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- libcutils/tests/AshmemTest.cpp
+++ libcutils/tests/AshmemTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3786f177cec4f49b0c29e85e1d9afb2da77d1bb761910592f6bea4ab8592b994
-size 7892
+oid sha256:a293d331679835a92e1a48f2a92b765491027aba0b52dbc6c3dcbd177293360f
+size 8492
