b21c6eaadbfb4f7bf2746b6d04ee6274b0873459
Mark Salyzyn <salyzyn@google.com>
2017-04-12 15:11:24 -0700

    liblog: allow event tags to include some punctuations
    event_log_tag parser complains about a period (.) in the name,
    we would consider such an enhancement to the tag names possible.
    I expect we would want to be able to support alphanumerics,
    underscore (_), period (.), minus (-), at (@) and comma (,) for
    starters as they are present in the other text log buffer tags.
    
    We introduce a local endOfTag function that is used during parsing and
    during android_lookupEventTagNum for submitting new tags.  This
    function caused us to enforce const char more closely.  By filtering
    in both places we resolve an issue that could have plagued us if
    garbage requests were made.
    
    Test: gTest liblog-unit-tests, logd-unit-tests & logcat-unit-tests
    Bug: 31456426
    Change-Id: I596b8706e843719ddac07ec40e1cd2875c214bed

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:995db054ddad52d5f2dd68447b24fcf6abe33814f9ff8dc3ef54724356285f15
+oid sha256:4abbe3e9e32b22362cc188f9fef4e8ecec0047638e5ae71b89610239849a348b
 size 53426
