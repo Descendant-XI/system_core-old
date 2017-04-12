cc060eafea942ca519d9f4b32b75abcec4d89993
Keun-young Park <keunyoung@google.com>
2017-04-11 18:59:56 -0700

    check ext4 magic before running next steps
    - mount, e2fsck, tune2fs will all fail if magic number does not match.
    - mismatch always happen for FDE and is wasting boot-up time to try
      all and fail always.
    - skip mount steps if it has invalid magic number and do not record
      fs_stat either.
    - For ext4 fs with corrupt superblock, e2fsck refuses to do anything if
      superblock magic is invalid. So simply running e2fsck does not help
      anyway.
    
    bug: 36231950
    Test: reboot ane check fs_mgr log from dmesg
    Change-Id: I9ad9e0cd30fd074b3bbf8f450bd401b133d5771a

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e922e4396ec696a8e80f3e5a3d04493a56ba59bf13a4b1b156c0f7851456b9b
-size 44167
+oid sha256:ea47d2aa44da086f9366e933d633facf81982f7ab76240edadb8a454f7c8e911
+size 45027
