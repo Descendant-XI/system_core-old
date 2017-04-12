32106fa8d7b1e9a760061369af05e3f37e689d20
Josh Gao <jmgao@google.com>
2017-04-12 13:57:06 -0700

    adb: don't try to resolve 'localhost'
    Misconfigured systems can have localhost pointing to an address that
    isn't 127.0.0.1 or ::1.
    
    adb is the only caller of the libcutils socket_loopback functions, so
    move them into adb and switch the implementations over to using
    INADDR_LOOPBACK and in6addr_loopback, instead of resolving 'localhost'
    when connecting.
    
    Bug: http://b/37282612
    Test: `killall adb; adb shell`
    Test: `killall adb; ip addr del 127.0.0.1/8 dev lo; adb shell`
    Change-Id: I01c1885f1d9757ad0f7b353dd04b4d1f057741c8

--- libcutils/include/cutils/sockets.h
+++ libcutils/include/cutils/sockets.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a13be117cb2498b90d6597d0976ebd6b4055a93d871224ecb6bc77510478f59c
-size 5234
+oid sha256:4f5387794b639ca73e1ac38906aa4e49074df20336d37640c8fd5360b269a0bb
+size 5137
--- libcutils/socket_loopback_server_unix.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:972fb184bca65937ed5a313fad450631fb3512a36cfa8a4e9bb4b435624504e9
-size 2183
