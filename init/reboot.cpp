08e30f2f452264e05aafda72d0fab81452b859a5
Todd Poynor <toddpoynor@google.com>
2017-04-13 15:17:24 -0700

    reboot: fix owner and permissions of last_reboot_reason file
    Default signature WriteStringToFile creates world-writeable files.
    Set owner and group system and remove read/write for non-owner.
    
    Bug: 37251463
    Test: Manual: reboot, inspect
    Change-Id: I6a29c678168dcae611b120dc52170f4eee7069a9

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:079d32a5864fac9fd89bc4b243d1f5d43d5c20f4655544f908052270e4be8f07
-size 15806
+oid sha256:434e7d2f2b086a11eb1e315734cfd49c618143f74f2ac17a21e502bef5be35c0
+size 15933
