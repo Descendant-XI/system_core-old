e5cdcedf6de4145c429e563d725450f4a8568c8f
Bowgo Tsai <bowgotsai@google.com>
2017-04-13 21:17:48 +0800

    Set libavb version into system property for Treble OTA
    Set ro.boot.avb_version to "AVB_VERSION_MAJOR.AVB_VERSION_MINOR".
    During Treble OTA match, the major version must be the same as that in
    the avb metadata on disk, while the minor version can be equal or
    greater to that in the avb metadata on disk.
    
    See how avb versioning work on the following link:
    
      https://android-review.googlesource.com/#/c/342757/
    
    Also renames AvbHashtreeDisabled() -> hashtree_disabled().
    
    Bug: 35322304
    Test: Early mount with AVB, checks [ro.boot.avb_version]: [1.0] exists.
    Test: Not enable AVB, checks [ro.boot.avb_version] doesn't exists.
    
    Change-Id: I5aaf476ca53c4fe817779518ba14b68ebcfdc6d6
    Merged-In: I5aaf476ca53c4fe817779518ba14b68ebcfdc6d6
    (cherry picked from commit 1a898c25f96150787b39d9a08ebdb9ab6fcb3846)

--- fs_mgr/include/fs_mgr_avb.h
+++ fs_mgr/include/fs_mgr_avb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:903376efdc01c3ec03c8e961a636dfc77cc79944b9e939afe685d42def6dda78
-size 3720
+oid sha256:bbd51db3c0eab5a406241f0f04a7b7ed4493cf916ebd80952968e8d838142d1d
+size 3822
--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef31efbcf3667b2d5a15370ab82826e9e55407094934d3a86a0baed3d0121af9
-size 52765
+oid sha256:fd1a3df94fdbfb89812c3ed230aaf5365662588aa89aa3e9a4543303c9dd8924
+size 52907
