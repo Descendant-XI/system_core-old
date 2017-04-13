e82dcb2a19f3cbda43b4a604f38b1cd90f64c291
Bowgo Tsai <bowgotsai@google.com>
2017-04-13 13:05:42 +0800

    fs_mgr: adds/changes some public APIs for early mount in init
    Several changes in this CL:
      - Moves class FsManagerAvbHandle to public API
      - Adds a parameter 'wait_for_verity_dev' for FsManagerAvbHandle::SetUpAvb()
        to allow not to wait for verity device gets created
      - Adds FsManagerAvbHandle::AvbHashtreeDisabled() to query whether AVB is disabled
      - Adds fs_mgr_is_avb() to query whether a fstab_rec has MF_AVB flag
    
    Bug: 33254008
    Test: test AVB on bullhead
    Change-Id: I89c43ca574ae632db8a700fc2590a1f80212c993

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83fc0bb606537b3dca5d03267a750637fb5b1248571d01ce03cc4d88d58ec5cf
-size 5098
+oid sha256:68bb890bf112175522e85ecba4008f377129976b32a4fbfc0adf821d261597e0
+size 5148
