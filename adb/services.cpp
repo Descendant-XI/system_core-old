b3acb16e871d755c0fceb5f10705cf9da86c6898
Steven Moreland <smoreland@google.com>
2017-04-13 23:48:57 -0700

    libcutils: compile with BOARD_VNDK_VERSION current
    - moved __android_log_is_debuggable to a new public header
        (log_properties.h)
    - vendor version of sched_policy uses ALOG* instead SLOG*
    
    Test: (sanity) liblog-unit-tests
    Test: (sanity) libcutils_test (noting b/b/32972117, two tests continue
      to fail)
    Test: system/core as a whole makes with BOARD_VNDK_VERSION := current
      now with no problems.
    Test: boots/works on internal marlin
    Bug: 33241851
    
    (cherry picked from commit 1f83aa424f537cf3f07e1d27dbbcc524818b5358)
    
    Merged-In: I5bc1f348dc0f0c8814bec5b5c3d2c52c825ab640
    Change-Id: I5bc1f348dc0f0c8814bec5b5c3d2c52c825ab640

--- adb/services.cpp
+++ adb/services.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e4f31e4ad7aaa338059c4b6957b5c88db94b63126fa349fcf48c46b316b5a17
-size 16637
+oid sha256:3c01bfe80da7da0976d19d29dc909f5dc5f7d9bf6343e945c359d79261d334a9
+size 16633
