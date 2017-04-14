92cc0de443d6fd490be866113d3bf4ddae513171
Elliott Hughes <enh@google.com>
2017-04-14 19:47:05 +0000

    Revert "Send property_service AVC messages to the kernel audit system"
    This reverts commit 8adb4d9d12d077ee87d1ce97742c6a5ba9aaaf9f.
    
    Change-Id: I0db29617f323ef31ca5a270d02b7668092e9c9e1

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32d4983a0a01b94878a46657299636fe80c00be75e11c38d6194dd4b4558b9f3
-size 3574
+oid sha256:8fc596911738d859b7efcaf58bbeaa9ca9a811e33bcc454dd1b4ef9a8cfe54b3
+size 3556
