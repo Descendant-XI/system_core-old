aaaadd8d5aed14821f79bb0bac8249f8ab368d21
Mark Salyzyn <salyzyn@google.com>
2017-04-14 07:41:11 -0700

    logcat: test: standardize rest() to let logs land when injecting
    Add an internal rest() function to set the standard for how long we
    should wait for a log message to land end to end into the logs.  Add
    a retry on -EBUSY for all descriptive tests along with a rest() after
    log injection so the tests can try to survive heavy Denial Of Service
    (DOS) loads.  Some of the resting was done at a coarse 1 second, when
    200ms will do just fine.
    
    Fix a few pesky issues surrounding test reliability and correctness:
    - stdint.h include missing.
    - missing a logcat_executable, means logcatd.descriptive testing was
      testing logcat instead.
    - count of interfering tests is 3 now: logcat, liblogcat and logcatd, if
      they all run in the same PID, we have to not fail.
    - Added a missed opportunity to add uniqueness to logcat.descriptive sync
      test.
    
    Test: gTest logcat-unit-tests
    Bug: 31456426
    Change-Id: Iaf2bee97878957f654613bb2e78e32379bb8d1b7

--- logcat/tests/logcat_test.cpp
+++ logcat/tests/logcat_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47e49cd67900590537ad9d5b5c18d452cc3396279faf38113abfdc01550fac72
-size 50462
+oid sha256:d5262a252ce3438d574765edcd3302414d90f9446e4664cd6ee2e81d414de518
+size 51761
