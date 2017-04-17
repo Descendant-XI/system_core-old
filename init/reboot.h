a77df7fa9ac2f42dcab1bdcd35535c84b2be19b1
Tom Cherry <tomcherry@google.com>
2017-04-17 16:34:20 -0700

    init: handle sys.powerctl immediately
    Currently if a process sets the sys.powerctl property, init adds this
    property change into the event queue, just like any other property.
    The actual logic to shutdown the device is not executed until init
    gets to the action associated with the property change.
    
    This is bad for multiple reasons, but explicitly causes deadlock in
    the follow scenario:
    
    A service is started with `exec` or `exec_start`
    The same service sets sys.powerctl indicating to the system to
    shutdown
    The same service then waits infinitely
    
    In this case, init doesn't process any further commands until the exec
    service completes, including the command to reboot the device.
    
    This change causes init to immediately handle sys.powerctl and reboot
    the device regardless of the state of the event queue, wait for exec,
    or wait for property conditions.
    
    Bug: 37209359
    Bug: 37415192
    
    Test: Init reboots normally
    Test: Update verifier can reboot the system
    Change-Id: Iff2295aed970840f47e56c4bacc93001b791fa35

--- init/reboot.h
+++ init/reboot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b59f52c93adee2305ee1ceb6ab769d19b3eecf54fa6a22d97246f8c626024735
-size 1155
+oid sha256:ee969f573650c470d7722fb612a1a52339705d9e9ec64291517af97f3821ff4b
+size 1266
