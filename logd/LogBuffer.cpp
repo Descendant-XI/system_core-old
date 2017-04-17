2934297467ed69c60347f7e0e50bca9cbdcb8b39
Mark Salyzyn <salyzyn@google.com>
2017-04-17 12:46:12 -0700

    logd: regression in handling watermark boundary.
    Deal with a regression introduced in commit
    5a34d6ea43d28f3b5d27bf6dd5b9fa31ec033531 (logd: drop mSequence from
    LogBufferElement) where log_time was compared against nsec() time
    miscalculating the watermark boundary.  When dealing with logcat
    -t/-T, or any tail reading, add a margin to prune to back off by a
    period of 3 seconds (pruneMargin).
    
    Test: gTest liblog-unit-tests logcat-unit-tests and logd-unit-tests
    Bug: 37378309
    Change-Id: I72ea858e4e7b5fa91741ea84c40d2e7c3c4aa031

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6435f0067e63335371153450ffae6de0e2b1c1c7e4034c36f995d3c7b1d16974
-size 43350
+oid sha256:5eef7c7aa08d1b1b650f83e509453bced6dd4a3343e18dad61e43d201960587d
+size 43474
