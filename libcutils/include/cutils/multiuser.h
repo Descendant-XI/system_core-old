64cc4f47fc969d2eda26d669f8c67bb02405de9a
Jeff Sharkey <jsharkey@android.com>
2017-04-17 14:45:16 -0600

    Allocate a GID range for external cache files.
    We can't reuse the GID range for internal cache files, otherwise
    we don't have a way to tease apart the difference when deciding if
    it's safe to move apps.
    
    Test: builds, boots
    Bug: 37193650
    Change-Id: I22c4e575cd557636e74c5c73035adb1d4dcbb7f7

--- libcutils/include/cutils/multiuser.h
+++ libcutils/include/cutils/multiuser.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28d5c0d731d3c4fde7d84a5cabae4de609b931e20267ac305629441bad350305
-size 1354
+oid sha256:35d4fa3c68075637524c3694b80ef653986f8f4432d01110c39fca6e44b6d103
+size 1430
