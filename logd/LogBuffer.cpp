cf25663dbcebdda58877ebfcdbdee820b5f74d1a
Mark Salyzyn <salyzyn@google.com>
2017-04-17 12:46:12 -0700

    logd: iterator corruption paranoia
    (cherry pick from commit 3614a0c5d4aec84fbc5f6cbf1e919e8b3825b818)
    
    Add checking for impossible(tm) scenarios within LogBuffer::flushTo:
    
    1) When iterating through the log entries, check if the iterator
       returns two identical element references and break out of the loop.
    2) Cap the maximum number of log entries we will skip while holding
       the iterator lock at 4194304, break out of the loop.
    
    We print a message to the kernel logs if we hit these cases.
    
    ToDo: Remove this paranoia at some future date.
    
    Test: gTest liblog-unit-tests logcat-unit-tests and logd-unit-tests
    Bug: 37378309
    Change-Id: I789594649db14093238828b9f6d1daeca8b780c2

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bba4023ddb947df67df654461d467f29e369259d755d5ad24d0ff5d344f4a54
-size 43498
+oid sha256:10024a566cde49aadb13ca71a5bfaf576cbdc787b97584c723b321d0a0dadc39
+size 43986
