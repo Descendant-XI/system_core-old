23cd7542cd3945fd4e1ca0f42f368b2aceb6c7fa
Jin Qian <jinqian@google.com>
2017-04-18 16:23:18 -0700

    fastboot: use filename instead of fd to generate filesystem image
    mke2fs tool takes a filename and has its own open function to handle
    output file. Change fastboot in preparation to switch from make_ext4
    to mke2fs.
    
    Test: fastboot format:ext4 userdata
    Bug: 35219933
    Change-Id: I7a31cb215f443a4a7cb0bfc23ec28c121a6101e6

--- fastboot/fs.h
+++ fastboot/fs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bfed82f91dbc51a37f5a8852e1f831fa801e80f6841a9306c94b4ffda4c3476
-size 359
+oid sha256:25013e13d76f97b4e4c0afc72f3f8e4de72a50b666908fb286d5d2b23e7d1a32
+size 366
