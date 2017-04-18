c80f1b8b08996476986c438e6ca011877d838d5a
Mark Salyzyn <salyzyn@google.com>
2017-04-18 08:43:16 -0700

    liblog: __android_logger_property_bool svelte+eng default
    When BOOL_DEFAULT_FLAG_ENG and/or BOOL_DEFAULT_FLAG_SVELTE is set for
    flags parameter in __android_logger_property_get_bool, they can not be
    overridden by a supplied property value.  Reset these two flags if
    there is something in the specified property.
    
    Test: gTest liblog-unit-tests, logcat-unit-tests and logd-unit-tests
    Bug: 37425809
    Change-Id: I5fc2d727e8c30bc1e781f8b365f44ec62dd36b4d

--- liblog/properties.c
+++ liblog/properties.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c638a3fa1d356120b29f6457acd8e250570bcd957a3d2842322eb3ee7bea607
-size 18314
+oid sha256:1986aec7f42c7afacef9673485e408a54332b0f8d060e0e2c3c69c3a43ee31da
+size 18413
