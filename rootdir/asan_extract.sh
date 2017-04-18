22829567fc1891a6fe63ea92fcc1d55f8200b697
Andreas Gampe <agampe@google.com>
2017-04-18 15:19:36 -0700

    Asan_extract: Add FDE check
    Under full-disk encryption, we must skip extracting libraries
    into the temporary /data directory. It is too small, and the
    framework will not be able to come up.
    
    Add a check for the vold.decrypt system property. As it turns
    out that we can't reliably use this, also add a size check
    (and use 512M = 131072 4K blocks as the limit).
    
    Bug: 36458146
    Test: m && m SANITIZE_TARGET=address SANITIZE_TARGET_SYSTEM=true
    Change-Id: Ib350094b049b6e75832d393527b8b62a58a7fece

--- rootdir/asan_extract.sh
+++ rootdir/asan_extract.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3426347697b9ba7747979ddce3ca12aa0812f260db4a997ac408cafa96ace98
-size 2016
+oid sha256:6b6b7fc6b8631e86133c0575f580d6d2db542f427a1b5f0ff2b1bc5599b87f53
+size 2560
