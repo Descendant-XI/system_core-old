aa1f35fc98af63a1bca9d575cbba1f9717b66572
Tom Cherry <tomcherry@google.com>
2017-04-20 16:38:54 -0700

    init: fix last_reboot_reason string
    This got moved when refactoring the reboot commands.
    
    Bug: 37540660
    Test: verify bullhead's last_reboot_reason is correct
    Change-Id: I3b86496fc469ca41645df7e7ba8bb51dd25b6b38

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e076d83b118dab700090d63d58fe3c512c50df63dfdd7df9ca63ef3738a4fb9
-size 17856
+oid sha256:1d6b6d76e0f442290bc3fd74c5900770e023181eaa41f96de4676dd6594c03e9
+size 17760
