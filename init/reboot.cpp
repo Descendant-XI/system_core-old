34345202c9271523e00568f199f60355cbd2d3e8
Keun-young Park <keunyoung@google.com>
2017-04-21 17:29:26 -0700

    add additional dump for timeout
    - add sysrq-trigger current tasks dump
    - This helps detecting kernel thread stuck in a specific driver
    
    bug: 37573746
    Test: python packages/services/Car/tools/bootanalyze/bootanalyze.py -r -c packages/services/Car/tools/bootanalyze/config.yaml -n 2000 -f -e 15 -w 30  -v -a
    
    (cherry picked from commit 1663e97fe17eea382aa9ba9d9a10d4e83ca6d7e6)
    
    Change-Id: I1684b9861fe3b797768ea9fdc3544263e0710ee1

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acd70409a3fb832489bf75f980b3c09b1c1c1b63222016f081d22fc4feeddb75
-size 17873
+oid sha256:b1d685a48858ccce137a83a6b5b3449b69380815ece330e162dc7d8267b03e53
+size 18074
