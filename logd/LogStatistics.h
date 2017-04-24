be95aa5a5631403f06e837db632fdad7657d13bd
Mark Salyzyn <salyzyn@google.com>
2017-04-24 19:52:00 +0000

    Revert "logd: add Chattiest LOG_TAG statistics"
    This reverts commit 903156ddaaad85f89210d809dddc17a9000342a4.
    
    Test: compile
    Bug: 37254265
    Change-Id: If9af79684119861ceecf4f88a4a4c84f991b188a

--- logd/LogStatistics.h
+++ logd/LogStatistics.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65779bee69eba49657c788ee6953b3766220a2ce9a100c114e55802523a1fa3f
-size 23178
+oid sha256:bc8fb458add4818eb81d8e829e50327f53c6bb9dae48e6f0f1aea045c6a5d0ae
+size 18588
