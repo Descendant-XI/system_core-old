7869baf03d98fb42f0562a2d530c16ac2eb1ad32
Tom Cherry <tomcherry@google.com>
2017-04-24 16:59:05 -0700

    ueventd: replace ueventd_parser.cpp with init_parser.cpp
    Previously init_parser.cpp was made generic and capable of parsing any
    number of differently named 'sections' or prefixed lines.  We now use
    these capabilities to do the parsing for ueventd.
    
    Bug: 36250207
    Bug: 33785894
    
    Test: boot bullhead and ensure the right /dev nodes exist
          with the right permissions set
    Test: verify no boot time difference
    Change-Id: I698ca962d414f8135af32f6c9cd778841b2b8b53

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd4180a208a9c6b9d8dee16a4b2059374d69d0f3b0fa8e53b9e2a63d61443691
-size 3589
+oid sha256:598b2f50d52ece3d886f21e582138a4c263c6b37f322608a65b6c8b6b3240c7c
+size 3564
--- init/ueventd_parser.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e47f36dcd2c67448f1a5d934056a2595100ff8df7c9505fb4cb3ed6046a4e92f
-size 6179
