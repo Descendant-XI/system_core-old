c6a8eb072b21af311366e19ff3c288d31ea35c52
Christopher Ferris <cferris@google.com>
2017-04-25 11:23:10 -0700

    Move libc_logging to libasync_safe.
    Move the name of the "private/libc_logging.h" header to <async_safe/log.h>.
    
    For use of libc_malloc_debug_backtrace, remove the libc_logging library.
    The library now includes the async safe log functions.
    
    Remove the references to libc_logging.cpp in liblog, it isn't needed because
    the code is already protected by a check of the __ANDROID__ define.
    
    Test: Compiled and boot bullhead device.
    Test: Run debuggerd unit tests.
    Test: Run liblog unit tests on target and host.
    Test: Run libmemunreachable unit tests (these tests are flaky though).
    Change-Id: Ie79d7274febc31f210b610a2c4da958b5304e402

--- debuggerd/handler/debuggerd_handler.cpp
+++ debuggerd/handler/debuggerd_handler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c07fb26fcc165afae528a528b3256d84f00e216a287a635b99043e2bd3a3ea02
-size 16909
+oid sha256:3dc591921b9d0c4b8e10d2cc73c54e68503366d29fbda2ed64029e16e731d580
+size 17149
