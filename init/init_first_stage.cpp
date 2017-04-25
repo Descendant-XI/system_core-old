062d3a3abc6b4c4c7dc15fed8467bff6ee7df61f
Bowgo Tsai <bowgotsai@google.com>
2017-04-25 13:09:47 +0800

    init: fix first stage mount failure when two fstab entries have verity_loc
    The previous check is incorrect because it compares the basename of
    previous verity_loc with the full path of current verity_loc.
    
    Changes it to compare the full device file path instead of just the basename
    of verity_loc. This can catch the case of two different verity_loc
    values with the same basename, e.g.,
    
      - verify=/dev/block/platform/SOC.0/by-name/metadata
      - verify=/dev/block/platform/SOC.1/by-name/metadata
    
    Bug: 37413399
    Bug: 37619597
    Test: first stage mount /system and /vendor with the following fs_mgr_flags on bullhead
            - wait,verify=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadataa
    Test: first stage mount /system and /vendor with different verity_loc values
          on bullhead, checks it bails out
    Change-Id: I017c8bd9f0790d45e08e57df9a2878e4f62c5f9c
    Merged-In: I017c8bd9f0790d45e08e57df9a2878e4f62c5f9c
    (cherry picked from commit 71881fffd68b6f9e69786ffa42592e64d490328e)

--- init/init_first_stage.cpp
+++ init/init_first_stage.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1570f66c282a50cf28720c1fb1932e814aee6956f5fb9ccbba5d2f46eb41a3d
-size 16233
+oid sha256:ec4651af1af63044758150a636472d6860a4b0ea7dab86a7ff12d64ccff19671
+size 16182
