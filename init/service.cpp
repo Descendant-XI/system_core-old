bd3b750ecef15d73e69cf84dc832e6c0e7329eb3
Tom Cherry <tomcherry@google.com>
2017-04-25 17:39:06 -0700

    init: remove 'computing context for service'... message
    This line shows up immediately before starting a service for each
    service without a 'seclabel' option, essentially becoming log spam.
    
    We already log if we fail to compute the context as well.
    
    Test: Boot bullhead
    
    Change-Id: Ibe91fd2dd9f53a8ae2ca95ccea1636ecef2af224

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab402abac0ae37ea11cc0a3ae5bad6b41b83fae341bc9876c60eba83614b35a
-size 36629
+oid sha256:01b931739a8ce9c1a182d2d02a2f2b83cc07be2d4f1e568dc4c2e0dab40c82aa
+size 36557
