cc17563d60fc17420402c329cfd9f08ccb09078a
Keun-young Park <keunyoung@google.com>
2017-04-26 12:36:14 -0700

    set default shutdown timeout to 6 secs
    - Test data shows that most shutdown finishes in 6 secs.
    - The original 10 secs is too long wih no shutdown animation
      running in screen.
    
    bug: 36657139
    Test: check time with reboot
    Change-Id: I9a805ddfde8156b066485902048d0cd01365c453

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae6fdc7100e158d0f762133c202e6ceeb023d05d6402eabc98001b004935cf78
-size 18057
+oid sha256:9d6898765d9e78b8e9a7ac2a7587d3daad4723c1a0759b56b13ff6ac08d8c143
+size 17993
