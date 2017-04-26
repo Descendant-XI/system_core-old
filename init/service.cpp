3655cbbff81cb3b15221c55229f6be7f08aaa073
Tom Cherry <tomcherry@google.com>
2017-04-25 17:31:06 -0700

    init: use full command for exec services' names
    Before:
    init: starting service 'exec 4 (/system/bin/vdc)'...
    init: starting service 'exec 5 (/system/bin/tzdatacheck)'...
    init: starting service 'exec 6 (/system/bin/bootstat)'...
    init: starting service 'exec 7 (/system/bin/bootstat)'...
    
    After:
    init: starting service 'exec 4 (/system/bin/vdc --wait cryptfs mountdefaultencrypted)'...
    init: starting service 'exec 5 (/system/bin/tzdatacheck /system/usr/share/zoneinfo /data/misc/zoneinfo)'...
    init: starting service 'exec 6 (/system/bin/bootstat -r post_decrypt_time_elapsed)'...
    init: starting service 'exec 7 (/system/bin/bootstat --record_boot_complete)'...
    
    Test: Boot bullhead, observe above logging change
    Change-Id: I59e2bd9e3d341bbed9537c972e47246d0a12769d

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68563030c355497a9cd0484eea34bc7eea6a11de1568ee6bbd9e7e5abd31e1f4
-size 36595
+oid sha256:dab402abac0ae37ea11cc0a3ae5bad6b41b83fae341bc9876c60eba83614b35a
+size 36629
