be073821de04306daa6a7191fcee2ba8a78a889e
Keun-young Park <keunyoung@google.com>
2017-04-27 16:25:06 -0700

    combine wait for exec log with service exit log
    - allows easier tracking of wait time from monitoring tools
    - this change also reduces unnecessary log spam
    - service exit log looks like this:
      init: Service 'exec 4 (/system/bin/otapreopt_slot)' (pid 611) exited with status 0 waiting took 0.060771 seconds
    
    bug: 37752410
    Test: reboot and check log
    Change-Id: I122902538697f33939eede548e39f155ec419e03

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01b931739a8ce9c1a182d2d02a2f2b83cc07be2d4f1e568dc4c2e0dab40c82aa
-size 36557
+oid sha256:afe19223e0ccd609ce5ad5533dc960cff55da91ef991e4d64cf19f827b6ea52c
+size 36661
