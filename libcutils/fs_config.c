482c003a3bf9c3af57ced21f55c03311f829c11a
Mark Salyzyn <salyzyn@google.com>
2017-04-28 09:43:36 -0700

    libcutils: fs_config.c mark vendor, odm and oem partitions in duplicate
    (cherry picked from commit f0b53d072688ecbe7e7d57631a74d59cfb914b15)
    (added "system/vendor/bin/hostapd" to list)
    
    Cover both direct and symlink indirect paths to the referenced
    files in the vendor, odm and oem partitions.
    
    Test: compile and hand-verify properties
    Bug: 37703469
    Change-Id: I5b3a887e904baee2ac193ac4a73aaaee0bbfdb9f

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bc2da55531fffa11ec9a7c0f59e3f5197940c01d72e2cf95d42a2675600aeae
-size 16297
+oid sha256:1c309af6472efd5b2be2b0577394f6c953a00d6b49c709bab2efef259d290cca
+size 17824
