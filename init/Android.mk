9a04b22e4faf7513bac11a4addd3c3b8960ee12e
Tom Cherry <tomcherry@google.com>
2017-05-01 14:16:41 -0700

    init: add an initializer for keychord_id_
    Add unit test to ensure all POD types of Service are initialized.
    
    Bug: 37855222
    Test: Ensure bugreport is triggered via keychord properly.
    Test: New unit tests
    Merged-In: If2cfea15a74ab417a7b909a60c264cb8eb990de7
    Change-Id: If2cfea15a74ab417a7b909a60c264cb8eb990de7
    (cherry picked from commit 7da548578c25683fe0082283303e16961df312da)

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eaec4ed244df5fd53b7daccd41edc3a7e53f772b8480ba6c0663b4284090826
-size 3483
+oid sha256:ae8994ed103bd512e5903b1f161dbf59492cba06f3f9f6e32d28cd2781fb5950
+size 3506
--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be4ff312d63916554bf345d32d0ffb435513f9edc79a5732559f81807d38a501
-size 36707
+oid sha256:fad4a7926de21b06996c68f6e9d6a4d5694abfe841a3eaff49b4567598f657ad
+size 36753
