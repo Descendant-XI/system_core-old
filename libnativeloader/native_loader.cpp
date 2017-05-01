c13ab9b68319d077bb0a344016224059269ae873
Dimitry Ivanov <dimitry@google.com>
2017-05-01 15:12:49 -0700

    Load vendor public libraries to sphal namespace
    Load vendor public libraries to sphal namespace
    if it exists - preserve old behavior of loading
    these libraries to default namespace if sphal
    namespace is not present on the device.
    
    Bug: http://b/37410104
    Test: cts-tradefed run singleCommand cts --skip-preconditions -m CtsJniTestCases
          on marlin (with enabled sphal configuration) and on angler where ld.config.txt
          is not present.
    
    Change-Id: Iaa3fa437ba2900acc2e5b9c78039fe1553e4c9dd

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff45d1c1644e165d07d012c8da5c772ba32e32c83f6673e33ba4a7f8b5a0922b
-size 18079
+oid sha256:4f45bc772e366586f137bf14b6a6e1dc55f37ded47449e1f92c9da8aaf0146d8
+size 19479
