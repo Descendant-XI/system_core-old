b6b3e2dcd4935485dddc47f7c65aeed8983705fc
Felipe Leme <felipeal@google.com>
2017-05-02 10:06:33 -0700

    Don't display bugreport progress when it recedes, for real...
    The previous fix was taking account just the progress reported by dumpstate,
    not progress/percentage. As such, it was not detecting the cases where the
    percentage decreased but the progress didn't.
    
    Bug: 37878670
    Test: m -j32 adb_test && ./out/host/linux-x86/nativetest64/adb_test/adb_test --gtest_filter=BugreportTest.*
    
    Change-Id: I5830028f3191a9b17f63aeed5c049b29fa7d1179
    (cherry picked from commit 4cc03611cdd8f381d199e63f353075d6c7a3c44d)

--- adb/bugreport.h
+++ adb/bugreport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9571b86aac24d7648effb2ee30ca16a9bb977137d34e321686d854233356a711
+oid sha256:3441848bdf31e159011aab1ae4a51c723045ddb0fff335103aa76d7e8f5c39bc
 size 1674
