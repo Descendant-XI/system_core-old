ecf8e7db0b3744aa0a6e9c0613bffe9b74fde815
Mark Salyzyn <salyzyn@google.com>
2017-05-03 15:26:07 -0700

    libcutils: fs_config: remove aliases
    Covered by the code automatically now.
    
    Test: gTest libcutils_test --gtest_filter=fs_config.*
    Bug: 37703469
    Change-Id: Iad6ba65e023845aaea7a181b277a7383c44bd937

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:394428c55c438dee33480feda1312966a9c6e4fa485e454e4e950391a06b3c19
-size 17862
+oid sha256:d52f347f88c3c5819a4c0a3bfc64add8a9bb407128cb476606d6e7a1014cdf43
+size 17640
