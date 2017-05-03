34f212bc879cf6010929d2f39179d531b1ad1016
Josh Gao <jmgao@google.com>
2017-05-03 14:10:39 -0700

    adb: add fdevent_run_on_main_thread.
    Add a function to run a function on the main thread, to allow fdevents
    that depend on a blocking function to be registered.
    
    Bug: http://b/37869663
    Test: adb_test on linux
    Change-Id: I84a0b372360420b7647057297b8f437e8afa874e

--- adb/fdevent_test.h
+++ adb/fdevent_test.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56b1942ad4a0030d6bb54fde6c456dda2d9798f036aa8847afd823a361c17e7f
-size 2002
+oid sha256:6f169b8ec0094fb93c1038fbd48715c13478c1bc86d9eeda328cb0a6ebc37dbf
+size 2044
