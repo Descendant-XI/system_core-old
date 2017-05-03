d3bcef6c4e2dfce4e012b4897bc22ae6fb052f1b
Josh Gao <jmgao@google.com>
2017-05-03 14:01:35 -0700

    adb: enable -Wthread-safety on linux/darwin.
    Delete the sysdeps/mutex tests that -Wthread-safety complains about.
    We're using the standard library's std::mutex on all platforms now,
    anyway.
    
    Test: mma
    Change-Id: I3bf958c72604b29dfb1d9c898d3c9aa34aed2685

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fb3201cfbe26886e8957bb8f5e59bfabfb535a796b187c9312b0e6d8e522530
-size 10548
+oid sha256:3a0035b5ccd3ff80f78b4e8cde6155153699796c441295445821f550ea09bb26
+size 10634
