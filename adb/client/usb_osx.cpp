f8b6a4bb88f3e08f5b8f6dac5bf0b4e71916c9ae
Josh Gao <jmgao@google.com>
2017-05-04 15:58:01 -0700

    adb: make darwin build with thread safety analysis.
    Test: mma on darwin
    Change-Id: I44dfc53fc381e60de83f3bc503691cdb8179ca91

--- adb/client/usb_osx.cpp
+++ adb/client/usb_osx.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0fe528902ad1dbf98536c49036b534f0365fe9a89136c2bcc128fd137f13a01
-size 18983
+oid sha256:e5be19ad615c99a5dc4b7bcd81deaf6ebe366ba7e0b4e866c25feedf0e7f27fe
+size 19054
