cf09c3f09e59827d6187eb4e7687dde477092bb7
Elliott Hughes <enh@google.com>
2017-05-03 22:43:23 -0700

    Clean up the partition name mapping in fastboot.
    Also remove -p.
    
    Bug: N/A
    Test: builds
    Change-Id: Iea55affc7262323c0d341003359d3175580efbfd

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45aecc3cc81c68a2ec1dd9050be4ca1244f6c1278ea58c4e467cea8e8927a024
-size 67736
+oid sha256:2e9e96b32d42bba8d89f66318320bfdfcdb56e3606321b560f6b6e9f3dcd760d
+size 67175
