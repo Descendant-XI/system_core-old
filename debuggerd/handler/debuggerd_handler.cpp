8b258b11201c297c2fe1d17d4ffb6da7cd38a0b6
Josh Gao <jmgao@google.com>
2017-05-04 17:12:57 -0700

    debuggerd_handler: use syscall(__NR_get[pt]id) instead of get[pt]id.
    bionic's cached values for getpid/gettid can be invalid if the crashing
    process manually invoked clone to create a thread or process, which
    will lead the crash_dump refusing to do anything, because it sees the
    actual values.
    
    Use the getpid/gettid syscalls directly to ensure correct values on
    this end.
    
    Bug: http://b/37769298
    Test: debuggerd_test
    Change-Id: I0b1e652beb1a66e564a48b88ed7fa971d61c6ff9

--- debuggerd/handler/debuggerd_handler.cpp
+++ debuggerd/handler/debuggerd_handler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dc591921b9d0c4b8e10d2cc73c54e68503366d29fbda2ed64029e16e731d580
-size 17149
+oid sha256:bcff189d1e7fa362ec8df7cacb1be7d03f0477973211d83cfe4851432dceb3a6
+size 17411
