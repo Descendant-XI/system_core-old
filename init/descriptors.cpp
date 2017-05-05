af94d19a52d087c90294cb74c999ef4afd7c541d
Mark Salyzyn <salyzyn@google.com>
2017-05-05 14:44:35 -0700

    init: add "+passcred" for socket to set SO_PASSCRED
    In the init scripts for socket, the type can have a suffix of
    "+passcred" to request that the socket be bound to report SO_PASSCRED
    credentials as part of socket transactions.
    
    Test: gTest logd-unit-tests --gtest_filter=logd.statistics right after boot
          (fails without logd.rc change)
    Bug: 37985222
    Change-Id: Ie5b50e99fb92fa9bec9a32463a0e6df26a968bfd

--- init/descriptors.cpp
+++ init/descriptors.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:558cccc8e1318007c95fbce39540e7a88e71415cf316fbcb8612547c702e2f06
-size 4218
+oid sha256:0db2f157f8dd668fb34e51a6b146471f888e14adf62a0d70700f6c4fa4759786
+size 4384
