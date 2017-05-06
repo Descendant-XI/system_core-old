0d461db90db6b52421245ca0976cf62975919223
Josh Gao <jmgao@google.com>
2017-05-05 18:11:23 -0700

    libdebuggerd: implement fallback register dumping on arm/aarch64.
    Bug: http://b/35439781
    Test: killall -ABRT media.codec on internal
    Change-Id: I7a23d3bfcf07ad584e677b2ef5fff28436ef0972

--- debuggerd/libdebuggerd/include/machine.h
+++ debuggerd/libdebuggerd/include/machine.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e3087f3b83e535bf133e9c47050e532956a3b7a40783db8283dddbef548b45d
-size 897
+oid sha256:8895e7a42e38caa1aea209a864dbf8ab238508ab7230a23eeb1907d6235108e6
+size 952
