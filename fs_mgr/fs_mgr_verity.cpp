0cfcc486bb7a9071880ae767b2dd0669f72e38e3
Bowgo Tsai <bowgotsai@google.com>
2017-05-08 17:15:20 +0800

    fs_mgr_verity: fix androidboot.veritymode=enforcing doesn't work
    Should set verity mode to VERITY_MODE_DEFAULT when
    androidboot.veritymode=enforcing.
    
    Bug: 38103331
    Test: `adb shell getprop partition.system.verified` returns 2 when veritymode=enforcing
    Test: `adb shell getprop partition.vendor.verified` returns 2 when veritymode=enforcing
    
    (cherry picked from commit 77cbfd6341a7a500fdb8e2f3cc4a4f721254f10e)
    
    Merged-Id: I2f34eb6509f91989ce726e540cf2c0d353347ede
    Change-Id: I2f34eb6509f91989ce726e540cf2c0d353347ede

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fa7e1ba87add245ac2bd0a262197220cd119992333e41083c67e40a70df948d
-size 24904
+oid sha256:80b81e8a9e57d90f96c0bc232515deefe5e0b9002f7ad7319afbca0f079700a6
+size 24898
