59c21d76aa250adbb5b763eb47a4b4430c1b7f98
Tom Cherry <tomcherry@google.com>
2017-05-08 13:38:15 -0700

    init: remove restorecon() from util.cpp
    restorecon() has become nothing more than a small wrapper around
    selinux_android_restore().  This itself isn't super problematic, but
    it is an obstacle for compiling util.cpp on the host as that function
    is not available on the host.
    
    Bug: 36970783
    Test: Boot bullhead
    Merged-In: I7e209ece6898f9a0d5eb9e5d5d8155c2f1ba9faf
    Change-Id: I7e209ece6898f9a0d5eb9e5d5d8155c2f1ba9faf

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a5426e3b5f416c1cdd4d00e183f5caa3e610584bc828b45c3393ef067135bb
-size 11891
+oid sha256:eca625ea8865b7e023ad7cf86352807b7c1880781c6d05dbccacffd93399407a
+size 11782
