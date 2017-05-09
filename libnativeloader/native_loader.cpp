c0249b12a64548833a958c479a038f575b230e72
Dimitry Ivanov <dimitry@google.com>
2017-05-08 22:24:24 -0700

    Explicitly enable greylist for classloader-namespaces
    The linker no longer enables greylist by default, it
    needs to be explicitly enabled by specifying corresponding
    flag.
    
    Bug: https://issuetracker.google.com/38146125
    Test: builds
    Change-Id: Ib593f2d9a35dbadffb436f5fbc9a2a7a8f64ada0

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c68fb940b12f0ae43980de28453c17eaa5b93897fbf9b2543b9fc31e3d15a8
-size 17671
+oid sha256:ff45d1c1644e165d07d012c8da5c772ba32e32c83f6673e33ba4a7f8b5a0922b
+size 18079
