cec3c5b0a52e28c6d425ecb243e3ee14656af29e
Dimitry Ivanov <dimitry@google.com>
2017-05-08 22:24:24 -0700

    Explicitly enable greylist for classloader-namespaces
    The linker no longer enables greylist by default, it
    needs to be explicitly enabled by specifying corresponding
    flag.
    
    Bug: https://issuetracker.google.com/38146125
    Test: builds
    Change-Id: Ib593f2d9a35dbadffb436f5fbc9a2a7a8f64ada0
    (cherry picked from commit 058b2ea8c52ed8ce33442eda3a39e49ecd11e6dd)

--- libnativeloader/include/nativeloader/dlext_namespaces.h
+++ libnativeloader/include/nativeloader/dlext_namespaces.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1b13034c6c1c4b5ddfac70a58da697a8f74500445dac11c276e1d7b3916d2cf
-size 5833
+oid sha256:7b0c20aab355c1d0d0de7fbeb9e073bbc6851065852fd0afde7f7b93396f734e
+size 6019
