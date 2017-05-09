20bf63537781f4507831116dea1d7863a6807100
Josh Gao <jmgao@google.com>
2017-05-08 18:37:17 -0700

    adb: make `adb kill-server` wait for the server to die.
    Make the host:kill service shutdown its socket on process exit, instead
    of immediately. Also, unify the two 'kill-server' implementations and
    hide _adb_connect.
    
    Bug: http://b/37104408
    Test: adb kill-server; adb start-server
    Change-Id: I9475f5d084d5fb91d33e393f2fd4e34056613384

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bf2cdf8ba103f4e5ca047cca04505d08ffee73dd14881c8b99d4a8d05ea016e
-size 45993
+oid sha256:73fdcc01c9dfe1f6ec0d2ea763b91f7014918bfbec168aa3cc0be8056a446232
+size 45819
