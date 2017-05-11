76e3312d258b1de01c247a8c601479b4f40f4378
Mark Salyzyn <salyzyn@google.com>
2017-05-11 13:28:33 -0700

    logd: logcat --clear respect pruneMargin
    While a reader is present, consider it a success, and not busy, if a
    buffer is pruned down to pruneMargin plus one second of additional
    margin of logspan.  If not busy, no need to trigger any mitigations
    regarding the readers, or to report any errors.
    
    Side Effects are we no longer mitigate the reader when performing
    chatty filtration. This is a positive side effect because we were
    getting --wrap wakeups that seemed premature.
    
    Add kickMe() and isBusy() methods to ease maintenance and uniformity
    of actions.
    
    Test: gTest liblog-unit-tests, logd-unit-tests & logcat-unit-tests
    Test: manual: 'logcat -b all -c' repeat in a loop, at various logging
          load levels, simultaneously 'logcat -b' all in another session.
    Bug: 38046067
    Change-Id: I3d0c8a2d416a25c45504eda3bfe70b6f6e09ab27

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bccc752549825164d3d712e63e5fc4ede6a90e6c25f2b9180d642e91be60b7c9
-size 43101
+oid sha256:5432386722bd820322e50870a9dcce985ebf26893587d8db0f3126b47396bdd7
+size 43339
