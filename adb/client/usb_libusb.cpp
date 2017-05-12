ff099d4a2937526873f772a194d7fb6a02aeaa9e
Josh Gao <jmgao@google.com>
2017-05-12 15:12:32 -0700

    adb: libusb: recognize devices with multiple interfaces.
    A bug was introduced by commit 8bf37d7a wherein we accidentally only
    look at the first interface of a device when checking whether a USB
    device was an ADB device or not.
    
    Bug: http://b/38201318
    Test: none
    Change-Id: I8e8e0963c77cd2cb03538d926ab735f4b57e52b7

--- adb/client/usb_libusb.cpp
+++ adb/client/usb_libusb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba590fc1695cfcb29d6290c166dfce37417a85f56ec35c5fdcdab986b1a904fc
-size 21732
+oid sha256:f812874b665aa63eabd3814dfb1fe75d3672457a5f13173d7df62dc394a5232e
+size 21738
