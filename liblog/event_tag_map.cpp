85e90c085c92715e90ba5dff00e9861160be185f
Mark Salyzyn <salyzyn@google.com>
2017-05-12 13:01:47 -0700

    liblog: tests: crashes in logcat.descriptive
    scanTagLine has some loops that do not check for a terminating nul,
    problematic for responses from the logger via __getEventTag.
    
    Test: gTest logcat-unit-tests --gtest_filter=*.descriptive
    Bug: 37791296
    Change-Id: Ib4e900b765b407bdee43e009d6625fe289863ddc

--- liblog/event_tag_map.cpp
+++ liblog/event_tag_map.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:387ed6ffd3073914c558445cf0259b168b5d9c354edbd1666c43d9225b84db2e
-size 19664
+oid sha256:e2bf16e2b67e7822aed38c0ed989b6f0d35d3c3d41ec7c73c6cacdda262189b5
+size 19696
