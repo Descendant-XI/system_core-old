f630f70f096570a7a03e3c7c1d012d03d4b069ea
Josh Gao <jmgao@google.com>
2017-05-12 11:21:30 -0700

    adb: libusb: switch to hotplug for device detection.
    Switch from polling in a loop to using libusb's hotplug API to detect
    when devices arrive and leave. Use this to remove devices that were
    inaccessible when they're unplugged.
    
    Bug: http://b/38170349
    Test: plugged in device
    Change-Id: Id157412eb46834debecb0cd45b47b1ced50c2274

--- adb/client/usb_libusb.cpp
+++ adb/client/usb_libusb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9d6808102642ba30411bbe62101db982483fb1df3e22197309dc2804ac8724e
-size 19908
+oid sha256:e8a5705b197e009aabb2e37ef02ee8b23819aa7b0f2d744f0cac1f6ea8ceafbf
+size 20141
