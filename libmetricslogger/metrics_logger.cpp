313e1eb10f955e7b8f32c773e6550a057b666bee
Ruchi Kandoi <kandoiruchi@google.com>
2017-05-15 18:03:35 -0700

    Add the functionality for logging counters.
    Also correct clang-format for libmetricslogger
    
    Test: Run libmetriclogger_test
    Change-Id: If61f0bdc394a43e249ec4691da70175eeaf4cec6

--- libmetricslogger/metrics_logger.cpp
+++ libmetricslogger/metrics_logger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d968483658f5f5ef7dc4832e484d14a6002002ca4cd4a351eed6fb38431b676a
-size 1173
+oid sha256:404eb898757ca256483383d4761229d4bdd530bbfbb8736d59dd9b789fcc3ffd
+size 1462
