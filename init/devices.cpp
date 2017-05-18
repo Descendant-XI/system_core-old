64e44c98ef45db1f5d85b938878b8e5d53d7cac3
Elliott Hughes <enh@google.com>
2017-05-18 09:13:15 -0700

    init should #include <sys/sysmacros.h>
    Bug: https://github.com/android-ndk/ndk/issues/398
    Test: builds
    Change-Id: I6428a6344a3ce649bd03ff9e9b0f156425c67f0c

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a6bae85f97cd1c0e61178ce91510e354e11050891d8290d6bcb692d61fb3334
-size 30196
+oid sha256:fd1be276c69b5bbe630c450f974702cd062a4fee87f9714ff02c81e9014f14dc
+size 30223
