0deefbdcd26e2097333321782325f8160d039c62
Dan Albert <danalbert@google.com>
2017-05-18 22:56:48 -0700

    Add --sync support to push.
    Passing --sync only copies files that are older on the device.
    
    Test: nose2
    Bug: None
    Change-Id: I2ff6c3d1fe29262c8ee50db316aa92fc38dd7147

--- adb/file_sync_service.h
+++ adb/file_sync_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:542b2e3cefb1bccfebaeb200924ce6c0eaa5e473073a761673e20515952457ff
-size 2715
+oid sha256:63daae44f5cacfaf5f6034a5bd25401ef95eab764956feaa52dac4efb2ad7ddf
+size 2726
