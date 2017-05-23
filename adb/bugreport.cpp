8ec16940bc3aed8569c6e8d25e81e38ac658d062
Felipe Leme <felipeal@google.com>
2017-05-23 16:56:47 -0700

    Always print the initial bugreport progress message.
    Otherwise, it might take seconds to display the initial percentage,
    and the user might thing something is wrong and CTRL-C the process.
    
    Fixes: 62003852
    Test: added BugreportTest.OkProgressZeroPercentIsNotIgnored
    Test: m -j32 adb_test && ./out/host/linux-x86/nativetest64/adb_test/adb_test --gtest_filter=BugreportTest.*
    
    Change-Id: I8e7dbb28666c825c1408d5d204f813064fc3048f

--- adb/bugreport.cpp
+++ adb/bugreport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b01aa5bddf3275c0ec7611b3e85dca27a49c214658563209e2e6e57d840a8e76
-size 11366
+oid sha256:8b93891cd5ae833ea0eee554179f62b887f588cb20af2f393cae6e67762cdcb7
+size 11394
