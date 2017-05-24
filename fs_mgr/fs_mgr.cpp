013020d404ec44a692934e54897295d942468444
Bowgo Tsai <bowgotsai@google.com>
2017-05-24 18:51:36 +0800

    fs_mgr: fix SIGSEGV when ostream << nullptr
    fstab_rec.fs_options might be nullptr when printing error message.
    Use android::base::StringPrintf() to '(null)' when needed.
    
    Bug: 37759782
    Test: Boot device and manaully trigger the output
    Change-Id: I1bdf4ba57331aaea9dd5e790f6bf9d9b8bdc8b53

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49405a61386aeb1ec1fdcc6c4aba157d4b579ed9808939f25d9bc715457b00b9
-size 49229
+oid sha256:4ddd2a11c8b947286cc60a61f877761db44a980f34e6a7af4b63314cdd29951c
+size 49432
