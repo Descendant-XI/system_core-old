b86afaa24484e196804659b9edc11118fdb2ac63
Josh Gao <jmgao@google.com>
2017-05-30 15:31:02 -0700

    crash_dump: add tracing.
    Add some tracing to figure out where time is going during a dump.
    
    Bug: http://b/62112103
    Test: systrace.py sched freq idle bionic
    Change-Id: Ic2a212beeb0bb0350b4d9c2cd7a4e70adc97752d

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adaad5e0ec349921f2cb5670021a7940df765d5e5dcd79889be7ac8c9aede192
-size 14323
+oid sha256:b9600b7b5419a3a4265b3bf5bd1778e31d0981b2483561af30b833b7bd1490fc
+size 14901
