eaaeaaf067ef4d34fe7c5e58b30230293e57c43f
Yifan Hong <elsk@google.com>
2017-06-01 14:34:29 -0700

    libbatterymonitor: vendor_available
    libbatterymonitor is a static library.
    Also removed unused libbinder dependency.
    
    Test: pass
    Bug: 111454508
    Merged-In: I1f17ee643e6ed1bb2c11a6f3189a448dba2927ac
    Change-Id: I1f17ee643e6ed1bb2c11a6f3189a448dba2927ac
    (cherry picked from commit ffff9aae0219091662811b5949ed3767d7df12aa)

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8f1d57eeb2d24e8bd52d313323f6638d4e6499cc5ceb47d9763937f1f283052
-size 5238
+oid sha256:9b001428febbc51d108565e87c9da94d0c39ec1ee8f72c8101c63f126983c420
+size 4963
