263d8b16a689faaceb553f8e4c17b6d536131c0a
Romain Guy <romainguy@google.com>
2017-05-31 19:13:47 -0700

    Send color space information with screenshots
    Version 2 of the framebuffer protocol in adb can now be used to read
    the color space information of the screenshot: unknown (native),
    sRGB or Display P3.
    
    Bug: 29940137
    Test: Manual
    Change-Id: I037da58e1c4f6e9e874d72348140c067640c6931

--- adb/framebuffer_service.cpp
+++ adb/framebuffer_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2aa16118f91eb7b268fed48e60733e2acea34c3ef689c2782d5adb4b8e9786
-size 5318
+oid sha256:41a9b86a5b68457c34d63e93df36bd0754c4e59d8a9fd35419de222459c97a7d
+size 5445
