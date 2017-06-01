3a7b044173267130f1cf546f488e2045adcd0f8a
Josh Gao <jmgao@google.com>
2017-06-01 12:08:10 -0700

    crash_dump: don't notify ActivityManager if it crashed.
    Bug: http://b/38427757
    Test: killall -ABRT system_server, plus added logging
    Merged-In: Ic15e0b0870b1ec08a2f165ad0e5356afed02eece
    Change-Id: Ic15e0b0870b1ec08a2f165ad0e5356afed02eece
    (cherry picked from commit b0e51e388b8c952e28280f0e7035dc18fb9aa4e5)

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26e9bc3f502dae34ebfe6850506ee9e0aa3d376498d617f1f8a5cd79990b85eb
-size 13779
+oid sha256:df3d901cf313d4e3f26b4aab9689cccb3f63366e4bcc57ae67d70a3a63203a57
+size 14018
