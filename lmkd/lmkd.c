f71fef60abbccd408d8f19efdc8c09f52eb19d67
Robert Benea <robenea@google.com>
2017-06-01 16:32:31 -0700

    Make lmkd use medium/critical mem pressure, and update soft limit based on adj score.
    Test: GO device (512MB/1GB), both show improvements under heavy load.
    
    Bug: 62626918
    
    Change-Id: I98afc8b1171db5b57056bc05d1f1ae9c5eed8506

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7025fd6d36bd4222e2af981cd28f5a0cfb937ea20c1b072cdd696e9659a82be
-size 20452
+oid sha256:9ba4351cc7b3e402229536e8b3df146c1a3dfd54106385ffe0f102f9eca7a6bf
+size 21026
