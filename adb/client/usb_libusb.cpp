9b6b2a8ed7446e27492e2391018789b5d194212e
Josh Gao <jmgao@google.com>
2017-06-05 15:08:13 -0700

    adb: libusb: don't try to delete a usb_handle twice.
    Previously, we would attempt to delete a usb_handle in both
    device_disconnected and usb_close. If the one in device_disconnected
    happened to happen first, usb_close would abort when it failed to find
    the handle it was supposed to own.
    
    Bug: http://b/62020217
    Test: unplugging device on darwin
    Change-Id: I6c6bf61bf89a4d9a23458c00b457080d3d6cc744

--- adb/client/usb_libusb.cpp
+++ adb/client/usb_libusb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f91cc5ddf4e7961a92e3e1c713a4366ecbc770d9969137a077313ef5f64be76
-size 22483
+oid sha256:79b64bd9df14a29a4295bd4f6833824bcd0a580953db1e3e73d15d6d0da6bd27
+size 22571
