2e2ac17bf0c5145cb6564a0da94a7c82027dbc48
Jaegeuk Kim <jaegeuk@google.com>
2017-06-05 09:24:19 -0700

    fs_mgr: call format_f2fs correctly with -f
    This patch formats f2fs with -f option.
    
    Change-Id: I3fc415f35d81f22cbf1eadebc619465d36205927
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- fs_mgr/fs_mgr_format.cpp
+++ fs_mgr/fs_mgr_format.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:000065a49f7b6258fb5c78cdd2b9d9d1f8fe665da645b12d643ebe1180149104
-size 3743
+oid sha256:815dd81566b9b3f5ae4dae47f74f8b331f78c0dcfc199f0d26439d1f84055b4e
+size 3803
