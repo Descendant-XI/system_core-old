c22f34e9ee3538532cbde7c15e87f7eca049d2cf
Josh Gao <jmgao@google.com>
2017-06-07 12:11:19 -0700

    adb: remove SendConnectOnHost.
    This logic appears to be racy, and it shouldn't actually be needed, if
    our devices follow the USB spec. Use libusb_set_interface_alt_setting
    on device initialization as well, to add one more thing that should
    reset the data toggles.
    
    Bug: http://b/32952319
    Test: python test_device.py
    Change-Id: I392198af3d72c524b893e5056afa2b4617cea49c

--- adb/transport.h
+++ adb/transport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa0aa1dffd907203c937bcd959aab1b74d137d3d5f8b82c5c11a6c227a03bc32
-size 8113
+oid sha256:879cb0cd7d9ad52ea104572d7f32e5a9cf737bc1cf0a7930cc497052967ae46f
+size 8034
