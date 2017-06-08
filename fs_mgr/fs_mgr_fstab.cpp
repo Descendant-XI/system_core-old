12cd12c2f6afe7fc0287e320ac6da1bfb8b289ea
Bowgo Tsai <bowgotsai@google.com>
2017-06-08 10:43:41 +0800

    first stage mount: removing the restriction of mount points
    Current first stage mount only allows three mount points: system, vendor
    and/or odm. This was introduced by project Treble to mount those
    verified partitions early. However, there might be some other custom
    partitions needs to be mounted early as well. This CL removes the
    restriction and does first stage mount for whatever specified in
    fstab-dt.
    
    Bug: 62423887
    Test: first stage mount /vendor with vboot 1.0
    Test: first stage mount /vendor with vboot 2.0 (AVB)
    
    Change-Id: I6c146c64e673c35c2823523ccbde193590430c48
    Merged-In: I6c146c64e673c35c2823523ccbde193590430c48
    (cherry picked from commit 06ed613e7363bd0b67aa7899f1c813c1a098d15f)

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef810022f28299c2390df2ef8031b0b90f9e4c0f674cfa9ab8dd4458ed561fc
-size 30345
+oid sha256:260c55f0514cf7ac800bec2932ab0a4ff0bab4570407b206d0d41859bc45a1c9
+size 30143
