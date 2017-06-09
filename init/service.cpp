3bc39681710607d775a37b58a1912e1d44157951
Tom Marshall <tdm.code@gmail.com>
2017-06-09 18:29:23 +0000

    init: Run restorecon_recursive asynchronously
    restorecon_recursive may take a long time if there are a lot of files on
    the volume.  This can trigger a watchdog timeout in any process that
    tries to set a property while it is running.  Fix this by running
    restorecon_recursive in its own process.
    
    See https://jira.lineageos.org/browse/BUGBASH-555
    
    Change-Id: I2ce26ff2b5bfc9a133ea42f4dbac50a3ac289c04

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e69d9e61517f5c99b0987b5effeee3131783c0f55d47211e01934bdb3f4e92e5
-size 38461
+oid sha256:b1dc62d1c9d08322d536226f73ac91783397bc48bf3795f81c8fffdf9d49ccd2
+size 38523
