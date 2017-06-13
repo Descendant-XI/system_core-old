c32210f572a5e65bd51e960b7634e6d89e20a6e8
Hung-ying Tyan <tyanh@google.com>
2017-06-13 11:23:06 +0800

    Revert "Revert "Load default prop from /system/etc/prop.default""
    This reverts commit 98a73a2ce10df46bc8d095413b415e284206836b.
    
    Bug: 37815285
    Bug: 62525809
    Test: Tested with ag/2398663 and ag/2400524. Booted pixel phones,
          checked the location of pro.default, verified the symlink
          at /default.prop, checked a few properties via adb shell and
          manually tested a few apps.
          Booted to recovery mode and ran 'adb sideload' successfully.
    Change-Id: I407412a7002b898ffb352cb5f331cab9c15be39a

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c10a4068b76f674a0d4c1eb763c90403942caffd123f4dfb217777ff227b8b
-size 20400
+oid sha256:2bd4208b24dfe44a6818c00d7c0c1001e1a740513bf2698d4d9bc76ec74ab965
+size 20528
