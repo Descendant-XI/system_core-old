e50b1303874f2d18b817b88fc687c47710f8091d
Jeff Vander Stoep <jeffv@google.com>
2017-06-14 11:15:00 -0700

    Remove restorecon of file_contexts.bin
    file_contexts.bin is dead. Long live split file_contexts.
    
    Test: build and flash angler.
    Change-Id: I962743ce3e98aefee14d972e04bf4e68bec633a5

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faefceb71179addc7f25e25e758dd9899005e37cc31e30d5deee13c2ce568939
-size 41216
+oid sha256:7cbf9101838244415596ab7de33cb3a9b3d583b927c1792d0e04b6adf24002c5
+size 41159
