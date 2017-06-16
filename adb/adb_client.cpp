7e1fa6f7d9f1d3994ea58c8c9628819e1ca6f046
Josh Gao <jmgao@google.com>
2017-06-16 15:34:34 -0700

    adb: increase the shell command length limit.
    Relax the shell command length limits when talking to an adbd with the
    shell protocol.
    
    shell is pretty much the only service that takes an arbitrarily long
    string, so this is somewhat safe.
    
    Bug: http://b/37716055
    Test: `adb shell $(python -c 'print "echo " + "f" * (32*1024)') | wc` on L and master
    
    Merged-In: I0737fd2244530ef8080f300cd3a3549a1ab93465
    Change-Id: I0737fd2244530ef8080f300cd3a3549a1ab93465

--- adb/adb_client.cpp
+++ adb/adb_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31fd77776a3595c9aa72a795cd65bae116ec49b0dcda44d74158b8e3cdb0fac7
-size 9532
+oid sha256:9b25b3406fca4de99f72016158674ee0a5d78e2eb7d7cb44eb2040d7eef51ab3
+size 9529
