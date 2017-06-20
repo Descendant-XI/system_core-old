d79b65b7bf57c92fd4effd8de5f2c49961494b98
Tom Cherry <tomcherry@google.com>
2017-06-19 17:41:41 -0700

    ueventd: remove PlatformDeviceList
    In order to create symlinks for USB and block devices, the path for
    their parent platform device must be known.
    
    Previously, ueventd would save each platform device that it encounters
    to a list and query this list when creating the symlinks.  That,
    however, is racy because the uevent socket does not differentiate
    uevents from device_init() and uevents sent by the kernel when
    probing a device first the first time.  The below scenario is the
    faulty case:
    
    1) Kernel probes parent platform device for a block device
    2) ueventd calls device_init() and starts processing uevents
    3) Kernel probes block device and sends its uevents
    4) ueventd picks up the block device uevent during its uevent processing,
       without yet regenerating the platform device uevent, causing improper
       symlinks to be created.
    
    This change stops storing the platform devices in a list, and instead
    traverses up the directory structure for each USB or block device
    until it reaches a platform device, defined as one whose subsystem is
    the platform bus.  This fixes the race and simplifies the ueventd
    code.
    
    Bug: 62436493
    Bug: 62681642
    Test: Boot bullhead
    Test: Boot sailfish
    Test: Boot hikey + hotplug/unplug sdcard
    Merged-In: I21636355d8e434f30e0cba568598a6cf139e67f9
    Change-Id: I21636355d8e434f30e0cba568598a6cf139e67f9

--- init/devices.cpp
+++ init/devices.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4fd518bf82252c5ac21a488709a8e2fe58e61a26dc2efec64c03632a38cc3cf
-size 31991
+oid sha256:fc676592d7129314d926b6cb367213537800e74d71e5c0ab9dc0f4cf5e6587a9
+size 31201
