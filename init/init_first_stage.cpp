f9bd8ed9a46aa4de23160ddcc647b31b69bd3907
Tom Cherry <tomcherry@google.com>
2017-06-21 18:10:47 +0000

    Revert "init: poll in first stage mount if required devices are not found"
    Bug: 62864413
    
    This reverts commit 5f4e8eac8a0db87a43ed7a930103f4241c22d479.
    
    Change-Id: I6f35b920bb52875c8ef430d3e20b205fda5165e8

--- init/init_first_stage.cpp
+++ init/init_first_stage.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:016752605c0dafc3f0af3ab16270eacf92eb6cea5f0d5df06a3dee6cd0f6a18c
-size 17675
+oid sha256:1e9598d8c5ac7221b4fb3e674a31439052228291c13fe1f4d0972735702b610a
+size 17057
