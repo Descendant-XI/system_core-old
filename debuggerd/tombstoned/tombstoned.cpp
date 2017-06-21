5ea6aac56f3a284f7a0527a8203c0730cdb65b16
Narayan Kamath <narayan@google.com>
2017-06-21 19:42:00 +0100

    tombstoned: Improve message on java trace completion.
    For java traces, log the kind of dump as well as the PID of the
    completed dump. This makes it easier to correlate dump requests with the
    actual file they're written to.
    
    Sample log statement:
    E /system/bin/tombstoned: Traces for pid 4737 written to: /data/anr/trace_00
    
    The message for native traces / tombstones remains unchanged because
    several tools parse it.
    
    Test: manual
    Bug: 32064548
    
    Change-Id: I7b3792dd5ae312ee0bc055c22ec3f7c747152072

--- debuggerd/tombstoned/tombstoned.cpp
+++ debuggerd/tombstoned/tombstoned.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a29dfab62a643af59931c2a51bef2239ddfe954d62ec3526be9b22c2633bc07
-size 12779
+oid sha256:60686b5cd6753a0da603a61f2b3453d2fed606a804af61f1ff1ab8e1217eac2f
+size 13167
