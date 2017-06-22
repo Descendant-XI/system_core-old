31d2daec10337d2f2358c8811e68b6b77d5a85cf
Ben Fennema <fennema@google.com>
2017-06-22 15:15:56 -0700

    libcutils: fs_config: fix "system/<partition>/" aliasing
    Pull prefix checking into fs_config_cmp and make prefix_cmp be a
    glorified partial ? strncmp() : strcmp()
    
    Results before:
    [ RUN      ] fs_config.system_alias
    
    [ ERROR ] system/core/libcutils/tests/fs_config.cpp:143:: vendor/lib/hw didn't match system/vendor/lib
    
    [ ERROR ] system/core/libcutils/tests/fs_config.cpp:143:: system/vendor/bin/wifi didn't match vendor/bin/wifi
    
    [ ERROR ] system/core/libcutils/tests/fs_config.cpp:143:: system/odm/bin/wifi didn't match odm/bin/wifi
    
    [ ERROR ] system/core/libcutils/tests/fs_config.cpp:143:: system/oem/bin/wifi didn't match oem/bin/wifi
    system/core/libcutils/tests/fs_config.cpp:247: Failure
    Value of: check_fs_config_cmp(fs_config_cmp_tests)
      Actual: true
    Expected: false
    [  FAILED  ] fs_config.system_alias (6 ms)
    
    Results after:
    [ RUN      ] fs_config.system_alias
    [       OK ] fs_config.system_alias (0 ms)
    
    Bug: 62204623
    Test: gTest libcutils-unit-test --gtest_filter=fs_config.*
    Change-Id: I8282827002816e86ea3014fed29eabf43b837815
    Signed-off-by: Ben Fennema <fennema@google.com>

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa6ae5db98ba586a4abfbb7f3213f516afaeb2692bf8df80ab6aae0295336b97
-size 17389
+oid sha256:1f1f469e24214c7553769a199ec8a9f5e4e4886f3d9295357b0f17cd54d969fc
+size 17509
