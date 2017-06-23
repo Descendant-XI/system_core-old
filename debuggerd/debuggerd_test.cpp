12bcde3ce8e82d0ce2b4bba0b80e8d363a36f968
Andreas Gampe <agampe@google.com>
2017-06-22 20:14:43 -0700

    Debuggerd: Fix debuggerd_test
    Follow-up to commit 695713e931f0436aca56be9c0dacf2a5dd4e56e7 in
    bionic. Change expectations of the abort callstack.
    
    (cherry picked from commit 26cbafb89277415109706bdd0b0813a439c2d0dc)
    
    Bug: 62810360
    Merged-In: I350a29ee1713a7ebdd50f2b9bdc2078c671e22c7
    Test: mmma system/core/debuggerd
    Test: adb shell /data/nativetest/debuggerd_test/debuggerd_test32
    Test: adb shell /data/nativetest64/debuggerd_test/debuggerd_test64
    Change-Id: I350a29ee1713a7ebdd50f2b9bdc2078c671e22c7

--- debuggerd/debuggerd_test.cpp
+++ debuggerd/debuggerd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c83ffac05e6a2c829243a9d24a966a07adf2fcf91264a9448848cef7a9be28c
-size 21367
+oid sha256:6afa4b33650a16142ef343c5c948c61cd5f366be4ebcc9c82ed059324d1092f6
+size 21364
