2dd235816754461b158e8be0d75bb10f7967ed37
Andreas Gampe <agampe@google.com>
2017-06-22 19:45:53 -0700

    Debuggerd: Extend crash_dump timeout to 5 seconds
    Some processes have lots of threads and minidebug-info. Unwinding
    these can take more than the original two seconds.
    
    Bug: 62828735
    Test: m
    Test: debuggerd_test
    Test: adb shell kill -s 6 `pid system_server`
    Merged-In: I0041bd01753135ef9d86783a3c6a5cbca1c5bbad
    Change-Id: I0041bd01753135ef9d86783a3c6a5cbca1c5bbad
    (cherry picked from commit b02851a984391d25bfc5b3094f281432906b47ed)

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df3d901cf313d4e3f26b4aab9689cccb3f63366e4bcc57ae67d70a3a63203a57
-size 14018
+oid sha256:52389ce56edbdd6fe3600947370fc2f565c5bcafd2fc1fe37961b01a303ee229
+size 14157
