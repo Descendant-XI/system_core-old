66dbd89fb367e972cfbb91704f3da8b0bbed383e
Jeff Vander Stoep <jeffv@google.com>
2017-06-23 13:36:13 -0700

    Revert "crash_dump: during early boot, output to kmsg on userdebug."
    This reverts commit bf2dd482412cb7b93f52d2ed2d9be9a32fa8d2f9.
    
    Addresses:
    avc: denied { relabelto } for name="kmsg_debug" dev="tmpfs" ino=10642
    scontext=u:r:init:s0 tcontext=u:object_r:device:s0 tclass=chr_file
    
    Bug: 62101480, 35197529
    Test: build and boot device. Verify selinux denial no longer occurs.
    Change-Id: I28ce16f50eec20ef15c1721f41b66f22a84e7cca

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faefceb71179addc7f25e25e758dd9899005e37cc31e30d5deee13c2ce568939
-size 41216
+oid sha256:96556db7e3fb9aefdd04ecfe09fbfd52f941f9242d96401e3fe00d28d998a323
+size 40985
