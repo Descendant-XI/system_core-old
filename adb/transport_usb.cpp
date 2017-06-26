aa6b185f119ee667ec86a78e7349fb54c42fee44
Josh Gao <jmgao@google.com>
2017-06-26 12:16:30 -0700

    Revert "adb: turn on libusb by default."
    This reverts commit f2f0b318508fec66fc07044b34f602960f399e5b.
    
    libusb seems to be causing flakiness on some machines. Disable it for
    now.
    
    Bug: http://b/62962248
    Test: python test_device.py (with DeviceOfflineTest commented out)
    Change-Id: Ia9de78ab772c22574cf5ca7facb78f22af6d7a71
    (cherry picked from commit 969110ca4aa8dd26a8c77f681b19d6f74c11cd93)

--- adb/transport_usb.cpp
+++ adb/transport_usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7be45215059231a5a0b1290fd4e75d56346bdcb2996984e7e52ad65fab2af23d
-size 5778
+oid sha256:e7ebac06380b72a7f6ac33547ce19d2b44a8239d032bd8d98d9219226985070a
+size 5775
