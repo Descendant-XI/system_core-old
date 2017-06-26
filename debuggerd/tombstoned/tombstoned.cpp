90a5ecd5bf5166afd79ffd0ce7478a2dacc3f895
Elliott Hughes <enh@google.com>
2017-06-26 13:54:05 -0700

    Allow configuration of the number of tombstones.
    Bug: http://b/62810514
    Test: altered the property, got more tombstones
    Change-Id: Iba8089915fa715658d2dfecb076c6a61321243bd

--- debuggerd/tombstoned/tombstoned.cpp
+++ debuggerd/tombstoned/tombstoned.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bed2ecab002ce0ad6f85e6b0e55add1117492ddf2b68a8b6665ed46f002c728e
-size 12781
+oid sha256:41f522fbb1435fa9b38c8f11d853a8969c7703a8ff8e8b39d1d499e85be2e968
+size 12826
