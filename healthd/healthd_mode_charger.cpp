a83bcd3184c65ae6a46b674f9263a528144088fe
Mark Salyzyn <salyzyn@google.com>
2017-06-27 09:19:09 -0700

    healthd: check console-ramoops-0
    On later kernels /sys/fs/pstore/console-ramoops becomes
    /sys/fs/pstore/console-ramoops-0
    
    Test: none
    Bug: 63058217
    Change-Id: I05be83e40dfc1730ed62b74a146123cdb8573ca6

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d992075ca68ce805a1ec855f7b0d8bd878a6aa341b0a2de276389024db0af701
-size 26424
+oid sha256:8e22f0bcb491716a3dd0f32a87f81326bb76d8c0c6f71ec09add63009133ea44
+size 26521
