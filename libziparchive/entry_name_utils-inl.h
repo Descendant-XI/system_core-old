c243a815c4fe0ed47bcf0b57f802a37180f758b5
Jiyong Park <jiyong@google.com>
2017-06-30 17:23:33 +0900

    Ran clang-format on libziparchive sources and headers
    Test: build aosp_arm
    Change-Id: I469b82b68f2c457f480fb9cd9da2026672985ce3

--- libziparchive/entry_name_utils-inl.h
+++ libziparchive/entry_name_utils-inl.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f26868883994e6730ee03d3eb7897f471bcb14dabbf43614209cfec7c00194fb
-size 1776
+oid sha256:e1a46c35f95c76eb9aa48a77458c663a60958d9e1df2fdc2d7f3d1829b87d602
+size 1775
