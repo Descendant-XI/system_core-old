08d406d2222a8084ee19cc2822a7ef7df0c5f660
Bowgo Tsai <bowgotsai@google.com>
2017-07-05 15:37:15 +0800

    fs_mgr: allow no verity metadata when the device is unlocked.
    To boot with generic system.img for project Treble, we should allow no verity
    metadata when the device is unlocked. The previous fix checks system property
    "ro.boot.flash.locked" but it's unavailable during first stage mount.
    This CL checks "androidboot.verifiedbootstate" in kernel command line instead.
    
    Bug: 63268209
    Test: boot sailfish without metadata on /vendor
    
    Change-Id: Ifd1dbeb2a2f09cd06903ecdd59bc94b3905a3fbd

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b8c794eaaf370a8fcdcd876e4abae400503b84aa6f384719a7965f421cfb46c
-size 49704
+oid sha256:9b2d762858563e9ed411cad9f1a347159c33402481155c8355555e87784be917
+size 50089
--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f756d0fdf24ae2f8408760357c48441ab2a7d60225537f5fbee92a0ed8998f3f
-size 26459
+oid sha256:a39c54a6a78dde5027eb717ed5cbc26bfcbf425ca242e6184ccee1a267a731fb
+size 26424
