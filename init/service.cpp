0ad1d35b29cc1b10deed1755c7cfc99663e14e6c
Keun-young Park <keunyoung@google.com>
2017-07-05 11:38:44 -0700

    add "shutdown critical" to service
    - "shutdown critical" prevents killing the service during
      shutdown. And the service will be started if not running.
    - Without it, services will be killed by SIGTERM / SIGKILL during shutdown.
    - Even services with "shutdown critical" will be killed if shutdown
      times out.
    - Removes ueventd and vold from hard coded list. Each service's rc will
      be updated to add "shutdown critical". watchdogd is still kept in the list.
    
    bug: 37626581
    Test: reboot and check last kmsg
    
    (cherry picked from commit cccb34fce8798f2a359c6eb0e394ef9460673629)
    
    Change-Id: I3c6aeb7151e64beca4b435f843ae64455217262d

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e69d9e61517f5c99b0987b5effeee3131783c0f55d47211e01934bdb3f4e92e5
-size 38461
+oid sha256:ddbeb20ecbacdca9fafe537e15497580e4d8af89c4b377fbd113d5ac3d869781
+size 38734
