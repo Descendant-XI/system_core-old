c553de957a63eb550fefc8e7569fffe0086f265b
Jin Qian <jinqian@google.com>
2017-07-06 11:43:45 -0700

    init: require e2fsdroid and mke2fs when building init
    init calls fs_mgr to format ext4 partitions. This requires
    e2fsdroid and mke2fs in /system/bin/
    
    Bug: 35219933
    Change-Id: Ia32fe438cd9b9332f8e18e0cbe7f61bd050adcb1

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:911bebbcaeebfcb38b6348a4b17dd38bee5b5c0f666c5de076bd99020c383ee8
-size 2053
+oid sha256:db869f74a4388a8fdbee82c9b427c61027854448824a538489f57638d3f30e4e
+size 2111
