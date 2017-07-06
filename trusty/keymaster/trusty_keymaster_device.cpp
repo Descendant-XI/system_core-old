0f4182e3325118179ce895767fb5d961d142b976
Jocelyn Bohr <bohr@google.com>
2017-07-06 16:24:01 -0700

    Enable non-secure side to receive messages > 4K
    AttestKeyResponse may be larger than 4K (always less than 8K) when
    attesting an RSA key. This change allows the non-secure side to read a
    response that may be larger than 4K by adding an additional bit
    indicating the end of a response. If a message command has the
    KEYMASTER_STOP_BIT set, then the non-secure side knows that the response
    has been fully read.
    
    Test: android.keystore.cts.KeyAttestationTest#testRsaAttestation passes
          with production attestation key and chain, when AttestKeyResponse is
          larger than 4K.
    
          Tested with other CTS tests when keymaster messages are smaller
          than 4K, still passes.
    
          Manual test to verify that a tipc error due to large message size is
          handled correctly.
    Bug: 63335726
    
    Change-Id: I8776ba7ca70da893648e15cfa770784ab31a2cb0

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1de60e1fb512024b7bffb8474c54ff0dc13df2ed2e088719bd043316a8efdd82
-size 28631
+oid sha256:40b4f930dda411cc8835bd1d6c64d2c5b8a109a78f9bb5620f50f8ee7f15b759
+size 28766
