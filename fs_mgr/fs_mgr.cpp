675dd3dd0aec4e75352aac2e3f91669fe9e60dab
Luis Hector Chavez <lhchavez@google.com>
2017-07-10 15:25:55 -0700

    fs_mgr: Always mount tmpfs with MS_NOEXEC
    This change adds MS_NOEXEC to the list of flags used in
    fs_mgr_do_tmpfs_mount().
    
    Bug: 62419743
    Test: aosp_bullhead-eng still boots
    Change-Id: I3ee3bb825a59437eba48e3a5b898dd2e385a9ab7

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c71260c63230a63902b96edb6f6493c9abbc1da8d862fc6ef83a17f5be5fbe9
-size 50096
+oid sha256:737850c64184a0f0bc7a9c1edd5998c103092b4d884ea190596b7fc1864872ad
+size 50108
