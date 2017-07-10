11e30f9cf1ed11cc3380f921a55dc20e17bcfa3e
Jin Qian <jinqian@google.com>
2017-07-10 16:04:41 -0700

    fastboot: call mke2fs to format ext4 filesystem on windows
    Bug: 35219933
    Change-Id: I0cc8c165176e8dc9cbe4b6a52679937bc872e2d0
    (cherry picked from commit 29fc859a6de4f5566652c4f5750e91c965d4b97f)

--- fastboot/fs.cpp
+++ fastboot/fs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1ee424ea7e90c8a25b07fd3ce6a18d62c1f1df5f3cf1a13f4ac61517da4ea6e
-size 5666
+oid sha256:20905206b57ecd7f1a27baa528187181c28c61f958d2584ecfe259a0497d7f18
+size 6694
