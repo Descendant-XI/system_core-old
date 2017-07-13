a4aa27b03b0d0414f55d368d2928402302803c4e
Luke Song <songwalker@google.com>
2017-07-13 15:10:35 -0700

    healthd: restructure healthd_mode_charger
    Refactor drawing code into separate class, and allow for split screen
    drawing with offset.
    
    Cherry picked from commit 7f386dcab98b4a2827b5ffe29d7d3de7637841c0
    
    Bug: 63541890
    Test: Verify charging mode animation
    Change-Id: I6c089460f55b8c2f75f4aa3153a5736f6f434b51

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:812bd75c93faceecd30223bb322fab60743283efa813dd04232c5cfbf5dacb20
-size 4655
+oid sha256:69bd46aaecd799574a7b6377e1dc18a1d11710f40836a60bafcfbabfc45241b0
+size 5285
