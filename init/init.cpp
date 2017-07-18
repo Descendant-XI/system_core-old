686a59f059f2450a4492c3abbc57042b935dedbd
Quang Luong <qal@google.com>
2017-07-17 18:03:13 -0700

    uml: init: add USER_MODE_LINUX cflag, USER_MODE_LINUX case in init.cpp
    Modified Android.mk to define cflag "USER_MODE_LINUX" if
    TARGET_USER_MODE_LINUX := true in BoardCofig.mk.
    Modified set_mmap_rnd_bits_action to return 0 if "USER_MODE_LINUX" is
    defined. This is needed since uml does not support the mmap_rnd_bits
    sysctl, and init would otherwise crash without this check.
    
    Test: manual
    
    Bug: 32523022
    Change-Id: I409ef64a1fa253bfb3f9fb59d0267be159819bb8
    Signed-off-by: Quang Luong <qal@google.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d2f596f2f944443d6e6aa71f1037a305ae052310c38c46389634493aef010e7
-size 39728
+oid sha256:7eaac8df8bfcea108b51b1b1889b9d2ef67f18948dd689513a4fea373ffff9fa
+size 39817
