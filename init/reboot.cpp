d9f5f68b30764ea80923f412542fa551c4ec4b97
Keun-young Park <keunyoung@google.com>
2017-07-18 10:58:28 -0700

    init: Do full shutdown even for thermal shutdown
    - Skipping SIGTERM / SIGKILL / umount brings race between block
      device driver and fs layer. Do umount before shutting down.
    - Reduce timeout to 1 sec for thermal shutdown and skip other time
      taking part like fsck.
    - Refactor waiting part to check time in ms so that 1 sec can
      have enough resolution.
    
    bug: 63686426
    Test: adb shell setprop sys.powerctl thermal-shutdown, adb shell setprop sys.powerctl reboot and check dmesg
    Merged-In: I048bac767b328c8d656a97fe65dde5f2b5bf4ae5
    Change-Id: I048bac767b328c8d656a97fe65dde5f2b5bf4ae5

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ba19c13fba5d211302904f94cf386ea6e0801f69df67ac6339351ad8cac3e41
-size 18863
+oid sha256:d6fb0cdddb85e11785344abca50b537a61655a561335b3aa49464e6e916c4d4a
+size 18916
