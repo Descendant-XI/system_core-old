7248e6cca05577ed18fd339a3744cb56d892f19a
Jin Qian <jinqian@google.com>
2017-07-19 15:19:16 -0700

    fastboot: add mke2fs and e2fsdroid to build package
    Bug: 23686092
    Bug: 63849632
    Change-Id: Iff0d92b7174597d43ee646847888aedc2080052a
    (cherry picked from commit 6ce78321a229083a4b7e06958232b2788886d193)

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9697a5aeabc3f849564ffd28dec34fc09fdcaf642a6b1a05c7fc99296a65c0d7
-size 3862
+oid sha256:ca055c0b3039f4e4c287a3aca0e746cf0ab7414278c8b21c80fc0cc3fbb35584
+size 4011
