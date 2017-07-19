4a3367fcd6e4f57600c64c0d438f9880e4d4c1d6
Tom Cherry <tomcherry@google.com>
2017-07-19 14:54:21 -0700

    ueventd: fixup ueventd_test.cpp
    Use ASSERT_EQ() instead of EXPECT_EQ() to prevent segfaults after
    failed API calls.
    
    Do not run setfscreatecon_IsPerThread unless we're in permissive mode
    as it will not pass otherwise.
    
    Test: init unit tests
    Change-Id: I70525d438e89f1ec036255890169a50b5007b4c4

--- init/ueventd_test.cpp
+++ init/ueventd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba00bd05bff9cc9ba4058cef2e92f477e66d90e6dbe281889a7e5d34d51be03b
-size 3701
+oid sha256:8454d8849fa8eefe6faa1138c865519f23626b1a0c4aa14e8210d760ba844e8a
+size 4046
