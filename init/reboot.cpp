5532768290e529897b05fc4146bd6f11455bc807
Wei Wang <wvw@google.com>
2017-07-24 13:08:41 -0700

    init: Fire shutdown trigger for thermal shutdown
    Recent change in init has bring normal shutdown sequence in
    thermal-shutdown condition. This CL will make sure init fire shutdown
    trigger where holds custom shutdown actions for vendor SoC/platform.
    
    (cherry picked from commit 1be2212319d1e74357a07aa2c98e5782dbf7997a)
    
    Bug: 63686426
    Test: adb shell setprop sys.powerctl thermal-shutdown
    Change-Id: Ieb8579fdf9c30c1a81d60466a7375c9784f3ca98

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9375c5e36dc91179e7e81f59c600d7f493c3be0e3bdb5855c51b2697714b879
-size 18963
+oid sha256:58cfb0e530cee26550e491ca8f03f38946f48df5603f37c03bd7484d0a3b351a
+size 18848
