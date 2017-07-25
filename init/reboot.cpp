c1dcca1b8f68e23563c20972c46e97675cd961e1
Wei Wang <wvw@google.com>
2017-07-25 10:52:08 -0700

    Do not umount roofs even if it is R/W.
    Latest device has rootfs instead of "/system" mount point
    
    (cherry picked from commit a01c27eef8e9361df63a62c916619fb809bc647b)
    
    Bug: 37737296
    Test: adb remount, reboot, and check log
    Change-Id: I315ecf71e85255fc55c3a80619920b456bad0956

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58cfb0e530cee26550e491ca8f03f38946f48df5603f37c03bd7484d0a3b351a
-size 18848
+oid sha256:f6228508b48193f33bcce48740b4525748aeb12e89e724560e5fefa45374d459
+size 18884
