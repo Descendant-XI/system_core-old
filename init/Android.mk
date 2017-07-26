ca0f0c68628c7b2260578ad0c35a0a9be3f3e5b9
Tom Cherry <tomcherry@google.com>
2017-07-26 14:17:09 -0700

    init: only use signed-integer-overflow sanitizer
    We've blown up twice in init due to the unsigned integer overflow
    sanitizer despite the overflows in question being both defined and
    intentional.
    
    Test: boot
    Change-Id: I08effe3202ac1367d858982ff5478b3a088bab37

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10212cc22c4c9077f12e7201797e22646dee67ec204ab906b739aa5e998dcafe
-size 2144
+oid sha256:27d40281f4ad0ff1b6f3440c441ae9aa3836453af29cb36804bcdc786800bcc9
+size 2160
