e1006bc8266b2e0c0934777d9e2dcf1b02eb2f4f
Jin Qian <jinqian@google.com>
2017-07-27 16:17:17 -0700

    fastboot: enable uninit_bg for ext4
    This speeds up mke2fs as well as e2fsck.
    
    Bug: 23686092
    Bug: 64032335
    Change-Id: I9f2d4c15e431647bb845d50a2361b2a9a091fba6

--- fastboot/fs.cpp
+++ fastboot/fs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1ee424ea7e90c8a25b07fd3ce6a18d62c1f1df5f3cf1a13f4ac61517da4ea6e
-size 5666
+oid sha256:9dbec2a0ffad972a5f18b7deec891dc63f93c204ca7c10d26e7ec660cb551595
+size 5739
