9ee090a255c2daedfd2a5d7cf0c4d2c3c142c9b2
Christopher Ferris <cferris@google.com>
2017-07-27 09:29:18 -0700

    Add support for the new unwind method.
    Also add a comment to the GetElf function to indicate that it never returns
    nullptr.
    
    Bug: 23762183
    
    Test: Built, nothing uses the new code.
    Test: However, I did run backtrace_test using this code, and all tests pass.
    
    Change-Id: I252b9c2497e2d3d94347dd6e506170bf50cbfe16

--- libunwindstack/include/unwindstack/MapInfo.h
+++ libunwindstack/include/unwindstack/MapInfo.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5b45ec0bafcb85ac3b930182b689a0a905c3613c069ec0fffad771ac6489618
-size 1385
+oid sha256:c7ed9ab70702b81ad37758dced1879c1637ce8998f315b891b90203909cf37d7
+size 1445
