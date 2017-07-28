4daa6d259f50935528d22f3b3042b83d0258efd8
Hung-ying Tyan <tyanh@google.com>
2017-07-28 19:09:33 +0800

    Move trusty reference implementations to /vendor
    Trusty implementations are provided by vendors. This patch moves
    the AOSP reference implementations to the vendor partition.
    
    Bug: 63085384
    Test: build gordon_peak which adopts trusty as the TEE and confirm
          that libtrusty and gateway.trusty are moved to /vendor.
    Test: build marlin which does not adopt trusty as the TEE and confirm
          that this patch has no effect on the build result.
    Change-Id: I9a5440071386b929058207fdef560ed2d7223ba3

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d295b8e7211cd802bf9ad96b68df5ffa61129eb5e2583e8b3c42d4fb6e1054
-size 3150
+oid sha256:5c20519b359628f5976cd30d3b33d15c81333f22117fdacf9610575ec7e0907a
+size 3772
