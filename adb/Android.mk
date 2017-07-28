54b9b85034bab9190bf289064b0550a1540554b3
Bowgo Tsai <bowgotsai@google.com>
2017-07-28 20:29:15 +0800

    Move adbd from root to system
    Bug: 63910933
    Test: normal boot sailfish, checks adb works
    Test: recovery boot sailfish, checks adb works
    Test: normal boot bullhead, checks adb works
    Test: recovery boot bullhead, checks adb works
    
    Change-Id: I1b27910c06a4172ca718b3344736ec8e3429f477

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e5791c24e129189304e53ad2a307487fe8985b2a8039b073c10514d585fcc90
-size 10946
+oid sha256:c9be35eb906c542aa82dbb5c14252ee88e54e3897f0e00a7a2d55d3a65edee11
+size 10841
