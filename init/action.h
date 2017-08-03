3f6d676202a5492f7d4b267a1c24677eb8b1091b
Tom Cherry <tomcherry@google.com>
2017-08-02 17:01:36 -0700

    init: use Result<T> for the parsing functions
    Test: boot bullhead
    Merged-In: I7f00c5f0f54dd4fe05df73e1d6a89b56d788e113
    Change-Id: I7f00c5f0f54dd4fe05df73e1d6a89b56d788e113

--- init/action.h
+++ init/action.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ee92bcfe16a3043f1f73288c9aedc358e3b9852cda9b2c02c7f49e5addfc564
-size 4414
+oid sha256:1d2d8098c6115d9abc289f30422f3ed6aaecd2ae11739db1be3686f1c55c1bab
+size 4401
--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf1386d4a031403d80ba19cc01ef4ba292b777ecc53622486d5c795010581af2
-size 35903
+oid sha256:caed2373db107129ba9135e7ba9c98cadb5ba5d25dc84801f348903d78b9fd4f
+size 35781
