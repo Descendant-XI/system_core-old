992281bfa79c56741334a006491c390320f7e4ca
Tom Cherry <tomcherry@google.com>
2017-08-02 17:01:36 -0700

    init: use Result<T> for the parsing functions
    Test: boot bullhead
    Change-Id: I7f00c5f0f54dd4fe05df73e1d6a89b56d788e113

--- init/parser.cpp
+++ init/parser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4efa210d11fb7e5357a5f4d7991668f55a90db2b2a5937aa548f9869835def59
-size 5605
+oid sha256:7463aad2e6e1d59b18457b7b787e42a97bedb0fe161ce7f6550e6793ec29ad11
+size 5557
--- init/ueventd.cpp
+++ init/ueventd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb3fc389cc11b02bef788a9608f7e4721c03680ffbdf3e4202740bf2e5196c1
-size 11222
+oid sha256:4ccdaaa395ec99ddecf7e96b9ef6c4abcb0a57808ad1503beddb04328db0270a
+size 11236
