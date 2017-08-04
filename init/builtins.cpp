6cb7b37640409096d3968d310e02551318f83a3d
Wei Wang <wvw@google.com>
2017-08-04 13:22:36 -0700

    init: Add readahead built-in command
    Inspired by ag/2659809/, this CL add readahead built-in command in init
    to let files be prefetched into pagecache for faster reading.
    Readahead happens in background but due to filesystem limitation it
    might take small amount of time in it reading the filesystem metadata
    needed to locate the requested blocks. So the command is executed in a
    forked process to not block init execution.
    
    Bug: 62413151
    Test: boottime, dumpcache
    Change-Id: I56c86e2ebc20efda4aa509e6efb736bd1d92baa5

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd3b05c5264e57830886e243fa6d9a77d169c706cfd0b7a682db33fc583d06d9
-size 29859
+oid sha256:c6675f115a4fb4ee534a0186eb0a8c8790f8873359a98c25f224f8a1a829c9ca
+size 32443
