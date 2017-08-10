0501d5ffbdf8afec66bcdbeb208f7d2e3c032caa
Tom Cherry <tomcherry@google.com>
2017-08-10 12:22:44 -0700

    init: split security functions out of init.cpp
    This change splits out the selinux initialization and supporting
    functionality into selinux.cpp and splits the security related
    initialization of the rng, etc to security.cpp.  It also provides
    additional documentation for SEPolicy loading as this has been
    requested by some teams.
    
    It additionally cleans up sehandle and sehandle_prop.  The former is
    static within selinux.cpp and new wrapper functions are created around
    selabel_lookup*() to better serve the users.  The latter is moved to
    property_service.cpp as it is isolated to that file for its usage.
    
    Test: boot bullhead
    Merged-In: Idc95d493cebc681fbe686b5160502f36af149f60
    Change-Id: Idc95d493cebc681fbe686b5160502f36af149f60
    (cherry picked from commit 9afb86b25d8675927cb37c86119a7ecf19f74819)

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93218d35ec68c33d3befeded3cc31d5895800f8615fde47211ad9ed6a7887d03
-size 30597
+oid sha256:8cc28f24dbf2cd0f8387d13245dbb250a6fee69108b1d20a5c1af77b71ec97cb
+size 30359
