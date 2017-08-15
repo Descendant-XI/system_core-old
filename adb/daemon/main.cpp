b059da8088bfff377883392a4110ee0b18b3ba09
Elliott Hughes <enh@google.com>
2017-08-15 07:58:20 -0700

    Don't include the build number in the adbd binary.
    We want repeatable builds for minimal OTAs.
    
    (cherrypick of 2e93eda69cf20d9c04d56ea6bf1e2ea62ad62eb4.)
    
    Bug: http://b/64634017
    Bug: 64709603 (presubmit balking at the line above)
    Test: repeated builds after `touch daemon/main.cpp` keep same md5sum
    Change-Id: Iedeb44377ea45bbf20e2212760802198a57dc14a

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50c025f19aeb61dbf0dbfef52d03c885c3b071b016143108a66823e26cd2cc5
-size 7948
+oid sha256:478c3b094f657bb0eadd797ebd663021458a38c9a20785b6bf8d54378b6ef219
+size 7930
