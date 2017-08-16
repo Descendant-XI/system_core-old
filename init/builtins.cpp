f2d9389ce02bc995111de3db48305ac073dc9b17
Wei Wang <wvw@google.com>
2017-08-16 11:34:50 -0700

    init: add option to read file fully on readahead
    Bug: 62413151
    Test: boottime, dumpcache
    Change-Id: I1a7b69f0619428e4db31c5a7639c5d895c89ecdb

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a63059d00f59ce82165f286400004b4dcbac4aa51ef3c0f9c0de0c3a88b76d2
-size 34547
+oid sha256:6844062aa886483f547bc0cc87145111b65c06c20e4674062011a5b750c9aae6
+size 35357
