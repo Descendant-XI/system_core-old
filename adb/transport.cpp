47e5328748061cb75e7305d9c37f36afd8add2b5
Josh Gao <jmgao@google.com>
2017-08-17 13:50:51 -0700

    adb: fix deadlock in kick_all_transports.
    Previously, kick_all_transports would deadlock if there were any
    inaccessible transports, because the transport kick function would call
    unregister_usb_transport, which attempts to take the already-held
    transport lock.
    
    Fix this by switching the transport lock over to a recursive mutex.
    
    Bug: 65419665
    Test: manual
    Change-Id: If61296ff4745e1699f3e216811c1383582627604
    (cherry picked from commit 1db71afe97c0c73167bc5d1fffea574b65bfa32c)

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f7f4a7b6faf3ad19023bc2766feb77e2d9a61140ebdd9818d39565e9ebd6cf6
-size 32728
+oid sha256:2456bb1d6862589940daac3cc95b03266c095d6169302541e9b32c90080a347d
+size 32872
