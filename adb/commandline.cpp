658995e525d7ace49079f45d5a49222daa5708f1
Huihong Luo <huisinro@google.com>
2017-08-17 15:23:08 -0700

    Fix bug #37284906, adb shell crashes on windows with invalid options. For example, "adb.exe shell -list" crashes without this fix. Test: adb.exe shell -list-packages
    Bug: 67320882
    Change-Id: I96be44128ff3ecab7804b70074cbde630b3fc3d1
    (cherry picked from commit af39885c9fec7faeca393e2dc54ff8b4bba3ca6c)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21a043c3e5069974a6b19671b7160ee9c6b64d4458996ff95e71fc44e199dd4a
-size 74979
+oid sha256:814a4b1b03d2041eae6a8cdd3f8fae2b6d81faad8b1e932d1b3bce70eb7f08f0
+size 75096
