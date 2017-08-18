132ab3f2e809edaca2fea8ad0ee036be7c679095
Tom Cherry <tomcherry@google.com>
2017-08-18 10:47:46 -0700

    init: only set ro.boottime.<service> properties once.
    Currently, init attempts to set ro.boottime.<service> properties
    whenever a service starts, however since these properties are ro. this
    means that an error is printed whenever a service is restarted.
    
    Since these properties are intended for reporting boottime, these
    subsequent writes during restarts are erroneous and therefore this
    change stops attempting to write them, thus silencing the error.
    
    Test: boot bullhead, restart processes, observe no error print
    Change-Id: I372f8d5c26590fc0661b92f632410e23e6418841

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caed2373db107129ba9135e7ba9c98cadb5ba5d25dc84801f348903d78b9fd4f
-size 35781
+oid sha256:a89928ccac9a1bbd96e6c7dc003d3e7493603ecb6c2ebbe523ca1d66fc7c00ff
+size 35912
