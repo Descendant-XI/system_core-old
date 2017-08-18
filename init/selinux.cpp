6457476dfe88bfa62d9ce8760f9a82fe71fb0034
Andreas Huber <andih@google.com>
2017-08-18 14:43:52 -0700

    Fix use-after-free of stack-allocated temporary string.
    Bug: 64848081
    Test: built and successfully booted again
    
    Change-Id: I93c899249bf2cc5ab8d880c0eaff471518e73121

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:799b15b0534527b0274bb78fc5fdc4a1c5d2cd6cc6c638c96b52192d683f918b
-size 17817
+oid sha256:c2a956bdd4ba40e8ba436aadf7700b20f80e45968dbcde412c7cd9a082db2373
+size 17879
