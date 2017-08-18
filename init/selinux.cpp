5930d6c059cff506be6eb8f8a3334d0e49e4a768
Tom Cherry <tomcherry@google.com>
2017-08-17 17:28:30 -0700

    init: replace panic() with LOG(FATAL)
    Test: boot bullhead
    Test: Introduce LOG(FATAL) at various points of init and ensure that
          it reboots to the bootloader successfully
    Test: Introduce LOG(FATAL) during DoReboot() and ensure that it reboots
          instead of recursing infinitely
    Test: Ensure that fatal signals reboot to bootloader
    
    Change-Id: I409005b6fab379df2d635e3e33d2df48a1a97df3

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff12406c194ce98f2c26c67f383ddcfdbb7850df3a72d839bb45deaedc05fd12
+oid sha256:c7c1987f73ab66f818c18775700ae1fe306abffdf4ffe81eda90ecfe2c5b3247
 size 16578
