ae212065c4d45300c63d823613ce0b01b8f07310
Josh Gao <jmgao@google.com>
2017-08-18 15:37:26 -0700

    crash_dump: print the identity of tracers.
    Instead of printing a useless "ptrace attach failed: strerror(EPERM)"
    message, print the name and pid of a competing tracer when we fail to
    attach because a process is already being ptraced.
    
    Bug: http://b/31531918
    Test: debuggerd_test32, debuggerd_test64 on aosp_angler
    Test: strace -p `pidof surfaceflinger`; debuggerd -b surfaceflinger
    Change-Id: Ifd3f80fe03de30ff38c0e0068560a7b12875f29d

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59497d2ae43120c2df22f84d3e9088518491ed9997d6845b5e9c58f7220f6409
-size 15040
+oid sha256:a9f96a5755da24e1a332ecb802ba2f71a964dd866c8e546ae456926bc8c628da
+size 15609
