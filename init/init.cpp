7a4ef96496872a2aed020c3ce34779d00285ff93
Andreas Huber <andih@google.com>
2017-08-21 15:53:54 -0700

    Fix use-after-free of stack-allocated temporary string.
    Bug: 64848081
    Test: built and successfully booted again
    Merged-In: I93c899249bf2cc5ab8d880c0eaff471518e73121
    
    Change-Id: I08e6f71a7c5151544b9434eb0d362a236202cd31

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8ffd863d00ef3ddd86e9a2d2e97ddb77e5e7825874169f8cd9605a64137f21c
-size 41199
+oid sha256:2dcef3421cc1e27bd2b15d4999fe3e3b09c2dff70503180e5b5826f4b788cc85
+size 41261
