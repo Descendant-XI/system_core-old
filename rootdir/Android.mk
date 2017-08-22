ed8f20c3bc0d7cd0878d2ddf520f886f3b92dfb0
Jiyong Park <jiyong@google.com>
2017-08-22 10:25:18 +0900

    add llndk|vndksp.libraries.txt to vndk_package
    List of llndk and vndk-sp libraries are written in the txt file so that
    they can be available at run-time. The information is used by
    libnativeloader to configure the classloader-namespace specially for
    vendor apks.
    
    Bug: 64882323
    Test: build 2017 pixel devices. check that the two files exist on
    /system/etc.
    
    Change-Id: Ifbe339a5862f6ef57a8213a14a022765ccf77283

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23b153e9d1679b2992e356fb3b6856274e08822a1a2afc0bf989143a076429f4
-size 7507
+oid sha256:5bbf35ba24725d7655b69decb13d14274bbfba84854894043d7fd6a6e3c39eed
+size 8953
