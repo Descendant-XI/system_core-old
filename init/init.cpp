0c229a9442c282f7b00398b56a04023f10006a18
Tom Cherry <tomcherry@google.com>
2017-08-25 15:10:48 -0700

    init: fix signal handling and LOG(FATAL) in child processes
    Child processes inherit the signal handlers from their parent process.
    In the case of init, fork()'ed processes, will attempt to reboot the
    system if they receive a fatal signal).  This is not the correct behavior;
    these processes should terminate due to the provided signal like other
    processes on the system.
    
    This is particularly important as there are multiple LOG(FATAL) calls
    in service.cpp for failures after fork() but before execv() when a
    service is started.
    
    Note, that pthread_atfork() is not a viable solution since clone() is
    used in some cases instead of fork() and atfork handlers are not
    called with clone().
    
    Bug: 65637054
    Test: LOG(FATAL) from a child process of init and see that it
          terminates due to a signal correctly
    Test: LOG(FATAL) from init proper and see that it reboots to the
          bootloader
    
    Change-Id: I875ebd7a5f6b3f5e3e2c028af3306917c4409db3

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dcef3421cc1e27bd2b15d4999fe3e3b09c2dff70503180e5b5826f4b788cc85
-size 41261
+oid sha256:408b93841855f7c8d3433d1b104c0960fee58104344057230af8bd5e9cd9af06
+size 41525
