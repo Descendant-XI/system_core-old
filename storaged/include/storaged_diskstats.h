33f82de6ee18979907b83604102ad88459520a3c
Jin Qian <jinqian@google.com>
2017-08-29 16:48:20 -0700

    storaged: clean up disk stats code
    Merged disk_stats_publisher into disk_stats_monitor class.
    
    Moved proc file check to disk_stats class instead of doing so in
    storaged, same for uid_io.
    
    Added is_zero function to check zero disk_stats.
    
    Added operators to compute disk_stats.
    
    Change-Id: I277ae51f6fe18464f92e45c03471343784e67093

---
+++ storaged/include/storaged_diskstats.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e813faed237e9653428af91086119d6ca806a24f3546572a8d134060fbbea69
+size 7082
--- storaged/include/storaged_utils.h
+++ storaged/include/storaged_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99b59e1de05c0da0796e469819c0b70d07eb76eeb3b463c0c8222c2729f3045b
-size 1561
+oid sha256:a42b688783e991728c4f686d9d0b2223190302ffbae845221be30e0628f99e0b
+size 1331
