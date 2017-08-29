18946fa757cc3ce6733eae2cd79a3218d79d53c7
Ting-Yuan Huang <laszio@google.com>
2017-08-29 15:48:30 -0700

    sdcard: mute false compiler alarm
    Clang static analyzer worries that strcpy to a field may overwrites
    other fields.  Use snprintf() instead.
    
    Test: built without seeing the warning.
    
    Change-Id: I75d8edf1353b5d052fd14a954362bd0632c258fa

--- sdcard/sdcard.cpp
+++ sdcard/sdcard.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d312204f59b8c004a0d03e910b2eb7769fb4152a15e664ec86eb7a484163aa68
-size 18122
+oid sha256:03735ee97831862ade218c1e413d3e5f4a9c1dc8aa3c18006d871064745c2c9c
+size 18296
