539ce0564e792737544dcefbf0ebfecc11118200
Bowgo Tsai <bowgotsai@google.com>
2017-08-31 06:26:47 +0000

    Revert "adbd: lessen security constraints when the device is unlocked"
    This reverts commit f1d3dbc32f18d9b3604da03bc043c1b4cc3f5a35.
    
    With the following changes to move /sbin/adbd to /system/bin/adbd,
    we don't need this workaround anymore.
    
      https://android-review.googlesource.com/#/q/topic:move-adbd-to-system+(status:open+OR+status:merged)
    
    Bug: 63313955
    Bug: 63381692
    Bug: 64822208
    Test: 'adb root' works in VTS for a non-A/B device (userdebug GSI + user boot.img)
    
    Change-Id: Ic1249d6abd7d6e6e7380a661df16d25447853a48
    Merged-In: Ic1249d6abd7d6e6e7380a661df16d25447853a48
    (cherry picked from commit 0603ec4294bc92449643608ee5f18e172d99fce0)

--- adb/Android.mk
+++ adb/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9be35eb906c542aa82dbb5c14252ee88e54e3897f0e00a7a2d55d3a65edee11
-size 10841
+oid sha256:9142b6ff1f21b1c57e2c78fecf64b860c3e4de34776a41332ba1619b5e482874
+size 10785
