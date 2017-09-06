808aae28a6d4bba033d6660a8888829cfcfb2b18
Tom Marshall <tdm.code@gmail.com>
2017-09-06 17:49:04 +0000

    Revert "Format formattable partitions if mount fails"
    This reverts commit 29dd6b6c01295222fee5ef2fc70692b2ecb12504.
    
    Change-Id: I7b76cd920019ae8cb7270b3f83e777ea9de7f7a4

--- fs_mgr/fs_mgr_roots.cpp
+++ fs_mgr/fs_mgr_roots.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:847ed6f191b4187c3243f022383c72b316349821d575d8cdb557ff0da74ed8ce
-size 5669
+oid sha256:784a09d15201f6b9b9171773b005bd1c3430e96e02c2b219d0cfcff689cfb2ab
+size 5248
