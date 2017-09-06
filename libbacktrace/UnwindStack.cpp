0cbc4eeba2494cb29687820a5d5c80238127ba41
Josh Gao <jmgao@google.com>
2017-09-06 14:35:35 -0700

    libbacktrace: expose libunwindstack Unwind routine.
    Test: backtrace_test32/64 on hikey960
    Change-Id: Iae6e342a8ffd13b4d5f76b39dd434c827f7a4d9e

--- libbacktrace/UnwindStack.cpp
+++ libbacktrace/UnwindStack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be36b71e57afba06e7990d8c04af1d1beaaa4f819ce3885cb3eaabad69a10e1b
-size 5840
+oid sha256:5586a639c966a98d6e64a995c110d43984c4fd750e2c266390ce074aceda4df4
+size 5866
