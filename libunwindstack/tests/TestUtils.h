fa884b41f17949484f081e45b988cc2169f24b4d
Christopher Ferris <cferris@google.com>
2017-09-06 14:15:28 -0700

    Fix UnwindTest repeatability.
    - Rewrite some of the UnwindTest tests to properly wait for the process
      to be ready.
    - Add a TestScopedPidReaper to make sure that fork process get killed even
      if the test fails. Add this to all tests that fail.
    - Create a quiesce function to be used by all of the tests that will
      wait after attaching to a process.
    
    Bug: 65287279
    
    Test: Ran unit tests on hikey960 board and on host repeatedly.
    Change-Id: I57084120396f34d8dfb852f3d814bef2056f1b54

---
+++ libunwindstack/tests/TestUtils.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3824475134c9a4edad1d8691a47ac2689f4af73db30165ee222da023ab2ffc08
+size 1381
--- libunwindstack/tests/UnwindTest.cpp
+++ libunwindstack/tests/UnwindTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1509b220fa211cf745a4e6d4bf1a63338790e22ae1952fd2956d7d56011c9497
-size 9258
+oid sha256:925f9b3b58c53a9ac66187c33633161ccdfe043d1be398267ab24e99bc2109d2
+size 9602
