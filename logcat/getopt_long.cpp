df78a56fb289df1e39ec8b2bc76b5db98cdb9bb6
Elliott Hughes <enh@google.com>
2017-09-07 13:36:14 -0700

    <stdbool.h> not necessary in C++.
    Bug: N/A
    Test: builds
    Change-Id: I1f32b202e353414e74ace87a7d5b495b5deb5f99

--- logcat/getopt_long.cpp
+++ logcat/getopt_long.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d71464356b89ba71648af9552f2c6c998d40dbf5a98920cd4b8fa286434a4c1
-size 15621
+oid sha256:8693b0d207e0f95ec05c2e33dd4db862ea8500d4bd75631b08d8e9bf59fc94a1
+size 15600
--- trusty/storage/tests/main.cpp
+++ trusty/storage/tests/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12616c25469ef8e6349c216c704715ac747e334e1f93dfd1df93b3674cf5160b
-size 93346
+oid sha256:b5deacf0a32cf0b5ccb6eb5b389de2dc4d340ea1be4c893c19678df13ce31119
+size 93325
