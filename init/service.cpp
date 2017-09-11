7edbb1e2e9af94307561fd1b627ef0b8007fdf47
Tom Cherry <tomcherry@google.com>
2017-09-11 16:08:54 -0700

    init: fix variable scope issue with ExpandArgs()
    ExpandArgs() was factored out of Service::Start() to clean up init,
    however this introduced a bug: the scope of expanded_args ends when
    ExpandArgs() returns, yet pointers to the c strings contained within
    those std::strings are returned from the function.  These pointers are
    invalid and have been seen to cause failures on real devices.
    
    This change moves the execv() into ExpandArgs() and renames it
    ExpandArgsAndExecv() to keep the clean separation of Service::Start()
    but fix the variable scope issue.
    
    Bug: 65303004
    Test: boot fugu
    Change-Id: I612128631f5b58d040bffcbc2220593ad16cd450
    (cherry picked from commit 5e405cacb1f3ef2265fcc71dbbf6a39c1e7dc1c1)

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a3f5d2421bec46dce58b6ed2f031f16d83d1d62103d4ae842e750a0d361de74
-size 41691
+oid sha256:92558fe5e4806dcccd9dd5f46e76a179c2aa5e9fc955adeb1c6262308210234b
+size 41679
