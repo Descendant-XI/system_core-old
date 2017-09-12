59784a91405c3911f5f7d202361a538b787b8aee
Bowgo Tsai <bowgotsai@google.com>
2017-09-12 17:56:37 +0800

    Removing $(PRODUCT_OUT)/root/root
    Bug: 65570851
    Test: boot sailfish
    Change-Id: I80c3c3de23c57343187fde207980731b5341bc1f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0d3125c3f020319d19eb8cefabfa3483f30ea5db5c2eb7ddc854c437e26f23
-size 9439
+oid sha256:036574f14505ca7f0ca7c791a84390ac3dc2e45b615cc77ba0e329a73258948d
+size 9434
