2e28dbd9d857c3b577e0ba66e8ec98913a724355
Josh Gao <jmgao@google.com>
2017-09-13 11:17:33 -0700

    adb: add lock to remove_socket.
    The comment that was previously here says that local_socket_list_lock
    must be taken, but this function is exposed to external callers that
    can't possibly take the lock.
    
    Bug: http://b/65419665
    Bug: 64709603 (presubmit balking at the line above)
    Test: python test_device.py
    Change-Id: I12d464933936b2a210a827ccf19ea201020d8d78
    (cherry picked from commit 62c92f0c0529d8d1817afb4ff0c83151e4bb0ea0)

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9843605d51f43a06a677f95550b92aafe6aacfc1c838f812b98c2483ba09134
-size 26163
+oid sha256:e60768e8e78ee8f10b73ecdd105574a78aa85d50dfef18993685bdf8df35adce
+size 26188
