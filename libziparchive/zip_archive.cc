8c149e6600c22762e791263beff1b752a96dbc09
Narayan Kamath <narayan@google.com>
2017-09-14 10:41:28 +0100

    DO NOT MERGE : Fix build breakage due to 2d516d2d46b1b1.
    Test: make
    Test: zip_archive_test
    Bug: 64211847
    
    (cherry picked from commit 547c7d9a0b6651ace0ade7849661ba6c20770ae6)
    (cherry picked from commit bfe4b5edb3097df66ddbf9639c3b3cd290734ed8)
    
    Change-Id: Ide48ce66542e152d88520dcd6abcd104e48137f6

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:857dbcfb972f93ed46e0bd9c827b789341f2f55c8dfcc7c1bc019f7e2d2a55aa
-size 39740
+oid sha256:864fdd35eb26093aa0cc8ae85b5e17fd61354e22d2b291ae956f1f1d1e04a9e5
+size 39816
