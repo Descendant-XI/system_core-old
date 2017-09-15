9ae0a67303521fca14e619b5aa6e669c06ee522a
Tom Cherry <tomcherry@google.com>
2017-09-15 15:44:04 -0700

    init: use protobuf for serialization of persistent properties
    I probably should have done this from the start...  There's a shim to
    convert my manually serialized format to protobuf, and since that has
    not yet shipped, it'll be reverted in a short period of time.
    
    Test: init unit tests
    Test: upgrade from legacy and intermediate property formats successfully
    Change-Id: Iad25f6c30d0b44d294230a53dd6876222d1c785b

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:072f161ffd7d6a1cd548f2b61ce3c7c2cfbd6fc644e86c574d1ee68ad509fca5
-size 22217
+oid sha256:09659afb766a25f21d0849661b74765605d8583815a0d2c3b3d1293494197b0f
+size 22301
