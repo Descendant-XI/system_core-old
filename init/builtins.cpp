fa1a01867ad2af10b28b3ce482feb99f55ea966b
Tom Cherry <tomcherry@google.com>
2017-09-18 12:16:27 -0700

    init: add exec_background command
    This command functions similarly to `exec` except that it does not
    cause init to halt executing commands until the process has
    terminated.  It is useful for launching simple one time background
    tasks.
    
    Bug: 65736247
    Test: create an exec_background service and see it function properly
    Change-Id: I719c8b85479b65201770aedc0a13191303007c11

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c50ff0ea0632ca796638eb37c5b7b0995d7a4b5bbb8b9f36b506e16649a3b43b
-size 35329
+oid sha256:d79c7fed5fc4dc86ee7c46d9f59d9d3ed9964a767f3d06163c5893dff5357b04
+size 35877
