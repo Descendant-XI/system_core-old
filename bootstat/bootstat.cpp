c305ccc8980fc34614f9778a46e371b5279dcbd9
Mark Salyzyn <salyzyn@google.com>
2017-09-20 08:37:46 -0700

    bootstat: Do not allow unknown boot reasons to land in first field.
    If we sniff an unknown boot reason from last kmsg, make sure it
    has a "reboot," prefix.
    
    Test: system/core/bootstat/boot_reason_test.sh
    Bug: 63736262
    Change-Id: Ia1c401b8899d1f0c56bd4f5d8d2d19b7fc889a30

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a9911cf4779074ceed1ae3acb9dab2d4b98ba077b015abb980715aec2d658da
-size 34281
+oid sha256:eed22bedefa986d067752cc58c7722f9be62f92e286ce1bed6783048b92b74d6
+size 34408
