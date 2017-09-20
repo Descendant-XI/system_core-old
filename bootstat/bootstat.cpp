a71795a0184a015fe44ac67d5aade28c304ff958
Mark Salyzyn <salyzyn@google.com>
2017-09-20 08:37:46 -0700

    bootstat: move boot reason validation transformation policy into subroutine.
    Allow for future policy adjustments.
    
    SideEffects: None
    Test: system/core/bootstat/boot_reason_test.sh
    Bug: 63736262
    Change-Id: I571fb7dafc6b80c75d2809a3da3f9b96784cef06

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b6c8fefd17cdeb66aa55e6de5a1bf8e8b31f2a718e77d386a8ef1aca985ca87
-size 34513
+oid sha256:3a9911cf4779074ceed1ae3acb9dab2d4b98ba077b015abb980715aec2d658da
+size 34281
