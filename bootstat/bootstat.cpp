46a3d6e055548ec57ee1fd990d183e324e55ef75
Mark Salyzyn <salyzyn@google.com>
2017-09-20 08:36:12 -0700

    bootstat: validate last kmsg and bootreason content more carefully
    Establish a tighter trust relationship with digitization of the
    battery level.  Validate the reboot reason collected from the last
    kmsg.  Validate the last reboot reason before using for enhancing
    system reboot reason.
    
    Test: system/core/bootstat/boot_reason_test.sh
    Bug: 63636262
    Change-Id: I45fbac4a33c09295cda89de3b73c93a884033e3b

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d392db64a969b37e1da78b3a57aebda6d2f62907c95e667fbce7bd1c0badd4d
-size 32965
+oid sha256:b9f29dadbb7557896f45a802df9624f6f453b6ae7b19ec15d5430c82eca96ce2
+size 33896
