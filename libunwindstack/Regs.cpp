ab389bd51f6a15f44259898bff85583ddbc66a2d
Josh Gao <jmgao@google.com>
2017-09-22 12:57:15 -0700

    libunwindstack: add method to iterate across registers.
    Add a function to iterate over Regs with a human-friendly name and the
    value of the register, to make it easy to string-format registers.
    
    Test: mma
    Change-Id: I8f33830fa79b245b5030b7bc9bc3c1c7ced80c75

--- libunwindstack/Regs.cpp
+++ libunwindstack/Regs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dca1d60314cab2512b6653f133d4e0549afc7cc66ae4b34c85725902fec87503
-size 15916
+oid sha256:c017d9ee666d5498e19394d93ed8f7ddaf9132f38eb6f817692b87b705cb88cf
+size 18788
