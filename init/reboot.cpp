5f7708222354d4392a24f92ad276276156101221
Jaegeuk Kim <jaegeuk@google.com>
2017-09-25 10:55:39 -0700

    init/reboot: call MNT_FORCE at the last umount(2)
    The last one will avoid errors=panic in ext4.
    
    Bug: 63981945
    Bug: 65481582
    Change-Id: I9c86afcce441767e24fc43668ab1ff6230155a9f
    Merged-In: I9c86afcce441767e24fc43668ab1ff6230155a9f
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    (cherry picked from commit 0f04f72c2de2f5c42e62f23306bc106dddcdabb1)

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d88641afbfdefd9e1fd950a23579bdc84dd5fb1828ea764a27515e1eb2afad2
-size 19992
+oid sha256:a2e65eda3c64049a1e540a5fe183246ee575aff861222d04a943171189971237
+size 20041
