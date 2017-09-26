b16f849d75d2d3b372d00fb1b201af79ad8969c6
Bowgo Tsai <bowgotsai@google.com>
2017-09-26 18:03:23 +0800

    fs_mgr_fstab: do an exact match when searching a mount point
    Currently when searching a mount point from a fstab, it checks whether the
    mount point of an fstab entry is the prefix of a given path, and the
    next char after the match is either '\0' or '/'.
    
    However, it will always return the fstab entry with mount point
    "/vendor" when searching path is "/vendor", "/vendor/abc" or "/vendor/cde"
    in the following fstab. Because "/vendor" is the prefix of "/vendor/abc"
    and "/vendor/cde", and the next char after the match is '/'.
    
        /dev/block/platform/.../by-name/vendor   /vendor      ext4  ro  wait
        /dev/block/platform/.../by-name/abc      /vendor/abc  ext4  ro  wait
        /dev/block/platform/.../by-name/cde      /vendor/cde  ext4  ro  wait
    
    Fix this by performing an exact match when searching the mount point.
    
    Bug: 63912287
    Test: boot sailfish
    
    Change-Id: I504655f5c71790c5d528085de416ce3c30d21fea

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f0b7a88f48664c3df3ea57b4dc8b2a76ef57852e57e5740f29b1c7ba2d47607
-size 30765
+oid sha256:3ea943484832550e4ec45079acfba36e7f40b4fed912cbba33f3e90b0de33761
+size 30798
