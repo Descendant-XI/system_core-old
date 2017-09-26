138ee21c05eb8311f0d251f426cca37618f13862
Christopher Ferris <cferris@google.com>
2017-09-25 19:23:07 -0700

    Update the Unwinder object and add tests.
    Changes:
    - Remove unused GetReturnAddressFromDefault function and tests.
    - Modify the unwinder to stop when a pc/sp in a device map.
    - Modify the unwinder to skip initial frames based on map names.
    - Unit tests that exercise all of the paths in the unwinder code.
    - Move the test Elf/ElfInterface objects into their own file.
    - Update RegsFake to handle extra cases.
    - Modify libbacktrace code to use this unwinder.
    
    The new unwinder does not implement the ignore frame functionality since
    this is not used very often and is better implemented using a skip frames
    in named libraries functionality.
    
    Test: Ran new unit tests, ran backtrace tests.
    Change-Id: Ifd65e9acd66ac5e2d0e04bd32a9ad870b54610ff

--- libunwindstack/include/unwindstack/Unwinder.h
+++ libunwindstack/include/unwindstack/Unwinder.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a01772442020a97d6b4ee364c3fbb6b46bbaf26e6f239b3dfc4fcd0342f1ca92
-size 1934
+oid sha256:d3f27fcbe75e40d6ddcef4b029ed5189921e9c479d8bb908ed3ab3209296de34
+size 2112
