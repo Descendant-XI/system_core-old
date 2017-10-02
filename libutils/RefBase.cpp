52639af12b01644bd1a2bd51c48180a79a702de8
Chih-Hung Hsieh <chh@google.com>
2017-10-02 15:20:07 -0700

    Use -Werror in system/core
    * Move -Wall -Werror from cppflags to cflags.
    * Fix/suppress warning on unused variables.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I1e05e96a1d0bcb2ccef1ce456504b3af57167cc5

--- libutils/RefBase.cpp
+++ libutils/RefBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d68e8edac262995fc320904d0cc640c05374c454f4d4430bf10978ac8fa2dcef
-size 27038
+oid sha256:32e23e19ba95eccf16eebfcaf81335c141381b0cafd0bbf4afb98ffd1b5b4fe1
+size 27035
