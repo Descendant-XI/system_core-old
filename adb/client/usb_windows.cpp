38f98064e6c3972af812b8ef78f32d179069e71e
Mark Salyzyn <salyzyn@google.com>
2017-10-04 15:05:40 -0700

    adb: clang-format for adb.h and client/usb_windows.cpp
    Future adjustments triggered cleanup and transition of adb/adb.h and
    adb/client/usb_windows.cpp to be moved to Android coding standard.
    
    Test: build
    Bug: 63736262
    Bug: 38446744
    Bug: 66912053
    Change-Id: I6eb3f0665b9670b9b3d5f5397f271605b48f4ff0

--- adb/client/usb_windows.cpp
+++ adb/client/usb_windows.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fd5ae7a2c24e29bebb29316f74f4f690ec672075a9af13ac9085c7499a334e7
-size 20289
+oid sha256:ef9e59a615a0fd8f1233dbb5af17417492ad32756ca4734008227c2a5c2239db
+size 21168
