a986eb139771362f02bac3c1fa0bb9dd9043e508
Yifan Hong <elsk@google.com>
2017-10-04 10:59:51 -0700

    Remove unused files in healthd.
    Test: mma
    Change-Id: I19c13aa3f1fdecd3f7072dccfe9dbd580f09e22a

--- healthd/BatteryPropertiesRegistrar.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8803defb8e10ed04f792f13f00ad8ae3a6730087d582b8c01fd044904a45914e
-size 4058
--- healthd/BatteryPropertiesRegistrar.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7b1ec32c3c12556b78f207c5f065614d4d6d057b6e65c30ed6ee92ec9bc012ff
-size 1793
