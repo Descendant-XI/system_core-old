cf3c2ced2a306d7a644b201747e161a4fd124c59
Christopher Ferris <cferris@google.com>
2017-10-05 16:05:44 -0700

    Use Unwinder object for unwind tool.
    Test: Ran unwind on arm and arm64 processes.
    Change-Id: I8a2a3ed31482044fe51d7e0d8c7f5588d5aad81c

--- libunwindstack/tools/unwind.cpp
+++ libunwindstack/tools/unwind.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a72c6459da34f34e876e2bd2e22bb16f0b555b5b1aae394521d68ed03900be2d
-size 5647
+oid sha256:b623b57cf885ffbaf30d2d0dd57cb21a64a94d02470153caa0486709cc1af38a
+size 2650
