6390a7fbb3602a87657b992fb03f3ee300235617
Tom Cherry <tomcherry@google.com>
2017-10-10 13:30:57 -0700

    Move some properties users to __system_property_read_callback()
    This is needed if they will ever handle ro. properties that have
    values longer than 92 characters.
    
    Bug: 23102347
    Bug: 34954705
    Test: read and write properties with value length > 92 characters
    Change-Id: I44aa135c97ec010f12162c30f743387810ae2c5d

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47e9c02a42a4f36b2c515af005983d3b400fa4a6ba251c4d29d75d863f9dff20
-size 49587
+oid sha256:f63017784bad80ef6868940c2022220aa516bbd774e1eaa5374e125ac0fea4fe
+size 49052
