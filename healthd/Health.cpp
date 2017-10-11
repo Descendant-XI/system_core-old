6064ff7fbe3541e7a11397164ce2a10e04161ab3
Yifan Hong <elsk@google.com>
2017-10-11 10:40:56 -0700

    Update for expanding @2.0::HealthInfo.
    Health service also sends other fields in callbacks.
    Test: builds
    Bug: 62229583
    
    Change-Id: Icb9215c263ba34055e77db3dbb047998bb2da4e8

--- healthd/Health.cpp
+++ healthd/Health.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e01152d21c9319ca2c0547b2dadd4d0ccdf63e8015498f8c302a527f60fb772f
-size 5102
+oid sha256:1854dddaedb1450bef7e5dff417017c5c839e5841af828f9a389856f4fe0f719
+size 5594
