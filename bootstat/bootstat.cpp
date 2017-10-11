78fdef553b9541b4dc07e89538907635e1717221
Mark Salyzyn <salyzyn@google.com>
2017-10-11 09:02:04 -0700

    bootstat: Add some reserved sys.boot.reason values to enumeration
    Add cold,powerkey, warm,s3_wakeup and hard,hw_reset so that
    sys.boot.reason values can also be enumerated.  Also add
    some reserved speculative entries associated with forced
    suspend to RAM and DISK; shutdown,suspend and shutdown,hibernate
    respectively.
    
    Test: system/core/bootstat/boot_reason_test.sh
    Bug: 67636061
    Change-Id: Ic43523748e6006aaca882f8eec7c1f0c08431bd8

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a74e56a897c1d06691f3c661c3b586448d3765cc8a11d1463b5c42d1842c46e
-size 33330
+oid sha256:973bef0df66a6e22fd55b25aa4ff9f79c75b792c6fdaaa2dd366acdf913a6973
+size 33515
