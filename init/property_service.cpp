7cced582dc4ae412a874c9b0d61c34a906bfb006
Tom Cherry <tomcherry@google.com>
2017-10-13 16:20:19 -0700

    Only allow UTF8 encoded property values
    Java already restricts properties to only UTF8 valid strings, and
    this change makes this restriction also apply to all native code.
    
    Bug: 63177684
    Test: new unit tests
    Change-Id: I9fa0ecc0da066b0a026db3497c0f0cbf3f5c2d5a

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d6ca465d916773fe58876ad7ac6998f975e84c953a6468a70bab685063ecab8
-size 22284
+oid sha256:19b8676f0312b139ed4e6ad410316e161f6d4f48547f95081fc330c7d09d2ffd
+size 22573
