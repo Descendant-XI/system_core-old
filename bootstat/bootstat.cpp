d14c580a9b0877e33c50596cf08f53bdfe0d0bee
James Hawkins <jhawkins@google.com>
2017-10-17 15:53:27 -0700

    bootstat: Add more boot reasons found by logging in the wild.
    Bug: none
    Test: none
    Change-Id: I7e388326be44aeb3e6fdb79d2e173dc17eb8bc1f

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccd51b134d1bd1bf31bec79f582e414edc17b3265998ab93a2638632d9970252
-size 34032
+oid sha256:835e320624fb5597df289cd20a082d92f2db4433ae063e9a63d6e64be35c9cd5
+size 34308
