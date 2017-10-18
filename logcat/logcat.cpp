712af43b7933a5affa20e00fb22c398a11c65ea3
Josh Gao <jmgao@google.com>
2017-10-18 15:42:00 -0700

    liblogcat: avoid double close.
    Bug: http://b/67960705
    Test: mma
    Change-Id: I0b9015f0b4e64631a3476966decce3b6210bb801

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6de1ae46b9ccfca43739f78e2dff2f4be77c4065c7ffadae626fc8e4903f2472
-size 69621
+oid sha256:139435d996aaa9169c50ea36a97ca131591bf907407babfa6dfb57702810f633
+size 69567
