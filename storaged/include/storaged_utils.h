ae9c10f6900a8e558aff08c4154e8e416a85d990
Jin Qian <jinqian@google.com>
2017-10-18 17:52:14 -0700

    storaged: split proto file into multiple CE areas
    Use user_id (from app uid) to determine file location.
    /data/misc_ce/<user_id>/storaged/storaged.proto
    
    Vold notifies storaged when a user's CE area becomes available.
    Then storaged restores data from the proto in that area and
    combines them into IO history.
    
    Vold also notifies storaged when the CE area is being deleted.
    Storaged clears internal history about this user and deletes the
    proto file.
    
    IO perf is stored in user_0 area since it's not user related.
    
    Test: dumpsys storaged before/after multiple users' unlock
    Bug: 63740245
    Change-Id: I39f923f6b09e9f2a29e9286ce02b3b3bcbfb9f94

--- storaged/include/storaged_utils.h
+++ storaged/include/storaged_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6f0180cf43becc141b0b3062a8d7a7cbda17dd339747e60e6062004ad4aa642
-size 1419
+oid sha256:6506f5b16c25fb03c2bd68d25085b7c439bf0cd546e37481a2462bef19baadee
+size 1492
--- storaged/storaged_service.cpp
+++ storaged/storaged_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6e3415c7136f373cd02736ab320586443fef3ab8354bcadae85e0c77f414241
-size 6814
+oid sha256:f2206c4488714398a08ea62ad37c12fa996fab80a549c6458556a60e5a60bc92
+size 7495
