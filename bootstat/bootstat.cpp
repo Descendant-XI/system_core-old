94bc4a257f236ce4dea122eb35718d9be2f52d79
Mark Salyzyn <salyzyn@google.com>
2017-10-24 15:35:34 -0700

    bootstat: add shutdown,thermal,battery
    Test: system/core/bootreason/boot_reason_test.sh
    Bug: 63736262
    Change-Id: I30d6b5308fd8616eae31ad13e925d66c9fc07367

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0e4166bae5fa07e3ea8f94aeaa8e9b2d93851ca77c7fabac874555c4dba50b5
-size 34335
+oid sha256:371de4eb5ffa8a918198acc7a72ff4d7ba8c0bf890a69282c52f93c88e34d754
+size 34373
