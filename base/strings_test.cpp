7d10a363e22348591da76c68478ea6ccd060b9aa
Tom Cherry <tomcherry@google.com>
2017-10-26 10:50:20 -0700

    Add test for android::base::Split with a trailing empty part.
    Test: this new unit test
    Change-Id: Ia14ba878f2866e333a69f1e6f7f56b66a7e657a5

--- base/strings_test.cpp
+++ base/strings_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7e37eb80873dc01eeba76d8773b0ddbf1f603e72b56799f22f68d7093a6430c
-size 8719
+oid sha256:fe54ee2cc912c1fb52e29e78e9612deeef9bdbbf02499a133c0c7c8f4961c897
+size 8962
