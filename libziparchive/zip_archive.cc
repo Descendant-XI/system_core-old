c4e584131c08f14597f078ffaf9eeb0bf34873f1
Narayan Kamath <narayan@google.com>
2017-10-26 14:42:39 +0100

    zip_archive: Make Inflate a public API.
    For use from zipalign and androidfw::ZipUtils.
    
    Test: make
    Bug: 35246701
    
    Change-Id: I1bdeff3770d0833dfc0807d8a69ba9fea525772a

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5df7212e1dd00e04573980cf52e7062a44712e07d46734094395609cf2fad408
-size 42231
+oid sha256:7f437afb37549e9ea47b1645300baca9d240facef1e11fd37767765acb790c0b
+size 42003
