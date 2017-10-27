d23c091f91e0c7ebd70c26dbfd1fc08b00872465
Tao Wu <lepton@google.com>
2017-10-27 11:29:13 -0700

    Fix race condition between restart and stop/reset.
    If there is a restart follow a stop/reset immediately or vice versa,
    clear previous flag bits.
    
    Test: manual - trigger restart after stop immediately to check if
    service get started.
    
    Change-Id: I4503177d7cb5ed054dbcf50cd8e09728415404d4

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2234c498fb301745d386f5b29b1372df9a25c86335f484a56f5fd522ebf5848b
-size 38352
+oid sha256:238d45b5b58eb4bb0caca2252f3f097cab224d535728c3883ce6473f382285e9
+size 38591
