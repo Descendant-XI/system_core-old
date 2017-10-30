52ef7326a183d37e2aff78bf6ecbb4e8f4ae4d4a
Narayan Kamath <narayan@google.com>
2017-10-30 11:17:28 +0000

    zip_archive: Allow crc_out to be nullptr in Inflate.
    Only compute the crc32 if required. In addition :
    - Add unit tests for Inflate that cover this addition.
    - Fix an inconsistency in return codes that was revealed
      by this new test.
    
    Bug: 35246701
    Test: zip_archive_tests
    Test: make; zipalign.
    
    (cherry picked from commit d9e85b14afd5a2ee14bb4df46cf576c5f9c25f24)
    
    Change-Id: I31d7554378f94fc8995f707471d57cb98311e2c2

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:402338ff2f1a293c48f70b0642c54afaeafd21e77729a218e72e369c7c22d952
-size 42520
+oid sha256:8ad989ebc44e5cec43f9172d6e63661254390405f5eec3b167808a73b3b40c5f
+size 42541
