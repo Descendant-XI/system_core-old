29f2437876eefc438617de2741dd892a9687ed1e
Yifan Hong <elsk@google.com>
2017-11-03 13:20:10 -0700

    do not getTransport before getting IHealth service.
    getService already has getTransport logic.
    
    Test: storaged unit tests
    
    Bug: 63702641
    Change-Id: I29be15e5713ac276fbbeda88224d8aef514319c6

--- storaged/storaged.cpp
+++ storaged/storaged.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c13fddcfe1ee15549965f6b3d695664df4fdf6f820ee63f174431dcd0631343
-size 11586
+oid sha256:40e52634594ea9a147ce8394b967804f1bbe6f6d265680ca8c7fae5e57b50086
+size 11407
