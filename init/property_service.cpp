5ff6731e5a3fa421e5c62848de4e859724602044
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-06 20:30:39 +0900

    init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    
    Former-commit-id: e036a461c7dd4d97e1df77979c85f3c198e1e784

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52b42b9d7919370d626bb4a2ab85cbac5da8633bbd25a4a0b344cf988481d8d5
-size 43401
+oid sha256:6e6cc1bbbf91300a70bb2295d462387fbc6e922044365fee805e1fd4ef1e84a8
+size 44510
