f0f193867214981d4c9e54f50e01687e32598843
Yifan Hong <elsk@google.com>
2017-11-06 12:59:09 -0800

    storaged: update for using 1.0::HealthInfo.
    Test: boots
    Bug: 63702641
    
    Change-Id: I97e322af97347c603f10b5b5b82cd3ae52c58b8f

--- storaged/storaged.cpp
+++ storaged/storaged.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c13fddcfe1ee15549965f6b3d695664df4fdf6f820ee63f174431dcd0631343
-size 11586
+oid sha256:4a99f1b5890e19cfa5eafa4e53b8f9a27eaef28aece6e4a7a6e40d58a818d247
+size 11579
