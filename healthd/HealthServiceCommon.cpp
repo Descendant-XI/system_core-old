c8a246ca88d54219ab767f093d35856b9e615ee1
Yifan Hong <elsk@google.com>
2017-11-07 15:02:10 -0800

    healthd: remove global binder fd
    It is not used elsewhere, so a local variable is
    sufficient.
    
    Also fix a shared lib order in soong file.
    
    Test: health VTS test
    Bug: 63702641
    Change-Id: I53cc208d8dd388fc89119c0044c5e30acf0740fb

--- healthd/HealthServiceCommon.cpp
+++ healthd/HealthServiceCommon.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de12af95363f76373e385a6965c62d44b9ce44037c64767300eb6b7243017b38
-size 3639
+oid sha256:309dce5eade33869e957f2520e4c3a6792a505af2b788cb7a305e9c5fd5d98d5
+size 3632
