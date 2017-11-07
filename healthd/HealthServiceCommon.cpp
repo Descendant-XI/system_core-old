9cbcb7cd995dfccc15b1ce139af3428a3df84b86
Yifan Hong <elsk@google.com>
2017-11-06 16:47:54 -0800

    Refactor common code between healthd and health@2.0-service
    Use healthd_board_* functions for the
    code that's different between them.
    
    Bug: 65990106
    Test: boots
    Test: VTS tests on healthd and health service
    Change-Id: I88e6f210cb91c0cf08a9d7c52910c38fa9a75130

---
+++ healthd/HealthServiceCommon.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de12af95363f76373e385a6965c62d44b9ce44037c64767300eb6b7243017b38
+size 3639
---
+++ healthd/HealthServiceDefault.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f87ea8ad316613631c6014eb6ad437833c018022ddf79697ddccb3698879db2
+size 879
