43e61a95461cfda721f5ee333d08e5bd811e5495
Yifan Hong <elsk@google.com>
2017-11-06 16:48:36 -0800

    healthd connect to health@1.0 hal
    This is the original code path (BatteryService
    -> healthd -> health 1.0 HAL). This ensures
    upgrade-ability for old devices to P.
    
    This is a partial revert of
    287c41fffeb144476092766466b86db9cfaf5e8c, with
    some modifications to the original healthd_board_*
    functions for healthd.
    
    Bug: 65990106
    Test: VTS test for healthd and health service
    Change-Id: If1c65e64e2fd6750369d52c8051ca6aa2b57ef27

--- healthd/HealthServiceHealthd.cpp
+++ healthd/HealthServiceHealthd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f87ea8ad316613631c6014eb6ad437833c018022ddf79697ddccb3698879db2
-size 879
+oid sha256:125f34c9765f50b78eebaf4b5b5b328f466ce9d0a63faf8c3519122788bbeec5
+size 2963
