ea0c43e815ea22d853e63a2612a6067afd1f24a4
Bowgo Tsai <bowgotsai@google.com>
2017-11-09 22:26:04 +0800

    first stage mount: abort when failed to parse fstab in device-tree
    The client of FirstStageMount class should check the existence of fstab
    in device-tree prior to using it. So raising a FATAL error inside
    FirstStageMount when failed to parsing the fstab, in order to expose
    more accurate error messages.
    
    Also fixing a comment in fs_mgr, where it might happen in either
    non-A/B or A/B.
    
    Bug: 69102431
    Test: boot sailfish
    Change-Id: Ifb525beaa3423b118644778bfe0f84bff9465303

--- init/init_first_stage.cpp
+++ init/init_first_stage.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b92eb66dab535b72ca0423b80ff0d5de9c0049cd12b19a37df67de68fdd51c78
-size 19022
+oid sha256:d45c67ff836a23fc108d3500346a827f26ded5c0fa9baf0db2d4818b9626e885
+size 19271
