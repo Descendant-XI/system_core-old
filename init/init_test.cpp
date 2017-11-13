7777266e8e6748cf7ee104cd2a5e8ea217b77b38
Steven Moreland <smoreland@google.com>
2017-11-13 15:31:54 -0800

    Allow a service to override another.
    For instance, on vendor.img:
    service foo /vendor/bin/nfc
        ...
    
    And then on odm.img:
    service foo /odm/bin/super-nfc
        override
    
    Allows a service on ODM to override a HAL on vendor.
    
    Bug: 69050941
    Test: boot, init_tests
    Change-Id: I4e908fb66e89fc6e021799fe1fa6603d3072d62a

--- init/init_test.cpp
+++ init/init_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcb186e9c6c134736cf7e4927db7208c225e1b79c61a2c6a3b8b9ff9a9259d1c
-size 5720
+oid sha256:7c678ad10a9a0b03716926481324fa8b8e007d69cf4ac10a5a6311ad953421ef
+size 6616
--- init/service.h
+++ init/service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97570b435ff9cb5cfadb3b5151ff8d9039982aba8eea7f85ce3a4ad7bd3f8b7d
-size 10608
+oid sha256:00b588f0a2af1763fbe37913349055a2ac726c67a05265922382bdf71bcf6f0f
+size 10761
