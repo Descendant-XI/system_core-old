914694e5a30633bb542bfceae57bcf00d4ed46c2
Yifan Hong <elsk@google.com>
2017-11-13 13:31:35 -0800

    healthd: clean up unused code
    With migration to health 2.0 HAL, there are unused code paths
    that can be removed.
    
    Test: health VTS test
    Test: charger_test
    Bug: 68724651
    Change-Id: I9af2f95432529a0bf6e10980a99014d570dfce93

--- healthd/charger_test.cpp
+++ healthd/charger_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a51a39f35df94ae9d490709dfccb3541c3c5403a6e29a30c81e8ac706f7d4c
-size 5417
+oid sha256:7d483ba6ae2c54279b5efc82d8ca75e2cfd8a695a7468411fad02117723cc22f
+size 5792
