0293ed09beca425280fa4a41d1ca2c2419ff5322
Yifan Hong <elsk@google.com>
2017-11-13 16:41:33 -0800

    healthd: move healthd_board_init call to Health::Health() c-tor
    The invocation is wrapped so that clients of -impl doesn't
    have to call healthd_board_init directly.
    
    Test: health VTS test
    Test: charger_test
    Bug: 69268160
    
    Change-Id: I61f820b80ce6b03fe51ca05d07ed7e09ca369d2d

--- healthd/HealthServiceDefault.cpp
+++ healthd/HealthServiceDefault.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f87ea8ad316613631c6014eb6ad437833c018022ddf79697ddccb3698879db2
-size 879
+oid sha256:8a93b244f4917a1826da9c368518fb05a2cda541f136894626d1151c0f61fc92
+size 1267
