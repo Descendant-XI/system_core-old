689b63e3f2e7ca7a708b447bae96b51cff84c601
Bowgo Tsai <bowgotsai@google.com>
2017-11-15 18:06:48 +0800

    Adding /odm support
    Treble system-as-root GSI needs to work on devices with and without /odm
    partition. This change creates the following symlinks for devices without
    /odm partition:
    
      /odm/app ->/vendor/odm/app
      /odm/bin ->/vendor/odm/bin
      /odm/etc ->/vendor/odm/etc
      /odm/firmware ->/vendor/odm/firmware
      /odm/framework ->/vendor/odm/framework
      /odm/lib -> /vendor/odm/lib
      /odm/lib64 -> /vendor/odm/lib64
      /odm/overlay -> /vendor/odm/overlay
      /odm/priv-app -> /vendor/odm/priv-app
    
    For devices with odm partition, /odm will be used as the mount point and
    those symlinks will be hidden after mount.
    
    Bug: 70678783
    Test: boot a A/B device without /odm partition, checks those symlinks exist
    Test: boot a A/B device with /odm partition, checks those symlinks don't exist
    Change-Id: I4d960b52e2754f461188f3ab562c751f502046ea

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff7d83e36770d21eebc9c6f2adcb5d45c047959e516cb8668f6c6b621570a4b
-size 11051
+oid sha256:0bcffb45a8a61a5bcd8df2c091ebe83e995fcda02d1e8cef5497452f4c64dd97
+size 12149
