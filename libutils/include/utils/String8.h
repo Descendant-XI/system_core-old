d7e4ec8748a6e1a65891480d91d99d74be51050e
Logan Chien <loganchien@google.com>
2017-11-22 18:31:10 +0800

    libutils: Cleanup unused class declaration
    This commit removes unused class declaration for SharedBuffer and
    TextOutput.  SharedBuffer has become internal implementation details
    since 282efae9c.  TextOutput usages have been removed since 9eb2a3b1.
    
    Test: AOSP and master build w/o problems
    Change-Id: I1871c4919a46f1ea8f41fb7eb79b4dc800b6f6f4

--- libutils/include/utils/String8.h
+++ libutils/include/utils/String8.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a191233b23bfc9a1fb54a8360a992c54b1c900f5d61f4938db61e58af63af495
-size 12569
+oid sha256:215a86c00a303ebb04ced991b61efce0c050b6833032067390f47c5459aac186
+size 12551
