c991951d0ab49bab5f71e156fb0921dce9e779d7
Christopher Ferris <cferris@google.com>
2017-11-27 14:50:38 -0800

    Allow multiple threads sharing a map to unwind.
    Add a mutex in MapInfo, and a mutex in Elf. Lock the creation of an Elf
    file using the MapInfo mutex, and lock when calling Step, GetFunctionName,
    or GetSoname since they can modify information in the object. It might
    be beneficial to use a fine grained lock in the future.
    
    Change the Maps object to contain a vector of MapInfo pointers rather
    than the total objects. This avoids copying this data around.
    
    Add a test to libbacktrace to verify that sharing a map while doing
    unwinds in different threads works.
    
    Add concurrency tests in libunwindstack to verify the locking works.
    
    Add always inline to the RegsGetLocal arm and aarch64 functions. I had
    a case where clang did not inline the code, so make sure this is specified.
    
    Bug: 68813077
    
    Test: New unit tests to cover the case. Passes all unit tests.
    Test: Ran a monkey test while dumping bugreports and verified that
    Test: no crashes in libunwind.
    Test: Remove the locking and verified that all of the concurrenty tests fail.
    Change-Id: I769e728c676f6bdae9e64ce4cdc03b6749beae03

--- libunwindstack/Elf.cpp
+++ libunwindstack/Elf.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:241ac4dfd0d2db41ca7629d318d330275e4e9cfbed0ddae25e7f3c2f0ece73fd
-size 5979
+oid sha256:c225fce6f1c1c861038fe8ca30804146398d2c8db565eb37e2031e203e2d615a
+size 6198
--- libunwindstack/MapInfo.cpp
+++ libunwindstack/MapInfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9debd4044213c5bcd1fd37d02b1cebdc2c8203937a49077eb43e0c48d347cf1b
-size 3288
+oid sha256:01a9ce129942a04fc1bf21c14e9234a78be142ab84f5d10409ef9cdc3f523268
+size 3420
