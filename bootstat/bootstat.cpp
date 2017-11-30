8fb1560b7604cbc13d2886770b271deb1dd22391
James Hawkins <jhawkins@google.com>
2017-11-30 15:01:40 -0800

    bootstat: Add four more boot reasons.
    Bug: none
    Test: none
    Change-Id: Ifd62e3a8bb6142e89fcde819eab1caba3d7933b7

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5f79ecdbf95455c8e3c4a6ab7234b6521dc2943a8c12f8bfbaf6f2f2dc9021e
-size 40521
+oid sha256:70249f6dece50833c6efdbda2d9c15b20859f4e4ecbe1b457caf6cacf1ee79c0
+size 40651
