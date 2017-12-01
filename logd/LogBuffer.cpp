921634674525fcbbb869c9b62727faf32da5ff29
Yao Chen <yaochen@google.com>
2017-12-01 15:48:19 -0800

    make log.tag.stats_log work for stats buffer.
    Test: adb shell setprop log.tag.stats_log S
          And saw the stats logs are silent.
    
    Change-Id: I7a9313f5f12029f9b8a8f070de1e1db659675525

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc5884fa2fa10e4b9fc54391ab3230b97b51709ba6739924fd1bf4da7160261
-size 44130
+oid sha256:85a90de085a2f748842c12812dcfc89941b6fd78bfbe30010c0e4f1f7fff245d
+size 44156
