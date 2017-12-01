4b5ae53e5b68f4c9d5601fc6fd3fbb13c70148dd
Ajit Kumar <kajit@qti.qualcomm.com>
2017-12-01 10:15:59 +0530

    Enabled SYS_MODULE capability for wifi@1.0-service
    Add sys_module capability to enabled insmod operation.
    
    Merged-In: I6436c3297e5318bd00ae3582549a11cb82ccbafd
    Change-Id: I6436c3297e5318bd00ae3582549a11cb82ccbafd
    (cherry picked from commit c2a059fbf005b29f9ca8a69ea48f58ffa1359abd)

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27bc8eec2c91284814d38033a6305fba4277e159f71ae59caabd582e6d88e2f3
-size 17722
+oid sha256:c25bd138df2aa9ea6bf18651e6ed1386e5fe90ce5af468784ea7852bdcb90cb1
+size 17794
