a9ce9d9a1198625b632a48e7c445fd36cdd5fb7f
Yabin Cui <yabinc@google.com>
2017-12-06 14:20:07 -0800

    base: allow creating tempfile in a custom temporary dir.
    Bug: http://b/70232908
    Test: none.
    Change-Id: I24b894793f6725186b3582a91db083427e51aa5a

--- base/include/android-base/test_utils.h
+++ base/include/android-base/test_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72e3481211574cb2c1a771cf787999ff0e304dbbf555f1e7625bbe831f2e50dc
-size 1514
+oid sha256:a1b06f05260e6f3bec2ca8f46da8e02f3bc92999e066487d548721b4f1d3563c
+size 1568
