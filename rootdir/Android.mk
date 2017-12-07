6efea0e938b02cf395edf6dc1e0b2af6148a781a
Jiyong Park <jiyong@google.com>
2017-12-07 09:59:48 +0900

    Reland "Prevent vendors from accessing private VNDK libs"
    This reverts commit 875fe42a81696f44cc03eeaa538f370b44639bb7.
    
    ... and fixes a bug that private VNDK libs are not accessible even to
    RenderScript framework libraries (such as libRS_internal.so). This is
    fixed by exceptionally allowing the private libs only to 'rs'
    namespace.
    
    Bug: 70260989
    Bug: 70279973
    Bug: 63878960
    Test: Build for walleye and tested Photos edit.
    Test: other renderscript app (camerascript) works as well
    Merged-In: I3978cbe2e869f6a1c214c755ded038349bde7b6b
    Change-Id: I3978cbe2e869f6a1c214c755ded038349bde7b6b
    (cherry picked from commit 781c47f4da82062c5445d53064566673c70119ee)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d79d7f8f6f0f929a3b70dcb1144dfb3bfd669236064ccc495a7c7ced7ebb7f
-size 9431
+oid sha256:fdf1ff6820cded17b6521aa3ac915d2ed019ec097b409969d7b6a64660d1af43
+size 9853
