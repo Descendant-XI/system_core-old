b445bfce2e0e017a72746c9a480f85299fde003b
Suren Baghdasaryan <surenb@google.com>
2017-12-08 12:58:52 -0800

    lmkd: add ability to monitor all vmpressure events
    Ability to monitor all available vmpressure event levels is needed
    to accommodate systems with different memory resources. Low memory
    systems can rely on medium and critical level events because working
    under memory pressure is usual mode of operation. High performance
    systems with more memory need to react earlier using also low
    vmpressure level events to free memory early and prevent low memory
    condition affecting its performance.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: I0cef1bd4c97d32c005045ae47f0ce3464ed98899
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f23173172a42e4a865c8f462a13080327a9607e60f1b358b38840057a91d4e1
-size 23640
+oid sha256:9667e594eae3aea4de10680223b78c1cf8aa7eeaf49aec546c653bfe585d8538
+size 24504
