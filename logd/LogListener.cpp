02d709cbfd2ef2dafca4f5a5da8cc7aeddbbdddf
Yao Chen <yaochen@google.com>
2017-12-08 23:49:26 +0000

    Revert "add aid check when writting into logd stats buffer. For now we require aid to be system."
    This reverts commit ec0c886da6ff8a1f2855f496b40fd4459c74e7ea.
    
    I will submit the new permission check through AOSP. Revert this CL to avoid merge conflict.
    
    Change-Id: I5d29ad2cda3579b37dd7136037af0c85c3917f6b

--- logd/LogListener.cpp
+++ logd/LogListener.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33cbce7b432a50c8670d4563ae43909e73c5c4348d96f70cb58154dea2cf803a
-size 5299
+oid sha256:d30f59e6f37653aa3948f296cb3066a318b9706a1ecf3a3dec00f15ec7b38d08
+size 5037
