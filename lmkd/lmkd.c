e86f8a28f38cd967f21495b92bdc21da322c1517
Suren Baghdasaryan <surenb@google.com>
2017-12-08 13:08:41 -0800

    lmkd: change defaults to disable event upgrade/downgrade logic
    (cherry pick from commit ad2fd9150bdbb9abdbc26c6a395f007b4cca7567)
    
    vmpressure upgrade/downgrade logic based on swap utilization works well
    for low memory devices because of a small swap size, however for high
    performance devices this measure is not a good indication of the memory
    pressure because of large swap resources. This change sets the default
    levels to disable upgrade/downgrade logic by default and each device
    can set these properties appropriately.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: Ifd4fbd4d6bb3e82f0f87b029df94934f1e7b1c9c
    Merged-In: Ifd4fbd4d6bb3e82f0f87b029df94934f1e7b1c9c
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:423a694a10cb2e7e8b3f56116182d124c2565c3f740d2f00f20cbff98963c3a2
-size 25050
+oid sha256:f77b3e2ef4bb57f8a68401c50cee5121862d071bd6bf1036df6a7c74d088ef55
+size 25130
