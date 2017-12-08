88077ab14f5d3f6bb6a6b1d4a3687a62c1c95628
Suren Baghdasaryan <surenb@google.com>
2017-12-08 13:17:06 -0800

    lmkd: add logic to kill the heaviest of the eligible processes
    (cherry pick from commit 662492ab1d21f138483a8f3943483924e8779d29)
    
    Killing the most memory-demanding process from the set of eligible
    processes yields better results on high-performance devices than
    killing the first one we could find. This is in line with how in-kernel
    lowmemorykiller driver chooses its victims.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: Ie1ef7f33f3e79698a9b4120c14490386d6129f9b
    Merged-In: Ie1ef7f33f3e79698a9b4120c14490386d6129f9b
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f77b3e2ef4bb57f8a68401c50cee5121862d071bd6bf1036df6a7c74d088ef55
-size 25130
+oid sha256:d358ccd0e8bf3fd7012a39ae88213c80b09bbcf8c0cd5d1ebac7ef6686080cae
+size 26036
