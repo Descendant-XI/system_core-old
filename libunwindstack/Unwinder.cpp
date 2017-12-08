fdc7462a0d4c9c732d0b592b8618d2fa3e8f19cc
Christopher Ferris <cferris@google.com>
2017-12-08 15:04:49 -0800

    Do not check arch for format check.
    Use a generic check if the address is 32 bits when using the default
    formating of a backtrace line instead of an arch check.
    
    Test: New unit tests pass.
    Change-Id: Id609abc037d7b437a02d52763aa91fbefe5f4d5b

--- libunwindstack/Unwinder.cpp
+++ libunwindstack/Unwinder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e171d2ec533bcea722a3691c0ea1e7cbf8180281e0aecabd435b2721e949ce2
-size 6514
+oid sha256:dc8f1ef93e33041ecf4ecb28049ea61c651aff99bb70f0b54d1420128f93c17f
+size 6480
