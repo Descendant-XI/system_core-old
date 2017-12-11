2096a351b7c205d769038ddcf4adca4c2bf434b1
Tom Cherry <tomcherry@google.com>
2017-12-11 01:40:07 -0800

    Introduce property types
    Properties right now can take any format, but that makes it hard to
    specify an API for these properties as Treble intends to do.
    Therefore this change introduces the idea of property types, described below.
    
    1) 'string' this is the default type and allows any property to be set.
    2) 'bool' this allows only boolean values (true|false|1|0)
    3) 'int' and 'uint' these allow signed and unsigned integer values
    respectively.
    4) 'double' this allows floating point numbers with double precision.
    5) 'size' this allows for strings matching [0-9]+[gkm].
    6) 'enum' this allows only a specific set of space deliminated values
    to be set, e.g. 'enum allow these strings' only allows one of 'allow',
    'these', or 'strings' to be set.
    
    Bug: 70858511
    Test: unit tests, test that properties are only set if their type matches
    Change-Id: I7a6b00fb43ec630d1f56c9e9a1f1b61d3914f603

---
+++ init/property_type.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfe88f401e89c62abff7f91ba0bf4dc780d73f06a8c3fc03f5a63638bee47796
+size 2304
---
+++ init/property_type_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b7ac7a63f6ca67fcf498afbb1a2e09c8e9950919a1dc06066dcf87b69bfe961
+size 3485
