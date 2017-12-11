cb0702f3d89e7b20c506a7ecd62d0a38016b31ef
Yabin Cui <yabinc@google.com>
2017-12-10 17:55:12 -0800

    libBacktraceOffline: try both .debug_frame and .ARM.exidx on arm.
    Currently, libBacktraceOffline doesn't try .ARM.exidx if a function
    appears in .debug_frame. This make it can't unwind through functions
    appear in both .debug_frame and .ARM.exidx, but having dwarf instructions
    not accepted by libunwind.
    This patch fixes this by trying first .debug_frame then .ARM.exidx on arm.
    
    Bug: http://b/69383534
    Test: run libbacktrace_test on arm.
    
    Change-Id: Ib95dd56d5cc123a20948e880b51b28ddc04b4a6e

--- libbacktrace/BacktraceOffline.cpp
+++ libbacktrace/BacktraceOffline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89629c5c6e43e537d8d3a5b09c013ecc42791a5d5acadd537f38b8e1c8d03288
-size 28988
+oid sha256:0ec84ac18fd76f10ceb99889aaab6eb2168bb5e0869768f70f64ce6d4238f8f2
+size 29082
--- libbacktrace/BacktraceOffline.h
+++ libbacktrace/BacktraceOffline.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:624312e8648b0f3e24492feb497f63bb9ac951655d34b2dab9a3ba46abdf9190
-size 2285
+oid sha256:1543caf8a9c5ad8b80a2fda9d05ce2902f159809a95afa240b2b9bfa4971519d
+size 2784
