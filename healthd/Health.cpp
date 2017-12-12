c71f10b399e0f76ee9887ed8b7465b4b69b7e996
Hridya Valsaraju <hridya@google.com>
2017-12-11 17:32:22 -0800

    Move healthd default implementation to hardware/interfaces.
    Bug: 70533363 63702641
    Test: make -j32
    
    Change-Id: Icb6eaea8641dc8ae53ed3af1fca81ddca39f8421

--- healthd/Health.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9b22498d545adb720b1e97cc0a5d78f5ab96cc94c2b39b3ff18101f448586447
-size 5418
--- healthd/healthd_common.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7cb88b355be8ebc71c832830a556c2c6884212fb56979063450c9e5a5b9a0eb8
-size 8350
