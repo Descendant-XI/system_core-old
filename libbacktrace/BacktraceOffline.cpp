8b7611d21a1fd2a32eb2b90878347b911dbe4d5d
Yabin Cui <yabinc@google.com>
2017-12-18 14:37:54 -0800

    libBacktraceOffline: improve unwinding callchains.
    1. Extend MAX_BACKTRACE_FRAMES to 256, as we can have callchains
       with length near 256 when recording Android applications.
    
    2. .eh_frame and .ARM.exidx doesn't know how to unwind from instructions
       setting up or destroying stack frames. It can lead to wrong callchains,
       which may contain pcs outside executable mapping areas. Stop unwinding
       once this is detected.
    
    3. Some CIE entries in .eh_frame/.debug_frame suggest unwinder reading
       personality routine from memory. However, that is not available in
       offline unwinding, and leads to ACCESS_MEM_FAILED error. Work around
       this by returning all zero data when failed to read data for some
       addresses.
    
    4. Some libraries have empty .eh_frame_hdr but valid .eh_frame.
       Work around this by building eh_frame_hdr manually.
    
    5. Prefer to use .debug_frame than .eh_frame. Because the former
       can unwind instructions setting up and destroying stack frames.
    
    Bug: http://b/69383534
    Test: run backtrace_test.
    Test: run simpleperf on some apps.
    
    Change-Id: I62dddda3865dd567c6be150ce5527dc2d3516256

--- libbacktrace/BacktraceOffline.cpp
+++ libbacktrace/BacktraceOffline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c44deb56eca7e5289549cd42f57ca9ec67b354192c2684ff33ad08e1c0ec687f
-size 29994
+oid sha256:c7ff8f89c673562b1b1c17d2f2143b399a958606d2135cc724a6434c4c5cf521
+size 37376
