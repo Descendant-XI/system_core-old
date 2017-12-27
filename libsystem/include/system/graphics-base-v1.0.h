e13597d9cece1edae3f615063c12cd4cdeaa45ae
Craig Donner <cdonner@google.com>
2017-12-27 14:55:21 -0800

    Create versioned graphics-base-* files.
    Rather than constantly updating the one file as HAL change,
    have a header per version. For now, the old file just includes
    the new ones, until all the usage of graphics-base.h can be
    cleaned up.
    
    Bug: 66900669
    Test: build
    Change-Id: I396ce24f8e62b52c129a2b276698e4f00b1993f7

---
+++ libsystem/include/system/graphics-base-v1.0.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f66c371c5c2c8d554776acb528b1c9e272553f08aee8b6981471f9c33609043d
+size 6860
---
+++ libsystem/include/system/graphics-base-v1.1.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e5041d6a8f878c5cc0bd172d0ad7604d1bad14112965106e655e5be9dc5d005
+size 768
