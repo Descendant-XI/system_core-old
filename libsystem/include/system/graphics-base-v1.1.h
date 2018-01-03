b93097333d3cf8c6e2d3feb59ac9fda774144e6d
Courtney Goeltzenleuchter <courtneygo@google.com>
2018-01-03 12:03:58 -0700

    Add Dataspace and PixelFormats to support HDR
    Bug: 63710530
    Test: make
    Change-Id: I40c668978972598d057e4302f27ebc65f59cf895

--- libsystem/include/system/graphics-base-v1.1.h
+++ libsystem/include/system/graphics-base-v1.1.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e5041d6a8f878c5cc0bd172d0ad7604d1bad14112965106e655e5be9dc5d005
-size 768
+oid sha256:cf7d225a3d5ae663a39af7aadc240ba7aede6f3c8975541265a21ae695475d99
+size 1073
