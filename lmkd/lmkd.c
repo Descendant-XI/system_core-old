9734f311c3e32a4ae073502c1563ad43cb363010
Suren Baghdasaryan <surenb@google.com>
2018-01-04 10:43:58 -0800

    lmkd: Add ability to trace lmkd kills
    (cherry pick from commit c71355991d4bafb4694f6252ac10e288a5fb9f75)
    
    For tracing lmkd kills inside kernel it is useful to have traces
    indicating when and which process lmkd is killing. By default the
    tracing is disabled.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: I3ceb2bde0c292eec55855cb4535927f3b4c5d08b
    Merged-In: I3ceb2bde0c292eec55855cb4535927f3b4c5d08b
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d358ccd0e8bf3fd7012a39ae88213c80b09bbcf8c0cd5d1ebac7ef6686080cae
-size 26036
+oid sha256:654519dfec493c86324b201e8711854681390dea4a806a34c81fa1b6fdc1e5fb
+size 26529
