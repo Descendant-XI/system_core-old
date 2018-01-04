96f43437d6b3e4ea25caf563ac7b86681ae152ac
Suren Baghdasaryan <surenb@google.com>
2018-01-04 08:50:48 -0800

    lmkd: Remove stale dependency on libprocessgroup
    (cherry pick from commit b333f83481d3de7a29a7aa8d27456af89581c12b)
    
    Remove stale dependencies and header file inclusions
    
    Change-Id: Ic0e7adb5bd2a0832937a831b6918e52ace4ad46a
    Merged-In: Ic0e7adb5bd2a0832937a831b6918e52ace4ad46a
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:654519dfec493c86324b201e8711854681390dea4a806a34c81fa1b6fdc1e5fb
-size 26529
+oid sha256:7535a441c6ba64cdd740f8f845457d950a48b418926abae9f405ea6e13f7c153
+size 26472
