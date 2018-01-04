0a23f4d5eead733d600868fdcb414cadf1581414
Steve Muckle <smuckle@google.com>
2018-01-04 08:42:15 -0800

    rootdir: make sure the /odm mountpoint is always available
    The odm partition will eventually be required. Prepare for this by
    creating its mount point.
    
    Bug: 37322799
    Test: run cts-dev -m CtsPermissionTestCases
    Change-Id: Ibd031b68dd7328c853ded401bb2690dbd6675141

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee03d000637c298c1c245d80bbdef42e229f51b807654f9379a3255ca314814c
-size 8420
+oid sha256:b891820972f83a611141b88610ac437ddc462b90d48a4d56120182dbe9fa8eaa
+size 8424
