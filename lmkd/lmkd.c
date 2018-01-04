7d5d63cb65bc715e886543dd3087d5fa974dddd3
Suren Baghdasaryan <surenb@google.com>
2018-01-04 09:16:21 -0800

    lmkd: Detect the highest level of vmpressure when event is detected
    (cherry pick from commit e82e15c242d32272fe3493b0d358329e6e3d9fa7)
    
    lmkd checks for vmpressure events using epoll_wait() with eventfds of
    all registered events. It's possible that multiple events of different
    priorities happen before epoll_wait() returns. For these cases we
    use conservative approach by assuming that the system is under the
    highest registered vmpressure levels. This speeds up lmkd response time
    to high memory pressure by not responding to possibly stale low pressure
    levels when vmpressure rises quickly.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: I79a85c3342e7e1b3a3be82945266b2cc60b437cf
    Merged-In: I79a85c3342e7e1b3a3be82945266b2cc60b437cf
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f33182ff09e8adca53e6829dd950db744e319a2356d9c74d86d7b7b850628355
-size 26492
+oid sha256:060b3943f8323cff249c6b6589dcf06d1b367b03bb8410547410bc5ac216eb99
+size 26803
