253fd9d457fdfed3d6601d07be4504a03076e554
James Hawkins <jhawkins@google.com>
2018-01-05 14:42:55 -0800

    bootstat: Add more boot reasons.
    These appear to be winding down...
    
    Bug: none
    Test: none
    Change-Id: I00bc84d8e50b1563f97f812f722aa6bdbd6265b2

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14d98784d6400e2e629baacbfdfc1fe3b48c157e86f0a5041610b924e63ef1b6
-size 41527
+oid sha256:58d73398c3e1abc534a47dbcde1696351f90225560f116179480ff1c9898e122
+size 41794
