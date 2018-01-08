034e49dd31bd6f5fbb39d08a178ae0dfb6c8b534
Luis Hector Chavez <lhchavez@google.com>
2018-01-08 13:02:18 -0800

    libcutils: Stop checking the uid of netlink messages
    This is a partial revert of
    https://android-review.googlesource.com/c/platform/system/core/+/528619.
    The uid-check was done before
    https://patchwork.kernel.org/patch/1525551/ was available, and thus is
    now unnecessary to perform such check, since we can just rely on the
    kernel patch now.
    
    Bug: 71632076
    Test: aosp_sailfish-userdebug still works
    
    Change-Id: I1d1319b774483d1714ecb4dd838a20e055acc2a3

--- libcutils/uevent.cpp
+++ libcutils/uevent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:312f5cb57f0c86ff61df84b5ca34aad37e39c0136ff7074dd058d842e220bfe6
-size 5276
+oid sha256:85b3a10d796fc0ec2dc168c42e5e652cc35c9d420ad14cb72402ef6922026880
+size 3497
