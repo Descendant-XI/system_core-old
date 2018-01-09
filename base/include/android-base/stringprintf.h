67f135e9c0c902e1cc2d68ce741c1606a538ca6f
Elliott Hughes <enh@google.com>
2018-01-09 11:16:55 -0800

    Add __attribute__((__format__)) to fastboot's die().
    Also make the <android-base/stringprintf.h> macro less likely to collide.
    
    Bug: N/A
    Test: builds
    Change-Id: I868a0a0002c9c53c1ec0670153df2af5f48fd969

--- base/include/android-base/stringprintf.h
+++ base/include/android-base/stringprintf.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44a7a7c6cdc36cd7d4fa21f927acd87226d509dfee3c1044485e1cfd821cf138
-size 1922
+oid sha256:bd99bc71f9537a03408026a7018ce42dc9b1f94d091dabd74e9247803438d62d
+size 2013
