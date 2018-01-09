2419234acf454db142b45aaa0e0559df68cc533a
Changrong Jin <jinchangrong@huawei.com>
2018-01-09 11:29:54 +0800

    fastboot: fix log print type mismatch
    Test: mma -j8
    Change-Id: I8b4373f4a044b802fc9f1dd4f998e880f7843b4a

--- fastboot/engine.cpp
+++ fastboot/engine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b559e2d5abb6218d2c04771a37c763a210faaa412a1bf177153c63d68cee0dc
-size 11554
+oid sha256:7b1df022163ac3d32f2c418e9bdb1933626fa9fa6e448be8b02325012f7e0239
+size 11556
