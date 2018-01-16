7a9ef62a756d7d158e297b68b1345a3c7d13128e
Roshan Pius <rpius@google.com>
2018-01-15 16:39:33 -0800

    fs_config: Move hostapd to /vendor/bin/hw
    Bug: 36646171
    Test: Able to turn on softap
    Change-Id: Ibd82581a12b4bd2e7d22825361706e35f292ce2b

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7bcf4974df31bda34926b8f4f6dc91baf76cf60c4b3f3422afbafe8583ba523
-size 17652
+oid sha256:75a1995761b5c8bbac496fabbf7fc638d14f6f114c37b35dadfd53d62f0d287c
+size 17983
