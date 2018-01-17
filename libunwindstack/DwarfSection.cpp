d7a1a2d7b5873ba1b28da4e9901b341373560496
Christopher Ferris <cferris@google.com>
2018-01-17 12:59:45 -0800

    Add support for getting a dex pc.
    Changes:
    - Change the register type from int16_t to uint32_t for the location data
      and the Eval processing. This is because the special dex pc is > 65535.
    - Add the ability for Dwarf register location information to point to a
      register that is itself a Dwarf location register.
    - Add dex_pc to the frame information.
    - Modify the unwind tool to print the dex pc if non-zero.
    
    This does not implement the printing of the dex information in anything
    but the unwind tool. It's not the final form of this printing.
    
    Bug: 72070049
    
    Test: Ran new unit tests.
    Test: Dumped stack while in interpreter running 137-cfi art test and
    Test: verified dex pc is set to non-zero.
    Change-Id: I6ce8a6b577fb4f92abacbd433b1f68977e272542

--- libunwindstack/DwarfSection.cpp
+++ libunwindstack/DwarfSection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e071f07e9fe9d6b8e67a4f2fafff1aa3e26212bd9e565c5521c5ab184b8728
-size 25742
+oid sha256:4bef2feace6a21b2902b7ebdce4b922d8c5518300d552bbc73efa50ec801c2d9
+size 26857
