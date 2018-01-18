fcba2697e4411b6f7c82e91ec57ca55ad43c757d
Hridya Valsaraju <hridya@google.com>
2018-01-17 23:02:56 -0800

    health: Modify IHealthInfo Callback interface to return V2.0 HealthInfo
    Bug: 71860528
    Test: vts-tradefed run vts -m VtsHalHealthV2_0
    Change-Id: I7de39ffa4c21772d8878c9a3f054bc6dbbde2fb9

--- healthd/HealthServiceCommon.cpp
+++ healthd/HealthServiceCommon.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eced0fe7221bafdd30ce7fb69e85741debd2d77339d323e0ad3dbceaf431874d
-size 3019
+oid sha256:1e22cd35019110b2aec2e92d47b9f2b673eb57e1baa9f90333347371d755645f
+size 3027
