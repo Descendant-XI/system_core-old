3ad56dd3212feb3ed9cb7bdca69ee96f563a6fe6
Suren Baghdasaryan <surenb@google.com>
2018-01-17 17:17:44 -0800

    lmkd: Allow killing multiple processes to downgrade memory pressure
    Record free memory at low vmpressure levels and whenever pressure
    increases beyond low free up enough memory to downgrade memory pressure
    to low. This is done by freeing enough memory to get to the max free
    memory levels seen during low vmpressure.
    The kill logic for Go devices is not changed as these devices are designed
    to operate under high memory pressure.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: Ic8396eee08013b1c709072a13525601d5c8bf1f1
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0faf45049d3d1d28b792160544beb6b074d08fb600cbecf92426c54262b914c0
-size 26327
+oid sha256:64fc6da6d0d1ee0248c19503dbd1e84aaca0ec0646fac944b9f6fa5c5bdd2191
+size 29531
