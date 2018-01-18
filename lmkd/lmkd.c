33201cb65c4a897ee3fef06d12f0824c6e9f8397
Suren Baghdasaryan <surenb@google.com>
2018-01-17 17:28:01 -0800

    lmkd: Implement kill timeout
    New ro.lmk.kill_timeout_ms property defines timeout in ms after a
    successful kill cycle for more kills to be considered. This is
    necessary because memory pressure after a kill does not go down
    instantly and system needs time to reflect new memory state. This
    timeout prevents extra kills in the period immediately after a
    kill cycle. By default it is set to 0 which disables this feature.
    
    Bug: 63631020
    Test: alloc-stress
    
    Change-Id: Ia847118c8c4a659a7fc38cd5cd0042acb514ae28
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64fc6da6d0d1ee0248c19503dbd1e84aaca0ec0646fac944b9f6fa5c5bdd2191
-size 29531
+oid sha256:8de9109f424804c8d79f38c499e21500f576562f97df1c19cbd217fe2c4a9929
+size 30499
