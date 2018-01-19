576ed17b6ef74d6fe55afa424d7f94f2cdc75bde
Christopher Ferris <cferris@google.com>
2018-01-19 10:26:36 -0800

    Add extra frame when dex_pc is non-zero.
    Use the art dex file library to read the dex data.
    
    Add unit tests for the UnwindDexFile code.
    
    Bug: 72070049
    
    Test: All unit tests continue to pass.
    Test: Dumped the backtrace of the 137-cfi test while running in interpreter
    Test: mode and verified that the stack trace is correct. Did this on host
    Test: and for arm/arm64.
    
    Change-Id: Ia6f343318c5dd6968a954015a7d59fdf101575b0

---
+++ libbacktrace/UnwindDexFile.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:692e9aaf3e5fed9ed0933a5e0f72b5e4a9371a73378972f59598883ef6772bac
+size 5237
--- libbacktrace/UnwindStackMap.h
+++ libbacktrace/UnwindStackMap.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdc2515c61b60986e3574f9afcb863a5edeb97c6f822b6835eda1447ee62b1b9
-size 1732
+oid sha256:3c97e4783ad62e8a86eca7a9682595764d98c2d4d3a9ea58da6d3b7aafeefc80
+size 2007
