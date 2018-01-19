e66cda1bd3b874c709bdfa9b1f4eb8e75c2bb051
Todd Poynor <toddpoynor@google.com>
2018-01-19 14:03:59 -0800

    Revert "Revert "healthd: don't report fake data for battery-less devices""
    This reverts commit 1bc572dfe8ed82d6774a0c7e057ae56c53b3ee20.
    
    The presubmit test that (incorrectly) fails on the original commit
    has been marked @FlakyTest and a bug is failed on Assistant.
    
    Bug: 34507420
    Test: manual: gce_x6_phone (no battery or charger),
          boots and stays booted, inspect properties,
          presubmit tests on gce_x86
    Change-Id: I5509bf5de9fceffdd5fd8abc3f1f4f6101119248

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba25d5b3456a6d2f4a36ee991edbd201d87e0ec55b902775c7bde3a9edeae5af
-size 24697
+oid sha256:9d26d6580d241af24a219e3f6fb7c68f736b8133fae5a7f0b4e3488b17124718
+size 24060
--- healthd/include/healthd/BatteryMonitor.h
+++ healthd/include/healthd/BatteryMonitor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b52c79b21d290ecf72e94338bf4d8917d1f58e04e0b0170929ba3d7dfc60c858
-size 2034
+oid sha256:b25774ee04ca0b645162321004b3989224801eea9c5b3d21bdca0fc55c294a35
+size 2003
