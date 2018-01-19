e2b66e9f0faabd80813cad6c565b9b57d8699f70
Suren Baghdasaryan <surenb@google.com>
2018-01-18 17:27:30 -0800

    lmkd: Select in-kernel vs userspace lmk based on kernel driver presence
    Currently selection criteria for in-kernel vs userspace lmk is kernel
    driver presence and device not being a Go device. This change removes
    Go device check leaving kernel driver presence to be the only selection
    criteria.
    
    Bug: 71502948
    
    Change-Id: I394a7920433a8d090e207ea86296356413a63fe7
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8de9109f424804c8d79f38c499e21500f576562f97df1c19cbd217fe2c4a9929
-size 30499
+oid sha256:ea25291a369141d69aaa705ac613791a1c3dde87a9eed76d258c56b3646af7b2
+size 30482
