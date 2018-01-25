5a64ed707d42e96af053eed7cb148162f868a0b7
Suren Baghdasaryan <surenb@google.com>
2018-01-24 16:51:41 -0800

    lmkd: Introduce liblmkd_utils for communicating with lmkd process
    A number of tools and tests require communication with lmkd.
    In order to avoid code duplication liblmkd_utils implements
    functions commonly used when interacting with lmkd process.
    Isolate communication protocol details into lmkd.h
    
    Bug: 63631020
    
    Change-Id: Id840983d55b7db60013d52dee0c3187943811822
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

---
+++ lmkd/include/liblmkd_utils.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eea3812c707a8b70a081564c53160e777adfcd8916061ded5247007559522b2
+size 1527
--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c6308ed5a601310b163bb437485acb4df7c28ac68d210e66e6ef6caae340bb3
-size 33794
+oid sha256:7fbf82fa456da4135e2a977e6e775f79640bca153ae41351e9a6e2217170f050
+size 34078
