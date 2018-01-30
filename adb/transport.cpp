f4a9e24676a51a3abe4a76051e6e21da5c778b2a
Josh Gao <jmgao@google.com>
2018-01-29 18:09:20 -0800

    adb: fix a leak.
    Test: none
    Change-Id: I469bd93180cd2a8b6408f73e8c8f164f72b3345c

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fd7ae792223a5d37872a5d8a327fd92e031e78ef329d826e3f8aa1118be9b11
-size 34051
+oid sha256:dcdc354a72beb944bdbad98d44445c5a2669da6fe1a77da4dc6a24968d10a09f
+size 34083
