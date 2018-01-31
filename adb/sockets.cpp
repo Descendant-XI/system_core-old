3f4659e12ea7ff3c04e72ff00845a3abf0c68e11
Josh Gao <jmgao@google.com>
2018-01-31 13:15:51 -0800

    adb: switch asocket's intrusive linked list to vectors.
    Test: python test_device.py
    Change-Id: I24d7f5d0401de77d80c7a2dd5a7dcb551943342d

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e60768e8e78ee8f10b73ecdd105574a78aa85d50dfef18993685bdf8df35adce
-size 26188
+oid sha256:976716c24c7f92f1c482854e3efdada12753642f4bc9616ffe8bb4542b037bea
+size 25869
