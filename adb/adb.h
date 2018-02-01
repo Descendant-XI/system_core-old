4a563930d0646e93d79e4467780d3ea52da69ff0
Josh Gao <jmgao@google.com>
2018-02-01 13:17:50 -0800

    adb: switch asocket::enqueue to std::string.
    Switch asocket over to taking a std::string instead of apacket* for
    data. This allows us to remove asocket specific fields from apacket*.
    
    Test: python test_device.py with x86_64 emulator, walleye
    Test: adb_test on host
    Change-Id: I9d157ff331a75ba49a54fdd4194e3f6cdff722f4

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:360c8a5396b82bfdfaf577cdea93dbe840280bcd2e5bfc20297f80a67ea84c70
-size 7189
+oid sha256:e3b0923fac37188f104d04871a2b5f869acc1f9b22de6a435f15b76cfc942df3
+size 7137
--- adb/socket.h
+++ adb/socket.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:764cb1cf7a4d68c8a04655cfc222aa6f87d3ba89c980a8fcb2ed121cb00ca697
-size 3463
+oid sha256:dc2c7a0a854bd9debb90379657b7f2fb9597eeb036b628885382f442aa7eb83f
+size 3542
