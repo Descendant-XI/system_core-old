64c348730016e821d228473b7966226a03f1c8c4
Tom Cherry <tomcherry@google.com>
2018-02-05 08:01:54 -0800

    init: add TODO for mount operations.
    mount operations should be done in vendor init context, but their
    complexity currently limits this.  Add a TODO to make this reason
    clear to those viewing the code.
    
    Bug: 72488820
    Test: N/A
    Change-Id: I8b6dd92aa79f31dc24603559ed6de0815facfcba

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ab79938a1631a8c962fa42f98590a1ed1100cafdfbe8ab2d9e81f64c936b1cb
-size 37729
+oid sha256:2b3ad7066921322c4476afb248338fa70bb93adc03431aa7b2db6efbcb60900e
+size 38056
