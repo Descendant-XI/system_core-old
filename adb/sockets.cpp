fe24dfc8e71e8ce989f44d945001559b3b5387a3
Josh Gao <jmgao@google.com>
2018-02-05 18:49:10 -0800

    adb: switch apacket over to a std::string payload.
    Test: python test_device.py with walleye/x86_64 emulator
    Change-Id: I0a18941af1cb2279e5019a24ace25741def1202f

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ccb48ef7eb33a8c109af8d133116f55f612ce23b9ddedc169af3de9386453bb
-size 25246
+oid sha256:c78ded74dbad238f12c5a87178ba9722bea2da991c0873ee4d6d7e72e7af0f56
+size 25218
