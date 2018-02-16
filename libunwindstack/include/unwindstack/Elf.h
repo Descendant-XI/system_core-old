931ca9b0a093738cd24487975b365bcffd6b0b77
Christopher Ferris <cferris@google.com>
2018-02-16 13:48:19 -0800

    Modify elf cache to handle elf_offsets properly.
    Bug: 73498823
    
    Test: All unit tests pass.
    Test: Simpleperf run that previously failed, passes now.
    Change-Id: Iff3a1f2f641a46ab9a0326579af3649f0c76fc65

--- libunwindstack/include/unwindstack/Elf.h
+++ libunwindstack/include/unwindstack/Elf.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4faaeef89340c823e3a773f94730835ac8059efe2af49675cbe1f890397bd7c
-size 3338
+oid sha256:1cd96a2015466e3faaf990767200c47c62cd7b1eb8d9b26fd484b8dbdb5fdfc1
+size 3390
