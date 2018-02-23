3151ca8baab4ee07638c172448337ac4584b2a0f
David Srbecky <dsrbecky@google.com>
2018-02-23 18:06:13 +0000

    Check for data races when reading JIT/DEX entries.
    Update the entries only when the list is modified by the runtime.
    
    Check that the list wasn't concurrently modified when being read.
    
    Bug: 124287208
    Test: libunwindstack_test
    Test: art/test.py -b --host -r -t 137-cfi
    Change-Id: I87ba70322053a01b3d5be1fdf6310e1dc21bb084

--- libunwindstack/tests/DexFileTest.cpp
+++ libunwindstack/tests/DexFileTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46e86074e0693f2fa5b8964ee7899054cde36b3d04166341d755ad99bb788f88
-size 6511
+oid sha256:fbd826a7d744aab9c57dbc6707af8476072293dee1a881dcc8085cc0243d2e97
+size 6493
--- libunwindstack/tests/JitDebugTest.cpp
+++ libunwindstack/tests/JitDebugTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e280b019ea36103093e2464e5ad0ba3a6f13613a9361b0edd9448bd0d22dddc3
-size 14090
+oid sha256:eafeaa0a4979fe8c6c9a3ea9c4314835a9ab0c1a9b146bd63330cdf705c9dc43
+size 14801
