f66d2cb689c87ca8168c6607731a5596627f5e97
Jaegeuk Kim <jaegeuk@google.com>
2018-02-22 18:10:05 -0800

    fs_mgr: add sysfs_path=%s to indicate a location of sysfs entry
    This patch adds sysfs_path=%s in fstab to get the sysfs entry path.
    One example is that vold is able to conduct UFS-specific operations given the
    below fstab entry.
    
    /dev/block/bootdevice/by-name/userdata  /data f2fs  noatime,nosuid,nodev,discard
      latemount,wait,check,fileencryption=ice,quota,formattable,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc
    
    Bug: 63264275
    Bug: 68721792
    Change-Id: I5097873893948f12296ac5eaa0c2a59a6e6dd9e1
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- fs_mgr/include_fstab/fstab/fstab.h
+++ fs_mgr/include_fstab/fstab/fstab.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:562e167b9637676a0e383fde348557ab90ee0c6dd0d3a5cc950f116f39a1014d
-size 3073
+oid sha256:3fe4156c4cf3dc386a22553464baee2073a2b4a658880d80ffb902212c6f2dc5
+size 3153
