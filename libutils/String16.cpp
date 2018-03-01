11f1ccae1fca1a85931dc5b4c92dbedf8b0a3fb6
Steven Moreland <smoreland@google.com>
2018-03-01 11:03:04 -0800

    String16: remove integer overflows
    Bug: 73826242
    Test: manual
    Change-Id: I32e13d61b944c1a527cf2d95473552d246e322be

--- libutils/String16.cpp
+++ libutils/String16.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05e1e5cad6728ffc83dbab7a4c2425664f97f429a7c64d39b5c661210838b790
-size 10803
+oid sha256:90ac2507f7e9f28fc71cc35643fa1366504afd1157d5e3279b359cef17393f29
+size 11153
