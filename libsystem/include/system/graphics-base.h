ac69e89740f35f24d24c9082ec62f5ec624d1c92
Chia-I Wu <olv@google.com>
2018-03-06 09:15:25 -0800

    graphics: move some flex pixel formats to graphcis-sw.h
    Some of the flex pixel formats are software-only.  Move them to
    graphics-sw.h to identify files that use them.
    
    Bug: 70526789
    Test: builds
    Change-Id: I42757af810028f53bb6389c637a00c493041a4fb

--- libsystem/include/system/graphics-base.h
+++ libsystem/include/system/graphics-base.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:899f34ab5aab2692328a3b42cd42f3a78c5c33ca87d268d921acd5d2c9a7cd16
-size 482
+oid sha256:6df93ba7063662584dcda1750737619c1800d7861036caf33cfdfcdcb1cec13a
+size 180
---
+++ libsystem/include/system/graphics-sw.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:957e089937f69aee425c4a2de7995ab42bfbee606567963af625fa429e7f5497
+size 574
