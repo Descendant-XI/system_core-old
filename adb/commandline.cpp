08dcf7821951578c0edef94ece27e1ff724a20b6
Josh Gao <jmgao@google.com>
2018-03-07 16:51:08 -0800

    adb: implement std::make_unique, start using it.
    Add an implementation of std::make_unique for Windows, where we're
    currently stuck with C++11, and switch some uses of new over to it.
    
    Bug: http://b/74616284
    Test: treehugger
    Change-Id: I99b85f07754adda7c525243480c3e0bce9a25ce7
    (cherry picked from commit 31b5be69f4f39365ce0faad85faba604a2509022)

--- adb/commandline.cpp
+++ adb/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94aaef5e791d2cf6102c516301e73448d5a63097fb229044255e6d764a56e1ca
-size 75346
+oid sha256:d9b9ed88637dc506b50e4d1b14f3310afe5c5d48857c9ba179b141c7214ca62e
+size 75392
---
+++ adb/sysdeps/memory.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3dd89ce12cb1bb4ab0df82f42d6cb5c7ccda00aca26bddf7eefb0be2ab41055
+size 1058
