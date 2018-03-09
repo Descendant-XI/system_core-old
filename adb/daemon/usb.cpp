a5ba55264904d606f79798f4a4c8b135e77dfd28
Luis Hector Chavez <lhchavez@google.com>
2018-03-09 10:14:01 -0800

    adb: Open FunctionFS files with more restrictive access modes
    This change opens the control, bulk-in, and bulk-out FunctionFS
    endpoints with O_RDONLY or O_WRONLY.
    
    Bug: 70349025
    Test: Android on Chrome OS can still use ADB
    Test: aosp_sailfish-userdebug can still use ADB
    
    Change-Id: I3895b626cf7cec8437c36bdc6c6e6055aaf3777c

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ec89862233eff30a112b79a2293e8f3c8b178c33c6119875cd16590259bdae
-size 18150
+oid sha256:0909c550e920faa16f450fbdcd27ec590f8cfbb8ae6aabb95a099891c84465a5
+size 18156
