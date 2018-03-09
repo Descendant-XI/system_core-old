71f156cf556231dab66679d6100ad6a500f9e774
Yabin Cui <yabinc@google.com>
2018-03-08 17:03:04 -0800

    base: Add TemporaryFile::DoNotRemove().
    Bug: http://b/73127105
    Test: none.
    Change-Id: I563c12bfb629ddd630568dda4817fb10cc9940a8

--- base/test_utils.cpp
+++ base/test_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80869e4b6bf8b9c7831a03626eae86b96cb988894fde113ea4f5351d589821eb
-size 3808
+oid sha256:e5ee51cb5d8b6c7733a5d5183c2426cd4cb8e389b9e07abb3aa75778f9a4a75d
+size 3836
