7a82db2decab8a0594697ea2ca97b2d1ff52c378
Christopher Ferris <cferris@google.com>
2018-03-15 14:35:01 -0700

    Handle ARM thumb function symbols.
    ARM thumb functions will have the zero bit set to one, which can cause
    function name lookups to fail.
    
    Add an ARM special GetFunctionName that handles this condition.
    
    Fix a couple of the function offsets in unit tests.
    
    Bug: 74844137
    
    Test: Ran unit tests.
    Test: Ran debuggerd -b on processes on a bullhead device.
    Change-Id: Ibd407db34eaaa641f91fdb4f589c44a0dcc0216a

--- libunwindstack/tools/unwind_info.cpp
+++ libunwindstack/tools/unwind_info.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d89789360f33a4c7a42f892638c145f3ea42243c123dbc2f2ff229ae60ce441
-size 6135
+oid sha256:f90da42d832f8446cd80bc7e6fa7da3610849055d277f56fd971023106f5a34e
+size 6070
