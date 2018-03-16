6c6468c7a725155774b61e53763dec407345e0dc
Mark Salyzyn <salyzyn@google.com>
2018-03-16 11:00:14 -0700

    bootstat: usb and rtc refinement
    For all known cases, if usb is present in the bootloader reason,
    then it is actually reporting a cold,charger canonical boot reason.
    This signifies that the device was powered down, and was woken
    up by the charger being connected.
    
    For all known cases, if rtc is present in the bootloader reason,
    then it is actually reporting a cold,rtc canonical boot reason.
    This signifies that the device was powered down, and was woken
    up by the rtc clock.
    
    Test: boot_reason_test.sh
    Bug: 74595769
    Bug: 63736262
    Change-Id: I04bede0b7ccaa1b859943f7def93521a8f7b25c6

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75e39a425a3fb05fdd833b3175f54de07314912581532ba5f7ebc26db343a3bd
-size 42863
+oid sha256:e9b1e9eab4a148b3bc356d71b31b1538f1c8871ee8fa514f61eb1adb7411b432
+size 42975
