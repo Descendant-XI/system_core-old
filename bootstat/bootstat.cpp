a2dce49ab9c42ed1b74aaf79adf157a9909d57b1
Mark Salyzyn <salyzyn@google.com>
2018-03-16 09:05:59 -0700

    bootstat: add support for suppressing needle match on output
    For aliasReasons allow one to optionally suppress needle for output
    member using a <bang> (!) character prefix.
    
    Test: boot_reason_test.sh
    Bug: 63736262
    Bug: 74595769
    Change-Id: If35518c08cf909c6c78a16275e9d8dfd0ff839a9

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9b1e9eab4a148b3bc356d71b31b1538f1c8871ee8fa514f61eb1adb7411b432
-size 42975
+oid sha256:27783d0b71cf88510f1fec4c5194a785851ec6e74aa218eeee64de9c6ff49afd
+size 43201
