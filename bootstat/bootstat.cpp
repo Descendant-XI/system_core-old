806664717c315e2f994af66124f800c39c955be6
Mark Salyzyn <salyzyn@google.com>
2018-03-16 11:00:26 -0700

    bootstat: label Mediatek boot reasons
    Comment which strings come from Mediatek devices to aid maintenance.
    
    Test: compile
    Bug: 74595769
    Bug: 63736262
    Change-Id: Ic60970bcbbb389e4158ee691c4c81e6514d3b870

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1bf2b963dfb39665ef1175f5e7338840792c347b3e391d36cf28b929dec5d89
-size 53375
+oid sha256:98938c279cd1062dc410ace5b302559f82a6087d9cd2f3dbe248650a366c516c
+size 53544
