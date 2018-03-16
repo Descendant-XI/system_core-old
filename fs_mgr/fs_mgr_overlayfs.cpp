868932c6c48b9aff08be654aadf9bbbd5b1ca890
Mark Salyzyn <salyzyn@google.com>
2018-03-16 11:00:26 -0700

    fs_mgr: add fs_mgr_access() as no-errno access(,F_OK) wrapper
    Simplification of code paths.
    
    Test: compile
    Bug: 74595769
    Bug: 63736262
    Change-Id: Ie03f3895bc4b520fc23854ab662d99497c31bc66

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30d997fefbd2aeaa76803a63f99b864c39dbc0dad8bde2f46eb9b1cfd587ea18
-size 20175
+oid sha256:3f495bbf314d84fe5647ca779068a64177d55b11dd64fc257361e344445af806
+size 20057
