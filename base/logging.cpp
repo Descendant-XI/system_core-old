a51ba3c2b0260c5d711bb6ca0e126dfaf9368f0c
Josh Gao <jmgao@google.com>
2018-03-16 14:25:42 -0700

    base: export GetThreadId.
    and stop using the incorrect (on Mac) version in adb..
    
    Test: mma
    Change-Id: If28f6c8d73b03db863be13ed88df81422c3aef87

--- base/logging.cpp
+++ base/logging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a81d7538a2f43ecbcf4a5698ed5e0a4e30feb2386892b6f738f427b594bc10e5
-size 14140
+oid sha256:ba6b5531d7ee89914f47b450db211024e70b209ccb2f32a19828833dec7d61af
+size 13477
---
+++ base/threads.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0eb289a1fd5317587beb8a94b0c501d7c01c01c493c4a44ee28afaf33cda0772
+size 1223
