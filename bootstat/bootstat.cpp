9b66b588c8072dd94b01eecfd098d3853a4e48c7
Mark Salyzyn <salyzyn@google.com>
2018-03-16 09:05:59 -0700

    bootstat: add support for regex in aliasReasons and powerkeys
    Add support for regex in aliasReasons for the alias member.  Use this
    new feature to check powerkey|power_key|PowerKey for a single entry.
    
    Test: boot_reason_test.sh
    Bug: 63736262
    Change-Id: Ia6add99b9e33f3197643dbaab88dde20aa726f90

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5bf9efd2738d1ddf59266115fc568da88dca5380f3de33f5574281fd78651a0
-size 42692
+oid sha256:75e39a425a3fb05fdd833b3175f54de07314912581532ba5f7ebc26db343a3bd
+size 42863
