6a65d1a3884e8a0b69dc7a1cf8ba9c174bfa00aa
Josh Gao <jmgao@google.com>
2018-03-19 13:20:29 -0700

    adb: don't immediately close a socket when write fails.
    When we fail to write to a local socket peer, we might still have data
    queued up to send to the other side. Defer closing the socket until
    we've failed to both read and write.
    
    Bug: http://b/74616284
    Test: python test_device.py
    Change-Id: Ifc4b8fe95369b4872e475c2ae4ee611dd2d8b9d7
    (cherry picked from commit 184f4805476af19c2e58dd96c2ce6aa241efaacf)

--- adb/socket_test.cpp
+++ adb/socket_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d61c7301660a288dd0abc1bf118b1df2926b0e7818c0a329c2add9093dbf6246
-size 12047
+oid sha256:e18461a857c80ad5a47c39c6fc5a5216888615bebcc38789918b77a75d758c90
+size 12034
