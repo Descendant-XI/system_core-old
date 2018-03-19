135252ff9ca2c2fe8861e1abe75e0c263bc4b2b3
Josh Gao <jmgao@google.com>
2018-03-19 15:19:45 -0700

    adb: make fdevent_run_on_main_thread's fd nonblocking.
    If we get a ton of fdevent_run_on_main_thread calls while running one
    of the handlers, the socket might become full, which will result in a
    deadlock in fdevent_run_on_main_thread when a write to the fd blocks
    with the mutex taken. Resolve this by making the fd nonblocking, which
    is safe because we always write after appending to the list, and read
    before emptying the list, which guarantees that if the byte we write is
    consumed, the std::function we appended will be run.
    
    Bug: http://b/74616284
    Test: adb_test
    Test: python test_device.py
    Change-Id: I29319bda2ad7b5a5cdcd91d1d0ddf39f7ab7d115

--- adb/fdevent.cpp
+++ adb/fdevent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e81cbf2f657eb13273d46e57b7c780f53ca039582d3e36918a91df9cc0f519
-size 13547
+oid sha256:280cf829880c65a9fb6f0673af303371698edc27e98b2b110f9d5148b7144ce3
+size 13958
