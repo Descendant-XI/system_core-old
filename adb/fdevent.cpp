949518357f041d5b2f9dcbe8d11767c940270787
Josh Gao <jmgao@google.com>
2018-03-20 13:10:26 -0700

    adb: delete vestigial SHELL_EXIT_NOTIFY_FD.
    There exists no path through which a value other than -1 can be written
    to the SHELL_EXIT_NOTIFY_FD.
    
    Bug: http://b/74616284
    Test: adb_test
    Test: adbd_test
    Test: python test_device.py
    Change-Id: I0630c302ba06bc76917f0445aea75d2dbe1dc865
    (cherry picked from commit dcc055af644ce87f1590ab82680f3e0ca4cee54c)

--- adb/fdevent.cpp
+++ adb/fdevent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be695e95026687e178a63a75efc7c5cd68b92f0418d660cb45792775cbc1c666
-size 13883
+oid sha256:238a9f5a173a7151f9b9eb1e8c0be9df5cb2976bdedfecd66ba423848f59aa72
+size 11499
