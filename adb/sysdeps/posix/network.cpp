50cfe3fd9be2e6e86b49b66ecb0c61849195af02
Josh Gao <jmgao@google.com>
2018-03-20 14:25:03 -0700

    adb: bump the local socket backlog to the maximum.
    The listen backlog seems to be more meaningful on Darwin than on Linux,
    resulting in connections failing with ECONNRESET. Bump it up to the
    maximum supported value to make this less likely. 128 pending
    connections ought to be enough for anybody.
    
    Bug: http://b/74616284
    Test: python test_device.py
    Change-Id: I5fe0205924188cf18ca1fc1204f923ab5523eeb2

--- adb/sysdeps/posix/network.cpp
+++ adb/sysdeps/posix/network.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:452b4bf6cf70ad22c71bd661329e08cf099bbd30635381fc80b70582aa62194e
-size 3866
+oid sha256:64bd984e29683a5b2edb403b2c264d99720fa5f9cef63068a1e7b4de278eea02
+size 3812
