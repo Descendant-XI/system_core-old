10f79569d8ef0e850b4361e52ffdaccb6b1bf694
Jerry Zhang <zhangjerry@google.com>
2018-03-19 17:54:39 -0700

    adb: Retry io in case of interrupt
    io_submit sleeps waiting for the gadget
    to be enabled. Currently if that sleep
    is interrupted it will shut down adb,
    causing it to have to start back up again.
    
    Rather than return EINTR if interrupted,
    io_submit actually completes and the EINTR
    is found later when looking through events.
    Since an io that is interrupted will be
    small anyway, add a loop to retry small ios.
    
    Also upgrade aio logs in accordance with
    their importance.
    
    Fixes: 75981904
    Test: adb works, logs show successful interrupt handling
    Change-Id: I35973fce130ee849ce59fef80d15b65afb816ba4
    (cherry picked from commit 6e9a327504e6aa2488b736fd5592525278a0916f)

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:531022fa49dee432e5fb625f05291b70d3f2073e9aa19a231a4c2f2d4ebd3417
-size 17138
+oid sha256:dcd1a674d27faeea27a555acff20e1c598f457694cc29ee7025573cd3af2d27e
+size 17425
