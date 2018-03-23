b093d7c192ad58cc2154c4e5fd5475969a8c9274
Christopher Ferris <cferris@google.com>
2018-03-23 12:51:43 -0700

    Method to avoid skipping frames for local unwinds.
    In cases where there might be a crash in the unwind library itself,
    we need a method to avoid skipping these frames or we won't be able
    to see the actual crash.
    
    Added unit test for this behavior.
    
    Bug: 74121887
    
    Test: Ran unit tests on host and target.
    Change-Id: I45825020c174016af39dd8ffdc67acb72a24ad4d

--- libbacktrace/UnwindStack.cpp
+++ libbacktrace/UnwindStack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a33a05a3d3ca80544bd8a9def5ae344eecddef1cd7baedf4b4c9f9743b6bff5
-size 8662
+oid sha256:60d4ced6e5d1194f9073c12cca984b4d3293bfdd4c965c8561edb49bfd8db841
+size 8713
