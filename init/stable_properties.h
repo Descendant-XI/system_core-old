6254e2f3c4c75533866421f1161165b710ad0783
Jaekyun Seok <jaekyun@google.com>
2018-03-28 12:21:03 +0900

    Allow vendor-init-actionable for ro.debuggable
    Some partners are using ro.debuggable as action trigger to config a
    product differently according to its value.
    
    Bug: 75987246
    Test: succeeded building and tested taimen
    Change-Id: I4cc57e7b52e17fc89e585afa0a8a10925e47fac8
    Merged-In: I4cc57e7b52e17fc89e585afa0a8a10925e47fac8
    (cherry picked from commit 4f214c5179b2bef7de84b8b5eb2c18fd3f7312a8)

--- init/stable_properties.h
+++ init/stable_properties.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b1b8d74eb52f934b9aee258eeeff444f150a431ad13555118838a8d6322bf79
-size 1602
+oid sha256:c1d8567ab365258d9db165e2d9b30a6f0a1d47936575a5ca117b459ddad4897b
+size 1623
