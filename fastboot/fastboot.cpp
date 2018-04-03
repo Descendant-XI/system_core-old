aa0ef528d9fe7228141d9a3185e6ceb6248c943d
Elliott Hughes <enh@google.com>
2018-04-03 13:30:18 -0700

    Clean up fastboot help:
    After:
    
      usage: fastboot [OPTION...] COMMAND...
    
      flashing:
       update ZIP                 Flash all partitions from an update.zip package.
       flashall                   Flash all partitions from $ANDROID_PRODUCT_OUT.
                                  On A/B devices, flashed slot is set as active.
                                  Secondary images may be flashed to inactive slot.
       flash PARTITION [FILENAME]
                                  Flash given partition only.
    
      basics:
       devices [-l]               List devices in bootloader (-l: with device paths).
       getvar NAME                Display given bootloader variable.
       reboot [bootloader]        Reboot device.
    
      locking/unlocking:
       flashing lock|unlock       Lock/unlock partitions for flashing
       flashing lock_critical|unlock_critical
                                  Lock/unlock 'critical' bootloader partitions.
       flashing get_unlock_ability
                                  Check whether unlocking is allowed (1) or not(0).
    
      advanced:
       erase PARTITION            Erase a flash partition.
       format[:FS_TYPE[:SIZE]] PARTITION
                                  Format a flash partition.
       set_active SLOT            Set the active slot.
       oem [COMMAND...]           Execute OEM-specific command.
    
      boot image:
       boot KERNEL [RAMDISK [SECOND]]
                                  Download and boot kernel from RAM.
       flash:raw PARTITION KERNEL [RAMDISK [SECOND]]
                                  Create boot image and flash it.
       -c CMDLINE                 Override kernel command line.
       --base ADDRESS             Set kernel base address (default: 0x10000000).
       --kernel-offset            Set kernel offset (default: 0x00008000).
       --ramdisk-offset           Set ramdisk offset (default: 0x01000000).
       --tags-offset              Set tags offset (default: 0x00000100).
       --page-size BYTES          Set flash page size (default: 2048).
       --header-version VERSION   Set boot image header version.
    
      Android Things:
       stage IN_FILE              Sends given file to stage for the next command.
       get_staged OUT_FILE        Writes data staged by the last command to a file.
    
      options:
       -w                         Wipe userdata.
       -u                         Do not erase partition first when formatting.
       -s SERIAL                  Specify a USB device.
       -s tcp|udp:HOST[:PORT]     Specify a network device.
       -i VENDOR_ID               Filter devices by USB vendor id.
       -S SIZE[K|M|G]             Use sparse files above this limit (0 to disable).
       --slot SLOT                Use SLOT; 'all' for both slots, 'other' for
                                  non-current slot (default: current active slot).
       --set-active[=SLOT]        Sets the active slot before rebooting.
       --skip-secondary           Don't flash secondary slots in flashall/update.
       --skip-reboot              Don't reboot device after flashing.
       --disable-verity           Sets disable-verity when flashing vbmeta.
       --disable-verification     Sets disable-verification when flashing vbmeta.
       --wipe-and-use-fbe         Enable file-based encryption, wiping userdata.
       --unbuffered               Don't buffer input or output.
       --verbose, -v              Verbose output.
       --version                  Display version.
       --help, -h                 Show this message.
    
    Bug: N/A
    Test: `fastboot --help`
    
    Change-Id: I3639fdee68de544f12e75004ccbb03b8386e64ed

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d389c6a36099ae0f1e269046f4b375d5c1ce6561d58b4b1f79915b5b0bc6b61f
-size 70636
+oid sha256:18fc9029d1c35ab218ae9e908823e7b44056ec2d12db6325ad44a8b1ca9d3649
+size 65795
