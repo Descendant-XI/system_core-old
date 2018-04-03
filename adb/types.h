880dfcc741a37bda87675867d0ec5144cdf82865
Josh Gao <jmgao@google.com>
2018-04-03 14:37:11 -0700

    adb: add IOVector.
    An IOVector is a collection of immutable reference counted blocks which
    can have its head detached at an arbitrary index. This is extremely
    useful for implementing packet-framed protocols like adb on top of a
    stream protocol like TCP: a stream reader can read blocks, append them
    to the end of the IOVector, and then pull packets off of the front.
    This also lends itself naturally towards scatter/gather I/O, which will
    enable us to read data from disk and send it across the wire with a
    theoretical minimum number of copies in USB, and one extra copy over
    TCP.
    
    Since this is basically a generalization of std::deque<Range>, delete
    Range and replace its uses with IOVector.
    
    Test: adb_test
    Test: wine adb_test.exe
    Change-Id: I06561ad0bb25a3a51b378b61d257b5b04b41d9c4

--- adb/types.h
+++ adb/types.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a839a13e277b5092542ded4fdc419e04c91be4ad47d8b90db87197602b0b5fa0
-size 4460
+oid sha256:60926fdbfa5e4a230d96dff105c92b58b9391982c8827cc47ac2fa74569077c8
+size 9827
