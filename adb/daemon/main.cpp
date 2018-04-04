e6bc0a0bcc02281d04a257054fdb98b85185db4a
Luis Hector Chavez <lhchavez@google.com>
2018-04-04 15:59:59 -0700

    adb: Correctly drop caps when ambient capabilities are used
    This change splits the capability-dropping step of adbd into two. This
    is more robust when ambient capabilities are being used, since minijail
    cannot currently handle that case.
    
    Bug: 77146512
    Test: grep Cap /proc/`pidof adbd`/status
          CapInh: 0000000000000000
          CapPrm: 0000000000000000
          CapEff: 0000000000000000
          CapBnd: 00000000000000c0
          CapAmb: 0000000000000000
    
    Change-Id: I0476a8d80f7a2497600196932542045f3bc87537
    Merged-In: I0476a8d80f7a2497600196932542045f3bc87537
    (cherry picked from commit daacf4f6f3aba6e0584103820100da7a7ea81123)

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:478c3b094f657bb0eadd797ebd663021458a38c9a20785b6bf8d54378b6ef219
-size 7930
+oid sha256:61685ca3595832215f7ffc3f3996f347235994d6ff6d817fe50438d2b90d78ee
+size 9004
