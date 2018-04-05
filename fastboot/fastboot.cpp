f1879bdd54bc73b6cb7c58df5875e6920c817775
Elliott Hughes <enh@google.com>
2018-04-05 16:12:47 -0700

    Add fastboot --os-version and --os-patch-level.
    Also move -c to --cmdline to match the python mkbootimg tool. Remove
    the short options for these rarely-used options.
    
    Remove #if 0 cruft from <bootimg/bootimg.h>.
    
    Bug: http://b/77340848
    Test: manual testing (need to fix http://b/77809061 for unit tests)
    Change-Id: Ieaa795566611bd89ba3fee39a1d9f6ad0713a587

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5490ea29a1a62aacc68acd9e447e568e7a3a4ef66482e56161f28a158174a2
-size 63198
+oid sha256:bde95a451561004ef363ad98bbdde9da0613ba6b024db8911b346e38df9a5c26
+size 64589
