55e75173acb5ed814457b01cf206625d172e5e8a
Josh Gao <jmgao@google.com>
2018-04-05 18:09:39 -0700

    adb: win32: properly set EBADF in some functions.
    Test: treehugger
    Change-Id: If3f29f9ee586e29652e9709b3f594a1376ed4bb3

--- adb/sysdeps_win32.cpp
+++ adb/sysdeps_win32.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d14f29bfe32abb767c07b066876c00e761461c3ce867b3fb5b5c12bf6c5f48c6
-size 100369
+oid sha256:f5118e1fc77850124dc794ddbb52f2ec7c3d0616b492d9a6cb900b7eac78a099
+size 100461
