b0b3c303a4c83d4b11e169f182cde90c9547d1e1
Josh Gao <jmgao@google.com>
2018-04-05 17:55:25 -0700

    adb: implement adb_writev.
    Change-Id: I55258c155d7b07368ebb45577b2e01ca804cf258
    Test: adb_test
    Test: python test_device.py

--- adb/sysdeps_win32.cpp
+++ adb/sysdeps_win32.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c426212e2ded7b5888aae4a0882dcc70a51fcd7e66639924b926d432a51a9d3
-size 98230
+oid sha256:0a2dd721f2029e0fd7ce7525cf76c1ff3f90f02388f25883cd17af06a441eaab
+size 100599
