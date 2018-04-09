f9353b12786e9d891dde7b5407145cb7c0e4b842
Pirama Arumuga Nainar <pirama@google.com>
2018-04-09 10:40:06 -0700

    Use the non-LTO, non-PGO version of libhwbinder
    Bug: http://b/77320844
    
    Android.mk doesn't have support for PGO either.  This causes linker
    error (about missing __llvm_profile symbols) in the PGO-instrumentation
    enabled builds in the release branch.
    
    Test: m ANDROID_PGO_INSTRUMENT=ALL
    Change-Id: I84fda4f1ac4f00ac26869507c6f5640b4ec9f6f3

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0027e151ae96abf5f299a3eaafb2d965fdb145acb7fb14e05a7b753de6b2c5ff
-size 4316
+oid sha256:501493fdf510313c6e22b565139685274bcd8e7f93e5b5c850fd0bcfb0b0928c
+size 4319
