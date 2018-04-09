90bbe297309b9289ff84e4c503bde9278909bfbe
Elliott Hughes <enh@google.com>
2018-04-09 13:58:41 -0700

    Minimal changes to build fastboot as a library.
    Bug: http://b/77809061
    Test: builds
    Change-Id: I0c388334d643a0a2abf0c03a29f336dc3af2f83f

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a190bdf9e606699a1564b7c94f5ea476c1859f376a3b1b634ebc3a7dcd2e0c49
-size 3740
+oid sha256:841c55649faea7a75343a09af36258fd996af8b39234d175df5e53b36cc8c76c
+size 4290
