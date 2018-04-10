0bd21e0ce663b3f84268f5cfa03b0a8755dab835
Elliott Hughes <enh@google.com>
2018-04-10 15:38:08 -0700

    Remove -i vendor id matching.
    We removed this from adb years ago.
    
    Bug: N/A
    Test: N/A
    Change-Id: I899c8433665e64a0c0302d2c30a8e689b8c83d65

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79d23aae8312ac0f3a71bd70fa3597e3fa4c9a434a59623c77d32590fa48aa7d
-size 62865
+oid sha256:bad7691150d0b97a8318530131085a7f0011c83a5d490e7f76247b37c9ed06f3
+size 62112
