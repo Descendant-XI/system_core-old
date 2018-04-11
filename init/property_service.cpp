8cc6f3cf85a0ad3af1dde5f05b4b924dc24e401e
Tom Cherry <tomcherry@google.com>
2018-04-11 15:50:00 -0700

    init: do not impose vendor_init restrictions on old vendor images
    Do not restrict vendor_init restrictions on vendor images that were
    built before P, as they will not have the correct permissions.
    
    Bug: 77732028
    Test: test new devices and see vendor_init still works
    Change-Id: I636a07b54fbfb248e1d1a68a8f3c4d047fd5a9e9

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c29c9280ae32a2836be63f978a6f68f281ca24b39dd635cc82274931690e6605
-size 28313
+oid sha256:cbbe24850ee8f98923e48e897742cc05de6dea14b60bfadd6409f0ca9bab94d9
+size 28431
