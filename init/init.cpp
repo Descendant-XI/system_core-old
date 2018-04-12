205704e1f4d228e6dc6cb8ce3747982021ea21f6
Ryan Prichard <rprichard@google.com>
2018-04-11 18:46:38 -0700

    init: use signalfd to catch SIGCHLD
    Previously, if init received too many SIGCHLD signals, then the write to
    signal_write_fd could fail with EAGAIN. The handler tried to log the
    EAGAIN error, and init deadlocked if the interrupted init process had
    already acquired a logging-related lock.
    
    Bug: b/77867680
    Test: manual
    Change-Id: Ief0b5e94d8517827a5a7d03773391ba3ba9447c4

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc96f60a5454c510ecad457ebf00d2164ad5050814d9c3f888f590db1c47477
-size 27519
+oid sha256:0fd774224ef544b528e4d7ad207bfc9490378da3d93f3084e9ac2609761e01d9
+size 28283
