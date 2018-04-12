e06faa82259108ba352ba3ce11accc01c9099cd7
Luis Hector Chavez <lhchavez@google.com>
2018-04-12 15:25:15 -0700

    Make bootstat container-friendly
    This change allows bootstat to read the ro.boot.boottime_offset
    property, which is set on devices where Android runs in a container.
    This is because the CLOCK_BOOTTIME clock is not reset when (from
    Android's perspective) the device restarts, since the host OS does not
    restart itself.
    
    Bug: 77273909
    Test: CtsBootStatsTestCases
    
    Change-Id: Ifb792864394be0b4686cc9d555c0afced856f4b4

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5c74e378b1a422e4f32284d1130f58d4386d3bbc24c0f4d336cf2a73851e1e6
-size 44575
+oid sha256:af641a9581413885b0d591180301dedfb693a50b0ac506ed671e2f472db0d976
+size 45195
