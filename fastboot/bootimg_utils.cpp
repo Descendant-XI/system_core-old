c05a4c087bf9b07f6a21ef1f0913287db7624aaf
Fernando Lugo <flugo@google.com>
2018-04-11 19:02:59 -0700

    fastboot: fix header_actual calculation
    Bug: 77920157
    Test: fastboot boot $OUT/kernel
    Change-Id: Ibe2f4b9a6476ac11937448e6547b5159af011633
    Signed-off-by: Fernando Lugo <flugo@google.com>

--- fastboot/bootimg_utils.cpp
+++ fastboot/bootimg_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ec738a929ba5006d569b2221bef9eb9d09d79459883956166e9c706b223ef7d
-size 3259
+oid sha256:57e53b19c610b8a9e3579b228f0c5e69c5a62f29f5fc6f38fcd4f62275a463b5
+size 3273
