d317630be4adccc90e014531f425da40f6f3e8d3
Dan Albert <danalbert@google.com>
2018-04-13 14:49:41 -0700

    Add missing @addtogroup tags.
    These NDK docs weren't in any groups, so they don't show up in the
    new site.
    
    Test: https://irina-dot-devsite.googleplex.com/ndk/reference/group/logging
    Test: https://irina-dot-devsite.googleplex.com/ndk/reference/group/sync
    Bug: http://b/77236573
    Change-Id: I300f96585dec94359ecf96fbf39c6dc3ec4579e5

--- libsync/include/ndk/sync.h
+++ libsync/include/ndk/sync.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ead1df68d6e250ef34472d52b682fe79624191fac7cd5d04894161b06c7bc5e5
-size 3132
+oid sha256:b34dbe128122ab5e6e84afd1c796e17e10adb1a240e19d1a19646b8c82772c9f
+size 3203
