17cfba36ced30a7dced527ed8db14a8d528be8e5
Suren Baghdasaryan <surenb@google.com>
2018-04-13 11:49:54 -0700

    lmkd: Fix usage of ro.lmk.kill_heaviest_task property
    lmkd should use ro.lmk.kill_heaviest_task property to select between
    algorithms for victim selection. Set ro.lmk.kill_heaviest_task default
    value to false in order to keep it compatible with previous versions
    of lmkd (killing the heaviest task is a new mechanism).
    
    Bug: 77299493
    Bug: 75322373
    Change-Id: I78d2dc79d9c54e636c26665605518d9c87b535b3
    Merged-In: I78d2dc79d9c54e636c26665605518d9c87b535b3
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    (cherry picked from commit 818b59b2292f87c3781a6f7a288d10e2453b0d13)

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f674469fd0129fb8cea0b7196114cc1ba220c3e66bda706a0bd5727d5712055
-size 34864
+oid sha256:e8436dfa3d88cd281da6d70e9f416da318473b4c8b20a56a94c25874f8716be3
+size 34826
