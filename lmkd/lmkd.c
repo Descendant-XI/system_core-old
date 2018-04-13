56053905b70bc4f85beb3d457a664c434caed1a2
Suren Baghdasaryan <surenb@google.com>
2018-04-13 13:53:43 -0700

    lmkd: Introduce support for legacy kill algorithm that uses minfree levels
    Add ability to switch to the algorithm used by lowmemorykiller driver
    for determining when to kill. It uses minfree levels to decide at which
    levels of free memory and file cache to kill a process. oom_adj_score
    is also determined by comparing current memory resources against minfree
    levels.
    ro.lmk.use_minfree_levels property is introduces for switching into this
    mode. By default it is disabled.
    
    Bug: 77299493
    Bug: 75322373
    Change-Id: I6b51972951026854a079fcda33d6786b7ed035e4
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66526468ce40449b44b8fcb3c28d19a9beb37bb2f92e74f7f4d21e1c603b00f3
-size 43124
+oid sha256:19e9ef25a0bf626a39c4cd56837260231240f1d45902e6b2d5d7f11c3e7799db
+size 45290
