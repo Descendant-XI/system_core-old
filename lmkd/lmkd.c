1a8c148b36abcfed84b7d1c49306b8cf2ecc207f
Suren Baghdasaryan <surenb@google.com>
2018-04-13 12:43:41 -0700

    lmkd: Optimize frequent file reads by keeping file descriptors open
    To check system memory state lmkd is using same files every time vmpressure
    event is received. Instead of opening and closing these files every time
    we store the file descriptor and use pread() to reread the file from
    the beginning.
    
    
    Bug: 77299493
    Bug: 75322373
    Merged-In: I8e27f8b9526e82e3cc313a02fce215c2e4dd3d29
    Change-Id: I8e27f8b9526e82e3cc313a02fce215c2e4dd3d29
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48a90854ad5be4c92d8689b4611a1791240d44fc43b97e5d76e82f305241a727
-size 35260
+oid sha256:1343357c31fd64da8c2eec57fef0cbb74667ee433c37e557ceb42611b95d87b3
+size 36943
