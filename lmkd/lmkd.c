91f6a37772eaea93fe63f9012f4c6ec822c3053c
Suren Baghdasaryan <surenb@google.com>
2018-04-13 13:11:51 -0700

    lmkd: Add zoneinfo and meminfo parsing routines
    /proc/zoneinfo and /proc/meminfo contain information necessary for lmkd
    to assess system memory state. Add routines to parse these files.
    
    
    Bug: 77299493
    Bug: 75322373
    Merged-In: Ie7d80bbb81fd0d2fc0629f6f678e6afc97fed1f6
    Change-Id: Ie7d80bbb81fd0d2fc0629f6f678e6afc97fed1f6
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1343357c31fd64da8c2eec57fef0cbb74667ee433c37e557ceb42611b95d87b3
-size 36943
+oid sha256:7342ac51a601d516a49ea339405407623eb247de6f3acb2c3395d68319fae339
+size 40628
