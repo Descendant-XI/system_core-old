afa4adce9b2962b92c8172fadba48bd42f854d34
Paul Crowley <paulcrowley@google.com>
2018-04-17 12:28:12 -0700

    Set property for metadata encryption on first boot
    Bug: 77335096
    Test: device boots twice with and without metadata encryption
    Change-Id: Iaed78288cb37865ba23833721b73b11414e7e862

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e80046c8d654b501c1c0e8615f4f96266ad06c191e4baf6e02a4af8fd553451c
-size 38256
+oid sha256:9f6e76f1df9e437e406e4bc17b34d71b7aaf518971ce28a2cc623760150e1ed3
+size 38310
