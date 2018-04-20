44750b83f4326ea888a090addae584112d678081
Josh Gao <jmgao@google.com>
2018-04-20 11:51:14 -0700

    libunwindstack: add Regs::Clone.
    Add a way to copy a Regs object.
    
    Bug: http://b/77296294
    Test: libunwindstack_test
    Change-Id: I28aff510aa3e1b677d5ae46dc3bfe7652817ce52
    (cherry picked from commit 2f37a15472945194fed528cb8d3104aa4865fc4c)

--- libunwindstack/include/unwindstack/Regs.h
+++ libunwindstack/include/unwindstack/Regs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aba159dc04bc7ee0d9fa07941eaddd97015c5b9dd992eca0906a14956127e16
-size 2968
+oid sha256:a3a260699d9bae625fb725f28f3db0f7243f3e244c5d72ca3b3365690bf05052
+size 2998
