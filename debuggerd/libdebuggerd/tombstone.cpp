b0ec80e919bf693154774ee309ab9dbd0ee9e265
Josh Gao <jmgao@google.com>
2018-04-20 12:04:41 -0700

    libdebuggerd: clone registers before we Unwind with them.
    Bug: http://b/77296294
    Test: manual inspection of tombstone generated by crasher
    Change-Id: I4b017701d7d2041db4aefbbb90977e99b844c328
    (cherry picked from commit 5d1c14f41bac357c730aa47d705c1f1da7625280)

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8107c1573b81b83f1a98c2f8a03fcb8aa6789ea9e382fc6560fc739f3b2a55c2
-size 21695
+oid sha256:d5b55f202903566b80b53c81c6276285d9abfdcb328a3e426627abaf01f89786
+size 21811
