e13bce60de66b8c4dbffceb73b35e01496f4088a
Tao Bao <tbao@google.com>
2018-04-24 10:54:21 -0700

    fastboot: sparse_file_len() returns int64_t.
    Check that the value fits in uint32_t that's supported by the current
    protocol.
    
    Also fix and sanity check the max_size before passing it to
    sparse_file_resparse(), which accepts `unsigned int`. This shouldn't
    happen in practice because of RESPARSE_LIMIT (1 GiB).
    
    Test: `fastboot flash` with small and large images.
    Change-Id: I0a8279fc14c54c40a70ddce65c3b25173c0d0a40

--- fastboot/protocol.cpp
+++ fastboot/protocol.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:319612ae21323f36fd746c0e0f5289d6c4057b589bfb77bd7c63f112bf1c7f97
-size 11087
+oid sha256:c7e763aa38b28e70bb9ca537c23e93231c7b0e5df766e8e141b92b8de370a529
+size 11144
