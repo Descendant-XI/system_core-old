bb5c2a9d7dc2768fe99fe5411c1dde4fc4aabe8a
Tom Cherry <tomcherry@google.com>
2018-04-24 14:13:30 -0700

    logd: notify kernel log readers of new messages
    LogBuffer::log() returns either a negative number on error or a
    positive number indicating the length of the message written.
    Therefore, the check to notify kernel log readers of a new message
    should be that this function's return value is > 0.
    
    Bug: 78209416
    Test: `adb logcat -b kernel` updates when new log messages are present
    Change-Id: Icc18c0c22e62340994e5c26aedb72282d61c1541

--- logd/LogKlog.cpp
+++ logd/LogKlog.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e61097786de31310687a94619ffff469dd2ee6ba870ee4b1898c7faa036fe5ee
-size 27098
+oid sha256:aa58c7da2b1666bf1c1b388b68f957a9817db0123b6d99e0f65e942300d16722
+size 27101
