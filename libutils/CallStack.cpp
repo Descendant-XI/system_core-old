744a3b80047ce6771595fa8486d99799185cb674
Hans Boehm <hboehm@google.com>
2018-04-27 16:35:18 -0700

    Prepare to fail in RefBase destructor if count is untouched
    Move towards crashing if a normally configured RefBase object is
    destroyed without ever incrementing the reference count. We've been
    threatening to do this for a long time. The previously last known
    violation had been fixed.
    
    This also fixes stack trace printing from RefBase, which had previously
    been broken, and which we found necessary to track down further
    violations of this rule.
    
    Unfortunately, we found several more violations with the aid of
    that fix. After existing CLs are submitted, there are
    still some failures, but they are no longer numerous. Thus this CL
    doesn't actually crash in the event of a violation, but does log a
    verbose stack trace if it encounters one.
    
    Bugs have been filed against the remaining known RefBase client offenders.
    We plan to enable crashing on usage violations once those are fixed.
    
    The fix for the stack trace printing breakage unfortunately requires
    the use of weak symbols in order to avoid a circular build dependency.
    We expect to eventually replace this with execinfo.h functionality.
    
    Some random reformatting, driven by consistency with current formatting
    requirements.
    
    Add missing include to BacktraceMap.h.
    
    Bug: 79112958
    Bug: 30292291
    Test: Boot AOSP, Master
    Change-Id: I8151c54560c3b6f75ffc4c48229f0388a2066958

--- libutils/CallStack.cpp
+++ libutils/CallStack.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f4df8253dcc1680e5726ea215680a8db0b5dc5e7217e324f6cbae645a443852
-size 2111
+oid sha256:c53518d9bf46fc4ee0184e37b62796d556c4ca4ddaa442ddd5ba2a2f5a5c6eb0
+size 2882
