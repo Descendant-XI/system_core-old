0ff46fe20c81f0d2d5342fe487d394cc51d06523
Bowgo Tsai <bowgotsai@google.com>
2018-04-30 16:13:42 +0800

    First-stage mount: avoid triggering a FATAL error
    Related AOSP changes:
      1. https://android-review.googlesource.com/#/c/platform/system/core/+/405009/
      2. https://android-review.googlesource.com/#/c/platform/system/core/+/532637/
    
    The second CL raises a FATAL error when it detects fstab-dt has no content
    during first-stage mount. However, with the first CL, the fstab-dt entry
    might be "skipped" when bootloader sets the status property to a value
    other than "ok"/"okay". (e.g., to skip mounting /vendor on upgrading
    devices which have no vendor partition).
    
    Use LOG(INFO) when there is nothing to mount here. The later stages
    should trigger a FATAL error when some important files in those
    partitions are not available, e.g., SEPolicy files.
    
    Bug: 78441220
    Test: boot a device
    Change-Id: Iae2f47d455679298bdb067d96b771a30c1a82e6f
    Merged-In: Iae2f47d455679298bdb067d96b771a30c1a82e6f
    (cherry picked from commit 8fe363f26017e39b80c6cd793f7b2a346da88104)

--- init/init_first_stage.cpp
+++ init/init_first_stage.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d80ac6896b0e898dc1bd01d4a29595af5b238655b272b811eaa7b9caafbcf291
-size 19679
+oid sha256:1c1c9a6020fa8d90c8fac1dbe86716c4dea65407c3fb9ae43ecc1ea80aae00f2
+size 19429
