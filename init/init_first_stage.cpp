42d00dca18b1028547d78c97bb4ca05d4315b5ca
David Anderson <dvander@google.com>
2018-05-01 13:57:14 -0700

    init: refactor first stage to not require fstab
    In order to support dm-linear devices, we need an additional first-stage
    step to ensure that required devices are created. This must happen before
    setting up dm-verity or mounting any first-stage partitions.
    
    This patch refactors FirstStageMount so that having a compatible fstab
    is optional. This will let us use InitRequiredDevices on systems that
    would not otherwise perform first-stage mounts.
    
    Bug: 78914864
    Test: non-AVB devices still boot
    Change-Id: I11265375a9900d983da8cabcc77d32c503ded02e

--- init/init_first_stage.cpp
+++ init/init_first_stage.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c1c9a6020fa8d90c8fac1dbe86716c4dea65407c3fb9ae43ecc1ea80aae00f2
-size 19429
+oid sha256:14ae9ff27cb36e6a0317bb9ab025fece682d788b7d0993318dd425466b29c092
+size 19298
