5a0eb71b88f2f41724953c6ffad77eadb06b724a
Luis Hector Chavez <lhchavez@google.com>
2018-05-02 10:47:01 -0700

    adb: Add a test for emulator connection
    This should prevent regressions in the future.
    
    Bug: 78991667
    Test: python system/core/adb/test_adb.py
    Change-Id: I4d6da40da82c6d79797cec82ffaf071d4b56ddc7

--- adb/test_adb.py
+++ adb/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2de004793fff3951bbc48a4dddea8e3598a6713603c44a8c7973e39f72c93a1
-size 15875
+oid sha256:cecf8af58fe9bea0aa8b7c7f31edf5cb6e2b8175cdc8ac6658c713f2b95292ec
+size 18319
