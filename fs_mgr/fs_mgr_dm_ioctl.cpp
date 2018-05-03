53d4de241d0335225e3a335bfd6a866b19e138ba
David Anderson <dvander@google.com>
2018-05-02 18:18:12 -0700

    fs_mgr: clean up dm ioctl flags
    DM_READONLY_FLAG should only be used when loading tables, and
    DM_STATUS_TABLE_FLAG should only be used when querying a table's status. This
    patch cleans up how we set flags to reflect when the kernel actually cares
    about them.
    
    Bug: 78914864
    Test: AVB device still boots
    
    Change-Id: I809d8c2ef2105849ebdd095bbe7f08f15ae63465

--- fs_mgr/fs_mgr_dm_ioctl.cpp
+++ fs_mgr/fs_mgr_dm_ioctl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24d37c35df9914108092e368741ad96e16c447407f430a9eb343e0ef0e85acea
-size 2522
+oid sha256:74b15b0406cbcf1cbd2d09c96bba70355a6378b30badab88551e08ba4374347d
+size 2452
