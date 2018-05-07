5809d847e3cc063cc6695da2040ecfa1da296425
Yao Chen <yaochen@google.com>
2018-05-07 16:57:13 -0700

    Move libstatssocket from frameworks/base to system/core/
    So that lmkd can build on PDK.
    
    Bug: 79349329
    Test: builds locally
    Merged-In: I981e6ef9f9769b873640e5f169a9495ccea2f25c
    Change-Id: I981e6ef9f9769b873640e5f169a9495ccea2f25c
    (cherry picked from commit b13a102c0afec2c0a1fbdafb9a00e3d55450b28b)

---
+++ libstats/statsd_writer.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7df55a10d0b18dafe5f2650c44fea7a179dfb5a199fcb29268e62f532b9401f4
+size 7530
---
+++ libstats/statsd_writer.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:711a1720d034af86cf884be3c031d37fba19f15b316e4ec0aaafd19cf165663b
+size 1557
