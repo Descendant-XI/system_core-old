cbb881793b0df7a69408bcc8e01d7c73bd2cca06
Logan Chien <loganchien@google.com>
2018-05-08 20:12:19 +0800

    Always use versioned VNDK directory
    Bug: 78605339
    Test: sailfish-userdebug builds and boots
    Test: sailfish boots with GSI
    Change-Id: I7da5845b5c1caed3b73d7665f1db672f2764c2a8
    (cherry picked from commit 6a145749be79978c533b82352cef96f3f7c02524)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bcffb45a8a61a5bcd8df2c091ebe83e995fcda02d1e8cef5497452f4c64dd97
-size 12149
+oid sha256:475e08e3749b370257e36a6e09f2f96965f36b1c6933d93896fbd88ce07ef628
+size 12089
