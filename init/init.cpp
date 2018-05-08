51564eec17e9e32e4acd1a26e63820dd2b37b8ad
Steven Moreland <smoreland@google.com>
2018-05-08 13:46:39 -0700

    init: ServiceList FindInterface
    FindService can't be used w/ interfaces due
    to the fact that multiple interfaces can be
    added to any given interface.
    
    Bug: 79418581
    Test: boot device, manually use ctl commands
    Change-Id: I7c152630462c9b7509473bc190f5b30460fcc2bc

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd1b70e889da351cb38ec390de4894fc4318c1cffea7db8b77d9e132b4e75e83
-size 29061
+oid sha256:8dab474691b0497d07f848f480282277c46b0d4fd3abacc65f3be21a58dc785b
+size 28619
