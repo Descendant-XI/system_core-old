d512c49d07eeb1e2aa29e6e2859a7d53ff4aab2b
Suren Baghdasaryan <surenb@google.com>
2018-05-10 16:10:56 -0700

    lmkd: Enable more logs during a kill
    Logs that provide information about memory conditions during a process
    kill event contain useful information and do not affect device
    performance because these events are rare. Enabling them even when
    ro.config.debug flag is not set will help in understanding low memory
    conditions.
    
    Bug: 79572814
    
    Change-Id: Iae6e9bb612b9a7904ca491de3f1ddc727f24c7e0
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cc708d468b93ab6c71e12f78f637d6c3529a1f612ae73714a0a35d7991cbb61
-size 44055
+oid sha256:3c11ca38f8f6cced04e1e94c116e66b4cd201bd4d87a9b7fc36de4c54c03a217
+size 43967
