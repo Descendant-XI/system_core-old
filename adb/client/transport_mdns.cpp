67841d962756539d4068e5e6c4a450e345a0df9e
Josh Gao <jmgao@google.com>
2018-05-14 11:16:02 -0700

    adb: fix uninitialized variable in AsyncServiceRef.
    Bug: none
    Test: treehugger
    Change-Id: I8ba0a70a772f88bfd701730a48d4eb32c6677b9e

--- adb/client/transport_mdns.cpp
+++ adb/client/transport_mdns.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b908623b17245ffa10585f4f3720822bae77a3a5a8ce4dca9adca640dcc4e972
-size 10251
+oid sha256:8ca0c2888bb6e58c81e4609f52b167fb1518772a3105bc51e0f86c868daf289f
+size 10259
