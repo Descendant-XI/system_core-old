381404593bee2275b01ce450a760c1680990b05d
Colin Cross <ccross@android.com>
2018-05-14 14:55:39 -0700

    Make memunreachable_test more robust against false negative leaks
    For some reason, the memunreachable tests are rock solid on the
    devices covered by APCT, but catch a ton of false-negatives on
    hikey960, which show up as failures that look like:
    system/core/libmemunreachable/tests/MemUnreachable_test.cpp:200: Failure
    Expected equality of these values:
      1U
        Which is: 1
      info.leaks.size()
        Which is: 0
    
    These happen when a stray copy of a pointer is lying around that
    points to the memory it is expected to leak.  The stray pointers
    can be on the stack or in the jemalloc thread cache of freed
    allocations, which is always considered active memory.
    
    Add some extra cleanups to get rid of old pointers.
    1.  Clear the tcache when destructing UnreachableMemoryInfo
    2.  Clear the stack and tcache before and after each test
    3.  Make MemunreachbleTest.twice match MemunreachableTest.stack
    
    Also fix MemunreachableTest.notdumpable, which was only passing
    when run as root, which was bypassing what the test was trying
    to cover.  Make the test pass when run as non-root, and skip
    when the test is running as root.
    
    Bug: 79701104
    Test: memunreachable_test
    Test: memunreachable_test as root
    Change-Id: Ia6c6df11e76405d08118afcc19c1fe80a6684c56
    Merged-In: Ia6c6df11e76405d08118afcc19c1fe80a6684c56
    (cherry picked from commit ca71f170b78e4707dbaa5bf4ddace47a455b4699)

--- libmemunreachable/MemUnreachable.cpp
+++ libmemunreachable/MemUnreachable.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6669052610ba0d8e370a8b4e45adf7d7d6098e731935492dc42cbe069f47fc4e
-size 17408
+oid sha256:86ce6b37faf62e6fb3cc41c986c869c2bf3078288f2a4220ca61260c2cd41a9d
+size 17947
