d1fcfef80ef876872ea6262f47769c76540dc06c
Josh Gao <jmgao@google.com>
2018-05-14 17:12:50 -0700

    adb: fix double close in jdwp_service.
    Bug: http://b/78793027
    Test: treehugger
    Change-Id: Ie66d12e70bdaf09148e3d672e3c7c3da9e8ab2fe

--- adb/daemon/jdwp_service.cpp
+++ adb/daemon/jdwp_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12257e7f64474796ac75d1540577b2bcdfbb1f674c082a63c0c1fb62134b405
-size 18148
+oid sha256:e5a6d448d2ca5341b6e89951ff011a6d947c2ba7d7930ab58b0d8764c962566a
+size 18120
