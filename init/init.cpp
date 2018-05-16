698e5cd320136cb0d8d43fc172508372a69bfc88
Mark Salyzyn <salyzyn@google.com>
2018-05-16 15:10:24 -0700

    init: refactor keychord for testing
    Move things around so that keychords.cpp is independent of service
    and init and can be individually tested with few dependencies.
    Permits also rolling out the keychords as a class in a future commit.
    Improve parser checking.
    
    Test: init_tests
    Bug: 64114943
    Change-Id: I82570bc6269ed478db784ec38a8bc916da2be2be

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78792eb8a1ec21632420a93551515fc929f9a8fdb0c1af0b4241a13d0218c6f3
-size 28287
+oid sha256:fc3c15adef455ff5cd6db71ee78d6133353a3daaa8dd5de34b6c1cdc0d03c017
+size 29288
