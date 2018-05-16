7181a37b7e88ec92f6b5133676e8ae19efd55b5c
Jerry Zhang <zhangjerry@google.com>
2018-05-15 17:02:50 -0700

    fastboot: Refactor string constants to constants.h
    This allows them to be shared between host and device
    implementations.
    
    Test: fastboot works
    Bug: 78793464
    Change-Id: I79271687850ade2a904fd03d6a97dad1a9ab8e1c

--- fastboot/engine.cpp
+++ fastboot/engine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:138cd9762b50bacc3a3bd2289ade806325081c0bf69a839ddfa60ccea4bd742a
-size 11242
+oid sha256:907d975e5502d3b2eafb72de664aaa6afed9646fcecb41466133a3f666d17ae7
+size 11342
