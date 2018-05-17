3d370f68fdedd34f41f183b3d2cb6ee317752a03
David Anderson <dvander@google.com>
2018-05-17 12:12:54 -0700

    adb: add remount -R for deduplicated ext4
    When using "adb remount" on a deduplicated filesystem, the current
    response is a warning that the remount will not work. This patch
    allows the user to specify an -R option. This option will reboot to recovery,
    run e2fsck to undo deduplication, and then reboot the device where "adb
    remount" will then succeed.
    
    In addition, if verity needs to be disabled to remount, it will be disabled in
    the same reboot cycle to minimize reboots.
    
    Bug: 64109868
    Test: adb remount -R on a deduplicated filesystem
    Change-Id: I812407499b2df6f4d2509e8d51878117108a6849

--- adb/daemon/remount_service.cpp
+++ adb/daemon/remount_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9e47fd338e8526995607f2be8b269f2d3626571dcf36d6721b20a30b13aee49
-size 8213
+oid sha256:d2f3c2c7e71d6236e970811633d9dde41fc5742dacf1bc64dd65c8cfb958abb7
+size 10400
