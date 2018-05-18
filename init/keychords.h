3d882a9f2fa1a606cb999181eb9728fc97b4d292
Mark Salyzyn <salyzyn@google.com>
2018-05-18 15:25:15 -0700

    init: switch out keychord id with std::vector match of chords
    Drop all references to keychord_id and id and instead use keycodes_
    as the id.  The keycodes are a std::vector<int> with an unique
    sorted-order emplacement method added in the parser.  Solves the
    academic issue with duplicate keychords and trigger all services
    that match rather than first match only.
    
    Test: init_tests
    Bug: 64114943
    Change-Id: I5582779d81458fda393004c551c0d3c03d9471e0

--- init/keychords.h
+++ init/keychords.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb982f08ff5a04a24aee8fc4b6a144dcb550778000255c68097a08987fca95ef
-size 2502
+oid sha256:69207bad127ba55a667614aec37e245ad6a73c96d1b75b306d642bcff6a0ba78
+size 2448
--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66aba0e5c8260bf9decae1fee52c2d6a5839c6136075b73a9a64dd81a9b192c0
-size 43000
+oid sha256:a30be3d17ad96ffc909178e1258f9467060f8ebfd863fbe53df2c620ff985116
+size 43031
