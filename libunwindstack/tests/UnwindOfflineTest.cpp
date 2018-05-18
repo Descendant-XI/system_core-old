1cf6be02b451d66ae8799a9017a276ad0efa3a3c
Christopher Ferris <cferris@google.com>
2018-05-17 18:37:38 -0700

    Fix bug when doing signal handler lookup.
    The new lld linker uses all non-zero offset executable maps. There was
    a bug when trying to find if the stack is in a signal handler that caused
    the code to read the wrong place in the elf. Fixed by not adding the elf
    offset to the relative pc.
    
    Also fixed the unwind_for_offline tool to dump multiple stacks if necessary.
    
    Added new offline unit test that would have failed with the old code.
    
    Bug: 79936827
    
    Test: Ran unit tests and libbacktrace unit tests.
    Test: Dumped backtraces of system pids.
    Test: Ran 137-cfi art test.
    Change-Id: Iaca8c18c2a314902e64c3f72831234297e8dcb1b

--- libunwindstack/tests/UnwindOfflineTest.cpp
+++ libunwindstack/tests/UnwindOfflineTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a63bc8194c02aa45e1ecad40f2390687a7a3af46a95b2978cb65f092151aed41
-size 54971
+oid sha256:dca785a1f59e7c3c68a396333f4660067d48b01ce96af175b81c8f076b039b5a
+size 59911
