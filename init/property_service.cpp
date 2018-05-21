21a200200baa94511d157f6a49fe9c638207b05f
DuXiao <duxiao44@huawei.com>
2018-05-21 14:43:56 +0800

    Property: Log errno for socket connect
    It's currently not clear that the error no indicating the
    failure to invoke the library function. This change introduces
    logs at the failing of socket connect, that could be clearly
    get the true reason.
    
    Test: Set system property with failed, and socket recv errno
          log will show
    
    Change-Id: I36bf66988811f953e679d1c5a468de0bf1ab0a05
    Signed-off-by: Jinguang Dong <dongjinguang@huawei.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14eb5a613ad1c3e90f99a6249a333643850ee11f1d8d3966aaefd2b449245873
-size 28394
+oid sha256:e053682281d543a06232bead4d67d7b9cfdb4a5504bb2ed80fb158e3842bcae7
+size 28566
