211dfd95cd11720b9317dd9a8b1a2bc6cc8d64b0
Josh Gao <jmgao@google.com>
2018-05-23 11:04:58 -0700

    Revert "Revert "adb: add support for O_CLOEXEC to unique_fd pipe wrapper.""
    Implement pipe2 ourselves on darwin.
    
    This reverts commit f139ba47a13de0637732af9fd70aab0e8c877e45.
    
    Test: none!
    Change-Id: I42c2a8d07712fe4ac75b5824f754e79a5b9f5cb4

--- adb/adb_unique_fd.h
+++ adb/adb_unique_fd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fb365902e7d562c99e4459a1f8ee5fd3eb7d32c7fd813a49a36e1eae765dcac
-size 1108
+oid sha256:64475e93b76190776f465a72c668d1f284e23b791332e7a01fe8f1888b436a02
+size 1883
