a7f1a4085094455dc02dcd1919f467fa773efff0
Elliott Hughes <enh@google.com>
2018-05-23 09:16:46 -0700

    Add StdioLogger for command-line tools.
    Bug: N/A
    Test: ran tests
    Change-Id: If366a4ea25aea1becdd3e443eba225e9bd52ebba

--- base/logging.cpp
+++ base/logging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:223e122a717a850ef0986403f0e2b067d1c143f7eda651bb554f7c676036ca03
-size 13945
+oid sha256:de1a3c911ee5062c1d705f1bcaeede5aa3e38e393740c1b95ec1b2d56198cef4
+size 14259
