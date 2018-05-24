1b19b4c837301e115ef959e25713e6f6de8fd91b
Christopher Ferris <cferris@google.com>
2018-05-24 16:01:55 -0700

    Change tests to use a temporary dir for files.
    Avoid using hard-coded paths for generated files. This can cause problems
    if the tests are run in parallel.
    
    Also fix a potential race condition in the unwind_through_unreadable_elf_remote
    test.
    
    Test: Ran tests in parallel and normally.
    Change-Id: Ib42522de49499766a29bda5bfffe737b56715e3f

--- libbacktrace/backtrace_test.cpp
+++ libbacktrace/backtrace_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24ecee6a668df8332a188a63b6e8d2668a0a2066ec1a8d3e9a5f473e786e519a
-size 61867
+oid sha256:dd0ad38aeaec6de3521ee43a4a954b7a49ab1bc5e9f79be9327e0623c9ac1b75
+size 61838
