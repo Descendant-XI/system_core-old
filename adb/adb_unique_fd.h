43a4493ef6386c5fbff853635679ac1d268c53a4
Josh Gao <jmgao@google.com>
2018-05-24 22:54:50 -0700

    adb: really fix the mac build.
    Test: changed `#if !defined(__APPLE__)` to `#if 0` and ran mma
    Change-Id: I173cccbfb7bad348ac4796a2d6cebb8490f63479

--- adb/adb_unique_fd.h
+++ adb/adb_unique_fd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df0aff2ef1dd8f9bbe4d0b91910fc88acf1d20ad6b6186bbedabe5cbeba36741
-size 1919
+oid sha256:9635ebe2e006746ac77133833b920a1ca59187cf7f9629e0f81ff36966dca1ff
+size 982
