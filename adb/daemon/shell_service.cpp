d43f2af91e13888e1e2412eb6f4b7b7f35395bba
Jiyong Park <jiyong@google.com>
2018-05-29 16:41:30 +0900

    Shared libs are supported in recovery mode
    adbd has been built as a static executable since the same binary was
    copied to the recovery partition where shared library is not supported.
    However, since we now support shared library in the recovery partition,
    adbd is built as a dynamic executable.
    
    In addition, the dependency from adbd to libdebuggerd_handler is removed
    as debuggerd is handled by the dynamic linker.
    
    A few more modules in /system/core are marked as recovery_available:
    true as they are transitive dependencies of the dynamic linker.
    
    This change also includes ld.config.recovery.txt which is the linker
    config file for the recovery mode. It is installed to /etc/ld.config.txt
    and contains linker namespace config for the dynamic binaries under
    /sbin.
    
    Bug: 63673171
    Test: `adb reboot recovery; adb devices` shows the device ID
    Test: Select 'mount /system' in the recovery mode, then `adb shell`.
    $ lsof -p `pidof adbd` shows that libm.so, libc.so, etc. are loaded from
    the /lib directory.
    
    Change-Id: I363d5a787863f1677ee40afb5d5841321ddaae77

--- adb/daemon/shell_service.cpp
+++ adb/daemon/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82a6fbaed2fd1f15b11284b54e5844e942c8ef0e5ffe5f084ef27b1358f82c81
-size 28707
+oid sha256:82fc9f242327bbcacc6124e309c72211d528e9121bd6c8a0685d343a72419f31
+size 29479
