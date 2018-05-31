864c7136fd1385c70fbffffe422668b2219431a5
Inseob Kim <inseob@google.com>
2018-05-31 13:03:58 +0900

    Support /product/bin
    Bug: 80422611
    Test: m -j succeeded and permissions of files under /product/bin are set
    correctly
    
    Change-Id: I83377d809c47c92da6b226d2853ea6a7462fb127

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50e7048768ce98f54ee93abbe20ca1af2a8dffabc47a4b9e63ad762cd3f3a99f
-size 16552
+oid sha256:bad8fd3d8c04779aa1bbd2064714b540adde02c877eda72dc91b4101dfd4923a
+size 16720
