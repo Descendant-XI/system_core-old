74726a08131f51adb4c3e43ac79727dd91802b97
Mark Salyzyn <salyzyn@google.com>
2018-06-06 13:51:16 -0700

    fs_mgr: avb should use built-in kernel command line parser
    Switch from open-coded parsing of /proc/cmdline to built-in
    fs_mgr_get_boot_config_from_kernel_cmdline function.
    
    Test: compile and smoke boot test
    Bug: 109821005
    Change-Id: I935d2e76c76ad417cbcee398e2afbf9b061d1719

--- fs_mgr/fs_mgr_avb.cpp
+++ fs_mgr/fs_mgr_avb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d1a3d484a97274cde55aa7f5f5abf3bda0897e5013368e0286cf7bf4e50c5ee
-size 23078
+oid sha256:8113676741be7edc593d43dcac11111cb2b62b469e207460d221bdd876f6b835
+size 22695
