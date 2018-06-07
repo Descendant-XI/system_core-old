a9306a46b97e90d46ecc5c67dc0e6199ad7cea34
David Anderson <dvander@google.com>
2018-06-07 15:36:09 -0700

    fs_mgr: Add a library for managing logical partitions.
    This library has methods to build, read, and modify logical partition tables
    based on device-mapper targets. Targets currently supported are
    dm-linear and dm-zero.
    
    Note: this is a revert of a revert, with changes to fix Mac build
    bustage.
    
    Bug: 79173901
    Test: N/A
    Change-Id: If89a788d1919ce8ddc6eedaecc9687a92f111dfa

---
+++ fs_mgr/liblp/reader.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10bea3b9158b2af88c95876d1d47a0160e575c052ef4e246682d60c3fff5d297
+size 11546
---
+++ fs_mgr/liblp/writer.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:12d424f3679764782ac7c280826f6272257ab81cdbe22b2c98ed90b5ebd15560
+size 9893
