51b6709421ef577c562944da511e40765c428a6c
David Anderson <dvander@google.com>
2018-06-14 15:03:53 -0700

    init: Create logical partitions via liblp.
    Currently, init can create logical partitions by hardcoding them in
    fs_mgr or by specifying them in device-tree. This change allows init to
    also create logical partitions by using liblp, which stores partition
    tables in a physical partition. The current name for this partition is
    "android".
    
    Two aspects of this code will change long-term. One, the prototype code
    using device-tree will be deleted once fastboot supports logical
    partitions. Two, libdm will obsolete most of the code in
    fs_mgr_dm_linear.cpp. For now however we preserve how the prototype code
    functions and we layer liblp on top of the existing dm_linear logic.
    
    Bug: 79173901
    Test: N/A
    Change-Id: If014a109da78fa12269bf0df0dda39028ac2d1aa

--- fs_mgr/fs_mgr_dm_linear.cpp
+++ fs_mgr/fs_mgr_dm_linear.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c4cbd9bf2bb20bee4a97620df5199be37747b7fda624a665ffce938ab9b906e
-size 4829
+oid sha256:a422109ebf65a721000e9788a3bb8e27326442f509202259e34c68dced0a8ecd
+size 5909
