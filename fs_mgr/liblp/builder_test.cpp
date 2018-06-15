85ce48388c18e7c1f1299d2f277fc125265e2552
David Anderson <dvander@google.com>
2018-06-15 11:13:29 -0700

    liblp: Add unit tests.
    Bug: 79173901
    Test: liblp_test gtest
    Change-Id: Iba77f0a0886741e459ab65122c28e8bd52ec93e8

---
+++ fs_mgr/liblp/builder_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d39b04857d4a8d2022af1ce5fdad7c528375dd43eac0dfd422a6e60fc2e25b2
+size 13223
---
+++ fs_mgr/liblp/utility_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d589c0ed7c24d5d24cb45a9b4fbe10c5fcc0265478b4cbc75a69fece60882d94
+size 1732
