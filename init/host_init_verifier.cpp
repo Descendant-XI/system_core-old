e7d93b1290792e18723c7b3260e5bed53a3363cd
Tom Cherry <tomcherry@google.com>
2018-06-19 15:18:40 -0700

    Don't check permissions bits on init scripts for host_init_verifier
    Also get rid of the copy in parser.  There's no incentive to switch to
    a tokenizer that doesn't modify the input, nor is there a reason to
    waste cycles making a copy of every init script as its processed.
    
    Bug: 36970783
    Test: boot
    Change-Id: I8aca9c9d6f1961e1ab35dee50691a6791fc6ec66

--- init/host_init_verifier.cpp
+++ init/host_init_verifier.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d489124730241bb73cb532720b72567db6e705f21ccaebb5e09ddba3b34652c3
-size 4902
+oid sha256:90263c4a7146f487301d605035e2f04d0f69fd2191ea3434aa1e9097575fcd30
+size 4914
