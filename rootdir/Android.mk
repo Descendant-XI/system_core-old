c5aeb19c96e99ac73cd6b8c3e4ef26cf5ea7363f
Doheon Lee <doheon1.lee@lge.com>
2018-06-19 15:23:21 +0900

    Determine product partition path on build time
    Path of product partitoin can be set as /product or /system/product
    whether generate extra product partition or not.
    Substitute %PRODUCT% to relevant path to know linker which path should
    search and permit.
    
    Bug: 110286945
    Test: m -j # Check /system/etc/ld.config.$(PLATFORM_VNDK_VERSION).txt
    Change-Id: I6ca177d0c9c5af00ad821879fece40848331fc8d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df6b0dc2813115f8d269d9bb066fea508402f31b6c41642b5cad3672e316e2b
-size 12406
+oid sha256:95086c0e5c88efdf417e0174b59be114d9a3a3cc287f09c7f1a0cb419ae88bac
+size 12474
