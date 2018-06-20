54df9708dd3bdf1c363702ff74315fb0e99d1546
David Anderson <dvander@google.com>
2018-06-19 18:13:07 -0700

    libdm: Implement LoadTableAndActivate.
    This change implements DeviceMapper::LoadTableAndActivate by serializing
    the given DmTable and issuing DM_TABLE_LOAD and DM_DEV_SUSPEND ioctls.
    In addition, this makes the CreateDevice() method private, and
    introduces a separate method for creating a device and loading a table
    as a single operation. This will obviate the need for separate inactive
    device cleanup logic later.
    
    Note that this change does not yet implement DmTable::Serialize().
    
    Bug: 110035986
    Test: N/A
    Change-Id: Ic8affe591db4930ce672b00db989978b57ca8cbf

--- fs_mgr/libdm/include/libdm/dm.h
+++ fs_mgr/libdm/include/libdm/dm.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c698f2cbf2e2bf5793107c8d6312a36e70c3e0e2044123c913a1dcbc787910a
-size 5474
+oid sha256:e37f6cdc2a9cb82fa16c5ddf52dff59b2fdf80b01c5b403ead1d58b6d1ef009c
+size 5561
--- fs_mgr/tools/dmctl.cpp
+++ fs_mgr/tools/dmctl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3e60fd047f8a776655971450a911545d3b1c0b90a17a1757640aeb9555a19e2
-size 5039
+oid sha256:331e7bf1aa725752394274fcaab5ef3cc4510e9351ecfcd1354be29a280009e7
+size 5106
