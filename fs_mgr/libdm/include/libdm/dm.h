187941bb72eff9ad1b29d2ae06287c718a33b9e9
David Anderson <dvander@google.com>
2018-06-20 13:15:31 -0700

    libdm: Add DmTargetTypeInfo and a unit test.
    This change refactors DmTarget. It was satisfying two separate use cases
    that have no overlap: (1) as a container for informational queries, and
    (2) for specifying table targets.
    
    The kernel's nomenclature for the former is a "target type," so the new
    class is named DmTargetTypeInfo.
    
    In addition, this change adds a unit test for
    DeviceMapper::GetAvailableTargets that ensures the device has at least a
    linear target type (with more TBD).
    
    Bug: 110035986
    Test: libdm_test gtest
    Change-Id: Icb87976801e8a1e6ec79e48b1d58c308d36279e5

--- fs_mgr/libdm/include/libdm/dm.h
+++ fs_mgr/libdm/include/libdm/dm.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e37f6cdc2a9cb82fa16c5ddf52dff59b2fdf80b01c5b403ead1d58b6d1ef009c
-size 5561
+oid sha256:e2f324638cdf2aed0f540c7db1cb08d9d66663766144a7813c1b65554c08a081
+size 5569
