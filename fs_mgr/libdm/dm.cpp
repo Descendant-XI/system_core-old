d1a0047f190aca26e3a30b7ccabaa521a7f2bcb6
David Anderson <dvander@google.com>
2018-06-19 18:24:40 -0700

    libdm: Move includes to a libdm folder.
    This places libdm includes into a libdm folder, to match how many other
    library includes are organized.
    
    Bug: 110035986
    Test: N/A
    Change-Id: I430b0cf749c8d16265481864f1f33927fd740a53

--- fs_mgr/libdm/dm.cpp
+++ fs_mgr/libdm/dm.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24d65d215e6394deef5d20e716a562cdaaa97412d1ef75a3e08ef76388d1055b
-size 8744
+oid sha256:b60da2d57a10279c46929923d98c105ab7b297e5c6079e49ffdcad59998d3529
+size 8519
---
+++ fs_mgr/libdm/include/libdm/dm.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c698f2cbf2e2bf5793107c8d6312a36e70c3e0e2044123c913a1dcbc787910a
+size 5474
