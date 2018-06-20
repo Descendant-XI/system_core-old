534c42c315ffc25d25ade880fff7c3148d8e1583
Mark Salyzyn <salyzyn@google.com>
2018-06-20 13:07:28 -0700

    fs_mgr: Add support in fs_mgr_read_fstab_file for /proc/mounts
    Generate fstab from /proc/mounts.  Basically like any regular fstab
    minus the requirement for the Android fs_mgr_flags.  Added a unit
    test to confirm that fs_mgr_read_fstab("/proc/mounts") matches the
    setmntent("/proc/mounts")+getmntent() content.
    
    As a result, discovered a bug in parse_flags that would report "ro"
    if the "rootcontext=..." flag was referenced.
    
    Test: fs_mgr_unit_test
    Bug: 109821005
    Change-Id: I635ee979e11ee089b09adbedb5f42b6ba78026de

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:697a010b4fba2dd0ae3ee25a43fbdda46df0fc9662776e722dcd04680d39feb5
-size 34907
+oid sha256:0670e1dc64134e84a42e039fe007c364c283e91f6424f1297fd80aa473419fa9
+size 34663
