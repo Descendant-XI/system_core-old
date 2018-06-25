3d055e8c816d2c0973924fc3850949470fc65a80
Pirama Arumuga Nainar <pirama@google.com>
2018-06-25 11:48:58 -0700

    Do not customize __format__ for Windows/MinGW to gnu_printf
    Bug: http://b/69933090
    Bug: http://b/69933068
    
    MinGW uses gnu_printf to force C99 printf analysis with GCC, but clang
    does not support gnu_printf.  So just use the default that's used for
    other platforms.  This also mirrors upstream commit
    https://sourceforge.net/p/mingw-w64/mingw-w64/ci/015e637b4b24d9915162ab877ed539ad0e657951/.
    
    Test: m native-host-cross with Clang.
    Change-Id: I3deb266d70e25296c4ae1d58637afbabc3d949c1

--- adb/sysdeps.h
+++ adb/sysdeps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12a62c16e0bbab6147296756093581c9bd531ec01ea1f2a44f1721dd8f6d8dd2
-size 19379
+oid sha256:bd763857d8ac2c788e8dacdcf886279f1f0a53d1b5b1e5fc7371ed6bbd0b3ea9
+size 19051
