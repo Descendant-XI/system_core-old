0509fafcaf1d9977c93ccbc49e7756112c61a1bf
Mark Salyzyn <salyzyn@google.com>
2018-06-28 09:21:55 -0700

    bootstat: record reboot reason history in persist.sys.boot.reason.history
    Retain the last four reboot reasons from latest to oldest, adding a
    detail of UTC epoch time for each.  Beef up boot_reason_test.sh to
    deal with multi-line content.
    
    Test: boot_reason_test.sh
    Bug: 110925971
    Change-Id: Ie2600434b95a885693f005a363ee38d9eba18ee7

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f10c9f15ca91fad7ef45a7e008050fc786af21009c43cc5a78f4d9614512f8af
-size 47434
+oid sha256:41bb472d6c6745d6a6878535575dd65a2786f8bcdb3d8229257ef570cb6496eb
+size 49334
