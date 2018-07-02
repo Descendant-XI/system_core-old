171e01f9378a7cc3c6d15b9cf89e1d7a9d9367f1
Elliott Hughes <enh@google.com>
2018-07-02 10:52:49 -0700

    libbase: assume clang.
    Bug: N/A
    Test: builds
    Change-Id: Ic3d3bee26496025324a3d070e3fcbf6d7b917709

--- base/include/android-base/unique_fd.h
+++ base/include/android-base/unique_fd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f32e050bf821b46a491d59fa0359d1d7c29b1a9d007f993a6f4f6c44c516fa1
-size 3886
+oid sha256:2774e5200bfd1405d0c3c76195fde50191c3ab1f71340ed934b1cbf863b20386
+size 3816
