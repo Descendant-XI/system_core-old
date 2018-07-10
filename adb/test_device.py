0175c891aae4a0a720cba0adfc58f8f78f2ac0f0
Josh Gao <jmgao@google.com>
2018-07-09 18:00:48 -0700

    adb: work around adbd push bug.
    We shipped (well, are about to ship) an adbd that spuriously fails to
    create directories upon push. Work around this in the adb client by
    running a mkdir on all of the directories we would have otherwise
    created.
    
    On devices where we perform the workaround, this coincidentally fixes
    a historic bug where we failed to push empty directories.
    
    Bug: http://b/25566053
    Bug: http://b/110953234
    Test: python test_device.py
    Change-Id: I690ec356c206fed4e5ab2c681c5570c8b231e26b

--- adb/test_device.py
+++ adb/test_device.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6efe28623061d307ac0c42ad6ba1acfc973566b2c53f2a8cd87fadfcfbfc5e5e
-size 50773
+oid sha256:3dd3913a871c5b8c072f12c31163f9570beb02150b3b1569b9912033e3b4f7e2
+size 50730
