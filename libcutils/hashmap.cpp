6fee2509e046cb759f07a12fe4cca9cf465566ec
Elliott Hughes <enh@google.com>
2018-07-11 14:26:40 -0700

    libcutils: remove some unused API.
    Bug: N/A
    Test: builds
    Change-Id: Iabe42353a708afde2611b5c642775849e2e45baa

--- libcutils/hashmap.cpp
+++ libcutils/hashmap.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9c38e8cb9a0d21f6c6629b48d757f7058c36d72083ce88ca2c4f3c8d16ac339
-size 9164
+oid sha256:b3e7cac06d40d27f3bd950b918f55ee781915619ecb6aa3ea8e95f4fb2f6358b
+size 7056
