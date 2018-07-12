79109912a25bc4bccf31dc1bc9d5f84f29617101
David Anderson <dvander@google.com>
2018-07-12 13:05:46 -0700

    liblp: Clean up public headers.
    This changes reader.h and writer.h to be private includes. A new liblp.h
    header now contains the public API surface of those two files, as well
    as some miscellanious functions previously in metadata_format.h.
    
    Bug: 79173901
    Test: N/A
    Change-Id: I40c5dda0c8e5765f8bccfd5c17b4c800b41be77b

--- fs_mgr/liblp/writer.cpp
+++ fs_mgr/liblp/writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db4c0392cb9fb418e0338281e064f8cf78a839ea0a7b2228f86c5d09f45a426
-size 13947
+oid sha256:bfd21fe43d5d759c593c00f50eb95e46d56de16e872a844fd45ba633d4c766d7
+size 13936
---
+++ fs_mgr/liblp/writer.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f8fbe064286082248a550402a77d763d536af647eb36a99918cab67672d300f
+size 1578
