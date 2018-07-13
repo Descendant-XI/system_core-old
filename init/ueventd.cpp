5d2fbe0b5b7267dd8aec62f1319fc90ea58db538
Tom Cherry <tomcherry@google.com>
2018-07-13 15:32:02 -0700

    ueventd: let scripts provide firmware directories
    Since some vendors will have firmware in mount points in
    /mnt/vendor/..., we extend the ueventd script language to allow
    specifying the firmware directories.
    
    Also, move the existing 4 directories to ueventd.rc as a primary user
    of this mechanism.
    
    Bug: 111337229
    Test: boot sailfish; firmwares load
    Change-Id: I0854b0b786ad761e40d2332312c637610432fce2

--- init/ueventd.cpp
+++ init/ueventd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c5f558035f8a4ba856e4a16ff8d5361ee18d4cb41c16090e84ade29a36a1ead
-size 11337
+oid sha256:c19d887f5ae5bca3b74ace872d6129995d9b2d54754199fd13fe9723520b39a1
+size 10852
