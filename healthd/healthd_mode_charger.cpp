0930a7d2f5757f2f8c8f6bb3789a250f5b8136f5
kentsou <kentsou@google.com>
2018-07-17 17:49:34 +0800

    charger: screen blank on disconnect and 0% not displayed
    Bug: 74771887
    Test: m -j succeeded and confirm offmode charge UI behavior
    Change-Id: I8c97230f6c1e32ac9949601a86added79a3ead82
    Signed-off-by: kentsou <kentsou@google.com>

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52e0e0b4805336b1a76d85e9b44bbb87658485fd05546aa7b0fe9e7d05152f32
-size 21330
+oid sha256:57afd6d4e93f4cf010a48cb143d401debe8a34727424b6255a7fbe3ee1b85aa7
+size 22476
