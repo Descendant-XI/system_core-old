3fd0248b53244b517d3ceb01a7587d0f8d04d761
Joel Fernandes <joelaf@google.com>
2018-07-17 13:00:17 -0700

    libcutils: ashmem: Avoid doing fd checks for ashmem calls
    Callers already verify that they are calling ashmem API on a valid fd by
    calling ashmem_valid first. Lets make the fstat syscall only if the
    ioctl returns -ENOTTY. This means in the regular case, only 1 syscall is
    needed (ioctl) vs the current 2 (fstat+ioctl).
    
    Some data to show improvements in reduction of vfs_getattr calls in the
    kernel by 10x when doing a camera.
    
    Test: Boot and camera CTS
    Bug: 111418894
    Change-Id: I992620bbe44355e54ba19eeac81da586c5e5a6e0
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- libcutils/ashmem-dev.cpp
+++ libcutils/ashmem-dev.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74fc7b06c232a8e46e44d4148fb76845426aac74fa6264bc5f28163aa3d04900
-size 5698
+oid sha256:cd89dc4c3907e364e75cb7b3038a6123d9bdbb406fc97ad58718b00e145d66f0
+size 5588
