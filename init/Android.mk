17026cb6c2110f6304c54859e2bd4ece67e8c6d7
Tom Cherry <tomcherry@google.com>
2018-07-20 14:57:00 -0700

    split first stage init into a separate executable
    In the future, systems with dm-linear will require a ramdisk to set up
    the mount for system.  In this world, first stage init will be a part
    of this ramdisk and handle setting up dm-linear, mounting the
    necessary partitions, then pivoting to the system image, which will
    become the root partition.
    
    This also enables previous devices without system-as-root, to be
    unified with system-as-root devices for all aspects of boot after the
    pivot_root.
    
    Bug: 79758715
    Test: boot hikey
    Test: boot sailfish, boot sailfish into recovery
    Change-Id: Iefa88a3ec5994e7989aa9f26f2de0351ffa5468b

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be5a31f4847849920cb2f3e48f84bfe1b383eb62c6d9c57be7a4ed080d233d45
-size 2300
+oid sha256:da021f0acbb1d70dc4de05fc25012d40ec6ff798e3d0fe72874ff6908ef5ad32
+size 1740
