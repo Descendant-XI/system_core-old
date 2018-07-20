21da0f2d9514462450c37a9162319ece09c6e6c4
Roberto Pereira <rpere@google.com>
2018-07-20 16:00:40 -0700

    Run clang-format on all trusty/keymaster .cpp and .h files
    Test: Compiles
    Bug: 110153632
    Change-Id: Ib6e1df87d3c3dfd8c507768d9018114a1b962d74

--- trusty/keymaster/trusty_keymaster_device.cpp
+++ trusty/keymaster/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f4ba41ff8b6703a7c7317ab188a69c88736517cf8a8313cdd59ab13ad0526bc
-size 29241
+oid sha256:73f9e7a57ae9ec597ab9327398f7c34536244caa2d33cfcddbf01102ceedc86c
+size 29243
--- trusty/keymaster/trusty_keymaster_device_test.cpp
+++ trusty/keymaster/trusty_keymaster_device_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc65904ebdc2a94d28c49e74082efe4a94f623b6dd11095ec781444156aeb35
-size 21406
+oid sha256:82689690933bedc84f82b71eb9a9a8bc04c12aa01b0c0ecc12bd982b575d3cba
+size 21559
