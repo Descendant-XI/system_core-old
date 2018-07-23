9538accccbc803d5c70343e41eff6e13caa1a6ea
Sandeep Patil <sspatil@google.com>
2018-07-23 14:50:26 -0700

    dmctl: Do not skip argument if not matched with '-ro'.
    'dmctl create' command would fail if '-ro' flag is not
    used. This happend because the argument in place of '-ro'
    is skipped unconditionally.
    
    Bug: 72722987
    Test:
     $ losetup /dev/block/loop0 fs.img
     $ dmctl create FOO linear 0 25600 /dev/block/loop0 0
    
    Change-Id: I3f6264336b9c9a1b5de76f3a1d29e6045453f9cb
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- fs_mgr/tools/dmctl.cpp
+++ fs_mgr/tools/dmctl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be8b2abd3449640ae29a65f0b763dc4b13825c586fef50ba35c45a84027d8541
-size 9849
+oid sha256:721123b70d87225e326c2dd07cf02cb6181459c745a6d1eac5feea25b9bc02ed
+size 9853
