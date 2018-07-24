7a5fcf2fe162bb2c6c6b7564e78ec20bc3e513ca
Yi Kong <yikong@google.com>
2018-07-24 16:34:27 -0700

    [libsysutils] Modernize codebase by replacing NULL with nullptr
    Fixes -Wzero-as-null-pointer-constant warning.
    
    Test: m
    Bug: 68236239
    Change-Id: I9e95daa0750f2622d1c4788941fdfae23ee61801

--- libsysutils/src/NetlinkEvent.cpp
+++ libsysutils/src/NetlinkEvent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:160eae424fd97e00bc5b705fc0869d4e22cfe8a0c2923512f3a49e72bf1e0e99
-size 22998
+oid sha256:cf16c6bd09c6744ec47468bf303d5364db449e996abe92f817edb7336703f90b
+size 23031
