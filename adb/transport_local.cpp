7b67a5e290856a0d4c027ad59ed2ff5413778d0c
Josh Gao <jmgao@google.com>
2018-07-25 17:21:49 -0700

    adb: fix register_socket_transport related double-closes.
    Multiple codepaths were closing the fd they passed into
    register_socket_transport on failure, which would close the fd itself.
    
    Switch things over to unique_fd to make it clear that we don't actually
    have to close on failure.
    
    Test: mma
    Change-Id: I2d9bdcb1142c24931d970f99ebdf9a8051daf05c

--- adb/transport_local.cpp
+++ adb/transport_local.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b712d93479e1e6ba42f81dda0966baefa201ba65a994802301de8cb9863fee65
-size 18111
+oid sha256:635eb9d80de337f02d8600b0653a8f8d665bf4b6bbbc3fb05be8c239609835a1
+size 17974
