13191bc252b0f022b747971138f1ccbca2dd3c67
Josh Gao <jmgao@google.com>
2018-07-25 17:27:34 -0700

    adb: fix double close in local_connect_arbitrary_ports.
    register_socket_transport takes ownership of the fd, leading to a
    double close if the registration fails.
    
    Test: mma
    Change-Id: I9d699af1a979bedc1dc466427436d47f09d0a88f

--- adb/transport_local.cpp
+++ adb/transport_local.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64df8a4b701e8b5ae983f1317d2dca78c226ee87a9b2f59c6abf7470b17f425d
-size 18095
+oid sha256:b712d93479e1e6ba42f81dda0966baefa201ba65a994802301de8cb9863fee65
+size 18111
