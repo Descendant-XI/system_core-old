337578dab1f75666ff1a2c02ad257d8f1a36c66b
David Anderson <dvander@google.com>
2018-07-26 14:30:35 -0700

    storaged: Don't duplicate uid_records from proto files.
    It is possible for storaged to load the same saved proto file multiple
    times, for example, if system_server crashes. In this case we do not
    want to fill io_history with duplicate entries. This patch elides
    records for which an app+userid record already exists for the same
    timestamp.
    
    Bug: 111578975
    Test: gtest storaged_test.load_uid_io_proto
    Change-Id: I87bc3e37d6464079cece03b5852285d79067b935
    Merged-In: I87bc3e37d6464079cece03b5852285d79067b935

--- storaged/include/storaged_uid_monitor.h
+++ storaged/include/storaged_uid_monitor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7439cd6bec7a9bd1dc263c95c94a26a5a098450ec3629905bdf0c9173288aa00
-size 3894
+oid sha256:41cb7552394b58ea0102fde52d1bee39c4fb879f5e5629f1f8218e74c4f781d5
+size 3964
