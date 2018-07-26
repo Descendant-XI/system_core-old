190d11bcb160cdd25dd5fb06c353cb0972fb5092
David Anderson <dvander@google.com>
2018-07-26 13:20:26 -0700

    storaged: Fix-up private variable names in uid_monitor.
    Bug: N/A
    Test: storaged builds
    Change-Id: Ifd21e25baa7b1c6ce41c5e0ec5247f47ba716e6e

--- storaged/include/storaged_uid_monitor.h
+++ storaged/include/storaged_uid_monitor.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af5820938580980642ae723b4c618a65bde6bf94ae642190cedd5cf0d2bb3df9
-size 3849
+oid sha256:7439cd6bec7a9bd1dc263c95c94a26a5a098450ec3629905bdf0c9173288aa00
+size 3894
