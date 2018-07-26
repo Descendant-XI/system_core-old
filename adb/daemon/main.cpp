dc1b5be7dd64a79a54a6c7652f74f55f7f99d5a5
Josh Gao <jmgao@google.com>
2018-07-25 18:13:44 -0700

    adbd: turn on fdsan warnings.
    Test: mma
    Test: python test_device.py
    Change-Id: Ic90fac0b731b0aa9fa934f4472b0c40f1263c340

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a689efbc562ea5496124133eec2f0e8c54fe9b74846d41355ab2ff91083d8a51
-size 9140
+oid sha256:9acddb37f4f52e4b21ab7c1db3d3e2a41781be12455df2a44a828cdf75963d1a
+size 9367
