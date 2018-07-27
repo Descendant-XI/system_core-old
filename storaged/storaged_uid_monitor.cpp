0c60f7cf40e1db16c133de0fca5491ac918b7e82
David Anderson <dvander@google.com>
2018-07-26 18:01:03 -0700

    storaged: Cap io_history when loading stats from disk.
    Similar to add_record_locked, load_uid_io_proto should respect the
    maximum number of records.
    
    Bug: 111578975
    Test: storaged_test.load_uid_io_proto
    Change-Id: Ic3c5095cdd09d2184f436813b5ab50d3ee0007b2
    Merged-In: Ic3c5095cdd09d2184f436813b5ab50d3ee0007b2

--- storaged/storaged_uid_monitor.cpp
+++ storaged/storaged_uid_monitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69970e88fc6a5b4b8aa7915149fee7c61a794d0a46c0b4f8d7a81dc51b698166
-size 18621
+oid sha256:5b71d504621bddd148f1183ff14679eefeffbd1a9ef361e2918c628ec8ae6a77
+size 18903
