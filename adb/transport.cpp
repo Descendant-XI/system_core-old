3cffad248f401c79a7b7056ac6f4d9d726a22114
Josh Gao <jmgao@google.com>
2018-07-31 14:12:59 -0700

    adb: more immediately try to reconnect connections.
    Most disconnects we're likely to encounter are cases where either we
    notice immediately and can start reconnecting almost immediately (adbd
    restarting because of `adb root`, etc.), or where we won't notice for a
    while anyway, so a 10 second sleep is somewhat meaningless.
    
    Test: adb root; time adb wait-for-device shell
    Change-Id: I18e9213dc4e84d735e9240118a368dcb38f21c78

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c788165a3239df1ed9e0b2b7b0c68d580f5154e09de2e040860d045d1aecd13
-size 40660
+oid sha256:14501a8b8671ac1fedd8e62dfadcd13bc00a096a5fcdbeb64be5e468ac256b56
+size 41076
