b7fb8c4ade6a9f84611c7740e822f478e448fded
Josh Gao <jmgao@google.com>
2018-07-30 18:51:55 -0700

    adb: don't pass time_point::max to condition_variable::wait_until.
    libstdc++ implements wait_until by calculating the offset between its
    default clock and the clock that it's given by calling now() on each
    and subtracting, and then adds that offset to the time_point argument.
    When time_point::max is used, this overflows, resulting in the
    reconnection thread spinning.
    
    Test: wine adb.exe server nodaemon
    Change-Id: Ife58f0aad14bc44c0804483d3ff2351c28b3d576

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6943f80b99bbafeca9f1be4008423eba34345da9d8fb4b4b74824990af29003
-size 40143
+oid sha256:4c788165a3239df1ed9e0b2b7b0c68d580f5154e09de2e040860d045d1aecd13
+size 40660
