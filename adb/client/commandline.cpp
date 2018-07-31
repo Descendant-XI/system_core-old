23c0e3f9b05dcfd6d868fffc743e7824e252ec33
Josh Gao <jmgao@google.com>
2018-07-31 15:36:03 -0700

    adb: fix error message for `adb {forward,reverse}`
    Test: `adb reverse`
    Change-Id: I6fa078ea7a0e019df5c6309df99ffdf2a3219194

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d7e428a2a61f065f3420686a0a26cd1e87e8b956782facf3309c934d261e9b
+oid sha256:d40c53e016f2089506243ed6ffc406a0ce7ec27ba93b2dda081e5e7248c7601c
 size 74696
