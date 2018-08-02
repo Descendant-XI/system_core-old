d7408250fefa6bcab8d1a49d50caeb495aec8d0a
Hridya Valsaraju <hridya@google.com>
2018-08-02 10:46:21 -0700

    Allow existance of partition to be checked before flashall or update
    Bug: 112108453
    Test: fastboot flashall with super.img and product-services.img in $OUT,
    fastboot update with super.img and product-services.img in image.zip.
    
    Change-Id: I5718b5e60c546c55af74292895b3f7fe169e4f02

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be51470f0753a7f74d3768d098ceca6b36a150cab4d0e2beef85be5d1be777bb
-size 60723
+oid sha256:24011c9efe113ed1177b711606efda85be322e8e0f11b04e3250e74cb9249013
+size 61898
