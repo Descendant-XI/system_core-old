3f0c9d854d344b10092a57f4de98bdb71e816fdf
David Anderson <dvander@google.com>
2018-08-06 15:32:57 -0700

    liblp: Allow flashing to readonly logical partitions.
    By default, logical partitions with the readonly flag are created with a
    readonly device. This change allows callers of CreateLogicalPartition
    to create writable devices so they can still be flashed.
    
    Bug: 78793464
    Test: fastboot flash product_services works with fastbootd
    Change-Id: Ia8d2761a3067e3b62815acbf0b6fb7f033072ba2

--- fs_mgr/include/fs_mgr_dm_linear.h
+++ fs_mgr/include/fs_mgr_dm_linear.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79b5a0c786886d67f44d52703294d1b39818b04bbd4fac5f4ef6c54e12ee1532
-size 1988
+oid sha256:5bcbfb7a8ce3ae8b75179c68b2234fb8fada839211d46068922e5a4acd7d0e0b
+size 2138
