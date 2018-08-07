a58683b401bfbac9a499e074c5c58b2330373075
Josh Gao <jmgao@google.com>
2018-08-07 16:07:25 -0700

    adb: make test_adb.py work on windows.
    Switch from os.pipe to socket.socketpair for the fake adbd termination
    termination signaller, that we can select on it on windows.
    
    Test: test_adb.py on windows (2 failures on windows)
    Change-Id: I37df06e465ec8be28cfb18a5c21ef30125195004

--- adb/test_adb.py
+++ adb/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6540ff29ab5e46ef94104ae0a6b5bbcc3e83720952ce29dbc59729bea635df23
-size 18928
+oid sha256:c929e26b0d9e272513d8ff2cf4dd4228ff34c3c51b675e40fad138a236a0118e
+size 18741
