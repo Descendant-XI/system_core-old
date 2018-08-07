51f3a912f1f90182b5d7f034de1e4ea5d6fd68fb
Mark Salyzyn <salyzyn@google.com>
2018-08-07 08:13:13 -0700

    llkd: switch to std::literals
    Replace std::string("<string>") with "<string>"s
    
    Test: build
    Bug: 33808187
    Bug: 111910505
    Bug: 80502612
    Change-Id: I8c7915b26719081d93f486c22d9a40b4ad548085

--- llkd/libllkd.cpp
+++ llkd/libllkd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e78e0587e77ba1eb4b0dde557e902d6b06c2d6b7b4975afde584aae43f93c74f
-size 44205
+oid sha256:8c52d376f706de60710f2ed5eb59afdcda36583c01dc53212ff750c19bf7e35b
+size 44187
