93fea861c8293bae1dd2b4d11314fda043bd45ea
Vic Yang <victoryang@google.com>
2018-08-07 10:18:22 -0700

    lmkd: Do not downgrade/ignore events when swap is full
    When the swap space is full, a pressure event is unlikely to resolve by
    itself.  In this case, do not downgrade or ignore the events.
    
    Bug: 112056451
    Test: Fill up swap on a 1GB device and check critical vmpressure events
          are not downgraded.
    Change-Id: If154dc364711bf7c86f32e24ddcd10be359386de

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3806b8720f7f6ca3fbae0e7ee1d839d1bbf7c2c88c218f4db149b1fd0d5aff76
-size 46545
+oid sha256:47eb5cbb846b225199a9b89700f0cf9699cfc70362daedc62d449fe41e8cbb05
+size 47000
