2d3033bd6505fc8b1b960cf0a0c36c3209f92c4b
Josh Gao <jmgao@google.com>
2018-08-08 14:26:03 -0700

    adb: disable test_device.FileOperationsTest.test_pull_symlink_dir.
    selinux prevents us from doing this.
    
    Test: python test_device.py
    Change-Id: I7117dd19348b0764638977b7c958331a4839ea4f

--- adb/test_device.py
+++ adb/test_device.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3746192846412c2f4f09910b5dcd58c00794be570b4c69e2c10dbd9da9258672
-size 50781
+oid sha256:1f0ebc57c7af99c516ef372c0d2dfa1698da01e5672d736a6f3f12e61d20cb4e
+size 50862
