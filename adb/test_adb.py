c52a40c892e2c810ee5620eb76ee67b052562a62
Josh Gao <jmgao@google.com>
2018-08-10 14:44:54 -0700

    adb: make disconnect stop reconnection immediately.
    Make `adb disconnect` remove transports immediately, instead of on
    their next reconnection cycle.
    
    Test: adb connect unreachable:12345; adb devices; adb disconnect; adb devices
    Change-Id: I35c8b57344e847575596d09216fc636be47dde64

--- adb/test_adb.py
+++ adb/test_adb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a94c6a36bb8d1e46a452e7e5f4afa91e23046e26b05beeb818daa492992f0b11
-size 18735
+oid sha256:a2f7fa9f70f78c535c94e63a113ef5cec97519e46634b4ad8420211483a5f118
+size 20350
