fffe3647140a8ede02702945ca61a952d6cae3cf
Spencer Low <CompareAndSwap@gmail.com>
2018-08-11 00:16:16 -0700

    adb: win32: test for Unicode console output
    Unit test for adb on Windows writing Unicode to an actual console
    Window, as opposed to a pipe or file.
    
    Test: Ran test on Ubuntu and Windows 10, tested version of adb with
    incorrect Unicode handling and verified that test failed
    
    Change-Id: Ibdda46d0fee83004537bcbb48a5c2fd6d3e1d593
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- adb/test_device.py
+++ adb/test_device.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f0ebc57c7af99c516ef372c0d2dfa1698da01e5672d736a6f3f12e61d20cb4e
-size 50862
+oid sha256:4adddf5574c8b1b8744a47b4ab1cf619bb3397abc54d00fdca11233ea5c3576a
+size 59506
