d1bf33a9239bc86e4ead0b06633fa69335deabcd
Hans Boehm <hboehm@google.com>
2018-08-17 11:40:39 -0700

    Check sp<>::clear() for data races
    sp<>::clear() presents the same risks of heap corruption in the presence
    of data races as does assignment. Add the same data race check.
    
    Bug: 112651574
    Test: Build and boot AOSP
    Change-Id: I75d4eedd756d521920e61ff9187509f9145d4235

--- libutils/include/utils/StrongPointer.h
+++ libutils/include/utils/StrongPointer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb14fca3cf82f7231e7cebe6dbef27ffc7d1eb836a1eb30963ec4e7174e5419d
-size 7319
+oid sha256:659c93124a13109ee4e05e534fc7e8c6518747ddf0e03296e3e800f17c1c4da0
+size 7446
