9763f650c0d9a9595399e25959f17f1ead5dc8f2
Aaron Wisner <awisner@google.com>
2018-08-22 11:01:04 -0500

    Add support to fastboot_driver for sending sparse images with CRC check
    The default argument will prevent anything from breaking
    
    Test: Build on glinux
    Change-Id: Ib427ab210476db1ec1c69c0a3238d0653e98b79a

--- fastboot/fastboot_driver.h
+++ fastboot/fastboot_driver.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e7af9d6e7e2a3ea0d3955033bbcc167cb27d10c087bfe1d130ad77926fff5ab
-size 6532
+oid sha256:810aef211e43e7d8556e921213cb0e0b4e78a6f6b77c16528e4cd60e283562df
+size 6554
