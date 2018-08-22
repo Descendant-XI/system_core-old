a44f08915df97188d1400c08e7b6d011d6090bfd
Tom Cherry <tomcherry@google.com>
2018-08-22 14:55:56 -0700

    init: accept -1 or 'unlimited' for an infinite rlimit
    Due to a bug with ParseUint(), init would defacto accept -1 for an
    infinite rlimit, but only on 64bit devices.  That bug is now fixed,
    such that -1 would be rejected by ParseUint() for all devices.
    
    This change explicitly checks for -1 for all devices or 'unlimited' to
    match ulimit's reporting and accepts either as an infinite rlimit.
    
    Bug: 112668205
    Test: new (and old) unit tests
    Change-Id: Ie28ff622cdf375a65ceb5f32ffb14fb3d5d9f2ba

--- init/rlimit_parser_test.cpp
+++ init/rlimit_parser_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:becb3a01b76ac6477e30661aa673b374e8bb7e74e874bcc43b0ab7fcc4ee4ba3
-size 5246
+oid sha256:b4dac58b2b9f88292201d5cf96bdf6046379d295faf20297cfec2053e2feb6fd
+size 6020
