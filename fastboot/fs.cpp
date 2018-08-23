a7a4638f80e0a498f6da52f58ff3b6c82b42fdc0
David Anderson <dvander@google.com>
2018-08-23 10:42:02 -0700

    fastboot: Use _WIN32 instead of WIN32.
    Bug: N/A
    Test: fastboot.exe builds
    Change-Id: I316611f581c80ec813b953a7e6074293399ab7d7

--- fastboot/fs.cpp
+++ fastboot/fs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9962f719281587080f2a34b6ab12f241f6e390c068ae2bc1967b002d8060fe28
-size 7650
+oid sha256:f5e9b992eff5037cbdf614e640710d190d23937da07ce6b7094fad2314b688cd
+size 7652
