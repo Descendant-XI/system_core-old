ccb576c3de43304d19c7dda37117224381f8ee3a
Yan, Shaopu <shaopu.yan@intel.com>
2018-08-23 14:47:56 +0800

    add the impementation of delete_key, delete_all_key for Legacy HAL
    the new layer (Keymaster2PassthroughContext) will call the related function
    operation, however, currently it’s null so it will have null pointer reference
    issue and we need to provide them in the keymater legacy HAL.
    
    Bug: 113084196
    Change-Id: Id1b0df47c03d341aedc7a0634cb101966143641c

--- trusty/keymaster/legacy/trusty_keymaster_device.cpp
+++ trusty/keymaster/legacy/trusty_keymaster_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fbef9504071307422f9c132c2afea4766c57cae61eedf1548d3400f4a1f4018
-size 26924
+oid sha256:b6c3ddafcce12c7d741d06ccdba507c9ef2e8661f653960d41080efba27d1cce
+size 28157
