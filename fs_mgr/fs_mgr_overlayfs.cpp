54a83aac49c6148c3a570f253058978b305a0a18
Mark Salyzyn <salyzyn@google.com>
2018-08-29 10:44:33 -0700

    fs_mgr: candidate list check mount_point
    if we specify mount_point /vendor, candidate list should not add
    /system as a candidate.  eg: adb disable-verity /vendor.
    
    Test: manual
    Bug: 109821005
    Change-Id: Id4d9ee617262cf1b68a790c9c43476d0f907353a

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d72bd7c1d0b39e1498a9b6aef67a5387841195fd321bfc27d7dc57b9022b77b1
-size 20118
+oid sha256:30d997fefbd2aeaa76803a63f99b864c39dbc0dad8bde2f46eb9b1cfd587ea18
+size 20175
