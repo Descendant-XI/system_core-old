4ded7277274e011ce7f9c25b550b91810388c6ed
Mark Salyzyn <salyzyn@google.com>
2018-08-29 10:44:33 -0700

    fastboot: use constants.h values
    Cleanup to utilize all the manifest values in constants.h.
    
    If the cli command _and_ the protocol name match, use a common
    source of convenient truth.  This should set a pattern for future
    additional commands.  When the command and the protocol differ,
    we want to introduce resistance as it results in confusion and
    maintenance issues.
    
    Test: compile
    Change-Id: Idad413c63cbbfcb6e851856105a5d5a9ef53ef29

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b19293b6c84e59c98ca1a3a74a340e515855be5c2d04876d1ce55e73bf1ddba
-size 69393
+oid sha256:babb93dfd4d97915c865794a902425622cacf28f0fa18a34729b949d06e1e2b3
+size 69467
