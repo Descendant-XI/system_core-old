099918a8d4aaeb25e37a907fb11a7e7def174cca
Josh Gao <jmgao@google.com>
2018-08-30 11:37:00 -0700

    adb: don't try to reconnect emulators.
    Bug: http://b/113533872
    Test: ./test_adb.py
    Change-Id: Id591f43b487cc3928390e30f11645990e34a51bf

--- adb/transport.h
+++ adb/transport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57c8c1e43f2d51e8aac0b06ddcb3e4ce9677773a298e2c2d197434f07808663f
-size 13051
+oid sha256:d4e8c532b98b0b5b5ed557b48ab6a89384810e0ad40507aa92595dcb8f07646a
+size 13099
--- adb/transport_local.cpp
+++ adb/transport_local.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ee96a2775d68de4af8b4d96edf4c4223b59876b024d496970dffd058f9106fc
-size 18215
+oid sha256:423b9a76bdd5ed740df18eb14562d07e86b78c75a5b9ae2e4b9345e0b293e719
+size 18404
