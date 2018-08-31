959c8c768f5b053382c2e1cc4c90619c63c7bdf5
David Anderson <dvander@google.com>
2018-08-31 16:44:25 -0700

    fastbootd: Implement getvar all.
    This implements getvar all by invoking each callback and writing an INFO
    status for each result. For commands that take arguments, the variable
    handler can specify a function that returns all possible arguments.
    Currently this only applies to partition variables.
    
    Bug: 78793464
    Test: fastboot getvar all works
    Change-Id: I1cf84e06bf67614b6f56171c0ee6ca5d7ac383c9

--- fastboot/device/fastboot_device.h
+++ fastboot/device/fastboot_device.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b10078fd1464f505912c0fb3c18655c00a01bcc01d455c3277e4d70f3528a799
-size 1804
+oid sha256:8a233f94fda2c6c1f41bfc0e8106efd8b6e60edf9ede8b27b5c3de4f005e616c
+size 1838
--- fastboot/device/variables.cpp
+++ fastboot/device/variables.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd9cdb0f758d0e8bac8013d7a92d6a66e9808aa2f1412d93760ce7aecc203f11
-size 7207
+oid sha256:3e9ca4c56882857a7f747e9e74e3df50028f5736ffa2687aebf6b5bec3eb12ce
+size 8469
