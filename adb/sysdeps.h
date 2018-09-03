ff5fa8005d3a93bf4e2618bb11ea56c6eebf13bb
Spencer Low <CompareAndSwap@gmail.com>
2018-09-03 16:03:22 -0700

    adb: win32: set thread names
    Use a new Windows 10 API, SetThreadDescription(). Background info from a Chrome developer:
    
    https://randomascii.wordpress.com/2015/10/26/thread-naming-in-windows-time-for-something-better/
    
    Test: Win10, Vista, adb shell
    Test: Ran windbg -pn adb.exe, used .dump /ma to create a dump, ran
    `dx -g @$curprocess.Threads' on the dump and it showed the thread names.
    
    Change-Id: I14ea7121605cb3fa45ce7b59e2ba5882a215b59f
    Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

--- adb/sysdeps.h
+++ adb/sysdeps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29bb982b3933a1dbbe8a997b55505edd189bbee132aadd20836428708ae58d3f
-size 18392
+oid sha256:011ae9004279bcb9340fa17aff4142d76668c22e728fff7bc64587b166afe5c8
+size 18117
