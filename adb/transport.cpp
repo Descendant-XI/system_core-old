29a046d3d25d2e7d0e03e57cdc1cecb54492735d
Elliott Hughes <enh@google.com>
2018-09-05 12:13:11 -0700

    adb: static constexpr.
    Also inline some single-use constants.
    
    Bug: N/A
    Test: ran tests
    Change-Id: I1ac028667772599291da402120a270d0667fed04

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd9f3ef60e415f0af349696bcf59d6fbe418294942c36ca300f92a778bc8261c
-size 42665
+oid sha256:f434f0947cd61090851c68c094266ffe73882420cbb17a70ec0e0e012766e3bb
+size 42546
