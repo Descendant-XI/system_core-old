1a8cfb409650dd0057da39972ee1a7584c664817
David Anderson <dvander@google.com>
2018-09-04 18:11:03 -0700

    fastbootd: Implement getvar hw-revision.
    Bug: 78793464
    Test: fastboot getvar hw-revision works
          fuzzy_fastboot Conformance.GetVarRevision passes
    
    Change-Id: I5a3e9893d61e18567f2f818ef06cad8e862d637f

--- fastboot/constants.h
+++ fastboot/constants.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42d7ef632cc1af3a6e41830995ddd65cfee9124e46c01472956473968d8fa52b
-size 2167
+oid sha256:383c43ef6a820d45214157e21196ce2ff96e4ba5f25b7c9100998081dc95cd50
+size 2208
