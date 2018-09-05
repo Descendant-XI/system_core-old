6c7096aa6059f18a1ccda9f16bfd0a283c2bbe13
Tom Cherry <tomcherry@google.com>
2018-09-05 15:11:44 -0700

    fastboot: clean up CheckRequirements
    CheckRequirements() had various issues that are cleaned up here,
    
    1) Move from C string parsing to C++
    2) Moved from C data structures to C++, including fixing memory leaks.
    3) Removed the 'cur_product' global and the 'query_save' function that
       stores it
    4) Actually writing tests for the parsing function for
    android-info.txt
    5) Check that a variable needs to be checked for a given product before
       trying to read it.  Previously, fastboot would fail if a variable
       isn't recognized on a device, even if the check should be ignored.
    
    A lot of flexibility is allowed for the input strings, to keep
    backwards compatibility with the previous parsers.
    
    Test: fastboot works, unit tests
    
    Change-Id: Idc3bba8b8fe829d8eefe5f6c495e63a9441c0b60

--- fastboot/engine.cpp
+++ fastboot/engine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32cc43c82b2e9b73e7eaa8b6845f75a417a6a4e77be75328506098982d741aa6
-size 8212
+oid sha256:8bc7dd50fe9cf14a6c773f4d8daafde390baed9cdb7c963f9a7add8bde3d93e1
+size 6167
