3b76c963f67633a7c1ecd9550613c7aa28e50f0f
Tao Bao <tbao@google.com>
2018-09-07 12:52:46 -0700

    healthd: charger uses shared libraries.
    With the change in [1], /sbin/charger always gets included into
    system.img (as opposed to ramdisk) and started by second-stage-init. It
    no longer needs to be statically linked. This CL switches it to use
    shared libraries, and moves it to /system/bin/charger.
    
    With this change, charger no longer gets installed into recovery image,
    which will be handled by a follow-up CL.
    
    [1] https://android-review.googlesource.com/c/platform/build/+/728287
    
    Bug: 73660730
    Test: Boot into charger mode on taimen. Check that charger UI works.
    Test: Run charger_test on taimen.
    Change-Id: Idff70cfad09c825cdba4d35a471ff7ecd5f8a8f7

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6ce43cb3d10c4b99a67c006fb39e2393d3a8e59901a37fa2d3d0caf020a4082
-size 4484
+oid sha256:679ee9bf38f0bd92c8809dc8ce688b212936f466ed96842dbb7a97cbc4978b2d
+size 4621
--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc33dd29e805fc742a651495c459780f17eaedf13116e45cfd631431eec746c0
-size 4373
+oid sha256:dfb8ec3f32116ae6b835ce0cb834e0af6ba388fd01e9812b6b4058c2e3b7cd0b
+size 4316
