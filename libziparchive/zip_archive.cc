756f734abaca39510eb434bdc7fc2434fb96d77a
Josh Gao <jmgao@google.com>
2018-09-07 12:44:40 -0700

    libziparchive: encode type in fdsan owner tag.
    Test: debuggerd `pidof system_server` | grep " fd "
    Change-Id: I05577adb94cf58b549aa0af03b42653e345bcd11

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2ae2f104617dad14b709d1f0383f9cfddc9851e49d8ffdf97d9b37abb62b2df
-size 44002
+oid sha256:7902760f909bdee3583ae56c9cd0a7564f658b90023236094cb96915e479fd74
+size 44211
