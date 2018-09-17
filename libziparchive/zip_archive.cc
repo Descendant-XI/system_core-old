c6df5e4d06225da34773a1ac79c24c5edf9f915f
Zimuzo <zezeozue@google.com>
2018-09-17 19:49:55 +0100

    Reduce libziparchive internal hashtable memory size
    Debugging memory allocations on the nexus launcher unveiled significant memory allocations for the hashatable used in libziparchive, ~1MB.
    This is partly because of the ZipString struct storing an entry in the table. The struct stored a pointer to a string (on 64 bit, 8 bytes) and the length to read from that pointer, 2 bytes. Because of alignment, the structure consumed 16 bytes, wasting 6 bytes.
    Now, we store entries in the hashtable as a ZipStringOffset. This new structure stores a 4 byte offset from a fixed location in the memory mapped file instead of the entire address, consuming 8 bytes with alignment.
    
    Bug: 79416399
    Test: Builds successfully and manual testing by opening launcher on Pixel 2 shows precisely 50% decrease in memory allocated for the hashtable. From 909312 bytes to 454656.
    Change-Id: I28b43699233fbee7f63fccae2d4fe96fcc07e5c4

--- libziparchive/zip_archive.cc
+++ libziparchive/zip_archive.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2ae2f104617dad14b709d1f0383f9cfddc9851e49d8ffdf97d9b37abb62b2df
-size 44002
+oid sha256:4b1be0928ed154a4ce7da9c33c08adc4c6a0d44b8c1bc41a1eb26726d87a7cdf
+size 45338
