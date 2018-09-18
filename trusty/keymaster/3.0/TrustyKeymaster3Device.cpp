0fa5c59c12940af197e1746e9f55f2e47c7fd566
Michael Ryleev <gmar@google.com>
2018-09-18 15:43:57 -0700

    trusty: keymaster3: Modify TrustyKeymaster3Device::update method
    Modify TrustyKeymaster3Device::update method to handle the case when
    amount of input data received exceeds a maximum amount supported by
    underlying transport. In such case, only send an portion of data that
    fits and allow higher levels to take care of the rest.
    
    This is not an ideal fix as it is not very efficient for large sets
    of data but at least it should work in more cases.
    
    Test: android.keystore.cts
    Change-Id: Id7360d0da3b87493193d480fc0c78c65dc1fc51f

--- trusty/keymaster/3.0/TrustyKeymaster3Device.cpp
+++ trusty/keymaster/3.0/TrustyKeymaster3Device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:936b8c251dc73af140c325dba5d0b3d862e77bc59903adae288bc7c6dc57a6d4
-size 17476
+oid sha256:6c1fecc6205bb3386bf4259e6fc470c926a162d60bcb56feff5d3fdb7d855314
+size 17914
