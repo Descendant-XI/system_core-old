102755e55a2ccb5e5890b742ca62657c86a402e9
Tom Cherry <tomcherry@google.com>
2018-09-19 16:25:51 -0700

    init: use '/system' as the system mount point for force_normal_boot
    We need to keep the right mount points in the fstab for other
    processes to access (/ for system-as-root or /system otherwise).
    Force_normal_boot devices are defacto not system-as-root,
    since they're booting through the recovery ramdisk and therefore must
    use /system as the system mount point.
    
    Bug: 114062208
    Test: boot pixel from recovery with force_normal_boot
    Change-Id: Ib0e4e4f8a29299ecdcd35e8f1415551c4c9745ad

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51535e9980462b1ebed06783dec2c12071e9dc36c8ebd775b038af928a367af7
-size 23214
+oid sha256:5bfc31eb69f1f5eab7e07622d4a34ef038d9280e3d2a0bed505b16c04889d0bf
+size 23382
