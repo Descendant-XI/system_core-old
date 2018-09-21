1784e814d354adef05a1e4953b1a0484ba9e095c
Paul Lawrence <paullawrence@google.com>
2018-09-21 10:49:38 -0700

    make checkpointing work on ext4
    Test: Test script passes
    
    Change-Id: Iafa337947f2fd456aa692ecb112ccc56638f7947

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc55b991c708316aaf0d7c616b02387a5249df3a03f4799c3b29ab6d77db06e6
-size 55065
+oid sha256:8dbe1296eff5d9f3f370f8e56d0fc1ff24327511d259fb3644e5c46b21dceea4
+size 56108
--- fs_mgr/libdm/include/libdm/dm_target.h
+++ fs_mgr/libdm/include/libdm/dm_target.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e9ae4a00119e5ea78b0131303d8c964e833d8725b69cacfdcf3f062b3b2c712
-size 5875
+oid sha256:183384d810fc91f3512b6eca8c21c97444829ec87d520d730c9ac3656db3947a
+size 6015
