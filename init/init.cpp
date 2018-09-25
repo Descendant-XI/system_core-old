0d0b89725ce72e701b9630499066cab5fd5dca9c
Hridya Valsaraju <hridya@google.com>
2018-09-25 15:43:42 -0700

    Set device lock status from kernel command line.
    Device is considered to be unlocked if
    androidboot.verifiedbootstate is "orange".
    
    Test: adb shell getprop ro.boot.flash.locked
    
    Change-Id: Id3aeec757908ea63a37e28ad880a6c71d53083ac

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b2ad6a77da94e0085dfd42be92a179a568b1822933c4944eb8ee4015960ac24
-size 26020
+oid sha256:3b65dc745240a5c13484f554db9e006b4ee15f7826cbf408602f7ae76fed5dd0
+size 26139
