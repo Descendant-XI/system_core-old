980d47b7c9bcb43fda980677bc68a1cbccd34d10
Mark Salyzyn <salyzyn@google.com>
2018-09-26 08:01:04 -0700

    bootstat: uvlo -> reboot,undervoltage
    uvlo anywhere in boot loader reason string will match and convert to
    reboot,undervoltage when propagated to system boot reason.
    
    Test: compile
    Bug: 63736262
    Change-Id: I14b0b7e8185aa4fb519efdb6cb1306718e9de69c

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09e7295c3bfb41750c2275ffd5bdd06796f28bcb45c1bba8ced72326b7c915fb
-size 51653
+oid sha256:ade9dd834052b21b48a3420aa49fcd60ac2e65ecbdf065325b8e568362c5899a
+size 51725
