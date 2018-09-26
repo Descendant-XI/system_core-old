b428fe0426c2f7baf1c61e9afc2e022a4f2236c9
Mark Salyzyn <salyzyn@google.com>
2018-09-26 09:55:25 -0700

    bootstat: smpl -> reboot,powerloss
    smpl anywhere in boot loader reason string will match and convert to
    reboot,powerloss when propagated to system boot reason.
    
    Test: compile
    Bug: 63736262
    Change-Id: I156bfefd05d2bab480408cf6bb1dc4c61c8983f9

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade9dd834052b21b48a3420aa49fcd60ac2e65ecbdf065325b8e568362c5899a
-size 51725
+oid sha256:aa1abb8cefc096a6205e8757b089a25aa521970379fe63186dd1b8c5e96e00d4
+size 51834
