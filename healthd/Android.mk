f834f6e2b5bf6dd0ed1a187207236b1391cfa740
Jayant Chowdhary <jchowdhary@google.com>
2018-10-01 22:50:07 +0000

    Reland "Add static libbinderthreadstate explicitly to charger module.""
    This reverts commit 17a65497bb33d2fffd83ee17871633af1faa6b47.
    
    Reason for revert: Dependencies which broke tests due to exclusion from LOCAL_JNI_SHARED_LIBS have been added. 
    
    Change-Id: I51e8ea7072026f233cd4a6c7e2142d92603af1f9

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:369d85330baf73ccef10701696700f11efa74a15cb2cd82a4e8c5403baf84b05
-size 4307
+oid sha256:8940a8439da1014201e6d18ca632692f05a204861686e68c0c95cba88a0da5f3
+size 4334
