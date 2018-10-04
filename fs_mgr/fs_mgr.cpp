6c79fe64421031bdff3996e180c8a3d199fd0096
Paul Lawrence <paullawrence@google.com>
2018-10-04 08:37:17 -0700

    Handle retry count correctly
    Test: vdc startCheckpoint 2 then reboot 3 times checking state
    
    Change-Id: I4eeda7f73d82a7c8b2469571fa558df2fac47354

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e26d6f8bb9df3ce064023e9351f461fff0ecbac2b51a4247c177679269a788fe
-size 56645
+oid sha256:3826ef598e5a16081fbc3a3c4757a69481cb01ddc0ce5045e036d5e8a74dd88d
+size 57775
