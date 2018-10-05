0541a37be76c03eba92dba48254de2f57f7dcc88
Rajeev Kumar <rajekumar@google.com>
2018-10-05 12:34:59 -0700

    Read memory stats from /proc/pid/stat file.
    (cherry pick from commit 0301683e49ab255769b15469487feaab3466167a)
    Bug: 117333340
    Test: Manual testing using alloc-stress tool
    Merged-In: Ie555933aafa6a6b7aa1dbf5518ebe804376e0afd
    
    Change-Id: I8ab08606dba7de2f65711204453067dbfbdcbdd8

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03d1e6f7448653fe6ff325959676f6fe30085d75d5eb6690e198ea01e4ab50dd
-size 58796
+oid sha256:8dcc2509b3eb3360f0e31d2a9751cf1f19fb37c99e9c26eebfa32283e1575c77
+size 60297
