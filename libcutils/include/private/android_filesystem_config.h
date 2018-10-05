2914b2b12c9b1f590057cfadb486e24f3a9c832a
Igor Murashkin <iam@google.com>
2018-10-05 16:26:28 -0700

    android_filesystem_config add AID for iorapd
    Add new AID for new binder native service (iorapd).
    
    Bug: 72170747
    Change-Id: I81facb3f22e1162dc49329b970532287833fcecd

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb1a8cbf4f2a2d88d41f162d942d3115c97b0564f9569e7ba237b95b4a34d96b
-size 10068
+oid sha256:fc610c9e226f65ac3288858cd016eb2ccb7c2762b747dccc6a9265ea5e03d4c7
+size 10145
