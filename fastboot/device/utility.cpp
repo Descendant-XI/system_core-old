4115c89b8e05dd50380079e3ef728dbabb58ceac
Hridya Valsaraju <hridya@google.com>
2018-10-08 12:16:10 -0700

    Check return status of ReadFileToString() in GetDeviceLockStatus()
    Return lock status of true if unable to read kernel command line.
    
    Test: fastboot getvar unlocked
    Bug: 117275849
    Change-Id: Id6c00c769bcdcdf87fcc9e88902713a8eff3e320

--- fastboot/device/utility.cpp
+++ fastboot/device/utility.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac6385a57e5e09e1495a626ff4f221ee7e763178a383d9f4368b4c739d231c46
-size 5886
+oid sha256:62d6ab66c9402af69a08c96879a2ec176f7ccb80487ef85c4d8bfdc3fe314be5
+size 5990
