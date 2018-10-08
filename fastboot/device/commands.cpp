748e7dddc64a53bcb94c60d289d872d6b6b3c607
Hridya Valsaraju <hridya@google.com>
2018-10-08 13:10:25 -0700

    Check maximum allowed download size for download command.
    Bug: 117272937
    Test: fastboot flashall
    Change-Id: Ied34a89129813a7f9fce6d4f48d8b65967558731

--- fastboot/device/commands.cpp
+++ fastboot/device/commands.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c22188e717ca11b5744edf27be6fcc1f076a1c79d5796d6118b58da718394e4
-size 16377
+oid sha256:883a45efb069c36456a0501329a6d44ffee612c073fab6fdee1d9634c9abeb8e
+size 16392
