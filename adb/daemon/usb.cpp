de584ddca8fc69b30c1c6f81a3f73c1558f95b63
Josh Gao <jmgao@google.com>
2018-10-08 14:42:19 -0700

    adbd: rename daemon/usb.cpp to daemon/usb_legacy.cpp.
    Change-Id: I0b369d1653220f7d9b542ad06998c5c2c3e129a2
    Test: mma

--- adb/daemon/usb.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:44e42c204ce4b208e819ee3a27755e5f8a3a4daf5cd2280831cd732d81bf2ccf
-size 9454
---
+++ adb/daemon/usb_legacy.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d69461745163ea0e6ef3467416317e5841807acf5229033f5fec5a8eb8b15e6
+size 9467
