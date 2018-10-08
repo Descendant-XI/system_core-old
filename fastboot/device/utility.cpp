05446f29004a782c13bb2c83533d92b266c728c4
Hridya Valsaraju <hridya@google.com>
2018-10-08 11:06:38 -0700

    Validate partition name when searching for physical partitions.
    Bug: 117274137
    Test: fastboot getvar has-slot:../by-name/system
    Change-Id: Ibc74a12f836a30bdf1fd7f6b2a1e941de2430c70

--- fastboot/device/utility.cpp
+++ fastboot/device/utility.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac068a380cc5f56e4ef5014a8ad2b44e9caf43557438abdb9c13beac04453049
-size 5697
+oid sha256:ac6385a57e5e09e1495a626ff4f221ee7e763178a383d9f4368b4c739d231c46
+size 5886
