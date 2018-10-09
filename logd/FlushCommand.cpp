631f27ec3fc2794fe09ba9c4facc7b39a77279e6
Tom Cherry <tomcherry@google.com>
2018-10-08 17:33:50 -0700

    logd: rework logic for LogTimeEntry
    LogTimeEntry's lifecycle is spread out in various locations.  It
    further seems incomplete as there is logic that assumes that its
    associated thread can exit while the underlying LogTimeEntry remains
    valid, however it doesn't appear that that is actually a supported
    situation.
    
    This change simplifies this logic to have only one valid state for a
    LogTimeEntry: it must have its thread running and be present in
    LastLogTimes.  A LogTimeEntry will never be placed into LastLogTimes
    unless its thread is running and its thread will remove its associated
    LogTimeEntry from LastLogTimes before it has exited.
    
    This admittedly breaks situations where a blocking socket gets issued
    multiple commands with different pid filters, tail lines, etc,
    however, I'm reasonably sure that these situations were already
    broken.  A check is added to close the socket in this case.
    
    Test: multiple logcat instances work, logd.reader.per's are cleaned up
    Change-Id: Ibe8651e7d530c5e9a8d6ce3150cd247982887cbe

--- logd/FlushCommand.cpp
+++ logd/FlushCommand.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6110b6e0e92364d228242621b26357db9ce393c6c1c99f5b97e45d5d5d451d87
-size 3517
+oid sha256:d3c291ef795cacca66b5b7270b3642850f0a8a931624bdf34e4e1868cbc23682
+size 2916
--- logd/LogTimes.h
+++ logd/LogTimes.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd11831a110064bc874e6d4d1ca502d2ea48bbb1929651e0d0eeac9b23d12528
-size 4148
+oid sha256:096c81fd09c70797532b548bc4cf24cb743f451eb0fe95551ae7b9fc8192fe3b
+size 2842
