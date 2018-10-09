81498f90b615721c83d86238a4f3e42b0c4f972a
Hridya Valsaraju <hridya@google.com>
2018-10-09 10:03:51 -0700

    Return partition size in hex format.
    This is required to pass fuzzy_fastboot conformance tests.
    Also, allow for zero sized partitions in fuzzy_fastboot.
    
    Test: ./fuzzy_fastboot --gtest_filter=Conformance.Slots
    Bug: 117220134
    
    Change-Id: Ifb12994a7796b081215084cb68b37674210aaa12

--- fastboot/fuzzy_fastboot/main.cpp
+++ fastboot/fuzzy_fastboot/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a0cdb0fa503b1cac43089a4c78915eb51af2242244b4158914cef013a49e130
-size 74940
+oid sha256:b756e9fcc25718695f186d0db87b7c23ae44abdd04c42364fc56e62b5b3006c7
+size 74987
