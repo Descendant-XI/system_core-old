72bef0bca1d96984750381c86fedb56cc63b5155
Wei Wang <wvw@google.com>
2018-10-09 12:42:06 -0700

    init: increase thermal shutdown timeout to 3s
    1) increase thermal shutdown timeout to 3s for process to save work
    2) respect property "ro.build.shutdown_timeout" in thermal shutdown if
       it is set less than default time - "3s"
    
    Bug: 112432890
    Test: Build
    Change-Id: Idc2b24dd44c1fab8f9b047fd2468de2ee45ff783
    Merged-In: Idc2b24dd44c1fab8f9b047fd2468de2ee45ff783
    (cherry picked from commit b5de088262b735267288bbdd3f4430f18f281967)

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba88f7491867dbc98f9fd6cae3da4ec8a3d56c9c70e02fda39c2c5c0a90f98c
-size 20382
+oid sha256:adc4545f7461410ade0cc9fc5c51b4c6b12a7773cb73cb6a7d2822bfcf405364
+size 20387
