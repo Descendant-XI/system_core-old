2751578415e568d524ea84010a8933e1119e112f
Srinivas Paladugu <srnvs@google.com>
2018-10-09 14:21:10 -0700

    lmkd: increase the soft limit for keyboard
    lmkd sets the soft limit parameters for Go devices.
    The limit for apps in the perceptible group is set to 16M.
    However this limit is not sufficient for the keyboard app to
    prevent pages from being re-claimed quickly. The mem usage of
    the keyboard app is around 55M most cases with some occasional
    spikes to 70-80M. Increasing the limit to 64M improves the warm
    startup latency for keyboard. It is still lower than the limits
    set for foreground and visible apps.
    
    Test: Go device (1G)
    Bug: 117517805
    Change-Id: Id50e49327cfd76126e41ef6503971845f29196af

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9259cf136822f12f187b9acb16899913ed67bb54696e7f4bbb33c0b4d4e4d6c
+oid sha256:1c6ab9cd07a3e736e75228f48f50e9c4ca66cec01e8034fc18773e5e01b304dd
 size 51693
