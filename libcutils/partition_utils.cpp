313899fa3fb4712298dc169861fadc279d52a295
bohu <bohu@google.com>
2018-10-09 06:06:56 -0700

    fs_mgr: fix incorrect parameter type
    BUG: 117426573
    Change-Id: Iffbdd3763dd19aa527bf805918a566477d122cfc

--- libcutils/partition_utils.cpp
+++ libcutils/partition_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:435fa7f116b733a182c6c64cda9ffb630fa45bf8ca84d43fc17c83800e7c021f
-size 1525
+oid sha256:b6411d8f55f58303ec87c002c3c14fbc25ff3c775ce13452dbaf36dddd09001b
+size 1534
