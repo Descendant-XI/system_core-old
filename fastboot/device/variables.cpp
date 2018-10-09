ea9fb2c83cf447ebae2f8341dc11da5acf3a97bf
Hridya Valsaraju <hridya@google.com>
2018-10-09 10:40:35 -0700

    Check validity of partition for getvar:partition-type
    Test: fastboot getvar partition-type:product_services_a
    Bug: 79480454
    
    Change-Id: I4020b0c94daf8fb86c29104aecc1eb8f44f89999

--- fastboot/device/variables.cpp
+++ fastboot/device/variables.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc08312a63f00a5e75097fb336689286536e999d65b3470362e28bf432229d11
-size 13354
+oid sha256:63ae40e59bc18dfef8e6eef7626a854250ba10e93bdff251befea911bde3dbc0
+size 13551
