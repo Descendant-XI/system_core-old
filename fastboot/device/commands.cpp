6cab693f26faadcacbfdca40f2810a461fdf72a5
Hridya Valsaraju <hridya@google.com>
2018-10-10 11:02:19 -0700

    set_active command should update current slot information.
    Boot control HAL implementations will return the current boot
    slot as the active slot. If a set_active command is issued on another
    slot, it should be considered as the new active slot and subsequent
    flashes should flash the same.
    
    Test: fastboot set_active "b", fastboot getvar current-slot
    Bug: 78793464
    Change-Id: Ida3817670de8e74a7d7ae2a905e7ac1756c6bdf1

--- fastboot/device/commands.cpp
+++ fastboot/device/commands.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c22188e717ca11b5744edf27be6fcc1f076a1c79d5796d6118b58da718394e4
-size 16377
+oid sha256:a3d8d8dac7d301e0428a9f6022ff563e8138afa88b5f8eb4b76fb162b4539e99
+size 16591
