1f5ab7f013b6faeb17a22b4d85d26a87e90c749c
Josh Gao <jmgao@google.com>
2018-10-11 13:49:24 -0700

    adb: extract helper for dumping a packet header.
    Test: mma
    Change-Id: I3e15296eb917d9df11ca13591d26d3aa54d66412

--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f434f0947cd61090851c68c094266ffe73882420cbb17a70ec0e0e012766e3bb
-size 42546
+oid sha256:0abe34cf16dca6e987cb03a5abae1f8dede104f47723ace20d043d3cee963dcd
+size 41397
