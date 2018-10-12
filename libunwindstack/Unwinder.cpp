ae184d48088b23bf644c7e69f51b98276f8d0d10
Christopher Ferris <cferris@google.com>
2018-10-12 16:33:42 -0700

    Fix problem adding too many frames.
    When adding a frame with a dex pc, two frames will be added total. However,
    if there is only enough room for a single frame, two get added any way.
    
    Only add a single frame in this case, and add a unit test for this case.
    
    Test: Passes unit tests.
    Change-Id: If320584b126967a042c623d8fdf3f51dbc1c2251

--- libunwindstack/Unwinder.cpp
+++ libunwindstack/Unwinder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:025c016df59d753844bebf60abdb8c249e251e46e61761deb74cfe9f52d76ac8
-size 9624
+oid sha256:4fea1b74cc86b1a41497824d69beb21193fcbd073cb2b9e6903140ea9a75a07f
+size 9815
