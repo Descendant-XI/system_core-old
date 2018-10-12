c1b37f76f1a465dd14e5a33e28e4a5162aaab223
Suren Baghdasaryan <surenb@google.com>
2018-10-12 11:28:33 -0700

    lmkd: Fix an invalid access to a pointer after it's freed
    pid_remove() frees a structure representing registered process and the
    pointer can't be used anymore. This change fixes an instance when pointer
    was used after it was freed. pid_remove() is moved to the end of the
    function and comments are added to prevent similar situation in the future.
    
    Bug: 117625315
    
    Change-Id: I6a922952a31232497b3f9caf87d5a21bd402db94
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9259cf136822f12f187b9acb16899913ed67bb54696e7f4bbb33c0b4d4e4d6c
-size 51693
+oid sha256:06c773d124cd1d5ed1dec4f64029fd88fc045d2fc784984162a30f4954dae0b1
+size 51938
