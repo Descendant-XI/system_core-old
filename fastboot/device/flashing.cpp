aa43f59c2f1ccfbe88be81e0f0d05e130b383c03
Mark Salyzyn <salyzyn@google.com>
2018-10-12 09:33:44 -0700

    fastboot: wipe overlayfs for partition
    Arrange to delete the overlayfs backing when a specified partition
    has been flashed.
    
    Test: manual
    Bug: 109821005
    Bug: 117605276
    Change-Id: I1c6a0341c6cd2ecfbb7c71bec5679a74d579aadd

--- fastboot/device/flashing.cpp
+++ fastboot/device/flashing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e093bb692ca3bb58993c04894cb7979a0de3a252554ffce75abab740f73bb82
-size 6168
+oid sha256:b0e57cc83af2683f1eb8a183d7499762861393ec0cd85f26b279516e185bf2bd
+size 7407
