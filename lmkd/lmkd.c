5bae7a6db8b20deefe5f8be0b487325672dc03b2
Suren Baghdasaryan <surenb@google.com>
2018-10-12 11:07:40 -0700

    lmkd: Add command to get number of kills
    Intrduce LMK_GETKILLCNT command for ActivityManager to get the number of
    kills from lmkd.
    
    Bug: 117126077
    Test: used lmkd_unit_test to verify correct reporting
    Change-Id: I09c720a7176b4df95efc544177cd2694f8d791be
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2477ba7b216ea98750a236d4b316b909b1f46c8393f1968e53a720b46a399d7
-size 52624
+oid sha256:0ba780f677764a6c9dc3b8f36c8fd8d1eb46c6393f87ed889ec3ed322d19371e
+size 55528
