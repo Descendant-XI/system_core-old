5bae0712972a6d246bc0c1b2a9626b63bbdd97c1
Tom Cherry <tomcherry@google.com>
2018-10-15 17:21:48 -0700

    init: clarify some text/README about 'critical' services
    'Critical' services have rebooted into bootloader, like all other
    catastrophic init crashes, for years now.  Update the text to match.
    
    Test: n/a
    Change-Id: Icfc41bf3e383958f14ecfaab9ca187e2c3dc7fd9

--- init/service.h
+++ init/service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8344f6494240cba3dbb4f1ea6bc1526e7f91e248a7082906742dc6cd68fba176
-size 12128
+oid sha256:40b2701e8165063c659daa4a0af72379a37e1283d2b89efe5c4500d4c2a1cebc
+size 12130
