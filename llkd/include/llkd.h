b376f476c5dfacff187f289b1006b7d71da8f836
Mark Salyzyn <salyzyn@google.com>
2018-10-18 14:39:27 -0700

    llkd: add bit_wait_io to stack monitoring
    This will discover if the I/O is starved.
    
    Add the ability to search for " <symbol>.cfi+0x".
    
    Cleaned up README.md to reflect current defaults.
    
    Test: none
    Bug: 113648929
    Change-Id: I990a54f99de536406fd752a490e60f962380d71a

--- llkd/include/llkd.h
+++ llkd/include/llkd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72bf9230f2b511d7429a22c0dee1168f6a4d6bdf300b7a80c8f3fc450bec5cef
-size 3419
+oid sha256:b2a14d075027a2f12c5e66c739578842af5cd7f8821ee50d6d823114e0e86eac
+size 3431
