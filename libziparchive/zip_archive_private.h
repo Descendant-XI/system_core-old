51493313f753450f072d7c272c8cacf608321822
Elliott Hughes <enh@google.com>
2018-10-19 16:09:39 -0700

    Add a simple MappedFile to libbase and switch fastboot and libziparchive over.
    This allows us to remove libziparchive's dependency on libutils.
    
    Bug: http://b/79112958
    Test: ran libbase and libziparchive tests, ran fastboot manually
    Change-Id: I95c651976dad222863e5b8c37d4514b778f5dce7

--- libziparchive/zip_archive_private.h
+++ libziparchive/zip_archive_private.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76057cb501fdc1ef77af039c287dc364daa00510a6be120456d7a420ff095481
-size 5647
+oid sha256:5b26dfededa0b4894583dd792071671205b7184ffe650effa04a872f27cf0b2b
+size 5482
