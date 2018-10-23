210b9f0e803259752f0ceccd065309f56dabaf7a
Christopher Ferris <cferris@google.com>
2018-10-23 12:04:26 -0700

    Always init gnu_debugdata interface if it exists.
    Every real call always sets the value to true to init the gnu_debugdata
    interface, so remove this parameter.
    
    Test: Builds, unit tests pass.
    Change-Id: I07fb3adbbd5b65b51c0dc7608561e820a5095051

--- libunwindstack/Unwinder.cpp
+++ libunwindstack/Unwinder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fea1b74cc86b1a41497824d69beb21193fcbd073cb2b9e6903140ea9a75a07f
-size 9815
+oid sha256:f718af4f7342b82204e7d5646068775f0ee790a53a7c5e368655a92ba3ae6ad5
+size 9809
