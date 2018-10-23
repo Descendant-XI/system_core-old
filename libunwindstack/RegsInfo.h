cd1112529e249555458b3132b9e38fd4f8589e69
Florian Mayer <fmayer@google.com>
2018-10-23 14:09:48 +0100

    Fix off by one error.
    Change-Id: I18a2d177f3212626b7091150f42dfe151e47570e

--- libunwindstack/RegsInfo.h
+++ libunwindstack/RegsInfo.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:120fbef6f2facf9cc3444bf3efdb596da3674abec72a54b00369e9e4ddb2f144
-size 1859
+oid sha256:45c587a952c114c9f8fe01b545e30e81d3b260ebebe3e24d66484c2c41142512
+size 1860
--- libunwindstack/tests/RegsInfoTest.cpp
+++ libunwindstack/tests/RegsInfoTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a95862cbe28868026539af5cbf1dc66abafa9a1e4b52722a1cd2fff78f5bd278
-size 2409
+oid sha256:bb5e32ae546359a8f5f2a88fd8c99fdb2458b9ba0d83a077606f6c810a4eb43d
+size 2587
