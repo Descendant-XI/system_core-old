a213a99d8b13b54363d8442f0ad2529033d447b7
Elliott Hughes <enh@google.com>
2018-10-24 14:06:45 -0700

    libbase: add O_CLOEXEC to <android-base/file.h>.
    We already expose O_BINARY, and O_CLOEXEC seems equally legitimate.
    
    Bug: N/A
    Test: builds
    Change-Id: I7f07e1bb2a5d6b5c5f293783c39ceab148fabefb

--- base/file.cpp
+++ base/file.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46f5f78b512614b3f476c792631f492d30e8c9bef2847ee4dd5dc9473cd54f50
-size 10830
+oid sha256:9e8aa0c5e3e36da7bf5374dd1720d9e62dd126da41b7e6cd793b8d586ea71097
+size 10800
