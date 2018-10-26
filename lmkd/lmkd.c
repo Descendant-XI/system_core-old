dcbd06a1039904250ef11adf2081dd65f7e7a376
Tim Murray <timmurray@google.com>
2018-10-25 17:05:41 -0700

    DO NOT MERGE: lmkd: retune rate at which processes are killed
    Kill a single process at a time and try to wait up to 100ms for
    that process to reclaim memory before triggering another kill.
    
    Test: boots, works
    bug: 116877958
    Change-Id: I6775d0534b3e3728c04389d3eae1a00e3cbf9f27
    (cherry picked from commit afb3a15f39e73978c071a58d3466993293c6d3b1)

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d95e9f769a274ae95f894ae822ad1a74402f047977e6c168b3c6346548fbda35
-size 45130
+oid sha256:cfaf18fe6042c6683bb0aa03d4611b971bc9b775d4ab1bfc0757811b5b5a9443
+size 45765
