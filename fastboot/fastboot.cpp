0839adc5870cb45b1a27367b899fec0aa8d29871
Elliott Hughes <enh@google.com>
2018-10-26 10:34:53 -0700

    More Mac build fixes.
    The libziparchive public headers that refer to `off64_t` also need the
    Mac workaround.
    
    In fastboot, there's a stray `lseek64` but since it's only for offset 0,
    any kind of seek is fine.
    
    Bug: N/A
    Test: builds
    Change-Id: I68b4f95202623ebf07ffe6c3e0e21437e7922c5b

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0dd469123eedab80c17e174204bb0556133d67812635c76da44ef9c2631b8b9
-size 69548
+oid sha256:4ca19ee90b489770d5cf7e6cd8815563f2987b69f0f21f1f3c460a5f631cdc9a
+size 69546
