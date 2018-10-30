0f709981763235159192bf4ef747c7916bbfdb27
Tom Cherry <tomcherry@google.com>
2018-10-29 17:08:23 -0700

    fs_mgr: fix userdebug/user argument type mismatch
    Test: build
    Change-Id: I394bd14ef987d3b763869e05fdbf1ed32153ce59

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59748a527f9888eaee2ea5a48aaf2e92bced0c446cf207e5c1aa712eccd81a9
-size 32560
+oid sha256:d91f8766fd99719e8081d39199ee7da9dbe02d0d8d966e320f4f8b3672ef64ef
+size 32548
