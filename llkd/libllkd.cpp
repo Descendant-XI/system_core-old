c53c39c69ab14fd0a352a53a36c7ec5a4f9d4fe9
Mark Salyzyn <salyzyn@google.com>
2018-10-31 10:38:15 -0700

    llkd: stutter pre-panic message to both last kernel and last logcat
    Test: compile
    Bug: 118712403
    Change-Id: I9067b9335b2685169bcf8b1dc0248f7ff4315046

--- llkd/libllkd.cpp
+++ llkd/libllkd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff7a3f16020aaaaae62d2ad2a3d8fb3a5a24882a67d4bf5d6e5d7baa21219991
-size 50575
+oid sha256:3966d7e98fd7cdd994d7d51673093f5b02f636938d2ca211426fed36e6c13f1d
+size 51071
