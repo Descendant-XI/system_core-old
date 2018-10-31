1742212d557b9e4d7cafac2031bef1b8d744b8a5
Mark Salyzyn <salyzyn@google.com>
2018-10-31 16:03:45 -0700

    llkd: Add waiting task to dump
    Consider reporting d (lock dump) and w (waiting tasks) after
    t (stack dump) as the ramoops buffer could be overflowed.
    
    Test: compile
    Bug: 118712403
    Change-Id: I64fac7e13c14a1cbc45c9e35fe7746f9b778dcf4

--- llkd/libllkd.cpp
+++ llkd/libllkd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a2e75a7682be8f3218d1a8ba189c954a18efbd7b5c42bfbeecdbfe74e3e6f66
-size 50204
+oid sha256:ff7a3f16020aaaaae62d2ad2a3d8fb3a5a24882a67d4bf5d6e5d7baa21219991
+size 50575
