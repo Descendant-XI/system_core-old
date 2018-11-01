db2a2e11542372523b546ed288524d524c2c59de
Josh Gao <jmgao@google.com>
2018-11-01 12:32:52 -0700

    base: add more error-checking overloads for unique_fd.
    unique_fd::operator int was a mistake...
    
    Bug: http://b/118818285
    Test: treehugger
    Change-Id: I3775dd7b4666abec8b5ce2a43092d9601eb1128f

--- base/include/android-base/unique_fd.h
+++ base/include/android-base/unique_fd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4743148111d5dfa0df12dd725e02a508f26e2270b36592891621f4b44369017c
-size 6441
+oid sha256:f365719669450ccb1c9a1ae6a87e750ba71f3bd3e7f58c0eae61b6f596a59bcc
+size 7019
