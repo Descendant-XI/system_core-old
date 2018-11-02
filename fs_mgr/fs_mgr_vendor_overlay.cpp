0e9b814a5dd8e85a6208d4ba18ee2530fa2cefea
Justin Yun <justinyun@google.com>
2018-11-02 17:45:24 +0900

    Check sepolicy context for mounting vendor overlay
    The vendor overlay source directory must have the same sepolicy
    context with the target vendor directory.
    Otherwise, the vendor overlay will fail to mount.
    
    Bug: 112955896
    Test: Check if a source directory that has a differnt context fails
          to mount on vendor directory.
    Change-Id: I2522a8ddc96d4d9b0f34d25a081d20d1ac2dd27a

--- fs_mgr/fs_mgr_vendor_overlay.cpp
+++ fs_mgr/fs_mgr_vendor_overlay.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59b554453cab7a43f9296bbc44942a248bc9d2e70082181398a168d98efdd39c
-size 4561
+oid sha256:e29b7ad1d4711df0856c21c1f3a788276c93b5545d61db002fc72e5b510d9eef
+size 4864
