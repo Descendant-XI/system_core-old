36edb5dae35b8877f9059105297f56dec82784d1
Mark Salyzyn <salyzyn@google.com>
2018-11-05 08:53:30 -0800

    fs_mgr: overlayfs earlier kernels do not need override_creds=off
    Kernels at or older than 4.6, the overlayfs drivers do not need,
    or support, override_creds=off.  Drop using the mount option.
    
    Commit 3fe6e52f062643676eb4518d68cee3bc1272091b introduced the
    need for override_creds=off, and none of the kernels that contain
    this patch without the patch that introduces override_creds can
    be used with Android.
    
    Test: compile
    Bug: 118882257
    Bug: 109821005
    Change-Id: I832c8ca3fce0269bdef4ce988541adb7ba9662ed

--- fs_mgr/include/fs_mgr_overlayfs.h
+++ fs_mgr/include/fs_mgr_overlayfs.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa11a38ed737d4bb36d9417f65e4c47a935ef697a1637c0d93456429f3bf3eb2
-size 1447
+oid sha256:d3240922e2a63585e10868f5535f76369f7d3316dcca79f03fd791547fb68ed3
+size 1543
