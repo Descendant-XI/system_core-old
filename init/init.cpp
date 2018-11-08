a3cee6008c3170b5e91795f675505fcdd52d2f8e
Jiyong Park <jiyong@google.com>
2018-11-08 17:14:35 +0900

    init parses *.rc files from APEXes
    Init now parses *.rc files from the APEXs when the apexd notifies the
    mount event via apexd.status sysprop.
    
    Bug: 117403679
    Test: m apex.test; adb root; adb push <builtfile> /data/apex; adb reboot
    adb root; adb shell setprop ctl.start apex.test; dmesg shows that init
    tries to start the service which doesn't exist.
    
    [   47.979657] init: Could not ctl.start for 'apex.test': Cannot find '/apex/com.android.example.apex/bin/test': No such file or directory
    
    Change-Id: I3f12355346eeb212eca4de85b6b73257283fa054

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bde93e4841afbea117608d6233b141eb14b5f77485edb7aa03223ac87240c2c9
-size 29007
+oid sha256:6d185db759471a988cd18816cde023677e0648ec02e347d646a9a3e5d76b5969
+size 29251
--- init/parser.h
+++ init/parser.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9af29a0f3fbc92e5e9297bfc043a8edd9a174190237aa0b04e7e8c2722f2410c
+oid sha256:65751032739e8f5a6489737faf9ec258b2968a50c69002a630ff67ca1433ea53
 size 3537
