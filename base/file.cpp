867ea2e0bbbc0723c2a68377c8a0763ec4632821
Mark Salyzyn <salyzyn@google.com>
2018-11-13 13:38:33 -0800

    base: TemporaryDir add DoNotRemove() method
    Recursively delete all contents in a temporary directory in the
    destructor.  If the caller wants the contents preserved, then
    call DoNotRemove().
    
    Test: compile
    Bug: 119313545
    Change-Id: I3601c06334920055a60dd3074b249824f4d6f510

--- base/file.cpp
+++ base/file.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aec66246131fa28da6823129f8c3f6d4b2e3617660fc226c9c144f766aaafd1d
-size 14001
+oid sha256:371a6b250d5ae66ac9d1ecebea6ec70b91699c1a43249dad705194c3f5a1b463
+size 14043
