93b78f0cca6cb52fca3955525bdad97038e654ba
Christopher Ferris <cferris@google.com>
2018-11-15 14:06:26 -0800

    Remove overly restrictive check for memory.
    Remove unit test that is not needed any more.
    
    Slightly reorganize the code around the changed check.
    
    Bug: 77958880
    
    Test: Ran backtrace tests with new linker options.
    Test: Ran libunwindstack unit tests.
    Change-Id: Ie95707323fb51616a093cafb83e0343fa24e7e61

--- libunwindstack/tests/MapInfoGetElfTest.cpp
+++ libunwindstack/tests/MapInfoGetElfTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f810f9c7be4bfdd6eaca8993e2857c782b420aa1a8cd3e1e9b7cda74346d6c5
-size 13238
+oid sha256:0558436093fbe871e85e3ad0d01dccbe57ae9309ff3492a57b431baf8144fc98
+size 12605
