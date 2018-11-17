be4e1f51ee95391eda30b41776274755a9738304
Lingfeng Yang <lfy@google.com>
2018-11-16 22:47:31 -0800

    Add mDNS services for pairing and connect
    - ADB Secure Pairing
    - ADB Secure Connect
    
    Nothing else is implemented.
    
    Bug: 111434128, 119490749
    
    Test: N/A
    Exempt-From-Owner-Approval: already approved
    Change-Id: I2e7873b62a3c7631451e47f6a301f8c4a5ffa2e2

--- adb/adb_mdns.h
+++ adb/adb_mdns.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93d0888ffec2590dad133d5de9d447667ff3f958794e07761f1c50891d92716e
-size 715
+oid sha256:453536c4ceca93d129e82a55d4fdfc317dfe421c7ca02dc969f2b5da43472de1
+size 1090
