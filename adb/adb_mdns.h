0a3a958a0e51106220356d644f899d120ca3a216
Lingfeng Yang <lfy@google.com>
2018-11-17 10:14:29 -0800

    make the client browse for appropriate mdns services
    This CL makes it so the client looks for the adb secure pairing
    and adb secure connect services. Nothing else should happen,
    but this should be useful to see if the right packet traffic for
    discoverability is happening.
    
    Bug: 111434128, 119490749
    
    Test: N/A
    Exempt-From-Owner-Approval: already approved
    Change-Id: I266bdb8526cf39bbfa131344dca2b1bb14c14a7b

--- adb/adb_mdns.h
+++ adb/adb_mdns.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:453536c4ceca93d129e82a55d4fdfc317dfe421c7ca02dc969f2b5da43472de1
-size 1090
+oid sha256:d1cca0c8329a2bea4ec1c203d2c17b6fac2e0d69b89da320ce0bc85a44650427
+size 1134
--- adb/client/transport_mdns.cpp
+++ adb/client/transport_mdns.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95f54b2af025e87deaf8718cce1ebfca399bab16a8c559951d22d07620a1b7fc
-size 10182
+oid sha256:aba44f3fb1119dcd0fea03c2dce78c3e3e3dac355f9507b21b362eb9f402cbe5
+size 10295
