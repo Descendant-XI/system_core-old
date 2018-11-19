b56b7fcb1aec2a1a959dd3dc5d69e759a6889ece
Mark Salyzyn <salyzyn@google.com>
2018-11-19 11:17:35 -0800

    fs_config: add product and product_services to the scanning.
    Test: compile
    Bug: 119310326
    Change-Id: I67ba155e4a03731d402cdfcf06ca80ad0c4ab2ed

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb2f779a547ec256fabf947c5733eddcc93617e7988e0b98ab06932909fd54a
-size 16524
+oid sha256:7c1119018e936ba5cb6a495eac2b98d789f9962ff75e1b9371501d7dd05e871e
+size 17370
