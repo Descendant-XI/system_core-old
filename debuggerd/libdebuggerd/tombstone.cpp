5094829de620e6581fbb6e88119a13f5fddea014
Ivan Lozano <ivanlozano@google.com>
2018-11-19 10:43:47 -0800

    Tombstone support for XOM-related SIGSEGVs.
    Make XOM related crashes a little less mysterious by adding an abort
    cause explaining the crash.
    
    Bug: 77958880
    Test: Abort cause in tombstone for a XOM-related crash.
    
    Change-Id: I7af1bc251d9823bc755ad98d8b3b87c12bbaecba

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e84da1edba4ee8d4d6dae30ca36c2080b9d4c0316b72c33dca548e9675b0548
-size 22505
+oid sha256:9769722a4b4d60dc529377b84235a4e0350a31201b902e8640d97853a3fd767e
+size 22965
