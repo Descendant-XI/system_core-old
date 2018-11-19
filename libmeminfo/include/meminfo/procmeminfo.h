55e5be8b3bcf8ee4668706e1b0f132cde9143640
Sandeep Patil <sspatil@google.com>
2018-11-19 11:38:40 -0800

    libmeminfo: Add support to fiter accounting based on page flags
    Bug: 114325007
    Bug: 111694435
    Test: libmeminfo_test 1 --gtest_filter=ValidateProcMemInfoFlags.*
    
    Change-Id: Ifa7006de78dd909b5b4db282a8c9931ebf97a68a
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- libmeminfo/include/meminfo/procmeminfo.h
+++ libmeminfo/include/meminfo/procmeminfo.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c3296eaefd6f36a69a3cae3a4bd9d5ce2a948f9fec72531605eb8723db275a5
-size 1388
+oid sha256:07686f601651c68b5a132ad012bdc8446dac195ab011481c4a2f0e3c29c43ccb
+size 1488
--- libmeminfo/procmeminfo.cpp
+++ libmeminfo/procmeminfo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93d27f149f519a0692a882d1938dd41eb05d62ec9db4875c65137d6783e2aa9f
-size 7252
+oid sha256:d1b313799d59914490730706daca53c7deef102f367fd499002d15a1b073dfdb
+size 7457
