a44fc6afd74e90916c1f77780789ee35fb7224b2
Jaegeuk Kim <jaegeuk@google.com>
2018-11-21 12:55:19 -0800

    make_f2fs: use -g android by default
    Bug: 119875846
    Change-Id: I0c6d642d474df5cc678ced9ec4c04027ee51c6d1
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- fastboot/fs.cpp
+++ fastboot/fs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5e9b992eff5037cbdf614e640710d190d23937da07ce6b7094fad2314b688cd
-size 7652
+oid sha256:25edf8ca4f10d8b1510bf19c1b1fb94aaf061b51289690f89661f0fd8631cb4b
+size 7480
