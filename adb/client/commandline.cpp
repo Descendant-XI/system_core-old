5e29a50caa90b52a90b431146154d4b949b2c294
David Anderson <dvander@google.com>
2018-11-21 13:43:22 -0800

    adbd: Automatically disable verity in adb remount.
    Before overlayfs, we supported deduplicated filesystems by undoing
    deduplication in recovery. This required an extra reboot cycle, so we
    changed "adb remount" to disable verity and boot to recovery in one
    command.
    
    After overlayfs, adb remount is still trying to undo deduplication,
    which leads to very confusing messages. This patch makes things a bit
    clearer. "adb remount" will disable verity, which installs overlayfs.
    "adb remount -R" will do the same except automatically reboot.
    
    Bug: N/A
    Test: adb remount on dynamic partitions device
    Change-Id: Id72f6b9e2297c2f4d5722d5679f6264fe660e631

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc2e61ad458037780e001466149df302f059f92613245b76ce65453fd99856e2
-size 66868
+oid sha256:7c4374cd6308522eaddd2e7bdd2db99d9272246bcb08f2bdcffecb902934bec0
+size 66964
--- adb/daemon/remount_service.cpp
+++ adb/daemon/remount_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d4907363ea8f29d3e5878cba62487b723015d91de300ecc6ffd7f0d14da48a3
-size 11799
+oid sha256:c1407624c7772cac75ccaf6c93b9ea99ec04a8e2904eb22ccc3b635a1d04f466
+size 11569
