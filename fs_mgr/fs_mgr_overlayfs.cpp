7a1e418d93540ca4425582a29ee7cbbbbb9478c9
Mark Salyzyn <salyzyn@google.com>
2018-11-26 09:57:17 -0800

    fs_mgr: overlay: disable overrides in recovery or fastbootd.
    Disable overrides if we are booting up in recovery or fastbootd.
    Check for existence of /system/bin/recovery.
    
    Test: adb-remount-test.sh
    Bug: 120034852
    Change-Id: I946eb605300226d31356ecac209d6367f4e13526

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92d87bce3b6866ab1629c4bbba05e90096a71885da4e3fc7ddb81ea2e079bfd1
-size 35090
+oid sha256:7edb2b374995f18e0bf80e9c7e3823d22287cebb40629e675277b6a4ec8a6d44
+size 35242
