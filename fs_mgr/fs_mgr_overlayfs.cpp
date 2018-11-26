68d13134d955a999bb513ffb0a863428aac6ef03
Mark Salyzyn <salyzyn@google.com>
2018-11-26 09:57:17 -0800

    fs_mgr: overlay: umount scratch more aggressively
    For specific teardown operations, if we mount scratch with the
    intention of it being temporary to strike out content for a specific
    partition, umount it.  Otherwise it could interfer with fastboot.
    
    Test: adb-remount-test.sh
    Bug: 120034852
    Change-Id: Ieff3f6ea0bcda5eb540e9acf6cab8a56d1453b3e

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:585411a7ff1433d5d51980f57556088111d2ebae81d9bd89a6a55ce272660e4a
-size 34842
+oid sha256:92d87bce3b6866ab1629c4bbba05e90096a71885da4e3fc7ddb81ea2e079bfd1
+size 35090
