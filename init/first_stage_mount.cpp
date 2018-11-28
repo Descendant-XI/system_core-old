7b8a73aa1d5e73edbdeec7d3428926821c40526a
Alistair Strachan <astrachan@google.com>
2018-11-27 17:24:58 -0800

    Skip logical partitions in non-AVB2 case too
    Follow up to the change made for AVB2 devices in
    I19371b05912240480dc50864a2c04131258a7103.
    
    The same consideration must be made in the fall through case, which
    is taken either if AVB is completely disabled, or the dm-verity / AVB1
    mechanism is used.
    
    Bug: 113175337
    Test: boot test on cuttlefish
    Change-Id: I99d46a2c2630c40f5f5c02279b11e423998a1e05

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b1e3aec43d210107c8082b331a9ff38654c016912280b9f5fda044abd24c27b
-size 22294
+oid sha256:01b6b0d411b914418b9b86d87994f8f355ea518ccb3862c3f7764e0d7c8775dc
+size 22353
