e5be5838c5b66422004402c02ce6cd956b068c4c
Alistair Strachan <astrachan@google.com>
2018-11-27 17:39:00 -0800

    Handle renamed "super" device correctly
    In the non-retrofit case, the metadata source partition will be
    hardcoded as "super", but such a partition may not exist or may have
    been overridden by super_partition=<something>. Make this work reliably
    by plumbing the resolved metadata block device through, and using it any
    time a partition called "super" is requested.
    
    This also fixes a case in the first_stage_mount code that assumed that
    detected a non-A/B, non-retrofit case by assuming the metadata source
    partition would be called "super".
    
    Bug: 113175337
    Test: boot test on cuttlefish
    Change-Id: I8ce1defb902e257eeb7170d697b378eba31faee3

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b76bd8524f4c2e067bc0e259f2ac83c12b50b122f64ae1cf0a67a8288cef4f87
-size 22391
+oid sha256:682a5a061183637994f4fca9af80b8a1f3452a13da297e3ae3929adf9a957330
+size 22559
