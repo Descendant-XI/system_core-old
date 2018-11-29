c79610594751f9ff923b416bce74617fb3df551a
Tom Cherry <tomcherry@google.com>
2018-11-29 13:24:09 -0800

    fs_mgr: fixup 'size' attributes of fstab.
    'off64_t' is the correct way to specify a size of a file system, so we
    update the various fs_mgr fstab attributes accordingly.
    
    Test: boot
    Change-Id: I07ebe687b7c215a8d07ce49d6d32e81b798d1cd3

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66a766eca9c88fd1e1221a240447ceacb40e76ba249809a1f4911e7d4a1f4c0e
-size 58198
+oid sha256:51ab40299af97fab4689965ffabb78958a75e277c7905981c295cb528b8cbec9
+size 58229
