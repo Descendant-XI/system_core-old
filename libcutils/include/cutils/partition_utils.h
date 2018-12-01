903ace0428ae3cb528ec2552a4189d5223699752
Tom Cherry <tomcherry@google.com>
2018-11-30 16:16:05 -0800

    Start using new C++ Fstab class widely
    Bug: 62292478
    Test: boot
    Test: adb-remount-test.sh
    
    Change-Id: Id4715af4c1f03e2cfc67de92d3ea58e933685e51

--- libcutils/include/cutils/partition_utils.h
+++ libcutils/include/cutils/partition_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2389b64239d5acbbbb249d6d13b85047e94199287a9fc068ddaad76cbdb8ca6
-size 822
+oid sha256:3089d05638cf8a30681d8419e99ce52da1b3a533ccae9cd2bc16a831cc034d5a
+size 828
