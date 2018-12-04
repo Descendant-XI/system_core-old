23afbfdb80de25d071dcd346a08bd3d7bb2d4e28
Mark Salyzyn <salyzyn@google.com>
2018-12-04 10:30:45 -0800

    llkd: make 100% sure process that triggers panic still exists
    There is time between inspection, filtration, determination and
    dumping before triggering panic, make 100% sure the process still
    exists.  If we had one false start, but another process triggers
    and panic in the same pass, then recognize that we have already
    dumped the data and skip it on the later ones.
    
    Test: llkd unit test.
    Bug: 33808187
    Bug: 120378563
    Bug: 120229612
    Change-Id: Iacaf82a3d58e5a3c18edcff3c8fa540b21da36f1

--- llkd/libllkd.cpp
+++ llkd/libllkd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:198b4e6c3466fa1059884655274b1084a5c30d8acd737032723ee0cce4d864eb
-size 51115
+oid sha256:e91da963f9f7fbbc0b43d31b75342dd0c8ed32836f98394bad81a8b13570b736
+size 51336
