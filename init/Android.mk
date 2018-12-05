825caca83e719b66cadea73884a4d2f06f1a698c
Tom Cherry <tomcherry@google.com>
2018-12-05 14:54:26 -0800

    Fix /init -> /system/bin/init symlink creation race
    A symlink for TARGET_ROOT_OUT as a post install command of a package
    that is also installed to TARGET_ROOT_OUT.  We hijack init.rc which
    satisfies this requirement for this symlink.
    
    Bug: 120402274
    Bug: 120460755
    Bug: 120509320
    Bug: 120554662
    Test: symlink is created
    Test: make bootimage && ls -la $OUT/root/init
    Change-Id: I6f1ac06ef152c36d7d7db4618d49a008338da39b
    (cherry picked from commit c12e205ec52bc03b907bfeb3adc64f6745f6e556)

--- init/Android.mk
+++ init/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b43d61a91fa978a9f6d1e6fbb8309926e9f050887d5e7d94311a123fa554131a
-size 2661
+oid sha256:3ce8b90e67c1237a743a3514f65d45c192ced3111cdcc9d3c1daf8cff53a420b
+size 2587
