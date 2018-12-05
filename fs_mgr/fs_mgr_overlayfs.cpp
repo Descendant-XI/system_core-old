3da22f69fe6d34f0c43db6cf3ae28cd638044255
Mark Salyzyn <salyzyn@google.com>
2018-12-05 10:45:06 -0800

    fs_mgr: overlay: split out fs_mgr_overlayfs_create_scratch()
    Reduce some complexity in fs_mgr_overlayfs_setup_scratch()
    
    Test: adb-remount-test.sh
    Bug: 119885423
    Change-Id: Idb769991f6342739c75654b1d42ab8f8633cbadf

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df4b3719e7a59c21c2f98befd9313447801cd13b1b6563e6c4dd9d138d90344
-size 36818
+oid sha256:a6d93916f0669a1928842581898b8cf93e3c31cff454cca6ede1aa9a6bc99f5f
+size 37221
