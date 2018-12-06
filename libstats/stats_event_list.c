974513d01deccd16dec14a8950f03ba7a2ac904f
Yao Chen <yaochen@google.com>
2018-12-06 13:03:12 -0800

    Add a function to pass byte array to the log context.
    The existing APIs will truncate the byte array in case of '\0' in between.
    
    Test: test_drive with atom 103
    Change-Id: I104d35b330c6b52e2fdce1f5d4305dca08228f1b

--- libstats/stats_event_list.c
+++ libstats/stats_event_list.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ab188365a14692adf16332695aad6f5abc6835ddd321be9cf92ab9b77d36702
-size 5611
+oid sha256:b7edec6e250766cc16f8086a0fc457e278055847e7d9499ec04847684ed0da82
+size 7028
