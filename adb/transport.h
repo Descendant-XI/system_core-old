f0d70de0404faf33f898af692b040d847f22d445
Josh Gao <jmgao@google.com>
2018-12-11 13:11:52 -0800

    adb: remove incorrect use of RTTI.
    We were dynamic_casting to UsbConnection to check for USB connections,
    but the actual type was a BlockingConnectionAdapter wrapping a
    UsbConnection, with the result that unplugging an inaccessible (due to
    permissions) device on Linux wouldn't make the device go away.
    
    Test: manual
    Change-Id: Icb4acea5fd3c3baa9691698686213e122e898e4a

--- adb/transport.h
+++ adb/transport.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8df95b545634281aef0143915f0d27e2450f45274c696b9e155f1fde318b669b
-size 13326
+oid sha256:08c98392e458692186df77bf96fc78641c613cdcb6aefb9dcdc79ce1787b0455
+size 13549
