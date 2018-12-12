97db601186c4d74615aa6f59ab1f70f56ee5b7cb
Mark Salyzyn <salyzyn@google.com>
2018-12-12 09:19:49 -0800

    llkd: check stack for wait_on_page_bit_killable
    User process in S state blocked by deadlock in I/O system
    
    wait_on_page_bit is covered by regular D state tracking.
    
    Bug: 120776455
    Test: long term stability on multiple devices
    Change-Id: Icdb99b8095f384cb440f0f2bdeba86c5991b9ef4

--- llkd/include/llkd.h
+++ llkd/include/llkd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53953e1ad04a2ff39c71f27d8eb258e5a6cd9795fb37c0addc0dbfb034eab355
-size 3662
+oid sha256:82d618f6f13c4dcddb9827fff615a65fd7fbd530ae972a09ca4f3b0d6ac7916e
+size 3694
