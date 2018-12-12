4062d307cb322a913b5d9aa3bf20fb042d42307e
Tom Cherry <tomcherry@google.com>
2018-12-12 10:30:10 -0800

    fs_mgr: remove -Wno-unused-variable from cflags
    Don't know why this was ever specified, but seems reasonable to remove
    it now that we're so close.
    
    Test: build
    Change-Id: Ia8d056cd1c9660b3c22531317098ace78e661d6a

--- fs_mgr/fs_mgr_slotselect.cpp
+++ fs_mgr/fs_mgr_slotselect.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35594a9b666fee3d125a2305dd62fc8f3ff1986ba466eae64def20588ccc4b90
-size 1658
+oid sha256:14b3c11b4d482dc21c69bfa388d4c68a413181e6a196dc2cc75dc81d22895894
+size 1647
--- fs_mgr/liblp/writer.cpp
+++ fs_mgr/liblp/writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea95f4d5a17bf67278cb0257f922b8fe36194c6e0c6a79e9846a12b28421d92d
-size 16828
+oid sha256:341ba444211a0b456de2bccc039137f641cb18ddfaa8610aa6a923631b8b95d2
+size 16774
