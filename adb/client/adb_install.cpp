5aea0f9aa5b61e6ff495aca356daf504ad19b1eb
Henry Daitx <daitx@google.com>
2018-12-12 10:40:57 +0000

    Change --fastdeploy behaviour
    --fastdeploy does not require -r anymore, and reverts to a normal install
    if the application is not already on the device.
    
    Bug: 120828611
    
    Test: mm -j72
    Test: adb install --fastdeploy --force-agent --local-agent /mnt/raid/boat-attack-apk/boat-attack-swappy.apk
    Change-Id: Ice2a71493a34ee7d0debabcce6a9aebb0af79e62

--- adb/client/adb_install.cpp
+++ adb/client/adb_install.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dcefede816275caf245e5cc4de7ed4958938b992a0c5e8320c80a8226b0bfe6
-size 17261
+oid sha256:c93c9ff652b428921e64c2bc0d0216a280adce09173a042cb91e48256efec3a0
+size 17352
--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7097535161a9c3b8de8acf32b75b31dd55d22adb26153f0dbc3498faeb1847f7
-size 65966
+oid sha256:18b1dfa3a32c67fe0af90050528844cbb37319098e0e4c35b797fa83234a1bb7
+size 65924
