ea641f4956433abcaaa73bc200a00a790b9e2cb0
Christopher Ferris <cferris@google.com>
2018-12-13 09:33:45 -0800

    Do not remove speculative frames in all cases.
    If the first frame of an unwind is a totally invalid pc that's not in
    any map, a speculative frame is added. Rather than deleting this frame
    if no more unwinding is possible, leave it. This fixes a case where
    the only frame you get is an invalid one, but the speculative frame
    winds up in a shared library or somewhere else and gets removed.
    
    Bug: 120505086
    
    Test: New unit tests to catch this case pass.
    Test: Verified original crashing program now emits two backtrace lines.
    Change-Id: I088dff21c057386dcdaeb3fc2578b24322683bd0

--- libunwindstack/tests/UnwinderTest.cpp
+++ libunwindstack/tests/UnwinderTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd752adb3972308d7f5df2cbe38bfdef6e9abac90753ca9aebb5c2c52e4a8ff2
-size 38900
+oid sha256:1af5b13069e01650571b6374549b9b77fde8f2b847181e23df6882d2b30e42d7
+size 41131
