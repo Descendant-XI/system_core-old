a87631bbe93d96b3400ec34ae81613db45eaef8a
Josh Gao <jmgao@google.com>
2018-12-13 14:21:00 -0800

    adb: finish switching service creation to string_view.
    Test: test_adb.py
    Test: test_device.py
    Test: $ANDROID_HOST_OUT/nativetest64/adb_test/adb_test
    Test: adb shell /data/nativetest64/adbd_test/adbd_test
    Change-Id: If4ea92aee1c0264d946de72483f8d715d96fcfd8

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:958886969c63613e7839c3ad2772e6e0a7535cc6be45d6bb3452e3aff4f67cd5
-size 46515
+oid sha256:09f5789f1b68cd0b0c843e5f0e3b69ec0b5dcd3997e27e8320e74b9b0e1b4c02
+size 46470
--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00db7761d7637c668cd0d8aa7b3103d2abf66a19547c9a355a37608f62953de6
-size 6959
+oid sha256:b7a05dc0fc418e55c8d54223779c049b093a65b352bcac8e50d4bbbaa4be0eb3
+size 6964
