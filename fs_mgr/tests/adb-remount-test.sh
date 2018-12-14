3cf4fe521540c29fb1bee0d1ec55b620f183b9a5
Mark Salyzyn <salyzyn@google.com>
2018-12-13 16:47:58 -0800

    fs_mgr: overlayfs: test w/o sepolicy in enforced mode
    Skip sepolicy test if device is not in enforcing mode.
    
    Test: adb-remount-test.sh
    Change-Id: I1558674819bda0c9f575d5202f693fec55c9d84f

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b4ea1b7467447ee154052759faf9257d8858af8d29df74e4ef1bee4b261ee3a
-size 20245
+oid sha256:4a94aa4db791f39443f6fef1a05c7b402bb9d87d3049fb6f82327f1ae45fb9d1
+size 20470
