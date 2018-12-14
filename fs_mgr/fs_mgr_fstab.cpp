de646711fe5aad750da1ce6ec5f5e97677ad49b4
Yi Kong <yikong@google.com>
2018-12-14 15:40:08 -0800

    Fix sign-compare warnings
    system/core/fs_mgr/fs_mgr_fstab.cpp:161:18: error: comparison of integers of different signs: 'int' and 'const uint64_t' (aka 'const unsigned long') [-Werror,-Wsign-compare]
            if (flag == j->flag) {
                ~~~~ ^  ~~~~~~~
    
    system/core/fs_mgr/fs_mgr.cpp:674:11: error: comparison of integers of different signs: 'int' and 'std::__1::vector<FstabEntry, std::__1::allocator<FstabEntry> >::size_type' (aka 'unsigned long') [-Werror,-Wsign-compare]
        if (i < fstab.size()) --i;
            ~ ^ ~~~~~~~~~~~~
    
    Test: m checkbuild
    Change-Id: I056add222a92f99134a49ac141bd143ddd54179b

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6efd4938468a97093b74b8eb63dd02ca96a1f8b636d2ad16ca11415f210c098
-size 36717
+oid sha256:cd0216963026ffa6b9bf9e3f34f3174da05c6e5aa04648c85f25435121a8f553
+size 36722
