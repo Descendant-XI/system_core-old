9326958d9241d959cd3764867b40b1b9964cce3c
David Anderson <dvander@google.com>
2018-12-17 17:07:34 -0800

    fastbootd: Only flash slots listed by the boot control HAL.
    Bug: N/A
    Test: flash when metadata slot count is >2
    Change-Id: I67481be0de162cab5da8d32c2e318489427f1932

--- fastboot/device/commands.cpp
+++ fastboot/device/commands.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c69d9ff39b1d45e1857f132e61ee842fd18f087640d8b0d2f7251bfb0c61c5c9
-size 17394
+oid sha256:ba612d485a231d738a502dc9be6f6f79a67dfb40ce37d2ffc526d5f2dfc3ad30
+size 17454
--- fastboot/device/flashing.cpp
+++ fastboot/device/flashing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2444fa3e17ae35f715b1265fff2c3e8023d07b7fd0561561c87650b72fd6db17
-size 6907
+oid sha256:9ba1d89bf34d7fcc76809f72ae19f1b1ccda721027cfcf2bdecc2fe899a1a9fc
+size 6915
