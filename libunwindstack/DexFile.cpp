0ac852c0de65b1fe527b2335e7307deaf49fd789
Martin Stjernholm <mast@google.com>
2018-12-19 13:40:11 +0000

    Revert "Snap for 5189750 from 8051c86223527fb65325f514069bcdc42ce6afd0 to qt-release"
    This partially reverts commit ef04bcfc65f31c18d7abd3aee85d3f84d5d51434.
    
    Reason for revert: Revert change breaking renderscript on marlin and
    sailfish that has already been reverted upstream (aosp/853492).
    
    Bug: 121110092
    Change-Id: I01d9309a5f650c8e5fd5125196cb533ffbc49720
    (cherry picked from commit 381f046c36f4e998c24fe6e6540c893b88554843)

--- libunwindstack/DexFile.cpp
+++ libunwindstack/DexFile.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cffeafc0c86ec6ef62f62bd0fe7a89d88354392eb62680e5f95468a232bcf11b
-size 3352
+oid sha256:9e090bd654520d7bb28bfcafb41554e226da2dbda581cc525f8996f5456e09c1
+size 7329
