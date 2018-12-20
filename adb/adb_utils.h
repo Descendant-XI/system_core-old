d66199630a00d880791cbd31a39cdc90d97a362b
Josh Gao <jmgao@google.com>
2018-12-20 13:17:04 -0800

    adb: add stroll equivalent for string_view.
    Test: $ANDROID_HOST_OUT/nativetest64/adb_test/adb_test
    Test: adb shell /data/nativetest64/adbd_test/adbd_test
    Change-Id: I1d89913474fcd1aa4a856b5e4583a3c1f076ddd4

--- adb/adb_utils.h
+++ adb/adb_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c54859724e7d4a316c376c0010d1a30b7afd46d31e2d9dec108881951becc4b4
-size 2968
+oid sha256:3ad5738100c5ca479104b7c03a269a0d71cfe3bdc6ea60c10d02f7d37e60d4bd
+size 3744
