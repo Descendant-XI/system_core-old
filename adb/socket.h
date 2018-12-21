3593bd28bad34a95d0025c6f970505d833c14419
Josh Gao <jmgao@google.com>
2018-12-20 17:00:13 -0800

    adb: switch connect_to_remote to string_view.
    Test: test_adb.py
    Test: test_device.py
    Test: $ANDROID_HOST_OUT/nativetest64/adb_test/adb_test
    Test: adb shell /data/nativetest64/adbd_test/adbd_test
    Change-Id: Icce121a4c62bf0fa636a35bcae31d057cdff8fd2

--- adb/socket.h
+++ adb/socket.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ae3e9512568c37eb67d6a176a2339b7c46b36ab087bfeba490d3916e33fa4e3
-size 3678
+oid sha256:1b6f65b83cf0b2903945b18387209e3a447c9a6da884a330457278f7f094923a
+size 3683
--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b829aa55e4cea0c5c1a059b3e0037a0c2ed14b85aba8d1c8ff301f2f87977cb
-size 26906
+oid sha256:b5dff37230440679bf5f91d85170ed2734c2cee7d51044b2dd0ea40d8d15f203
+size 27100
