e3bfcbb847784f4cd0a6425b0fe44db0b722b35d
Suren Baghdasaryan <surenb@google.com>
2018-12-21 11:41:50 -0800

    libprocessgroup: Add support for task profiles
    Abstract usage of cgroups into task profiles that allows for changes
    in cgroup hierarchy and version without affecting framework codebase.
    Rework current processgroup and sched_policy API function implementations
    to use task profiles instead of hardcoded paths and attributes.
    Mount cgroups using information from cgroups.json rather than from init.rc
    
    Exempt-From-Owner-Approval: already approved in internal master
    
    Bug: 111307099
    Test: builds, boots
    
    Change-Id: If5532d6dc570add825cebd5b5148e00c7d688e32
    Merged-In: If5532d6dc570add825cebd5b5148e00c7d688e32
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04108d80d2f5c03535e258d3be1a424470d7f218db80e3734c2cc7bcb55d1207
-size 4356
+oid sha256:cc33dd29e805fc742a651495c459780f17eaedf13116e45cfd631431eec746c0
+size 4373
--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ecef7e9bcf5e1fbc095718a06967a3c27749b16e88ea2d16731b54c8fd84224
-size 11315
+oid sha256:1e590a569946a3691d3ae717a130879188be56867c7c0cba22e266c736cd4f65
+size 14078
