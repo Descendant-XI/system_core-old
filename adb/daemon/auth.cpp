65f152841da82f0f1786f918e9a6048dd42469a1
Michael Groover <mpgroover@google.com>
2018-12-28 20:35:37 -0800

    Notify framework on adb disconnect
    Bug: 111656592
    Test: atest AdbDebuggingManagerTest
    Change-Id: I84f0b076799b0628663fde1a14609bc71c5a9ed3

--- adb/daemon/auth.cpp
+++ adb/daemon/auth.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:446027da76c4d5db10adf0fae839051dbcb734b625946eeb63db155a5188ca43
-size 6864
+oid sha256:22868256adbf2a8665105264748765cea5c72543c7fc59ed6a0a62a5ebe45227
+size 7138
