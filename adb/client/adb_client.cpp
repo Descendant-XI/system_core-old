659e6d95d23dff1315c62c510d30182b86c186ac
Cody Schuffelen <schuffelen@google.com>
2019-01-02 14:17:29 -0800

    Combine tcp_connect and socket_spec_connect.
    This will make it easier to add new types of transports by combining the
    logic the adb server and adb daemon use to connect to things.
    
    Bug:121166534
    Test: adb connect against a cuttlefish instance over the shm proxy.
    Change-Id: Ic7fc848c60a85eef968c3735838c87cb7fdaf38b

--- adb/client/adb_client.cpp
+++ adb/client/adb_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:869dbae91be52069a94cf5cddadb656bbdb13c498a4c058fc3db44eade43135a
-size 10398
+oid sha256:cddc955102465b5569fafbb43d362ab9f9edf2836bee5d1d646e05c3707561ac
+size 10446
--- adb/transport_local.cpp
+++ adb/transport_local.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:129849c953738819ea6e3f349f5f520cd9dfdfea5dacb1b19a87d9f6594860b6
-size 18408
+oid sha256:731b0cac2ff38562bb098a878c4e58b403006e03c15ec9c06e002f342c022b59
+size 17881
