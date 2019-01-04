471d10749d3f6647940063c8ffda735483d5e44a
David Anderson <dvander@google.com>
2019-01-03 18:13:27 -0800

    liblp: Allow GSI metadata to bypass slot suffixing checks.
    Bug: 121210348
    Test: manual test
    Change-Id: I40634c72b0255197a5c16e23553f4cf54bc32579

--- fs_mgr/liblp/builder.cpp
+++ fs_mgr/liblp/builder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e64bde610e5b9dade0ce89780d5aabdcac5c8b978b9162026aa972018d17fe
-size 39625
+oid sha256:4121c02e1e0a65257f5b193a4e6d747dc7ee24d6efb25e6b62d77ee74edb498c
+size 39767
