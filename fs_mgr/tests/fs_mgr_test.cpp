65e516b305e8e2348a2713b9c1559878dcbcb610
Bowgo Tsai <bowgotsai@google.com>
2019-01-07 15:30:27 +0800

    Support host build for libdm and libfstab
    The host builds for both libs are needed for libfs_avb host unit test.
    Also replaces strlcat()/strlcpy() with snprintf() because the former
    doesn't have a glibc version. Or switch char* to std::string*.
    
    Bug: 112103720
    Bug: 117960205
    Test: m libdm ARCH=x86_64
    Test: m libfstab ARCH=x86_64
    Test: atest libdm_test
    Test: atest fs_mgr_unit_test
    Test: boot a device
    
    Change-Id: Id9b92b5286b8ed9ab0d80f18ab5802dcfeb83dfa

--- fs_mgr/tests/fs_mgr_test.cpp
+++ fs_mgr/tests/fs_mgr_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c9cbfde3db108bf98bf22532f3681206f36647b818bd766cf9729f4d8b73866
-size 8383
+oid sha256:e5d1b2c5da5113ad89915c9f0896344ce6f69a4cbf4078440e6c5b839562946a
+size 9491
