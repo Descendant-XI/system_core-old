8463d20f03364ae10b6c7b6bd1c3d3b965ca3591
Yifan Hong <elsk@google.com>
2019-01-08 13:33:52 -0800

    fs_mgr: overlayfs_mount_scratch don't write to system_other
    This change removes this denial:
    avc: denied { write } for comm="init" name="sda6" dev="tmpfs" \
        ino=25715 scontext=u:r:init:s0 tcontext=u:object_r:system_block_device:s0 \
        tclass=blk_file permissive=0
    
    The reason is that during any mount_all in init,
    fs_mgr_overlayfs_mount_all() will mount a list of candidates for
    scratch, which includes system_other. However, in order to probe
    if /overlay directory exist in the partition, it only needs read
    access to the block device, and mount the partition as readonly.
    If the block device is a true candidate (i.e. it does have /overlay),
    re-mount it as writable.
    
    Test: flash, wipe, boot, denial goes away, cppreopt is successful
    Test: boot the second time, no denials (no cppreopt this time)
    
    Fixes: 122454600
    
    Change-Id: I465b363eac755d79711e4f82955cd98450527122

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a997d4420a3ba5d88e80f329353c8a77ca418b4e352105939c5b947788e3ccc
-size 36124
+oid sha256:8c0d04fffc8420eb0da6245840f38280c6e0b505a6295f57d6c6bd1b8c9f1a9e
+size 36582
