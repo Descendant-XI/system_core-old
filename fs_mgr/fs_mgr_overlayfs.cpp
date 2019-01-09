b6c238ccd8dcf756691107705d913d5552fa0149
Yifan Hong <elsk@google.com>
2019-01-09 11:13:09 -0800

    fs_mgr: system_other does not want overlayfs.
    system wants overlayfs, and system_other is a valid candidate;
    but system_other does not want overlayfs.
    
    Addresses these denials:
    avc: denied { read write } for comm="tune2fs" name="sda6" dev="tmpfs" ino=15114
      scontext=u:r:fsck:s0 tcontext=u:object_r:system_block_device:s0 tclass=blk_file permissive=0
    avc: denied { read } for comm="e2fsck" name="sda6" dev="tmpfs" ino=15114
      scontext=u:r:fsck:s0 tcontext=u:object_r:system_block_device:s0 tclass=blk_file permissive=0
    avc: denied { read write } for comm="e2fsck" name="sda6" dev="tmpfs" ino=15114
      scontext=u:r:fsck:s0 tcontext=u:object_r:system_block_device:s0 tclass=blk_file permissive=0
    
    Fixes: 122454600
    Test: boot, no tune2fs and e2fsck denials
    
    Change-Id: Icce20f1463cfde034a56f25590cba9f8fe5f41a4

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0b517e7ece5a2c24dcc68dc657274bd63359d642590bc23bfac420521c131f7
-size 36885
+oid sha256:50f807618d421f83a62162cb320c7792858790b40aa26c38bfdd51e745b2ad9d
+size 36986
