dad92f2b53228f925e2c761bc8e781145e910b94
Yabin Cui <yabinc@google.com>
2019-01-08 16:31:56 -0800

    Add setuid/setgid capabilities to simpleperf_app_runner.
    Also add simpleperf_app_runner in shell utilities.
    
    Bug: 118835348
    Test: build and boot.
    Test: run simpleperf_app_runner manually.
    
    Change-Id: I538503dabfa8ff192f4b3029206a62613eacdf32

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88a3c48d6d34af117499727bc90e43ce54bede0b016295c7e7995801a880f916
-size 17370
+oid sha256:91d2910e55cbc44ae5878fb7fe5d7dfaf334a9f7259980429cddf23ead7c2a45
+size 17595
