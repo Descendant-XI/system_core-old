0e6514854ce45102b54e1ccaa2ad0f9ef18c1fb4
Josh Gao <jmgao@google.com>
2019-01-09 17:01:49 -0800

    debuggerd: switch to base::{Send,Receive}FileDescriptors.
    Bug: http://b/12204763
    Test: debuggerd_test
    Change-Id: I0be40916214de51ab36fd6bd6d44090a84312e51

--- debuggerd/tombstoned/tombstoned_client.cpp
+++ debuggerd/tombstoned/tombstoned_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92fa11e60748b4c527442bd7be60a2f45a0f5ca578d31b8c01a1a70262fb8e09
-size 3340
+oid sha256:42d27fb0b09c4d3b14d9d96828c0a69d750ac702e940d46b8d38d3fbfc9f4b90
+size 3431
