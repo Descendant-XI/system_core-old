ad5660a27a7bd2372e0c3b0c37671af6db5b18a4
Andreas Gampe <agampe@google.com>
2019-01-10 08:54:45 -0800

    Libbase: NOLINT legacy unique_fd operator int
    The use in implicit conversion is intentional.
    
    Test: m
    Change-Id: I6c756e772b274e8a525ba2ad792cc4a8e1fc0176

--- base/include/android-base/unique_fd.h
+++ base/include/android-base/unique_fd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71a59eb97b7f02e5434f68ea43dfb30b437b9c74e6a0d22c5b2c2d608f00420b
-size 7349
+oid sha256:686b869fb4c40c027bad5f17fbffd17c4b9eac8ef5212f6d28e9063c4c397a6f
+size 7360
