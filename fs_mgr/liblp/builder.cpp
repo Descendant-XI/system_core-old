55a9afe524350fff6a86e2359a1aa4961796ec97
Ramon Pantin <pantin@google.com>
2019-01-10 10:59:59 -0800

    Fixed a few minor nits in liblp
    Bug: 122616553
    Test: built and ran liblp_test on Pixel 3 with Q weekly build
    
    Added missing __attribute__((packed)) in two metadata structures.
    Fixed error logging message when repairing primary metadata.
    Few very minor additions related to metadata validation.
    Fixed an off by one error in the validation of partition name length.
    
    Change-Id: Ic777baf97871c786a209da7c32bc13c1360a8482
    Signed-off-by: Ramon Pantin <pantin@google.com>

--- fs_mgr/liblp/builder.cpp
+++ fs_mgr/liblp/builder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12cdec0d42198beba336ed1b579584fc462bd63d8493df7e816d73e149d8d144
-size 39080
+oid sha256:7a9602d4584a6305d76c0edc153baa6749b4f32e86ec5c6f59b739e866f7fb06
+size 39277
--- fs_mgr/liblp/writer.cpp
+++ fs_mgr/liblp/writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:341ba444211a0b456de2bccc039137f641cb18ddfaa8610aa6a923631b8b95d2
+oid sha256:e13f4f1a5ea08750ed9b9a9c41d557b452f04be56f7c7968ae0e3e5670815ee3
 size 16774
