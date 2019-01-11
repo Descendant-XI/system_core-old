51d41b7ce487612a32eb730e5544f8583810a607
Elliott Hughes <enh@google.com>
2019-01-11 13:50:05 -0800

    Admit that there's more `adb install` help available.
    Test: adb --help
    Change-Id: I1e4d4250ef8bc55cfa660f87ebdbd7d664509266

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44543d239817acd9c03e331c721ccfdadbec3938c99ddf7cbec741a6e9d05fdb
-size 67657
+oid sha256:7db9f13db8c46f5dac7ed71c9f93d5f85847db7c57b68470b51751a84b474e68
+size 67697
