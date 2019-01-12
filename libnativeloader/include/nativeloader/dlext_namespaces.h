6443e79caca855da4357630f3781a47cf153e217
Nicolas Geoffray <ngeoffray@google.com>
2019-01-12 15:01:20 +0000

    Move to C API of libnativeloader.
    Test: m
    Bug: 119840313
    Bug: 122710865
    Change-Id: Id5b08ef5de0d38cb678a50e45d38dfb8107c4a1c

--- libnativeloader/include/nativeloader/dlext_namespaces.h
+++ libnativeloader/include/nativeloader/dlext_namespaces.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f36363e8599bb18881cef82f9e4318639cc7e2bc2ee00a75adbcebb0a516306c
-size 6098
+oid sha256:322dff039a245f138bd974f12bfb90ac7f5cbcb9d1775ad4f8feac2b2d2ff69d
+size 5856
