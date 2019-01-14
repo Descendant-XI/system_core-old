3048bafef83f66b2b3eae0f0b91ec26c7ef2481f
Sandeep Patil <sspatil@google.com>
2019-01-13 16:47:20 -0800

    meminfo: Add SmapsOrRollupPss
    Adds SmapsOrRollup parsing methods to only read Pss of the
    process fomr /proc/<pid>/{smaps, smaps_rollup}.
    
    Bug: 111694435
    Test: libmeminfo_test 1 --gtest_filter=TestProcMemInfo.*
    
    Change-Id: I31b982ae5ff2bb5b165ea33f6c57755ee34cbbc7
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- libmeminfo/libmeminfo_test.cpp
+++ libmeminfo/libmeminfo_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1e23c1b3e34d0b2320ad33d5a3b9da05d398baf4f3925ffa0ae8b69f91ecb53
-size 29047
+oid sha256:6fc22e8ba99f0426551763c8442fb180902da1107c52721082188553e31340e3
+size 30454
