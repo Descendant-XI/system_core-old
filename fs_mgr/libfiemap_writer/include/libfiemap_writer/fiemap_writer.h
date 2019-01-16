545087c88fe0ac76d83a4b2459f47f1b311157bd
David Anderson <dvander@google.com>
2019-01-15 18:09:50 -0800

    fiemap_writer: Add a progress notification.
    Bug: 121211685
    Test: fiemap_writer_test gtest
    Change-Id: I554b888521682281f0971b8f0c411d8e0bd3062c
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- fs_mgr/libfiemap_writer/include/libfiemap_writer/fiemap_writer.h
+++ fs_mgr/libfiemap_writer/include/libfiemap_writer/fiemap_writer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abff883f1516cfdec4d8fe3833bd4a880ce4f2d01993348791ca37cb343455ba
-size 3582
+oid sha256:3002765a8d72a5b61d8a322d7df38d1bee7a7a2c3e50c4e67c713cfdb74f66f3
+size 3933
