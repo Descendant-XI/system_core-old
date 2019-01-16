bc4854c0a1da56bed717e7ebf5cf20d26537e66f
Nicolas Geoffray <ngeoffray@google.com>
2019-01-16 20:20:27 +0000

    libnativeloader: remove stubs for now.
    Seems to break VMDebug test.
    
    Also add missed strdup in returned error message.
    
    bug: 122957265
    Test: VMDebug
    Change-Id: I64727d707f20c0e6afcb5455edf20c99011a81dc

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e2459a3f248e4b8f0ee2f595d3876a568a4ea9ed3b0a5ba151846fa9d6c9f94
-size 29324
+oid sha256:6ecea12bae60bc56812e61f20af630f4280a6a8efbf955d09079991770efe786
+size 29332
