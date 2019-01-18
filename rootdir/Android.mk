1dd23d14a5c2da48ce92c649a6b7bee0e22e88f7
Dan Willemsen <dwillemsen@google.com>
2019-01-18 15:14:34 -0800

    Remove LOCAL_MODULE_TAGS := debug
    It's replaced with entries in PRODUCT_PACKAGES_DEBUG in
    build/make/target/product/base_system.mk
    
    Test: treehugger
    Change-Id: I4dc69c34ddc2c494fc74bc4afee6efa240c9b0d3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aea84c3c70f575f33a3932fb1be1b968b8d5875a892037affed142af43eaa78
-size 11576
+oid sha256:a209419d91b203bf66377fd4746db683000e28af0418ff8503c6b5877d452015
+size 11549
