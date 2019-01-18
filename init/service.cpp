d7f2101646609e114e7690b0bb2318ee6bfd8c02
Jiyong Park <jiyong@google.com>
2019-01-18 12:29:23 +0900

    Revert "Identify post-apexd crashing processes"
    This reverts commit c661b660fe1eaa82e13ca898390517a94df256c8.
    Reason: Breaks some 3p apps.
    
    Bug: 122920047
    Test: run the app, login.
    Change-Id: Ie56ea5516887b885579764929e82e4fe684a7dcf
    (cherry picked from commit 60b89aee4c00020124732a82095663d4b5a9aefb)

--- init/service.cpp
+++ init/service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70de149b8b9308eb65b5630a0f6d341ad9391aba793a7dd55ac51b160e019f24
-size 49150
+oid sha256:c6910811b346034ca19cd560393ebef3263f4da7405722024ebc64dcaa24dabf
+size 49169
