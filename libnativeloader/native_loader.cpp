71e0d2c91452d36ed0f65a85d8c63cc4f1337db7
Nicolas Geoffray <ngeoffray@google.com>
2019-01-18 10:05:13 +0000

    Pass the Java caller's location to OpenNativeLibrary.
    Will be used to find the linker namespace to use.
    
    Bug: 122874359
    Test: m, gtest, run-test, CtsJdwpTests
    
    Change-Id: I3f3be4a1d864741d585ab3ed9ffb93fab262516e

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ecea12bae60bc56812e61f20af630f4280a6a8efbf955d09079991770efe786
-size 29332
+oid sha256:c95b9e07cdd9b7187d2c9f681c8b0b307f3a085dc1ad293d3b7cbe5882892b47
+size 29405
