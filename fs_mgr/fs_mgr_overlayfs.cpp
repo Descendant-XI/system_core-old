90572f27255458746ec66e7ca31913c3e3df0199
Mark Salyzyn <salyzyn@google.com>
2019-01-22 08:54:56 -0800

    fs_mgr: overlayfs: erofs is in staging
    erofs is in upstream staging and in 4.19, and we see some Android
    partners considering it as an option instead of squashfs.
    
    However, squashfs and erofs filesystems are always 100% full, so
    we can safely remove the string checking instead and just rely on
    the statvfs call.  We will provide lip-service to our support in
    comment only.
    
    This does _not_ mean we support the decision by our partners to use
    erofs, as we have not evaluated the performance.
    
    Test: adb_remount_test.sh
    Bug: 109821005
    Change-Id: I80ea3a33b9da6684ee95489132bc8a88b2310ac8

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bde3dbd3c6906cdc5acecae75467366d7e7d438f8c77acf94ab652be626fc56
-size 37192
+oid sha256:28634bd71d3991ec89c7dce5d66e36c7ba54d88f739a4b0331edd3ce7e9b0ace
+size 37168
