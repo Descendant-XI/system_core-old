428e77ea4b33fbcb756d847e2712176eaa4400b6
Mark Salyzyn <salyzyn@google.com>
2019-01-22 14:05:32 -0800

    fs_mgr: overlayfs: validate that kernel supports scratch filesystem
    Check for /sys/fs/<fs_type> existence to confirm kernel has
    the file system type available.
    
    Test: adb-remount-test.sh
    Bug: 109821005
    Bug: 123079041
    Change-Id: Ic4388e2044bccea8b8edc7762d4ac9b3047206ac

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28634bd71d3991ec89c7dce5d66e36c7ba54d88f739a4b0331edd3ce7e9b0ace
-size 37168
+oid sha256:8044c28a53e64f6550da17761a74491730ed21f44544b65c1ba66ae1046dd6a0
+size 37258
