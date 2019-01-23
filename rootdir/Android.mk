d9818f0e3b0e14995bfa761c30a70056264c9c38
Neil Fuller <nfuller@google.com>
2019-01-23 09:58:29 +0000

    Add a symlink for the ICU .dat file
    The ICU .dat file was moved into the runtime APEX file
    in commit b6d855f081c232309961f31c7c7c8a76abf79c3c.
    There are some apps that know the old location and its
    absence causes them to fail.
    
    This change adds a symlink from the old directory to
    the new directory. The ICU .dat file changes its name
    with every ICU major release so this is simpler than
    linking the file itself.
    
    Bug: 119293618
    Bug: 120853401
    Bug: 122985829
    Test: make droid / inspect output
    Test: Confirm broken app works on an internal master build
    Change-Id: I452dcb5e52975011c9ebd3db2caa621bbefedaf3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aea84c3c70f575f33a3932fb1be1b968b8d5875a892037affed142af43eaa78
-size 11576
+oid sha256:eb42f2be19c1284501eac0c2e14806c0f39d2e4087655f7c0f11b2230b1e1007
+size 12222
