97a11ca979cc6747e3bd85d34b0bbc4a7cc13e4c
Mark Salyzyn <salyzyn@google.com>
2019-01-25 09:08:42 -0800

    fs_mgr: ReadDefaultFstab suppress ReadFstabFromDt logging
    Since we are moving the fstab from DT into the ramdisk fstab, the
    logging from ReadFstabFromDt when reading the default fstab is
    turning into logging noise.
    
    Test: compile
    Bug: 122602260
    Change-Id: Icba0962c13d701afce2dc7c4f23712dd47ea0100

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db38366372f094686c33fee411c0ef3e3b5d46bee2877fbbf74c02e00e493bc0
-size 39692
+oid sha256:f3fc25c5b4eeb99d8f19b4a5387e974d0bd75a28fc4f131a282f15649bbd0344
+size 39764
