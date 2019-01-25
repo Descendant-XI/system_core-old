72fa00c9fbd729c94754903264d08e1212154e1b
Mark Salyzyn <salyzyn@google.com>
2019-01-25 11:05:40 -0800

    fs_mgr: remount: add -R argument
    The -R flag tells remount it can reboot to disable verity or to
    run fsck on an ext4 deduped filesystem, or both.
    
    Testing may include a manual component because adb-remount-test.sh
    needs to run from a device in an enable-verity state to test this.
    
    Only recognizes chained avb.
    
    Test: adb-remount-test.sh
    Bug: 122602260
    Change-Id: I6ce4372532d9b933dcca9e2bec544d525b76c4d9

--- fs_mgr/fs_mgr_remount.cpp
+++ fs_mgr/fs_mgr_remount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8faec2f3d66572f9f8459dcacdaa3e23c1a2298559692d272f5ee16ab31273fc
-size 10614
+oid sha256:184d4a9a6c0f234f9a70d9158eb58233361849b28ae88a1523307afcf0eccbc1
+size 12149
--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99a168c9671e87d66bacfd052a7d9cf864f6dd6c96a98839d13eab21a08d3253
-size 35926
+oid sha256:d8b69c0f279d45feed9d522e7bb73a8c840666db25a5590bb9de0364fa8ac17b
+size 36874
