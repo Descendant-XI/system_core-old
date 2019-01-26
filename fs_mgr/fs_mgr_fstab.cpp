2836f5e995b4c59830a831346684bf22e1435785
Tom Cherry <tomcherry@google.com>
2019-01-25 17:13:50 -0800

    fs_mgr: remove old fs_mgr functions that are not currently used
    New code should be using the new Fstab class, so remove these older
    functions that don't have current users.
    
    Test: build
    Change-Id: I3ba2b12b838b4b46979963869939fd4b65264738

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b52e0bb2566b74760de8c0e17ab1b3adf83fd8bc60a07da4a7362fbd3e46d191
-size 36738
+oid sha256:44e3e1be43954430309d338348da28baa13bc04658d8812897aa2df6ac1c1fcf
+size 34851
