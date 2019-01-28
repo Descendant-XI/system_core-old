a6be0a077b3592d9edcf7b5e35cd286401bf0c8b
Greg Kaiser <gkaiser@google.com>
2019-01-28 06:17:44 -0800

    adb: Fix incorrect logging statement
    We were logging "fd.get()" after we had already done a
    "std::move(fd)".  That won't log the value we were hoping for.
    We instead cache the file descriptor value in a local int prior
    to the move(), and log that.
    
    Test: TreeHugger
    Change-Id: I715874ac63329280ffb55881fb2590fb31dc2457

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae3ab1d28b2524fb3092fa3cac0b09533d253f51b657027edaba1784afe39710
-size 27456
+oid sha256:49228d25a5e9b153b14c556105c760ba87784b68164c7e1031afa0650c56ab91
+size 27485
