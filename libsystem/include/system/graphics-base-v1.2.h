b77cc910574ab842e7c8dc4ee00cf6efc72fa7be
Emilian Peev <epeev@google.com>
2019-01-27 20:08:27 -0800

    Add graphics-base-v1.2 autogen headers
    Graphics headers generated by:
    hidl-gen -Lexport-header -o
    $ANDROID_BUILD_TOP/system/core/include/system/graphics-base-v1.2.h
    android.hardware.graphics.common@1.2
    
    Bug: 109735087
    Test: Android builds successfully
    Change-Id: Ie847fdd808ed0daaea917d705a8dc51c6633142a

--- libsystem/include/system/graphics-base-v1.2.h
+++ libsystem/include/system/graphics-base-v1.2.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afd6c9bb283135cbd1c5cd4b16daffcbf0146eb7e73b93599b6c7fc1e5fb721b
-size 794
+oid sha256:a588ceeb5528b39de457374784f7af696c24e945285e3b51f1f52952190992d3
+size 847
