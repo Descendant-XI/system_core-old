e5153d06c050f6d023540199d3d573c1e8d4176b
Martin Stjernholm <mast@google.com>
2019-01-29 18:19:17 +0000

    Add compat symlinks into the Runtime APEX for the ART binaries.
    This is a precaution in case there are dependencies on the binaries in the
    old location. b/124106384 tracks eliminating them (if any).
    
    Test: Flash and check symlinks are there with adb shell ls -l system/bin
    Test: adb shell system/bin/<binary>
    Bug: 113373927
    Bug: 124106384
    Change-Id: Ib4102fe55117611f68184102e68a10ea47de0065
    (cherry picked from commit 7f77dbfe64100e31e16e69a5bb5912d50a7c4962)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce55286fb4944bf36f35e3c6415339406188235f96cdba9c822a4a0e942cef2
-size 12798
+oid sha256:66b33fd2a25e976cd65daba5dd9405566df2f6df971210b710b2df41ff7720ca
+size 13145
