a5d5c60c54d504f6e5a47e35980e80182b1d6164
David Anderson <dvander@google.com>
2019-01-29 17:44:01 -0800

    Add user/group for gsid.
    Bug: 122556707
    Test: manual test
    Change-Id: I18e44ff1bbb3c43cfc8f6f2997f9358052ce249b

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8498bb99834c86156c30de931970cd6cfdee9b31f86928aca07d2e907a86a82
-size 10334
+oid sha256:b0ecde3c67288702ab93c6e460d3dc939205b188dff2b7ed1c304b71cd5486ec
+size 10392
