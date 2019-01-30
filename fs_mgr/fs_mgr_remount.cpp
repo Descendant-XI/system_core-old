39db8089d2779749405febe312c3e6e1c48b9e95
Mark Salyzyn <salyzyn@google.com>
2019-01-30 10:19:15 -0800

    fs_mgr_remount: support legacy devices (marlin)
    Deal with first version of verity for legacy products.
    
    Test: system/core/fs_mgr/tests/adb-remount-test.sh
    Bug: 120448575
    Bug: 123079041
    Change-Id: I7a2dd8309cbb19751fdbb05d4efc30c486615e04

--- fs_mgr/fs_mgr_remount.cpp
+++ fs_mgr/fs_mgr_remount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:184d4a9a6c0f234f9a70d9158eb58233361849b28ae88a1523307afcf0eccbc1
-size 12149
+oid sha256:350279e59e4fe4c818e7d4bb8eca6b4ca1c1eaa999f0a7253bf5a79d859e9f44
+size 12441
