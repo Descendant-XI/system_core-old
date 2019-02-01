623de50ff0005d3ff87b67d20f1500b481c29851
Wei Wang <wvw@google.com>
2019-02-01 11:53:58 -0800

    Restrict zram_writeback permission
    Bug: 74582279
    Bug: 122659265
    Test: Build
    Change-Id: Iba610929bc02d7ee5a590f8ec5b440269dbc12f5

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:490254d2701c239c0ea2218be6aab012692e36d4ca501b915f62983e444bb6e0
+oid sha256:b8fa0f80dc2ff140481cde6fcbba3babc2027a4a6a3d7638caf4ca4ac0055f7a
 size 63283
