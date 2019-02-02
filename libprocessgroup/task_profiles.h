61c1021cc4f2662415dd9eb07e32745f97c2c133
Suren Baghdasaryan <surenb@google.com>
2019-02-02 14:19:41 -0800

    Fix non-Android build targets
    Non-Android build targets are missing sys/prctl.h header and functionality
    should be disabled for them like it was done previously inside
    sched_policy.cpp. Also make the set_sched_policy/get_sched_policy
    functionality backward compatible by creating stubs for non-Android
    targets.
    
    Bug: 111307099
    Test: built sdk_gphone_x86-sdk_addon_mac target using forrest
    
    Change-Id: I1c195267e287a84a21c588bd61d7c452bff6cfbe
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- libprocessgroup/task_profiles.h
+++ libprocessgroup/task_profiles.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0468eaa891b3928b1b47f87114239ee358581856bd6b780480f0bca065ab31f
-size 3931
+oid sha256:ef6b91340987dc69f6ebb4b74ec3dceb2ca9de634304cf1922305f02ccc1f6c8
+size 4185
