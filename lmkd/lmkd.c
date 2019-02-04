f0a700c783b329a7e2e7a328af67ef1abde0e3ba
Greg Kaiser <gkaiser@google.com>
2019-02-04 08:50:10 -0800

    lmdk: Remove redundant 'if' check
    We already know that "polling" must be non-zero at this point,
    because it hasn't been modified since our check on line 1960.
    So we remove this check for code clarity.
    
    Test: TreeHugger
    Change-Id: I069d9fd0eef70748a5333733dd0518d1ac8021b7

--- lmkd/lmkd.c
+++ lmkd/lmkd.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:024178771f2f2de65b7cb9fd122fd9cfb34c8d67de9c9633a2991d37438a963b
-size 61022
+oid sha256:2523e79e9dd3468a0942ad3a9731cb0356e1b999a82c67397c8dab2babe2c113
+size 60961
