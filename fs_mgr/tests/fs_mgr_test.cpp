eb41862bef68984a249b3241d4c387a61ad8a3e6
Tom Cherry <tomcherry@google.com>
2019-02-04 13:33:32 -0800

    Remove old C fs_mgr interface
    Remove superfluous fs_mgr flags as well.
    
    Bug: 62292478
    Test: tree hugger, fs_mgr_unit_test
    Change-Id: Ib307400941b46ca8bcb51d479668efd82fc3ed2d

--- fs_mgr/tests/fs_mgr_test.cpp
+++ fs_mgr/tests/fs_mgr_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bcb33aba8981f073932e91f40c0f02dc7a194f4e3f4927752c1d5bc87ba7676
-size 35398
+oid sha256:efd90dc5a78aeb2208502e2d4db9c83c94f49ca1148a0593354ef81c128b2c7d
+size 36431
