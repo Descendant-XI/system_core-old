d2938a1778a28be72679aa876885ac231fcd5901
Elliott Hughes <enh@google.com>
2019-02-06 14:28:32 -0800

    MappedFile and FileMap should support zero-length mappings.
    Bug: http://b/119818070 "app crashes when reading asset of zero length"
    Test: ran tests
    Change-Id: Idd2ad6f6e72c8e445aff78a460fac96dea41c950

--- libutils/FileMap.cpp
+++ libutils/FileMap.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acb41c1aef05a8cae1f848f7a6cae7af111d57c5cc319633a00ad595cceaebd7
-size 6681
+oid sha256:0a0c19f59b0e5e790ebb6d33f0291a244dbe767969163d978ea6aa86baeb3ff3
+size 6699
