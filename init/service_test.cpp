78cf7aeb97ef303983ea86208c0b3611100daa28
Tom Cherry <tomcherry@google.com>
2019-02-06 10:45:56 -0800

    init: allow services to have no capabilities set
    In particular, this allows services running as the root user to have
    capabilities removed instead of always having full capabilities.
    
    Test: boot device with a root service with an empty capabilities
          option in init showing no capabilities in /proc/<pid>/status
    Change-Id: I569a5573ed4bc5fab0eb37ce9224ab708e980451

--- init/service_test.cpp
+++ init/service_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8061564738bb9f6a610521e65fc0abbddedd688cbba20bb078d67d334cf8cab
-size 6531
+oid sha256:0f9ce82dd38d300b033d751b75c361f11e22384e70fece0b7811378b4a90a71f
+size 6525
