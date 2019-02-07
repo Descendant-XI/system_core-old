11106d7cd52781081ef67f216c60f4056a9a65c1
Elliott Hughes <enh@google.com>
2019-02-07 12:41:37 -0800

    Clean up libcutils/libutils tests.
    Move tests in the same directory as the corresponding code, so it's
    easier to see what is/isn't tested.
    
    Fix naming of libcutils_tests (plural) to match the singular that's more
    common (even though the plural makes more sense to me).
    
    Add these two to system/core/'s TEST_MAPPING.
    
    Remove obsolete AndroidTest.xml.
    
    Fix a flaky (timing-dependent) libcutils test.
    
    Test: ran tests
    Change-Id: I7e0a31ff45c8a152562bf66fc97161594249366e

--- libutils/tests/Looper_test.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4333c13a924a3f97f100d2d727cf8c1e6c2f2531ee7729001a7142eda5eeebed
-size 26819
--- libutils/tests/TestHelpers.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:48818f419e444ed694288e71e0bdf0ef5c1bef73f62b441841f0a00bb2b71276
-size 1736
