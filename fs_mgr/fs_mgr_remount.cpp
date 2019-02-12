669566c62d2df0762e8128e27d312dd562b4c82d
Mark Salyzyn <salyzyn@google.com>
2019-02-12 15:26:34 -0800

    fs_mgr: remount: log to stderr and logd
    Logging normally goes to stderr, also send output redirected to the
    Android Logger.  Required if command is exec'd, useful if commanded
    from shell.
    
    Test: manual confirmation of both outputs.
    Bug: 122602260
    Change-Id: Ibc2e14bd4fad561514c0c33741da8ca6f00af3f3

--- fs_mgr/fs_mgr_remount.cpp
+++ fs_mgr/fs_mgr_remount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:724f170158b57cc03db99bd35549c5497fc764bc7fc879c6a44041b61f61df1c
-size 9880
+oid sha256:22d0350eb54db19de217273adec8e66512da7009f04db73ddcc623e76b85307e
+size 10000
