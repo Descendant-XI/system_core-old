4e53d3120ac4712617f8eafd843de5167c5575f7
Suren Baghdasaryan <surenb@google.com>
2019-02-14 14:40:41 -0800

    Re-enable file descriptor caching and add option to skip caching
    This reverts commit bee9f5718bd2dfedb615767bbd25147b4f3eed15
    "libprocessgroup: Disable file descriptor caching temporarily" and adds
    option to use SetTaskProfiles and SetProcessProfiles without file caching.
    This option is used from JNI to avoid access denials because cached files
    are not whitelisted for JNI usage.
    
    Bug: 123868658
    Bug: 123043091
    Test: boot using svelte target
    Change-Id: I76b9d6af8a1dd4464cb3cf3e6dc327980efdf361
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- libprocessgroup/include/processgroup/processgroup.h
+++ libprocessgroup/include/processgroup/processgroup.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff690dc54e3f70f40ac944527d5b021f1aaa2886624dec6c9f038d90c4ca6c54
-size 2342
+oid sha256:bebab76bb86a6deb85569219bba1c05aac0318f42c9d12d7fecb811d71290c62
+size 2420
