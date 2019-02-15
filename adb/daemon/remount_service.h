a4fa7a1b091d7ba5efca9db795e2dee00a1937aa
Mark Salyzyn <salyzyn@google.com>
2019-02-15 07:45:26 -0800

    adb: use /system/bin/remount command
    Replace direct logic in support for 'adb remount' with an exec out to
    /system/bin/remount to do the heavy lifting.  Remount success and
    failure strings are reported by the adb remount service in response
    to the various reported errors, freeing up the remount command itself
    from the legacy of script expectations.
    
    Test: adb-remount-test.sh
    Bug: 122602260
    Change-Id: I686fa465f463b881bbb38f709d780a95e463be80

--- adb/daemon/remount_service.h
+++ adb/daemon/remount_service.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8717c983c2a83855c0249536498693ada88b0bba245557b89d48515f3b00f8b3
-size 820
+oid sha256:8c263e6efe8d8962d7e32614a726f33b4e94eecf70ef50baa5bf4bf63f8dee78
+size 767
