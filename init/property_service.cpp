fa8e1a2e1567139891cf68d854458caf921abb47
Tom Cherry <tomcherry@google.com>
2019-02-16 12:03:19 -0800

    init: Ignore "ro." restrictions when reading prop files
    "ro." properties do not make sense for prop files, especially with the
    way that the system/product and vendor/odm partitions are meant to
    override each other.  To fix this, we ignore the fact that "ro."
    properties are "write once" when first loading in property files.
    
    We then adjust the order of property file loading, such that we read
    partitions from least to most specific, reading in order: system,
    vendor, odm, product, product_services.
    
    Bug: 122864654
    Test: ro. properties can override when reading build props with
          appropriate permissions
    Test: ro. properties do not override when lacking permissions to
          be set by a given partition
    Change-Id: Ib9a5f77e95d9df647cdde7a5c937bb3425c066fa

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5802e4c3ed99a4a6071c92b38b985bcae009d91a7b8b1fb2fa41f0e2efc7dbc
-size 34164
+oid sha256:cd008099b5d14d861e39e2e5f62fbdd46dfcc6df7a507b8603facb0929817416
+size 36281
