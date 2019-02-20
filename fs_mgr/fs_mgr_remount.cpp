4cba02ef344ef73ddac171558670fa8388280d14
Mark Salyzyn <salyzyn@google.com>
2019-02-20 08:32:36 -0800

    fs_mgr: add remount command
    There is currently no good option for callers to setup overlayfs
    on-device, it is automated as part of the adb services.  Add a
    remount command that does what is needed that simulates the salient
    behaviors of the adb remount command.
    
    Clean up some noise restoring device to original state when done.
    
    Test: adb-remount-test.sh
    Bug: 122602260
    Change-Id: Idf213800a8182cb1c51600c8f574df8a8cd68d4a

---
+++ fs_mgr/fs_mgr_remount.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:724f170158b57cc03db99bd35549c5497fc764bc7fc879c6a44041b61f61df1c
+size 9880
--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e8544455cf67368f71b0ff9335854d20445a7ef191c706cfb62a2750cdfb1a
-size 34705
+oid sha256:99a168c9671e87d66bacfd052a7d9cf864f6dd6c96a98839d13eab21a08d3253
+size 35926
