99a9e42fe13ff7705f0bd16767a826802ca754d8
Kiyoung Kim <kiyoungkim@google.com>
2019-02-20 18:04:39 +0900

    Add product apk support from libnativeloader
    Currently libnativeloader appends vendor lib path if apk is from vendor
    partition. Similar to this, as part of mainline core, we should add
    product lib path if apk if from product partition.
    
    Bug: 124705551
    Test: m -j && tested from crosshatch with product apps (such as Wallpaper)
    Change-Id: I8690167ca8098015c8ae4bbb4f028404c5e94dc0

--- libnativeloader/native_loader_lazy.cpp
+++ libnativeloader/native_loader_lazy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f31deb0e91d3c93a454f78d4762736f413c42dbea768060b790facf2d7a1742
-size 3535
+oid sha256:384186d6de5e531b5e6e00775b46924064bd9c1f903018289dfdeedd740a9f68
+size 3528
