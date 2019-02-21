a76bff7b8425db4b9f9081fa254f318f6c1919fe
Josh Gao <jmgao@google.com>
2019-02-20 17:16:37 -0800

    adb: switch adb_io.h to string_view.
    Test: mma
    Change-Id: Ifa16502bac18429491426c45ece562657cff1689

--- adb/adb_io.cpp
+++ adb/adb_io.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a215e03bf4a40db4cf898da6a18add022dd6f9b92e01b3d6411e07fdf9b944eb
-size 5910
+oid sha256:92d1570b0c45b093f911f56413e8e1ecd663ad445d4eba9d11d55eabd7bd93ac
+size 5906
