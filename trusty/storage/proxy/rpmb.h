25c41a06bcbb67c504ca90fc73867abb6a355d40
Matthew Maurer <mmaurer@google.com>
2019-02-21 14:23:11 -0800

    Clang-format before updating storage proxy
    These files were previously not clang-format clean. I am submitting the
    clang-format cleanup in its own CL to avoid mixing up the code I'm
    adding/adjusting with old code which needed to be reformatted.
    
    Bug: 124277696
    Test: m
    Change-Id: I8a57ca97925a16bee10b15d2013a5dcf87b0ed15

--- trusty/storage/proxy/rpmb.h
+++ trusty/storage/proxy/rpmb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cba22cdf37f91f2c6905e5bcad5ab19e07c0c7f8af715b5c3b74b2fc5733e060
+oid sha256:2609de004772a9dc7dd1b7df550ce0a739fa52905825b7a5015c2d44a5ceea3f
 size 828
