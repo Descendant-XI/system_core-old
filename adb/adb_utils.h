6ab2eae3fbd6f61419add201284e2951557c0dc7
Josh Gao <jmgao@google.com>
2019-02-20 19:46:26 -0800

    adb: make ParseUint reject garbage at the end by default.
    Test: adb_test on host
    Change-Id: Ie63d89bd08f0b296a3d54ff66aae85fe52d8cd0f

--- adb/adb_utils.h
+++ adb/adb_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3dfbf4ab0e87484f85d1aad9b4b0467053c6070e5c34582d6e5ab1f2f1d65b
-size 3720
+oid sha256:3630b3a223e355e76c792467014a2968149f24fa32451976a739fc5e4cfd6bed
+size 3774
