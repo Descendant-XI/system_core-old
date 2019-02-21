d3ae2d0ae17d7d87dc2d6e28b2a05756c3e398dc
Wei Wang <wvw@google.com>
2019-02-21 11:25:16 -0800

    Reduce log spam from libprocessgroup
    EBUSY is expected when removing process group path if process is still
    active. ESRCH is expected when kill if process died already. ENOENT is
    also expected when opening cgroup path if process died already.
    This CL also skip removing parent path if child failed when remove
    process group.
    
    Bug: 125340804
    Test: Build and boot
    Change-Id: Ief3b9cb913035a4050f6cf79c8b1e2f098e18244

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e590a569946a3691d3ae717a130879188be56867c7c0cba22e266c736cd4f65
-size 14078
+oid sha256:6873e1be765dede834335841a50cc8c6bdcab87e4b2a616a28e0d91155f0cddf
+size 14573
