01b7b23b51324cd1a94cf84e67f5e467294132cf
Josh Gao <jmgao@google.com>
2019-02-22 15:20:27 -0800

    adb: wait for device to disconnect upon `adb root`.
    Previously, there was a race where if adb root took longer than 3
    seconds to take effect, we'd return early and allow subsequent commands
    to be targeted at the still-not-dead transport, and spuriously fail.
    
    Bug: http://b/124244488
    Test: test_device.py
    Change-Id: I791a4f82946eb28e4d37729ab0ed3b7fc05b42a2

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:445c8b708bf97465ea862b5247d4ecc1c99208ecf563467251a0dbd8920aa1b3
-size 68184
+oid sha256:e4a73f84f0890ca65cc4d61b5d05e214f65ff7224d544b94cac1e6777e7ddd61
+size 68179
