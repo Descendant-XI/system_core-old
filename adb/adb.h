b8d4dc31b9e77c7ccb12ed7c4e16a94e8a547c24
Josh Gao <jmgao@google.com>
2019-02-22 15:26:15 -0800

    adb: increment server version.
    Increment the server version for adb_connect with transport id, and
    wait-for-disconnect.
    
    Bug: http://b/124244488
    Test: none
    Change-Id: Ib50d289b68fce4befbf1f5d9507d7e6f9cc1f4f5

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efd3807508df3861bed6b371419fd507f00aca3dfa3e7d4bda04d2a851ff2320
+oid sha256:3e382d0e019adc1182e7ccec4fdd5d7940d26d77a0e892e113f770b74594f115
 size 7323
