22c315640daf41cc94256a8d1ddc3ce259b060a9
Jiyong Park <jiyong@google.com>
2019-02-25 16:41:38 +0900

    Don't bind-mount bionic files
    Bind-mounting of the bionic files on /bionic/* paths no longer required
    as there are direct symlinks from bionic files in /system partition to
    the corresponding bionic files in the runtime APEX. e.g.,
    
    /system/lib/libc.so -> /apex/com.android.runtime/lib/bionic/libc.so
    
    Bug: 125549215
    Test: m; devices boots
    Change-Id: I4a43101c3e3e2e14a81001d6d65a8a4b727df385

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3dbc4dbc8fb859588edfc6077203208c919e6f3d99fbf69f1292dbf84b33513
-size 43640
+oid sha256:5af8e24f00bdf1b75e7be43d7ef00e86aff1a53dad1b0d9a0968acec614b7d1c
+size 43334
