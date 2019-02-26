b674ef08c71cebef061a259c4cc1a29284ec63c6
David Anderson <dvander@google.com>
2019-02-25 17:31:56 -0800

    libfiemap_writer: Allow callers to query the block device of a file.
    Bug: 126249541
    Test: manual test
    Change-Id: I478bcf60caf0cedc84189c39fee4b6c4f94b6ed6

--- fs_mgr/libfiemap_writer/fiemap_writer.cpp
+++ fs_mgr/libfiemap_writer/fiemap_writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:294425e73ed774d6feb78b38f53866d054399d6af6e9cb19447c4eeeb47fb79f
-size 18993
+oid sha256:6e47f01b88043abc63bd90171b564a1d6fa9075409150cac9a6a014733d2cd61
+size 19121
