3c9f821541548cb3a5dcfc89858c7bc2ba752d1b
Mark Salyzyn <salyzyn@google.com>
2019-02-26 12:36:47 -0800

    fs_mgr: overlayfs: test: add inRecovery check
    Handle a device in recovery mode gracefully.  Handle a device
    that fails to boot into a commanded state more gracefully.
    
    Deal with regression where die() calls restore(), and we can not
    have this under the conditions where we are ignoring the error
    in a subshell.
    
    Test: adb-remount-test.sh
    Bug: 118225373
    Bug: 123079041
    Change-Id: Ie37beb245d0ec55eb00757cdb93da34ff9c42827

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce6de1ff81c44513128babb99c6e2c669b92d664f2cafe00120b56802b40b82
-size 32937
+oid sha256:c7e8544455cf67368f71b0ff9335854d20445a7ef191c706cfb62a2750cdfb1a
+size 34705
