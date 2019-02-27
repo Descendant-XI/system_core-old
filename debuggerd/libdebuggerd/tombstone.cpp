26d4f9c8d387c6945ad5033be2b844ca3ace3f86
Florian Mayer <fmayer@google.com>
2019-02-27 18:00:37 +0000

    Fix copy / move behaviour of Maps object.
    Currently, moving or copying a Maps object leads to double free of MapInfo.
    
    Even moving a Maps object  did not prevent this, as after a move
    the object only has to be in an "unspecified but valid state", which can
    be the original state for a vector of raw pointers (but not for a vector
    of unique_ptrs).
    
    Changing to unique_ptrs is the most failsafe way to make sure we never
    accidentally destruct MapInfo.
    
    Test: atest libuwindstack_test
          Failed LocalUnwinderTest#unwind_after_dlopen which also fails at master.
    
    Change-Id: Id1c9739b334da5c1ba532fd55366e115940a66d3

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8d916fca6973aae5cd4cfda615c48b625bb38124ffc259f8315127a6015670
-size 23724
+oid sha256:ed916d7502f71f31db779e4ffc7b638149e32c9888af55825dc95d6a23376585
+size 23714
--- libunwindstack/include/unwindstack/Maps.h
+++ libunwindstack/include/unwindstack/Maps.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a15cefb44b4ee4d163bf8a834a41209e5e3a3fba28922b0a92263a4624f3963b
-size 3130
+oid sha256:b8c243eeafa514b998c2813359f6cb39d0c2b7e2e8e91d2a42803b61dd9ef713
+size 3465
