d77d198608275a0d0322c9bf1c6ff08e326c97a5
David Anderson <dvander@google.com>
2019-03-02 00:28:35 -0800

    Support FiemapWriters that extend across multiple files.
    This introduces a new SplitFiemap class that will divide an allocation
    request across multiple FiemapWriters. This is primarily useful on
    filesystems that have onerous restrictions on maximum file sizes. Vfat,
    for example, supports a maximum of 4GiB, which is too small to satisfy
    larger userdata size requests.
    
    Bug: 126230649
    Test: fiemap_writer_test gtest
    Change-Id: I3c95d341e4e94e0c44bbf0e8553c34ccfdcd155b

--- fs_mgr/libfiemap_writer/fiemap_writer.cpp
+++ fs_mgr/libfiemap_writer/fiemap_writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36b656a34c85a803ad3b1166e29e7ad6cdac0c9e590565a40cd9196c5d889d7a
-size 22330
+oid sha256:560799165e7daf60a62df0f0fa51c37c99aa925979288b8ec5210a6a0dee9496
+size 22336
