2b7c418ce1d2c0253e0e106356b1624dbcca815f
Elliott Hughes <enh@google.com>
2019-03-13 11:15:06 -0700

    run-as: improve diagnostics on failure.
    Bug: http://b/128523258
    Test: manual
    Change-Id: I4430a8d309c0e9b5315aaae3e7d223c05b7ea3e5

--- run-as/run-as.cpp
+++ run-as/run-as.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d80cf66d86c62128641c289167b837168c50a6e3c669d4aa388e15f52ab4738
-size 9001
+oid sha256:d473068bb445e4cfd4b711b8adce04db3f67c90c06a2f8c85feb10306aae4c62
+size 9335
