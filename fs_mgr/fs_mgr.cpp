ffb04357ce9bf56207e6fd0d6f47f558a5657daa
Mark Salyzyn <salyzyn@google.com>
2019-03-14 15:34:35 -0700

    fs_mgr: overlay: suppress noise associated with test mounting scratch partition
    Add internal fs_mgr_is_ext4 and fs_mgr_is_f2fs to get heads up on
    mount failures and thus bypass trying.
    
    Test: adb-remount-test.sh
    Bug: 109821005
    Change-Id: Ieb1f8c19ced930b6fe2d1791ef710ce528da7e37

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34fea28cb8dbc3d4f06363f919f8bc6e254b8ff5dc822a5f72baa7fdaadb96ab
-size 61023
+oid sha256:b02e6ed65649da4fa435efe1d638254a3b220003bd8cd65ff5af988c5db96167
+size 62273
