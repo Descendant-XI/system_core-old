8d2d5a528e1565f8823e00d4890b8ab80edce386
David Anderson <dvander@google.com>
2019-03-13 19:10:48 -0700

    init: Support booting off GSIs installed to non-userdata block devices.
    Bug: 126230649
    Bug: 127953521
    Test: GSI boots when installed to sdcard on hikey960
          GSI boots when installed to /data/gsi
    Change-Id: Id59926ebe363939c8c5745bb1bf5bd13722dae7e

--- fs_mgr/tools/dmctl.cpp
+++ fs_mgr/tools/dmctl.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce330c202d32d1c75825222e9cf9f05c3002b37d6afde91bcacd26919dd184d
-size 12899
+oid sha256:ff0c07e0e5af1a4555629fd22051a73289da21e0adfeccc9570a7da0dce80f49
+size 12897
