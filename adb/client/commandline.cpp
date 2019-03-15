afc4c8f1a985272c26b4be74f929341290757ad4
Josh Gao <jmgao@google.com>
2019-03-15 14:49:25 -0700

    adb: avoid sign extension of shell return code.
    Windows has int32_t return codes, which results in return codes 128-255
    being sign extended into a negative number. Manually truncate the return
    codes we get to preserve their values.
    
    Test: test_device.py on windows
    Change-Id: If41d6d469350301704f6ecff72ad17b412db3e04

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05fe2d110c7a174b7e2adad1e4a236359114275e6fa2e2cde33147640afd161b
-size 69248
+oid sha256:0167b4a5e07c204147b3eec230f257d56cbdae38ad64c8e2a4012fc738ff9269
+size 69509
