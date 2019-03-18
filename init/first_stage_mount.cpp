a6ed322ea572a61b9419bdd91aa147c06de9dd02
Mark Salyzyn <salyzyn@google.com>
2019-03-18 10:27:00 -0700

    Check if we need to Poll for devices after RegenerateUevents
    If the required_devices_partition_names is cleared, no need to Poll.
    
    Test: manual check boot time, and adb-remount-test.sh
    Bug: 128834849
    Change-Id: I044ee2752a7f32b084ff6e88b4b586accdfb78f0

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c92e7d87e4277fd7e49189a0a6a637d22e9d6651a249f64632b56da4017b7224
-size 31542
+oid sha256:3d2f2f54a9afc87d98d30b0ba1e96121a679ee8bfc0b65b783fd77af1ef8e803
+size 31941
