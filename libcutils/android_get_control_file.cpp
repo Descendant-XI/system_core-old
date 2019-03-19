f229f9b3049a7efe3ce99f3eacc1628174452ab6
Yifan Hong <elsk@google.com>
2019-03-19 11:39:14 -0700

    libcutils: android_get_control_file uses realpath.
    If the path to android_get_control_file is a symlink, the final sanity
    check will fail that the fd does not have the same path as the given
    file. We can't expect callers to readlink() because this would change
    the environment key. Instead, try to call realpath on both paths.
    
    Bug: 126233777
    Test: lpdump
    Change-Id: I0df10d7dbe3e572b8335faad812e5cd80bff1733

--- libcutils/android_get_control_file.cpp
+++ libcutils/android_get_control_file.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d066b954558a58f2b3f7ff7fbe5e24af8bad42df416031c21b36e4f346cba6b6
-size 3212
+oid sha256:b2f27bdeb52d2836649b54c3431e3007d6444340c6807c56a23f1155a5606962
+size 3241
