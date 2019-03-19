107e2ff9cc1715fa9b6c6a118957b8345bfbf91c
Mark Salyzyn <salyzyn@google.com>
2019-03-19 13:50:57 -0700

    fs_mgr: remount: manufacture a missing / entry for default fstab
    Adjust adb-remount-test.sh to run on cuttlefish.
    
    Test: adb-remount-test.sh
    Bug: 128033396
    Change-Id: I86825a519fcc821f7da67b0570d7eb2043ae4b42

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7050fd9a4da4d433a58a7184cd69c3c0d8097d6e4801588609ad29f2835deebc
-size 38094
+oid sha256:9a1b6b4b7e407bb70a12081147f5f0990efe01f771ea89deb75d0968006cb327
+size 38351
