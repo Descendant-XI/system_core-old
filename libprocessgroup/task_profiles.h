efb6695b649458a6f17d239fb6233294af76cb4e
Yifan Hong <elsk@google.com>
2019-03-22 17:01:08 -0700

    libprocessgroup: use libcgrouprc to read cgroup.rc
    Use the LL-NDK library to read cgroup.rc.
    
    As a side-effect, the CgroupController class is changed
    to a wrapper of ACgroupController* from the library. The
    class itself essentially becomes the pointer, and can be
    trivially copied. Modify all client code for this change.
    
    Test: builds and boots
    Bug: 123664216
    
    Change-Id: I635d9885db62e82f37421f6d20f59c84a6f4cbb2
    Merged-In: I635d9885db62e82f37421f6d20f59c84a6f4cbb2

--- libprocessgroup/task_profiles.h
+++ libprocessgroup/task_profiles.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:027f096e5cc58e3f69b53eac79276384bdbec77b208af9e95b8a76681c87fbfd
-size 4258
+oid sha256:861ba220833f3c6a8f96e47286fbd3c47ee081d7367b2d4fdc0d43d34a8ab719
+size 4246
