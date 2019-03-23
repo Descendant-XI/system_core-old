0b7636eea75f893cba547954333c4bb5a435ca30
Yifan Hong <elsk@google.com>
2019-03-22 17:21:58 -0700

    libprocessgroup_setup: use libcgrouprc_format
    Use CgroupController definition from libcgrouprc_format, not
    libprocessgroup, because the wire format will be removed
    from libprocessgroup later.
    
    Bug: 123664216
    Test: builds
    Change-Id: If5e2301a1b65c3f6a51a1661cfeeed4e299f634e

--- libprocessgroup/setup/cgroup_map_write.cpp
+++ libprocessgroup/setup/cgroup_map_write.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8f5c63a91ca7706bfa6986b514c0db1d7918444a60deeb732c568523c640e6a
-size 11628
+oid sha256:3f4418dbf3b219f90d363389688a4e403ffcc994a5a05029a13aef20d5c1c04a
+size 11854
