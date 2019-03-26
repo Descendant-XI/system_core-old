da687fb58dabd60fb27e8d8c88c775cf87ca9533
Josh Gao <jmgao@google.com>
2019-03-26 13:06:38 -0700

    adbd: increment writes_submitted_ before submitting writes.
    If we fail to submit writes for some reason (e.g. the USB cable was
    unplugged), another thread that's waiting on the write mutex can enter
    SubmitWrites and attempt to resubmit the writes that we already failed
    to submit, leading to a failed assertion of !IoBlock::pending.
    
    Increment writes_submitted_ before actually calling io_submit, so we
    skip over these writes and fall through to exit.
    
    Bug: http://b/129134256
    Test: manually unplugged a blueline
    Change-Id: I2fd0034e45db22c8f637c81039ce686b7aa6a03b

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41db19de9cecf4c67dc758ad8df4bd9d521039e31724504f19f587baeb5850f6
-size 22638
+oid sha256:b6ca1005ff15f0e10c146c9575aeeeade4dfaec7cf8c75e462913e4364328f2e
+size 22652
