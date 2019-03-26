5df07dda52b20b82f4864ddb699eb327a776cf00
Yao Chen <yaochen@google.com>
2019-03-26 13:51:53 -0700

    Report last atom tag of the failed stats log.
    Test: manually tested with statsd
    
    Change-Id: I4de61a2eea393e8518cb76147598778293440a81
    Merged-In: I4de61a2eea393e8518cb76147598778293440a81
    (cherry picked from commit cf776d9b833952991294b2b898ea4a69b4c2731b)

--- libstats/include/stats_event_list.h
+++ libstats/include/stats_event_list.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac6f173256a7bd673c1d8761a662beb818332de8b4e208ac64f5783851c61189
-size 6712
+oid sha256:fd73663d1ca85ae7f2f2aa02f8529525410a938a827f610b2ab97a45281bf0c2
+size 6726
--- libstats/stats_event_list.c
+++ libstats/stats_event_list.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a864dec94483947e53bb83533950a546ee65bfdff4b4be9f6ac541dc72d2b824
-size 7042
+oid sha256:f41ed323ff6afe7c4169e1562cf8f0c2dbcec30fc94ee1e0fee9a591b5ab36cf
+size 7056
