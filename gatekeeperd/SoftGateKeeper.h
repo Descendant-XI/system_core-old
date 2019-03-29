4ecfa43184f8bdd53ce69615c02ceb4c2dfafebd
Janis Danisevskis <jdanis@google.com>
2019-03-29 11:14:31 -0700

    Gatekeeperd maintenance
    * Move gatekeeper aidl definition to system/core/gatekeeperd
      * Retire hand written IGateKeeperService and use generated instead
      * Adjust gatekeeperd to use the generated aidl stubs.
      * Annotated enroll parameters with @nullable to match the
        documentation and the way it was used. (The hand written
        code was tolerant to null parameters, but it was undefined behavior.)
    * Removed Software implementation from gatekeeperd.
      * Also removed the upgrade path.
      * Software implementation including test moved to
        hardware/interfaces/gatekeeper/1.0/software
    
    Change-Id: I72b734db6f67b79b29c2629764490d75d179908a
    Test: Manually tested setting pin and login.

--- gatekeeperd/SoftGateKeeper.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:52e5ca3f1cd342fa8490278fa3b1ee80a1ba4110905138edd266f90de0ac4c61
-size 5791
---
+++ gatekeeperd/binder/android/service/gatekeeper/IGateKeeperService.aidl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d29adea88d3505a908fe2a40d9f6946e100bc66a0f5a44fca8b7055056a8bdce
+size 3756
