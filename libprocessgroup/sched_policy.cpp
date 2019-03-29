0b9962a6268dd29e57fb688be88205a15e22e9b8
Rick Yiu <rickyiu@google.com>
2019-03-29 20:03:47 +0800

    Let blkio cgroup follow cpuset cgroup only
    Some app may have different cgroup settings in cpuset and schedtune for its
    threads, so let blkio follow cpuset only, which represents the app's current
    state more accurately. Otherwise, if that thread is doing IO, then its
    performance will be affected because its blkio group is in lower priority
    group as schedtune.
    
    ex: an app is now in top-app, but some thread of it set schedtune group to
    background, and blkio follows schedtune because it is called later.
    
    Main thread:
    6:schedtune:/top-app
    5:memory:/
    4:cpuset:/top-app
    3:cpuacct:/uid_1000/pid_8766
    2:cpu:/
    1:blkio:/
    0::/
    
    Some thread:
    6:schedtune:/background
    5:memory:/
    4:cpuset:/top-app
    3:cpuacct:/uid_1000/pid_8766
    2:cpu:/
    1:blkio:/background
    0::/
    
    Bug: 124727032
    Test: blkio has same settings with cpuset
    Change-Id: I9a140c7d9d93e1dd43c34c8cf066f4a62e2bf604

--- libprocessgroup/sched_policy.cpp
+++ libprocessgroup/sched_policy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e22e5d5c2d0ae249f651214af48006cfcdf1fe48804e2adac8db0d760a230f5
-size 7144
+oid sha256:0f1f7f9eff1a2896c9f198e1f8b0c181eacaec74e560030cd7ae88156610b3b9
+size 6824
