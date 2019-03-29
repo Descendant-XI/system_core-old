3c8e3199fed124b279295d02908f214f40d84d5d
Janis Danisevskis <jdanis@google.com>
2019-03-29 11:14:31 -0700

    Gatekeeperd maintenance
    * Move gatekeeper aidl definition to system/core/gatekeeperd
      * Retire hand written IGateKeeperService and use generated instead
      * Adjust gatekeeperd to use the generated aidl stubs.
      * Annotated enroll parameters with @nullable to match the
        documentation and the way it was used. (The hand written
        code was tolerant to null parameters, but it was undefined behavior.)
    * Removed Software implementation from gatekeeperd.
      * Also removed the upgrade path.
      * Software implementation including test moved to
        hardware/interfaces/gatekeeper/1.0/software
    
    Change-Id: I72b734db6f67b79b29c2629764490d75d179908a
    Merged-In: I72b734db6f67b79b29c2629764490d75d179908a
    Test: Manually tested setting pin and login.

--- gatekeeperd/IGateKeeperService.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7771d95e5821b85d8301f22b011a8011fcf4faa86b064ddb1775e339b7a7fda
-size 4379
--- gatekeeperd/gatekeeperd.cpp
+++ gatekeeperd/gatekeeperd.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:153ba033cd6d398c2202d4915d6fdd8c19b51cd179a980891468a7a49a92d861
-size 18426
+oid sha256:2062c4f26aa5b62cf2c579154ddaba0ffb3f8eaf512b7e809ccd71674808a7d7
+size 15798
