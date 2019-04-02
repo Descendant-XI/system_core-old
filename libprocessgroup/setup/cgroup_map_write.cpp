f022815c1d7e5ad0c70cd1d0bd6639e4d907d78b
Yifan Hong <elsk@google.com>
2019-04-02 15:07:48 -0700

    CgroupSetupCgroups: don't leak fd.
    - If file is unlinked, the mmapped region is still there.
    - If file is truncated, a SIGBUS will be seen, and holding
      the fd doesn't help.
    
    Test: boots (sanity)
    Bug: 123664216
    Change-Id: I6683804bc795fab6798891a4471e5fe58fbffe13

--- libprocessgroup/setup/cgroup_map_write.cpp
+++ libprocessgroup/setup/cgroup_map_write.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed0aa3df75ca420ba09fdc0d67c3459f91ce6a0f12211dce2d534d1c3b882b6c
-size 11847
+oid sha256:4f9b14e831407d0fc92fd19066e43a76f821e1c538366364608be3763f7078aa
+size 11476
