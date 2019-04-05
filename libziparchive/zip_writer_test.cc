fbdf8609f8ea4e1bdbec17e00d5af2383f67df2b
Nick Kralevich <nnk@google.com>
2019-04-05 10:08:44 -0700

    zip_writer_test.cc: fix implicit integer truncation
    converting from a size_t to a uint8_t results in a loss of precision,
    which triggers ubsan's implicit-unsigned-integer-truncation checker.
    Make the conversion explicit.
    
    This change allows the ziparchive-tests to pass with ubsan enabled.
    
    Test: atest ziparchive-tests
    Test: compiles and boots
    Bug: 122975762
    Change-Id: I63f28b58f1ca1f4c57323494cb1f4a41e0f34fba

--- libziparchive/zip_writer_test.cc
+++ libziparchive/zip_writer_test.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1b5628f14cf319cf31cb058b65471b84256f97c31741dfb8c8ca30cf0fbd4fa
-size 12378
+oid sha256:d169da9b8b65c909d8c5c25dee8f5202291b809eecbe125c552257abd3f1d3df
+size 12400
