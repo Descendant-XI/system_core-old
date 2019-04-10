2a743d07e2c0baacf3cdc8eadf35018439528fcd
Hridya Valsaraju <hridya@google.com>
2019-04-10 16:31:31 -0700

    Allow fuzzy_fastboot number to run for a specific device serial number
    Test: ./fuzzy_fastboot --serial=826X003L --gtest_filter=*Logical*
    Bug: 117181762
    Change-Id: I9dec510aa604b7994f25ce26edb87d7f6ec3e875
    Merged-In: I9dec510aa604b7994f25ce26edb87d7f6ec3e875
    (cherry picked from commit b9051a3e6559b14759015d679c97d0e9e5fad9a4)

--- fastboot/fuzzy_fastboot/fixtures.cpp
+++ fastboot/fuzzy_fastboot/fixtures.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff405ecaa33e50fc82ca1dd1b1eda6e56f4d40735c6e62d72381ee704dc8ceb9
-size 10947
+oid sha256:cd780c1c11d9f0ca2da7f804b9e213d04a215b4ceeaa94266e08513a941c4997
+size 11004
--- fastboot/fuzzy_fastboot/main.cpp
+++ fastboot/fuzzy_fastboot/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2872ec68abe3744dfd25806b33a5cb0f957da02e013943749cbe5ce24fd8a3a5
-size 79324
+oid sha256:6a93f70720a7d9a1b48404a0d04b025abed0d1ac8302ec19a32ba170ed6a4b2c
+size 79503
