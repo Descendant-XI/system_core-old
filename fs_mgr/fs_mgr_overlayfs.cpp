a5b2433f85dc3445ca1ff18ea37164ce6a28476d
Mark Salyzyn <salyzyn@google.com>
2019-04-11 12:58:19 -0700

    fs_mgr: overlay check shared blocks for / if /system is not
    For bringup devices that do not have right-sized partition, and if
    /system is now root, check / to be sure.
    
    Consider unshare blocks for ext4 as dead code, provide a strong
    message to caller to provide all the dependencies to overlayfs.
    
    Test: adb-remount-test.sh
    Bug: 130327601
    Change-Id: Iffa7c5f24d8f409e71f89fe9ece274d8c476f6fc
    Merged-In: Iffa7c5f24d8f409e71f89fe9ece274d8c476f6fc

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebc84e661f9b0a560c10cf60208714546ac5c9dde68865a381c6613d16ae5e86
-size 37576
+oid sha256:9ee985619457af5e1b184aefb92db1c2b041c0549dbe80a8936e13c2609518ca
+size 37727
