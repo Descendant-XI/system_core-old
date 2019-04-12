3d25b21ff7c3abf6b2c1c3a16a042fab55653de2
Mark Salyzyn <salyzyn@google.com>
2019-04-12 16:00:15 -0700

    adb-remount-test: wait-for-screen
    If we reboot too agressively after a fresh flash either just before
    test is run, or after vendor is flashed, we run the risk the device
    will consider it a bad boot and head towards recovery or revert to
    previous system.
    
    Add checks to wait for the screen.
    
    This can result in the test reporting issues with boot complete,
    which will not fail the test currently, but can be used to determine
    if the device under test is in a boot loop or fragile state.
    
    Test: fastboot flashall ; adb-remount-test.sh
    Bug: 132070014
    Change-Id: Ia1b3800c44222cb8fbd9b00e897b32a256996ebc

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a7625277548cff05d8a77a85b8a5f310f75a636feae92b952decfa76ddec7c
-size 40724
+oid sha256:6d3fdfcc1aa6c3d69ed8f4361e32a567b4c6b0f96c94f90d3323c48c6edbe9c1
+size 42099
