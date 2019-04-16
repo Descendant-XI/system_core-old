365a924f9d30506c55b6a676a4d5af09acad3a08
Josh Gao <jmgao@google.com>
2019-04-16 13:17:08 -0700

    debuggerd: call setsid in our children.
    There appears to be a kernel bug that causes SIGHUP and SIGCONT to be
    sent to the parent process group we spawn from if the process group
    contains stopped jobs (e.g. the parent itself, because of wait_for_gdb).
    
    Call setsid in all of our children to prevent this from happening.
    
    Bug: http://b/31124563
    Test: adb shell 'setprop debug.debuggerd.wait_for_gdb 1; killall -ABRT surfaceflinger'
    Change-Id: I1a48d70886880a5bfbe2deb80d48deece55faf09
    (cherry picked from commit 18cb6812474e00050efab19e242c8a04ed470bec)

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63d831cb7ae3f4c346797f35132d70d861b40660cee27b2bd73115c12d26a3bf
-size 19923
+oid sha256:fa1ea87c92212c67dc70a855a97ce37bdea8ad090b72033e974c58acb08dec4d
+size 20191
--- debuggerd/handler/debuggerd_handler.cpp
+++ debuggerd/handler/debuggerd_handler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adc2dd68cca1070ffbd2fd76df7629f01ee00cdaec1c37cb65ddc6f595e8de45
-size 22005
+oid sha256:ede3f384920b753ce8fafbec6eb164d4a71efdc37aeec07cfaccb352e8e15ca7
+size 22289
