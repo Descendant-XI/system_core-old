7e3164e7d92442ef72ac5335c350ffa23268c444
SzuWei Lin <szuweilin@google.com>
2019-04-22 17:27:52 +0800

    Remove the mount points defined in skip_mount.cfg from ReadDefaultFstab()
    The first stage init skips mounting the mount points defined in
    skip_mount.cfg, but these mount points still return from
    ReadDefaultFstab(). The behavior causes some error logic which
    try to access the partition which had been skipped.
    
    After applying the patch. ReadDefaultFstab() will not contain the
    skipped mount points.
    
    Bug: 128961335
    Test: `fastboot delete-logical-partition product_a`
    Test: `fastboot flash system aosp_arm64-userdebug`
    Test: `fastboot -w reboot` and boot to home screen
    Change-Id: I3156260b5d37647dbecf98ca90601a089bea5c46

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d1c4f08029af0f5bb028bfda88b12a0452eb8c73b959acd32cba3b516710d5f
-size 32946
+oid sha256:bde04358e594f28a306b7e9a1e5e97a5caf61de4b2cc895420d6869fa3f85464
+size 31833
