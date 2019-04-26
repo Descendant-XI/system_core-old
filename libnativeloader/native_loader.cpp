5fa2b14151330e979b57b8f99af8f0f120d45cf9
Andreas Gampe <agampe@google.com>
2019-04-26 08:51:50 -0700

    Nativeloader: Restrict anonymous namespace creation
    Create the anonymous namespace only from a non-empty library path.
    
    This is a better heuristic than the current "first-come" one. It
    allows creating classloaders that are not directly related to an
    app's "main" classloader, that is, do not have any library path.
    This is the case, for example, for shared libraries, including
    preloaded ones.
    
    Longer-term, the anonymous namespace creation should be made
    explicit, so that the framework can have full control.
    
    (cherry picked from commit b9df7d936c7dfd1741b56e722065a46ac1e50607)
    
    Bug: 130623656
    Test: m
    Test: manual
    Merged-In: I8251363b372e365d10ae09d23df93d76388ac7f9
    Change-Id: I8251363b372e365d10ae09d23df93d76388ac7f9

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61876dddf5eab9e520f7e0d6d7af1d8d5ee70f4e1addeaaf28a7fedd7c6568d2
-size 36820
+oid sha256:227d7301d28e7efdcf9996abb9b50fb766ef22ab3e6da82abce7489401e0e9c3
+size 36934
