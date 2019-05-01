b0fea88851d1891bc429ce6d2161615a8cf84047
Josh Gao <jmgao@google.com>
2019-05-01 16:53:53 -0700

    adbd: avoid starting multiple worker threads.
    A previous patch intended to make receiving multiple FUNCTIONFS_ENABLEs
    non-fatal, but failed to do so because we would try to spawn another
    worker thread and hit a different assertion.
    
    Bug: http://b/130638368
    Test: echo mem | adb shell "su 0 sh -c 'cat > /sys/power/state'"
    Change-Id: I53456112244d8b4f7d26df6ec6961389fca70498
    (cherry picked from commit 910ce0ff0852418c1a1e126bf0b083dcf24d5899)

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ef48797ff2cffd84d717b04d2a4e2dbbcf03bd14d41dfba62707d5b9b095ac9
-size 24564
+oid sha256:0ae3047347c8190a4fba3d3d60cd29acaac754d0b7453685f1bbf4f61444f6c1
+size 24704
