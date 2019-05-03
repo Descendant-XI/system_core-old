972b9cdc6696f8c6fb50388b069383e8417aceab
Jiyong Park <jiyong@google.com>
2019-05-04 00:30:23 +0900

    Hide non-bridged/bridged namespaces behind NativeLoaderNamespace class
    NativeLoaderNamespace fully abstracts the non-bridged (so called
    android-) and bridged namespaces.
    
    Bug: 130388701
    Test: build & pass presubmit tests
    Change-Id: I3d5ca7515711e7638f4a5ab4d3a150185c3d17ac

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e293e5faf115fceec83a28016c8d0418ce8a4d1f5ca00cea04486d91a93b3a0f
-size 8997
+oid sha256:02d06b65d21d81f6f5766638335ea9d09412342412b0a84d8adbf9930ea5c656
+size 8534
