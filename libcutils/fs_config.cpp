173f2d2f1c5ebd19bc0185f8d076ba3d49da7cd4
Min Yun <min.yun@lge.com>
2019-05-03 17:48:17 +0900

    Add default executable permission in odm/bin
    In odm's binary files can't have executable permission.
    Add default executable permission in odm/bin and vendor/odm/bin.
    Previously partitions that except system, only appeared in
    system partition if product haven't separated partition.
    After support ODM, vendor/odm should considered.
    
    Bug : 124465978
    Test : Check each file's permission.
    system/vendor/bin/* -> 0755
    system/product/bin/* -> 0755
    system/product_services/bin/* -> 0644
    system/oem/bin/* -> 0644
    
    vendor/odm/bin/* -> 0755
    vendor/product/bin/* -> 0644
    vendor/product_services/bin/* -> 0644
    
    Change-Id: I5cee48474fceaf73853b2013d58017bd64953d86

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81a1463b6501f8401a1d5bf04214c45918d53d47d363b4a4e44a65857f6d19c5
-size 17958
+oid sha256:20c140c71571456ed5b7ac58a8695a270c15cfd8bffbc9d94b3451d20aea7419
+size 18315
