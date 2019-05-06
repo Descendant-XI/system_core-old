a620b09e1c0d3e40cb8dface70000c264850be3a
Mark Salyzyn <salyzyn@google.com>
2019-05-06 09:19:43 -0700

    adb-remount-test: report usb user on unexpected connection
    Test: adb-remount-test.sh
    Bug: 132070014
    Change-Id: I1bc4009b71cac4ac4e06df38cc4da77f2819b88b

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d3fdfcc1aa6c3d69ed8f4361e32a567b4c6b0f96c94f90d3323c48c6edbe9c1
-size 42099
+oid sha256:3f531fb35df70d289211ec5ee0ede31c2bdc7129140e5e8fdf45d87e68f371ae
+size 42131
