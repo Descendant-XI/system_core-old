9a54c8a4498c17dff909a18ae0892f494740114c
Mark Salyzyn <salyzyn@google.com>
2019-05-08 12:44:50 -0700

    init: move "started at" environment var names to headers
    Move "FIRST_STAGE_STARTED_AT" to env_first_stage_started_at variable
    in first_stage_init.h.  Move "SELINUX_STARTED_AT" to
    env_selinux_started_at variable in selinux.h.
    
    Test: build
    Bug: 124491153
    Bug: 129780532
    Change-Id: I512d1835dcee5a3d034ac453b27b0709eed23cff

--- init/selinux.h
+++ init/selinux.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78a82511ebe635c22cac10d555f48b1fddde03201989b6bbc9878bee99becaf9
-size 1260
+oid sha256:03fd90b79b66c28cfe828336c91d2040f6757ff46e95d9f4c7a7214e0eb0c75a
+size 1330
