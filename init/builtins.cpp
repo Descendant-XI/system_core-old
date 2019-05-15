868e47e7acae3e40a1e6af3714546912846ee783
Martijn Coenen <maco@google.com>
2019-05-15 22:04:13 +0200

    class_start_post_data also starts disabled services.
    This keyword was introduced to support restarting services on devices
    using APEX and FDE. The current implementation is not a restart, but
    rather a 'reset' followed by a 'start', because the real /data must be
    mounted in-between those two actions. But we effectively want this to be
    a restart, which means that we also want to start 'disabled' services
    that were running at the time we called 'class_reset_post_data'.
    
    To implement this, keep track of whether a service was running when its
    class was reset at post-data, and start all those services.
    
    Bug: 132592548
    Test: manual testing on FDE Taimen
    Change-Id: I1e81e2c8e0ab2782150073d74e50e4cd734af7b9

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04d00ace72a9e7910a577d1fbdc8dd7e9bcf79b018368cbf4a5587bbebdcff0f
-size 45650
+oid sha256:553ec8a8906b5018e1b6329f3b9e6ac5fd52ed8e7578aeb5767f18f9718ca845
+size 45729
