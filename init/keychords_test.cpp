927f4bbaeba1577f75b08307153e4ac6957f6047
Tom Cherry <tomcherry@google.com>
2019-05-16 16:54:49 -0700

    init: replace Result<> with expected<>
    Android-base has an implementation of the future std::expected<>.
    This provides the same baseline functionality as Result<>, so use it
    instead of our own version.
    
    Bug: 132145659
    Test: boot, init unit tests
    Change-Id: I11e61bcb5719b262a6420483ed51a762826a9e23

--- init/keychords_test.cpp
+++ init/keychords_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d86cc9b262e01edfdba922f981b61565b4c326d4d8109e2efe0ed76a1f07a7f5
-size 9740
+oid sha256:a61d6937426e4e8da882be6aaadf9ea7b14e89d9f947d2a11b8535cf6e3ab401
+size 9726
