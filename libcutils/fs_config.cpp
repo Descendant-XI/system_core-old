e72f3b4d45571ea968abe5ab1d3f8c08b629e907
Bowgo Tsai <bowgotsai@google.com>
2019-05-17 15:40:18 +0800

    Moving /odm/build.prop to /odm/etc/buid.prop
    In device root directory, we have the following symlinks:
      - /odm/app -> /vendor/odm/app
      - /odm/bin -> /vendor/odm/bin
      - /odm/etc -> /vendor/odm/etc
      ...
    
    This allows the Generic System Image (GSI) to be used on both devices:
      1) Has a physical odm partition, where those symlink will be hidden
         when /odm is used as the mount point
      2) Has no physical odm partition and fallback to /vendor/odm/.
    
    We can't just have the symlink /odm -> /vendor/odm, because the former
    devices won't have /vendor/odm directory, which leads to mount failure
    when the mount point /odm is resolved to /vendor/odm.
    
    The existing /vendor/odm/build.prop won't be loaded in the latter
    devices, because there is no symlink
        - /odm/build.prop -> /vendor/odm/build.prop.
    
    Note that init blocks reading through direct symlinks (O_NOFOLLOW) so
    the above symlink won't work either. This CL moves the odm build.prop
    to /odm/etc/build.prop for init to load it (symlinks in earlier
    components of the path will still be followed by O_NOFOLLOW).
    
    Bug: 132128501
    Test: boot a device and checks /odm/etc/build.prop is loaded
    Change-Id: I0733c277baa67c549bb45599abb70aba13fbdbcf

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c47e3dcf4a318a8cd82f82abb2e68f99cadfbec6f873432753b199399139ead6
-size 18132
+oid sha256:3250587c4916bc8b496293098f5d1df65d02a8250849e74e9aeddf4c07c342b3
+size 18262
