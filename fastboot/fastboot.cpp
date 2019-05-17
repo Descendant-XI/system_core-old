323925bba987439b73b11f8a369ff355c2a03d0e
Hridya Valsaraju <hridya@google.com>
2019-05-17 14:24:19 -0700

    Get max-download-size from device during fastbootd for flashall/update
    Currently, during a 'fastboot flashall/fastboot update', the 'getvar
    max-download-size' command is issued once to the device when it is in
    bootloader mode and the same value is used even after the device boots
    into fastbootd. If the max-download-size returned by bootloader is
    greater than the max-download-size in fastbootd, this could break flash
    as large images are broken down into chunks before downloading by using
    the max-download-size variable. This will cause fastbootd to return
    an error since it checks whether the buffer being downloaded has a size
    greater than the max-download-size limit.
    
    Test: fastboot flashall
    Bug: 132917902
    
    Change-Id: Ife7c1ec0583d80d4a31ecf01f1fc14a8365afe0d
    Merged-In: Ife7c1ec0583d80d4a31ecf01f1fc14a8365afe0d
    (cherry picked from commit 83d856e4c59b6c02eeb7d468892b157a13f7cbf5)

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b740703f53574fc789df100d435ec8a3b375b236dc9b2b4a4c3e3cb42c51eaa1
-size 73365
+oid sha256:17bb51eec9717d61249ecc26901225f2666300ecceedadb4374886adc10577e1
+size 73531
