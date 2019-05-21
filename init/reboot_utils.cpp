2e6145604f4035e4123e423cfbab51ebe88951c7
Tom Cherry <tomcherry@google.com>
2019-05-21 13:53:05 -0700

    init: make fatal reboot target configurable
    Currently, if init encounters a fatal issues it reboots to fastboot
    but this may be not desirable in all cases, especially the case of
    critical services crashing.  Therefore this change adds the ability
    for vendors to customize the reboot target via the
    androidboot.init_fatal_reboot_target= kernel command line.
    
    This applies to all LOG(FATAL) messages as well as fatal signals in
    userdebug/eng builds, except for signals before logging is enabled in
    first stage init.
    
    Bug: 121006328
    Test: device reboots to configurable target with LOG(FATAL)
    Test: device reboots to configurable target after a segfault in the
          various stages of init
    Test: device reboots to fastboot without a configured target
    Change-Id: I16ea9e32e2fee08dece3d33b697d7a08191d607b
    Merged-In: I16ea9e32e2fee08dece3d33b697d7a08191d607b
    (cherry picked from commit 75e13baf32ae1a54a9e43b7d7d6bdcc6f8d545d7)

--- init/reboot_utils.cpp
+++ init/reboot_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c82382f8cb09db7b893ca5287d60be5a88b8b0365d49b26b269d9a860401932
-size 4910
+oid sha256:caa368db0cbc90130cba9984673bcdead3b2e6ec7a552f9c77acbcc81324d837
+size 5830
