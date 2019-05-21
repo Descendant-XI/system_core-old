58bd58711086cdcf61f26246101182232689ad1c
Josh Gao <jmgao@google.com>
2019-05-21 16:07:03 -0700

    adbd: respond to device-to-host control transfers.
    Failing to write to the endpoint results in subsequent reads on ep0 to
    fail with EL2HLT, so do an empty write to fulfill the transfer.
    
    Bug: http://b/131867920
    Test: manually tested with modified auto client
    Change-Id: If2eec162ca4b31a9974c7bd5802be51cee9e2708
    Merged-In: If2eec162ca4b31a9974c7bd5802be51cee9e2708
    (cherry-picked from internal commit af086e9a95cb52cdc7432c58e221ab86fafed87a)

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cde08aa765819543d3746e5583f73eebc8f6aadf1b628be7ec134025595025a
-size 26772
+oid sha256:e3937425ccc1efb55bbbed16c9436ea12026d9b160eb4aaf30d109199957b4e6
+size 27032
