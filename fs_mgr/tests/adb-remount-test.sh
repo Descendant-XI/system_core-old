72fb65bc1e689b09c762a5b0b0df3889fc15e21d
Mark Salyzyn <salyzyn@google.com>
2019-05-22 11:00:55 -0700

    adb-remount-test: check vendor image signature before using
    When developing and using the adb remount test, if device under test
    is flashed from another source than the current visible tree, make
    sure that the vendor image as-built and visible in a sandbox build
    is not used indiscriminantly.
    
    Test: adb-remount-test.sh
    Bug: 132070014
    Change-Id: I30569a7c871f4c4038b0f7f9c05f5f1a5d12c766

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f531fb35df70d289211ec5ee0ede31c2bdc7129140e5e8fdf45d87e68f371ae
-size 42131
+oid sha256:6d69e7c924d6aeaadba9779bc4ed4915741e610e45cc5acb7e1ba72dcc6286ba
+size 42357
