4d46275407a4634ebd84d8ca55ebad09c883790d
Mark Salyzyn <salyzyn@google.com>
2019-05-22 08:22:17 -0700

    adb-remount-test: parameterize fastboot_wait & adb_wait, increase timeout
    Increase adb_wait time to 3 minutes since blueline device takes
    maximum 2:38 (ten samples) to perform a ramdump should an
    inopportune kernel panic occur.
    
    Test: adb-remount-test.sh
    Bug: 132070014
    Change-Id: Icfbb799f9420035a755090c9fc5fc2ee05dd68d3

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e87edf9c6332f62129ca7d5ebe1cae29a3ae7cfc4db218fd9304a88ed4016fbd
-size 43078
+oid sha256:82132200c18716f3895340379afe353546253fdd9a1c079bc793d6aa6e4cc400
+size 43302
