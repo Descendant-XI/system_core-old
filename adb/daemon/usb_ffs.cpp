e7acb42f5bb9d2eb314134e9bc0e2d5d4e5b34af
Hemant Kumar <hemantk@codeaurora.org>
2019-05-21 17:55:04 -0700

    adb: daemon: Assign valid fd to usb_handle ep0 file descriptor
    Bug: http://b/129283234
    Test: treehugger
    Change-Id: I2d005e17ccb45af95351c074cc53f6cfc53b5fdd
    Merged-In: I2d005e17ccb45af95351c074cc53f6cfc53b5fdd
    (cherry picked from commit 1cbe5edbc3b02213e4061bf443dcf1ac8a7ba8bf)

--- adb/daemon/usb_ffs.cpp
+++ adb/daemon/usb_ffs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f7729592be30c086ec537680a53946f10799a088ea2947ccd82b4d1e8b56728
-size 9388
+oid sha256:20545cdf00340cce20268eb7bd407708b21b7ad8fc9c184c54bea49e7439fa97
+size 9392
