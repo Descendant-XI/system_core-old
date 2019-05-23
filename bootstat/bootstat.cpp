bbdbcd7442e36b59dc9d1ecd2343b91f3696fcec
Mark Salyzyn <salyzyn@google.com>
2019-05-23 09:07:54 -0700

    recovery: report compliant reboot reason (Part Deux)
    shutdown and reboot should have a corresponding sub-reason.
    
    Updating:
        "reboot,userrequested,fastboot"
        "reboot,userrequested,recovery"
        "reboot,userrequested,recovery,ui"
        "shutdown,userrequested,fastboot"
        "shutdown,userrequested,recovery"
        "reboot,unknown#" (Can't happen, debug)
    
    Test: none
    Bug: 133326470
    Change-Id: Icf1ab0d462ec2de2272914a36994a095998d6186

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ce7a35a56f282e5a3433b46491e2cf2250b9afd3244418a5fc721aca1bd0c4
-size 53746
+oid sha256:1fd12ddb71a5737eb7339868000724df5f6f64ec6623fed40cc2ed9572bb11c5
+size 53806
