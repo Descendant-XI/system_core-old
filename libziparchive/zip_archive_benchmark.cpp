0bf1150522ca7a95fe6666af59b514479b24546c
Elliott Hughes <enh@google.com>
2019-05-22 18:56:41 -0700

    Add a std::string overload to Next.
    All but one existing caller actually wants a std::string.
    
    Bug: http://b/129068177
    Test: treehugger
    Change-Id: I428c4453edaae74451db56e9542e4e462f08d43a

--- libziparchive/zip_archive_benchmark.cpp
+++ libziparchive/zip_archive_benchmark.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aee97b6bdaba9f68fbd2ae0f33d627468a6922d046c00e25adae3c881cbfb67
-size 2499
+oid sha256:5dc813628134d9fd5492c2fe1c9388463f44946ef5ea87fda84e61349038eaaf
+size 2501
