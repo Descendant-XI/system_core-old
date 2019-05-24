6d33c4e3dd595d3b6aca062ce0466a8b8c20e4d7
Mark Salyzyn <salyzyn@google.com>
2019-05-24 09:46:09 -0700

    adb-remount-test: do not brick devices that support overlayfs but do not use it
    restore() should not run adb enable-verity if device does not
    use overlayfs even though it supports it.
    
    Test: adb-remount-test
    Bug: 132070014
    Change-Id: I55d0e1a87eca9c5f258a1587c844f2a6e4b13b29

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d7969aa3389919628bd3d6325752233ceacc3152951d1600124e14a4c4cfcf6
-size 47613
+oid sha256:4c6fe19b87f20d8feb96e5cc7d628f2e9bc3cfc730e28f00f025978ec340fe21
+size 47859
