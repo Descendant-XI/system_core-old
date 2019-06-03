5438b699a414d4d07b564db03e7e13fe5879052f
Greg Kaiser <gkaiser@google.com>
2019-06-03 14:12:42 -0700

    fastboot: windows: Minor printf format fixes
    We recently changed 'xfer' to a size_t, so update the DBG format
    accordingly.  We also fix a long standing issue with format for
    the unsigned 'read'.
    
    Test: TreeHugger
    Change-Id: I3dc5f26a033fea64119016802bc9cdb54bbb7b52

--- fastboot/usb_windows.cpp
+++ fastboot/usb_windows.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d8ba077d0560a53218dc47a810b3a1c03a9cdb422bfaf7b7c8726d6e486405c
-size 12452
+oid sha256:1dd7802898133d9ae77e9477bc63156b39a2821c4b5a0c4b0d3a766c2576e905
+size 12453
