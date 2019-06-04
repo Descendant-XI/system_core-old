cea61455883b1185018078a9b9ca782a17ef55e5
Tao Bao <tbao@google.com>
2019-06-04 14:37:06 -0700

    adb: Allow `adb rescue getprop`.
    Which will dump all the allowed properties, similar to `adb shell
    getprop`.
    
    Bug: 134027350
    Test: Run the command under rescue mode.
    Change-Id: Id668224098006d71ee192c8c2bea5d791d2423c1

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc46c510acfc03560b39258256e60ab50b77d02c8ef7f418397f35e7e1cab45c
-size 71740
+oid sha256:5b31c5646f89e7f2688e0a5875c03c063186e0f8511b3682b3ed9a11733dbff7
+size 72203
