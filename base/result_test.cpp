c39cfecb5edb2fb9eab8f415734df6c8bf095fe5
Jiyong Park <jiyong@google.com>
2019-06-05 18:33:01 +0900

    Add expected<void,E>
    Result<void> could be used instead of Result<Success>.
    
    Bug: 132145659
    Test: libbase_test
    Change-Id: I7f079a94d06c5ecb8b0303ced981f0995253dc40

--- base/result_test.cpp
+++ base/result_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d95873f543bfce11b127b080a777e03527717befe787ba4b9be54533c79e3b1
-size 11078
+oid sha256:56334fea5b9b12f61604bc20b8f54739f751ebcbde20f47a858cfbd4f6049784
+size 11790
