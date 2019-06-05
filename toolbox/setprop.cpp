6310846ad2ab1efa292219c51062c514518b7306
Tom Cherry <tomcherry@google.com>
2019-06-05 10:26:54 -0700

    toolbox: add setprop, start, and stop
    Move these from toybox as they're Android specific and more easily
    maintained here.
    
    A few changes during the conversion:
    * Report error in setprop if value is not a UTF8 string; this would
      previously fail only after attempting to set the property
    * Inform users to check dmesg for further failure messages for all
      three programs
    * Only start/stop zygote_secondary if the device is not single arch
    
    Bug: 117321744
    Bug: 133901248
    Test: setprop works, start and stop work on single and multi-arch.
    
    Change-Id: Id2194cf2b65221bde38aff91f0e86b33edb37f42

---
+++ toolbox/setprop.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7accb1d4aa453290be6cd259ff56767d5bc1a1c48af2f585f524896d3a2fb62
+size 2648
---
+++ toolbox/start.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4901e71e30d3a9fbfbfd131e2bd58d534420fd45a6d445db76de41fc734fceb
+size 2743
