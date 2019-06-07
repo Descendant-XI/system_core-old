f36a80881e7aecce87ffc8b1bb51cda70f6d578a
Janis Danisevskis <jdanis@google.com>
2019-06-06 22:05:41 -0700

    Replace legacy trusty gatekeeper HAL with HIDLized version
    This patch replaces the legacy libhardware based gatekeeper HAL with a
    true HIDL based implementation.
    
    Test: Workes with trusty gatekeeper
    Change-Id: I072b0c3fc74523400132aacd34e2f2cac9cf261b
    Merged-In: I072b0c3fc74523400132aacd34e2f2cac9cf261b

--- trusty/gatekeeper/trusty_gatekeeper.h
+++ trusty/gatekeeper/trusty_gatekeeper.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e392d9b28f837df93a5b1c84c54c2bdd8aca1a52319cbd391d410a214f0f512
-size 5058
+oid sha256:52ac6c9a865bf205b5eb223b304cb6f0d043d184987517a3b9452457bbe9b983
+size 3268
