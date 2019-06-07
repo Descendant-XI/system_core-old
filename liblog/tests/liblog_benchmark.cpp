85096b849abcd18d03edb8dc523946ea64bed4ba
Elliott Hughes <enh@google.com>
2019-06-07 08:38:01 -0700

    liblog: fix benchmark build after google-benchmark upgrade.
    Test: treehugger
    Change-Id: Id5a67680c23c22355d7ff2eb5cf66a8ef799b83e

--- liblog/tests/liblog_benchmark.cpp
+++ liblog/tests/liblog_benchmark.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:643ffd637bb62624a3a7a606d107da6016ddbdb077a8dd2d82199bbd7b773669
-size 31503
+oid sha256:b5318f72bd06771586ea6b5113a0258ff455f652066e3e34aa66e8c82e5c2a27
+size 31535
