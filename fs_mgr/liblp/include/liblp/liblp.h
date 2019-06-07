ae84da0598debbe38fd15a66d69e36c819fd5700
David Anderson <dvander@google.com>
2019-06-07 10:44:19 -0700

    liblp: Add helpers for finding partitions and partition sizes.
    These tend to get manually, so let's promote to actual helpers.
    
    Bug: 134536978
    Test: liblp_test gtest
    Change-Id: Ifb79c8d6f247cc3f9635bf6adfd1c99907340002

--- fs_mgr/liblp/include/liblp/liblp.h
+++ fs_mgr/liblp/include/liblp/liblp.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9022d19922e37d028beae3cfe169b192f839acbac7ccfd6ef146580ae08ecb3d
-size 4918
+oid sha256:f4fa747ab6f8b8974bfc814a06ff138fdb1cfcfe5881a10508e49c570918e6a6
+size 5140
