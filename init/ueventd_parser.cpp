b8bb11b219e8817f5d53d9aca0a452fcd61a7ef4
Tom Cherry <tomcherry@google.com>
2019-06-10 11:08:01 -0700

    init: replace Result<Success> with Result<void>
    Now that Result<T> is actually expected<T, ...>, and the expected
    proposal states expected<void, ...> as the way to indicate an expected
    object that returns either successfully with no object or an error,
    let's move init's Result<Success> to the preferred Result<void>.
    
    Bug: 132145659
    Test: boot, init unit tests
    Change-Id: Ib2f98396d8e6e274f95a496fcdfd8341f77585ee

--- init/ueventd_parser.cpp
+++ init/ueventd_parser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f5bd12b1976ede0bc8c024581965304a024b3b1f6305f910e9389e6f880953
-size 8280
+oid sha256:f3d7123bdcbfd4685ba93e92092abd667094d2facbe04605dfca2e070879fbe6
+size 8151
