100fa12d39bc1eb43843b813f4c9a569e7b9bca0
Josh Gao <jmgao@google.com>
2019-06-17 15:05:37 -0700

    Reland "adbd: don't close ep0 until we receive FUNCTIONFS_UNBIND."
    Read until we receive FUNCTIONFS_BIND after every USB descriptor setup,
    instead of only after initial creation of the control fd, so we properly
    handle a rebind caused by a change of the USB configuration.
    
    This reverts commit 55b31933f41b1c550b788fa2054010c51f56111b.
    
    Bug: http://b/133132986
    Bug: http://b/135155652
    Test: manually enabled/disabled MTP with nonblocking_ffs enabled and disabled
    Change-Id: Ibb3f46b8f84854ede47a669a85e07e16a5dd6008

--- adb/daemon/include/adbd/usb.h
+++ adb/daemon/include/adbd/usb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5b51855ee6378469bf0fa6f02c80dd512790cea65441913783365bbacf22c29
-size 2066
+oid sha256:dd7890ddc51e9e491c17ad80fa0d332c65d5d7eeaf2409412eb7ec53ab8c20d4
+size 2289
