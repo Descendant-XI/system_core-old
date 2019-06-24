af4e66de9baabeaaab62f398798a5bb1048cdc9e
Steven Moreland <smoreland@google.com>
2019-06-24 12:55:25 -0700

    Remove binder dependency in init.
    - unused
    - using binder requires twoway calls (since getting a service from
        servicemanager requires two calls)
    
    Bug: 135768100
    Test: N/A
    Change-Id: Idc41f487bad2d8343e99ded98812f3a84e2b8e37

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f963afb78eea336ccce7c8ff4be6cc6e4fa1fb75974221e19769aa21e26c650f
-size 29447
+oid sha256:b32b47f0306e4fc2f790ab47a4c6bd6a93d203f14f4c65ec65f2e6b97b615340
+size 28558
