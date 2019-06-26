84cdf3585c63b0281ac086ed12f3350dca5f483f
Christopher Ferris <cferris@google.com>
2019-06-26 15:08:51 -0700

    Increase timeout to avoid flaky tests.
    Also, print extra information when an error occurs.
    
    Bug: 149562320
    
    Test: Ran unit tests.
    Change-Id: I3404549db0ac18a4a82a99b2620556db96376bca
    Merged-In: I3404549db0ac18a4a82a99b2620556db96376bca
    (cherry picked from commit afc0ff7fe06aee9822fac97e52d1fba09c695d8b)

--- debuggerd/debuggerd_test.cpp
+++ debuggerd/debuggerd_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5a2337dccb992e729889026006691cf2a8e2a49bbf19636c768906a24adda1b
-size 32377
+oid sha256:be38db11fd6e8c85ded952de9608c736d3d3ce2618db6264576058baa8595293
+size 32441
