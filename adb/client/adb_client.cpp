1d8d0bc10e2b0ee93da9b96b8e61d41a82ee070c
Yurii Zubrytskyi <zyy@google.com>
2019-06-27 13:47:17 -0700

    [adb] Cache features set inside the client process
    Test: manual
    Change-Id: I99962edd85e9694c3ca309624368cda6355da008

--- adb/client/adb_client.cpp
+++ adb/client/adb_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:093b6b74bbe58cd4e43cc1000b18217f23ab35ae4648565aa608c9171cac7f7f
-size 13049
+oid sha256:84e8321da2a1cfa623cd64146593abd4346fb5642767230f9232dd2642749fab
+size 13207
