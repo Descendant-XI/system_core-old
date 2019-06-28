69e15fae21ed16168bae406d406c585a8068ce64
Tom Cherry <tomcherry@google.com>
2019-06-28 13:37:10 -0700

    Revert "Modularize logd."
    logd isn't meant to be modularized.  The previous user was using a
    small subset of LogListener.cpp, which is now copied into their
    project.
    
    Test: liblog, logd unit tests
    
    This reverts commit fafea32468e0d59c1ce2eb845dd07a0b4e746cf0.
    
    Change-Id: I05ec764db2d9395f2d5b69a1a610c9c55240ab3a

--- logd/LogBuffer.h
+++ logd/LogBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c3d8fc8f46816f87d92a66515a4f9f380d837f0dca8381fd1f529fa0890bfeb
-size 6013
+oid sha256:096e02e330fa476fbc6af9c781547c4b46ecb4c74cfbda2ec999714db756e04e
+size 5935
--- logd/LogBufferInterface.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eec6a1e7e39817e38564a85b5ba3cd52b270181344857de2f9c8d89e4680f31c
-size 743
