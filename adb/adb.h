5aaea4ce275ee7148a8717c92380879d92b508d5
Josh Gao <jmgao@google.com>
2019-06-28 13:50:37 -0700

    adb: move fdevent to its own folder.
    Preparatory refactoring for platform-specific implementations.
    
    Test: mma
    Change-Id: I0f600122ac89241788c5f3300f362fd9ef02ddcd

--- adb/adb.h
+++ adb/adb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76c98b01cc45d2ade161b60c4875b7749e1289d0952c5214f9c9d92475472596
-size 7362
+oid sha256:bdbc7d99cfb594a18afd4298b8ea92cd0aec23936520a87ef60ad3ee67358b58
+size 7370
--- adb/daemon/auth.cpp
+++ adb/daemon/auth.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:280b5eca41d142af470d4d50aa70039db1ca56861b821fba2d6a6fea608f2914
-size 6882
+oid sha256:00760b84aaf4fc3c130c5bc703ce5aeb1065c5f7b0a1c3b18b4f41b04c66b706
+size 6890
