5517cad2df6916fe82589df041f711d1c67ac163
Steven Moreland <smoreland@google.com>
2019-07-02 18:15:03 -0700

    Remove utf32 functions.
    These don't appear to be used by anything.
    
    Bug: N/A
    Test: binary inspection + code inspection
    Change-Id: I6c12db26c320a66bcf6e28618c6e9f61b40d985e

--- libutils/String8.cpp
+++ libutils/String8.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46998f81b5c1d2f437a8b658e051210be41cceb26846efa990d44b77576f8f02
-size 14906
+oid sha256:914a6f507f0441d3733eb481a8bb55ed28f441d386561be162f4b7cae36f07e6
+size 14576
