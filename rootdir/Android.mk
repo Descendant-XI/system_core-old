81b4ceae3a9b1d2c0fbdffa0af2d2beaa1011cce
Victor Chang <vichang@google.com>
2019-07-05 16:40:15 +0100

    Move ICU .dat
    Move the symlink destination of /system/usr/icu from Runtime APEX to
    i18n APEX
    
    Bug: 137009149
    Test: Device boots
    Change-Id: I86ff83a28d4124892d2b5d84ff09bc539f76c941

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:520eed97f2ad4920ebd518e21f78f0ddff31d3f328389a132da1f07237baccf5
-size 13366
+oid sha256:aa363b5d3e81b3fb0ec9f5cebaca8cb6779542f151cf86b415925fe56089ac90
+size 13363
