a9234ac18fccec6642b89c90d6c5077ae81d8dce
Josh Gao <jmgao@google.com>
2019-07-08 18:16:19 -0700

    adb: fdevent: extract Create/Destroy from fdevent_context_poll.
    Test: adb_test
    Change-Id: Ida308e8e281cbb2954277196a51945f124ce5823

--- adb/fdevent/fdevent_poll.cpp
+++ adb/fdevent/fdevent_poll.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf01cb996c388c81132dd50de8e56aef641751a476d5c877d75e16e4f8d70249
-size 12503
+oid sha256:53dea01884aa2d287276d6f37f61ea9d6278881680b630568eb1c48c7d00d84e
+size 11712
