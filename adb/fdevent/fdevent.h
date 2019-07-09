2fd160f6a2035994617d5555335487c03e8df61f
Josh Gao <jmgao@google.com>
2019-07-08 18:08:06 -0700

    adb: fdevent: move TerminateLoop to fdevent_context.
    Test: adb_test
    Change-Id: Ib1f20aefdb36603e2ceac4197c02551f557056ee

--- adb/fdevent/fdevent.h
+++ adb/fdevent/fdevent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cfadc1a8702d11ebf5bac9beda79de466b4730f68189a39059dac1dccc8a774
-size 4320
+oid sha256:34b1bfe78a7aa2ce803ab28986073fb9d4a9b86a679e9308a848d75ed558b545
+size 4453
