9eab62296548fa7472e9810d468680b7344c1a23
Josh Gao <jmgao@google.com>
2019-07-08 17:37:23 -0700

    adb: fdevent: move run queue to fdevent_context.
    Make the run queue logic reusable between implementations of fdevent by
    moving it to the abstract base class.
    
    Test: adb_test
    Change-Id: If2f72e3ddc8007304bca63aa75446fa117267b25

--- adb/fdevent/fdevent.h
+++ adb/fdevent/fdevent.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68a8775581bba7fecd13ad2f394f2b0fa26b386c1432b426622110a4729b348f
-size 3790
+oid sha256:36bfffd5c131ba420e7c7868e329fd16d89db5a3635eb05d703e1eca21119c22
+size 4247
