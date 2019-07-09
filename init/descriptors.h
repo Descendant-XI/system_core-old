b03463227e8ac19464f6dfbad9278b2f49e7fff2
Tom Cherry <tomcherry@google.com>
2019-07-09 13:33:36 -0700

    init: clean up file / socket descriptor creation
    clang-tidy hinted that some of this code wasn't right.  Looking
    deeper, there is really not much related to file and socket
    descriptors, except that they're published in similar ways to the
    environment.  All of the abstraction into a 'Descriptor' class takes
    us further away from specifying what we really mean.
    
    This removes that abstraction, adds stricter checks and better errors
    for parsing init scripts, reports sockets and files that are unable to
    be acquired before exec, and updates the README.md for the passcred
    option.
    
    Test: build, logd (uses files and sockets) works
    Change-Id: I59e611e95c85bdbefa779ef69b32b9dd4ee203e2

--- init/descriptors.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2d5d1d9820239c7067b45824e34d32829ba73335dfe70d6ca5b0231217c85883
-size 2511
--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb9bfb5faecdad9701cbc3ee129dabd79a329e9b4cb6d04535fb920c76376f0
-size 15220
+oid sha256:d55c49b706ea1c9c5e7ef9fa80454c53c1e8abf70cf12100a92931e8312837da
+size 15262
