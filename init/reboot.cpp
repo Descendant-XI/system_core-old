495b15b1166bcc4ef90f58c593cd7acd71af54cb
Sam Protsenko <semen.protsenko@linaro.org>
2019-07-10 19:02:25 +0300

    init: Write the reason in BCB on "reboot recovery"
    Without this change "adb reboot recovery" leads to normal boot.
    
    Change-Id: I361d0a1f6f6f2c57f3dc80102c21970b462c9b9c
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09f60097ac7145d916e145ca0c92eb5cef8aa204681441f178eccef0a1a986ec
-size 28178
+oid sha256:baf2312e4d336bec675cfdaeee3ef73fb8bf0c2ca09938dd3e5e355dd6f07bb3
+size 28521
