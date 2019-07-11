e2b30e1d937b6fd1d716052979669581fe94c398
David Anderson <dvander@google.com>
2019-07-10 18:09:50 -0700

    fs_mgr: Remove the timeout parameter to DestroyLogicalPartition.
    This is no longer needed as CreateLogicalPartition() ensures the
    obtained path will not race with device deletion.
    
    Bug: 135771280
    Test: device builds, flashes
    Change-Id: I821290aa08fede99d5c51cd68681c351a1ea97bc

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fbc1f0374d290dbbff283c7a05c74bcb26d2c1532e8bdcabbbb05bb795760f6
-size 39433
+oid sha256:3700061465d3e8046e0232772fa8f42d66ca7fecc9b97f52dbbb5e0d84aee322
+size 39420
--- fs_mgr/fs_mgr_priv.h
+++ fs_mgr/fs_mgr_priv.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7e12fa6d27b60273e2acc8fa69629e1acfe32b1d17b349dcfeac9740ce24560
-size 4255
+oid sha256:1d626b230e5fb54c333bcbdaff4a2cd465c2cb059218e424db9d4b7520c3bbc5
+size 4199
