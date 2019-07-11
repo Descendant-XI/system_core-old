0c47d4aa3c6767f7b2da35b6b3e29b4f05a01d3c
David Anderson <dvander@google.com>
2019-07-11 15:43:13 -0700

    Remove libfiemap_writer from fs_mgr and move it to gsid.
    gsid is the only consumer for this library, and it's unlikely to grow
    beyond gsid.
    
    Bug: 134536978
    Test: gsid builds
    Change-Id: Ib632b1dd8de3e29c02d1fe443a1e409af5f1257d

--- fs_mgr/libfiemap_writer/split_fiemap_writer.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fb1637cceddcc9a4533192a5d48b55de90bf0e4396ce0adfed8f335588e83b82
-size 9751
--- fs_mgr/libfiemap_writer/utility.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c13aa92411f404d0979cfa215464534ca612b0ba9e955fbc4a167766439cd2e6
-size 2119
