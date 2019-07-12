ebde0717040b7918e2887e680a72c2f0a7e6764f
Yurii Zubrytskyi <zyy@google.com>
2019-07-12 14:11:54 -0700

    [adb] Optimize adbd's usb reading
    Try to not allocate as many blocks on the heap, and reuse
    memory instead of copying it
    
    Get rid of unique_ptr and shared_ptr where possible, move
    the Block objects themselves
    
    Overall this reduces the time spent in memcpy() from 30% to
    15% of the whole 'adb push' command, and gets rid of about 5%
    of the time spent in the malloc/free calls
    
    Test: builds
    Change-Id: I8995115274b6f08a4df13c58183c928ef384a767

--- adb/types.h
+++ adb/types.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:476f753bad26c0bc73dfb19b2436d5ae1994c71dbaebadabb094654dd84f2094
-size 10200
+oid sha256:086043aaa4be480be57b00af28fd783ba2f5a54b48ac814fad090d7bef5a72d4
+size 7252
