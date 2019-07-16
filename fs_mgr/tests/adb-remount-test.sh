e2b80e8ccf88f1d62d20068a03317f189c510b4a
Mark Salyzyn <salyzyn@google.com>
2019-07-16 07:25:05 -0700

    adb-remount-test add --no-wait-screen option
    If the device screen drivers or frameworks are compromised, permit
    remount testing without waiting for full boot complete to occur with
    the --no-wait-screen option.  In the same vein to support development,
    add --wait-adb and --wait-fastboot options to adjust the timeout.
    
    For TreeHugger testing where other (previous) tests demonstrate
    framework or screen regressions, it _may_ be advised to switch to the
    --no-wait-screen option so that we do not register a false signal.
    It should be noted that some of the past issues with overlayfs and
    adb remount could introduce boot up, framework or screen regressions,
    so it is advised to not use the option if that interlocking logic can
    not be set up.
    
    Test: adb-remount-test.sh
    Bug: 138649540
    Change-Id: Idf8f4a0eb6d7c9139bd4f2c600d14a70dc71902f

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b990344f24d8db084c81549d6143bfbcee6af291f1e35ea20e4718c8be5a68d7
-size 47928
+oid sha256:253b80d344f274490553d31488e8490e890446ff3eba95e918cba49f47cebdda
+size 48666
