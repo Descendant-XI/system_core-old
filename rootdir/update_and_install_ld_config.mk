64976f772551d7852aa06a0bd458226c9f18325a
Kiyoung Kim <kiyoungkim@google.com>
2019-07-18 20:21:05 +0900

    Replace xargs with other shell command
    Recently Android build team changed environment to use prebuilt toybox
    binaries for common tools (aosp/852849). However, as toybox xargs does
    not support -I option, update_and_install_ld_config.mk started to fail
    filtering out private vndk libraries from the list. To resolve this,
    xargs usages are replaced with other shell command (read).
    
    Bug: 137832162
    Test: m -j && tested on Cuttlefish
    Change-Id: I218ce6272c781f826b87c8efc0d3c77bf6bc3af5
    Merged-In: I218ce6272c781f826b87c8efc0d3c77bf6bc3af5
    (cherry picked from commit 95d55818a46d793295db5e90fa55fd0b01bed059)

--- rootdir/update_and_install_ld_config.mk
+++ rootdir/update_and_install_ld_config.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8904b0eff1f6a1f7a4ecdc43d107ef2edcd48a05d9f3d48ccc5168c2cc83555
-size 8859
+oid sha256:7e7d913da453a0855d1176df6944fd98189c8deae27160ac74dce1c0cd88cbc3
+size 8860
