f2ddb1790568b19a5b2e57d5eb1c485fb1a7f770
Joshua Gilpatrick <gijosh@google.com>
2019-07-19 09:42:12 -0700

    Fastdeploy converted to c++ and bin2c on the jar.
    Test: Added test for DeployPatchGenerator and PatchUtils cpp files.
    Bug: 138130943
    Change-Id: I6a128eff4cda00dd05c1bccec71e2678a8b13324

--- adb/client/fastdeploy.cpp
+++ adb/client/fastdeploy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48333bd1c3f4d25050678aeef5dae57ef0b09b1860b536411c547cd416c3bc03
-size 13616
+oid sha256:b7701322f68ee066023d59d42bb32bcd17cba335487a056bd44cdd2f10d9204e
+size 12682
