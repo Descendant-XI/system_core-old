8037e7219c406781d2973d4eb1f91e806091b919
Jiyong Park <jiyong@google.com>
2019-07-22 20:29:08 +0900

    add nopreload option in public.libraries.txt
    A lib with 'nopreload' option in public.libraries.txt is not preloaded
    during zygote. This is useful for seldom used public libraries; they
    don't contribute to the zygote startup time and only affect the apps
    that they are used.
    
    Bug: 132911956
    Test: libnativeloader_test
    Change-Id: I6f97c90e6721aec7f2f96c8fc7b963b34f8edd3e

--- libnativeloader/native_loader_test.cpp
+++ libnativeloader/native_loader_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2afc078da6d0585c5d3af86ad9945e29983a54c8af6b491e2c4dd9a7657e5664
-size 23166
+oid sha256:4ce5a3065307fa2f03ae8ba5b8fc5bd8694d5d72905f037d81bf7349b8ad8e0f
+size 25781
