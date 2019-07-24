a87af7b63a1e16d3a5f33e7505fee04f507dc5bc
Mark Salyzyn <salyzyn@google.com>
2019-07-24 15:24:28 -0700

    adb-remount-test: support devices that do not have verity
    This allows the test to run to completion on cuttlefish
    
    Test: adb-remount-test.sh
    Bug: 138649540
    Change-Id: Ief00af72af94bf14c1d69499b12ece8cfce2f552

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:253b80d344f274490553d31488e8490e890446ff3eba95e918cba49f47cebdda
-size 48666
+oid sha256:8a3918112cce03942354f2c4746d63b21d9ef65802687cba722d9a6fbd88080d
+size 48811
