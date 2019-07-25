73f8427484b15ea2c3d4ae14a7ff3473ed2b41f7
Jiyong Park <jiyong@google.com>
2019-07-26 08:14:09 +0900

    Don't fail catastrophically on non-existing namespace
    When a new namespace is added to ld.config.txt, it has to be reflected
    to native_loader_test.cpp. However, failing to do so crashes the test
    instead of showing an error message that the namespace hasn't been
    added.
    
    Fixing the issue by not returning nullptr when the namespace isn't
    found.
    
    Bug: 138304345
    Test: libnativeloader_test
    Change-Id: I9eee1293956caac69f95a946604a2e6f665bde89

--- libnativeloader/native_loader_test.cpp
+++ libnativeloader/native_loader_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b18a9f9ec3964433cfbfc11340f511ff2d94143b23fc6835fa17c4bfa5460fd
-size 22595
+oid sha256:48766655669e700e47ede63170786bbcf07e3839f880b3a8b7bc0f925f5b388f
+size 22651
