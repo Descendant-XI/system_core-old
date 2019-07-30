056924858b51e28068f9cc3793bcd4dbe3f5eef9
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2019-07-30 16:41:20 +0530

    init: set SetKptrRestrict before early-init
    As part of debugging long run issue we are enabling
    module info as part of debug build under the config key
    DEBUG_MODULE_LOAD_INFO , But doing so can lead to info leak
    as modules/dlkm are loaded in early-init and setting the
    kptr_restriction is done after early-init.
    
    So moving setting of kptr_restrict much before early-init.
    
    Bug: 138641073
    
    Change-Id: Ic3c0ca57a94c8b20136a15331dd646bd8825b625

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75ee7f203f67cfe2dbf16b380ef988ba5c4c3d21022da93394994f5626b6ae15
+oid sha256:8bc66da82a447315e31d8c1af3f636d99e9cb1e77b139a5fd9b0c63435fc045c
 size 30362
