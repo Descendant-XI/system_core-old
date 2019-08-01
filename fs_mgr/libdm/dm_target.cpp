7ca45e9a5e4330daf9cc649ab96e9576ca55f7ab
David Anderson <dvander@google.com>
2019-08-01 14:37:35 -0700

    Implement basic libsnapshot functionality.
    This CL implements some of the libsnapshot internals necessary to work
    with update_engine. In particular it implements snapshot and update
    state, as well as creating and mapping snapshot devices. It does not
    implement anything related to merging, nor does it implement the full
    update_engine flow.
    
    Update state is stored in /metadata/ota/state. To synchronize callers of
    libsnapshot, we always flock() this file at the top of public functions
    in SnapshotManager. Internal functions are only called while the lock is
    held, and a "LockedFile" guard object is always passed through to
    indicate proof-of-lock.
    
    Low-level functions, such as snapshot management, have been moved to
    private methods. Higher-level methods designed for update_engine will
    ultimately call into these.
    
    This CL also adds some functional tests for SnapshotManager. Test state
    is stored in /metadata/ota/test to avoid conflicts with the rest of the
    system.
    
    Bug: 136678799
    Test: libsnapshot_test gtest
    Change-Id: I78c769ed33b307d5214ee386bb13648e35db6cc6

--- fs_mgr/libdm/dm_target.cpp
+++ fs_mgr/libdm/dm_target.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:500a5a00a733762bf149396a75bfe94b0489b627ddb8651eec29f0df5fbc8ccc
-size 7618
+oid sha256:b75aab7c2c29d268b90b317715fb0f11cb1b90c8e8c4c7cb606425e75ae8da3b
+size 8040
--- fs_mgr/libdm/include/libdm/dm_target.h
+++ fs_mgr/libdm/include/libdm/dm_target.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f3e074a0d21d9f4ba22267b2840048e426ae25aff624b6991c882da1197a3c1
-size 10318
+oid sha256:2d43f91a08bffe2307fe59f4e9a869f8b8786c9be249733a5fe2a62f83f7dbb5
+size 10471
