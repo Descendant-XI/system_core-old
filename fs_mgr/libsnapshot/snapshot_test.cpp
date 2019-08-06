d9a893a82681bf5f220ecd53471b809ce904c94b
David Anderson <dvander@google.com>
2019-08-06 14:55:54 -0700

    libsnapshot: Don't force-unmap in DeleteSnapshot().
    It is hard to re-use this function in the merge code when it forcefully
    unmaps the snapshot, because the snapshot may have been rewritten to be
    a dm-linear device. Instead, leave the decision up to the caller.
    
    Bug: 136678799
    Test: libsnapshot_test gtest
    Change-Id: I03c027c0781696885a5a5654d3049287cc16ecd0

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d3db5efbf1d021c8040ae37c551bf752f51126ba5c538f43db19fbeefee2f95
-size 6046
+oid sha256:36b49e293c0c94dfdc4340b8d78529a6412a81c6303953dd79ceaf3b3a7d198d
+size 6114
