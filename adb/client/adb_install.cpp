02e913cf267117c697ee9b16d57602238bd8256b
Elliott Hughes <enh@google.com>
2019-08-05 17:06:04 -0700

    adb: fail better in install-multiple.
    If we're failing out, don't print any messages from the shutdown code:
    it's not interesting (and actively confusing!) to talk about the
    "Success" of finalizing an aborted session.
    
    Also fail early if stat fails the first time (we have to tell
    PackageManager how much data to expect, for some reason).
    
    Also use `bool` as the boolean type and add quotes around filenames in
    error messages.
    
    `install_multi_package` looks like the same nonsense copy & pasted, but
    I don't know what that is, and haven't seen any bug reports, so I'm
    ignoring that for now.
    
    Bug: http://b/133522725
    Test: manual
    Change-Id: Id054dff44f21f4c4cc5877dcc2735fc1eb25f9a0

--- adb/client/adb_install.cpp
+++ adb/client/adb_install.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:453741ca2bd5dc104a4bbf321a3cbcbd9b0e307f899f10081cf0f4d3250cf2b1
-size 25637
+oid sha256:acb7e3111c794ebc4b1608235511d4a5bf4f915bc133305b42c61635add0bcbf
+size 25780
