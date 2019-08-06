8cbecca9b519768e3956023e1bc3a8249ddc70ee
Yifan Hong <elsk@google.com>
2019-08-05 19:33:50 -0700

    libsnapshot: correct DeviceInfo paths
    Test: pass
    Bug: 135752105
    Change-Id: Icb8eb35db9c297537131076721eb7ce868d59625

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b177137203031b254cb9616096d9b809dbf174185e16c88ef7ff6e77612c8a2d
-size 19451
+oid sha256:7c0a1ea3cf0897bc6f8ede44ad8eba2a533e886b325af7b6f18912738bf5c693
+size 19446
