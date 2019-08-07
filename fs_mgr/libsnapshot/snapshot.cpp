01c59f8f744a472b9ac06b01276efab734819032
David Anderson <dvander@google.com>
2019-08-07 15:52:30 -0700

    libsnapshot: Unmap COW devices when deleting snapshots.
    When we stopped unmapping snapshots in DeleteSnapshot, this also stopped
    unmapping the COW device. Of course, the caller has no way to unmap this
    since it's an implementation detail. Therefore unmap it in
    DeleteSnapshot.
    
    This scenario can arise after rewriting or removing a snapshot-merge
    device.
    
    Bug: 136678799
    Test: manual test
    Change-Id: I9d4c8bcfbb95b4454edd1cf0853d51b441e895fa

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:975769b853c84148359dc5da100cd4af985d242df89f8718560cbc275e77c351
-size 19814
+oid sha256:fe683ff293baad618471eee1d7a35b9acd6f423fce4757154b39e689c275963f
+size 19926
