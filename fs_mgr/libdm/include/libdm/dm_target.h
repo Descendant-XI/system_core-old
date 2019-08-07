6c02022bdd7a06b561c67c8ab75ab807577293e1
Paul Lawrence <paullawrence@google.com>
2019-08-07 15:16:48 -0700

    Fix ext4/metadata/udc issue
    Test: Cannot currently test since no device supports this combo
    Bug: 137924328
    Change-Id: I6474211e7229323da831f99d9572283e0c2d49df

--- fs_mgr/libdm/include/libdm/dm_target.h
+++ fs_mgr/libdm/include/libdm/dm_target.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f3e074a0d21d9f4ba22267b2840048e426ae25aff624b6991c882da1197a3c1
-size 10318
+oid sha256:524971cb012f691d19ce1cdbeb3e7b63f08bc193d7e9801a953950d966b8699b
+size 10411
