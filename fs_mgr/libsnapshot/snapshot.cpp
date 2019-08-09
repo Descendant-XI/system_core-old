2466969cd8692613f7ecfd6667b14d436cbcff77
David Anderson <dvander@google.com>
2019-08-09 16:26:31 -0700

    libsnapshot: Track the source slot across reboots.
    The purpose of this is twofold. First, we need a way to detect whether
    the device has booted into the new slot after an update. Second, we need
    a quick-and-dirty test for first-stage init so it can avoid searching
    for snapshots if it doesn't need to.
    
    We can solve both of these problems by storing the source slot in
    /metadata/ota/snapshot-boot. The existence of the file implies a more
    complex check is needed, and the file stores the source slot.
    
    Test: libsnapshot_test gtest
    Bug: 139204329
    Bug: 139089801
    Change-Id: I919dd1c27a65734c61f3a9bdffefb32bdebd723b

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea1170bc1ee609df6e529e02ef33bab65294201ed8cb0dc0cd4473f372d351a8
-size 40169
+oid sha256:0620d7ad14e3ecc75fccdd56f62481e2bc936c0ae00c07ff06b0bc699c43b771
+size 41901
