d458849f21f58ded5579465ddc1f6e80272ca12a
Yifan Hong <elsk@google.com>
2019-08-12 19:13:58 -0700

    liblp: Don't halve super partition for virtual a/b devices
    For Virtual A/B devices, we snapshot the partitions. Hence
    there is no need to prioritize using the second half of super
    partition for _b partitions.
    
    Test: liblp_test_static
    Bug: 135752105
    
    Change-Id: I8bd669e6ffa0a4d114a1b386133179bd2819099b

--- fs_mgr/liblp/builder.cpp
+++ fs_mgr/liblp/builder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53b760454b31f0527b30c0f01b61dc482f03780668f4978c59704de9e3001199
-size 44982
+oid sha256:c1e25fa210b7627419ae89b2e7ff63ef33cafa213015687adff4d4ce252ff73d
+size 45065
--- fs_mgr/liblp/include/liblp/builder.h
+++ fs_mgr/liblp/include/liblp/builder.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bb56954e6f27be9d506d2f7d9508e456541930b1dd53235008f807bd8a35c7b
-size 15869
+oid sha256:70e59a64ffc8b10fef9f9baa59448d1bd807e14e66fb09ac34866c1a554cdea8
+size 15877
