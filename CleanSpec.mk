888850104c7446b15a99608c04d710b03a994f43
Josh Gao <jmgao@google.com>
2019-08-15 14:05:12 -0700

    Move adbd to an apex.
    Test: adb shell "su 0 readlink /proc/\`pidof adbd\`/exe"
    Change-Id: I84dfe4d1b28b619f98c03a2c8eeef2c783d30af2

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21ebdef5d04e9a875f169dfebeaf3cb46d6994b515261133a2e26491eed5024b
-size 5257
+oid sha256:f51a9778891cb967f48fe24512874555530a0d1b039a6a9f029cedd0f3b35b31
+size 5318
