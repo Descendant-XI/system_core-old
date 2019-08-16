8a9156b68e1df8dfa1557ddb343c8546ec028359
Yifan Hong <elsk@google.com>
2019-08-16 15:00:27 -0700

    libsnapshot: Refactor CreateLogicalAndSnapshotPartitions
    Create SnapshotManager::MapPartitionWithSnapshot that deals
    with one partition so that the code can be re-used later.
    
    Test: libsnapshot_test
    
    Change-Id: Iba36e6c05f8541397323361f7ceb62a6472d2e1c

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f54c17dab7d2fa37b8b4269d42922b3228b7c12e76c24639ca06209138affe50
-size 53349
+oid sha256:4b946a608e9a79eb21969fb22760dea44ae816b8fccc9f020b45522c4d4daf65
+size 54600
