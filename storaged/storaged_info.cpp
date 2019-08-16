47f7fa3e068e91a96e310f21a2cf992dd57be4e2
Hridya Valsaraju <hridya@google.com>
2019-08-16 14:00:04 -0700

    Do not access ext_csd register in debugfs from storaged
    Starting from Android R, platform code cannot access files in debugfs
    since it does not guarantee ABI stability. This CL removes the code in
    storaged that accesses the EMMC's ext_csd register from debugfs as a
    backup if it is unable to obtain the information from Health HAL.
    
    Test: build and boot.
    Bug: 134669095
    Change-Id: I3bfeb3963de5fb5e6bb3eaa9df43baacad164816

--- storaged/storaged_info.cpp
+++ storaged/storaged_info.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2abd5984f5c10e2cb27355391364193194db6e739bb9b656480e7ed08d9fce1
-size 11049
+oid sha256:5b9761e82eb353ac9a6bf1bfdef7fb0df851d1e6e6a46cdcbb48d4e13d88e122
+size 9741
