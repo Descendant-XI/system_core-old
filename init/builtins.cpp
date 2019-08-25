844e2042d37273ff42f94c736083cbb4a6fbc43f
Paul Crowley <paulcrowley@google.com>
2019-08-25 11:54:40 -0700

    Convert fscrypt_set_directory_policy to C++
    Bug: 140027478
    Test: compiles, boots
    Change-Id: Ic0c8b163fe37b11787cab49cc2eea38a1de377e9

--- init/builtins.cpp
+++ init/builtins.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0192d7268cb17dd5346e22a38299c465d314831a2fb635ea71b3591e6e22ec
-size 45099
+oid sha256:bb43e8b0787fd4753ae49f7f669e09f0f435e70b2618253ef46255ddab28405c
+size 45091
