7bf378d59fbd07f22956587be46f1c682de595df
Victor Hsieh <victorhsieh@google.com>
2019-08-26 11:26:27 -0700

    Refactor mini-keyctl and split a static library
    Test: mini-keyctl still works in command line
    Bug: 112038744
    Change-Id: I08006d8befa69e4bf416a2bed9e1813725877147

---
+++ libkeyutils/mini_keyctl/mini_keyctl.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:047887cf16ec709aacbfc0106cecd48a5ec3e4728fcfa3895728270e97f9aa89
+size 5258
---
+++ libkeyutils/mini_keyctl/mini_keyctl_utils.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bc96c1f7cd691c2572ee62b3216ea0f7b6aba8a9fcf5d69c4dd01f031b2ae51
+size 2791
