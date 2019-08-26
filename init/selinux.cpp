385e8df70962bb9f14f3137510b054092547a5e0
Tom Cherry <tomcherry@google.com>
2019-08-26 13:57:51 -0700

    init: send property_set failures to the audit netlink socket
    Bug: 139816248
    Test: see audit messages for failed property set during property file
          reading
    
    Change-Id: I2b6a0448aa4cb494e924070928b0fd0eb5d5c998

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:729374ae550d904c2564aaa9d411acecc7fb768a66282f7f057aae4cfe2b0935
-size 22141
+oid sha256:b1b7a5c51d07029f1e799814935e7c221c0b31e305a3972cbf8b1aa3fd1e69ae
+size 23142
