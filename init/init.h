28265c219efba5f97c7b7a6640eaf095c9e62c41
Tom Cherry <tomcherry@google.com>
2019-08-26 16:33:40 +0000

    Revert "init: run property service in a thread"
    This reverts commit 26f5e7da3a8d99813d1db00bfb04e4ccd49e3221.
    
    Reason for revert: bluecross boot stability issue
    
    Bug: 140009641
    Change-Id: I7ddb9509dfb2c6f644037129aa9d3fb9ff1740aa

--- init/init.h
+++ init/init.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20e76d38ae2de5fefb431296231f7b295ab95acf9dcc67b771bbb1f5c91be6e4
-size 1201
+oid sha256:69f09f45c10c899d1928fcba987fecb4d900f21956312ddcb152ddeca4de876d
+size 1318
