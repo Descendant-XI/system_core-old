9ad3e4e2461ad9b328cb9d466b5cd227bc78a9a6
Alessio Balsini <balsini@google.com>
2019-08-27 18:03:17 +0100

    CreateSnapshot: reduce zero-filling for CoW images
    Instead of zero-filling the whole image, only clean the dm-snapshot
    metadata header of the CoW file.
    
    Zero-filling a large image may take a long time, and this is a safe, but
    not necessary operation if the operation is intended to prepare an
    initial CoW file that will be used with a dm-snapshot device.
    According to the Linux kernel code, only the first 32 bits of the CoW
    file is used to detect if the dm-snapshot device under creation will be
    at an initial state or the continuation of a previous dm-snapshot.
    CREATE_IMAGE_RESET_SNAP is a flag that reduces the zeroing of the image
    file to the first file chunk.
    
    Change-Id: Ibc9fb3b6d19666a92125c988687ff8dacfad47be
    Depends-On: I242e57a9a622fbb738bf82f8a260af1b13810069
    Bug: 139378014
    Test: manual, snapshot_test
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5238bd3e0d44a4d34c18502ba2748bfc4d97e51b9e4220f0dca9bb5285532de9
-size 52846
+oid sha256:416658b28573246db678aef233d8af4ec0140413b5e975fa68962ee55931442f
+size 53946
