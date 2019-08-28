579b2f754318e6ea8c4365c02e3ac3d7c4412feb
Bowgo Tsai <bowgotsai@google.com>
2019-08-28 17:56:51 +0800

    Separate system_ext_sepolicy.cil out of system sepolicy
    Currently system sepolicy can be extended via:
      - BOARD_PLAT_PUBLIC_SEPOLICY_DIR
      - BOARD_PLAT_PRIVATE_SEPOLICY_DIR
    
    To support having a single shared core system sepolicy, those
    customization should be moved to the newly added system_ext partition.
    
    System-ext-specific sepolicy files will be installed into
    /system_ext/etc/selinux/*. system_ext_sepolicy.cil is merged into
    precompiled_sepolicy at build-time. In case precompiled_sepolicy can't
    be used (e.g. system-only-ota), the init will merge this file with
    the rest of the sepolicy at runtime.
    
    Bug: 137712473
    Test: boot aosp_crosshatch with system_ext_sepolicy.cil
    Test: boot aosp_crosshatch without system_ext_sepolicy.cil
    Change-Id: Ib37622271b559d65462de99ff424be831004508a

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51846c37b046500b68dc4b8a962b9d574d3f530bf4c8e0cb3484bb59d10228a7
-size 23170
+oid sha256:9d0cfc29476bde1beff4ce37f5521c3ba7a09f0ac2edffda8cab1eebd35f43fb
+size 24828
