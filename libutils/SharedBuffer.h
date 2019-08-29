2674d11b8037b69df00c19f3777a2dda5c44a179
Vic Yang <victoryang@google.com>
2019-08-29 22:47:07 +0000

    Revert "libutils: Introduce StaticString16"
    This reverts commit d4cb4894348818b90457d94580ed4d611c5d11b0.
    
    Reason for revert: Breaking aosp_bonito-userdebug
    
    Change-Id: Iea72f39d40f476002ce0ad6b5ce3b4e1ca570de7

--- libutils/SharedBuffer.h
+++ libutils/SharedBuffer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:996a2b4a2afbffeaa0fdfbf6d593111b0567df59d14ffd2e92be643d5707abb1
-size 4923
+oid sha256:e7222e0e229f23e58afcbd7200fe6b801dbf7521e6ea8a4f3e30402669a20987
+size 4627
