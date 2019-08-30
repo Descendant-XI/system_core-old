3dc5c4f71bc4d8f2d84847441e1d326ed8b172da
Paul Lawrence <paullawrence@google.com>
2019-08-30 11:11:44 -0700

    Fix typo
    Test: builds
    Change-Id: I4b48e8b3d457382579216482249e734f74768c71

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:729374ae550d904c2564aaa9d411acecc7fb768a66282f7f057aae4cfe2b0935
-size 22141
+oid sha256:41680add75a2207223bbc736b794f74cd51848a58361205d4b58e2d8c9bee443
+size 22169
