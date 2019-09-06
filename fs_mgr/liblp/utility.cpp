599036774aeb35dcae45422e6fc2724f33172cc3
David Anderson <dvander@google.com>
2019-09-05 18:56:43 -0700

    liblp: Add a helper to differentiate empty from non-empty images.
    Also, finally explain the difference between these somewhere.
    
    Bug: 140204341
    Test: lpdump
    Change-Id: I128780b01a28e893afac65f3aa4f4a8d36032d63

--- fs_mgr/liblp/utility.cpp
+++ fs_mgr/liblp/utility.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcb9dffbdfca86b9dedca8f18caa3e1f896c473d1e662ed2d2f1f134866220f8
-size 9799
+oid sha256:3ab5c7f5f7d97e5edde370a8d5b63fefcf64e0b310a72f19010b347d4f6444a1
+size 9818
