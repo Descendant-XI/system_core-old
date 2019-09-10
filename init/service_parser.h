26a529dc10e47e350e5e054ed6fcb4e2e537003d
Tom Cherry <tomcherry@google.com>
2019-09-10 10:40:47 -0700

    init: add reboot_on_failure service option
    This replaces the recently added `exec_reboot_on_failure` builtin, since
    it'll be cleaner to extend service definitions than extending `exec`.
    This is in line with what we decided when adding `exec_start` instead
    of extending `exec` to add parameters for priority.
    
    Test: `exec_start` a service with a reboot_on_failure option and watch
          the system reboot appropriately when the service is not found and when
          the service terminates with a non-zero exit code.
    
    Change-Id: I332bf9839fa94840d159a810c4a6ba2522189d0b

--- init/service_parser.h
+++ init/service_parser.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:441b5d8d6a65e54bb3f03ba6499db2cc20258deaaeeaf1f77c58b4b38e6874b5
-size 4278
+oid sha256:244bc7eaab2bab446f251bb03ddeac102d729a98c9d49bae538d645e1f01fa19
+size 4350
