0a48059cd680ed35e742121f966057ab7a8e2cb0
Howard Chen <howardsoc@google.com>
2019-09-11 18:22:01 +0800

    Mount multiple DSU partitions when present.
    There might be partitions like product or system_ext in addition
    to the system partition. Those partitions can contain dependencies
    required by the system so we need to make the init to recognize
    these partitions and mount them accordingly.
    
    Bug: 140092208
    Test: gsi_tool install & reboot
    Change-Id: Ie5ef063292c3656e79cce9f887f23a8faa1ba2be

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:befc917909dae1e5b41a0647659a8727777713b2d148227cd01a3bf0fc485067
-size 30241
+oid sha256:aa681f402f65bd7801eab5b455df6e1f5ba38cc614f7cc011f7b6d9b2f065b74
+size 32947
