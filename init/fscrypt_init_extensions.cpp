611d18a39c20d548629385aefe79baa28dc01238
Paul Crowley <paulcrowley@google.com>
2019-09-11 15:02:44 -0700

    Create /data/per_boot
    Bug: 140882488
    Test: Booted twice, checked logs to ensure encryption
        is different each time, adb created files in directory.
    Change-Id: I44f746acd1040f7baa9123d4824ba39b194f287b

--- init/fscrypt_init_extensions.cpp
+++ init/fscrypt_init_extensions.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9590aefc0b4f0548bd2a1d529f6f964aee3874307f0cb577c8cec05daaced669
-size 6201
+oid sha256:77d37985bf25c46adaf2e7be7b9906279ff8cf3290eb8bffae3c1396411290e4
+size 6575
