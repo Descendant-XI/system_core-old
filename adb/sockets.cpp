6dc4ff8820a569aef36c2ea1500c67e588f40ddb
Josh Gao <jmgao@google.com>
2019-09-13 00:12:26 +0800

    adb: fix old host transport selection.
    We regressed handling of the old host transport selection syntax, which
    broke users that reimplement adb themselves (e.g. Studio via ddmlib).
    
    Bug: https://issuetracker.google.com/140369526
    Test: adb raw "host-serial:822X0028S:forward:tcp:42929;localabstract:/com.example.ndktest-0/platform-1568299082100.sock"
    Test: ./test_device.py
    Change-Id: Iaaec8fde952316fe9bf2a6f6c6c4a3bc9f74bf72

--- adb/sockets.cpp
+++ adb/sockets.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f6fd40055324ebe911e73d6e5f587c277cd0774099ac1ab43681c354b10ce0
-size 28995
+oid sha256:b854343ecf370f6be04223ebbab3f804cc499cba3beefdd82fbffe9c9b3bf92e
+size 29057
