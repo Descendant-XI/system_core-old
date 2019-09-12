2524b8371720569b3d86566db5d06ec97e727f05
Yifan Hong <elsk@google.com>
2019-09-11 18:15:22 -0700

    fs_mgr: CreateDmTable takes CreateLogicalPartitionParams
    It has too many arguments.
    
    Also fixed CreateLogicalPartitionParams::InitDefaults because
    it doesn't use the provided partition opener to read metadata
    (which breaks tests).
    
    Test: libsnapshot_test
    Test: liblp_test
    
    Change-Id: I74cf8d468274f741c6f3743438fe8855b2aef15b

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43c3a4ae469e7945fb3d316e2a7abb7122221ddfd785bbade791c8ce4cd5490a
-size 60644
+oid sha256:5d179f3cfcb391ee8b7a408cf357cc2ae90b2bc99c6b688f0ecc4dd5efc6dd9b
+size 60303
