01f715328b143d462f47c2f144281916741353cb
Rick Yiu <rickyiu@google.com>
2019-09-16 19:07:17 +0800

    libprocessgroup: add support to set aggregate profiles
    To support setting multiple profiles with one call. The json format
    is as below example.
    
      "AggregateProfiles": [
        ...
        {
          "Name": "SCHED_SP_BACKGROUND",
          "Profiles": [ "HighEnergySaving", "LowIoPriority", "TimerSlackHigh" ]
        },
        ...
      }
    
    Bug: 139521784
    Test: SetProfile works as expected
    Change-Id: Ibe14ed57d5169cafcbcbbdb054df3ed171a2f6a2

--- libprocessgroup/task_profiles.h
+++ libprocessgroup/task_profiles.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03b6651fda7f48feb55b1e55d5eb38b621e13997d00fcb81f47efacf532b0eff
-size 4752
+oid sha256:b5f07a35d5c1baf8a269c6f8d39c816345a2ebe837d6f1cb4762428d813e712d
+size 5382
