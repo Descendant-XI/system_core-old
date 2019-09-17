e76c8db683a6ee72de5601645e1886d9b48dc885
Josh Gao <jmgao@google.com>
2019-09-17 17:34:41 +0800

    adbd: allow port configuration on host.
    Test: manual
    Change-Id: I68cbd9bdb253a68463cc65e63a764ad4fa410c1c

--- adb/daemon/main.cpp
+++ adb/daemon/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd9a46f3544775aff023200929c0f75fa3b1b5dcd07e3ba67be8ca2a0bd87920
-size 10115
+oid sha256:c7c1304570be0c4736c078ad1531dde03725fbb33592f6dfa3cf02a87f69d967
+size 10248
