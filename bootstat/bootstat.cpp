c5eb7c9d2fe0b26b40ec7d6e6a65bae165e1274d
Tom Cherry <tomcherry@google.com>
2019-09-18 09:41:36 -0700

    Simply reboot if the boringssl self test fails
    If this check fails and an OTA or mainline module update has recently
    happened, we want to rollback the recent change.  The easiest way to
    handle this is to reboot, which will trigger the fallback mechanisms
    that are already in place.
    
    Bug: 141082587
    Test: device reboots if self test fails
    Test: device rolls back a recently applied OTA with failing self test
    Test: device rolls back a recently applied conscrypt apex update with
          failing self test
    Change-Id: Iff879deff09d347262dc7a2acadb9164a5029d4a

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:030cf4302ea4d98d7fb85174cfbe6ac610ee57babf4f1dd3c904434add27e7f4
-size 54008
+oid sha256:4d4abbbd7587b5d8abc872824f3b34986aa927155db6ccfc69fe2e55fb686d8b
+size 54057
