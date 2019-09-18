9a41aeb27c3c9c0228be9274b41dc5723e86567e
Steven Moreland <smoreland@google.com>
2019-09-18 12:12:11 -0700

    libutils: increase test TIMING_TOLERANCE_MS
    25 -> 100, because we are seeing larger values sometimes on cuttlefish.
    
    Fixes: 141212746
    Test: libutils_test
    Change-Id: I371416473b97f46891104ff9d893ec38c25728aa

--- libutils/Looper_test.cpp
+++ libutils/Looper_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e870fe7f32b1c1311103e7531bddb64b5b12bf1396fe245839b6e5a740150bf
-size 27794
+oid sha256:de7a920901416fc2a58935df576f7ca09e04bf7adff09633adbc0377641cfc12
+size 27867
