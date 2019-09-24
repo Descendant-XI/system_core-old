375043fc6e0eea850f3b33e7b383f146532bd5c9
Yifan Hong <elsk@google.com>
2019-09-23 18:33:48 -0700

    liblp: convert more functions to use string_view (#5)
    Test: builds
    
    Change-Id: I59ef370489b36410e3eaf90dc2623efaeefd613b

--- fs_mgr/liblp/builder.cpp
+++ fs_mgr/liblp/builder.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f289bf8e822087facc9a611382f0f9dcd712b8cb3cd4e988ad6a613f36139907
-size 47485
+oid sha256:d90861460524bacd37876e0de8acedf9cdfc53519f02e932ea641aacaeb90c51
+size 47483
