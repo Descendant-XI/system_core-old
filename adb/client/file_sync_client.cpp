af50d09b3cfa0f333bfe168337171d5d6f73a8b2
Josh Gao <jmgao@google.com>
2019-09-26 01:49:56 +0800

    adb: fix infinite loop when attempting to push to //foo.
    dirname (on glibc, at least) preserves multiple leading slashes, and we
    were looping until path != "/", which would lead to an infinite loop
    when attempting to push to a path like //data/local/tmp.
    
    Bug: http://b/141311284
    Test: python -m unittest test_device.FileOperationsTest.test_push_multiple_slash_root
    Change-Id: I182b3e89ef52579c716fdb525e9215f1fe822477

--- adb/client/file_sync_client.cpp
+++ adb/client/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0da21c9139989c75b77423436e10c0a883fb29811fc2b79e34d06c704a41d90
-size 43184
+oid sha256:243ac9bf6b19e5627237ee14a6cffe735cd6c12e8c9923f32cf738fa17c19884
+size 43413
--- adb/test_device.py
+++ adb/test_device.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10e7bde02889bff3cbdfdfdbcd6a7e16431aeece19590db93f073381204224a0
-size 62445
+oid sha256:fd5e6de82d2eebe7a23223597cbb5c10a014f68694958ee16c997ac46e67dd8b
+size 62944
