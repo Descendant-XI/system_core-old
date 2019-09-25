1b6609c902e6c56b475c2a11d446ca3bc362c071
Yifan Hong <elsk@google.com>
2019-09-24 18:58:00 -0700

    libsnapshot: refactor CreateUpdateSnapshots
    This function is too big. Refactor it a bit.
    
    - Reorder checks / variable initializations a bit.
      They are independent of each other, so they can be
      safely reordered.
    - Put snapshot creation and initialization to their own
      functions so that CreateUpdateSnapshots looks shorter.
    
    Test: libsnapshot_test
    Change-Id: I135b415d8e046dd91d31326fe7962ae44be4ccf8

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ada1bdc791c54c447e28ae67213397bd48d91e5e57ed4f714cc6325619654ed
-size 78065
+oid sha256:3731b0981e16f79a8abec4aa9ad7042cbafe8e23f913ef255f7badd3fd750116
+size 79429
