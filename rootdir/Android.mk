2dadb238d0d98ba3f4397c51be4eaa9628599fee
Kiyoung Kim <kiyoungkim@google.com>
2019-09-26 17:18:48 +0900

    Deprecate ld.config.txt for fully treblelized devices
    From latest change fully treblelized devices started to use
    ld.config.txt from generator instead of the one under /system/etc. As so
    existing ld.config.txt is no longer required. This change removes all
    ld.config.txt content and build script for it.
    
    Bug: 138920271
    Test: m -j passed
    Test: Tested from Cuttlefish
    Change-Id: I9e2d4b305442a4fccc4378a997ec92bfd6cddb84

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28fe820b4b97b452733ff31c6662ae3d8b30f4298f4eb00d24ed00838cd4057e
-size 16438
+oid sha256:751246786b14333740a0aa8af13f3eb1e87b8bb2bd729c9ae22599d1fc1064db
+size 16197
