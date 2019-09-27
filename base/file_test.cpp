f7812fb96456748410994442b566ad00c7134fa1
Alex Buynytskyy <alexbuy@google.com>
2019-09-27 11:11:24 -0700

    Using WideChar->UTF8 versions of Windows API to obtain temp folder.
    This will correctly handle non-ascii chars in paths.
    +Windows specific tests for non-unicode chars and root of disk as a temp
    folder.
    
    Test: atest libbase_test
    
    Change-Id: Ief3ee26df93e122250441bfe41f0440fe62bfadc

--- base/file_test.cpp
+++ base/file_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e194fe739e50dcaca0df4267372b7f0e8dfa76c476d4e7544f3d8f2bfeb16510
-size 9153
+oid sha256:e09326064f615ddd8f1b4caa6d03ef9f82fda2477ec4e2de7ba8fc06a29d254a
+size 11578
