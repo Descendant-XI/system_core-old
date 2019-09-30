c20b02faff8a6fd9c3f15b166503b8665cc69a19
Tom Cherry <tomcherry@google.com>
2019-09-30 14:06:23 -0700

    liblog: remove unused test #defines and transport code
    We only ever have the default logger now, so we can remove this code.
    
    Test: liblog unit tests
    
    Change-Id: Ic3755d1c807c53192c614860a3cbe985748a3fe8

--- liblog/tests/liblog_test.cpp
+++ liblog/tests/liblog_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6c4a3a879cb5e8c3a9e1bf4c60192434feac69c2735bfc36bd40c8adba37021
-size 97014
+oid sha256:13d0998d8bb9c2923289210d5733785afd6f20e2b92d3e65ac2da6f27527d035
+size 91275
