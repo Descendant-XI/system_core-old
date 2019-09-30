874e7b18583167b7a76fa18ee811b0bbbac75408
Martin Stjernholm <mast@google.com>
2019-10-01 00:41:21 +0100

    Rename the runtime linker namespace following ART/Runtime APEX split.
    Remove the kludge in libnativeloader to deal with the inconsistency wrt the
    name of the APEX package.
    
    Test: Build & boot
    Test: atest system/core/libnativeloader (on cf_x86_phone)
    Bug: 139408016
    Change-Id: I6115b49237c78c2ea4aa943ca4fe0b296b5a2b62

--- libnativeloader/native_loader_test.cpp
+++ libnativeloader/native_loader_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ce5a3065307fa2f03ae8ba5b8fc5bd8694d5d72905f037d81bf7349b8ad8e0f
-size 25781
+oid sha256:31b96b52806047064b9ada10fe21f8703d522137eeae1ffac9a42a00ae2b0491
+size 25749
