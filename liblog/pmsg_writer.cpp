536ae022b8b469ce26ae107414ef01a8a8850eb3
Tom Cherry <tomcherry@google.com>
2019-10-01 16:24:55 -0700

    liblog: remove more endianness functions
    And fix a mistake from the earlier CL.
    
    Bug: 141962035
    Test: liblog-unit-tests
    Change-Id: Id41c47e760f79c0e8bdea54f3644752e2bf7494e

--- liblog/pmsg_writer.cpp
+++ liblog/pmsg_writer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:831ff5ebb50afc379df561f90276bcd11a68bc2bf189948f9565cf4ac76bf50d
-size 7576
+oid sha256:031de3e321c74deecb875d2fafe7d395a78db99339d07ea2fd57edf19e947b76
+size 7577
