479dfcba19d4f5834f20862b4733f694aebca8a8
Tom Cherry <tomcherry@google.com>
2019-10-01 17:29:13 -0700

    liblog: remove a few unused items
    Test: build
    Change-Id: I8a31ad3a9fcd6f74f3a0221d38b64e8e2ea7ea2d

--- liblog/logger.h
+++ liblog/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba4b663b7e95f9496ffcd4e41e33e49342db86500e273618d5a12c865501632b
-size 5540
+oid sha256:c204c5b7497093feda36de266a6a41981d62e086cafca9bffdfa9afcc3759694
+size 5419
