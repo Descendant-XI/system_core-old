e691c9e87add6af8bbab4219f66e4a770370f0cc
Tom Cherry <tomcherry@google.com>
2019-10-01 17:13:43 -0700

    liblog: fix prio and tag validation code
    These checks are not needed; all callers are in this file and there's
    only one possible situation: that we have valid pointers and sizes.
    
    Test: liblog-unit-tests, log.tag.* properties still work
    Change-Id: I8b385d5124c2d93cd99a352e30f745b075c8bd09

--- liblog/logger_write.cpp
+++ liblog/logger_write.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9106d85dcdbbf711445ca9f5c1f8413492cc585403a4ac2d1c8ebba6a648185
-size 14363
+oid sha256:517cc39a0c56fac59877929be0e5f83bcc849517ed5e5a83996b5c17bb8e6cf1
+size 14015
