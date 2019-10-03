684ccc644bd4713cd6fa2af0493cceff3bd75b57
Christopher Ferris <cferris@google.com>
2019-10-02 17:41:16 -0700

    Fix static GetLoadBias function.
    The load bias value set in ReadProgramHeaders is out of sync with the
    algorithm used in the static GetLoadBias function.
    
    Sync the two and add tests to verify that they stay in sync.
    
    Test: Unit tests pass.
    Change-Id: I20ac0104970a22a92a5314a41dcadad0c9c22e64

--- libunwindstack/tests/ElfInterfaceTest.cpp
+++ libunwindstack/tests/ElfInterfaceTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:249760f439c5d00f83805bae33d0a207e57d05530b2df1af5cded4f2d28ab8e0
-size 45373
+oid sha256:247157126ea6831665f64bfe33e3860fe2216d366820226ddde5b86dbf9be311
+size 49593
