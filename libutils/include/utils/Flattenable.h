6402ab655d3392e7ceaf82e729939f765652bea6
Steven Moreland <smoreland@google.com>
2019-10-07 17:31:17 -0700

    FlattenableUtils::align memsets
    Bug: 141890807
    Test: boot, check data is zero'd
    Change-Id: I45aaeac369f4c5cf3eb44f61c233e00f870a5c79
    (cherry picked from commit bf824f8fa50d9f7390057278fefb58e83323a644)

--- libutils/include/utils/Flattenable.h
+++ libutils/include/utils/Flattenable.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a065f767bee75154cca3d935f341db9b2bbd76de99ce93c0704221b88314ecf6
-size 6883
+oid sha256:8922329ea54521674f0042a51ad706cd7d36f3657be85944f1e2a1083111b2c4
+size 7118
