fa598a55b06f6d4527d2ea9f6521f909651abec3
Tom Cherry <tomcherry@google.com>
2019-10-08 09:05:26 -0700

    liblog: remove alarm in logd_reader.cpp
    There is an alarm() call that provides a 30 second timeout in case
    logd is unavailable.  The main reason for this is in the case that logd
    is crashing, debuggerd is ptrace'ing logd, and tombstoned is
    attempting to dump log messages.  In this case, with no other checks
    and without this alarm, tombstoned will deadlock when dumping log
    messages.
    
    However, tombstoned already has two mechanisms to prevent the above
    situation from happening:
    1) It checks that the thread name that is is dumping is either logd or
       starts with "logd." and skips dumping logs in this case.
    2) It does not dump logs if it is running in process for any process.
    
    Calling alarm() or modifying signal handlers from general purpose
    libraries is not recommended either, so without a strong reason to
    keep this, this change removes it.
    
    This also shortens the liblog.wrap_mode_blocks test time, since the 30
    second issue that it ensures does not happen has been fundamentally
    removed.
    
    Test: `kill -8 `pidof logd`` succeeds without delay
    Test: liblog, logd unit tests
    Change-Id: Id8a40544645d220e49f7ba299201af80a0c44de9

--- liblog/logd_reader.cpp
+++ liblog/logd_reader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7c83639fe2924490e759e8b998af054a7a953a51be852d30f92b8d66289bb7a
-size 14118
+oid sha256:7781ddd8df1d48d5d155ebabfb4666cba248d02d3dc56cc6ff1e79fe62e025f8
+size 12663
