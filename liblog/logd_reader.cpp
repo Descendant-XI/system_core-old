0d227a35168823921f18bcbd6528d68ff6bafc43
Tom Cherry <tomcherry@google.com>
2019-10-08 10:18:20 -0700

    liblog: remove code checking for fd = 0
    Fix a typo where I check socket()'s return value for 0 instead of -1
    as an error.
    
    Remove code that checks if socket() returns 0 as the actual fd that it
    returns.  This should not happen in any well formed program.
    
    Test: liblog unit tests
    Change-Id: I1d878e85d9a39155d68c6c84e9cf9b0db8d1b3a4

--- liblog/logd_reader.cpp
+++ liblog/logd_reader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7c83639fe2924490e759e8b998af054a7a953a51be852d30f92b8d66289bb7a
-size 14118
+oid sha256:7d4238e588280fa1e3d578cc92d2bb527b50d3093dee2d23a9a1d0dfe0135b36
+size 13950
