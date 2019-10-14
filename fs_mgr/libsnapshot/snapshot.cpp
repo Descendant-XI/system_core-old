1fff65b02fc7e2e5797e898fa879122b828bc5d3
Alessio Balsini <balsini@google.com>
2019-10-14 20:56:58 +0100

    Publish chunk size in utility.h
    The chunk size of the snapshot should be kept consistent among different
    files.
    Move it to libsnapshot/utility.h to improve its visibility.
    
    Change-Id: I1cae6530a07c88f3a0091ca7d8bb2eb590a6710f
    Bug: 140835698
    Test: m
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dc3870c443814e8c6856649f30e88015421a25b95dc01729feceee095ff56ef
-size 77311
+oid sha256:e6ff284703ae5631f120fe1683ea1fbcfbda04b8a49b3314591b83609a47ecff
+size 77221
