98f7b823827d1424ffe4d01f3bb75e7f021fe84c
David Anderson <dvander@google.com>
2019-10-15 13:14:53 -0700

    libsnapshot: Add a helper for handling data wipes in recovery.
    This patch also fixes a bug where the Initiated state resulted in
    a SNAPSHOTTED state, despite not relevant to recovery or the
    bootloader. SNAPSHOTTED implies the current slot will run off a
    snapshot.
    
    Bug: 139156011
    Test: manual test
    Change-Id: I30dbb0e79906550f08d4cd71194b1361fd5315c0

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78d0861f5cf2c2ccc168435ed7920ad2b5502efdd66066f1161314f5189d5f7f
-size 53490
+oid sha256:04c889917761faac2a1979ec215cb22d62d61c49569a0333abf8cde373aa5216
+size 56375
