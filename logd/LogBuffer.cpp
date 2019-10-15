3a906f6b164b03ee3b6400988ac4939be520117c
Tom Cherry <tomcherry@google.com>
2019-10-15 15:10:26 -0700

    logd: always report the UID of a log message
    logd currently only reports the UID of a log message for 'privileged'
    readers (those with a uid or gid of root, system, or log).  However,
    UIDs are not particularly sensitive.  Much more importantly,
    non-privileged readers can only see less messages from their own UID,
    so this restriction is essentially a no-op, as those readers will
    already know their own uid.
    
    Test: liblog and logd unit tests
    Change-Id: I9da7d15eb840ba3200128391e70d618eec79f988

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc4b36eba87ce3071f87a4a1dafbf46cf07092e951234dc800804a761d7b25c9
-size 43929
+oid sha256:61d51ddb03c0091b1c4ea5ddb0f14462d911e7e3e3f7d20b62156d7fe8339395
+size 43917
