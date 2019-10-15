fe80558cfa12ed857ba2111109f158b71177f22e
Tom Cherry <tomcherry@google.com>
2019-10-15 16:53:11 -0700

    Remove old logger_entry_v* formats
    logger_entry and logger_entry_v2 were used for the kernel logger,
    which we have long since deprecated.  logger_entry_v3 is the same as
    logger_entry_v4 without a uid field, so it is trivially removable,
    especially since we're now always providing uids in log messages.
    
    liblog and logd already get updated in sync with each other, so we
    have no reason for backwards compatibility with their format.
    
    Test: build, unit tests
    Change-Id: I27c90609f28c8d826e5614fdb3fe59bde22b5042

--- liblog/logger_read.cpp
+++ liblog/logger_read.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e37b15bc31135aec17994304f7d1cd5d48bd6b9c2c7c101bdb2f1597e6b0487a
-size 12864
+oid sha256:3444dbad83254f1a9e8f1f090ab90b0637939bbf2c37f01d612b54f1a16681b6
+size 12388
