702b87694c852f65972590f9083a01fbcb867a51
Tom Cherry <tomcherry@google.com>
2019-10-16 10:16:44 -0700

    liblog: remove unused parts of android_log_transport_context
    Neither logMsg nor ret are needed in this struct anymore.
    
    Test: build
    Change-Id: Ic75158153767dd57d56a71b551a2b106cbdab6d7

--- liblog/logger_read.cpp
+++ liblog/logger_read.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3444dbad83254f1a9e8f1f090ab90b0637939bbf2c37f01d612b54f1a16681b6
-size 12388
+oid sha256:96f94e9ffe8384e9bc590e0abb5a0e23e3c98ba2a296c39fff2127521db8766c
+size 12324
--- liblog/pmsg_reader.cpp
+++ liblog/pmsg_reader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d079b9cd84e2b38a62fc7171065b3e313b082f1de5d64fea06861dd2cf4d564b
-size 15398
+oid sha256:1fcb22abad3485843c8d4925f18dd09359f89505aa9f2dbfa623397533c5b7f3
+size 15328
