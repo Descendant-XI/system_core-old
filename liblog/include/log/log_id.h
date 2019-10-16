9e455432662ed440a3055fe7597d6bb8778b48ed
Tom Cherry <tomcherry@google.com>
2019-10-16 15:45:24 -0700

    liblog: fixup log_id_t
    1) We don't need two copies of log_id_t
    2) We don't need misleading sizeof_log_id_t or typeof_log_id_t macros
    3) logd should use android_log_header_t explicitly for its recv buffer
       size
    4) Following on from b/129272512, we're settling that returning
       LOG_ID_MAX is an acceptable return value from
       android_name_to_log_id().
    
    Bug: 129272512
    Test: build, liblog, logcat unit tests
    Change-Id: I67fb964a4a0ae9cb6e1514ca110e47e00dfcfa9a

--- liblog/include/log/log_id.h
+++ liblog/include/log/log_id.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4602c57f6962a61f28a2f3933b4c3d78de59861aa390a69ee570db842e55298
-size 1558
+oid sha256:c9497213aed22bee6ffef2deee0259bd765b1aaa61f165a47f6117303fd00301
+size 1161
