7a4f2d7db96994b3b0c3054d77deaad870be36c1
Tom Cherry <tomcherry@google.com>
2019-10-16 17:12:39 -0700

    liblog: remove checks for __GNUC__
    We already assume this in many other parts of the build.
    
    Test: build
    Change-Id: Ie685e83963eac942d0d176bb2394b5d3a5b86cac

--- liblog/include/log/log_id.h
+++ liblog/include/log/log_id.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6e698e87034cd2e9beedf2ad10e35c8230d1f47aa5c442d897f151bb42022a0
-size 1620
+oid sha256:c4602c57f6962a61f28a2f3933b4c3d78de59861aa390a69ee570db842e55298
+size 1558
