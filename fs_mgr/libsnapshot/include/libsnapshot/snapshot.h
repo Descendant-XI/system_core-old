024388a74dd928c45382f434c915bdf334bc7e69
Yifan Hong <elsk@google.com>
2019-10-17 18:25:04 -0700

    libsnapshot: Add EnsureMetadataMounted
    In recovery, client is responsible for calling
    EnsureMetadataMounted before doing (almost) all
    operations on SnapshotManager, e.g.
    
    - CancelUpdate() before sideloading
    - BeginUpdate() on retrofit Virtual A/B before sideloading
    - Finishing merge before flashing
    
    Test: libsnapshot_test
    Test: recovery sideload
    Bug: 140749209
    Change-Id: I1034a7fa74e31b6850896e61e86341239dbf2699

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1f250de31df1c7c05b4dc2ced787fab7884c8c158d39a77915b76b272eca99b
-size 18676
+oid sha256:a023259304b575ad118697bd6549669718e31693cbada2f8470618d79aecaabf
+size 19599
--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02b19ea14c54462ec40a6028ce40ed52be86d0c775bd165259da539b0b13676a
-size 79340
+oid sha256:864891bb65ee5c9e3bce2775d64e452bad0a5e3cd7fe4c848be318bc5aad7954
+size 79892
