8e2bc4998cca56f5dc55354b7c60948d322dea53
Christopher Ferris <cferris@google.com>
2019-10-21 18:59:42 -0700

    Fix handling of PT_GNU_EH_FRAME.
    The bias for the PT_GNU_EH_FRAME was using the paddr instead of vaddr.
    This doesn't match the way the load bias is calculated, which always
    use vaddr - offset, so change to use vaddr.
    
    Found on an old x86 device that has a vdso that sets vaddr differently
    from paddr.
    
    Add a new offline test to catch this case and update the elf interface
    unit tests.
    
    Also, fix a small bug in the unwind_for_offline tool.
    
    Bug: 142365899
    
    Test: Unit tests pass.
    Change-Id: I5f0bf062dd8ee45aa8553189ba493ec962e0b059

--- libunwindstack/tests/ElfInterfaceTest.cpp
+++ libunwindstack/tests/ElfInterfaceTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f885e1ce0221368bd522e93f5d880571ddbe8fb3e67864f41d9ea98785ed5fe2
+oid sha256:20b3cbecca17231d5d8c56957150dc7bee67ce5abc31ecfa420238acfae374d5
 size 61969
