33af8ad508f673ae76f00430fdd9c0d90d5aeef3
Suren Baghdasaryan <surenb@google.com>
2019-10-22 17:12:01 -0700

    lmkd: Support process types when registering a process
    Add an optional process type field into lmkd registration protocol so that
    applications can be distinguished from services.
    
    Bug: 129011369
    Test: boot and verify native service registration
    Change-Id: Ie610b5d07cbe247a55ab31bc079ee5c5923bea11
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- lmkd/include/lmkd.h
+++ lmkd/include/lmkd.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb081be059a96bac909e7fff9db143a9fadc6ee2308b12ed18c3aff7236f55c4
-size 5934
+oid sha256:8081527a5befd35cc3f84ec2546dc9a32fb69a650c7113b22d04efff58f6a491
+size 6394
