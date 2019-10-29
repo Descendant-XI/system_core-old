0e0fd7a9656cc3b6935318e34112c544cafe7116
Elliott Hughes <enh@google.com>
2019-10-28 21:35:52 -0700

    zipinfo: support DOS attributes.
    golang doesn't include Unix mode by default.
    
    Also show all the deflate variants ("defN" versus "defX").
    
    Cope better with being called directly rather than via symlink.
    
    Test: manual
    Change-Id: I23b441c847ce9a557ea866b3c43bdf0542b26f10

--- libziparchive/include/ziparchive/zip_archive.h
+++ libziparchive/include/ziparchive/zip_archive.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b48a42e99d497eb76c442003263fec66a4a86376c9bae5d1094b5567149f87f
-size 9193
+oid sha256:5920b6df0bcce8639e76d45c0638e5a34ffc78c6326a58feb380c72fcb90cab4
+size 9542
