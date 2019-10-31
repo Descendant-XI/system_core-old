ae66aead9c68c63f240aa64b6b0b13247bbd4848
Callum Ryan <callumryan@fb.com>
2019-10-31 07:21:42 -0700

    Allow the adb server to bind on ::1
    Currently the adb server can only bind on 127.0.0.1 or * if -a is
    provided. This diff adds the ability for adb to bind on ::1 as
    well for use cases where you might want to force adb to be IPv6
    only.
    
    To bind the adb server on ::1
    ```
    $ ./adb -L tcp:[::1]:1234 server
    $ lsof -nPi :1234
    COMMAND ... NODE NAME
    adb     ...  TCP [::1]:1234 (LISTEN)
    ```
    The original behaviour is also retained, so this would only affect
    users explicitly specifying ::1 in the socket spec
    ```
    $ export ANDROID_ADB_SERVER_PORT=1234
    $ ./adb server
    $ lsof -nPi :1234
    COMMAND ... NODE NAME
    adb     ...  TCP 127.0.0.1:1234 (LISTEN)
    ```
    
    Note: I've only implemented this behaviour for posix base systems,
    due to my limited understanding of networking on windows. If
    needed I can do some research to implement it there as well, but
    there is currently no IPv6 support at all on the windows side.
    
    Test: New unit test and the commands in the summary
    
    Change-Id: I23e4531e8dfda4de9348124ad7491d728aecdbf7

--- adb/sysdeps/posix/network.cpp
+++ adb/sysdeps/posix/network.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03cce4d98003cfc285a60fff8b55043253e12f9e6c07368efafa706dba4ead79
-size 4693
+oid sha256:8f518269061cff8eb1dd617d5088fe2174c50f467ed0b1ce9a2362b27bb7fd02
+size 4796
