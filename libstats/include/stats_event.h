72bd74ad923d43914e5a4dbd00976a3bd685a19c
Ruchir Rastogi <ruchirr@google.com>
2019-10-31 15:55:23 -0700

    Move type ids to stats_event.h
    This allows the parsing code within statsd to access the type ids.
    Also, we move stats_event.h to the include directory for consistency.
    
    Test: m -j libstatssocket
    
    Change-Id: I1db7fc9e9d0efd2225612353a8fc49bb13220ff0

---
+++ libstats/include/stats_event.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd465f87719516085a202ba89730ced81996a5a7513a22838d8aaae82dc32752
+size 3876
--- libstats/stats_event.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0fe13bc791fcc5d6d9c5947e421001c18776332dc04fcbacf7d410108ddc5f4b
-size 3569
