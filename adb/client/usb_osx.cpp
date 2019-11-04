4fee257977a7919ea4b7c6358e9ca224efc8026f
Daniel Colascione <dancol@google.com>
2019-11-04 11:33:58 -0800

    Properly finish daemon init when initial USB scan complete
    Call adb_notify_device_scan_complete when we finish enumerating USB
    devices. The original intent of the code appears to have been to have
    adb_wait_for_device_initialization return as soon as we've finished
    looking around at the system USB environment on daemon startup, but at
    some point, we forgot to actually notify the init thread that we
    finished scanning all the USB devices, forcing the wait on init_cv to
    time out after three seconds on every daemon initialization.
    
    After this change, the daemon starts in a few milliseconds on my Linux
    machine
    
    Test: killall adb && sleep 1 && time adb server && adb shell ls
    Change-Id: I0bc1da7a597d2077dd2b591560d03798b05905b7

--- adb/client/usb_osx.cpp
+++ adb/client/usb_osx.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d64a914f88d15130bc0028a7f1ccdd7438e08121e74f0c50259ceb6eaa31145
-size 19619
+oid sha256:33424d8ca84e3c367e631d7ed89a376a0c3265eac62c6960d38dacff97188b02
+size 19662
