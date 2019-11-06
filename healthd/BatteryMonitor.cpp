483278b76231d6b62868281dbd469970b89ca390
John Stultz <john.stultz@linaro.org>
2019-11-06 00:23:34 +0000

    healthd: Fix incorrect return check
    In commit 1d4368b4943b ("[REFACTOR] healthd: BatteryMonitor
    use health 2.1 types"), the mapSysfsString() function was
    changed to return NULL instead of -1 on error.
    
    Everywher else that function is used commit 1d4368b4943b
    tweaked the return value check from (ret < 0) to (!ret),
    but one spot was missed.
    
    This patch corrects the one missed check so that we compare
    against null rather then negative value, since the return
    value can no longer be negative.
    
    This issue was found by inspection, and I've not seen an actual
    problem with it, but thought it should be corrected.
    
    Change-Id: I0a6564274d01276352a5dc0660b8ffbe748b5fde
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b649b4048166c88a0e3aca64b901c59002b37888912e595ca255cdea48b4438
-size 24261
+oid sha256:0bb49ee5b10b081cc7a3aea25274573ae915006ab67b84beda0c221f4d207d98
+size 24258
