17a476c59b5243ba6d43325232d4e24dc37779ba
steven_fann <steven_fann@realtek.com>
2019-11-06 19:19:47 +0800

    Allow fuzzy_fastboot test devices over internet
    Bug: 144667236
    Test: fuzzy_fastboot --serial=192.168.1.104:5555 --gtest_filter=*Logical*
    
    Change-Id: I02e62f706d0a3a19a9b7b56788abe05759d2d63d

--- fastboot/fuzzy_fastboot/usb_transport_sniffer.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f42f6fddad83439f5ca20d8363b315d27011bc559f48f156748ba37a81c2b79
-size 3166
--- fastboot/tcp.cpp
+++ fastboot/tcp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45653b4b3236d037423e8974d6a4d714dd2eb86d59283c7768d8024504a0829
-size 6697
+oid sha256:493bcd73992404318ebcfba83f1aba85019ba7bc1a70f282bf04b601bf9a9218
+size 6768
