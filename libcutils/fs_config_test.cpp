9189f29e9c9c939eb3b9529e0308b6a28d8388fa
Tom Cherry <tomcherry@google.com>
2019-11-06 09:29:56 -0800

    libcutils: update fs_config tests for current behavior
    Update fs_config tests for the new behavior in
    a8eb00720ca0ea02ace5942e54860c07ca35a14f.
    
    Before the above CL, fs_config_cmp() would match any partition prefix
    to any path, even if there is not a logical relationship between them.
    For example, these two lines in the test the demonstrate the changed
    behavior:
    
    { true,  "vendor/lib",             "system/vendor/lib/hw",    true },
    { true,  "system/vendor/lib",      "vendor/lib/hw",           true },
    
    The first line should match and does; it is testing that files located
    at system/vendor/lib/hw are matched by an fs_path_config entry
    specified as vendor/lib.  This is to allow for applying the policy we
    have for the vendor partition to files in /system/vendor in the case
    that there is no vendor partition.
    
    The second line should not match.  This is testing that a file
    that's located at vendor/lib/hw is matched by an fs_path_config entry
    specified as system/vendor/lib.  This is backwards; we do not want to
    have policy specified for system/vendor to impact policy for the
    vendor partition.
    
    Also, we never have any relationships from /system/oem to /oem or
    /system/odm to /odm, so these are logically unrelated and should fail
    to match.  We do however have a relationship from /vendor/odm to /odm,
    so this test is added.
    
    Test: libcutils unit tests pass on CF
    Change-Id: I026f0233e00bbd0aad9bc0fb701aef000d2a037c

--- libcutils/fs_config_test.cpp
+++ libcutils/fs_config_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcf0cc5c7e150295b5e176b53034cc27f4a024e2d7fc490a1e294e8ff8dbb940
-size 10659
+oid sha256:2a7da1a85d87a208a88bb00b59557f8efdd3cb04f028cea805d87ca41254895a
+size 10734
