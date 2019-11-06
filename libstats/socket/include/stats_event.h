2f89d05175c8df258854e7366b667e31cc708cc6
Muhammad Qureshi <muhammadq@google.com>
2019-11-06 15:15:23 -0800

    Create stats_buffer_writer
    Move iovec* construction and noteLogLoss from stats_event_list to
    stats_buffer_writer.h.
    Expose write_buffer_to_statsd that takes in a byte array and writes
    it to the statsd socket. Currently exposed for StatsLog.write JNI call.
    
    This change also allows getting rid of stats_event_list once all clients
    have migrated to stats_event
    
    Bug: 145619049
    Test: m -j libstatssocket
    Change-Id: I0048e392c2f5039eb70dacf4e91a43d3f32e8749

--- libstats/socket/include/stats_event.h
+++ libstats/socket/include/stats_event.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43194e5298142971c669e04ffb07754007de748151a5c3626cc591672a299739
-size 4949
+oid sha256:ec9a15fd1b53cbd86d32fc7e93feed69c1536bd0f817fb8cab7ad7f5d631598b
+size 4981
