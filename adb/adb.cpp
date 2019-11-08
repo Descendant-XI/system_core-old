9c596c2e2f3f7b6ae90023d83475467f8ff9e872
Daniel Colascione <dancol@google.com>
2019-11-07 20:48:21 -0800

    Delay initial accept() until server initialized
    When the adb client starts the adb server, it waits until the server
    reports that it's fully-initialized (via reply-fd) before executing
    its adb client operation. This wait prevent that adb client from
    talking to the server while it's initializing and becoming confused.
    But if a *different* adb client connects to the server while it's
    initializing, *that* client can temporarily observe unexpected state
    while the server initializes itself. For example, such a client can
    observe a device that's alive and connected as being offline while the
    server connects to it.
    
    The new socket activation support makes this race more apparent, since
    in the socket activation configuration, there's no initial adb client
    waiting for the server's all-clear indication and so even the first
    client observes the partially-initialized server state.
    
    This CL prevents the server accepting *any* client connection until
    the server has fully initialized itself, preventing all clients, not
    just the initial client, from observing a
    partially-initialized server.
    
    Test: test_adb.py; test_adb gtest binary
    Test: [with socket activation] adb kill-server; adb devices
    Change-Id: I5d399ee62436eee63340b6b8b0f64131ad17ac65

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f878e8a2cf5c4c0a6c470bb27c5b55ccb5544e3d17f33bf8090cd663d801c3ef
-size 48930
+oid sha256:2eac2287c3d2fe2669d16cc61ce4c9050462e16d810ecac65d6c3c1bfa7a00c7
+size 49049
