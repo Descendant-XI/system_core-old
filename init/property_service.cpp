12446ac91eb8e8c0fa97a0f8ff372e920db21d00
Tom Cherry <tomcherry@google.com>
2019-11-08 17:54:27 -0800

    init: check property type in host_init_verifier
    We have all of the 'type' information for properties available during
    build time, so let's check this when setting properties in init.
    
    Test: setprop apexd.status bad results in:
    host_init_verifier: Command 'setprop apexd.status bad'
    (out/soong/.intermediates/system/core/rootdir/init.rc/android_x86_core/init.rc:927)
    failed: Property type check failed, value doesn't match expected type
    'enum starting ready'
    host_init_verifier: Failed to parse init script
    'out/soong/.intermediates/system/core/rootdir/init.rc/android_x86_core/init.rc'
    with 1 errors
    Test: CF builds without that error
    
    Change-Id: Iaad07747c09f4a10b2b816c455d6e8a485357ab9

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e721684587d7e424339ee03eb4b15e51edf629dcb80cfd42bdf238794b10e2ca
-size 40483
+oid sha256:d1297560349efa98ca520658d86b8bf7d11b67750dd91de8baf02d92edfaf535
+size 40464
