a8597e3195f4ea843f0614d28e4b3eac2c62d7f2
Orion Hodson <oth@google.com>
2019-11-14 19:24:10 +0000

    Add cpu-set properties to serializer test
    Bug: 141446571
    Bug: 149395059
    Test: atest propertyinfoserializer_tests
    Change-Id: I1232fec5f1d0be7e5fb6aa5c46f6c2c5529bfc6c
    Merged-In: I1232fec5f1d0be7e5fb6aa5c46f6c2c5529bfc6c
    (cherry picked from commit 8e77b789f884b5ab43945569132af1ced8e33f37)

--- property_service/libpropertyinfoserializer/property_info_serializer_test.cpp
+++ property_service/libpropertyinfoserializer/property_info_serializer_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a868ce95841bc51d4aec6555a72bdbbcdb5cd9228288222fd617ca8632a2cef
-size 47389
+oid sha256:7685f568fea88ede38ad35c0adcea5ddef2b9b5cdc6931f52db36c67bd0dd3da
+size 47598
