c8d7cdf197ab045302b78df73b9a8cc752fd4815
Daniel Colascione <dancol@google.com>
2019-11-14 00:48:36 -0800

    Expose ParseBool from libbase
    Also change the properties implementation to call the new API.  We use
    this ParseBool API in the new SystemProperties implementation, with
    which we want the libbase property API to be consistent.
    
    Test: included
    Change-Id: I89cb3eb4e1203a6bb0da41914dad720e44c00303

---
+++ base/parsebool.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c67cbf09b096a9c759298782211c71ac5991adabd2ea75b4b2b15f541a38cab8
+size 1069
---
+++ base/parsebool_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecfae90bb93a87cb90fbef753164c4cd0aef693cb0353a7268486a04cac88de4
+size 1315
