5272379fbb62e1713d704d1aa230f41f3334d710
Greg Kaiser <gkaiser@google.com>
2019-11-19 06:53:22 -0800

    liblog: Assure shifting behavior is consistent
    Shifting a signed 32-bit value by 31 bits is implementation-
    defined behavior.  So we change to an unsigned value for our
    shift by 31 bits, and go ahead and change the others to
    unsigned for consistency.
    
    Test: TreeHugger
    Change-Id: Ib98f9b1e468d28dafd09e86273bf76beb1ea1fa5

--- liblog/logger.h
+++ liblog/logger.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba264a5a670229acfb626ca2b0e6118f5e3c953e9266fd36c272ee316f2724b2
-size 3426
+oid sha256:aa6e23a069575cc0b6de0b445a838499e22747bc32f60b34e06bd65b5a7e31d3
+size 3429
