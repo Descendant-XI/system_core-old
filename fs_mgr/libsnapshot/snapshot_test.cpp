b07cd7465a4cf6835eeb470c043f8d84b25a6be3
Yifan Hong <elsk@google.com>
2019-11-26 13:20:42 -0800

    libsnapshot: Fix test MergeInRecovery
    After MergeInRecovery, metadata is left unmounted.
    Mount it before doing any tests, and calling CancelUpdate
    in SnapshotUpdateTest::Cleanup().
    
    Test: libsnapshot_test
    Change-Id: I194d15cfb6b8f529dfd5490ff3bb650b403b244f

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b251d1c043f6b0b29df81b7e13e3bdeb3f7265e1f2b12bcc33d47dcf2bbac95d
-size 56383
+oid sha256:8fe63f0302ffcd1756246b56d6d4383f8abe0a5e36e655da23b20e03544fa27d
+size 56519
