989d601899dd39b940e8d3a16e3208453389948d
Yifan Hong <elsk@google.com>
2019-11-26 11:29:28 -0800

    libsnapshot: Check for snapshot status none
    NONE is an invalid state, and should be considered
    as "no snapshot".
    
    Test: libsnapshot_test
    
    Change-Id: Ie13b1b157dc79ed639ab6ac19e28abb61b064221

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8744455f38abca0356de0a5d9a04d6ade991b70f63b19888c1cf84fb10b9542
-size 87045
+oid sha256:ef4f99c0638011b3c33b777e8c865640600839ecffa75c75d06c5c37c8cf8820
+size 87741
