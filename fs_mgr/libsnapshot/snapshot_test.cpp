5e9141d4ee3cd333fc6bb8971ea4b4030b51b040
Yifan Hong <elsk@google.com>
2019-11-26 13:25:27 -0800

    libsnapshot: Fix DataWipeRollbackInRecovery test
    HandleImminentDataWipe left /metadata unmounted in recovery.
    Test: run it
    
    Change-Id: Ifc76203e46cf5e783d4badf7f8e3e8d0b78eaeb0

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe63f0302ffcd1756246b56d6d4383f8abe0a5e36e655da23b20e03544fa27d
-size 56519
+oid sha256:cc22e2d8cc8b7f066e6c1139fe154584d79991980459f48bc9e0199bc4a5b636
+size 56615
