72d5458d656a28bb0641513c5c5ec0f2499e014d
Yifan Hong <elsk@google.com>
2019-11-26 12:40:48 -0800

    libsnapshot: Fix test UpdateBootControlHal
    Test: run it
    Change-Id: I885404184f35a8d6ecb2fe267bd48b25f1164e20

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b251d1c043f6b0b29df81b7e13e3bdeb3f7265e1f2b12bcc33d47dcf2bbac95d
-size 56383
+oid sha256:20f388e588cbd766fa4568db0ec741adde727b92b6d7b282eed74ca306ce832f
+size 56376
