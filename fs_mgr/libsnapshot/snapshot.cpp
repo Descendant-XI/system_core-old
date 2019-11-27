b60acdc89e8eed4148088feddeebfb056b95faf4
David Anderson <dvander@google.com>
2019-11-26 17:18:24 -0800

    libsnapshot: Remove all image data and metadata when cancelling an update.
    If for some reason an lp_metadata file becomes corrupted, this allows
    the OTA state to be wiped completely clean.
    
    Bug: N/A
    Test: manual test
    Change-Id: I194c99dc9a7af624a31b564435305892bb3cf48a

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:660b54df671864471589a7ae7dcd0db21e6a2068fe934c44e5780c9b120fab26
-size 87669
+oid sha256:f2ba7b917ccd944cdd74bff9310beb70fba91d2a8ea93dfc0350b9c016895765
+size 88680
