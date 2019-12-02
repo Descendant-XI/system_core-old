6632769a6350ff5a3e05a0aa35f27159d75ccce8
Yifan Hong <elsk@google.com>
2019-12-02 11:48:49 -0800

    libsnapshot: Add test for accounting for hash tree
    Test: libsnapshot_Test
    Bug: 145180464
    Change-Id: If8546dea89fdd7ec7499522a232a777699c52d82

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04eb9124c78144b018193dcc298cdbe980e1ad529180df28db1a143482f2176d
-size 56608
+oid sha256:5fa95bf8ec1ad72f45c5272cfe84f619f28b065b6330484a269cc38ed3a36948
+size 60158
