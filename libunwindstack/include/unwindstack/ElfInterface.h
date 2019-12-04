a859ae229a0f21b77c12c12a93d5f0048c106489
Christopher Ferris <cferris@google.com>
2019-12-03 17:13:49 -0800

    Fix support finding global variables.
    The code was not properly getting the variable addresses and using
    the offset and address fields of the .data section.
    
    Fix all of that, and update the tests.
    
    Bug: 145162678
    
    Test: Unit tests pass.
    Test: ./art/test/run-test --dex2oat-jobs 4 --host --prebuild --compact-dex-level fast --jit --no-relocate --runtime-option -Xcheck:jni  137-cfi
    Test: ./art/test/testrunner/testrunner.py -t 137 --host
    Change-Id: Ic61c4487334fd2273cda9c56eb1a3b525a03edb7

--- libunwindstack/include/unwindstack/ElfInterface.h
+++ libunwindstack/include/unwindstack/ElfInterface.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0440491eaa211ecc3392e5b65c8e551569164af79ea567b687987f7e2a568ea5
-size 7713
+oid sha256:8f3ca9f7408aa1b9654c1e9661f78cd065c3ff3eb3701c4c6fe8de3ddc0011b8
+size 8008
