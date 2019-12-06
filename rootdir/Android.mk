bd8993c3c0d984027718e8fce8abd6a88000964b
Oliver Nguyen <olivernguyen@google.com>
2019-12-06 15:58:04 -0800

    Set Clang coverage environment variables.
    Bug: 143977934
    Test: m CLANG_COVERAGE=true and check LLVM_PROFILE_FILE is set
    Change-Id: I5aea5a38b9bec1670bf9c8d11d9c466c3b83e4d8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7ed779ee8605de5e4877a32fbbf9cb96241006c49b9e86375d6559b7193ff4a
-size 11411
+oid sha256:bc4639dddd26dbb917ec2ce2b994e5636206f1f55aaf7ed30a8831f913e8d73d
+size 11699
