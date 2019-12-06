fbac34e183140a174985c0a9ac24900aef042894
Tom Cherry <tomcherry@google.com>
2019-12-06 10:25:37 -0800

    logcat: more error printing clean-up
    * The --help text is way too long to print after each error, so simply
      print the errors and exit.
    * Report errno in a few cases where it was previously not reported
    * Fix more punctuation
    * Fix '?' and ':' getopt_long() return cases for long options.
    
    Test: errors look better
    Change-Id: I57058a2250e9f3c3431f104e43f0eb5ec60d8c8a

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66ffa11910c47c819d85a199cd58bf76c7f5b3841c6618dfdd6bc96bf5f807c2
-size 47778
+oid sha256:e7d2fcafcf14b81eed6ced866205e51217030787ade72291c7f300afd619afe7
+size 47143
