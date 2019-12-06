412de621f98524bfc57e8e88072e706337fdb737
Bowgo Tsai <bowgotsai@google.com>
2019-12-06 11:44:18 +0800

    Allowing the fs_mgr flag of avb_keys to be a dir
    Hard coding multiple keys in the avb_keys flag isn't flexible and
    causes some pains when upgrading an Android codebase.
    e.g., from Android 10 to Android 11.
    
    This CL supports specifying a directory for the avb_keys
    for fs_mgr to list then use the avb keys under the directory.
    
    Bug: 144399552
    Test: config a fstab using avb_keys as a dir to boot
    Test: atest libfs_avb_test
    Test: atest libfs_avb_internal_test
    Change-Id: Ie74845d8c8e4aa45e8a9e3b862424cec641f8090

--- fs_mgr/libfs_avb/util.h
+++ fs_mgr/libfs_avb/util.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab235090f6924e0221c2eca9f7be4c4581df328655f611946ac060679fe2e509
-size 1902
+oid sha256:9799707ca8b45e46a0df68b5f24ca5c123e5752b87787e580bad89d2279c9f98
+size 2151
