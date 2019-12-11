8ffe764620d844b98e7b2d6198c479594e567f50
Ruchir Rastogi <ruchirr@google.com>
2019-12-10 18:46:23 -0800

    StatsEventCompat
    StatsEventCompat is a wrapper library that toggles between the old
    logging scheme and the new logging scheme. It was designed particularly
    for the DNS Resolver module and should not be used by others.
    
    We will merge libstatspush_compat and libstatssocket_q in a future CL.
    
    Test: m libstatspush_compat
    Test: bit libstatspush_compat_test:* (passes on Q and R)
    Bug: 145534143
    
    Change-Id: Ib355031f3573101ea90bd8694861fbfc33b0c788
    Merged-In: Idf35ccb6669798166475f08b2fbab40534b5db19

---
+++ libstats/push_compat/include/StatsEventCompat.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:466865af9b662445ea612906cfe037b6c82d8250f3a4a42ccda65e6bb4d45778
+size 2225
--- libstats/socket/include/stats_event.h
+++ libstats/socket/include/stats_event.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bf716b73533897299b6662bc5b2b724e0ec79ce189f32e53549dbc273fac698
-size 4999
+oid sha256:8da2cce2b7cf542896e6d46a4193fe6b60b2936756d5f61dc16a5ecfa800182a
+size 6236
