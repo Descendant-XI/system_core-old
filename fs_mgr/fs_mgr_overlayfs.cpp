eceeb0797c5a51143c61488b02ec5b9d1c9d17f5
David Anderson <dvander@google.com>
2019-12-10 21:18:28 -0800

    remount: Refactor fs_mgr_overlayfs_teardown
    This pulls code for mapping the scratch device into a separate function.
    It also avoids implicitly failing by passing an empty device string.
    Finally, it calls GetScratchDevice, to remove a caller of the deprecated
    method fs_mgr_overlayfs_scratch_device().
    
    Bug: 134949511
    Test: adb remount and adb_remount_test.sh
    Change-Id: If4a543d3fa26af3f8578ec8b236859c8e4d9bfd8

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:143686aca571bd71dd12da638b6fcce22b51f740659f289e2791e918dc38eb2e
-size 48494
+oid sha256:9c64027eacbcca7803a298161222ed2bc8da81fc79cc6132de0baa250fdcd131
+size 48743
