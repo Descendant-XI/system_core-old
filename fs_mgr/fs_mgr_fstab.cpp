4e8b2321ccaf605f6295581483979f62fd931262
Bowgo Tsai <bowgotsai@google.com>
2019-12-11 15:14:00 +0800

    Allowing /avb/q-developer-gsi.avbpubkey for DSU
    Adding the key to allow a device boot a Q-Deverloper-GSI.
    
    Bug: 144399552
    Test: boots a developer Q-GSI on a Pixel device
    Change-Id: If79149689afd4b662c4cae3a8b48e3a5dc57d00c
    Merged-In: I6bc1e068368e64cb0bbf6ef63805444b35a1fdfb

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3080077ce0b48023db9ef6ac8e44012581b3b30413dff8ea7532eb4a479d632
-size 29603
+oid sha256:aa3ad9d395bfc9598903114e46f483266c0f786ae25056e0d1c9eecd5c261453
+size 29781
