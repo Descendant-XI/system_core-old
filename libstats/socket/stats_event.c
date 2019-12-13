6f6f58da0d3f0b5f3d9c4df4d10d47984b0f14e1
Ruchir Rastogi <ruchirr@google.com>
2019-12-12 17:16:59 -0800

    Improve stats_event memory usage
    We now truncate the buffer to the appropriate length when clients call
    stats_event_build().
    
    Benchmarking tests indicate that truncating the buffer to the
    appropriate length increases the cost clients pay to write to the socket
    by 2%. This is negligible enough that I decided to truncate the buffer
    for both pushed and pulled atoms in order to simplify the API.
    
    Test: m libstatssocket
    Test: bit libstatssocket_benchmark:*
    Bug: 144126231
    Change-Id: Ife976bb383ecff8de5064730692a95e2a3a82c9d

--- libstats/socket/stats_event.c
+++ libstats/socket/stats_event.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f616887c7f6faeea98c755ddf270815ba37aba67cfc4d6628d0ae9bf1c7c3135
-size 11343
+oid sha256:7451117f0f39f16ec09b4a6336ceb1fad46a66bbb32b016820d0ad5b14ec71ec
+size 11654
