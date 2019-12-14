6749659a5b011da16ba72817b37aa56cf96de4b3
Yifan Hong <elsk@google.com>
2019-12-13 19:42:52 -0800

    libsnapshot: SnapshotUpdateTest::AddOperation
    Reusable test code.
    Test: libsnapshot_test
    
    Change-Id: I00a1a460d84c01b0dbd24b668293df87962e8b00

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:029412ffdada854f3af0e05bc883e149bb63294745bbfba01287e7f8f4fe8a22
-size 65207
+oid sha256:d0f40b9ff03a7930507d6c0269b591d135dd5579380dbb8d781008fa77818526
+size 64935
