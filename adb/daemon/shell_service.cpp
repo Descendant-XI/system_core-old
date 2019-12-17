160e2b70c4609840aa7529502b5b977c28bb108b
Josh Gao <jmgao@google.com>
2019-12-16 17:13:51 -0800

    adb: use poll instead of select in shell_service.
    Bug: http://b/141955761
    Test: test_device.py
    Test: adbd_test
    Change-Id: I8976304a1011e81e85f8d90d95d36ecd82834f5f

--- adb/daemon/shell_service.cpp
+++ adb/daemon/shell_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5ac886d9a6c06b57faa8ab3758e6684bcee944eab7fe7ca4ab34e3ad60e786
-size 34179
+oid sha256:91bbf1f3cee5952e1fb2bbf5a4a23eb5d252b4ebc2364e99840d733952fda379
+size 34629
