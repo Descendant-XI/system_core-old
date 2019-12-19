5055da34c22aef53754a3b9dd293fe244a87250d
Yifan Hong <elsk@google.com>
2019-12-19 10:45:54 -0800

    libsnapshot: no underscores in test names
    This is required for gtest.
    Test: run it
    
    Change-Id: I9b0d7ee651c283c00035104d4257d8e8addf4db0

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:029412ffdada854f3af0e05bc883e149bb63294745bbfba01287e7f8f4fe8a22
-size 65207
+oid sha256:2af7c43ebd4205c6660247ae607180414f934df9f3782f6273b579981e305af8
+size 65190
