30d2f1c461f43389f8fb1e1d394d589579decc99
Nikita Ioffe <ioffe@google.com>
2019-12-20 16:34:48 +0000

    Unify logic for resetting properties before userspace reboot
    Since I was there, added two more properties to reset, and switched
    ordering of sys.init.updatable_crashing and
    sys.init.updatable_crashing_process_name setprops to make sure that
    process name is already set when apexd/PackageWatchdog get's notified
    about sys.init.updatable_crashing.
    
    Also fixed a typo in what HandleUserspaceReboot function.
    
    Test: adb reboot userspace
    Bug: 135984674
    Change-Id: I954ec49aae0734cda1bd833ad68f386ecd808f73

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b60b207b06e0dcac631dc78e92245f985b6a02078c0024cdc635c16e93d48c86
-size 38444
+oid sha256:a6e5855ab6a9155710dfe535df9ce78ce4b149d4603eb0fed046e15327ddf056
+size 38445
