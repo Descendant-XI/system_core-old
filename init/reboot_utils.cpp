1e74cbfa686f87ef63edcae86c7367754a5b32f4
Woody Lin <woodylin@google.com>
2019-12-27 18:14:13 +0800

    InitFatalReboot: Panic the system if init_fatal_panic is "true"
    While handling the fatal signals and abort, exits from the signal
    handler to panic the system if "androidboot.init_fatal_panic" in cmdline
    is configured as "true".
    
    Bug: 146818493
    Change-Id: I59fffb7598ce981383ae24961a97fd2fd8e8d64e

--- init/reboot_utils.cpp
+++ init/reboot_utils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ceffb91b5ab237a552b621b78f2a4e137ebdf352565b40c018dbf638c21fc33d
-size 6401
+oid sha256:390c88129b936fd4e3214a5a787d7d6642abafdeee947b9eb3e3c946f13e4bdc
+size 6663
