af501f6f83ea7f6acb54fa9af0d0dfbbea1fe770
Will Shiu <Will.Shiu@mediatek.com>
2020-01-03 16:05:00 +0800

    Adding wait for a device file before mount
    Adding the function, fs_mgr_wait_for_file(), to ensure the
    device file exists before to mount
    
    Bug: 158064601
    Merged-In: I74a68224073932773be18a79f9334d83ea5b6947
    Change-Id: I74a68224073932773be18a79f9334d83ea5b6947
    (cherry picked from commit 589b7dfd83a47eb0dbaa1f7bfa95b92c449f18c0)

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c55a5e0faa1becbfa38597476378580be4f0285482207b94f71fa3f730e77ef9
-size 81133
+oid sha256:7716a608eed2bcb84fdb1cb449637b5e6829827f554ea9b2e272baf9653fcf38
+size 81330
