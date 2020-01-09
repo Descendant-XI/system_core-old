89d8e49067adac242f58e8429b62d5c021af723a
Tej Singh <singhtejinder@google.com>
2020-01-09 11:48:36 -0800

    Remove extra indirection in native puller API
    The indirection is unneeded since we already take a function pointer.
    
    Test: builds
    Change-Id: Ice426d7dcb090aaf87a377ede233a4eb18f5eb30

--- libstats/pull/include/stats_pull_atom_callback.h
+++ libstats/pull/include/stats_pull_atom_callback.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf9de2a84dc403bb8a375c2461c48d2399415d16d6fc9e2e9586210ebb710371
-size 1545
+oid sha256:f50e28c73f1dd187e089a224d060b95d5ecce1e69fa6e501b770f738e4e5f09c
+size 1544
