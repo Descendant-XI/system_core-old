31962d4628fa30b7a970baee938c6b770bececab
Ryan Savitski <rsavitski@google.com>
2020-01-14 22:02:53 +0000

    init: add builtin check for perf_event LSM hooks
    Historically, the syscall was controlled by a system-wide
    perf_event_paranoid sysctl, which is not flexible enough to allow only
    specific processes to use the syscall. However, SELinux support for the
    syscall has been upstreamed recently[1] (and is being backported to
    Android R release common kernels).
    [1] https://github.com/torvalds/linux/commit/da97e18458fb42d7c00fac5fd1c56a3896ec666e
    
    As the presence of these hooks is not guaranteed on all Android R
    platforms (since we support upgrades while keeping an older kernel), we
    need to test for the feature dynamically. The LSM hooks themselves have
    no way of being detected directly, so we instead test for their effects,
    so we perform several syscalls, and look for a specific success/failure
    combination, corresponding to the platform's SELinux policy.
    
    If hooks are detected, perf_event_paranoid is set to -1 (unrestricted),
    as the SELinux policy is then sufficient to control access.
    
    This is done within init for several reasons:
    * CAP_SYS_ADMIN side-steps perf_event_paranoid, so the tests can be done
      if non-root users aren't allowed to use the syscall (the default).
    * init is already the setter of the paranoid value (see init.rc), which
      is also a privileged operation.
    * the test itself is simple (couple of syscalls), so having a dedicated
      test binary/domain felt excessive.
    
    I decided to go through a new sysprop (set by a builtin test in
    second-stage init), and keeping the actuation in init.rc. We can change
    it to an immediate write to the paranoid value if a use-case comes up
    that requires the decision to be made earlier in the init sequence.
    
    Bug: 137092007
    Change-Id: Ib13a31fee896f17a28910d993df57168a83a4b3d

--- init/security.h
+++ init/security.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e91debebe884f3e6bbac747e4505ae3d0a1b6195eae5763b5494930e9e6a2fbb
-size 1040
+oid sha256:07b6707057e7bb3e3db4d69cc0ba29c3b6f05ec6814132020a9a809587b4c0ee
+size 1106
