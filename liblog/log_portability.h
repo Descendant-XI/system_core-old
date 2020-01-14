639374a99b81bc586a3f76295c7447106fe60645
Tom Cherry <tomcherry@google.com>
2020-01-14 09:52:10 -0800

    liblog: use libbase_headers for TEMP_FAILURE_RETRY
    Test: build
    Change-Id: Id3b57ff0327d4632e91960e5e70f3aa21992ed15

--- liblog/log_portability.h
+++ liblog/log_portability.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:effe7aa1abfc8bbfb50ce7fe589d3c7016740f42f7a5f945202472a511347792
-size 1363
+oid sha256:0245daa9461aee0c43e6b73fed6636eece1d17d10f010ffa583b6322c61c1ac0
+size 908
