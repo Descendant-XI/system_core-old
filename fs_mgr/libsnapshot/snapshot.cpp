8819d14ef81adbc416488b136d091f61a78cb980
Yifan Hong <elsk@google.com>
2020-01-13 18:34:59 -0800

    SnapshotManager::WaitForMerge gives more info
    It now returns Ok() if successful, NeedsReboot() if merge
    it should be checked again after reboot, and Error() for
    other errors.
    
    This wraps UpdateState to help clients interpret the UpdateState value.
    
    Also separate SnapshotManager::Return from FiemapStatus since they are
    for different libraries and have (potentially) different set of error codes.
    
    Test: libsnapshot_test
    Bug: 138808328
    Change-Id: I8c95417c2b0b7b2a362beb12585f861453a79278

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fdf4e83773b00e5862992d0619aa2f1149df4d5dc261e1a342dfa5a077b9a49
-size 94714
+oid sha256:bb13a5707b51c1b4a9082dfdc0646e0a85c0d344824593a69583e8fc638f2993
+size 95049
