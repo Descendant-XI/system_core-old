2a7784dea03e46939491e1a08c5199774be72e48
Nikita Ioffe <ioffe@google.com>
2020-01-15 23:23:13 +0000

    Whitelist reboot reasons related to userspace reboot failure
    Test: system/core/bootstat/boot_reason_test.sh
    Bug: 135984674
    Change-Id: I31422329f1109273909293a83913ae801eb1fe52

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d4abbbd7587b5d8abc872824f3b34986aa927155db6ccfc69fe2e55fb686d8b
-size 54057
+oid sha256:f5c0d8dac027076704a353dcf6eed6679690f8bd0b869cf447106a28d1e4c87e
+size 54304
--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2737c444c84156842d27b43d2478fdc09b36f583c4a37273c3592d9ba2fadad4
-size 39145
+oid sha256:8fcb68d5cf0e798279599b9dc02322fb74a6497c3edbcea899ec75d7eadddee2
+size 39155
