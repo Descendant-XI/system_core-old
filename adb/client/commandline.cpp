09efa089af1f56cd45d7fd8405cb45a845a40a22
Junyong Sun <sunjy516@gmail.com>
2020-01-15 16:34:58 +0800

    adb: windows: fix adb execute root cmd with no tips
    fwrite can cross windows platform to show log on terminal,
    so we use it instead of WriteFdExactly.
    
    Change-Id: I252c15a0bc72e1dbd98b7b5ec8000ecdcde16416
    Signed-off-by: Junyong Sun <sunjy516@gmail.com>

--- adb/client/commandline.cpp
+++ adb/client/commandline.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c94c7b02fcdeff105643fe2943bf4e79cf06d9cf418dace9859610330b176396
-size 72812
+oid sha256:d3630dd1c74fc2be574924b289571f0cbaebc2965242b0227d31d7cd79cc2514
+size 72800
