9f046ec8793c21b1ae87a665fc1e5012cc32431a
Alessio Balsini <balsini@google.com>
2020-01-17 16:52:02 +0000

    When encountered an unexpected status, mention it
    Instead of just mentioning that something wrong has been found when
    parsing UpdateState, also log the value of the erroneous status.
    
    Bug: none
    Test: m
    Change-Id: I487a115a9347199059a684ef682bec79fa5f26a9
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f0dba1b6728fe182ced36b7bfacb5c26744d90b9b647b07386cc13810f29c60
-size 94845
+oid sha256:a8c4c1c8d2b746dffa0e2c70dc432f4e02ac6bee488304dc8baad12397c80e29
+size 94950
