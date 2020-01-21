3a1efd20986bf271765f880308d5022d797e098f
Davide Pallotti <dpallotti@google.com>
2020-01-21 10:54:36 +0100

    Make OSTREAM_STRING_POINTER_USAGE_WARNING build even with GCC
    This is for projects built with GCC that import parts of Android that,
    despite not having Android-specific dependencies, still end up
    depending on logging.h.
    Also removes outdated notes.
    
    Change-Id: I5a47b302bcaeeb935592d8fc7ad2fe5068d226c3

--- base/include/android-base/logging.h
+++ base/include/android-base/logging.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7398f493575eac0a4e82bfa8a70e9637ea84a4951dd72a8e85e73373e77b0ada
-size 18972
+oid sha256:77467b587b8dd605f0f3ca8204196a7d49b1a0729258e9b2ddef19e464740d96
+size 18753
