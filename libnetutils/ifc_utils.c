5bd198f70268d01b7c7104a7d394aba8dd5c3e15
Maciej Żenczykowski <maze@google.com>
2020-01-21 23:47:42 -0800

    add nodad argument to ifc_act_on_address() and add it to header file
    Test: atest
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: Ia45f00ecc5eae035ed2e25d5b73008acb77fa6c2

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ad7ad0df793559f9e6ff09be473cc5ac8bf5a811b5486d8a52da1591a6a62d3
-size 19179
+oid sha256:8f22bf4ec3f1af46244653c4b3ebd91f498a31fa1af8de31bfb051a7d55d790f
+size 19272
--- libnetutils/include/netutils/ifc.h
+++ libnetutils/include/netutils/ifc.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e75fb21f36aaf22c3e817bc512507b7f7de4509ab688241e176399171d4e814f
-size 2408
+oid sha256:71f01473bb99ae1f1d0193113c466bfd684bc186814d28ca4d6d982abb20931e
+size 2568
