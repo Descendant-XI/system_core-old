20d0ced2b51f8754abc68c6e3a4cd2a56b8de795
Christopher Ferris <cferris@google.com>
2020-01-22 12:17:06 -0800

    Properly handle empty map after read-only map.
    Recently, the maps for an elf in memory might show up looking like:
    
      f0000-f1000 0 r-- /system/lib/libc.so
      f1000-f2000 0 ---
      f2000-f3000 1000 r-x /system/lib/libc.so
      f3000-f4000 2000 rw- /system/lib/libc.so
    
    The problem is that there is logic in the code that assumed that the
    map before the execute map must be the read-only map. In the case
    above, this is not true. Add a new prev_real_map that will point
    to the previous map that is not one of these empty maps.
    
    This will fix the backtraces that look like this:
    
      #00  pc 0000000000050d58  /apex/com.android.runtime/lib64/bionic/libc.so!libc.so (offset 0x50000) (syscall+24) (BuildId: 5252408bf30e395d49ee270b54c77ca4)
    
    To get rid of the !libc.so and the offset value, which is not correct.
    
    Added new unit tests to verify this.
    Added new offline test which an empty map between read-only and execute
    map. Before this change, the backtraces had lines like
    libc.so!libc.so (offset XXX) would be present.
    
    Bug: 148075852
    
    Test: Ran unit tests.
    Change-Id: Ie04bfc96b8f91ed885cb1e655cf1e346efe48a45

--- libunwindstack/tests/RegsTest.cpp
+++ libunwindstack/tests/RegsTest.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886cc832717a2888de199513204e42c3de263aa65fc71fa4d40d620a207c1b60
-size 10453
+oid sha256:da4cb53d67557ab0ca82cc0bb4d68c1370e1b583aece4ddcadf3cf95bc91bbf6
+size 10462
