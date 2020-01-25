172b6e548c599b19f523ded3f8459baec39b7175
David Anderson <dvander@google.com>
2020-01-24 19:19:27 -0800

    Fix mounting /data on Virtual A/B devices without metadata encryption.
    When using partitions backed by /data, for example during a Virtual A/B
    merge or "adb remount" on a Virtual A/B device, the userdata block
    device is seen as in-use when /data mounts in second-stage init. This
    subsequently prevents mount() or e2fsck from working. Metadata-encrypted
    devices are not affected, because dm-default-key provides a wrapping
    block device that can be used exclusively.
    
    This patch addresses the problem by detecting when userdata has
    device-mapper dependencies. If it does, and the device is not
    metadata-encrypted, we introduce a dm-linear wrapper around userdata.
    It is created on demand, but like logical partitions, it exists until
    the device reboots.
    
    Bug: 134949511
    Test: adb remount, cuttlefish boots
    Change-Id: Ifbfea1591a6e58978fdaffd6ef889afabd10e270

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09f98dfd2c0166c90aeab7352cc92e2d43f0728a2652bf3ad2d6fb8ef617b164
-size 70710
+oid sha256:e97db445b8405a6602e496a09894643f4ba0ff1db35a7c3e8825253ea66e7dcb
+size 73970
