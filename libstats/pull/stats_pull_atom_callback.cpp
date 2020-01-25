7b75c45a1b9d1c4a1fda36419ba21d85ac7c2f48
Ruchir Rastogi <ruchirr@google.com>
2020-01-24 18:21:36 -0800

    Migrate libstatspull to libbinder_ndk
    Since libstatspull will be placed within the APEX, we need to move to
    libbinder_ndk.
    
    Test: m -j libstatspull
    Test: atest LibStatsPullTests
    Bug: 145213327
    Change-Id: I86dd6e759efa5eb20bffc76492d5fa72cf279872

--- libstats/pull/stats_pull_atom_callback.cpp
+++ libstats/pull/stats_pull_atom_callback.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:567b1fb900a114ce6884ea43dbb73e4e3728b577498ee5a5347552cc146b678d
-size 9009
+oid sha256:d17759c63f54a00757293165044f454ae37c4243feceef43a6fbc0507a86a71c
+size 9024
