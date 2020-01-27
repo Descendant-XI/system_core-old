1d0fab4f5c1bb3f71ba89737fb210db4200f6c7e
Tom Cherry <tomcherry@google.com>
2020-01-27 15:45:52 -0800

    liblog: remove fake_log_device
    This is now dead code as __android_log_stderr_logger() has superseded it.
    
    Bug: 147496863
    Test: build
    Change-Id: Ibdea2961ec4fc093bf9e35581dc1c46db2cff06a

--- liblog/fake_log_device.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8d9c6286be7166412b7df37f12f45d464229fa444bd44b29be2665278f726a28
-size 15090
--- liblog/fake_log_device.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:36c7e8afc493314d0f88a96e1be572ce8eff3d9d787b7560814c0b2bbf1ff1c4
-size 1011
