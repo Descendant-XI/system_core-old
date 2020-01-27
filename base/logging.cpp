c235336ee1530eca71f765f21d66929523a9c5d2
Tom Cherry <tomcherry@google.com>
2020-01-27 08:35:13 -0800

    liblog: use default tag for loggability checks if no tag is provided
    Bug: 116329414
    Bug: 119867234
    Test: new unit tests
    Change-Id: I92a3f4f95e5f482f6fe20f17ed83c8ed367b06dc

--- base/logging.cpp
+++ base/logging.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79667ba3c511b52461d3bce22e04025e7a23e94c952f120775adaaf67ad7ab62
-size 19163
+oid sha256:5e3440db1ddb6bf32706794cf23247110fb6fb5e32eff79c1b1d59425a4d657c
+size 19064
