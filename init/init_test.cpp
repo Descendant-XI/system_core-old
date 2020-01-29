917509480dd4f3737a748a63666e274f35e0dd5d
Tom Cherry <tomcherry@google.com>
2020-01-29 14:09:24 -0800

    init: handle property service callbacks asynchronously
    A previous change moved property_service into its own thread, since
    there was otherwise a deadlock whenever a process called by init would
    try to set a property.  This new thread, however, would send a message
    via a blocking socket to init for each property that it received,
    since init may need to take action depending on which property it is.
    Unfortunately, this means that the deadlock is still possible, the
    only difference is the socket's buffer must be filled before init deadlocks.
    
    There are possible partial solutions here: the socket's buffer may be
    increased or property_service may only send messages for the
    properties that init will take action on, however all of these
    solutions still lead to eventual deadlock.  The only complete solution
    is to handle these messages asynchronously.
    
    This change, therefore, adds the following:
    1) A lock for instructing init to reboot
    2) A lock for waiting on properties
    3) A lock for queueing new properties
    4) A lock for any actions with ServiceList or any Services, enforced
       through thread annotations, particularly since this code was not
       designed with the intention of being multi-threaded.
    
    Bug: 146877356
    Bug: 148236233
    Test: boot
    Test: kill hwservicemanager without deadlock
    Merged-In: I84108e54217866205a48c45e8b59355012c32ea8
    Change-Id: I84108e54217866205a48c45e8b59355012c32ea8
    (cherry picked from commit 7205c6293341c82701e849fa29cfab66916d1052)

--- init/init_test.cpp
+++ init/init_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e6ce8a13f5787afad0a70bcaefa427e8e7bde90495c8f36c36bfcf357f06a34
-size 8107
+oid sha256:a0516f14fd606b71399e9d4e427506626be9ebec99ccef81c0db222724757277
+size 8154
