36a59ce6b99bbac730e4b5bf7d8c74e2f9af45be
Bowgo Tsai <bowgotsai@google.com>
2020-01-31 23:06:09 +0800

    first_stage_mount: reading all avb_keys before chroot
    Avb keys used to verify a partition are stored in the first-stage
    ramdisk. However, after /system is mounted, init will chroot into it.
    This makes those keys inaccessible for later mounts, e.g., /vendor or
    /product.
    
    This change retains avb keys by reading all of them before chroot
    into /system.
    
    Note that it is intentional to perform public matching for both
    preload_avb_key_blobs and fstab_entry.avb_keys in libfs_avb.
    As some keys might only be availble before init chroots into /system,
    e.g., /avb/key1 in the first-stage ramdisk, while other keys might
    only be available after the chroot, e.g., /system/etc/avb/key2.
    
    Bug: 147585411
    Test: specify avb_keys for a partition and checks the keys are preloaded
    Test: atest libfs_avb_test
    Test: atest libfs_avb_internal_test
    Change-Id: I6bd490c4215480db2937cdfc3fea0d616e224a91

--- fs_mgr/libfs_avb/include/fs_avb/fs_avb.h
+++ fs_mgr/libfs_avb/include/fs_avb/fs_avb.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca2006a7dd27aa45dd4057c43158f86f609569abe7d67b63587218910dc2d5ea
-size 5961
+oid sha256:595a8b4cb3523dfbf26a5810c63feb8d1c935d2d869da936de6264b8916d284b
+size 6314
