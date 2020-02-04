62cf8a4bb0a451b212a3e21e17dca5b79c1be8f4
Alessio Balsini <balsini@google.com>
2020-02-04 18:59:10 +0000

    Extra logging for metadata errors
    Increase logging in metadata image management.
    
    Bug: none
    Test: m
    Change-Id: Ie49ca9ac90cd593c95d31f4651ae724d617d5695
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libfiemap/metadata.cpp
+++ fs_mgr/libfiemap/metadata.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9092cedc8347ceff9ae5c65d74f5cefa909ef92d563353d347e288125e44ea77
-size 7157
+oid sha256:9628f4641058f16bf37764b638dfeb55cdd4978ffb8fda14bb263d7880a63965
+size 7443
