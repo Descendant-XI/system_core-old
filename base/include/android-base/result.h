dd19f4fa943fd81ca637bdef545bf1325ece465a
Tom Cherry <tomcherry@google.com>
2020-02-04 15:18:29 -0800

    Check Errorf()/ErrnoErrorf() format string at compile time
    fmtlib provides compile time checking of format strings that we're not
    currently using.  This change makes Errorf() and ErrnoErrorf() into
    macros such that we can take advantage of this capability.
    
    Test: build successfully normally
    Test: fail the build if using an invalid format string
    Change-Id: Icb8ba8cb973bbd1fa4755a62e7598bdbb0113757

--- base/include/android-base/result.h
+++ base/include/android-base/result.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:403139f56c03efc4d5883d2d820eedad73e691047b4733fce17c25f1be595178
-size 7286
+oid sha256:1832b93f19984dabf62479cb76c092bc9c01ca5f2ab4000e4073966699fb7e4c
+size 7691
