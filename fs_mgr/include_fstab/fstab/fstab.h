22e52fff6f32768376ea00a0bf6cdfe91fa3e1e7
Barani Muthukumaran <quic_bmuthuku@quicinc.com>
2020-02-06 23:46:37 -0800

    Add wrapped key support for metadata encryption
    Change metadata_cipher fstab option to metadata_encryption
    that includes encryption flags in addition to the cipher.
    wrappedkey_v0 encryption flag is used to denote that the
    inline encryption hardware supports wrapped keys. dm-default-key
    device is created and a wrappedkey is provided along with the
    optional wrappedkey_v0 argument.
    
    Bug: 147733587
    
    Test: FBE validation with Fscrypt v2 + inline crypt + wrapped
    key changes kernel and metadata encryption with wrapped key.
    
    Change-Id: Id1a18db175680dd6b0adb4594d06566eb1285785

--- fs_mgr/include_fstab/fstab/fstab.h
+++ fs_mgr/include_fstab/fstab/fstab.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f62eb4cac4a2aa6892aff27c50999454390f41a1bf96bb226f6f1ca3de8e57d
-size 4150
+oid sha256:d8c0fd9e8a0a406d5ac0e8cda3302a5c187e36d6873e3dfb92da87b64708deb6
+size 4154
