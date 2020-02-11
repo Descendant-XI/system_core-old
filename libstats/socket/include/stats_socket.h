8ba378a933ba36203966be120f507aae11c50766
Muhammad Qureshi <muhammadq@google.com>
2020-02-11 14:12:59 -0800

    Expose AStatsSocket_close() from the statsd apex
    As part of statsd becoming a Mainline module in R, we need to formalize
    the statsd APIs called from outside the statsd apex.
    
    Bug: 145923416
    Test: m
    Change-Id: I296a018cc93d95bda77d95f8161acbbe983a5d51

---
+++ libstats/socket/include/stats_socket.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59f74dd473120d3b76880ef149be7cac007a55aa0b72a0d2f78663135d3bcb26
+size 866
---
+++ libstats/socket/stats_socket.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99d31e3fc506c62dec43880306d85b98738ba2e4f48a943d04568de1554d4500
+size 742
