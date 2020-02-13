7f8cdbfa15aa361e3d70b633599d16d928747de9
Alex Buynytskyy <alexbuy@google.com>
2020-02-13 06:52:04 -0800

    Incremental installations in adb, client/host side.
    Test: adb install --incremental <apk>
    
    Change-Id: I74e3eaf9718a16272bc533bc8298dfcf81120caa

--- adb/adb_trace.h
+++ adb/adb_trace.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5c8d5e91167b2b861ad7810f91b0fa8386cf7fe94326fa3572437a76efc791d
-size 1833
+oid sha256:665348f6efc0089904b44e4b42296b05aaa80816a304627edce350b6517534d6
+size 1838
