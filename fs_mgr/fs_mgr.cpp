2a37cc83f44102bc205333f214e9edf7bd1d775e
Wei Wang <wvw@google.com>
2020-02-14 14:22:16 -0800

    fsmgr: print unabbreviated log when exec child commands
    logs need to be printed at time when it is generated by the child
    process which is very useful to understand the boottime impact of
    those blocking commands.
    
    Bug: 149418646
    Test: boot
    Change-Id: Ibee343af6088d57b2976bceb812abb58f3b3f610

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73b9ba2aa9a227361554346004c9384050b6445d74b4e1edf0b34d2d8e4f4064
-size 75768
+oid sha256:49dd66db6b5feadf1ec7d0374e62839a19b6f8a76c302febef479abf7fc148e1
+size 75815
