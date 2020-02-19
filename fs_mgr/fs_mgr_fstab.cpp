495a99357ddae04fecc1a10544ee0203bb68a77c
Bowgo Tsai <bowgotsai@google.com>
2020-02-19 17:46:22 +0800

    Allowing R and S developer GSI for DSU
    Adding new keys to allow a device boot a R-Deverloper-GSI and
    S-Developer-GSI, respectively.
    
    Also removing previous keys for R-Compliance-GSI and
    S-Compliance-GSI, respectively. We will be allowing CTS-on-GSI
    to run on unlocked state in R and later releases.
    
      https://android-review.googlesource.com/c/platform/test/suite_harness/+/1228453
    
    Bug: 149805495
    Test: boot a device
    Change-Id: I000365d0b407605b426c4695c6ac614150ee9b13
    Merged-In: I6bc1e068368e64cb0bbf6ef63805444b35a1fdfb

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa3ad9d395bfc9598903114e46f483266c0f786ae25056e0d1c9eecd5c261453
-size 29781
+oid sha256:e431642b8a8ee659dca88d475383ffdc7231fc4475096f4f4de9765a2079105e
+size 29801
