aa91c80e19d9c21d90d75ed33a8a194ee3b94aab
Yifan Hong <elsk@google.com>
2020-02-19 20:52:51 -0800

    libsnapshot: delete InitiateMergeAndWait.
    Now that update_engine is responsible for initiating the
    merge, InitiateMergeAndWait function becomes useless.
    
    Bug: 147696014
    Test: libsnapshot_test
    Change-Id: I5473dc543ca8ac2fd31f597720847b02d0e5e33d

--- fs_mgr/libsnapshot/snapshot_test.cpp
+++ fs_mgr/libsnapshot/snapshot_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9162aadac62e17573f81543f00430f2b6a827ba93138ddc0359b14451143dbd5
-size 69087
+oid sha256:2d82360f3f059df5c88a653dea9ce4e62a4f36f174e46749ddaeec0a30fda840
+size 69276
