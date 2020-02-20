3ab57707473f64d1e417a4c99602e647cf262287
Alessio Balsini <balsini@google.com>
2020-02-20 01:06:57 +0000

    libsnapshot: fix stats write in Resume()
    The Resume() operation is supposed to increment the resume counter, but
    the updated counter value was not written to the device.
    Fix by adding the write operation and refactoring the code.
    
    Bug: none
    Test: m
    Change-Id: I3fffd61cc779c59e2780900809f0ce0b84258e78
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libsnapshot/snapshot_stats.h
+++ fs_mgr/libsnapshot/snapshot_stats.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8bda9bea3c6642741d5ee52c3cd7bba0fbff42f46ee430a1103eb9b9cbd6510
-size 1278
+oid sha256:908f438a0e2e4cc4021ee5ffcefdb113e6530ff4de7efe6918315ad8786d3a1e
+size 1324
