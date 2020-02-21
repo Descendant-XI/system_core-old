cddb02a6f0c2e32d96e8f361ab80fd793b7ce4dd
Yifan Hong <elsk@google.com>
2020-02-20 19:51:05 -0800

    libsnapshot: remove snapshots properly after flashing
    If updated then immediately flashed target slot at unverified
    stage, update_engine attempts to call RemoveAllSnapshots, but
    it used to fail because unmapping fails. In reality, these devices
    are dm-linear targets, so unmapping them is optional.
    
    - Introduce a GetSnapshotFlashingStatus function that expose more
    information than AreAllSnapshotsCancelled. We can use the returned
    map<string, bool> to determine whether a partition is flashed or
    not.
    - Introduce ShouldUnmapDeleteSnapshot which determines whether
    unmapping / deleteting a snapshot is needed in RemoveAllSnapshots.
    
    Test: apply OTA, flash target slot, then inspect logs
    Bug: 147696014
    Change-Id: I0853d1e213566af2a3401e46fac7d9586cee7aaf
    Merged-In: I0853d1e213566af2a3401e46fac7d9586cee7aaf

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bbbe038b9b54c3236d78910c039017f69587d71f3e12a2bc7d36032014dfebe
-size 23791
+oid sha256:60503ec0d8d8712202e1c9d0b4536c3be2db88275b1af8d2d327a34942fd0b07
+size 24621
