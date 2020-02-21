03c2f571975f4fd0a92c15417713ca2296c4e25f
George Burgess IV <gbiv@google.com>
2020-02-21 14:36:31 -0800

    incremental_server: fix a use of uninitalized memory
    Without this, the caller is likely to assume that their buffer is
    fully usable, which clang's analyzer doesn't believe is the case.
    
    Another option is to set `*size` to nonzero.
    
    Caught by the static analyzer:
    system/core/adb/client/incremental_server.cpp:111:31: warning: 1st
    function call argument is an uninitialized value
    [clang-analyzer-core.CallAndMessage]
    
    Bug: http://b/150032044
    Test: TreeHugger
    Change-Id: Ib844aa4ab3ebb297ca8f6f4289bbe3212275275b
    Merged-In: Ib844aa4ab3ebb297ca8f6f4289bbe3212275275b
    (cherry picked from commit 19b500bd505dce85d90f5be6f318399aed9cc990)

--- adb/client/incremental_server.cpp
+++ adb/client/incremental_server.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185fafbe2b4a5906f7ef8a31fe472c314acd4e9dc3cb91a58315dfffd39c3a2e
-size 18201
+oid sha256:a3862a69a6db784c0f9c520fee26aa96557de2f7c77218ebc9f1da6c64976567
+size 18202
