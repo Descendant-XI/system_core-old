4e8b89f68029779122a7c592769a16b5a4dc6406
Elliott Hughes <enh@google.com>
2020-02-24 16:17:19 -0800

    debuggerd: add an opt-out for OEMs that would rather collect core dumps.
    On userdebug/eng devices, check a system property to see whether we
    should create tombstones or not. OEMs that would rather have core dumps
    can set this property and configure /proc/sys/kernel/core_pattern
    appropriately.
    
    Bug: https://issuetracker.google.com/149663286
    Test: set the property, cause a crash
    Change-Id: If894b4582a1820b64bdae819cec593b7710cb6e3
    (cherry picked from commit 530ab81e4215c86832294434f032306656c8f3b7)

--- debuggerd/include/debuggerd/handler.h
+++ debuggerd/include/debuggerd/handler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5df67a12e413cadbc62ca413adc177a9ffe2f0d3402bae31ea991ba5c82b6c3a
-size 2277
+oid sha256:a41859dc706dc61bbdc10a453d9c5c958784063345ea7994dd2cf19deaf4800a
+size 2569
