8cb0f7e160144e05746b64fd1435a754dbeda864
Howard Chen <howardsoc@google.com>
2020-02-25 16:31:10 +0800

    Refine the exported DSU status
    The new exported DSU status removes the need to make blocking binder
    calls out of system server during device boot.
    
    Bug: 149790245
    Bug: 149716497
    Test: adb shell am start-activity \
        -n com.android.dynsystem/com.android.dynsystem.VerificationActivity \
        -a android.os.image.action.START_INSTALL \
        -d file:///storage/emulated/0/Download/system.raw.gz \
        --el KEY_SYSTEM_SIZE $(du -b system.raw|cut -f1) \
        --el KEY_USERDATA_SIZE 8589934592
    Change-Id: I27fae316214498407a73474ca8b93aec3518e4b5
    Merged-In: I27fae316214498407a73474ca8b93aec3518e4b5
    (cherry picked from commit 2e1c6b26a1fe4799b830c2a6d536afd44c718491)

--- init/init.cpp
+++ init/init.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d2afa666bef54118888d3d11e33b89bfa3c27adc1f7576e30f19de3add3c6be
-size 29390
+oid sha256:2cef9b03aa24e0abaa9850f2b259f1ed612f699995f56143b219158dae72a6d6
+size 29466
