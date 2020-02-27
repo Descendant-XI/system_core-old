4084accb641467e5c7fd1b76dbbc77454e91473f
Josh Gao <jmgao@google.com>
2020-02-26 16:39:20 -0800

    adbd: add runtime-configurable logging.
    Add some requested logging options that can be turned on at runtime
    without having to restart adbd.
    
    Bug: http://b/141959374
    Test: manual
    Change-Id: Ib97acc6d199e0b91238a6758e18b7cb75f8688d9
    (cherry picked from commit 52d0b67f191319e20272d404659ec2f967d2769c)

--- adb/adb.cpp
+++ adb/adb.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8522f0f7a4c33f41d6f6bdad538e984acc231672e94e42f67eef28fb377a9fde
-size 49869
+oid sha256:6b6e1f8ade8644643670209ae216db5f567b4fabce44066127daea31dd5af83a
+size 50083
