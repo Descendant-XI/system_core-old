290e6e165a6f67972a85b7d9bc7b83c3812fed0c
Josh Gao <jmgao@google.com>
2020-03-04 19:34:08 -0800

    adb: implement compression for file sync.
    This improves performance when syncing by up to 2x (remote cuttlefish
    goes from 11.9 MB/s to 21.3 MB/s, blueline over USB 2.0 from 36 MB/s
    to 70 MB/s).
    
    This results in a slight drop in push speeds over USB 3.0 (125 -> 115
    MB/s on blueline), presumably because we're compressing and extracting
    on only a single thread, but the gains over lower bandwidth transports
    make this worth it to submit this now and parallelize later.
    
    Bug: https://issuetracker.google.com/150827486
    Test: ADB_COMPRESSION={0, 1} test_device.py (with new/old adbd)
    Change-Id: Ic2a0c974f1b6efecda115f87d336e3caac810035
    Merged-In: Ic2a0c974f1b6efecda115f87d336e3caac810035
    (cherry picked from commit 939fc19aee7f6a19ce0f4fa91557bbbe9cd974f5)

--- adb/client/file_sync_client.cpp
+++ adb/client/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b005b0b6ee099038acacef299b163879f81e7fcab14fbcf04c4d2af3f9e1fe3c
-size 47954
+oid sha256:1e3fe385182c59895a36ef514840be6e492a21c0c88f77bc45db9d25fa44a4b7
+size 56545
--- adb/transport.cpp
+++ adb/transport.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4441ab4ffc60c914f018dc244868571441e5b290a856b83a57fb2a3dc67c7ee2
-size 47190
+oid sha256:6b0fc4d93be1ea5e2115d850ef316198b621a5e8f17d5e76f36b574a84e08586
+size 47377
