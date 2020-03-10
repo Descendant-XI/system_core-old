f17d7f069357282ae0652464c48a7d5fe6771d3f
Tom Cherry <tomcherry@google.com>
2020-03-10 09:08:02 -0700

    Revert "init: handle property service callbacks asynchronously"
    This is apparently causing problems with reboot.
    
    This reverts commit d2dab830d3ec260ccd982d147d4eb0ef74071261.
    
    Bug: 150863651
    Test: build
    Merged-In: Ib8a4835cdc8358a54c7acdebc5c95038963a0419
    Change-Id: Ib8a4835cdc8358a54c7acdebc5c95038963a0419

--- init/action_manager.cpp
+++ init/action_manager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d58d307c5e678330e842e3da146c8f39ee014954f5656a69508b1bc14e184048
-size 4311
+oid sha256:2a7038bdb3dd2dfed6a31d9f0c6349f9a00814c75774d4421a8e22f930eb9db0
+size 3943
