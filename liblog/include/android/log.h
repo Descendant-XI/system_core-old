9202b9b75b6c355943f1269f614d45e1d45373fa
Tom Cherry <tomcherry@google.com>
2020-03-12 09:37:05 -0700

    liblog: fix header guards ordering
    __ANDROID_API__ isn't defined on host, so this check will fail due to
    -Wundef.  The || is short circuited through, so we can check for host
    first and only have the right side of the || evaluated for device
    builds.
    
    Bug: 150898477
    Bug: 151255209
    Test: build
    Merged-In: I2e0bc055ac0c7ba8262875801cdaed9cc30d2cef
    Change-Id: I2e0bc055ac0c7ba8262875801cdaed9cc30d2cef
    (cherry picked from commit 227ac5645450bca279735a2a0dbba77c24f5fbb4)

--- liblog/include/android/log.h
+++ liblog/include/android/log.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0786202724622e958698ac18af11a441002a455fc004baff508514339b5b739c
+oid sha256:cc96411b163ed5725b4e04185eeeccfaefd5d52a5b1e5a0a27a871ea879f3479
 size 11071
