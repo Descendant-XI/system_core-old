08492c414649ca9ec29a2a96e312962995bb0fe2
Tom Cherry <tomcherry@google.com>
2020-03-12 11:14:19 -0700

    liblog: move LOG_ID_DEFAULT into the log_id enum
    As requested during the API review.
    
    Bug: 150898477
    Test: liblog and libbase unit tests
    Merged-In: Id67203bbfbc3e116b6b6efd40428afe48203dcfc
    Change-Id: Id67203bbfbc3e116b6b6efd40428afe48203dcfc
    (cherry picked from commit 6085cd5a645f95dca9fd80ca8df9dc0c26cf5969)

--- liblog/include/android/log.h
+++ liblog/include/android/log.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4904dedaac43b4e39a76f83efd7cb6c619a27d73a13dd2aa37ba83b812084c
-size 11103
+oid sha256:764a12229a08c14051980cebe69db825cf6f351b7be006eddd20f94651080ae4
+size 10913
