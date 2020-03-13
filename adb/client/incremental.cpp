3e846a2ae4f5d528297ec849d42925e37bf17118
Alex Buynytskyy <alexbuy@google.com>
2020-03-13 08:39:31 -0700

    Implement the new v4 signing scheme in adb
    To be submitted along with changes in apksigner tool and the framework.
    Merged to AOSP after that.
    
    Test: adb install --incremental <apk>
    
    go/apk-v4-signature-format
    
    Bug: b/151241461
    Change-Id: I26e187f8e389e31e2759037057b96fc6c9cb1e94

--- adb/client/incremental.cpp
+++ adb/client/incremental.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1397a8db5aa497e59857350c65f7dd934ad39200d4444272ade45baba1726bd
-size 7687
+oid sha256:277f6f9f06e5ed366ae7ab2d974e77d9d9200198b5ada8f40600fdf161072090
+size 7572
