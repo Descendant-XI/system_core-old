92ee45259a794e0b68b879dce8d827935be935a6
Jooyung Han <jooyung@google.com>
2020-03-17 03:43:03 +0900

    base: dlopen/dlsym for liblog when SDK_VER <= 29
    libbase doesn't have to rely on dlopen/dlsym to use liblog's new symbols
    when it is built for __ANDROID_SDK_VERSION__ > 29.
    
    Bug: 150860940
    Test: TARGET_BUILD_APPS="com.android.adbd com.android.resolv" m
          objdump -T ...shared_com.android.resolv/libbase.so | grep LIBLOG_R
            => should be none because resolv apex is targeting 29
          objdump -T ...shared_com.android.adbd/libbase.so | grep LIBLOG_R
            => should list all new symbols because adbd apex is targeting R
          objdump -T ...shared/libbase.so | grep LIBLOG_R
            => should list all new symbols
    
    Change-Id: I7f7f16510d7637cd380fe35ea9ff3e804f38851d

--- base/liblog_symbols.cpp
+++ base/liblog_symbols.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6ec2e80c661a8f0ea3cd00fc94c3e42f82c01b0e5aac2d49e5f1a0379c7fe3b
-size 3133
+oid sha256:4273f0aa82497dd2e031271e2ed2d76aa2910974027615cdf33ae0d4dea75608
+size 3114
