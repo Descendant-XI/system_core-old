65250803ba386276d35944e3ecd9aadcbad07a22
Yurii Zubrytskyi <zyy@google.com>
2020-03-19 00:21:08 -0700

    [adb] Print fewer progress messages for push/pull
    Windows console IO is terribly slow. Reducing the number of
    printed progress messages speeds up the transfer rate
    from 80 to 130 MB/s on Windows laptop
    
    Bug: 151900478
    Test: adb push/pull
    Change-Id: I223284c8a662bd8f2b8ba280cdcc8c930d3e5205

--- adb/client/file_sync_client.cpp
+++ adb/client/file_sync_client.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb45baec6d094b20f0a2c2460fba2bca42ba0982e0baab44c4b3f0c0d7e574d5
-size 44364
+oid sha256:77da7298f7d2a51f686d1c0eaea5fed8dc73f2dbc8949c7b39f345866b60aa0a
+size 44542
