d41851df09f8e6ba5e4c9c6bbc7f8270de34679d
David Anderson <dvander@google.com>
2020-03-19 23:09:45 -0700

    init: Remove special-case hacks for the super partition.
    Rather than special-casing super in the uevent listener, call Realpath()
    after we've generated its symlink. This allows us to generalize the
    uevent listener code.
    
    Also, rename "metadata partition" to "super partition" in a few places,
    as this terminology is a bit dated.
    
    Bug: 150315914
    Test: first-stage init boots
    Change-Id: I2bf3bebf3360f097582df7fba95f3c1753feb2e2
    Merged-In: I2bf3bebf3360f097582df7fba95f3c1753feb2e2

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37fe7d90e74b443eff010f49d380dbcb9d5ee160c0a69babdb95dc791a7c684c
-size 35508
+oid sha256:653cf75ccb08751d1792312380fee1171dbfc4137fd026f43992911373250bcc
+size 35608
