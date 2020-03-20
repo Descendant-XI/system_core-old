6a97c3886684fe8d14f756e4583ea34a5aed7bb3
Tej Singh <singhtejinder@google.com>
2020-03-20 12:13:40 -0700

    Minor followups from api feedback
    Bug: 150788562
    Test: m
    Change-Id: I3473e49bc3f3682300b036ddeb032c3776d8d830

--- libstats/pull/stats_pull_atom_callback.cpp
+++ libstats/pull/stats_pull_atom_callback.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec2d500a8fcb7cd33a2e8e20358944e733158d5c0072e50a234e34bce4bfdab0
-size 9820
+oid sha256:b5733d717adaa9c7534339a0f296addafccc170a88d427108eca4b66789b0de5
+size 9808
