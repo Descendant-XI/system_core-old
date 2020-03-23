873810c2b062c1196cb9efd378a3d0a6f0aa2cc5
Joshua Duong <joshuaduong@google.com>
2020-03-23 11:01:25 -0700

    Fix pairing aes_128_gcm key initialization.
    Bug: 150719467
    
    Test: atest adb_pairing_auth_test
    Test: check 'adb pair' command on all three platforms work
    Change-Id: Idfc64fe7bed2d09f4da9d2f7df70f9d6ae4e8fa3

--- adb/pairing_auth/tests/aes_128_gcm_test.cpp
+++ adb/pairing_auth/tests/aes_128_gcm_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91d8895b465a66220586fa1861b2c76956ca1a6a62c148df57ab1cd89825282
-size 4788
+oid sha256:09933971fe03a70e19020a05cc9e526117455a494f8fef8bb9aa8a0b3b4dc0f4
+size 2095
