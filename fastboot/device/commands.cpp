6cb3a9e6ffc206cf64d40fbfa04b3fba9ff5755a
David Anderson <dvander@google.com>
2020-03-23 23:43:45 -0700

    fastbootd: Unmount /cache before flashing.
    Bug: 150112538
    Test: fastboot flash cache on non-A/B device
    Change-Id: I8a86bc4d12e4b3be020bbe47e02262a5aaa913a7
    Merged-In: I8a86bc4d12e4b3be020bbe47e02262a5aaa913a7

--- fastboot/device/commands.cpp
+++ fastboot/device/commands.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a20dbe0472054c9ca5eaba9afb806e7dc6d2fbf5cb0b66e58f94b389f1d2f9cd
-size 24250
+oid sha256:3b54476514ab4f980ac5ae132a10c35a3f4693f0cbaedd4fa1f54dd03243aa41
+size 24276
