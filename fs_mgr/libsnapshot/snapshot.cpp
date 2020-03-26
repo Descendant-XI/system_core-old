f0d181e4f5b695befbfde97ebc0ba8760d2439eb
David Anderson <dvander@google.com>
2020-03-26 14:48:30 -0700

    libsnapshot: Fix intermittent test failure due to missing null check.
    Bug: 152547190
    Test: vts_snapshot_test
    Change-Id: If7adc4fba61325e52d6268531ce0a15b00c0a380
    (cherry picked from commit 533b2b62965a6ce3509b07fefc178232547fe58d)
    Merged-In: If7adc4fba61325e52d6268531ce0a15b00c0a380

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12801bb1f15ac11f5f48801bc638417d2a8ca71224ff8b68c584f2e9d2a447bc
-size 101673
+oid sha256:bfc95de118a1e4fd79dd672fe618b57626879135fc7621f7244d19adbcd57cec
+size 101711
