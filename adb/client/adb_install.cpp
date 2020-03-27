7ea6aa27d3243f239ec29733e120f28b6303e9da
Yurii Zubrytskyi <zyy@google.com>
2020-03-26 18:20:39 -0700

    [adb] Use incremental installation by default
    This CL turns on the incremental installation for all
    "adb install ..." commands where no explicit mode has been set.
    To disable this, set the ADB_INSTALL_DEFAULT_INCREMENTAL
    environment variable to 0/n/no/false. Unset to enable back
    
    + improve the install command argument parsing a bit: allow
      --wait for all installation modes, --incr is enough for
      an incremental install (and --no-incr to disable it)
    
    Bug: 150183149
    Test: adb install with different apks and command line switches
    Change-Id: I1a237f34b70d920146746ab16104e28ef555a5fd
    Merged-In: I1a237f34b70d920146746ab16104e28ef555a5fd

--- adb/client/adb_install.cpp
+++ adb/client/adb_install.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3abf49c6c37c75ae23214317407c53d06d7ab04d4b25ef5debc252e775e5d3ec
-size 28440
+oid sha256:84867e8ddc59cfabd9ed150ad4de53b2bcc8f8c9172b2ee38cd8c4edc446c65e
+size 32025
