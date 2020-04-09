9858e44a3b80f8041ac9c416e05bf8b0dbd067d8
Tom Cherry <tomcherry@google.com>
2020-04-09 14:53:55 -0700

    liblog: support extended logger_entry headers
    logger_entry has a hdr_size field meant for backwards compatibility,
    however there are a few checks that expect it to be a specific size.
    More problematically, it has a variable length array element, msg[0]
    at the end, that would be incorrect if the header size was not kept
    static.
    
    This change fixes up those issues to facilitate future additions.
    
    Bug: 119867234
    Test: logging works with extended size headers, logging unit tests
    Test: newly added unit tests with extended size header
    Merged-In: I14b2b0855d219d11f374b53a5aa8e05cd6a65eef
    Change-Id: I14b2b0855d219d11f374b53a5aa8e05cd6a65eef
    (cherry picked from commit d3ecc66b9cb38974ef0d19ed9e25074bbe70f1fa)

--- liblog/logprint.cpp
+++ liblog/logprint.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04e384ba6807272f20fd24f0115d141a8f9abfa50a926470317b08bd46823a25
-size 51114
+oid sha256:ed1840d3bed8c38af760acd3ae6710ae9cc090c353b3c3e51d3b39824cbf3d5d
+size 51252
