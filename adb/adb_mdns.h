4e06b568b1038ff80caa3cb9ac1844c2aacb9273
Joshua Duong <joshuaduong@google.com>
2020-04-16 15:58:19 -0700

    adb: change mdns tls service names (RFC 6763).
    Even though mdnsresponder seems to allow us to use _adb_secure_connect
    and _adb_secure_pairing as service names, these names violate the syntax
    outlined in RFC6763, and may not be compatible with other dns-sd
    implementations.
    
    Also address some comments from a previous CL.
    
    Bug: 154268895
    
    Test: atest adbd_test
    Change-Id: Ia872e976fc4276587b500a827a41d46d9dc755dd
    Merged-In: Ia872e976fc4276587b500a827a41d46d9dc755dd

--- adb/adb_mdns.h
+++ adb/adb_mdns.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd6862e13ae14279ecb9d4187ed82e300a563cc80d9fba6ab95d6ae132c9767f
-size 3093
+oid sha256:3eeebf3a5bc8c2ca079d983692b9331db9137c1019ef387c2e3557d1b59085e0
+size 3555
