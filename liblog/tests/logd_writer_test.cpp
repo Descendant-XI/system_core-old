31bb5f5e65ddc9e0910c130fb3bdde62758ffdde
Tom Cherry <tomcherry@google.com>
2020-04-24 17:15:26 -0700

    liblog: remove Rwlocks for logd_socket and pmsg_fd
    These historically used atomics to manage their lifetime.  They were
    unfortunately unsafe and later replace with a RwLock.  A lock is
    also problematic as it is too heavy weight for the typical use case
    and implies that logging is neither async nor fork safe.
    
    This change returns us to using atomics with two key changes:
    1) compare_exchange_strong() is used instead of atomic_exchange().
       The latter has a race condition where a separate thread could have
       read the atomic value into a register, while the thread performing
       the atomic_exchange closes that FD.  The new code only changes the
       FD in the atomic if it is uninitialized.
    2) Using the fact that DGRAM sockets can have connect() called on them
       multiple times, it uses a single logd_socket for the duration of
       the program.
    
    These sockets are thread/async/fork safely created and accessed.
    
    The one caveat is __android_log_close(), which is intended only to be
    used by zygote when it is single threaded and is therefore not thread
    safe.  It will close this socket and reset the underlying variable,
    such that the next log message will go through the above
    initialization.
    
    Bug: 65062446
    Test: logging works, logging unit tests
    Test: new unit test
    Merged-In: Ia4dbf7479dbe50683d124558ab2f83bff53b8f5f
    Change-Id: Ia4dbf7479dbe50683d124558ab2f83bff53b8f5f
    (cherry picked from commit 7acfba274c2f556b2615de917ca88408b5e0f845)

---
+++ liblog/tests/logd_writer_test.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc0305f81a32589b098580d9d4c8e46111e19c873e0a63d633cc5a849bbc88ff
+size 3652
--- liblog/tests/rwlock_test.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9a213268511dcd5f4c01c1fce367e7a8373f087dfc733c0ba05a63b3eda9faa
-size 2209
