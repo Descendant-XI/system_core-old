1d11dc24bc3a5f1656f2a71e0e2d0f0856c446bf
Maciej Żenczykowski <maze@google.com>
2020-05-06 13:36:55 -0700

    libnetutils/packet.c - fix a socket leak on bind error
    and clean up the code a little bit while at it.
    
    Test: builds, atest
    Bug: 155297277
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I01a10e36f852cde1b93a91f95b51294a434885ab
    Merged-In: I01a10e36f852cde1b93a91f95b51294a434885ab

--- libnetutils/packet.h
+++ libnetutils/packet.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d69b1bf8871e1a45c895fc9bba27240546d77fc3179eb8245195b410a33cfeb4
-size 953
+oid sha256:4adf85e4642c962d818ace04af14b5b02fb46e855471140295227cd752faeb56
+size 991
