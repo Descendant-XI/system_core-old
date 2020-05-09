bf9692f7286c6bb5c3e4f06ea86e34bf2a4cb5af
Chris Renshaw <osm0sis@outlook.com>
2020-05-09 06:53:32 -0300

    init: add vendor.*  keys to spoof safetynet
    aswinas@pixysos: add some more props from magisk hide to userspace hack by arter97
    
    
    Former-commit-id: fc79269db601c9cd0dad3781d4e6ee8f209c55fc

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e6cc1bbbf91300a70bb2295d462387fbc6e922044365fee805e1fd4ef1e84a8
-size 44510
+oid sha256:e9287a11dbe5cbb430c6f458490047acc0ae9a5f80c8223ef81e1bff91e1ca37
+size 44797
