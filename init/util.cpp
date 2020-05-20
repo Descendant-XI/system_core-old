d98a97066607e34ec890ed5204e4ed8f7f4041f3
Alistair Delva <adelva@google.com>
2020-05-20 16:24:00 -0700

    Add ro.boot.fstab_suffix and modify mount_all to use it
    Currently the ReadDefaultFstab function, which calls GetFstabPath,
    makes some assumptions about what the fstab will be called and where
    it is located. This is being used by vold to set up userdata encryption
    and for gsid, and is even used in the default boot control HAL, so it
    has become quite baked.
    
    The original way for a board to specify things to mount was to use the
    "mount_all /path/to/fstab" command in init.rc. However, due to the
    above functionality, the path after mount_all is no longer very useful,
    as it cannot differ from the inferred path, or userdata encryption and
    other features will be broken.
    
    On Cuttlefish, we have an interest in being able to test alternative
    userdata configurations (ext4 vs f2fs, encryption on/off, etc.) and
    currently the only way to achieve this is to either a) modify the
    ro.hardware or ro.hardware.platform properties, which breaks a bunch
    of things like default HAL filenames, or regenerate our odm.img or
    vendor.img filesystems. We can't simply install another fstab and
    point to it with "mount_all".
    
    This change allows the fstab path to be omitted from "mount_all", and
    adds another property which overrides the existing checks for
    fstab.${ro.hardware} and fstab.${ro.hardware.platform}. Specifying
    ${ro.boot.fstab_suffix} will cause fstab.${ro.boot.fstab_suffix}
    to be checked first.
    
    Bug: 142424832
    Test: booted cuttlefish with 'mount_all ${ro.hardware} --late'
    Test: booted cuttlefish with 'mount_all --late'
    Test: booted cuttlefish with 'mount_all --late' and fstab_suffix=f2fs
    Test: partially booted cuttlefish with 'mount_all ${ro.hardware}'
    Test: partially booted cuttlefish with 'mount_all'
    Change-Id: I3e10f66aecfcd48bdb9ebf1d304b7aae745cbd3c
    Merged-In: I3e10f66aecfcd48bdb9ebf1d304b7aae745cbd3c

--- init/util.cpp
+++ init/util.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e002e0c738e49dd6ac965f3abec8de513e2b0441e7f24ec52ae14c271e725b7
-size 22851
+oid sha256:fd4592fd11d690d23313dccba1ec6ae98ec463dc54f4f44d6bbd110921d3fc40
+size 24535
