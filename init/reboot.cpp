4a6dee6c21f14f9f85d8914fe480fc5b19e88e4f
Martijn Coenen <maco@google.com>
2020-05-28 16:42:10 +0200

    Abort FUSE filesystems during shutdown.
    To ensure we can shutdown cleanly, and don't hang an outstanding
    requests to a FUSE host daemon that has already exited.
    
    Bug: 153411204
    Test: inspect logs during shutdown
    Change-Id: I8e6479bd54dbc1fc85b087617aa6b16be9f15a3b

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9db1a2878d0f5defcd65575125ae18e11919660c12121376bdb7a1986f263b2
-size 41541
+oid sha256:03437369b6a63910be5d963afb9def512ccf1a2b6fad56956377e68f59f48d1b
+size 41742
