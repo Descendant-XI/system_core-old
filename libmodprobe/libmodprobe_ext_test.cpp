aed651774bdf9d3e9a49caee8addd34eb0453370
Steve Muckle <smuckle@google.com>
2020-05-29 16:30:33 -0700

    add libmodprobe api to query the number of modules loaded
    In the short term this will be used to help implement support for
    multiple kernel module directories but it may be useful in other
    contexts as well.
    
    Bug: 157645635
    Change-Id: I15a252b6e9394292c8f3557ed65112c935830441
    Merged-In: I15a252b6e9394292c8f3557ed65112c935830441

--- libmodprobe/libmodprobe_ext_test.cpp
+++ libmodprobe/libmodprobe_ext_test.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd89e04d01d43e5aaadb62d9cd6ab99c9e5f6fbabd481cb6c8baf17d1785ace9
-size 2571
+oid sha256:3b631be40593eabcef5174dadf3ec501af7baa521cea0d3b3331091b9c4ce348
+size 2592
