ec02c35a2d44900a1e99289c18120e4777dae194
Tej Singh <singhtejinder@google.com>
2020-05-29 15:21:08 -0700

    Retry pullfinish with empty payload if call fails
    If the first call for pullFinished fails, it is likely because the
    transaction is too large. Currently, if this happens statsd will just
    sleep until the timeout. With this change, the client will retry calling
    pullFinish if the first attempt fails, but with an empty payload (and
    with success = false) to cause the puller to fail fast, and so statsd
    does not wait for the timeout.
    
    Test: atest LibStatsPullTests
    Bug: 157768117
    Change-Id: I3af991fae47bb7c09bf8a6fc83f28556b836c6e4

--- libstats/pull/stats_pull_atom_callback.cpp
+++ libstats/pull/stats_pull_atom_callback.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5733d717adaa9c7534339a0f296addafccc170a88d427108eca4b66789b0de5
-size 9808
+oid sha256:c33d3804f1e28ca10b83d66e90fd72185a032417d087777d6cddeba3f9f2a3f7
+size 10004
