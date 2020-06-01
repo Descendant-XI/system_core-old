f4774a9640f0427f93bc6cb01f5ce0797333a17b
Paul Lawrence <paullawrence@google.com>
2020-06-01 12:58:04 -0700

    Set block size in dm-bow
    Fix block-level checkpointing to work correctly when used in combination with
    512 byte hardware sectors and metadata encryption with dm-default-key v2.
    
    Bug: 153512828
    Test: Parameter is passed to dm-bow based on first_api_level
    Change-Id: Ic0a071221559271db20b06b2f17459b5b041e02d
    Merged-In: Ic0a071221559271db20b06b2f17459b5b041e02d

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c55a5e0faa1becbfa38597476378580be4f0285482207b94f71fa3f730e77ef9
-size 81133
+oid sha256:30066c63ac1617711907eebb5f2f89057aef652c18dec699be75f49141fce56c
+size 82496
