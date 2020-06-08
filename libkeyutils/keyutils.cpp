cb6ab2d6d0dd0e88499c853945369355ff7ae1db
Elliott Hughes <enh@google.com>
2020-06-08 09:27:17 -0700

    Stop using varargs in libkeyutils.
    It's error-prone, and our specific usage of it here upsets ubsan.
    
    Bug: https://issuetracker.google.com/158428513
    Test: treehugger
    Change-Id: I3a6b68865e6b4c37ac005f5f24c3d6e1de7c5bac
    Merged-In: I3a6b68865e6b4c37ac005f5f24c3d6e1de7c5bac

--- libkeyutils/keyutils.cpp
+++ libkeyutils/keyutils.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f636de045b2ad402cfaa6521a6bb434ef0bb7fdede7c69d4e0638e5f236ad00
-size 3076
+oid sha256:73d8403a808b5315c917917d0fc68a37a8db73d4d52eb828f9a73bb3cf424fd9
+size 2841
