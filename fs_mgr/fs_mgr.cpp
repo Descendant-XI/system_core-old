fd6c13e57ece74330b19b61948d312add6a809d8
Randall Huang <huangrandall@google.com>
2020-06-12 00:07:53 +0000

    Try to recover corrupted ext4 /data with backup superblock
    If the superblock of /data is corrupted, fs_mgr would skip check_fs.
    But, e2fsck actually may reference backup superblock to recover
    the filesystem. This fix gives fs_mgr second chance to fix corrupted
    ext4 /data.
    
    Bug: 156200421
    Test: boot with corrupted ext4 superblock
    
    Signed-off-by: Randall Huang <huangrandall@google.com>
    Change-Id: Ieb89e7274d772962fe37927dcd62567dd5aa4657
    (cherry picked from commit 72abd7b246f733e5f417e2bc529c9b482c4a778b)

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a3763dd6e729255d342cce01d4f2466384d019d58b5ce610a360188d64c5502
-size 82693
+oid sha256:90ba3c6287423e932484734df691a516eda0ccc62eea93d8cc61141ca33c2c5b
+size 83965
