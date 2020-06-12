ee196fd8f0645a2e696c1b101726c5054519b332
Paul Lawrence <paullawrence@google.com>
2020-06-12 07:32:23 -0700

    Don't call block checkpoint functions above dm-default-key
    Bug: 156225476
    Test: Build for f2fs and ext4 device, make sure checkpoints roll back
    and commit
    
    Merged-In: I7a772ff712dec9e69df175de840d69d296c65923
    Change-Id: I7a772ff712dec9e69df175de840d69d296c65923

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c55a5e0faa1becbfa38597476378580be4f0285482207b94f71fa3f730e77ef9
-size 81133
+oid sha256:448f694d7ac38eb175804cfe9968a5b3b25f9d3783989fc7fb3b85f059304d64
+size 81400
