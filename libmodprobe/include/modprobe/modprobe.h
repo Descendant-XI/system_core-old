77e66791ac246f29ebf1f833b9170eaf7f73ebb7
Mark Salyzyn <salyzyn@google.com>
2020-06-15 11:51:59 -0700

    modprobe: Use more inclusive language for modprobe and libmodprobe
    blacklist is replaced with blocklist.
    
    Test: none
    Bug: 151950334
    Merged-In: I59f9fde5900b9aee82aca1eab4a6ded3d136063b
    Change-Id: I59f9fde5900b9aee82aca1eab4a6ded3d136063b

--- libmodprobe/include/modprobe/modprobe.h
+++ libmodprobe/include/modprobe/modprobe.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d369785124c5892ea41a05e9826ee2ab337a751cb9c2734ac176c50e7c1eba3
+oid sha256:399ca3a67d7106c8a1ef713107e8e98dce5e87d1fe514557ed4f5a27a553ef40
 size 3301
