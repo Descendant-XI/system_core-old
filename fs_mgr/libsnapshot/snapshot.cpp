8c9cf385b82fa385af3bc6fbab08c1fffe2eb3dc
Yifan Hong <elsk@google.com>
2020-07-07 13:23:07 -0700

    Try locking after mounting metadata
    In rescue mode, if /metadata is mounted but /metadata/ota does
    not exist, immediately unmount /metadata and fallback to the code
    path when /metadata is not mounted; that is, old partitions are
    overwritten.
    
    Test: in recovery, select wipe then immediately sideload
    Bug: 160457903
    Change-Id: I412d62b7005c81a7126106edc471622e6a7ef813

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0beb052c4c0502da00336250a2da40124d4920e7165e6a3fff16db43ff1a0c91
-size 107043
+oid sha256:5e22884c592c9badd8cc57d7a7ae0680a2bc578c29f115f676498fba2fe2ff0d
+size 107691
