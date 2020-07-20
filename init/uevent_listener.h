4c6a716c05fbca6745c75412e62efcbe2c09d6c2
P.Adarsh Reddy <quic_padarshr@quicinc.com>
2020-07-21 03:09:28 +0530

    Ueventd: Fix a corner case in ReadUevent() that triggers duplicate firmware loading.
    Presently, within ReadUevent(), true is returned for a successful case
    as well as for the case where we read an invalid uevent (overflowed
    buffer)where the Uevent object is not cleared, and the caller calls the
    callback (with the earlier stored uevent object),leading to duplicate
    firmware loading.
    
    Uevent uevent;
    while (ReadUevent(&uevent)) {
      if (callback(uevent) == ListenerAction::kStop) return;
    }
    
    Scenario:
    1. Proper Uevent received and callback is called (firmware loading
       is triggered).
    2. Overflowed uevent is received as part of the same ReadUevent session,
       ReadUevent() returns true, but the uevent object is not cleared and
       still has earlier event values.
    3. Callback is called again, leading to duplicate firmware load.
    
    Handle it by adding explicit return codes to let the caller know if the
    uevent read is invalid, and the caller can ignore it and read
    further pending uevents.
    
    Bug: 161580785
    Test: in AOSP
    Merged-In: I09e80052337fd1495b968dc02ecff5ceb683da18
    Change-Id: I09e80052337fd1495b968dc02ecff5ceb683da18
    (cherry picked from commit a86bf05a012e871c41b4676b0cc947b1baea194b)

--- init/uevent_listener.h
+++ init/uevent_listener.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eda2d9b811db84428dfe60f9c9c6ba6afa0df053cc4f2f7dd814603b8d1dd15
-size 1886
+oid sha256:005ab7ec2a5d4b6662c91042c8003b4a93c9fc7a7eda3b555ee90ae74811363e
+size 2133
