ece058f7c752f2a558783d24d6e96c8b858e2848
Eric Biggers <ebiggers@google.com>
2020-08-04 11:11:13 -0700

    fs_mgr: Revert "Try to recover corrupted ext4 /data with backup superblock"
    This reverts commit 72abd7b246f733e5f417e2bc529c9b482c4a778b
    (change Ia39af3340c0e241f62557b7c2cc8b800443342f9).
    
    When vold enables either FDE or metadata encryption, it encrypts the
    filesystem in-place.  Unfortunately, due to a bug, for ext4 filesystems
    it hasn't been encrypting the backup superblocks.
    
    Also, in read_ext4_superblock(), the check for
    StartsWith(blk_device, "/dev/block/dm-") can return true even if the
    encryption mapping hasn't been added yet, since when a GSI image is
    booted the userdata block device is a logical volume using dm-linear.
    
    The result is that read_ext4_superblock() can recognize a backup
    superblock when the encryption mapping hasn't been added yet, causing
    e2fsck to run without the encryption mapping and corrupt the filesystem.
    
    https://android-review.googlesource.com/c/platform/system/vold/+/1385029
    will fix this for new or factory-reset devices.  However, there probably
    are many existing devices that already have their backup superblocks
    unencrypted.  Therefore, the EncryptInPlace fix isn't enough and we have
    to revert the change that started using the backup superblocks too.
    
    Bug: 161871210
    Bug: 162479411
    Change-Id: I279f84c072bc6c8d3e251a5e95c78f8d6c0d50ba
    Merged-In: I279f84c072bc6c8d3e251a5e95c78f8d6c0d50ba
    (cherry picked from dc3e897a881291c7803dd1fc517915952396dd98)

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7432ee9f9510452a655570a699b1326c1e53caaed1d8ed73a31dfda41e2d9b57
-size 84228
+oid sha256:a44fe67c8f2302eedaa945f17531c2c7cc16611db90132876abdec258ecac366
+size 82960
