eec2bb36181f27f2102303e649f3630a87e60603
Yifan Hong <elsk@google.com>
2020-08-06 16:12:37 -0700

    libsnapshot_test: skip global setup on non-VAB devices.
    On non-VAB devices, fake super image creation may fail because it
    may not support f2fs file pinning.
    
    Also changes global setup to a testing::Environment object because it is
    the recommended way in gtest's guide. SnapshotTestEnvironement::TearDown
    is automatically executed before RUN_ALL_TEST returns.
    
    Test: vts_libsnapshot_test
    Test: make IsVirtualAbEnabled() return false, then all tests are
    skipped.
    
    Bug: 162557082
    Change-Id: I1382f9a4ddec146fa2d8cfb21ac66ca22a2e110f
    (cherry picked from commit a7c00a0943411596fab0632bbbd52f3d7e2e68ba)
    Merged-In: I1382f9a4ddec146fa2d8cfb21ac66ca22a2e110f

--- fs_mgr/libsnapshot/test_helpers.cpp
+++ fs_mgr/libsnapshot/test_helpers.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c23234048c9d8378aee8a52911977e37af9a888cd4b37ca4a27cf558c99c1191
-size 8435
+oid sha256:22884d40aa188731ab13aa80801d0770cbf41cd4a34229d23bd31ca6e456ee36
+size 8578
