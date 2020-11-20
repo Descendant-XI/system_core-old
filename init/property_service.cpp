5d964c311e250364c3f003fab42648ebf43981b9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 20:33:21 -0300

    init: Only set safetynet props if not eng build
    Change-Id: Ic07539b4a7a97316720defd000425d1b6d15fd67
    
    Former-commit-id: 9d4ca9403943feecd6f902e69d581aad3ee84839
    Change-Id: Ic34d95c23afd8caf95c7b2a2517650dbf116fdde

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022654c5b989ed8bb3f48269ea88f6605e157ecfff53d2e7801a794bc8b4ae66
-size 45409
+oid sha256:ab00cd8c50b83f393b8d984152b607123e72fc2552ce19a415aa1c8c0f0555e1
+size 45564
