462ad3a7c398dabd611b4f45c05870237de29329
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:58:40 -0300

    init: Allow forcing custom build fingerprint [2/2]
    Change-Id: Iae1db7ad74d3cb14c2cd4405f7ede13a4444bc5a
    
    Former-commit-id: e139f1e09832c5117cddf551240442a8528c5c99

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e5c4c72b5999bc9271610bb209797ee585c94d750e1b4b044b2ccb93799246a
-size 44777
+oid sha256:022654c5b989ed8bb3f48269ea88f6605e157ecfff53d2e7801a794bc8b4ae66
+size 45409
